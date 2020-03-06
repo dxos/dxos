aea72089ea959ba7b7426849c503b148cc5f5378
Max Fierro <elmasse@gmail.com>
2020-03-06 13:17:33 -0300

    Add EchoModel.

--- packages/echodb/src/index.js
+++ packages/echodb/src/index.js
@@ -2,6 +2,7 @@
 // Copyright 2020 DxOS
 //
 
+export * from './model';
 export * from './mutation';
 export * from './object';
 export * from './util';
---
+++ packages/echodb/src/model.js
@@ -0,0 +1,67 @@
+//
+// Copyright 2020 DxOS
+//
+
+import { Model } from '@dxos/data-client';
+
+import { MutationUtil } from './mutation';
+import { ObjectModel } from './object';
+import { createId, fromObject, parseId } from './util';
+
+/**
+ * Stream adapter.
+ */
+// TODO(burdon): Move to @dxos/echodb.
+export class EchoModel extends Model {
+
+  _model = new ObjectModel();
+
+  getObjectsByType(type) {
+    return this._model.getObjectsByType(type);
+  }
+
+  createItem(type, properties) {
+    const mutations = fromObject({
+      id: createId(type),
+      properties
+    });
+
+    // TODO(burdon): Create single message.
+    mutations.forEach((mutation) => {
+      this.appendMessage({
+        __type_url: type,
+        ...mutation
+      });
+    });
+  }
+
+  updateItem(id, properties) {
+    const { type } = parseId(id);
+    const mutations = fromObject({
+      id,
+      properties
+    });
+
+    // TODO(burdon): Create single message.
+    mutations.forEach((mutation) => {
+      this.appendMessage({
+        __type_url: type,
+        ...mutation
+      });
+    });
+  }
+
+  deleteItem(id) {
+    const { type } = parseId(id);
+    const mutation = MutationUtil.createMessage(id, undefined, { deleted: true });
+
+    this.appendMessage({
+      __type_url: type,
+      ...mutation
+    });
+  }
+
+  onUpdate(messages) {
+    this._model.applyMutations(messages);
+  }
+}
