a57d4714d2e7ae07cb9fc56b53430d2941ccb910
richburdon <rich.burdon@gmail.com>
2020-05-19 22:00:45 -0400

    Testing.

--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -2,13 +2,14 @@
 
 #
 # Build static site for deploy by Netlify, etc.
+# TODO(burdon): Broken: https://github.com/storybookjs/storybook/issues/1291
 #
 
 yarn build:storybook
 
 mkdir -p ./out
 
-for i in "spore" "globe"
+for i in "canvas" "globe" "spore" "widgets"
 do
   cp -R ./packages/$i/out ./out/$i
 done
