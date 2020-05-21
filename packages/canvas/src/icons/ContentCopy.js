528ddbfc5e255cc2797156b51af1ecd1f4de4e66
rzadp <przemek@ethworks.io>
2020-05-21 10:58:37 +0200

    Typo

--- packages/canvas/src/icons/ContentCopy.js
+++ packages/canvas/src/icons/ContentCopy.js
@@ -8,10 +8,10 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 /**
  * https://www.materialui.co/icons?s=content-copy
  */
-const ConentCopy = (props) => (
+const ContentCopy = (props) => (
   <SvgIcon {...props} viewBox="0 0 48 48">
     <path d="M32 2H8C5.79 2 4 3.79 4 6v28h4V6h24V2zm6 8H16c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h22c2.21 0 4-1.79 4-4V14c0-2.21-1.79-4-4-4zm0 32H16V14h22v28z"/>
   </SvgIcon>
 );
 
-export default ConentCopy;
+export default ContentCopy;
