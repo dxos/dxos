a67399070c0fa1fabe9ef25970add9a41872b078
Rich Burdon <rich.burdon@gmail.com>
2020-07-09 21:36:58 -0400

    Tests passing.

--- packages/echo-db/src/demo.test.ts
+++ packages/echo-db/src/demo.test.ts
@@ -8,21 +8,51 @@ import ram from 'random-access-memory';
 
 import { FeedStore } from '@dxos/feed-store';
 
+import { ObjectStore, fromObject } from './object-store';
+
+// TODO(burdon): Deprecate FeedStore.create async constructor, discoveryKey, etc.
+// TODO(burdon): Item streams
+// TODO(burdon): Protobuf, encoding.
+
+class ObjectModel {
+
+  _store = new ObjectStore();
+}
+
+class ModelFactory {
+
+  // TODO(burdon): Shim?
+  _feedStore: any;
+
+  constructor(feedStore: any) {
+    this._feedStore = feedStore;
+  }
+
+  createModel() {
+    const model = new ObjectModel();
+    return model;
+  }
+}
+
 test('model', async done => {
-  // TODO(burdon): Remove async constructor.
   const feedStore = new FeedStore(ram);
   await feedStore.open();
 
+  // TODO(burdon): Pass write stream to modelfactory?
   const feed = await feedStore.openFeed('test');
 
-  const stream = feedStore.createReadStream();
+  // const modelFactory = new ModelFactory(feedStore);
+  // const model = modelFactory.createModel();
+
+  // TODO(burdon): Indexer should consume feed? (Manage live collection of all models?)
+  const stream = feedStore.createReadStream({ live: true });
   stream.on('data', (data: any) => {
-    console.log(':::::', data);
     done();
   });
 
-  // TODO(burdon): Protobuf, encoding.
-  feed.append(JSON.stringify({ message: 'test' }));
-});
+  // TODO(burdon): Use model to write.
+  feed.append(JSON.stringify(fromObject({ id: 'object-1', properties: { foo: 100 } })));
+  feed.append(JSON.stringify(fromObject({ id: 'object-1', properties: { bar: 200 } })));
 
-// TODO(burdon): Item streams
+  // TODO(burdon): Open stream and replay?
+});
