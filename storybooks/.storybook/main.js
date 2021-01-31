10d5f9411de1b181059808684542f7f8ef35b5fd
richburdon <rich.burdon@gmail.com>
2021-01-31 18:21:27 -0500

    Storybooks.

--- storybooks/.storybook/main.js
+++ storybooks/.storybook/main.js
@@ -4,12 +4,11 @@
 
 module.exports = {
   stories: [
-    '../../packages/canvas/**/*.stories.js',
-    '../../packages/core/**/*.stories.js',
-    '../../packages/experimental/**/*.stories.js',
-    '../../packages/globe/**/*.stories.js',
-    '../../packages/isometric/**/*.stories.js',
-    '../../packages/spore/**/*.stories.js',
-    // '../../packages/widgets/**/*.stories.js',
-  ],
+    '../../packages/gem-canvas/**/*.stories.js',
+    '../../packages/gem-core/**/*.stories.tsx',
+    '../../packages/gem-experimental/**/*.stories.tsx',
+    '../../packages/gem-globe/**/*.stories.js',
+    '../../packages/gem-spore/**/*.stories.tsx',
+    '../../packages/gem-widgets/**/*.stories.tsx'
+  ]
 };
