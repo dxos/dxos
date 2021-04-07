d39cc9792fac9b90cbda4821800e954a9baa9c8c
Dmytro Maretskyi <maretskii@gmail.com>
2021-04-07 15:11:17 +0200

    Enable eslint

--- echo/echo-protocol/src/proto/gen/dxos.ts
+++ echo/echo-protocol/src/proto/gen/dxos.ts
@@ -1,8 +1,8 @@
 import { codec, Message } from "@dxos/credentials";
 import { PublicKey } from "@dxos/crypto";
 import { Timeframe } from "../../spacetime";
-import * as dxos_echo from "./dxos/echo";
 import * as dxos_echo_testing from "./dxos/echo/testing";
+import * as dxos_echo from "./dxos/echo";
 import * as dxos_echo_snapshot from "./dxos/echo/snapshot";
 import * as google_protobuf from "./google/protobuf";
 export interface FeedMessage {
