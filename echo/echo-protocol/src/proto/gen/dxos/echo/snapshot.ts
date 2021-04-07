2ebb49c9152a3a78ce6a97560be6dcec368b9916
Dmytro Maretskyi <maretskii@gmail.com>
2021-04-07 15:13:58 +0200

    Regen

--- echo/echo-protocol/src/proto/gen/dxos/echo/snapshot.ts
+++ echo/echo-protocol/src/proto/gen/dxos/echo/snapshot.ts
@@ -2,8 +2,8 @@ import { codec, Message } from "@dxos/credentials";
 import { PublicKey } from "@dxos/crypto";
 import { Timeframe } from "../../../../spacetime";
 import * as dxos from "../../dxos";
-import * as dxos_echo from "../echo";
 import * as dxos_echo_testing from "./testing";
+import * as dxos_echo from "../echo";
 import * as google_protobuf from "../../google/protobuf";
 export interface PartySnapshot {
     partyKey?: Uint8Array;
