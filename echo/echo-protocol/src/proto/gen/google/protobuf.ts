151e60ec2231563028c3fa90932b8c05ac630f5b
Dmytro Maretskyi <maretskii@gmail.com>
2021-04-09 15:54:58 +0200

    Hook echo-protocol package

--- echo/echo-protocol/src/proto/gen/google/protobuf.ts
+++ echo/echo-protocol/src/proto/gen/google/protobuf.ts
@@ -3,9 +3,9 @@ import { PublicKey } from "@dxos/crypto";
 import { Timeframe } from "../../../spacetime";
 import * as dxos from "../dxos";
 import * as dxos_echo from "../dxos/echo";
-import * as dxos_echo_remote from "../dxos/echo/remote";
 import * as dxos_echo_testing from "../dxos/echo/testing";
 import * as dxos_echo_snapshot from "../dxos/echo/snapshot";
+import * as dxos_echo_remote from "../dxos/echo/remote";
 export interface Any {
     type_url?: string;
     value?: Uint8Array;
