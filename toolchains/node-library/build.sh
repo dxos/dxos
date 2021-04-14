7a49ba51731c3e10d9b6d3b8122dbe988984eb02
Dmytro Maretskyi <maretskii@gmail.com>
2021-04-14 18:36:29 +0200

    Extract eslint config

--- toolchains/node-library/build.sh
+++ toolchains/node-library/build.sh
@@ -1,13 +1,18 @@
 set -e
 
+TOOLCHAIN_DIR=$(dirname "$0")
+
+PATH="$TOOLCHAIN_DIR/node_modules/.bin:$PATH"
+
 # Generate protobuf definitions
-mkdir -p src/proto/gen && build-protobuf src/proto/defs/dxos.proto -s src/proto/substitutions.ts -o src/proto/gen
+mkdir -p src/proto/gen
+build-protobuf src/proto/defs/dxos.proto -s src/proto/substitutions.ts -o src/proto/gen
 
 # Typescript build
 tsc
 
 # Lint
-eslint 'src/**/*.ts'
+eslint --config "$TOOLCHAIN_DIR/.eslintrc.js" 'src/**/*.ts'
 
 # Test
-jest 'src/**/*.test.ts'
+# jest 'src/**/*.test.ts'
