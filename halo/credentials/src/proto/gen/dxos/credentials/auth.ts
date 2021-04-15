7562e480a979910a41ac98d7eae92f9d1a33c860
rzadp <roopert7@gmail.com>
2021-04-15 13:55:22 +0200

    Regenerate gen

--- halo/credentials/src/proto/gen/dxos/credentials/auth.ts
+++ halo/credentials/src/proto/gen/dxos/credentials/auth.ts
@@ -2,10 +2,10 @@ import { Schema as CodecSchema } from "@dxos/codec-protobuf";
 import { PublicKey } from "@dxos/crypto";
 import { SecretKey } from "../../../../keys";
 import { DecodedAny } from "../../../any";
-import * as dxos_credentials_identity from "./identity";
-import * as dxos_credentials_greet from "./greet";
-import * as dxos_credentials_keys from "./keys";
 import * as dxos_credentials from "../credentials";
+import * as dxos_credentials_greet from "./greet";
+import * as dxos_credentials_identity from "./identity";
+import * as dxos_credentials_keys from "./keys";
 import * as dxos_credentials_party from "./party";
 import * as google_protobuf from "../../google/protobuf";
 export interface Auth {
--- halo/credentials/src/proto/gen/dxos/credentials/party.ts
+++ halo/credentials/src/proto/gen/dxos/credentials/party.ts
@@ -2,11 +2,11 @@ import { Schema as CodecSchema } from "@dxos/codec-protobuf";
 import { PublicKey } from "@dxos/crypto";
 import { SecretKey } from "../../../../keys";
 import { DecodedAny } from "../../../any";
-import * as dxos_credentials_identity from "./identity";
-import * as dxos_credentials_greet from "./greet";
-import * as dxos_credentials_keys from "./keys";
-import * as dxos_credentials_auth from "./auth";
 import * as dxos_credentials from "../credentials";
+import * as dxos_credentials_auth from "./auth";
+import * as dxos_credentials_greet from "./greet";
+import * as dxos_credentials_identity from "./identity";
+import * as dxos_credentials_keys from "./keys";
 import * as google_protobuf from "../../google/protobuf";
 export interface PartyCredential {
     type?: PartyCredential.Type;
