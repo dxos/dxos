c01377f441e1308b1c80ec8591cd33dacce68bdf
rzadp <roopert7@gmail.com>
2021-04-21 15:42:27 +0200

    Reproduce the issue

--- codec-protobuf/codec-protobuf/test/codec.test.ts
+++ codec-protobuf/codec-protobuf/test/codec.test.ts
@@ -1,18 +1,23 @@
-import { Schema } from "../src/codec"
+//
+// Copyright 2021 DXOS.org
+//
+
+import { join } from 'path';
 import * as pb from 'protobufjs';
-import { anySubstitutions } from "../src";
-import { join } from "path";
+
+import { anySubstitutions } from '../src';
+import { Schema } from '../src/codec';
 
 describe('extending protobuf', () => {
   it('extends proto with another file', async () => {
-    const anyProto = await pb.load(join(__dirname, './any.proto'))
-    const anotherProto = await pb.load(join(__dirname, './another.proto'))
+    const anyProto = await pb.load(join(__dirname, './any.proto'));
+    const anotherProto = await pb.load(join(__dirname, './another.proto'));
 
     const schema = new Schema(anyProto, anySubstitutions);
     const codec = schema.tryGetCodecForType('dxos.test.any.Wrapper');
-    
-    codec.addJson(anotherProto.toJSON())
-    
+
+    codec.addJson(anotherProto.toJSON());
+
     const data = {
       payload: {
         __type_url: 'dxos.test.another.AnotherMessage',
@@ -20,9 +25,31 @@ describe('extending protobuf', () => {
       }
     };
 
-    const encoded = codec.encode(data)
-    const decoded = codec.decode(encoded)
+    const encoded = codec.encode(data);
+    const decoded = codec.decode(encoded);
 
-    expect(decoded).toEqual(data)
-  })
-})
+    expect(decoded).toEqual(data);
+  });
+
+  it('Extends proto with duplicate keys', async () => {
+    const anyProto = await pb.load(join(__dirname, './any.proto'));
+    const anotherProtoWithAny = await pb.load(join(__dirname, './anotherWithAny.proto'));
+
+    const schema = new Schema(anyProto, anySubstitutions);
+    const codec = schema.tryGetCodecForType('dxos.test.any.Wrapper');
+
+    codec.addJson(anotherProtoWithAny.toJSON());
+
+    const data = {
+      payload: {
+        __type_url: 'dxos.test.another.AnotherMessage',
+        foo: 'foo'
+      }
+    };
+
+    const encoded = codec.encode(data);
+    const decoded = codec.decode(encoded);
+
+    expect(decoded).toEqual(data);
+  });
+});
