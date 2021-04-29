ca9b045121172dfac170bf288560e1db0be1e3af
Rich Burdon <rich.burdon@gmail.com>
2021-04-29 18:46:48 -0400

    Start of tutorials.

---
+++ echo/echo-demo/stories/tutorials.story.tsx
@@ -0,0 +1,29 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import { withKnobs } from '@storybook/addon-knobs';
+import React from 'react';
+
+export default {
+  title: 'Tutorials',
+  decorators: [withKnobs]
+};
+
+export const Stage1 = () => {
+  return (
+    <div>Stage1</div>
+  );
+};
+
+export const Stage2 = () => {
+  return (
+    <div>Stage2</div>
+  );
+};
+
+export const Stage3 = () => {
+  return (
+    <div>Stage3</div>
+  );
+};
