ae3d5f67373dc6733b6f83448689bf16bb2eb24b
przemek <przemek@ethworks.io>
2021-04-30 15:13:19 +0000

    Change structure of the monorepo

--- echo/text-model/src/proto/gen/dxos/echo/text.ts
+++
@@ -1,7 +0,0 @@
-export interface Mutation {
-    update?: Uint8Array;
-    clientId?: number;
-}
-export interface Snapshot {
-    data?: Uint8Array;
-}
--- echo/text-model/src/proto/gen/index.ts
+++
@@ -1,8 +0,0 @@
-import { Schema } from "@dxos/codec-protobuf";
-import * as dxos_echo_text from "./dxos/echo/text";
-export interface TYPES {
-    "dxos.echo.text.Mutation": dxos_echo_text.Mutation;
-    "dxos.echo.text.Snapshot": dxos_echo_text.Snapshot;
-}
-export const schemaJson = JSON.parse("{\"nested\":{\"dxos\":{\"nested\":{\"echo\":{\"nested\":{\"text\":{\"nested\":{\"Mutation\":{\"fields\":{\"update\":{\"type\":\"bytes\",\"id\":1},\"clientId\":{\"type\":\"int32\",\"id\":2}}},\"Snapshot\":{\"fields\":{\"data\":{\"type\":\"bytes\",\"id\":1}}}}}}}}}}}");
-export const schema = Schema.fromJson<TYPES>(schemaJson);
