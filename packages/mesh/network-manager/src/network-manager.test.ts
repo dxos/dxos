698444a7e60d5ab156cb0d55f042a36e83c4e6b7
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-04-30 19:52:57 +0200

    Fix echo tests (finally) (#38)
    * CR
    
    * Fix swarm test
    
    * Add test with many in-memory swarms
    
    * Lint
    
    * Fix network manager
    
    * Lint
    
    * Unskip party-manager tests
    
    * Unskip more tests
    
    * Unskip Contacts test

--- packages/mesh/network-manager/src/network-manager.test.ts
+++ packages/mesh/network-manager/src/network-manager.test.ts
@@ -5,9 +5,10 @@
 import { expect, mockFn } from 'earljs';
 import waitForExpect from 'wait-for-expect';
 
-import { Event, sleep } from '@dxos/async';
+import { Event, latch, sleep } from '@dxos/async';
 import { PublicKey } from '@dxos/crypto';
 import { Protocol } from '@dxos/protocol';
+import { range } from '@dxos/util';
 
 import { NetworkManager } from './network-manager';
 import { TestProtocolPlugin, testProtocolProvider } from './testing/test-protocol';
@@ -188,6 +189,42 @@ describe('Network manager', () => {
         expect(mockReceiveA).toHaveBeenCalledWith([expect.a(Protocol), 'Foo A']);
         expect(mockReceiveB).toHaveBeenCalledWith([expect.a(Protocol), 'Foo B']);
       });
+    });
+
+    test('large amount of peers and connections', async () => {
+      const numTopics = 5;
+      const peersPerTopic = 5;
+
+      await Promise.all(range(numTopics).map(async () => {
+        const topic = PublicKey.random();
+
+        await Promise.all(range(peersPerTopic).map(async () => {
+          const peerId = PublicKey.random();
+          const { plugin } = await createPeer({ topic, peerId, inMemory: true });
+
+          const [done, pongReceived] = latch(peersPerTopic - 1);
+
+          plugin.on('connect', async (protocol: Protocol) => {
+            const remoteId = PublicKey.from(protocol.getSession().peerId);
+
+            plugin.send(remoteId.asBuffer(), 'ping');
+          });
+
+          plugin.on('receive', (protocol, data) => {
+            const remoteId = PublicKey.from(protocol.getSession().peerId);
+
+            if (data === 'ping') {
+              plugin.send(remoteId.asBuffer(), 'pong');
+            } else if (data === 'pong') {
+              pongReceived();
+            } else {
+              throw new Error(`Invalid message: ${data}`);
+            }
+          });
+
+          await done;
+        }));
+      }));
     });
   });
 });
