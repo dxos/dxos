36e03efadd03d6e944279d4cd68794240fe15682
Przemyslaw Rzad <przemek@ethworks.io>
2021-05-06 16:27:31 +0200

    Echo demo storybooks - 3 stages (#46)

--- packages/echo/echo-demo/stories/tutorials.story.tsx
+++ packages/echo/echo-demo/stories/tutorials.story.tsx
@@ -3,27 +3,127 @@
 //
 
 import { withKnobs } from '@storybook/addon-knobs';
+import faker from 'faker';
 import React from 'react';
+
+import { Button } from '@material-ui/core';
+
+import { createKeyPair } from '@dxos/crypto';
+import { Party } from '@dxos/echo-db';
+import { ObjectModel } from '@dxos/object-model';
+import { useClient, useItems, useParties, useProfile } from '@dxos/react-client';
+
+import { ClientInitializer } from './ClientInitializer';
 
 export default {
   title: 'Tutorials',
   decorators: [withKnobs]
 };
 
-export const Stage1 = () => {
+const Stage1Consumer = () => {
+  const client = useClient();
+  const profile = useProfile();
+
+  const handleCreateProfile = async () => {
+    await client.createProfile({ ...createKeyPair(), username: faker.name.firstName() });
+  };
+
+  if (!profile?.username) {
+    return (
+      <Button variant="contained" onClick={handleCreateProfile}>Create profile</Button>
+    );
+  }
+
   return (
-    <div>Stage1</div>
+    <p>Welcome, {profile.username}</p>
   );
+};
+
+const PartyTitle = ({ party }: {party: Party}) => {
+  return <>{party.title ?? party.key.toHex()}</>;
+};
+
+const PartyItemCount = ({ party }: {party: Party}) => {
+  const items = useItems({ partyKey: party.key });
+  return <><PartyTitle party={party}/> has {items.length} items.</>;
+};
+
+interface Stage2ConsumerProps {
+  PartyDetails?: ({ party: Party }) => JSX.Element
+}
+
+const Stage2Consumer = ({ PartyDetails = PartyTitle }: Stage2ConsumerProps) => {
+  const client = useClient();
+  const profile = useProfile();
+  const parties = useParties();
+
+  const handleCreateParty = async () => {
+    const party = await client.echo.createParty();
+    await party.setTitle(faker.company.companyName());
+  };
+
+  if (!profile?.username) {
+    return null;
+  }
+
+  return (<>
+    {parties?.length ? (
+      <ul>
+        {parties.map(party => (
+          <li key={party.key.toHex()}><PartyDetails party={party}/></li>
+        ))}
+      </ul>
+    ) : null}
+    <Button variant="contained" onClick={handleCreateParty}>Create party</Button>
+  </>);
+};
+
+const Stage3Consumer = () => {
+  const parties = useParties();
+
+  const handleCreateItem = async (party: Party) => {
+    await party.database.createItem({ model: ObjectModel });
+  };
+
+  return (<>
+    <ul>
+      {parties?.map(party => (<>
+        <li>
+          {party.title ?? party.key.toHex()}
+          <Button variant="text" onClick={() => handleCreateItem(party)}>Add item</Button>
+        </li>
+      </>))}
+    </ul>
+
+  </>);
+};
+
+export const Stage1 = () => {
+  return (<>
+    <h3>Uses Client</h3>
+    <ClientInitializer>
+      <Stage1Consumer/>
+    </ClientInitializer>
+  </>);
 };
 
 export const Stage2 = () => {
-  return (
-    <div>Stage2</div>
-  );
+  return (<>
+    <h3>Creates a party</h3>
+    <ClientInitializer>
+      <Stage1Consumer/>
+      <Stage2Consumer/>
+    </ClientInitializer>
+  </>);
 };
 
 export const Stage3 = () => {
-  return (
-    <div>Stage3</div>
-  );
+  return (<>
+    <h3>Adds items to parties</h3>
+    <ClientInitializer>
+      <Stage1Consumer/>
+      <Stage2Consumer PartyDetails={PartyItemCount}/>
+      <Stage3Consumer />
+    </ClientInitializer>
+  </>);
 };
