b23745644e17cca02dad9b800fa866271d58c105
Rich Burdon <rich.burdon@gmail.com>
2021-05-08 19:24:48 -0400

    Updated create dialog.

--- packages/echo/echo-demo/stories/demos/search.story.tsx
+++ packages/echo/echo-demo/stories/demos/search.story.tsx
@@ -6,16 +6,16 @@ import faker from 'faker';
 import React, { useState } from 'react';
 
 import {
+  Box,
   Button,
   Chip,
-  CircularProgress,
-  Dialog, DialogActions,
+  Dialog,
+  DialogActions,
   DialogContent,
   DialogTitle,
   Fab,
   IconButton,
-  ListItem,
-  ListItemIcon,
+  LinearProgress,
   TextField,
   Toolbar,
   Typography
@@ -33,7 +33,16 @@ import GridIcon from '@material-ui/icons/ViewModule';
 import ProjectIcon from '@material-ui/icons/WorkOutline';
 
 import { Item, Party } from '@dxos/echo-db';
-import { OBJECT_ORG, OBJECT_PERSON, OBJECT_PROJECT, LINK_PROJECT, LINK_EMPLOYEE, labels, Generator } from '@dxos/echo-testing';
+import {
+  OBJECT_ORG,
+  OBJECT_PERSON,
+  OBJECT_PROJECT,
+  LINK_PROJECT,
+  LINK_EMPLOYEE,
+  labels,
+  Generator,
+  OBJECT_TASK
+} from '@dxos/echo-testing';
 import { ObjectModel } from '@dxos/object-model';
 import { useSelection, searchSelector } from '@dxos/react-client';
 
@@ -109,6 +118,9 @@ const useStyles = makeStyles(theme => ({
     bottom: 20,
     left: 'auto',
     position: 'fixed'
+  },
+  field: {
+    marginBottom: theme.spacing(2)
   }
 }));
 
@@ -117,14 +129,15 @@ const VIEW_CARDS = 2;
 const VIEW_GRID = 3;
 const VIEW_GRAPH = 4;
 
-const icons = {
+const TYPES = {
   [OBJECT_ORG]: OrgIcon,
   [OBJECT_PERSON]: PersonIcon,
-  [OBJECT_PROJECT]: ProjectIcon
+  [OBJECT_PROJECT]: ProjectIcon,
+  [OBJECT_TASK]: DefaultIcon
 };
 
 const Icon = ({ item: { type } }: {item: Item<any>}) => {
-  const Icon = (icons as any)[type ?? ''] || DefaultIcon;
+  const Icon = (TYPES as any)[type ?? ''] || DefaultIcon;
   if (!Icon) {
     return null;
   }
@@ -212,6 +225,65 @@ const cardAdapter = (classes: ReturnType<typeof useStyles>): CardAdapter => ({
   }
 });
 
+// TODO(burdon): Use ToggleButton, ToggleButtonGroup (react 5; currently incompatible with storybook).
+const ToggleGroup = ({ type, onChange }: { type: string, onChange: (key: string) => void }) => {
+  return (
+    <div>
+      {Object.entries(TYPES).map(([key, Icon]) => (
+        <IconButton key={key} disabled={key === type} onClick={() => onChange(key)}>
+          <Icon />
+        </IconButton>
+      ))}
+    </div>
+  );
+};
+
+interface ItemDialogProperties {
+  open: boolean
+  handleCreate?: ({ type, name }: { type: string, name: string }) => void
+  handleClose?: () => void
+}
+
+// TODO(burdon): Use in card view (with links).
+const ItemDialog = ({ open, handleCreate, handleClose }: ItemDialogProperties) => {
+  const classes = useStyles();
+  const [type, setType] = useState(OBJECT_ORG);
+  const [name, setName] = useState('');
+
+  const handleCreateItem = () => {
+    if (handleCreate && name.trim().length) {
+      handleCreate({ type, name });
+      setName('');
+    }
+  };
+
+  return (
+    <Dialog open={open} fullWidth maxWidth='sm'>
+      <DialogTitle>Create Item</DialogTitle>
+      <DialogContent>
+        <Box m={2} flexDirection='column'>
+          <div className={classes.field}>
+            <ToggleGroup type={type} onChange={type => setType(type)} />
+          </div>
+
+          <TextField
+            autoFocus
+            fullWidth
+            label='Name'
+            value={name}
+            onChange={event => setName(event.currentTarget.value)}
+            variant='outlined'
+          />
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button color='primary' variant='contained' onClick={handleCreateItem}>Create</Button>
+        <Button color='secondary' onClick={handleClose}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
 interface HomeProps {
   onCreate: () => void,
   onJoin: (invitationCode: string) => void
@@ -234,10 +306,14 @@ const Home = ({ onCreate, onJoin }: HomeProps) => {
           label='Invitation code'
           spellCheck={false}
         />
+
+        <div style={{ height: 8, marginTop: 16 }}>
+          {inProgress && <LinearProgress />}
+        </div>
+
+        {error && <Typography>{String(error)}</Typography>}
       </DialogContent>
       <DialogActions>
-        {error && <p>{String(error)}</p>}
-        {inProgress && <CircularProgress />}
         <Button
           color='secondary'
           variant='contained'
@@ -296,22 +372,21 @@ const Main = ({ party, generator }: MainProps) => {
     </IconButton>
   );
 
-  const [showAdd, setShowAdd] = useState(false);
-  const [orgName, setOrgName] = useState('');
+  const [showCreate, setShowCreate] = useState(false);
 
-  async function handleAdd () {
+  const handleCreate = async ({ type, name }: { type: string, name: string }) => {
     await generator.database.createItem({
       model: ObjectModel,
-      type: OBJECT_ORG,
-      props: {
-        name: orgName,
+      type,
+      props: { // TODO(burdon): Factor out generator.
+        name: name,
         description: faker.lorem.sentence(),
         labels: faker.random.arrayElements(labels, faker.random.number({ min: 0, max: 3 }))
       }
     });
-    setShowAdd(false);
-    setOrgName('');
-  }
+
+    setShowCreate(false);
+  };
 
   const handleCopyInvite = async () => {
     const invitation = await party.createInvitation({
@@ -321,6 +396,28 @@ const Main = ({ party, generator }: MainProps) => {
 
     await navigator.clipboard.writeText(JSON.stringify(invitation.toQueryParameters()));
   };
+
+  items.sort((a, b) => {
+    const getType = (type: string) => {
+      const TYPE_ORDER = {
+        [OBJECT_ORG]: 1,
+        [OBJECT_PERSON]: 2,
+        [OBJECT_PROJECT]: 3,
+        [OBJECT_TASK]: 4
+      };
+
+      return TYPE_ORDER[type as keyof typeof TYPE_ORDER] || Infinity;
+    };
+
+    const order = getType(a.type) < getType(b.type) ? -1 : getType(a.type) > getType(b.type) ? 1 : 0;
+    if (order !== 0) {
+      return order;
+    }
+
+    const titleA = a.model.getProperty('name');
+    const titleB = b.model.getProperty('name');
+    return titleA < titleB ? -1 : titleA > titleB ? 1 : 0;
+  });
 
   // TODO(burdon): Show/hide components to maintain state (and test subscriptions). Show for first time on select.
   return (
@@ -335,17 +432,6 @@ const Main = ({ party, generator }: MainProps) => {
         </div>
         <Button onClick={handleCopyInvite}>Copy invite</Button>
       </Toolbar>
-
-      {showAdd && (
-        <ListItem>
-          <ListItemIcon>
-            <OrgIcon />
-          </ListItemIcon>
-          <TextField id='org-name' label='Org name' value={orgName} onChange={e => setOrgName(e.currentTarget.value)}/>
-          <Button color='primary' onClick={handleAdd}>Add</Button>
-          <Button color='secondary' onClick={() => setShowAdd(false)}>Cancel</Button>
-        </ListItem>
-      )}
 
       <div className={classes.content}>
         {view === VIEW_LIST && (
@@ -384,14 +470,16 @@ const Main = ({ party, generator }: MainProps) => {
         )}
       </div>
 
-      <Fab className={classes.fab} color='primary' aria-label='add' onClick={() => setShowAdd(true)}>
+      <ItemDialog open={showCreate} handleCreate={handleCreate} handleClose={() => setShowCreate(false)} />
+
+      <Fab className={classes.fab} color='primary' aria-label='add' onClick={() => setShowCreate(true)}>
         <AddIcon />
       </Fab>
     </div>
   );
 };
 
-// TOOD(burdon): Implement router.
+// TODO(burdon): Implement router.
 
 export const Primary = () => {
   const { party, generator, createParty, joinParty } = useGenerator();
