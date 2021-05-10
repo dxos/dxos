fc9f4e9f728d859e9e4d543b638a6c78c159e9a6
egorgripasov <gripasovegor@gmail.com>
2021-05-10 12:19:09 -0400

    add messenger-model

---
+++ packages/echo/messenger-model/src/index.ts
@@ -0,0 +1,8 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+export * from './messenger-model';
+export const MESSENGER_PAD = 'dxos.org/pad/messenger';
+export const MESSENGER_TYPE_CHANNEL = 'dxos.org/type/messenger/channel';
+export const MESSENGER_TYPE_MESSAGE = 'dxos.org/type/messenger/message';
