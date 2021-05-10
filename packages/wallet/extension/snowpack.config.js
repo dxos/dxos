507f4e9245ae7bca412b146d43be8b257289f3a4
Przemyslaw Rzad <przemek@ethworks.io>
2021-05-10 16:23:21 +0200

    Introduce skeleton of a Wallet Extension (#61)

---
+++ packages/wallet/extension/snowpack.config.js
@@ -0,0 +1,22 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' }
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    metaUrlPath: 'snowpack'
+  },
+};
