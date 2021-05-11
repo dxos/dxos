b5104de6de799b27385812d6a3052edfdd46d23b
Egor Gripasov <gripasovegor@gmail.com>
2021-05-11 12:58:07 -0400

    Add gravity. (#66)
    * An attempt to add gravity.
    
    * add messenger-model
    
    * fix messenger-model
    
    * make build work
    
    * fix bot env mapping
    
    * ignore bots output.
    
    * Make first test work.
    
    * Add linter.
    
    * fix tests
    
    * add pnpm-lock.yaml
    
    * Try absolute path.
    
    * fix test
    
    * Update pnpm-lock.yaml
    
    * Try tsc.
    
    * rm babel from bots
    
    * upd deps.
    
    * cleanup
    
    * fix

---
+++ packages/echo/messenger-model/src/messenger-model.ts
@@ -0,0 +1,36 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import { MutationMeta } from '@dxos/echo-protocol';
+import { ModelMeta, Model } from '@dxos/model-factory';
+
+import { Message, schema } from './proto';
+
+export class MessengerModel extends Model<Message> {
+  static meta: ModelMeta = {
+    type: 'wrn://protocol.dxos.org/teamwork/messenger',
+    mutation: schema.getCodecForType('dxos.teamwork.messenger.Message')
+  };
+
+  private readonly _messages: Message[] = [];
+
+  get messages () {
+    return this._messages;
+  }
+
+  async _processMessage (meta: MutationMeta, message: Message) {
+    this._messages.push(message);
+    this._messages.sort((msgA, msgB) => parseInt(msgA.timestamp) - parseInt(msgB.timestamp));
+    return true;
+  }
+
+  async sendMessage (message: Pick<Message, 'text' | 'sender'>) {
+    const receipt = await this.write({
+      text: message.text,
+      timestamp: Date.now().toString(),
+      sender: message.sender
+    });
+    await receipt.waitToBeProcessed();
+  }
+}
