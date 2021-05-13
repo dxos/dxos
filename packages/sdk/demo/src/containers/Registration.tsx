454419ba481307c3d849b1a4dc127283e0b8a36b
rzadp <roopert7@gmail.com>
2021-05-13 20:38:53 +0200

    Recovery

--- packages/sdk/demo/src/containers/Registration.tsx
+++ packages/sdk/demo/src/containers/Registration.tsx
@@ -3,20 +3,18 @@
 //
 
 import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
 
 import { Dialog, DialogContent, DialogContentText, LinearProgress } from '@material-ui/core';
 import RestoreIcon from '@material-ui/icons/Restore';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { keyPairFromSeedPhrase } from '@dxos/credentials';
-import { decrypt } from '@dxos/crypto';
-import { useClient, useConfig } from '@dxos/react-client';
-// import { useQuery, createUrl } from '@dxos/react-router';
+import { useClient } from '@dxos/react-client';
 import { FullScreen } from '@dxos/react-ux';
 
 import DialogHeading from '../components/DialogHeading';
 import RegistrationDialog from '../components/RegistrationDialog';
+import { sleep } from '../../../client/node_modules/@dxos/async/dist/src';
 
 const useStyles = makeStyles((theme) => ({
   progressBar: {
@@ -27,51 +25,36 @@ const useStyles = makeStyles((theme) => ({
 const Registration = () => {
   const classes = useStyles();
 
-  const [open, setOpen] = useState(true);
-  // const history = useHistory();
-  // const { redirectUrl = '/', ...rest } = useQuery();
   const client = useClient();
-  const config = useConfig();
   const [recovering, setRecovering] = useState(false);
 
-  const clearIdentity = async () => {
-    setOpen(false);
-
-    // TODO(telackey): Replace with feedStore.deleteAll() once that is published in @dxos/feed-store
-    // cf. https://github.com/dxos/feed-store/pull/13
-    await Promise.all(client.feedStore.getDescriptors().map(({ path }) => client.feedStore.deleteDescriptor(path)));
-  };
-
   const handleFinishCreate = async (username: string, seedPhrase: string) => {
-    await clearIdentity();
     const identityKeyPair = keyPairFromSeedPhrase(seedPhrase);
     await client.createProfile({ ...identityKeyPair, username });
-
-    // await client.partyManager.identityManager.initializeForNewIdentity({
-    //   identityDisplayName: username || keyToString(client.partyManager.identityManager.publicKey),
-    //   deviceDisplayName: keyToString(client.partyManager.identityManager.deviceManager.publicKey)
-    // });
-
-    // history.push(createUrl(redirectUrl, rest));
   };
 
   const handleFinishRestore = async (seedPhrase: string) => {
-    await clearIdentity();
     setRecovering(true);
-    await client.echo.recoverHalo(seedPhrase);
+
+    // TODO(rzadp): Recovering halo requires another device to be online.
+    // No point in having real recovering in storybook as long as we don't have device initiations as well.
+    await sleep(2000); // Simulate recovering.
+    // await client.echo.recoverHalo(seedPhrase);
+
+    await client.createProfile(keyPairFromSeedPhrase(seedPhrase)) // We don't have device recovery, so just recreating halo from scratch.
+
     setRecovering(false);
-    // history.push(createUrl(redirectUrl, rest));
   };
 
-  const keyringDecrypter = async (data: string, passphrase: string) => {
-    await client.echo.keyring.loadJSON(decrypt(data, passphrase));
+  const keyringDecrypter = () => {
+    throw new Error('Keyring decrypted not implemented.');
   };
 
   return (
     <FullScreen>
       <RegistrationDialog
         keyringDecrypter={keyringDecrypter as any}
-        open={open}
+        open={true}
         debug={true}
         onFinishCreate={handleFinishCreate}
         onFinishRestore={handleFinishRestore}
--- packages/sdk/demo/stories/tutorial/stage4.story.tsx
+++ packages/sdk/demo/stories/tutorial/stage4.story.tsx
@@ -11,12 +11,23 @@ import { createKeyPair } from '@dxos/crypto';
 import { ClientInitializer, useClient, useParties, useProfile } from '@dxos/react-client';
 
 import { Registration } from '../../src';
+import { JsonTreeView } from '@dxos/react-ux';
 
 /**
  * Walk through the registration flow
  */
 export const Stage4 = () => {
   const App = () => {
+    const profile = useProfile();
+
+    if (!!profile) {
+      return (
+        <Box m={2}>
+          <JsonTreeView data={profile} />
+        </Box>
+      )
+    }
+
     return (
       <>
         <Box m={2}>
@@ -34,6 +45,6 @@ export const Stage4 = () => {
 };
 
 export default {
-  title: 'Tutorials/Stage 3',
+  title: 'Tutorials/Stage 4',
   component: Stage4
 };
