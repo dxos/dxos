c7cfd7373052fcfcb1a5c783641d3a797c205897
rzadp <roopert7@gmail.com>
2021-05-14 13:11:59 +0200

    Add dxos stuff

--- packages/wallet/extension/src/background/background.ts
+++ packages/wallet/extension/src/background/background.ts
@@ -1,4 +1,24 @@
-setInterval(() => {
-  console.log('background.ts interval')
-}, 1000)
+import { Client, ClientConfig } from "@dxos/client";
+import { createKeyPair } from "@dxos/crypto";
 
+const config: ClientConfig = {
+  storage: {
+    persistent: true
+  },
+  snapshots: true,
+  snapshotInterval: 10
+};
+
+(async () => {
+  console.log('Creating client...');
+  const client = new Client(config);
+  await client.initialize();
+  const profile = await client.getProfile();
+  console.log({profile})
+  
+  if (!profile) {
+    console.log('Creating new profile...')
+    await client.createProfile({...createKeyPair(), username: 'DXOS User'})
+    console.log({profile: await client.getProfile()})
+  }
+})()
