9826b96cf511c9536ec94528abe1abd1d3b71ab2
rzadp <roopert7@gmail.com>
2021-05-14 15:30:44 +0200

    Lint

--- packages/wallet/extension/src/background/background.ts
+++ packages/wallet/extension/src/background/background.ts
@@ -1,12 +1,15 @@
-import { Client, ClientConfig } from "@dxos/client";
-import { createKeyPair } from "@dxos/crypto";
+//
+// Copyright 2021 DXOS.org
+//
+
+import { Client, ClientConfig } from '@dxos/client';
+import { createKeyPair } from '@dxos/crypto';
 
 const config: ClientConfig = {
   storage: {
     persistent: true,
     type: 'idb',
-    // path: `/tmp/dxos-${Date.now()}`
-    path: `/tmp/dxos-2`
+    path: '/tmp/dxos'
   }
 };
 
@@ -14,12 +17,13 @@ const config: ClientConfig = {
   console.log('Creating client...');
   const client = new Client(config);
   await client.initialize();
+
   const profile = await client.getProfile();
-  console.log({profile})
-  
+  console.log({ profile });
+
   if (!profile) {
-    console.log('Creating new profile...')
-    await client.createProfile({...createKeyPair(), username: 'DXOS User'})
-    console.log({profile: await client.getProfile()})
+    console.log('Creating new profile...');
+    await client.createProfile({ ...createKeyPair(), username: 'DXOS User' });
+    console.log({ profile: await client.getProfile() });
   }
-})()
+})();
