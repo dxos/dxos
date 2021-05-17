8bfa9900ddb67d042d7e0fd9d3619424b0d73b7a
Dmytro Maretskyi <maretskii@gmail.com>
2021-05-17 14:51:23 +0200

    Cleanup polyfills

--- packages/wallet/extension/scripts/globals-plugin.js
+++ packages/wallet/extension/scripts/globals-plugin.js
@@ -1,36 +1,12 @@
 const path = require('path')
-const fs = require('fs')
-const esbuild = require('esbuild')
 
 function NodeGlobalsPolyfillPlugin() {
     return {
         name: 'node-globals-polyfill',
-        setup({ initialOptions, onResolve, onLoad }) {
-            onResolve({ filter: /_node-buffer-polyfill_\.js/ }, (arg) => {
-                return {
-                    path: path.resolve(__dirname, './Buffer.js'),
-                }
-            })
-            onResolve({ filter: /_node-process-polyfill_\.js/ }, (arg) => {
-                return {
-                    path: path.resolve(__dirname, './process.js'),
-                }
-            })
-
-            onLoad({ filter: /_virtual-process-polyfill_\.js/ }, (arg) => {
-                const data = fs
-                    .readFileSync(path.resolve(__dirname, './process.js'))
-                    .toString()
-
-                return {
-                    loader: 'js',
-                    contents: data,
-                }
-            })
-
+        setup({ initialOptions }) {
             const polyfills = [
-                path.resolve(__dirname, './_virtual-process-polyfill_.js'),
-                path.resolve(__dirname, './_Buffer.js')
+                path.resolve(__dirname, './polyfills/process.js'),
+                path.resolve(__dirname, './polyfills/Buffer.js')
             ]
             if (initialOptions.inject) {
                 initialOptions.inject.push(...polyfills)
