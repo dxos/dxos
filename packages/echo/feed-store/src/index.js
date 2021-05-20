a7d37b1e21dfa7624dfd20a7363359acc1440a6d
DXOS <npm@dxos.network>
2021-05-20 10:23:32 +0000

    Remove index.js in root directory

--- packages/echo/feed-store/src/index.js
+++ packages/echo/feed-store/src/index.js
@@ -3,6 +3,10 @@
 //
 
 import createBatchStream from './create-batch-stream';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('source-map-support').install();
+
 export * from './feed-store';
 export * from './feed-descriptor';
 export { createBatchStream };
