54e527b7ff222c7171a667264232c4ef8db583e4
Przemyslaw Rzad <przemek@ethworks.io>
2021-05-20 10:31:48 +0200

    Wallet - protobuf and streaming responses (#87)
    * WIP
    
    * Manual refresh
    
    * Pub parties
    
    * Working version
    
    * Subscriptions working
    
    * Less async
    
    * async
    
    * Reverse order
    
    * Remove redundant file
    
    * Comments
    
    * Lint

--- packages/wallet/extension/src/popup/components/Application.tsx
+++ packages/wallet/extension/src/popup/components/Application.tsx
@@ -7,34 +7,57 @@ import React, { useState, useEffect } from 'react';
 import { JsonTreeView } from '@dxos/react-ux';
 
 import { schema } from '../../proto/gen';
-import { useBackground } from '../hooks';
+import { useRpcClient } from '../hooks';
+
+const getProfileRequest = schema.getCodecForType('dxos.wallet.extension.GetProfileRequest').encode({});
+const getPartiesRequest = schema.getCodecForType('dxos.wallet.extension.GetPartiesRequest').encode({});
+const createPartyRequest = schema.getCodecForType('dxos.wallet.extension.CreatePartyRequest').encode({});
 
 const Application = () => {
-  const [profile, setProfile] = useState<any | undefined>();
-  const background = useBackground();
+  const [profile, setProfile] = useState<any | undefined>(undefined);
+  const [parties, setParties] = useState<any[] | undefined>(undefined);
+  const rpcClient = useRpcClient();
 
   useEffect(() => {
-    if (background === undefined) {
+    if (rpcClient === undefined) {
       return;
     }
 
-    const listener = (message: any) => {
-      const decodedMessage = schema.getCodecForType('dxos.wallet.extension.ProfileResponse').decode(message);
-      console.log('Popup received: ', decodedMessage);
-      setProfile(decodedMessage);
+    setImmediate(async () => {
+      const response = await rpcClient.call('GetProfile', getProfileRequest);
+      const profile = schema.getCodecForType('dxos.wallet.extension.GetProfileResponse').decode(response);
+      setProfile(profile);
+    });
+
+    const partiesListener = (message: Uint8Array) => {
+      const parties = schema.getCodecForType('dxos.wallet.extension.GetPartiesResponse').decode(message);
+      setParties(parties.partyKeys);
     };
-    background.onMessage.addListener(listener);
-    background.postMessage({ method: 'GetProfile' });
-    return () => background.onMessage.removeListener(listener);
-  }, [background]);
+
+    const responseStream = rpcClient.callAndSubscribe('GetParties', getPartiesRequest);
+    responseStream.message.on(partiesListener);
+    return () => responseStream.message.off(partiesListener);
+  }, [rpcClient]);
+
+  if (!rpcClient) {
+    return <p>Connecting to background...</p>;
+  }
 
   if (!profile) {
     return <p>No profile loaded.</p>;
   }
 
+  const handleCreateParty = async () => {
+    const response = await rpcClient.call('CreateParty', createPartyRequest);
+    const party = schema.getCodecForType('dxos.wallet.extension.CreatePartyResponse').decode(response);
+    console.log('Created party: ', party.partyKey);
+  };
+
   return (
     <div style={{ minWidth: 400 }}>
       <JsonTreeView data={profile} />
+      <p>Number of parties: {parties?.length ?? 0}.</p>
+      <button onClick={handleCreateParty}>Create party</button>
     </div>
   );
 };
--- packages/wallet/extension/src/popup/hooks/index.ts
+++ packages/wallet/extension/src/popup/hooks/index.ts
@@ -2,4 +2,4 @@
 // Copyright 2021 DXOS.org
 //
 
-export * from './useBackground';
+export * from './useRpcClient';
