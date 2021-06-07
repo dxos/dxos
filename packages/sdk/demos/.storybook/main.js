28da1064b1d3934e6470fb1c23fee4dd2e19b049
rzadp <roopert7@gmail.com>
2021-06-07 16:35:27 +0200

    Remove docgen to make storybook compiling

--- packages/sdk/demos/.storybook/main.js
+++ packages/sdk/demos/.storybook/main.js
@@ -13,6 +13,9 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-links'
   ],
+  typescript: {
+    reactDocgen: 'none',
+  },
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
@@ -21,9 +24,9 @@ module.exports = {
           loader: require.resolve('ts-loader'),
         },
         // Optional
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        }
+        // {
+        //   loader: require.resolve('react-docgen-typescript-loader'),
+        // }
       ],
     });
     config.resolve.extensions.push('.ts', '.tsx');
