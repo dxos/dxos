e28c4b562f5874f467a1dfe5f2184e5fd8be3ed1
rzadp <roopert7@gmail.com>
2021-06-08 14:25:49 +0200

    Tutorials stage 3

--- packages/sdk/demos/playwright/smoke.test.ts
+++ packages/sdk/demos/playwright/smoke.test.ts
@@ -53,4 +53,14 @@ describe('Smoke tests for demo storybooks', () => {
 
     await user.page!.waitForSelector('//li[text()=\'Rau - Sporer\']'); // deterministic thanks to faker seed.
   });
+
+  test('Tutorials stage 3', async () => {
+    expect(user.page).toBeDefined();
+    await user.page!.goto(`${startUrl}/iframe.html?id=tutorials-stage-3--stage-3&viewMode=story`);
+    await createHalo(user.page!)
+    await createParty(user.page!)
+    await user.page!.click('//span[text()=\'Create item\']')
+
+    await user.page!.waitForSelector('//p[text()=\'Priscilla Stamm\']'); // deterministic thanks to faker seed.
+  });
 });
