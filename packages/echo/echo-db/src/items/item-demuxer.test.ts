1f07a81f5e107bf06a4089c58c3a2ec6f008ef51
Dmytro Maretskyi <maretskii@gmail.com>
2021-06-09 13:19:10 +0200

    Add test grouping

--- packages/echo/echo-db/src/items/item-demuxer.test.ts
+++ packages/echo/echo-db/src/items/item-demuxer.test.ts
@@ -23,199 +23,201 @@ const log = debug('dxos:echo:item-demuxer:test');
 
 const createPublicKey = () => PublicKey.from(createKeyPair().publicKey);
 
-test('set-up', async () => {
-  const feedKey = createPublicKey();
-  const memberKey = createPublicKey();
+describe('Item demuxer', () => {
+  test('set-up', async () => {
+    const feedKey = createPublicKey();
+    const memberKey = createPublicKey();
 
-  const modelFactory = new ModelFactory()
-    .registerModel(TestModel)
-    .registerModel(DefaultModel);
+    const modelFactory = new ModelFactory()
+      .registerModel(TestModel)
+      .registerModel(DefaultModel);
 
-  const writeStream = createTransform<EchoEnvelope, IEchoStream>(
-    async (message: EchoEnvelope): Promise<IEchoStream> => ({
-      meta: {
-        feedKey: feedKey.asUint8Array(),
-        memberKey: memberKey.asUint8Array(),
-        seq: 0
-      },
-      data: message
-    })
-  );
+    const writeStream = createTransform<EchoEnvelope, IEchoStream>(
+      async (message: EchoEnvelope): Promise<IEchoStream> => ({
+        meta: {
+          feedKey: feedKey.asUint8Array(),
+          memberKey: memberKey.asUint8Array(),
+          seq: 0
+        },
+        data: message
+      })
+    );
 
-  const itemManager = new ItemManager(modelFactory, createMockFeedWriterFromStream(writeStream));
-  const itemDemuxer = new ItemDemuxer(itemManager, modelFactory);
-  writeStream.pipe(itemDemuxer.open());
+    const itemManager = new ItemManager(modelFactory, createMockFeedWriterFromStream(writeStream));
+    const itemDemuxer = new ItemDemuxer(itemManager, modelFactory);
+    writeStream.pipe(itemDemuxer.open());
 
-  //
-  // Query for items.
-  //
+    //
+    // Query for items.
+    //
 
-  const [updatedItems, onUpdateItem] = latch();
-  const items = await itemManager.queryItems();
-  const unsubscribe = items.subscribe((items: Item<any>[]) => {
-    expect(items).toHaveLength(1);
-    onUpdateItem();
-  });
+    const [updatedItems, onUpdateItem] = latch();
+    const items = await itemManager.queryItems();
+    const unsubscribe = items.subscribe((items: Item<any>[]) => {
+      expect(items).toHaveLength(1);
+      onUpdateItem();
+    });
 
-  const itemId = createId();
-  const message: EchoEnvelope = {
-    itemId,
-    genesis: {
-      itemType: 'dxn://dxos/item/test',
-      modelType: TestModel.meta.type
-    }
-  };
-  await writeStream.write(message);
-
-  //
-  // Wait for mutations to be processed.
-  //
-
-  await updatedItems;
-
-  //
-  // Update item (causes mutation to be propagated).
-  //
-
-  const item = itemManager.getItem(itemId);
-  expect(item).toBeTruthy();
-
-  const [updated, onUpdate] = latch();
-  const model: TestModel = item?.model as TestModel;
-  model.subscribe(model => {
-    expect((model as TestModel).keys.length).toBe(1);
-    onUpdate();
-  });
-
-  await model.setProperty('title', 'Hello');
-
-  //
-  // Wait for model mutation to propagate.
-  // TODO(burdon): Should trigger itemManager update?
-  //
-
-  await updated;
-
-  log('Properties', model.keys);
-  expect(model.keys.length).toBe(1);
-  unsubscribe();
-});
-
-it('ignores unknown models', async () => {
-  const modelFactory = new ModelFactory()
-    .registerModel(TestModel)
-    .registerModel(DefaultModel);
-
-  const writeStream = createTransform<EchoEnvelope, IEchoStream>(
-    async (message: EchoEnvelope): Promise<IEchoStream> => ({
-      meta: {
-        feedKey: createPublicKey().asUint8Array(),
-        memberKey: createPublicKey().asUint8Array(),
-        seq: 0
-      },
-      data: message
-    })
-  );
-  const itemManager = new ItemManager(modelFactory, createMockFeedWriterFromStream(writeStream));
-  const itemDemuxer = new ItemDemuxer(itemManager, modelFactory);
-  writeStream.pipe(itemDemuxer.open());
-
-  writeStream.write(checkType<EchoEnvelope>({
-    itemId: 'foo',
-    genesis: {
-      modelType: 'unknown model'
-    }
-  }));
-  writeStream.write(checkType<EchoEnvelope>({
-    itemId: 'bar',
-    genesis: {
-      modelType: TestModel.meta.type
-    }
-  }));
-  await itemManager.queryItems().update.waitForCount(1);
-  const items = itemManager.queryItems().value;
-  expect(items).toHaveLength(1);
-  expect(items[0].model).toBeInstanceOf(TestModel);
-});
-
-it('ignores unknown models on snapshot restore', async () => {
-  const modelFactory = new ModelFactory()
-    .registerModel(TestModel)
-    .registerModel(DefaultModel);
-
-  const itemManager = new ItemManager(modelFactory);
-  const itemDemuxer = new ItemDemuxer(itemManager, modelFactory);
-
-  await itemDemuxer.restoreFromSnapshot({
-    items: [
-      {
-        itemId: 'foo',
-        modelType: 'unknown model',
-        model: {
-          array: {
-            mutations: [
-              {
-                mutation: Buffer.from('abc'),
-                meta: {
-                  feedKey: createPublicKey().asUint8Array(),
-                  memberKey: createPublicKey().asUint8Array(),
-                  seq: 0
-                }
-              }
-            ]
-          }
-        }
+    const itemId = createId();
+    const message: EchoEnvelope = {
+      itemId,
+      genesis: {
+        itemType: 'dxn://dxos/item/test',
+        modelType: TestModel.meta.type
       }
-    ]
+    };
+    await writeStream.write(message);
+
+    //
+    // Wait for mutations to be processed.
+    //
+
+    await updatedItems;
+
+    //
+    // Update item (causes mutation to be propagated).
+    //
+
+    const item = itemManager.getItem(itemId);
+    expect(item).toBeTruthy();
+
+    const [updated, onUpdate] = latch();
+    const model: TestModel = item?.model as TestModel;
+    model.subscribe(model => {
+      expect((model as TestModel).keys.length).toBe(1);
+      onUpdate();
+    });
+
+    await model.setProperty('title', 'Hello');
+
+    //
+    // Wait for model mutation to propagate.
+    // TODO(burdon): Should trigger itemManager update?
+    //
+
+    await updated;
+
+    log('Properties', model.keys);
+    expect(model.keys.length).toBe(1);
+    unsubscribe();
   });
 
-  expect(itemManager.queryItems().value).toHaveLength(0);
-});
+  it('ignores unknown models', async () => {
+    const modelFactory = new ModelFactory()
+      .registerModel(TestModel)
+      .registerModel(DefaultModel);
 
-it('models can be registered after item was already created', async () => {
-  const modelFactory = new ModelFactory()
-    .registerModel(ObjectModel)
-    .registerModel(DefaultModel);
+    const writeStream = createTransform<EchoEnvelope, IEchoStream>(
+      async (message: EchoEnvelope): Promise<IEchoStream> => ({
+        meta: {
+          feedKey: createPublicKey().asUint8Array(),
+          memberKey: createPublicKey().asUint8Array(),
+          seq: 0
+        },
+        data: message
+      })
+    );
+    const itemManager = new ItemManager(modelFactory, createMockFeedWriterFromStream(writeStream));
+    const itemDemuxer = new ItemDemuxer(itemManager, modelFactory);
+    writeStream.pipe(itemDemuxer.open());
 
-  const writeStream = createTransform<EchoEnvelope, IEchoStream>(
-    async (message: EchoEnvelope): Promise<IEchoStream> => ({
-      meta: {
-        feedKey: randomBytes(),
-        memberKey: randomBytes(),
-        seq: 0
-      },
-      data: message
-    })
-  );
-  const itemManager = new ItemManager(modelFactory, createMockFeedWriterFromStream(writeStream));
-  const itemDemuxer = new ItemDemuxer(itemManager, modelFactory);
-  writeStream.pipe(itemDemuxer.open());
-
-  writeStream.write(checkType<EchoEnvelope>({
-    itemId: 'foo',
-    genesis: {
-      modelType: TestModel.meta.type
-    }
-  }));
-  writeStream.write(checkType<EchoEnvelope>({
-    itemId: 'bar',
-    genesis: {
-      modelType: ObjectModel.meta.type
-    }
-  }));
-  {
+    writeStream.write(checkType<EchoEnvelope>({
+      itemId: 'foo',
+      genesis: {
+        modelType: 'unknown model'
+      }
+    }));
+    writeStream.write(checkType<EchoEnvelope>({
+      itemId: 'bar',
+      genesis: {
+        modelType: TestModel.meta.type
+      }
+    }));
     await itemManager.queryItems().update.waitForCount(1);
     const items = itemManager.queryItems().value;
     expect(items).toHaveLength(1);
-    expect(items[0].model).toBeInstanceOf(ObjectModel);
-  }
-
-  modelFactory.registerModel(TestModel);
-
-  {
-    await itemManager.queryItems({ id: 'foo' }).update.waitForCount(1);
-    const items = itemManager.queryItems({ id: 'foo' }).value;
-    expect(items).toHaveLength(1);
     expect(items[0].model).toBeInstanceOf(TestModel);
-  }
+  });
+
+  it('ignores unknown models on snapshot restore', async () => {
+    const modelFactory = new ModelFactory()
+      .registerModel(TestModel)
+      .registerModel(DefaultModel);
+
+    const itemManager = new ItemManager(modelFactory);
+    const itemDemuxer = new ItemDemuxer(itemManager, modelFactory);
+
+    await itemDemuxer.restoreFromSnapshot({
+      items: [
+        {
+          itemId: 'foo',
+          modelType: 'unknown model',
+          model: {
+            array: {
+              mutations: [
+                {
+                  mutation: Buffer.from('abc'),
+                  meta: {
+                    feedKey: createPublicKey().asUint8Array(),
+                    memberKey: createPublicKey().asUint8Array(),
+                    seq: 0
+                  }
+                }
+              ]
+            }
+          }
+        }
+      ]
+    });
+
+    expect(itemManager.queryItems().value).toHaveLength(0);
+  });
+
+  it('models can be registered after item was already created', async () => {
+    const modelFactory = new ModelFactory()
+      .registerModel(ObjectModel)
+      .registerModel(DefaultModel);
+
+    const writeStream = createTransform<EchoEnvelope, IEchoStream>(
+      async (message: EchoEnvelope): Promise<IEchoStream> => ({
+        meta: {
+          feedKey: randomBytes(),
+          memberKey: randomBytes(),
+          seq: 0
+        },
+        data: message
+      })
+    );
+    const itemManager = new ItemManager(modelFactory, createMockFeedWriterFromStream(writeStream));
+    const itemDemuxer = new ItemDemuxer(itemManager, modelFactory);
+    writeStream.pipe(itemDemuxer.open());
+
+    writeStream.write(checkType<EchoEnvelope>({
+      itemId: 'foo',
+      genesis: {
+        modelType: TestModel.meta.type
+      }
+    }));
+    writeStream.write(checkType<EchoEnvelope>({
+      itemId: 'bar',
+      genesis: {
+        modelType: ObjectModel.meta.type
+      }
+    }));
+    {
+      await itemManager.queryItems().update.waitForCount(1);
+      const items = itemManager.queryItems().value;
+      expect(items).toHaveLength(1);
+      expect(items[0].model).toBeInstanceOf(ObjectModel);
+    }
+
+    modelFactory.registerModel(TestModel);
+
+    {
+      await itemManager.queryItems({ id: 'foo' }).update.waitForCount(1);
+      const items = itemManager.queryItems({ id: 'foo' }).value;
+      expect(items).toHaveLength(1);
+      expect(items[0].model).toBeInstanceOf(TestModel);
+    }
+  });
 });
