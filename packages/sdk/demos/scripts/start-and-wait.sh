e627b741e683797ebe48e29318e7d22f1c802c54
rzadp <roopert7@gmail.com>
2021-06-09 10:31:22 +0200

    Playwright for demos storybooks

---
+++ packages/sdk/demos/scripts/start-and-wait.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#
+# Copyright 2020 DXOS.org
+#
+
+# This script waits in app on the whatever web server is used
+# Until it starts actually serving the proper index.html file
+
+rushx storybook --quiet & 
+set +e
+i=0
+until curl "http://localhost:9001" | grep -q "Storybook"
+do
+  ((i++))
+  if [ "$i" -gt 30 ]
+  then
+    exit -1
+  fi
+  echo "Sleeping..."
+  sleep 3
+done
+
+echo "Storybook started."
