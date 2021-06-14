2b93d0c3092d590c2e23f5515ac3861d0481692d
rzadp <roopert7@gmail.com>
2021-06-14 11:37:02 +0200

    WIP: identity

--- packages/wallet/playground/src/components/App.tsx
+++ packages/wallet/playground/src/components/App.tsx
@@ -2,12 +2,38 @@
 // Copyright 2020 DXOS.org
 //
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useRpcClient, schema, GetProfileResponse } from '@dxos/wallet-core';
+
+const getProfileRequest = schema.getCodecForType('dxos.wallet.extension.GetProfileRequest').encode({})
 
 const App = () => {
+  const [profile, setProfile] = useState<GetProfileResponse | undefined>(undefined);
+  const rpcClient = useRpcClient();
+
+  useEffect(() => {
+    if (rpcClient === undefined) {
+      return;
+    }
+
+    setImmediate(async () => {
+      const response = await rpcClient.call('GetProfile', getProfileRequest);
+      const profile = schema.getCodecForType('dxos.wallet.extension.GetProfileResponse').decode(response);
+      setProfile(profile);
+    });
+  }, [rpcClient]);
+
+  if (!rpcClient) {
+    return <p>Connecting to the DXOS Wallet Extension...</p>;
+  }
+
+  if (!profile) {
+    return <p>No profile loaded.</p>;
+  }
+
   return (
-    <div>
-      <p>Hello Wallet Extension</p>
+    <div style={{ minWidth: 400 }}>
+      <p>Hello, {profile.username ?? profile.publicKey.toHex()}</p>
     </div>
   );
 };
