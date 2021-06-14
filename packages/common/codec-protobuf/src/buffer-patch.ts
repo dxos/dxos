218643fcb3fa79306742854ca98ee960f59cd5b6
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-06-14 17:28:32 +0200

    Fix buffer errors in @dxos/protocol (#142)
    * Add buffer patch
    
    * Fix type error
    
    * Refactor _buildMessage
    
    * Lint
    
    * Lint
    
    * Lint
    
    * Add Buffer.from to _send
    
    * Better typing in protocol
    
    * Lint
    
    * Fix type errors
    
    * Remove console.log

---
+++ packages/common/codec-protobuf/src/buffer-patch.ts
@@ -0,0 +1,15 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import type { Codec } from './codec';
+
+/**
+ * Protobuf codec returns instances of Uint8Arrays, but some storages expect to receive Buffers. This function patches the encode method to convert result into a Bufffer.
+ */
+export function patchBufferCodec (codec: Codec<any>) {
+  return {
+    encode: (x: any) => Buffer.from(codec.encode(x)),
+    decode: codec.decode.bind(codec)
+  };
+}
--- packages/common/codec-protobuf/src/index.ts
+++ packages/common/codec-protobuf/src/index.ts
@@ -7,3 +7,4 @@ export * from './codec';
 export * from './any';
 export * from './schema';
 export * from './service';
+export * from './buffer-patch';
