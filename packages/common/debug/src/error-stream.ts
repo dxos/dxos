131886808767393aa8592bc5e46b10d8ac1b5ccf
rzadp <roopert7@gmail.com>
2021-06-17 11:29:37 +0200

    Dont query the stack in ErrorStream

--- packages/common/debug/src/error-stream.ts
+++ packages/common/debug/src/error-stream.ts
@@ -10,19 +10,13 @@ export type ErrorHandlerCallback = (error: Error) => void;
  * Represents a stream of errors that entities can expose.
  */
 export class ErrorStream {
-  private readonly _creationStack: string;
-
   private _handler: ErrorHandlerCallback | undefined;
 
   private _unhandledErrors = 0;
 
-  constructor () {
-    this._creationStack = getStackTrace();
-  }
-
   assertNoUnhandledErrors () {
     if (this._unhandledErrors > 0) {
-      throw new Error(`Assertion failed: expected no unhandled errors to be thrown, but ${this._unhandledErrors} were thrown.\nThey originated from:\n${this._creationStack}`);
+      throw new Error(`Assertion failed: expected no unhandled errors to be thrown, but ${this._unhandledErrors} were thrown.`);
     }
   }
 
@@ -46,6 +40,5 @@ export class ErrorStream {
     this._unhandledErrors++;
 
     console.error(error);
-    console.error(`The above unhandled error originated from:\n${this._creationStack}`);
   }
 }
