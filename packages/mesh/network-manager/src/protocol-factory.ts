9d04d83ccb298373cb1d2f24420ffe76a449ec18
rzadp <roopert7@gmail.com>
2021-06-22 21:36:50 +0200

    Some fixes

--- packages/mesh/network-manager/src/protocol-factory.ts
+++ packages/mesh/network-manager/src/protocol-factory.ts
@@ -6,26 +6,32 @@ import assert from 'assert';
 import debug from 'debug';
 
 import { discoveryKey, keyToString } from '@dxos/crypto';
-import { Protocol } from '@dxos/protocol';
+import { Extension, Protocol } from '@dxos/protocol';
+import { ProtocolProvider } from './network-manager';
 
 const log = debug('dxos:network-manager:protocol-factory');
+
+interface ProtocolFactoryOptions {
+  plugins: any[],
+  getTopics: () => Buffer[],
+  initiator: boolean,
+  session: Record<string, any>
+}
 
 /**
  * Returns a function that takes a channel parameter, returns a Protocol object
  * with its context set to channel, plugins from plugins parameter and session
  * set to session parameter.
  *
- * @param {Object} session TODO(burdon): Document this.
- * @param {[plugin]} plugins array of Protocol plugin objects to add to created Protocol objects
- * @param {function() : Buffer[]} getTopics retrieve a list of known topic keys to encrypt by public key.
- * @return {ProtocolProvider}
+ * @param plugins array of Protocol plugin objects to add to created Protocol objects
+ * @param getTopics retrieve a list of known topic keys to encrypt by public key.
  * @deprecated
  */
 // TODO(dboreham): Deprecate, replace with protocol provider factory functions.
-export const protocolFactory = ({ session = {}, plugins = [], getTopics }) => {
+export const protocolFactory = ({ session = {}, plugins = [], getTopics, initiator }: ProtocolFactoryOptions): ProtocolProvider => {
   assert(getTopics);
   // eslint-disable-next-line no-unused-vars
-  return ({ channel }) => {
+  return ({ channel }: {channel: Buffer}) => {
     const protocol = new Protocol({
       streamOptions: { live: true },
       discoveryToPublicKey: (dk) => {
@@ -33,14 +39,17 @@ export const protocolFactory = ({ session = {}, plugins = [], getTopics }) => {
         if (publicKey) {
           protocol.setContext({ topic: keyToString(publicKey) });
         }
+        assert(publicKey, 'PublicKey not found in discovery.')
         return publicKey;
-      }
+      },
+      initiator,
+      userSession: session,
+      discoveryKey: channel
     });
 
     protocol
-      .setSession(session)
       .setExtensions(plugins.map(plugin => plugin.createExtension()))
-      .init(channel);
+      .init();
 
     log('Created protocol');
     return protocol;
@@ -49,17 +58,14 @@ export const protocolFactory = ({ session = {}, plugins = [], getTopics }) => {
 
 /**
  * Creates a ProtocolProvider for simple transport connections with only one protocol plugin.
- * @param rendezvousKey
- * @param peerId
- * @param protocolPlugin
- * @return {ProtocolProvider}
  */
-export const transportProtocolProvider = (rendezvousKey, peerId, protocolPlugin) => {
+export const transportProtocolProvider = (rendezvousKey: Buffer, peerId: Buffer, protocolPlugin: any, opts?: {initiator: boolean}): ProtocolProvider => {
   return protocolFactory({
     getTopics: () => {
       return [rendezvousKey];
     },
     session: { peerId },
-    plugins: [protocolPlugin]
+    plugins: [protocolPlugin],
+    initiator: !!opts?.initiator
   });
 };
