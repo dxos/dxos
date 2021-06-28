86822797f89d6de89f6731969bacb1ad3a307857
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-06-28 14:46:19 +0200

    Enable gravity tests (#161)
    * Convert tests to mocha
    
    * Remove default debug scopes
    
    * Bring back debug scopes
    
    * Remove global{Setup,Teardown}

--- packages/gravity/gravity-core/src/testing/setup.ts
+++ packages/gravity/gravity-core/src/testing/setup.ts
@@ -2,18 +2,30 @@
 // Copyright 2020 DXOS.org
 //
 
-import { spawn } from 'child_process';
+import { spawn, exec } from 'child_process';
+import debug from 'debug';
+import fs from 'fs-extra';
+import { before } from 'mocha';
+import path from 'path';
 
-import { OVERRIDE_CONFIG } from '../config';
+import { FACTORY_OUT_DIR, FACTORY_BOT_DIR, OVERRIDE_CONFIG } from '../config';
+
+const log = debug('dxos:gravity:testing:setup');
 
 const LOCAL = 'localhost';
 
 const signalUrl = new URL(OVERRIDE_CONFIG.DX_SIGNAL_ENDPOINT);
 
-const testSetup = async () => {
+before(async () => {
+  log('setup');
   if (signalUrl.hostname === LOCAL) {
     spawn('npx', ['signal', '--port', signalUrl.port.toString()]);
   }
-};
+});
 
-export default testSetup;
+after(async () => {
+  log('teardown');
+  await fs.remove(path.join(process.cwd(), FACTORY_OUT_DIR));
+  await fs.remove(path.join(process.cwd(), FACTORY_BOT_DIR));
+  await exec(`kill $(ps aux | grep 'bin/signal.js --port ${signalUrl.port}' | awk '{print $2}')`);
+});
