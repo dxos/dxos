ee7eb9c16bed657cdadff9c2d0c6586109d993d1
rzadp <roopert7@gmail.com>
2021-06-29 13:52:29 +0200

    Rename, lint

--- packages/wallet/playground/src/hooks/useContentScript.ts
+++ packages/wallet/playground/src/hooks/useContentScript.ts
@@ -2,8 +2,7 @@
 // Copyright 2021 DXOS.org
 //
 
-import { createWindowMessagePort } from '@dxos/wallet-core';
-import { useBackgroundService } from '@dxos/wallet-core';
+import { createWindowMessagePort, useBackgroundService } from '@dxos/wallet-core';
 
 export const useContentScript = () => {
   return useBackgroundService({ port: createWindowMessagePort() });
