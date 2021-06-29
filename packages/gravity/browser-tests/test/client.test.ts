872779837bcd0aef841b5a6f942b1f98436084c0
Dmytro Maretskyi <maretskii@gmail.com>
2021-06-29 17:23:48 +0200

    Add item test

--- packages/gravity/browser-tests/test/client.test.ts
+++ packages/gravity/browser-tests/test/client.test.ts
@@ -7,6 +7,7 @@ import { Mocha } from 'mocha';
 import { Client } from '@dxos/client'
 import { createKeyPair } from '@dxos/crypto';
 import 'source-map-support/register'
+import { ObjectModel } from '../../../echo/object-model/dist/src';
 
 Mocha.describe('Client', () => {
   Mocha.it('open & close', async () => {
@@ -29,6 +30,26 @@ Mocha.describe('Client', () => {
 
     const profile = client.getProfile();
     expect(profile.username).toEqual('DXOS test')
+
+    await client.destroy()
+  });
+
+  Mocha.it('create party', async () => {
+    const client = new Client()
+
+    await client.initialize()
+
+    await client.createProfile({
+      ...createKeyPair(),
+      username: 'DXOS test'
+    })
+
+    const party = await client.echo.createParty()
+    
+    const item = await party.database.createItem({ model: ObjectModel })
+    await item.model.setProperty('foo', 'bar')
+
+    expect(item.model.getProperty('foo')).toEqual('bar')
 
     await client.destroy()
   });
