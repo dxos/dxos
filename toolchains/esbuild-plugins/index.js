c714740d415cf0d421aa8c0e7188866e89f57c2a
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-06-29 17:53:12 +0200

    Add Client browser tests (#164)
    * WIP
    
    * REmove build
    
    * Add esbuild polyfills
    
    * Add item test
    
    * Add more tests
    
    * Lint

--- toolchains/esbuild-plugins/index.js
+++ toolchains/esbuild-plugins/index.js
@@ -60,9 +60,66 @@ function FixGracefulFsPlugin() {
   }
 }
 
+/**
+* @returns {import('esbuild').Plugin}
+*/
+function NodeModulesPlugin() {
+  return {
+    name: 'node-modules-plugin',
+    setup({ onResolve }) {
+      onResolve({ filter: /^assert$/ }, arg => {
+        return {
+          path: require.resolve('assert/'), // Appending slash makes node resolve the installed package from node_modules.
+        }
+      })
+      onResolve({ filter: /^stream$/ }, arg => {
+        return {
+          path: require.resolve('readable-stream'),
+        }
+      })
+      onResolve({ filter: /^path$/ }, arg => {
+        return {
+          path: require.resolve('path-browserify'),
+        }
+      })
+      onResolve({ filter: /^events$/ }, arg => {
+        return {
+          path: require.resolve('events/'),
+        }
+      })
+      onResolve({ filter: /^util$/ }, arg => {
+        return {
+          path: require.resolve('util/'),
+        }
+      })
+      onResolve({ filter: /^http$/ }, arg => {
+        return {
+          path: require.resolve('./polyfills/empty-module-stub.js'),
+        }
+      })
+      onResolve({ filter: /^https$/ }, arg => {
+        return {
+          path: require.resolve('./polyfills/empty-module-stub.js'),
+        }
+      })
+      onResolve({ filter: /^module$/ }, arg => {
+        return {
+          path: require.resolve('./polyfills/module.js'),
+        }
+      })
+      onResolve({ filter: /^crypto$/ }, arg => { // TODO
+        return {
+          path: require.resolve('./polyfills/empty-module-stub.js'),
+        }
+      })
+    }
+  }
+}
+
 module.exports = {
   NodeGlobalsPolyfillPlugin,
   FixGracefulFsPlugin,
-  FixMemdownPlugin
+  FixMemdownPlugin,
+  NodeModulesPlugin
 }
 
