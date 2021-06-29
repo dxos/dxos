d52c7bbf4f3a2143c9f1e0781e4b6acd21eca914
Dmytro Maretskyi <maretskii@gmail.com>
2021-06-29 17:34:39 +0200

    Add more tests

--- packages/gravity/browser-tests/test/client.test.ts
+++ packages/gravity/browser-tests/test/client.test.ts
@@ -4,53 +4,86 @@
 
 import expect from 'expect';
 import { Mocha } from 'mocha';
-import { Client } from '@dxos/client'
+
+import { Client } from '@dxos/client';
 import { createKeyPair } from '@dxos/crypto';
-import 'source-map-support/register'
+
+import 'source-map-support/register';
+import { testInvitationAuthenticator } from '../../../echo/echo-db/dist/src';
 import { ObjectModel } from '../../../echo/object-model/dist/src';
+import { testSecretProvider } from '../../../halo/credentials/dist/src';
 
 Mocha.describe('Client', () => {
   Mocha.it('open & close', async () => {
-    const client = new Client()
+    const client = new Client();
 
-    await client.initialize()
+    await client.initialize();
 
-    await client.destroy()
+    await client.destroy();
   });
 
   Mocha.it('create profile', async () => {
-    const client = new Client()
+    const client = new Client();
 
-    await client.initialize()
+    await client.initialize();
 
     await client.createProfile({
       ...createKeyPair(),
       username: 'DXOS test'
-    })
+    });
 
     const profile = client.getProfile();
-    expect(profile.username).toEqual('DXOS test')
+    expect(profile.username).toEqual('DXOS test');
 
-    await client.destroy()
+    await client.destroy();
   });
 
   Mocha.it('create party', async () => {
-    const client = new Client()
+    const client = new Client();
 
-    await client.initialize()
+    await client.initialize();
 
     await client.createProfile({
       ...createKeyPair(),
       username: 'DXOS test'
-    })
+    });
 
-    const party = await client.echo.createParty()
-    
-    const item = await party.database.createItem({ model: ObjectModel })
-    await item.model.setProperty('foo', 'bar')
+    const party = await client.echo.createParty();
 
-    expect(item.model.getProperty('foo')).toEqual('bar')
+    const item = await party.database.createItem({ model: ObjectModel });
+    await item.model.setProperty('foo', 'bar');
 
-    await client.destroy()
+    expect(item.model.getProperty('foo')).toEqual('bar');
+
+    await client.destroy();
+  });
+
+  // TODO(marik-d): Fails with "RTCError: Transport channel closed".
+  Mocha.it.skip('invitations', async () => {
+    const client = new Client();
+    await client.initialize();
+    await client.createProfile({
+      ...createKeyPair(),
+      username: 'DXOS test'
+    });
+
+    const party = await client.echo.createParty();
+    const item = await party.database.createItem({ model: ObjectModel, type: 'dxn://test' });
+    await item.model.setProperty('foo', 'bar');
+
+    const otherClient = new Client();
+    await otherClient.initialize();
+    await otherClient.createProfile({
+      ...createKeyPair(),
+      username: 'DXOS test 2'
+    });
+
+    const invite = await party.createInvitation(testInvitationAuthenticator);
+    const otherParty = await otherClient.echo.joinParty(invite, testSecretProvider);
+
+    const otherItem = otherParty.database.queryItems({ type: 'dxn://test' }).first;
+    expect(otherItem.model.getProperty('foo')).toEqual('bar');
+
+    await client.destroy();
   });
 });
