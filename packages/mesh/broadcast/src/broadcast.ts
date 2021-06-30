bb2510707d6912ad772578bb5077f5d24fd4335a
Dmytro Maretskyi <maretskii@gmail.com>
2021-06-30 09:31:43 +0200

    Use @dxos/crypto in broadcast

--- packages/mesh/broadcast/src/broadcast.ts
+++ packages/mesh/broadcast/src/broadcast.ts
@@ -3,7 +3,7 @@
 //
 
 import assert from 'assert';
-import crypto from 'crypto';
+import { randomBytes } from '@dxos/crypto';
 import debug from 'debug';
 import LRU, { Lru } from 'tiny-lru';
 
@@ -93,7 +93,7 @@ export class Broadcast<P extends Peer = Peer> {
     assert(middleware.send);
     assert(middleware.subscribe);
 
-    const { id = crypto.randomBytes(32), maxAge = 10 * 1000, maxSize = 1000 } = options;
+    const { id = randomBytes(32), maxAge = 10 * 1000, maxSize = 1000 } = options;
 
     this._id = id;
 
@@ -111,7 +111,7 @@ export class Broadcast<P extends Peer = Peer> {
    * Broadcast a flooding message to the peers neighbors.
    */
   async publish (data: Uint8Array, options: PublishOptions = {}): Promise<Packet | undefined> {
-    const { seqno = crypto.randomBytes(32) } = options;
+    const { seqno = randomBytes(32) } = options;
 
     assert(Buffer.isBuffer(data));
     assert(Buffer.isBuffer(seqno));
