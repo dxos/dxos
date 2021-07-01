229a36cb5c0cf16312297cea6cde4c953c412fb0
Josiah Witt <josiah@witt.life>
2021-06-30 21:47:50 -0400

    feat(tutorials): update storybook config

---
+++ packages/sdk/tutorials/apps/tasks-app/.storybook/preview.js
@@ -0,0 +1,12 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+export const parameters = {
+  layout: 'fullscreen',
+  options: {
+    storySort: {
+      order: ['Demo']
+    }
+  }
+};
---
+++ packages/sdk/tutorials/apps/tasks-app/stories/stage1.story.jsx
@@ -0,0 +1,47 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import React from 'react';
+
+import { createKeyPair } from '@dxos/crypto';
+import { ClientInitializer, useClient, useProfile } from '@dxos/react-client';
+import { JsonTreeView } from '@dxos/react-ux';
+import ProfileDialog from '../src/components/ProfileDialog';
+
+/**
+ * Create the user's HALO profile.
+ */
+export const Stage1 = () => {
+  const App = () => {
+    const client = useClient();
+    const profile = useProfile();
+
+    const handleCreateProfile = async ({ username }) => {
+      if (username) {
+        // TODO(burdon): Default keyPair?
+        const { publicKey, secretKey } = createKeyPair();
+        await client.createProfile({ publicKey, secretKey, username });
+      }
+    };
+
+    return (
+      <>
+        <ProfileDialog open={!profile} onClose={handleCreateProfile} />
+        <JsonTreeView data={profile} />
+      </>
+    );
+  };
+
+  // TODO(burdon): Must not hide ClientInitializer in tutorial (magic).
+  return (
+    <ClientInitializer>
+      <App />
+    </ClientInitializer>
+  );
+};
+
+export default {
+  title: 'Tutorials/Stage 1',
+  component: Stage1
+};
