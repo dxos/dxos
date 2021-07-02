7cda7eeeecb32da6fc0e1e8e1a69b2dbe2121d08
Ramiro Graziano <ramiro@gm2dev.com>
2021-07-02 06:26:02 -0300

    feat(tutorials): Cover Tasks App with Playwright (#179)

---
+++ packages/sdk/tutorials/apps/tasks-app/playwright/tasks-app.test.ts
@@ -0,0 +1,67 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import { firefox } from 'playwright';
+
+import { Browser, TaskApp } from './utils';
+
+describe('Tasks App Test Cases', () => {
+  const initialUrl = 'http://localhost:3000/';
+
+  let alice: TaskApp;
+
+  beforeAll(() => {
+    jest.setTimeout(30000);
+
+    alice = new TaskApp(new Browser());
+
+    return alice.browser.launch(firefox, initialUrl)
+      .then(() => alice.profile.create('Alice'));
+  });
+
+  afterAll(() => alice.browser.get().close());
+
+  describe('As a user with a profile already created then', () => {
+    test('I should be presented with the app', () => alice.checkAppIsLoaded())
+  });
+
+  describe('As a user with a profile already created then', () => {
+    test('I should be able to create a new task list', () => alice.createTaskList("My Example List"))
+  });
+
+  describe('As a user with a task list already created', () => {
+    const listName = "My Initial Tasks"
+
+    beforeEach(() => alice.createTaskList(listName));
+
+    test('I should be able to add new tasks to the list', () => alice.createTask(listName, "Clone the repo"))
+  });
+
+  describe('As a user with a task already created', () => {
+    const listName = "My Checked Tasks"
+    const taskName = "Install dependencies"
+
+    beforeEach(async () => {
+      await alice.createTaskList(listName);
+
+      await alice.createTask(listName, taskName);
+    });
+
+    test('I should be able to check and uncheck a task on the list', () => alice.swapTaskState(taskName))
+  });
+
+  describe('As a user with a task already created', () => {
+    const listName = "My Tasks To Remove"
+    const taskName = "Develop and test and break"
+
+    beforeEach(async () => {
+      await alice.createTaskList(listName);
+
+      await alice.createTask(listName, taskName);
+    });
+
+    test('I should be able to remove a task from the list', () => alice.removeTask(taskName))
+  });
+
+});
