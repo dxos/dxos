0d486cf7a297ff2feef1e138f93593c25446518a
Vladyslav Yatsenko <jacenko.vlad@gmail.com>
2021-07-05 17:54:17 +0200

    Revert "Remove changelogs"
    This reverts commit 82624be4a050905ab593c8b0b8418a99ff31582b.

---
+++ packages/wallet/extension/src/content/content.ts
@@ -0,0 +1,22 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import { browser } from 'webextension-polyfill-ts';
+
+(() => {
+  const port = browser.runtime.connect(); // Port to extension's background script.
+
+  port.onMessage.addListener((message, port) => {
+    // Passing through a message from background script back to the app.
+    window.postMessage({ payloadFromContentScriptToApp: message }, '*');
+  });
+
+  window.addEventListener('message', (event) => {
+    const ourPayload = event?.data?.payloadFromAppToContentScript;
+    if (ourPayload) {
+      // passing through a message from the App to the background script.
+      port.postMessage(ourPayload);
+    }
+  });
+})();
