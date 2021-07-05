9698d48401431c7021cb5474aa4fc3eb45869e47
Dmytro Maretskyi <maretskii@gmail.com>
2021-07-05 11:34:08 +0200

    Add test rig

--- packages/echo/object-model/src/object-model.test.ts
+++ packages/echo/object-model/src/object-model.test.ts
@@ -4,41 +4,13 @@
 
 // This file is not compiled or run because it introduces a circular dependency on echo-db
 
-import { sleep } from '@dxos/async';
-import { createId, PublicKey } from '@dxos/crypto';
-import { FeedWriter } from '@dxos/echo-protocol';
-
 import { ObjectModel } from './object-model';
-import { ObjectMutationSet } from './proto';
-
-const createModel = () => {
-  const feedKey = PublicKey.random();
-  const memberKey = PublicKey.random();
-  let seq = 0;
-
-  const writer: FeedWriter<ObjectMutationSet> = {
-    write: async (mutation) => {
-      await sleep(10);
-
-      const meta = {
-        feedKey,
-        memberKey,
-        seq: seq++
-      };
-
-      // TODO(marik-d): Investigate why setImmediate is required.
-      setImmediate(() => model.processor.write({ mutation, meta } as any));
-
-      return meta;
-    }
-  };
-
-  const model = new ObjectModel(ObjectModel.meta, createId(), writer);
-  return model;
-};
+import { TestRig } from './test-rig';
+import expect from 'expect';
+import { it as test } from 'mocha'
 
 it('can set a property', async () => {
-  const model = createModel();
+  const {model} = new TestRig(ObjectModel);
 
   await model.setProperty('foo', 'bar');
 
@@ -46,7 +18,7 @@ it('can set a property', async () => {
 });
 
 it('property updates are optimistically applied', async () => {
-  const model = createModel();
+  const {model} = new TestRig(ObjectModel);
 
   const promise = model.setProperty('foo', 'bar');
 
---
+++ packages/echo/object-model/src/test-rig.ts
@@ -0,0 +1,34 @@
+import { createId } from "@dxos/crypto";
+import { Model, ModelConstructor } from "@dxos/model-factory";
+import { FeedWriter } from '@dxos/echo-protocol';
+import { PublicKey } from '@dxos/crypto'
+
+type ModelMessageOf<M extends Model<any>> = M extends Model<infer T> ? T : never;
+
+export class TestRig<M extends Model<any>> {
+  public readonly model: M;
+
+  constructor(constructor: ModelConstructor<M>) {
+    const feedKey = PublicKey.random();
+    const memberKey = PublicKey.random();
+    let seq = 0;
+
+    const writer: FeedWriter<ModelMessageOf<M>> = {
+      write: async (mutation) => {
+        const meta = {
+          feedKey,
+          memberKey,
+          seq: seq++
+        };
+  
+        // TODO(marik-d): Investigate why setImmediate is required.
+        setImmediate(() => this.model.processor.write({ mutation, meta } as any));
+  
+        return meta;
+      }
+    };
+  
+
+    this.model = new constructor(constructor.meta, createId(), writer)
+  }  
+}
