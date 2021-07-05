83aa517cbb4295ce2b426bbc0a0a8c20f70fc394
Dmytro Maretskyi <maretskii@gmail.com>
2021-07-05 12:12:15 +0200

    Multi-peer replication

--- packages/echo/object-model/src/object-model.test.ts
+++ packages/echo/object-model/src/object-model.test.ts
@@ -11,33 +11,45 @@ import { Timeframe } from '@dxos/echo-protocol';
 import { ObjectModel } from './object-model';
 import { TestRig } from './test-rig';
 
-it('can set a property', async () => {
-  const rig = new TestRig(ObjectModel);
-  const { model } = rig.createPeer();
+describe('TestRig', () => {
+  it('can set a property', async () => {
+    const rig = new TestRig(ObjectModel);
+    const { model } = rig.createPeer();
 
-  await model.setProperty('foo', 'bar');
+    await model.setProperty('foo', 'bar');
 
-  expect(model.getProperty('foo')).toEqual('bar');
-});
+    expect(model.getProperty('foo')).toEqual('bar');
+  });
 
-it('property updates are optimistically applied', async () => {
-  const rig = new TestRig(ObjectModel);
-  const { model } = rig.createPeer();
+  it('property updates are optimistically applied', async () => {
+    const rig = new TestRig(ObjectModel);
+    const { model } = rig.createPeer();
 
-  const promise = model.setProperty('foo', 'bar');
+    const promise = model.setProperty('foo', 'bar');
 
-  expect(model.getProperty('foo')).toEqual('bar');
+    expect(model.getProperty('foo')).toEqual('bar');
 
-  await promise;
-});
+    await promise;
+  });
 
-it('timeframe is updated after a mutation', async () => {
-  const rig = new TestRig(ObjectModel);
-  const peer = rig.createPeer();
+  it('timeframe is updated after a mutation', async () => {
+    const rig = new TestRig(ObjectModel);
+    const peer = rig.createPeer();
 
-  expect(peer.timeframe.get(peer.key)).toEqual(undefined);
+    expect(peer.timeframe.get(peer.key)).toEqual(undefined);
 
-  await peer.model.setProperty('foo', 'bar');
+    await peer.model.setProperty('foo', 'bar');
 
-  expect(peer.timeframe.get(peer.key)).toEqual(0);
-});
+    expect(peer.timeframe.get(peer.key)).toEqual(0);
+  });
+
+  it('two peers', async () => {
+    const rig = new TestRig(ObjectModel);
+    const a = rig.createPeer();
+    const b = rig.createPeer();
+
+    await a.model.setProperty('foo', 'bar');
+
+    expect(b.model.getProperty('foo')).toEqual('bar');
+  });
+})
--- packages/echo/object-model/src/test-rig.ts
+++ packages/echo/object-model/src/test-rig.ts
@@ -6,13 +6,14 @@ import { createId, PublicKey } from '@dxos/crypto';
 import { FeedWriter, Timeframe, WriteReceipt, MutationMeta } from '@dxos/echo-protocol';
 import { Model, ModelConstructor } from '@dxos/model-factory';
 import { ComplexMap } from '@dxos/util';
+import debug from 'debug'
+
+const log = debug('dxos:echo:model-test-rig')
 
 type ModelMessageOf<M extends Model<any>> = M extends Model<infer T> ? T : never;
 
 export class TestRig<M extends Model<any>> {
   private readonly _peers = new ComplexMap<PublicKey, TestPeer<M>>(x => x.toHex())
-
-  private _seq = 0;
 
   constructor (
     private readonly _modelConstructor: ModelConstructor<M>
@@ -36,28 +37,45 @@ export class TestRig<M extends Model<any>> {
   }
 
   private _writeMessage (peerKey: PublicKey, mutation: ModelMessageOf<M>): WriteReceipt {
-    const seq = this._seq++;
+    const seq = this._peers.get(peerKey)!.mutations.length;
 
-    const meta: MutationMeta = {
-      feedKey: peerKey.asUint8Array(),
-      memberKey: peerKey.asUint8Array(),
-      seq
-    };
-
-    for (const peer of this._peers.values()) {
-      // Process the message later, after resolving mutation-write promise. Doing otherwise breaks the model.
-      setImmediate(() => peer.processMutation(meta, mutation));
-    }
+    log(`Write ${peerKey}:${seq}`)
+    this._peers.get(peerKey)!.mutations.push(mutation);
+    
+    // Process the message later, after resolving mutation-write promise. Doing otherwise breaks the model.
+    setImmediate(() => this._replicate())
 
     return {
       feedKey: peerKey,
       seq
     };
   }
+
+  private _replicate() {
+    for (const peer of this._peers.values()) {
+      for(const [feed, {mutations}] of this._peers) {
+        const timeframeSeq = peer.timeframe.get(feed)
+        const startingIndex = timeframeSeq === undefined ? 0 : timeframeSeq + 1
+        log(`Replicating feed ${feed} -> ${peer.key} range [${startingIndex}; ${mutations.length})`)
+
+        for(let i = startingIndex; i < mutations.length; i++) {
+          const meta: MutationMeta = {
+            feedKey: feed.asUint8Array(),
+            memberKey: feed.asUint8Array(),
+            seq: i,
+          };
+          log(`Process ${feed}:${i} -> ${peer.key}`)
+          peer.processMutation(meta, mutations[i])
+        }
+      }
+    }
+  }
 }
 
 export class TestPeer<M extends Model<any>> {
   public timeframe = new Timeframe()
+
+  public mutations: ModelMessageOf<M>[] = [];
 
   constructor (
     public readonly model: M,
