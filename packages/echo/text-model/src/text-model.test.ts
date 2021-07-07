77fc635cd0e5f1fc80dcf9b9221ea1311ee0fcb3
Dmytro Maretskyi <maretskii@gmail.com>
2021-07-07 10:29:16 +0200

    Add conflict test

--- packages/echo/text-model/src/text-model.test.ts
+++ packages/echo/text-model/src/text-model.test.ts
@@ -2,6 +2,9 @@
 // Copyright 2020 DXOS.org
 //
 
+import expect from 'expect';
+import { it as test } from 'mocha';
+
 import { createModelTestBench } from '@dxos/echo-db';
 
 import { TextModel } from './text-model';
@@ -40,5 +43,20 @@ describe('TextModel', () => {
     for await (const peer of peers) {
       await peer.close();
     }
+  });
+
+  // This is a race condition and sometimes passes and sometimes not.
+  test.skip('conflict', async () => {
+    const { items: [item1, item2] } = await createModelTestBench({ model: TextModel });
+    item1.model.modelUpdate.on(() => console.log(`m1 ${item1.model.textContent}`));
+    item2.model.modelUpdate.on(() => console.log(`m2 ${item1.model.textContent}`));
+
+    item1.model.insert(0, 'Hello');
+    await item2.model.modelUpdate.waitForCount(1);
+    item1.model.insert(5, ' world');
+    item2.model.insert(5, '!');
+
+    await item1.model.modelUpdate.waitForCount(1);
+    expect(item1.model.textContent).toBe('Hello world!');
   });
 });
