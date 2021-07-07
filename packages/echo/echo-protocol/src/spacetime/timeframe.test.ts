55881e29ec806367c2ecceb4e9de3eaae6dc738f
Dmytro Maretskyi <maretskii@gmail.com>
2021-07-07 15:32:47 +0200

    Add Tiemframe tests

--- packages/echo/echo-protocol/src/spacetime/timeframe.test.ts
+++ packages/echo/echo-protocol/src/spacetime/timeframe.test.ts
@@ -129,4 +129,66 @@ describe('spacetime', () => {
       expect(tf3.frames()).toHaveLength(2);
     }
   });
+
+  describe('compare', () => {
+    const feedA = PublicKey.random();
+    const feedB = PublicKey.random();
+    const feedC = PublicKey.random();
+
+    test('comparable', () => {
+      const a = new Timeframe([[feedA, 5], [feedB, 3]]);
+      const b = new Timeframe([[feedA, 10], [feedB, 3]]);
+
+      expect(Timeframe.compare(a, b)).toEqual(-1);
+      expect(Timeframe.compare(b, a)).toEqual(1);
+    });
+
+    test('comparable 2', () => {
+      const a = new Timeframe([[feedA, 5]]);
+      const b = new Timeframe([[feedA, 10]]);
+
+      expect(Timeframe.compare(a, b)).toEqual(-1);
+      expect(Timeframe.compare(b, a)).toEqual(1);
+    });
+
+    test('comparable 3', () => {
+      const a = new Timeframe([[feedB, 3]]);
+      const b = new Timeframe([[feedA, 10], [feedB, 3]]);
+
+      expect(Timeframe.compare(a, b)).toEqual(-1);
+      expect(Timeframe.compare(b, a)).toEqual(1);
+    });
+
+    test('equal', () => {
+      const a = new Timeframe([[feedA, 5], [feedB, 3]]);
+      const b = new Timeframe([[feedA, 5], [feedB, 3]]);
+
+      expect(Timeframe.compare(a, b)).toEqual(0);
+      expect(Timeframe.compare(b, a)).toEqual(0);
+    });
+
+    test('non-comparable', () => {
+      const a = new Timeframe([[feedA, 5]]);
+      const b = new Timeframe([[feedB, 3]]);
+
+      expect(Timeframe.compare(a, b)).toEqual(null);
+      expect(Timeframe.compare(b, a)).toEqual(null);
+    });
+
+    test('non-comparable 2', () => {
+      const a = new Timeframe([[feedA, 5], [feedC, 7]]);
+      const b = new Timeframe([[feedB, 3], [feedC, 7]]);
+
+      expect(Timeframe.compare(a, b)).toEqual(null);
+      expect(Timeframe.compare(b, a)).toEqual(null);
+    });
+
+    test('non-comparable 3', () => {
+      const a = new Timeframe([[feedA, 5], [feedB, 3]]);
+      const b = new Timeframe([[feedB, 7]]);
+
+      expect(Timeframe.compare(a, b)).toEqual(null);
+      expect(Timeframe.compare(b, a)).toEqual(null);
+    });
+  });
 });
