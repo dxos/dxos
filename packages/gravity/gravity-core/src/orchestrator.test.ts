542a18b681a0189a836242ba06f46852f6d85ad1
Dmytro Maretskyi <maretskii@gmail.com>
2021-07-09 14:23:29 +0200

    Lint

--- packages/gravity/gravity-core/src/orchestrator.test.ts
+++ packages/gravity/gravity-core/src/orchestrator.test.ts
@@ -1,12 +1,17 @@
-import { Orchestrator } from "./orchestrator";
-import { it as test } from 'mocha'
+//
+// Copyright 2021 DXOS.org
+//
+
+import { it as test } from 'mocha';
+
+import { Orchestrator } from './orchestrator';
 
 describe('Orchestrator', () => {
   test('start & stop', async () => {
     const orchestrator = await Orchestrator.create({ local: true });
-  
+
     await orchestrator.start();
-  
+
     await orchestrator.destroy();
   });
-})
+});
