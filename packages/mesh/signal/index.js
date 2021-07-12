f98315340ad4bba6ceb2ab24fb85f950eee6f26e
Przemyslaw Rzad <przemek@ethworks.io>
2021-07-12 13:09:53 +0200

    Add signal to the monorepo (#212)

---
+++ packages/mesh/signal/index.js
@@ -0,0 +1,5 @@
+//
+// Copyright 2020 DxOS.
+//
+
+module.exports = require('./src/broker');
---
+++ packages/mesh/signal/src/signal/index.js
@@ -0,0 +1,8 @@
+//
+// Copyright 2020 DxOS.
+//
+
+const { PeerMap } = require('./peer-map');
+const { SignalServer } = require('./signal-server');
+
+module.exports = { PeerMap, SignalServer };
