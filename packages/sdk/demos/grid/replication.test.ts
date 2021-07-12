3018c55603dfb4e1f262ff381117a67f709cdcb7
rzadp <roopert7@gmail.com>
2021-07-12 15:51:22 +0200

    refactor

--- packages/sdk/demos/grid/replication.test.ts
+++ packages/sdk/demos/grid/replication.test.ts
@@ -19,38 +19,49 @@ describe('Replication in a grid', function () {
   const primaryUrl = 'http://localhost:9001/iframe.html?id=demo--replication-grid&viewMode=story'
   const screenWidth = robot.getScreenSize().width;
   const screenHeight = robot.getScreenSize().height;
+  const clientWidth = screenWidth / COLUMNS
+  const clientHeight = screenHeight / ROWS
   let inviter: Client;
   let party: Party;
+  let invitation: string
 
   before(async function () {
     inviter = new Client(ONLINE_CONFIG);
     await inviter.initialize();
     await inviter.createProfile({ ...createKeyPair(), username: 'inviter' });
     party = await inviter.echo.createParty();
+
+    const invitationDescriptor = await party.createInvitation({
+      secretProvider: async () => Buffer.from('0000'),
+      secretValidator: async () => true
+    });
+    invitation = JSON.stringify(invitationDescriptor.toQueryParameters());
   });
+
+  const createGrid = async (): Promise<Browser[]> => {
+    const result: Browser[] = []
+    
+    for (let row = 0; row < ROWS; row++) {
+      for (let col = 0; col < COLUMNS; col++) {
+        const gridUser = new Browser()
+        await gridUser.launchBrowser(chromium, primaryUrl, {args: [
+          `--window-position=${clientWidth*col},${clientHeight*row}`,
+          `--window-size=${clientWidth},${clientHeight}`
+        ]})
+        await gridUser.page!.fill('#start-dialog-invitation-input', invitation)
+        gridUser.page!.click('//span[text()=\'Join Party\']') // no await, go to next client
+        result.push(gridUser)
+      }
+    }
+
+    return result;
+  }
 
   after(async () => {
     // Close the browsers manually...
   });
 
   it('Opens 8 clients', async () => {
-    const clientWidth = screenWidth / COLUMNS
-    const clientHeight = screenHeight / ROWS
-    for (let row = 0; row < ROWS; row++) {
-      for (let col = 0; col < COLUMNS; col++) {
-        const invitation = await party.createInvitation({
-          secretProvider: async () => Buffer.from('0000'),
-          secretValidator: async () => true
-        });
-        const invitationText = JSON.stringify(invitation.toQueryParameters());
-        const gridUser = new Browser()
-        await gridUser.launchBrowser(chromium, primaryUrl, {args: [
-          `--window-position=${clientWidth*col},${clientHeight*row}`,
-          `--window-size=${clientWidth},${clientHeight}`
-        ]})
-        await gridUser.page!.fill('#start-dialog-invitation-input', invitationText)
-        gridUser.page!.click('//span[text()=\'Join Party\']') // no await, go to next client
-      }
-    }
+    await createGrid();
   })
 });
