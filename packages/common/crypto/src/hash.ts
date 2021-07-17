042bbaeec186cd07f0aae25ec9230062b5472abd
Josiah Witt <josiah@witt.life>
2021-07-17 13:59:47 -0400

    feat(crypto): use webcrypto for hashing

--- packages/common/crypto/src/hash.ts
+++ packages/common/crypto/src/hash.ts
@@ -5,44 +5,56 @@
 import assert from 'assert';
 import CryptoJS from 'crypto-js';
 
+// TODO(wittjosiah): node webcrypto types
+import { webcrypto as crypto } from 'crypto';
+
+// @ts-ignore TODO
+const { subtle } = crypto;
+
 /**
  *
  * @param algorithm
  * @param plaintext
- * @returns {string}
+ * @returns {Promise<string>}
  * @private
  */
-const _hash = (algorithm: string, plaintext: string) => {
+const _hash = async (algorithm: string, plaintext: string) => {
   assert(typeof algorithm === 'string');
   assert(typeof plaintext === 'string');
 
-  return CryptoJS[algorithm](plaintext).toString();
+  if (algorithm === 'RIPEMD160') {
+    return CryptoJS[algorithm](plaintext).toString();
+  } else {
+    const hash = await subtle.digest(algorithm, plaintext);
+    const buffer = Buffer.from(hash)
+    return buffer.toString('hex');
+  }
 };
 
 /**
  * Creates a SHA-1 hash of the supplied string, returned as a hexadecimal string.
  * @param {string} text
- * @returns {string}
+ * @returns {Promise<string>}
  */
-export const sha1 = (text: string) => _hash('SHA1', text);
+export const sha1 = async (text: string) => _hash('SHA-1', text);
 
 /**
  * Creates a SHA-256 hash of the supplied string, returned as a hexadecimal string.
  * @param {string} text
- * @returns {string}
+ * @returns {Promise<string>}
  */
-export const sha256 = (text: string) => _hash('SHA256', text);
+export const sha256 = async (text: string) => _hash('SHA-256', text);
 
 /**
  * Creates a SHA-512 hash of the supplied string, returned as a hexadecimal string.
  * @param {string} text
- * @returns {string}
+ * @returns {Promise<string>}
  */
-export const sha512 = (text: string) => _hash('SHA512', text);
+export const sha512 = async (text: string) => _hash('SHA-512', text);
 
 /**
- * Creates a SHA-512 hash of the supplied string, returned as a hexadecimal string.
+ * Creates a RIPEMD160 hash of the supplied string, returned as a hexadecimal string.
  * @param {string} text
- * @returns {string}
+ * @returns {Promise<string>}
  */
-export const ripemd160 = (text: string) => _hash('RIPEMD160', text);
+export const ripemd160 = async (text: string) => _hash('RIPEMD160', text);
