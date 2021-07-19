eba934e299f6dd895691a80d0910d7928d0e7a3f
rzadp <roopert7@gmail.com>
2021-07-19 11:07:20 +0200

    Loading the extension

--- packages/wallet/extension/src/test/playwright.test.ts
+++ packages/wallet/extension/src/test/playwright.test.ts
@@ -4,23 +4,34 @@
 //
 
 import expect from 'expect';
+import { v4 } from 'uuid';
 import { BrowserContext, chromium, Page, Browser } from 'playwright';
+
+const EXTENSION_PATH = `${process.cwd()}/dist`
 
 describe('Playwright tests for Wallet Extension', async function () {
   this.timeout(30000);
   this.retries(1);
-  let browser: Browser
   let context: BrowserContext
   let page: Page
+  let userDataDir: string
 
   before(async() => {
-    browser = await chromium.launch({headless: !!process.env.CI})
-    context = await browser.newContext();
+    const userDataDir = `${process.cwd()}/src/test/temp/${v4()}`
+    context = await chromium.launchPersistentContext(userDataDir, {
+      headless: !!process.env.CI,
+      args: [
+        '--enable-remote-extensions',
+        `--disable-extensions-except=${EXTENSION_PATH}`,
+        `--load-extension=${EXTENSION_PATH}`,
+        '--no-sandbox',
+      ]
+    })
     page = await context.newPage();
   })
 
   it('Installs properly', async () => {
-    await page.goto('http://google.pl')
+    await page.goto('chrome://extensions/')
   })
 
   after(async() => {
