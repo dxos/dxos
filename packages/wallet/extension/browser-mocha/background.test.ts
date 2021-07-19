f0414dc9eb7154b6a21cb66ba4651977d8c76a7e
rzadp <roopert7@gmail.com>
2021-07-19 15:55:49 +0200

    Trying connetions

---
+++ packages/wallet/extension/browser-mocha/background.test.ts
@@ -0,0 +1,19 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import expect from 'expect';
+import { createWindowMessagePort, useBackgroundService, schema } from '@dxos/wallet-core';
+import { RpcPort, ProtoRpcClient, createRpcClient } from '@dxos/rpc';
+
+
+
+it('Connects to the background service through the content script', async () => {
+  expect(2 + 2).toEqual(4);
+
+  const port = createWindowMessagePort()
+  const service = schema.getService('dxos.wallet.extension.BackgroundService')
+  const rpcClient = createRpcClient(service, {port})
+
+  await rpcClient.open()
+});
