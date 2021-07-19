28d8dd53c4c7c18bc8da78f05353615c4c4919ac
rzadp <roopert7@gmail.com>
2021-07-19 12:40:42 +0200

    Lint

--- packages/wallet/extension/src/test/playwright.test.ts
+++ packages/wallet/extension/src/test/playwright.test.ts
@@ -3,41 +3,39 @@
 // Copyright 2020 DXOS.org
 //
 
-import expect from 'expect';
+import { BrowserContext, chromium, Page } from 'playwright';
 import { v4 } from 'uuid';
-import { BrowserContext, chromium, Page, Browser } from 'playwright';
 
-const EXTENSION_PATH = `${process.cwd()}/dist`
-const EXTENSION_ID = 'nlblcnolkmdjhafclifedafnifbcmpph' // TODO(rzadp): When will this change?
+const EXTENSION_PATH = `${process.cwd()}/dist`;
+const EXTENSION_ID = 'nlblcnolkmdjhafclifedafnifbcmpph'; // TODO(rzadp): When will this change?
 
-describe('Playwright tests for Wallet Extension', async function () {
+describe('Playwright tests for Wallet Extension', function () {
   this.timeout(30000);
   this.retries(1);
-  let context: BrowserContext
-  let page: Page
-  let userDataDir: string
+  let context: BrowserContext;
+  let page: Page;
 
-  before(async() => {
-    const userDataDir = `${process.cwd()}/src/test/temp/${v4()}`
+  before(async () => {
+    const userDataDir = `${process.cwd()}/src/test/temp/${v4()}`;
     context = await chromium.launchPersistentContext(userDataDir, {
       headless: !!process.env.CI,
       args: [
         '--enable-remote-extensions',
         `--disable-extensions-except=${EXTENSION_PATH}`,
         `--load-extension=${EXTENSION_PATH}`,
-        '--no-sandbox',
+        '--no-sandbox'
       ]
-    })
+    });
     page = await context.newPage();
-  })
+  });
 
   it('Installs properly', async () => {
-    await page.goto(`chrome-extension://${EXTENSION_ID}/popup/fullscreen.html`)
-    await page.waitForSelector("//*[contains(text(),'Welcome to DXOS')]")
-  })
+    await page.goto(`chrome-extension://${EXTENSION_ID}/popup/fullscreen.html`);
+    await page.waitForSelector("//*[contains(text(),'Welcome to DXOS')]");
+  });
 
-  after(async() => {
+  after(async () => {
     await page.close();
     await context.close();
-  })
+  });
 });
