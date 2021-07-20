e9eb54329925f7fc28dab7230d5f8911a4334b30
rzadp <roopert7@gmail.com>
2021-07-20 10:04:05 +0200

    Extend tests

--- packages/wallet/extension/browser-mocha/background.test.ts
+++ packages/wallet/extension/browser-mocha/background.test.ts
@@ -3,15 +3,37 @@
 //
 
 import expect from 'expect';
-import { createWindowMessagePort, useBackgroundService, schema } from '@dxos/wallet-core';
+import { createWindowMessagePort, useBackgroundService, schema, BackgroundService } from '@dxos/wallet-core';
 import { RpcPort, ProtoRpcClient, createRpcClient } from '@dxos/rpc';
+import faker from 'faker';
 
-it('Connects to the background service through the content script', async () => {
-  expect(2 + 2).toEqual(4);
 
-  const port = createWindowMessagePort()
-  const service = schema.getService('dxos.wallet.extension.BackgroundService')
-  const rpcClient = createRpcClient(service, {port})
+describe('Background service tested through the content script', () => {
+  let rpcClient: ProtoRpcClient<BackgroundService>
 
-  await rpcClient.open()
-});
+  before(async () => {
+    const port = createWindowMessagePort()
+    const service = schema.getService('dxos.wallet.extension.BackgroundService')
+    rpcClient = createRpcClient(service, {port})
+  
+    await rpcClient.open()
+  })
+
+  it('Retrieves empty profile at first', async () => {
+    const profile = await rpcClient.rpc.GetProfile({});
+    expect(profile.publicKey).toBeUndefined();
+    expect(profile.username).toBeUndefined();
+  });
+  
+  it('Can create a profile', async () => {
+    const username = faker.name.firstName();
+    await rpcClient.rpc.CreateProfile({username})
+    const profile = await rpcClient.rpc.GetProfile({})
+    expect(profile.username).toEqual(username)
+    expect(profile.publicKey).toBeDefined();
+  });
+
+  after(async () => {
+    await rpcClient.close();
+  })
+})
