50c5b0e3e10e539c1048b5fcda60feca0bef85da
Przemyslaw Rzad <przemek@ethworks.io>
2021-07-22 10:00:01 +0200

    Introduce playwright for Wallet Extension (#264)

---
+++ common/scripts/install-dependencies.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+set -e
+
+apt-get update
+apt-get install -y autoconf automake make g++ libtool libxtst-dev libpng-dev libx11-dev
---
+++ packages/wallet/extension/src/utils/extensionId.ts
@@ -0,0 +1,8 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+// TODO(rzadp): When/how does this change?.
+export const getExtensionId = async () => {
+  return 'nlblcnolkmdjhafclifedafnifbcmpph';
+};
