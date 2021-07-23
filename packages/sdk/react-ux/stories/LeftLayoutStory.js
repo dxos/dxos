11fe94dea775422e26ccda874f793b1ff7ab55c9
rzadp <roopert7@gmail.com>
2021-07-23 10:33:59 +0200

    Lint

--- packages/sdk/react-ux/stories/LeftLayoutStory.js
+++ packages/sdk/react-ux/stories/LeftLayoutStory.js
@@ -4,11 +4,11 @@
 
 import React, { useState, useCallback } from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import { FullScreen, LeftDrawer } from '../src';
 
--- packages/sdk/react-ux/stories/RightLayoutStory.js
+++ packages/sdk/react-ux/stories/RightLayoutStory.js
@@ -3,12 +3,12 @@
 //
 
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import { FullScreen, RightDrawer } from '../src';
 
