4d5bb74835fb0464a475ba7d2cc23afbd6e2fbff
rzadp <roopert7@gmail.com>
2021-07-27 10:50:58 +0200

    Simple test

---
+++ packages/common/random-access-multi-storage/src/browser-mocha.test.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import expect from 'expect';
+import 'source-map-support/register';
+
+describe('testing browser storages', () => {
+  it('simple', async () => {
+    expect(2+2).toEqual(4);
+  });
+});
