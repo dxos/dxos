671e6fa12833ab3bd92384538f340d1f198da94a
Zarco Nontol <zarconontol@gmail.com>
2021-08-04 16:59:45 -0300

    fix: disable first party feature

--- packages/sdk/tutorials/apps/tasks-app/src/components/Root.js
+++ packages/sdk/tutorials/apps/tasks-app/src/components/Root.js
@@ -15,13 +15,13 @@ import ProfileDialog from './ProfileDialog';
 const Root = () => {
   const client = useClient();
   const profile = useProfile();
-  
-  const createFirstParty = async () => {
-    // Default Party Creation
-    const partyDemoTitle = 'My First Party!';
-    const party = await client.echo.createParty({ title: partyDemoTitle });
-    await party.setProperty('title', partyDemoTitle);
-  };
+  // TODO(zarco): add a get parameter to trigger the first party so we avoid triggering all the time.
+  // const createFirstParty = async () => {
+  //   // Default Party Creation
+  //   const partyDemoTitle = 'My First Party!';
+  //   const party = await client.echo.createParty({ title: partyDemoTitle });
+  //   await party.setProperty('title', partyDemoTitle);
+  // };
 
   if (!profile) {
     const handleRegistration = async ({ username }) => {
