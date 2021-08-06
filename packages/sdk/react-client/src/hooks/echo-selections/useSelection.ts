e3ec8841ce39863c39cab0cf23c411aa1f8fdad9
Dmytro Maretskyi <maretskii@gmail.com>
2021-08-06 12:52:15 +0200

    Accept falsy values in useSelection

--- packages/sdk/react-client/src/hooks/echo-selections/useSelection.ts
+++ packages/sdk/react-client/src/hooks/echo-selections/useSelection.ts
@@ -5,27 +5,36 @@
 import { useEffect, useState } from 'react';
 
 import { SelectionResult } from '@dxos/echo-db';
+import { Falsy } from '@dxos/util';
 
 /**
  * Hook to generate values from a selection using a selector function.
+ * 
+ * NOTE: 
+ * All values that may change the selection result,
+ * apart from changes in ECHO database itself, must be passed to deps array
+ * for updates to work correctly.
  *
- * @param [selection] Source selection (can be initially undefined).
- * @param selector Callback to generate data from the source selection.
- * @param [deps] Array of values that trigger the selector when changed.
+ * @param selectionResult Selection from which to query data. Can be falsy - in that case the hook will return undefined.
+ * @param deps Array of values that trigger the selector when changed.
  */
 export function useSelection<T> (
-  selectionResult: SelectionResult<T>,
+  selectionResult: SelectionResult<T> | Falsy,
   deps: readonly any[] = []
 ): T | undefined {
-  const [data, setData] = useState(() => selectionResult.getValue());
+  const [data, setData] = useState(() => !!selectionResult ? selectionResult.getValue() : undefined);
 
   // Update data when deps change.
   useEffect(() => {
-    const unsub = selectionResult.update.on(() => {
+    if(selectionResult) {
+      const unsub = selectionResult.update.on(() => {
+        setData(selectionResult.getValue());
+      });
       setData(selectionResult.getValue());
-    });
-    setData(selectionResult.getValue());
-    return unsub;
+      return unsub;
+    } else {
+      setData(undefined);
+    }
   }, deps);
 
   return data;
