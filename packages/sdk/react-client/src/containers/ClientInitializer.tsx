c39e85cacc0a888259fbf7f0c9a06077aa0bbf1f
Zarco Nontol <zarconontol@gmail.com>
2021-08-13 17:07:06 -0300

    feat: change page title and integrate error boundary (#346)
    * feat: change page title and integrate error boundary
    
    * fix: move create client definition inside useEffect
    
    * fix lint issues
    
    * adjust playwright test
    
    * add reset storage test
    
    * add reset storage actions
    
    * add helper title tags for tests
    
    * refactor: Error Boundary moved to react-client
    
    * fix linter and refactor error component temporaly
    
    * adjust error boundary and integrate it within ClientInitializer
    
    * remove console log
    
    * remove comments
    
    * fix(react-client): make error props as optional
    
    * feat: remove material ui from client and add default loader

--- packages/sdk/react-client/src/containers/ClientInitializer.tsx
+++ packages/sdk/react-client/src/containers/ClientInitializer.tsx
@@ -2,44 +2,86 @@
 // Copyright 2020 DXOS.org
 //
 
-import React, { useState, useEffect, ReactNode } from 'react';
-
-import { LinearProgress } from '@material-ui/core';
+import React, { useState, useEffect, ReactNode, ErrorInfo } from 'react';
 
 import { Client, ClientConfig } from '@dxos/client';
 import { MaybePromise } from '@dxos/util';
 
+import { ErrorComponentType, ErrorBoundary, ErrorCallbackType } from '../components/ErrorBoundary';
 import ClientProvider from './ClientProvider';
 
 interface ClientInitializerProperties {
   children?: ReactNode
   config?: ClientConfig | (() => MaybePromise<ClientConfig>)
+  errorComponent?: React.ComponentType<ErrorComponentType>,
+  loaderComponent?: React.ComponentType,
+  onError?: ErrorCallbackType
 }
 
 /**
  * Initializes and provides a client instance given a config object or config generator.
  * To be used with `useClient` hook.
  */
-const ClientInitializer = ({ children, config = {} }: ClientInitializerProperties) => {
+const ClientInitializer = ({ children, config = {}, errorComponent, loaderComponent, onError }: ClientInitializerProperties) => {
   const [client, setClient] = useState<Client | undefined>();
-
+  const [error, setError] = useState<undefined | Error>(undefined);
   useEffect(() => {
-    setImmediate(async () => {
-      const client = new Client(typeof config === 'function' ? await config() : config);
-      await client.initialize();
-
-      setClient(client);
-    });
+    const createClient = async () => {
+      try {
+        const client = new Client(typeof config === 'function' ? await config() : config);
+        await client.initialize();
+        setClient(client);
+      } catch (error) {
+        setError(error);
+        console.error(error);
+        handleError(error);
+      }
+    };
+    setImmediate(createClient);
   }, []);
 
+  const handleRestart = () => {
+    window.location.reload();
+  };
+
+  const handleReset = async () => {
+    if (client) {
+      await client.reset();
+    }
+  };
+
+  const handleError = (error: Error, errorInfo?: ErrorInfo) => {
+    console.error(error, errorInfo);
+    if (onError) {
+      onError(error, errorInfo);
+    }
+  };
+
+  if (error && errorComponent) {
+    const ErrorComponent = errorComponent;
+    return (<ErrorComponent onRestart={handleRestart} onReset={handleReset} error={error} />);
+  }
+
   if (!client) {
-    return <LinearProgress />;
+    if (loaderComponent) {
+      const ExternalLoaderComponent = loaderComponent;
+      return (<ExternalLoaderComponent />);
+    }
+
+    return <div>Loading Client...</div>;
   }
 
   return (
-    <ClientProvider client={client}>
-      {children}
-    </ClientProvider>
+    <ErrorBoundary
+      // It's important to print the error to the console here so sentry can report it.
+      onError={handleError}
+      onRestart={handleRestart}
+      onReset={handleReset}
+    >
+      <ClientProvider client={client}>
+        {children}
+      </ClientProvider>
+    </ErrorBoundary>
   );
 };
 
