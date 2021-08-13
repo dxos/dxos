3a1bddce6dcbd7e4d4b6b5e0444253daec2a1f42
rzadp <roopert7@gmail.com>
2021-08-13 10:26:17 +0200

    Lintino

--- packages/sdk/devtools/src/hooks/bridge.ts
+++ packages/sdk/devtools/src/hooks/bridge.ts
@@ -4,9 +4,10 @@
 
 import { useContext } from 'react';
 
+import { raise } from '@dxos/debug';
+
 import { Context } from '../Provider';
 import { DevtoolsBridge } from '../bridge';
-import { raise } from '@dxos/debug';
 
 export const useBridge = (): [DevtoolsBridge] => {
   const { bridge } = useContext(Context) ?? raise(new Error('Devtools context not set'));
