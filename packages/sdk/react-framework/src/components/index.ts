d50a6944b4955c9cef6758168177d990a31f8bd0
rzadp <roopert7@gmail.com>
2021-08-20 10:23:21 +0200

    Place react-framework in the monorepo

--- packages/sdk/react-framework/src/components/index.ts
+++ packages/sdk/react-framework/src/components/index.ts
@@ -4,13 +4,11 @@
 
 import DialogHeading from './DialogHeading';
 import ErrorView from './ErrorView';
-import PinlessRedeemDialog from './PinlessRedeemDialog';
 import ProfileDialog from './ProfileDialog';
 import RedeemDialog from './RedeemDialog';
 
 export {
   DialogHeading,
-  PinlessRedeemDialog,
   RedeemDialog,
   ProfileDialog,
   ErrorView
