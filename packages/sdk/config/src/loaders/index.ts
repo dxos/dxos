1aec9de9ab46bd00e1973e3946dd9928ba8edec7
Vitaliy Mysak <vitaliy@ethworks.io>
2021-08-23 18:36:16 +0200

    sdk/config: export browser variables
    Fixes typescript error when importing `Dynamics` from `@dxos/config`.
    
    We swap index/browser files during build in ConfigPlugin.js,
    but typescript does not know that and complains,
    so we need to make signatures of those files the same.

--- packages/sdk/config/src/loaders/index.ts
+++ packages/sdk/config/src/loaders/index.ts
@@ -14,6 +14,14 @@ import { mapFromKeyValues } from '../config';
 
 const DEFAULT_BASE_PATH = path.resolve(process.cwd(), 'config');
 
+// NOTE: we need to export LocalStorage and Dynamics
+// for typescript to typecheck browser code.
+// See ConfigPlugin.js:33
+
+export const LocalStorage = () => 0;
+
+export const Dynamics = () => 0;
+
 export const Envs = (basePath = DEFAULT_BASE_PATH) => {
   const content = yaml.load(fs.readFileSync(path.resolve(basePath, 'envs-map.yml'), { encoding: 'utf8' }));
   return mapFromKeyValues(content, process.env);
