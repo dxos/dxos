1f7d6fd969710f86a2c55c46b19f5d6cd4b44f3e
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-08-30 14:25:20 +0200

    Add simulated network latency & test party members display names (#398)
    * Add test for members joining parties
    
    * Remove .only
    
    * Add doc comment
    
    * Lint
    
    * Add simulated network latency
    
    * Fix tests
    
    * Lint
    
    * Remove .only
    
    * Fix text-model tests

--- packages/echo/text-model/src/text-model.test.ts
+++ packages/echo/text-model/src/text-model.test.ts
@@ -11,7 +11,7 @@ import { TextModel } from './text-model';
 
 describe('TextModel', () => {
   test('insert', async () => {
-    const { peers, items: [item1, item2] } = await createModelTestBench({ model: TextModel });
+    const { items: [item1, item2] } = await createModelTestBench({ model: TextModel });
 
     item1.model.insert(0, 'Hello World!');
 
@@ -25,7 +25,6 @@ describe('TextModel', () => {
     expect(item1.model.textContent).toBe('Hello DXOS World!');
 
     // TODO(burdon): Errors. Race condition?
-    console.log(peers);
     // for await (const peer of peers) {
     //   await peer.close();
     // }
@@ -47,19 +46,21 @@ describe('TextModel', () => {
 
   test('conflict', async () => {
     const { items: [item1, item2] } = await createModelTestBench({ model: TextModel });
-    item1.model.modelUpdate.on(() => console.log(`m1 ${item1.model.textContent}`));
-    item2.model.modelUpdate.on(() => console.log(`m2 ${item2.model.textContent}`));
+    // item1.model.modelUpdate.on(() => console.log(`m1 ${item1.model.textContent}`));
+    // item2.model.modelUpdate.on(() => console.log(`m2 ${item2.model.textContent}`));
 
     item1.model.insert(0, 'Hello');
     await item2.model.modelUpdate.waitForCount(1);
 
+    const updatesPromise = Promise.all([
+      item1.model.modelUpdate.waitForCount(2),
+      item2.model.modelUpdate.waitForCount(2)
+    ]);
+
     item1.model.insert(5, '!');
     item2.model.insert(5, '?');
 
-    await Promise.all([
-      item1.model.modelUpdate.waitForCount(1),
-      item2.model.modelUpdate.waitForCount(1)
-    ]);
+    await updatesPromise;
 
     expect(item1.model.textContent).toBe(item2.model.textContent);
   });
