9b0565e5f699c74cc7665efc64e315a8fbe7e08e
Dmytro Maretskyi <maretskii@gmail.com>
2021-08-30 11:55:22 +0200

    Use createTestBroker in more places

--- packages/gravity/browser-tests/browser-test/setup.js
+++ packages/gravity/browser-tests/browser-test/setup.js
@@ -4,11 +4,8 @@
 
 /* eslint @typescript-eslint/no-var-requires: 0 */
 
-const { randomBytes } = require('@dxos/crypto');
-const { createBroker } = require('@dxos/signal');
+const { createTestBroker } = require('@dxos/signal');
 
 module.exports = async () => {
-  const topic = randomBytes();
-
-  await createBroker(topic, { logLevel: 'warn', hyperswarm: { bootstrap: false } }).start();
+  await createTestBroker()
 };
