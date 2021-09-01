c9fb79981ec16d2256bbba201b170357d9beaa4b
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-09-01 12:13:17 +0200

    Add global lint fix script (#415)
    * Skip test
    
    * Add global lint fix command

--- packages/mesh/network-manager/src/network-manager.browser-test.ts
+++ packages/mesh/network-manager/src/network-manager.browser-test.ts
@@ -2,14 +2,15 @@
 // Copyright 2021 DXOS.org
 //
 
-import { webRTCTests } from './network-manager.blueprint-test';
+import { inMemoryTests, webRTCTests } from './network-manager.blueprint-test';
 
 describe('Network manager', () => {
   describe('WebRTC transport', () => {
     webRTCTests();
   }).timeout(10_000);
 
-  // describe('In-memory transport', () => {
-  //   inMemoryTests();
-  // }).timeout(30_000);
+  // TODO(marik-d): In-memory tests seem to be broken in the browser. Need more investigation.
+  describe.skip('In-memory transport', () => {
+    inMemoryTests();
+  }); // .timeout(30_000);
 });
