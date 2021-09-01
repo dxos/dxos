056459da268418d955d28b75e442dc8ba6e43be3
Vladyslav Yatsenko <jacenko.vlad@gmail.com>
2021-09-01 18:02:16 +0200

    Add test

---
+++ packages/echo/echo-db/src/parties/metadata-store.test.ts
@@ -0,0 +1,31 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import expect from 'expect';
+
+import { PublicKey } from '@dxos/crypto';
+
+import { createRamStorage } from '../util';
+import { MetadataStore } from './metadata-store';
+
+describe('MetadataStore', async () => {
+  it('in-memory', async () => {
+    const store = new MetadataStore(createRamStorage());
+  
+    const empty = await store.load();
+    expect(empty.parties?.length).toBe(0);
+  
+    const publicKey = PublicKey.random();
+    await store.save({ parties: [{ key: publicKey }]});
+    const updated = await store.load();
+    expect(updated.parties?.length).toBe(1);
+    expect(updated.parties?.[0].key).toEqual(publicKey);
+
+    // TODO(yivlad): clearing storage doesn't work
+    // await store.clear();
+    // const cleared = await store.load();
+    // console.log(cleared);
+    // expect(cleared.parties?.length).toBe(0);
+  });
+});
