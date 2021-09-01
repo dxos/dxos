5aac2e527a9d91e9e069a64704960ba90ced9077
Dmytro Maretskyi <maretskii@gmail.com>
2021-09-01 10:02:26 +0200

    Gracefully close the procol

--- packages/mesh/protocol/src/testutils.ts
+++ packages/mesh/protocol/src/testutils.ts
@@ -5,7 +5,7 @@ import { Protocol } from "./protocol";
 /**
  * Connect two protocols in-memory.
  * 
- * The returned promise will most likely reject after protocols were closed, so its best to ignore the error.
+ * If protocol is closed because of an error, this error will be propagated through the returned promise.
  */
 export function pipeProtocols(a: Protocol, b: Protocol) {
   return createPromiseFromCallback(cb => pump(a.stream as any, b.stream as any, a.stream as any, cb))
