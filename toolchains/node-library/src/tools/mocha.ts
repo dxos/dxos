0c53806f9183ffd6049f8639d66794585d530c6d
rzadp <roopert7@gmail.com>
2021-09-08 15:25:54 +0200

    No force close

--- toolchains/node-library/src/tools/mocha.ts
+++ toolchains/node-library/src/tools/mocha.ts
@@ -5,7 +5,7 @@
 import { execTool } from './common';
 
 export function execMocha (additionalArgs: string[] = []) {
-  execTool('mocha', ['-r', 'ts-node/register/transpile-only', '--exit', '-t', '15000', 'src/**/*.test.ts', ...additionalArgs], {
+  execTool('mocha', ['-r', 'ts-node/register/transpile-only', '--no-exit', '-t', '15000', 'src/**/*.test.ts', ...additionalArgs], {
     stdio: ['inherit', 'inherit', process.stdout] // Redirect stderr > stdout.
   });
 }
