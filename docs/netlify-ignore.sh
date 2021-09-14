8b253495434905861995d8308ab038e58b722200
rzadp <roopert7@gmail.com>
2021-09-14 15:29:04 +0200

    Cosmetic

--- docs/netlify-ignore.sh
+++ docs/netlify-ignore.sh
@@ -2,7 +2,7 @@
 
 if [[ $BRANCH == "production" ]]
 then
-  exit 1 # Always deploy on production branch
+  exit 1 # Always deploy on production branch.
 else
   # Deploy only if something has changed.
   git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF .
