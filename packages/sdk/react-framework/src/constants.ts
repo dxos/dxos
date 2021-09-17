3e4dd52709291b913fc275fa6d1db806ad52b3a9
Daniel Toloto <dctoloto@gmail.com>
2021-09-17 17:18:26 -0300

    fix(react-framework): fix eslint errors

--- packages/sdk/react-framework/src/constants.ts
+++ packages/sdk/react-framework/src/constants.ts
@@ -6,5 +6,5 @@ export const REDEEM_ERRORS = {
   INVALID_CODE: 'Invalid invitation code.',
   NOT_AUTHORIZED: 'Invitation not authorized.',
   TIMEOUT: 'Timed out.',
-  ALREADY_CONNECTED: 'Already connected.',
+  ALREADY_CONNECTED: 'Already connected.'
 };
