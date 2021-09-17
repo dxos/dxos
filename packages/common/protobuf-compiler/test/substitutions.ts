ac88ce7fdf4ee14bd5c0902c7f96638e65025c01
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-09-17 13:21:14 +0200

    Fix primitive type encoding in protobuf & add a timestamp example (#470)
    * Fix primitive types & add a timestamp test
    
    * Lint
    
    * Update snapshots

--- packages/common/protobuf-compiler/test/substitutions.ts
+++ packages/common/protobuf-compiler/test/substitutions.ts
@@ -1,6 +1,7 @@
 import { MyKey } from "./my-key";
 import { MyInterfaceSubstitution } from './my-interface'
 import { anySubstitutions, Schema as CodecSchema } from "@dxos/codec-protobuf";
+import { Timestamp } from "./gen/google/protobuf";
 
 export default {
   'dxos.test.Key': {
@@ -10,6 +11,16 @@ export default {
   'dxos.test.SubstitutedByInterface': {
     encode: (value: MyInterfaceSubstitution) => value,
     decode: (value: any): MyInterfaceSubstitution => value,
+  },
+  'google.protobuf.Timestamp': {
+    encode: (value: Date): Timestamp => {
+      const unixMilliseconds = value.getTime()
+      return {
+        seconds: (unixMilliseconds / 1000).toString(),
+        nanos: (unixMilliseconds % 1000) * 1e6,
+      }
+    },
+    decode: (value: Timestamp): Date => new Date(parseInt(value.seconds ?? '0') * 1000 + (value.nanos ?? 0) / 1e6),
   },
   ...anySubstitutions,
 };
