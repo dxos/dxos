ee597441e8b4efd833cc2eaf489bcfbfcf730c8a
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2021-09-29 17:35:56 +0200

    Fix devtools extension start (#505)

--- packages/sdk/devtools/src/containers/ItemsViewer.tsx
+++ packages/sdk/devtools/src/containers/ItemsViewer.tsx
@@ -13,8 +13,12 @@ export default function ItemsViewer () {
   const devtoolsHost = useDevtoolsHost();
   const data = useStream(() => devtoolsHost.SubscribeToItems({}));
 
-  if (!data?.data) {
+  if (data === undefined) {
     return <div> Loading items... </div>;
+  }
+
+  if (data.data === undefined) {
+    return <div> No items to display. </div>;
   }
 
   return (
