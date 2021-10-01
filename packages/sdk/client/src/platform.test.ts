1f4f5e4fee96fa65a8647ea64ee7c25d32ec75c3
Vladyslav Yatsenko <jacenko.vlad@gmail.com>
2021-10-01 17:55:15 +0200

    Replace jest with mocha in client

--- packages/sdk/client/src/platform.test.ts
+++ packages/sdk/client/src/platform.test.ts
@@ -2,6 +2,9 @@
 // Copyright 2020 DXOS.org
 //
 
+import expect from 'expect';
+import { it as test } from 'mocha';
+
 import { isNode } from './platform';
 
 test('knows when running in node', () => {
