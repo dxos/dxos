abe30f6abf0e9efb719a8f1deb3fde63b78b55d7
Vladyslav Yatsenko <jacenko.vlad@gmail.com>
2021-10-01 18:01:01 +0200

    Introduce react-registry-client

---
+++ packages/sdk/react-registry-client/src/hooks/queries/useResources.ts
@@ -0,0 +1,37 @@
+//
+// Copyright 2020 DXOS.org
+//
+import { useEffect, useState } from 'react';
+import { CID, DomainInfo, IQuery, IRegistryClient, RegistryRecord, Resource } from '@dxos/registry-client';
+
+import { useRegistry } from "..";
+
+interface Result {
+  resources: Resource[],
+  error?: unknown
+} 
+
+/**
+ * Returns matching resources.
+ */
+ export const useResources = (query?: IQuery): Result => {
+  const registry = useRegistry();
+  const [error, setError] = useState<any>(undefined)
+  const [resources, setResources] = useState<Resource[]>([]);
+
+  useEffect(() => {
+    setImmediate(async () => {
+      try {
+        const resources = await registry?.getResources(query);
+        setResources(resources ?? []);
+      } catch(e: unknown) {
+        setError(e);
+      }
+    });
+  }, [query]);
+
+  return {
+    resources,
+    error
+  };
+};
