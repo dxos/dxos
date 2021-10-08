a24cf26068a8cc2356cb4dcc3a31a0e78dd4d69b
rzadp <roopert7@gmail.com>
2021-10-08 20:20:05 +0200

    progress

--- packages/common/random-access-multi-storage/src/shims.d.ts
+++ packages/common/random-access-multi-storage/src/shims.d.ts
@@ -5,5 +5,4 @@
 declare module 'random-access-memory';
 declare module '@dxos/random-access-chrome-file';
 declare module 'random-access-file';
-declare module 'random-access-web/mutable-file-wrapper';
 declare module 'random-access-idb';
