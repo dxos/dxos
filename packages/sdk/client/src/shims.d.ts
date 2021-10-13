7eb24cbf2d59d95db78ac23064011ac2a9bb68a8
Rich Burdon <rich.burdon@gmail.com>
2021-10-13 14:38:52 -0400

    Fix shims.

--- packages/sdk/client/src/shims.d.ts
+++ packages/sdk/client/src/shims.d.ts
@@ -2,5 +2,10 @@
 // Copyright 2020 DXOS.org
 //
 
-declare module '@wirelineio/registry-client';
+// TODO(burdon): Standardize shimes (e.g., demos/src/@types)?
+
 declare module 'jsondown';
+declare module 'lodash.defaultsdeep';
+
+// TODO(burdon): remove.
+declare module '@wirelineio/registry-client';
