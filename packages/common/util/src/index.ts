2818864a064f206a095aec007374d39aea8d9a7c
rzadp <roopert7@gmail.com>
2021-10-20 11:18:44 +0200

    Only one place for raise

--- packages/common/util/src/index.ts
+++ packages/common/util/src/index.ts
@@ -7,7 +7,6 @@ export * from './json';
 export * from './map';
 export * from './range';
 export * from './types';
-export * from './raise';
 export * from './randomInt';
 export * from './subscription';
 export * from './uint8array';
--- packages/common/util/src/raise.ts
+++
@@ -1,7 +0,0 @@
-//
-// Copyright 2021 DXOS.org
-//
-
-export const raise = (error: Error): never => {
-  throw error;
-};
