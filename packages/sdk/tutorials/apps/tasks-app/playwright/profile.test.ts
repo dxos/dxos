fc8541976238515824a02386b254c4b025a6a814
rzadp <roopert7@gmail.com>
2021-10-20 15:11:41 +0200

    Use mocha in tasks app for playwright

--- packages/sdk/tutorials/apps/tasks-app/playwright/profile.test.ts
+++ packages/sdk/tutorials/apps/tasks-app/playwright/profile.test.ts
@@ -6,23 +6,22 @@ import { firefox } from 'playwright';
 
 import { Browser, TaskApp } from './utils';
 
-describe('Profile Test Cases', () => {
+describe.only('Profile Test Cases', () => {
   const initialUrl = 'http://localhost:3000/';
   let alice: TaskApp;
 
-  beforeAll(() => {
-    jest.setTimeout(10000);
+  before(() => {
     alice = new TaskApp(new Browser());
 
     return alice.browser.launch(firefox, initialUrl);
   });
 
-  afterAll(() => alice.browser.get().close());
+  after(() => alice.browser.get().close());
 
   describe('As a new user starting the app then', () => {
-    test('I should be prompted to create my profile', () => alice.profile.checkCreationIsPrompted())
+    it('I should be prompted to create my profile', () => alice.profile.checkCreationIsPrompted())
 
-    test('I should be able to create my profile', () =>
+    it('I should be able to create my profile', () =>
       alice.profile.create('Alice')
         .then(() => alice.checkAppIsLoaded())
     )
--- packages/sdk/tutorials/apps/tasks-app/playwright/storage.test.ts
+++ packages/sdk/tutorials/apps/tasks-app/playwright/storage.test.ts
@@ -10,8 +10,7 @@ describe('Storage Test Cases', () => {
   const initialUrl = 'http://localhost:3000/';
   let alice: TaskApp;
 
-  beforeAll(async () => {
-    jest.setTimeout(30000);
+  before(async () => {
     alice = new TaskApp(new Browser());
 
     await alice.browser.launch(firefox, initialUrl);
@@ -25,15 +24,15 @@ describe('Storage Test Cases', () => {
     await alice.profile.checkCreationIsPrompted();
   });
 
-  afterAll(() => alice.browser.get().close());
+  after(() => alice.browser.get().close());
 
-  describe.each([
+  [
     { profileName: 'Alice 1', listName: 'My Example List 1', taskName: 'Clone the repo 1', },
     { profileName: 'Alice 2', listName: 'My Example List 2', taskName: 'Clone the repo 2', },
     { profileName: 'Alice 3', listName: 'My Example List 3', taskName: 'Clone the repo 3', },
     { profileName: 'Alice 4', listName: 'My Example List 4', taskName: 'Clone the repo 4', },
     { profileName: 'Alice 5', listName: 'My Example List 5', taskName: 'Clone the repo 5', },
-  ])('As a user with a profile and items created then (%#: %j)', ({ profileName, listName, taskName }) => {
+  ].map(({ profileName, listName, taskName }) => describe(`As a user ${profileName} with a profile and items created then`, () => {
     beforeEach(async () => {
       await alice.profile.create(profileName).then(() => alice.checkAppIsLoaded());
 
@@ -42,10 +41,10 @@ describe('Storage Test Cases', () => {
       await alice.createTask(listName, taskName);
     });
 
-    test('I should be able to reset my storage', async () => {
+    it('I should be able to reset my storage', async () => {
       await alice.profile.resetStorage()
       await alice.profile.checkCreationIsPrompted();
     });
-  });
+  }))
 });
 
