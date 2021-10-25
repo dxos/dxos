a7902e292fce8adff1793c91fe84d3a086b125e3
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2021-10-25 13:02:43 +0200

    Add new implementation of JSONTreeView (#579)

---
+++ packages/sdk/react-components/src/ReactJsonView.tsx
@@ -0,0 +1,35 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import { useTheme } from '@mui/material';
+import React from 'react';
+import ReactJsonView from 'react-json-view';
+
+import { JsonTreeViewProps } from './JsonTreeViewProps';
+
+/**
+ * Displays the config JSON object.
+ */
+export const JsonTreeView = ({
+  data = {},
+  onSelect
+}: JsonTreeViewProps) => {
+  const theme = useTheme();
+
+  // TODO(burdon): Replace with class that allows custom rendering (e.g., CID, links, etc.)
+  // https://www.npmjs.com/package/react-json-view
+  return (
+    <ReactJsonView
+      src={data}
+      theme={theme.palette.mode === 'dark' ? 'tomorrow' : 'rjv-default'}
+      iconStyle='circle'
+      displayDataTypes={false}
+      displayObjectSize={false}
+      quotesOnKeys={false}
+      sortKeys={true}
+      onSelect={onSelect}
+      // collapsed={1}
+    />
+  );
+};
