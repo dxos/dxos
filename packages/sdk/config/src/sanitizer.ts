2f3d1ffa91a98498f9810968a0d5b9f55af6f688
Dmytro Maretskyi <maretskii@gmail.com>
2021-10-27 14:51:14 +0200

    Detect unexepected config keys

--- packages/sdk/config/src/sanitizer.ts
+++ packages/sdk/config/src/sanitizer.ts
@@ -2,6 +2,8 @@
 // Copyright 2021 DXOS.org
 //
 
+import { Type } from 'protobufjs';
+
 import { schema } from './proto/gen';
 import { Config as ConfigObject } from './proto/gen/dxos/config';
 
@@ -15,5 +17,35 @@ export function sanitizeConfig (value: any): ConfigObject {
     console.warn(`Invalid config: ${error}`);
   }
 
+  const ctx: Context = { errors: [] };
+  visitMessage(configRootType.protoType, value, '', ctx);
+  if (ctx.errors.length > 0) {
+    console.warn(`Invalid config:\n${ctx.errors.join('\n')}`);
+  }
+
   return value;
+}
+
+interface Context {
+  errors: string[]
+}
+
+function visitMessage (type: Type, value: any, path: string, context: Context) {
+  for (const key of Object.keys(value)) {
+    if (!type.fields[key]) {
+      context.errors.push(`Unexpected key: ${path}.${key}`);
+      continue;
+    }
+
+    const field = type.fields[key];
+    if (field.repeated) {
+      continue; // TODO(marik-d): Implement.
+    }
+
+    field.resolve();
+    if (!field.resolvedType || !(field.resolvedType instanceof Type)) {
+      continue;
+    }
+    visitMessage(field.resolvedType, value[key], `${path}.${key}`, context);
+  }
 }
