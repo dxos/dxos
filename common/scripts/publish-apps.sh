a332e980c2534dbf8e4808df4704991c92ba509e
rzadp <roopert7@gmail.com>
2021-10-28 15:25:14 +0200

    Optionally create profile

--- common/scripts/publish-apps.sh
+++ common/scripts/publish-apps.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-dx profile init --name $DX_PROFILE --template-url "$DX_PROFILE_URL"
+[[ -e ~/.dx/profile/default ]] || dx profile init --name $DX_PROFILE --template-url "$DX_PROFILE_URL"
 DXOS_DOMAIN="${DXOS_DOMAIN:-dxos}"
 
 for APP_PATH in "packages/sdk/demos" "packages/sdk/tutorials/apps/tasks-app"
