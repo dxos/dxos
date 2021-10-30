9e01850cd6ec57e06597e3244e40bee38b86b84d
Dmytro Maretskyi <maretskii@gmail.com>
2021-10-30 18:25:56 +0200

    Move devtools & halo protos into @dxos/proto

--- packages/sdk/devtools/src/proto/substitutions.ts
+++ packages/sdk/devtools/src/proto/substitutions.ts
@@ -6,17 +6,17 @@ import { PublicKey } from '@dxos/crypto';
 import type { ConnectionEvent } from '@dxos/network-manager';
 
 export default {
-  'dxos.credentials.keys.PubKey': {
+  'dxos.halo.keys.PubKey': {
     encode: (value: PublicKey) => ({ data: value.asUint8Array() }),
     decode: (value: any) => PublicKey.from(value.data)
+  },
+  'dxos.halo.keys.PrivKey': {
+    encode: (value: Buffer) => ({ data: new Uint8Array(value) }),
+    decode: (value: any) => PublicKey.from(value.data).asBuffer()
   },
   'google.protobuf.Timestamp': {
     encode: (value: number) => ({ seconds: value / 1000 }),
     decode: (value: any) => +(value.seconds + '000')
-  },
-  'dxos.credentials.keys.PrivKey': {
-    encode: (value: Buffer) => ({ data: new Uint8Array(value) }),
-    decode: (value: any) => PublicKey.from(value.data).asBuffer()
   },
   'dxos.devtools.SubscribeToSwarmInfoResponse.SwarmInfo.ConnectionInfo.Json': {
     encode: (value: ConnectionEvent) => ({ data: JSON.stringify(value) }),
