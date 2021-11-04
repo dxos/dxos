6e6fd40ac377dd3adf3713242a79a03c4665d8ac
Przemyslaw Rzad <przemek@ethworks.io>
2021-11-04 13:29:45 +0100

    Sort out member list (#613)

--- packages/sdk/demos/src/components/index.ts
+++ packages/sdk/demos/src/components/index.ts
@@ -8,7 +8,6 @@ export { default as DebugItemList } from './DebugItemList';
 export { default as DialogHeading } from './DialogHeading';
 export { default as EchoGraph } from './EchoGraph';
 export { default as ImportKeyringDialog } from './ImportKeyringDialog';
-export { default as MemberList } from './MemberList';
 export { default as PartyCard } from './PartyCard';
 export { default as RegistrationDialog } from './RegistrationDialog';
 export { default as SearchBar } from './SearchBar';
