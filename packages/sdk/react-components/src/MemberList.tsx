a9bac31da37a058428c04c606e79e876712fe1ab
Przemyslaw Rzad <przemek@ethworks.io>
2021-11-04 13:29:45 +0100

    Sort out member list (#613)

--- packages/sdk/react-components/src/MemberList.tsx
+++ packages/sdk/react-components/src/MemberList.tsx
@@ -8,15 +8,12 @@ import { AvatarGroup, Box } from '@mui/material';
 
 import { PartyMember } from '@dxos/echo-db';
 
-import { MemberAvatar, ShareButton } from './MemberAvatar';
-
-// TODO(burdon): Move to react-components.
+import { MemberAvatar } from './MemberAvatar';
 
 /**
  * List of member avatars.
  */
-// TODO(burdon): Rename.
-const BasicMemberList = ({ members }: {
+export const MemberList = ({ members }: {
   members: PartyMember[]
 }) => {
   return (
@@ -31,31 +28,6 @@ const BasicMemberList = ({ members }: {
           <MemberAvatar key={member.publicKey.toString()} member={member} />
         ))}
       </AvatarGroup>
-    </Box>
-  );
-};
-
-/**
- * Displays a horizontal list of avataors.
- */
-export const MemberList = ({
-  members,
-  onShare
-}: {
-  members: PartyMember[],
-  onShare?: () => void
-}) => {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row'
-      }}
-    >
-      <BasicMemberList members={members} />
-      {onShare && ( // TODO(burdon): Break into multiple components.
-        <ShareButton onClick={onShare} />
-      )}
     </Box>
   );
 };
