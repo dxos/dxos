6fc34150e35cf0ef87a00e44071d1bd0397ee079
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2021-11-08 11:30:10 +0100

    Bot toy prototype (#605)

---
+++ packages/bot/botkit/src/bot-controller/bot-controller.ts
@@ -0,0 +1,27 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import { createRpcServer, RpcPeer, RpcPort } from '@dxos/rpc';
+
+import { schema } from '../proto/gen';
+import { BotFactoryService } from '../proto/gen/dxos/bot';
+
+/**
+ * Exposes BotFactoryService for external agents.
+ */
+export class BotController {
+  private readonly _rpc: RpcPeer;
+
+  constructor (botFactory: BotFactoryService, port: RpcPort) {
+    this._rpc = createRpcServer({
+      service: schema.getService('dxos.bot.BotFactoryService'),
+      handlers: botFactory,
+      port
+    });
+  }
+
+  async start (): Promise<void> {
+    await this._rpc.open();
+  }
+}
