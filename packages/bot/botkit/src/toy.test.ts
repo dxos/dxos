eb33fa0d9e57282d3469e767f93d8dc5587be72a
Vladyslav Yatsenko <jacenko.vlad@gmail.com>
2021-11-09 18:03:24 +0100

    Factor out client creation

--- packages/bot/botkit/src/toy.test.ts
+++ packages/bot/botkit/src/toy.test.ts
@@ -22,152 +22,165 @@ import { decodeInvitation, encodeInvitation } from './testutils/intivitations';
 const ECHO_TYPE = 'bot/text';
 
 describe('In-Memory', () => {
-  it('Spawns a bot', async () => {
-    const [agentPort, botControllerPort] = createLinkedPorts();
+  describe('No client', () => {
+    it('Spawns a bot', async () => {
+      const [agentPort, botControllerPort] = createLinkedPorts();
 
-    let botInitialized = false;
+      let botInitialized = false;
 
-    const agent = new BotFactoryClient(agentPort);
+      const agent = new BotFactoryClient(agentPort);
 
-    const botContainer = new InProcessBotContainer(() => {
-      return {
-        Initialize: async () => {
-          botInitialized = true;
-          return {};
-        },
-        Command: async (request) => {
-          return { response: request.command };
-        },
-        Stop: async () => ({})
-      };
+      const botContainer = new InProcessBotContainer(() => {
+        return {
+          Initialize: async () => {
+            botInitialized = true;
+            return {};
+          },
+          Command: async (request) => {
+            return { response: request.command };
+          },
+          Stop: async () => ({})
+        };
+      });
+      const botFactory = new BotFactory(botContainer);
+
+      const botController = new BotController(botFactory, botControllerPort);
+
+      await Promise.all([
+        botController.start(),
+        agent.start()
+      ]);
+
+      const { id: botId } = await agent.botFactory.SpawnBot({});
+      expect(botId).toBeDefined();
+
+      const { bots } = await agent.botFactory.GetBots({});
+      expect(bots).toHaveLength(1);
+      expect(bots![0].status).toBe(Bot.Status.RUNNING);
+      expect(botInitialized).toBe(true);
+
+      const command = PublicKey.random().asUint8Array();
+      const repsonse = await agent.botFactory.SendCommand({ botId, command });
+
+      expect(repsonse.response).toBeDefined();
+      expect(Buffer.from(command).equals(Buffer.from(repsonse.response!))).toBe(true);
+
+      await agent.botFactory.Stop({ id: botId });
+      agent.stop();
     });
-    const botFactory = new BotFactory(botContainer);
-
-    const botController = new BotController(botFactory, botControllerPort);
-
-    await Promise.all([
-      botController.start(),
-      agent.start()
-    ]);
-
-    const { id: botId } = await agent.botFactory.SpawnBot({});
-    expect(botId).toBeDefined();
-
-    const { bots } = await agent.botFactory.GetBots({});
-    expect(bots).toHaveLength(1);
-    expect(bots![0].status).toBe(Bot.Status.RUNNING);
-    expect(botInitialized).toBe(true);
-
-    const command = PublicKey.random().asUint8Array();
-    const repsonse = await agent.botFactory.SendCommand({ botId, command });
-
-    expect(repsonse.response).toBeDefined();
-    expect(Buffer.from(command).equals(Buffer.from(repsonse.response!))).toBe(true);
-
-    await agent.botFactory.Stop({ id: botId });
-    agent.stop();
   });
 
-  it('Spawns a bot with a client', async () => {
-    const [agentPort, botControllerPort] = createLinkedPorts();
+  describe('With a DXOS client', () => {
+    let client: Client;
+    let party: Party;
 
-    const botContainer = new InProcessBotContainer(() => {
-      const client = new Client();
-      let party: Party | undefined;
+    beforeEach(async () => {
+      client = new Client();
+      await client.initialize();
+      await client.echo.halo.createIdentity({ ...createKeyPair() });
+      await client.echo.halo.create('Agent');
+      party = await client.echo.createParty();
+    });
 
-      return {
-        Initialize: async (request) => {
-          await client.initialize();
-          await client.echo.halo.createIdentity({ ...createKeyPair() });
-          await client.echo.halo.create('Bot');
+    afterEach(async () => {
+      await client.destroy();
+    });
 
-          if (request.invitation?.data) {
-            assert(request.secret, 'Secret must be provided with invitation');
-            const invitation = decodeInvitation(request.invitation.data);
-            // TODO(yivlad): Field secret is actually a buffer here.
-            const botSecretProvider: SecretProvider = async () => request.secret as Buffer;
-            party = await client.echo.joinParty(invitation, botSecretProvider);
-          }
+    it('Spawns a bot with a client', async () => {
+      const [agentPort, botControllerPort] = createLinkedPorts();
 
-          return {};
-        },
-        Command: async (request) => {
-          if (!party) {
-            throw new Error('Bot is not initialized');
-          }
+      const botContainer = new InProcessBotContainer(() => {
+        const client = new Client();
+        let party: Party | undefined;
 
-          if (!request.command) {
-            throw new Error('No command');
-          }
+        return {
+          Initialize: async (request) => {
+            await client.initialize();
+            await client.echo.halo.createIdentity({ ...createKeyPair() });
+            await client.echo.halo.create('Bot');
 
-          await party.database.createItem({
-            model: ObjectModel,
-            type: ECHO_TYPE,
-            props: {
-              text: PublicKey.from(request.command).toString()
+            if (request.invitation?.data) {
+              assert(request.secret, 'Secret must be provided with invitation');
+              const invitation = decodeInvitation(request.invitation.data);
+              // TODO(yivlad): Field secret is actually a buffer here.
+              const botSecretProvider: SecretProvider = async () => request.secret as Buffer;
+              party = await client.echo.joinParty(invitation, botSecretProvider);
             }
-          });
 
-          return { response: request.command };
+            return {};
+          },
+          Command: async (request) => {
+            if (!party) {
+              throw new Error('Bot is not initialized');
+            }
+
+            if (!request.command) {
+              throw new Error('No command');
+            }
+
+            await party.database.createItem({
+              model: ObjectModel,
+              type: ECHO_TYPE,
+              props: {
+                text: PublicKey.from(request.command).toString()
+              }
+            });
+
+            return { response: request.command };
+          },
+          Stop: async () => {
+            await client.destroy();
+            return {};
+          }
+        };
+      });
+
+      const botFactory = new BotFactory(botContainer);
+
+      const botController = new BotController(botFactory, botControllerPort);
+
+      const botFactoryClient = new BotFactoryClient(agentPort);
+
+      await Promise.all([
+        botController.start(),
+        botFactoryClient.start()
+      ]);
+
+      const partySecret = Buffer.from('SECRET');
+      const secretProvider: SecretProvider = async () => partySecret;
+      const secretValidator: SecretValidator = async (invitation, secret) => secret.equals(partySecret);
+
+      const invitation = await party.createInvitation({ secretProvider, secretValidator });
+      const { id } = await botFactoryClient.botFactory.SpawnBot({
+        invitation: {
+          data: encodeInvitation(invitation)
         },
-        Stop: async () => {
-          await client.destroy();
-          return {};
-        }
-      };
+        secret: partySecret
+      });
+      assert(id);
+
+      const text = PublicKey.random().asUint8Array();
+      await botFactoryClient.botFactory.SendCommand({
+        botId: id,
+        command: text
+      });
+
+      await party.database.waitForItem({
+        type: ECHO_TYPE
+      });
+
+      const items = party.database.select(s => s
+        .filter({ type: ECHO_TYPE })
+        .items
+      ).getValue();
+
+      expect(items.length).toBe(1);
+      const savedText = items[0].model.getProperty('text');
+      expect(savedText).toBe(PublicKey.from(text).toString());
+
+      await botFactoryClient.botFactory.Stop({ id });
+
+      botFactoryClient.stop();
     });
-
-    const botFactory = new BotFactory(botContainer);
-
-    const botController = new BotController(botFactory, botControllerPort);
-
-    const botFactoryClient = new BotFactoryClient(agentPort);
-    const botFactoryClientDXOSClient = new Client();
-    await botFactoryClientDXOSClient.initialize();
-    await botFactoryClientDXOSClient.echo.halo.createIdentity({ ...createKeyPair() });
-    await botFactoryClientDXOSClient.echo.halo.create('Agent');
-    const party = await botFactoryClientDXOSClient.echo.createParty();
-
-    await Promise.all([
-      botController.start(),
-      botFactoryClient.start()
-    ]);
-
-    const partySecret = Buffer.from('SECRET');
-    const secretProvider: SecretProvider = async () => partySecret;
-    const secretValidator: SecretValidator = async (invitation, secret) => secret.equals(partySecret);
-
-    const invitation = await party.createInvitation({ secretProvider, secretValidator });
-    const { id } = await botFactoryClient.botFactory.SpawnBot({
-      invitation: {
-        data: encodeInvitation(invitation)
-      },
-      secret: partySecret
-    });
-    assert(id);
-
-    const text = PublicKey.random().asUint8Array();
-    await botFactoryClient.botFactory.SendCommand({
-      botId: id,
-      command: text
-    });
-
-    await party.database.waitForItem({
-      type: ECHO_TYPE
-    });
-
-    const items = party.database.select(s => s
-      .filter({ type: ECHO_TYPE })
-      .items
-    ).getValue();
-
-    expect(items.length).toBe(1);
-    const savedText = items[0].model.getProperty('text');
-    expect(savedText).toBe(PublicKey.from(text).toString());
-
-    await botFactoryClient.botFactory.Stop({ id });
-
-    await botFactoryClientDXOSClient.destroy();
-    botFactoryClient.stop();
   });
 });
