40f887253b7345af532303fe3b5d6c9836218c01
Rich Burdon <rich.burdon@gmail.com>
2021-11-13 21:17:25 -0500

    Minor text field settings.

--- packages/sdk/react-client/src/hooks/invitations/utils.ts
+++ packages/sdk/react-client/src/hooks/invitations/utils.ts
@@ -7,7 +7,7 @@ import base from 'base-x';
 import { InvitationDescriptor } from '@dxos/echo-db';
 
 // Encode with only alpha-numberic characters.
-const base62 = base('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+export const base62 = base('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
 export const encodeInvitation = (invitation: InvitationDescriptor) => {
   const buffer = Buffer.from(JSON.stringify(invitation.toQueryParameters()));
