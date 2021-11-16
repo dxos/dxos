9bb48c4593527554b6a6679d4186239dd127852b
rzadp <roopert7@gmail.com>
2021-11-16 11:56:41 +0100

    More steps

--- packages/sdk/client/src/remote-client.test.ts
+++ packages/sdk/client/src/remote-client.test.ts
@@ -4,12 +4,14 @@
 
 import expect from 'expect';
 import { it as test } from 'mocha';
+import waitForExpect from 'wait-for-expect';
 
 import { createBundledRpcServer, createLinkedPorts } from '@dxos/rpc';
 import { afterTest } from '@dxos/testutils';
 
 import { Client } from './client';
 import { clientServiceBundle } from './interfaces';
+import { latch } from '@dxos/async';
 
 const createServiceProviderPort = async () => {
   const [proxyPort, hostPort] = createLinkedPorts();
@@ -52,4 +54,29 @@ describe('Remote client', () => {
     expect(client.halo.profile).toBeDefined();
     await client.destroy();
   }).timeout(200);
+
+  test.only('creates and joins a HALO invitation', async () => {
+    const inviterRpcPort = await createServiceProviderPort();
+    const inviteeRpcPort = await createServiceProviderPort();
+
+    const inviter = new Client({ system: { remote: true } }, { rpcPort: inviterRpcPort });
+    await inviter.initialize();
+    const invitee = new Client({ system: { remote: true } }, { rpcPort: inviteeRpcPort });
+    await invitee.initialize();
+
+    await inviter.halo.createProfile({ username: 'test-user' });
+
+    inviter.services.ProfileService.CreateInvitation().subscribe(inviterInvitation => {
+      console.log({inviterInvitation})
+    }, (error) => {throw error})
+
+    
+    const [inviteeProfileLatch, inviteeProfileTrigger] = latch();
+    invitee.services.ProfileService.SubscribeProfile().subscribe(inviteeProfile => {
+      if (inviteeProfile.profile?.username === 'test-user') {
+        inviteeProfileTrigger();
+      }
+    }, error => {throw error})
+    await inviteeProfileLatch
+  }).timeout(5000);
 });
