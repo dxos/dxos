e4c4ad37581f3d3b7ecfa1975a5acc7b7a5b92f0
Josiah Witt <josiah@witt.life>
2021-11-17 08:47:16 -0500

    refactor: update messenger model types (#653)
    * refactor: update messenger model types
    
    * chore: lint
    
    * chore: pr feedback

---
+++ packages/echo/messenger-model/src/types.ts
@@ -0,0 +1,14 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+/**
+ * Standard type of an ObjectModel parent for a set of messages.
+ * Includes metadata for a message channel (e.g., title).
+ */
+export const TYPE_MESSENGER_CHANNEL = 'dxos:type.messenger.channel';
+
+/**
+ * Standard type of a set of messages as defined by the MessengerModel.
+ */
+export const TYPE_MESSENGER_MESSAGES = 'dxos:type.messenger.messages';
