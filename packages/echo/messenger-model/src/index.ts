b44f736619c24de441d7dc36676c30ee30ab2c26
Josiah Witt <josiah@witt.life>
2021-11-17 08:47:16 -0500

    refactor: update messenger model types (#653)
    * refactor: update messenger model types
    
    * chore: lint
    
    * chore: pr feedback

--- packages/echo/messenger-model/src/index.ts
+++ packages/echo/messenger-model/src/index.ts
@@ -2,7 +2,6 @@
 // Copyright 2020 DXOS.org
 //
 
-export * from './messenger-model';
-export const MESSENGER_PAD = 'dxos.org/pad/messenger';
-export const MESSENGER_TYPE_CHANNEL = 'dxos.org/type/messenger/channel';
-export const MESSENGER_TYPE_MESSAGE = 'dxos.org/type/messenger/message';
+export * from './model';
+export * from './proto';
+export * from './types';
