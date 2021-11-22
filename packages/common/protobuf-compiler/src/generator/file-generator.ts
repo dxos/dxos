2b553dcccb5c3a2509640a328db46f65f1e1ae0d
Dmytro Maretskyi <maretskii@gmail.com>
2021-11-22 17:35:54 +0100

    Fix

--- packages/common/protobuf-compiler/src/generator/file-generator.ts
+++ packages/common/protobuf-compiler/src/generator/file-generator.ts
@@ -94,8 +94,8 @@ function createStreamImport () {
   return f.createImportDeclaration(
     [],
     [],
-    f.createImportClause(false, undefined, f.createNamedImports([
-      f.createImportSpecifier(true, undefined, f.createIdentifier('Stream'))
+    f.createImportClause(true, undefined, f.createNamedImports([
+      f.createImportSpecifier(false, undefined, f.createIdentifier('Stream'))
     ])),
     f.createStringLiteral(CODEC_MODULE.importSpecifier(''))
   );
