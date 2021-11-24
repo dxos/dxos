bddd385a5d2315447cb8b4a9366575a0c3c89186
Przemyslaw Rzad <przemek@ethworks.io>
2021-11-24 14:00:02 +0100

    chore: Restore tests on `main` (#672)

--- packages/sdk/tutorials/apps/tasks-app/src/App.tsx
+++ packages/sdk/tutorials/apps/tasks-app/src/App.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import { ClientInitializer } from '@dxos/react-client';
-import { ErrorView } from '@dxos/react-framework';
+import { ErrorView, FrameworkContextProvider } from '@dxos/react-framework';
 import { LinearProgress } from '@mui/material';
 
 import Root from './components/Root';
@@ -12,9 +12,11 @@ import { initConfig } from './config';
 
 const App = () => {
   return (
-    <ClientInitializer config={initConfig} loaderComponent={() => <LinearProgress/>} errorComponent={ErrorView}>
-      <Root />
-    </ClientInitializer>
+    <FrameworkContextProvider>
+      <ClientInitializer config={initConfig} loaderComponent={() => <LinearProgress/>} errorComponent={ErrorView}>
+        <Root />
+      </ClientInitializer>
+    </FrameworkContextProvider>
   );
 };
 
