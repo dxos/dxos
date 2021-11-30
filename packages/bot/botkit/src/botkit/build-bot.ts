9badda9acc47c1a67335b9b6c489acb00755058e
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2021-12-01 00:21:29 +0100

    Package bots (#660)

---
+++ packages/bot/botkit/src/botkit/build-bot.ts
@@ -0,0 +1,30 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import { build } from 'esbuild';
+
+export interface BuildBotOptions {
+  entryPoint: string,
+  outfile: string
+}
+
+export const buildBot = async ({ entryPoint, outfile } : BuildBotOptions) => {
+  await build({
+    bundle: true,
+    entryPoints: [entryPoint],
+    outfile,
+    write: true,
+    format: 'cjs',
+    platform: 'node',
+    external: [
+      'fatfs',
+      'runtimejs',
+      'wrtc',
+      'bip32',
+      'typeforce',
+      'sodium-universal',
+      'sodium-native'
+    ]
+  });
+};
--- packages/bot/botkit/src/index.ts
+++ packages/bot/botkit/src/index.ts
@@ -2,4 +2,9 @@
 // Copyright 2021 DXOS.org
 //
 
+export * from './bot-container';
+export * from './bot-controller';
+export * from './bot-factory';
 export * from './bot-handle';
+export * from './bots';
+export * from './testutils';
