07a58164a26c8e268cdf89e4cfaa9a4e7db25d65
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-12-02 15:03:19 +0100

    fix: Incorrect model meta on re-hydrated models (#712)
    * Fix incorrect model meta
    
    * Add client test and lint

--- packages/echo/model-factory/src/model-factory.test.ts
+++ packages/echo/model-factory/src/model-factory.test.ts
@@ -2,14 +2,11 @@
 // Copyright 2020 DXOS.org
 //
 
-import { latch } from '@dxos/async';
-import { createId, createKeyPair } from '@dxos/crypto';
-import { TestItemMutation, createMockFeedWriterFromStream } from '@dxos/echo-protocol';
-import { createTransform } from '@dxos/feed-store';
+import { createId, zeroKey } from '@dxos/crypto';
+import { MockFeedWriter, TestItemMutation } from '@dxos/echo-protocol';
 
 import { ModelFactory } from './model-factory';
 import { TestModel } from './testing';
-import { ModelMessage } from './types';
 
 describe('model factory', () => {
   test('model constructor', async () => {
@@ -22,54 +19,28 @@ describe('model factory', () => {
   });
 
   test('model mutation processing', async () => {
-    const { publicKey: feedKey } = createKeyPair();
     const itemId = createId();
-
-    const objects: TestItemMutation[] = [];
-
-    // Transform outbound mutations to inbounds model messsges (create loop).
-    const writeStream = createTransform<TestItemMutation, ModelMessage<TestItemMutation>>(
-      async mutation => {
-        objects.push(mutation);
-        const out: ModelMessage<TestItemMutation> = {
-          meta: {
-            memberKey: feedKey,
-            feedKey,
-            seq: 1
-          },
-          mutation
-        };
-
-        return out;
-      }
-    );
 
     // Create model.
     const modelFactory = new ModelFactory().registerModel(TestModel);
-    const model = modelFactory.createModel<TestModel>(TestModel.meta.type, itemId, createMockFeedWriterFromStream(writeStream));
+    const feedWriter = new MockFeedWriter<TestItemMutation>();
+    const model = modelFactory.createModel<TestModel>(TestModel.meta.type, itemId, feedWriter as any);
     expect(model).toBeTruthy();
+    feedWriter.written.on(([message, meta]) => model.processMessage({
+      feedKey: meta.feedKey.asUint8Array(),
+      memberKey: zeroKey(),
+      seq: meta.seq
+    }, message));
 
     // Update model.
     await model.setProperty('title', 'Hello');
-    expect(objects).toHaveLength(1);
-    expect(objects[0]).toEqual({
+    expect(feedWriter.messages).toHaveLength(1);
+    expect(feedWriter.messages[0]).toEqual({
       key: 'title',
       value: 'Hello'
     });
 
     // Expect model has not been updated (mutation has not been processed).
-    expect(model.getProperty('title')).toBeFalsy();
-
-    // Listen for updates.
-    const [update, onUpdate] = latch(1);
-    model.subscribe(onUpdate);
-
-    // Loop model output to input.
-    writeStream.pipe(model.processor);
-
-    // Wait for message to be processed.
-    await update;
-
     // Expect model to have been updated.
     expect(model.getProperty('title')).toEqual('Hello');
   });
