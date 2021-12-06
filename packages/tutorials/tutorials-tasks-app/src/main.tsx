51847d21b95b23b2553000fe322c550f9b3ebb36
rzadp <roopert7@gmail.com>
2021-12-06 10:58:40 +0100

    Repair tasks app

---
+++ packages/tutorials/tutorials-tasks-app/src/main.tsx
@@ -0,0 +1,15 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
