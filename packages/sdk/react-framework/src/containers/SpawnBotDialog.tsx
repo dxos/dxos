bd1ee56a0d7ae60b1a5a17339f2c02555be58c87
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2021-12-09 14:08:26 +0100

    feat: Storybook bot demo (#747)

---
+++ packages/sdk/react-framework/src/containers/SpawnBotDialog.tsx
@@ -0,0 +1,98 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import React, { useEffect, useState } from 'react';
+
+import { Button, TextField } from '@mui/material';
+
+import { BotFactoryClient, BotHandle } from '@dxos/bot-factory-client';
+import { PartyProxy } from '@dxos/client';
+import { PublicKey } from '@dxos/crypto';
+import { useClient } from '@dxos/react-client';
+import { Dialog } from '@dxos/react-components';
+
+import { handleKey } from '../helpers';
+
+export interface SpawnBotDialogProps {
+  open: boolean,
+  onClose: () => void,
+  party: PartyProxy,
+  onBotCreated: (bot: BotHandle) => void
+}
+
+export const SpawnBotDialog = ({
+  open,
+  onClose,
+  party,
+  onBotCreated
+} : SpawnBotDialogProps) => {
+  const client = useClient();
+  const [botPath, setBotPath] = useState('./stories/bots/start-story-bot');
+  const [processing, setProcessing] = useState(false);
+  const [botFactoryClient, setBotFactoryClient] = useState<BotFactoryClient>();
+
+  const handleSpawnProcess = async (path: string) => {
+    if (!botFactoryClient) {
+      return;
+    }
+    try {
+      setProcessing(true);
+      const botHandle = await botFactoryClient.spawn(
+        { localPath: path },
+        client,
+        party
+      );
+      onBotCreated(botHandle);
+      onClose();
+    } finally {
+      setProcessing(false);
+    }
+  };
+
+  const getDialogProps = () => {
+    const inviteBotContent = (
+      <TextField
+        autoFocus
+        fullWidth
+        placeholder='Type in path to a local bot.'
+        spellCheck={false}
+        value={botPath}
+        onChange={(event) => setBotPath(event.target.value)}
+        onKeyDown={handleKey('Enter', () => handleSpawnProcess(botPath))}
+      />
+    );
+
+    const joinPartyActions = (
+      <>
+        <Button onClick={onClose}>Close</Button>
+        <Button disabled={processing || !botFactoryClient} onClick={() => handleSpawnProcess(botPath)}>Spawn</Button>
+      </>
+    );
+
+    return {
+      title: 'Spawn bot',
+      processing,
+      content: inviteBotContent,
+      actions: joinPartyActions
+    };
+  };
+
+  const dialogProps = getDialogProps();
+
+  useEffect(() => {
+    const bfc = new BotFactoryClient(client.echo.networkManager);
+    setImmediate(async () => {
+      await bfc.start(PublicKey.from('e61469c04e4265e145f9863dd4b84fd6dee8f31e10160c38f9bb3c289e3c09bc'));
+      setBotFactoryClient(bfc);
+    });
+  }, []);
+
+  return (
+    <Dialog
+      maxWidth='xs'
+      open={open}
+      {...dialogProps}
+    />
+  );
+};
---
+++ packages/sdk/react-framework/stories/SpawnBotDialog.stories.tsx
@@ -0,0 +1,128 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import React, { useEffect, useState } from 'react';
+
+import { Box, Button, TextField, Toolbar } from '@mui/material';
+
+import { PartyProxy } from '@dxos/client';
+import { Item } from '@dxos/echo-db';
+import { ObjectModel } from '@dxos/object-model';
+import {
+  ClientProvider,
+  ProfileInitializer,
+  useClient,
+  useSelection
+} from '@dxos/react-client';
+import { FullScreen } from '@dxos/react-components';
+
+import {
+  ErrorBoundary,
+  FrameworkContextProvider,
+  SpawnBotDialog
+} from '../src';
+import { Column } from './helpers';
+
+export default {
+  title: 'react-framework/SpawnBotDialog'
+};
+
+const TEST_TYPE = 'TEST_TYPE';
+
+const User = () => {
+  const [open, setOpen] = useState(false);
+  const [party, setParty] = useState<PartyProxy>();
+  const [testText, setTestText] = useState('');
+  const [textItem, setTextItem] = useState<Item<ObjectModel>>();
+  const client = useClient();
+  const counterItems = useSelection(party?.database.select(s => s
+    .filter({ type: 'DXOS_COUNTER' })
+    .items)
+  , [party?.key]);
+
+  const handleCreateParty = async () => {
+    const party = await client.echo.createParty();
+    setParty(party);
+  };
+
+  useEffect(() => {
+    void handleCreateParty();
+  }, []);
+
+  useEffect(() => {
+    setImmediate(async () => {
+      const textItem = await party?.database.createItem({
+        type: TEST_TYPE,
+        model: ObjectModel,
+        props: {
+          text: testText
+        }
+      });
+      setTextItem(textItem);
+    });
+  }, [party]);
+
+  useEffect(() => {
+    if (textItem) {
+      void textItem.model.setProperty('text', testText);
+    }
+  }, [testText]);
+
+  if (!party) {
+    return null;
+  }
+
+  return (
+    <Box>
+      <Toolbar>
+        <Button onClick={() => setOpen(true)}>Spawn bot</Button>
+      </Toolbar>
+      {open && (
+        <SpawnBotDialog
+          party={party}
+          open={open}
+          onClose={() => setOpen(false)}
+          onBotCreated={() => window.alert('Bot created')}
+        />
+      )}
+      <Box sx={{ marginTop: 2, padding: 1 }}>
+        Number of occurences of word &quot;DXOS&quot; in below text: {(counterItems?.length && counterItems[0].model.getProperty('counter')) ?? 0}
+      </Box>
+      <Box sx={{ marginTop: 2, padding: 1 }}>
+        <TextField
+          fullWidth
+          placeholder='Type in some text with word DXOS...'
+          multiline
+          rows={5}
+          spellCheck={false}
+          value={testText}
+          onChange={(event) => setTestText(event.target.value)}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export const Primary = () => {
+  return (
+    <FullScreen>
+      <ErrorBoundary>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-around'
+        }}>
+          <ClientProvider config={{ services: { signal: { server: 'ws://localhost:4000' } } }}>
+            <ProfileInitializer>
+              <FrameworkContextProvider>
+                <Column>
+                  <User />
+                </Column>
+              </FrameworkContextProvider>
+            </ProfileInitializer>
+          </ClientProvider>
+        </Box>
+      </ErrorBoundary>
+    </FullScreen>
+  );
+};
