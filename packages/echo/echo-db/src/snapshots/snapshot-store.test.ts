1cb2da3ad5ed3c65ccded9d708fe470a198d951c
Dmytro Maretskyi <maretskii@gmail.com>
2021-12-10 12:58:24 +0100

    Group tests

--- packages/echo/echo-db/src/snapshots/snapshot-store.test.ts
+++ packages/echo/echo-db/src/snapshots/snapshot-store.test.ts
@@ -13,27 +13,29 @@ import { SnapshotStore } from './snapshot-store';
 
 const createPublicKey = () => PublicKey.from(createKeyPair().publicKey);
 
-test('in-memory', async () => {
-  const store = new SnapshotStore(createRamStorage());
+describe('SnapshotStore', () => {
+  test('in-memory', async () => {
+    const store = new SnapshotStore(createRamStorage());
 
-  const key1 = createPublicKey();
-  const key2 = createPublicKey();
+    const key1 = createPublicKey();
+    const key2 = createPublicKey();
 
-  expect(await store.load(key1)).toBeUndefined();
-  expect(await store.load(key2)).toBeUndefined();
+    expect(await store.load(key1)).toBeUndefined();
+    expect(await store.load(key2)).toBeUndefined();
 
-  const snapshot: PartySnapshot = {
-    partyKey: key1.asBuffer(),
-    database: {
-      items: [{
-        itemId: createId(),
-        itemType: 'dxn://example/test'
-      }]
-    }
-  };
+    const snapshot: PartySnapshot = {
+      partyKey: key1.asBuffer(),
+      database: {
+        items: [{
+          itemId: createId(),
+          itemType: 'dxn://example/test'
+        }]
+      }
+    };
 
-  await store.save(snapshot);
+    await store.save(snapshot);
 
-  expect(await store.load(key1)).toEqual(snapshot);
-  expect(await store.load(key2)).toBeUndefined();
+    expect(await store.load(key1)).toEqual(snapshot);
+    expect(await store.load(key2)).toBeUndefined();
+  });
 });
