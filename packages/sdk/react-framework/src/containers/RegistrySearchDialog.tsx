0790f8459471bc1f4054e906a1755521cff711f2
Josiah Witt <josiah@witt.life>
2021-12-20 13:53:08 -0500

    feat: Add RegistrySearchDialog

---
+++ packages/sdk/react-framework/src/containers/RegistrySearchDialog.tsx
@@ -0,0 +1,103 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import React, { useEffect, useState } from 'react';
+
+import { Autocomplete, Button, TextField } from '@mui/material';
+
+import { Dialog } from '@dxos/react-components';
+import { Resource } from '@dxos/registry-client';
+
+export interface RegistrySearchDialogProps {
+  open: boolean
+  title?: string
+  onSearch: (searchInput: string) => Promise<Resource[]>
+  onSelect: (resource: Resource) => Promise<void>
+  onClose?: () => void
+  modal?: boolean
+}
+
+/**
+ *
+ */
+export const RegistrySearchDialog = ({
+  open,
+  title,
+  onSearch,
+  onSelect,
+  onClose,
+  modal
+}: RegistrySearchDialogProps) => {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchOptions, setSearchOptions] = useState<Resource[]>([]);
+  const [selectedResource, setSelectedResource] = useState<Resource | null>(null);
+
+  useEffect(() => {
+    setImmediate(async () => {
+      const resources = await onSearch(searchInput);
+      setSearchOptions(resources);
+    });
+  }, [searchInput]);
+
+  const handleReset = () => {
+    setSearchInput('');
+    setSearchOptions([]);
+    setSelectedResource(null);
+  };
+
+  const handleClose = () => {
+    onClose?.();
+    handleReset();
+  };
+
+  const handleSelect = async () => {
+    selectedResource && await onSelect?.(selectedResource);
+    handleReset();
+  }
+
+  const content = (
+    <Autocomplete
+      fullWidth
+      options={searchOptions}
+      getOptionLabel={option => option.id.toString()}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label='Search'
+          placeholder='Search'
+          variant='standard'
+          autoFocus
+          spellCheck={false}
+        />
+      )}
+      inputValue={searchInput}
+      onInputChange={(event, newValue) => setSearchInput(newValue)}
+      value={selectedResource}
+      onChange={(event, newValue) => setSelectedResource(newValue)}
+    />
+  );
+
+  const actions = (
+    <>
+      <Button onClick={handleClose}>Close</Button>
+      <Button
+        disabled={!selectedResource}
+        onClick={handleSelect}
+      >
+        Select
+      </Button>
+    </>
+  );
+
+  return (
+    <Dialog
+      maxWidth='xs'
+      modal={modal}
+      open={open}
+      title={title || 'Registry Search'}
+      content={content}
+      actions={actions}
+    />
+  );
+};
---
+++ packages/sdk/react-framework/stories/RegistrySearchDialog.stories.tsx
@@ -0,0 +1,30 @@
+import React, { useCallback, useMemo, useState } from 'react';
+
+import { Box, Button } from '@mui/material';
+
+import { sleep } from '@dxos/async';
+
+import { RegistrySearchDialog } from '../src';
+import { createMockRegistry } from './helpers';
+
+export default {
+  title: 'react-framework/RegistrySearchDialog',
+};
+
+export const Primary = () => {
+  const mockRegistry = useMemo(() => createMockRegistry(), []);
+  const handleSearch = useCallback((searchInput: string) => mockRegistry.queryResources({ text: searchInput }), []);
+  const [open, setOpen] = useState(true);
+
+  return (
+    <Box margin={2}>
+      <Button onClick={() => setOpen(true)}>Open</Button>
+      <RegistrySearchDialog
+        open={open}
+        onSearch={handleSearch}
+        onSelect={() => sleep(1000).then(() => setOpen(false))}
+        onClose={() => setOpen(false)}
+      />
+    </Box>
+  );
+};
