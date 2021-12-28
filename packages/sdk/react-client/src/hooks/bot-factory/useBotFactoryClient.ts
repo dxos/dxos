72ab8f8afa1671411c9430e59bb5985954ec30e5
yivlad <jacenko.vlad@gmail.com>
2021-12-28 15:41:11 +0100

    WIP

--- packages/sdk/react-client/src/hooks/bot-factory/useBotFactoryClient.ts
+++ packages/sdk/react-client/src/hooks/bot-factory/useBotFactoryClient.ts
@@ -24,7 +24,7 @@ export const useBotFactoryClient = (): BotFactoryClient | undefined => {
       await botFactoryClient.start(PublicKey.from(topic));
       setBotFactoryClient(botFactoryClient);
     });
-    return () => botFactoryClient.stop();
+    return () => void botFactoryClient.stop();
   }, []);
 
   return botFactoryClient;
