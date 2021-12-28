203f714a07e4070f38f1224c324c9811aa6ac41b
Dmytro Maretskyi <maretskii@gmail.com>
2021-12-28 12:51:14 +0100

    Two passing tests

--- packages/echo/text-model/src/remote.test.ts
+++ packages/echo/text-model/src/remote.test.ts
@@ -2,6 +2,8 @@ import { createInMemoryDatabase, createRemoteDatabaseFromDataServiceHost } from 
 import { ModelFactory } from '@dxos/model-factory';
 import { it as test } from 'mocha';
 import { TextModel } from './text-model';
+import expect from 'expect';
+import { ObjectModel } from '@dxos/object-model';
 
 describe('With remote database', () => {
   test('create and write text', async () => {
@@ -11,6 +13,25 @@ describe('With remote database', () => {
 
     const text = await frontend.createItem({
       model: TextModel, type: 'example:type.text'
+    });
+    await text.model.insert(0, 'Hello world');
+
+    expect(text.model.textContent).toEqual('Hello world');
+  })
+
+  test('create with parent', async () => {
+    const modelFactory = new ModelFactory().registerModel(TextModel).registerModel(ObjectModel);
+    const backend = await createInMemoryDatabase(modelFactory);
+    const frontend = await createRemoteDatabaseFromDataServiceHost(modelFactory, backend.createDataServiceHost());
+
+    const parent = await frontend.createItem({
+      model: ObjectModel,
+    });
+
+    const text = await frontend.createItem({
+      model: TextModel,
+      type: 'example:type.text',
+      parent: parent.id,
     });
     await text.model.insert(0, 'Hello world');
 
