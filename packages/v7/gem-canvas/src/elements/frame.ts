6e073b844fcc1c7495ab459f667867bd6f41ce9c
richburdon <rich.burdon@gmail.com>
2022-01-08 08:41:15 -0500

    Fix.

--- packages/v7/gem-canvas/src/elements/frame.ts
+++ packages/v7/gem-canvas/src/elements/frame.ts
@@ -95,24 +95,24 @@ export const createFrame = (): D3Callable => {
       .data(active ? ['_frame_'] : [])
       .join('rect')
       .classed('frame', true)
-        .attr('x', x)
-        .attr('y', y)
-        .attr('width', width)
-        .attr('height', height);
+      .attr('x', x)
+      .attr('y', y)
+      .attr('width', width)
+      .attr('height', height);
 
     group
       .selectAll('circle')
       .data(resizable ? handles : [], (handle: Handle) => handle.id)
       .join('circle')
-        .call(handleDrag((handle, delta, mod, commit) => {
-          const bounds = computeBounds({ x, y, width, height }, handle, delta);
-          const data = base.createData(bounds, mod, commit);
-          base.onUpdate(data);
-        }))
-        .classed('frame-handle', true)
-        .attr('cx', ({ p }) => cx + p[0] * width / 2)
-        .attr('cy', ({ p }) => cy + p[1] * height / 2)
-        .attr('r', 5); // TODO(burdon): Grow as zoomed.
+      .call(handleDrag((handle, delta, mod, commit) => {
+        const bounds = computeBounds({ x, y, width, height }, handle, delta);
+        const data = base.createData(bounds, mod, commit);
+        base.onUpdate(data);
+      }))
+      .classed('frame-handle', true)
+      .attr('cx', ({ p }) => cx + p[0] * width / 2)
+      .attr('cy', ({ p }) => cy + p[1] * height / 2)
+      .attr('r', 5); // TODO(burdon): Grow as zoomed.
     // eslint-enable indent
   };
 };
