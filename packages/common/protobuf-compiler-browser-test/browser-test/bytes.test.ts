3b975459829fd8410122f37f870562ab5affc2e7
rzadp <roopert7@gmail.com>
2022-01-13 11:48:24 +0100

    Lint

--- packages/common/protobuf-compiler-browser-test/browser-test/bytes.test.ts
+++ packages/common/protobuf-compiler-browser-test/browser-test/bytes.test.ts
@@ -1,30 +1,25 @@
-import { Stream } from '@dxos/codec-protobuf';
+//
+// Copyright 2022 DXOS.org
+//
+
 import expect from 'expect';
 
-/**
- * We import the files by a path here, because:
- * - We don't want to clutter protobuf-compiler exports with testing stuff.
- * - Testing package is created separately because protobuf-compiler cannot use our toolchain (because of circular dependencies)
- * - Importing by path is fine because it is executed locally only, the package will be kept close to protobuf-compiler, and is not published.
-*/
-
-import { ComplexFields, Scalars, TaskList, TaskType, WithTimestamp } from '../../protobuf-compiler/test/proto/gen/dxos/test'
-import { schema } from '../../protobuf-compiler/test/proto/gen'
-import { MyKey } from '../../protobuf-compiler/test/my-key';
+import { schema } from '../../protobuf-compiler/test/proto/gen';
+import { Scalars } from '../../protobuf-compiler/test/proto/gen/dxos/test';
 
 it('bytes fields get decoded to Uint8Array', () => {
   const codec = schema.getCodecForType('dxos.test.Scalars');
 
   const initial: Scalars = {
-    bytesField: new Uint8Array(Buffer.from('world')),
-  }
+    bytesField: new Uint8Array(Buffer.from('world'))
+  };
 
-  const encoded = codec.encode(initial)
-  
+  const encoded = codec.encode(initial);
+
   expect(encoded).toBeInstanceOf(Uint8Array);
 
-  const decoded = codec.decode(encoded)
+  const decoded = codec.decode(encoded);
 
-  expect(decoded).toEqual(initial)
-  expect(decoded.bytesField).toBeInstanceOf(Uint8Array)
-})
+  expect(decoded).toEqual(initial);
+  expect(decoded.bytesField).toBeInstanceOf(Uint8Array);
+});
