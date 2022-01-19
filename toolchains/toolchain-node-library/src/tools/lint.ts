e5865ce92b220630013de682211806e459dc0e15
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2022-01-19 14:42:49 +0200

    chore: Add timeout to individual package scripts (#847)
    * Add package timeout
    
    * chore: Add timeout to individual package scripts

--- toolchains/toolchain-node-library/src/tools/lint.ts
+++ toolchains/toolchain-node-library/src/tools/lint.ts
@@ -8,9 +8,9 @@ import { TOOLCHAIN_PACKAGE_DIR } from '../common';
 import { Project } from '../project';
 import { execTool } from './common';
 
-export function execLint (project: Project, additionalArgs: string[] = []) {
+export async function execLint (project: Project, additionalArgs: string[] = []) {
   const config = project.isReactPackage
     ? join(TOOLCHAIN_PACKAGE_DIR, '.eslintrc.react.js')
     : join(TOOLCHAIN_PACKAGE_DIR, '.eslintrc.js');
-  execTool('eslint', ['--config', config, '{src,test,browser-test,stories,playwright}/**/*.{js,ts,jsx,tsx}', ...additionalArgs]);
+  await execTool('eslint', ['--config', config, '{src,test,browser-test,stories,playwright}/**/*.{js,ts,jsx,tsx}', ...additionalArgs]);
 }
