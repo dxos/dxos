2726a74d766107d70e82d356e897c5f57e4f57da
Josiah Witt <josiah@witt.life>
2022-01-19 11:39:19 -0500

    feat: Add onJoin callback to JoinPartyDialog

--- packages/sdk/react-framework/src/containers/JoinPartyDialog.tsx
+++ packages/sdk/react-framework/src/containers/JoinPartyDialog.tsx
@@ -7,17 +7,23 @@ import React from 'react';
 import { useClient } from '@dxos/react-client';
 
 import { JoinDialog, JoinDialogProps } from './JoinDialog';
+import { PartyProxy } from '@dxos/client';
+
+export interface JoinPartyDialogProps extends Omit<JoinDialogProps, 'onJoin' | 'title'> {
+  onJoin?: (party: PartyProxy) => Promise<void> | void
+}
 
 /**
  * Manages the workflow of joining a Party invitation.
  */
-export const JoinPartyDialog = (props: Omit<JoinDialogProps, 'onJoin' | 'title'>) => {
+export const JoinPartyDialog = ({ onJoin, ...props }: JoinPartyDialogProps) => {
   const client = useClient();
 
   const handleJoin: JoinDialogProps['onJoin'] = async ({ invitation, secretProvider }) => {
     const redeemeingInvitation = client.echo.acceptInvitation(invitation);
     redeemeingInvitation.authenticate(await secretProvider());
-    await redeemeingInvitation.getParty();
+    const party = await redeemeingInvitation.getParty();
+    await onJoin?.(party);
   };
 
   return (
