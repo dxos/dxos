cbca865669533b0dbb1b0ee11284f2d786be1624
Marik-D <maretskii@gmail.com>
2022-01-19 12:21:33 +0100

    chore: Add timeout to individual package scripts

--- toolchains/toolchain-node-library/src/main.ts
+++ toolchains/toolchain-node-library/src/main.ts
@@ -15,7 +15,7 @@ import { execLint } from './tools/lint';
 import { execMocha } from './tools/mocha';
 import { execPackageScript } from './tools/packageScript';
 
-const PACKAGE_TIMEOUT = 10 //10 * 60 * 1000;
+const PACKAGE_TIMEOUT = 10 * 60 * 1000;
 
 interface BuildOptions {
   watch?: boolean
@@ -26,7 +26,7 @@ interface BuildOptions {
  *
  * @param opts.watch Keep tsc running in watch mode.
  */
-function execBuild (opts: BuildOptions = {}) {
+async function execBuild (opts: BuildOptions = {}) {
   const project = Project.load();
 
   if (project.packageJsonContents.jest) {
@@ -59,7 +59,7 @@ function execBuild (opts: BuildOptions = {}) {
       ? join(project.packageRoot, 'src/proto/substitutions.ts')
       : undefined;
 
-    execTool('build-protobuf', [
+    await execTool('build-protobuf', [
       '-o',
       join(project.packageRoot, 'src/proto/gen'),
       ...(substitutions ? ['-s', substitutions] : []),
@@ -68,29 +68,28 @@ function execBuild (opts: BuildOptions = {}) {
   }
 
   console.log(chalk.bold`\ntypescript`);
-  execTool('tsc', opts.watch ? ['--watch'] : []);
+  await execTool('tsc', opts.watch ? ['--watch'] : []);
 }
 
-function execTest (userArgs?: string[]) {
+async function execTest (userArgs?: string[]) {
   const project = Project.load();
   const forceClose = project.toolchainConfig.forceCloseTests ?? false;
   const jsdom = project.toolchainConfig.jsdom ?? false;
 
   if (project.toolchainConfig.testingFramework === 'mocha') {
     console.log(chalk.bold`\nmocha`);
-    execMocha({ userArgs, forceClose, jsdom });
+    await execMocha({ userArgs, forceClose, jsdom });
   } else {
     console.log(chalk.bold`\njest`);
-    execJest({ project, userArgs, forceClose });
+    await execJest({ project, userArgs, forceClose });
   }
 }
 
 function setPackageTimeout () {
-  console.log('set timeout')
   setTimeout(() => {
     process.stderr.write(chalk`{red error}: Timed out in ${PACKAGE_TIMEOUT / 1000}s\n`);
     process.exit(1);
-  }, 10);
+  }, PACKAGE_TIMEOUT);
 }
 
 // eslint-disable-next-line no-unused-expressions
@@ -105,9 +104,9 @@ yargs(process.argv.slice(2))
         default: false
       })
       .strict(),
-    (argv) => {
+    async (argv) => {
       const before = Date.now();
-      execBuild({ watch: argv.watch });
+      await execBuild({ watch: argv.watch });
       console.log(chalk`\n{green.bold BUILD COMPLETE} in {bold ${Date.now() - before}} ms`);
     }
   )
@@ -116,24 +115,24 @@ yargs(process.argv.slice(2))
     'build, lint, and test the package',
     yargs => yargs
       .strict(),
-    () => {
+    async () => {
       const project = Project.load();
 
       setPackageTimeout();
 
       const before = Date.now();
-      execBuild();
+      await execBuild();
 
       console.log(chalk.bold`\neslint`);
-      execLint(project);
+      await execLint(project);
 
-      execTest();
+      await execTest();
 
       // Additional test steps execution placed here to allow to run tests without additional steps.
       // Additional test steps are executed by default only when build:test is run.
       for (const step of project.toolchainConfig.additionalTestSteps ?? []) {
         console.log(chalk.bold`\n${step}`);
-        execPackageScript(project, step, []);
+        await execPackageScript(project, step, []);
       }
 
       console.log(chalk`\n{green.bold CHECK COMPLETE} in {bold ${Date.now() - before}} ms`);
@@ -143,32 +142,32 @@ yargs(process.argv.slice(2))
     'lint',
     'run linter',
     yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
-    ({ _ }) => {
+    async ({ _ }) => {
       const project = Project.load();
-      execLint(project, _.slice(1).map(String));
+      await execLint(project, _.slice(1).map(String));
     }
   )
   .command(
     'test',
     'run tests',
     yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
-    ({ _ }) => {
+    async ({ _ }) => {
       setPackageTimeout();
 
-      execTest(_.slice(1).map(String));
+      await execTest(_.slice(1).map(String));
     }
   )
   .command<{ command: string }>(
     ['* <command>', 'run <command>'],
     'run script or a tool',
     yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
-    ({ command, _ }) => {
+    async ({ command, _ }) => {
       const project = Project.load();
 
       if (project.packageJsonContents.scripts?.[command]) {
-        execCommand(project.packageJsonContents.scripts?.[command], _.map(String));
+        await execCommand(project.packageJsonContents.scripts?.[command], _.map(String));
       } else {
-        execCommand(command, _.map(String));
+        await execCommand(command, _.map(String));
       }
     }
   )
--- toolchains/toolchain-node-library/src/tools/common.ts
+++ toolchains/toolchain-node-library/src/tools/common.ts
@@ -3,40 +3,52 @@
 //
 
 import chalk from 'chalk';
-import { spawnSync, SpawnSyncOptionsWithBufferEncoding, SpawnSyncReturns } from 'child_process';
+import { ChildProcess, spawn, SpawnSyncOptionsWithBufferEncoding } from 'child_process';
 
 import { TOOLCHAIN_PACKAGE_DIR } from '../common';
 
-function printChildStatus (child: SpawnSyncReturns<any>, name: string, startTime: number) {
-  if (child.status === null) {
-    process.stderr.write(chalk`{red error}: ${name} terminated due to a signal: ${child.signal}\n`);
+function printChildStatus (child: ChildProcess, name: string, startTime: number) {
+  if (child.exitCode === null) {
+    process.stderr.write(chalk`{red error}: ${name} terminated due to a signal: ${child.signalCode}\n`);
     process.exit(1);
-  } else if (child.status !== 0) {
-    process.stderr.write(chalk`{red error}: ${name} exited with code ${child.status}\n`);
-    process.exit(child.status ?? 1);
+  } else if (child.exitCode !== 0) {
+    process.stderr.write(chalk`{red error}: ${name} exited with code ${child.exitCode}\n`);
+    process.exit(child.exitCode ?? 1);
   } else {
     console.log(chalk`{green.bold OK} in {bold ${Date.now() - startTime}} ms`);
   }
 }
 
-export function execTool (name: string, args: string[] = [], opts?: SpawnSyncOptionsWithBufferEncoding) {
+export async function execTool (name: string, args: string[] = [], opts?: SpawnSyncOptionsWithBufferEncoding) {
   const before = Date.now();
 
-  const child = spawnSync(`${TOOLCHAIN_PACKAGE_DIR}/node_modules/.bin/${name}`, args, { stdio: 'inherit', ...opts });
+  const child = spawn(`${TOOLCHAIN_PACKAGE_DIR}/node_modules/.bin/${name}`, args, { stdio: 'inherit', ...opts });
+
+  await new Promise<void>((resolve, reject) => {
+    child.on('exit', (code) => {
+      resolve();
+    });
+  });
 
   printChildStatus(child, name, before);
 }
 
-export function execCommand (command: string, args: string[]) {
+export async function execCommand (command: string, args: string[]) {
   const before = Date.now();
 
-  const child = spawnSync(command, args, {
+  const child = spawn(command, args, {
     shell: true,
     stdio: 'inherit',
     env: {
       ...process.env,
       PATH: `${TOOLCHAIN_PACKAGE_DIR}/node_modules/.bin:${process.cwd()}/node_modules/.bin:${process.env.PATH}`
     }
+  });
+
+  await new Promise<void>((resolve, reject) => {
+    child.on('exit', (code) => {
+      resolve();
+    });
   });
 
   printChildStatus(child, command, before);
