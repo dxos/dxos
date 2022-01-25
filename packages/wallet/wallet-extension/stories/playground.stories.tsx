b8dfa97899cdc0738ca17f498b588120af70de93
Marik-D <maretskii@gmail.com>
2022-01-25 13:49:47 +0100

    feat: Playground for the wallet popup

---
+++ packages/wallet/wallet-extension/stories/playground.stories.tsx
@@ -0,0 +1,46 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import React from 'react';
+
+import { createTheme, ThemeProvider } from '@mui/material';
+
+import { ClientProvider } from '@dxos/react-client';
+
+import { Main } from '../src/popup/containers/Main';
+import { WithSnackbarContext } from '../src/popup/hooks/useSnackbar';
+
+export default {
+  title: 'Wallet'
+};
+
+export const Popup = () => {
+  return (
+    <div style={{ backgroundColor: '#ccc', width: '100vw', height: '100vh' }}>
+      <div style={CONTAINER_STYLE}>
+        <ThemeProvider theme={theme}>
+          <ClientProvider>
+            <WithSnackbarContext>
+              <Main />
+            </WithSnackbarContext>
+          </ClientProvider>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
+
+const theme = createTheme();
+
+const CONTAINER_STYLE = {
+  position: 'absolute',
+  left: '50vw',
+  top: '50vh',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#fff',
+  margin: 0,
+  overflow: 'hidden',
+  width: 600,
+  height: 450
+} as const;
