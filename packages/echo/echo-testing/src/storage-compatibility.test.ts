be0a02d51f827093c1ce4cf76b8f9e29b3abba15
Marik-D <maretskii@gmail.com>
2022-01-25 17:02:59 +0100

    Add test that loads data from local storage

--- packages/echo/echo-testing/src/storage-compatibility.test.ts
+++ packages/echo/echo-testing/src/storage-compatibility.test.ts
@@ -1,2 +1,29 @@
+import { Item } from '@dxos/echo-db';
+import { ObjectModel } from '@dxos/object-model';
 import expect from 'expect';
 import { it as test } from 'mocha';
+import { join } from 'path';
+import { createPersistentInstance } from './persistant-instance';
+
+test('can load from saved state', async () => {
+  const storagePath = join(__dirname, '../saved-state');
+
+  const echo = createPersistentInstance(storagePath);
+  await echo.open();
+
+  expect(echo.halo.getProfile()?.username).toEqual('Test profile');
+
+  const person: Item<ObjectModel> = echo.queryParties().first.database.select(s => s.filter({ type: 'dxos:item.person' }).items).expectOne();
+
+  expect(person.model).toBeInstanceOf(ObjectModel)
+  expect(person.model.toObject()).toEqual({ firstName: 'Edith', lastName: 'Smith' });
+
+  expect(person.links).toHaveLength(1);
+  expect(person.links[0].type).toEqual('dxos:link.employee');
+
+  const company = person.links[0].target;
+  expect(company.type).toEqual('dxos:item.company');
+  expect(company.model.toObject()).toEqual({ name: 'DXOS' });
+
+  await echo.close();
+})
