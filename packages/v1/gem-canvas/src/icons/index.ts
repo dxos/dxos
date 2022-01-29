18eccc27d16247080492a568919540b51343da41
RB <rich.burdon@gmail.com>
2022-01-29 16:47:30 -0500

    Remove v1 canvas.

--- packages/v1/gem-canvas/src/icons/index.ts
+++
@@ -1,7 +0,0 @@
-//
-// Copyright 2020 DXOS.org
-//
-
-export { default as ContentCopy } from './ContentCopy';
-export { default as ContentCut } from './ContentCut';
-export { default as ContentPaste } from './ContentPaste';
