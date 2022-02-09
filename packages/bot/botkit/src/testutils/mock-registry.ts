4639c150c3d0b9cf2b4ebdd1ea656faf51340f28
Josiah Witt <josiah@witt.life>
2022-02-09 15:07:17 -0500

    fix: botkit test

--- packages/bot/botkit/src/testutils/mock-registry.ts
+++ packages/bot/botkit/src/testutils/mock-registry.ts
@@ -16,7 +16,7 @@ export const MOCK_BOT_DXN = 'dxos:bot:mock';
 
 export const createMockRegistryWithBot = (botPath: string) => {
   const types = createMockTypes();
-  const botTypeRecord = types.find(type => type.messageName === 'bot');
+  const botTypeRecord = types.find(type => type.messageName === '.dxos.type.Bot');
   assert(botTypeRecord, 'Bot type not found: bot');
   const records = createMockResourceRecords();
   const botRecord = createMockResourceRecord({
