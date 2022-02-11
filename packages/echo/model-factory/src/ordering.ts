f79c3cc8cee2d2f56106922267b31a88fc9ba9d4
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2022-02-11 13:37:13 +0200

    feat: Strong ordering for models (#927)
    * Add ordering function
    
    * Strong ordering
    
    * Remove test
    
    * Lint
    
    * Rename
    
    * Remove comment
    
    * Consistency
    
    * Hardcode theme
    
    * Fix echo issues
    
    * Lint
    
    * Use test rig in messenger model
    
    * Add test for update event
    
    * Lint
    
    * Cleanup
    
    * Regenerate lockfile

---
+++ packages/echo/model-factory/src/ordering.ts
@@ -0,0 +1,27 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { PublicKey } from '@dxos/crypto';
+import { Timeframe } from '@dxos/echo-protocol';
+
+import { ModelMessage } from './types';
+
+/**
+ * Ensures strong ordering of a mutation queue.
+ *
+ * If appending a new mutation would break the strong order, it returns the proper index to insert the new mutation.
+ * Otherwise it returns `exising.length`.
+ */
+export function getInsertionIndex (existing: ModelMessage<Uint8Array>[], newMutation: ModelMessage<Uint8Array>) {
+  for (let i = 0; i < existing.length; i++) {
+    const existingTimeframe = Timeframe.merge(existing[i].meta.timeframe, new Timeframe([[PublicKey.from(existing[i].meta.feedKey), existing[i].meta.seq - 1]]));
+    const deps = Timeframe.dependencies(newMutation.meta.timeframe, existingTimeframe);
+    if (deps.isEmpty()) {
+      if (PublicKey.from(newMutation.meta.feedKey).toHex() < PublicKey.from(existing[i].meta.feedKey).toHex()) {
+        return i;
+      }
+    }
+  }
+  return existing.length;
+}
--- packages/echo/text-model/src/text-model.test.ts
+++ packages/echo/text-model/src/text-model.test.ts
@@ -5,32 +5,27 @@
 import expect from 'expect';
 import { it as test } from 'mocha';
 
-import { createModelTestBench } from '@dxos/echo-db';
 import { MockFeedWriter } from '@dxos/echo-protocol';
-import { ModelFactory } from '@dxos/model-factory';
+import { ModelFactory, TestRig } from '@dxos/model-factory';
 
 import { TextModel } from './text-model';
 
 describe('TextModel', () => {
   test('insert', async () => {
-    const { items: [item1, item2], peers } = await createModelTestBench({ model: TextModel });
-    after(async () => Promise.all(peers.map(peer => peer.close())));
+    const rig = new TestRig(new ModelFactory().registerModel(TextModel), TextModel);
+    const peer1 = rig.createPeer();
+    const peer2 = rig.createPeer();
 
-    item1.model.insert(0, 'Hello World!');
+    peer1.model.insert(0, 'Hello World!');
 
-    await item2.model.update.waitForCount(1);
-    expect(item2.model.textContent).toBe('Hello World!');
+    await peer2.model.update.waitForCount(1);
+    expect(peer2.model.textContent).toBe('Hello World!');
 
     // TODO(burdon): Test delete.
-    const words = item1.model.textContent.split(' ');
-    item2.model.insert(words[0].length, ' DXOS');
-    await item1.model.update.waitForCount(1);
-    expect(item1.model.textContent).toBe('Hello DXOS World!');
-
-    // TODO(burdon): Errors. Race condition?
-    // code for await (const peer of peers) {
-    // code   await peer.close();
-    // code }
+    const words = peer1.model.textContent.split(' ');
+    peer2.model.insert(words[0].length, ' DXOS');
+    await peer1.model.update.waitForCount(1);
+    expect(peer1.model.textContent).toBe('Hello DXOS World!');
   });
 
   test('snapshot', async () => {
@@ -47,25 +42,19 @@ describe('TextModel', () => {
   });
 
   test('conflict', async () => {
-    const { items: [item1, item2], peers } = await createModelTestBench({ model: TextModel });
-    after(async () => Promise.all(peers.map(peer => peer.close())));
+    const rig = new TestRig(new ModelFactory().registerModel(TextModel), TextModel);
+    const peer1 = rig.createPeer();
+    const peer2 = rig.createPeer();
 
-    // code item1.model.update.on(() => console.log(`m1 ${item1.model.textContent}`));
-    // code item2.model.update.on(() => console.log(`m2 ${item2.model.textContent}`));
+    peer1.model.insert(0, 'Hello');
+    await rig.waitForReplication();
 
-    item1.model.insert(0, 'Hello');
-    await item2.model.update.waitForCount(1);
+    rig.configureReplication(false);
+    peer1.model.insert(5, '!');
+    peer2.model.insert(5, '?');
+    rig.configureReplication(true);
+    await rig.waitForReplication();
 
-    const updatesPromise = Promise.all([
-      item1.model.update.waitForCount(2),
-      item2.model.update.waitForCount(2)
-    ]);
-
-    item1.model.insert(5, '!');
-    item2.model.insert(5, '?');
-
-    await updatesPromise;
-
-    expect(item1.model.textContent).toBe(item2.model.textContent);
+    expect(peer1.model.textContent).toBe(peer2.model.textContent);
   });
 });
