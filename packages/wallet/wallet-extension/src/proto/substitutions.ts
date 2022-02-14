e019077abdd49daed2cb41a562e8d38ad371a4cc
Marik-D <maretskii@gmail.com>
2022-02-14 13:17:51 +0100

    Use own proto defs in wallet-extension

---
+++ packages/wallet/wallet-extension/src/proto/substitutions.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import { newAnySubstitutions, timestampSubstitutions } from '@dxos/codec-protobuf';
+import { publicKeySubstitutions } from '@dxos/crypto';
+
+export default {
+  ...timestampSubstitutions,
+  ...publicKeySubstitutions,
+  ...newAnySubstitutions
+};
