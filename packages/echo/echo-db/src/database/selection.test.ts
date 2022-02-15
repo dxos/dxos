8a89aadfadbeca698cdda37658faa1a6a764929a
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2022-02-15 11:52:14 +0200

    chore: Add test for selection API events (#940)

--- packages/echo/echo-db/src/database/selection.test.ts
+++ packages/echo/echo-db/src/database/selection.test.ts
@@ -5,11 +5,12 @@
 import expect from 'expect';
 import { it as test } from 'mocha';
 
-import { Event } from '@dxos/async';
+import { Event, promiseTimeout } from '@dxos/async';
 import { ItemID, ItemType } from '@dxos/echo-protocol';
 import { ModelFactory } from '@dxos/model-factory';
 import { ObjectModel } from '@dxos/object-model';
 
+import { Entity } from '.';
 import { Item } from './item';
 import { Link } from './link';
 import { createRootSelector } from './selection';
@@ -203,6 +204,35 @@ describe('Selection', () => {
         items[0],
         items[1]
       ]);
+    });
+  });
+
+  describe('events', () => {
+    test('events get filtered correctly', async () => {
+      const update = new Event<Entity[]>();
+      const select = createRootSelector(() => items, () => update, null as any);
+
+      const query = select({ type: OBJECT_ORG })
+        .children()
+        .query();
+
+      {
+        const promise = query.update.waitForCount(1);
+        update.emit([items[2]]);
+        await promiseTimeout(promise, 10, new Error('timeout'));
+      }
+
+      {
+        const promise = query.update.waitForCount(1);
+        update.emit([]);
+        await expect(promiseTimeout(promise, 10, new Error('timeout'))).rejects.toThrow('timeout');
+      }
+
+      {
+        const promise = query.update.waitForCount(1);
+        update.emit([items[0]]);
+        await expect(promiseTimeout(promise, 10, new Error('timeout'))).rejects.toThrow('timeout');
+      }
     });
   });
 });
