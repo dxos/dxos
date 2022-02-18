ef3b9819a9a3bba39f2e60d24c27a1e68c50b05e
sebikap-gm2 <97112934+sebikap-gm2@users.noreply.github.com>
2022-02-18 07:23:54 -0300

    fix(echo-db): When adding an item, parent item gets update triggered (#953)
    * When adding an item, parent item gets update triggered
    
    * Clean up and lint fixes
    
    * Refactor
    
    Co-authored-by: Marik-D <maretskii@gmail.com>
    Co-authored-by: Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>

--- packages/echo/echo-db/src/database/item-demuxer.ts
+++ packages/echo/echo-db/src/database/item-demuxer.ts
@@ -78,6 +78,7 @@ export class ItemDemuxer {
         } else {
           entity = await this._itemManager.constructItem({
             ...modelOpts,
+            parentId: itemMutation?.parentId,
             itemType
           });
         }
