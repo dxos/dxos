0b951738dec6ff1ab148d112a66603fab426c092
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2022-02-20 16:37:33 +0200

    fix: Race condition during devtools initialization. (#961)
    * Fix race condition in devtools
    
    * Lint

--- packages/devtools/devtools-extension/src/main-panel/main.ts
+++ packages/devtools/devtools-extension/src/main-panel/main.ts
@@ -20,6 +20,34 @@ void (async () => {
       }
     }
   }, { rpcPort: port });
+  await waitToBeReady();
   await client.initialize();
   initPanel(client);
 })();
+
+const TIMEOUT = 1000;
+
+function waitToBeReady () {
+  return new Promise<void>((resolve, reject) => {
+    const start = Date.now();
+
+    function check () {
+      chrome.devtools.inspectedWindow.eval(
+        '!!(window.__DXOS__.devtoolsReady);',
+        (result, isException) => {
+          if (!result || isException) {
+            if (Date.now() - start > TIMEOUT) {
+              reject(new Error('Timeout on waiting for client API to initialize.'));
+            } else {
+              setTimeout(check, 50);
+            }
+          } else {
+            resolve();
+          }
+        }
+      );
+    }
+
+    check();
+  });
+}
