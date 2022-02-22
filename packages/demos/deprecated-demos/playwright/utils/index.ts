f88bce5ea2c1c1cc73de1c9507deb13be2cdfb79
Rich Burdon <rich.burdon@gmail.com>
2022-02-22 16:19:30 -0500

    chore: echo demos config (#971)
    * Updated demos.
    
    * New demos with updated GEM library.
    
    * Data generator.
    
    * Schema.
    
    * Update gem deps.
    
    * Updated gem.
    
    * Layout.
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Update table and graph.
    
    * DataGrid.
    
    * Lint.
    
    * Update GH action.
    
    * Card list component with create button.
    
    * Editor.
    
    * Fix lint.
    
    * Fix lint.
    
    * Fix update.
    
    * List.
    
    * Searchbar.
    
    * Two-player mode.
    
    * Lint.
    
    * Rename config.
    
    * Hello world starter.
    
    * Fix.
    
    * Hello world demo.
    
    * Fix.
    
    * Clean-up.
    
    * Fix.
    
    * Fix config.

--- packages/demos/deprecated-demos/playwright/utils/index.ts
+++ packages/demos/deprecated-demos/playwright/utils/index.ts
@@ -2,17 +2,5 @@
 // Copyright 2020 DXOS.org
 //
 
-import { ConfigObject } from '@dxos/config';
-
 export * from './Browser';
-
-export const ONLINE_CONFIG: ConfigObject = {
-  version: 1,
-  runtime: {
-    services: {
-      signal: {
-        server: 'wss://apollo3.kube.moon.dxos.network/dxos/signal'
-      }
-    }
-  }
-};
+export * from './config';
