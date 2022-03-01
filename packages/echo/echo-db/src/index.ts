45a9b22e68a49251698cc1b7de63e175de5561b2
Rich Burdon <rich.burdon@gmail.com>
2022-02-28 20:50:35 -0500

    Reorder.

--- packages/echo/echo-db/src/index.ts
+++ packages/echo/echo-db/src/index.ts
@@ -4,14 +4,14 @@
 
 export { codec } from '@dxos/echo-protocol';
 
-export * from './echo';
-export * from './invitations';
 export * from './database';
+export * from './echo';
+export * from './errors';
+export * from './halo';
+export * from './invitations';
+export * from './metadata';
 export * from './parties';
 export * from './pipeline';
 export * from './result';
 export * from './snapshots';
 export * from './util';
-export * from './halo';
-export * from './errors';
-export * from './metadata';
