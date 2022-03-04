c4c86aec35b60a5f387413b59c4daf23b9a908b4
Josiah Witt <josiah@witt.life>
2022-03-04 08:21:33 -0500

    refactor: Unify client reset (#1026)

--- packages/wallet/wallet-extension/src/popup/containers/Main.tsx
+++ packages/wallet/wallet-extension/src/popup/containers/Main.tsx
@@ -55,7 +55,7 @@ export const Main = () => {
   const handleReset = async () => {
     setInProgress(true);
     try {
-      await client.halo.reset();
+      await client.reset();
       window.location.reload();
     } catch (e: any) {
       console.error(e);
