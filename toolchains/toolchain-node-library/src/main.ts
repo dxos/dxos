1e5a31d2926f4b6997fa4937135229062c430201
Josiah Witt <josiah@witt.life>
2022-03-10 13:49:56 -0500

    feat: Reusable toolchain

--- toolchains/toolchain-node-library/src/main.ts
+++ toolchains/toolchain-node-library/src/main.ts
@@ -6,7 +6,7 @@ import chalk from 'chalk';
 import * as fs from 'fs';
 import { sync as glob } from 'glob';
 import { join } from 'path';
-import yargs, { Arguments } from 'yargs';
+import { Arguments, Argv } from 'yargs';
 
 import { Config, defaults } from './config';
 import { Project } from './project';
@@ -52,10 +52,6 @@ interface BuildOptions {
  */
 async function execBuild (config: Config, options: BuildOptions = {}) {
   const project = Project.load(config);
-
-  if (project.packageJsonContents.eslintConfig && !project.packageJsonContents.toolchain?.allowExtendedEslintConfig) {
-    process.stderr.write(chalk`{yellow warn}: eslint config in package.json is ignored.\n`);
-  }
 
   try {
     fs.rmSync(join(project.packageRoot, config.protobuf.output), { recursive: true, force: true });
@@ -169,152 +165,151 @@ async function execTest (config: Config, userArgs?: string[]) {
 }
 
 /**
- * Main yargs entry-point.
+ * Builds core yargs commands for toolchain.
+ * Returns yargs so it can be chained with custom commands.
  */
-// eslint-disable-next-line no-unused-expressions
-yargs(process.argv.slice(2))
-
-  .option('verbose', {
-    alias: 'v',
-    type: 'boolean',
-    default: false
-  })
-
-//
-// Build
-//
-
-  .command<{ verbose?: boolean, watch?: boolean }>(
-    'build',
-    'Build the package.',
-    yargs => yargs
-      .option('watch', {
-        alias: 'w',
-        type: 'boolean',
-        default: false
-      })
-      .strict(),
-    async (argv) => {
-      await execBuild(defaults, { verbose: argv.verbose, watch: argv.watch });
-    }
-  )
-
-  .command(
-    'build:bundle',
-    'Build a bundle for the package.',
-    yargs => yargs
-      .option('minify', {
-        type: 'boolean',
-        default: false
-      })
-      .strict(),
-    handler<{ minify: boolean }>('Bundle', async (argv) => {
-      await execBuildBundle(defaults, { minify: argv.minify });
+export const setupCoreCommands = (yargs: Argv) => (
+  yargs
+    .option('verbose', {
+      alias: 'v',
+      type: 'boolean',
+      default: false
     })
-  )
 
-  .command(
-    'build:test',
-    'build, lint, and test the package',
-    yargs => yargs
-      .strict(),
-    handler('Tests', async () => {
-      const project = Project.load(defaults);
-      await execBuild(defaults);
-      await execLint(project);
-      await execTest(defaults);
+  //
+  // Build
+  //
 
-      // Additional test steps execution placed here to allow to run tests without additional steps.
-      // Additional test steps are executed by default only when build:test is run.
-      for (const step of project.toolchainConfig.additionalTestSteps ?? []) {
-        console.log(chalk`\n{green.bold ${step}}`);
-        await execScript(project, step, []);
+    .command<{ verbose?: boolean, watch?: boolean }>(
+      'build',
+      'Build the package.',
+      yargs => yargs
+        .option('watch', {
+          alias: 'w',
+          type: 'boolean',
+          default: false
+        })
+        .strict(),
+      async (argv) => {
+        await execBuild(defaults, { verbose: argv.verbose, watch: argv.watch });
       }
-    }, true)
-  )
+    )
 
-//
-// ESBuild server/book
-// TODO(burdon): Out directory's index.html overwritten build build:bundle
-//
-
-  .command(
-    'build:book',
-    'Build the storybook for the package.',
-    yargs => yargs
-      .option('minify', {
-        type: 'boolean',
-        default: false
+    .command(
+      'build:bundle',
+      'Build a bundle for the package.',
+      yargs => yargs
+        .option('minify', {
+          type: 'boolean',
+          default: false
+        })
+        .strict(),
+      handler<{ minify: boolean }>('Bundle', async (argv) => {
+        await execBuildBundle(defaults, { minify: argv.minify });
       })
-      .strict(),
-    handler<{ minify: boolean }>('Build book', async (argv) => {
-      await execBuildBook(defaults, { minify: argv.minify });
-    })
-  )
+    )
 
-  .command(
-    'book',
-    'Run the storybook for the package.',
-    yargs => yargs
-      .strict(),
-    async () => {
-      await execBook();
-    }
-  )
+    .command(
+      'build:test',
+      'build, lint, and test the package',
+      yargs => yargs
+        .strict(),
+      handler('Tests', async () => {
+        const project = Project.load(defaults);
+        await execBuild(defaults);
+        await execLint(project);
+        await execTest(defaults);
 
-  .command(
-    'start',
-    'Run a dev server for the package.',
-    yargs => yargs
-      .strict(),
-    async () => {
-      await execStart();
-    }
-  )
+        // Additional test steps execution placed here to allow to run tests without additional steps.
+        // Additional test steps are executed by default only when build:test is run.
+        for (const step of project.toolchainConfig.additionalTestSteps ?? []) {
+          console.log(chalk`\n{green.bold ${step}}`);
+          await execScript(project, step, []);
+        }
+      }, true)
+    )
 
-//
-// Testing
-//
+  //
+  // ESBuild server/book
+  // TODO(burdon): Out directory's index.html overwritten build build:bundle
+  //
 
-  .command(
-    'test',
-    'run tests',
-    yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
-    handler('Tests', async ({ _ }) => {
-      await execTest(defaults, _.slice(1).map(String));
-    }, true)
-  )
+    .command(
+      'build:book',
+      'Build the storybook for the package.',
+      yargs => yargs
+        .option('minify', {
+          type: 'boolean',
+          default: false
+        })
+        .strict(),
+      handler<{ minify: boolean }>('Build book', async (argv) => {
+        await execBuildBook(defaults, { minify: argv.minify });
+      })
+    )
 
-//
-// Lint
-//
-
-  .command(
-    'lint',
-    'run linter',
-    yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
-    async ({ _ }) => {
-      const project = Project.load(defaults);
-      await execLint(project, _.slice(1).map(String));
-    }
-  )
-
-//
-// Run scripts.
-//
-
-  .command<{ command: string }>(
-    ['* <command>', 'run <command>'],
-    'run script or a tool',
-    yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
-    async ({ command, _ }) => {
-      const project = Project.load(defaults);
-      if (project.packageJsonContents.scripts?.[command]) {
-        await execCommand(project.packageJsonContents.scripts?.[command], _.map(String));
-      } else {
-        await execCommand(command, _.map(String));
+    .command(
+      'book',
+      'Run the storybook for the package.',
+      yargs => yargs
+        .strict(),
+      async () => {
+        await execBook();
       }
-    }
-  )
+    )
 
-  .argv;
+    .command(
+      'start',
+      'Run a dev server for the package.',
+      yargs => yargs
+        .strict(),
+      async () => {
+        await execStart();
+      }
+    )
+
+  //
+  // Testing
+  //
+
+    .command(
+      'test',
+      'run tests',
+      yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
+      handler('Tests', async ({ _ }) => {
+        await execTest(defaults, _.slice(1).map(String));
+      }, true)
+    )
+
+  //
+  // Lint
+  //
+
+    .command(
+      'lint',
+      'run linter',
+      yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
+      async ({ _ }) => {
+        const project = Project.load(defaults);
+        await execLint(project, _.slice(1).map(String));
+      }
+    )
+
+  //
+  // Run scripts.
+  //
+
+    .command<{ command: string }>(
+      ['* <command>', 'run <command>'],
+      'run script or a tool',
+      yargs => yargs.parserConfiguration({ 'unknown-options-as-args': true }),
+      async ({ command, _ }) => {
+        const project = Project.load(defaults);
+        if (project.packageJsonContents.scripts?.[command]) {
+          await execCommand(project.packageJsonContents.scripts?.[command], _.map(String));
+        } else {
+          await execCommand(command, _.map(String));
+        }
+      }
+    )
+);
