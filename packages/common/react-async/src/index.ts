3916ee32deb1db93b0a5d118cb0529fba8899958
Rich Burdon <rich.burdon@gmail.com>
2022-03-16 14:24:09 -0400

    feat: React async utils. (#1054)
    * New React async util.
    
    * Immutability helper.

---
+++ packages/common/react-async/src/index.ts
@@ -0,0 +1,11 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+export * from './useAsyncEffect';
+export * from './useControlledState';
+export * from './useDynamicRef';
+export * from './useMounted';
+export * from './useStateUpdater';
+export * from './useStateWithRef';
+export * from './useTimestamp';
---
+++ packages/common/react-async/src/useTimestamp.ts
@@ -0,0 +1,27 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { useEffect, useState } from 'react';
+
+/**
+ * Provides a timestamp that can be used to force re-rendering based on deps.
+ *
+ * ```tsx
+ * const [, update] = useTimestamp();
+ * const handleRefresh = () => update();
+ * ```
+ *
+ * @param deps
+ */
+export const useTimestamp = (deps?: any[]): [number, () => void, number | undefined] => {
+  const [{ timestamp, previous }, setTimestamp] = useState<{ timestamp: number, previous?: number }>({
+    timestamp: Date.now()
+  });
+
+  useEffect(() => {
+    setTimestamp({ timestamp: Date.now(), previous: timestamp });
+  }, deps || []);
+
+  return [timestamp, () => setTimestamp({ timestamp: Date.now(), previous: timestamp }), previous];
+};
