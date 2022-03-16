a65fe666a8fe90bf337b2bbad604073dc0f9b69a
Rich Burdon <rich.burdon@gmail.com>
2022-03-16 14:24:09 -0400

    feat: React async utils. (#1054)
    * New React async util.
    
    * Immutability helper.

---
+++ packages/common/react-async/src/useStateUpdater.test.tsx
@@ -0,0 +1,58 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import expect from 'expect';
+import 'raf/polyfill';
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import waitForExpect from 'wait-for-expect';
+
+import { useStateUpdater } from './useStateUpdater';
+
+// Don't copy this.
+const complex = {
+
+};
+
+const Test = () => {
+  const [value,, updateValue] = useStateUpdater({ complex, items: [] });
+  useEffect(() => {
+    updateValue({
+      items: {
+        $push: [1, 2, 3]
+      }
+    });
+  }, []);
+
+  return (
+    <pre>{JSON.stringify(value)}</pre>
+  );
+};
+
+let rootContainer: any;
+
+beforeEach(() => {
+  rootContainer = document.createElement('div');
+  document.body.appendChild(rootContainer);
+});
+
+afterEach(() => {
+  document.body.removeChild(rootContainer);
+  rootContainer = null;
+});
+
+describe('useStateMutator', () => {
+  it('udpates the value.', async () => {
+    act(() => {
+      ReactDOM.render(<Test />, rootContainer);
+    });
+
+    const pre = rootContainer.querySelector('pre');
+    await waitForExpect(() => {
+      const data = JSON.parse(pre.textContent);
+      expect(data).toEqual({ complex, items: [1, 2, 3] });
+    });
+  });
+});
