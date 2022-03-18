76adabbad596aa9b779c177ee6e39b51f5df8a40
Sebastian Kaplanski <sebastian.kaplanski@gm2dev.com>
2022-03-18 14:33:17 -0300

    Added story to test party import

--- packages/demos/kitchen-sink/stories/InvitationDialog.stories.tsx
+++ packages/demos/kitchen-sink/stories/InvitationDialog.stories.tsx
@@ -3,10 +3,14 @@
 //
 
 import faker from 'faker';
-import React from 'react';
+import React, { useState } from 'react';
 
+import { Box } from '@mui/material';
+
+import { Party } from '@dxos/client';
 import { ClientProvider, ProfileInitializer } from '@dxos/react-client';
 import { FullScreen } from '@dxos/react-components';
+import { usePartyImportExport } from '@dxos/react-framework';
 
 import { InvitationDialog } from '../src';
 
@@ -24,6 +28,7 @@ const App = () => {
         title='Kitchen Sink'
         onCreate={() => {}}
         onJoin={(invitationCode: string) => {}}
+        onImportParty={() => {}}
       />
     </FullScreen>
   );
@@ -34,6 +39,52 @@ export const Primary = () => {
     <ClientProvider>
       <ProfileInitializer>
         <App />
+      </ProfileInitializer>
+    </ClientProvider>
+  );
+};
+
+const InvitationDialogImportPartyStory = () => {
+  const [party, setParty] = useState<Party>();
+  const { onImportParty } = usePartyImportExport();
+
+  const handleImportParty = async (partyFile: File) => {
+    const importedParty = await onImportParty(partyFile);
+    setParty(importedParty);
+  };
+
+  return (
+    <FullScreen>
+      <InvitationDialog
+        open
+        title='Import Party Testing'
+        onCreate={() => {}}
+        onJoin={() => {}}
+        onImportParty={handleImportParty}
+      />
+      {party && (
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          fontSize: 20,
+          '.MuiBackdrop-root': {
+            bgcolor: undefined
+          }
+        }}>
+          <span>Party was imported. Party key: </span>
+          <span>{party.key.toHex()}</span>
+        </Box>
+      )}
+    </FullScreen>
+  )
+};
+
+export const ImportParty = () => {
+  return (
+    <ClientProvider>
+      <ProfileInitializer>
+        <InvitationDialogImportPartyStory />
       </ProfileInitializer>
     </ClientProvider>
   );
