454da6b8ea13fe4e4f6be9ce2f8d94ebbf4c73e4
egorgripasov <gripasovegor@gmail.com>
2022-03-21 14:56:47 -0400

    fix: move running bots out of cwd & ipfs fix.

--- packages/bot/botkit/src/config.ts
+++ packages/bot/botkit/src/config.ts
@@ -2,12 +2,17 @@
 // Copyright 2020 DXOS.org
 //
 
+import { join } from 'path';
+import { homedir } from 'os';
+
 import { Config, mapFromKeyValues } from '@dxos/config';
 
 import defaults from './defaults.json';
 import envmap from './env-map.json';
 
 export const BOT_CONFIG_FILENAME = 'bot.yml';
+
+export const BOT_OUT_DIR = join(homedir(), '.dx', 'bots', 'out');
 
 /**
  * Get config from default or specified .yml file.
