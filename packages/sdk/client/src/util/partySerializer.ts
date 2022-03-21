6b6eb0dc948f14045eaa22b3a84780a16d5c5817
Sebastian Kaplanski <sebastian.kaplanski@gm2dev.com>
2022-03-21 16:17:05 -0300

    Fixed proto/schema import

--- packages/sdk/client/src/util/partySerializer.ts
+++ packages/sdk/client/src/util/partySerializer.ts
@@ -2,8 +2,9 @@
 // Copyright 2022 DXOS.org
 //
 
-import { Party, proto } from '..';
+import { Party } from '../api';
 import { Client } from '../client';
+import { schema } from '../proto/gen/';
 
 export class PartySerializer {
   private _client: Client;
@@ -18,7 +19,7 @@ export class PartySerializer {
 
   async importParty (partyFileToImport: File) {
     const data = await this._deserializeParty(partyFileToImport);
-    return await this._client.echo.cloneParty(proto.schema.getCodecForType('dxos.echo.snapshot.PartySnapshot').decode(data));
+    return await this._client.echo.cloneParty(schema.getCodecForType('dxos.echo.snapshot.PartySnapshot').decode(data));
   }
 
   private async _deserializeParty (file: File) {
@@ -27,6 +28,6 @@ export class PartySerializer {
 
   private async _serializeParty (party: Party) {
     const snapshot = await party.createSnapshot();
-    return new Blob([proto.schema.getCodecForType('dxos.echo.snapshot.PartySnapshot').encode(snapshot)]);
+    return new Blob([schema.getCodecForType('dxos.echo.snapshot.PartySnapshot').encode(snapshot)]);
   }
 }
