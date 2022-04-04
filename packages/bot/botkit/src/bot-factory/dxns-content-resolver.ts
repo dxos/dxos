fc8618d7dfd70b350dfbd0335854c1965772102e
Egor Gripasov <gripasovegor@gmail.com>
2022-04-04 13:42:16 -0400

    fix: spawn bots by version/tag. (#1097)
    * fix: spawn bots by version/tag.
    
    * fix tests.

--- packages/bot/botkit/src/bot-factory/dxns-content-resolver.ts
+++ packages/bot/botkit/src/bot-factory/dxns-content-resolver.ts
@@ -9,14 +9,14 @@ import { CID, DXN, IRegistryClient, RegistryDataRecord } from '@dxos/registry-cl
 import { BotPackageSpecifier } from '../proto/gen/dxos/bot';
 import { Bot } from '../proto/gen/dxos/type';
 
-export type ContentResolverResult = Omit<BotPackageSpecifier, 'dxn'>;
+export type ContentResolverResult = Omit<BotPackageSpecifier, 'dxn' | 'name'>;
 
 export interface ContentResolver {
   /**
    * Resolves resource identifier to a specifier where the content can be downloaded from.
    * @returns A specifier where the content can be downloaded from.
    */
-  resolve: (dxn: string) => Promise<ContentResolverResult>;
+  resolve: ({ dxn, name }: { dxn?: string, name?: string }) => Promise<ContentResolverResult>;
 }
 
 export class DXNSContentResolver implements ContentResolver {
@@ -24,14 +24,15 @@ export class DXNSContentResolver implements ContentResolver {
     private readonly _registry: IRegistryClient
   ) {}
 
-  async resolve (dxn: string): Promise<ContentResolverResult> {
-    const botDXN = DXN.parse(dxn);
-    const botResourceRecord = await this._registry.getResourceRecord<RegistryDataRecord<Bot>>(botDXN, 'latest');
-    assert(botResourceRecord, `Bot resource not found: ${dxn.toString()}`);
+  async resolve ({ dxn, name }: { dxn?: string, name?: string }): Promise<ContentResolverResult> {
+    const [packageDXN, versionOrTag = 'latest'] = (name ?? dxn)!.split('@', 2);
+    const botDXN = DXN.parse(packageDXN);
+    const botResourceRecord = await this._registry.getResourceRecord<RegistryDataRecord<Bot>>(botDXN, versionOrTag);
+    assert(botResourceRecord, `Bot resource not found: ${packageDXN.toString()}@${versionOrTag}`);
     const botIpfsCID = botResourceRecord.record.data.hash;
     const botLocalPath = botResourceRecord.record.data.localPath;
     if (!botIpfsCID && !botLocalPath) {
-      throw new Error(`Unable to resolve bot content byt the provided dxn: ${dxn.toString()}`);
+      throw new Error(`Unable to resolve bot content byt the provided dxn: ${packageDXN.toString()}`);
     }
 
     const result: ContentResolverResult = {};
