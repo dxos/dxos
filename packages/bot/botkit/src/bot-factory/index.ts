4f133c49c2d5e36c697667cb546b5d06fc780615
Egor Gripasov <gripasovegor@gmail.com>
2022-04-13 18:56:08 -0400

    fix!: bots respawn. (#1116)
    * fix: desired state of a bot.
    
    * feat!: respawn all bots on restart.
    
    * fix: fix tests.

--- packages/bot/botkit/src/bot-factory/index.ts
+++ packages/bot/botkit/src/bot-factory/index.ts
@@ -5,5 +5,6 @@
 export * from './bot-factory';
 export * from './bot-handle';
 export * from './bot-controller';
+export * from './fs-bot-snapshot-storage';
 export * from './ipfs-content-loader';
 export * from './dxns-content-resolver';
