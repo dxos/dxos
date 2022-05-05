798fc2519d64e388625afb7a1e520563fb6955b4
Sebikap <sebastian.kaplanski@gm2dev.com>
2022-05-05 15:45:20 -0300

    feat(Schema): useSchemaBuilder hook

---
+++ packages/sdk/react-client-testing/src/hooks/useSchemaBuilder.ts
@@ -0,0 +1,15 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { useMemo } from 'react';
+
+import { Party } from '@dxos/client';
+import { SchemaBuilder } from '@dxos/client-testing';
+
+/**
+ * @param party
+ */
+export const useSchemaBuilder = (party: Party) => {
+  return useMemo(() => party ? new SchemaBuilder(party.database) : undefined, [party?.key.toHex()]);
+};
