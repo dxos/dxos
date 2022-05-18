6a02d19b666de0f5da71e2d435168e6a2a876943
Sebikap <sebastian.kaplanski@gm2dev.com>
2022-05-18 11:51:21 -0300

    dnd panel improvements

--- packages/sdk/react-client-testing/stories/helpers/DragAndDropDebugPanel.tsx
+++ packages/sdk/react-client-testing/stories/helpers/DragAndDropDebugPanel.tsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 
+import { Party } from '@dxos/client';
 import { truncateKey } from '@dxos/debug';
 
 const DEBUG_PANEL_WIDTH = 400;
@@ -13,27 +14,37 @@ type Order = { [key: string]: string }
 interface DragAndDropDebugPanelProps {
   previousOrder: Order | undefined,
   order: Order
+  party?: Party
   width? : number
 }
 
 export const DragAndDropDebugPanel = ({
   previousOrder,
   order,
+  party,
   width = DEBUG_PANEL_WIDTH
 }: DragAndDropDebugPanelProps) => {
+  const getStringToDisplay = (key: string) => {
+    if (!party) {
+      return truncateKey(key, 5);
+    }
+    const [item] = party?.select({ id: key }).exec().entities ?? [];
+    return truncateKey(key, 5) + ' - ' + item?.model.get('title').substring(0, 5);
+  };
+
   const reduceKeyLength = (order: {[key: string]: string}) => {
-    return Object.assign({}, ...Object.entries(order).map(([key, value]) => ({
-      [truncateKey(key, 5)]: truncateKey(value, 5)
+    return Object.assign({}, ...Object.entries(order).map(([leftId, rightId]) => ({
+      [getStringToDisplay(leftId)]: getStringToDisplay(rightId)
     })));
   };
 
   return (
-    <div style={{ display: 'flex', fontSize: 18 }}>
+    <div style={{ display: 'flex', fontSize: 14 }}>
       <div style={{ width }}>
         <h3>Previous <span style={{ fontWeight: 100 }}>(truncated)</span></h3>
         <pre>{JSON.stringify(previousOrder ? reduceKeyLength(previousOrder) : {}, undefined, 2)}</pre>
       </div>
-      <div style={{ marginLeft: 8, width }}>
+      <div style={{ marginLeft: 16, width }}>
         <h3>Current <span style={{ fontWeight: 100 }}>(truncated)</span></h3>
         <pre>{JSON.stringify(reduceKeyLength(order), undefined, 2)}</pre>
       </div>
