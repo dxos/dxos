773cb53f112a53ffdac964328b44452f093a0345
Josiah Witt <josiah@witt.life>
2022-05-30 08:27:55 -0400

    feat(devtools): Render panel before client is ready with a loader (#1208)
    * feat(devtools): Render panel before client is ready with a loader
    
    * chore: Fix logs

--- packages/devtools/devtools/src/App.tsx
+++ packages/devtools/devtools/src/App.tsx
@@ -2,25 +2,45 @@
 // Copyright 2022 DXOS.org
 //
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
+import { Event } from '@dxos/async';
 import { Client } from '@dxos/client';
 import { ClientProvider } from '@dxos/react-client';
+import { FullScreen } from '@dxos/react-components';
 import { ErrorBoundary } from '@dxos/react-toolkit';
 
+import { Loader } from './components';
 import { PanelsContainer } from './containers';
 import { sections } from './sections';
 import { theme } from './theme';
 
-export const App = ({ client }: { client: Client }) => (
-  <ErrorBoundary>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <ClientProvider client={client}>
-        <PanelsContainer sections={sections} />
-      </ClientProvider>
-    </ThemeProvider>
-  </ErrorBoundary>
+const Main = ({ client }: { client: Client }) => (
+  <ClientProvider client={client}>
+    <PanelsContainer sections={sections} />
+  </ClientProvider>
 );
+
+export const App = ({ clientReady }: { clientReady: Event<Client> }) => {
+  const [client, setClient] = useState<Client>();
+
+  useEffect(() => {
+    clientReady.on(client => setClient(client));
+  }, []);
+
+  return (
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <FullScreen>
+          <Loader loading={!client} label='Looking for DXOS Client...' />
+        </FullScreen>
+
+        {client && <Main client={client} />}
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+};
--- packages/devtools/devtools/src/bootstrap.tsx
+++ packages/devtools/devtools/src/bootstrap.tsx
@@ -5,20 +5,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Event } from '@dxos/async';
 import { Client } from '@dxos/client';
 
 import { App } from './App';
 
 export interface Shell {
   tabId: number,
-  connect(cb: (client: Client) => void): void;
+  connect(cb: (clientReady: Event<Client>) => void): void;
   onReload(cb: () => void): void;
 }
 
 export const initialize = (shell: Shell) => {
-  shell.connect(client => {
+  shell.connect(clientReady => {
     ReactDOM.render(
-      <App client={client} />,
+      <App clientReady={clientReady} />,
       document.getElementById('root')
     );
   });
