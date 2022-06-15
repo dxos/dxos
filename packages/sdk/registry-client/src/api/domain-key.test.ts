ac30833edf574bd34da81a591c54015fca4e5bd1
Josiah Witt <josiah@witt.life>
2022-06-15 14:20:26 -0400

    refactor(registry-client): Add tag to DXN (#1241)

---
+++ packages/sdk/registry-client/src/api/domain-key.test.ts
@@ -0,0 +1,19 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import { expect } from 'chai';
+
+import { randomBytes } from '@dxos/crypto';
+
+import { DomainKey } from './domain-key';
+
+describe('DomainKey', () => {
+  it('has key length of 32 bytes', () => {
+    expect(() => new DomainKey(randomBytes(32))).to.not.throw();
+  });
+
+  it('throws with invalid key length', () => {
+    expect(() => new DomainKey(randomBytes(24))).to.throw();
+  });
+});
