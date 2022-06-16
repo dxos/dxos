3e860b8a9724f3b559457641cfb9e81eb3d65695
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2022-06-16 11:32:51 +0200

    chore: Add extra tests for feed handling and replication (#1245)
    * Open new admitted feed in party-core
    
    * Refactor replication test
    
    * Unskip 3 devices test
    
    * Lint

--- packages/echo/echo-db/src/parties/party-core.test.ts
+++ packages/echo/echo-db/src/parties/party-core.test.ts
@@ -5,17 +5,19 @@
 import expect from 'expect';
 import { it as test } from 'mocha';
 
+import { promiseTimeout } from '@dxos/async';
 import { createFeedAdmitMessage, createPartyGenesisMessage, Keyring, KeyType } from '@dxos/credentials';
-import { PublicKey } from '@dxos/crypto';
-import { codec } from '@dxos/echo-protocol';
+import { createId, PublicKey } from '@dxos/crypto';
+import { codec, Timeframe } from '@dxos/echo-protocol';
 import { FeedStore } from '@dxos/feed-store';
+import { createTestProtocolPair } from '@dxos/mesh-protocol';
 import { ModelFactory } from '@dxos/model-factory';
 import { ObjectModel } from '@dxos/object-model';
 import { createStorage, STORAGE_RAM } from '@dxos/random-access-multi-storage';
 import { afterTest } from '@dxos/testutils';
 
 import { MetadataStore } from '../metadata';
-import { PartyFeedProvider } from '../pipeline';
+import { PartyFeedProvider, ReplicatorProtocolPluginFactory } from '../pipeline';
 import { SnapshotStore } from '../snapshots';
 import { createRamStorage } from '../util';
 import { PartyCore } from './party-core';
@@ -65,10 +67,7 @@ describe('PartyCore', () => {
       [partyKey]
     ));
 
-    // The Party key is an inception key; its SecretKey must be destroyed once the Party has been created.
-    await keyring.deleteSecretKey(partyKey);
-
-    return { party, feedKey: feed.key, feed, feedStore };
+    return { party, feedKey: feed.key, feed, feedStore, partyKey, keyring, partyFeedProvider };
   };
 
   test('create & have the feed key admitted', async () => {
@@ -114,5 +113,229 @@ describe('PartyCore', () => {
       expect(child.parent).toEqual(parent);
       expect(parent.children).toContain(child);
     }
+  });
+
+  test('feed admit message triggers new feed to be opened', async () => {
+    const { party, partyKey, keyring, partyFeedProvider, feedStore } = await setup();
+
+    const feedKey = await keyring.createKeyRecord({ type: KeyType.FEED });
+
+    const eventFired = feedStore.feedOpenedEvent.waitForCount(1);
+    await party.processor.writeHaloMessage(createFeedAdmitMessage(
+      keyring,
+      party.key,
+      feedKey.publicKey,
+      [partyKey]
+    ));
+    await promiseTimeout(eventFired, 1000, new Error('timeout'));
+    expect(partyFeedProvider.getFeeds().find(k => k.key.equals(feedKey.publicKey))).toBeTruthy();
+  });
+
+  test('opens feed from hints', async () => {
+    const storage = createStorage('', STORAGE_RAM);
+    const feedStore = new FeedStore(storage, { valueEncoding: codec });
+    afterTest(async () => feedStore.close());
+
+    const keyring = new Keyring();
+
+    const metadataStore = new MetadataStore(createRamStorage());
+
+    const modelFactory = new ModelFactory().registerModel(ObjectModel);
+    const snapshotStore = new SnapshotStore(createStorage('', STORAGE_RAM));
+
+    const partyKey = await keyring.createKeyRecord({ type: KeyType.PARTY });
+
+    const partyFeedProvider = new PartyFeedProvider(metadataStore, keyring, feedStore, partyKey.publicKey);
+
+    const otherFeedKey = PublicKey.random();
+
+    const party = new PartyCore(
+      partyKey.publicKey,
+      partyFeedProvider,
+      modelFactory,
+      snapshotStore,
+      PublicKey.random()
+    );
+
+    await partyFeedProvider.createOrOpenWritableFeed();
+
+    const feedOpened = feedStore.feedOpenedEvent.waitForCount(1);
+
+    await party.open([{ type: KeyType.FEED, publicKey: otherFeedKey }]);
+    afterTest(async () => party.close());
+
+    await feedOpened;
+
+    expect(partyFeedProvider.getFeeds().some(k => k.key.equals(otherFeedKey))).toEqual(true);
+  });
+
+  test('manually create item', async () => {
+    const { party, partyFeedProvider } = await setup();
+    await party.open();
+
+    const feed = await partyFeedProvider.createOrOpenWritableFeed();
+
+    const itemId = createId();
+    await feed.feed.append({
+      echo: {
+        itemId,
+        genesis: {
+          itemType: 'dxos:example',
+          modelType: ObjectModel.meta.type
+        },
+        timeframe: new Timeframe()
+      }
+    });
+
+    await promiseTimeout(party.database.waitForItem({ id: itemId }), 1000, new Error('timeout'));
+  });
+
+  test('admit a second feed to the party', async () => {
+    const { party, keyring, partyKey, feedStore } = await setup();
+    await party.open();
+
+    const feedKey = await keyring.createKeyRecord({ type: KeyType.FEED });
+    const fullKey = keyring.getFullKey(feedKey.publicKey);
+    const feed2 = await feedStore.openReadWriteFeed(fullKey!.publicKey, fullKey!.secretKey!);
+
+    await party.processor.writeHaloMessage(createFeedAdmitMessage(
+      keyring,
+      party.key,
+      feed2.key,
+      [partyKey]
+    ));
+
+    const itemId = createId();
+    await feed2.append({
+      echo: {
+        itemId,
+        genesis: {
+          itemType: 'dxos:example',
+          modelType: ObjectModel.meta.type
+        },
+        timeframe: new Timeframe()
+      }
+    });
+
+    await promiseTimeout(party.database.waitForItem({ id: itemId }), 1000, new Error('timeout'));
+  });
+
+  test('admit feed and then open it', async () => {
+    const { party, keyring, partyKey, feedStore } = await setup();
+    await party.open();
+
+    const feedKey = await keyring.createKeyRecord({ type: KeyType.FEED });
+    const fullKey = keyring.getFullKey(feedKey.publicKey);
+
+    await party.processor.writeHaloMessage(createFeedAdmitMessage(
+      keyring,
+      party.key,
+      feedKey.publicKey,
+      [partyKey]
+    ));
+
+    const feed2 = await feedStore.openReadWriteFeed(fullKey!.publicKey, fullKey!.secretKey!);
+    const itemId = createId();
+    await feed2.append({
+      echo: {
+        itemId,
+        genesis: {
+          itemType: 'dxos:example',
+          modelType: ObjectModel.meta.type
+        },
+        timeframe: new Timeframe()
+      }
+    });
+
+    await promiseTimeout(party.database.waitForItem({ id: itemId }), 1000, new Error('timeout'));
+  });
+
+  test('self-admitting feed with a hint', async () => {
+    const { party, keyring, partyKey, feedStore } = await setup();
+    await party.open();
+
+    const feedKey = await keyring.createKeyRecord({ type: KeyType.FEED });
+    const fullKey = keyring.getFullKey(feedKey.publicKey);
+    const feed2 = await feedStore.openReadWriteFeed(fullKey!.publicKey, fullKey!.secretKey!);
+
+    await party.processor.takeHints([{
+      type: KeyType.FEED,
+      publicKey: feedKey.publicKey
+    }]);
+
+    await feed2.append({
+      halo: createFeedAdmitMessage(
+        keyring,
+        party.key,
+        feedKey.publicKey,
+        [partyKey]
+      )
+    });
+
+    const itemId = createId();
+    await feed2.append({
+      echo: {
+        itemId,
+        genesis: {
+          itemType: 'dxos:example',
+          modelType: ObjectModel.meta.type
+        },
+        timeframe: new Timeframe()
+      }
+    });
+
+    await promiseTimeout(party.database.waitForItem({ id: itemId }), 1000, new Error('timeout'));
+  });
+
+  test('two instances replicating', async () => {
+    const peer1 = await setup();
+
+    const storage = createStorage('', STORAGE_RAM);
+    const feedStore = new FeedStore(storage, { valueEncoding: codec });
+    afterTest(async () => feedStore.close());
+
+    const metadataStore = new MetadataStore(createRamStorage());
+
+    const modelFactory = new ModelFactory().registerModel(ObjectModel);
+    const snapshotStore = new SnapshotStore(createStorage('', STORAGE_RAM));
+
+    const partyFeedProvider = new PartyFeedProvider(metadataStore, peer1.keyring, feedStore, peer1.party.key);
+
+    const party2 = new PartyCore(
+      peer1.party.key,
+      partyFeedProvider,
+      modelFactory,
+      snapshotStore,
+      PublicKey.random()
+    );
+
+    const feed2 = await partyFeedProvider.createOrOpenWritableFeed();
+
+    await peer1.party.processor.writeHaloMessage(createFeedAdmitMessage(
+      peer1.keyring,
+      peer1.party.key,
+      feed2.key,
+      [peer1.partyKey]
+    ));
+
+    await party2.open([{
+      publicKey: peer1.feedKey,
+      type: KeyType.FEED
+    }]);
+    afterTest(async () => party2.close());
+
+    createTestProtocolPair(
+      new ReplicatorProtocolPluginFactory(
+        peer1.partyFeedProvider,
+        peer1.party.processor.getActiveFeedSet()
+      ).createPlugins().map(r => r.createExtension()),
+      new ReplicatorProtocolPluginFactory(
+        partyFeedProvider,
+        peer1.party.processor.getActiveFeedSet()
+      ).createPlugins().map(r => r.createExtension())
+    );
+
+    const item1 = await peer1.party.database.createItem();
+    await promiseTimeout(party2.database.waitForItem({ id: item1.id }), 1000, new Error('timeout'));
   });
 });
