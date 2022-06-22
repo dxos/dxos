7a8f695e6444f6fad3e305a9594d78f04a7f6680
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-06-22 13:08:08 +0300

    Refactored similar code to one method in File.ts

--- packages/common/random-access-multi-storage/src/interfaces/File.ts
+++ packages/common/random-access-multi-storage/src/interfaces/File.ts
@@ -21,104 +21,46 @@ export interface FileInternal {
 }
 
 export class File {
-    private _fileInternal: FileInternal;
-    constructor (fileInternal: FileInternal) {
-      this._fileInternal = fileInternal;
-    }
+  constructor (private readonly _fileInternal: FileInternal) {}
 
-    read (offset: number, size: number, cb?: (err: Error | null, data?: Buffer) => void): Promise<Buffer> {
-      const promise = new Promise<Buffer>((resolve, reject) => {
-        this._fileInternal.read(offset, size, (err: Error | null, data?: Buffer) => {
+  private _createPromise<Type> (fileMethod: (...args: any[]) => void, cb?: (err: Error | null, data?: Type) => void, ...args: (number | Buffer)[]): Promise<Type> {
+    const promise = new Promise<Type>(
+      (resolve, reject) => {
+        fileMethod(...args, (err: Error | null, data?: Type) => {
           cb?.(err, data);
           if (err) {
             return reject(err);
           }
-          return resolve(data as Buffer);
+          return resolve(data as Type);
         });
       });
-      if (cb) {
-        promise.catch((_) => {});
-      }
-      return promise;
+    if (cb) {
+      promise.catch((_) => {});
     }
+    return promise;
+  }
 
-    write (offset: number, data: Buffer, cb?: (err: Error | null) => void): Promise<void> {
-      const promise = new Promise<void>((resolve, reject) => {
-        this._fileInternal.write(offset, data, (err: Error | null) => {
-          cb?.(err);
-          if (err) {
-            return reject(err);
-          }
-          return resolve();
-        });
-      });
-      if (cb) {
-        promise.catch((_) => {});
-      }
-      return promise;
-    }
+  read (offset: number, size: number, cb?: (err: Error | null, data?: Buffer) => void): Promise<Buffer> {
+    return this._createPromise<Buffer>(this._fileInternal.read.bind(this._fileInternal), cb, offset, size);
+  }
 
-    del (offset: number, data: Buffer, cb?: (err: Error | null) => void): Promise<void> {
-      const promise = new Promise<void>((resolve, reject) => {
-        this._fileInternal.del(offset, data, (err: Error | null) => {
-          cb?.(err);
-          if (err) {
-            return reject(err);
-          }
-          return resolve();
-        });
-      });
-      if (cb) {
-        promise.catch((_) => {});
-      }
-      return promise;
-    }
+  write (offset: number, data: Buffer, cb?: (err: Error | null) => void): Promise<void> {
+    return this._createPromise<void>(this._fileInternal.write.bind(this._fileInternal), cb, offset, data);
+  }
 
-    stat (cb?: (err: Error | null, data?: FileStat) => void): Promise<FileStat> {
-      const promise = new Promise<FileStat>((resolve, reject) => {
-        this._fileInternal.stat((err: Error | null, data?: FileStat) => {
-          cb?.(err, data);
-          if (err) {
-            return reject(err);
-          }
-          return resolve(data as FileStat);
-        });
-      });
-      if (cb) {
-        promise.catch((_) => {});
-      }
-      return promise;
-    }
+  del (offset: number, data: Buffer, cb?: (err: Error | null) => void): Promise<void> {
+    return this._createPromise<void>(this._fileInternal.del.bind(this._fileInternal), cb, offset, data);
+  }
 
-    close (cb?: (err: Error | null) => void): Promise<void> {
-      const promise = new Promise<void>((resolve, reject) => {
-        this._fileInternal.close((err: Error | null) => {
-          cb?.(err);
-          if (err) {
-            return reject(err);
-          }
-          return resolve();
-        });
-      });
-      if (cb) {
-        promise.catch((_) => {});
-      }
-      return promise;
-    }
+  stat (cb?: (err: Error | null, data?: FileStat) => void): Promise<FileStat> {
+    return this._createPromise<FileStat>(this._fileInternal.stat.bind(this._fileInternal), cb);
+  }
 
-    destroy (cb?: (err: Error | null) => void): Promise<void> {
-      const promise = new Promise<void>((resolve, reject) => {
-        this._fileInternal.destroy((err: Error | null) => {
-          cb?.(err);
-          if (err) {
-            return reject(err);
-          }
-          return resolve();
-        });
-      });
-      if (cb) {
-        promise.catch((_) => {});
-      }
-      return promise;
-    }
+  close (cb?: (err: Error | null) => void): Promise<void> {
+    return this._createPromise<void>(this._fileInternal.close.bind(this._fileInternal), cb);
+  }
+
+  destroy (cb?: (err: Error | null) => void): Promise<void> {
+    return this._createPromise<void>(this._fileInternal.destroy.bind(this._fileInternal), cb);
+  }
 }
