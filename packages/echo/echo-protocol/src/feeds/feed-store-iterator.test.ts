2f476787330ddab504a08d8362caa225bd4b9664
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-06-30 13:37:30 +0300

    Fix echo-protocols

--- packages/echo/echo-protocol/src/feeds/feed-store-iterator.test.ts
+++ packages/echo/echo-protocol/src/feeds/feed-store-iterator.test.ts
@@ -29,7 +29,7 @@ describe('feed store iterator', () => {
       numMessages: 10
     };
 
-    const feedStore = new FeedStore(createStorage('', StorageType.RAM), { valueEncoding: codec });
+    const feedStore = new FeedStore(createStorage('', StorageType.RAM).directory('feed'), { valueEncoding: codec });
 
     //
     // Create the ordered feed stream.
@@ -152,7 +152,7 @@ describe('feed store iterator', () => {
   });
 
   test('skipping initial messages', async () => {
-    const feedStore = new FeedStore(createStorage('', StorageType.RAM), {
+    const feedStore = new FeedStore(createStorage('', StorageType.RAM).directory('feed'), {
       valueEncoding: schema.getCodecForType('dxos.echo.testing.TestItemMutation')
     });
 
