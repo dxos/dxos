5dbbe2028308e22aa2c6be5d19e872795cf6ff78
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-07-04 14:30:10 +0300

    Fix destroy directory test for node storage

--- packages/common/random-access-multi-storage/src/implementations/node-storage.ts
+++ packages/common/random-access-multi-storage/src/implementations/node-storage.ts
@@ -3,25 +3,23 @@
 //
 
 import del from 'del';
+import { join } from 'path';
 import raf from 'random-access-file';
 
 import { File } from '../interfaces';
 import { StorageType } from '../interfaces/storage-types';
 import { AbstractStorage } from './abstract-storage';
 
+/**
+ * Storage interface implementation for Node.
+ */
 export class NodeStorage extends AbstractStorage {
   public override type: StorageType = StorageType.NODE;
 
   protected _createFile (filename: string, path: string, opts: any = {}): File {
-    return new File(
-      raf(filename, { directory: path, ...opts })
-    );
-  }
-
-  protected override async _destroyFilesInPath (path: string) {
-    const destroyPromise = super._destroyFilesInPath(path);
-    await del(this._path);
-    return destroyPromise;
+    const file = new File(raf(filename, { directory: path, ...opts }));
+    this._addFile(join(path, filename), file);
+    return file;
   }
 
   async _destroy () {
