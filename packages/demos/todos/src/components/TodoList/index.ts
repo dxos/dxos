7ece89bc5aa5b10ba4f249e0d61e4f9dc7675cb7
Josiah Witt <josiah@witt.life>
2022-07-06 10:46:59 -0400

    feat: Add todos demo (#1311)
    * feat: Add todos demo
    
    * chore(todos): Update browserslist
    
    * chore(todos): Temporarily disable linting

---
+++ packages/demos/todos/src/components/TodoList/index.ts
@@ -0,0 +1,6 @@
+import { TodoList, TodoListProps } from './TodoList';
+import { TodoItem } from './models';
+
+export type { TodoItem, TodoListProps };
+
+export default TodoList;
---
+++ packages/demos/todos/src/routes/index.ts
@@ -0,0 +1,24 @@
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import HomeIcon from '@mui/icons-material/Home';
+import TerrainIcon from '@mui/icons-material/Terrain';
+
+import asyncComponentLoader from '@/utils/loader';
+
+import { Pages, Routes } from './types';
+
+const routes: Routes = {
+  [Pages.Welcome]: {
+    component: asyncComponentLoader(() => import('@/pages/Welcome')),
+    path: '/',
+    title: 'TODO',
+    icon: HomeIcon,
+  },
+  [Pages.NotFound]: {
+    component: asyncComponentLoader(() => import('@/pages/NotFound')),
+    path: '*',
+  },
+};
+
+export default routes;
