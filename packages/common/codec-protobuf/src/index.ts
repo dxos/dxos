ab90066d22f855d900339dcd2c474c88c6a035cf
Josiah Witt <josiah@witt.life>
2022-07-12 12:47:01 -0400

    refactor(config): Factor protobufjs usage out to codec-protobuf

--- packages/common/codec-protobuf/src/index.ts
+++ packages/common/codec-protobuf/src/index.ts
@@ -6,6 +6,7 @@ export * from './buffer-patch';
 export * from './codec';
 export * from './common';
 export * from './interface';
+export * from './sanitizer';
 export * from './schema';
 export * from './service';
 export * from './stream';
