27a626c8118944abf3223c299ae0377b6638733b
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-07-13 14:00:05 +0300

    Fix offer/answer RPC in ReliableMessenger

---
+++ packages/mesh/network-manager/src/signal/reliable-messenger.test.ts
@@ -0,0 +1,127 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { expect, mockFn } from 'earljs';
+import { it as test, describe } from 'mocha';
+import waitForExpect from 'wait-for-expect';
+
+import { Awaited } from '@dxos/async';
+import { PublicKey } from '@dxos/crypto';
+import { createTestBroker } from '@dxos/signal';
+import { randomInt } from '@dxos/util';
+
+import { Answer, Message } from '../proto/gen/dxos/mesh/signal';
+import { ReliableMessenger } from './reliable-messenger';
+import { SignalApi } from './signal-api';
+import { SignalClient } from './signal-client';
+
+describe('SignalMessager', () => {
+  let topic: PublicKey;
+  let peer1: PublicKey;
+  let peer2: PublicKey;
+  let api1: SignalClient;
+  let api2: SignalClient;
+
+  let broker1: Awaited<ReturnType<typeof createTestBroker>>;
+  const signalApiPort1 = randomInt(10000, 50000);
+  const signalApiUrl1 = 'http://0.0.0.0:' + signalApiPort1;
+
+  before(async () => {
+    broker1 = await createTestBroker(signalApiPort1);
+  });
+
+  beforeEach(() => {
+    topic = PublicKey.random();
+    peer1 = PublicKey.random();
+    peer2 = PublicKey.random();
+  });
+
+  after(async function () {
+    this.timeout(0);
+    await api1.close();
+    await broker1.stop();
+  });
+
+  test('message between 2 clients', async () => {
+
+    const signalMock1 = mockFn<(msg: Message) => Promise<void>>().resolvesTo();
+    const messenger1 = new ReliableMessenger(
+      (message: Message) => api1.signal(message as SignalApi.SignalMessage),
+      signalMock1,
+      (() => {}) as any
+    );
+    api1 = new SignalClient(signalApiUrl1, (async () => {}) as any, (message: Message) => messenger1.receiveMessage(message));
+
+    const messenger2 = new ReliableMessenger(
+      (message: Message) => api2.signal(message as SignalApi.SignalMessage),
+      (() => {}) as any,
+      (() => {}) as any
+    );
+    api2 = new SignalClient(signalApiUrl1, (async () => {}) as any, (message: Message) => messenger2.receiveMessage(message));
+
+    await api1.join(topic, peer1);
+    await api2.join(topic, peer2);
+
+    const msg: Message = {
+      id: peer2,
+      remoteId: peer1,
+      sessionId: PublicKey.random(),
+      topic,
+      data: { signal: {foo: 'bar'} }
+    };
+    await messenger2.signal(msg);
+
+    await waitForExpect(() => {
+      expect(signalMock1).toHaveBeenCalledWith([msg]);
+    }, 4_000);
+  }).timeout(5_000);
+
+  test('offer/answer', async () => {
+    // const signalMock1 = mockFn<(msg: Message) => Promise<void>>().resolvesTo();
+    const messenger1 = new ReliableMessenger(
+      (message: Message) => api1.signal(message as SignalApi.SignalMessage),
+      (async () => {}) as any,
+      async (message: Message) => new Promise<Answer>(
+        (resolve, reject) => resolve({ accept: true })
+      )
+    );
+    api1 = new SignalClient(
+      signalApiUrl1,
+      (async () => {}) as any,
+      (message: Message) => {
+        console.log('onSignal1', message);
+        return messenger1.receiveMessage(message);
+      });
+
+    const messenger2 = new ReliableMessenger(
+      (message: Message) => api2.signal(message as SignalApi.SignalMessage),
+      (() => {}) as any,
+      async (message: Message) => new Promise<Answer>(
+        (resolve, reject) => resolve({ accept: true })
+      )
+    );
+    api2 = new SignalClient(
+      signalApiUrl1,
+      (async () => {}) as any,
+      (message: Message) => {
+        console.log('onSignal2', message);
+        return messenger2.receiveMessage(message);
+      });
+
+    await api1.join(topic, peer1);
+    await api2.join(topic, peer2);
+
+    const sessionId = PublicKey.random();
+    const msg: Message = {
+      id: peer1,
+      remoteId: peer2,
+      sessionId: sessionId,
+      topic,
+      data: { offer: { } }
+    };
+    const answer = await messenger1.offer(msg);
+    const expectedAns = { accept: true };
+    expect(answer).toEqual(expectedAns);
+  });
+});
---
+++ packages/mesh/network-manager/src/signal/reliable-messenger.ts
@@ -0,0 +1,76 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import assert from 'assert';
+
+import { PublicKey } from '@dxos/crypto';
+import { ComplexMap } from '@dxos/util';
+
+import { Answer, Message } from '../proto/gen/dxos/mesh/signal';
+
+/**
+ * Adds offer/answer RPC and reliable messaging.
+ *
+ * TODO(mykola): https://github.com/dxos/protocols/issues/1316
+ */
+export class ReliableMessenger {
+  private readonly _offerRecords: ComplexMap<PublicKey, OfferRecord> = new ComplexMap(key => key.toHex());
+
+  constructor (
+    private readonly _sendMessage: (message: Message) => Promise<void>,
+    private readonly _onSignal: (message: Message) => Promise<void>,
+    private readonly _onOffer: (message: Message) => Promise<Answer>
+  ) {}
+
+  async receiveMessage (message: Message): Promise<void> {
+    if (message.data?.offer) {
+      await this._resolveOffer(message);
+    } else if (message.data?.answer) {
+      await this._resolveAnswers(message);
+    } else if (message.data?.signal) {
+      await this._onSignal(message);
+    }
+  }
+
+  async signal (message: Message): Promise<void> {
+    assert(message.data.signal);
+    await this._sendMessage(message);
+  }
+
+  async offer (message: Message): Promise<Answer> {
+    const promise = new Promise<Answer>((resolve, reject) => {
+      assert(message.sessionId);
+      this._offerRecords.set(message.sessionId, { resolve, reject });
+    });
+    await this._sendMessage(message);
+    return promise;
+  }
+
+  private async _resolveAnswers (message: Message): Promise<void> {
+    assert(message.sessionId);
+    const offer = this._offerRecords.get(message.sessionId);
+    if (offer) {
+      this._offerRecords.delete(message.sessionId);
+      assert(message.data?.answer);
+      offer.resolve(message.data.answer);
+    }
+  }
+
+  private async _resolveOffer (message: Message): Promise<void> {
+    const answer = await this._onOffer(message);
+    const answerMessage = {
+      id: message.remoteId,
+      remoteId: message.id,
+      topic: message.topic,
+      sessionId: message.sessionId,
+      data: { answer: answer }
+    };
+    await this._sendMessage(answerMessage);
+  }
+}
+
+interface OfferRecord {
+  resolve: (answer: Answer) => void;
+  reject: (error?: Error) => void;
+}
