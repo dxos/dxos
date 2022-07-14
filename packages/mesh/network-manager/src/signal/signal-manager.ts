7c9dede2bd1d5855594a041fec0d69896c8bca5e
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-07-14 15:51:32 +0300

    Split signal messanging and signal peer discovering into seperate interfaces

--- packages/mesh/network-manager/src/signal/signal-manager.ts
+++ packages/mesh/network-manager/src/signal/signal-manager.ts
@@ -4,10 +4,13 @@
 
 import { Event } from '@dxos/async';
 import { PublicKey } from '@dxos/protocols';
+import { Answer, Message } from '../proto/gen/dxos/mesh/signal';
 
 import { SignalApi } from './signal-api';
 
-// TODO(burdon): Document methods.
+/**
+ * Signal peer discovery interface.
+ */
 export interface SignalConnection {
   /**
    * Find peers (triggers async event).
@@ -15,25 +18,40 @@ export interface SignalConnection {
   lookup (topic: PublicKey): void
 
   /**
+   * 
+   */
+  // TODO(burdon): Document.
+  join (topic: PublicKey, peerId: PublicKey): void
+
+  /**
+   * 
+   */
+  // TODO(burdon): Document.
+  leave (topic: PublicKey, peerId: PublicKey): void
+}
+
+/**
+ * Signal peer messaging interface.
+ */
+export interface SignalMessaging {
+  /**
    *
    */
   // TODO(burdon): Document.
-  offer (msg: SignalApi.SignalMessage): Promise<SignalApi.Answer>
+  offer (msg: Message): Promise<Answer>
 
   /**
    * Send message to peer.
    */
-  signal (msg: SignalApi.SignalMessage): Promise<void>
+  signal (msg: Message): Promise<void>
 }
 
-export interface SignalManager extends SignalConnection {
+export interface SignalManager extends SignalConnection, SignalMessaging {
   statusChanged: Event<SignalApi.Status[]>
   commandTrace: Event<SignalApi.CommandTrace>
   peerCandidatesChanged: Event<[topic: PublicKey, candidates: PublicKey[]]>
   onSignal: Event<SignalApi.SignalMessage>
 
   getStatus (): SignalApi.Status[]
-  join (topic: PublicKey, peerId: PublicKey): void
-  leave (topic: PublicKey, peerId: PublicKey): void
   destroy(): Promise<void>
 }
