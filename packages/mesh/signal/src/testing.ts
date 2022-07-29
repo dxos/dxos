16795074145b409a380be0006b1118897d40a6da
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-07-29 13:20:59 +0300

    Use precompiled bins for testing

--- packages/mesh/signal/src/testing.ts
+++ packages/mesh/signal/src/testing.ts
@@ -2,11 +2,62 @@
 // Copyright 2021 DXOS.org
 //
 
+import * as process from 'process';
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { debug } from 'debug';
+import { randomInt } from '@dxos/util'
+
+const log = debug('dxos:mesh:signal:testing');
+
+export class TestBroker{
+  private readonly port: number;
+  private serverProcess: ChildProcessWithoutNullStreams;
+
+  constructor(port: number = 8080){
+    this.port = port;
+    this.serverProcess = this.startProcess();
+  }
+  
+  public startProcess(): ChildProcessWithoutNullStreams {
+    const arch = ['x64', 'amd64', 'ppc64'].includes(process.arch) ? 'amd64' : ['arm64'].includes(process.arch) ? 'arm64' : '32';
+    if (arch === '32') {
+      throw new Error('32 bit architecture not supported');
+    }
+  
+    const os = process.platform;
+    if (!['darwin', 'linux'].includes(os)) {
+      throw new Error(`Unsupported platform: ${os}`);
+    }
+
+    const server = spawn(`signal-test-${os}-${arch} -port ${this.port} server`, [], {});
+
+    server.stdout.on('data', (data) => {
+      log(`TestServer: ${data}`);
+    });
+
+
+    server.on('error', (err) => {
+      log(`TestServer ERROR: ${err}`);
+    });
+
+    return server
+  }
+
+  public stop(): void {
+    this.serverProcess.kill('SIGINT');
+  }
+
+  public url(): string {
+    return `wss://localhost:${this.port}/ws`;
+  }
+}
+
 /**
  * Creates a test instance of the signal server with swarming disabled and starts it.
  *
  * @param port Port to start the signal server on, random by default.
  */
-export const createTestBroker = (port?: string | number): void => {
-
+export const createTestBroker = (port?: number): TestBroker => {
+  const server = new TestBroker(port ?? randomInt(10000, 50000));
+  return server;
 };
