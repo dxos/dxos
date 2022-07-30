3fc38dd5a3cdb1e64456db0579cdb8f1dbda49ce
Josiah Witt <josiah@witt.life>
2022-07-30 11:21:30 -0400

    feat(react-components): Add Skeleton with render delay

---
+++ packages/sdk/react-components/src/Skeleton/Skeleton.tsx
@@ -0,0 +1,34 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import React, { useEffect, useState } from 'react';
+
+import { Box, Fade, Skeleton as MuiSkeleton, SkeletonProps as MuiSkeletonProps } from '@mui/material';
+
+import { useMounted } from '@dxos/react-async';
+
+export interface SkeletonProps extends MuiSkeletonProps {
+  delay?: number
+}
+
+export const Skeleton = ({ delay = 500, ...props }: SkeletonProps) => {
+  const [show, setShow] = useState(false);
+  const isMounted = useMounted();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isMounted()) {
+        setShow(true);
+      }
+    }, delay);
+  }, []);
+
+  return (
+    <Fade in={show} timeout={500}>
+      <Box>
+        <MuiSkeleton {...props} />
+      </Box>
+    </Fade>
+  );
+};
