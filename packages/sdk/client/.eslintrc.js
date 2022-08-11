2d5825d43e0efb5fb30ab4c5d4113cb0bdcfc8b0
Dmytro Maretskyi <maretskii@gmail.com>
2022-08-11 14:11:34 +0200

    Full repo linting

--- packages/sdk/client/.eslintrc.js
+++ packages/sdk/client/.eslintrc.js
@@ -1,3 +1,6 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@dxos/eslint-plugin/eslint-patch')
+
 module.exports = {
   ignorePatterns: [
     'src/version.ts' // Generated file.
