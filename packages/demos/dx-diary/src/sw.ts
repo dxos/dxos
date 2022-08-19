009a8fe7b1ce5b2f9d99fe9d47eb6442fbc750df
Dmytro Maretskyi <maretskii@gmail.com>
2022-08-19 04:34:57 +0200

    Create dx-diary package

---
+++ packages/demos/dx-diary/src/sw.ts
@@ -0,0 +1,24 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+// Chrome > Devtools > Application > Manifest | Service Workers (Update on reload)
+// chrome://serviceworker-internals > Inspect
+
+self.addEventListener('install', (event: any) => {
+  event.waitUntil(
+    caches.open('kai').then((cache) => cache.addAll([
+      '/index.html',
+      '/main.js'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (event: any) => {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+console.log('Service worker started.');
