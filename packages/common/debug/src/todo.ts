be94cef6e5ec19179ff3c63ba61a36aae0434fcf
Dmytro Maretskyi <maretskii@gmail.com>
2022-08-24 13:30:25 -0400

    Add todo function

---
+++ packages/common/debug/src/todo.ts
@@ -0,0 +1,10 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+/**
+ * Throws an error. Can be used in an expression instead of a value
+ */
+ export const todo = (message?: string): never => {
+  throw new Error(message ?? 'Not implemented.');
+};
