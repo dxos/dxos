d4eab7ad2edbe54ce356506b73d755d426f01889
Josiah Witt <josiah@witt.life>
2022-08-25 17:40:45 -0400

    build(wallet): Use Vite (#1456)

---
+++ packages/sdk/config/src/plugin/rollup-plugin.js
@@ -0,0 +1,26 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import assert from 'assert';
+
+import { definitions } from './definitions';
+
+export function ConfigPlugin ({ dynamic = false, publicUrl = '' } = {}) {
+  dynamic = process.env.CONFIG_DYNAMIC === 'true' ? true : dynamic;
+  assert(typeof dynamic === 'boolean', `dynamic: Expected boolean, got: ${typeof dynamic}`);
+
+  const contents = Object.entries(definitions({ dynamic, publicUrl }))
+    .map(([key, value]) => `window.${key} = ${JSON.stringify(value)};`)
+    .join('\n');
+
+  return {
+    name: 'dxos-config',
+    transform (code, module) {
+      // Based on https://github.com/mmirca/rollup-plugin-entry-code-injector/blob/6ce979fcea31a75537c00748fbe25ed14f340624/commonjs/index.js.
+      if (this.getModuleInfo(module).isEntry) {
+        return contents + code;
+      }
+    }
+  };
+}
---
+++ packages/sdk/config/src/plugin/types.ts
@@ -0,0 +1,25 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+export interface ConfigPluginOpts {
+  /**
+   * The Dynamics() config.yml file is special, it will be loaded if the dynamic property is set to false.
+   * If dynamic is set to true each app will try to load from an endpoint (using {publicUrl}/config/config.json),
+   * wire app serve adds config endpoints for each app serving the global config file (~/.wire/remote.yml).
+   *
+   * The usual pattern is to set it to CONFIG_DYNAMIC env variable.
+   * When running app locally this should be set to false or nil to serve local config.
+   * And when publishing the app to DXNS the cli-app will set that variable to true automatically.
+   *
+   * @default false
+   */
+  dynamic?: boolean
+
+  /**
+   * Public URL of the published app. Also used to load the dynamic config.
+   *
+   * @default ''
+   */
+  publicUrl?: string
+}
