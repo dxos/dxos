41c8f1214374c560bff05f6a7e06984d6fb51c5c
Rich Burdon <rich.burdon@gmail.com>
2022-08-29 11:56:50 -0400

    nop

--- packages/sdk/registry-client/src/polkadot/interfaces/augment-api-events.ts
+++ packages/sdk/registry-client/src/polkadot/interfaces/augment-api-events.ts
@@ -1,12 +1,16 @@
 // Auto-generated via `yarn polkadot-types-from-chain`, do not edit
--
+
+//
+// Copyright 2022 DXOS.org
+//
+
+import type { ApiTypes } from '@polkadot/api/types';
 import type { Text } from '@polkadot/types';
 import type { BalanceStatus } from '@polkadot/types/interfaces/balances';
 import type { AuthorityList } from '@polkadot/types/interfaces/grandpa';
 import type { AccountId, Balance, Hash } from '@polkadot/types/interfaces/runtime';
 import type { DispatchError, DispatchInfo, DispatchResult } from '@polkadot/types/interfaces/system';
 import type { AccountKey, Domain, DomainKey, Multihash } from 'sample-polkadotjs-typegen/polkadot/interfaces/registry';
-import type { ApiTypes } from '@polkadot/api/types';
 
 declare module '@polkadot/api/types/events' {
   export interface AugmentedEvents<ApiType> {
@@ -14,122 +18,122 @@ declare module '@polkadot/api/types/events' {
       /**
        * A balance was set by root. \[who, free, reserved\]
        **/
-      BalanceSet: AugmentedEvent<ApiType, [AccountId, Balance, Balance]>;
+      BalanceSet: AugmentedEvent<ApiType, [AccountId, Balance, Balance]>
       /**
        * Some amount was deposited (e.g. for transaction fees). \[who, deposit\]
        **/
-      Deposit: AugmentedEvent<ApiType, [AccountId, Balance]>;
+      Deposit: AugmentedEvent<ApiType, [AccountId, Balance]>
       /**
        * An account was removed whose balance was non-zero but below ExistentialDeposit,
        * resulting in an outright loss. \[account, balance\]
        **/
-      DustLost: AugmentedEvent<ApiType, [AccountId, Balance]>;
+      DustLost: AugmentedEvent<ApiType, [AccountId, Balance]>
       /**
        * An account was created with some free balance. \[account, free_balance\]
        **/
-      Endowed: AugmentedEvent<ApiType, [AccountId, Balance]>;
+      Endowed: AugmentedEvent<ApiType, [AccountId, Balance]>
       /**
        * Some balance was reserved (moved from free to reserved). \[who, value\]
        **/
-      Reserved: AugmentedEvent<ApiType, [AccountId, Balance]>;
+      Reserved: AugmentedEvent<ApiType, [AccountId, Balance]>
       /**
        * Some balance was moved from the reserve of the first account to the second account.
        * Final argument indicates the destination balance type.
        * \[from, to, balance, destination_status\]
        **/
-      ReserveRepatriated: AugmentedEvent<ApiType, [AccountId, AccountId, Balance, BalanceStatus]>;
+      ReserveRepatriated: AugmentedEvent<ApiType, [AccountId, AccountId, Balance, BalanceStatus]>
       /**
        * Transfer succeeded. \[from, to, value\]
        **/
-      Transfer: AugmentedEvent<ApiType, [AccountId, AccountId, Balance]>;
+      Transfer: AugmentedEvent<ApiType, [AccountId, AccountId, Balance]>
       /**
        * Some balance was unreserved (moved from reserved to free). \[who, value\]
        **/
-      Unreserved: AugmentedEvent<ApiType, [AccountId, Balance]>;
+      Unreserved: AugmentedEvent<ApiType, [AccountId, Balance]>
       /**
        * Generic event
        **/
-      [key: string]: AugmentedEvent<ApiType>;
-    };
+      [key: string]: AugmentedEvent<ApiType>
+    }
     grandpa: {
       /**
        * New authority set has been applied. \[authority_set\]
        **/
-      NewAuthorities: AugmentedEvent<ApiType, [AuthorityList]>;
+      NewAuthorities: AugmentedEvent<ApiType, [AuthorityList]>
       /**
        * Current authority set has been paused.
        **/
-      Paused: AugmentedEvent<ApiType, []>;
+      Paused: AugmentedEvent<ApiType, []>
       /**
        * Current authority set has been resumed.
        **/
-      Resumed: AugmentedEvent<ApiType, []>;
+      Resumed: AugmentedEvent<ApiType, []>
       /**
        * Generic event
        **/
-      [key: string]: AugmentedEvent<ApiType>;
-    };
+      [key: string]: AugmentedEvent<ApiType>
+    }
     registry: {
-      AccountCreated: AugmentedEvent<ApiType, [AccountKey]>;
-      DomainRegistered: AugmentedEvent<ApiType, [Domain]>;
-      RecordAdded: AugmentedEvent<ApiType, [AccountId, Multihash]>;
-      ResourceRegistered: AugmentedEvent<ApiType, [DomainKey, Text, Multihash]>;
+      AccountCreated: AugmentedEvent<ApiType, [AccountKey]>
+      DomainRegistered: AugmentedEvent<ApiType, [Domain]>
+      RecordAdded: AugmentedEvent<ApiType, [AccountId, Multihash]>
+      ResourceRegistered: AugmentedEvent<ApiType, [DomainKey, Text, Multihash]>
       /**
        * Generic event
        **/
-      [key: string]: AugmentedEvent<ApiType>;
-    };
+      [key: string]: AugmentedEvent<ApiType>
+    }
     sudo: {
       /**
        * The \[sudoer\] just switched identity; the old key is supplied.
        **/
-      KeyChanged: AugmentedEvent<ApiType, [AccountId]>;
+      KeyChanged: AugmentedEvent<ApiType, [AccountId]>
       /**
        * A sudo just took place. \[result\]
        **/
-      Sudid: AugmentedEvent<ApiType, [DispatchResult]>;
+      Sudid: AugmentedEvent<ApiType, [DispatchResult]>
       /**
        * A sudo just took place. \[result\]
        **/
-      SudoAsDone: AugmentedEvent<ApiType, [DispatchResult]>;
+      SudoAsDone: AugmentedEvent<ApiType, [DispatchResult]>
       /**
        * Generic event
        **/
-      [key: string]: AugmentedEvent<ApiType>;
-    };
+      [key: string]: AugmentedEvent<ApiType>
+    }
     system: {
       /**
        * `:code` was updated.
        **/
-      CodeUpdated: AugmentedEvent<ApiType, []>;
+      CodeUpdated: AugmentedEvent<ApiType, []>
       /**
        * An extrinsic failed. \[error, info\]
        **/
-      ExtrinsicFailed: AugmentedEvent<ApiType, [DispatchError, DispatchInfo]>;
+      ExtrinsicFailed: AugmentedEvent<ApiType, [DispatchError, DispatchInfo]>
       /**
        * An extrinsic completed successfully. \[info\]
        **/
-      ExtrinsicSuccess: AugmentedEvent<ApiType, [DispatchInfo]>;
+      ExtrinsicSuccess: AugmentedEvent<ApiType, [DispatchInfo]>
       /**
        * An \[account\] was reaped.
        **/
-      KilledAccount: AugmentedEvent<ApiType, [AccountId]>;
+      KilledAccount: AugmentedEvent<ApiType, [AccountId]>
       /**
        * A new \[account\] was created.
        **/
-      NewAccount: AugmentedEvent<ApiType, [AccountId]>;
+      NewAccount: AugmentedEvent<ApiType, [AccountId]>
       /**
        * On on-chain remark happened. \[origin, remark_hash\]
        **/
-      Remarked: AugmentedEvent<ApiType, [AccountId, Hash]>;
+      Remarked: AugmentedEvent<ApiType, [AccountId, Hash]>
       /**
        * Generic event
        **/
-      [key: string]: AugmentedEvent<ApiType>;
-    };
+      [key: string]: AugmentedEvent<ApiType>
+    }
   }
 
   export interface DecoratedEvents<ApiType extends ApiTypes> extends AugmentedEvents<ApiType> {
-    [key: string]: ModuleEvents<ApiType>;
+    [key: string]: ModuleEvents<ApiType>
   }
 }
