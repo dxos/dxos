75084d9599748d00a1a037700f4e7b117a9b1dac
Josiah Witt <josiah@witt.life>
2022-08-30 11:58:07 -0400

    test: Add storybook to wallet

--- tools/eslint-plugin/configs/typescript.js
+++ tools/eslint-plugin/configs/typescript.js
@@ -5,7 +5,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json'
+    project: './tsconfig.base.json'
   },
   extends: [
     'plugin:@typescript-eslint/recommended'
