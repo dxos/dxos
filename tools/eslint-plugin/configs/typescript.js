15d7215e77edeb954f03c03c861160f96cfe24c0
Josiah Witt <josiah@witt.life>
2022-08-30 15:03:02 -0400

    build: Tweak vite executor to work with tsconfig.json

--- tools/eslint-plugin/configs/typescript.js
+++ tools/eslint-plugin/configs/typescript.js
@@ -5,7 +5,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.base.json'
+    project: './tsconfig.json'
   },
   extends: [
     'plugin:@typescript-eslint/recommended'
