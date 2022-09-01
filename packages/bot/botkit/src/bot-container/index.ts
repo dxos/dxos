3a40e7b899df138a96c0358decfe10ea9a01347d
Dmytro Maretskyi <maretskii@gmail.com>
2022-09-01 16:16:17 -0400

    Remove all bot stuff

--- packages/bot/botkit/src/bot-container/index.ts
+++
@@ -1,7 +0,0 @@
-//
-// Copyright 2021 DXOS.org
-//
-
-export * from './bot-container';
-export * from './node-container';
-export * from './in-process';
--- packages/bot/botkit/src/testutils/index.ts
+++
@@ -1,7 +0,0 @@
-//
-// Copyright 2021 DXOS.org
-//
-
-export * from './broker-setup';
-export * from './client-setup';
-export * from './mock-registry';
