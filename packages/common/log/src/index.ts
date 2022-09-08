d6d6060c1b189d54be2b314466ff867ca6a272f2
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2022-09-08 14:57:45 -0400

    feat: Logging API (#1507)
    * WIP logger
    
    * Try to merge lockfile
    
    * Tooling
    
    * Update tooling
    
    * Tooling
    
    * WIP
    
    * Logging
    
    * WIP
    
    * Fix ts decorator support
    
    * New syntax
    
    * fix shit
    
    * Correct line numbers
    
    * Display line numbers
    
    * Add color
    
    * Shorten file paths
    
    * Add log filtering
    
    * Use in echo
    
    * Slight refactoring and review.
    
    * Patch source maps
    
    * Remove circular deps.
    Introduce config class.
    
    * Fix
    
    * Fix the preprocessor
    
    * Lint
    
    * Lockfile
    
    * Lint
    
    * Add env variables to cache key
    
    * Comments
    
    * CR
    
    Co-authored-by: Rich Burdon <rich.burdon@gmail.com>

---
+++ packages/common/log/src/index.ts
@@ -0,0 +1,8 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+export * from './config';
+export * from './log';
+export { CONSOLE_PROCESSOR, DEBUG_PROCESSOR } from './processors';
+export { getCurrentOwnershipScope } from './ownership';
