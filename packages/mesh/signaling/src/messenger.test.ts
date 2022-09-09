927e231d10c6e009be169247dea9d1026f1f78fa
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-09-09 22:12:24 +0300

    Add Message.listen()

--- packages/mesh/signaling/src/messenger.test.ts
+++ packages/mesh/signaling/src/messenger.test.ts
@@ -2,14 +2,14 @@
 // Copyright 2022 DXOS.org
 //
 
-import { expect } from 'earljs';
+import { expect, mockFn } from 'earljs';
 import waitForExpect from 'wait-for-expect';
 
 import { PublicKey } from '@dxos/protocols';
 import { createTestBroker, TestBroker } from '@dxos/signal';
 import { afterTest } from '@dxos/testutils';
 
-import { Messenger } from './messenger';
+import { Messenger, Message } from './messenger';
 import { Any } from './proto/gen/google/protobuf';
 import { SignalManagerImpl } from './signal-manager-impl';
 
@@ -24,11 +24,9 @@ describe('Messenger', () => {
     broker.stop();
   });
 
-  const setup = async (): Promise<{messenger: Messenger, received: [author: PublicKey, payload: Any][]}> => {
-    const received: [author: PublicKey, payload: Any][] = [];
-    const receiveMock = async (author: PublicKey, payload: Any) => {
-      received.push([author, payload]);
-    };
+  const setup = async () => {
+    const received: Message[] = [];
+    const receiveMock = (message: Message) => received.push(message);
 
     const peerId = PublicKey.random();
 
@@ -39,9 +37,10 @@ describe('Messenger', () => {
 
     const messenger = new Messenger({
       ownPeerId: peerId,
-      receive: receiveMock,
       signalManager
     });
+
+    messenger.listen({ listener: receiveMock });
 
     return {
       messenger,
@@ -58,10 +57,16 @@ describe('Messenger', () => {
       value: Buffer.from('0')
     };
 
-    await messenger1.message(messenger2.ownPeerId, payload);
+    await messenger1.message({ recipient: messenger2.ownPeerId, payload });
+
+    const message: Message = {
+      author: messenger1.ownPeerId,
+      recipient: messenger2.ownPeerId,
+      payload
+    };
 
     await waitForExpect(() => {
-      expect(received2[0]).toEqual([messenger1.ownPeerId, payload]);
+      expect(received2[0]).toBeAnObjectWith(message);
     }, 5_000);
   });
 
@@ -75,9 +80,14 @@ describe('Messenger', () => {
         type_url: 'a',
         value: Buffer.from('0')
       };
-      await messenger1.message(messenger2.ownPeerId, payload);
+      await messenger1.message({ recipient: messenger2.ownPeerId, payload });
+      const message: Message = {
+        author: messenger1.ownPeerId,
+        recipient: messenger2.ownPeerId,
+        payload
+      };
       await waitForExpect(() => {
-        expect(received2[0]).toEqual([messenger1.ownPeerId, payload]);
+        expect(received2[0]).toBeAnObjectWith(message);
       }, 3_000);
     }
 
@@ -86,9 +96,14 @@ describe('Messenger', () => {
         type_url: 'b',
         value: Buffer.from('1')
       };
-      await messenger1.message(messenger3.ownPeerId, payload);
+      await messenger1.message({ recipient: messenger3.ownPeerId, payload });
+      const message: Message = {
+        author: messenger1.ownPeerId,
+        recipient: messenger3.ownPeerId,
+        payload
+      };
       await waitForExpect(() => {
-        expect(received3[0]).toEqual([messenger1.ownPeerId, payload]);
+        expect(received3[0]).toBeAnObjectWith(message);
       }, 3_000);
     }
 
@@ -97,9 +112,127 @@ describe('Messenger', () => {
         type_url: 'c',
         value: Buffer.from('2')
       };
-      await messenger2.message(messenger1.ownPeerId, payload);
+      await messenger2.message({ recipient: messenger1.ownPeerId, payload });
+      const message: Message = {
+        author: messenger2.ownPeerId,
+        recipient: messenger1.ownPeerId,
+        payload
+      };
       await waitForExpect(() => {
-        expect(received1[0]).toEqual([messenger2.ownPeerId, payload]);
+        expect(received1[0]).toBeAnObjectWith(message);
+      }, 3_000);
+    }
+  });
+
+  it('Message routing', async () => {
+    const { messenger: messenger1 } = await setup();
+    const { messenger: messenger2, received: received2 } = await setup();
+
+    // Subscribe first listener for second messenger.
+    const listener1 = mockFn<(message: Message) => void>().returns();
+    messenger2.listen({
+      payloadType: '1',
+      listener: listener1
+    });
+
+    // Subscribe first listener for second messenger.
+    const listener2 = mockFn<(message: Message) => void>().returns();
+    messenger2.listen({
+      payloadType: '1',
+      listener: listener2
+    });
+
+    // Subscribe third listener for second messenger.
+    const listener3 = mockFn<(message: Message) => void>().returns();
+    messenger2.listen({
+      payloadType: '2',
+      listener: listener3
+    });
+
+    // Message from the 1st peer to the 2nd peer with payload type "1".
+    {
+      const payload: Any = {
+        type_url: '1',
+        value: Buffer.from('0')
+      };
+      await messenger1.message({ recipient: messenger2.ownPeerId, payload });
+      const message: Message = {
+        author: messenger1.ownPeerId,
+        recipient: messenger2.ownPeerId,
+        payload
+      };
+      // 3 listeners (default one that was returned by setup() and 2 that listen for type "1") should receive message.
+      await waitForExpect(() => {
+        expect(received2.at(-1)!).toBeAnObjectWith(message);
+        expect(listener1).toHaveBeenCalledWith([message]);
+        expect(listener2).toHaveBeenCalledWith([message]);
+        expect(listener3).not.toHaveBeenCalledWith([message]);
+      }, 3_000);
+    }
+  });
+
+  it('Unsubscribe listener', async () => {
+    const { messenger: messenger1 } = await setup();
+    const { messenger: messenger2 } = await setup();
+
+    // Subscribe first listener for second messenger.
+    const messages1: Message[] = [];
+    messenger2.listen({
+      payloadType: '1',
+      listener: message => {
+        messages1.push(message);
+      }
+    });
+
+    // Subscribe first listener for second messenger.
+    const messages2: Message[] = [];
+    const listenerHandle2 = messenger2.listen({
+      payloadType: '1',
+      listener: message => {
+        messages2.push(message);
+      }
+    });
+
+    // Message from the 1st peer to the 2nd peer with payload type "1".
+    {
+      const payload: Any = {
+        type_url: '1',
+        value: Buffer.from('0')
+      };
+      await messenger1.message({ recipient: messenger2.ownPeerId, payload });
+      const message: Message = {
+        author: messenger1.ownPeerId,
+        recipient: messenger2.ownPeerId,
+        payload
+      };
+      // 2 subscribed listeners should receive message.
+      await waitForExpect(() => {
+        expect(messages1[0]).toEqual(message);
+        expect(messages2[0]).toEqual(message);
+      }, 3_000);
+    }
+
+    // Unsubscribe second listener.
+    listenerHandle2.unsubscribe();
+
+    // Message from the 1st peer to the 2nd peer with payload type "1".
+    {
+      const payload: Any = {
+        type_url: '1',
+        value: Buffer.from('0')
+      };
+      await messenger1.message({ recipient: messenger2.ownPeerId, payload });
+      const message: Message = {
+        author: messenger1.ownPeerId,
+        recipient: messenger2.ownPeerId,
+        payload
+      };
+      // 1 listener that was not unsubscribed should receive message.
+      await waitForExpect(() => {
+        expect(messages1[1]).toEqual(message);
+        expect(messages1.length).toEqual(2);
+        expect(messages2.length).toEqual(1);
+        // expect(listener2)([message]);
       }, 3_000);
     }
   });
--- packages/mesh/signaling/src/messenger.ts
+++ packages/mesh/signaling/src/messenger.ts
@@ -1,4 +1,3 @@
-
 //
 // Copyright 2022 DXOS.org
 //
@@ -10,34 +9,41 @@ import { PublicKey } from '@dxos/protocols';
 import { Any } from './proto/gen/google/protobuf';
 import { SignalManager } from './signal-manager';
 
-interface MessengerOptions {
-  ownPeerId: PublicKey
-  receive: (author: PublicKey, payload: Any) => Promise<void>
-  signalManager: SignalManager
-}
-
 const log = debug('dxos:signaling:messenger');
 
+type Listener = ({
+  author,
+  recipient,
+  payload
+}: {
+  author: PublicKey
+  recipient: PublicKey
+  payload: Any
+}) => any;
+
+interface MessengerOptions {
+  ownPeerId: PublicKey
+  signalManager: SignalManager
+}
 export class Messenger {
   private readonly _ownPeerId: PublicKey;
-  private readonly _receive: (author: PublicKey, payload: Any) => Promise<void>;
   private readonly _signalManager: SignalManager;
+  private readonly _listeners = new Map<string, Map<number, Listener>>();
+  private _listenerIndex = 0;
 
-  constructor ({
-    ownPeerId,
-    receive,
-    signalManager
-  }: MessengerOptions) {
+  constructor ({ ownPeerId, signalManager }: MessengerOptions) {
     this._ownPeerId = ownPeerId;
-    this._receive = receive;
 
     this._signalManager = signalManager;
 
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    this._signalManager.subscribeMessages(this._ownPeerId).then(() => log(`Subscribed for messages peerId=${this._ownPeerId}`), error => log(`Error: ${error}`));
-    this._signalManager.onMessage.on(([author, recipient, payload]) => {
-      log(`Received message from ${author}`);
-      this._receive(author, payload);
+    this._signalManager.subscribeMessages(this._ownPeerId).then(
+      () => log(`Subscribed for messages peerId=${this._ownPeerId}`),
+      (error) => log(`Error: ${error}`)
+    );
+    this._signalManager.onMessage.on(message => {
+      log(`Received message from ${message.author}`);
+      this._handleMessage(message);
     });
   }
 
@@ -46,7 +52,54 @@ export class Messenger {
   }
 
   // TODO(mykola): make reliable.
-  async message (recipient: PublicKey, payload: Any): Promise<void> {
+  async message ({
+    recipient,
+    payload
+  }: {
+    recipient: PublicKey
+    payload: Any
+  }): Promise<void> {
     await this._signalManager.message(this._ownPeerId, recipient, payload);
   }
+
+  listen ({
+    payloadType,
+    listener
+  }: {
+    payloadType?: string
+    listener: Listener
+  }): ListeningHandle {
+    const firstKey = payloadType ?? '';
+    const secondKey = this._listenerIndex++;
+    if (this._listeners.has(firstKey)) {
+      this._listeners.get(firstKey)?.set(secondKey, listener);
+    } else {
+      this._listeners.set(firstKey, new Map([[secondKey, listener]]));
+    }
+
+    return {
+      unsubscribe: () => {
+        this._listeners.get(firstKey)?.delete(secondKey);
+      }
+    };
+  }
+
+  private async _handleMessage (message: Message): Promise<void> {
+    if (this._listeners.has('')) {
+      [...this._listeners.get('')!.values()].forEach(listener => listener(message));
+    }
+    if (this._listeners.has(message.payload.type_url)) {
+      [...this._listeners.get(message.payload.type_url)!.values()].forEach(listener => listener(message));
+    }
+  }
+}
+
+export interface ListeningHandle {
+  unsubscribe: () => any
+}
+
+export interface Message {
+  author: PublicKey
+  recipient: PublicKey
+  payload: Any
 }
