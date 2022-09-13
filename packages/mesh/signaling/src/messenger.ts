fe827e2e9cdfe787f995e4768c26e31c2865a71c
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-09-13 14:27:09 +0300

    Move Messenger generic Listeners to separate Map

--- packages/mesh/signaling/src/messenger.ts
+++ packages/mesh/signaling/src/messenger.ts
@@ -28,6 +28,7 @@ interface MessengerOptions {
 export class Messenger {
   private readonly _signalManager: SignalManager;
   private readonly _listeners = new Map<string, Map<number, Listener>>();
+  private readonly _defaultListeners = new Map<number, Listener>();
   private _listenerIndex = 0;
 
   constructor ({ signalManager }: MessengerOptions) {
@@ -50,27 +51,32 @@ export class Messenger {
     payloadType?: string
     listener: Listener
   }): ListeningHandle {
-    const firstKey = payloadType ?? '';
-    const secondKey = this._listenerIndex++;
-    if (this._listeners.has(firstKey)) {
-      this._listeners.get(firstKey)?.set(secondKey, listener);
+    const index = this._listenerIndex++;
+    if (!payloadType) {
+      this._defaultListeners.set(index, listener);
     } else {
-      this._listeners.set(firstKey, new Map([[secondKey, listener]]));
+      if (this._listeners.has(payloadType)) {
+        this._listeners.get(payloadType)?.set(index, listener);
+      } else {
+        this._listeners.set(payloadType, new Map([[index, listener]]));
+      }
     }
 
     return {
       unsubscribe: () => {
-        this._listeners.get(firstKey)?.delete(secondKey);
+        if (!payloadType) {
+          this._defaultListeners.delete(index);
+        } else {
+          this._listeners.get(payloadType)?.delete(index);
+        }
       }
     };
   }
 
   private async _handleMessage (message: Message): Promise<void> {
-    if (this._listeners.has('')) {
-      [...this._listeners.get('')!.values()].forEach((listener) =>
-        listener(message)
-      );
-    }
+    [...this._defaultListeners.values()].forEach((listener) =>
+      listener(message)
+    );
     if (this._listeners.has(message.payload.type_url)) {
       [...this._listeners.get(message.payload.type_url)!.values()].forEach(
         (listener) => listener(message)
