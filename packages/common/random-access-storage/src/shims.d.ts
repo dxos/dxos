5a5fa865674e94ffd1e0e996a26d9d95a1a99de4
Rich Burdon <rich.burdon@gmail.com>
2022-09-13 11:14:46 -0400

    chore: Reorganize by platform (pattern). (#1522)
    * chore: Reorganize by platform (pattern).

--- packages/common/random-access-storage/src/shims.d.ts
+++ packages/common/random-access-storage/src/shims.d.ts
@@ -6,5 +6,3 @@ declare module 'random-access-file';
 declare module 'random-access-idb';
 declare module 'random-access-memory';
 declare module 'random-access-web/mutable-file-wrapper';
-
-declare module '@dxos/random-access-chrome-file';
