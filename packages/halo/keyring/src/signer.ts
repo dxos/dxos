1c553bf36f4fe8320171549f5784901227d1735e
Dmytro Maretskyi <maretskii@gmail.com>
2022-09-13 11:52:58 -0400

    Finish implementing keyring

--- packages/halo/keyring/src/signer.ts
+++ packages/halo/keyring/src/signer.ts
@@ -1,21 +1,25 @@
-import { PublicKey } from "@dxos/protocols";
+//
+// Copyright 2022 DXOS.org
+//
+
+import { PublicKey } from '@dxos/protocols';
 
 export interface Signer {
   /**
    * Sign a message with the given key.
    * Key must be present in the keyring.
    */
-  sign: (key: PublicKey, message: Uint8Array) => Promise<Uint8Array>;
+  sign: (key: PublicKey, message: Uint8Array) => Promise<Uint8Array>
 
   /**
    * Verify a signature with the given key.
    * Key might not be present in the keyring.
    */
-  verify: (key: PublicKey, message: Uint8Array, signature: Uint8Array) => Promise<boolean>;
+  verify: (key: PublicKey, message: Uint8Array, signature: Uint8Array) => Promise<boolean>
 
   /**
    * Export the key in the raw format.
    * May throw an error if the key is not exportable.
    */
-  exportKey: (key: PublicKey) => Promise<Uint8Array>;
+  exportKey: (key: PublicKey) => Promise<Uint8Array>
 }
