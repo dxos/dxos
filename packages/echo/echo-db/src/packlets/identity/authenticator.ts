23c2d43951b17d44990cdc35b3f0eec2c5c50518
Dmytro Maretskyi <maretskii@gmail.com>
2022-09-15 18:58:34 -0400

    Add halo auth functions

--- packages/echo/echo-db/src/packlets/identity/authenticator.ts
+++ packages/echo/echo-db/src/packlets/identity/authenticator.ts
@@ -1,8 +1,30 @@
-import { AuthProvider } from "../space/auth-plugin";
+import { schema } from "@dxos/echo-protocol";
+import { verifyCredential } from "@dxos/halo-protocol";
+import { PublicKey } from "@dxos/protocols";
+import { ComplexSet } from "@dxos/util";
+import { AuthProvider, AuthVerifier } from "../space/auth-plugin";
 import { CredentialSigner } from "./credential-signer";
 
-// export const createHaloAuthProvider: AuthProvider = (signer: CredentialSigner): CredentialSigner => nonce => signer({
-//   assertion: {
-//     "@type": "dxos.halo.credentials.Auth",
-//   }
-// })
+export const createHaloAuthProvider = (signer: CredentialSigner): AuthProvider => async nonce => {
+  const credential = await signer.createCredential({
+    assertion: {
+      "@type": "dxos.halo.credentials.Auth",
+    },
+    subject: signer.getIssuer(),
+    nonce,
+  })
+
+  return schema.getCodecForType('dxos.halo.credentials.Credential').encode(credential)
+}
+
+export const createHaloAuthVerifier = (getDeviceSet: () => ComplexSet<PublicKey>): AuthVerifier => async auth => {
+  const credential = schema.getCodecForType('dxos.halo.credentials.Credential').decode(auth)
+  const deviceSet = getDeviceSet()
+
+  const result = await verifyCredential(credential)
+  if (result.kind === 'fail') {
+    return false;
+  }
+
+  return deviceSet.has(credential.issuer)
+}
--- packages/echo/echo-db/src/packlets/identity/credential-signer.ts
+++ packages/echo/echo-db/src/packlets/identity/credential-signer.ts
@@ -1,36 +1,45 @@
 import { TypedMessage } from "@dxos/echo-protocol"
-import { Chain, createCredential, Credential } from "@dxos/halo-protocol"
+import { Chain, createCredential, Credential, MessageType } from "@dxos/halo-protocol"
 import { Signer } from "@dxos/keyring"
 import { PublicKey } from "@dxos/protocols"
 
 export type CredentialSignerParams = {
   subject: PublicKey
-  assertion: TypedMessage
+  assertion: MessageType
   nonce?: Uint8Array
 }
 
-export type CredentialSigner = (params: CredentialSignerParams) => Promise<Credential>
+export interface CredentialSigner {
+  getIssuer(): PublicKey
+  createCredential: (params: CredentialSignerParams) => Promise<Credential>
+}
 
 /**
  * Issue credentials directly signed by the issuer.
  */
-export const createKeyCredentialSigner = (signer: Signer, key: PublicKey): CredentialSigner => params => createCredential({
-  issuer: key,
-  keyring: signer,
+export const createKeyCredentialSigner = (signer: Signer, key: PublicKey): CredentialSigner => ({
+  getIssuer: () => key,
+  createCredential: params => createCredential({
+    issuer: key,
+    keyring: signer,
 
-  assertion: params.assertion as any,
-  subject: params.subject
-});
+    assertion: params.assertion as any,
+    subject: params.subject
+  }),
+})
 
 /**
  * Issue credentials with transitive proof via a chain.
  */
-export const createChainCredentialSigner = (signer: Signer, chain: Chain, signingKey: PublicKey): CredentialSigner => params => createCredential({
-  issuer: signingKey,
-  keyring: signer,
-  signingKey,
-  chain,
+export const createChainCredentialSigner = (signer: Signer, chain: Chain, signingKey: PublicKey): CredentialSigner => ({
+  getIssuer: () => chain.credential.issuer,
+  createCredential: params => createCredential({
+    issuer: chain.credential.issuer,
+    keyring: signer,
+    signingKey,
+    chain,
 
-  assertion: params.assertion as any,
-  subject: params.subject
-});
+    assertion: params.assertion as any,
+    subject: params.subject
+  })
+})
