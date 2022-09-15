0790e5d7014052f1abfffa022ccaedeea83f5604
Josiah Witt <josiah@witt.life>
2022-09-15 14:06:26 -0400

    feat(rpc-tunnel): Create package (#1506)
    https://github.com/dxos/braneframe/issues/732

---
+++ e2e/rpc-tunnel/playwright/tests/iframe.test.ts
@@ -0,0 +1,35 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { expect, Page, test } from '@playwright/test';
+import waitForExpect from 'wait-for-expect';
+
+const config = {
+  baseUrl: 'http://127.0.0.1:5173/'
+};
+
+test.describe('iframe', () => {
+  let page: Page;
+
+  test.beforeAll(async ({ browser }) => {
+    const context = await browser.newContext();
+    page = await context.newPage();
+    await page.goto(`${config.baseUrl}/iframe.html`);
+  });
+
+  test('loads and connects.', async () => {
+    await waitForExpect(async () => {
+      const isVisible = await page.isVisible(':has-text("value")');
+      expect(isVisible).toBeTruthy();
+    });
+  });
+
+  test('parent and child share source of truth.', async () => {
+    const a = await page.locator('p:right-of(:text("value"), 10)').textContent();
+    const b = await page.frameLocator('#test-iframe').locator('p:right-of(:text("value"), 10)').textContent();
+    const [intA, intB] = [a!, b!].map(str => parseInt(str.slice(1)));
+
+    expect(Math.abs(intA - intB)).toBeLessThan(50);
+  });
+});
---
+++ e2e/rpc-tunnel/src/iframe.tsx
@@ -0,0 +1,100 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import React, { StrictMode, useRef, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { useAsyncEffect } from '@dxos/react-async';
+import { JsonTreeView } from '@dxos/react-components';
+import { createProtoRpcPeer } from '@dxos/rpc';
+import { createIframeParentPort, createIframePort } from '@dxos/rpc-tunnel';
+
+import { schema } from './proto';
+import { TestClient } from './test-client';
+
+const IN_IFRAME = window.parent !== window;
+
+const App = () => {
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  const [closed, setClosed] = useState(true);
+  const [error, setError] = useState<string>();
+  const [value, setValue] = useState<string>();
+
+  useAsyncEffect(async () => {
+    if (IN_IFRAME) {
+      const port = createIframePort('http://127.0.0.1:5173');
+      const client = new TestClient();
+      const server = createProtoRpcPeer({
+        requested: {
+          TestStreamService: schema.getService('dxos.test.rpc.TestStreamService')
+        },
+        exposed: {
+          TestStreamService: schema.getService('dxos.test.rpc.TestStreamService')
+        },
+        handlers: client.handlers,
+        port
+      });
+      await server.open();
+
+      client.subscribe(value => setValue(String(value)));
+    } else {
+      const port = await createIframeParentPort(iframeRef.current!, 'http://localhost:5173');
+      const client = createProtoRpcPeer({
+        requested: {
+          TestStreamService: schema.getService('dxos.test.rpc.TestStreamService')
+        },
+        exposed: {},
+        handlers: {},
+        port
+      });
+      await client.open();
+
+      const stream = client.rpc.TestStreamService.testCall({ data: 'requestData' });
+      stream.subscribe(msg => {
+        setValue(msg.data);
+      }, error => {
+        if (error) {
+          setError(error.message);
+        }
+        setClosed(true);
+      });
+    }
+
+    setClosed(false);
+  }, []);
+
+  return (
+    <div style={{
+      display: 'flex'
+    }}>
+      <div style={{
+        flexGrow: 1
+      }}>
+        <JsonTreeView data={{
+          closed,
+          error,
+          value
+        }} />
+      </div>
+      {!IN_IFRAME && (
+        <iframe
+          ref={iframeRef}
+          id='test-iframe'
+          // If main app is loaded from 127.0.0.1, localhost is cross-origin.
+          //   https://stackoverflow.com/a/5268240/2804332
+          src='http://localhost:5173/iframe.html'
+          style={{
+            flexGrow: 1
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
