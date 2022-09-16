314c4d295e81e9ae4a5fd8f52e1d69edd72f2f6a
Dmytro Maretskyi <maretskii@gmail.com>
2022-09-16 15:42:58 -0400

    Add identity-manager

---
+++ packages/echo/echo-db/src/packlets/identity/identity-manager.test.ts
@@ -0,0 +1,64 @@
+import { Keyring } from "@dxos/keyring";
+import { codec } from "@dxos/echo-protocol";
+import { FeedStore } from "@dxos/feed-store";
+import { createStorage, Storage, StorageType } from "@dxos/random-access-storage";
+import { MetadataStore } from "../metadata";
+import { IdentityManager } from "./identity-manager";
+import { NetworkManager } from "@dxos/network-manager";
+import { MemorySignalManager, MemorySignalManagerContext } from "@dxos/messaging";
+import { afterTest } from "@dxos/testutils";
+import { it as test } from 'mocha'
+import expect from 'expect'
+
+describe('identity/identity-manager', () => {
+
+  
+  const setup = async ({
+    signalContext = new MemorySignalManagerContext(),
+    storage = createStorage({ type: StorageType.RAM }),
+  }: {
+    signalContext?: MemorySignalManagerContext,
+    storage?: Storage
+  } = {}) => {
+    const metadata = new MetadataStore(storage.createDirectory('metadata'))
+    const keyring = new Keyring(storage.createDirectory('keyring'));
+    const feedStore = new FeedStore(storage.createDirectory('feeds'), { valueEncoding: codec });
+
+    const networkManager = new NetworkManager({
+      signalManager: new MemorySignalManager(signalContext)
+    })
+
+    return new IdentityManager(
+      metadata,
+      keyring,
+      feedStore,
+      networkManager
+    )
+  }
+
+  test('creates identity', async () => {
+    const identityManager = await setup()
+    await identityManager.open()
+    afterTest(() => identityManager.close())
+
+    const identity = await identityManager.createIdentity();
+    expect(identity).toBeTruthy();
+  });
+
+  test('reload from storage', async () => {
+    const storage = createStorage({ type: StorageType.RAM });
+
+    const identityManager1 = await setup({ storage })
+    await identityManager1.open()
+    const identity1 = await identityManager1.createIdentity();
+    await identityManager1.close()
+
+    const identityManager2 = await setup({ storage })
+    await identityManager2.open()
+    expect(identityManager2.identity).toBeDefined()
+    expect(identityManager2.identity!.identityKey).toEqual(identity1.identityKey)
+    expect(identityManager2.identity!.deviceKey).toEqual(identity1.deviceKey)
+
+    // TODO(dmaretskyi): Check that identity is "alive" (space is working and can write mutations).
+  })
+})
---
+++ packages/echo/echo-db/src/packlets/identity/identity-manager.ts
@@ -0,0 +1,133 @@
+import { MetadataStore } from "../metadata";
+import { Identity } from "./identity";
+import { Keyring } from '@dxos/keyring'
+import { FeedStore } from "@dxos/feed-store";
+import { AdmittedFeed, CredentialGenerator, IdentityRecord, SpaceRecord } from "@dxos/halo-protocol";
+import assert from "assert";
+import { MOCK_AUTH_PROVIDER, MOCK_AUTH_VERIFIER, Space, SwarmIdentity } from "../space";
+import { Timeframe } from "@dxos/protocols";
+import { NetworkManager, Plugin } from "@dxos/network-manager";
+
+interface ConstructSpaceParams {
+  spaceRecord: SpaceRecord;
+  swarmIdentity: SwarmIdentity;
+  networkPlugins: Plugin[];
+}
+
+export class IdentityManager {
+  private _identity?: Identity;
+
+  constructor(
+    private readonly _metadataStore: MetadataStore,
+    private readonly _keyring: Keyring,
+    private readonly _feedStore: FeedStore,
+    private readonly _networkManager: NetworkManager,
+  ) {}
+
+  get identity() {
+    return this._identity;
+  }
+
+  async open() {
+    await this._metadataStore.load();
+    
+    const identityRecord = this._metadataStore.getIdentityRecord();
+    if (identityRecord) {
+      this._identity = await this._constructIdentity(identityRecord);
+      await this._identity.open()
+      await this._identity.ready();
+    }
+  }
+
+  async close() {
+    await this._identity?.close();
+  }
+
+  private async _constructIdentity(identityRecord: IdentityRecord) {
+    assert(!this._identity);
+
+    const space = await this._constructSpace({
+      spaceRecord: identityRecord.haloSpace,
+      swarmIdentity: {
+        peerKey: identityRecord.deviceKey,
+        credentialProvider: MOCK_AUTH_PROVIDER,
+        credentialAuthenticator: MOCK_AUTH_VERIFIER
+      },
+      networkPlugins: []
+    });
+
+    return new Identity({
+      identityKey: identityRecord.identityKey,
+      deviceKey: identityRecord.deviceKey,
+      signer: this._keyring,
+      space
+    });
+  }
+
+  private async _constructSpace({ spaceRecord, swarmIdentity, networkPlugins }: ConstructSpaceParams) {
+    const controlFeed = await this._feedStore.openReadWriteFeedWithSigner(spaceRecord.writeControlFeedKey, this._keyring);
+    const dataFeed = await this._feedStore.openReadWriteFeedWithSigner(spaceRecord.writeDataFeedKey, this._keyring);
+
+    // Might be the same feed as the control feed on the top. 
+    // It's important to initialize it after writable feeds so that the feed is in the writable state.
+    const genesisFeed = await this._feedStore.openReadOnlyFeed(spaceRecord.genesisFeedKey);
+
+    return new Space({
+      spaceKey: spaceRecord.spaceKey,
+      genesisFeed,
+      controlFeed,
+      dataFeed,
+      // TODO(dmaretskyi): This might always be the empty timeframe.
+      initialTimeframe: new Timeframe(),
+      feedProvider: key => this._feedStore.openReadOnlyFeed(key),
+      networkManager: this._networkManager,
+      networkPlugins,
+      swarmIdentity
+    });
+  }
+
+  async createIdentity() {
+    assert(!this._identity, 'Identity already exists.');
+
+    const controlFeedKey = await this._keyring.createKey();
+
+    const identityRecord: IdentityRecord = {
+      identityKey: await this._keyring.createKey(),
+      deviceKey: await this._keyring.createKey(),
+      haloSpace: {
+        spaceKey: await this._keyring.createKey(),
+        genesisFeedKey: controlFeedKey,
+        writeControlFeedKey: controlFeedKey,
+        writeDataFeedKey: await this._keyring.createKey(),
+      }
+    }
+    const identity = await this._constructIdentity(identityRecord);
+    await identity.open();
+
+    {
+      const generator = new CredentialGenerator(this._keyring, identityRecord.identityKey, identityRecord.deviceKey);
+      const credentials = [
+        ...await generator.createSpaceGenesis(identityRecord.haloSpace.spaceKey, identityRecord.haloSpace.genesisFeedKey),
+        await generator.createFeedAdmission(identityRecord.haloSpace.spaceKey, identityRecord.haloSpace.writeDataFeedKey, AdmittedFeed.Designation.DATA),
+
+        // Write device chain
+        await generator.createDeviceAuthorization(identityRecord.deviceKey),
+      ];
+
+      for (const credential of credentials) {
+        await identity.controlMessageWriter?.write({
+          '@type': 'dxos.echo.feed.CredentialsMessage',
+          credential
+        });
+      }
+    }
+
+    // await this._keyring.deleteKey(identityRecord.identityKey);
+    // await this._keyring.deleteKey(identityRecord.haloSpace.spaceKey);
+
+    await this._metadataStore.setIdentityRecord(identityRecord);
+    this._identity = identity;
+    await this._identity.ready();
+    return identity;
+  }
+}
