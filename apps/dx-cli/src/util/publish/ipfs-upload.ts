e63c8881fc81ed48326fc9f2dc230ce6d7871d1c
Egor Gripasov <gripasovegor@gmail.com>
2022-09-19 22:16:52 -0400

    fix: dx-cli app commands. (#1523)
    * fix: dx-cli app commands.
    
    * fix: update config.proto
    
    * fix: build & publish.
    
    * fix: read dx.yml.
    
    * fix: working ipfs publish.
    
    * fix: deploy.
    
    * fix: rm wrong fields.
    
    * fix: better list; docs & test script.
    
    * fix: test.
    
    * fix: .json?
    
    * fix: signal to use common kube port.
    
    * fix: better err handling.
    
    * Update apps/dx-cli/README.md
    
    Co-authored-by: Josiah Witt <josiah@witt.life>
    
    * fix: apply suggested changes.
    
    * fix: fix merge.
    
    Co-authored-by: Josiah Witt <josiah@witt.life>

---
+++ apps/dx-cli/src/util/publish/ipfs-upload.ts
@@ -0,0 +1,42 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import assert from 'assert';
+import fs from 'fs';
+import { CID, create, globSource } from 'ipfs-http-client';
+
+import { ConfigProto } from '@dxos/config';
+
+interface UploadOptions {
+  timeout: string | number
+  progress?: (bytes: number, path?: string) => void
+  pin?: boolean
+}
+
+export const uploadToIPFS = async (path: string, config?: ConfigProto, options?: UploadOptions): Promise<CID> => {
+  const { timeout, pin = true, progress } = options || {};
+
+  const ipfsServer = config?.runtime?.services?.ipfs?.server;
+  assert(ipfsServer, 'Invalid IPFS Server.');
+
+  const ipfsClient = create({
+    url: ipfsServer,
+    timeout: timeout || '1m'
+  });
+
+  if (!fs.existsSync(path)) {
+    throw new Error('Incorrect path to definitons. File or directory does not exist');
+  }
+  if (fs.lstatSync(path).isDirectory()) {
+    const files = [];
+    for await (const file of ipfsClient.addAll(globSource(path, '**/*'), { progress, pin, wrapWithDirectory: true })) {
+      files.push(file);
+    }
+    return files[files.length - 1].cid;
+  } else {
+    const content = fs.readFileSync(path);
+    const addResult = await ipfsClient.add(content, { pin });
+    return addResult.cid;
+  }
+};
