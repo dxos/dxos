24cf878c8f58ca66f9b93dc5e4e69359fa7c6d4f
Josiah Witt <josiah@witt.life>
2022-09-19 20:59:22 -0400

    chore: Downgrade to React 17 (#1525)
    We're not yet using any new features and Docusaurus requires 17 still so downgrading in preparation for adding that.

--- packages/sdk/react-client/src/hooks/client/client.test.tsx
+++ packages/sdk/react-client/src/hooks/client/client.test.tsx
@@ -2,9 +2,9 @@
 // Copyright 2020 DXOS.org
 //
 
-import { renderHook } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 import expect from 'expect';
-import React, { Component, PropsWithChildren } from 'react';
+import React from 'react';
 
 import { Client } from '@dxos/client';
 import { ConfigProto } from '@dxos/config';
@@ -15,35 +15,9 @@ import { useClient } from './useClient';
 describe('Client hook', () => {
   const render = () => useClient();
 
-  it.skip('should throw when used outside a context', () => {
-    // TODO(wittjosiah): Fix and factor out.
-    // Based on https://github.com/testing-library/react-testing-library/pull/991#issuecomment-1207138334
-    let error;
-    const { result } = renderHook(render, {
-      wrapper: class Wrapper extends Component<PropsWithChildren<unknown>> {
-        constructor (props: PropsWithChildren<unknown>) {
-          super(props);
-          this.state = { hasError: false };
-        }
-
-        static getDerivedStateFromError () {
-          // Update state so the next render will show the fallback UI.
-          return { hasError: true };
-        }
-
-        override componentDidCatch (err: Error) {
-          console.log({ err });
-          error = err;
-        }
-
-        override render () {
-          return this.props.children;
-        }
-      }
-    });
-
-    expect(error).toBeDefined();
-    expect(result).not.toBeDefined();
+  it('should throw when used outside a context', () => {
+    const { result } = renderHook(render);
+    expect(result.error?.message).toBeDefined();
   });
 
   it('should return client when used properly in a context', () => {
