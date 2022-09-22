75d48154fc56f1214fd42260f117b911faa52ec7
Egor Gripasov <gripasovegor@gmail.com>
2022-09-22 09:49:04 -0400

    fix: config updates. (#1551)

--- packages/sdk/config/src/plugin/types.ts
+++ packages/sdk/config/src/plugin/types.ts
@@ -10,7 +10,7 @@ export interface ConfigPluginOpts {
 
   /**
    * The Dynamics() config.yml file is special, it will be loaded if the dynamic property is set to false.
-   * If dynamic is set to true each app will try to load from an endpoint (using {publicUrl}/config/config.json),
+   * If dynamic is set to true each app will try to load from an endpoint (using {publicUrl}/.well-known/dx/config),
    * wire app serve adds config endpoints for each app serving the global config file (~/.wire/remote.yml).
    *
    * The usual pattern is to set it to CONFIG_DYNAMIC env variable.
