f862aef22e82d4b20fcb08c2dcb51a5691ad8f30
Josiah Witt <josiah@witt.life>
2022-09-22 15:46:22 -0400

    feat: Add app templates (#1539)
    https://github.com/dxos/kube/issues/439

--- apps/templates/hello/src/stories/App.stories.tsx
+++ apps/templates/hello/src/stories/App.stories.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 import { ClientProvider } from '@dxos/react-client';
 import { ProfileInitializer } from '@dxos/react-client-testing';
 
-import { App } from '../src';
+import { App } from '../App';
 import { ONLINE_CONFIG } from './config';
 
 export default {
