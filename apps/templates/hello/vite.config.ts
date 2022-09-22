67966b14a7fef3868a7351e9059be141bab84c4f
Josiah Witt <josiah@witt.life>
2022-09-22 15:46:22 -0400

    feat: Add app templates (#1539)
    https://github.com/dxos/kube/issues/439

---
+++ apps/templates/hello/vite.config.ts
@@ -0,0 +1,61 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+import { dxosPlugin } from '@dxos/vite-plugin';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '', // Ensures relative path to assets.
+  optimizeDeps: {
+    force: true,
+    include: [
+      '@dxos/client',
+      '@dxos/config',
+      '@dxos/react-client',
+      '@dxos/react-components',
+      '@dxos/react-toolkit'
+    ]
+  },
+  build: {
+    commonjsOptions: {
+      include: [
+        /packages/,
+        /node_modules/
+      ]
+    }
+  },
+  plugins: [
+    dxosPlugin(__dirname),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 30000000
+      },
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Hello DXOS',
+        short_name: 'Hello',
+        description: 'DXOS Hello World Application',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
