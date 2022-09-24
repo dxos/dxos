a76e73f420e37a48fd72e79da82e944d66070dfc
Josiah Witt <josiah@witt.life>
2022-09-24 16:00:37 -0400

    docs: Refactor sidebar gen

--- apps/vuepress/vuepress.config.ts
+++ apps/vuepress/vuepress.config.ts
@@ -4,14 +4,16 @@
 
 import { searchPlugin } from '@vuepress/plugin-search';
 import frontMatter from 'front-matter';
-import { existsSync, readdirSync, readFileSync } from 'fs';
+import { existsSync, lstatSync, readdirSync, readFileSync } from 'fs';
 import { join } from 'path';
 import { defaultTheme, defineUserConfig, SidebarGroupCollapsible, SidebarItem, UserConfig } from 'vuepress';
 
+const DOCS_PATH = join(__dirname, '/docs');
+
 const parseFrontMatter = (path: string): [number, SidebarItem | string] => {
-  const content = readFileSync(join(__dirname, 'docs', path), 'utf-8');
+  const content = readFileSync(path, 'utf-8');
   const { attributes } = frontMatter<{ position: number, label: string }>(content);
-  const link = `/${path}`;
+  const link = path.slice(DOCS_PATH.length);
   const item = attributes.label ? {
     text: attributes.label,
     link
@@ -20,39 +22,41 @@ const parseFrontMatter = (path: string): [number, SidebarItem | string] => {
   return [attributes.position, item];
 };
 
-// TODO(wittjosiah): Make recursive and see what it looks like in the UI.
-// TODO(wittjosiah): Match Zhena's desired tree shape for API docs.
-// TODO(wittjosiah): Generate navbar dropdown for API docs.
-const sidebar = () => readdirSync(join(__dirname, '/docs'))
+type SidebarObject = SidebarGroupCollapsible | SidebarItem | string
+type NumberedSidebarObject = [number, SidebarObject]
+
+const sidebarSection = (path: string) => readdirSync(path)
+  .map((file): NumberedSidebarObject | null => {
+    const filePath = join(path, file);
+    if (file.endsWith('.md')) {
+      return parseFrontMatter(filePath);
+    }
+
+    if (!lstatSync(filePath).isDirectory()) {
+      return null;
+    }
+
+    const infoPath = join(path, file, '_category_.json');
+    const sectionInfo = existsSync(infoPath) ? JSON.parse(readFileSync(infoPath, 'utf-8')) : {};
+    const children = sidebarSection(filePath);
+
+    return [sectionInfo.position, {
+      text: sectionInfo.label ?? file,
+      collapsible: true,
+      children
+    }];
+  })
+  .filter((section): section is NumberedSidebarObject => !!section)
+  .sort(([a], [b]) => a - b)
+  .map(([, section]) => section);
+
+const sidebar = () => readdirSync(DOCS_PATH)
   .filter(area => !area.includes('.'))
   .reduce((sidebar, area) => {
-    const areaSidebar = readdirSync(join(__dirname, 'docs', area))
-      .map((section): [number, SidebarGroupCollapsible | SidebarItem | string] => {
-        if (section.endsWith('.md')) {
-          return parseFrontMatter(join(area, section));
-        }
-
-        const infoPath = join(__dirname, 'docs', area, section, '_category_.json');
-        const sectionInfo = existsSync(infoPath) ? JSON.parse(readFileSync(infoPath, 'utf-8')) : {};
-
-        const children = readdirSync(join(__dirname, 'docs', area, section))
-          .filter(page => page.endsWith('.md'))
-          .map(page => parseFrontMatter(join(area, section, page)))
-          .sort(([a], [b]) => a - b)
-          .map(([, page]) => page);
-
-        return [sectionInfo.position, {
-          text: sectionInfo.label ?? section,
-          collapsible: true,
-          children
-        }];
-      })
-      .sort(([a], [b]) => a - b)
-      .map(([, section]) => section);
 
     return {
       ...sidebar,
-      [`/${area}`]: areaSidebar
+      [`/${area}`]: sidebarSection(join(__dirname, 'docs', area))
     };
   }, {});
 
@@ -67,6 +71,7 @@ const config: UserConfig = defineUserConfig({
     docsRepo: 'dxos/dxos',
     docsBranch: 'wittjosiah/09-23-docs_Vuepress',
     docsDir: 'apps/vuepress/docs',
+    // TODO(wittjosiah): Generate navbar dropdown for API docs.
     navbar: [
       {
         text: 'Guide',
@@ -86,6 +91,7 @@ const config: UserConfig = defineUserConfig({
   plugins: [
     // Config: https://vuepress.github.io/reference/plugin/search.html
     searchPlugin()
+    // TODO(wittjosiah): Match Zhena's desired tree shape for API docs. See typedoc-plugin-markdown.
     // TODO(wittjosiah): Can these link to each other?
     // typedocPlugin({
     //   entryPoints: ['../../packages/echo/echo-db/src/index.ts'],
