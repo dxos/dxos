445692723a20b8e9e96e10d6b672d7f787cf46df
Dmytro Maretskyi <maretskii@gmail.com>
2022-10-02 09:39:26 +0200

    Move api into packlets

--- packages/core/echo/echo-db/src/index.ts
+++ packages/core/echo/echo-db/src/index.ts
@@ -7,8 +7,7 @@ export * from './packlets/database';
 export * from './packlets/errors';
 export * from './packlets/metadata';
 export * from './packlets/space';
-
-export * from './api';
+export * from './packlets/api';
 
 // Stubs
 export type PartyMember = any
