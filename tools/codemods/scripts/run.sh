f021d1c696fd7bc10d38d029ab2c4dd6a5671286
Dmytro Maretskyi <maretskii@gmail.com>
2022-10-04 11:38:04 +0200

    x

--- tools/codemods/scripts/run.sh
+++ tools/codemods/scripts/run.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-pnpm jscodeshift --parser=ts --extensions=ts "$SCRIPT_DIR/../../../**/*.ts" "$@"
+pnpm jscodeshift --parser=ts --extensions=ts "$@" $SCRIPT_DIR/../../../**/*.ts
