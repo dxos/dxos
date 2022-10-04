156c90f934ae54dfe3d6abc9551ed8e3a489fda2
Dmytro Maretskyi <maretskii@gmail.com>
2022-10-04 11:31:46 +0200

    Add codemod

---
+++ tools/codemods/src/imports.ts
@@ -0,0 +1,53 @@
+import { Transform } from 'jscodeshift'
+
+const transform: Transform = (fileInfo, api, options) => {
+  const j = api.jscodeshift;
+
+  const replaceList = [options.replace].flat()
+  const root = j(fileInfo.source)
+
+  root
+    .find(j.ImportSpecifier)
+    .forEach(path => {
+      const identifier = path.node.imported.name
+      const source = path.parent.value.source.value
+
+      const replaceTarget = getReplaceTarget(replaceList, source, identifier)
+      if(!replaceTarget) {
+        return
+      }
+
+      console.log({ identifier, source, replaceTarget })
+
+      path.parent.insertAfter(j.importDeclaration(
+        [j.importSpecifier(j.identifier(identifier))],
+        j.literal(replaceTarget)
+      ))
+
+      // Remove old import
+      if(path.parent.value.specifiers.length === 1) {
+        j(path.parent).remove()
+      } else {
+        j(path).remove()
+      }
+
+      
+      console.log({ path: path.parent.value })
+    })
+
+  return root.toSource()
+}
+
+const getReplaceTarget = (replaceList: string[], source: string, identifier: string): string | undefined => {
+  for(const replace of replaceList) {
+    
+    const [from, to] = replace.split(':')
+    const [pkg, id] = from.split('#')
+    
+    if(source === pkg && identifier === id) {
+      return to
+    }
+  }
+}
+
+export default transform
