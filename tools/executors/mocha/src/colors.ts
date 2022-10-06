fd9e653032cdba22f408fcf86071ad527ecda715
Dmytro Maretskyi <maretskii@gmail.com>
2022-10-06 16:50:58 +0200

    Lint

--- tools/executors/mocha/src/colors.ts
+++ tools/executors/mocha/src/colors.ts
@@ -1,6 +1,10 @@
-import { inspect } from 'util'
+//
+// Copyright 2022 DXOS.org
+//
+
+import { inspect } from 'util';
 
 // This should be executed globally to enable colors in mocha
 
-inspect.defaultOptions.colors = true
-process.stdout.hasColors = () => true
+inspect.defaultOptions.colors = true;
+process.stdout.hasColors = () => true;
