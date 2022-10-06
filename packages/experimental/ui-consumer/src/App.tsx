77618780853d04d3eba8594b9fbf3c1dd71f0ca3
Will @ DXOS <will@braneframe.com>
2022-10-05 21:37:34 -0700

    Adjust things for fun and also to learn.

--- packages/experimental/ui-consumer/src/App.tsx
+++ packages/experimental/ui-consumer/src/App.tsx
@@ -5,5 +5,5 @@
 import React from 'react';
 
 export const App = () => {
-  return <button className='btn btn-accent'>Potato</button>;
+  return <button className='btn btn-outline'>Potato</button>;
 };
--- packages/ui/theme-plugin/src/index.ts
+++ packages/ui/theme-plugin/src/index.ts
@@ -47,12 +47,43 @@ const themePlugin = (options: VitePluginTailwindOptions) => {
                 ...(env.mode === 'development' && { mode: 'jit' }),
                 content: [
                   resolve(__dirname, '../node_modules/daisyui/dist/**/*.js'),
-                  // 'node_modules/react-daisyui/dist/**/*.js',
                   ...config.content
                 ],
                 plugins: [daisyui],
                 daisyui: {
-                  themes: ['light', 'dark']
+                  themes: [{
+                    light: {
+                      primary: '#570df8',
+                      'primary-content': '#ffffff',
+                      secondary: '#f000b8',
+                      'secondary-content': '#ffffff',
+                      accent: '#37cdbe',
+                      'accent-content': '#163835',
+                      neutral: '#3d4451',
+                      'neutral-content': '#ffffff',
+                      'neutral-focus': '#000000',
+                      'base-100': '#ffffff',
+                      'base-200': '#F2F2F2',
+                      'base-300': '#E5E6E6',
+                      'base-content': '#1f2937'
+                    }
+                  }, {
+                    dark: {
+                      primary: '#661AE6',
+                      'primary-content': '#ffffff',
+                      secondary: '#D926AA',
+                      'secondary-content': '#ffffff',
+                      accent: '#1FB2A5',
+                      'accent-content': '#ffffff',
+                      neutral: '#191D24',
+                      'neutral-content': '#dddddd',
+                      'neutral-focus': '#ffffff',
+                      'base-100': '#2A303C',
+                      'base-200': '#242933',
+                      'base-300': '#20252E',
+                      'base-content': '#dddddd'
+                    }
+                  }]
                 }
               }),
               autoprefixer
