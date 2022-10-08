a7f3928cc0b6e13a0a392039615cc4b8ac5e8b8b
Josiah Witt <josiah@witt.life>
2022-10-08 09:50:20 -0400

    feat: POC shared client architecture

---
+++ packages/sdk/shared-client/vite.config.ts
@@ -0,0 +1,38 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { defineConfig } from 'vite';
+
+import { dxosPlugin } from '@dxos/vite-plugin';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '', // Ensures relative path to assets.
+  optimizeDeps: {
+    force: true,
+    include: [
+      '@dxos/async',
+      '@dxos/client',
+      '@dxos/codec-protobuf',
+      '@dxos/log',
+      '@dxos/protocols',
+      '@dxos/react-async',
+      '@dxos/react-components',
+      '@dxos/rpc',
+      '@dxos/rpc-tunnel'
+    ]
+  },
+  build: {
+    commonjsOptions: {
+      include: [
+        /packages/,
+        /node_modules/
+      ]
+    }
+  },
+  plugins: [dxosPlugin()],
+  worker: {
+    plugins: [dxosPlugin()]
+  }
+});
