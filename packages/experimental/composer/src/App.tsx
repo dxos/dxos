11de11ff318705d3010b18736c755fa177c4b560
Will @ DXOS <will@braneframe.com>
2022-10-07 18:29:27 -0700

    Rename ui-consumer to composer. Add more shades to 'primary'.

---
+++ packages/experimental/composer/src/App.tsx
@@ -0,0 +1,15 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import React from 'react';
+
+import { Main } from '@dxos/react-ui';
+
+export const App = () => {
+  return (
+    <Main>
+
+    </Main>
+  );
+};
