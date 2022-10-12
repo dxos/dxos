9276716367439d24f697aed84425308034e59267
Josiah Witt <josiah@witt.life>
2022-10-12 08:50:23 -0400

    refactor: Use client services host

--- packages/sdk/vault/testing/main.ts
+++ packages/sdk/vault/testing/main.ts
@@ -8,5 +8,9 @@ void (async () => {
   const client = new Client({ runtime: { client: { mode: 2 /* remote */ } } });
   await client.initialize();
 
+  if (!client.halo.profile) {
+    await client.halo.createProfile();
+  }
+
   console.log(client.info);
 })();
