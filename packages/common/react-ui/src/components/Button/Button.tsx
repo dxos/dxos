58fe086684031cd24f84974499834b4a8f3c2db3
Will @ DXOS <will@braneframe.com>
2022-10-11 18:01:42 -0700

    Get buttons right.

--- packages/common/react-ui/src/components/Button/Button.tsx
+++ packages/common/react-ui/src/components/Button/Button.tsx
@@ -5,15 +5,17 @@
 import cx from 'classnames';
 import React, { forwardRef } from 'react';
 
+import { defaultFocus } from '../../styles';
+
 export type ButtonProps = React.ComponentProps<'button'> & {
   variant?: 'default' | 'primary'
 };
 
 export const buttonClassName = (variant: ButtonProps['variant']) => cx(
   'inline-flex select-none items-center justify-center rounded-md px-4 py-2 text-sm font-medium',
-  variant === 'default' && 'shadow-md bg-white text-neutral-900 hover:bg-neutral-50 dark:bg-neutral-750 dark:text-neutral-100 dark:hover:bg-neutral-800',
-  variant === 'primary' && 'shadow-md bg-primary-550 text-white hover:bg-primary-600 dark:bg-primary-100 dark:text-black dark:hover:bg-primary-200',
-  'focus:outline-none focus-visible:ring focus-visible:ring-offset-1 focus-visible:ring-black focus-visible:ring-offset-white dark:focus-visible:ring-white dark:focus-visible:ring-offset-black',
+  variant === 'default' && 'shadow-md border border-neutral-100 bg-white text-neutral-900 hover:bg-neutral-50 dark:border-neutral-650 dark:bg-neutral-750 dark:text-neutral-100 dark:hover:bg-neutral-800',
+  variant === 'primary' && 'shadow-md border border-primary-700 bg-primary-600 text-white hover:bg-primary-650',
+  defaultFocus,
   // Register all radix states
   'group',
   'radix-state-open:bg-neutral-50 dark:radix-state-open:bg-neutral-900',
