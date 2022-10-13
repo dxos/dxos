33c851d122221707e3101cf077a9af07623d1eb7
Will @ DXOS <will@braneframe.com>
2022-10-12 22:26:09 -0700

    Input manages internal state for debouncing later. Fix exports.

--- packages/common/react-ui/src/components/index.ts
+++ packages/common/react-ui/src/components/index.ts
@@ -3,7 +3,8 @@
 //
 
 export * from './Button/Button';
-export * from './Dialog';
+export * from './Group/Group';
 export * from './Heading/Heading';
+export * from './Input/Input';
 export * from './Loading/Loading';
 export * from './Main/Main';
