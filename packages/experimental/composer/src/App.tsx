c898f9214c75c35bd5573f555f84bca43955cc00
Will @ DXOS <will@braneframe.com>
2022-10-13 11:48:44 -0700

    Fix config.

--- packages/experimental/composer/src/App.tsx
+++ packages/experimental/composer/src/App.tsx
@@ -12,9 +12,33 @@ import { PartyProvider } from './context/PartyProvider';
 import { ProfileProvider } from './context/ProfileProvider';
 import { TextItemProvider } from './context/TextItemProvider';
 
+const clientConfig = {
+  runtime: {
+    services: {
+      signal: {
+        server: 'wss://halo.dxos.org/.well-known/dx/signal'
+      },
+      ice: [
+        { urls: 'stun:demo.kube.dxos.org:3478' },
+        {
+          urls: 'turn:demo.kube.dxos.org:3478',
+          username: 'dxos',
+          credential: 'dxos'
+        },
+        { urls: 'stun:kube.dxos.org:3478' },
+        {
+          urls: 'turn:kube.dxos.org:3478',
+          username: 'dxos',
+          credential: 'dxos'
+        }
+      ]
+    }
+  }
+};
+
 export const App = () => {
   return (
-    <ClientProvider>
+    <ClientProvider config={clientConfig}>
       <ProfileProvider>
         <PartyProvider>
           <TextItemProvider>
