a93716213fa1cb367d194ab96cd1952c100d170d
Josiah Witt <josiah@witt.life>
2022-10-13 07:01:26 -0400

    test: Use local mode in default test config
    Providing remote mode an extended timeout makes automatic mode not
    viable in tests.

--- packages/sdk/client/src/packlets/proxies/client.ts
+++ packages/sdk/client/src/packlets/proxies/client.ts
@@ -42,6 +42,9 @@ export const defaultConfig: ConfigProto = { version: 1 };
 export const defaultTestingConfig: ConfigProto = {
   version: 1,
   runtime: {
+    client: {
+      mode: Runtime.Client.Mode.LOCAL
+    },
     services: {
       signal: {
         server: 'ws://localhost:4000/.well-known/dx/signal'
