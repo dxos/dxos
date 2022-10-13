91f5a94416bbf2851de33fec1b1509f6b164cc78
Josiah Witt <josiah@witt.life>
2022-10-12 21:21:20 -0400

    refactor(halo-app): Merge with Vault POC

--- packages/apps/halo-app/src/main.tsx
+++ packages/apps/halo-app/src/main.tsx
@@ -7,15 +7,29 @@ import { createRoot } from 'react-dom/client';
 // import debug from 'debug'
 // import { log } from '@dxos/log'
 
+import { createWorkerPort } from '@dxos/rpc-tunnel';
+
 // debug.enable('dxos:*')
 // log.config.filter='debug'
 
 import { App } from './App';
 
-// TODO(wittjosiah): StrictMode causing issues with the react sdk, re-enable once fixed.
-createRoot(document.getElementById('root')!)
-  .render(
-    // <StrictMode>
-    <App />
-    // </StrictMode>
-  );
+if (typeof SharedWorker !== 'undefined') {
+  void (async () => {
+    const worker = new SharedWorker(
+      new URL('./shared-worker', import.meta.url),
+      { type: 'module' }
+    );
+    const rpcPort = createWorkerPort({ channel: 'dxos:app', port: worker.port });
+
+    // TODO(wittjosiah): StrictMode causing issues with the react sdk, re-enable once fixed.
+    createRoot(document.getElementById('root')!)
+      .render(
+          // <StrictMode>
+          <App rpcPort={rpcPort} />
+          // </StrictMode>
+      );
+  })();
+} else {
+  throw new Error('Requires a browser with support for shared workers.');
+}
