a0ab6f41e761df45ad85863f27b8d3723e8e48bb
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-10-14 14:15:22 +0300

    Move cast read hack to memory-storage

--- packages/common/random-access-storage/src/common/memory-storage.ts
+++ packages/common/random-access-storage/src/common/memory-storage.ts
@@ -2,11 +2,11 @@
 // Copyright 2021 DXOS.org
 //
 
-import ram from 'random-access-memory';
-import { RandomAccessStorage } from 'random-access-storage';
+import ram from "random-access-memory";
+import { Callback, RandomAccessStorage } from "random-access-storage";
 
-import { AbstractStorage } from './abstract-storage';
-import { StorageType } from './storage';
+import { AbstractStorage } from "./abstract-storage";
+import { StorageType } from "./storage";
 
 /**
  * Storage interface implementation for RAM.
@@ -15,13 +15,30 @@ import { StorageType } from './storage';
 export class MemoryStorage extends AbstractStorage {
   public override type: StorageType = StorageType.RAM;
 
-  protected override _createFile (path: string, filename: string): RandomAccessStorage {
-    return ram();
+  protected override _createFile(
+    path: string,
+    filename: string
+  ): RandomAccessStorage {
+    return this._castReadToBuffer(ram());
   }
 
-  protected override _openFile (file: RandomAccessStorage): RandomAccessStorage {
+  protected override _openFile(file: RandomAccessStorage): RandomAccessStorage {
     const newFile = file.clone!();
     (newFile as any).closed = false;
-    return newFile;
+    return this._castReadToBuffer(newFile);
+  }
+
+  private _castReadToBuffer(file: RandomAccessStorage): RandomAccessStorage {
+    // Hack to make return type consistent on all platforms
+    const trueRead = file.read.bind(file);
+    file.read = (offset: number, size: number, cb: Callback<Buffer>) =>
+      trueRead(offset, size, (error: Error | null, data?: Buffer) => {
+        if (error) {
+          return cb(error);
+        } else {
+          return cb(error, Buffer.from(data!));
+        }
+      });
+    return file;
   }
 }
