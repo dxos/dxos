3133eee1e5748e5910473bc82c70e65782b87a4d
Dmytro Maretskyi <maretskii@gmail.com>
2022-10-14 15:19:03 +0200

    Unify tests

--- packages/core/mesh/network-manager/src/network-manager.test.ts
+++ packages/core/mesh/network-manager/src/network-manager.test.ts
@@ -2,35 +2,22 @@
 // Copyright 2021 DXOS.org
 //
 
-// @dxos/mocha platform=nodejs
-
 import { schema } from '@dxos/protocols';
 import { BridgeService } from '@dxos/protocols/proto/dxos/mesh/bridge';
 import { createLinkedPorts, createProtoRpcPeer } from '@dxos/rpc';
-import { createTestBroker, TestBroker } from '@dxos/signal';
 
 import { webRTCTests, inMemoryTests, webRTCProxyTests } from './network-manager.blueprint-test';
 import { WebRTCTransportService } from './transport';
 
-const PORT = 12087;
+// Signal server will be started by the setup script.
+const SIGNAL_URL = `ws://localhost:4000/.well-known/dx/signal`;
 
 describe('Network manager', function () {
   describe('WebRTC transport', function () {
-    let broker: TestBroker;
-
-    before(async function () {
-      broker = await createTestBroker(PORT);
-    });
-
-    after(function () {
-      broker?.stop();
-    });
-
-    webRTCTests({ signalUrl: `ws://localhost:${PORT}/.well-known/dx/signal` });
+    webRTCTests({ signalUrl: SIGNAL_URL });
   }).timeout(10_000);
 
   describe('WebRTC proxy transport', function () {
-    let broker: TestBroker;
     const [rpcPortA, rpcPortB] = createLinkedPorts();
 
     const webRTCTransportService: BridgeService = new WebRTCTransportService();
@@ -59,18 +46,16 @@ describe('Network manager', function () {
     });
 
     before(async function () {
-      broker = await createTestBroker(PORT);
       await service.open();
       await rpcClient.open();
     });
 
     after(function () {
-      broker?.stop();
       service.close();
       rpcClient.close();
     });
 
-    webRTCProxyTests({ signalUrl: `ws://localhost:${PORT}/.well-known/dx/signal`, bridgeService: rpcClient.rpc.BridgeService });
+    webRTCProxyTests({ signalUrl: SIGNAL_URL, bridgeService: rpcClient.rpc.BridgeService });
   });
 
   describe('In-memory transport', function () {
