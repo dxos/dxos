132af2d2abfa60b3a5a12d68df52fe75547d410e
willdx <will@braneframe.com>
2022-10-18 16:42:06 -0700

    Move useTranslation to hooks.

---
+++ packages/common/react-uikit/src/hooks/useTranslation.ts
@@ -0,0 +1,7 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { useTranslation as useNaturalTranslation } from 'react-i18next';
+
+export const useTranslation = useNaturalTranslation();
--- packages/common/react-uikit/src/index.ts
+++ packages/common/react-uikit/src/index.ts
@@ -2,4 +2,5 @@
 // Copyright 2022 DXOS.org
 //
 
+export { useTranslation, TFunction } from 'react-i18next';
 export * from './components';
