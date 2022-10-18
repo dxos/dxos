da1af094b98c1a579fd2d282fe2fb0ed41b0d923
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-10-18 16:30:27 +0300

    Fix FirefoxStorage

--- packages/common/random-access-storage/src/browser/firefox-storage.ts
+++ packages/common/random-access-storage/src/browser/firefox-storage.ts
@@ -5,13 +5,13 @@
 import raw from 'random-access-web/mutable-file-wrapper';
 
 import { StorageType } from '../common';
-import { BrowserStorage } from './browser-storage';
+import { IDbStorage } from './idb-storage';
 
 /**
  * Storage interface implementation for Firefox browser.
  * https://github.com/random-access-storage/random-access-web
  */
-export class FirefoxStorage extends BrowserStorage {
+export class FirefoxStorage extends IDbStorage {
   public override type: StorageType = StorageType.FIREFOX;
 
   protected override _createFileStorage (path: string) {
