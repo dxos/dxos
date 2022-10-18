778cb6aaea36c62dc8517eec897a76f19734dd36
Josiah Witt <josiah@witt.life>
2022-10-18 10:36:28 -0400

    ci: Simplify publish command

--- .circleci/scripts/publish.sh
+++ .circleci/scripts/publish.sh
@@ -4,8 +4,8 @@ BRANCH = $(git branch --show-current)
 
 if [ $BRANCH -eq "release" ]; then
   DX_CONFIG = ../../packages/devtools/dx-cli/config/config.yml
-  ../../packages/devtools/dx-cli/bin/run app publish --verbose
 else
   DX_CONFIG = ../../packages/devtools/dx-cli/config/dev-config.yml
-  ../../packages/devtools/dx-cli/bin/run app publish --tag dev --verbose
 fi
+
+  ../../packages/devtools/dx-cli/bin/run app publish --verbose
