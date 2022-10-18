1ccf9938c7259b70767ad3eaeb5265d21a651ca9
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-10-18 16:22:04 +0300

    Delete BrowserStorage class

--- packages/common/random-access-storage/src/browser/idb-storage.ts
+++ packages/common/random-access-storage/src/browser/idb-storage.ts
@@ -3,18 +3,59 @@
 //
 
 import idb from 'random-access-idb';
+import { RandomAccessStorage } from 'random-access-storage';
 
-import { StorageType } from '../common';
-import { BrowserStorage } from './browser-storage';
+import {
+  AbstractStorage,
+  File,
+  FileWrap,
+  getFullPath,
+  StorageType
+} from '../common';
 
 /**
  * Storage interface implementation for index DB.
  * https://github.com/random-access-storage/random-access-idb
  */
-export class IDbStorage extends BrowserStorage {
+export class IDbStorage extends AbstractStorage {
   public override type: StorageType = StorageType.IDB;
+  private readonly _fileStorage: (filename: string, opts?: {}) => RandomAccessStorage;
 
-  protected override _createFileStorage (path: string) {
+  constructor (path: string) {
+    super(path);
+    this._fileStorage = this._createFileStorage(path);
+  }
+
+  protected _createFileStorage (path: string): (filename: string, opts?: {}) => RandomAccessStorage {
     return idb(path);
+  }
+
+  protected override _createFile (path: string, filename: string): File {
+    const fullPath = getFullPath(path, filename);
+    return new FileWrap(this._fileStorage(fullPath));
+  }
+
+  protected override _openFile (file: File): File {
+    (file.native as any).closed = false;
+    return file;
+  }
+
+  protected override async _destroy () {
+    // eslint-disable-next-line no-undef
+    return new Promise<void>((resolve, reject) => {
+      const request = indexedDB.deleteDatabase(this.path);
+      request.onsuccess = () => {
+        resolve();
+      };
+      request.onupgradeneeded = () => {
+        reject(new Error('Upgrade needed.'));
+      };
+      request.onblocked = () => {
+        reject(new Error('Blocked.'));
+      };
+      request.onerror = (err: any) => {
+        reject(err);
+      };
+    });
   }
 }
