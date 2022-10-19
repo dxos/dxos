c6d51498952369385081c6cd23fec5eeda4031c1
willdx <will@braneframe.com>
2022-10-18 20:13:02 -0700

    Fix TKey export.

--- packages/common/react-uikit/src/index.ts
+++ packages/common/react-uikit/src/index.ts
@@ -7,6 +7,6 @@ import type { TFunction as NaturalTFunction } from 'react-i18next';
 export { useTranslation } from 'react-i18next';
 
 export type TFunction = NaturalTFunction;
-export type TKey = Parameters<TFunction>;
+export type TKey = Parameters<TFunction>[0];
 
 export * from './components';
