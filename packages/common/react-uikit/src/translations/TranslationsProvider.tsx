b89447ae25f108b4874bdbb8a0098e6ab4f39dc1
willdx <will@braneframe.com>
2022-10-18 17:43:43 -0700

    Fix structure of translations.

---
+++ packages/common/react-uikit/src/translations/TranslationsProvider.tsx
@@ -0,0 +1,31 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import i18Next from 'i18next';
+import React, { PropsWithChildren, ReactNode, Suspense } from 'react';
+import { initReactI18next, useTranslation } from 'react-i18next';
+
+import { Loading } from '@dxos/react-ui';
+
+import * as enUS from './locales/en-US';
+
+export const resources = {
+  'en-US': enUS
+} as const;
+
+export const i18n = i18Next.use(initReactI18next).init({
+  resources,
+  lng: 'en-US',
+  interpolation: {
+    escapeValue: false
+  }
+});
+
+export const TranslationsProvider = ({
+  children,
+  fallback = <Loading />
+}: PropsWithChildren<{ fallback?: ReactNode }>) => {
+  const { t: _t } = useTranslation();
+  return <Suspense fallback={fallback}>{children}</Suspense>;
+};
--- packages/common/react-uikit/src/translations/index.tsx
+++ packages/common/react-uikit/src/translations/index.tsx
@@ -2,38 +2,4 @@
 // Copyright 2022 DXOS.org
 //
 
-import i18Next from 'i18next';
-import React, { PropsWithChildren, ReactNode, Suspense } from 'react';
-import {
-  initReactI18next,
-  useTranslation as useNaturalTranslation,
-  TFunction as NaturalTFunction
-} from 'react-i18next';
-
-import { Loading } from '@dxos/react-ui';
-
-import * as enUS from './en-US';
-
-export const useTranslation = useNaturalTranslation;
-
-export type TFunction = NaturalTFunction
-
-export const resources = {
-  'en-US': enUS
-} as const;
-
-export const i18n = i18Next.use(initReactI18next).init({
-  resources,
-  lng: 'en-US',
-  interpolation: {
-    escapeValue: false
-  }
-});
-
-export const TranslationsProvider = ({
-  children,
-  fallback = <Loading />
-}: PropsWithChildren<{ fallback?: ReactNode }>) => {
-  const { t: _t } = useNaturalTranslation();
-  return <Suspense fallback={fallback}>{children}</Suspense>;
-};
+export * from './TranslationsProvider';
