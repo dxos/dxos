7cb46cfb0cf930f17f3c6b5700ad37dde2b0b528
Zhenya <zhenya@zhenya.co>
2022-10-19 03:16:30 -0400

    added functions and values

---
+++ docs/docs/api/@dxos/react-client/functions/useSecretProvider.md
@@ -0,0 +1,2 @@
+# Function `useSecretProvider`
+> Declared in [`packages/sdk/react-client/src/hooks/invitations/useSecretProvider.ts:13`](https://github.com/dxos/protocols/blob/main/packages/sdk/react-client/src/hooks/invitations/useSecretProvider.ts#L13)
---
+++ docs/src/templates/api/package/values/Value.t.ts
@@ -0,0 +1,48 @@
+import { ReflectionKind, JSONOutput as S } from "typedoc";
+import {
+  Input,
+  TemplateFunction,
+  text,
+  sources,
+  File,
+  JSONFile,
+  comment,
+  packagesInProject,
+  reflectionsOfKind,
+  stringifyType,
+} from "../..";
+
+const template: TemplateFunction<Input> = ({ input, outputDirectory }) => {
+  const packages = packagesInProject(input);
+  return packages
+    .map((pkage) => {
+      const values = reflectionsOfKind(
+        pkage,
+        ReflectionKind.Variable
+      ) as S.DeclarationReflection[];
+      return values
+        .map((avalue) => {
+          
+          const dir = [outputDirectory, pkage.name ?? "", "values"];
+          return [
+            new File({
+              path: [...dir, `${avalue.name}.md`],
+              content: text`
+                # Value \`${avalue.name}\`
+                ${sources(avalue)}
+
+                ${comment(avalue.comment)}
+                `,
+            }),
+            new JSONFile({
+              path: [...dir, `${avalue.name}.json`],
+              content: avalue,
+            }),
+          ];
+        })
+        .flat();
+    })
+    .flat();
+};
+
+export default template;
