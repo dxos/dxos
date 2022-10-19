0142b3ae620718b050aa2a6a1f5344359549732e
Will Shown <thure@users.noreply.github.com>
2022-10-19 13:23:14 -0700

    feat: UI kit stub (#1690)

---
+++ packages/common/react-ui/src/components/CompoundButton/CompoundButton.stories.tsx
@@ -0,0 +1,48 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import '@dxosTheme';
+import { ArrowRight, ClockCounterClockwise } from 'phosphor-react';
+import React, { PropsWithChildren } from 'react';
+
+import { templateForComponent } from '../../testing';
+import { CompoundButton, CompoundButtonProps } from './CompoundButton';
+
+export default {
+  title: 'react-ui/CompoundButton',
+  component: CompoundButton
+};
+
+const Container = ({ children }: PropsWithChildren<{}>) => (
+  <div className='flex gap-4'>{children}</div>
+);
+
+const Template = ({ ...args }: Omit<CompoundButtonProps, 'ref'>) => (
+  <Container>
+    <CompoundButton {...args} />
+    <CompoundButton {...args} disabled />
+  </Container>
+);
+
+export const Default = templateForComponent(Template)({ });
+Default.args = {
+  children: 'Hello',
+  description: 'This is a compound button',
+  before: <ClockCounterClockwise className='w-5 h-5' />,
+  after: <ArrowRight className='w-5 h-5' />,
+  disabled: false
+};
+
+export const Primary = () => (
+  <Template
+    {...{
+      children: 'Hello',
+      description: 'This is a compound button',
+      before: <ClockCounterClockwise className='w-5 h-5' />,
+      after: <ArrowRight className='w-5 h-5' />,
+      disabled: false,
+      variant: 'primary'
+    }}
+  />
+);
