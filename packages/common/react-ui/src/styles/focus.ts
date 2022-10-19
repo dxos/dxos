8d4f54072a2022b2295cf8a1eabd5430fcc5f1a2
willdx <will@braneframe.com>
2022-10-18 17:12:04 -0700

    Adjust focus colors.

--- packages/common/react-ui/src/styles/focus.ts
+++ packages/common/react-ui/src/styles/focus.ts
@@ -2,4 +2,4 @@
 // Copyright 2022 DXOS.org
 //
 
-export const defaultFocus = 'focus:outline-none focus-visible:hover:outline-none dark:focus-visible:hover:outline-none focus-visible:ring focus-visible:ring-offset-1 focus-visible:ring-primary-650 focus-visible:ring-offset-white dark:focus-visible:ring-primary-100 dark:focus-visible:ring-offset-black';
+export const defaultFocus = 'focus:outline-none focus-visible:hover:outline-none dark:focus-visible:hover:outline-none focus-visible:ring focus-visible:ring-offset-1 focus-visible:ring-primary-500 focus-visible:ring-offset-white dark:focus-visible:ring-primary-200 dark:focus-visible:ring-offset-black';
