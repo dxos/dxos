a7968c201856c1ab6b3fce77c35b1fec4ad345ab
willdx <will@braneframe.com>
2022-10-19 23:14:06 -0700

    Fix Composer.

--- packages/experimental/composer/src/components/ProviderFallback.tsx
+++ packages/experimental/composer/src/components/ProviderFallback.tsx
@@ -4,11 +4,12 @@
 
 import React from 'react';
 
-import { Heading, Loading } from '@dxos/react-ui';
+import { Heading } from '@dxos/react-ui';
+import { UntranslatedLoading, LoadingSize } from '@dxos/react-uikit';
 
 export const ProviderFallback = ({ message }: { message: string }) => (
   <div className='py-8 flex flex-col gap-4' aria-live='polite'>
-    <Loading size='lg' />
+    <UntranslatedLoading label={message} size={LoadingSize.lg} />
     <Heading level={1} className='text-lg font-light text-center'>
       {message}
     </Heading>
