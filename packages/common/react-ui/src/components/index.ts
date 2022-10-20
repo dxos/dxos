0bc7d65b8b86ec779955fc2b2cbd8a0df7ba98f3
willdx <will@braneframe.com>
2022-10-19 18:32:00 -0700

    Export new components.

--- packages/common/react-ui/src/components/index.ts
+++ packages/common/react-ui/src/components/index.ts
@@ -2,12 +2,14 @@
 // Copyright 2022 DXOS.org
 //
 
+export * from './Alert';
 export * from './Button';
 export * from './CompoundButton';
+export * from './Dialog';
 export * from './Group';
 export * from './Heading';
 export * from './Input';
 export * from './Main';
-export * from './Dialog';
+export * from './QrCode';
 export * from './Tooltip';
 export * from './UiProvider';
