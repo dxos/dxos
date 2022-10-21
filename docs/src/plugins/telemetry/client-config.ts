0842e667f6e33bf15d5a674b760864e0e644b64c
Josiah Witt <josiah@witt.life>
2022-10-21 16:28:48 -0400

    feat(docs): Telemetry plugin

---
+++ docs/src/plugins/telemetry/client-config.ts
@@ -0,0 +1,39 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { defineClientConfig } from '@vuepress/client';
+import { isNavigationFailure } from 'vue-router';
+
+declare global {
+  const __VUEPRESS_SSR__: boolean;
+}
+
+// https://vuepress.github.io/advanced/cookbook/usage-of-client-config.html
+export default defineClientConfig({
+  enhance: async ({ router }) => {
+    if (__VUEPRESS_SSR__) {
+      return;
+    }
+
+    const { init, page } = await import('@dxos/telemetry');
+    init({ apiKey: 'qjDvCXeTYe1dhJy7GN8Oa1ePcdi05DlS' });
+
+    const route = router.currentRoute.value;
+    page({
+      machineId: 'default',
+      identityId: 'default',
+      name: route.name?.toString() ?? route.path
+    });
+
+    router.afterEach((to, from, failure) => {
+      if (!isNavigationFailure(failure)) {
+        page({
+          machineId: 'default',
+          identityId: 'default',
+          name: to.name?.toString() ?? to.path
+        });
+      }
+    });
+  }
+});
