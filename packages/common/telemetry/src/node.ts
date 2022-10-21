11ec06e87923a6a4e8c0aa838a29b9beb94f11ce
Josiah Witt <josiah@witt.life>
2022-10-21 16:42:22 -0400

    feat(telemetry): Create package (#1723)

---
+++ packages/common/telemetry/src/node.ts
@@ -0,0 +1,74 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import Analytics from 'analytics-node';
+import assert from 'node:assert';
+
+import { captureException } from '@dxos/sentry';
+
+import { EventOptions, InitOptions, PageOptions } from './types';
+
+let analytics: Analytics;
+
+/**
+ *
+ */
+export const init = (options: InitOptions) => {
+  const apiKey = options.apiKey ?? process.env.DXOS_TELEMETRY_KEY;
+  assert(apiKey, 'Key required to send telemetry');
+
+  analytics = new Analytics(apiKey, {
+    flushAt: options.batchSize,
+    enable: options.enable
+  });
+};
+
+/**
+ *
+ */
+export const page = ({ machineId, identityId: anonymousId, ...options }: PageOptions) => {
+  assert(analytics, 'Analytics not initialized');
+
+  analytics.page({
+    ...options,
+    anonymousId,
+    properties: {
+      ...options.properties,
+      machineId
+    }
+  });
+};
+
+/**
+ *
+ */
+export const event = ({
+  machineId,
+  identityId: anonymousId,
+  name: event,
+  ...options
+}: EventOptions) => {
+  assert(analytics, 'Analytics not initialized');
+
+  analytics.track({
+    ...options,
+    anonymousId,
+    event,
+    properties: {
+      ...options.properties,
+      machineId
+    }
+  });
+};
+
+/**
+ *
+ */
+export const flush = async () => {
+  assert(analytics, 'Analytics not initialized');
+
+  await analytics.flush(err => {
+    captureException(err);
+  });
+};
