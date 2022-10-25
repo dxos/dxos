abefb78ba1c4b18990ebb72c36f26557f1c46f60
Josiah Witt <josiah@witt.life>
2022-10-25 17:21:22 -0400

    feat(docs): Telemetry plugin (#1729)

--- docs/vuepress.config.ts
+++ docs/vuepress.config.ts
@@ -2,12 +2,13 @@
 // Copyright 2022 DXOS.org
 //
 
+import { viteBundler } from '@vuepress/bundler-vite';
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components';
 import { searchPlugin } from '@vuepress/plugin-search';
 import { join, resolve } from 'node:path';
 import { defaultTheme, defineUserConfig, UserConfig } from 'vuepress';
 
-import { apiSidebar, DOCS_PATH, link, PINNED_PACKAGES, showcasePlugin, sidebarSection } from './src';
+import { apiSidebar, DOCS_PATH, link, PINNED_PACKAGES, showcasePlugin, sidebarSection, telemetryPlugin } from './src';
 
 // Config: https://vuepress.github.io/reference/config.html
 const config: UserConfig = defineUserConfig({
@@ -62,8 +63,27 @@ const config: UserConfig = defineUserConfig({
     }),
     // Config: https://vuepress.github.io/reference/plugin/search.html
     searchPlugin(),
+    telemetryPlugin(),
     await showcasePlugin()
-  ]
+  ],
+  bundler: viteBundler({
+    viteOptions: {
+      resolve: {
+        alias: {
+          'node:assert': 'assert/'
+        }
+      },
+      optimizeDeps: {
+        force: true,
+        include: ['@dxos/telemetry']
+      },
+      build: {
+        commonjsOptions: {
+          include: [/packages/, /node_modules/]
+        }
+      }
+    }
+  })
 });
 
 export default config;
