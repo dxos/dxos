6bfedd7d031dac661ffe75575147a6c4a2c2c06d
Dmytro Maretskyi <maretskii@gmail.com>
2022-10-27 19:44:29 +0200

    Lint

--- packages/core/mesh/broadcast/src/broadcast.test.ts
+++ packages/core/mesh/broadcast/src/broadcast.test.ts
@@ -46,7 +46,7 @@ class Peer extends EventEmitter {
       ...opts
     });
 
-    this._broadcast.packet.on(packet => {
+    this._broadcast.packet.on((packet) => {
       this._messages.set(packetId(packet), Buffer.from(packet.data!).toString('utf-8'));
       this.emit('packet', packet);
     });
