e1b068ddae256a7f4455feff4790e1e0114cbefb
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-10-27 14:06:58 +0300

    Comment out broken stuff

--- packages/devtools/devtools/src/sections.tsx
+++ packages/devtools/devtools/src/sections.tsx
@@ -41,75 +41,75 @@ import {
 } from './containers';
 
 export const sections: Section[] = [
-  {
-    title: 'CLIENT',
-    items: [
-      {
-        id: 'config',
-        title: 'Config',
-        icon: <ConfigIcon />,
-        panel: <ConfigPanel />
-      },
-      {
-        id: 'storage',
-        title: 'Storage',
-        icon: <StorageIcon />,
-        panel: <StoragePanel />
-      }
-    ]
-  },
-  {
-    title: 'HALO',
-    items: [
-      {
-        id: 'halo.profile',
-        title: 'Profile',
-        icon: <ProfileIcon />,
-        panel: <ProfilePanel />
-      },
-      {
-        id: 'halo.keyring',
-        title: 'Keyring',
-        icon: <KeyIcon />,
-        panel: <KeyringPanel />
-      },
-      {
-        id: 'halo.messagess',
-        title: 'Messages',
-        icon: <MessagesIcon />,
-        panel: <CredentialMessagesPanel />
-      }
-    ]
-  },
-  {
-    title: 'ECHO',
-    items: [
-      {
-        id: 'echo.parties',
-        title: 'Parties',
-        icon: <PartiesIcon />,
-        panel: <PartiesPanel />
-      },
-      {
-        id: 'echo.feeds',
-        title: 'Feeds',
-        icon: <FeedsIcon />,
-        panel: <FeedsPanel />
-      },
-      {
-        id: 'echo.items',
-        title: 'Items',
-        icon: <ItemsIcon />,
-        panel: <ItemsPanel />
-      },
-      {
-        id: 'echo.snapshots',
-        title: 'Snapshots',
-        icon: <SnapshotsIcon />,
-        panel: <SnapshotsPanel />
-      }
-    ]
-  },
+  // {
+  //   title: 'CLIENT',
+  //   items: [
+  //     {
+  //       id: 'config',
+  //       title: 'Config',
+  //       icon: <ConfigIcon />,
+  //       panel: <ConfigPanel />
+  //     },
+  //     {
+  //       id: 'storage',
+  //       title: 'Storage',
+  //       icon: <StorageIcon />,
+  //       panel: <StoragePanel />
+  //     }
+  //   ]
+  // },
+  // {
+  //   title: 'HALO',
+  //   items: [
+  //     {
+  //       id: 'halo.profile',
+  //       title: 'Profile',
+  //       icon: <ProfileIcon />,
+  //       panel: <ProfilePanel />
+  //     },
+  //     {
+  //       id: 'halo.keyring',
+  //       title: 'Keyring',
+  //       icon: <KeyIcon />,
+  //       panel: <KeyringPanel />
+  //     },
+  //     {
+  //       id: 'halo.messagess',
+  //       title: 'Messages',
+  //       icon: <MessagesIcon />,
+  //       panel: <CredentialMessagesPanel />
+  //     }
+  //   ]
+  // },
+  // {
+  //   title: 'ECHO',
+  //   items: [
+  //     {
+  //       id: 'echo.parties',
+  //       title: 'Parties',
+  //       icon: <PartiesIcon />,
+  //       panel: <PartiesPanel />
+  //     },
+  //     {
+  //       id: 'echo.feeds',
+  //       title: 'Feeds',
+  //       icon: <FeedsIcon />,
+  //       panel: <FeedsPanel />
+  //     },
+  //     {
+  //       id: 'echo.items',
+  //       title: 'Items',
+  //       icon: <ItemsIcon />,
+  //       panel: <ItemsPanel />
+  //     },
+  //     {
+  //       id: 'echo.snapshots',
+  //       title: 'Snapshots',
+  //       icon: <SnapshotsIcon />,
+  //       panel: <SnapshotsPanel />
+  //     }
+  //   ]
+  // },
   {
     title: 'MESH',
     items: [
@@ -134,7 +134,7 @@ export const sections: Section[] = [
         panel: <SignalPanel />
       }
     ]
-  },
+  }
   // {
   //   title: 'DXNS',
   //   items: [
@@ -146,21 +146,21 @@ export const sections: Section[] = [
   //     }
   //   ]
   // },
-  {
-    title: 'DEBUG',
-    items: [
-      {
-        id: 'debug.logging',
-        title: 'Logging',
-        icon: <LoggingIcon />,
-        panel: <LoggingPanel />
-      },
-      {
-        id: 'rpc',
-        title: 'RPC Trace',
-        icon: <MessagesIcon />,
-        panel: <RpcTracePanel />
-      }
-    ]
-  }
+  // {
+  //   title: 'DEBUG',
+  //   items: [
+  //     {
+  //       id: 'debug.logging',
+  //       title: 'Logging',
+  //       icon: <LoggingIcon />,
+  //       panel: <LoggingPanel />
+  //     },
+  //     {
+  //       id: 'rpc',
+  //       title: 'RPC Trace',
+  //       icon: <MessagesIcon />,
+  //       panel: <RpcTracePanel />
+  //     }
+  //   ]
+  // }
 ];
