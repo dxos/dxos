5eea7e8343b54061b01bf2cf817022c6c9fe1fb6
Josiah Witt <josiah@witt.life>
2022-10-28 16:21:23 -0400

    refactor(halo-app): Factor out useTelemetry hook

--- packages/apps/halo-app/src/App.tsx
+++ packages/apps/halo-app/src/App.tsx
@@ -3,19 +3,17 @@
 //
 
 import { ErrorBoundary } from '@sentry/react';
-import React, { useEffect, useRef } from 'react';
-import { HashRouter, useLocation, useRoutes } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { HashRouter, useRoutes } from 'react-router-dom';
 
 import { Client } from '@dxos/client';
 import { Config, Defaults, Dynamics, Envs } from '@dxos/config';
-import { useAsyncEffect } from '@dxos/react-async';
 import { ClientProvider } from '@dxos/react-client';
 import { UiKitProvider } from '@dxos/react-uikit';
-import * as Sentry from '@dxos/sentry';
-import * as Telemetry from '@dxos/telemetry';
 import { TextModel } from '@dxos/text-model';
 
 import { ErrorsProvider, FatalError } from './components';
+import { useTelemetry } from './hooks';
 import {
   AppLayout,
   AppsPage,
@@ -33,49 +31,10 @@ import {
 } from './pages';
 import translationResources from './translations';
 
-const DX_ENVIRONMENT = process.env.DX_ENVIRONMENT ?? 'development';
-const DX_RELEASE = process.env.DX_RELEASE ?? 'development';
-const SENTRY_DESTINATION = process.env.SENTRY_DSN;
-const TELEMETRY_API_KEY = process.env.SEGMENT_API_KEY;
-
 const configProvider = async () => new Config(await Dynamics(), await Envs(), Defaults());
 
 const Routes = () => {
-  const location = useLocation();
-
-  // TODO(wittjosiah): Store preference for disabling telemetry.
-  //   At minimum should be stored locally (i.e., localstorage), possibly in halo preference.
-  //   Needs to be hooked up to settings page for user visibility.
-
-  useEffect(() => {
-    if (SENTRY_DESTINATION) {
-      Sentry.init({
-        destination: SENTRY_DESTINATION,
-        environment: DX_ENVIRONMENT,
-        release: DX_RELEASE,
-        // TODO(wittjosiah): Configure this.
-        sampleRate: 1.0
-      });
-    }
-
-    Telemetry.init({
-      apiKey: TELEMETRY_API_KEY,
-      enable: Boolean(TELEMETRY_API_KEY)
-    });
-  }, []);
-
-  useAsyncEffect(async () => {
-    // TODO(wittjosiah): Store uuid in halo for the purposes of usage metrics.
-    // await client.halo.getGlobalPreference('dxosTelemetryIdentifier');
-    const identityId = undefined;
-    Telemetry.page({
-      identityId,
-      properties: {
-        environment: DX_ENVIRONMENT,
-        release: DX_RELEASE
-      }
-    });
-  }, [location]);
+  useTelemetry();
 
   return useRoutes([
     {
