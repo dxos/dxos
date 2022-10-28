e62a1ade3ec90b9c51b8284145a251650e318026
Josiah Witt <josiah@witt.life>
2022-10-28 11:25:59 -0400

    fix(halo-app): Wait for dynamic config to resolve in worker (#1761)

--- packages/apps/halo-app/src/main.tsx
+++ packages/apps/halo-app/src/main.tsx
@@ -14,9 +14,8 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
-createRoot(document.getElementById('root')!)
-  .render(
-      // <StrictMode>
-      <App />
-      // </StrictMode>
-  );
+createRoot(document.getElementById('root')!).render(
+  // <StrictMode>
+  <App />
+  // </StrictMode>
+);
