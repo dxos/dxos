6663229781fb9f1ce502c41b0ef5c423d075b8bb
willdx <will@braneframe.com>
2022-10-31 14:59:46 -0700

    Add Tag to react-ui.

---
+++ packages/common/react-ui/src/components/Tag/Tag.tsx
@@ -0,0 +1,24 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import cx from 'classnames';
+import React, { PropsWithChildren, ReactNode } from 'react';
+
+export interface TagProps extends React.ComponentProps<'span'> {
+  children?: ReactNode;
+}
+
+export const Tag = ({ children, ...props }: PropsWithChildren<TagProps>) => {
+  return (
+    <span
+      {...props}
+      className={cx(
+        'bg-neutral-100 text-neutral-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-neutral-700 dark:text-neutral-300',
+        props.className
+      )}
+    >
+      {children}
+    </span>
+  );
+};
