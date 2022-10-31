ef6c77679a6b1f684a52d2062e8eefe43f518775
Josiah Witt <josiah@witt.life>
2022-10-31 08:06:27 -0400

    ci: Debug logs

--- .circleci/scripts/publish.sh
+++ .circleci/scripts/publish.sh
@@ -3,17 +3,22 @@
 BRANCH=$(git branch --show-current)
 ROOT=$(git rev-parse --show-toplevel)
 PACKAGE=${PWD##*/}
+PACKAGE_CAPS=${PACKAGE^^}
+PACKAGE_ENV=${PACKAGE_CAPS//-/_}
 
 if [ $BRANCH = "release" ]; then
   export DX_CONFIG="$ROOT/packages/devtools/dx-cli/config/config.yml"
-  eval export SENTRY_DESTINATION=$"${PACKAGE^^}_SENTRY_DSN"
-  eval export TELEMETRY_API_KEY=$"${PACKAGE^^}_SEGMENT_API_KEY"
+  eval "export SENTRY_DESTINATION=$"${PACKAGE_ENV}_SENTRY_DSN""
+  eval "export TELEMETRY_API_KEY=$"${PACKAGE_ENV}_SEGMENT_API_KEY""
   export DX_ENVIRONMENT=production
 else
   export DX_CONFIG="$ROOT/packages/devtools/dx-cli/config/config-dev.yml"
-  eval export SENTRY_DESTINATION=$"${PACKAGE^^}_DEV_SENTRY_DSN"
-  eval export TELEMETRY_API_KEY=$"${PACKAGE^^}_DEV_SEGMENT_API_KEY"
+  eval "export SENTRY_DESTINATION=$"${PACKAGE_ENV}_DEV_SENTRY_DSN""
+  eval "export TELEMETRY_API_KEY=$"${PACKAGE_ENV}_DEV_SEGMENT_API_KEY""
   export DX_ENVIRONMENT=dev
 fi
+
+echo "Publishing $PACKAGE..."
+echo "Environment: $DX_ENVIRONMENT"
 
 $ROOT/packages/devtools/dx-cli/bin/run app publish --verbose
