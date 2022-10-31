ccc98e266911312ac8813a0c4e2b3bbcdf1811d2
Josiah Witt <josiah@witt.life>
2022-10-31 08:03:39 -0400

    build: Align and hook up env vars to deploy

--- .circleci/scripts/publish.sh
+++ .circleci/scripts/publish.sh
@@ -2,11 +2,18 @@
 
 BRANCH=$(git branch --show-current)
 ROOT=$(git rev-parse --show-toplevel)
+PACKAGE=${PWD##*/}
 
 if [ $BRANCH = "release" ]; then
   export DX_CONFIG="$ROOT/packages/devtools/dx-cli/config/config.yml"
+  eval export SENTRY_DESTINATION=$"${PACKAGE^^}_SENTRY_DSN"
+  eval export TELEMETRY_API_KEY=$"${PACKAGE^^}_SEGMENT_API_KEY"
+  export DX_ENVIRONMENT=production
 else
   export DX_CONFIG="$ROOT/packages/devtools/dx-cli/config/config-dev.yml"
+  eval export SENTRY_DESTINATION=$"${PACKAGE^^}_DEV_SENTRY_DSN"
+  eval export TELEMETRY_API_KEY=$"${PACKAGE^^}_DEV_SEGMENT_API_KEY"
+  export DX_ENVIRONMENT=dev
 fi
 
 $ROOT/packages/devtools/dx-cli/bin/run app publish --verbose
