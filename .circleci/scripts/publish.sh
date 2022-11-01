0438ed77d2126723940e4286003ca9bab5ba6394
Josiah Witt <josiah@witt.life>
2022-11-01 15:25:31 -0400

    ci: Update publish script

--- .circleci/scripts/publish.sh
+++ .circleci/scripts/publish.sh
@@ -5,8 +5,14 @@ ROOT=$(git rev-parse --show-toplevel)
 
 if [ $BRANCH = "release" ]; then
   export DX_CONFIG="$ROOT/packages/devtools/dx-cli/config/config.yml"
+
+  VERSION=$(cat package.json | jq '.version')
+
+  $ROOT/packages/devtools/dx-cli/bin/run app publish \
+    --version $VERSION
+    --skipExisting
+    --verbose
 else
   export DX_CONFIG="$ROOT/packages/devtools/dx-cli/config/config-dev.yml"
+  $ROOT/packages/devtools/dx-cli/bin/run app publish --verbose
 fi
-
-$ROOT/packages/devtools/dx-cli/bin/run app publish --verbose
