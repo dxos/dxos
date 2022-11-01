c72521e60ff8dfea7ee178704ea6ab04bac95413
Rich Burdon <rich.burdon@gmail.com>
2022-10-31 21:44:07 -0400

    feat: Logger with options and config file.

---
+++ packages/common/log/src/context.ts
@@ -0,0 +1,56 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { LogConfig, LogFilter, LogLevel } from './config';
+import { OwnershipScope } from './ownership';
+
+/**
+ * Optional object passed to the logging API.
+ */
+export type LogContext = Record<string, any>;
+
+/**
+ * Generated meta data from source map.
+ */
+export interface LogMetadata {
+  file: string;
+  line: number;
+
+  // TODO(burdon): Document.
+  ownershipScope: OwnershipScope | undefined;
+
+  // Useful for pre-processor hook debugging.
+  bugcheck?: string;
+}
+
+/**
+ * Record for current log line.
+ */
+export interface LogEntry {
+  level: LogLevel;
+  message: string;
+  ctx?: LogContext;
+  meta?: LogMetadata;
+  error?: Error;
+}
+
+/**
+ * Processes (e.g., prints, forwards) log entries.
+ */
+export type LogProcessor = (config: LogConfig, entry: LogEntry) => void;
+
+const matchFilter = (filter: LogFilter, level: LogLevel, path: string) => {
+  return level >= filter.level && (!filter.pattern || path.includes(filter.pattern));
+};
+
+/**
+ * Determines if the current line should be logged (called by the processor).
+ */
+export const shouldLog = (config: LogConfig, level: LogLevel, path: string): boolean => {
+  if (config.filters === undefined) {
+    return true;
+  } else {
+    return config.filters.some((filter) => matchFilter(filter, level, path));
+  }
+};
--- packages/common/log/src/log.ts
+++ packages/common/log/src/log.ts
@@ -2,13 +2,20 @@
 // Copyright 2022 DXOS.org
 //
 
-import { defaultConfig, LogConfig, LogContext, LogLevel, LogMetadata, LogProcessor, LogProcessorType } from './config';
-import { CONSOLE_PROCESSOR, DEBUG_PROCESSOR } from './processors';
+import { LogOptions, LogConfig, LogLevel } from './config';
+import { LogContext, LogMetadata } from './context';
+import { getConfig } from './options';
 
-export type Logger = (message: string, ctx?: LogContext, meta?: LogMetadata) => void;
+/**
+ * Logging function.
+ */
+type Logger = (message: string, ctx?: LogContext, meta?: LogMetadata) => void;
 
-export interface Log extends Logger {
-  config: LogConfig;
+/**
+ * Properties accessible on the logging function.
+ */
+interface Log extends Logger {
+  config: (options: LogOptions) => void;
 
   debug: Logger;
   info: Logger;
@@ -18,26 +25,35 @@ export interface Log extends Logger {
   catch: (error: Error, ctx?: LogContext, meta?: LogMetadata) => void;
 }
 
-export const log: Log = (...params) => processLog(LogLevel.DEBUG, ...params);
+interface LogImp extends Log {
+  _config: LogConfig;
+}
 
-log.config = defaultConfig;
+/**
+ * Global logging function.
+ */
+// TODO(burdon): Instance loggers? (e.g., provide additional displayed logging context/filtering).
+export const log: LogImp = (...params) => processLog(LogLevel.DEBUG, ...params);
+
+log._config = getConfig();
+
+log.config = (options: LogOptions) => {
+  log._config = getConfig(options);
+};
 
 log.debug = (...params) => processLog(LogLevel.DEBUG, ...params);
 log.info = (...params) => processLog(LogLevel.INFO, ...params);
 log.warn = (...params) => processLog(LogLevel.WARN, ...params);
 log.error = (...params) => processLog(LogLevel.ERROR, ...params);
 
-// TODO(burdon): Options for stack or not.
+// TODO(burdon): Option to display/hide stack.
 log.catch = (error: Error, ctx, meta) => processLog(LogLevel.ERROR, String(error.stack), ctx, meta, error);
 
-export const processors: { [index: string]: LogProcessor } = {
-  [LogProcessorType.CONSOLE]: CONSOLE_PROCESSOR,
-  [LogProcessorType.DEBUG]: DEBUG_PROCESSOR
-};
-
+/**
+ * Process the current log call.
+ */
 const processLog = (level: LogLevel, message: string, ctx?: LogContext, meta?: LogMetadata, error?: Error) => {
-  const processor = processors[log.config.processor!] ?? CONSOLE_PROCESSOR;
-  processor(log.config, {
+  log._config.processor(log._config, {
     level,
     message,
     ctx,
