594d6bf7642640b45849a6f3f1ccf2285c67d263
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-11-03 15:14:42 +0200

    Fix race condition in subscribeToSpaces

--- packages/sdk/client-services/src/packlets/devtools/spaces.ts
+++ packages/sdk/client-services/src/packlets/devtools/spaces.ts
@@ -4,7 +4,6 @@
 
 import { Stream } from '@dxos/codec-protobuf';
 import { Space } from '@dxos/echo-db';
-import { log } from '@dxos/log';
 import { SubscribeToPartiesRequest, SubscribeToPartiesResponse } from '@dxos/protocols/proto/dxos/devtools/host';
 import { PartyMetadata } from '@dxos/protocols/proto/dxos/echo/metadata';
 
@@ -12,8 +11,9 @@ import { ServiceContext } from '../services';
 
 export const subscribeToSpaces = (context: ServiceContext, { partyKeys = [] }: SubscribeToPartiesRequest) =>
   new Stream<SubscribeToPartiesResponse>(({ next }) => {
+    let unsubscribe: () => void;
+
     const update = async () => {
-      await context.initialized.wait();
       const parties: Space[] = [...context.spaceManager!.spaces.values()];
       const filteredParties = parties.filter(
         (party) => !partyKeys?.length || partyKeys.some((partyKey) => partyKey.equals(party.key))
@@ -36,14 +36,13 @@ export const subscribeToSpaces = (context: ServiceContext, { partyKeys = [] }: S
       });
     };
 
-    let unsubscribe: () => void;
+    setImmediate(async () => {
+      await context.initialized.wait();
+      unsubscribe = context.spaceManager!.update.on(() => update());
 
-    // Send initial parties.
-    update()
-      .then(() => {
-        unsubscribe = context.spaceManager!.update.on(() => update());
-      })
-      .catch((err) => log.catch(err));
+      // Send initial parties.
+      await update();
+    });
 
     return () => {
       unsubscribe?.();
