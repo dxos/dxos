cda155460f4019d12b445af8bedc850cefa3dea1
Josiah Witt <josiah@witt.life>
2022-11-04 10:37:11 -0400

    feat(cli): Reintroduce

--- .circleci/scripts/publish.sh
+++ .circleci/scripts/publish.sh
@@ -6,14 +6,14 @@ PACKAGE=${PWD##*/}
 PACKAGE_CAPS=${PACKAGE^^}
 PACKAGE_ENV=${PACKAGE_CAPS//-/_}
 
-if [ $BRANCH = "release" ]; then
+if [ $1 = "production" ]; then
   eval "export SENTRY_DESTINATION=$"${PACKAGE_ENV}_SENTRY_DSN""
   eval "export TELEMETRY_API_KEY=$"${PACKAGE_ENV}_SEGMENT_API_KEY""
   export DX_ENVIRONMENT=production
-  DX_CONFIG="$ROOT/packages/devtools/dx-cli/config/config.yml"
+  DX_CONFIG="$ROOT/packages/devtools/cli/config/config.yml"
   VERSION=$(cat package.json | jq '.version')
 
-  $ROOT/packages/devtools/dx-cli/bin/run app publish \
+  pnpm -w nx bin cli app publish app publish \
     --config=$DX_CONFIG \
     --version=$VERSION \
     --skipExisting \
@@ -22,9 +22,9 @@ else
   eval "export SENTRY_DESTINATION=$"${PACKAGE_ENV}_DEV_SENTRY_DSN""
   eval "export TELEMETRY_API_KEY=$"${PACKAGE_ENV}_DEV_SEGMENT_API_KEY""
   export DX_ENVIRONMENT=development
-  DX_CONFIG="$ROOT/packages/devtools/dx-cli/config/config-dev.yml"
+  DX_CONFIG="$ROOT/packages/devtools/cli/config/config-dev.yml"
 
-  $ROOT/packages/devtools/dx-cli/bin/run app publish \
+  pnpm -w nx bin cli app publish app publish \
     --config=$DX_CONFIG \
     --verbose
 fi
