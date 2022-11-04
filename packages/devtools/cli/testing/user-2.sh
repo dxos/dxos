e76402314677b887606a6c724ff673bdd76ee228
Josiah Witt <josiah@witt.life>
2022-11-04 12:04:11 -0400

    chore: Cleanup all references to dx-cli (#1807)

--- packages/devtools/cli/testing/user-2.sh
+++ packages/devtools/cli/testing/user-2.sh
@@ -16,7 +16,7 @@ PUBLIC_KEY=$($dx halo --json | jq --raw-output '.public_key' 2>/dev/null)
 if [ "$PUBLIC_KEY" = 'null' ];
 then
   # TODO(burdon): Error attempt to create space with null halo before creating profile.
-  #   Error [OpenError]: Error parsing JSON in /tmp/dxos/dx-cli/user-1/keystore/data.json: Unexpected end of JSON input
+  #   Error [OpenError]: Error parsing JSON in /tmp/dxos/cli/user-1/keystore/data.json: Unexpected end of JSON input
   $dx reset
   $dx halo create
 fi
