789f5044d505c65d3e6b954e8172d8d5683a6cb3
Josiah Witt <josiah@witt.life>
2022-11-05 11:28:49 -0400

    feat(halo-app): Toggle telemetry

--- packages/apps/halo-app/src/pages/IdentityPage/IdentityPage.tsx
+++ packages/apps/halo-app/src/pages/IdentityPage/IdentityPage.tsx
@@ -2,17 +2,21 @@
 // Copyright 2022 DXOS.org
 //
 
-import { Eraser } from 'phosphor-react';
-import React, { useState } from 'react';
+import { Activity, Eraser } from 'phosphor-react';
+import React, { useMemo, useState } from 'react';
 
 import { useClient, useIdentity } from '@dxos/react-client';
 import { QrCode, useTranslation, Button, getSize, Input, AlertDialog } from '@dxos/react-uikit';
+import * as Telemetry from '@dxos/telemetry';
 import { humanize } from '@dxos/util';
+
+import { BASE_PROPERTIES, getIdentifier } from '../../telemetry';
 
 export const IdentityPage = () => {
   const client = useClient();
   const profile = useIdentity();
   const [displayName, setDisplayName] = useState(profile?.displayName ?? '');
+  const telemetryDisabled = useMemo(() => localStorage.getItem('__TELEMETRY_DISABLED__') === 'true', []);
   const { t } = useTranslation('halo');
 
   const confirmString = humanize(profile!.identityKey.toHex());
@@ -35,6 +39,36 @@ export const IdentityPage = () => {
             onClick={() => client.halo.setGlobalPreference('displayName', displayName)}
           >Update</Button>
         )} */}
+      <AlertDialog
+        title={telemetryDisabled ? t('enable telemetry label') : t('disable telemetry label')}
+        description={telemetryDisabled ? t('enable telemetry description') : t('disable telemetry description')}
+        openTrigger={
+          <Button variant='outline' className='flex gap-1 w-full'>
+            <Activity className={getSize(5)} />
+            {telemetryDisabled ? t('enable telemetry label') : t('disable telemetry label')}
+          </Button>
+        }
+        cancelTrigger={<Button>{t('cancel label', { ns: 'uikit' })}</Button>}
+        confirmTrigger={
+          <Button
+            onClick={() => {
+              Telemetry.event({
+                identityId: getIdentifier(client),
+                name: 'halo-app:telemetry:toggle',
+                properties: {
+                  ...BASE_PROPERTIES,
+                  value: !telemetryDisabled
+                }
+              });
+              localStorage.setItem('__TELEMETRY_DISABLED__', String(!telemetryDisabled));
+              window.location.reload();
+            }}
+            className='text-error-700 dark:text-error-400'
+          >
+            {telemetryDisabled ? t('enable telemetry label') : t('disable telemetry label')}
+          </Button>
+        }
+      />
       <AlertDialog
         title={t('reset device label')}
         openTrigger={
