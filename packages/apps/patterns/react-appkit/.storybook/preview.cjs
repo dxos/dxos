5879e9177e89e907d54c55860d188247f471c15c
Josiah Witt <josiah@witt.life>
2022-11-05 07:19:23 -0400

    feat(react-appkit): Create package (#1788)
    * feat(react-appkit): Create package
    
    * chore(react-appkit): Versions

---
+++ packages/apps/patterns/react-appkit/.storybook/preview.cjs
@@ -0,0 +1,33 @@
+import React, { createElement, useEffect } from 'react';
+import { useDarkMode } from 'storybook-dark-mode';
+import { UiKitProvider } from '../src';
+import { ClientProvider } from '../src/testing';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
+};
+
+const ThemeWrapper = ({ children }) => {
+  // render your custom theme provider
+  const darkMode = useDarkMode();
+  useEffect(() => {
+    document.documentElement.classList[darkMode ? 'add' : 'remove']('dark');
+  }, [darkMode]);
+  return children;
+};
+
+export const decorators = [
+  (Story) => (
+    createElement(UiKitProvider, {
+      children: createElement(ThemeWrapper, {
+        children: createElement(Story)
+      })
+    })
+  )
+];
