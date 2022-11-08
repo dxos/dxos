aa64ae97fe35201e70c74b59e23fd01f37241791
willdx <will@braneframe.com>
2022-11-08 13:45:04 -0800

    Use wrapper hook in JoinSpacePanel.

--- packages/sdk/react-uikit/src/components/JoinSpace/JoinSpacePanel.tsx
+++ packages/sdk/react-uikit/src/components/JoinSpace/JoinSpacePanel.tsx
@@ -2,13 +2,13 @@
 // Copyright 2022 DXOS.org
 //
 
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { InvitationEncoder, PublicKey } from '@dxos/client';
 import { useClient } from '@dxos/react-client';
 
-import { useInvitationStatus, InvitationState } from '../../experimental';
+import { useInvitationStatus, InvitationState, InvitationWrapper, InvitationStatus } from '../../experimental';
 import { SingleInputStep } from '../SingleInputStep';
 
 export interface JoinSpacePanelProps {
@@ -26,16 +26,25 @@ export const JoinSpacePanel = ({
   const { t } = useTranslation();
   const client = useClient();
   const [invitationCode, setInvitationCode] = useState(initialInvitationCode ?? '');
+  const [invitationSecret, setInvitationSecret] = useState(initialInvitationCode ?? '');
 
-  const { status, cancel, error, connect, result } = useInvitationStatus();
+  const { status, cancel, error, connect, result, haltedAt } = useInvitationStatus();
 
-  const redeemInvitation = useCallback(() => {
-    connect(client.halo.acceptInvitation(InvitationEncoder.decode(parseInvitation(invitationCode))));
+  const onConnectNext = useCallback(() => {
+    connect(
+      client.echo.acceptInvitation(
+        InvitationEncoder.decode(parseInvitation(invitationCode))
+      ) as unknown as InvitationWrapper
+    );
   }, [invitationCode]);
+
+  const onAuthenticateNext = useCallback(() => {
+    console.warn('[onAuthenticateNext]', 'not implemented', invitationSecret);
+  }, [invitationSecret]);
 
   useEffect(() => {
     if (initialInvitationCode) {
-      void redeemInvitation();
+      void onConnectNext();
     }
   }, []);
 
@@ -45,25 +54,51 @@ export const JoinSpacePanel = ({
     }
   }, [status, result]);
 
-  return (
-    <SingleInputStep
-      {...{
-        pending: status === InvitationState.CONNECTING || status === InvitationState.AUTHENTICATING,
-        inputLabel: t('invitation code label'),
-        inputPlaceholder: t('invitation code placeholder'),
-        inputProps: {
-          initialValue: invitationCode
-        },
-        onChange: setInvitationCode,
-        onNext: redeemInvitation,
-        onCancelPending: cancel,
-        ...(error && {
+  const cursor =
+    status === InvitationState.ERROR || status === InvitationState.TIMEOUT || status === InvitationState.CANCELLED
+      ? haltedAt!
+      : status;
+
+  const stepProps = useMemo(() => {
+    return cursor < InvitationState.CONNECTED
+      ? {
+          pending: status === InvitationState.CONNECTING,
+          inputLabel: t('invitation code label', { ns: 'uikit' }),
+          inputPlaceholder: t('invitation code placeholder', { ns: 'uikit' }),
           inputProps: {
-            validationMessage: error,
-            validationValence: 'error'
-          }
-        })
-      }}
-    />
+            initialValue: invitationCode,
+            autoFocus: true,
+            ...(error && {
+              validationMessage: `Untranslated error code: ${error}`, // todo: provide usable error message
+              validationValence: 'error' as const
+            })
+          },
+          onChange: setInvitationCode,
+          onNext: onConnectNext,
+          onCancelPending: cancel
+        }
+      : {
+          pending: status === InvitationState.AUTHENTICATING,
+          inputLabel: t('invitation secret label', { ns: 'uikit' }),
+          inputPlaceholder: t('invitation secret placeholder', { ns: 'uikit' }),
+          inputProps: {
+            initialValue: '',
+            autoFocus: true,
+            ...(error && {
+              validationMessage: `Untranslated error code: ${error}`, // todo: provide usable error message
+              validationValence: 'error' as const
+            })
+          },
+          onChange: setInvitationSecret,
+          onNext: onAuthenticateNext,
+          onCancelPending: cancel
+        };
+  }, [cursor, error, onAuthenticateNext, invitationSecret, invitationCode, onConnectNext, status, result, t]);
+
+  return (
+    <>
+      <InvitationStatus {...{ status, haltedAt }} className='mbs-3' />
+      <SingleInputStep {...stepProps} />
+    </>
   );
 };
