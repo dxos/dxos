fb514f366075a1efbfe2bd88f50315392739e248
Dmytro Maretskyi <maretskii@gmail.com>
2022-11-08 11:58:21 +0100

    Logging for shared worker

--- packages/apps/halo-app/src/shared-worker.ts
+++ packages/apps/halo-app/src/shared-worker.ts
@@ -4,16 +4,26 @@
 
 import { Config, Defaults, Dynamics } from '@dxos/config';
 import { PortMuxer } from '@dxos/rpc-tunnel';
+import { log } from '@dxos/log'
 
 import { WorkerRuntime } from './worker/worker-runtime';
 
+log.config({ filter: 'info' })
+
 const workerRuntime = new WorkerRuntime(async () => new Config(await Dynamics(), Defaults()));
 
-void workerRuntime.start().catch((err) => {
-  console.error(err);
-});
+const start = Date.now();
+void workerRuntime.start().then(
+  () => {
+    log.info('worker ready', { initTimeMs: Date.now() - start });
+  },
+  (err) => {
+    log.catch(err);
+  } 
+);
 
 onconnect = async (event) => {
+  log.info('onconnect', { event });
   const muxer = new PortMuxer(event.ports[0]);
 
   await workerRuntime.createSession({
