4fa459249106ef1b4601a06ee6c53026729489d3
Rich Burdon <rich.burdon@gmail.com>
2022-11-09 15:00:42 -0500

    feat: change invitations to request/response initiated from the guest. (#1832)
    * feat: integrate new invitations with main.
    
    * Use service metadata for the lazy loaded service
    
    * Pnpm lock update
    
    * fix(halo-app): Integrate with new invitations (#1822)
    
    * test(client): Add test party invitations between two peers
    
    * fix: Trying to get halo-app to build
    
    * Add experimental component and hook for invitation status. Refactor broken invitation handlers to use experimental hook.
    
    * Fix merge issues in useInvitationStatus.
    
    * Fix result handling.
    
    * fix(halo-app): Remove usage of client mode
    
    * Fix prop.
    
    * fix(hello-template): Builds
    
    * fix(halo-app): Starts up (but remote client fails to initialize)
    
    * Fix logger detection in shared worker
    
    * Logger types
    
    * Fix logger detection for shared workers (again)
    
    * Logging for shared worker
    
    * Fix types
    
    * Increase timeout
    
    * Remove lingering common/react-uikit.
    
    * Update `useInvitationStatus` to support wrapper.
    
    * Use wrapper hook in JoinSpacePanel.
    
    * Use wrapper hook in JoinIdentityPage.
    
    * Encourage re-mount of SingleInputStep by separating closures.
    
    * Assume invitation wrappers for SpaceSettingsPage.
    
    * Assume invitation wrappers for DevicesPage.
    
    * Invitation lists support async create method.
    
    * Update lockfile
    
    * Use new invitations list values.
    
    * Use promise when creating space invitation.
    
    * Fix build.
    
    * Fix import.
    
    * fix: Consistent use of ObservableInvitation and build fixes
    
    * fix: devtools build
    
    * fix: Make kitchen sink build
    
    * test: Fix client invitation test
    
    * chore: Lint
    
    Co-authored-by: willdx <will@braneframe.com>
    Co-authored-by: Dmytro Maretskyi <maretskii@gmail.com>
    
    * feat: Make invitation flow request/response initiated by guest.
    
    Co-authored-by: Dmytro Maretskyi <maretskii@gmail.com>
    Co-authored-by: Mykola Veremchuk <veremchukmukola@gmail.com>
    Co-authored-by: Josiah Witt <josiah@witt.life>
    Co-authored-by: willdx <will@braneframe.com>

--- packages/sdk/client-services/src/packlets/invitations/space-invitations-service.test.ts
+++ packages/sdk/client-services/src/packlets/invitations/space-invitations-service.test.ts
@@ -26,11 +26,19 @@ describe('services/space-invitation-service', function () {
 
     assert(peer1.spaceManager);
     assert(peer1.spaceInvitations);
-    const service1: InvitationsService = new SpaceInvitationsServiceImpl(peer1.spaceManager, peer1.spaceInvitations);
+    const service1: InvitationsService = new SpaceInvitationsServiceImpl(
+      peer1.identityManager,
+      peer1.spaceManager,
+      peer1.spaceInvitations
+    );
 
     assert(peer2.spaceManager);
     assert(peer2.spaceInvitations);
-    const service2: InvitationsService = new SpaceInvitationsServiceImpl(peer2.spaceManager, peer2.spaceInvitations);
+    const service2: InvitationsService = new SpaceInvitationsServiceImpl(
+      peer2.identityManager,
+      peer2.spaceManager,
+      peer2.spaceInvitations
+    );
 
     const space1 = await peer1.spaceManager.createSpace();
 
@@ -74,11 +82,19 @@ describe('services/space-invitation-service', function () {
 
     assert(peer1.spaceManager);
     assert(peer1.spaceInvitations);
-    const service1: InvitationsService = new SpaceInvitationsServiceImpl(peer1.spaceManager, peer1.spaceInvitations);
+    const service1: InvitationsService = new SpaceInvitationsServiceImpl(
+      peer1.identityManager,
+      peer1.spaceManager,
+      peer1.spaceInvitations
+    );
 
     assert(peer2.spaceManager);
     assert(peer2.spaceInvitations);
-    const service2: InvitationsService = new SpaceInvitationsServiceImpl(peer2.spaceManager, peer2.spaceInvitations);
+    const service2: InvitationsService = new SpaceInvitationsServiceImpl(
+      peer2.identityManager,
+      peer2.spaceManager,
+      peer2.spaceInvitations
+    );
 
     const space1 = await peer1.spaceManager.createSpace();
     const cancelled = new Trigger();
