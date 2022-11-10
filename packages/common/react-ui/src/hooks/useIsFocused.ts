aa2f0927dcf52cb00f271c189ccb95011e9bf91c
Will Shown <thure@users.noreply.github.com>
2022-11-10 08:43:17 -0800

    feat: PIN input component (#1834)

---
+++ packages/common/react-ui/src/hooks/useIsFocused.ts
@@ -0,0 +1,38 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+// Based upon the useIsFocused hook which is part of the `rci` project:
+/// https://github.com/leonardodino/rci/blob/main/packages/use-is-focused
+
+import { useEffect, useRef, useState, RefObject } from 'react';
+
+export const useIsFocused = (inputRef: RefObject<HTMLInputElement>) => {
+  const [isFocused, setIsFocused] = useState<boolean | undefined>(undefined);
+  const isFocusedRef = useRef<boolean | undefined>(isFocused);
+
+  isFocusedRef.current = isFocused;
+
+  useEffect(() => {
+    const input = inputRef.current;
+    if (!input) {
+      return;
+    }
+
+    const onFocus = () => setIsFocused(true);
+    const onBlur = () => setIsFocused(false);
+    input.addEventListener('focus', onFocus);
+    input.addEventListener('blur', onBlur);
+
+    if (isFocusedRef.current === undefined) {
+      setIsFocused(document.activeElement === input);
+    }
+
+    return () => {
+      input.removeEventListener('focus', onFocus);
+      input.removeEventListener('blur', onBlur);
+    };
+  }, [inputRef, setIsFocused]);
+
+  return isFocused;
+};
