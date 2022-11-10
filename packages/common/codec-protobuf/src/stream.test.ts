df2fa3991741ce6d891fba83ac5d94b2446eec78
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2022-11-10 14:26:36 +0100

    feat: Context (#1836)
    * Add new package
    
    * First iteration with basic tests
    
    * Async task wrappers in context
    
    * Add task-scheduling tests
    
    * Integrate context into events
    
    * Integrate context into stream
    
    * Lint

--- packages/common/codec-protobuf/src/stream.test.ts
+++ packages/common/codec-protobuf/src/stream.test.ts
@@ -70,6 +70,33 @@ describe('Stream', function () {
     nextCb('second');
     expect(received).to.deep.equal(['first', 'second']);
   });
+
+  it('closing stream disposes the context', function () {
+    let disposed = false;
+    const stream = new Stream<string>(({ ctx }) => {
+      ctx.onDispose(() => {
+        disposed = true;
+      });
+    });
+    expect(disposed).to.be.false;
+    stream.close();
+    expect(disposed).to.be.true;
+  });
+
+  it('thrown errors are caught be context', function () {
+    const stream = new Stream<string>(({ ctx }) => {
+      throw new Error('test');
+    });
+
+    let error!: Error;
+    stream.subscribe(
+      () => {},
+      (err) => {
+        error = err!;
+      }
+    );
+    expect(error.message).to.equal('test');
+  });
 });
 
 // To not introduce a dependency on @dxos/async.
