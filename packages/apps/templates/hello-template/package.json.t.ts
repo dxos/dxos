203ec897a8f67b48ebf9ef17a116c54a2c6c8034
Josiah Witt <josiah@witt.life>
2022-11-11 13:31:08 -0500

    fix: realign app templates (#1856)

--- packages/apps/templates/hello-template/package.json.t.ts
+++ packages/apps/templates/hello-template/package.json.t.ts
@@ -17,12 +17,16 @@ const template: TemplateFunction<Input> = ({ input }) => /* javascript */ `{
   "version": "${packageJson.version}",
   "private": true,
   "description": "DXOS 'hello world' application template.",
+  ${input.monorepo ? '' : `
+  "homepage": "https://dxos.org",
+  "bugs": "https://github.com/dxos/dxos/issues",
+  "repository": "github:dxos/dxos",
   "license": "MIT",
   "author": "DXOS.org",
-  "repository": "github:dxos/dxos",
-  "scripts": {
+  `}"scripts": {
     "build": "tsc",
     "bundle": "vite build",
+    "deploy": "dx app publish",
     "preview": "vite preview",
     "serve": "vite",
     "storybook": "start-storybook -p 9009 --no-open"
@@ -30,17 +34,23 @@ const template: TemplateFunction<Input> = ({ input }) => /* javascript */ `{
   "dependencies": {
     "@dxos/client": "${input.monorepo ? 'workspace:*' : packageJson.version}",
     "@dxos/config": "${input.monorepo ? 'workspace:*' : packageJson.version}",
+    "@dxos/react-appkit": "${input.monorepo ? 'workspace:*' : packageJson.version}",
     "@dxos/react-client": "${input.monorepo ? 'workspace:*' : packageJson.version}",
-    "@dxos/react-components": "${input.monorepo ? 'workspace:*' : packageJson.version}",
-    "@dxos/react-toolkit": "${input.monorepo ? 'workspace:*' : packageJson.version}",
-    "@mui/icons-material": "^5.8.0",
-    "@mui/material": "^5.8.1",
-    "react": "^17.0.2",
-    "react-dom": "^17.0.2"
+    "@dxos/react-composer": "${input.monorepo ? 'workspace:*' : packageJson.version}",
+    "@dxos/text-model": "${input.monorepo ? 'workspace:*' : packageJson.version}",
+    "@dxos/react-ui": "${input.monorepo ? 'workspace:*' : packageJson.version}",
+    "@dxos/react-uikit": "${input.monorepo ? 'workspace:*' : packageJson.version}",
+    "@dxos/util": "${input.monorepo ? 'workspace:*' : packageJson.version}",
+    "classnames": "^2.3.2",
+    "phosphor-react": "^1.4.1",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-router-dom": "^6.3.0",
+    "url-join": "^5.0.0"
   },
   "devDependencies": {
     "@babel/core": "^7.18.13",
-    "@dxos/react-client-testing": "${input.monorepo ? 'workspace:*' : packageJson.version}",
+    "@dxos/cli": "${input.monorepo ? 'workspace:*' : packageJson.version}",
     "@dxos/vite-plugin": "${input.monorepo ? 'workspace:*' : packageJson.version}",
     "@storybook/addon-actions": "^6.5.10",
     "@storybook/addon-essentials": "^6.5.10",
@@ -50,14 +60,16 @@ const template: TemplateFunction<Input> = ({ input }) => /* javascript */ `{
     "@storybook/mdx2-csf": "^0.0.3",
     "@storybook/react": "^6.5.10",
     "@storybook/testing-library": "^0.0.13",
-    "@types/react": "^17.0.24",
-    "@types/react-dom": "^17.0.9",
+    "@types/react": "^18.0.21",
+    "@types/react-dom": "^18.0.6",
     "@vitejs/plugin-react": "^2.0.1",
     "require-from-string": "^2.0.2",
-    "typescript": "^4.7.2",
+    "storybook-dark-mode": "^1.1.2",
+    "typescript": "^4.8.4",
     "vite": "3.0.9",
     "vite-plugin-pwa": "^0.12.4",
-    "webpack": "^5.74.0"
+    "webpack": "^5.74.0",
+    "workbox-window": "^6.5.4"
   }${input.monorepo ? '' : `,
   "pnpm": {
     "patchedDependencies": {
