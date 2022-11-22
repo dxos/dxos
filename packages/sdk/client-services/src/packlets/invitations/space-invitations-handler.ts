8fa34b0a9c696a1427cfc117c2afe6253ae829bc
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2022-11-22 10:55:17 +0100

    feat: Identity display name (#1945)

--- packages/sdk/client-services/src/packlets/invitations/space-invitations-handler.ts
+++ packages/sdk/client-services/src/packlets/invitations/space-invitations-handler.ts
@@ -14,6 +14,7 @@ import { createProtocolFactory, NetworkManager, StarTopology, SwarmConnection } 
 import { createRpcPlugin, RpcPlugin } from '@dxos/protocol-plugin-rpc';
 import { schema } from '@dxos/protocols';
 import { Invitation } from '@dxos/protocols/proto/dxos/client/services';
+import { ProfileDocument } from '@dxos/protocols/proto/dxos/halo/credentials';
 import { createProtoRpcPeer } from '@dxos/rpc';
 
 import {
@@ -64,19 +65,25 @@ export class SpaceInvitationsHandler extends AbstractInvitationsHandler<Space> {
     let authenticationCode: string;
     const complete = new Trigger<PublicKey>();
     const plugin = new RpcPlugin(async (port) => {
+      let guestProfile: ProfileDocument | undefined;
+
       const peer = createProtoRpcPeer({
         exposed: {
           SpaceHostService: schema.getService('dxos.halo.invitations.SpaceHostService')
         },
         handlers: {
           SpaceHostService: {
-            requestAdmission: async () => {
+            requestAdmission: async ({ profile }) => {
               log('responding with admission offer', {
+                guestProfile: profile,
                 host: this._signingContext.deviceKey,
                 spaceKey: space.key
               });
 
+              guestProfile = profile;
+
               // TODO(burdon): Is this the right place to set this state?
+              // TODO(dmaretskyi): Should we expose guest's profile in this callback?
               observable.callback.onAuthenticating?.(invitation);
               return {
                 spaceKey: space.key,
@@ -111,7 +118,8 @@ export class SpaceInvitationsHandler extends AbstractInvitationsHandler<Space> {
                     deviceKey,
                     space.key,
                     controlFeedKey,
-                    dataFeedKey
+                    dataFeedKey,
+                    guestProfile
                   )
                 );
 
@@ -206,7 +214,9 @@ export class SpaceInvitationsHandler extends AbstractInvitationsHandler<Space> {
 
         // 1. Send request.
         log('sending admission request', { guest: this._signingContext.deviceKey });
-        const { spaceKey, genesisFeedKey } = await peer.rpc.SpaceHostService.requestAdmission();
+        const { spaceKey, genesisFeedKey } = await peer.rpc.SpaceHostService.requestAdmission({
+          profile: this._signingContext.profile
+        });
 
         // 2. Get authentication code.
         // TODO(burdon): Test timeout (options for timeouts at different steps).
