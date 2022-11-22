f2f557dc8acd0d4c5f935419b762174e28b89845
Dmytro Maretskyi <maretskii@gmail.com>
2022-11-22 16:06:51 +0100

    Logger

--- packages/common/log/src/scope.ts
+++ packages/common/log/src/scope.ts
@@ -4,25 +4,35 @@
 
 const logInfoProperties = Symbol('logInfoProperties');
 
+/**
+ * Decorate fields, properties, or methods to automatically include their values in log messages.
+ * 
+ * Example:
+ * 
+ * ```typescript
+ * @logInfo
+ * peerId: PublicKey;
+ * ```
+ */
 export const logInfo = (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
   // console.log(target, propertyKey, descriptor);
   (target[logInfoProperties] ??= []).push(propertyKey);
 };
 
+/**
+ * Introspects class instance to find decorated metadata.
+ * @param scope Class instance.
+ */
 export const gatherLogInfoFromScope = (scope: any): Record<string, any> => {
   const res: Record<string, any> = {};
 
   const prototype = Object.getPrototypeOf(scope);
   const infoProps = prototype[logInfoProperties] ?? [];
   for (const prop of infoProps) {
-    if (typeof scope[prop] === 'function') {
-      try {
-        res[prop] = scope[prop]();
-      } catch (err: any) {
-        res[prop] = err.message;
-      }
-    } else {
-      res[prop] = scope[prop];
+    try {
+      res[prop] = typeof scope[prop] === 'function' ? scope[prop]() : scope[prop];
+    } catch (err: any) {
+      res[prop] = err.message;
     }
   }
 
