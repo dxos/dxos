84cbe5acf6ea07a299fbbe8b7987fdcb102fedd3
Rich Burdon <rich.burdon@gmail.com>
2022-11-23 13:45:12 -0500

    Export tests without polluting imports.

---
+++ packages/common/feed-store/testing.d.ts
@@ -0,0 +1,11 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+/**
+ * Enable testing imports:
+ * ```tsc
+ * import { TestBuilder } from '@dxos/feed-store/testing';
+ * ```
+ */
+export * from './dist/src/testing';
