4993826abacbe661919d89b04a3c41cfe449bf07
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-11-25 17:19:23 +0200

    Fix WebRTCTransport

--- packages/core/mesh/network-manager/src/transport/webrtc-transport.ts
+++ packages/core/mesh/network-manager/src/transport/webrtc-transport.ts
@@ -40,7 +40,7 @@ export class WebRTCTransport implements Transport {
 
     this._peer.on('signal', async (data) => {
       log('signal', data);
-      await this.params.sendSignal({ json: data });
+      await this.params.sendSignal({ json: { data } });
     });
 
     this._peer.on('connect', () => {
@@ -71,8 +71,8 @@ export class WebRTCTransport implements Transport {
 
   signal(signal: Signal) {
     assert(this._peer, 'Connection not ready to accept signals.');
-    assert(signal.json, 'Signal message must contain signal data.');
-    this._peer.signal(signal.json);
+    assert(signal.json.data, 'Signal message must contain signal data.');
+    this._peer.signal(signal.json.data);
   }
 
   private async _disconnectStreams() {
