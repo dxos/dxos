6da0161d29465de515cac74f79ac1c0798365146
Will Shown <thure@users.noreply.github.com>
2022-11-25 11:00:33 -0500

    fix: Demo feedback responses 3 (#1968)

--- packages/apps/patterns/react-list/src/components/List/List.tsx
+++ packages/apps/patterns/react-list/src/components/List/List.tsx
@@ -88,7 +88,7 @@ const ListLoaded = ({ space, list, listItems: propsListItems }: ListLoadedProps)
             };
             return acc;
           }, {}),
-        onChangePeriod: 300,
+        onChangePeriod: 640,
         onAction,
         createListItemId
       }}
