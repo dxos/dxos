d2fc3ac5d5fc5ccf8b593ab4bae442ae427a1eaa
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-11-28 12:59:49 +0200

    Use switch case

--- packages/common/codec-protobuf/src/substitutions/struct.ts
+++ packages/common/codec-protobuf/src/substitutions/struct.ts
@@ -5,27 +5,39 @@
 export type Struct = Record<string, any>;
 
 export const encodeStructValue = (structValue: any): any => {
-  const encoders: Record<string, (v: any) => any> = {
-    undefined: () => ({ nullValue: 0 }),
-    number: (v: number) => ({ numberValue: v }),
-    string: (v: string) => ({ stringValue: v }),
-    boolean: (v: boolean) => ({ boolValue: v }),
-    object: (v: any) => {
+  const valueType = typeof structValue;
+  switch (valueType) {
+    case 'undefined': {
+      return { nullValue: 0 };
+      break;
+    }
+    case 'number': {
+      return { numberValue: structValue };
+      break;
+    }
+    case 'string': {
+      return { stringValue: structValue };
+      break;
+    }
+    case 'boolean': {
+      return { boolValue: structValue };
+      break;
+    }
+    case 'object': {
       // null, Array, Object will have typeof 'object'
-      if (v === null) {
+      if (structValue === null) {
         return { nullValue: 0 };
       }
-      if (Array.isArray(v)) {
-        return { listValue: { values: v.map(encodeStructValue) } };
+      if (Array.isArray(structValue)) {
+        return { listValue: { values: structValue.map(encodeStructValue) } };
       }
-      return { structValue: encodeStruct(v) };
+      return { structValue: encodeStruct(structValue) };
+      break;
     }
-  };
-  const valueType = typeof structValue;
-  if (valueType in encoders) {
-    return encoders[valueType](structValue);
-  } else {
-    throw new Error(`Unsupported type: ${valueType}`);
+    default: {
+      throw new Error(`Unsupported type: ${valueType}`);
+      break;
+    }
   }
 };
 
@@ -37,19 +49,35 @@ export const encodeStruct = (struct: Struct): any => ({
 });
 
 export const decodeStructValue = (value: any): any => {
-  const decoders: Record<string, (v: any) => any> = {
-    nullValue: () => null,
-    numberValue: (v: number) => v,
-    stringValue: (v: string) => v,
-    boolValue: (v: boolean) => v,
-    structValue: (v: Struct) => decodeStruct(v),
-    listValue: (v: { values: any[] }) => v.values.map(decodeStructValue)
-  };
-  const valueType = Object.keys(value)[0];
-  if (valueType in decoders) {
-    return decoders[valueType](value[valueType]);
-  } else {
-    throw new Error(`Unsupported type: ${valueType}`);
+  const [key, v] = Object.entries(value)[0];
+  switch (key) {
+    case 'nullValue': {
+      return null;
+      break;
+    }
+    case 'numberValue': {
+      return v;
+      break;
+    }
+    case 'stringValue': {
+      return v;
+      break;
+    }
+    case 'boolValue': {
+      return v;
+      break;
+    }
+    case 'structValue': {
+      return decodeStruct(v);
+      break;
+    }
+    case 'listValue': {
+      return v.values.map(decodeStructValue);
+      break;
+    }
+    default:
+      throw new Error(`Unsupported type: ${valueType}`);
+      break;
   }
 };
 
