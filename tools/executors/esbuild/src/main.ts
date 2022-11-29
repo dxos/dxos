528ccc6bc9ba92733ba72ba0dde599044b6d4c7c
Josiah Witt <josiah@witt.life>
2022-11-29 10:47:14 -0500

    build: Include meta build output

--- tools/executors/esbuild/src/main.ts
+++ tools/executors/esbuild/src/main.ts
@@ -5,7 +5,7 @@
 import type { ExecutorContext } from '@nrwl/devkit';
 import { build, Platform } from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import { join } from 'path';
 import * as ts from 'typescript';
 
@@ -15,6 +15,7 @@ export interface EsbuildExecutorOptions {
   bundle: boolean;
   bundlePackages: string[];
   entryPoints: string[];
+  metafile: boolean;
   outputPath: string;
   platforms: Platform[];
 }
@@ -27,18 +28,20 @@ export default async (options: EsbuildExecutorOptions, context: ExecutorContext)
 
   const packagePath = join(context.workspace.projects[context.projectName!].root, 'package.json');
 
-  const results = await Promise.all(
-    options.platforms.map((platform) => {
+  const errors = await Promise.all(
+    options.platforms.map(async (platform) => {
       const outdir = options.entryPoints.length > 1 ? `${options.outputPath}/${platform}` : undefined;
       const outfile = options.entryPoints.length <= 1 ? `${options.outputPath}/${platform}.js` : undefined;
+      const metafile =
+        options.entryPoints.length > 1 ? `${outdir}/meta.json` : `${options.outputPath}/${platform}.meta.json`;
 
-
-      return build({
+      const result = await build({
         entryPoints: options.entryPoints,
         outdir,
         outfile,
         format: 'cjs',
         write: true,
+        metafile: options.metafile,
         bundle: options.bundle,
         platform,
         plugins: [
@@ -49,7 +52,8 @@ export default async (options: EsbuildExecutorOptions, context: ExecutorContext)
           {
             name: 'log-transform',
             setup: ({ onLoad, onEnd }) => {
-              let files = 0, time = 0;
+              let files = 0;
+              let time = 0;
 
               onLoad({ filter: /\.ts/ }, async (args) => {
                 const source = await readFile(args.path, 'utf8');
@@ -74,19 +78,23 @@ export default async (options: EsbuildExecutorOptions, context: ExecutorContext)
                 };
               });
 
-              onEnd(() => {
-                if(false) {
-                  console.log(`Log preprocessing took ${time}ms for ${files} files (${(time / files).toFixed(0)} ms/file).`);
-                }
-              })
+              if (context.isVerbose) {
+                onEnd(() => {
+                  console.log(
+                    `Log preprocessing took ${time}ms for ${files} files (${(time / files).toFixed(0)} ms/file).`
+                  );
+                });
+              }
             }
           }
         ]
       });
+
+      await writeFile(metafile, JSON.stringify(result.metafile), 'utf-8');
+
+      return result.errors;
     })
   );
 
-  const errors = results.map((result) => result.errors).flat();
-
-  return { success: errors.length === 0 };
+  return { success: errors.flat().length === 0 };
 };
