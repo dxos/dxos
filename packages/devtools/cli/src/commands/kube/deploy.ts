4577832268e049cc0bea2b975dabf2087ec65606
egorgripasov <gripasovegor@gmail.com>
2022-12-01 18:19:45 -0500

    feat: start of kube cli extension.

---
+++ packages/devtools/cli/src/commands/kube/deploy.ts
@@ -0,0 +1,62 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { Flags } from '@oclif/core';
+
+import { Client } from '@dxos/client';
+
+import { BaseCommand } from '../../base-command';
+import { DEFAULT_PROVIDER, DigitalOceanProvider, Provider, printKubes } from '../../util/provider';
+
+export default class Deploy extends BaseCommand {
+  static override description = 'Deploy KUBE.';
+  static override flags = {
+    ...BaseCommand.flags,
+    hostname: Flags.string({
+      description: 'Hostname',
+      required: true
+    }),
+    provider: Flags.string({
+      description: 'Cloud Provider',
+      default: DEFAULT_PROVIDER
+    }),
+    dev: Flags.boolean({
+      description: 'Deploy latest version from dev channel',
+      default: false
+    })
+  };
+
+  async run(): Promise<any> {
+    const { flags } = await this.parse(Deploy);
+    const { hostname, provider: providerName, dev } = flags;
+
+    try {
+      return await this.execWithClient(async (client: Client) => {
+        if (!client.halo.profile) {
+          this.log('Halo profile should be initialized first.');
+        }
+        // TODO(egorgripasov): HALO <-> KUBE integration.
+        let provider: Provider;
+        switch (providerName) {
+          case DEFAULT_PROVIDER: {
+            provider = new DigitalOceanProvider(this.clientConfig!);
+            break;
+          }
+          default: {
+            throw new Error(`Unknown provider: ${providerName}`);
+          }
+        }
+
+        const kube = await provider.deploy({
+          hostname,
+          dev
+        });
+
+        printKubes([kube]);
+      });
+    } catch (err: any) {
+      this.error(err, { exit: 1 });
+    }
+  }
+}
---
+++ packages/devtools/cli/src/util/provider/provider.ts
@@ -0,0 +1,54 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { CliUx } from '@oclif/core';
+
+export const KUBE_TAG = 'kube';
+
+export const DEFAULT_PROVIDER = 'digitalocean';
+
+export interface KubeDeployOptions {
+  hostname: string;
+  region?: string;
+  memory?: number;
+  dev: boolean;
+}
+
+export type KUBE = {
+  hostname: string;
+  createdAt: string;
+  ipAddress: string;
+};
+
+export interface Provider {
+  /**
+   * Deploy KUBE to a Provider.
+   */
+  deploy(options: KubeDeployOptions): Promise<KUBE>;
+  getSshKeys(): Promise<any>;
+}
+
+export const mapKubes = (kubes: KUBE[]) => {
+  return kubes.map((kube) => ({
+    key: kube.hostname,
+    ipAddress: kube.ipAddress
+  }));
+};
+
+export const printKubes = (kubes: KUBE[], flags = {}) => {
+  CliUx.ux.table(
+    mapKubes(kubes),
+    {
+      key: {
+        header: 'Hostname'
+      },
+      ipAddress: {
+        header: 'IP Address'
+      }
+    },
+    {
+      ...flags
+    }
+  );
+};
