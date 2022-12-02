0f56e17e947af200fac7028504c1a9da18b393fa
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-02 17:00:28 +0200

    Add reannounce test

--- packages/core/mesh/teleport-plugin-presence/src/presence-extension.test.ts
+++ packages/core/mesh/teleport-plugin-presence/src/presence-extension.test.ts
@@ -5,7 +5,9 @@
 import expect from 'expect';
 import waitForExpect from 'wait-for-expect';
 
+import { latch } from '@dxos/async';
 import { PublicKey } from '@dxos/keys';
+import { PeerState } from '@dxos/protocols/proto/dxos/mesh/teleport/presence';
 import { describe, test } from '@dxos/test';
 
 import { createPresencePair } from './testing';
@@ -20,5 +22,26 @@ describe('PresenceExtension', () => {
     });
     await waitForExpect(() => expect(presence1.getPeerStates()[0].peerId).toEqual(peerId2));
     await waitForExpect(() => expect(presence2.getPeerStates()[0].peerId).toEqual(peerId1));
+  });
+
+  test('Peer reannounces itself by interval', async () => {
+    const peerId1 = PublicKey.random();
+    const peerId2 = PublicKey.random();
+    const { presence1 } = await createPresencePair({
+      peerId1,
+      peerId2
+    });
+    let oldState: PeerState;
+    const [announcedTwice, inc] = latch({ count: 2 });
+    presence1.updatePeerStates.on((peerStates) => {
+      expect(peerStates[0].peerId).toEqual(peerId2);
+      if (!oldState) {
+        oldState = peerStates[0];
+      } else {
+        expect(peerStates[0].timestamp.getTime()).toBeGreaterThan(oldState.timestamp.getTime());
+      }
+      inc();
+    });
+    await announcedTwice();
   });
 });
