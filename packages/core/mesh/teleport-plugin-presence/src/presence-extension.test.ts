4bdfe327063efb59a6f45dc91aebf32853cc9866
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-02 12:57:14 +0200

    Init commit

---
+++ packages/core/mesh/teleport-plugin-presence/src/presence-extension.test.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import expect from 'expect';
+
+import { Event, sleep } from '@dxos/async';
+import { describe, test } from '@dxos/test';
+
+describe('PresenceExtension', () => {
+  test('first test', async () => {});
+});
---
+++ packages/core/mesh/teleport-plugin-presence/src/presence-extension.ts
@@ -0,0 +1,10 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import assert from 'assert';
+
+/**
+ * Manages replication between a set of feeds for a single teleport session.
+ */
+export class ReplicatorExtension implements TeleportExtension {}
