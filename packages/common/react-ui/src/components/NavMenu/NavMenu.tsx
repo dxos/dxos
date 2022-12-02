41ebd810697345f0042b70c027d373961b09f7ea
willdx <will@braneframe.com>
2022-12-01 16:31:42 -0800

    Refactor NavMenu to use slots.

--- packages/common/react-ui/src/components/NavMenu/NavMenu.tsx
+++ packages/common/react-ui/src/components/NavMenu/NavMenu.tsx
@@ -40,8 +40,17 @@ export type NavMenuItem =
   | NavMenuInvokerItemProps
   | NavMenuSeparatorProps;
 
-export interface NavMenuProps extends ComponentProps<typeof NavigationMenuPrimitive.Root> {
+export interface NavMenuSlots {
+  root?: Omit<ComponentProps<typeof NavigationMenuPrimitive.Root>, 'children'>;
+  viewport?: ComponentProps<typeof NavigationMenuPrimitive.Viewport>;
+  list?: Omit<ComponentProps<typeof NavigationMenuPrimitive.List>, 'children'>;
+  indicator?: Omit<ComponentProps<typeof NavigationMenuPrimitive.Indicator>, 'children'>;
+  indicatorIcon?: ComponentProps<'div'>;
+}
+
+export interface NavMenuProps {
   items: NavMenuItem[];
+  slots?: NavMenuSlots;
 }
 
 const NavMenuInvokerItem = forwardRef(
@@ -129,10 +138,16 @@ const isTooltipLinkItem = (o: any): o is NavMenuTooltipLinkItemProps => 'tooltip
 const isLinkItem = (o: any): o is NavMenuLinkItemProps => 'triggerLinkProps' in o;
 const isSeparator = (o: any): o is NavMenuSeparatorProps => 'separator' in o;
 
-export const NavMenu = ({ items, ...rootProps }: NavMenuProps) => {
+export const NavMenu = ({ items, slots = {} }: NavMenuProps) => {
   return (
-    <NavigationMenuPrimitive.Root {...rootProps} className={mx('flex justify-center', rootProps.className)}>
-      <NavigationMenuPrimitive.List className='relative flex flex-row items-center gap-1 rounded-lg bg-white dark:bg-neutral-750 p-1 button-elevation overflow-x-auto'>
+    <NavigationMenuPrimitive.Root {...slots.root} className={mx('flex justify-center', slots.root?.className)}>
+      <NavigationMenuPrimitive.List
+        {...slots.list}
+        className={mx(
+          'relative flex flex-row items-center gap-1 rounded-lg bg-white dark:bg-neutral-750 p-1 button-elevation overflow-x-auto',
+          slots.list?.className
+        )}
+      >
         {items.map((item: NavMenuItem, i) => {
           return isTooltipLinkItem(item) ? (
             <NavMenuTooltipLinkItem key={i} {...item} />
@@ -146,15 +161,22 @@ export const NavMenu = ({ items, ...rootProps }: NavMenuProps) => {
         })}
 
         <NavigationMenuPrimitive.Indicator
+          {...slots.indicator}
           className={mx(
-            'z-10',
-            'top-[100%] flex items-end justify-center h-2 overflow-hidden',
+            'z-10 top-[100%] flex items-end justify-center h-2 overflow-hidden',
             'radix-state-visible:animate-fade-in',
             'radix-state-hidden:animate-fade-out',
-            'transition-[width_transform] duration-[250ms] ease-[ease]'
+            'transition-[width_transform] duration-[250ms] ease-[ease]',
+            slots.indicator?.className
           )}
         >
-          <div className='top-1 relative bg-white dark:bg-neutral-750 w-2 h-2 rotate-45' />
+          <div
+            {...slots?.indicatorIcon}
+            className={mx(
+              'top-1 relative bg-white dark:bg-neutral-750 w-2 h-2 rotate-45',
+              slots.indicatorIcon?.className
+            )}
+          />
         </NavigationMenuPrimitive.Indicator>
       </NavigationMenuPrimitive.List>
 
@@ -165,13 +187,15 @@ export const NavMenu = ({ items, ...rootProps }: NavMenuProps) => {
         }}
       >
         <NavigationMenuPrimitive.Viewport
+          {...slots.viewport}
           className={mx(
-            'relative mt-2 shadow-lg rounded-md bg-white dark:bg-neutral-750 overflow-hidden',
+            'relative mbs-2 shadow-lg rounded-md bg-white dark:bg-neutral-750 overflow-hidden',
             'w-radix-navigation-menu-viewport',
             'h-radix-navigation-menu-viewport',
             'radix-state-open:animate-scale-in-content',
             'radix-state-closed:animate-scale-out-content',
-            'origin-[top_center] transition-[width_height] duration-300 ease-[ease]'
+            'origin-[top_center] transition-[width_height] duration-300 ease-[ease]',
+            slots.viewport?.className
           )}
         />
       </div>
