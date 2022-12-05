ae8c043d46053c39838fa4c879505d013d63689f
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-05 17:09:25 +0200

    Refactor PresenceExtension to call callback on Announce

--- packages/core/mesh/teleport-plugin-presence/src/presence-extension.ts
+++ packages/core/mesh/teleport-plugin-presence/src/presence-extension.ts
@@ -4,27 +4,25 @@
 
 import assert from 'assert';
 
-import { Event, Trigger } from '@dxos/async';
+import { Trigger } from '@dxos/async';
 import { PublicKey } from '@dxos/keys';
 import { log } from '@dxos/log';
 import { schema } from '@dxos/protocols';
 import { PeerState, PresenceService } from '@dxos/protocols/proto/dxos/mesh/teleport/presence';
 import { createProtoRpcPeer, ProtoRpcPeer } from '@dxos/rpc';
 import { ExtensionContext, TeleportExtension } from '@dxos/teleport';
-import { ComplexMap, ComplexSet } from '@dxos/util';
+import { ComplexSet } from '@dxos/util';
 
-export type PresenceOptions = {
-  connections?: PublicKey[];
-  resendAnnounce?: number;
-  offlineTimeout?: number;
+export type PresenceParams = {
+  connections: PublicKey[];
+  resendAnnounce: number;
+  onAnnounce: (peerState: PeerState) => Promise<void>;
 };
 
 /**
  * Sending presence pings between a set peers for a single teleport session.
  */
 export class PresenceExtension implements TeleportExtension {
-  public updatePeerStates = new Event<PeerState[]>();
-  private readonly _receivedAnnounces = new ComplexMap<PublicKey, PeerState>(PublicKey.hash);
   private readonly _sentAnnounces = new ComplexSet<PublicKey>(PublicKey.hash); // TODO(mykola): Memory leak, never cleaned up?
   private _sendInterval?: NodeJS.Timeout;
 
@@ -32,31 +30,20 @@ export class PresenceExtension implements TeleportExtension {
   private _extensionContext?: ExtensionContext;
   private readonly _opened = new Trigger();
 
-  private _options: PresenceOptions;
-
-  constructor({ connections = [], resendAnnounce = 1000, offlineTimeout = 30_000 }: PresenceOptions = {}) {
-    assert(offlineTimeout, 'offlineTimeout must be set');
-    this._options = { connections, resendAnnounce, offlineTimeout };
-  }
+  constructor(private readonly _params: PresenceParams) {}
 
   setConnections(connections: PublicKey[]) {
-    this._options.connections = connections;
+    this._params.connections = connections;
   }
 
-  sendAnnounces(peerStates: PeerState[]) {
-    peerStates.forEach(async (peerState) => {
-      assert(this._opened, 'Extension not opened');
-      assert(this._rpc, 'RPC not initialized');
-      if (this._sentAnnounces.has(peerState.peerId) || this._extensionContext?.remotePeerId === peerState.peerId) {
-        return;
-      }
-      this._sentAnnounces.add(peerState.peerId);
-      await this._rpc.rpc.PresenceService.announce(peerState);
-    });
-  }
-
-  getPeerStates() {
-    return [...this._receivedAnnounces.values()];
+  async sendAnnounce(peerState: PeerState) {
+    assert(this._opened, 'Extension not opened');
+    assert(this._rpc, 'RPC not initialized');
+    if (this._sentAnnounces.has(peerState.peerId)) {
+      return;
+    }
+    this._sentAnnounces.add(peerState.peerId);
+    await this._rpc.rpc.PresenceService.announce(peerState);
   }
 
   async onOpen(context: ExtensionContext): Promise<void> {
@@ -71,21 +58,14 @@ export class PresenceExtension implements TeleportExtension {
       },
       handlers: {
         PresenceService: {
-          announce: async (peerState: PeerState) => {
-            const oldPeerState = this._receivedAnnounces.get(peerState.peerId);
-            if (oldPeerState && oldPeerState.timestamp.getTime() > peerState.timestamp.getTime()) {
-              return;
-            }
-            this._receivedAnnounces.set(peerState.peerId, peerState);
-            this.updatePeerStates.emit([...this._receivedAnnounces.values()]);
-          }
+          announce: async (peerState: PeerState) => await this._params.onAnnounce(peerState)
         }
       },
       port: context.createPort('rpc', { contentType: 'application/presence, messageType="dxos.rpc.Message"' }) // TODO(mykola): Different contentType?
     });
     await this._rpc.open();
     await this._sendAnnounce();
-    this._sendInterval = setInterval(() => this._sendAnnounce(), this._options.resendAnnounce);
+    this._sendInterval = setInterval(() => this._sendAnnounce(), this._params.resendAnnounce);
     this._opened.wake();
   }
 
@@ -98,7 +78,7 @@ export class PresenceExtension implements TeleportExtension {
   private async _sendAnnounce() {
     await this._rpc?.rpc.PresenceService.announce({
       peerId: this._extensionContext!.localPeerId,
-      connections: this._options.connections,
+      connections: this._params.connections,
       messageId: PublicKey.random(),
       timestamp: new Date()
     });
