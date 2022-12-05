c70d0d623164edd1ee5ab27cfddcd3ac782228f7
Zhenya <zhenya@zhenya.co>
2022-12-05 16:54:26 -0500

    wip plate upgrades

--- packages/common/plate/src/includeExclude.ts
+++ packages/common/plate/src/includeExclude.ts
@@ -1,15 +1,15 @@
 import minimatch from 'minimatch';
 
-export const includeExclude = (
-  collection: string[],
-  options?: { include?: (string | RegExp)[]; exclude?: (string | RegExp)[] }
-): string[] => {
-  const { include, exclude } = { ...options };
+export const includeExclude = <T>(
+  collection: T[],
+  options?: { include?: (string | RegExp)[]; exclude?: (string | RegExp)[]; transform?: (element: T) => string }
+): T[] => {
+  const { include, exclude, transform } = { transform: (x: any) => x.toString(), ...options };
   const matches = (pattern: string | RegExp, value: string) =>
     pattern instanceof RegExp ? pattern.test(value) : minimatch(value, pattern);
   return collection.filter(
     (value) =>
-      (include?.length ? include.some((pattern) => matches(pattern, value)) : true) &&
-      !(exclude ?? []).some((pattern) => matches(pattern, value))
+      (include?.length ? include.some((pattern) => matches(pattern, transform(value))) : true) &&
+      !(exclude ?? []).some((pattern) => matches(pattern, transform(value)))
   );
 };
