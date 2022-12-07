eef112f0f029cf553c2f645fe1d77d8b0d09bb8d
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2022-12-07 16:02:44 +0200

    chore(teleport): Add testing entrypoint (#2043)

--- packages/core/mesh/teleport/src/teleport.test.ts
+++ packages/core/mesh/teleport/src/teleport.test.ts
@@ -3,49 +3,24 @@
 //
 
 import { expect } from 'chai';
-import { pipeline } from 'node:stream';
 
 import { latch } from '@dxos/async';
-import { PublicKey } from '@dxos/keys';
 import { log } from '@dxos/log';
 import { afterTest, describe, test } from '@dxos/test';
 
-import { Teleport } from './teleport';
 import { TestExtension } from './test-extension';
-
-const setup = () => {
-  const peerId1 = PublicKey.random();
-  const peerId2 = PublicKey.random();
-
-  const peer1 = new Teleport({ initiator: true, localPeerId: peerId1, remotePeerId: peerId2 });
-  const peer2 = new Teleport({ initiator: false, localPeerId: peerId2, remotePeerId: peerId1 });
-
-  pipeline(peer1.stream, peer2.stream, (err) => {
-    if (err && err.code !== 'ERR_STREAM_PREMATURE_CLOSE') {
-      log.catch(err);
-    }
-  });
-  pipeline(peer2.stream, peer1.stream, (err) => {
-    if (err && err.code !== 'ERR_STREAM_PREMATURE_CLOSE') {
-      log.catch(err);
-    }
-  });
-  afterTest(() => peer1.close());
-  afterTest(() => peer2.close());
-
-  return { peer1, peer2 };
-};
+import { TestBuilder } from './testing';
 
 describe('Teleport', () => {
   test('sends rpc via TestExtension', async () => {
-    const { peer1, peer2 } = setup();
-
-    await Promise.all([peer1.open(), peer2.open()]);
+    const builder = new TestBuilder();
+    afterTest(() => builder.destroy());
+    const { peer1, peer2 } = await builder.createPipedPeers();
 
     const extension1 = new TestExtension();
-    peer1.addExtension('example.testing.rpc', extension1);
+    peer1.teleport!.addExtension('example.testing.rpc', extension1);
     const extension2 = new TestExtension();
-    peer2.addExtension('example.testing.rpc', extension2);
+    peer2.teleport!.addExtension('example.testing.rpc', extension2);
 
     await extension1.test();
     log('test1 done');
@@ -57,15 +32,14 @@ describe('Teleport', () => {
   });
 
   test('disconnect', async () => {
-    // eslint-disable-next-line mocha/no-sibling-hooks
-    const { peer1, peer2 } = setup();
-
-    await Promise.all([peer1.open(), peer2.open()]);
+    const builder = new TestBuilder();
+    afterTest(() => builder.destroy());
+    const { peer1, peer2 } = await builder.createPipedPeers();
 
     const extension1 = new TestExtension();
-    peer1.addExtension('example.testing.rpc', extension1);
+    peer1.teleport!.addExtension('example.testing.rpc', extension1);
     const extension2 = new TestExtension();
-    peer2.addExtension('example.testing.rpc', extension2);
+    peer2.teleport!.addExtension('example.testing.rpc', extension2);
     await extension1.test();
     log('test1 done');
     await extension2.test();
@@ -78,15 +52,14 @@ describe('Teleport', () => {
   });
 
   test('destroy is idempotent', async () => {
-    // eslint-disable-next-line mocha/no-sibling-hooks
-    const { peer1, peer2 } = setup();
-
-    await Promise.all([peer1.open(), peer2.open()]);
+    const builder = new TestBuilder();
+    afterTest(() => builder.destroy());
+    const { peer1, peer2 } = await builder.createPipedPeers();
 
     const extension1 = new TestExtension();
-    peer1.addExtension('example.testing.rpc', extension1);
+    peer1.teleport!.addExtension('example.testing.rpc', extension1);
     const extension2 = new TestExtension();
-    peer2.addExtension('example.testing.rpc', extension2);
+    peer2.teleport!.addExtension('example.testing.rpc', extension2);
     await extension1.test();
     log('test1 done');
     await extension2.test();
