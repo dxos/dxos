4aef42ed052b72a6657665ffb58b4d29b79e66d0
Zhenya <zhenya@zhenya.co>
2022-12-07 00:35:42 -0500

    add todo

--- packages/common/plate/src/index.ts
+++ packages/common/plate/src/index.ts
@@ -2,9 +2,10 @@
 // Copyright 2022 DXOS.org
 //
 // export * from './file/index';
+export * from './file/index';
 import { File as BaseFile } from './file/index';
 import { TemplateResultMetadata } from './executeFileTemplate';
-export class File<R = string, M extends TemplateResultMetadata = TemplateResultMetadata> extends BaseFile<R, M> {};
+export class File<R = string, M extends TemplateResultMetadata = TemplateResultMetadata> extends BaseFile<R, M> {}
 export * from './config';
 export * from './executeFileTemplate';
 export * from './executeDirectoryTemplate';
