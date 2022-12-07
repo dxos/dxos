582f1f621577350463f00326260bba2f66fb00b4
Zhenya <zhenya@zhenya.co>
2022-12-07 00:43:55 -0500

    review feedback

--- packages/common/plate/src/file/exists.ts
+++ packages/common/plate/src/file/exists.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { promises as fs } from "fs";
 
+//TODO: factor out to own fs package like @dxos/fs
 export const exists = async (...args: string[]): Promise<boolean> => {
   try {
     const result = await fs.stat(path.join(...args));
--- packages/common/plate/src/regexp.ts
+++ packages/common/plate/src/regexp.ts
@@ -1,1 +1,7 @@
+/**
+ * This utility helps escape RegEx symbols from a string to allow it to be used as a pure pattern without setting off
+ * any dots, dashes and other RegExp directives
+ * @param str the regex string to escape (which may contain regex characters)
+ * @returns a string passable to a RegExp constructor
+ */
 export const escapeRegExp = (str: string) => String(str).replace(/([.*+?=^!:${}()|[\]\/\\])/g, '\\$1');
