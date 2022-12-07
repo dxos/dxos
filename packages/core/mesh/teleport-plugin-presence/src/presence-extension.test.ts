f867d7050f06daaec7924acd341339945bfb7a84
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-07 16:37:16 +0200

    Fix extension tests

--- packages/core/mesh/teleport-plugin-presence/src/presence-extension.test.ts
+++ packages/core/mesh/teleport-plugin-presence/src/presence-extension.test.ts
@@ -7,59 +7,63 @@ import waitForExpect from 'wait-for-expect';
 
 import { latch } from '@dxos/async';
 import { PeerState } from '@dxos/protocols/proto/dxos/mesh/teleport/presence';
-import { describe, test } from '@dxos/test';
+import { TestBuilder, TestPeer } from '@dxos/teleport/testing';
+import { afterTest, describe, test } from '@dxos/test';
 
-import { TestBuilder } from './testing';
+import { PresenceExtension } from './presence-extension';
 
 describe('PresenceExtension', () => {
   test('Two peers announce each other', async () => {
     const builder = new TestBuilder();
-    const { agent1, agent2 } = await builder.createPipedAgents();
+    afterTest(() => builder.destroy());
+    const { peer1, peer2 } = await builder.createPipedPeers();
 
     const received1: PeerState[] = [];
-    agent1.initializePresence({
-      connections: [agent2.peerId],
-      resendAnnounce: 100,
+    const extension1 = new PresenceExtension({
+      connections: [peer2.peerId],
+      resendAnnounce: 50,
       onAnnounce: async (peerState: PeerState) => {
         received1.push(peerState);
       }
     });
+    peer1.teleport!.addExtension('dxos.mesh.teleport.presence', extension1);
 
     const received2: PeerState[] = [];
-    agent2.initializePresence({
-      connections: [agent1.peerId],
-      resendAnnounce: 100,
+    const extension2 = new PresenceExtension({
+      connections: [peer1.peerId],
+      resendAnnounce: 50,
       onAnnounce: async (peerState: PeerState) => {
         received2.push(peerState);
       }
     });
-    await waitForExpect(() => expect(received1[0].peerId).toEqual(agent2.peerId));
-    await waitForExpect(() => expect(received2[0].peerId).toEqual(agent1.peerId));
+    peer2.teleport!.addExtension('dxos.mesh.teleport.presence', extension2);
+
+    await waitForExpect(() => expect(received1[0].peerId).toEqual(peer2.peerId));
+    await waitForExpect(() => expect(received2[0].peerId).toEqual(peer1.peerId));
   });
 
   test('Peer reannounces itself by interval', async () => {
     const builder = new TestBuilder();
-    const { agent1, agent2 } = await builder.createPipedAgents();
+    afterTest(() => builder.destroy());
+    const { peer1, peer2 } = await builder.createPipedPeers();
 
-    agent1.initializePresence({
-      connections: [agent2.peerId],
-      resendAnnounce: 100,
-      onAnnounce: async () => {}
-    });
-
-    const received: PeerState[] = [];
-    const [announcedTwice, inc] = latch({ count: 2 });
-    agent2.initializePresence({
-      connections: [agent1.peerId],
-      resendAnnounce: 100,
+    const [announced10Times, inc] = latch({ count: 10 });
+    const extension1 = new PresenceExtension({
+      connections: [peer2.peerId],
+      resendAnnounce: 50,
       onAnnounce: async (peerState: PeerState) => {
-        expect(peerState.peerId).toEqual(agent1.peerId);
-        expect(peerState.connections).toEqual([agent2.peerId]);
-        received.push(peerState);
         inc();
       }
     });
+    peer1.teleport!.addExtension('dxos.mesh.teleport.presence', extension1);
 
-    await announcedTwice();
+    const extension2 = new PresenceExtension({
+      connections: [peer1.peerId],
+      resendAnnounce: 50,
+      onAnnounce: async (peerState: PeerState) => {}
+    });
+    peer2.teleport!.addExtension('dxos.mesh.teleport.presence', extension2);
+
+    await announced10Times();
   });
 });
