21ee48c645918086a52bdb283a71eaad524608bf
Josiah Witt <josiah@witt.life>
2022-12-08 09:37:55 -0500

    fix(config): Re-add plugin dynamic env var parsing (#2072)

--- packages/sdk/config/src/loaders/browser.js
+++ packages/sdk/config/src/loaders/browser.js
@@ -6,20 +6,26 @@
 
 /* global __DXOS_CONFIG__ __CONFIG_DYNAMICS__ __CONFIG_ENVS__ __CONFIG_DEFAULTS__ */
 
+import { log } from '@dxos/log';
+
 const CONFIG_ENDPOINT = '/.well-known/dx/config';
 
 export const LocalStorage = (item = 'options') => JSON.parse(window.localStorage.getItem(item) || '{}');
 
 export const Dynamics = async () => {
   const { publicUrl = '', dynamic } = __DXOS_CONFIG__;
-  return dynamic
-    ? await fetch(`${publicUrl}${CONFIG_ENDPOINT}`)
-        .then((res) => res.json())
-        .catch((error) => {
-          console.warn('Failed to fetch dynamic config.', error);
-          return __CONFIG_DYNAMICS__;
-        })
-    : __CONFIG_DYNAMICS__;
+  if (!dynamic) {
+    log('dynamics disabled', __CONFIG_DYNAMICS__);
+    return __CONFIG_DYNAMICS__;
+  }
+
+  log('fetching config...', { publicUrl });
+  return await fetch(`${publicUrl}${CONFIG_ENDPOINT}`)
+    .then((res) => res.json())
+    .catch((error) => {
+      log.warn('Failed to fetch dynamic config.', error);
+      return __CONFIG_DYNAMICS__;
+    });
 };
 
 export const Envs = () => __CONFIG_ENVS__;
