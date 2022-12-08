d257a2d0cc4993cd45b41010dc5ae42b04721a8b
Josiah Witt <josiah@witt.life>
2022-12-07 20:57:44 -0500

    refactor: revert rollup plugin changes

--- packages/sdk/config/src/plugin/rollup-plugin.ts
+++ packages/sdk/config/src/plugin/rollup-plugin.ts
@@ -2,19 +2,23 @@
 // Copyright 2022 DXOS.org
 //
 
-import alias from '@rollup/plugin-alias';
+import { Plugin } from 'rollup';
 
 import { definitions } from './definitions';
 import { ConfigPluginOpts } from './types';
 
-export const ConfigPlugin = (options: ConfigPluginOpts = {}) => {
-  const entries = Object.entries(definitions(options)).map(([find, replacement]) => ({
-    find,
-    replacement: JSON.stringify(replacement)
-  }));
+export const ConfigPlugin = (options: ConfigPluginOpts = {}): Plugin => {
+  const contents = Object.entries(definitions(options))
+    .map(([key, value]) => `globalThis.${key} = ${JSON.stringify(value)};`)
+    .join('\n');
 
-  const plugin = alias({ entries });
-  plugin.name = 'dxos-config';
-
-  return plugin;
+  return {
+    name: 'dxos-config',
+    transform(code, module) {
+      // Based on https://github.com/mmirca/rollup-plugin-entry-code-injector/blob/6ce979fcea31a75537c00748fbe25ed14f340624/commonjs/index.js.
+      if (this.getModuleInfo(module)?.isEntry) {
+        return contents + code;
+      }
+    }
+  };
 };
