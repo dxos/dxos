6d9f5657e1e4b8c76090dc3ebc15b8e1ae289966
Zhenya <zhenya@zhenya.co>
2022-12-09 13:20:40 -0500

    tasks template

--- packages/apps/templates/tasks-template/config.t.ts
+++ packages/apps/templates/tasks-template/config.t.ts
@@ -1,3 +1,7 @@
-export default {
-  inherits: '../bare-template'
-};
+import { z, defineConfig } from '@dxos/plate';
+import inherited from '../bare-template/config.t';
+
+export default defineConfig({
+  inherits: '../bare-template',
+  inputShape: inherited.inputShape
+});
