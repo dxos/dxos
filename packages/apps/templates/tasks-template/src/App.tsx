8db4e6fbd04d84ebad537904f3c62bae40cf22a0
Zhenya <zhenya@zhenya.co>
2022-12-09 13:20:40 -0500

    tasks template

--- packages/apps/templates/tasks-template/src/App.tsx
+++ packages/apps/templates/tasks-template/src/App.tsx
@@ -1,6 +1,37 @@
-// //
-// // Copyright 2022 DXOS.org
-// //
+import { defineTemplate, renderSlots, text, Imports } from '@dxos/plate';
+import config from '../config.t';
+
+export default defineTemplate(
+  ({ input, defaultOutputFile, slots }) => {
+    const { react } = input;
+    const imports = new Imports();
+    const render = renderSlots(slots)({ imports });
+    const ClientProvider = imports.lazy('ClientProvider', '@dxos/react-client');
+    const { Config, Dynamics, Defaults } = imports.lazy(['Config', 'Dynamics', 'Defaults'], '@dxos/config');
+    return !react
+      ? null
+      : text`
+      import React from 'react';
+      ${() => imports.render(defaultOutputFile)}
+
+      const config = async () => new ${Config()}(await ${Dynamics()}(), ${Defaults()}());
+
+      export const App = () => {
+        return (
+          <${ClientProvider()} config={config}>
+            ${render.content()}
+          </${ClientProvider()}>
+        )
+      }
+      `;
+  },
+  {
+    config,
+    slots: {
+      content: '{/* your components here */}'
+    }
+  }
+);
 
 // import '@dxosTheme';
 // import React from 'react';
