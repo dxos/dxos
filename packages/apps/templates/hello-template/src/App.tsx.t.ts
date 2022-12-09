294f5ce435b8920b62eb532fbb9224f1be5445cb
Zhenya <zhenya@zhenya.co>
2022-12-09 01:51:03 -0500

    fix bare and hello templates

---
+++ packages/apps/templates/hello-template/src/App.tsx.t.ts
@@ -0,0 +1,10 @@
+import { defineTemplate, text } from '@dxos/plate';
+import AppTsx from "../../bare-template/src/App.tsx.t";
+import config from '../config.t';
+
+export default defineTemplate(async (context) => {
+  const { slots, ...rest } = context;
+  return AppTsx({ ...rest, slots: {
+    content: ({ imports }) => `{/* content */}`
+  }});
+}, { config });
--- packages/common/plate/src/index.ts
+++ packages/common/plate/src/index.ts
@@ -1,15 +1,14 @@
 //
 // Copyright 2022 DXOS.org
 //
-// export * from './file/index';
 export * from './file/index';
 import { File as BaseFile } from './file/index';
 import { TemplateResultMetadata } from './executeFileTemplate';
 export class File<R = string, M extends TemplateResultMetadata = TemplateResultMetadata> extends BaseFile<R, M> {}
+export * from './util/templateLiterals';
+export * from './util/catFiles';
+export * from './util/imports';
 export * from './config';
 export * from './executeFileTemplate';
-export * from './executeDirectoryTemplate';
-export * from './templateLiterals';
-export * from './catFiles';
 export * from './executeDirectoryTemplate';
 export { z } from 'zod';
