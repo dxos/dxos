a76c8a014a32996005b8719752269c2ec6647d20
Zhenya <zhenya@zhenya.co>
2022-12-09 03:11:21 -0500

    basics added to hello template

--- packages/apps/templates/hello-template/src/App.tsx.t.ts
+++ packages/apps/templates/hello-template/src/App.tsx.t.ts
@@ -1,10 +1,16 @@
 import { defineTemplate, text } from '@dxos/plate';
-import AppTsx from "../../bare-template/src/App.tsx.t";
+import AppTsx from '../../bare-template/src/App.tsx.t';
 import config from '../config.t';
 
-export default defineTemplate(async (context) => {
-  const { slots, ...rest } = context;
-  return AppTsx({ ...rest, slots: {
-    content: ({ imports }) => `{/* content */}`
-  }});
-}, { config });
+export default defineTemplate(
+  async (context) => {
+    const { slots, ...rest } = context;
+    return AppTsx({
+      ...rest,
+      slots: {
+        content: ({ imports }) => `<${imports.use('Welcome', './Welcome')} />`
+      }
+    });
+  },
+  { config }
+);
