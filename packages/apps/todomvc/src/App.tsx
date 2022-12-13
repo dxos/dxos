1c90a2be2d555b79215726de76fd5135b0dc0c00
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-13 18:16:41 +0200

    Move providers out of component

--- packages/apps/todomvc/src/App.tsx
+++ packages/apps/todomvc/src/App.tsx
@@ -11,29 +11,27 @@ import { ClientProvider } from '@dxos/react-client';
 
 import { Main, SpaceList } from './components';
 
-export const App = () => {
-  const configProvider = async () => new Config(await Dynamics(), Defaults());
-  const servicesProvider = (config: Config) =>
-    process.env.DX_VAULT === 'false' ? fromHost(config) : fromIFrame(config);
-  return (
-    <ClientProvider config={configProvider} services={servicesProvider}>
-      <HashRouter>
-        <Routes>
-          <Route path='/' element={<SpaceList />}>
-            <Route path='/:space' element={<Main />} />
-            <Route path='/:space/:state' element={<Main />} />
-          </Route>
-        </Routes>
-      </HashRouter>
-      <footer className='info'>
-        <p>Double-click to edit a todo</p>
-        <p>
-          Created by <a href='https://github.com/dxos/'>DXOS</a>
-        </p>
-        <p>
-          Based on <a href='https://todomvc.com'>TodoMVC</a>
-        </p>
-      </footer>
-    </ClientProvider>
-  );
-};
+const configProvider = async () => new Config(await Dynamics(), Defaults());
+const servicesProvider = (config: Config) => (process.env.DX_VAULT === 'false' ? fromHost(config) : fromIFrame(config));
+
+export const App = () => (
+  <ClientProvider config={configProvider} services={servicesProvider}>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<SpaceList />}>
+          <Route path='/:space' element={<Main />} />
+          <Route path='/:space/:state' element={<Main />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+    <footer className='info'>
+      <p>Double-click to edit a todo</p>
+      <p>
+        Created by <a href='https://github.com/dxos/'>DXOS</a>
+      </p>
+      <p>
+        Based on <a href='https://todomvc.com'>TodoMVC</a>
+      </p>
+    </footer>
+  </ClientProvider>
+);
