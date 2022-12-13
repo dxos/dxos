6308270512975252bb6e7fe2a3aa10aea5d347d2
Josiah Witt <josiah@witt.life>
2022-12-13 15:21:15 -0500

    ci: staging flow

--- .circleci/scripts/publish.sh
+++ .circleci/scripts/publish.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+APPS=[
+  './packages/apps/halo-app',
+  './packages/apps/tasks-app',
+  './packages/apps/composer-app',
+  './packages/apps/todomvc',
+  './packages/devtools/devtools',
+  './docs'
+];
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 ROOT=$(git rev-parse --show-toplevel)
 PACKAGE=${PWD##*/}
 PACKAGE_CAPS=${PACKAGE^^}
@@ -8,21 +18,38 @@ PACKAGE_ENV=${PACKAGE_CAPS//-/_}
 eval "export SENTRY_DESTINATION=$"${PACKAGE_ENV}_SENTRY_DSN""
 eval "export TELEMETRY_API_KEY=$"${PACKAGE_ENV}_SEGMENT_API_KEY""
 
-if [ $1 = "production" ]; then
-  export DX_ENVIRONMENT=production
-  DX_CONFIG="$ROOT/packages/devtools/cli/config/config.yml"
-  VERSION=$(cat package.json | jq -r '.version')
+for APP in $APPS; do
+  pushd $APP
 
-  $ROOT/packages/devtools/cli/bin/run app publish \
-    --config=$DX_CONFIG \
-    --version=$VERSION \
-    --skipExisting \
-    --verbose
-else
-  export DX_ENVIRONMENT=development
-  DX_CONFIG="$ROOT/packages/devtools/cli/config/config-dev.yml"
+  if [ $BRANCH = "production" ]; then
+    export DX_ENVIRONMENT=production
+    DX_CONFIG="$ROOT/packages/devtools/cli/config/config.yml"
+    VERSION=$(cat package.json | jq -r '.version')
 
-  $ROOT/packages/devtools/cli/bin/run app publish \
-    --config=$DX_CONFIG \
-    --verbose
-fi
+    $ROOT/packages/devtools/cli/bin/run app publish \
+      --config=$DX_CONFIG \
+      --version=$VERSION \
+      --skipExisting \
+      --verbose
+  elif [ $BRANCH = "staging" ]; then
+    export DX_ENVIRONMENT=staging
+    DX_CONFIG="$ROOT/packages/devtools/cli/config/config-staging.yml"
+    VERSION=$(cat package.json | jq -r '.version')
+
+    $ROOT/packages/devtools/cli/bin/run app publish \
+      --config=$DX_CONFIG \
+      --version=$VERSION \
+      --skipExisting \
+      --verbose
+  else
+    export DX_ENVIRONMENT=development
+    DX_CONFIG="$ROOT/packages/devtools/cli/config/config-dev.yml"
+
+    $ROOT/packages/devtools/cli/bin/run app publish \
+      --config=$DX_CONFIG \
+      --verbose
+  fi
+
+  popd
+done
+
