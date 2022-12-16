2f8fabb63d5b262c15bbfafc1997f6cc7fd094b7
Zhenya <zhenya@zhenya.co>
2022-12-16 03:30:20 -0500

    lint fixing

--- packages/apps/samples/bare/src/pwa.d.ts
+++ packages/apps/samples/bare/src/pwa.d.ts
@@ -4,10 +4,10 @@
 
 // TODO(wittjosiah): Including 'vite-plugin-pwa/client' in tsconfig types breaks react typing.
 // Taken from https://github.com/vite-pwa/vite-plugin-pwa/blob/bc3fab15d0b73994c57435ee13d7d9ce8c18cd55/client.d.ts.
-declare module "virtual:pwa-register/react" {
+declare module 'virtual:pwa-register/react' {
   // eslint-disable-next-line
   // @ts-ignore ignore when react is not installed
-  import type { Dispatch, SetStateAction } from "react";
+  import type { Dispatch, SetStateAction } from 'react';
 
   export interface RegisterSWOptions {
     immediate?: boolean;
@@ -19,19 +19,14 @@ declare module "virtual:pwa-register/react" {
      * @deprecated Use 'onRegisteredSW' instead.
      * @param registration The service worker registration if available.
      */
-    onRegistered?: (
-      registration: ServiceWorkerRegistration | undefined
-    ) => void;
+    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
     /**
      * Called once the service worker is registered (requires version '0.12.8+').
      *
      * @param swScriptUrl The service worker script url.
      * @param registration The service worker registration if available.
      */
-    onRegisteredSW?: (
-      swScriptUrl: string,
-      registration: ServiceWorkerRegistration | undefined
-    ) => void;
+    onRegisteredSW?: (swScriptUrl: string, registration: ServiceWorkerRegistration | undefined) => void;
     onRegisterError?: (error: any) => void;
   }
 
