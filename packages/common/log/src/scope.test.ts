02c4aeccfe0e824419f76867ad691f54150a23c2
Dmytro Maretskyi <maretskii@gmail.com>
2022-12-22 16:23:46 +0100

    Add contextual hierrarhical logging

--- packages/common/log/src/scope.test.ts
+++ packages/common/log/src/scope.test.ts
@@ -5,7 +5,7 @@
 import { describe, test } from '@dxos/test';
 
 import { log } from './log';
-import { logInfo } from './scope';
+import { logInfo, setLogParent } from './scope';
 
 describe('Scope capture', function () {
   test('field instance', function () {
@@ -27,5 +27,38 @@ describe('Scope capture', function () {
 
     foo.run();
     bar.run();
+  });
+
+  test('field instance', function () {
+    class Parent {
+      constructor(
+        private readonly _id: string,
+        public child: Child
+      ) {
+        setLogParent(this, child)
+      }
+
+      @logInfo
+      get id() {
+        return this._id;
+      }
+    }
+
+    class Child {
+      constructor(private readonly _id: string) {}
+
+      @logInfo
+      get id() {
+        return this._id;
+      }
+
+      run() {
+        log('run');
+      }
+    }
+
+
+    const parent1 = new Parent('parent1', new Child('child1'));
+    parent1.child.run();
   });
 });
