e55f30bf9c50cdbafadb54e207fff23f2409fd45
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-23 17:55:38 +0200

    Make getNetworkMode return just value

--- packages/sdk/client-services/src/packlets/network/network-service.ts
+++ packages/sdk/client-services/src/packlets/network/network-service.ts
@@ -2,26 +2,14 @@
 // Copyright 2022 DXOS.org
 //
 
-import { Stream } from '@dxos/codec-protobuf';
 import { NetworkManager } from '@dxos/network-manager';
-import {
-  NetworkService,
-  SetNetworkModeRequest,
-  GetNetworkModeResponse
-} from '@dxos/protocols/proto/dxos/client/services';
+import { NetworkService, SetNetworkModeRequest } from '@dxos/protocols/proto/dxos/client/services';
 
 export class NetworkServiceImpl implements NetworkService {
   constructor(private readonly networkManager: NetworkManager) {}
 
-  getNetworkMode() {
-    return new Stream<GetNetworkModeResponse>(({ next }) => {
-      next({ mode: this.networkManager.networkMode });
-      const unsubscribe = this.networkManager.networkModeChanged.on((mode) => {
-        next({ mode });
-      });
-
-      return unsubscribe;
-    });
+  async getNetworkMode() {
+    return { mode: this.networkManager.networkMode };
   }
 
   async setNetworkMode(request: SetNetworkModeRequest) {
