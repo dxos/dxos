22e3c72612d4cd13fb196756ec51c7391f53ea93
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-23 14:41:07 +0200

    Add useNetworkMode

--- packages/sdk/react-client/src/network/useNetworkMode.ts
+++ packages/sdk/react-client/src/network/useNetworkMode.ts
@@ -2,4 +2,19 @@
 // Copyright 2022 DXOS.org
 //
 
-export const useNetworkMode = () => {};
+import { useEffect, useState } from 'react';
+
+import { useClientServices } from '../client';
+import { useStream } from '../util';
+import { NetworkMode } from '@dxos/protocols/proto/dxos/client/services';
+
+export const useNetworkMode = () => {
+  const services = useClientServices();
+
+  if (!services) {
+    return;
+  }
+
+  const value = useStream(() => services.NetworkService.getNetworkMode(), { mode: NetworkMode.ONLINE }, [services]);
+  return value;
+};
