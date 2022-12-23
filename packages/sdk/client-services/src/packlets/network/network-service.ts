7058f95736de0fac5abf0a2d43b44ac4e011c1b0
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-23 13:35:47 +0200

    Add NetworkServiceImpl

---
+++ packages/sdk/client-services/src/packlets/network/network-service.ts
@@ -0,0 +1,20 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { NetworkManager } from '@dxos/network-manager';
+import { NetworkService, SetNetworkModeRequest } from '@dxos/protocols/proto/dxos/client/services';
+
+export class NetworkServiceImpl implements NetworkService {
+  constructor(private readonly networkManager: NetworkManager) {}
+
+  async getNetworkMode() {
+    return {
+      mode: this.networkManager.networkMode
+    };
+  }
+
+  async setNetworkMode(request: SetNetworkModeRequest) {
+    await this.networkManager.setNetworkMode(request.mode);
+  }
+}
