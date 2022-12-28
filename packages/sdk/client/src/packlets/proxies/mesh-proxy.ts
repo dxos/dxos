5932c38e2744dcec3a442c7f3a00ba621d56d826
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-12-28 21:30:15 +0200

    Add MESH Proxy

---
+++ packages/sdk/client/src/packlets/proxies/mesh-proxy.ts
@@ -0,0 +1,46 @@
+//
+// Copyright 2021 DXOS.org
+//
+
+import assert from 'node:assert';
+
+import { ClientServicesProvider } from '@dxos/client-services';
+import { Context } from '@dxos/context';
+import { log } from '@dxos/log';
+import { NetworkStatus } from '@dxos/protocols/proto/dxos/client/services';
+
+/**
+ * Public API for MESH services.
+ */
+export class Mesh {
+  private _ctx?: Context;
+  private _networkStatus?: NetworkStatus;
+
+  // prettier-ignore
+  constructor(
+      private readonly _serviceProvider: ClientServicesProvider
+  ) {}
+
+  get networkStatus() {
+    assert(this._networkStatus, 'NetworkStatus is not initialized.');
+    return this._networkStatus;
+  }
+
+  open() {
+    this._ctx = new Context({ onError: (err) => log.catch(err) });
+
+    const networkStatusStream = this._serviceProvider.services.NetworkService.subscribeNetworkStatus();
+    networkStatusStream.subscribe((networkStatus) => {
+      this._networkStatus = networkStatus;
+    });
+
+    this._ctx.onDispose(() => {
+      networkStatusStream.close();
+      this._networkStatus = undefined;
+    });
+  }
+
+  close() {
+    this._ctx?.dispose();
+  }
+}
