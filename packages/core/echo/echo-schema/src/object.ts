1730dd09441e0a3afe4b2ea9fe11696c5f91becb
Dmytro Maretskyi <maretskii@gmail.com>
2022-12-29 15:24:38 +0100

    X

--- packages/core/echo/echo-schema/src/object.ts
+++ packages/core/echo/echo-schema/src/object.ts
@@ -146,6 +146,7 @@ export class EchoObjectBase {
     if (!this._item) {
       return this._uninitialized![key];
     } else {
+      this._database?._logObjectAccess(this);
       return this._getModelProp(key);
     }
   }
@@ -154,6 +155,7 @@ export class EchoObjectBase {
     if (!this._item) {
       this._uninitialized![key] = value;
     } else {
+      this._database?._logObjectAccess(this);
       this._setModelProp(key, value);
     }
   }
