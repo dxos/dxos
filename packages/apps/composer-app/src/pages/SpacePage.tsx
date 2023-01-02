b5c16d9b3fc2a32f46e19164bf022091de1882a0
willdx <will@braneframe.com>
2023-01-02 15:25:26 -0800

    Remove redundant ComposerDocument container.

--- packages/apps/composer-app/src/pages/SpacePage.tsx
+++ packages/apps/composer-app/src/pages/SpacePage.tsx
@@ -7,15 +7,21 @@ import { useOutletContext } from 'react-router-dom';
 
 import type { Item, Space } from '@dxos/client';
 import { useSelection } from '@dxos/react-client';
+import { Composer, DOCUMENT_TYPE } from '@dxos/react-composer';
 import { Loading } from '@dxos/react-uikit';
 import type { TextModel } from '@dxos/text-model';
-
-import { ComposerDocument, COMPOSER_DOCUMENT } from '../containers';
 
 export const SpacePage = () => {
   const { space } = useOutletContext<{ space: Space }>();
 
-  const [item] = useSelection<Item<TextModel>>(space?.select().filter({ type: COMPOSER_DOCUMENT })) ?? [];
+  const [item] = useSelection<Item<TextModel>>(space?.select().filter({ type: DOCUMENT_TYPE })) ?? [];
 
-  return item ? <ComposerDocument item={item} /> : <Loading label='Loading' size='md' />;
+  return item ? (
+    <Composer
+      item={item}
+      className='z-0 rounded bg-white text-neutral-900 w-full p-4 dark:bg-neutral-850 dark:text-white min-bs-[12em]'
+    />
+  ) : (
+    <Loading label='Loading' size='md' />
+  );
 };
--- packages/apps/composer-app/src/pages/SpacesPage.tsx
+++ packages/apps/composer-app/src/pages/SpacesPage.tsx
@@ -6,9 +6,8 @@ import React, { useCallback } from 'react';
 
 import { Space } from '@dxos/client';
 import { SpacesPage as BaseSpacesPage } from '@dxos/react-appkit';
+import { DOCUMENT_TYPE } from '@dxos/react-composer';
 import { TextModel } from '@dxos/text-model';
-
-import { COMPOSER_DOCUMENT } from '../containers';
 
 export type SpacesPageProps = {};
 
@@ -16,7 +15,7 @@ export const SpacesPage = (props: SpacesPageProps) => {
   const createComposerDocument = useCallback(async (space: Space) => {
     await space.database.createItem({
       model: TextModel,
-      type: COMPOSER_DOCUMENT
+      type: DOCUMENT_TYPE
     });
   }, []);
   return <BaseSpacesPage onSpaceCreate={createComposerDocument} />;
