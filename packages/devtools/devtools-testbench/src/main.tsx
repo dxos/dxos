049313b6e766d6246e3de39e8279f8d777d4d4de
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-01-06 15:12:45 +0200

    misc

--- packages/devtools/devtools-testbench/src/main.tsx
+++ packages/devtools/devtools-testbench/src/main.tsx
@@ -1,15 +1,29 @@
 //
-// Copyright 2022 DXOS.org
+// Copyright 2020 DXOS.org
 //
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './App';
+import { App } from './app';
 
-createRoot(document.getElementById('root')!)
-  .render(
-    // <StrictMode>
-      <App />
-    // </StrictMode>
-  );
+// TODO(burdon): Next
+//  - API: space.experimental.db
+//  - View selector
+//  - Sortable task list w/ OrderedSet
+//  - Soft delete (skip filter)
+
+// TODO(burdon): Responsive Masonry (web/desktop/mobile). Multiple browsers.
+// TODO(burdon): Materialized links (referential integrity).
+// TODO(burdon): Cards (see kitchen sink).
+//  - https://cruip.com/demos/mosaic/?ref=highscore
+// TODO(burdon): Virtual table.
+// TODO(burdon): PWA/Electron.
+// TODO(burdon): Super app (WeChat/Twitter) for IPFS: https://youtu.be/zRcl77pnbgY?t=1835 (Scott Galloway)
+
+import './style.css';
+
+(() => {
+  // TODO(burdon): Get debug from config.
+  createRoot(document.getElementById('root')!).render(<App debug={false} />);
+})();
--- packages/devtools/devtools-testbench/vite.config.ts
+++ packages/devtools/devtools-testbench/vite.config.ts
@@ -6,13 +6,15 @@ import ReactPlugin from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { VitePluginFonts } from 'vite-plugin-fonts';
 
+import { ThemePlugin } from '@dxos/react-ui/plugin';
 import { ConfigPlugin } from '@dxos/config/vite-plugin';
 
 import packageJson from './package.json';
 
 const env = (value?: string) => (value ? `"${value}"` : undefined);
-const DX_RELEASE = process.env.NODE_ENV === 'production' ? `@dxos/devtools@${packageJson.version}` : undefined;
+const DX_RELEASE = process.env.NODE_ENV === 'production' ? `@dxos/tasks-app@${packageJson.version}` : undefined;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,60 +23,61 @@ export default defineConfig({
     host: true
   },
   define: {
+    'process.env.LOG_FILTER': env(process.env.LOG_FILTER),
+    'process.env.LOG_BROWSER_PREFIX': env(process.env.LOG_BROWSER_PREFIX),
+    'process.env.DX_VAULT': env(process.env.DX_VAULT),
     'process.env.DX_ENVIRONMENT': env(process.env.DX_ENVIRONMENT),
-    'process.env.DX_RELEASE': env(DX_RELEASE),
-    'process.env.SENTRY_DESTINATION': env(process.env.SENTRY_DESTINATION),
-    'process.env.TELEMETRY_API_KEY': env(process.env.TELEMETRY_API_KEY),
-    'process.env.IPDATA_API_KEY': env(process.env.IPDATA_API_KEY)
+    'process.env.DX_RELEASE': env(DX_RELEASE)
   },
   optimizeDeps: {
     force: true,
     include: [
-      '@dxos/async',
-      '@dxos/client',
-      '@dxos/client-services',
       '@dxos/config',
-      '@dxos/debug',
-      '@dxos/devtools-mesh',
-      '@dxos/feed-store',
       '@dxos/keys',
-      '@dxos/messaging',
-      '@dxos/messenger-model',
-      '@dxos/model-factory',
-      '@dxos/network-manager',
-      '@dxos/object-model',
+      '@dxos/log',
       '@dxos/protocols',
-      '@dxos/react-appkit',
-      '@dxos/react-async',
-      '@dxos/react-client',
-      '@dxos/react-components',
-      '@dxos/react-registry-client',
-      '@dxos/react-toolkit',
-      '@dxos/registry-client',
-      '@dxos/text-model',
-      '@dxos/timeframe',
-      '@dxos/util'
+      '@dxos/protocols/proto/dxos/client/services',
+      '@dxos/protocols/proto/dxos/halo/credentials',
+      '@dxos/protocols/proto/dxos/rpc',
+      '@dxos/protocols/proto/dxos/mesh/bridge',
+      '@dxos/protocols/proto/dxos/client',
+      '@dxos/protocols/proto/dxos/config',
+      '@dxos/protocols/proto/dxos/halo/keys',
+      '@dxos/protocols/proto/dxos/echo/feed',
+      '@dxos/protocols/proto/dxos/halo/invitations',
+      '@dxos/protocols/proto/dxos/echo/model/object'
     ]
   },
   build: {
+    outDir: 'out/tasks',
     commonjsOptions: {
       include: [/packages/, /node_modules/]
     }
   },
+  // TODO(burdon): Add fonts.
   plugins: [
     ConfigPlugin(),
+    ThemePlugin({
+      content: [
+        resolve(__dirname, './index.html'),
+        resolve(__dirname, './src/**/*.{js,ts,jsx,tsx}'),
+        resolve(__dirname, './node_modules/@dxos/react-ui/dist/**/*.mjs'),
+        resolve(__dirname, './node_modules/@dxos/react-uikit/dist/**/*.mjs'),
+        resolve(__dirname, './node_modules/@dxos/react-appkit/dist/**/*.mjs'),
+        resolve(__dirname, './node_modules/@dxos/react-list/dist/**/*.mjs')
+      ]
+    }),
     ReactPlugin(),
     VitePWA({
       registerType: 'autoUpdate',
-      // TODO(wittjosiah): Bundle size is massive.
       workbox: {
         maximumFileSizeToCacheInBytes: 30000000
       },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico'],
       manifest: {
-        name: 'DXOS Devtools',
-        short_name: 'Devtools',
-        description: 'DXOS Devtools Application',
+        name: 'DXOS Kai',
+        short_name: 'Kai',
+        description: 'DXOS Kai Demo',
         theme_color: '#ffffff',
         icons: [
           {
@@ -86,6 +89,33 @@ export default defineConfig({
             src: 'icons/icon-256.png',
             sizes: '256x256',
             type: 'image/png'
+          }
+        ]
+      }
+    }),
+
+    // https://www.npmjs.com/package/vite-plugin-fonts
+    // https://fonts.google.com
+    VitePluginFonts({
+      google: {
+        injectTo: 'head-prepend',
+        // prettier-ignore
+        families: [
+          'Roboto',
+          'Roboto Mono',
+          'DM Sans',
+          'DM Mono',
+          'Montserrat'
+        ]
+      },
+
+      custom: {
+        preload: false,
+        injectTo: 'head-prepend',
+        families: [
+          {
+            name: 'Sharp Sans',
+            src: 'node_modules/@dxos/assets/assets/fonts/sharp-sans/*.ttf'
           }
         ]
       }
