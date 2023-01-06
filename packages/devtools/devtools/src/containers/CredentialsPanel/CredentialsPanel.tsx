90697416581f9c113fa4843c312711ff1ca4984c
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-01-06 16:54:17 +0200

    feat(devtools): Add Credentials panel (#2216)

---
+++ packages/devtools/devtools/src/containers/CredentialsPanel/CredentialsPanel.tsx
@@ -0,0 +1,62 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import React, { useEffect, useState } from 'react';
+
+import { PublicKey } from '@dxos/keys';
+import { Credential } from '@dxos/protocols/proto/dxos/halo/credentials';
+import { useClientServices, useDevtools, useStream } from '@dxos/react-client';
+import { JsonTreeView } from '@dxos/react-components';
+
+import { KeySelect, Panel } from '../../components';
+
+export const CredentialsPanel = () => {
+  const devtoolsHost = useDevtools();
+  if (!devtoolsHost) {
+    return null;
+  }
+  const spaces = useStream(() => devtoolsHost.subscribeToSpaces({}), {}).spaces ?? [];
+
+  const [selectedSpaceKey, setSelectedSpaceKey] = useState<PublicKey>();
+
+  const services = useClientServices();
+  if (!services) {
+    return null;
+  }
+
+  const [credentials, setCredentials] = useState<Credential[]>([]);
+
+  useEffect(() => {
+    if (!selectedSpaceKey) {
+      return;
+    }
+    const stream = services.SpacesService.queryCredentials({ spaceKey: selectedSpaceKey });
+    const newCredentials: Credential[] = [];
+
+    stream.subscribe((credential) => {
+      newCredentials.push(credential);
+      setCredentials([...newCredentials]);
+    });
+
+    return () => {
+      stream.close();
+    };
+  }, [selectedSpaceKey?.toHex()]);
+
+  return (
+    <Panel
+      controls={
+        <KeySelect
+          label='Space'
+          keys={spaces.map(({ key }) => key)}
+          selected={selectedSpaceKey}
+          onChange={(key) => setSelectedSpaceKey(key)}
+          humanize={true}
+        />
+      }
+    >
+      <JsonTreeView size='small' data={credentials} />
+    </Panel>
+  );
+};
