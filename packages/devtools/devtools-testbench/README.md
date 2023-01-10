dd7b91a21b6a51fe0eae23b9f174c928df155567
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-01-10 13:55:32 +0200

    feat: Add Devtools TestBench (#2217)

---
+++ packages/devtools/devtools-testbench/README.md
@@ -0,0 +1,6 @@
+# DXOS Devtools Test-Bench
+
+Package with merged side-by-side Devtools and Kai. For rapid development of Kai and Devtools
+
+## Start
+`pnpm nx serve devtools-testbench`
---
+++ packages/devtools/devtools-testbench/src/main.tsx
@@ -0,0 +1,31 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { AppView } from '@dxos/kai';
+
+import { App } from './app';
+
+import '@dxos/kai/src/style.css';
+import './style.css';
+
+// TODO(burdon): Get debug from config.
+createRoot(document.getElementById('root')!).render(
+  <App
+    views={[
+      AppView.DASHBOARD,
+      AppView.ORGS,
+      AppView.PROJECTS,
+      AppView.CONTACTS,
+      AppView.KANBAN,
+      AppView.TASKS,
+      AppView.GRAPH,
+      AppView.EDITOR,
+      AppView.GAME
+    ]}
+    debug={false}
+  />
+);
