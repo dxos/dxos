8662b2b928a080ed3786ab4c18c6b22d276da781
willdx <will@braneframe.com>
2023-01-09 17:45:12 -0800

    Fix some button styles.

--- packages/common/react-components/src/components/Button/buttonStyles.ts
+++ packages/common/react-components/src/components/Button/buttonStyles.ts
@@ -9,7 +9,7 @@ import { ButtonProps } from './ButtonProps';
 
 export const primaryAppButtonColors = 'bg-primary-600 text-white hover:bg-primary-650';
 export const defaultAppButtonColors = 'bg-white text-neutral-900 dark:bg-neutral-750 dark:text-neutral-50';
-export const defaultOsButtonColors = 'bg-neutral-50/50 text-neutral-950 dark:bg-neutral-800/50 dark:text-neutral-50';
+export const defaultOsButtonColors = 'bg-white/50 text-neutral-900 dark:bg-neutral-800/50 dark:text-neutral-50';
 export const ghostAppButtonColors = '';
 
 export const buttonStyles = (
@@ -22,7 +22,7 @@ export const buttonStyles = (
     'inline-flex select-none items-center justify-center transition-color duration-100',
     props.compact ? 'pli-2 plb-1.5' : 'pli-4 plb-2',
     isOsTheme ? 'rounded font-system-medium text-xs' : 'rounded-md font-medium text-sm',
-    isOsTheme ? 'hover:bg-neutral-50 dark:hover:bg-neutral-800' : defaultHover(props),
+    isOsTheme ? 'hover:bg-white dark:hover:bg-neutral-800' : defaultHover(props),
     !isOsTheme && resolvedVariant === 'default' && 'border border-neutral-100 dark:border-neutral-650',
     resolvedVariant !== 'ghost' && !props.disabled && 'hover:focus:border-neutral-100 hover:border-transparent',
     resolvedVariant === 'default' && (isOsTheme ? defaultOsButtonColors : defaultAppButtonColors),
@@ -34,7 +34,7 @@ export const buttonStyles = (
     defaultFocus,
     props.disabled
       ? defaultDisabled
-      : resolvedVariant !== 'outline' && resolvedVariant !== 'ghost' && 'button-elevation',
+      : !isOsTheme && resolvedVariant !== 'outline' && resolvedVariant !== 'ghost' && 'button-elevation',
     // Register all radix states
     'group',
     defaultActive
