7c2f1997f0e44e29ff74a698b1db53be42f24f1e
willdx <will@braneframe.com>
2023-01-10 18:57:08 -0800

    Animate sidebar open & close

--- packages/apps/patterns/react-ui/src/layouts/PanelSidebar/PanelSidebar.tsx
+++ packages/apps/patterns/react-ui/src/layouts/PanelSidebar/PanelSidebar.tsx
@@ -2,10 +2,12 @@
 // Copyright 2023 DXOS.org
 //
 
+import { Transition } from '@headlessui/react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import React, {
   createContext,
   Dispatch,
+  Fragment,
   PropsWithChildren,
   ReactNode,
   SetStateAction,
@@ -49,31 +51,50 @@ export const PanelSidebarProvider = ({
 }: PropsWithChildren<PanelSidebarProviderProps>) => {
   const { t } = useTranslation('os');
   const [displayState, setDisplayState] = useState<PanelSidebarState>('hide');
+  const isOpen = displayState === 'show';
   return (
     <PanelSidebarContext.Provider value={{ setDisplayState, displayState }}>
-      <DialogPrimitive.Root open={displayState === 'show'}>
-        <DialogPrimitive.Content
-          className={mx(
-            'fixed block-start-0 block-end-0 is-[272px] z-50',
-            'bg-neutral-50 dark:bg-neutral-950',
-            inlineStart ? 'inline-start-0' : 'inline-end-0'
-          )}
+      <DialogPrimitive.Root open={isOpen}>
+        <Transition
+          show={isOpen}
+          as={Fragment}
+          enter='ease-in-out duration-200'
+          enterFrom='inline-start-[-272px]'
+          enterTo='inline-start-0'
+          leave='ease-in-out duration-200'
+          leaveFrom='inline-start-0'
+          leaveTo='inline-start-[-272px]'
         >
-          <DialogPrimitive.Title className='sr-only'>{t('sidebar label')}</DialogPrimitive.Title>
-          Hello this is sidebar
-        </DialogPrimitive.Content>
+          <DialogPrimitive.Content
+            className={mx('fixed block-start-0 block-end-0 is-[272px] z-50', 'bg-neutral-50 dark:bg-neutral-950')}
+          >
+            <DialogPrimitive.Title className='sr-only'>{t('sidebar label')}</DialogPrimitive.Title>
+            Hello this is sidebar
+          </DialogPrimitive.Content>
+        </Transition>
         {fixedBlockStartChildren && (
           <div
             role='none'
             className={mx(
-              'fixed inline-start-0 is-[100vw] block-start-0 z-[49]',
-              displayState === 'show' && 'inline-start-[272px]'
+              'fixed is-[100vw] block-start-0 z-[49] transition-[inset-inline-start,inset-inline-end] duration-200 ease-in-out',
+              isOpen ? 'inline-start-[272px]' : 'inline-start-0'
             )}
           >
             {fixedBlockStartChildren}
           </div>
         )}
-        <DialogPrimitive.Overlay className={defaultOverlay} onClick={() => setDisplayState('hide')} />
+        <Transition
+          show={isOpen}
+          as={Fragment}
+          enter='linear duration-200'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='linear duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <DialogPrimitive.Overlay className={defaultOverlay} onClick={() => setDisplayState('hide')} />
+        </Transition>
         {children}
       </DialogPrimitive.Root>
     </PanelSidebarContext.Provider>
