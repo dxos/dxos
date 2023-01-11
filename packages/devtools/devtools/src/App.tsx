d85aaea6bc7cd9a084578cabbd958962c28d6ddd
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-01-11 17:02:58 +0200

    Delete mui stuff

--- packages/devtools/devtools/src/App.tsx
+++ packages/devtools/devtools/src/App.tsx
@@ -4,20 +4,16 @@
 
 import React, { useState } from 'react';
 
-import { Box, CssBaseline, ThemeProvider } from '@mui/material';
-
 import { Client, DEFAULT_CLIENT_ORIGIN, fromHost, fromIFrame } from '@dxos/client';
 import { Config, Defaults, Dynamics } from '@dxos/config';
 import { useTelemetry } from '@dxos/react-appkit';
 import { useAsyncEffect } from '@dxos/react-async';
 import { ClientContext } from '@dxos/react-client';
-import { FullScreen } from '@dxos/react-components-deprecated';
 import { ErrorBoundary } from '@dxos/react-toolkit';
 
 import { PanelsContainer } from './containers';
 import { ClientAndServices } from './initialize';
 import { sections } from './sections';
-import { theme } from './theme';
 
 const DEFAULT_TARGET = `vault:${DEFAULT_CLIENT_ORIGIN}`;
 
@@ -82,18 +78,11 @@ export const App = () => {
 
   return (
     <ErrorBoundary>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <FullScreen>
-          <ClientContext.Provider value={value}>
-            <Telemetry />
+      <ClientContext.Provider value={value}>
+        <Telemetry />
 
-            <Box sx={{ display: 'flex', flex: 1, overflow: 'hidden' }}>
-              <PanelsContainer sections={sections} />
-            </Box>
-          </ClientContext.Provider>
-        </FullScreen>
-      </ThemeProvider>
+        <PanelsContainer sections={sections} />
+      </ClientContext.Provider>
     </ErrorBoundary>
   );
 };
