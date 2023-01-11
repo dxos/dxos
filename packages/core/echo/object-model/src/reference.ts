3b76229d17a77c5bc209937a9205d2f578c33028
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-01-11 13:00:17 +0200

    feat(object-model): Add Reference (#2209)

---
+++ packages/core/echo/object-model/src/reference.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { ItemID } from '@dxos/protocols';
+
+export class Reference {
+  // prettier-ignore
+  constructor(
+    public readonly itemId: ItemID
+  ) {}
+}
