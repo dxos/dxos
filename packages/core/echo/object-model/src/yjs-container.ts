e10e23c43296c09f03ae99bce7582b94581a0987
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-01-12 12:51:53 +0200

    Add copyright

--- packages/core/echo/object-model/src/yjs-container.ts
+++ packages/core/echo/object-model/src/yjs-container.ts
@@ -1,21 +1,25 @@
-import { YJS } from '@dxos/protocols/proto/dxos/echo/model/object'
-import assert from 'assert'
-import * as Y from 'yjs'
+//
+// Copyright 2023 DXOS.org
+//
 
-const ARRAY_KEY = 'a'
+import { YJS } from '@dxos/protocols/proto/dxos/echo/model/object';
+import assert from 'assert';
+import * as Y from 'yjs';
+
+const ARRAY_KEY = 'a';
 
 export class OrderedArray {
   static fromSnapshot(snapshot: YJS) {
-    const doc = new Y.Doc()
-    Y.applyUpdateV2(doc, snapshot.payload)
-    return new OrderedArray(doc, doc.getArray(ARRAY_KEY))
+    const doc = new Y.Doc();
+    Y.applyUpdateV2(doc, snapshot.payload);
+    return new OrderedArray(doc, doc.getArray(ARRAY_KEY));
   }
 
   static fromValues(values: any[]) {
-    const doc = new Y.Doc()
-    const array = doc.getArray(ARRAY_KEY)
-    array.insert(0, values)
-    return new OrderedArray(doc, array)
+    const doc = new Y.Doc();
+    const array = doc.getArray(ARRAY_KEY);
+    array.insert(0, values);
+    return new OrderedArray(doc, array);
   }
 
   // prettier-ignore
@@ -28,36 +32,36 @@ export class OrderedArray {
     return {
       id: new Uint8Array(),
       payload: Y.encodeStateAsUpdateV2(this.doc)
-    }
+    };
   }
 
   toArray() {
-    return this.array.toArray() 
+    return this.array.toArray();
   }
 
   transact(tx: () => void): YJS {
-    let updateReceived: YJS | undefined
+    let updateReceived: YJS | undefined;
     const cb = (update: any) => {
       updateReceived = {
         id: new Uint8Array(),
         payload: update
-      }
-    }
-    
+      };
+    };
+
     try {
-      this.doc.once('updateV2', cb)
+      this.doc.once('updateV2', cb);
       this.doc.transact(() => {
-        tx()
-      })
+        tx();
+      });
     } finally {
-      this.doc.off('updateV2', cb)
+      this.doc.off('updateV2', cb);
     }
 
-    assert(updateReceived)
-    return updateReceived
+    assert(updateReceived);
+    return updateReceived;
   }
 
   apply(mutation: YJS) {
-    Y.applyUpdateV2(this.doc, mutation.payload)
+    Y.applyUpdateV2(this.doc, mutation.payload);
   }
 }
