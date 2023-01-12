63c2df3315cf0b85a86f668286cfb879ab2be3da
Josiah Witt <josiah@witt.life>
2023-01-12 09:19:46 -0500

    fix: stop SpaceList from automatically creating invitations

--- packages/experimental/kai/src/containers/SpaceList.tsx
+++ packages/experimental/kai/src/containers/SpaceList.tsx
@@ -2,52 +2,16 @@
 // Copyright 2022 DXOS.org
 //
 
-import { Planet, ShareNetwork } from 'phosphor-react';
-import React, { useEffect, useState } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Link, useHref, useParams } from 'react-router-dom';
+import { Planet } from 'phosphor-react';
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 
-import { Invitation, CancellableInvitationObservable, InvitationEncoder } from '@dxos/client';
-import { PublicKey } from '@dxos/keys';
 import { useSpaces } from '@dxos/react-client';
 import { getSize, mx } from '@dxos/react-components';
-
-import { Button } from '../components';
-import { useSpace } from '../hooks';
 
 export const SpaceList = () => {
   const { spaceKey: currentSpaceKey, view } = useParams();
   const spaces = useSpaces();
-  const { space } = useSpace();
-  const [observable, setObservable] = useState<CancellableInvitationObservable>();
-
-  useEffect(() => {
-    const swarmKey = PublicKey.random();
-    const observable = space.createInvitation({
-      swarmKey,
-      type: Invitation.Type.MULTIUSE_TESTING
-    });
-
-    const unsubscribe = observable.subscribe({
-      onConnecting: () => {
-        setObservable(observable);
-      },
-      onConnected: () => {},
-      onSuccess: () => {},
-      onError: (error) => {
-        console.error(error);
-      }
-    });
-
-    return () => {
-      unsubscribe();
-      void observable.cancel();
-      setObservable(undefined);
-    };
-  }, [space]);
-
-  // TODO(burdon): Constant re-render after connected: space updated?
-  const url = useHref(observable ? `/join/${InvitationEncoder.encode(observable.invitation!)}` : '/');
 
   return (
     <div className='flex flex-col'>
@@ -62,15 +26,6 @@ export const SpaceList = () => {
           <div className='flex flex-1 font-mono cursor-pointer'>
             <Link to={`/${space.key.truncate()}/${view}`}>{space.key.truncate()}</Link>
           </div>
-          {space.key.truncate() === currentSpaceKey && (
-            <div className='flex items-center'>
-              <CopyToClipboard text={window.origin + '/' + url}>
-                <Button>
-                  <ShareNetwork className={mx(getSize(5), 'cursor-pointer')} />
-                </Button>
-              </CopyToClipboard>
-            </div>
-          )}
         </div>
       ))}
     </div>
