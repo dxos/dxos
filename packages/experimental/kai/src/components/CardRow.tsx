3fedfd4dec69d06b54869df82b487cd1d44a575e
Rich Burdon <rich.burdon@gmail.com>
2023-01-14 13:43:02 -0500

    feat: Fix input (#2279)
    Co-authored-by: Mykola Veremchuk <veremchukmukola@gmail.com>

--- packages/experimental/kai/src/components/CardRow.tsx
+++ packages/experimental/kai/src/components/CardRow.tsx
@@ -11,12 +11,13 @@ export const CardRow: FC<{ sidebar?: ReactNode; action?: ReactNode; header: Reac
   children
 }) => {
   return (
-    <div className='flex flex-col p-1 pl-2 pr-2'>
+    <div className='flex flex-col w-full p-1 pl-2 pr-2'>
       <div className='flex items-center'>
         {sidebar && <div className='flex flex-shrink-0 mr-2'>{sidebar}</div>}
         <div className='flex flex-1'>{header}</div>
         {action && <div className='flex ml-2'>{action}</div>}
       </div>
+
       {children && <div className='flex flex-col'>{children}</div>}
     </div>
   );
