7151f50f1d43c1e8cccda60652fc793e9d9b746f
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2023-01-15 16:23:12 +0100

    feat: toJSON for schemaless echo objects (#2262)

--- packages/core/echo/echo-schema/src/defs.ts
+++ packages/core/echo/echo-schema/src/defs.ts
@@ -4,6 +4,7 @@
 
 export const id = Symbol('id');
 export const schema = Symbol('schema');
+export const type = Symbol('type');
 export const proxy = Symbol('proxy');
 export const deleted = Symbol('deleted');
 export const base = Symbol('base');
