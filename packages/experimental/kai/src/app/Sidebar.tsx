e8750053a078ac40439edd402dd5935472369476
Will Shown <thure@users.noreply.github.com>
2023-01-16 16:03:10 -0800

    fix: Sidebar layout & motion (#2290)

--- packages/experimental/kai/src/app/Sidebar.tsx
+++ packages/experimental/kai/src/app/Sidebar.tsx
@@ -2,7 +2,7 @@
 // Copyright 2022 DXOS.org
 //
 
-import { PlusCircle } from 'phosphor-react';
+import { PlusCircle, X } from 'phosphor-react';
 import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -43,12 +43,22 @@ export const Sidebar = () => {
   };
 
   return (
-    <div className='flex flex-1 flex-col overflow-auto min-bs-full'>
+    <div
+      role='none'
+      className='flex flex-col overflow-auto min-bs-full backdrop-blur bg-neutral-50/[.33] dark:bg-neutral-950/[.33]'
+    >
+      <div className='flex justify-end bg-orange-500/50 lg:bg-orange-400'>
+        <Button onClick={toggleSidebar} className='p-3'>
+          <span className='sr-only'>Close sidebar</span>
+          <X className={getSize(6)} />
+        </Button>
+      </div>
       {/* Spaces */}
-      <div className='flex flex-shrink-0 flex-col overflow-y-scroll'>
+      <div className='flex shrink-0 flex-col overflow-y-scroll'>
         <SpaceList />
         <div className='p-3'>
           <Button className='flex' title='Create new space' onClick={handleCreateSpace}>
+            <span className='sr-only'>Create new space</span>
             <PlusCircle className={getSize(6)} />
           </Button>
         </div>
@@ -57,9 +67,9 @@ export const Sidebar = () => {
       <div className='flex flex-1'></div>
 
       {/* Members */}
-      <div className='flex flex-col flex-shrink-0 mt-6'>
+      <div className='flex flex-col shrink-0 mt-6'>
         <div className='flex p-1 pl-3 mb-2 text-xs'>Members</div>
-        <div className='flex flex-shrink-0 pl-3'>
+        <div className='flex shrink-0 pl-3'>
           <MemberList spaceKey={space.key} />
         </div>
       </div>
