1ac10045e1ba4414f6ead6f56b7bfffff936db02
thure <will@braneframe.com>
2023-01-20 15:28:26 -0800

    Create stub reducer for join flow.

--- packages/apps/patterns/react-ui/src/panels/JoinPanel/JoinPanel.tsx
+++ packages/apps/patterns/react-ui/src/panels/JoinPanel/JoinPanel.tsx
@@ -2,16 +2,71 @@
 // Copyright 2023 DXOS.org
 //
 import * as AlertPrimitive from '@radix-ui/react-alert-dialog';
-import React from 'react';
+import React, { useReducer } from 'react';
 
+import type { Profile } from '@dxos/client';
 import { ThemeContext, useId } from '@dxos/react-components';
 
-import { JoinSpaceHeading, JoinSpaceHeadingProps } from './JoinSpaceHeading';
+import { JoinSpaceHeading } from './JoinSpaceHeading';
 
-export type JoinPanelProps = Pick<JoinSpaceHeadingProps, 'spaceTitle'>;
+type Space = { properties: { title: string } };
 
-export const JoinPanel = ({ spaceTitle }: JoinPanelProps) => {
+export interface JoinPanelProps {
+  space: Space;
+  deviceHasIdentities?: boolean;
+}
+
+interface IdentityAction {
+  type: 'select identity' | 'added identity';
+  identity: Profile;
+}
+
+interface EmptyJoinAction {
+  type: 'deselect identity' | 'cancel addition' | 'add identity';
+}
+
+interface AdditionMethodAction {
+  type: 'select addition method';
+  method: 'restore identity' | 'accept device invitation' | 'create identity';
+}
+
+type JoinAction = IdentityAction | EmptyJoinAction | AdditionMethodAction;
+
+type JoinView =
+  | 'identities enum'
+  | 'addition method enum'
+  | 'restore identity init'
+  | 'accept device invitation'
+  | 'create identity init'
+  | 'identity added'
+  | 'accept space invitation';
+
+interface JoinState {
+  activeView: JoinView;
+  space: Space;
+  selectedIdentity: Profile | null;
+}
+
+export const JoinPanel = ({ space, deviceHasIdentities }: JoinPanelProps) => {
   const titleId = useId('joinTitle');
+
+  const spaceTitle = space.properties.title;
+
+  const reducer = (state: JoinState, action: JoinAction) => {
+    switch (action.type) {
+      case 'add identity':
+        break;
+      // todo: fill this in with the correct logic
+    }
+    return state;
+  };
+
+  const [_joinState, _dispatchJoinAction] = useReducer(reducer, {
+    space: { properties: { title: spaceTitle } },
+    activeView: deviceHasIdentities ? 'identities enum' : 'addition method enum',
+    selectedIdentity: null
+  });
+
   return (
     <AlertPrimitive.Root defaultOpen>
       <ThemeContext.Provider value={{ themeVariant: 'os' }}>
@@ -23,6 +78,7 @@ export const JoinPanel = ({ spaceTitle }: JoinPanelProps) => {
           <div role='none' className='is-full max-is-[480px]'>
             <JoinSpaceHeading titleId={titleId} spaceTitle={spaceTitle} onClickExit={() => {}} />
           </div>
+          {/* todo: choose what to render */}
         </AlertPrimitive.Content>
       </ThemeContext.Provider>
     </AlertPrimitive.Root>
