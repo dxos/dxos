816789f4df17e71dfa1e2e5d99eaa289a9705f3a
Will Shown <thure@users.noreply.github.com>
2023-01-20 12:02:46 -0800

    fix: Restore correct `ThemeConfig` in `devtools` (#2328)

---
+++ packages/experimental/kai/theme-extensions.js
@@ -0,0 +1,16 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+const { osThemeExtension } = require('@dxos/react-ui/theme-extensions');
+
+module.exports = {
+  osThemeExtension,
+  kaiThemeExtension: {
+    spacing: {
+      appbar: '48px',
+      framepicker: '36px',
+      topbars: '84px'
+    }
+  }
+};
