01a4463e4b42310f4b2b553da34ae3267bda8bbe
Zhenya <zhenya@braneframe.com>
2023-01-25 15:39:25 -0500

    wip tasks app upgrades

---
+++ packages/apps/tasks-app/src/components/TaskList.tsx
@@ -0,0 +1,71 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { Plus } from 'phosphor-react';
+import React from 'react';
+
+import { Button, getSize, Loading } from '@dxos/react-components';
+
+import { CheckboxItem, Input, List } from '.';
+import { TaskList, Task } from '../proto';
+import { id } from '@dxos/echo-schema';
+
+export type TaskListProps = {
+  taskList: TaskList;
+  onTitleChanged?: (title: string) => any;
+  onTaskCreate?: () => any;
+  onTaskTitleChanged?: (task: Task, title: string) => any;
+  onTaskCompleteChanged?: (task: Task, completed: boolean) => any;
+  onTaskDeleted?: (task: Task) => any;
+};
+
+export const TaskListComponent = (props: TaskListProps) => {
+  const { taskList, onTitleChanged, onTaskCreate, onTaskTitleChanged, onTaskCompleteChanged, onTaskDeleted } = props;
+  if (!taskList) {
+    return <Loading label='Loading' />;
+  }
+  const empty = <div className='py-5 px-3 text-neutral-500'>There are no tasks to show</div>;
+  console.log(taskList);
+  return (
+    <div role='none' className='my-5 py-2 px-6 bg-white dark:bg-neutral-700/50 rounded shadow'>
+      <div>
+        <Input
+          className='text-xl'
+          placeholder='Title this list ...'
+          defaultValue={taskList.title ?? ''}
+          onBlur={(e) => onTitleChanged?.(e.target.value)}
+        />
+      </div>
+      <List empty={empty}>
+        {(taskList.tasks ?? [])?.map((task) => (
+          <CheckboxItem
+            key={task[id]}
+            {...{
+              placeholder: 'type here',
+              text: task.title,
+              isChecked: task.completed,
+              onChecked: (completed) => onTaskCompleteChanged?.(task, completed),
+              onTextChanged: (title) => onTaskTitleChanged?.(task, title),
+              onDeleteClicked: () => onTaskDeleted?.(task),
+              onInputKeyUp: (e) => {
+                if (e.key === 'Enter') {
+                  // TODO: go to next item or create new
+                } else if (e.key === 'Up') {
+                  // TODO: go up one item
+                } else if (e.key === 'Down') {
+                  // TODO: go to next item or create new
+                }
+              }
+            }}
+          />
+        ))}
+      </List>
+      <div role='none' className='my-5'>
+        <Button className='rounded-full p-3 border-none' onClick={() => onTaskCreate?.()}>
+          <Plus className={getSize(5)} />
+        </Button>
+      </div>
+    </div>
+  );
+};
