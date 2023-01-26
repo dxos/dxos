765fd5ba4ef2586002086607732cc9f09ce29072
Josiah Witt <josiah@witt.life>
2023-01-26 14:58:35 -0500

    fix: build

--- packages/experimental/kai/src/main.tsx
+++ packages/experimental/kai/src/main.tsx
@@ -17,11 +17,17 @@ import '../style.css';
 
 const bool = (str?: string): boolean => (str ? /(true|1)/i.test(str) : false);
 
+// TODO(wittjosiah): Migrate to ES Modules.
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 const initialState: AppState = {
+  // @ts-ignore
   dev: bool(import.meta.env.VITE_DEV),
+  // @ts-ignore
   debug: bool(import.meta.env.VITE_DEBUG),
+  // @ts-ignore
   pwa: bool(import.meta.env.VITE_PWA)
 };
+/* eslint-enable @typescript-eslint/ban-ts-comment */
 
 /**
  * Progressive web app registration and notifications.
