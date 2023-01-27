ab90283ab14e99e0c066594db8130949203fbdcb
Josiah Witt <josiah@witt.life>
2023-01-27 11:05:57 -0500

    fix: sentry reporting

---
+++ packages/apps/patterns/react-appkit/src/telemetry/telemetry.ts
@@ -0,0 +1,73 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import type { Client, Config } from '@dxos/client';
+import * as Sentry from '@dxos/sentry';
+import * as Telemetry from '@dxos/telemetry';
+import { humanize } from '@dxos/util';
+
+export const BASE_TELEMETRY_PROPERTIES: any = {};
+
+if (navigator.storage?.estimate) {
+  setInterval(async () => {
+    const storageEstimate = await navigator.storage.estimate();
+    BASE_TELEMETRY_PROPERTIES.storageUsage = storageEstimate.usage;
+    BASE_TELEMETRY_PROPERTIES.storageQuota = storageEstimate.quota;
+  }, 10e3);
+}
+
+// TODO(wittjosiah): Store uuid in halo for the purposes of usage metrics.
+// await client.halo.getGlobalPreference('dxosTelemetryIdentifier');
+export const getTelemetryIdentifier = (client: Client) => {
+  const profile = client.halo.profile;
+  if (profile) {
+    humanize(profile.identityKey);
+  }
+
+  return undefined;
+};
+
+export const isTelemetryDisabled = (namespace: string) =>
+  localStorage.getItem(`${namespace}:telemetry-disabled`) === 'true';
+
+// TODO(wittjosiah): Store preference for disabling telemetry.
+//   At minimum should be stored locally (i.e., localstorage), possibly in halo preference.
+//   Needs to be hooked up to settings page for user visibility.
+export const initializeAppTelemetry = async (namespace: string, config: Config) => {
+  const release = `${namespace}@${config.get('runtime.app.build.version')}`;
+  const environment = config.get('runtime.app.env.DX_ENVIRONMENT');
+  BASE_TELEMETRY_PROPERTIES.release = release;
+  BASE_TELEMETRY_PROPERTIES.environment = environment;
+  const telemetryDisabled = isTelemetryDisabled(namespace);
+
+  const SENTRY_DESTINATION = config.get('runtime.app.env.DX_SENTRY_DESTINATION');
+  Sentry.init({
+    enable: Boolean(SENTRY_DESTINATION) && !telemetryDisabled,
+    destination: SENTRY_DESTINATION,
+    environment,
+    release,
+    // TODO(wittjosiah): Configure this.
+    sampleRate: 1.0
+  });
+
+  const TELEMETRY_API_KEY = config.get('runtime.app.env.DX_TELEMETRY_API_KEY');
+  Telemetry.init({
+    apiKey: TELEMETRY_API_KEY,
+    enable: Boolean(TELEMETRY_API_KEY) && !telemetryDisabled
+  });
+
+  const IPDATA_API_KEY = config.get('runtime.app.env.DX_IPDATA_API_KEY');
+  if (IPDATA_API_KEY) {
+    await fetch(`https://api.ipdata.co?api-key=${IPDATA_API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        BASE_TELEMETRY_PROPERTIES.city = data.city;
+        BASE_TELEMETRY_PROPERTIES.region = data.region;
+        BASE_TELEMETRY_PROPERTIES.country = data.country;
+        BASE_TELEMETRY_PROPERTIES.latitude = data.latitude;
+        BASE_TELEMETRY_PROPERTIES.longitude = data.longitude;
+      })
+      .catch((err) => Sentry.captureException(err));
+  }
+};
--- packages/common/sentry/src/node.ts
+++ packages/common/sentry/src/node.ts
@@ -2,15 +2,27 @@
 // Copyright 2022 DXOS.org
 //
 
-import { init as sentryInit, setTag } from '@sentry/node';
+import {
+  init as naturalInit,
+  addBreadcrumb as naturalAddBreadcrumb,
+  captureException as naturalCaptureException,
+  setTag
+} from '@sentry/node';
 import type { Event } from '@sentry/node';
+
+import { log } from '@dxos/log';
 
 import { InitOptions } from './types';
 
-export { captureException } from '@sentry/node';
-
+/**
+ * To use this SDK, call the init function as early as possible in the main entry module.
+ * To set context information or send manual events, use the provided methods.
+ *
+ * @param options {InitOptions}
+ */
 export const init = (options: InitOptions) => {
-  sentryInit({
+  naturalInit({
+    enabled: options.enable ?? true,
     dsn: options.destination,
     serverName: options.installationId,
     release: options.release,
@@ -37,4 +49,31 @@ const scrub = (event: Event) => {
       frame.filename = filename && filename[filename.length - 1];
     });
   });
+};
+
+/**
+ * Records a new breadcrumb which will be attached to future events.
+ *
+ * Breadcrumbs will be added to subsequent events to provide more context on user's actions prior to an error or crash.
+ *
+ * https://docs.sentry.io/platforms/javascript/enriching-events/breadcrumbs/
+ *
+ * @param breadcrumb — The breadcrumb to record.
+ */
+export const addBreadcrumb: typeof naturalAddBreadcrumb = (breadcrumb) => {
+  naturalAddBreadcrumb(breadcrumb);
+  log('add breadcrumb', breadcrumb);
+};
+
+/**
+ * Captures an exception event and sends it to Sentry.
+ *
+ * @param exception — An exception-like object.
+ * @param captureContext — Additional scope data to apply to exception event.
+ * @returns — The generated eventId.
+ */
+export const captureException: typeof naturalCaptureException = (exception, captureContext) => {
+  const eventId = naturalCaptureException(exception, captureContext);
+  log('capture exception', { exception, eventId, ...captureContext });
+  return eventId;
 };
