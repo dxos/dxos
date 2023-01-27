a40fbc5aead0340162d83f2e74df4c7a455a19bf
Josiah Witt <josiah@witt.life>
2023-01-27 16:38:55 -0500

    fix: config/telemetry race condition in bundles

--- packages/apps/composer-app/src/main.tsx
+++ packages/apps/composer-app/src/main.tsx
@@ -2,10 +2,16 @@
 // Copyright 2020 DXOS.org
 //
 
-import '@dxosTheme';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '@dxosTheme';
+import { Config, Defaults } from '@dxos/config';
+import { initializeAppTelemetry } from '@dxos/react-appkit';
+
 import { App } from './App';
+import { namespace } from './Routes';
+
+void initializeAppTelemetry(namespace, new Config(Defaults()));
 
 createRoot(document.getElementById('root')!).render(<App />);
