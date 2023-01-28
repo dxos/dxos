5e49c633e5f487b56f15bbeb73916dbfcb7378f5
Josiah Witt <josiah@witt.life>
2023-01-27 19:07:33 -0500

    docs: add loader for showcase preview (#2364)

--- docs/src/components/Showcase.vue
+++ docs/src/components/Showcase.vue
@@ -1,28 +1,64 @@
 <template>
-  <!-- TODO(wittjosiah): Move suspense to app-level? -->
+  <div class="showcase-preview">
   <Suspense>
-    <ClientOnly>
-      <!-- TODO(wittjosiah): CodeSandbox not respecting patch-package from api. -->
-      <RenderReactDemo
-        :demo="demo"
-        :fork="false"
-      />
-    </ClientOnly>
+    <!-- TODO(wittjosiah): CodeSandbox not respecting patch-package from api. -->
+    <RenderReactDemo
+      :demo="demo"
+      :fork="false"
+    />
+
+    <template #fallback>
+      <div class="showcase-loading" ref="loading">
+        Loading...
+      </div>
+    </template>
   </Suspense>
+  </div>
 </template>
 
-<script lang='ts'>
-  import { defineComponent } from 'vue';
+<style>
+.showcase-preview {
+  border: 1px solid var(--c-border);
+  border-radius: 6px;
+  padding: 1em;
+  position: relative;
+}
+
+.showcase-loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+
+  transition-property: opacity;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+}
+
+.showcase-loading.show {
+  opacity: 1;
+}
+</style>
+
+<script setup lang='ts'>
+  import { onMounted, ref } from 'vue';
 
   import RenderReactDemo from './RenderReactDemo.vue';
 
-  export default defineComponent({
-    components: { RenderReactDemo },
-    props: {
-      demo: {
-        type: String,
-        required: true
-      }
+  defineProps({
+    demo: {
+      type: String,
+      required: true
     }
+  });
+
+  const loading = ref<HTMLDivElement | null>(null);
+
+  onMounted(() => {
+    setTimeout(() => {
+      if (loading.value) {
+        loading.value.classList.add('show');
+      }
+    }, 300);
   });
 </script>
