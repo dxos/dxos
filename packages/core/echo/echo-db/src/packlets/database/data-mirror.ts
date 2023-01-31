c3e61d8954fc3e575c7e6b92f516684428030d9d
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2023-01-31 13:01:23 +0100

    refactor: Data service (#2407)
    Co-authored-by: Mykola Veremchuk <veremchukmukola@gmail.com>

--- packages/core/echo/echo-db/src/packlets/database/data-mirror.ts
+++ packages/core/echo/echo-db/src/packlets/database/data-mirror.ts
@@ -6,8 +6,8 @@ import debug from 'debug';
 import assert from 'node:assert';
 
 import { Trigger } from '@dxos/async';
+import { failUndefined } from '@dxos/debug';
 import { PublicKey } from '@dxos/keys';
-import { Model } from '@dxos/model-factory';
 import { MutationMetaWithTimeframe } from '@dxos/protocols';
 import { DataService } from '@dxos/protocols/proto/dxos/echo/service';
 
@@ -35,28 +35,44 @@ export class DataMirror {
   async open() {
     const loaded = new Trigger();
 
-    const entities = this._dataService.subscribeEntitySet({
+    const entities = this._dataService.subscribe({
       spaceKey: this._spaceKey
     });
     entities.subscribe(
-      async (diff) => {
-        loaded.wake();
-        for (const addedEntity of diff.added ?? []) {
-          log(`Construct: ${JSON.stringify(addedEntity)}`);
-          assert(addedEntity.itemId);
-          assert(addedEntity.genesis);
-          assert(addedEntity.genesis.modelType);
+      async (msg) => {
+        for (const object of msg.objects ?? []) {
+          assert(object.itemId);
 
-          const entity = await this._itemManager.constructItem({
-            itemId: addedEntity.itemId,
-            itemType: addedEntity.genesis.itemType,
-            modelType: addedEntity.genesis.modelType,
-            parentId: addedEntity.itemMutation?.parentId,
-            snapshot: { itemId: addedEntity.itemId }
-          });
+          let entity: Entity<any>;
+          if (object.genesis) {
+            log('Construct', { object });
+            assert(object.genesis.modelType);
+            entity = await this._itemManager.constructItem({
+              itemId: object.itemId,
+              itemType: object.genesis.itemType,
+              modelType: object.genesis.modelType,
+              parentId: object.itemMutation?.parentId,
+              snapshot: { itemId: object.itemId } // TODO(dmaretskyi): Fix.
+            });
+          } else {
+            entity = (await this._itemManager.entities.get(object.itemId)) ?? failUndefined();
+          }
 
-          this._subscribeToUpdates(entity);
+          if (object.snapshot) {
+            log('reset to snapshot', { object });
+            entity._stateManager.resetToSnapshot(object);
+          } else if (object.mutations) {
+            for (const mutation of object.mutations) {
+              log('mutate', { id: object.itemId, mutation });
+              assert(mutation.meta);
+              assert(mutation.meta.timeframe, 'Mutation timeframe is required.');
+              await entity._stateManager.processMessage(mutation.meta as MutationMetaWithTimeframe, mutation.mutation);
+            }
+          }
         }
+
+        // Notify that initial set of items has been loaded.
+        loaded.wake();
       },
       (err) => {
         log(`Connection closed: ${err}`);
@@ -65,29 +81,9 @@ export class DataMirror {
 
     // Wait for initial set of items.
     await loaded.wait();
-  }
 
-  private _subscribeToUpdates(entity: Entity<Model<any>>) {
-    const stream = this._dataService.subscribeEntityStream({
-      spaceKey: this._spaceKey,
-      itemId: entity.id
-    });
-    stream.subscribe(
-      async (update) => {
-        log(`Update[${entity.id}]: ${JSON.stringify(update)}`);
-        if (update.object.snapshot) {
-          entity._stateManager.resetToSnapshot(update.object);
-        } else if (update.object.mutations) {
-          for (const mutation of update.object.mutations) {
-            assert(mutation.meta);
-            assert(mutation.meta.timeframe, 'Mutation timeframe is required.');
-            await entity._stateManager.processMessage(mutation.meta as MutationMetaWithTimeframe, mutation.mutation);
-          }
-        }
-      },
-      (err) => {
-        log(`Connection closed: ${err}`);
-      }
-    );
+    return () => {
+      entities.close();
+    };
   }
 }
--- packages/core/echo/echo-db/src/packlets/database/data-service-host.ts
+++ packages/core/echo/echo-db/src/packlets/database/data-service-host.ts
@@ -6,20 +6,12 @@ import debug from 'debug';
 import assert from 'node:assert';
 
 import { Stream } from '@dxos/codec-protobuf';
-import { failUndefined, raise } from '@dxos/debug';
 import { FeedWriter } from '@dxos/feed-store';
 import { PublicKey } from '@dxos/keys';
-import { ItemID } from '@dxos/protocols';
 import { DataMessage } from '@dxos/protocols/proto/dxos/echo/feed';
 import { EchoObject } from '@dxos/protocols/proto/dxos/echo/object';
-import {
-  MutationReceipt,
-  SubscribeEntitySetResponse,
-  SubscribeEntityStreamRequest,
-  SubscribeEntityStreamResponse
-} from '@dxos/protocols/proto/dxos/echo/service';
+import { MutationReceipt, SubscribeResponse } from '@dxos/protocols/proto/dxos/echo/service';
 
-import { EntityNotFoundError } from '../errors';
 import { Item } from './item';
 import { ItemDemuxer } from './item-demuxer';
 import { ItemManager } from './item-manager';
@@ -39,95 +31,54 @@ export class DataServiceHost {
   ) {}
 
   /**
-   * Returns a stream with a list of active entities in the space.
+   * Real-time subscription to data objects in a space.
    */
-  subscribeEntitySet(): Stream<SubscribeEntitySetResponse> {
-    return new Stream(({ next }) => {
-      const trackedSet = new Set<ItemID>();
+  subscribe(): Stream<SubscribeResponse> {
+    return new Stream(({ next, ctx }) => {
+      // send current state
+      const objects: EchoObject[] = Array.from(this._itemManager.entities.values()).map((entity) => {
+        assert(entity instanceof Item);
 
-      const entityInfo = (id: ItemID): EchoObject => {
-        const entity = this._itemManager.entities.get(id) ?? failUndefined();
+        // TODO(dmaretskyi): Extract this to a method on Entity.
+        const { snapshot, mutations } = entity._stateManager.createSnapshot();
+
         return {
-          itemId: id,
+          itemId: entity.id,
           genesis: {
             itemType: entity.type,
             modelType: entity.modelType
           },
-          itemMutation:
-            entity instanceof Item
-              ? {
-                  parentId: entity.parent?.id
-                }
-              : undefined
+          snapshot,
+          mutations,
+          itemMutation: {
+            parentId: entity.parent?.id
+          }
         };
-      };
+      });
 
-      const update = () => {
-        const added = new Set<ItemID>();
-        const deleted = new Set<ItemID>();
+      next({
+        objects
+      });
 
-        for (const entity of this._itemManager.entities.keys()) {
-          if (!trackedSet.has(entity)) {
-            added.add(entity);
-            trackedSet.add(entity);
-          }
-        }
+      // subscribe to mutations
 
-        for (const entity of trackedSet) {
-          if (!this._itemManager.entities.has(entity)) {
-            deleted.add(entity);
-            trackedSet.delete(entity);
-          }
-        }
-
+      this._itemDemuxer.mutation.on(ctx, (mutation) => {
+        log('Object update', { mutation });
         next({
-          added: Array.from(added).map((id) => entityInfo(id)),
-          deleted: Array.from(added).map((id): EchoObject => ({ itemId: id }))
-        });
-      };
-
-      update();
-      return this._itemManager.debouncedUpdate.on(update);
-    });
-  }
-
-  /**
-   * Returns a stream of uppdates for a single entity.
-   *
-   * First message is a snapshot of the entity.
-   * Subsequent messages are updates.
-   */
-  subscribeEntityStream(request: SubscribeEntityStreamRequest): Stream<SubscribeEntityStreamResponse> {
-    return new Stream(({ next }) => {
-      assert(request.itemId);
-      const entityItem = this._itemManager.items.find((item) => item.id === request.itemId);
-      if (!entityItem) {
-        raise(new EntityNotFoundError(request.itemId));
-      }
-      const snapshot = this._itemDemuxer.createItemSnapshot(entityItem as Item);
-
-      log(`Entity stream ${request.itemId}: ${JSON.stringify({ snapshot })}`);
-      next({ object: snapshot });
-
-      return this._itemDemuxer.mutation.on((mutation) => {
-        if (mutation.data.itemId !== request.itemId) {
-          return;
-        }
-
-        log(`Entity stream ${request.itemId}: ${JSON.stringify({ mutation })}`);
-        // assert(mutation.data.mutations?.length === 1, 'Only single mutation per item supported');
-        next({
-          object: {
-            mutations: mutation.data.mutations?.map((m) => ({
-              mutation: m.mutation,
-              meta: {
-                feedKey: PublicKey.from(mutation.meta.feedKey),
-                memberKey: PublicKey.from(mutation.meta.memberKey),
-                seq: mutation.meta.seq,
-                timeframe: mutation.meta.timeframe
-              }
-            }))
-          }
+          objects: [
+            {
+              ...mutation.data,
+              mutations: mutation.data.mutations?.map((m) => ({
+                mutation: m.mutation,
+                meta: {
+                  feedKey: PublicKey.from(mutation.meta.feedKey),
+                  memberKey: PublicKey.from(mutation.meta.memberKey),
+                  seq: mutation.meta.seq,
+                  timeframe: mutation.meta.timeframe
+                }
+              }))
+            }
+          ]
         });
       });
     });
