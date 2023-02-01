d6378728b85e764f90f8bd45286eab3f8d2d87e2
quaisx <alex@braneframe.com>
2023-02-01 16:36:51 -0500

    checkout from the proper branch

--- packages/gravity/gravity-k8s/run_agent.sh
+++ packages/gravity/gravity-k8s/run_agent.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 TIMESTAMP=`date "+%Y%m%d-%H%M%S"`
+
 cp ./config/config-${GRAVITY_TEST_SCENARIO}.yml ./config/config.yml
-mkdir -P /tmp/dxos
-export LOG_FILE=/tmp/dxos/${GRAVITY_TEST_SCENARIO}.${TIMESTAMP}.${$}.log
-nohup /opt/filebeat-8.5.3/filebeat &
+mkdir -p /tmp/dxos
+export LOG_FILE=/tmp/dxos/gravity-${GRAVITY_TEST_SCENARIO}.${TIMESTAMP}.${$}.log
+chmod a+x /opt/filebeat-8.5.3-linux-x86_64/filebeat
+cd /opt/filebeat-8.5.3-linux-x86_64
+nohup ./filebeat &
+cd /dxos/dxos/packages/gravity/gravity-agent
 pnpm run agent start --verbose --config ./config/config.yml --spec ./config/spec-test-${GRAVITY_TEST_SCENARIO}.yml 2>&1 | tee /tmp/gravity-results/gravity.${GRAVITY_TEST_SCENARIO}.${TIMESTAMP}.${$}.log
