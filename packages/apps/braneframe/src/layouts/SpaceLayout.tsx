4e5f083b73ccfe0173b356d26ea9bccc4318fd49
Josiah Witt <josiah@witt.life>
2023-02-01 17:29:02 -0500

    feat(braneframe): create app (#2373)

---
+++ packages/apps/braneframe/src/layouts/SpaceLayout.tsx
@@ -0,0 +1,34 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import React from 'react';
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
+
+import { PublicKey } from '@dxos/client';
+import { Menubar, Separator, SpaceMenu, SpacesLink } from '@dxos/react-appkit';
+import { useIdentity, useSpace } from '@dxos/react-client';
+import { IdentityPopover } from '@dxos/react-ui';
+
+import { Main } from '../components';
+
+export const SpaceLayout = () => {
+  const { space: spaceHex } = useParams();
+  const spaceKey = PublicKey.safeFrom(spaceHex);
+  const space = spaceKey && useSpace(spaceKey);
+  const identity = useIdentity();
+  const navigate = useNavigate();
+  return (
+    <>
+      <Menubar>
+        <SpacesLink onClickGoToSpaces={() => navigate('..')} />
+        <Separator className='grow' />
+        {space && <SpaceMenu space={space} onClickManageSpace={() => navigate('settings')} />}
+        {identity && <IdentityPopover {...{ identity }} />}{' '}
+      </Menubar>
+      <Main>
+        <Outlet context={{ space }} />
+      </Main>
+    </>
+  );
+};
---
+++ packages/apps/braneframe/src/layouts/SpaceSettingsLayout.tsx
@@ -0,0 +1,33 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import React from 'react';
+import { generatePath, Outlet, useNavigate, useParams } from 'react-router-dom';
+
+import { PublicKey } from '@dxos/client';
+import { Menubar, Separator, SpaceLink } from '@dxos/react-appkit';
+import { useIdentity, useSpace } from '@dxos/react-client';
+import { IdentityPopover } from '@dxos/react-ui';
+
+import { Main } from '../components';
+
+export const SpaceSettingsLayout = () => {
+  const { space: spaceHex } = useParams();
+  const spaceKey = PublicKey.safeFrom(spaceHex);
+  const space = spaceKey && useSpace(spaceKey);
+  const identity = useIdentity();
+  const navigate = useNavigate();
+  return (
+    <>
+      <Menubar>
+        <Separator className='grow' />
+        {space && <SpaceLink onClickGoToSpace={() => navigate(generatePath('/spaces/:space', { space: spaceHex! }))} />}
+        {identity && <IdentityPopover {...{ identity }} />}
+      </Menubar>
+      <Main>
+        <Outlet context={{ space }} />
+      </Main>
+    </>
+  );
+};
