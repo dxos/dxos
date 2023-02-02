a8f80c3ae7c9afc90f19e73b1e719d4cf35ed19b
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-02 20:14:43 +0200

    Add fields

--- packages/core/echo/echo-schema/src/schema.ts
+++ packages/core/echo/echo-schema/src/schema.ts
@@ -7,79 +7,91 @@ import * as pb from 'protobufjs';
 import { TypeFilter } from './database';
 import { strip } from './util';
 
-export enum FieldType {
-  NUMBER = 'number',
-  BYTES = 'bytes',
-  STRING = 'string',
-  BOOLEAN = 'boolean',
-  OBJECT = 'object',
-  ARRAY = 'array'
-}
+export type EchoType =
+  | {
+      kind: 'number' | 'string' | 'boolean' | 'bytes';
+    }
+  | {
+      /**
+       * Plain JS object.
+       */
+      kind: 'record'; // TODO(mykola).
+      objectType: string;
+    }
+  | {
+      kind: 'ref';
+      objectType: string;
+    }
+  | {
+      kind: 'array';
+      elementType: EchoType;
+    };
 
 export type EchoSchemaField = {
-  // TODO(mykola): Add Enums support.
   name: string;
-  type: FieldType;
-  typeName: string;
-  nestedFields?: EchoSchemaField[] | 'basic-type' | 'cyclic-type' | 'enum';
+  type: EchoType;
 };
 
 const getFields = (type: pb.Type, typeStack: string[] = []): EchoSchemaField[] => {
   type.fieldsArray.forEach((field) => field.resolve());
 
   return type.fieldsArray.map((field) => {
-    let type: FieldType;
-    switch (field.type) {
-      case 'double':
-      case 'float':
-      case 'int32':
-      case 'uint32':
-      case 'sint32':
-      case 'fixed32':
-      case 'sfixed32':
-      case 'int64':
-      case 'uint64':
-      case 'sint64':
-      case 'fixed64':
-      case 'sfixed64':
-        type = FieldType.NUMBER;
-        break;
-      case 'string':
-        type = FieldType.STRING;
-        break;
-      case 'bytes':
-        type = FieldType.BYTES;
-        break;
-      case 'bool':
-        type = FieldType.BOOLEAN;
-        break;
-      default:
-        if (field.repeated) {
-          type = FieldType.ARRAY;
-        } else {
-          type = FieldType.OBJECT;
-        }
-    }
+    const getComplexType = (type: pb.Type): EchoType => {
+      if (type.options && type.options['(object)']) {
+        return { kind: 'ref', objectType: type.fullName.replace('.', '') };
+      } else {
+        return { kind: 'record', objectType: type.fullName.replace('.', '') };
+      }
+    };
 
-    let nestedFields: EchoSchemaField[] | 'basic-type' | 'cyclic-type' | 'enum';
-    switch (field.resolvedType?.constructor) {
-      case pb.Enum:
-        nestedFields = 'enum';
-        break;
-      case pb.Type:
-        nestedFields = !typeStack.includes(field.type)
-          ? getFields(field.resolvedType as pb.Type, [...typeStack, field.type])
-          : 'cyclic-type';
-        break;
-      default:
-        nestedFields = 'basic-type';
-    }
+    const getBasicType = (type: string): EchoType => {
+      switch (type) {
+        case 'double':
+        case 'float':
+        case 'int32':
+        case 'uint32':
+        case 'sint32':
+        case 'fixed32':
+        case 'sfixed32':
+        case 'int64':
+        case 'uint64':
+        case 'sint64':
+        case 'fixed64':
+        case 'sfixed64':
+          return { kind: 'number' };
+        case 'string':
+          return { kind: 'string' };
+        case 'bytes':
+          return { kind: 'bytes' };
+        case 'bool':
+          return { kind: 'boolean' };
+        default:
+          throw new Error(`Unknown type: ${type}`);
+      }
+    };
+
+    const getEchoType = (field: pb.Field): EchoType => {
+      if (field.resolvedType instanceof pb.Type) {
+        if (field.repeated) {
+          return { kind: 'array', elementType: getComplexType(field.resolvedType) };
+        } else {
+          return getComplexType(field.resolvedType);
+        }
+      } else if (field.resolvedType instanceof pb.Enum) {
+        // TODO(mykola): Add enums support.
+        throw new Error('Enums are not supported yet.');
+      }
+
+      if (field.repeated) {
+        return { kind: 'array', elementType: getBasicType(field.type) };
+      } else {
+        return getBasicType(field.type);
+      }
+    };
 
     return {
       name: field.name,
-      type,
-      typeName: field.type,
-      nestedFields
+      type: getEchoType(field)
     };
   });
 };
