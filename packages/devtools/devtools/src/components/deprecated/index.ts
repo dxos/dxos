cc9ec08061b5315cb37a0542c67b33086a701f37
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-02 16:00:02 +0200

    Delete deprecated

--- packages/devtools/devtools/src/components/deprecated/index.ts
+++ packages/devtools/devtools/src/components/deprecated/index.ts
@@ -2,11 +2,4 @@
 // Copyright 2020 DXOS.org
 //
 
-export * from './Autocomplete';
-export * from './ErrorBoundary';
-export * from './KeySelect';
-export * from './KeyTable';
 export * from './MessageTable';
-export * from './Panel';
-export * from './SpaceTable';
-export * from './Table';
