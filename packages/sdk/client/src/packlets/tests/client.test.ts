e8eed554d24d3599d5d156fd08d9992a4d7af628
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-02-03 14:53:43 +0200

    feat: Add friendly error when accessing ECHO without identity (#2423)

--- packages/sdk/client/src/packlets/tests/client.test.ts
+++ packages/sdk/client/src/packlets/tests/client.test.ts
@@ -55,6 +55,16 @@ describe('Client', () => {
     expect(client.initialized).to.be.false;
   });
 
+  test('create space before identity', async () => {
+    const testBuilder = new TestBuilder();
+
+    const client = new Client({ services: testBuilder.createClientServicesHost() });
+    await client.initialize();
+    afterTest(() => client.destroy());
+
+    expect(() => client.echo.createSpace()).to.throw('This device has no HALO identity available');
+  }).timeout(1_000);
+
   // TODO(burdon): Memory store is reset on close (feed store is closed).
   test.skip('creates identity then resets the memory storage', async () => {
     const config = new Config();
