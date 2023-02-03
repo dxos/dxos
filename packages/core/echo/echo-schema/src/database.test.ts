e3b7f67b4a9b01744b718a1e8891eb6cd61d5599
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2023-02-03 13:34:49 +0100

    fix: Fix carret jumping in input components (#2424)

--- packages/core/echo/echo-schema/src/database.test.ts
+++ packages/core/echo/echo-schema/src/database.test.ts
@@ -73,30 +73,53 @@ describe('EchoDatabase', () => {
     expect(task.details.deadline).toEqual('2021-01-01');
   });
 
-  test('select', async () => {
-    const router = new DatabaseRouter();
-    const db = await createDatabase(router);
+  describe('subscription', () => {
+    test('updates are propagated', async () => {
+      const router = new DatabaseRouter();
+      const db = await createDatabase(router);
 
-    const task = new Document();
-    await db.save(task);
+      const task = new Document();
+      await db.save(task);
 
-    let counter = 0;
-    const selection = router.createSubscription(() => {
-      counter++;
+      let counter = 0;
+      const selection = router.createSubscription(() => {
+        counter++;
+      });
+      selection.update([task]);
+
+      task.title = 'Test title';
+      await waitForExpect(() => expect(counter).toBeGreaterThanOrEqual(1));
+
+      task.assignee = new Document({ name: 'user-1' });
+      await waitForExpect(() => expect(counter).toBeGreaterThanOrEqual(2));
+
+      task.assignee.name = 'user-2';
+      selection.update([task, task.assignee]);
+
+      task.assignee.name = 'user-3';
+      await waitForExpect(() => expect(counter).toBeGreaterThanOrEqual(3));
     });
-    selection.update([task]);
 
-    task.title = 'Test title';
-    await waitForExpect(() => expect(counter).toBeGreaterThanOrEqual(1));
+    test('updates are synchronous', async () => {
+      const router = new DatabaseRouter();
+      const db = await createDatabase(router);
 
-    task.assignee = new Document({ name: 'user-1' });
-    await waitForExpect(() => expect(counter).toBeGreaterThanOrEqual(2));
+      const task = new Document();
+      await db.save(task);
 
-    task.assignee.name = 'user-2';
-    selection.update([task, task.assignee]);
+      const actions = [];
+      const selection = router.createSubscription(() => {
+        actions.push('update');
+      });
+      selection.update([task]);
 
-    task.assignee.name = 'user-3';
-    await waitForExpect(() => expect(counter).toBeGreaterThanOrEqual(3));
+      actions.push('before');
+      task.title = 'Test title';
+      actions.push('after');
+
+      // NOTE: This order is required for input components in react to function properly when directly bound to ECHO objects.
+      expect(actions).toEqual(['before', 'update', 'after']);
+    });
   });
 
   test('query', async () => {
