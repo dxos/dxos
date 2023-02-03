2b3e458878c24a49a59b17c83d9f93c5b233ee03
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-02-03 12:18:19 +0200

    feat: Add schema introspection (#2418)

--- packages/core/echo/echo-schema/src/schema.ts
+++ packages/core/echo/echo-schema/src/schema.ts
@@ -7,11 +7,102 @@ import * as pb from 'protobufjs';
 import { TypeFilter } from './database';
 import { strip } from './util';
 
+export type EchoType =
+  | {
+      kind: 'number' | 'string' | 'boolean' | 'bytes';
+    }
+  | {
+      /**
+       * Plain JS object.
+       */
+      kind: 'record'; // TODO(mykola) Figure out a better name.
+      objectType: string;
+      // TODO(mykola): Add ability to list fields.
+    }
+  | {
+      kind: 'ref';
+      objectType: string;
+    }
+  | {
+      kind: 'array';
+      elementType: EchoType;
+    }
+  | {
+      kind: 'enum';
+      enumType: string;
+      // TODO(mykola): Add ability to list enum values.
+    };
+
 export type EchoSchemaField = {
   name: string;
+  type: EchoType;
+};
 
-  // TODO(burdon): Replace with enum for type.
-  isOrderedSet?: boolean;
+const getFields = (type: pb.Type): EchoSchemaField[] => {
+  type.fieldsArray.forEach((field) => field.resolve());
+
+  return type.fieldsArray.map((field) => {
+    const getComplexType = (type: pb.Type | pb.Enum): EchoType => {
+      if (type instanceof pb.Enum) {
+        return { kind: 'enum', enumType: type.fullName.slice(1) };
+      } else if (type instanceof pb.Type) {
+        if (type.options && type.options['(object)']) {
+          return { kind: 'ref', objectType: type.fullName.slice(1) };
+        } else {
+          return { kind: 'record', objectType: type.fullName.slice(1) };
+        }
+      }
+
+      throw new Error(`Unknown type: ${type}`);
+    };
+
+    const getBasicType = (type: string): EchoType => {
+      switch (type) {
+        case 'double':
+        case 'float':
+        case 'int32':
+        case 'uint32':
+        case 'sint32':
+        case 'fixed32':
+        case 'sfixed32':
+        case 'int64':
+        case 'uint64':
+        case 'sint64':
+        case 'fixed64':
+        case 'sfixed64':
+          return { kind: 'number' };
+        case 'string':
+          return { kind: 'string' };
+        case 'bytes':
+          return { kind: 'bytes' };
+        case 'bool':
+          return { kind: 'boolean' };
+        default:
+          throw new Error(`Unknown type: ${type}`);
+      }
+    };
+
+    const getEchoType = (field: pb.Field): EchoType => {
+      if (field.resolvedType) {
+        if (field.repeated) {
+          return { kind: 'array', elementType: getComplexType(field.resolvedType) };
+        } else {
+          return getComplexType(field.resolvedType);
+        }
+      }
+
+      if (field.repeated) {
+        return { kind: 'array', elementType: getBasicType(field.type) };
+      } else {
+        return getBasicType(field.type);
+      }
+    };
+
+    return {
+      name: field.name,
+      type: getEchoType(field)
+    };
+  });
 };
 
 /**
@@ -24,11 +115,7 @@ export class EchoSchemaType {
   constructor(
     private readonly _type: pb.Type
   ) {
-    _type.fieldsArray.forEach((field) => field.resolve());
-    this.fields = _type.fieldsArray.map((field) => ({
-      name: field.name,
-      isOrderedSet: field.repeated && field.resolvedType !== null
-    }));
+    this.fields = getFields(_type);
   }
 
   get name() {
