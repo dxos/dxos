8eaba36b937d33b2726b82e81cd1072d37a20da2
quaisx <alex@braneframe.com>
2023-02-03 10:20:15 -0500

    add k8s changes

--- packages/gravity/gravity-k8s/run_agent.sh
+++ packages/gravity/gravity-k8s/run_agent.sh
@@ -5,9 +5,9 @@ cp ./config/config-${GRAVITY_TEST_SCENARIO}.yml ./config/config.yml
 mkdir -p /tmp/dxos
 export LOG_CONFIG=log-config.yml
 export GRAVITY_LOG_FILE=/tmp/dxos/gravity-${GRAVITY_TEST_SCENARIO}.${TIMESTAMP}.${$}.log
-chmod a+x /opt/filebeat-8.5.3-linux-arm64/filebeat
-cd /opt/filebeat-8.5.3-linux-arm64
-nohup ./filebeat &
+# chmod a+x /opt/filebeat-8.5.3-linux-arm64/filebeat
+# cd /opt/filebeat-8.5.3-linux-arm64
+# nohup ./filebeat &
 cd /dxos/dxos/packages/gravity/gravity-agent
 cat ./gravity-*.log 2>&1 | tee /tmp/gravity-results/gravity.${GRAVITY_TEST_SCENARIO}.${TIMESTAMP}.${$}.log
 pnpm run agent start --verbose --config ./config/config.yml --spec ./config/spec-test-${GRAVITY_TEST_SCENARIO}.yml 2>&1 | tee /tmp/gravity-results/gravity.${GRAVITY_TEST_SCENARIO}.${TIMESTAMP}.${$}.log
