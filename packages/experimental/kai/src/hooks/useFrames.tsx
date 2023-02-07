96bc23f29bbe691588cb13d969b9cadda2764b47
Rich Burdon <rich.burdon@gmail.com>
2023-02-07 15:30:15 -0500

    feat: Fix default frame (#2452)

--- packages/experimental/kai/src/hooks/useFrames.tsx
+++ packages/experimental/kai/src/hooks/useFrames.tsx
@@ -23,7 +23,7 @@ export enum FrameID {
   SANDBOX = 'sandbox'
 }
 
-export const defaultFrameId = FrameID.EXPLORER;
+export const defaultFrameId = FrameID.STACK;
 
 // prettier-ignore
 const activeFrames = [
@@ -31,8 +31,8 @@ const activeFrames = [
   FrameID.REGISTRY,
   FrameID.STACK,
   FrameID.TABLE,
-  FrameID.TASKS,
-  FrameID.EXPLORER
+  FrameID.TASKS
+  // FrameID.EXPLORER
   // FrameID.KANBAN,
   // FrameID.NOTES
 ];
