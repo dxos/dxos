9e676ef22a07f20d2622d51523796f8150b0d9a4
thure <will@braneframe.com>
2023-02-07 15:49:45 -0800

    Make selectable actually work.

--- packages/common/react-components/src/components/List/List.tsx
+++ packages/common/react-components/src/components/List/List.tsx
@@ -2,17 +2,20 @@
 // Copyright 2023 DXOS.org
 //
 
+import { CheckedState } from '@radix-ui/react-checkbox';
 import { createCollection } from '@radix-ui/react-collection';
 import { createContextScope, Scope } from '@radix-ui/react-context';
 import { Primitive } from '@radix-ui/react-primitive';
 import { DotsSixVertical } from 'phosphor-react';
-import React, { FC, forwardRef, ReactNode } from 'react';
+import React, { FC, forwardRef, ReactNode, useCallback, useState } from 'react';
 
 import { useForwardedRef, useId } from '../../hooks';
 import { getSize } from '../../styles';
 import { mx } from '../../util';
-import { Checkbox } from '../Checkbox';
+import { Checkbox, CheckboxProps } from '../Checkbox';
 import { defaultListItemHeading, defaultListItemEndcap } from './listStyles';
+
+// TODO (thure): A lot of the accessible affordances for this kind of thing need to be implemented per https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/listbox_role
 
 const LIST_NAME = 'List';
 
@@ -26,12 +29,15 @@ interface ListProps {
 }
 
 interface ListItemData {
-  before?: ReactNode;
-  after?: ReactNode;
+  selected?: CheckboxProps['checked'];
+  defaultSelected?: CheckboxProps['defaultChecked'];
 }
 
 interface ListItemProps extends ListItemData {
   children?: ReactNode;
+  before?: ReactNode;
+  after?: ReactNode;
+  onSelectedChange?: CheckboxProps['onCheckedChange'];
 }
 
 type ListItemElement = React.ElementRef<typeof Primitive.li>;
@@ -49,7 +55,7 @@ const List: FC<ListProps> = (props: ScopedProps<ListProps>) => {
   const { __scopeSelect, children, variant = 'ordered', selectable = false } = props;
   const Root = variant === 'ordered' || variant === 'ordered-draggable' ? 'ol' : 'ul';
   return (
-    <Root {...(selectable && { role: 'listbox' })}>
+    <Root {...(selectable && { role: 'listbox', 'aria-multiselectable': true })}>
       <ListProvider
         {...{
           scope: __scopeSelect,
@@ -63,15 +69,31 @@ const List: FC<ListProps> = (props: ScopedProps<ListProps>) => {
   );
 };
 const ListItem = forwardRef<ListItemElement, ListItemProps>((props: ScopedProps<ListItemProps>, forwardedRef) => {
-  const { __scopeSelect, before, after, children } = props;
+  const { __scopeSelect, before, after, children, selected: propsSelected, defaultSelected, onSelectedChange } = props;
   const ref = useForwardedRef(forwardedRef);
   const { variant, selectable } = useListContext(LIST_NAME, __scopeSelect);
+
+  const [internalSelected, setInternalSelected] = useState(defaultSelected);
+  const selected = typeof propsSelected === 'undefined' ? internalSelected : propsSelected;
+  const onCheckedChange = useCallback(
+    (checked: CheckedState) => {
+      if (typeof propsSelected === 'undefined') {
+        setInternalSelected(checked);
+      } else {
+        onSelectedChange?.(checked);
+      }
+    },
+    [onSelectedChange]
+  );
+
   const headingId = useId('listItem');
+
   return (
     <Primitive.li
       ref={ref}
+      aria-labelledby={headingId}
       {...{
-        ...(selectable && { role: 'option' }),
+        ...(selectable && { role: 'option', 'aria-selected': !!selected }),
         className: 'flex'
       }}
     >
@@ -81,7 +103,11 @@ const ListItem = forwardRef<ListItemElement, ListItemProps>((props: ScopedProps<
         </div>
       )}
       <div role='none' className={defaultListItemEndcap}>
-        {selectable ? <Checkbox labelId={headingId} className='mbs-2.5' /> : before}
+        {selectable ? (
+          <Checkbox labelId={headingId} className='mbs-2.5' {...{ checked: selected, onCheckedChange }} />
+        ) : (
+          before
+        )}
       </div>
       {selectable && before && (
         <div role='none' className={defaultListItemEndcap}>
