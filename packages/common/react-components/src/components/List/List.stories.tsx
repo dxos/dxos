e4465eaf62c361a63494cea39ecf7659e567c904
thure <will@braneframe.com>
2023-02-07 22:43:11 -0800

    Add task list stories. Adjust Inputs to support subdued variant.

--- packages/common/react-components/src/components/List/List.stories.tsx
+++ packages/common/react-components/src/components/List/List.stories.tsx
@@ -7,9 +7,13 @@ import { arrayMove } from '@dnd-kit/sortable';
 import { Play, PushPin } from 'phosphor-react';
 import React, { ComponentPropsWithoutRef, useState } from 'react';
 
+import { useId } from '../../hooks';
 import { getSize } from '../../styles';
 import { mx } from '../../util';
+import { Input } from '../Input';
 import { List, ListItem } from './List';
+
+type DragEndEvent = Parameters<Exclude<ComponentPropsWithoutRef<typeof List>['onDragEnd'], undefined>>[0];
 
 export default {
   component: List
@@ -23,16 +27,12 @@ export const Default = {
         text: `List item ${index + 1}`
       }))
     );
-    const handleDragEnd = (
-      event: Parameters<Exclude<ComponentPropsWithoutRef<typeof List>['onDragEnd'], undefined>>[0]
-    ) => {
+    const handleDragEnd = (event: DragEndEvent) => {
       const { active, over } = event;
-
       if (active.id !== over?.id) {
         setItems((items) => {
           const oldIndex = items.findIndex((item) => item.id === active.id);
           const newIndex = items.findIndex((item) => item.id === over?.id);
-
           return arrayMove(items, oldIndex, newIndex);
         });
       }
@@ -50,6 +50,67 @@ export const Default = {
           </ListItem>
         ))}
       </List>
+    );
+  },
+  args: {
+    selectable: true,
+    variant: 'ordered-draggable'
+  }
+};
+
+export const TaskListItems = {
+  render: ({ ...args }) => {
+    const ListInstance = () => {
+      const listId = useId('L');
+      const titleId = useId('listTitle');
+      const [items, setItems] = useState(
+        [...Array(6)].map((_, index) => ({
+          id: `${listId}--listItem-${index}`,
+          text: `${listId} item ${index + 1}`
+        }))
+      );
+      const handleDragEnd = (event: DragEndEvent) => {
+        const { active, over } = event;
+        if (active.id !== over?.id) {
+          setItems((items) => {
+            const oldIndex = items.findIndex((item) => item.id === active.id);
+            const newIndex = items.findIndex((item) => item.id === over?.id);
+            return arrayMove(items, oldIndex, newIndex);
+          });
+        }
+      };
+      return (
+        <>
+          <h2 id={titleId}>{listId}</h2>
+          <List {...args} labelId={titleId} onDragEnd={handleDragEnd} listItemIds={items.map(({ id }) => id)}>
+            {items.map(({ id, text }) => (
+              <ListItem
+                key={id}
+                id={id}
+                before={<Play className={mx(getSize(5), 'mbs-2.5')} />}
+                after={<PushPin className={mx(getSize(5), 'mbs-2.5')} />}
+              >
+                <Input
+                  variant='subdued'
+                  label={id}
+                  defaultValue={text}
+                  slots={{
+                    label: { className: 'sr-only' },
+                    root: { className: 'm-0' },
+                    input: { className: 'p-1 mbs-1' }
+                  }}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </>
+      );
+    };
+    return (
+      <>
+        <ListInstance />
+        <ListInstance />
+      </>
     );
   },
   args: {
