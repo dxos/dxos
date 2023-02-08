7c7a749386e4fe297c852f5b965037ded82d73dc
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-08 12:25:35 +0200

    Utilize lazy imports in Kai
    Co-authored-by: Dmytro Maretskyi <maretskii@gmail.com>

--- packages/experimental/kai/src/frames/index.ts
+++ packages/experimental/kai/src/frames/index.ts
@@ -2,22 +2,20 @@
 // Copyright 2022 DXOS.org
 //
 
-// TODO(burdon): Investigate runtime failure (sort when fixed).
-/*
-export const StackFrame = React.lazy(() => import('./StackFrame'));
-*/
+import React from 'react';
 
-export * from './CalendarFrame';
-export * from './ChessFrame';
-export * from './DocumentFrame';
-export * from './ExplorerFrame';
-export * from './FileFrame';
-export * from './KanbanFrame';
-export * from './MapFrame';
-export * from './NotesFrame';
-export * from './RegistryFrame';
-export * from './SandboxFrame';
-export * from './SketchFrame';
-export * from './StackFrame';
-export * from './TableFrame';
-export * from './TasksFrame';
+export const BraneFrame = React.lazy(() => import('./BraneFrame'));
+export const CalendarFrame = React.lazy(() => import('./CalendarFrame'));
+export const ChessFrame = React.lazy(() => import('./ChessFrame'));
+export const DocumentFrame = React.lazy(() => import('./DocumentFrame'));
+export const ExplorerFrame = React.lazy(() => import('./ExplorerFrame'));
+export const FileFrame = React.lazy(() => import('./FileFrame'));
+export const KanbanFrame = React.lazy(() => import('./KanbanFrame'));
+export const MapFrame = React.lazy(() => import('./MapFrame'));
+export const NotesFrame = React.lazy(() => import('./NotesFrame'));
+export const RegistryFrame = React.lazy(() => import('./RegistryFrame'));
+export const SandboxFrame = React.lazy(() => import('./SandboxFrame'));
+export const SketchFrame = React.lazy(() => import('./SketchFrame'));
+export const StackFrame = React.lazy(() => import('./StackFrame'));
+export const TableFrame = React.lazy(() => import('./TableFrame'));
+export const TasksFrame = React.lazy(() => import('./TasksFrame'));
