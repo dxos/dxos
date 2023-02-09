166d6952ca87ce883fe7da3f7f417158f0b33c54
Will Shown <thure@users.noreply.github.com>
2023-02-08 17:24:56 -0800

    feat: List component (#2453)

---
+++ packages/common/react-components/src/components/List/listStyles.ts
@@ -0,0 +1,9 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { getSize } from '../../styles';
+import { mx } from '../../util';
+
+export const defaultListItemEndcap = mx(getSize(10), 'overflow-hidden flex items-start justify-center');
+export const defaultListItemHeading = 'flex-1 min-bs-10';
