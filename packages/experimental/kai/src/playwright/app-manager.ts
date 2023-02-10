d5e17efca75f8cf9da79884722bf0c599fcd1c0c
Josiah Witt <josiah@witt.life>
2023-02-10 15:55:31 -0500

    test(kai): fix playwright test (#2476)

--- packages/experimental/kai/src/playwright/app-manager.ts
+++ packages/experimental/kai/src/playwright/app-manager.ts
@@ -5,7 +5,7 @@
 import type { Context as MochaContext } from 'mocha';
 import type { ConsoleMessage, Page } from 'playwright';
 
-import { sleep, synchronized, Trigger } from '@dxos/async';
+import { sleep, Trigger } from '@dxos/async';
 import { setupPage } from '@dxos/test';
 
 // TODO(wittjosiah): Get this from executor.
@@ -20,61 +20,7 @@ export class AppManager {
 
   constructor(private readonly mochaContext: MochaContext) {}
 
-  // Getters
-
-  async currentSpace() {
-    const url = await this.page.url();
-    return url.split('/').find((part) => part.includes('...'));
-  }
-
-  async stackIsVisible() {
-    await this._init();
-
-    return (
-      (await this.page.isVisible('a:has-text("Stack")')) &&
-      (await this.page.isVisible('h2:text("Organizations")')) &&
-      (await this.page.isVisible('h2:text("Contacts")')) &&
-      (await this.page.isVisible('h2:text("Tasks")')) &&
-      (await this.page.isVisible('h2:text("Projects")'))
-    );
-  }
-
-  // Actions
-
-  async createIdentity(name: string) {
-    await this._init();
-    await this.page.click('data-testid=create-identity-button');
-    await this.page.keyboard.type(name);
-    await this.page.keyboard.press('Enter');
-    await sleep(500); // Allow time for redirect.
-  }
-
-  async shareSpace() {
-    await this._init();
-    this._invitationCode = new Trigger<string>();
-    await this.page.click('data-testid=space-settings');
-    await this.page.click('data-testid=create-invitation-button');
-    return await this._invitationCode.wait();
-  }
-
-  async getAuthenticationCode() {
-    await this._init();
-    this._authenticationCode = new Trigger<string>();
-    return await this._authenticationCode.wait();
-  }
-
-  async joinSpace(invitationCode: string) {
-    await this._init();
-    await this.page.goto(`${BASE_URL}/#/space/join?invitation=${invitationCode}`);
-  }
-
-  async authenticate(authenticationCode: string) {
-    await this._init();
-    await this.page.keyboard.type(authenticationCode);
-  }
-
-  @synchronized
-  private async _init() {
+  async init() {
     if (this._initialized) {
       return;
     }
@@ -87,6 +33,46 @@ export class AppManager {
     this.page = page;
     this.page.on('console', (message) => this._onConsoleMessage(message));
     this._initialized = true;
+  }
+
+  // Getters
+
+  async currentSpace() {
+    const url = await this.page.url();
+    return url.split('/').find((part) => part.includes('...'));
+  }
+
+  async kaiIsVisible() {
+    return await this.page.getByTestId('kai-bug').isVisible();
+  }
+
+  // Actions
+
+  async createIdentity(name: string) {
+    await this.page.click('data-testid=create-identity-button');
+    await this.page.keyboard.type(name);
+    await this.page.keyboard.press('Enter');
+    await sleep(500); // Allow time for redirect.
+  }
+
+  async shareSpace() {
+    this._invitationCode = new Trigger<string>();
+    await this.page.click('data-testid=space-settings');
+    await this.page.click('data-testid=create-invitation-button');
+    return await this._invitationCode.wait();
+  }
+
+  async getAuthenticationCode() {
+    this._authenticationCode = new Trigger<string>();
+    return await this._authenticationCode.wait();
+  }
+
+  async joinSpace(invitationCode: string) {
+    await this.page.goto(`${BASE_URL}/#/space/join?invitation=${invitationCode}`);
+  }
+
+  async authenticate(authenticationCode: string) {
+    await this.page.keyboard.type(authenticationCode);
   }
 
   private async _onConsoleMessage(message: ConsoleMessage) {
