c7e0a9079718db2f5dc3321f617da1514bab456d
Rich Burdon <rich.burdon@gmail.com>
2023-02-09 22:11:02 -0500

    feat: Move components. (#2465)

---
+++ packages/experimental/kai/src/components/Explorer/RadialTree.js
@@ -0,0 +1,138 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import * as d3 from 'd3';
+
+// Copyright 2022 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/radial-tree
+// https://observablehq.com/@d3/tree
+export const RadialTree = (data, options = {}) => {
+  const {
+    // data is either tabular (array of objects) or hierarchy (nested objects)
+    path, // as an alternative to id and parentId, returns an array identifier, imputing internal nodes
+    id = Array.isArray(data) ? (d) => d.id : null, // if tabular data, given a d in data, returns a unique identifier (string)
+    parentId = Array.isArray(data) ? (d) => d.parentId : null, // if tabular data, given a node d, returns its parent’s identifier
+    children, // if hierarchical data, given a d in data, returns its children
+
+    tree = d3.tree, // layout algorithm (typically d3.tree or d3.cluster)
+    separation = tree === d3.tree
+      ? (a, b) => (a.parent === b.parent ? 1 : 2) / a.depth
+      : (a, b) => (a.parent === b.parent ? 1 : 2),
+    sort, // how to sort nodes prior to layout (e.g., (a, b) => d3.descending(a.height, b.height))
+    label, // given a node d, returns the display name
+    title, // given a node d, returns its hover text
+    link, // given a node d, its link (if any)
+    linkTarget = '_blank', // the target attribute for links (if any)
+
+    width = 0, // outer width, in pixels
+    height = 0, // outer height, in pixels
+    margin = 0, // shorthand for margins
+    marginTop = margin, // top margin, in pixels
+    marginRight = margin, // right margin, in pixels
+    marginBottom = margin, // bottom margin, in pixels
+    marginLeft = margin, // left margin, in pixels
+    radius = Math.min(width - marginLeft - marginRight, height - marginTop - marginBottom) / 3, // outer radius
+    r = 4, // radius of nodes
+
+    fill = '#999', // fill for nodes
+    stroke = '#555', // stroke for links
+    strokeWidth = 1.5, // stroke width for links
+    strokeOpacity = 0.4, // stroke opacity for links
+    strokeLinejoin, // stroke line join for links
+    strokeLinecap, // stroke line cap for links
+
+    halo = '#fff', // color of label halo
+    haloWidth = 3 // padding around the labels
+  } = options;
+
+  // If id and parentId options are specified, or the path option, use d3.stratify
+  // to convert tabular data to a hierarchy; otherwise we assume that the data is
+  // specified as an object {children} with nested objects (a.k.a. the “flare.json”
+  // format), and use d3.hierarchy.
+  const root =
+    path != null
+      ? d3.stratify().path(path)(data)
+      : id != null || parentId != null
+      ? d3.stratify().id(id).parentId(parentId)(data)
+      : d3.hierarchy(data, children);
+
+  // Sort the nodes.
+  if (sort) {
+    root.sort(sort);
+  }
+
+  // Compute labels and titles.
+  const descendants = root.descendants();
+  const getLabel = label === null ? null : descendants.map((d) => label(d.data, d));
+
+  // Compute the layout.
+  tree()
+    .size([2 * Math.PI, radius])
+    .separation(separation)(root);
+
+  // TODO(burdon): Factor out.
+  const svg = d3
+    .create('svg')
+    .attr('viewBox', [-marginLeft - radius, -marginTop - radius, width, height])
+    .attr('width', width)
+    .attr('height', height)
+    .attr('style', 'max-width: 100%; width: 100%; height: 100%; height: intrinsic;')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', 10);
+
+  svg
+    .append('g')
+    .attr('fill', 'none')
+    .attr('stroke', stroke)
+    .attr('stroke-opacity', strokeOpacity)
+    .attr('stroke-linecap', strokeLinecap)
+    .attr('stroke-linejoin', strokeLinejoin)
+    .attr('stroke-width', strokeWidth)
+    .selectAll('path')
+    .data(root.links())
+    .join('path')
+    .attr(
+      'd',
+      d3
+        .linkRadial()
+        .angle((d) => d.x)
+        .radius((d) => d.y)
+    );
+
+  const node = svg
+    .append('g')
+    .selectAll('a')
+    .data(root.descendants())
+    .join('a')
+    // .attr('xlink:href', link == null ? null : (d) => link(d.data, d))
+    .attr('target', link == null ? null : linkTarget)
+    .attr('transform', (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`);
+
+  node
+    .append('circle')
+    .attr('fill', (d) => (d.children ? stroke : fill))
+    .attr('r', r);
+
+  if (title != null) {
+    node.append('title').text((d) => title(d.data, d));
+  }
+
+  if (getLabel) {
+    node
+      .append('text')
+      .attr('transform', (d) => `rotate(${d.x >= Math.PI ? 180 : 0})`)
+      .attr('dy', '0.32em')
+      // eslint-disable-next-line no-mixed-operators
+      .attr('x', (d) => (d.x < Math.PI === !d.children ? 6 : -6))
+      // eslint-disable-next-line no-mixed-operators
+      .attr('text-anchor', (d) => (d.x < Math.PI === !d.children ? 'start' : 'end'))
+      .attr('paint-order', 'stroke')
+      .attr('stroke', halo)
+      .attr('stroke-width', haloWidth)
+      .text((d, i) => getLabel[i]);
+  }
+
+  return svg.node();
+};
--- packages/experimental/kai/src/components/Explorer/TidyTree.js
+++ packages/experimental/kai/src/components/Explorer/TidyTree.js
@@ -4,39 +4,31 @@
 
 import * as d3 from 'd3';
 
-// Copyright 2022 Observable, Inc.
+// Copyright 2021 Observable, Inc.
 // Released under the ISC license.
-// https://observablehq.com/@d3/radial-tree
-// TODO(burdon): Factor out (to plexus?)
-export const Tree = (
-  data,
-  {
+// https://observablehq.com/@d3/tree
+export const TidyTree = (data, options = {}) => {
+  const {
     // data is either tabular (array of objects) or hierarchy (nested objects)
     path, // as an alternative to id and parentId, returns an array identifier, imputing internal nodes
     id = Array.isArray(data) ? (d) => d.id : null, // if tabular data, given a d in data, returns a unique identifier (string)
     parentId = Array.isArray(data) ? (d) => d.parentId : null, // if tabular data, given a node d, returns its parent’s identifier
     children, // if hierarchical data, given a d in data, returns its children
+
     tree = d3.tree, // layout algorithm (typically d3.tree or d3.cluster)
-    separation = tree === d3.tree
-      ? (a, b) => (a.parent === b.parent ? 1 : 2) / a.depth
-      : (a, b) => (a.parent === b.parent ? 1 : 2),
+    curve = d3.curveBumpX, // curve for the link
     sort, // how to sort nodes prior to layout (e.g., (a, b) => d3.descending(a.height, b.height))
     label, // given a node d, returns the display name
     title, // given a node d, returns its hover text
     link, // given a node d, its link (if any)
     linkTarget = '_blank', // the target attribute for links (if any)
-    width = 0, // outer width, in pixels
-    height = 0, // outer height, in pixels
-    margin = 0, // shorthand for margins
-    marginTop = margin, // top margin, in pixels
-    marginRight = margin, // right margin, in pixels
-    marginBottom = margin, // bottom margin, in pixels
-    marginLeft = margin, // left margin, in pixels
-    radius = Math.min(width - marginLeft - marginRight, height - marginTop - marginBottom) / 3, // outer radius
+
+    width, // outer width, in pixels
+    height: initialHeight, // outer height, in pixels
     r = 3, // radius of nodes
-    padding = 1, // horizontal padding for first and last column
+    padding = 4, // horizontal padding for first and last column
+
     fill = '#999', // fill for nodes
-    fillOpacity, // fill opacity for nodes
     stroke = '#555', // stroke for links
     strokeWidth = 1.5, // stroke width for links
     strokeOpacity = 0.4, // stroke opacity for links
@@ -44,8 +36,8 @@ export const Tree = (
     strokeLinecap, // stroke line cap for links
     halo = '#fff', // color of label halo
     haloWidth = 3 // padding around the labels
-  } = {}
-) => {
+  } = options;
+
   // If id and parentId options are specified, or the path option, use d3.stratify
   // to convert tabular data to a hierarchy; otherwise we assume that the data is
   // specified as an object {children} with nested objects (a.k.a. the “flare.json”
@@ -64,19 +56,42 @@ export const Tree = (
 
   // Compute labels and titles.
   const descendants = root.descendants();
-  const L = label == null ? null : descendants.map((d) => label(d.data, d));
+  const getLabel = label == null ? null : descendants.map((d) => label(d.data, d));
 
   // Compute the layout.
-  tree()
-    .size([2 * Math.PI, radius])
-    .separation(separation)(root);
+  const dx = 8;
+  const dy = width / (root.height + padding);
+  tree().nodeSize([dx + 8, dy])(root);
+
+  // Center the tree.
+  let x0 = Infinity;
+  let x1 = -x0;
+  root.each((d) => {
+    if (d.x > x1) {
+      x1 = d.x;
+    }
+    if (d.x < x0) {
+      x0 = d.x;
+    }
+  });
+
+  // Compute the default height.
+  let height = initialHeight;
+  if (initialHeight === undefined) {
+    height = x1 - x0 + dx * 2;
+  }
+
+  // Use the required curve
+  if (typeof curve !== 'function') {
+    throw new Error('Unsupported curve');
+  }
 
   const svg = d3
     .create('svg')
-    .attr('viewBox', [-marginLeft - radius, -marginTop - radius, width, height])
+    .attr('viewBox', [(-dy * padding) / 2, x0 - dx - 32, width, height])
     .attr('width', width)
     .attr('height', height)
-    .attr('style', 'max-width: 100%; width: 100%; height: 100%; height: intrinsic;')
+    .attr('style', 'max-width: 100%; height: auto; height: intrinsic;')
     .attr('font-family', 'sans-serif')
     .attr('font-size', 10);
 
@@ -94,9 +109,9 @@ export const Tree = (
     .attr(
       'd',
       d3
-        .linkRadial()
-        .angle((d) => d.x)
-        .radius((d) => d.y)
+        .link(curve)
+        .x((d) => d.y)
+        .y((d) => d.x)
     );
 
   const node = svg
@@ -104,9 +119,9 @@ export const Tree = (
     .selectAll('a')
     .data(root.descendants())
     .join('a')
-    .attr('xlink:href', link == null ? null : (d) => link(d.data, d))
+    // .attr('xlink:href', link == null ? null : (d) => link(d.data, d))
     .attr('target', link == null ? null : linkTarget)
-    .attr('transform', (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`);
+    .attr('transform', (d) => `translate(${d.y},${d.x})`);
 
   node
     .append('circle')
@@ -117,19 +132,16 @@ export const Tree = (
     node.append('title').text((d) => title(d.data, d));
   }
 
-  if (L) {
+  if (getLabel) {
     node
       .append('text')
-      .attr('transform', (d) => `rotate(${d.x >= Math.PI ? 180 : 0})`)
       .attr('dy', '0.32em')
-      // eslint-disable-next-line no-mixed-operators
-      .attr('x', (d) => (d.x < Math.PI === !d.children ? 6 : -6))
-      // eslint-disable-next-line no-mixed-operators
-      .attr('text-anchor', (d) => (d.x < Math.PI === !d.children ? 'start' : 'end'))
+      .attr('x', (d) => (d.children ? -6 : 6))
+      .attr('text-anchor', (d) => (d.children ? 'end' : 'start'))
       .attr('paint-order', 'stroke')
       .attr('stroke', halo)
       .attr('stroke-width', haloWidth)
-      .text((d, i) => L[i]);
+      .text((d, i) => getLabel[i]);
   }
 
   return svg.node();
