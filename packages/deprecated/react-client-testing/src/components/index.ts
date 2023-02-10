e1b5c85e610a90c542404c00dcbaf5385d65aab7
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-10 13:13:53 +0200

    Delete client-testing

--- packages/deprecated/react-client-testing/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-//
-// Copyright 2022 DXOS.org
-//
-
-export * from './CreateSpaceDialog';
-export * from './ExportMenu';
-export * from './ImportMenu';
-export * from './ImportIpfsDialog';
-export * from './SelectionEditor';
