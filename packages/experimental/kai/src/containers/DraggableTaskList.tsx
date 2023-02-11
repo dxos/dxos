f04e858bbfb4b4e78268bf1c724203c3ac89a150
thure <will@braneframe.com>
2023-02-10 17:47:59 -0800

    Simplify carefully.

--- packages/experimental/kai/src/containers/DraggableTaskList.tsx
+++ packages/experimental/kai/src/containers/DraggableTaskList.tsx
@@ -2,10 +2,10 @@
 // Copyright 2022 DXOS.org
 //
 
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 
 import { id } from '@dxos/echo-schema';
-import { EditableList, EditableListProps } from '@dxos/react-appkit';
+import { EditableList } from '@dxos/react-appkit';
 import { withReactor } from '@dxos/react-client';
 import { randomString } from '@dxos/react-components';
 
@@ -21,24 +21,16 @@ export const DraggableTaskList: FC<{
   tasks: Task[];
   onCreate?: (task: Task) => void;
   onDrag?: (active: number, over: number) => void;
-}> = withReactor(({ tasks, onCreate, onDrag }) => {
+  onDelete?: (task: Task) => void;
+}> = withReactor(({ tasks, onCreate, onDrag, onDelete }) => {
   const space = useSpace(); // TODO(burdon): Factor out.
-  const [newTask, setNewTask] = useState<Task>();
-  useEffect(() => {
-    setNewTask(new Task());
-  }, []);
-
-  const handleCreateTask = async (task: Task) => {
-    if (task.title?.length) {
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const handleCreateTask = async () => {
+    if (newTaskTitle.length) {
+      const task = new Task({ title: newTaskTitle ?? '' });
       await space.experimental.db.save(task);
+      setNewTaskTitle('');
       onCreate?.(task);
-      setNewTask(new Task());
-    }
-  };
-
-  const handleDragEnd = (i1: number, i2: number) => {
-    if (onDrag) {
-      onDrag(i1, i2);
     }
   };
 
@@ -54,55 +46,19 @@ export const DraggableTaskList: FC<{
   //  - Split current task if pressing Enter in the middle.
 
   return (
-    <div>
-      <DraggableTaskListContainer
-        items={tasks.map((task) => task[id])}
-        onMoveItem={onDrag ? handleDragEnd : undefined}
-        tasks={tasks}
-        newTask={newTask}
-        onCreate={handleCreateTask}
-      />
-    </div>
+    <EditableList
+      completable
+      id={id in tasks ? (tasks as unknown as { [id]: string })[id] : randomString()}
+      labelId='omitted'
+      onMoveItem={onDrag}
+      itemIdOrder={tasks.map((task) => task[id])}
+      nextItemTitle={newTaskTitle}
+      onChangeNextItemTitle={({ target: { value } }) => setNewTaskTitle(value)}
+      onClickAdd={handleCreateTask}
+    >
+      {tasks.map((task) => (
+        <TaskItem key={task[id]} task={task} onDelete={onDelete} />
+      ))}
+    </EditableList>
   );
-});
-
-export const DraggableTaskListContainer: FC<{
-  tasks: Task[];
-  items: string[];
-  onMoveItem?: EditableListProps['onMoveItem'];
-  newTask?: Task;
-  onCreate?: (task: Task) => void;
-  onDelete?: (task: Task) => void;
-}> = withReactor(({ tasks, newTask, onCreate, onDelete, items, onMoveItem }) => {
-  const [newTaskTitle, setNewTaskTitle] = useState('');
-  return (
-    <>
-      <EditableList
-        completable
-        id={id in tasks ? (tasks as unknown as { [id]: string })[id] : randomString()}
-        labelId='omitted'
-        onMoveItem={onMoveItem}
-        itemIdOrder={items}
-        nextItemTitle={newTaskTitle}
-        onChangeNextItemTitle={({ target: { value } }) => setNewTaskTitle(value)}
-        onClickAdd={() => {
-          onCreate?.(new Task({ title: newTaskTitle ?? '' }));
-          setNewTaskTitle('');
-        }}
-      >
-        {tasks.map((task) => (
-          <DraggableTaskItem key={task[id]} task={task} onDelete={onDelete} />
-        ))}
-      </EditableList>
-    </>
-  );
-});
-
-export const DraggableTaskItem: FC<{
-  task: Task;
-  onEnter?: (task: Task) => void;
-  onDelete?: (task: Task) => void;
-}> = withReactor(({ task, onEnter, onDelete }) => {
-  // TODO(burdon): Dragging doesn't handle variable height items?
-  return <TaskItem {...{ task, onDelete, onEnter }} />;
 });
