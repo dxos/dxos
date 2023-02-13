44ee0b585bf6e8e7adea34eaff8512c6a73140b9
Will Shown <thure@users.noreply.github.com>
2023-02-12 22:57:33 -0800

    fix: Use `!` modifier instead of removing a rule for now (#2485)

--- packages/apps/patterns/react-ui/src/layouts/PanelSidebar/PanelSidebar.tsx
+++ packages/apps/patterns/react-ui/src/layouts/PanelSidebar/PanelSidebar.tsx
@@ -80,8 +80,7 @@ export const PanelSidebarProvider = ({ children, slots }: PropsWithChildren<Pane
         <DialogPrimitive.Content
           {...slots?.content}
           className={mx(
-            // TODO(burdon): Fix '__block-start-0'
-            'fixed __block-start-0 block-end-0 is-sidebar z-50 overscroll-contain overflow-x-hidden overflow-y-auto',
+            'fixed block-start-0 block-end-0 is-sidebar z-50 overscroll-contain overflow-x-hidden overflow-y-auto',
             'transition-[inset-inline-start,inset-inline-end] duration-200 ease-in-out',
             transitionShow ? 'inline-start-0' : '-inline-start-sidebar',
             slots?.content?.className
