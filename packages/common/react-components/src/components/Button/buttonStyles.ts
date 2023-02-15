d793c14993e3186b876ded32869f6518797100d5
thure <will@braneframe.com>
2023-02-14 16:21:10 -0800

    Improve hover for ghost buttons. Add tokens for hover in light mode.

--- packages/common/react-components/src/components/Button/buttonStyles.ts
+++ packages/common/react-components/src/components/Button/buttonStyles.ts
@@ -2,7 +2,7 @@
 // Copyright 2022 DXOS.org
 //
 
-import { defaultDisabled, hover, defaultActive, osActive, themeVariantFocus } from '../../styles';
+import { defaultDisabled, hover, defaultActive, osActive, focus } from '../../styles';
 import { mx } from '../../util';
 import { ThemeContextValue } from '../ThemeProvider';
 import { ButtonProps } from './ButtonProps';
@@ -11,7 +11,7 @@ export const primaryAppButtonColors =
   'bg-primary-550 dark:bg-primary-550 text-white hover:bg-primary-600 dark:hover:bg-primary-600 hover:text-white dark:hover:text-white';
 export const defaultAppButtonColors = 'bg-white text-neutral-800 dark:bg-neutral-800 dark:text-neutral-50';
 export const defaultOsButtonColors = 'bg-white/50 text-neutral-900 dark:bg-neutral-750/50 dark:text-neutral-50';
-export const ghostAppButtonColors = '';
+export const ghostAppButtonColors = 'hover:bg-neutral-400/10 dark:hover:bg-neutral-200/10';
 
 export const buttonStyles = (
   props: Partial<ButtonProps> = {},
@@ -27,13 +27,12 @@ export const buttonStyles = (
     !isOsTheme && resolvedVariant !== 'outline' && ' hover:border-transparent dark:hover:border-transparent',
     !isOsTheme && resolvedVariant === 'default' && 'border border-neutral-100 dark:border-neutral-750',
     resolvedVariant === 'default' && (isOsTheme ? defaultOsButtonColors : defaultAppButtonColors),
-    resolvedVariant === 'ghost' && ghostAppButtonColors,
+    !props.disabled && resolvedVariant === 'ghost' && ghostAppButtonColors,
     resolvedVariant === 'primary' && 'border border-primary-550',
     resolvedVariant === 'primary' && primaryAppButtonColors,
     resolvedVariant === 'outline' &&
       'text-neutral-700 border border-neutral-600 dark:border-neutral-300 dark:text-neutral-150',
-    themeVariantFocus(themeVariant),
-    'outline-none', // TODO(burdon): Temporary until styled.
+    focus({ disabled: props.disabled }, themeVariant),
     props.disabled
       ? defaultDisabled
       : !isOsTheme && resolvedVariant !== 'outline' && resolvedVariant !== 'ghost' && 'button-elevation',
--- packages/common/react-components/src/styles/hover.ts
+++ packages/common/react-components/src/styles/hover.ts
@@ -12,6 +12,6 @@ export const hover = (
     !disabled &&
     (themeVariant === 'os'
       ? 'transition-colors duration-100 linear hover:bg-white/75 dark:hover:bg-neutral-750/75'
-      : 'transition-colors duration-100 linear hover:text-black dark:hover:text-white hover:bg-neutral-100/30 dark:hover:bg-neutral-750')
+      : 'transition-colors duration-100 linear hover:text-black dark:hover:text-white hover:bg-neutral-25 dark:hover:bg-neutral-750')
   );
 };
