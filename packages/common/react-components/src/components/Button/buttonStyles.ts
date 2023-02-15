0a6c74773e6cebc65f4424223b476adf9a35ae4b
thure <will@braneframe.com>
2023-02-14 17:32:57 -0800

    Improve ghost button styles.

--- packages/common/react-components/src/components/Button/buttonStyles.ts
+++ packages/common/react-components/src/components/Button/buttonStyles.ts
@@ -11,7 +11,8 @@ export const primaryAppButtonColors =
   'bg-primary-550 dark:bg-primary-550 text-white hover:bg-primary-600 dark:hover:bg-primary-600 hover:text-white dark:hover:text-white';
 export const defaultAppButtonColors = 'bg-white text-neutral-800 dark:bg-neutral-800 dark:text-neutral-50';
 export const defaultOsButtonColors = 'bg-white/50 text-neutral-900 dark:bg-neutral-750/50 dark:text-neutral-50';
-export const ghostAppButtonColors = 'hover:bg-neutral-400/10 dark:hover:bg-neutral-200/10';
+export const ghostAppButtonColors =
+  'hover:bg-transparent dark:hover:bg-transparent hover:text-primary-500 dark:hover:text-primary-300';
 
 export const buttonStyles = (
   props: Partial<ButtonProps> = {},
