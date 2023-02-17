762f5bdfa4915c8771da79ece2ad0ca5287a0f68
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-02-17 10:52:56 +0200

    feat: Add Credentials API (#2502)

---
+++ packages/core/halo/credentials/src/presentations/proof.ts
@@ -0,0 +1,43 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { Signer } from '@dxos/crypto';
+import { PublicKey } from '@dxos/keys';
+import { Chain, Presentation, Proof } from '@dxos/protocols/proto/dxos/halo/credentials';
+
+import { SIGNATURE_TYPE_ED25519 } from '../credentials';
+import { getPresentationProofPayload } from './signing';
+
+export const signPresentation = async ({
+  presentation,
+  signer,
+  signerKey,
+  chain,
+  nonce
+}: {
+  presentation: Presentation;
+  signer: Signer;
+  signerKey: PublicKey;
+  chain?: Chain;
+  nonce?: Uint8Array;
+}): Promise<Presentation> => {
+  const proof: Proof = {
+    type: SIGNATURE_TYPE_ED25519,
+    value: new Uint8Array(),
+    creationDate: new Date(),
+    signer: signerKey,
+    nonce
+  };
+
+  const signedPayload = getPresentationProofPayload(presentation.credentials ?? [], proof);
+  proof.value = await signer.sign(signerKey, signedPayload);
+  if (chain) {
+    proof.chain = chain;
+  }
+
+  return {
+    credentials: presentation.credentials,
+    proofs: [...(presentation.proofs ?? []), proof]
+  };
+};
--- packages/sdk/client-services/src/packlets/services/service-host.test.ts
+++ packages/sdk/client-services/src/packlets/services/service-host.test.ts
@@ -2,12 +2,17 @@
 // Copyright 2023 DXOS.org
 //
 
-import { latch } from '@dxos/async';
+import { expect } from 'chai';
+
+import { latch, Trigger } from '@dxos/async';
 import { Config } from '@dxos/config';
+import { verifyPresentation } from '@dxos/credentials';
+import { PublicKey } from '@dxos/keys';
 import { MemorySignalManagerContext } from '@dxos/messaging';
+import { Credential } from '@dxos/protocols/proto/dxos/halo/credentials';
 import { afterTest, describe, test } from '@dxos/test';
 
-import { createServiceHost } from '../testing';
+import { createMockCredential, createServiceHost } from '../testing';
 
 describe('ClientServicesHost', () => {
   test('queryCredentials', async () => {
@@ -27,5 +32,69 @@ describe('ClientServicesHost', () => {
     afterTest(() => stream.close());
 
     await done();
+  });
+
+  test('write and query credentials', async () => {
+    const host = createServiceHost(new Config(), new MemorySignalManagerContext());
+    await host.open();
+    afterTest(() => host.close());
+
+    await host.services.ProfileService!.createProfile({});
+
+    const testCredential = await createMockCredential({
+      signer: host._serviceContext.keyring,
+      issuer: host._serviceContext.identityManager.identity!.deviceKey
+    });
+
+    // Test if Profile exposes haloSpace key.
+    const haloSpace = new Trigger<PublicKey>();
+    host.services.ProfileService!.subscribeProfile()!.subscribe(({ profile }) => {
+      if (profile?.haloSpace) {
+        haloSpace.wake(profile.haloSpace);
+      }
+    });
+
+    await host.services.SpacesService?.writeCredentials({
+      spaceKey: await haloSpace.wait(),
+      credentials: [testCredential]
+    });
+
+    const credentials = host.services.SpacesService!.queryCredentials({ spaceKey: await haloSpace.wait() });
+    const queriedCredential = new Trigger<Credential>();
+    credentials.subscribe((credential) => {
+      if (credential.subject.id.equals(testCredential.subject.id)) {
+        queriedCredential.wake(credential);
+      }
+    });
+    afterTest(() => credentials.close());
+
+    await queriedCredential.wait();
+  });
+
+  test('sign presentation', async () => {
+    const host = createServiceHost(new Config(), new MemorySignalManagerContext());
+    await host.open();
+    afterTest(() => host.close());
+
+    await host.services.ProfileService!.createProfile({});
+
+    const testCredential = await createMockCredential({
+      signer: host._serviceContext.keyring,
+      issuer: host._serviceContext.identityManager.identity!.deviceKey
+    });
+
+    const nonce = new Uint8Array([0, 0, 0, 0]);
+
+    const presentation = await host.services.ProfileService!.signPresentation({
+      presentation: {
+        credentials: [testCredential]
+      },
+      nonce
+    });
+
+    expect(presentation.proofs?.[0].nonce).to.deep.equal(nonce);
+    expect(await verifyPresentation(presentation)).to.deep.equal({
+      kind: 'pass'
+    });
   });
 });
