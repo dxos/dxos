3a7d7313bdb468905c1e3e41aaf52cf5bb8a7913
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-20 18:49:10 +0200

    Add smoke test

--- packages/devtools/devtools-extension/src/playwright/smoke.spec.ts
+++ packages/devtools/devtools-extension/src/playwright/smoke.spec.ts
@@ -2,11 +2,48 @@
 // Copyright 2023 DXOS.org
 //
 
-import { test, expect } from './fixtures';
+import type { Context as MochaContext } from 'mocha';
+import { expect } from 'chai';
+import { Page } from 'playwright';
 
-test.describe('Popup', () => {
-  test('our extension loads', async ({ page, extensionId }) => {
+import { beforeAll, describe, setupPage, test, extensionId } from '@dxos/test';
+import { asyncTimeout } from '@dxos/async';
+
+export class ExtensionManager {
+  extensionId!: string;
+  page!: Page;
+
+  private _initialized = false;
+
+  constructor(private readonly mochaContext: MochaContext) {}
+
+  async init() {
+    if (this._initialized) {
+      return;
+    }
+
+    const { page, context } = await setupPage(this.mochaContext, { bridgeLogs: true });
+
+    debugger;
+    this.extensionId = await asyncTimeout(extensionId(context), 30_000);
     await page.goto(`chrome-extension://${extensionId}/popup.html`);
-    await expect(page.locator('body')).toHaveText('my-extension popup');
+
+    await page.waitForSelector('body');
+
+    this.page = page;
+    this._initialized = true;
+  }
+}
+
+describe('Basic test', () => {
+  let extensionManager: ExtensionManager;
+
+  beforeAll(async function () {
+    extensionManager = new ExtensionManager(this);
+  });
+
+  test('our extension loads', async () => {
+    await extensionManager.init();
+    expect(extensionManager.page.locator('body')).to.equal('my-extension popup');
   });
 });
