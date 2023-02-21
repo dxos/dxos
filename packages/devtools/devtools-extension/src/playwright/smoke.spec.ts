f738a02c5561c84ce72c8580d67e11e9d8d98b6b
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-21 14:58:01 +0200

    Factor out ExtensionManager

--- packages/devtools/devtools-extension/src/playwright/smoke.spec.ts
+++ packages/devtools/devtools-extension/src/playwright/smoke.spec.ts
@@ -3,36 +3,10 @@
 //
 
 import { expect } from 'chai';
-import type { Context as MochaContext } from 'mocha';
-import { Page } from 'playwright';
 
-import { asyncTimeout } from '@dxos/async';
-import { beforeAll, describe, setupPage, test, extensionId } from '@dxos/test';
+import { beforeAll, describe, test } from '@dxos/test';
 
-export class ExtensionManager {
-  extensionId!: string;
-  page!: Page;
-
-  private _initialized = false;
-
-  constructor(private readonly mochaContext: MochaContext) {}
-
-  async init() {
-    if (this._initialized) {
-      return;
-    }
-
-    const { page, context } = await setupPage(this.mochaContext, { bridgeLogs: true });
-
-    this.extensionId = await asyncTimeout(extensionId(context), 2_000);
-    await page.goto(`chrome-extension://${this.extensionId}/popup.html`);
-
-    await page.waitForSelector('body');
-
-    this.page = page;
-    this._initialized = true;
-  }
-}
+import { ExtensionManager } from './extension-manager';
 
 describe('Basic test', () => {
   let extensionManager: ExtensionManager;
