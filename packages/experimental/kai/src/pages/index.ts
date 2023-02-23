bbd13b16a1455b4a0869d32ec37c7ce3b6000518
Josiah Witt <josiah@witt.life>
2023-02-22 21:40:49 -0500

    refactor(kai): improve flickering

--- packages/experimental/kai/src/pages/index.ts
+++ packages/experimental/kai/src/pages/index.ts
@@ -4,6 +4,5 @@
 
 import React from 'react';
 
-export const IdentityPage = React.lazy(() => import('./IdentityPage'));
 export const SettingsPage = React.lazy(() => import('./SettingsPage'));
 export const SpacePage = React.lazy(() => import('./SpacePage'));
