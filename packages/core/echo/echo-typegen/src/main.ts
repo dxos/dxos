958b2e159771a6588b349d4976fa62ec0f7ceda0
Dmytro Maretskyi <maretskii@gmail.com>
2023-02-23 12:25:35 +0100

    hardcode schema definitions

--- packages/core/echo/echo-typegen/src/main.ts
+++ packages/core/echo/echo-typegen/src/main.ts
@@ -9,9 +9,57 @@ import * as pb from 'protobufjs';
 
 import { SourceBuilder, generate } from './codegen';
 
+
+export type ProtoResolver = (origin: string, target: string) => string | any | null;
+
+/**
+ * Custom proto file resolver.
+ */
+export function createProtoResolver(original: ProtoResolver, baseDir?: string): ProtoResolver {
+  // NOTE: Must be function to preserve `this`.
+  return function (this: any, origin, target) {
+    if (target === 'dxos/schema.proto') {
+      return 'dxos/schema.proto'
+    }
+
+    return original.call(this, origin, target);
+  };
+}
+
+const registerResolver = (baseDir?: string) => {
+  pb.Root.prototype.resolvePath = createProtoResolver(pb.Root.prototype.resolvePath, baseDir);
+};
+
+
+const loadProtobufBuiltins = () => {
+  const common = require('protobufjs/src/common')
+
+  common("dxos/schema.proto", {
+    nested: {
+      dxos: {
+        nested: {
+          schema: {
+            nested: {
+              Text: {
+                fields: {
+                  test: {
+                    type: "string",
+                    id: 1
+                  },
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
 const main = (source: string, out: string) => {
   console.log(`Reading: ${source}`);
   const root = new pb.Root();
+  debugger;
   root.loadSync(source);
 
   const builder = new SourceBuilder();
@@ -25,5 +73,8 @@ const main = (source: string, out: string) => {
 
 // TODO(burdon): Yargs
 const [, , source, out] = argv;
+
+registerResolver();
+loadProtobufBuiltins();
 
 main(source, out);
