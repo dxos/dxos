00adf94f621716e06d8d0b5b81392b1e865214e6
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-24 14:29:37 +0200

    Add reset button

--- packages/devtools/devtools/src/containers/RootContainer.tsx
+++ packages/devtools/devtools/src/containers/RootContainer.tsx
@@ -6,12 +6,32 @@ import React from 'react';
 import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 
 import { useClientServices } from '@dxos/react-client';
-import { TreeView, TreeViewItem } from '@dxos/react-components';
+import { Button, TreeView, TreeViewItem } from '@dxos/react-components';
 
 import { useSections } from '../hooks';
 
 // TODO(burdon): Restructure sections (panels).
 // TODO(burdon): Sections as hook.
+
+const Footer = () => {
+  const services = useClientServices();
+  if (!services) {
+    return null;
+  }
+  return (
+    <div className='flex flex-col shrink-0 my-4'>
+      <Button
+        variant='ghost'
+        onClick={() => {
+          services?.SystemService.reset();
+        }}
+        className='justify-start mli-2'
+      >
+        <span className='mis-2'>Reset</span>
+      </Button>
+    </div>
+  );
+};
 
 export const RootContainer = () => {
   const sections = useSections();
@@ -22,11 +42,6 @@ export const RootContainer = () => {
     navigate(item.id);
   };
 
-  const services = useClientServices();
-  if (!services) {
-    return null;
-  }
-
   return (
     <div className='flex w-full h-screen overflow-hidden'>
       <div className={'flex flex-col w-[180px] overflow-hidden overflow-y-auto bg-gray-200 border-gray-400 border-r'}>
@@ -36,6 +51,8 @@ export const RootContainer = () => {
           onSelect={handleSelect}
           expanded={sections.map((section) => section.id)}
         />
+        <div className='flex-1' />
+        <Footer />
       </div>
 
       <div className='flex flex-1 flex-col overflow-hidden bg-white'>
