8d5128fbd8e896832d4fe4aac5754453101667d2
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-24 14:53:56 +0200

    Simplify testbench setup

--- packages/devtools/devtools/src/testbench/devtools/App.tsx
+++ packages/devtools/devtools/src/testbench/devtools/App.tsx
@@ -3,18 +3,9 @@
 //
 
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
 
-import { appkitTranslations, Fallback } from '@dxos/react-appkit';
-import { ClientContext } from '@dxos/react-client';
-import { ThemeProvider } from '@dxos/react-components';
-import { ErrorBoundary } from '@dxos/react-toolkit';
-
-import { DevtoolsContextProvider, useProxiedClient, useRoutes } from '../../hooks';
-
-const Routes = () => {
-  return useRoutes();
-};
+import { Devtools } from '../../app';
+import { useProxiedClient } from '../../hooks';
 
 export const App = () => {
   const client = useProxiedClient();
@@ -22,21 +13,5 @@ export const App = () => {
     return null;
   }
 
-  return (
-    <ThemeProvider
-      appNs='devtools'
-      resourceExtensions={[appkitTranslations]}
-      fallback={<Fallback message='Loading...' />}
-    >
-      <ErrorBoundary>
-        <ClientContext.Provider value={client}>
-          <DevtoolsContextProvider>
-            <HashRouter>
-              <Routes />
-            </HashRouter>
-          </DevtoolsContextProvider>
-        </ClientContext.Provider>
-      </ErrorBoundary>
-    </ThemeProvider>
-  );
+  return <Devtools client={client} />;
 };
