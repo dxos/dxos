ee65e9a522d0cd13929ab264b8e4c9ee8e65c3d1
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-02-27 14:43:38 +0200

    feat: Devtools stability (#2577)

--- packages/devtools/devtools/src/index.ts
+++ packages/devtools/devtools/src/index.ts
@@ -2,7 +2,4 @@
 // Copyright 2020 DXOS.org
 //
 
-export * from './components';
-export * from './containers';
-export * from './hooks';
-export * from './panels';
+export { Devtools } from './app';
