1188ee5002272d8e0bc61375b166921826b41acc
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-02-27 14:43:38 +0200

    feat: Devtools stability (#2577)

---
+++ packages/devtools/devtools/src/app/Devtools.tsx
@@ -0,0 +1,47 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+
+import { ClientServices } from '@dxos/client-services';
+import { appkitTranslations, Fallback, useTelemetry } from '@dxos/react-appkit';
+import { Client, ClientContext } from '@dxos/react-client';
+import { ThemeProvider } from '@dxos/react-components';
+import { ErrorBoundary } from '@dxos/react-toolkit';
+
+import { DevtoolsContextProvider, useRoutes } from '../hooks';
+
+const Routes = () => {
+  return useRoutes();
+};
+
+const Telemetry = () => {
+  useTelemetry({ namespace: 'devtools', router: false });
+  return null;
+};
+
+// Entry point that does not have opinion on Client, so it can be reused in extension.
+export const Devtools = ({ context }: { context?: { client: Client; services?: ClientServices } }) => {
+  const fallback = <Fallback message='Loading...' />;
+
+  if (!context) {
+    return fallback;
+  }
+
+  return (
+    <ThemeProvider appNs='devtools' resourceExtensions={[appkitTranslations]} fallback={fallback}>
+      <ErrorBoundary>
+        <ClientContext.Provider value={context}>
+          <DevtoolsContextProvider>
+            <HashRouter>
+              <Telemetry />
+              <Routes />
+            </HashRouter>
+          </DevtoolsContextProvider>
+        </ClientContext.Provider>
+      </ErrorBoundary>
+    </ThemeProvider>
+  );
+};
