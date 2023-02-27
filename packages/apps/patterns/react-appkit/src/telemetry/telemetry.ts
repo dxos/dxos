539810a3954b8c40242c4090450ae1ced8539dcf
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-02-27 13:39:41 +0200

    Add  check for initialized

--- packages/apps/patterns/react-appkit/src/telemetry/telemetry.ts
+++ packages/apps/patterns/react-appkit/src/telemetry/telemetry.ts
@@ -20,7 +20,7 @@ if (navigator.storage?.estimate) {
 // TODO(wittjosiah): Store uuid in halo for the purposes of usage metrics.
 // await client.halo.getGlobalPreference('dxosTelemetryIdentifier');
 export const getTelemetryIdentifier = (client: Client) => {
-  if (!client) {
+  if (!client?.initialized) {
     return undefined;
   }
   const profile = client.halo.identity;
