9600dc2badd4aa03f337d91a5f5b1fbe0af2054b
Rich Burdon <rich.burdon@gmail.com>
2023-02-28 07:26:08 -0500

    chore: Remove deprecated packages (#2588)
    Co-authored-by: Mykola Veremchuk <veremchukmukola@gmail.com>

--- packages/deprecated/react-components-deprecated/stories/helpers/index.ts
+++
@@ -1,6 +0,0 @@
-//
-// Copyright 2020 DXOS.org
-//
-
-export * from './Container';
-export * from './ThemeProvider';
--- packages/deprecated/react-toolkit/stories/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-//
-// Copyright 2020 DXOS.org
-//
-
-export * from './ClientInitializer';
-export * from './Column';
-export * from './Container';
