bfe860450abbcbcfa6873c9290c696bfb8bb5e51
egorgripasov <gripasovegor@gmail.com>
2023-03-01 16:33:52 +0000

    fix: lint.

--- packages/devtools/cli/src/commands/kube/auth.ts
+++ packages/devtools/cli/src/commands/kube/auth.ts
@@ -5,7 +5,7 @@
 import chalk from 'chalk';
 import assert from 'node:assert';
 
-import { Trigger, asyncTimeout, sleep } from '@dxos/async';
+import { Trigger, asyncTimeout } from '@dxos/async';
 import { Client } from '@dxos/client';
 import { PublicKey } from '@dxos/keys';
 import { Credential } from '@dxos/protocols/proto/dxos/halo/credentials';
@@ -15,13 +15,17 @@ import { SupervisorRpcPeer } from '../../util';
 
 const timeout = 500;
 
-const getCredentials = async (client: Client, type: string, predicate?: (value: Credential) => boolean): Promise<Credential[]> => {
+const getCredentials = async (
+  client: Client,
+  type: string,
+  predicate?: (value: Credential) => boolean
+): Promise<Credential[]> => {
   const credentialsQuery = client.halo.queryCredentials({ type });
   const trigger = new Trigger<Credential[]>();
   credentialsQuery.subscribe({
     onUpdate: (credentials: Credential[]) => {
       if (predicate) {
-        credentials = credentials.filter(predicate)
+        credentials = credentials.filter(predicate);
       }
       if (credentials.length) {
         trigger.wake(credentials);
@@ -47,10 +51,12 @@ export default class Auth extends BaseCommand {
     this.log(chalk`{gray Initiating presentation sequence..}`);
     // TODO(egorgripasov): Find AuthorizedDevice by KubeAccess credential.
     const { deviceKey } = client.halo.device!;
-    const authDeviceCreds = await getCredentials(client, 'dxos.halo.credentials.AuthorizedDevice', cred => PublicKey.equals(cred.subject.id, deviceKey!));
+    const authDeviceCreds = await getCredentials(client, 'dxos.halo.credentials.AuthorizedDevice', (cred) =>
+      PublicKey.equals(cred.subject.id, deviceKey!)
+    );
 
     if (!authDeviceCreds.length) {
-      throw new Error("No authorized devices");
+      throw new Error('No authorized devices');
     }
 
     // Init auth sequence.
@@ -58,7 +64,9 @@ export default class Auth extends BaseCommand {
       const { nonce, kubeKey } = await supervisor.rpc.initAuthSequence();
 
       // Find proper KubeAccess credential.
-      const kubeAccessCreds = await getCredentials(client, 'dxos.halo.credentials.KubeAccess', cred => PublicKey.equals(cred.issuer, kubeKey));
+      const kubeAccessCreds = await getCredentials(client, 'dxos.halo.credentials.KubeAccess', (cred) =>
+        PublicKey.equals(cred.issuer, kubeKey)
+      );
 
       if (!kubeAccessCreds.length) {
         this.log(chalk`{gray No kube access credentials - requesting..}`);
@@ -66,7 +74,7 @@ export default class Auth extends BaseCommand {
         this.log(chalk`{gray Kube access credentials found - requesting session token..}`);
       }
 
-      const credsToPresent = [authDeviceCreds[0].id, kubeAccessCreds[0]?.id].filter(Boolean)
+      const credsToPresent = [authDeviceCreds[0].id, kubeAccessCreds[0]?.id].filter(Boolean);
       // Create presentation.
       const presentation = await client.halo.presentCredentials({
         ids: credsToPresent as PublicKey[],
@@ -86,7 +94,6 @@ export default class Auth extends BaseCommand {
         // TODO(burdon): Error if called twice with no halo.
         //  Error [OpenError]: Error parsing JSON in /tmp/user-1/dx/cli/keystore/data.json: Unexpected end of JSON input
         this.log(chalk`{red Profile not initialized.}`);
-        return;
       } else {
         const { credential, token } = await this.presentAuthCredentials(client);
         assert(!!credential || !!token, 'No credentials or token received.');
