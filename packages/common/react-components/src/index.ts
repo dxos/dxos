1c0cb4b5de2a3defdfbdb47f67c0a0be5e9a6864
Will Shown <thure@users.noreply.github.com>
2023-03-06 13:59:52 -0800

    chore: Update docs to describe how build works generally. (#2659)
    Resolves #2657.

--- packages/common/react-components/src/index.ts
+++ packages/common/react-components/src/index.ts
@@ -4,6 +4,7 @@
 
 export { useTranslation, Trans } from 'react-i18next';
 export * from './components';
+export * from './config';
 export * from './hooks';
 export * from './props';
 export * from './styles';
