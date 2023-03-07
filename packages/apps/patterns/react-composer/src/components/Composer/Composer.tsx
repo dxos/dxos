b5b06f64f35e6c9be0ffb77e41b9f1a91103d777
Will Shown <thure@users.noreply.github.com>
2023-03-06 16:44:23 -0800

    fix(react-composer): a-priori attempt to fix remounting (#2664)

--- packages/apps/patterns/react-composer/src/components/Composer/Composer.tsx
+++ packages/apps/patterns/react-composer/src/components/Composer/Composer.tsx
@@ -48,111 +48,108 @@ export const useComposerEditor = ({ document, field = 'content', placeholder, sl
   // TODO(wittjosiah): Provide own translations?
   //   Maybe default is not translated and translated placeholder can be provided by the app.
   const { t } = useTranslation('appkit');
-  return useEditor(
-    {
-      extensions: [
-        StarterKit.configure({
-          // Extensions
-          history: false,
-          // Nodes
-          blockquote: {
-            HTMLAttributes: {
-              class: 'mlb-2 border-is-4 border-neutral-500/50 pis-5'
-            }
-          },
-          bulletList: {
-            HTMLAttributes: {
-              class:
-                // todo (thure): Tailwind was not seeing `[&>li:before]:content-["•"]` as a utility class, but it would work if instead of `"•"` it was `"X"`… why?
-                'mlb-2 grid grid-cols-[min-content_1fr] [&>li:before]:content-[attr(marker)] [&>li:before]:mlb-1 [&>li:before]:mie-2'
-            }
-          },
-          codeBlock: {
-            HTMLAttributes: {
-              class: 'mlb-2 font-mono bg-neutral-500/10 p-3 rounded'
-            }
-          },
-          heading: false, // (thure): `StarterKit` doesn’t let you configure how headings are rendered, see `Heading` below.
-          horizontalRule: {
-            HTMLAttributes: {
-              class: 'mlb-4 border-neutral-500/50'
-            }
-          },
-          listItem: false, // (thure): `StarterKit` doesn’t let you configure how list items are rendered, see `ListItem` below.
-          orderedList: {
-            HTMLAttributes: {
-              class:
-                'mlb-2 grid grid-cols-[min-content_1fr]  [&>li:before]:content-[counters(section,_".")_"._"] [counter-reset:section] [&>li:before]:mlb-1'
-            }
-          },
-          paragraph: {
-            HTMLAttributes: {
-              class: 'mlb-1'
-            }
-          },
-          // Marks
-          bold: {
-            HTMLAttributes: {
-              class: 'font-bold'
-            }
-          },
-          code: {
-            HTMLAttributes: {
-              class: 'font-mono bg-neutral-500/10 rounded pli-1.5 mli-0.5 plb-0.5 -mlb-0.5'
-            }
-          },
-          italic: {
-            HTMLAttributes: {
-              class: 'italic'
-            }
-          },
-          strike: {
-            HTMLAttributes: {
-              class: 'line-through'
-            }
+  return useEditor({
+    extensions: [
+      StarterKit.configure({
+        // Extensions
+        history: false,
+        // Nodes
+        blockquote: {
+          HTMLAttributes: {
+            class: 'mlb-2 border-is-4 border-neutral-500/50 pis-5'
           }
-        }),
-        Heading.extend({
-          renderHTML({ node, HTMLAttributes }) {
-            const hasLevel = this.options.levels.includes(node.attrs.level);
-            const level: Levels = hasLevel ? node.attrs.level : this.options.levels[0];
-
-            return [
-              `h${level}`,
-              mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
-                class: headingClassNames[level]
-              }),
-              0
-            ];
+        },
+        bulletList: {
+          HTMLAttributes: {
+            class:
+              // todo (thure): Tailwind was not seeing `[&>li:before]:content-["•"]` as a utility class, but it would work if instead of `"•"` it was `"X"`… why?
+              'mlb-2 grid grid-cols-[min-content_1fr] [&>li:before]:content-[attr(marker)] [&>li:before]:mlb-1 [&>li:before]:mie-2'
           }
-        }),
-        ListItem.extend({
-          renderHTML: ({ HTMLAttributes }) => [
-            'li',
-            mergeAttributes(HTMLAttributes, {
-              marker: '• ',
-              class: 'contents before:[counter-increment:section]'
-            }),
-            ['div', { role: 'none' }, 0]
-          ]
-        }),
-        // https://github.com/ueberdosis/tiptap/tree/main/packages/extension-collaboration
-        Collaboration.configure({ document: document.doc!, field }),
-        Placeholder.configure({
-          placeholder: placeholder ?? t('composer placeholder'),
-          emptyEditorClass: 'before:content-[attr(data-placeholder)] before:absolute opacity-50 cursor-text'
-        })
-      ],
-      editorProps: {
-        attributes: {
-          class: mx('focus:outline-none focus-visible:outline-none', slots.editor?.className),
-          spellcheck: slots.editor?.spellCheck === false ? 'false' : 'true',
-          tabindex: slots?.editor?.tabIndex ? String(slots?.editor?.tabIndex) : '0'
+        },
+        codeBlock: {
+          HTMLAttributes: {
+            class: 'mlb-2 font-mono bg-neutral-500/10 p-3 rounded'
+          }
+        },
+        heading: false, // (thure): `StarterKit` doesn’t let you configure how headings are rendered, see `Heading` below.
+        horizontalRule: {
+          HTMLAttributes: {
+            class: 'mlb-4 border-neutral-500/50'
+          }
+        },
+        listItem: false, // (thure): `StarterKit` doesn’t let you configure how list items are rendered, see `ListItem` below.
+        orderedList: {
+          HTMLAttributes: {
+            class:
+              'mlb-2 grid grid-cols-[min-content_1fr]  [&>li:before]:content-[counters(section,_".")_"._"] [counter-reset:section] [&>li:before]:mlb-1'
+          }
+        },
+        paragraph: {
+          HTMLAttributes: {
+            class: 'mlb-1'
+          }
+        },
+        // Marks
+        bold: {
+          HTMLAttributes: {
+            class: 'font-bold'
+          }
+        },
+        code: {
+          HTMLAttributes: {
+            class: 'font-mono bg-neutral-500/10 rounded pli-1.5 mli-0.5 plb-0.5 -mlb-0.5'
+          }
+        },
+        italic: {
+          HTMLAttributes: {
+            class: 'italic'
+          }
+        },
+        strike: {
+          HTMLAttributes: {
+            class: 'line-through'
+          }
         }
+      }),
+      Heading.extend({
+        renderHTML({ node, HTMLAttributes }) {
+          const hasLevel = this.options.levels.includes(node.attrs.level);
+          const level: Levels = hasLevel ? node.attrs.level : this.options.levels[0];
+
+          return [
+            `h${level}`,
+            mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
+              class: headingClassNames[level]
+            }),
+            0
+          ];
+        }
+      }),
+      ListItem.extend({
+        renderHTML: ({ HTMLAttributes }) => [
+          'li',
+          mergeAttributes(HTMLAttributes, {
+            marker: '• ',
+            class: 'contents before:[counter-increment:section]'
+          }),
+          ['div', { role: 'none' }, 0]
+        ]
+      }),
+      // https://github.com/ueberdosis/tiptap/tree/main/packages/extension-collaboration
+      Collaboration.configure({ document: document.doc, field }),
+      Placeholder.configure({
+        placeholder: placeholder ?? t('composer placeholder'),
+        emptyEditorClass: 'before:content-[attr(data-placeholder)] before:absolute opacity-50 cursor-text'
+      })
+    ],
+    editorProps: {
+      attributes: {
+        class: mx('focus:outline-none focus-visible:outline-none', slots.editor?.className),
+        spellcheck: slots.editor?.spellCheck === false ? 'false' : 'true',
+        tabindex: slots?.editor?.tabIndex ? String(slots?.editor?.tabIndex) : '0'
       }
-    },
-    [document, document?.doc]
-  );
+    }
+  });
 };
 
 /**
