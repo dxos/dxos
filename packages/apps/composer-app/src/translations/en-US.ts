9b9e7121dcaa31169a92c996ad67d0cb9f4f5603
thure <will@braneframe.com>
2023-03-08 15:18:32 -0800

    Use Dialog for importing to avoid conflicts.

--- packages/apps/composer-app/src/translations/en-US.ts
+++ packages/apps/composer-app/src/translations/en-US.ts
@@ -11,5 +11,9 @@ export const composer = {
   'loading document message': 'Loading document…',
   'no spaces message': 'Create or join a space to start creating documents',
   'export to markdown label': 'Export to Markdown',
-  'import from markdown label': 'Import from Markdown'
+  'import from markdown label': 'Import from Markdown',
+  'confirm import title': 'Overwrite this document’s content?',
+  'confirm import body':
+    'Importing a Markdown file will overwrite this document’s content. If you’d rather import into a new document, first create a new document and then import into it.',
+  'upload file message': 'Drag file here or click to browse'
 };
