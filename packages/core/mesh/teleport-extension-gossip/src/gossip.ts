db43b7e3ac5f6908f3e9838574c3de534602c5f6
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-03-08 14:33:23 +0200

    Add gossip manager

---
+++ packages/core/mesh/teleport-extension-gossip/src/gossip.ts
@@ -0,0 +1,103 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { scheduleTask } from '@dxos/async';
+import { Context } from '@dxos/context';
+import { PublicKey } from '@dxos/keys';
+import { log } from '@dxos/log';
+import { GossipMessage } from '@dxos/protocols/proto/dxos/mesh/teleport/gossip';
+import { ComplexMap, ComplexSet } from '@dxos/util';
+
+import { GossipExtension } from './gossip-extension';
+
+export type GossipParams = {
+  localPeerId: PublicKey;
+};
+
+/**
+ * Gossip extensions manager.
+ * Keeps track of all peers that are connected to the local peer.
+ * Routes received announces to all connected peers.
+ * Exposes API send announce to everybody and subscribe to .
+ */
+export class Gossip {
+  private readonly _ctx = new Context({
+    onError: (err) => {
+      log.catch(err);
+    }
+  });
+
+  constructor(private readonly _params: GossipParams) {}
+
+  private readonly _listeners = new Map<string, Set<(message: GossipMessage) => null>>();
+
+  private readonly _receivedMessages = new ComplexSet<PublicKey>(PublicKey.hash); // TODO(mykola): Memory leak. Never cleared.
+
+  // remotePeerId -> PresenceExtension
+  private readonly _connections = new ComplexMap<PublicKey, GossipExtension>(PublicKey.hash);
+
+  createExtension({ remotePeerId }: { remotePeerId: PublicKey }): GossipExtension {
+    const extension = new GossipExtension({
+      onAnnounce: async (message) => {
+        if (this._receivedMessages.has(message.messageId)) {
+          return;
+        }
+        this._receivedMessages.add(message.messageId);
+        scheduleTask(this._ctx, async () => {
+          await this._propagateAnnounce(message);
+        });
+      },
+      onClose: async (err) => {
+        if (err) {
+          log.catch(err);
+        }
+        if (this._connections.has(remotePeerId)) {
+          this._connections.delete(remotePeerId);
+        }
+      }
+    });
+    this._connections.set(remotePeerId, extension);
+
+    return extension;
+  }
+
+  sendMessage(channel: string, payload: any) {
+    return Promise.all(
+      [...this._connections.values()].map((extension) =>
+        extension
+          .sendAnnounce({
+            peerId: this._params.localPeerId,
+            messageId: PublicKey.random(),
+            channelId: channel,
+            payload
+          })
+          .catch((err) => log.warn(err))
+      )
+    );
+  }
+
+  listen(channel: string, callback: (payload: any) => null) {
+    if (!this._listeners.has(channel)) {
+      this._listeners.set(channel, new Set());
+    }
+    this._listeners.get(channel)!.add(callback);
+
+    return {
+      unsubscribe: () => {
+        this._listeners.get(channel)!.delete(callback);
+      }
+    };
+  }
+
+  private _propagateAnnounce(message: GossipMessage) {
+    return Promise.all(
+      [...this._connections.entries()].map(async ([remotePeerId, extension]) => {
+        if (this._params.localPeerId.equals(message.peerId) || remotePeerId.equals(message.peerId)) {
+          return;
+        }
+        return extension.sendAnnounce(message).catch((err) => log.catch(err));
+      })
+    );
+  }
+}
