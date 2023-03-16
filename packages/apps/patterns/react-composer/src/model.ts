35935e331d0040c8d451fe5d92b4565bc761feed
Josiah Witt <josiah@witt.life>
2023-03-16 17:06:19 -0400

    feat(text-model): support plain text (#2738)

---
+++ packages/apps/patterns/react-composer/src/model.ts
@@ -0,0 +1,61 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { useMemo, useReducer } from 'react';
+import * as awarenessProtocol from 'y-protocols/awareness';
+
+import { Identity, Space, Text } from '@dxos/client';
+import { useSubscription } from '@dxos/react-client';
+import type { YText, YXmlFragment } from '@dxos/text-model';
+
+import { SpaceProvider } from './yjs';
+
+type Awareness = awarenessProtocol.Awareness;
+type Provider = { awareness: Awareness };
+
+// TODO(wittjosiah): Factor out to common package? @dxos/react-client?
+export type ComposerModel = {
+  id: string;
+  content: string | YText | YXmlFragment;
+  provider?: Provider;
+  peer?: {
+    id: string;
+    name?: string;
+  };
+};
+
+export type UseTextModelOptions = {
+  identity?: Identity;
+  space?: Space;
+  text?: Text;
+};
+
+// TODO(wittjosiah): Factor out to common package? @dxos/react-client?
+export const useTextModel = ({ identity, space, text }: UseTextModelOptions): ComposerModel | undefined => {
+  // TODO(wittjosiah): Support `observer` with `forwardRef`.
+  const [, forceUpdate] = useReducer((state) => state + 1, 0);
+  useSubscription(forceUpdate, [text]);
+
+  const provider = useMemo(() => {
+    if (!space || !text?.doc) {
+      return undefined;
+    }
+
+    return new SpaceProvider({ space, doc: text.doc });
+  }, [identity, space, text?.doc]);
+
+  if (!text?.doc || !text?.content) {
+    return undefined;
+  }
+
+  return {
+    id: text.doc.guid,
+    content: text.content,
+    provider,
+    peer: identity && {
+      id: identity.identityKey.toHex(),
+      name: identity.profile?.displayName
+    }
+  };
+};
