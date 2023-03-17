d306fe7b8d768576729b4c43d0036e5bf069f07c
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-03-17 12:11:02 +0200

    Add options parsing

--- packages/core/echo/echo-schema/src/schema.ts
+++ packages/core/echo/echo-schema/src/schema.ts
@@ -11,7 +11,7 @@ import { TextModel } from '@dxos/text-model';
 import { TypeFilter } from './query';
 import { strip } from './util';
 
-export type EchoType =
+export type EchoType = { options?: { [key: string]: any } } & (
   | {
       kind: 'number' | 'string' | 'boolean' | 'bytes';
     }
@@ -36,90 +36,12 @@ export type EchoType =
       kind: 'enum';
       enumType: string;
       // TODO(mykola): Add ability to list enum values.
-    };
+    }
+);
 
 export type EchoSchemaField = {
   name: string;
   type: EchoType;
-};
-
-const isTextObject = (typeName: string) => typeName.split('.').at(-1) === 'Text';
-
-const getFields = (type: pb.Type): EchoSchemaField[] => {
-  type.fieldsArray.forEach((field) => field.resolve());
-
-  return type.fieldsArray.map((field) => {
-    const getComplexType = (type: pb.Type | pb.Enum): EchoType => {
-      if (type instanceof pb.Enum) {
-        return { kind: 'enum', enumType: type.fullName.slice(1) };
-      } else if (type instanceof pb.Type) {
-        if (isTextObject(type.fullName)) {
-          return {
-            kind: 'ref',
-            objectType: type.fullName.slice(1),
-            modelType: TextModel.meta.type
-          };
-        } else if (type.options && type.options['(object)']) {
-          return {
-            kind: 'ref',
-            objectType: type.fullName.slice(1),
-            modelType: DocumentModel.meta.type
-          };
-        } else {
-          return { kind: 'record', objectType: type.fullName.slice(1) };
-        }
-      }
-
-      throw new Error(`Unknown type: ${type}`);
-    };
-
-    const getBasicType = (type: string): EchoType => {
-      switch (type) {
-        case 'double':
-        case 'float':
-        case 'int32':
-        case 'uint32':
-        case 'sint32':
-        case 'fixed32':
-        case 'sfixed32':
-        case 'int64':
-        case 'uint64':
-        case 'sint64':
-        case 'fixed64':
-        case 'sfixed64':
-          return { kind: 'number' };
-        case 'string':
-          return { kind: 'string' };
-        case 'bytes':
-          return { kind: 'bytes' };
-        case 'bool':
-          return { kind: 'boolean' };
-        default:
-          throw new Error(`Unknown type: ${type}`);
-      }
-    };
-
-    const getEchoType = (field: pb.Field): EchoType => {
-      if (field.resolvedType) {
-        if (field.repeated) {
-          return { kind: 'array', elementType: getComplexType(field.resolvedType) };
-        } else {
-          return getComplexType(field.resolvedType);
-        }
-      }
-
-      if (field.repeated) {
-        return { kind: 'array', elementType: getBasicType(field.type) };
-      } else {
-        return getBasicType(field.type);
-      }
-    };
-
-    return {
-      name: field.name,
-      type: getEchoType(field)
-    };
-  });
 };
 
 /**
@@ -230,3 +152,85 @@ const filterNamespaces = ({
 
   return filtered;
 };
+
+const getFields = (type: pb.Type): EchoSchemaField[] => {
+  type.fieldsArray.forEach((field) => field.resolve());
+  return type.fieldsArray.map((field) => {
+    const echoField: EchoSchemaField = {
+      name: field.name,
+      type: getEchoType(field)
+    };
+    if (field.options) {
+      echoField.type.options = field.options;
+    }
+    return echoField;
+  });
+};
+
+const getEchoType = (field: pb.Field): EchoType => {
+  if (field.resolvedType) {
+    if (field.repeated) {
+      return { kind: 'array', elementType: getComplexType(field.resolvedType) };
+    } else {
+      return getComplexType(field.resolvedType);
+    }
+  }
+
+  if (field.repeated) {
+    return { kind: 'array', elementType: getBasicType(field.type) };
+  } else {
+    return getBasicType(field.type);
+  }
+};
+
+const getComplexType = (type: pb.Type | pb.Enum): EchoType => {
+  if (type instanceof pb.Enum) {
+    return { kind: 'enum', enumType: type.fullName.slice(1) };
+  } else if (type instanceof pb.Type) {
+    if (isTextObject(type.fullName)) {
+      return {
+        kind: 'ref',
+        objectType: type.fullName.slice(1),
+        modelType: TextModel.meta.type
+      };
+    } else if (type.options && type.options['(object)']) {
+      return {
+        kind: 'ref',
+        objectType: type.fullName.slice(1),
+        modelType: DocumentModel.meta.type
+      };
+    } else {
+      return { kind: 'record', objectType: type.fullName.slice(1) };
+    }
+  }
+
+  throw new Error(`Unknown type: ${type}`);
+};
+
+const getBasicType = (type: string): EchoType => {
+  switch (type) {
+    case 'double':
+    case 'float':
+    case 'int32':
+    case 'uint32':
+    case 'sint32':
+    case 'fixed32':
+    case 'sfixed32':
+    case 'int64':
+    case 'uint64':
+    case 'sint64':
+    case 'fixed64':
+    case 'sfixed64':
+      return { kind: 'number' };
+    case 'string':
+      return { kind: 'string' };
+    case 'bytes':
+      return { kind: 'bytes' };
+    case 'bool':
+      return { kind: 'boolean' };
+    default:
+      throw new Error(`Unknown type: ${type}`);
+  }
+};
+
+const isTextObject = (typeName: string) => typeName.split('.').at(-1) === 'Text';
