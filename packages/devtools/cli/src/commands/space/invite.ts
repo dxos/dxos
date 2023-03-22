526cd3882d7d56e1c6199119731d0fc8eb7fce7e
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-03-22 18:23:07 +0200

    feat: Join CLI as device in HALO (#2795)

--- packages/devtools/cli/src/commands/space/invite.ts
+++ packages/devtools/cli/src/commands/space/invite.ts
@@ -2,6 +2,8 @@
 // Copyright 2022 DXOS.org
 //
 
+import { Args } from '@oclif/core';
+
 import { Client } from '@dxos/client';
 import { truncateKey } from '@dxos/debug';
 
@@ -11,11 +13,7 @@ import { selectSpace } from '../../util';
 // TODO(burdon): Reconcile invite/share.
 export default class Invite extends BaseCommand {
   static override description = 'Create space invitation.';
-  static override args = [
-    {
-      name: 'key'
-    }
-  ];
+  static override args = { key: Args.string({ required: true }) };
 
   async run(): Promise<any> {
     const { args } = await this.parse(Invite);
