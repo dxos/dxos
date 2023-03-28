6507c3467ad46a3d9d06869dd0cad7eaeccaf910
Will Shown <thure@users.noreply.github.com>
2023-03-28 13:59:36 -0700

    feat(react-composer): Allow Markdown theme to be extended (#2866)

--- packages/apps/composer-app/src/pages/DocumentPage.tsx
+++ packages/apps/composer-app/src/pages/DocumentPage.tsx
@@ -351,7 +351,13 @@ const MarkdownDocumentPage = observer(({ document, space }: { document: Composer
               role: 'none',
               className: 'pli-6 mbs-4'
             },
-            editor: { className: 'pbe-20' }
+            editor: {
+              markdownTheme: {
+                '& .cm-line': {
+                  paddingInline: '1.5rem'
+                }
+              }
+            }
           }}
         />
       </DocumentPageContent>
