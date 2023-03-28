220ec8646accbf1c6e881e28edffc19ef2c48dd1
Zhenya <zhenya@braneframe.com>
2023-03-28 01:52:22 -0400

    lint

--- packages/core/mesh/network-manager/src/tests/memory-transport.test.ts
+++ packages/core/mesh/network-manager/src/tests/memory-transport.test.ts
@@ -17,46 +17,47 @@ describe('Memory transport', () => {
   const testBuilder = new TestBuilder();
   basicTestSuite(testBuilder);
 
-  // eslint-ignore-line
-  test.skip('load', async () => {
-    const NUM_PAIRS = 100;
-    const NUM_ROUNDS = 10_000;
-    const PACKET_SIZE = 1_000;
+  test
+    .skip('load', async () => {
+      const NUM_PAIRS = 100;
+      const NUM_ROUNDS = 10_000;
+      const PACKET_SIZE = 1_000;
 
-    const pairs = await Promise.all(
-      range(NUM_PAIRS).map(async () => {
-        const peer1 = testBuilder.createPeer();
-        const peer2 = testBuilder.createPeer();
-        await openAndCloseAfterTest([peer1, peer2]);
+      const pairs = await Promise.all(
+        range(NUM_PAIRS).map(async () => {
+          const peer1 = testBuilder.createPeer();
+          const peer2 = testBuilder.createPeer();
+          await openAndCloseAfterTest([peer1, peer2]);
 
-        const topic = PublicKey.random();
-        const [swarm1, swarm2] = await joinSwarm([peer1, peer2], topic, () => new FullyConnectedTopology());
+          const topic = PublicKey.random();
+          const [swarm1, swarm2] = await joinSwarm([peer1, peer2], topic, () => new FullyConnectedTopology());
 
-        return {
-          peer1,
-          peer2,
-          topic,
-          swarm1,
-          swarm2
-        };
-      })
-    );
-
-    const message = randomBytes(PACKET_SIZE / 2).toString('hex');
-
-    for (let i = 0; i < NUM_ROUNDS; i++) {
-      console.log(`Round ${i}/${NUM_ROUNDS}`);
-      await Promise.all(
-        pairs.map(async ({ swarm1, swarm2 }) => {
-          await exchangeMessages(swarm1, swarm2, message);
+          return {
+            peer1,
+            peer2,
+            topic,
+            swarm1,
+            swarm2
+          };
         })
       );
-    }
 
-    await Promise.all(
-      pairs.map(async ({ peer1, peer2, topic }) => {
-        await leaveSwarm([peer1, peer2], topic);
-      })
-    );
-  }).timeout(1_000_000);
+      const message = randomBytes(PACKET_SIZE / 2).toString('hex');
+
+      for (let i = 0; i < NUM_ROUNDS; i++) {
+        console.log(`Round ${i}/${NUM_ROUNDS}`);
+        await Promise.all(
+          pairs.map(async ({ swarm1, swarm2 }) => {
+            await exchangeMessages(swarm1, swarm2, message);
+          })
+        );
+      }
+
+      await Promise.all(
+        pairs.map(async ({ peer1, peer2, topic }) => {
+          await leaveSwarm([peer1, peer2], topic);
+        })
+      );
+    })
+    .timeout(1_000_000);
 });
