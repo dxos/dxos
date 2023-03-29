b8a9f2cba44da7f76ea67b5ba2fd01a3a54dd7ac
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-03-29 16:40:47 +0300

    Add protocols tracing

--- packages/common/sentry/src/tracing.ts
+++ packages/common/sentry/src/tracing.ts
@@ -2,11 +2,11 @@
 // Copyright 2023 DXOS.org
 //
 
-import { startTransaction } from '@sentry/node';
+import { setUser, startTransaction } from '@sentry/node';
 import { Transaction, Span } from '@sentry/types';
 import assert from 'node:assert';
 
-import { getContextFromEntry, log, LogEntry, LogLevel, LogProcessor } from '@dxos/log';
+import { getContextFromEntry, log, LogLevel, LogProcessor } from '@dxos/log';
 
 let TX!: Transaction;
 const SPAN_MAP = new Map<string, Span>();
@@ -35,88 +35,75 @@ export const finish = () => {
   TX.finish();
 };
 
-export type TraceStatus = 'ok' | 'error';
-
-export type TracingContext = {
-  span?: {
-    id: string;
-    command?: 'begin' | 'end';
-    parent?: string;
-    status?: TraceStatus;
-  };
-};
-
 export const SENTRY_PROCESSOR: LogProcessor = (config, entry) => {
   if (entry.level !== LogLevel.TRACE) {
     return;
   }
-  const context = getContextFromEntry(entry) as TracingContext;
+  const context = getContextFromEntry(entry);
+
+  if (entry.message === 'dxos.halo.identity' && context?.identityKey) {
+    setUser({
+      id: context.identityKey,
+      username: context.profileName
+    });
+  }
 
   if (context?.span) {
     switch (context.span.command) {
       case 'begin': {
-        beginSpan(entry, context);
+        const id = context.span.id;
+
+        if (!id || SPAN_MAP.has(id)) {
+          return;
+        }
+
+        let parentSpan: Span = TX;
+        if (context.span.parent) {
+          parentSpan = SPAN_MAP.get(context.span.parent) || TX;
+        }
+
+        const span = parentSpan.startChild({
+          op: entry.message,
+          data: {
+            ...context.span.data,
+            '@dxos/log': { ...entry, context }
+          }
+        });
+        SPAN_MAP.set(context.span.id, span);
         break;
       }
+
       case 'end': {
         const span = SPAN_MAP.get(context.span.id);
         if (span) {
+          span.setStatus(getSpanStatus(context.span.status));
+          context.span.data && Object.entries(context.span.data).forEach(([key, value]) => span.setData(key, value));
           span.finish();
           SPAN_MAP.delete(context.span.id);
         }
         break;
       }
-      default: {
-        const id = context.span.id;
-        if (!id || !SPAN_MAP.has(id)) {
-          beginSpan(entry, context);
-        } else {
-          const span = SPAN_MAP.get(context.span.id)!;
-          span.updateWithContext({
-            op: entry.message,
-            data: { ...entry, context },
-            status: getSpanStatus(context.span.status)
-          });
-        }
 
+      case 'update': {
+        const span = SPAN_MAP.get(context.span.id);
+        if (span) {
+          context.span.data && Object.entries(context.span.data).forEach(([key, value]) => span.setData(key, value));
+        }
         break;
       }
     }
   }
 };
 
-const beginSpan = (entry: LogEntry, context: TracingContext) => {
-  assert(context.span);
-  const id = context.span.id;
-
-  if (!id || SPAN_MAP.has(id)) {
-    return;
-  }
-
-  let parentSpan: Span = TX;
-  if (context.span.parent) {
-    parentSpan = SPAN_MAP.get(context.span.parent) || TX;
-  }
-
-  const span = parentSpan.startChild({
-    op: entry.message,
-    status: getSpanStatus(context.span.status),
-    data: {
-      ...entry,
-      context
-    }
-  });
-  SPAN_MAP.set(context.span.id, span);
-};
-
 /**
  * @see https://develop.sentry.dev/sdk/event-payloads/span/#:~:text=this%20value%20explicitly.-,status,-Optional.%20Describes%20the
  */
-const getSpanStatus = (status?: TraceStatus) => {
+const getSpanStatus = (status?: 'ok' | 'error') => {
   switch (status) {
     case 'ok':
       return 'ok';
     case 'error':
       return 'not_found';
   }
+  return status ?? 'ok';
 };
--- packages/core/protocols/src/tracing.ts
+++ packages/core/protocols/src/tracing.ts
@@ -1,7 +1,76 @@
-// TODO(dmaretskyi): Trace helpers go here.
-
 //
 // Copyright 2023 DXOS.org
 //
 
-export {};
+export type Command = 'begin' | 'end' | 'update';
+
+export type TraceStatus = 'ok' | 'error';
+
+export type DataType = { [key: string]: any };
+
+export type TracingContext = {
+  span?: {
+    id: string;
+    command?: Command;
+    parent?: string;
+    status?: TraceStatus;
+    data?: DataType;
+  };
+};
+
+export type Trace = {
+  begin: (opts: { id: string; parentId: string; data?: DataType }) => TracingContext;
+  end: (opts: { id: string; status?: 'ok' | 'error'; data?: DataType }) => TracingContext;
+  update: (opts: { id: string; data: Record<string, string> }) => TracingContext;
+  error: (opts: { id: string; error: Error; data?: DataType }) => TracingContext;
+};
+
+export const trace: Trace = {
+  begin: ({ id, parentId, data }) => {
+    return {
+      span: {
+        command: 'begin',
+        id,
+        parent: parentId,
+        data
+      }
+    };
+  },
+
+  end: ({ id, status, data }) => {
+    return {
+      span: {
+        command: 'end',
+        id,
+        status,
+        data
+      }
+    };
+  },
+
+  update: ({ id, data }) => {
+    return {
+      span: {
+        command: 'update',
+        id,
+        data
+      }
+    };
+  },
+
+  error: ({ id, error, data }) => {
+    return {
+      span: {
+        command: 'end',
+        id,
+        status: 'error',
+        data: {
+          errorMessage: error.message,
+          errorName: error.name,
+          errorStack: error.stack,
+          ...data
+        }
+      }
+    };
+  }
+};
