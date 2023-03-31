489551e579aaa0ba24fdaebada6f8c35b9ff770f
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-03-31 15:07:24 +0300

    Fix timings

--- packages/core/mesh/network-manager/src/swarm/peer.ts
+++ packages/core/mesh/network-manager/src/swarm/peer.ts
@@ -296,8 +296,8 @@ export class Peer {
 
 const increaseInterval = (interval: number) => {
   if (interval === 0) {
-    return 50;
-  } else if (interval < 50) {
+    return 500;
+  } else if (interval < 500) {
     return 500;
   } else if (interval < 1000) {
     return 1000;
