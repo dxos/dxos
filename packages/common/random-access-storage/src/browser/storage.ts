8b2a699829db22ff3597049d0e553a5301c10e64
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-04-04 21:50:07 +0300

    Fix default storage

--- packages/common/random-access-storage/src/browser/storage.ts
+++ packages/common/random-access-storage/src/browser/storage.ts
@@ -7,13 +7,19 @@ import { FirefoxStorage } from './firefox-storage';
 import { IDbStorage } from './idb-storage';
 import { WebFS } from './web-fs';
 
+let INSIDE_WEBKIT: boolean;
+try {
+  if (mochaExecutor.environment === 'webkit') {
+    INSIDE_WEBKIT = true;
+  }
+} catch (e) {
+  INSIDE_WEBKIT = false;
+}
+
 export const createStorage: StorageConstructor = ({ type, root = '' } = {}): Storage => {
   if (type === undefined) {
-    if (
-      typeof navigator.storage.getDirectory === 'function' &&
-      (!mochaExecutor || mochaExecutor.environment !== 'webkit')
-    ) {
-      // WEBFS is not supported in webkit.
+    if (typeof navigator.storage.getDirectory === 'function' && !INSIDE_WEBKIT) {
+      // WEBFS is not supported in webkit test environment but it passes check for navigator.storage.getDirectory.
       return new WebFS(root);
     } else {
       return new IDbStorage(root);
