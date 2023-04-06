b245be852e85ebeefa5a23aea89d97b3c7caed63
thure <will@braneframe.com>
2023-04-06 15:49:04 -0700

    Implement backupSpace.

--- packages/apps/composer-app/src/util/backup.ts
+++ packages/apps/composer-app/src/util/backup.ts
@@ -2,6 +2,8 @@
 // Copyright 2023 DXOS.org
 //
 
+import JSZip from 'jszip';
+
 import { Space } from '@dxos/client';
 
 import { ComposerDocument } from '../proto';
@@ -22,9 +24,19 @@ export type SpaceBackup = {
   items: ComposerDocumentBackup[];
 };
 
-export const backup = async (space: Space): Promise<SpaceBackup> => {
+export const getSpaceBackup = async (space: Space): Promise<SpaceBackup> => {
   const itemsQuery = space.db.query(ComposerDocument.filter());
   const namesCount = new Map<string, number>();
+  const getFileName = (title: string) => {
+    if (namesCount.has(title)) {
+      const count = namesCount.get(title)!;
+      namesCount.set(title, count + 1);
+      return `${title} (${count})`;
+    } else {
+      namesCount.set(title, 1);
+      return title;
+    }
+  };
   return {
     origin: {
       key: space.key.toHex(),
@@ -32,8 +44,19 @@ export const backup = async (space: Space): Promise<SpaceBackup> => {
     },
     items: itemsQuery.objects.map((document) => ({
       origin: { id: document.id, title: document.title },
-      // todo(thure): de-collide similar names
-      fileName: document.title
+      fileName: `${getFileName(document.title)}.md`
     }))
   };
+};
+
+export const backupSpace = async (space: Space): Promise<Blob> => {
+  const backup = await getSpaceBackup(space);
+  const backupPackage = new JSZip();
+  backupPackage.file('backup.json', JSON.stringify(backup));
+  const items = backupPackage.folder('items');
+  backup.items.forEach((docBackup) => {
+    const document = space.db.getObjectById(docBackup.origin!.id!) as ComposerDocument;
+    items!.file(docBackup.fileName, document?.content.content?.toString() ?? '');
+  });
+  return backupPackage.generateAsync({ type: 'blob' });
 };
