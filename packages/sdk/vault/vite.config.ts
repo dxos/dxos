5ba5af6dfe76f58927211efdb390fa2647610ff9
Josiah Witt <josiah@witt.life>
2023-04-06 20:12:28 -0400

    chore: temp skip sentry upload for vault (#2964)

--- packages/sdk/vault/vite.config.ts
+++ packages/sdk/vault/vite.config.ts
@@ -60,16 +60,17 @@ export default defineConfig({
     }),
     ReactPlugin(),
     // https://docs.sentry.io/platforms/javascript/sourcemaps/uploading/vite
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          sentryVitePlugin({
-            org: 'dxos',
-            project: 'vault',
-            include: './dist/bundle',
-            authToken: process.env.SENTRY_RELEASE_AUTH_TOKEN
-          })
-        ]
-      : [])
+    // TODO(wittjosiah): Get github to recognize sentry token.
+    // ...(process.env.NODE_ENV === 'production'
+    //   ? [
+    //       sentryVitePlugin({
+    //         org: 'dxos',
+    //         project: 'vault',
+    //         include: './dist/bundle',
+    //         authToken: process.env.SENTRY_RELEASE_AUTH_TOKEN
+    //       })
+    //     ]
+    //   : [])
   ],
   worker: {
     format: 'es',
