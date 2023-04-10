c1ffd649941291c025b5c2fa2e026af208c97260
Rich Burdon <rich.burdon@gmail.com>
2023-04-09 20:37:08 -0400

    test: Kai sidebar story (#2975)

--- packages/apps/patterns/react-icons/.storybook/main.cjs
+++ packages/apps/patterns/react-icons/.storybook/main.cjs
@@ -12,8 +12,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-react-router-v6'
+    '@storybook/addon-interactions'
   ],
   framework: {
     name: '@storybook/react-vite',
