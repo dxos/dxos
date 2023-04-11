23ff576f56f6a50e512f8cd7d585531769e0ea8c
Josiah Witt <josiah@witt.life>
2023-04-11 11:51:49 -0400

    fix(client): revert default remote source (#2988)

--- packages/sdk/client/src/packlets/client/config.ts
+++ packages/sdk/client/src/packlets/client/config.ts
@@ -7,8 +7,7 @@ import { ConfigProto } from '@dxos/config';
 export const DEFAULT_CLIENT_CHANNEL = 'dxos:app';
 export const DEFAULT_SHELL_CHANNEL = 'dxos:shell';
 
-export const DEFAULT_CLIENT_ORIGIN =
-  process.env.NODE_ENV === 'production' ? 'https://halo.dxos.org/vault.html' : 'http://localhost:3967/vault.html';
+export const DEFAULT_CLIENT_ORIGIN = 'https://halo.dxos.org/vault.html';
 
 export const EXPECTED_CONFIG_VERSION = 1;
 
