75fb41e40dee01ddec1bd8f592ea651769b01b00
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2023-04-15 17:36:12 +0200

    fix: Sketch frame (#3040)

--- packages/experimental/kai/src/hooks/useAppState.tsx
+++ packages/experimental/kai/src/hooks/useAppState.tsx
@@ -15,11 +15,11 @@ export const defaultFrames = [
   'dxos.module.frame.inbox',
   'dxos.module.frame.calendar',
   'dxos.module.frame.contact',
-  'dxos.module.frame.file'
+  'dxos.module.frame.file',
   // 'dxos.module.frame.kanban',
   // 'dxos.module.frame.table',
   // 'dxos.module.frame.note',
-  // 'dxos.module.frame.sketch',
+  'dxos.module.frame.sketch'
   // 'dxos.module.frame.chess',
   // 'dxos.module.frame.sandbox',
   // 'dxos.module.frame.maps',
