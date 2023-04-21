38295991493cd608b4578414ce51043faa446cc7
Josiah Witt <josiah@witt.life>
2023-04-21 10:54:42 -0400

    chore: hide csb

--- docs/src/components/RenderReactDemo.vue
+++ docs/src/components/RenderReactDemo.vue
@@ -10,14 +10,15 @@
         <path d="M215.79,118.17a8,8,0,0,0-5-5.66L153.18,90.9l14.66-73.33a8,8,0,0,0-13.69-7l-112,120a8,8,0,0,0,3,13l57.63,21.61L88.16,238.43a8,8,0,0,0,13.69,7l112-120A8,8,0,0,0,215.79,118.17ZM109.37,214l10.47-52.38a8,8,0,0,0-5-9.06L62,132.71l84.62-90.66L136.16,94.43a8,8,0,0,0,5,9.06l52.8,19.8Z"></path>
       </svg>
     </vue-custom-tooltip>
-    <form v-if="forkable" ref="codesandbox" action="https://codesandbox.io/api/v1/sandboxes/define" method="POST" target="_blank">
+    <!-- TODO(wittjosiah): CodeSandbox forking requires too many extra steps for it to work currently. -->
+    <!-- <form v-if="forkable" ref="codesandbox" action="https://codesandbox.io/api/v1/sandboxes/define" method="POST" target="_blank">
       <input type="hidden" name="parameters" :value="parameters" />
       <vue-custom-tooltip v-if="forkable" label="Fork to CodeSandbox">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256" @click="handleCodesandbox">
           <path d="M223.68,66.15,135.68,18a15.94,15.94,0,0,0-15.36,0l-88,48.18a16,16,0,0,0-8.32,14v95.64a16,16,0,0,0,8.32,14l88,48.17a15.88,15.88,0,0,0,15.36,0l88-48.17a16,16,0,0,0,8.32-14V80.18A16,16,0,0,0,223.68,66.15ZM168,152v50.09l-32,17.52V132.74l80-43.8v32l-43.84,24A8,8,0,0,0,168,152Zm-84.16-7L40,121v-32l80,43.8v86.87L88,202.09V152A8,8,0,0,0,83.84,145Zm-.7-88.41,41,22.45a8,8,0,0,0,7.68,0l41-22.45,34.48,18.87L128,118.88,48.66,75.44ZM128,32h0l28.2,15.44L128,62.89,99.8,47.45ZM40,139.22l32,17.52v36.59L40,175.82Zm144,54.11V156.74l32-17.52v36.6Z"></path>
         </svg>
       </vue-custom-tooltip>
-    </form>
+    </form> -->
   </div>
   <div class="peers">
     <div v-for="i in peerCount" ref="peers"></div>
