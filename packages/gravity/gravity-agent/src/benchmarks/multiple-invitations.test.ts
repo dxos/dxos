7b6e5d650065935475022cff410cec0f1df71b96
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-04-21 14:38:25 +0300

    Add logs

--- packages/gravity/gravity-agent/src/benchmarks/multiple-invitations.test.ts
+++ packages/gravity/gravity-agent/src/benchmarks/multiple-invitations.test.ts
@@ -7,6 +7,7 @@ import { expect } from 'chai';
 import { asyncTimeout } from '@dxos/async';
 import { Expando, SpaceProxy } from '@dxos/client';
 import { performInvitation } from '@dxos/client-services/testing';
+import { log } from '@dxos/log';
 import { describe, test, afterTest } from '@dxos/test';
 import { Builder } from '@dxos/test-builder';
 
@@ -28,30 +29,47 @@ describe('Multiple invitations', () => {
     }
 
     const baseSpace = await peers[0].client.createSpace();
+    log.info(`created space, properties id ${baseSpace.properties.id}`);
     await baseSpace.waitUntilReady();
     const key = baseSpace.key;
 
     const data = { some: 'random', object: 'for testing' };
     const { id: expandoId } = baseSpace.db.add(new Expando(data));
+    log.info(`added expando, id ${expandoId}`);
 
     for (let index = 0; index < numberOFPeers; index++) {
       if (index === numberOFPeers - 1) {
         break;
       }
+
+      log.info(`STRESTEST Peer${index} loading space...`);
       const space = peers[index].client.getSpace(key);
       expect(space).to.exist;
-      await asyncTimeout(space!.waitUntilReady(), 2_000);
+      await asyncTimeout(space!.waitUntilReady(), 3_000);
       expect((space?.db.getObjectById(expandoId) as Expando).toJSON()).to.include(data);
+      log.info(`STRESTEST Peer${index} loaded space`);
 
-      await asyncTimeout(
+      log.info(`STRESTEST Peer${index} inviting Peer${index + 1}`);
+      const [host, guest] = await asyncTimeout(
         Promise.all(
           performInvitation({
             host: space as SpaceProxy,
             guest: peers[index + 1].client
           })
         ),
-        1_000
+        10_000
       );
+
+      if (host.error) {
+        log.error('STRESTEST Host error', host.error);
+        throw host.error;
+      }
+      if (guest.error) {
+        log.error('STRESTEST Guest error', host.error);
+        throw guest.error;
+      }
+
+      log.info('STRESTEST Invitation complete');
     }
   })
     .tag('stress')
