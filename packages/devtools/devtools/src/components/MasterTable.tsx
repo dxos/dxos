d9435ca0ef73619ccdf861b91c82c2893ff84c63
Josiah Witt <josiah@witt.life>
2023-04-21 11:38:34 -0400

    feat(devtools): logs panel (#3071)

--- packages/devtools/devtools/src/components/MasterTable.tsx
+++ packages/devtools/devtools/src/components/MasterTable.tsx
@@ -4,23 +4,24 @@
 
 import React, { useState } from 'react';
 
-import { TableColumn, Table } from '@dxos/mosaic';
+import { TableColumn, Table, TableSlots } from '@dxos/mosaic';
 
 import { JsonView } from './JsonView';
 
 export type MasterTableProps<T extends {}> = {
   columns: TableColumn<T>[];
   data: T[];
+  slots?: TableSlots;
 };
 
 // TODO(burdon): Create storybook.
-export const MasterTable = <T extends {}>({ columns, data }: MasterTableProps<T>) => {
+export const MasterTable = <T extends {}>({ columns, data, slots }: MasterTableProps<T>) => {
   const [selected, setSelected] = useState<T>();
 
   return (
     <div className='flex flex-1 overflow-hidden'>
       <div className='flex w-1/2 overflow-hidden border-r'>
-        <Table<T> columns={columns} data={data} selected={selected} onSelect={setSelected} />
+        <Table<T> columns={columns} data={data} slots={slots} selected={selected} onSelect={setSelected} />
       </div>
 
       <div className='flex w-1/2 overflow-hidden'>{selected && <JsonView data={selected} />}</div>
