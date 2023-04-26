8920ff4ac190780ed9cc5dc2488ef0374de06e6c
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-04-26 16:47:56 +0300

    Add run-tests script

--- packages/gravity/kube-testing/src/main.ts
+++ packages/gravity/kube-testing/src/main.ts
@@ -1,3 +1,23 @@
 //
 // Copyright 2023 DXOS.org
 //
+import path from 'node:path';
+
+import { SignalServerRunner } from '@dxos/signal';
+import { randomInt } from '@dxos/util';
+
+const PATH_TO_KUBE_REPO = '/Users/mykola/Documents/dev/kube/';
+
+const startSignalServer = async () => {
+  debugger;
+  const binPath = path.join(PATH_TO_KUBE_REPO, 'cmds/signal/signal-test/main.go');
+  const signalRunner = new SignalServerRunner({
+    binCommand: `go run ${binPath}`,
+    signalArguments: ['p2pserver'],
+    port: randomInt(10000, 50000)
+  });
+  await signalRunner.waitUntilStarted();
+  return signalRunner;
+};
+
+await startSignalServer();
