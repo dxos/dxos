94aabd886b65937188f196ff3814e37499f0464f
thure <will@braneframe.com>
2023-05-04 08:51:35 -0700

    Fix ref forwarding in List and Tree.

--- packages/ui/aurora/src/components/List/List.tsx
+++ packages/ui/aurora/src/components/List/List.tsx
@@ -4,7 +4,7 @@
 
 import { CaretDown, CaretRight, DotsSixVertical } from '@phosphor-icons/react';
 import { Slot } from '@radix-ui/react-slot';
-import React, { ComponentPropsWithoutRef } from 'react';
+import React, { ComponentPropsWithoutRef, forwardRef } from 'react';
 
 import { Density } from '@dxos/aurora-types';
 import {
@@ -41,35 +41,38 @@ const useListDensity = ({ density, variant }: Pick<ListProps, 'density' | 'varia
   return variant === 'ordered-draggable' ? 'coarse' : contextDensity ?? 'coarse';
 };
 
-const List = ({ className, children, ...props }: ListProps) => {
+const List = forwardRef<HTMLOListElement, ListProps>(({ className, children, ...props }, forwardedRef) => {
   const { tx } = useThemeContext();
   const density = useListDensity(props);
 
   return (
     <DensityProvider density={density}>
-      <ListPrimitive {...props} className={tx('list.root', 'list', {}, className)}>
+      <ListPrimitive {...props} className={tx('list.root', 'list', {}, className)} ref={forwardedRef}>
         {children}
       </ListPrimitive>
     </DensityProvider>
   );
-};
+});
 
 type ListItemEndcapProps = ThemedClassName<ComponentPropsWithoutRef<'div'>> & { asChild?: boolean };
 
-const ListItemEndcap = ({ children, className, asChild, ...props }: ListItemEndcapProps) => {
-  const Root = asChild ? Slot : 'div';
-  const density = useDensityContext();
-  const { tx } = useThemeContext();
-  return (
-    <Root
-      {...(!asChild && { role: 'none' })}
-      {...props}
-      className={tx('list.item.endcap', 'list__listItem__endcap', { density }, className)}
-    >
-      {children}
-    </Root>
-  );
-};
+const ListItemEndcap = forwardRef<HTMLDivElement, ListItemEndcapProps>(
+  ({ children, className, asChild, ...props }, forwardedRef) => {
+    const Root = asChild ? Slot : 'div';
+    const density = useDensityContext();
+    const { tx } = useThemeContext();
+    return (
+      <Root
+        {...(!asChild && { role: 'none' })}
+        {...props}
+        className={tx('list.item.endcap', 'list__listItem__endcap', { density }, className)}
+        ref={forwardedRef}
+      >
+        {children}
+      </Root>
+    );
+  }
+);
 
 const MockListItemOpenTrigger = ({
   className,
@@ -98,72 +101,85 @@ const MockListItemDragHandle = ({
 
 type ListItemHeadingProps = ThemedClassName<ListPrimitiveItemHeadingProps>;
 
-const ListItemHeading = ({ children, className, ...props }: ListItemHeadingProps) => {
-  const { tx } = useThemeContext();
-  const density = useDensityContext();
-  return (
-    <ListPrimitiveItemHeading
-      {...props}
-      className={tx('list.item.heading', 'list__listItem__heading', { density }, className)}
-    >
-      {children}
-    </ListPrimitiveItemHeading>
-  );
-};
+const ListItemHeading = forwardRef<HTMLParagraphElement, ListItemHeadingProps>(
+  ({ children, className, ...props }, forwardedRef) => {
+    const { tx } = useThemeContext();
+    const density = useDensityContext();
+    return (
+      <ListPrimitiveItemHeading
+        {...props}
+        className={tx('list.item.heading', 'list__listItem__heading', { density }, className)}
+        ref={forwardedRef}
+      >
+        {children}
+      </ListPrimitiveItemHeading>
+    );
+  }
+);
 
 type ListItemDragHandleProps = ThemedClassName<ListPrimitiveItemDragHandleProps>;
 
-const ListItemDragHandle = ({ className, children, ...props }: ListItemDragHandleProps) => {
-  const { tx } = useThemeContext();
-  return (
-    <ListPrimitiveItemDragHandle
-      {...props}
-      className={tx('list.item.dragHandle', 'list__listItem__dragHandle', {}, className)}
-    >
-      {children || (
-        <DotsSixVertical className={tx('list.item.dragHandleIcon', 'list__listItem__dragHandle__icon', {})} />
-      )}
-    </ListPrimitiveItemDragHandle>
-  );
-};
+const ListItemDragHandle = forwardRef<HTMLDivElement, ListItemDragHandleProps>(
+  ({ className, children, ...props }, forwardedRef) => {
+    const { tx } = useThemeContext();
+    return (
+      <ListPrimitiveItemDragHandle
+        {...props}
+        className={tx('list.item.dragHandle', 'list__listItem__dragHandle', {}, className)}
+        ref={forwardedRef}
+      >
+        {children || (
+          <DotsSixVertical className={tx('list.item.dragHandleIcon', 'list__listItem__dragHandle__icon', {})} />
+        )}
+      </ListPrimitiveItemDragHandle>
+    );
+  }
+);
 
 type ListItemOpenTriggerProps = ThemedClassName<ListPrimitiveItemOpenTriggerProps>;
 
-const ListItemOpenTrigger = ({ __listItemScope, className, children, ...props }: ListItemOpenTriggerProps) => {
-  const { tx } = useThemeContext();
-  const density = useDensityContext();
-  const { toggleOpenLabel } = useListContext(LIST_NAME, __listItemScope);
-  const { open } = useListItemContext(LIST_ITEM_NAME, __listItemScope);
-  const Icon = open ? CaretDown : CaretRight;
-  return (
-    <ListPrimitiveItemOpenTrigger
-      {...props}
-      className={tx('list.item.openTrigger', 'list__listItem__openTrigger', { density }, className)}
-    >
-      {typeof toggleOpenLabel === 'string' ? <span className='sr-only'>{toggleOpenLabel}</span> : toggleOpenLabel}
-      {children || (
-        <Icon
-          {...{
-            weight: 'bold',
-            className: tx('list.item.openTriggerIcon', 'list__listItem__openTrigger__icon', {})
-          }}
-        />
-      )}
-    </ListPrimitiveItemOpenTrigger>
-  );
-};
+const ListItemOpenTrigger = forwardRef<HTMLButtonElement, ListItemOpenTriggerProps>(
+  ({ __listItemScope, className, children, ...props }, forwardedRef) => {
+    const { tx } = useThemeContext();
+    const density = useDensityContext();
+    const { toggleOpenLabel } = useListContext(LIST_NAME, __listItemScope);
+    const { open } = useListItemContext(LIST_ITEM_NAME, __listItemScope);
+    const Icon = open ? CaretDown : CaretRight;
+    return (
+      <ListPrimitiveItemOpenTrigger
+        {...props}
+        className={tx('list.item.openTrigger', 'list__listItem__openTrigger', { density }, className)}
+        ref={forwardedRef}
+      >
+        {typeof toggleOpenLabel === 'string' ? <span className='sr-only'>{toggleOpenLabel}</span> : toggleOpenLabel}
+        {children || (
+          <Icon
+            {...{
+              weight: 'bold',
+              className: tx('list.item.openTriggerIcon', 'list__listItem__openTrigger__icon', {})
+            }}
+          />
+        )}
+      </ListPrimitiveItemOpenTrigger>
+    );
+  }
+);
 
 type ListItemProps = ThemedClassName<ListPrimitiveItemProps>;
 
-const ListItem = ({ className, children, ...props }: ListItemProps) => {
+const ListItem = forwardRef<HTMLLIElement, ListItemProps>(({ className, children, ...props }, forwardedRef) => {
   const { tx } = useThemeContext();
   const density = useDensityContext();
   return (
-    <ListPrimitiveItem {...props} className={tx('list.item.root', 'list__listItem', { density }, className)}>
+    <ListPrimitiveItem
+      {...props}
+      className={tx('list.item.root', 'list__listItem', { density }, className)}
+      ref={forwardedRef}
+    >
       {children}
     </ListPrimitiveItem>
   );
-};
+});
 
 export {
   List,
--- packages/ui/react-appkit/src/components/Tree/Tree.tsx
+++ packages/ui/react-appkit/src/components/Tree/Tree.tsx
@@ -2,7 +2,7 @@
 // Copyright 2023 DXOS.org
 //
 
-import React, { forwardRef, ForwardRefExoticComponent } from 'react';
+import React, { forwardRef } from 'react';
 
 import {
   List,
@@ -24,24 +24,20 @@ type TreeProps = ListProps;
 
 type TreeItemProps = ListItemProps;
 
-const TreeRoot = (props: TreeProps) => {
-  return <List {...props} />;
-};
+const TreeRoot = forwardRef<HTMLOListElement, TreeProps>((props, forwardedRef) => {
+  return <List {...props} ref={forwardedRef} />;
+});
 
 type TreeBranchProps = ListScopedProps<TreeProps>;
 
-// todo(thure): Ideally this should not have to be explicitly typed, blocked by https://github.com/microsoft/TypeScript/issues/47663
-const TreeBranch: ForwardRefExoticComponent<TreeBranchProps> = forwardRef<HTMLOListElement, TreeBranchProps>(
-  ({ __listScope, ...props }: TreeBranchProps) => {
-    const { headingId } = useListItemContext(LIST_ITEM_NAME, __listScope);
+const TreeBranch = forwardRef<HTMLOListElement, TreeBranchProps>(({ __listScope, ...props }, forwardedRef) => {
+  const { headingId } = useListItemContext(LIST_ITEM_NAME, __listScope);
+  return <List {...props} aria-labelledby={headingId} ref={forwardedRef} />;
+});
 
-    return <List {...props} aria-labelledby={headingId} />;
-  }
-);
-
-const TreeItem = (props: ListItemProps) => {
-  return <ListItem role='treeitem' {...props} />;
-};
+const TreeItem = forwardRef<HTMLLIElement, ListItemProps>((props, forwardedRef) => {
+  return <ListItem role='treeitem' {...props} ref={forwardedRef} />;
+});
 
 type TreeItemHeadingProps = ListItemHeadingProps;
 
