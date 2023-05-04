9de4afbdd2c633d9207547814b780cdb59e1baf9
thure <will@braneframe.com>
2023-05-04 14:03:44 -0700

    Fix nonsense.

--- packages/ui/aurora-theme/src/styles/components/index.ts
+++ packages/ui/aurora-theme/src/styles/components/index.ts
@@ -6,3 +6,4 @@ export * from './button';
 export * from './input';
 export * from './dropdown-menu';
 export * from './list';
+export * from './main';
--- packages/ui/aurora/src/components/index.ts
+++ packages/ui/aurora/src/components/index.ts
@@ -5,6 +5,7 @@
 export * from './Button';
 export * from './Input';
 export * from './List';
+export * from './Main';
 
 export * from './DensityProvider';
 export * from './ElevationProvider';
