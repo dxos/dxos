2aa3e1ea1201700f5a399a7b90d8d0875d572583
Will Shown <thure@users.noreply.github.com>
2023-05-10 16:13:38 -0700

    fix(aurora): Facilitate better slotting for `Avatar` (#3160)

--- packages/ui/aurora/src/components/Avatar/Avatar.tsx
+++ packages/ui/aurora/src/components/Avatar/Avatar.tsx
@@ -6,15 +6,14 @@ import {
   Root as AvatarRootPrimitive,
   AvatarProps as AvatarRootPrimitiveProps,
   Image as AvatarImagePrimitive,
-  AvatarImageProps as AvatarImagePrimitiveProps,
-  Fallback as AvatarFallbackPrimitive,
-  AvatarFallbackProps as AvatarFallbackPrimitiveProps
+  ImageLoadingStatus,
+  Fallback as AvatarFallbackPrimitive
 } from '@radix-ui/react-avatar';
 import { createContext } from '@radix-ui/react-context';
 import { Primitive } from '@radix-ui/react-primitive';
 import { Slot } from '@radix-ui/react-slot';
 import { toSvg } from 'jdenticon';
-import React, { ComponentPropsWithRef, forwardRef, useMemo } from 'react';
+import React, { ComponentPropsWithRef, forwardRef, PropsWithChildren, useMemo } from 'react';
 
 import { Size } from '@dxos/aurora-types';
 import { useId } from '@dxos/react-hooks';
@@ -22,29 +21,93 @@ import { useId } from '@dxos/react-hooks';
 import { useThemeContext } from '../../hooks';
 
 type AvatarVariant = 'square' | 'circle';
+type AvatarStatus = 'active' | 'inactive';
 
-type AvatarRootProps = AvatarRootPrimitiveProps & { labelId?: string; size?: Size; variant?: AvatarVariant };
+type AvatarRootProps = PropsWithChildren<Partial<AvatarContextValue>>;
 
-type AvatarContextValue = { labelId: string; maskId: string; size: Size; variant: AvatarVariant };
+type AvatarContextValue = {
+  labelId: string;
+  descriptionId: string;
+  maskId: string;
+  size: Size;
+  variant: AvatarVariant;
+  status?: AvatarStatus;
+};
 const AVATAR_NAME = 'Avatar';
 const [AvatarProvider, useAvatarContext] = createContext<AvatarContextValue>(AVATAR_NAME);
 
-const AvatarRoot = forwardRef<HTMLSpanElement, AvatarRootProps>(
-  ({ size = 10, variant = 'circle', className, labelId: propsLabelId, ...props }, forwardedRef) => {
-    const labelId = useId('avatar__label', propsLabelId);
-    const maskId = useId('mask');
-    const { tx } = useThemeContext();
-    return (
-      <AvatarProvider {...{ labelId, maskId, size, variant }}>
-        <AvatarRootPrimitive
-          {...props}
-          className={tx('avatar.root', 'avatar', { size, variant }, className)}
-          ref={forwardedRef}
+const AvatarRoot = ({
+  size = 10,
+  variant = 'circle',
+  status,
+  children,
+  labelId: propsLabelId,
+  descriptionId: propsDescriptionId
+}: AvatarRootProps) => {
+  const labelId = useId('avatar__label', propsLabelId);
+  const descriptionId = useId('avatar__description', propsDescriptionId);
+  const maskId = useId('mask');
+  return <AvatarProvider {...{ labelId, descriptionId, maskId, size, variant, status }}>{children}</AvatarProvider>;
+};
+
+type AvatarProps = AvatarRootPrimitiveProps;
+
+const Avatar = forwardRef<HTMLSpanElement, AvatarProps>(({ children, className, ...props }, forwardedRef) => {
+  const { labelId, descriptionId, maskId, size, variant, status } = useAvatarContext('AvatarStatus');
+  const { tx } = useThemeContext();
+  const imageSizeNumber = size === 'px' ? 1 : size * 4;
+  const statusIconSize = size > 9 ? 4 : size < 6 ? 2 : 3;
+  const maskSize = statusIconSize * 4 + 2;
+  const maskCenter = imageSizeNumber - (statusIconSize * 4) / 2;
+  return (
+    <AvatarRootPrimitive
+      role='img'
+      {...props}
+      className={tx('avatar.root', 'avatar', { size, variant }, className)}
+      ref={forwardedRef}
+      aria-labelledby={labelId}
+      aria-describedby={descriptionId}
+    >
+      <svg
+        viewBox={`0 0 ${imageSizeNumber} ${imageSizeNumber}`}
+        width={imageSizeNumber}
+        height={imageSizeNumber}
+        className={tx('avatar.frame', 'avatar__status', {}, className)}
+      >
+        <defs>
+          <mask id={maskId}>
+            {variant === 'circle' ? (
+              <circle fill='white' cx='50%' cy='50%' r='50%' />
+            ) : (
+              <rect fill='white' width='100%' height='100%' />
+            )}
+            {status && (
+              <circle
+                fill='black'
+                cx={`${(100 * maskCenter) / imageSizeNumber}%`}
+                cy={`${(100 * maskCenter) / imageSizeNumber}%`}
+                r={`${(50 * maskSize) / imageSizeNumber}%`}
+              />
+            )}
+          </mask>
+        </defs>
+        {children}
+      </svg>
+      {status === 'inactive' ? (
+        <Moon
+          mirrored
+          weight='fill'
+          className={tx('avatar.statusIcon', 'avatar__status__icon', { size: statusIconSize, status })}
         />
-      </AvatarProvider>
-    );
-  }
-);
+      ) : (
+        <Circle
+          weight='fill'
+          className={tx('avatar.statusIcon', 'avatar__status__icon', { size: statusIconSize, status })}
+        />
+      )}
+    </AvatarRootPrimitive>
+  );
+});
 
 type AvatarLabelProps = Omit<ComponentPropsWithRef<typeof Primitive.span>, 'id'> & {
   asChild?: boolean;
@@ -67,73 +130,23 @@ const AvatarLabel = forwardRef<HTMLSpanElement, AvatarLabelProps>(
   }
 );
 
-type AvatarImageProps = AvatarImagePrimitiveProps;
+type AvatarDescriptionProps = Omit<ComponentPropsWithRef<typeof Primitive.span>, 'id'> & {
+  asChild?: boolean;
+  srOnly?: boolean;
+};
 
-const AvatarImage = forwardRef<HTMLImageElement, AvatarImageProps>((props, forwardedRef) => {
-  const { labelId } = useAvatarContext('AvatarImage');
-  return <AvatarImagePrimitive role='img' {...props} aria-labelledby={labelId} ref={forwardedRef} />;
-});
-
-type AvatarFallbackProps = AvatarFallbackPrimitiveProps;
-
-const AvatarFallback = forwardRef<HTMLSpanElement, AvatarFallbackProps>((props, forwardedRef) => {
-  const { labelId } = useAvatarContext('AvatarFallback');
-  return <AvatarFallbackPrimitive role='img' {...props} aria-labelledby={labelId} ref={forwardedRef} />;
-});
-
-type AvatarStatusProps = ComponentPropsWithRef<'svg'> & { status?: 'active' | 'inactive' };
-
-const AvatarStatus = forwardRef<SVGSVGElement, AvatarStatusProps>(
-  ({ children, className, status, ...props }, forwardedRef) => {
-    const { labelId, maskId, size, variant } = useAvatarContext('AvatarStatus');
+const AvatarDescription = forwardRef<HTMLSpanElement, AvatarDescriptionProps>(
+  ({ asChild, srOnly, className, ...props }, forwardedRef) => {
+    const Root = asChild ? Slot : Primitive.span;
     const { tx } = useThemeContext();
-    const imageSizeNumber = size === 'px' ? 1 : size * 4;
-    const statusIconSize = size > 9 ? 4 : size < 6 ? 2 : 3;
-    const maskSize = statusIconSize * 4 + 2;
-    const maskCenter = imageSizeNumber - (statusIconSize * 4) / 2;
+    const { descriptionId } = useAvatarContext('AvatarDescription');
     return (
-      <>
-        <svg
-          role='img'
-          {...props}
-          viewBox={`0 0 ${imageSizeNumber} ${imageSizeNumber}`}
-          width={imageSizeNumber}
-          height={imageSizeNumber}
-          aria-labelledby={labelId}
-          className={tx('avatar.status', 'avatar__status', {}, className)}
-        >
-          <defs>
-            <mask id={maskId}>
-              {variant === 'circle' ? (
-                <circle fill='white' cx='50%' cy='50%' r='50%' />
-              ) : (
-                <rect fill='white' width='100%' height='100%' />
-              )}
-              {status && (
-                <circle
-                  fill='black'
-                  cx={`${(100 * maskCenter) / imageSizeNumber}%`}
-                  cy={`${(100 * maskCenter) / imageSizeNumber}%`}
-                  r={`${(50 * maskSize) / imageSizeNumber}%`}
-                />
-              )}
-            </mask>
-          </defs>
-          {children}
-        </svg>
-        {status === 'inactive' ? (
-          <Moon
-            mirrored
-            weight='fill'
-            className={tx('avatar.statusIcon', 'avatar__status__icon', { size: statusIconSize, status })}
-          />
-        ) : (
-          <Circle
-            weight='fill'
-            className={tx('avatar.statusIcon', 'avatar__status__icon', { size: statusIconSize, status })}
-          />
-        )}
-      </>
+      <Root
+        {...props}
+        id={descriptionId}
+        ref={forwardedRef}
+        className={tx('avatar.description', 'avatar__description', { srOnly }, className)}
+      />
     );
   }
 );
@@ -145,6 +158,30 @@ const AvatarMaskedImage = forwardRef<SVGImageElement, AvatarMaskedImageProps>((p
   return <image width='100%' height='100%' {...props} mask={`url(#${maskId})`} ref={forwardedRef} />;
 });
 
+type AvatarImageProps = ComponentPropsWithRef<'image'> & {
+  onLoadingStatusChange?: (status: ImageLoadingStatus) => void;
+};
+
+const AvatarImage = forwardRef<SVGImageElement, AvatarImageProps>((props, forwardedRef) => {
+  return (
+    <AvatarImagePrimitive asChild>
+      <AvatarMaskedImage {...props} ref={forwardedRef} />
+    </AvatarImagePrimitive>
+  );
+});
+
+type AvatarFallbackProps = ComponentPropsWithRef<'image'> & {
+  delayMs?: number;
+};
+
+const AvatarFallback = forwardRef<SVGImageElement, AvatarFallbackProps>((props, forwardedRef) => {
+  return (
+    <AvatarFallbackPrimitive asChild>
+      <AvatarMaskedImage {...props} ref={forwardedRef} />
+    </AvatarFallbackPrimitive>
+  );
+});
+
 const useJdenticonHref = (value: string, size: Size) => {
   return useMemo(
     () => `data:image/svg+xml;utf8,${encodeURIComponent(toSvg(value, size === 'px' ? 1 : size * 4, { padding: 0 }))}`,
@@ -154,20 +191,22 @@ const useJdenticonHref = (value: string, size: Size) => {
 
 export {
   AvatarRoot,
+  Avatar,
   AvatarImage,
   AvatarFallback,
   AvatarLabel,
-  AvatarStatus,
-  AvatarMaskedImage,
+  AvatarDescription,
   useJdenticonHref,
   useAvatarContext
 };
 
 export type {
+  AvatarStatus,
+  AvatarVariant,
   AvatarRootProps,
+  AvatarProps,
   AvatarImageProps,
   AvatarFallbackProps,
   AvatarLabelProps,
-  AvatarStatusProps,
-  AvatarMaskedImageProps
+  AvatarDescriptionProps
 };
