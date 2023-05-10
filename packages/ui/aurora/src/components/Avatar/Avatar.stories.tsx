f2b7ac5f485cdbc5c8fa253e30687ebfbdfadbd8
Will Shown <thure@users.noreply.github.com>
2023-05-10 16:13:38 -0700

    fix(aurora): Facilitate better slotting for `Avatar` (#3160)

--- packages/ui/aurora/src/components/Avatar/Avatar.stories.tsx
+++ packages/ui/aurora/src/components/Avatar/Avatar.stories.tsx
@@ -8,12 +8,12 @@ import React from 'react';
 import { Size } from '@dxos/aurora-types';
 
 import {
+  Avatar,
+  AvatarDescription,
   AvatarFallback,
   AvatarImage,
   AvatarLabel,
-  AvatarMaskedImage,
   AvatarRoot,
-  AvatarStatus,
   useJdenticonHref
 } from './Avatar';
 
@@ -21,6 +21,7 @@ type StorybookAvatarProps = {
   imgSrc?: string;
   fallbackValue: string;
   label: string;
+  description: string;
   status?: 'active' | 'inactive';
   variant?: 'circle' | 'square';
   size?: Size;
@@ -31,21 +32,19 @@ const StorybookAvatar = ({
   size = 12,
   variant = 'circle',
   label,
+  description,
   fallbackValue,
   imgSrc
 }: StorybookAvatarProps) => {
   const jdenticon = useJdenticonHref(fallbackValue ?? '', size);
   return (
-    <AvatarRoot {...{ size, variant }}>
-      <AvatarLabel>{label}</AvatarLabel>
-      <AvatarStatus status={status}>
-        <AvatarImage asChild>
-          <AvatarMaskedImage href={imgSrc} />
-        </AvatarImage>
-        <AvatarFallback asChild>
-          <AvatarMaskedImage href={jdenticon} />
-        </AvatarFallback>
-      </AvatarStatus>
+    <AvatarRoot {...{ size, variant, status }}>
+      <Avatar>
+        <AvatarImage href={imgSrc} />
+        <AvatarFallback href={jdenticon} />
+      </Avatar>
+      <AvatarLabel className='block'>{label}</AvatarLabel>
+      <AvatarDescription className='block'>{description}</AvatarDescription>
     </AvatarRoot>
   );
 };
@@ -57,7 +56,8 @@ export default {
 export const Default = {
   args: {
     fallbackValue: '20970b563fc49b5bb194a6ffdff376031a3a11f9481360c071c3fed87874106b',
-    label: 'Display name',
+    label: 'Username',
+    description: 'Status',
     status: 'active',
     variant: 'circle',
     size: 12
