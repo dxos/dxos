ec94f07fee43eeb9126b947a04a222f9474ac451
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-05-11 16:25:31 +0300

    fix(kube-testing): Fix empty array stat

--- packages/gravity/kube-testing/src/main.ts
+++ packages/gravity/kube-testing/src/main.ts
@@ -2,31 +2,35 @@
 // Copyright 2023 DXOS.org
 //
 
+import { PublicKey } from '@dxos/keys';
+
 import { runPlan } from './plan/run-plan';
 import { SignalTestPlan } from './plan/signal-spec';
 
 void runPlan({
   plan: new SignalTestPlan(),
   spec: {
-    servers: 1,
-    agents: 30,
-    serversPerAgent: 1,
+    servers: 3,
+    agents: 20,
+    serversPerAgent: 2,
     signalArguments: [
-      // 'p2pserver'
-      'globalsubserver'
+      'p2pserver'
+      // 'globalsubserver'
     ],
-    topicCount: 1,
-    topicsPerAgent: 1,
+    topicCount: 5,
+    topicsPerAgent: 3,
     startWaitTime: 1_000,
     discoverTimeout: 5_000,
     repeatInterval: 200,
     agentWaitTime: 5_000,
-    duration: 10_000,
+    duration: 30_000,
     type: 'discovery'
     // serverOverride: 'ws://localhost:1337/.well-known/dx/signal'
   },
   options: {
-    staggerAgents: 5
-    // repeatAnalysis: '/Users/dmaretskyi/Projects/protocols/packages/gravity/kube-testing/out/results/2023-05-11T10:49:46-0cba/test.json'
+    staggerAgents: 5,
+    randomSeed: PublicKey.random().toHex()
+    // repeatAnalysis:
+    // '/Users/mykola/Documents/dev/dxos/packages/gravity/kube-testing/out/results/2023-05-11T12:29:06-c896/test.json'
   }
 });
