64dcf0ac0f92d5065468fe9fbd491f58acd46e83
Josiah Witt <josiah@witt.life>
2023-05-15 08:13:57 -0400

    feat(composer-extension): embed mode (#3171)
    Co-authored-by: Will Shown <thure@users.noreply.github.com>

---
+++ packages/apps/composer-app/src/components/ResolverDialog/ResolverContext.tsx
@@ -0,0 +1,133 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import React, { Context, createContext, PropsWithChildren, useContext, useEffect, useMemo, useState } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
+
+import { Document } from '@braneframe/types';
+import { log } from '@dxos/log';
+import { Space, useIdentity, useQuery, useSpaces, Text } from '@dxos/react-client';
+import { ShellProvider } from '@dxos/react-shell';
+
+import { matchSpace } from '../../util';
+import { DocumentResolverProps, SpaceResolverProps } from './ResolverProps';
+
+const useLocationIdentifier = () => {
+  const { '*': location } = useParams();
+  let source, id;
+  try {
+    const url = location ? new URL(location) : undefined;
+    source = url?.hostname.split('.').reverse().join('.');
+    id = url?.pathname.slice(1);
+  } catch (err) {
+    log.catch(err);
+  }
+  return [source, id];
+};
+
+export const SpaceResolverContext: Context<SpaceResolverProps> = createContext<SpaceResolverProps>({
+  space: null,
+  setSpace: (_space) => {}
+});
+
+export const SpaceResolverProvider = ({ children }: PropsWithChildren<{}>) => {
+  const [searchParams] = useSearchParams();
+  const spaceInvitationCode = searchParams.get('spaceInvitationCode');
+  const haloInvitationCode = searchParams.get('haloInvitationCode');
+  const identity = useIdentity({ login: true });
+  const identityHex = identity?.identityKey.toHex();
+  const [source, id] = useLocationIdentifier();
+  const spaces = useSpaces({ all: true });
+
+  const [nextSpace, setSpace] = useState<Space | null>(null);
+
+  const space = useMemo(
+    () =>
+      nextSpace ?? (identityHex ? spaces.find((space) => matchSpace(space, identityHex, source, id)) ?? null : null),
+    [nextSpace, identityHex, source, id]
+  );
+
+  return (
+    <ShellProvider
+      space={space || undefined}
+      spaceInvitationCode={spaceInvitationCode}
+      haloInvitationCode={haloInvitationCode}
+      onJoinedSpace={(nextSpaceKey) => {
+        console.warn('TODO: onJoinedSpace', nextSpaceKey);
+      }}
+    >
+      <SpaceResolverContext.Provider value={{ space, setSpace, source, id, identityHex }}>
+        {children}
+      </SpaceResolverContext.Provider>
+    </ShellProvider>
+  );
+};
+
+const defaultDocumentResolverContext: DocumentResolverProps = {
+  document: null,
+  setDocument: (_document) => {}
+};
+
+export const DocumentResolverContext: Context<DocumentResolverProps> =
+  createContext<DocumentResolverProps>(defaultDocumentResolverContext);
+
+const DocumentsQueryableDocumentResolverProvider = ({
+  space,
+  source,
+  id,
+  children
+}: PropsWithChildren<{ space: Space; source: string; id: string }>) => {
+  const [document, setDocument] = useState<Document | null>(null);
+
+  const documents = useQuery(space, (obj) => {
+    const keys = obj.meta?.keys;
+    return keys?.find((key: any) => key.source === source && key.id === id);
+  });
+
+  useEffect(() => {
+    const handler = (event: MessageEvent) => {
+      if (event.source !== window.parent) {
+        return;
+      }
+
+      if (event.data.type === 'initial-data') {
+        const nextDocument = new Document({
+          meta: {
+            keys: [{ source, id }]
+          },
+          content: new Text(event.data.content)
+        });
+        space.db.add(nextDocument);
+        setDocument(nextDocument);
+      }
+    };
+
+    // todo(thure): Won't this always be the case initially?
+    if (documents.length === 0) {
+      window.addEventListener('message', handler);
+      window.parent.postMessage({ type: 'request-initial-data' }, 'https://github.com');
+      return () => window.removeEventListener('message', handler);
+    } else {
+      setDocument(documents[0] as Document);
+    }
+  }, [space, documents]);
+
+  return (
+    <DocumentResolverContext.Provider value={{ document, setDocument }}>{children}</DocumentResolverContext.Provider>
+  );
+};
+
+export const DocumentResolverProvider = ({ children }: PropsWithChildren<{}>) => {
+  const { space, source, id } = useContext(SpaceResolverContext);
+
+  return space && source && id ? (
+    <DocumentsQueryableDocumentResolverProvider space={space} source={source} id={id}>
+      {children}
+    </DocumentsQueryableDocumentResolverProvider>
+  ) : (
+    <DocumentResolverContext.Provider value={defaultDocumentResolverContext}>
+      {children}
+    </DocumentResolverContext.Provider>
+  );
+};
