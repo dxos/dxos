223a36a4d1291fc76efbe51d7631a47850b374c0
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-05-19 14:46:05 +0300

    Fix test

--- packages/devtools/cli/src/commands/app/app.test.ts
+++ packages/devtools/cli/src/commands/app/app.test.ts
@@ -8,11 +8,18 @@ import path from 'path';
 
 import { describe, test } from '@dxos/test';
 
+import { runCommand } from '../../util';
+
 describe('App', () => {
   const tmpFolder = path.join(__dirname, '../../../tmp/dx');
 
+  afterEach(() => {
+    fs.rmSync(tmpFolder, { recursive: true, force: true });
+  });
+
   test('create', async () => {
     const appName = 'test-app';
+    await runCommand(`app create ${appName}`, tmpFolder);
     expect(fs.existsSync(path.join(tmpFolder, appName, 'dx.yml'))).to.be.true;
   });
 });
