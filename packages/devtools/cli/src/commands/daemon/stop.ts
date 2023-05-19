a88e2b79ef7b2cdc75bb7eb766d52aa659619b46
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-05-19 17:14:08 +0300

    Add stop command

---
+++ packages/devtools/cli/src/commands/daemon/stop.ts
@@ -0,0 +1,42 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { Args } from '@oclif/core';
+import { Proc } from 'pm2';
+
+import { log } from '@dxos/log';
+
+import { BaseCommand } from '../../base-command';
+import { getPm2 } from '../../daemon/pm2';
+
+export default class Stop extends BaseCommand {
+  static override enableJsonFlag = true;
+  static override description = 'Stop daemon process.';
+
+  static override args = {
+    ...BaseCommand.args,
+    name: Args.string({
+      name: 'Process name.',
+      default: 'dev',
+    }),
+  };
+
+  async run(): Promise<any> {
+    const pm2 = await getPm2();
+    const params = await this.parse(Stop);
+
+    await new Promise<Proc>((resolve, reject) => {
+      pm2.stop(params.args.name, (err, proc) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(proc!);
+        }
+      });
+    });
+
+    this.log('Stopped');
+    pm2.disconnect();
+  }
+}
