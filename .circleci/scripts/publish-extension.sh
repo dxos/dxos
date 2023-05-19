cba048e419a948c0b5a3f11ce9fc92353a41f7e7
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-05-19 12:30:42 +0300

    chore: Use REFRESH_TOKEN in extension publish script (#3210)

--- .circleci/scripts/publish-extension.sh
+++ .circleci/scripts/publish-extension.sh
@@ -6,27 +6,25 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 ROOT=$(git rev-parse --show-toplevel)
 
 # See https://developer.chrome.com/docs/webstore/using_webstore_api/
+# See how to obtaine ACCESS_TOKEN from REFRESH_TOKEN https://circleci.com/blog/continuously-deploy-a-chrome-extension/
 if [ $BRANCH = "production" ]; then
   pushd $EXTENSION
 
   PACKAGE=${PWD##*/}
-  PACKAGE_CAPS=${PACKAGE^^}
+  PACKAGE_CAPS='DEVTOOLS-EXTENSION'
   PACKAGE_ENV=${PACKAGE_CAPS//-/_}
 
   eval "CLIENT_ID=$"${PACKAGE_ENV}_CLIENT_ID""
   eval "CLIENT_SECRET=$"${PACKAGE_ENV}_CLIENT_SECRET""
-  eval "AUTH_CODE=$"${PACKAGE_ENV}_AUTH_CODE""
+  eval "REFRESH_TOKEN=$"${PACKAGE_ENV}_REFRESH_TOKEN""
   eval "APP_ID=$"${PACKAGE_ENV}_APP_ID""
 
   ZIP_PATH=${ROOT}/artifacts/${PACKAGE}.zip
   mkdir ${ROOT}/artifacts
   zip -r $ZIP_PATH ./out/${PACKAGE}
 
-  # NOTE: you can curl ACCESS_TOKEN only once per hour with AUTH_CODE. 
-  #   That ACCESS_TOKEN will be valid for an hour, and in ideal world you need to store it, because you can not curl it again for this hour.
-  #   In our use case, it is ok to be able to run this script only once per hour and to not store ACCESS_TOKEN anywhere, because extension publish takes more than a day.
   echo ---------------------------------------------------
-  ACCESS_TOKEN=$(curl "https://accounts.google.com/o/oauth2/token" -d "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$AUTH_CODE&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
+  ACCESS_TOKEN=$(curl "https://accounts.google.com/o/oauth2/token" -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
   echo "${PACKAGE_ENV}: Obtained ACCESS_TOKEN: ${ACCESS_TOKEN}"
 
   echo ---------------------------------------------------
