4c5ece8d4c4a8b3f52788059b0ff75f95adc1b7a
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-05-23 17:01:18 +0300

    feat: Run CLI commands with daemon client-services (#3219)

---
+++ packages/devtools/cli/src/commands/daemon/stop.ts
@@ -0,0 +1,29 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { Args } from '@oclif/core';
+
+import { BaseCommand } from '../../base-command';
+
+export default class Stop extends BaseCommand {
+  static override enableJsonFlag = true;
+  static override description = 'Stop daemon process.';
+
+  static override args = {
+    ...BaseCommand.args,
+    name: Args.string({
+      name: 'Process name.',
+      env: 'DX_PROFILE',
+    }),
+  };
+
+  async run(): Promise<any> {
+    return await this.execWithDaemon(async (daemon) => {
+      const params = await this.parse(Stop);
+      await daemon.stop(params.args.name);
+
+      this.log('Stopped');
+    });
+  }
+}
