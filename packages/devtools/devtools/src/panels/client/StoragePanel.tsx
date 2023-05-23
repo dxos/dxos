11fdc778706e527d7ec36e32bb175bf2c41d2006
Dmytro Maretskyi <maretskii@gmail.com>
2023-05-23 12:37:59 +0200

    show feeds

--- packages/devtools/devtools/src/panels/client/StoragePanel.tsx
+++ packages/devtools/devtools/src/panels/client/StoragePanel.tsx
@@ -2,20 +2,18 @@
 // Copyright 2021 DXOS.org
 //
 
-import { GitCommit, HardDrive, LinkBreak, LinkSimple, LinkSimpleBreak, ShareNetwork } from '@phosphor-icons/react';
-import React, { useEffect, useState } from 'react';
+import { GitCommit, HardDrive, LinkSimple, ShareNetwork } from '@phosphor-icons/react';
+import React, { useState } from 'react';
 
-import { ConnectionInfo, SwarmInfo } from '@dxos/protocols/proto/dxos/devtools/swarm';
 import { TreeView, TreeViewItem } from '@dxos/react-appkit';
 import { useDevtools, useStream } from '@dxos/react-client';
-import { humanize } from '@dxos/util';
 
-import { ConnectionInfoView } from '../../components/ConnectionInfoView';
-import { StorageInfo } from '@dxos/protocols/proto/dxos/devtools/host';
+import { StorageInfo, SubscribeToFeedsResponse } from '@dxos/protocols/proto/dxos/devtools/host';
 import { useAsyncEffect } from '@dxos/react-async';
 import bytes from 'bytes';
+import { humanize } from '@dxos/util';
 
-const getInfoTree = (storageInfo: StorageInfo): TreeViewItem[] => [
+const getInfoTree = (storageInfo: StorageInfo, feedInfo: SubscribeToFeedsResponse): TreeViewItem[] => [
   {
     id: 'origin',
     Icon: GitCommit,
@@ -35,6 +33,28 @@ const getInfoTree = (storageInfo: StorageInfo): TreeViewItem[] => [
             <span className='text-gray-400'>{bytes.format(storageInfo.storageUsage)}</span>
           </div>   
         ),
+        items: [
+          {
+            id: 'feeds',
+            Icon: ShareNetwork,
+            Element: (
+              <div className='flex gap-2 overflow-hidden whitespace-nowrap'>
+                <span>feeds</span>
+                <span className='text-gray-400'>{feedInfo.feeds?.length ?? 0}</span>
+              </div>   
+            ),
+            items: feedInfo.feeds?.map(feed => ({
+              id: feed.feedKey.toHex(),
+              Icon: LinkSimple,
+              Element: (
+                <div className='flex gap-2 overflow-hidden whitespace-nowrap'>
+                  <span>{humanize(feed.feedKey)}</span>
+                  <span className='text-gray-400'>{bytes.format(feed.diskUsage)}</span>
+                </div>
+              ),
+            }))
+          }
+        ]
       }
     ]
   },
@@ -43,6 +63,7 @@ const getInfoTree = (storageInfo: StorageInfo): TreeViewItem[] => [
 const StoragePanel = () => {
   const devtoolsHost = useDevtools();
   const [storageInfo, setStorageInfo] = useState<StorageInfo | undefined>();
+  const feeds = useStream(() => devtoolsHost.subscribeToFeeds({}), {}, []);
 
   useAsyncEffect(async () => {
     setStorageInfo(await devtoolsHost.getStorageInfo());
@@ -55,7 +76,7 @@ const StoragePanel = () => {
   return (
     <div className='flex h-full overflow-hidden'>
       <TreeView
-        items={getInfoTree(storageInfo)}
+        items={getInfoTree(storageInfo, feeds)}
         expanded={['origin', 'storage']}
         slots={{
           value: {
