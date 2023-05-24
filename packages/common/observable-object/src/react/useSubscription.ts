15f8522ed5d9bdb218439a6a5ec27d5dc88832a5
Josiah Witt <josiah@witt.life>
2023-05-24 15:05:34 -0400

    feat: observable object (#3228)

--- packages/common/observable-object/src/react/useSubscription.ts
+++ packages/common/observable-object/src/react/useSubscription.ts
@@ -4,18 +4,13 @@
 
 import { useEffect, useRef } from 'react';
 
-import { Selection, SubscriptionHandle } from '@dxos/echo-schema';
-
-import { useClient } from '../client';
+import { Selection, SubscriptionHandle, createSubscription } from '../access-observer';
 
 /**
  * Create reactive selection.
- * Calls the callback when any object from the selection changes.
- * Also calls the callback when the selection changes and during the first render.
+ * Calls the callback when the selection changes and during the first render.
  */
 export const useSubscription = (cb: () => void, selection: Selection) => {
-  const client = useClient();
-
   // Make sure that the callback is always the one from the latest render.
   // Without this, we would always call the callback from the initial render,
   // which might contain stale data.
@@ -25,7 +20,7 @@ export const useSubscription = (cb: () => void, selection: Selection) => {
   const subscriptionRef = useRef<SubscriptionHandle>();
 
   useEffect(() => {
-    subscriptionRef.current = client.dbRouter.createSubscription(() => {
+    subscriptionRef.current = createSubscription(() => {
       callbackRef.current();
     });
 
