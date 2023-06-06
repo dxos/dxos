c416b78d74d29c4bd3eb57de9645bfd2f8d64193
thure <will@braneframe.com>
2023-06-06 16:21:56 -0700

    Create stub stack plugin package.

---
+++ packages/apps/composer-plugins/stack/src/components/Stack.tsx
@@ -0,0 +1,7 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import React from 'react';
+
+export const Stack = () => <span>Stack surface</span>;
---
+++ packages/apps/composer-plugins/stack/src/stories/StackSurface.stories.tsx
@@ -0,0 +1,19 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import React from 'react';
+
+import { Stack } from '../components';
+
+const StackSurface = () => {
+  return <Stack />;
+};
+
+export default {
+  component: StackSurface,
+};
+
+export const Default = {
+  args: {},
+};
