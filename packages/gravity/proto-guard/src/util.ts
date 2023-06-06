5ba0d13264a30e5131f3d4b2dfd960ae29673daf
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-06-06 17:04:11 +0300

    feat: Add storage version to metadata (#3344)

--- packages/gravity/proto-guard/src/util.ts
+++ packages/gravity/proto-guard/src/util.ts
@@ -8,8 +8,6 @@ import pkgUp from 'pkg-up';
 import { Config } from '@dxos/client';
 import { raise } from '@dxos/debug';
 
-const STORAGE_VERSION_FILENAME = '.storage-version';
-
 export const getPackageDir = () =>
   path.dirname(pkgUp.sync({ cwd: __dirname }) ?? raise(new Error('No package.json found')));
 
@@ -20,18 +18,12 @@ export const getStorageDir = () => {
   return storageDir;
 };
 
-export const getStorageVersion = () => {
-  const filePath = path.join(getPackageDir(), STORAGE_VERSION_FILENAME);
-  return Number(fs.readFileSync(filePath).toString());
-};
-
-/**
- * Bumps storage version in .storage-version file
- */
-export const bumpStorageVersion = () => {
-  const filePath = path.join(getPackageDir(), STORAGE_VERSION_FILENAME);
-  const version = Number(fs.readFileSync(filePath).toString());
-  fs.writeFileSync(filePath, `${(version + 1).toString()}\n`);
+export const getLatestStorage = () => {
+  const versions = fs
+    .readdirSync(getStorageDir())
+    .map((version) => Number(version))
+    .filter((version) => !Number.isNaN(version));
+  return Math.max(...versions, 1);
 };
 
 export const getConfig = (path: string) =>
