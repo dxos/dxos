1aae885df6b883bf96cffcbbeb355f46c4b463d5
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-06-06 13:35:27 +0300

    Add STORAGE_VERSION const

---
+++ packages/core/protocols/src/storage.ts
@@ -0,0 +1,10 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+/**
+ * Version for the schema of the stored data as defined in dxos.echo.metadata.EchoMetadata.
+ *
+ * Should be incremented every time there's a breaking change to the stored data.
+ */
+export const STORAGE_VERSION = 2;
