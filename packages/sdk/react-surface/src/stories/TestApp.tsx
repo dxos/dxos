6b672204fd27f059231f2cbfd0968c33da858705
thure <will@braneframe.com>
2023-06-13 01:56:40 -0700

    Fix lint.

--- packages/sdk/react-surface/src/stories/TestApp.tsx
+++ packages/sdk/react-surface/src/stories/TestApp.tsx
@@ -5,26 +5,12 @@
 import React from 'react';
 
 import { PluginContextProvider } from '../framework';
-import {
-  ClientPlugin,
-  GraphPlugin,
-  RoutesPlugin,
-  SplitViewPlugin,
-  ThemePlugin,
-  TreeViewPlugin,
-} from '../plugins';
+import { ClientPlugin, GraphPlugin, RoutesPlugin, SplitViewPlugin, ThemePlugin, TreeViewPlugin } from '../plugins';
 
 export const TestApp = () => {
   return (
     <PluginContextProvider
-      plugins={[
-        RoutesPlugin,
-        ThemePlugin,
-        ClientPlugin,
-        GraphPlugin,
-        TreeViewPlugin,
-        SplitViewPlugin,
-      ]}
+      plugins={[RoutesPlugin, ThemePlugin, ClientPlugin, GraphPlugin, TreeViewPlugin, SplitViewPlugin]}
     />
   );
 };
