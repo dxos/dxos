7a97eab1455934e660fc88b0661bfda613b49e5e
thure <will@braneframe.com>
2023-06-12 18:34:55 -0700

    Overwrite production Composer with extensible Composer. Move (most) Composer-specific plugins out of surface plugins. Rename surface to `react-surface`.

---
+++ packages/sdk/react-surface/src/plugins/RoutesPlugin.tsx
@@ -0,0 +1,53 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import React from 'react';
+import { Outlet, RouteObject, useRoutes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
+import { observer } from '@dxos/observable-object/react';
+
+import { definePlugin, Plugin, usePluginContext } from '../framework';
+
+export type RouterPluginProvides = {
+  router: {
+    routes?: () => RouteObject[];
+    useNavigator?: () => void;
+  };
+};
+
+export type RouterPlugin = Plugin<RouterPluginProvides>;
+
+const routerPlugins = (plugins: Plugin[]): RouterPlugin[] => {
+  return (plugins as RouterPlugin[]).filter((p) => p.provides?.router);
+};
+
+const Root = observer(({ plugins }: { plugins: RouterPlugin[] }) => {
+  plugins.map((plugin) => plugin.provides.router.useNavigator?.());
+  return <Outlet />;
+});
+
+export const RoutesPlugin = definePlugin({
+  meta: {
+    id: 'RoutesPlugin',
+  },
+  provides: {
+    // TODO(wittjosiah): Should be BrowserRouter.
+    context: ({ children }) => <BrowserRouter>{children}</BrowserRouter>,
+    components: {
+      default: () => {
+        const pluginContext = usePluginContext();
+        const plugins = routerPlugins(pluginContext.plugins);
+        const routes = plugins.map((plugin) => plugin.provides.router.routes?.() ?? []).flat();
+        return useRoutes([
+          {
+            path: '/',
+            element: <Root plugins={plugins} />,
+            children: routes,
+          },
+        ]);
+      },
+    },
+  },
+});
