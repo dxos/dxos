00f172f7dc9c4ccd368dc84debee7363575bfb4f
Will Shown <thure@users.noreply.github.com>
2023-06-13 18:51:50 -0700

    feat(composer-extension): Space management increment (#3466)

--- packages/apps/composer-app/src/plugins/GithubMarkdownPlugin/components/GithubEchoResolverProviders/ResolverDialog.tsx
+++ packages/apps/composer-app/src/plugins/GithubMarkdownPlugin/components/GithubEchoResolverProviders/ResolverDialog.tsx
@@ -2,20 +2,16 @@
 // Copyright 2023 DXOS.org
 //
 
-import React, { useContext } from 'react';
+import React from 'react';
 
-import { Button, DensityProvider, useTranslation } from '@dxos/aurora';
-import { osTx } from '@dxos/aurora-theme';
+import { Button, useTranslation } from '@dxos/aurora';
 import { ShellLayout } from '@dxos/client';
-import { Loading } from '@dxos/react-appkit';
 import { useClient } from '@dxos/react-client';
 import { useShell } from '@dxos/react-shell';
 
-import { SpaceResolverContext } from './ResolverContext';
 import { ResolverTree } from './ResolverTree';
 
 export const ResolverDialog = () => {
-  const { space } = useContext(SpaceResolverContext);
   const { t } = useTranslation('composer');
   const client = useClient();
   const shell = useShell();
@@ -29,34 +25,16 @@ export const ResolverDialog = () => {
   };
 
   return (
-    <DensityProvider density='fine'>
-      <div role='none' className={osTx('dialog.overlay', 'dialog--resolver__overlay', {}, 'static bs-full')}>
-        <div
-          role='none'
-          className={osTx(
-            'dialog.content',
-            'dialog--resolver__content',
-            {},
-            'p-2 bs-72 flex flex-col shadow-none bg-transparent',
-          )}
-        >
-          {!space ? (
-            <>
-              <ResolverTree />
-              <div role='group' className='shrink-0 flex is-full gap-2'>
-                <Button classNames='grow' onClick={handleCreateSpace}>
-                  {t('create space label', { ns: 'appkit' })}
-                </Button>
-                <Button classNames='grow' onClick={handleJoinSpace}>
-                  {t('join space label', { ns: 'appkit' })}
-                </Button>
-              </div>
-            </>
-          ) : (
-            <Loading label={t('resolver init document message')} />
-          )}
-        </div>
+    <>
+      <ResolverTree />
+      <div role='group' className='shrink-0 flex is-full gap-2'>
+        <Button classNames='grow' onClick={handleCreateSpace}>
+          {t('create space label', { ns: 'appkit' })}
+        </Button>
+        <Button classNames='grow' onClick={handleJoinSpace}>
+          {t('join space label', { ns: 'appkit' })}
+        </Button>
       </div>
-    </DensityProvider>
+    </>
   );
 };
