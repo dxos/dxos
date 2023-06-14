4c9e8f821d1fafce43250e5d7fab714f58485591
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-06-14 12:32:07 +0300

    Lint fix

--- packages/core/agent/src/echo-proxy.js
+++ packages/core/agent/src/echo-proxy.js
@@ -3,12 +3,12 @@
 // Copyright 2023 DXOS.org
 //
 Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = require('express');
+const express1 = require('express');
 // TODO(burdon): Express ECHO http server (with Client to daemon/agent).
 // TODO(burdon): Factor out daemon from CLI.
 // TODO(burdon): GET Query/POST upsert.
 const echoProxy = () => {
-  const app = (0, express_1.default)();
+  const app = (0, express1.default)();
   app.listen(3000, () => {
     console.log('running...');
   });
