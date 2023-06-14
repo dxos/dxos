8c9ffd2310b666d932419f7d151dd12184a22cb9
Will Shown <thure@users.noreply.github.com>
2023-06-13 23:18:37 -0700

    feat(aurora): `Link` (#3467)

---
+++ packages/ui/aurora/src/components/Link/Link.stories.tsx
@@ -0,0 +1,13 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import '@dxosTheme';
+
+import { Link } from './Link';
+
+export default {
+  component: Link,
+} as any;
+
+export const Default = { args: { children: 'Hello', href: '#' } };
