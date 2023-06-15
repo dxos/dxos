1cc25b185bac55c2c24c3df5a5ebe592942dc0cf
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-06-15 15:23:23 +0300

    test(cli-monitor): Add `package.json` log

--- tools/monitors/cli-monitor/test/cli.test.ts
+++ tools/monitors/cli-monitor/test/cli.test.ts
@@ -4,10 +4,19 @@
 
 import { expect } from 'chai';
 import { exec } from 'node:child_process';
+import { readFileSync } from 'node:fs';
 import { promisify } from 'node:util';
+import pkgUp from 'pkg-up';
+
+const throwError = (err: Error) => {
+  throw err;
+};
 
 describe('CLI', () => {
   it('prints config', async () => {
+    const packagePath = pkgUp.sync({ cwd: __dirname }) ?? throwError(new Error('package.json not found'));
+    console.log('package.json', readFileSync(packagePath, { encoding: 'utf-8' }));
+
     const { stdout } = await promisify(exec)('npm exec dx config', { cwd: __dirname });
     expect(stdout).to.contain('"version": 1');
   }).timeout(5000);
