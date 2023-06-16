c2d08931f085e0aefbb9df4a98a0ccf2a1c24f66
thure <will@braneframe.com>
2023-06-16 13:08:04 -0700

    Prevent unnecessary rerenders by removing FunctionComponent literal in `components` reducer. Fix Composer build.

--- packages/apps/composer-app/src/plugins/MarkdownPlugin/MarkdownPlugin.tsx
+++ packages/apps/composer-app/src/plugins/MarkdownPlugin/MarkdownPlugin.tsx
@@ -2,15 +2,13 @@
 // Copyright 2023 DXOS.org
 //
 
-import React from 'react';
-
 import { ComposerModel } from '@dxos/aurora-composer';
 import { TypedObject } from '@dxos/echo-schema';
 import { ObservableObject } from '@dxos/observable-object';
 import { PluginDefinition, definePlugin } from '@dxos/react-surface';
 import { YText } from '@dxos/text-model';
 
-import { MarkdownMain, MarkdownProperties } from './components';
+import { MarkdownProperties, MarkdownMainStandalone } from './components';
 
 export const isMarkdown = (datum: unknown): datum is ComposerModel =>
   datum && typeof datum === 'object'
@@ -34,7 +32,7 @@ export const MarkdownPlugin: PluginDefinition = definePlugin({
       switch (role) {
         case 'main':
           if (Array.isArray(datum) && isMarkdown(datum[0]) && isMarkdownProperties(datum[1])) {
-            return () => <MarkdownMain model={datum[0]} properties={datum[1]} layout='standalone' />;
+            return MarkdownMainStandalone;
           }
           break;
       }
