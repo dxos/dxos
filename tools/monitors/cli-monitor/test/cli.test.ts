59811757c34643ad4ab967d2d2dbfc55ac26084b
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-06-20 13:03:33 +0300

    Revert CLI monitor

--- tools/monitors/cli-monitor/test/cli.test.ts
+++ tools/monitors/cli-monitor/test/cli.test.ts
@@ -4,28 +4,10 @@
 
 import { expect } from 'chai';
 import { exec } from 'node:child_process';
-import { readFileSync } from 'node:fs';
-import { dirname, join } from 'node:path';
 import { promisify } from 'node:util';
-import pkgUp from 'pkg-up';
-
-const throwError = (err: Error) => {
-  throw err;
-};
 
 describe('CLI', () => {
   it('prints config', async () => {
-    const packagePath = pkgUp.sync({ cwd: __dirname }) ?? throwError(new Error('package.json not found'));
-    console.log('package.json devDeps', JSON.parse(readFileSync(packagePath, { encoding: 'utf-8' })).devDependencies);
-    console.log(
-      'cli version',
-      JSON.parse(
-        readFileSync(join(dirname(packagePath), 'node_modules', '@dxos/cli', 'package.json'), { encoding: 'utf-8' }),
-      ).version,
-    );
-
-    console.log('HOME', typeof process !== 'undefined' ? process?.env?.HOME : 'tmp');
-
     const { stdout, stderr } = await promisify(exec)('npm exec dx config', { cwd: __dirname });
     console.log('stderr', stderr);
     expect(stdout).to.contain('"version": 1');
