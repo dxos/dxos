79e85301f9f2163e9aedabea6bdae57c1103ea2b
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-06-21 16:16:13 +0300

    Add lockKey

--- packages/core/agent/src/agent.ts
+++ packages/core/agent/src/agent.ts
@@ -63,7 +63,7 @@ export class Agent {
   async start() {
     // Create client services.
     // TODO(burdon): Check lock.
-    this._services = fromHost({ config: this._config });
+    this._services = fromHost({ config: this._config, lockKey: this._options.profile });
     await this._services.open();
 
     // Create client.
