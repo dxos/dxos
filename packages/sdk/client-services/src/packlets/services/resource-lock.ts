f472ca5a1bf265cd8b1d00c492b80fc3fc9560c0
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-06-21 12:54:42 +0300

    Add ResourceLock interface

---
+++ packages/sdk/client-services/src/packlets/services/resource-lock.ts
@@ -0,0 +1,21 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { MaybePromise } from '@dxos/util';
+
+export type ResourceLockOptions = {
+  lockKey: string;
+  onAcquire?: () => MaybePromise<void>;
+  onRelease?: () => MaybePromise<void>;
+};
+
+/**
+ * Resource lock. Used to coordinate access to shared resources.
+ * For example, start only one `ServicesHost` at a time.
+ */
+export interface ResourceLock {
+  acquire(): Promise<void>;
+
+  release(): Promise<void>;
+}
