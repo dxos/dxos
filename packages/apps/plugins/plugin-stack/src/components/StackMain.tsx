d1c066fe68e5ebe57fc8a808dbb6d9ee34f96c5e
Will Shown <thure@users.noreply.github.com>
2023-06-22 16:32:21 -0700

    feat(plugin-stack): Add anywhere, remove, drag and drop (#3532)

--- packages/apps/plugins/plugin-stack/src/components/StackMain.tsx
+++ packages/apps/plugins/plugin-stack/src/components/StackMain.tsx
@@ -2,53 +2,137 @@
 // Copyright 2023 DXOS.org
 //
 
-import React, { Fragment, useEffect, useState } from 'react';
+import { CaretRight, Minus, Plus } from '@phosphor-icons/react';
+import get from 'lodash.get';
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { Main, Input, List, Button, useTranslation, randomString } from '@dxos/aurora';
-import { defaultBlockSeparator, mx } from '@dxos/aurora-theme';
+import {
+  Main,
+  Input,
+  List,
+  ListItem,
+  Button,
+  useTranslation,
+  randomString,
+  DensityProvider,
+  DragEndEvent,
+} from '@dxos/aurora';
+import { defaultBlockSeparator, getSize, mx, surfaceElevation } from '@dxos/aurora-theme';
 import { subscribe } from '@dxos/observable-object';
 import { Surface } from '@dxos/react-surface';
 
 import { StackModel, StackProperties, StackSectionModel, StackSections } from '../props';
+import { arrayMoveInPlace } from '../util';
+
+type StackSectionProps = {
+  onAdd: () => void;
+  onRemove: () => void;
+  section: StackSectionModel;
+};
+
+const AddSection = ({ onClick }: { onClick: StackSectionProps['onAdd'] }) => {
+  const { t } = useTranslation('dxos:stack');
+  return (
+    <Button variant='ghost' onClick={onClick} classNames='plb-0 pli-0.5 -mlb-1'>
+      <span className='sr-only'>{t('add section label')}</span>
+      <Plus className={getSize(4)} />
+      <CaretRight className={getSize(3)} />
+    </Button>
+  );
+};
+
+const StackSection = ({ onAdd, onRemove, section }: StackSectionProps) => {
+  const { t } = useTranslation('dxos:stack');
+  return (
+    <DensityProvider density='fine'>
+      <AddSection onClick={onAdd} />
+      <ListItem.Root id={section.object.id} classNames='flex gap-2 items-start justify-start'>
+        <ListItem.Heading classNames='sr-only'>
+          {get(section, 'object.title', t('generic section heading'))}
+        </ListItem.Heading>
+        <div role='none' className='p-1 -m-1 self-stretch flex flex-col'>
+          <Button variant='ghost' classNames='p-0' onClick={onRemove}>
+            <span className='sr-only'>{t('remove section label')}</span>
+            <Minus className={getSize(4)} />
+          </Button>
+          <ListItem.DragHandle classNames='grow' />
+        </div>
+        <div
+          role='none'
+          className={mx(surfaceElevation({ elevation: 'group' }), 'bg-white dark:bg-neutral-925 grow rounded')}
+        >
+          <Surface role='section' data={section} />
+        </div>
+      </ListItem.Root>
+    </DensityProvider>
+  );
+};
 
 // todo(thure): `observer` causes infinite rerenders if used here.
 const StackMainImpl = ({ sections }: { sections: StackSections }) => {
-  const { t } = useTranslation('dxos:stack');
   const [_, setIter] = useState([]);
+
   useEffect(() => {
     // todo(thure): TypeScript seems to get the wrong return value from `ObservableArray.subscribe`
     return sections[subscribe](() => setIter([])) as () => void;
   }, []);
+
+  const handleAdd = useCallback(
+    (start: number) => {
+      const section: StackSectionModel = {
+        source: { resolver: 'dxos:markdown', guid: randomString() },
+        object: {
+          id: randomString(),
+          content: '',
+        },
+      };
+      sections.splice(start, 0, section);
+    },
+    [sections],
+  );
+
+  const handleRemove = useCallback(
+    (start: number) => {
+      sections.splice(start, 1);
+    },
+    [sections],
+  );
+
+  const handleDragEnd = useCallback((event: DragEndEvent) => {
+    const { active, over } = event;
+    if (active.id !== over?.id) {
+      const oldIndex = sections.findIndex((section) => section.object.id === active.id);
+      const newIndex = sections.findIndex((section) => section.object.id === over?.id);
+      arrayMoveInPlace<StackSectionModel>(sections, oldIndex, newIndex);
+    }
+  }, []);
+
   return (
-    <article>
-      {sections
-        // todo(thure): This filter should be unnecessary; why is the first (or only?) value sometimes some sort of array-like object?
-        .filter((section) => 'source' in section)
-        .map((section, o) => {
-          return (
-            <Fragment key={section.source.guid}>
-              <Surface role='section' data={section} />
-            </Fragment>
-          );
-        })}
-      <div role='none' className='p-4'>
-        <Button
-          onClick={() => {
-            const section: StackSectionModel = {
-              source: { resolver: 'dxos:markdown', guid: randomString() },
-              object: {
-                id: randomString(),
-                content: '',
-                title: '',
-              },
-            };
-            sections.splice(sections.length, 0, section);
-          }}
-        >
-          {t('add section label')}
-        </Button>
+    <>
+      <List
+        variant='ordered-draggable'
+        onDragEnd={handleDragEnd}
+        listItemIds={sections.map(({ object: { id } }) => id)}
+        classNames='pis-1 pie-2'
+      >
+        {sections
+          // todo(thure): This filter should be unnecessary; why is the first (or only?) value sometimes some sort of array-like object?
+          .filter((section) => 'source' in section)
+          .map((section, start) => {
+            return (
+              <StackSection
+                key={section.object.id}
+                onAdd={() => handleAdd(start)}
+                onRemove={() => handleRemove(start)}
+                section={section}
+              />
+            );
+          })}
+      </List>
+      <div role='none' className='pis-1 pie-2'>
+        <AddSection onClick={() => handleAdd(sections.length)} />
       </div>
-    </article>
+    </>
   );
 };
 
@@ -59,7 +143,7 @@ export const StackMain = ({
 }) => {
   const { t } = useTranslation('dxos:stack');
   return (
-    <Main.Content classNames='min-bs-[100vh] mli-auto max-is-[60rem] bg-white dark:bg-neutral-925'>
+    <Main.Content classNames='min-bs-[100vh] mli-auto max-is-[60rem]'>
       <Input.Root>
         <Input.Label srOnly>{t('stack title label')}</Input.Label>
         <Input.TextInput
@@ -69,10 +153,8 @@ export const StackMain = ({
           onChange={({ target: { value } }) => (properties.title = value)}
         />
       </Input.Root>
-      <div role='separator' className={mx(defaultBlockSeparator, 'mli-3 opacity-50')} />
-      <List>
-        <StackMainImpl sections={stack.sections} />
-      </List>
+      <div role='separator' className={mx(defaultBlockSeparator, 'mli-3 mbe-2 opacity-50')} />
+      <StackMainImpl sections={stack.sections} />
     </Main.Content>
   );
 };
