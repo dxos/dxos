39c98822deb59bed2b2fd32c78db06553473553f
Will Shown <thure@users.noreply.github.com>
2023-06-22 16:32:21 -0700

    feat(plugin-stack): Add anywhere, remove, drag and drop (#3532)

--- packages/apps/plugins/plugin-stack/src/props.ts
+++ packages/apps/plugins/plugin-stack/src/props.ts
@@ -4,23 +4,27 @@
 
 import { subscribe, ObservableArray } from '@dxos/observable-object';
 
-export type StackSections = ObservableArray<StackSectionModel>;
+export type StackSections = ObservableArray<StackSectionModel<any>>;
 
-export type StackSectionModel = {
+export type StackObject = { id: string };
+
+export type GenericStackObject = StackObject & { [key: string]: any };
+
+export type StackSectionModel<T extends StackObject = GenericStackObject> = {
   source: { resolver: string; guid: string };
-  object: unknown;
+  object: T;
 };
 
-export type StackModel = {
+export type StackModel<T extends StackObject = GenericStackObject> = {
   id: string;
-  sections: ObservableArray<StackSectionModel>;
+  sections: ObservableArray<StackSectionModel<T>>;
 };
 
 export type StackProperties = {
   title?: string;
 };
 
-export const isStack = (datum: unknown): datum is StackModel =>
+export const isStack = <T extends StackObject = GenericStackObject>(datum: unknown): datum is StackModel<T> =>
   datum && typeof datum === 'object'
     ? 'id' in datum &&
       typeof datum.id === 'string' &&
