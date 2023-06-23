9f02c02c97ea7212a2715f7616649a000fb2ccc6
Josiah Witt <josiah@witt.life>
2023-06-22 20:11:36 -0400

    feat(composer-app): local files (#3484)
    Co-authored-by: thure <will@braneframe.com>

--- packages/apps/plugins/plugin-markdown/src/MarkdownPlugin.tsx
+++ packages/apps/plugins/plugin-markdown/src/MarkdownPlugin.tsx
@@ -2,17 +2,73 @@
 // Copyright 2023 DXOS.org
 //
 
-import { PluginDefinition, definePlugin } from '@dxos/react-surface';
+import React from 'react';
 
-import { MarkdownMainStandalone, MarkdownMainEmbedded } from './components';
+import { ComposerModel, MarkdownComposerProps } from '@dxos/aurora-composer';
+import { createStore } from '@dxos/observable-object';
+import { observer } from '@dxos/observable-object/react';
+import { definePlugin, Plugin, PluginDefinition } from '@dxos/react-surface';
+
+import { MarkdownMain, StandaloneLayout } from './components';
 import { MarkdownSection } from './components/MarkdownSection';
-import { isMarkdown, isMarkdownProperties } from './props';
+import { MarkdownProperties, isMarkdown, isMarkdownPlaceholder, isMarkdownProperties } from './props';
 import translations from './translations';
+
+const store = createStore<{ onChange: NonNullable<MarkdownComposerProps['onChange']>[] }>({ onChange: [] });
+
+const MarkdownMainStandalone = observer(
+  ({ data: [model, properties] }: { data: [ComposerModel, MarkdownProperties]; role?: string }) => {
+    return (
+      <MarkdownMain
+        model={model}
+        properties={properties}
+        layout='standalone'
+        onChange={(text) => store.onChange.forEach((onChange) => onChange(text))}
+      />
+    );
+  },
+);
+
+const MarkdownMainEmbedded = ({
+  data: [model, properties, _],
+}: {
+  data: [ComposerModel, MarkdownProperties, 'embedded'];
+  role?: string;
+}) => {
+  return <MarkdownMain model={model} properties={properties} layout='embedded' />;
+};
+
+const MarkdownMainEmpty = ({ data: [model, properties] }: { data: [any, MarkdownProperties] }) => {
+  return (
+    <StandaloneLayout model={model} properties={properties}>
+      <model.content />
+    </StandaloneLayout>
+  );
+};
+
+export type MarkdownProvides = {
+  markdown: {
+    onChange: MarkdownComposerProps['onChange'];
+  };
+};
+
+type MarkdownPlugin = Plugin<MarkdownProvides>;
+
+export const markdownPlugins = (plugins: Plugin[]): MarkdownPlugin[] => {
+  return (plugins as MarkdownPlugin[]).filter((p) => Boolean(p.provides?.markdown));
+};
 
 // TODO(wittjosiah): This explicit type should not be necessary, should be inferred from `definePlugin`.
 export const MarkdownPlugin: PluginDefinition = definePlugin({
   meta: {
     id: 'dxos:markdown',
+  },
+  ready: async (plugins) => {
+    markdownPlugins(plugins).forEach((plugin) => {
+      if (plugin.provides.markdown.onChange) {
+        store.onChange.push(plugin.provides.markdown.onChange);
+      }
+    });
   },
   provides: {
     translations,
@@ -26,6 +82,8 @@ export const MarkdownPlugin: PluginDefinition = definePlugin({
             } else {
               return MarkdownMainStandalone;
             }
+          } else if (Array.isArray(datum) && isMarkdownPlaceholder(datum[0]) && isMarkdownProperties(datum[1])) {
+            return MarkdownMainEmpty;
           }
           break;
         case 'section':
