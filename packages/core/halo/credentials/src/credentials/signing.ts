bd5f7d149a99914fb3d2636e1397b90b2952b3ff
Dmytro Maretskyi <maretskii@gmail.com>
2023-06-26 11:12:41 +0200

    Stable signing

--- packages/core/halo/credentials/src/credentials/signing.ts
+++ packages/core/halo/credentials/src/credentials/signing.ts
@@ -6,6 +6,7 @@ import stableStringify from 'json-stable-stringify';
 
 import { PublicKey } from '@dxos/keys';
 import { Credential } from '@dxos/protocols/proto/dxos/halo/credentials';
+import { Timeframe } from '@dxos/timeframe';
 
 /**
  * @returns The input message to be signed for a given credential.
@@ -36,10 +37,13 @@ export const canonicalStringify = (obj: any) =>
      * We also skip @type.
      */
     // TODO(dmaretskyi): Should we actually skip the @type field?
-    replacer: (key: any, value: any) => {
+    replacer: function(this: any, key: any, value: any) {
       if (key.toString().startsWith('__') || key.toString() === '@type') {
         return undefined;
       }
+
+      // Value before .toJSON() is called.
+      const original = this[key];
 
       if (value) {
         if (PublicKey.isPublicKey(value)) {
@@ -51,8 +55,55 @@ export const canonicalStringify = (obj: any) =>
         if (value instanceof Uint8Array || (value.data && value.type === 'Buffer')) {
           return Buffer.from(value).toString('hex');
         }
+        if (original instanceof Timeframe) {
+          // Uses old key truncation method (339d...9d66) to keep backwards compatibility.
+          return original.frames().reduce((frames: Record<string, number>, [key, seq]) => {
+            frames[truncateKey(key)] = seq;
+            return frames;
+          }, {})
+        }
       }
 
       return value;
     },
   });
+
+/**
+ * Old key truncation method (339d...9d66) to keep backwards compatibility with credentials signed with old method
+ */
+const truncateKey = (key: PublicKey) => {
+  const str = key.toHex()
+  return `${str.substring(0, 4)}...${str.substring(str.length - 4)}`;
+}
+
+
+/**
+ * export const truncateKey = (key: any, { length = 8, start }: TruncateKeyOptions = {}) => {
+const str = String(key);
+if (str.length <= length) {
+  return str;
+}
+
+return start
+  ? `${str.slice(0, length)}...`
+  : `${str.substring(0, length / 2)}...${str.substring(str.length - length / 2)}`;
+};
+
+{
+"04009285": 20,
+"0415004f": 0,
+"0415e6d7": 9964,
+"042a4fa9": 8,
+"0448e62f": 3,
+"04775053": 257,
+"04a6b603": 97,
+"04bc5c9d": 198,
+"04da9930": 59,
+"04df0449": 676,
+"04e122ae": 5435,
+"04ee588b": 1703
+}
+
+
+ */
+
