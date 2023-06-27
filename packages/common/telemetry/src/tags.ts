e2d3f80f581a7fa0627949f0df436a4e5c556bbe
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-06-27 14:57:36 +0300

    Fix return type

--- packages/common/telemetry/src/tags.ts
+++ packages/common/telemetry/src/tags.ts
@@ -5,12 +5,12 @@
 import { ConfigProto } from '@dxos/config';
 import { log } from '@dxos/log';
 
-export const getLocalTelemetryTags = async () => {
+export const getLocalTelemetryTags = async (): Promise<string[]> => {
   const localKubeConfigUrl = 'http://kube.local/.well-known/dx/config';
 
   log('fetching config...', { localKubeConfigUrl });
   return await fetch(localKubeConfigUrl)
-    .then((res) => (res.json() as Promise<ConfigProto>).then((config) => config?.runtime?.kube?.telemetry?.tags))
+    .then((res) => (res.json() as Promise<ConfigProto>).then((config) => config?.runtime?.kube?.telemetry?.tags ?? []))
     .catch((error) => {
       log('Failed to fetch telemetry tags', error);
       return [];
