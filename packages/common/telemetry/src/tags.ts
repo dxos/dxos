d71fd8c624b53a845d8e1ecb5c9a842e17f26bbf
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-06-27 18:02:52 +0300

    feat(telemetry): Add ability to fetch tags from local kube (#3574)

---
+++ packages/common/telemetry/src/tags.ts
@@ -0,0 +1,22 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { ConfigProto } from '@dxos/config';
+import { log } from '@dxos/log';
+
+/**
+ * Fetches the local kube config and returns the telemetry tags.
+ * Intention is to use this to tag telemetry events as `internal`.
+ */
+export const getLocalTelemetryTags = async (): Promise<string[]> => {
+  const localKubeConfigUrl = 'http://kube.local/.well-known/dx/config';
+
+  log('fetching config...', { localKubeConfigUrl });
+  return await fetch(localKubeConfigUrl)
+    .then((res) => (res.json() as Promise<ConfigProto>).then((config) => config?.runtime?.kube?.telemetry?.tags ?? []))
+    .catch((error) => {
+      log('Failed to fetch telemetry tags', error);
+      return [];
+    });
+};
