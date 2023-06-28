3d3047f5431b62ac469c533f3e94305b6c61e2f7
Dmytro Maretskyi <maretskii@gmail.com>
2023-06-28 16:49:33 +0200

    ADD FILENAME TO TABLE

--- packages/devtools/devtools/src/panels/client/LoggingPanel.tsx
+++ packages/devtools/devtools/src/panels/client/LoggingPanel.tsx
@@ -24,6 +24,11 @@ const columns: TableColumn<LogEntry>[] = [
     accessor: (entry) => Object.entries(levels).find(([, level]) => level === entry.level)?.[0]
   },
   {
+    Header: 'File',
+    width: 80,
+    accessor: entry => `${entry.meta?.file}:${entry.meta?.line}`
+  },
+  {
     Header: 'Message',
     width: 200,
     accessor: 'message'
