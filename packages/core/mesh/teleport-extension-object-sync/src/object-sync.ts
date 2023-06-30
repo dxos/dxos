0e88376ab0fa7695be8274a9b9114012522d036f
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-06-30 14:30:48 +0300

    feat(echo-pipeline): Cancel Epoch download if there is new Epoch detected (#3589)

--- packages/core/mesh/teleport-extension-object-sync/src/object-sync.ts
+++ packages/core/mesh/teleport-extension-object-sync/src/object-sync.ts
@@ -3,10 +3,9 @@
 //
 
 import { scheduleTask, trackLeaks, Trigger } from '@dxos/async';
-import { Context } from '@dxos/context';
+import { cancelWithContext, Context } from '@dxos/context';
 import { log } from '@dxos/log';
 import { DataObject } from '@dxos/protocols/proto/dxos/mesh/teleport/objectsync';
-import { entry } from '@dxos/util';
 
 import { ObjectSyncExtension } from './object-sync-extension';
 
@@ -15,11 +14,16 @@ export type ObjectSyncParams = {
   setObject: (data: DataObject) => Promise<void>;
 };
 
+type DownloadRequest = {
+  trigger: Trigger<DataObject>;
+  counter: number;
+};
+
 @trackLeaks('open', 'close')
 export class ObjectSync {
   private readonly _ctx = new Context();
 
-  private readonly _downloadRequests = new Map<string, Trigger<DataObject>>();
+  private readonly _downloadRequests = new Map<string, DownloadRequest>();
   private readonly _extensions = new Set<ObjectSyncExtension>();
 
   constructor(private readonly _params: ObjectSyncParams) {}
@@ -30,15 +34,37 @@ export class ObjectSync {
     await this._ctx.dispose();
   }
 
-  download(id: string): Promise<DataObject> {
+  download(ctx: Context, id: string): Promise<DataObject> {
     log('download', { id });
-    const trigger = entry(this._downloadRequests, id).orInsert(new Trigger<DataObject>()).value;
 
-    for (const extension of this._extensions) {
-      extension.updateWantListInASeparateTask(new Set(this._downloadRequests.keys()));
+    const existingRequest = this._downloadRequests.get(id);
+    if (existingRequest) {
+      existingRequest.counter++;
+      return existingRequest.trigger.wait();
     }
 
-    return trigger.wait();
+    const request = {
+      trigger: new Trigger<DataObject>(),
+      counter: 1,
+    };
+
+    this._downloadRequests.set(id, request);
+
+    this._updateExtensionsWantList();
+
+    ctx?.onDispose(() => {
+      // Remove request if context is disposed and nobody else requests it.
+      const request = this._downloadRequests.get(id);
+      if (!request) {
+        return;
+      }
+      if (--request.counter === 0) {
+        this._downloadRequests.delete(id);
+      }
+      this._updateExtensionsWantList();
+    });
+
+    return ctx ? cancelWithContext(ctx, request.trigger.wait()) : request.trigger.wait();
   }
 
   createExtension() {
@@ -69,7 +95,7 @@ export class ObjectSync {
         }
         log('received', { obj });
         await this._params.setObject(obj);
-        this._downloadRequests.get(obj.id)?.wake(obj);
+        this._downloadRequests.get(obj.id)?.trigger.wake(obj);
         this._downloadRequests.delete(obj.id);
 
         for (const extension of this._extensions) {
@@ -80,5 +106,11 @@ export class ObjectSync {
       },
     });
     return extension;
+  }
+
+  private _updateExtensionsWantList() {
+    for (const extension of this._extensions) {
+      extension.updateWantListInASeparateTask(new Set(this._downloadRequests.keys()));
+    }
   }
 }
