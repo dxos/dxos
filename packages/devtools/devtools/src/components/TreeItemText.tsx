22a21a46962569d2399c6b41663d6709bd5026e3
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-07-11 12:33:26 +0300

    feat(devtools): Add interactive json tree view improve UI/UX (#3327)

--- packages/devtools/devtools/src/components/TreeItemText.tsx
+++ packages/devtools/devtools/src/components/TreeItemText.tsx
@@ -1,13 +1,13 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from 'react';
 
 export type TreeItemTextProps = {
   primary: ReactNode;
   secondary?: ReactNode;
-}
+};
 
 export const TreeItemText = ({ primary, secondary }: TreeItemTextProps) => (
   <div className='flex gap-2 overflow-hidden whitespace-nowrap'>
     <span>{primary}</span>
     <span className='text-gray-400'>{secondary}</span>
   </div>
-)
+);
