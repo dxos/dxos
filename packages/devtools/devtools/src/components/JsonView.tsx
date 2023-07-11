baf14a61aae8971ba671046db841faa7a2e23cbf
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-07-11 14:11:00 +0300

    Revert "Change deps"
    This reverts commit c441a1951d002a6b3a25818cce9996ea4bfd30e5.

--- packages/devtools/devtools/src/components/JsonView.tsx
+++ packages/devtools/devtools/src/components/JsonView.tsx
@@ -4,7 +4,7 @@
 
 import React, { FC } from 'react';
 // eslint-disable-next-line no-restricted-imports
-import { JsonTree } from 'react-editable-json-tree';
+import { JSONTree } from 'react-json-tree';
 
 import { schema } from '@dxos/protocols';
 
@@ -15,7 +15,7 @@ export const JsonView: FC<{ data?: Object; className?: string }> = ({ data, clas
 
   return (
     <div className={className}>
-      <JsonTree data={replaced} readOnly={true} isCollapsed={() => false} />
+      <JSONTree data={replaced} theme='monokai' invertTheme={true} />
     </div>
   );
 };
