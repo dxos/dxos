c48cf40582efa4d17852db9719f65e470650fe75
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-07-11 16:59:44 +0300

    refactor(cli): Reconcile `agent run` and `agent start` commands (#3635)

--- packages/core/agent/src/forever/forever-daemon.ts
+++ packages/core/agent/src/forever/forever-daemon.ts
@@ -62,7 +62,7 @@ export class ForeverDaemon implements Daemon {
       // Run the `dx agent run` CLI command.
       // TODO(burdon): Call local run services binary directly (not via CLI)?
       forever.startDaemon(process.argv[1], {
-        args: ['agent', 'run', `--profile=${profile}`, '--socket'],
+        args: ['agent', 'start', '--foreground', `--profile=${profile}`, '--socket'],
         uid: profile,
         logFile: path.join(logDir, 'daemon.log'), // Forever daemon process.
         outFile: path.join(logDir, 'out.log'), // Child stdout.
