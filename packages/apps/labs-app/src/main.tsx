39c402d1e39d43d52b0e198070d37458eb04e03b
Rich Burdon <rich.burdon@gmail.com>
2023-07-11 15:17:22 +0100

    First cut of new labs app for labs plugins.

---
+++ packages/apps/labs-app/src/main.tsx
@@ -0,0 +1,45 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import '@dxosTheme';
+
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { ClientPlugin } from '@braneframe/plugin-client';
+import { GraphPlugin } from '@braneframe/plugin-graph';
+import { MarkdownPlugin } from '@braneframe/plugin-markdown';
+import { SpacePlugin } from '@braneframe/plugin-space';
+import { SplitViewPlugin } from '@braneframe/plugin-splitview';
+import { StackPlugin } from '@braneframe/plugin-stack';
+import { ThemePlugin } from '@braneframe/plugin-theme';
+import { TreeViewPlugin } from '@braneframe/plugin-treeview';
+import { UrlSyncPlugin } from '@braneframe/plugin-url-sync';
+import { Config, Defaults } from '@dxos/config';
+import { initializeAppTelemetry } from '@dxos/react-appkit/telemetry';
+import { PluginContextProvider } from '@dxos/react-surface';
+
+import { LocalFilesPlugin, GithubPlugin } from './plugins';
+
+void initializeAppTelemetry({ namespace: 'composer-app', config: new Config(Defaults()) });
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <PluginContextProvider
+      plugins={[
+        ThemePlugin(),
+        ClientPlugin(),
+        GraphPlugin(),
+        TreeViewPlugin(),
+        UrlSyncPlugin(),
+        SplitViewPlugin(),
+        SpacePlugin(),
+        MarkdownPlugin(),
+        StackPlugin(),
+        GithubPlugin(),
+        LocalFilesPlugin(),
+      ]}
+    />
+  </StrictMode>,
+);
