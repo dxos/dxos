b55e9fa7b830dec66d7c0c21d0857fe186b29fb7
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-07-12 13:23:48 +0300

    Format

--- packages/core/agent/src/file-lock.test.ts
+++ packages/core/agent/src/file-lock.test.ts
@@ -1,95 +1,98 @@
-import { sleep } from "@dxos/async"
-import { log } from "@dxos/log"
-import { describe, test } from "@dxos/test"
-import chai, { expect } from "chai"
-import chaiAsPromised from "chai-as-promised"
-import { flock } from 'fs-ext'
-import { spawn } from "node:child_process"
-import { FileHandle, constants, open } from "node:fs/promises"
-import { join } from "node:path"
+//
+// Copyright 2023 DXOS.org
+//
 
-chai.use(chaiAsPromised)
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { flock } from 'fs-ext';
+import { spawn } from 'node:child_process';
+import { FileHandle, constants, open } from 'node:fs/promises';
+import { join } from 'node:path';
+
+import { sleep } from '@dxos/async';
+import { log } from '@dxos/log';
+import { describe, test } from '@dxos/test';
+
+chai.use(chaiAsPromised);
 
 const lock = async (filename: string): Promise<FileHandle> => {
-  const handle = await open(filename, constants.O_CREAT)
+  const handle = await open(filename, constants.O_CREAT);
   await new Promise<void>((resolve, reject) => {
     flock(handle.fd, 'exnb', (err) => {
       if (err) {
-        reject(err)
-        return
+        reject(err);
+        return;
       }
-      resolve()
-    })
-  })
-  return handle
-}
+      resolve();
+    });
+  });
+  return handle;
+};
 
 const unlock = async (handle: FileHandle) => {
   await new Promise<void>((resolve, reject) => {
     flock(handle.fd, 'un', (err) => {
       if (err) {
-        reject(err)
-        return
+        reject(err);
+        return;
       }
-      resolve()
-    })
-  })
-  await handle.close()
-}
+      resolve();
+    });
+  });
+  await handle.close();
+};
 
-
-describe.only('FileLocking', () => {
+describe('FileLocking', () => {
   test('basic', async () => {
-    const filename = join('/tmp', `lock-${Math.random()}.lock`)
+    const filename = join('/tmp', `lock-${Math.random()}.lock`);
 
-    const handle = await lock(filename)
+    const handle = await lock(filename);
     await expect(lock(filename)).to.be.rejected;
 
-    await unlock(handle)
+    await unlock(handle);
 
-    log.break()
+    log.break();
 
-    const handle2 = await lock(filename)
-    await unlock(handle2)
-  })
+    const handle2 = await lock(filename);
+    await unlock(handle2);
+  });
 
   test('released when process exists', async () => {
-    const filename = join('/tmp', `lock-${Math.random()}.lock`)
+    const filename = join('/tmp', `lock-${Math.random()}.lock`);
 
     // TODO(dmaretskyi): Self-contained so when function.toString is called the code runs.
-    // @ts-ignore
-    function lockInProcess(filename) {
-      const { open, constants } = require('node:fs/promises')
-      const { flock } = require('fs-ext')
+    const lockInProcess = (filename: string) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { open, constants } = require('node:fs/promises');
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { flock } = require('fs-ext');
 
-      // @ts-ignore
-      open(filename, constants.O_CREAT).then((handle) => {
-        // @ts-ignore
-        flock(handle.fd, 'exnb', (err) => {
+      open(filename, constants.O_CREAT).then((handle: FileHandle) => {
+        flock(handle.fd, 'exnb', (err: Error) => {
           if (err) {
-            console.error(err)
-            return
+            console.error(err);
+            return;
           }
           // Hang
-          setTimeout(() => {}, 1_000_000)
-        })
-      })
-    }
-
-
-    const processHandle = spawn('node', ['-e', `(${lockInProcess.toString()})(${JSON.stringify(filename)})`], { stdio: 'inherit' })
+          setTimeout(() => {}, 1_000_000);
+        });
+      });
+    };
+    const processHandle = spawn('ts-node', ['-e', `(${lockInProcess.toString()})(${JSON.stringify(filename)})`], {
+      stdio: 'inherit',
+    });
 
     // Wait for process to start
-    await sleep(50)
+    await sleep(50);
 
     await expect(lock(filename)).to.be.rejected;
-    
-    processHandle.kill()
 
-    // Wait for process to be killed  
-    await sleep(50)
+    processHandle.kill();
 
-    const handle = await lock(filename)
-    await unlock(handle)
-  })
-})
+    // Wait for process to be killed
+    await sleep(50);
+
+    const handle = await lock(filename);
+    await unlock(handle);
+  });
+});
