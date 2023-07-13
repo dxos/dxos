e0e6b3beef267ec4943e4868714b54e01cf27137
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2023-07-13 10:44:34 +0100

    feat: Print start errors in CLI daemon; add `lock-file` package (#3634)
    Co-authored-by: Mykola Veremchuk <veremchukmukola@gmail.com>

---
+++ packages/core/agent/src/printer.ts
@@ -0,0 +1,78 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import assert from 'node:assert';
+import fs from 'node:fs';
+import { FileHandle } from 'node:fs/promises';
+
+import { scheduleTask } from '@dxos/async';
+import { Context } from '@dxos/context';
+
+enum FileState {
+  INIT = 'INIT',
+  READING = 'READING',
+  REACHED_END = 'REACHED_END',
+  DONE = 'DONE',
+}
+
+export class Printer {
+  private state = FileState.INIT;
+  private stream?: fs.ReadStream;
+  private offset: number;
+  private waited = 0;
+
+  private readonly _ctx = new Context();
+
+  constructor(private readonly params: { file: FileHandle; start: number; timeout: number; interval: number }) {
+    this.offset = params.start;
+  }
+
+  async start() {
+    assert(this.state === FileState.INIT);
+    await this._readToEnd();
+  }
+
+  private async _readToEnd() {
+    this.state = FileState.READING;
+    const stream = this.params.file.createReadStream({ start: this.offset, end: Infinity });
+    stream.on('data', this._handleData.bind(this));
+    stream.on('end', this._handleEnd.bind(this));
+  }
+
+  private async _handleData(data: Buffer) {
+    if (this.state !== FileState.READING) {
+      return;
+    }
+    console.log(data.toString());
+    this.offset += data.length;
+  }
+
+  private async _handleEnd() {
+    this.state = FileState.REACHED_END;
+
+    if (this._finished()) {
+      return;
+    }
+
+    scheduleTask(
+      this._ctx,
+      async () => {
+        await this._readToEnd();
+        this.waited += this.params.interval;
+      },
+      this.params.interval,
+    );
+  }
+
+  private _finished() {
+    return this.state === FileState.DONE || this.waited > this.params.timeout;
+  }
+
+  async close() {
+    this.state = FileState.DONE;
+    void this._ctx.dispose();
+    this.stream?.destroy();
+    await this.params.file.close();
+  }
+}
