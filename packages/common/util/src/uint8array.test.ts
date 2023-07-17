d3fd4dddde22b9bc5f3254a4d901c3de42da3d22
Dmytro Maretskyi <maretskii@gmail.com>
2023-07-17 13:30:21 +0200

    Add conversion functions

---
+++ packages/common/util/src/uint8array.test.ts
@@ -0,0 +1,27 @@
+import { test, describe } from '@dxos/test';
+import { arrayToBuffer, bufferToArray } from './uint8array';
+import { expect } from 'chai';
+
+describe('uint8array', () => {
+  test('arrayToBuffer', () => {
+    const data = new Uint8Array([1, 2, 3, 4]);
+
+    const buffer = arrayToBuffer(data.subarray(1));
+    expect(buffer.toString('hex')).to.eq('020304');
+
+    // Buffer shares memory with Uint8Array.
+    buffer[0] = 0;
+    expect(data[1]).to.eq(0);
+  })
+
+  test('bufferToArray', () => {
+    const data = Buffer.from([1, 2, 3, 4]);
+
+    const array = bufferToArray(data.subarray(1));
+    expect(array.toString()).to.eq('2,3,4');
+
+    // Uint8Array shares memory with Buffer.
+    array[0] = 0;
+    expect(data[1]).to.eq(0);
+  })
+});
--- packages/common/util/src/uint8array.ts
+++ packages/common/util/src/uint8array.ts
@@ -15,3 +15,19 @@ export const arraysEqual = (a: Uint8Array, b: Uint8Array): boolean => {
 
   return true;
 };
+
+/**
+ * Non-copying conversion of Uint8Array to Buffer.
+ * The resulting buffer will share the same allocated memory as the Uint8Array.
+ */
+export const arrayToBuffer = (array: Uint8Array): Buffer => {
+  return Buffer.from(array.buffer, array.byteOffset, array.byteLength);
+}
+
+/**
+ * Non-copying conversion of Buffer to Uint8Array.
+ * The resulting Uint8Array will share the same allocated memory as the Buffer.
+ */
+export const bufferToArray = (buffer: Buffer): Uint8Array => {
+  return new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength);
+}
