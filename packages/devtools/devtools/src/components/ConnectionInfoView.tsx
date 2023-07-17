97878bff35487ab178cdf17c4f18aef85b5a16d8
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-07-17 14:58:41 +0300

    Add treeeview

--- packages/devtools/devtools/src/components/ConnectionInfoView.tsx
+++ packages/devtools/devtools/src/components/ConnectionInfoView.tsx
@@ -2,12 +2,15 @@
 // Copyright 2021 DXOS.org
 //
 
+import { Upload } from '@phosphor-icons/react';
 import React from 'react';
 
 import { ConnectionInfo } from '@dxos/protocols/proto/dxos/devtools/swarm';
+import { TreeView } from '@dxos/react-appkit';
 
 import { DetailsTable } from './DetailsTable';
 import { JsonView } from './JsonView';
+import { TreeItemText } from './TreeItemText';
 
 export interface ConnectionInfoViewProps {
   connectionInfo: ConnectionInfo;
@@ -30,6 +33,28 @@ export const ConnectionInfoView = ({ connectionInfo, onReturn }: ConnectionInfoV
         <JsonView
           data={{
             events: connectionInfo.events,
+          }}
+        />
+      ),
+      streams: (
+        <TreeView
+          items={
+            connectionInfo.streams?.map((streamStat) => ({
+              id: streamStat.id.toString(),
+              Icon: Upload,
+              Element: (
+                <TreeItemText
+                  primary={`| Up: ${streamStat.bytesSent} | Down: ${streamStat.bytesReceived} |`}
+                  secondary={streamStat.tag}
+                />
+              ),
+            })) ?? []
+          }
+          expanded={connectionInfo.streams?.map((streamStat) => streamStat.id.toString())}
+          slots={{
+            value: {
+              className: 'overflow-hidden text-gray-400 truncate pl-2',
+            },
           }}
         />
       ),
