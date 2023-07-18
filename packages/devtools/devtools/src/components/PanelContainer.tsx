7afdfcfd7f07ed13122658ee1928fb29859a5785
Rich Burdon <rich.burdon@gmail.com>
2023-07-18 13:01:13 +0100

    feat: Experimental threads plugin (#3654)

--- packages/devtools/devtools/src/components/PanelContainer.tsx
+++ packages/devtools/devtools/src/components/PanelContainer.tsx
@@ -19,6 +19,6 @@ export const PanelContainer: FC<{ toolbar?: ReactNode; children: ReactNode; clas
   );
 };
 
-export const Toolbar: FC<{ children: ReactNode }> = ({ children }) => (
+export const Toolbar: FC<{ children?: ReactNode }> = ({ children }) => (
   <div className='flex space-x-2 p-2'>{children}</div>
 );
