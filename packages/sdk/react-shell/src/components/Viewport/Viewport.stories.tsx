645e5b7a9d73d57c737bd4e194cb4a8611065b64
Will Shown <thure@users.noreply.github.com>
2023-07-19 17:42:05 -0700

    feat(react-shell): `Viewport` (#3690)

---
+++ packages/sdk/react-shell/src/components/Viewport/Viewport.stories.tsx
@@ -0,0 +1,59 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import React from 'react';
+
+import { AlertDialog, Button } from '@dxos/aurora';
+
+import { useViewportContext, Viewport, ViewportScopedProps } from './Viewport';
+
+type StorybookViewportProps = {};
+
+const Views = ({ __viewportScope }: ViewportScopedProps<{}>) => {
+  const { setActiveView } = useViewportContext('StorybookViews', __viewportScope);
+  return (
+    <Viewport.Views>
+      <Viewport.View id='one' classNames='p-4'>
+        <p>One</p>
+        <p className='invisible'>Two</p>
+        <p className='invisible'>Three</p>
+        <Button onClick={() => setActiveView('two')}>Next</Button>
+      </Viewport.View>
+      <Viewport.View id='two' classNames='p-4'>
+        <p className='invisible'>One</p>
+        <p>Two</p>
+        <p className='invisible'>Three</p>
+        <Button onClick={() => setActiveView('three')}>Next</Button>
+      </Viewport.View>
+      <Viewport.View id='three' classNames='p-4'>
+        <p className='invisible'>One</p>
+        <p className='invisible'>Two</p>
+        <p>Three</p>
+        <Button onClick={() => setActiveView('one')}>Next</Button>
+      </Viewport.View>
+    </Viewport.Views>
+  );
+};
+
+const StorybookViewport = (props: StorybookViewportProps) => {
+  return (
+    <AlertDialog.Root defaultOpen>
+      <AlertDialog.Overlay>
+        <AlertDialog.Content classNames='p-0'>
+          <Viewport.Root defaultActiveView='one'>
+            <Views />
+          </Viewport.Root>
+        </AlertDialog.Content>
+      </AlertDialog.Overlay>
+    </AlertDialog.Root>
+  );
+};
+
+export default {
+  component: StorybookViewport,
+};
+
+export const Default = {
+  args: {},
+};
