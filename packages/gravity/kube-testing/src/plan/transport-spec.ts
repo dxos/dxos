b018231ee122e857f3f418aebf1ef91ac5b32b58
egorgripasov <gripasovegor@gmail.com>
2023-07-20 11:43:08 +0200

    fix: cleanup

--- packages/gravity/kube-testing/src/plan/transport-spec.ts
+++ packages/gravity/kube-testing/src/plan/transport-spec.ts
@@ -72,10 +72,13 @@ export class TransportTestPlan implements TestPlan<TransportTestSpec, TransportA
 
     log.info(`creating ${swarmTopicIds.length} swarms`, { agentIdx });
 
+    // Swarms to join.
     const swarms = swarmTopicIds.map((swarmTopicId, swarmIdx) => {
       const swarmTopic = PublicKey.from(swarmTopicId);
       return peer.createSwarm(swarmTopic);
     });
+
+    // const delayedSwarm = peer.createSwarm(PublicKey.from('delayed'));
 
     log.info('swarms created', { agentIdx });
 
@@ -91,48 +94,79 @@ export class TransportTestPlan implements TestPlan<TransportTestSpec, TransportA
 
       log.info('testRun iteration', { iterationId: testCounter });
 
-      // How many connections established within the desired duration.
-      await Promise.all(
-        swarms.map(async (swarm, swarmIdx) => {
-          log.info('joining swarm', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
-          await cancelWithContext(context, swarm.join());
+      /**
+       * Join swarm and wait till all peers are connected.
+       */
+      const joinSwarm = async (swarmIdx: number, swarm: any) => {
+        log.info('joining swarm', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
+        await cancelWithContext(context, swarm.join());
 
-          log.info('swarm joined', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
-          await sleep(spec.desiredSwarmTimeout);
-          log.info('number of connections within duration', {
+        log.info('swarm joined', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
+        await sleep(spec.desiredSwarmTimeout);
+        log.info('number of connections within duration', {
+          agentIdx,
+          swarmIdx,
+          swarmTopic: swarm.topic,
+          connections: swarm.protocol.connections.size,
+          numOfAgents,
+        });
+
+        /**
+         * Wait till all peers are connected (with timeout).
+         */
+        const waitTillConnected = async () => {
+          await cancelWithContext(
+            context,
+            swarm.protocol.connected.waitForCondition(
+              () => swarm.protocol.connections.size === Object.keys(agents).length - 1,
+            ),
+          );
+          log.info('all peers connected', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
+        };
+
+        asyncTimeout(waitTillConnected(), spec.fullSwarmTimeout).catch((error) => {
+          log.info('all peers not connected', {
             agentIdx,
             swarmIdx,
             swarmTopic: swarm.topic,
             connections: swarm.protocol.connections.size,
             numOfAgents,
           });
+        });
+      };
 
-          // Wait till all peers are connected (with timeout).
-          const waitTillConnected = async () => {
-            await cancelWithContext(
-              context,
-              swarm.protocol.connected.waitForCondition(
-                () => swarm.protocol.connections.size === Object.keys(agents).length - 1,
-              ),
-            );
-            log.info('all peers connected', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
-          };
+      /**
+       * Leave swarm and wait till all peers are disconnected.
+       */
+      const leaveSwarm = async (swarmIdx: number, swarm: any) => {
+        log.info('closing swarm', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
+        await cancelWithContext(context, swarm.leave());
+        log.info('swarm closed', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
 
-          asyncTimeout(waitTillConnected(), spec.fullSwarmTimeout).catch((error) => {
-            log.info('all peers not connected', {
-              agentIdx,
-              swarmIdx,
-              swarmTopic: swarm.topic,
-              connections: swarm.protocol.connections.size,
-              numOfAgents,
-            });
+        /**
+         * Wait till all peers are disconnected (with timeout).
+         */
+        const waitTillDisconnected = async () => {
+          await cancelWithContext(
+            context,
+            swarm.protocol.disconnected.waitForCondition(() => swarm.protocol.connections.size === 0),
+          );
+          log.info('all peers disconnected', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
+        };
+
+        asyncTimeout(waitTillDisconnected(), spec.fullSwarmTimeout).catch((error) => {
+          log.info('all peers not disconnected', {
+            agentIdx,
+            swarmIdx,
+            swarmTopic: swarm.topic,
+            connections: swarm.protocol.connections.size,
           });
-        }),
-      );
+        });
+      };
 
-      await env.syncBarrier(`swarms are ready on ${testCounter}`);
-      await sleep(10_000);
-
+      /**
+       * Iterate over all swarms and all agents.
+       */
       const forEachSwarm = async (callback: (swarmIdx: number, swarm: any, agentId: string, ) => Promise<void>) => {
         await Promise.all(
           Object.keys(env.params.agents)
@@ -146,92 +180,100 @@ export class TransportTestPlan implements TestPlan<TransportTestSpec, TransportA
         );
       }
 
-      log.info('starting streams', { agentIdx });
+      // Join all swarms.
+      // How many connections established within the desired duration.
+      {
+        log.info('joining all swarms', { agentIdx });
 
-      // TODO(egorgripasov): Multiply by iterration number.
-      const desiredStreems = (numOfAgents - 1) * spec.swarmsPerAgent;
-      let actualStreams = 0;
+        await Promise.all(
+          swarms.map(async (swarm, swarmIdx) => {
+            await joinSwarm(swarmIdx, swarm);
+          }),
+        );
 
-      await forEachSwarm(async (swarmIdx, swarm, agentId) => {
-        log.info('starting stream', { agentIdx, swarmIdx });
-        try {
-          const streamTag = `stream-test-${testCounter}-${env.params.agentId}-${agentId}-${swarmIdx}`;
-          await swarm.protocol.startStream(PublicKey.from(agentId), streamTag, spec.streamLoadInterval, spec.streamLoadChunkSize);
-          actualStreams++;
-          log.info('test stream started', { agentIdx, swarmIdx });
-        } catch (error) {
-          log.info('test stream failed', { agentIdx, swarmIdx, error });
-        }
-      });
+        await env.syncBarrier(`swarms are ready on ${testCounter}`);
+      }
 
-      log.info('streams started', { testCounter, agentIdx, desiredStreems, actualStreams });
-      await env.syncBarrier(`streams are started at ${testCounter}`);
+      await sleep(10_000);
 
-      await sleep(spec.streamsDelay)
+      // Start streams on all swarms.
+      {
+        log.info('starting streams', { agentIdx });
 
-      log.info('start testing connections', { agentIdx, testCounter });
+        // TODO(egorgripasov): Multiply by iterration number.
+        const desiredStreems = (numOfAgents - 1) * spec.swarmsPerAgent;
+        let actualStreams = 0;
 
-      const desiredConnections = (numOfAgents - 1) * spec.swarmsPerAgent;
-      let actualConnections = 0;
+        await forEachSwarm(async (swarmIdx, swarm, agentId) => {
+          log.info('starting stream', { agentIdx, swarmIdx });
+          try {
+            const streamTag = `stream-test-${testCounter}-${env.params.agentId}-${agentId}-${swarmIdx}`;
+            await swarm.protocol.startStream(PublicKey.from(agentId), streamTag, spec.streamLoadInterval, spec.streamLoadChunkSize);
+            actualStreams++;
+            log.info('test stream started', { agentIdx, swarmIdx });
+          } catch (error) {
+            log.info('test stream failed', { agentIdx, swarmIdx, error });
+          }
+        });
+
+        log.info('streams started', { testCounter, agentIdx, desiredStreems, actualStreams });
+        await env.syncBarrier(`streams are started at ${testCounter}`);
+      }
+
+      await sleep(spec.streamsDelay);
 
       // Test connections.
-      await forEachSwarm(async (swarmIdx, swarm, agentId) => {
-        log.info('testing connection', { agentIdx, swarmIdx });
-        try {
-          await swarm.protocol.testConnection(PublicKey.from(agentId), 'hello world');
-          actualConnections++;
-          log.info('test connection succeded', { agentIdx, swarmIdx });
-        } catch (error) {
-          log.info('test connection failed', { agentIdx, swarmIdx, error });
-        }
-      });
+      {
+        log.info('start testing connections', { agentIdx, testCounter });
 
-      log.info('test connections done', { testCounter, agentIdx, desiredConnections, actualConnections });
-      await env.syncBarrier(`connections are tested on ${testCounter}`);
+        const desiredConnections = (numOfAgents - 1) * spec.swarmsPerAgent;
+        let actualConnections = 0;
 
-      log.info('closing streams', { agentIdx });
+        await forEachSwarm(async (swarmIdx, swarm, agentId) => {
+          log.info('testing connection', { agentIdx, swarmIdx });
+          try {
+            await swarm.protocol.testConnection(PublicKey.from(agentId), 'hello world');
+            actualConnections++;
+            log.info('test connection succeded', { agentIdx, swarmIdx });
+          } catch (error) {
+            log.info('test connection failed', { agentIdx, swarmIdx, error });
+          }
+        });
 
-      await forEachSwarm(async (swarmIdx, swarm, agentId) => {
-        log.info('closing stream', { agentIdx, swarmIdx });
-        try {
-          const streamTag = `stream-test-${testCounter}-${env.params.agentId}-${agentId}-${swarmIdx}`;
-          const stats = await swarm.protocol.closeStream(PublicKey.from(agentId), streamTag);
+        log.info('test connections done', { testCounter, agentIdx, desiredConnections, actualConnections });
+        await env.syncBarrier(`connections are tested on ${testCounter}`);
+      }
 
-          log.info('test stream closed', { agentIdx, swarmIdx, ...stats });
-        } catch (error) {
-          log.info('test stream closing failed', { agentIdx, swarmIdx, error });
-        }
-      });
+      // Close streams.
+      {
+        log.info('closing streams', { agentIdx });
 
-      log.info('streams closed', { testCounter, agentIdx, desiredStreems, actualStreams });
-      await env.syncBarrier(`streams are closed at ${testCounter}`);
+        await forEachSwarm(async (swarmIdx, swarm, agentId) => {
+          log.info('closing stream', { agentIdx, swarmIdx });
+          try {
+            const streamTag = `stream-test-${testCounter}-${env.params.agentId}-${agentId}-${swarmIdx}`;
+            const stats = await swarm.protocol.closeStream(PublicKey.from(agentId), streamTag);
 
-      log.info('closing swarms');
-      await Promise.all(
-        swarms.map(async (swarm, swarmIdx) => {
-          log.info('closing swarm', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
-          await cancelWithContext(context, swarm.leave());
-          log.info('swarm closed', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
+            log.info('test stream closed', { agentIdx, swarmIdx, ...stats });
+          } catch (error) {
+            log.info('test stream closing failed', { agentIdx, swarmIdx, error });
+          }
+        });
 
-          // Wait till all peers are disconnected (with timeout).
-          const waitTillDisconnected = async () => {
-            await cancelWithContext(
-              context,
-              swarm.protocol.disconnected.waitForCondition(() => swarm.protocol.connections.size === 0),
-            );
-            log.info('all peers disconnected', { agentIdx, swarmIdx, swarmTopic: swarm.topic });
-          };
+        log.info('streams closed', { testCounter, agentIdx });
+        await env.syncBarrier(`streams are closed at ${testCounter}`);
+      }
 
-          asyncTimeout(waitTillDisconnected(), spec.fullSwarmTimeout).catch((error) => {
-            log.info('all peers not disconnected', {
-              agentIdx,
-              swarmIdx,
-              swarmTopic: swarm.topic,
-              connections: swarm.protocol.connections.size,
-            });
-          });
-        }),
-      );
+      // Leave all swarms.
+      {
+        log.info('closing all swarms');
+
+        await Promise.all(
+          swarms.map(async (swarm, swarmIdx) => {
+            await leaveSwarm(swarmIdx, swarm);
+          }),
+        );
+      }
     };
 
     scheduleTaskInterval(
