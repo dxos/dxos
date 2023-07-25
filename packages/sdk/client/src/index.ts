2d6a257baee5049fd4e091662b797b48cdbb49a6
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-07-25 14:39:22 +0300

    Add todo

--- packages/sdk/client/src/index.ts
+++ packages/sdk/client/src/index.ts
@@ -6,4 +6,6 @@ export { Config, Defaults, Dynamics, Envs, Local } from '@dxos/config';
 export { PublicKey, type PublicKeyLike } from '@dxos/keys';
 export { ApiError } from '@dxos/errors';
 export { Client, ClientOptions } from './client/client';
+
+// TODO(mykola): Do not expose SpaceProxy.
 export { SpaceProxy } from './echo';
