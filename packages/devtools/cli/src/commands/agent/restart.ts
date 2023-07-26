2a699a650d600cd69f0c8b53bc30682df199045c
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-07-26 13:22:57 +0300

    Add force flag to restart command

--- packages/devtools/cli/src/commands/agent/restart.ts
+++ packages/devtools/cli/src/commands/agent/restart.ts
@@ -2,11 +2,18 @@
 // Copyright 2023 DXOS.org
 //
 
+import { Flags } from '@oclif/core';
+
 import { BaseCommand } from '../../base-command';
 
 export default class Restart extends BaseCommand<typeof Restart> {
   static override enableJsonFlag = true;
   static override description = 'Restart agent daemon.';
+
+  static override flags = {
+    ...BaseCommand.flags,
+    force: Flags.boolean({ description: 'Force restart.' }),
+  };
 
   async run(): Promise<any> {
     return await this.execWithDaemon(async (daemon) => {
