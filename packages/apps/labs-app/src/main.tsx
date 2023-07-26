5cacd7cdacddd07a118273a1e2b538981080c9b0
Josiah Witt <josiah@witt.life>
2023-07-26 08:43:11 -0400

    feat: introduce pwa plugin and error plugin (#3733)

--- packages/apps/labs-app/src/main.tsx
+++ packages/apps/labs-app/src/main.tsx
@@ -7,14 +7,18 @@ import '@dxosTheme';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// import { LocalFilesPlugin, GithubPlugin } from '@braneframe/composer-app';
 import { ClientPlugin } from '@braneframe/plugin-client';
 import { DebugPlugin } from '@braneframe/plugin-debug';
 import { DndPlugin } from '@braneframe/plugin-dnd';
 import { DrawingPlugin } from '@braneframe/plugin-drawing';
+import { ErrorPlugin } from '@braneframe/plugin-error';
+import { LocalFilesPlugin } from '@braneframe/plugin-files';
+import { GithubPlugin } from '@braneframe/plugin-github';
 import { GraphPlugin } from '@braneframe/plugin-graph';
+import { IntentPlugin } from '@braneframe/plugin-intent';
 import { KanbanPlugin } from '@braneframe/plugin-kanban';
 import { MarkdownPlugin } from '@braneframe/plugin-markdown';
+import { PwaPlugin } from '@braneframe/plugin-pwa';
 import { SpacePlugin } from '@braneframe/plugin-space';
 import { SplitViewPlugin } from '@braneframe/plugin-splitview';
 import { StackPlugin } from '@braneframe/plugin-stack';
@@ -44,9 +48,14 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PluginContextProvider
       plugins={[
+        IntentPlugin(),
         ThemePlugin(),
         ClientPlugin(),
         DndPlugin(),
+        // Outside of error boundary so that updates are not blocked by errors.
+        PwaPlugin(),
+        // Inside theme provider so that errors are styled.
+        ErrorPlugin(),
         GraphPlugin(),
         TreeViewPlugin(),
         UrlSyncPlugin(),
@@ -54,8 +63,8 @@ createRoot(document.getElementById('root')!).render(
         SpacePlugin(),
         MarkdownPlugin(),
         StackPlugin(),
-        // GithubPlugin(),
-        // LocalFilesPlugin(),
+        GithubPlugin(),
+        LocalFilesPlugin(),
         DrawingPlugin(),
         KanbanPlugin(),
         ThreadPlugin(),
