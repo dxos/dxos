72621b571d317ea4101bf151bece0b02770168bc
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-08-02 17:31:20 +0300

    Add success log

--- packages/devtools/cli/src/commands/halo/join.ts
+++ packages/devtools/cli/src/commands/halo/join.ts
@@ -55,6 +55,7 @@ export default class Join extends BaseCommand<typeof Join> {
       ux.action.start('Waiting for peer to finish invitation');
       await invitationSuccess;
       ux.action.stop();
+      this.log(chalk`{green Peer connected.}`);
     });
   }
 }
