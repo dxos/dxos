5edf62b66e93dbcc0b15ea0ab6fea4f4f4785378
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-08-03 08:40:33 +0300

    fix(cli): Fix space hanging after invitation is completed (#3806)

--- packages/devtools/cli/src/commands/space/create.ts
+++ packages/devtools/cli/src/commands/space/create.ts
@@ -4,9 +4,11 @@
 
 import { Args } from '@oclif/core';
 
+import { asyncTimeout } from '@dxos/async';
 import { Client } from '@dxos/client';
 
 import { BaseCommand } from '../../base-command';
+import { SPACE_WAIT_TIMEOUT, spaceWaitError } from '../../timeouts';
 
 export default class Create extends BaseCommand<typeof Create> {
   static override enableJsonFlag = true;
@@ -17,7 +19,7 @@ export default class Create extends BaseCommand<typeof Create> {
     const { name } = this.args;
     return await this.execWithClient(async (client: Client) => {
       const space = await client.createSpace();
-      await space.waitUntilReady();
+      await asyncTimeout(space.waitUntilReady(), SPACE_WAIT_TIMEOUT, spaceWaitError());
       space.properties.name = name;
       const data = {
         key: space.key.toHex(),
