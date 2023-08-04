64c4cfeb4a48758fb187b00ad40b9ae100bc3d80
egorgripasov <gripasovegor@gmail.com>
2023-08-04 11:49:12 +0200

    fix: address comments

--- packages/core/mesh/teleport/src/muxing/muxer.ts
+++ packages/core/mesh/teleport/src/muxing/muxer.ts
@@ -196,15 +196,13 @@ export class Muxer {
     }
     this._destroying = true;
 
-    Promise.resolve(
-      this._sendCommand(
-        {
-          destroy: {
-            error: err?.message,
-          },
+    this._sendCommand(
+      {
+        destroy: {
+          error: err?.message,
         },
-        SYSTEM_CHANNEL_ID,
-      ),
+      },
+      SYSTEM_CHANNEL_ID,
     )
       .then(() => {
         this._dispose();
