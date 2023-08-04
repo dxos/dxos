efa25a58cfc2f6880cf8ed8f196a42cac8f2f429
Egor Gripasov <gripasovegor@gmail.com>
2023-08-04 12:36:15 +0200

    fix: address comments for network testbench / improvements (#3812)

--- packages/core/mesh/teleport/src/index.ts
+++ packages/core/mesh/teleport/src/index.ts
@@ -4,6 +4,5 @@
 
 export * from './muxing';
 export * from './teleport';
-export * from './testing/test-extension';
-export * from './testing/test-extension-with-streams';
+export * from './testing';
 export * from './rpc-extension';
