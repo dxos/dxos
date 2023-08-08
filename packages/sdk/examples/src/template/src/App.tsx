3e458418b1d1c71eafe83ad59494e63cd768e3a2
thure <will@braneframe.com>
2023-08-08 14:59:11 -0700

    Remove OS variant of `aurora-theme`.

--- packages/sdk/examples/src/template/src/App.tsx
+++ packages/sdk/examples/src/template/src/App.tsx
@@ -5,14 +5,14 @@
 import React from 'react';
 
 import { ThemeProvider } from '@dxos/aurora';
-import { appTx } from '@dxos/aurora-theme';
+import { auroraTx } from '@dxos/aurora-theme';
 import { Client, ClientProvider, PublicKey } from '@dxos/react-client';
 
 import { Demo } from './components';
 
 const App = ({ spaceKey, clients }: { spaceKey: PublicKey; clients: Client[] }) => {
   return (
-    <ThemeProvider tx={appTx}>
+    <ThemeProvider tx={auroraTx}>
       <div className='flex place-content-evenly'>
         {clients.map((client, id) => (
           <ClientProvider key={id} client={client}>
