7cd7adeefd4fd398235d514db1cace2903e87b9e
Nathan A. Ferch <nf@marginal.net>
2023-08-15 16:01:34 -0400

    chore: adopt node-datachannel shim from libp2p
    as of commit e664d14f

---
+++ packages/core/mesh/network-manager/src/transport/datachannel/index.ts
@@ -0,0 +1,12 @@
+import node from 'node-datachannel';
+import { IceCandidate } from './rtc-ice-candidate';
+import { PeerConnection } from './rtc-peer-connection';
+import { SessionDescription } from './rtc-session-description';
+
+export { SessionDescription as RTCSessionDescription };
+export { IceCandidate as RTCIceCandidate };
+export { PeerConnection as RTCPeerConnection };
+
+export function cleanup(): void {
+  node.cleanup();
+}
