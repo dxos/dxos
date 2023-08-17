390ac2a6293f5c2f2879a9b6642b4bf4a4806d5b
Zhenya <zhenyasav@users.noreply.github.com>
2023-08-17 13:24:50 -0400

    chore: purify shell components (#3907)

--- packages/sdk/react-shell/src/panels/SpacePanel/SpacePanel.stories.tsx
+++ packages/sdk/react-shell/src/panels/SpacePanel/SpacePanel.stories.tsx
@@ -5,12 +5,17 @@
 import '@dxosTheme';
 import React from 'react';
 
-import { DensityProvider, ElevationProvider, useThemeContext } from '@dxos/aurora';
 import { PublicKey } from '@dxos/keys';
+import { Invitation } from '@dxos/react-client/invitations';
 import { ClientDecorator } from '@dxos/react-client/testing';
 
+import { SpaceMemberListImpl } from '../../components';
+import { StorybookDialog } from '../../components/StorybookDialog';
+import { alice } from '../../testing/fixtures/identities';
+import { inviteWithState } from '../../testing/fixtures/invitations';
 import { SpacePanelImpl } from './SpacePanel';
 import { SpacePanelImplProps } from './SpacePanelProps';
+import { SpaceManagerImpl } from './steps';
 
 const noOpProps: SpacePanelImplProps = {
   titleId: 'storybookSpacePanel__title',
@@ -20,25 +25,42 @@ const noOpProps: SpacePanelImplProps = {
   space: { key: PublicKey.random(), properties: { name: 'Example space' } },
 };
 
-const StorybookSpacePanel = (args: Partial<SpacePanelImplProps>) => {
-  const spacePanelProps = { ...noOpProps, ...args };
-  const { tx } = useThemeContext();
-  return (
-    <DensityProvider density='fine'>
-      <ElevationProvider elevation='chrome'>
-        <div role='group' className={tx('dialog.content', 'dialog', { inOverlayLayout: false }, 'p-1')}>
-          <SpacePanelImpl {...spacePanelProps} />
-        </div>
-      </ElevationProvider>
-    </DensityProvider>
-  );
-};
+const SpacePanel = (args: Partial<SpacePanelImplProps>) => (
+  <StorybookDialog>
+    <SpacePanelImpl {...noOpProps} {...args} />
+  </StorybookDialog>
+);
 
 export default {
-  component: StorybookSpacePanel,
+  component: SpacePanel,
 };
 
 export const SpaceManager = {
   decorators: [ClientDecorator()],
   args: { activeView: 'space manager' },
+};
+
+export const SpaceInvitationManager = {
+  decorators: [ClientDecorator()],
+  args: { activeView: 'space invitation manager' },
+};
+
+export const PureSpaceManager = () => {
+  return (
+    <StorybookDialog>
+      <SpacePanelImpl
+        {...noOpProps}
+        activeView='space manager'
+        SpaceManager={(props) => {
+          return (
+            <SpaceManagerImpl
+              {...props}
+              invitations={[inviteWithState(Invitation.State.CONNECTING)]}
+              SpaceMemberList={(props) => <SpaceMemberListImpl members={[alice]} />}
+            />
+          );
+        }}
+      />
+    </StorybookDialog>
+  );
 };
