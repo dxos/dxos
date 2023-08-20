af4a570b9aa0e7bb5586a5bf67675a426067d399
Dmytro Maretskyi <maretskii@gmail.com>
2023-08-20 17:01:55 +0200

    Tracing UI

--- packages/devtools/devtools/src/panels/client/TracingPanel.tsx
+++ packages/devtools/devtools/src/panels/client/TracingPanel.tsx
@@ -1,8 +1,10 @@
 import { Resource, Span } from "@dxos/protocols/proto/dxos/tracing";
 import { useClient } from "@dxos/react-client";
 import React, { useEffect, useRef, useState } from "react";
-import { JsonView } from "../../components";
+import { JsonTreeView, JsonView, PanelContainer } from "../../components";
 import { FlameGraph } from 'react-flame-graph';
+import { ArrowLeft, ArrowRight } from "@phosphor-icons/react";
+import { Table, TableColumn } from "@dxos/mosaic";
 
 type State = {
   resources: Map<number, Resource>
@@ -38,32 +40,57 @@ const TracingPanel = () => {
     }
   }, [])
 
+  const [selectedFlameIndex, setSelectedFlameIndex] = useState(0);
+
   
-  const rootSpan = [...state.current.spans.values()].find((s) => s.parentId === undefined);
-  const flameGraph = buildFlameGraph(state.current, rootSpan?.id ?? 0);
+  const roots = [...state.current.spans.values()].filter((s) => s.parentId === undefined);
+  const flameGraph = buildFlameGraph(state.current, roots[Math.min(selectedFlameIndex, roots.length - 1)]?.id ?? 0);
+
+  const selectPrev = () => {
+    setSelectedFlameIndex(idx => Math.max(0, idx - 1));
+  }
+
+  const selectNext = () => {
+    setSelectedFlameIndex(idx => Math.min(roots.length - 1, idx + 1));
+  }
 
   if(flameGraph === undefined) {
     return
   }
 
   return (
-    <FlameGraph
-      data={flameGraph}
-      height={200}
-      width={400}
-    />
+    <PanelContainer>
+      <div className="h-1/2 overflow-auto">
+        <Table compact columns={columns} data={Array.from(state.current.resources.values())} />
+      </div>
+      <div>
+        <div className="flex flex-row items-baseline justify-items-center m-2">
+          <ArrowLeft className="cursor-pointer" onClick={selectPrev} />
+          <div className="flex-1 text-center">{selectedFlameIndex + 1} / {roots.length}</div>
+          <ArrowRight className="cursor-pointer" onClick={selectNext} />
+        </div>
+        <FlameGraph
+          data={flameGraph}
+          height={200}
+          width={400}
+        />
+      </div>
+    </PanelContainer>
   );
-
-  return (
-    <JsonView
-      data={{
-        resources: [...state.current.resources.values()],
-        spans: [...state.current.spans.values()],
-        flameGraph: flameGraph
-      }}
-    />
-  )
 }
+
+const columns: TableColumn<Resource>[] = [
+  {
+    Header: 'Id',
+    width: 15,
+    accessor: 'id'
+  },
+  {
+    Header: 'Name',
+    width: 80,
+    accessor: row => `${row.className}#${row.instanceId}`,
+  }
+];
 
 
 const buildFlameGraph = (state: State, rootId: number): any => {
