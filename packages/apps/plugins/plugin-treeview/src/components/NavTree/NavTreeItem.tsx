5da1c9b2653c448b90e8a152e08c46999bd9d9fa
Will Shown <thure@users.noreply.github.com>
2023-08-23 00:25:53 -0700

    fix(plugin-treeview,aurora-theme): Polish TreeView (#3963)

--- packages/apps/plugins/plugin-treeview/src/components/NavTree/NavTreeItem.tsx
+++ packages/apps/plugins/plugin-treeview/src/components/NavTree/NavTreeItem.tsx
@@ -9,15 +9,17 @@ import React, { FC, forwardRef, ForwardRefExoticComponent, RefAttributes, useEff
 
 import { SortableProps } from '@braneframe/plugin-dnd';
 import { Graph } from '@braneframe/plugin-graph';
-import { Button, DropdownMenu, Tooltip, TreeItem, useSidebars, useTranslation } from '@dxos/aurora';
-import { focusRing, getSize } from '@dxos/aurora-theme';
+import { Button, DropdownMenu, Tooltip, TreeItem, useId, useSidebars, useTranslation } from '@dxos/aurora';
+import { focusRing, getSize, mx } from '@dxos/aurora-theme';
 
-import { SharedTreeItemProps, TREE_VIEW_PLUGIN } from '../../types';
+import { useTreeView } from '../../TreeViewContext';
+import { TREE_VIEW_PLUGIN } from '../../types';
 import { sortActions } from '../../util';
 import { CollapsibleHeading } from './CollapsibleHeading';
 import { NavTree } from './NavTree';
 import { NavigableHeading } from './NavigableHeading';
-import { levelPadding } from './style-fragments';
+import { levelPadding } from './navtree-fragments';
+import { SharedTreeItemProps } from './props';
 
 type SortableBranchTreeViewItemProps = SharedTreeItemProps & Pick<SortableProps, 'rearranging'>;
 
@@ -50,10 +52,12 @@ export const NavTreeItem: ForwardRefExoticComponent<TreeViewItemProps & RefAttri
   TreeViewItemProps
 >(({ node, level, draggableListeners, draggableAttributes, style, rearranging }, forwardedRef) => {
   const isBranch = node.properties?.role === 'branch' || node.children.length > 0;
+  const labelId = useId('navtree__item');
 
   const actions = sortActions(node.actions);
   const { t } = useTranslation(TREE_VIEW_PLUGIN);
   const { navigationSidebarOpen } = useSidebars();
+  const { active: treeViewActive } = useTreeView();
 
   const suppressNextTooltip = useRef<boolean>(false);
   const [optionsTooltipOpen, setOptionsTooltipOpen] = useState(false);
@@ -62,6 +66,7 @@ export const NavTreeItem: ForwardRefExoticComponent<TreeViewItemProps & RefAttri
   const [open, setOpen] = useState(level < 1);
 
   const disabled = !!node.properties?.disabled;
+  const active = treeViewActive === node.id;
 
   useEffect(() => {
     // todo(thure): Open if child within becomes active
@@ -69,20 +74,26 @@ export const NavTreeItem: ForwardRefExoticComponent<TreeViewItemProps & RefAttri
 
   return (
     <TreeItem.Root
+      id={labelId}
       collapsible={isBranch}
       open={!disabled && open}
       onOpenChange={(nextOpen) => setOpen(disabled ? false : nextOpen)}
-      classNames={['rounded block', focusRing, levelPadding(level), rearranging && 'invisible']}
+      classNames={[
+        'rounded block',
+        focusRing,
+        active && 'bg-neutral-75 dark:bg-neutral-850',
+        rearranging && 'invisible',
+      ]}
       {...draggableAttributes}
       {...draggableListeners}
       style={style}
       ref={forwardedRef}
     >
-      <div role='none' className='flex items-start pie-1'>
+      <div role='none' className={mx(levelPadding(level), 'flex items-start')}>
         {isBranch ? (
-          <CollapsibleHeading open={open} node={node} level={level} />
+          <CollapsibleHeading {...{ open, node, level, active, id: labelId }} />
         ) : (
-          <NavigableHeading node={node} level={level} />
+          <NavigableHeading {...{ node, level, active, id: labelId }} />
         )}
         {actions.length > 0 && (
           <Tooltip.Root
