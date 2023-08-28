42cab0ac701864aa489f86adf1b23adf93491be1
Josiah Witt <josiah@witt.life>
2023-08-28 09:21:53 -0400

    fix(react-surface): catch plugin initialization errors

--- packages/apps/plugins/plugin-debug/src/stories/DebugPlugin.stories.tsx
+++ packages/apps/plugins/plugin-debug/src/stories/DebugPlugin.stories.tsx
@@ -6,7 +6,7 @@ import '@dxosTheme';
 import React from 'react';
 
 import { ThemePlugin } from '@braneframe/plugin-theme';
-import { PluginContextProvider, Surface } from '@dxos/react-surface';
+import { PluginProvider, Surface } from '@dxos/react-surface';
 
 import { DebugPlugin } from '../DebugPlugin';
 
@@ -27,7 +27,7 @@ const DebugPluginStoryPlugin = () => ({
 });
 
 const DebugSurfacesApp = () => (
-  <PluginContextProvider plugins={[ThemePlugin(), DebugPlugin(), DebugPluginStoryPlugin()]} />
+  <PluginProvider plugins={[ThemePlugin(), DebugPlugin(), DebugPluginStoryPlugin()]} />
 );
 
 export default {
