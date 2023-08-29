94ce35a8df0e0812b0a521d408b8c97452350be0
Rich Burdon <rich.burdon@gmail.com>
2023-08-29 12:22:18 -0400

    feat: Space save indicator (#4001)

--- packages/apps/plugins/plugin-treeview/src/components/VersionInfo.tsx
+++ packages/apps/plugins/plugin-treeview/src/components/VersionInfo.tsx
@@ -2,7 +2,7 @@
 // Copyright 2023 DXOS.org
 //
 
-import format from 'date-fns/format';
+import formatDistance from 'date-fns/formatDistance';
 import React, { FC } from 'react';
 
 import { Config } from '@dxos/react-client';
@@ -15,10 +15,27 @@ export const VersionInfo: FC<{ config: Config }> = ({ config }) => {
     window.open(`https://github.com/dxos/dxos/commit/${commitHash}`, 'commit');
   };
   return (
-    <div className='flex items-center p-2 gap-2 font-mono text-xs text-neutral-500'>
-      <div className='flex cursor-pointer gap-2' onClick={handleOpen}>
-        <span>v{version}</span>
-        {timestamp && <span>({format(new Date(timestamp), 'MMM dd')})</span>}
+    <div className='flex items-center p-2 gap-2 font-thin text-xs text-neutral-500'>
+      <div className='flex w-full gap-2'>
+        <span
+          title={timestamp && formatDistance(new Date(timestamp), new Date(), { addSuffix: true })}
+          className='font-mono cursor-pointer'
+          onClick={handleOpen}
+        >
+          v{version}
+        </span>
+        <div role='none' className='grow' />
+        <span>
+          Powered by{' '}
+          <a
+            href='https://dxos.org'
+            rel='noreferrer'
+            target='_blank'
+            className='font-normal hover:text-neutral-700 hover:dark:text-neutral-200'
+          >
+            DXOS
+          </a>
+        </span>
       </div>
     </div>
   );
