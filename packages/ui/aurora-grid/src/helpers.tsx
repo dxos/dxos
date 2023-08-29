b3ea7f59df88842ae8001e66c7dc4f6ee35b677d
Rich Burdon <rich.burdon@gmail.com>
2023-08-29 16:25:49 -0400

    feat: Devtools feed montioring (#4000)

--- packages/ui/aurora-grid/src/helpers.tsx
+++ packages/ui/aurora-grid/src/helpers.tsx
@@ -9,7 +9,7 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import React from 'react';
 
 import { Tooltip } from '@dxos/aurora';
-import { chromeSurface } from '@dxos/aurora-theme';
+import { chromeSurface, inputSurface } from '@dxos/aurora-theme';
 import { PublicKey } from '@dxos/keys';
 import { stripUndefinedValues } from '@dxos/util';
 
@@ -142,10 +142,10 @@ export class ColumnBuilder<TData extends RowData> {
 //  Reuse button fragments for hoverColors, selected, primary, etc.
 // TODO(burdon): Compact mode (smaller than density fine).
 export const defaultGridSlots: GridSlots = {
-  root: { className: chromeSurface },
+  root: { className: inputSurface },
   header: { className: [chromeSurface, 'border-b p-1 text-left font-thin opacity-90'] },
   footer: { className: [chromeSurface, 'border-t p-1 text-left font-thin opacity-90'] },
-  // cell: { className: 'p-1' },
+  cell: { className: 'pr-2' },
   row: { className: 'cursor-pointer hover:bg-neutral-50' },
   focus: { className: 'ring-1 ring-teal-500 ring-inset' },
   selected: { className: '!bg-teal-100' },
