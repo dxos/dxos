628dce450858720b9ed08b6e98e9f86d5e7b140f
Will Shown <thure@users.noreply.github.com>
2023-08-30 14:10:48 -0700

    feat(plugin-space): Migrate items (#4029)

--- packages/apps/plugins/plugin-treeview/src/components/TreeItemDragOverlay.tsx
+++ packages/apps/plugins/plugin-treeview/src/components/TreeItemDragOverlay.tsx
@@ -5,14 +5,16 @@
 import React from 'react';
 
 import { Graph } from '@braneframe/plugin-graph';
-import { List } from '@dxos/aurora';
+import { List, DensityProvider } from '@dxos/aurora';
 
 import { NavTreeItem } from './NavTree';
 
 export const TreeItemDragOverlay = ({ data }: { data: Graph.Node }) => {
   return (
-    <List variant='unordered'>
-      <NavTreeItem node={data} level={2} isOverlay />
-    </List>
+    <DensityProvider density='fine'>
+      <List variant='unordered'>
+        <NavTreeItem node={data} level={2} isOverlay />
+      </List>
+    </DensityProvider>
   );
 };
