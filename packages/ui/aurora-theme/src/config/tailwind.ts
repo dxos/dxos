d02f2c397620f9be5e4408a5fb24384652fd45a1
Will Shown <thure@users.noreply.github.com>
2023-08-30 01:49:53 -0700

    feat(aurora): Popper overflows (#4036)

--- packages/ui/aurora-theme/src/config/tailwind.ts
+++ packages/ui/aurora-theme/src/config/tailwind.ts
@@ -249,83 +249,22 @@ export const tailwindConfig = ({
           slider: '0 0 0 5px rgba(0, 0, 0, 0.3)',
         },
         keyframes: {
-          // Dropdown menu
-          'scale-in': {
-            '0%': { opacity: '0', transform: 'scale(0)' },
-            '100%': { opacity: '1', transform: 'scale(1)' },
+          // Popper chrome
+          slideDownAndFade: {
+            from: { opacity: 0, transform: 'translateY(-2px)' },
+            to: { opacity: 1, transform: 'translateY(0)' },
           },
-          'slide-down': {
-            '0%': { opacity: '0', transform: 'translateY(-10px)' },
-            '100%': { opacity: '1', transform: 'translateY(0)' },
+          slideLeftAndFade: {
+            from: { opacity: 0, transform: 'translateX(2px)' },
+            to: { opacity: 1, transform: 'translateX(0)' },
           },
-          'slide-up': {
-            '0%': { opacity: '0', transform: 'translateY(10px)' },
-            '100%': { opacity: '1', transform: 'translateY(0)' },
+          slideUpAndFade: {
+            from: { opacity: 0, transform: 'translateY(2px)' },
+            to: { opacity: 1, transform: 'translateY(0)' },
           },
-          // Tooltip
-          'slide-up-fade': {
-            '0%': { opacity: '0', transform: 'translateY(2px)' },
-            '100%': { opacity: '1', transform: 'translateY(0)' },
-          },
-          'slide-right-fade': {
-            '0%': { opacity: '0', transform: 'translateX(-2px)' },
-            '100%': { opacity: '1', transform: 'translateX(0)' },
-          },
-          'slide-down-fade': {
-            '0%': { opacity: '0', transform: 'translateY(-2px)' },
-            '100%': { opacity: '1', transform: 'translateY(0)' },
-          },
-          'slide-left-fade': {
-            '0%': { opacity: '0', transform: 'translateX(2px)' },
-            '100%': { opacity: '1', transform: 'translateX(0)' },
-          },
-          // Navigation menu
-          'enter-from-right': {
-            '0%': { transform: 'translateX(200px)', opacity: '0' },
-            '100%': { transform: 'translateX(0)', opacity: '1' },
-          },
-          'enter-from-left': {
-            '0%': { transform: 'translateX(-200px)', opacity: '0' },
-            '100%': { transform: 'translateX(0)', opacity: '1' },
-          },
-          'exit-to-right': {
-            '0%': { transform: 'translateX(0)', opacity: '1' },
-            '100%': { transform: 'translateX(200px)', opacity: '0' },
-          },
-          'exit-to-left': {
-            '0%': { transform: 'translateX(0)', opacity: '1' },
-            '100%': {
-              transform: 'translateX(-200px)',
-              opacity: '0',
-            },
-          },
-          'scale-in-content': {
-            '0%': {
-              transform: 'rotateX(-30deg) scale(0.9)',
-              opacity: '0',
-            },
-            '100%': {
-              transform: 'rotateX(0deg) scale(1)',
-              opacity: '1',
-            },
-          },
-          'scale-out-content': {
-            '0%': {
-              transform: 'rotateX(0deg) scale(1)',
-              opacity: '1',
-            },
-            '100%': {
-              transform: 'rotateX(-10deg) scale(0.95)',
-              opacity: '0',
-            },
-          },
-          'fade-in': {
-            '0%': { opacity: '0' },
-            '100%': { opacity: '1' },
-          },
-          'fade-out': {
-            '0%': { opacity: '1' },
-            '100%': { opacity: '0' },
+          slideRightAndFade: {
+            from: { opacity: 0, transform: 'translateX(-2px)' },
+            to: { opacity: 1, transform: 'translateX(0)' },
           },
           // Toast
           'toast-hide': {
@@ -367,24 +306,11 @@ export const tailwindConfig = ({
           },
         },
         animation: {
-          // Dropdown menu
-          'scale-in': 'scale-in 0.2s ease-in-out',
-          'slide-down': 'slide-down 0.6s cubic-bezier(0.16, 1, 0.3, 1)',
-          'slide-up': 'slide-up 0.6s cubic-bezier(0.16, 1, 0.3, 1)',
-          // Tooltip
-          'slide-up-fade': 'slide-up-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-          'slide-right-fade': 'slide-right-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-          'slide-down-fade': 'slide-down-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-          'slide-left-fade': 'slide-left-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-          // Navigation menu
-          'enter-from-right': 'enter-from-right 0.25s ease',
-          'enter-from-left': 'enter-from-left 0.25s ease',
-          'exit-to-right': 'exit-to-right 0.25s ease',
-          'exit-to-left': 'exit-to-left 0.25s ease',
-          'scale-in-content': 'scale-in-content 0.2s ease',
-          'scale-out-content': 'scale-out-content 0.2s ease',
-          'fade-in': 'fade-in 0.2s ease',
-          'fade-out': 'fade-out 0.2s ease',
+          // Popper chrome
+          slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+          slideLeftAndFade: 'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+          slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+          slideRightAndFade: 'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
           // Toast
           'toast-hide': 'toast-hide 100ms ease-in forwards',
           'toast-slide-in-right': 'toast-slide-in-right 150ms cubic-bezier(0.16, 1, 0.3, 1)',
