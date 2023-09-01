95a8df74ba258af9202620d566e2d073a0ee0683
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-09-01 15:51:13 +0300

    Uncomment code

--- packages/devtools/cli/testing/multiple-profiles/generate.sh
+++ packages/devtools/cli/testing/multiple-profiles/generate.sh
@@ -6,10 +6,9 @@ OBJECTS=100;
 MUTATIONS=10000;
 
 echo "[$PROFILE]: init";
-echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx agent start)";
-# echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx reset --force)";
-# echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx halo create $PROFILE)";
-# echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx halo identity)";
-# echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx space create testSpaces)";
-# echo "[$PROFILE]: generating";
-# echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx debug generate "04" --objects=$OBJECTS --mutations=$MUTATIONS) genereated $OBJECTS objects with $MUTATIONS mutations";
+echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx reset --force)";
+echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx halo create $PROFILE)";
+echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx halo identity)";
+echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx space create testSpaces)";
+echo "[$PROFILE]: generating";
+echo "[$PROFILE]: $(DX_PROFILE=$PROFILE dx debug generate "04" --objects=$OBJECTS --mutations=$MUTATIONS) genereated $OBJECTS objects with $MUTATIONS mutations";
