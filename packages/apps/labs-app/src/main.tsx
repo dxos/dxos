31e7ec6d1db4a9ac33b3915ca7d1990c3477747f
Rich Burdon <rich.burdon@gmail.com>
2023-09-05 17:39:52 -0400

    feat: Map plugin (#4107)

--- packages/apps/labs-app/src/main.tsx
+++ packages/apps/labs-app/src/main.tsx
@@ -19,13 +19,13 @@ import { GridPlugin } from '@braneframe/plugin-grid';
 import { IntentPlugin } from '@braneframe/plugin-intent';
 import { IpfsPlugin } from '@braneframe/plugin-ipfs';
 import { KanbanPlugin } from '@braneframe/plugin-kanban';
+import { MapPlugin } from '@braneframe/plugin-map';
 import { MarkdownPlugin } from '@braneframe/plugin-markdown';
 import { PwaPlugin } from '@braneframe/plugin-pwa';
 import { SketchPlugin } from '@braneframe/plugin-sketch';
 import { SpacePlugin } from '@braneframe/plugin-space';
 import { SplitViewPlugin } from '@braneframe/plugin-splitview';
 import { StackPlugin } from '@braneframe/plugin-stack';
-import { TemplatePlugin } from '@braneframe/plugin-template';
 import { ThemePlugin } from '@braneframe/plugin-theme';
 import { ThreadPlugin } from '@braneframe/plugin-thread';
 import { TreeViewPlugin } from '@braneframe/plugin-treeview';
@@ -73,21 +73,20 @@ const main = async () => {
           SpacePlugin(),
 
           // Composer
+          FilesPlugin(),
+          GithubPlugin(),
           MarkdownPlugin(),
           StackPlugin(),
-          GithubPlugin(),
-          FilesPlugin(),
 
           // Labs
+          ChessPlugin(),
           DebugPlugin(),
           GridPlugin(),
           IpfsPlugin(),
-          TemplatePlugin(),
-          SketchPlugin(),
           KanbanPlugin(),
+          MapPlugin(),
+          SketchPlugin(),
           ThreadPlugin(),
-          ChessPlugin(),
-          TemplatePlugin(),
         ]}
       />
     </StrictMode>,
---
+++ packages/apps/plugins/plugin-map/src/components/MapMain.tsx
@@ -0,0 +1,31 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import React, { FC } from 'react';
+import { MapContainer } from 'react-leaflet';
+
+import { SpacePluginProvides } from '@braneframe/plugin-space';
+import { Main } from '@dxos/aurora';
+import { coarseBlockPaddingStart, fixedInsetFlexLayout } from '@dxos/aurora-theme';
+import { TypedObject } from '@dxos/client/echo';
+import { findPlugin, usePlugins } from '@dxos/react-surface';
+
+import { MapControl } from './MapControl';
+
+export const MapMain: FC<{ data: TypedObject }> = ({ data: object }) => {
+  const { plugins } = usePlugins();
+  const spacePlugin = findPlugin<SpacePluginProvides>(plugins, 'dxos.org/plugin/space');
+  const space = spacePlugin?.provides?.space.active;
+  if (!space) {
+    return null;
+  }
+
+  return (
+    <Main.Content classNames={[fixedInsetFlexLayout, coarseBlockPaddingStart]}>
+      <MapContainer className='flex-1 w-full h-screen border-t border-neutral-200 dark:border-neutral-800'>
+        <MapControl />
+      </MapContainer>
+    </Main.Content>
+  );
+};
