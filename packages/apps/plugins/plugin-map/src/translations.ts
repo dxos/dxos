7eeea3714159f335f9e046dcea5d80da9a549eb0
Rich Burdon <rich.burdon@gmail.com>
2023-09-05 17:39:52 -0400

    feat: Map plugin (#4107)

---
+++ packages/apps/plugins/plugin-map/src/translations.ts
@@ -0,0 +1,18 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { MAP_PLUGIN } from './types';
+
+export default [
+  {
+    'en-US': {
+      [MAP_PLUGIN]: {
+        'plugin name': 'Map',
+        'object title placeholder': 'New map',
+        'create object label': 'Create map',
+        'delete object label': 'Delete',
+      },
+    },
+  },
+];
