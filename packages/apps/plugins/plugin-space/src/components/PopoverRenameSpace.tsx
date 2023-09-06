80478113749de4d187375d4923c259569dad38c6
Will Shown <thure@users.noreply.github.com>
2023-09-06 15:54:25 -0700

    fix(aurora-theme,plugin-table): Popover polish (#4141)

--- packages/apps/plugins/plugin-space/src/components/PopoverRenameSpace.tsx
+++ packages/apps/plugins/plugin-space/src/components/PopoverRenameSpace.tsx
@@ -13,7 +13,7 @@ export const PopoverRenameSpace = ({ data: [_, space] }: { data: [string, Space]
   const { t } = useTranslation(SPACE_PLUGIN);
   // todo(thure): Why does the input value need to be uncontrolled to work?
   return (
-    <div role='none' className='flex gap-2'>
+    <div role='none' className='p-1 flex gap-2'>
       <div role='none' className='flex-1'>
         <Input.Root>
           <Input.Label srOnly>{t('space name label')}</Input.Label>
