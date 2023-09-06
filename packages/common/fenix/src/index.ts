d60467a7d1b22973f5c2e14dfdb580d782787e2c
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-09-06 17:03:20 +0300

    Add index.ts

---
+++ packages/common/fenix/src/index.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+//
+// Inspired by https://github.com/foreversd/forever
+// Their copywrite notice is included below.
+// Copyright (C) 2010 Charlie Robbins & the Contributors
+//
+
+export * from './daemon-manager';
+export * from './watchdog';
