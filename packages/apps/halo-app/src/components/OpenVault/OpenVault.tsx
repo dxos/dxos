3aa0ecaead1f614a8a9ae1e5245de910704e4b84
Josiah Witt <josiah@witt.life>
2023-09-11 18:49:07 -0400

    fix(halo-app): fix open vault button

--- packages/apps/halo-app/src/components/OpenVault/OpenVault.tsx
+++ packages/apps/halo-app/src/components/OpenVault/OpenVault.tsx
@@ -5,11 +5,11 @@
 import React from 'react';
 
 import { Button, useTranslation } from '@dxos/aurora';
-import { ShellLayout, useShell } from '@dxos/react-client';
+import { useShell } from '@dxos/react-client';
 
 export const OpenVault = () => {
   const { t } = useTranslation('halo');
   const shell = useShell();
 
-  return <Button onClick={() => shell.setLayout(ShellLayout.DEVICE_INVITATIONS)}>{t('open vault label')}</Button>;
+  return <Button onClick={() => shell.open()}>{t('open vault label')}</Button>;
 };
