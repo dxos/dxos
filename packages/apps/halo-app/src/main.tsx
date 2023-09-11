4c4e7ea7ce7ba1c4fb9792eb7cc4f240b669b7fb
Josiah Witt <josiah@witt.life>
2023-09-11 18:55:26 -0400

    refactor: introduce telemetry plugin (#4190)

--- packages/apps/halo-app/src/main.tsx
+++ packages/apps/halo-app/src/main.tsx
@@ -2,22 +2,61 @@
 // Copyright 2020 DXOS.org
 //
 
+import '@dxosTheme';
+
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import '@dxosTheme';
-import { initializeAppTelemetry } from '@dxos/react-appkit/telemetry';
-import { Config, Defaults } from '@dxos/react-client';
+import { ClientPlugin } from '@braneframe/plugin-client';
+import { ErrorPlugin } from '@braneframe/plugin-error';
+import { PwaPlugin } from '@braneframe/plugin-pwa';
+import { TelemetryPlugin } from '@braneframe/plugin-telemetry';
+import { ThemePlugin, TranslationsProvides } from '@braneframe/plugin-theme';
+import { Config, Defaults } from '@dxos/config';
+import { TypedObject } from '@dxos/echo-schema';
+import { PluginDefinition, PluginProvider } from '@dxos/react-surface';
 
-import { App } from './App';
-import { namespace } from './util';
+import { OpenVault, ProgressBar } from './components';
+import translations from './translations';
 
-void initializeAppTelemetry({ namespace, config: new Config(Defaults()) });
+// TODO(wittjosiah): This ensures that typed objects are not proxied by deepsignal. Remove.
+// https://github.com/luisherranz/deepsignal/issues/36
+(globalThis as any)[TypedObject.name] = TypedObject;
 
-const root = createRoot(document.getElementById('root')!);
-
-root.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PluginProvider
+      fallback={
+        <div className='flex h-screen justify-center items-center'>
+          <ProgressBar indeterminate />
+        </div>
+      }
+      plugins={[
+        TelemetryPlugin({ namespace: 'composer-app', config: new Config(Defaults()) }),
+        ThemePlugin({ appName: 'HALO' }),
+        // Outside of error boundary so that updates are not blocked by errors.
+        PwaPlugin(),
+        // Inside theme provider so that errors are styled.
+        ErrorPlugin(),
+        ClientPlugin(),
+        {
+          meta: {
+            id: 'dxos.org/plugin/halo-app',
+          },
+          provides: {
+            translations,
+            components: {
+              default: () => {
+                return (
+                  <div className='flex h-screen justify-center items-center'>
+                    <OpenVault />
+                  </div>
+                );
+              },
+            },
+          },
+        } as PluginDefinition<TranslationsProvides>,
+      ]}
+    />
   </StrictMode>,
 );
