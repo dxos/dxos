b354a47e6537deb4c11e0156819db6f91f56d6d5
Josiah Witt <josiah@witt.life>
2023-09-11 18:55:26 -0400

    refactor: introduce telemetry plugin (#4190)

--- packages/apps/halo-app/src/components/ProgressBar/index.ts
+++ packages/apps/halo-app/src/components/ProgressBar/index.ts
@@ -2,5 +2,4 @@
 // Copyright 2023 DXOS.org
 //
 
-export * from './OpenVault';
 export * from './ProgressBar';
---
+++ packages/apps/halo-app/src/translations.ts
@@ -0,0 +1,13 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+export default [
+  {
+    'en-US': {
+      halo: {
+        'open vault label': 'Open Vault',
+      },
+    },
+  },
+];
