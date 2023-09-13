6df38a964d96d5aa9706660ef3e33cad19a361b4
Josiah Witt <josiah@witt.life>
2023-09-13 15:14:18 -0400

    chore(vault): update deps (#4235)

--- packages/apps/plugins/plugin-telemetry/src/telemetry/index.ts
+++ packages/apps/plugins/plugin-telemetry/src/telemetry/index.ts
@@ -3,6 +3,9 @@
 //
 
 // NOTE: Telemetry is exported separately so that it can be used without pulling in React.
+// TODO(wittjosiah): Factor out, this package has other surface peer dependencies that end up
+//   needing to be installed in the consuming package even if they aren't needed.
+//   This is mostly here so that it's not in appkit.
 
 export * from './listeners';
 export * from './telemetry';
