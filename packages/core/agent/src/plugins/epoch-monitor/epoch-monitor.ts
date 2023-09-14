ac97ad0389b99b47abfb3a3324deffd9cf7d1337
Dmytro Maretskyi <maretskii@gmail.com>
2023-09-14 16:58:53 +0200

    remove monitor flag

--- packages/core/agent/src/plugins/epoch-monitor/epoch-monitor.ts
+++ packages/core/agent/src/plugins/epoch-monitor/epoch-monitor.ts
@@ -2,16 +2,16 @@
 // Copyright 2023 DXOS.org
 //
 
+import { scheduleTask } from '@dxos/async';
 import { Client, PublicKey } from '@dxos/client';
 import { Space } from '@dxos/client-protocol';
+import { Context } from '@dxos/context';
 import { checkCredentialType } from '@dxos/credentials';
 import { invariant } from '@dxos/invariant';
 import { log } from '@dxos/log';
 import { Runtime } from '@dxos/protocols/proto/dxos/config';
 import { ComplexMap } from '@dxos/util';
 
-import { scheduleTask } from '@dxos/async';
-import { Context } from '@dxos/context';
 import { AbstractPlugin } from '../plugin';
 
 type Options = Required<Runtime.Agent.Plugins.EpochMontior>;
@@ -21,8 +21,8 @@ const DEFAULT_OPTIONS: Options = {
   minMessagesBetweenEpochs: 10_000,
   minTimeBetweenEpochs: 120_000,
   minInactivityBeforeEpoch: 30_000,
-  maxInactivityDelay: 60_000
-}
+  maxInactivityDelay: 60_000,
+};
 
 /**
  * Pipeline monitor:
@@ -31,25 +31,25 @@ const DEFAULT_OPTIONS: Options = {
  */
 // TODO(burdon): Create test.
 export class EpochMonitor extends AbstractPlugin {
-  private _ctx = new Context()
+  private _ctx = new Context();
   private _monitors = new ComplexMap<PublicKey, SpaceMonitor>(PublicKey.hash);
 
   private _options?: Options = undefined;
-  
+
   /**
    * Monitor spaces for which the agent is the leader.
-  */
- async open() {
+   */
+  async open() {
     invariant(this._client);
 
     const config = this._client.config.values.runtime?.agent?.plugins?.epochMonitor;
-    if(!config) {
-      log.info('epoch monitor disabled from config')
+    if (!config || config.enabled === false) {
+      log.info('epoch monitor disabled from config');
       return;
     }
 
     this._options = { ...DEFAULT_OPTIONS, ...config };
-    log.info('epoch monitor open', { options: this._options })
+    log.info('epoch monitor open', { options: this._options });
 
     const process = (spaces: Space[]) => {
       spaces.forEach(async (space) => {
@@ -58,7 +58,7 @@ export class EpochMonitor extends AbstractPlugin {
           const monitor = new SpaceMonitor(this._client!, space, this._options);
           this._monitors.set(space.key, monitor);
 
-          log.info('init', { space: space.key, isOpen: space.isOpen })
+          log.info('init', { space: space.key, isOpen: space.isOpen });
 
           // Process asynchronously.
           scheduleTask(this._ctx, async () => {
@@ -66,7 +66,7 @@ export class EpochMonitor extends AbstractPlugin {
           });
         }
       });
-    }
+    };
 
     const sub = this._client.spaces.subscribe(process);
     process(this._client.spaces.get());
@@ -74,7 +74,7 @@ export class EpochMonitor extends AbstractPlugin {
   }
 
   async close() {
-    this._ctx.dispose();
+    await this._ctx.dispose();
     this._monitors.forEach((monitor) => {
       void monitor.close();
     });
@@ -94,14 +94,18 @@ class SpaceMonitor {
     private readonly _client: Client,
     private readonly _space: Space,
     private readonly _options: Options,
-  ) { }
+  ) {}
 
   async open() {
     await this._space.waitUntilReady();
 
     // Monitor spaces owned by this agent.
     if (!this._client!.halo.identity.get()!.identityKey.equals(this._space.internal.data.creator!)) {
-      log.info('space is not owned by this agent', { key: this._space.key, creator: this._space.internal.data.creator, identityKey: this._client!.halo.identity.get()!.identityKey })
+      log.info('space is not owned by this agent', {
+        key: this._space.key,
+        creator: this._space.internal.data.creator,
+        identityKey: this._client!.halo.identity.get()!.identityKey,
+      });
       return;
     }
 
@@ -109,8 +113,16 @@ class SpaceMonitor {
 
     const sub = this._space.pipeline.subscribe(async (pipeline) => {
       // Cancel creation if base epoch has changed.
-      if(this._maxTimeoutTask !== undefined && pipeline.currentEpoch && this._previousEpochNumber !== pipeline.currentEpoch.subject.assertion.number) {
-        log.info('epoch changed, cancelling epoch creation', { key: this._space.key, previousEpochNumber: this._previousEpochNumber, currentEpochNumber: pipeline.currentEpoch.subject.assertion.number });
+      if (
+        this._maxTimeoutTask !== undefined &&
+        pipeline.currentEpoch &&
+        this._previousEpochNumber !== pipeline.currentEpoch.subject.assertion.number
+      ) {
+        log.info('epoch changed, cancelling epoch creation', {
+          key: this._space.key,
+          previousEpochNumber: this._previousEpochNumber,
+          currentEpochNumber: pipeline.currentEpoch.subject.assertion.number,
+        });
         clearTimeout(this._maxTimeoutTask!);
         this._maxTimeoutTask = undefined;
         clearTimeout(this._epochCreationTask!);
@@ -128,10 +140,13 @@ class SpaceMonitor {
       // TODO(burdon): Rather than total messages, implement inequality in timeframe?
       invariant(checkCredentialType(pipeline.currentEpoch!, 'dxos.halo.credentials.Epoch'));
 
-      const newMessages = pipeline.currentDataTimeframe!.newMessages(pipeline.currentEpoch.subject.assertion.timeframe)
+      const newMessages = pipeline.currentDataTimeframe!.newMessages(pipeline.currentEpoch.subject.assertion.timeframe);
       const timeSinceLastEpoch = Date.now() - pipeline.currentEpoch.issuanceDate.getTime();
 
-      if (newMessages > this._options.minMessagesBetweenEpochs && timeSinceLastEpoch > this._options.minTimeBetweenEpochs) {
+      if (
+        newMessages > this._options.minMessagesBetweenEpochs &&
+        timeSinceLastEpoch > this._options.minTimeBetweenEpochs
+      ) {
         if (!this._maxTimeoutTask) {
           log.info('wanting to create epoch', { key: this._space.key, options: this._options });
           this._previousEpochNumber = pipeline.currentEpoch.subject.assertion.number;
@@ -141,7 +156,6 @@ class SpaceMonitor {
           clearTimeout(this._epochCreationTask);
         }
         this._epochCreationTask = setTimeout(this._createEpoch.bind(this), this._options.minInactivityBeforeEpoch);
-
       }
     });
     this._ctx.onDispose(() => sub.unsubscribe());
@@ -163,7 +177,7 @@ class SpaceMonitor {
     try {
       log.info('creating epoch', { key: this._space.key });
       await this._space.internal.createEpoch();
-      log.info('epoch created')
+      log.info('epoch created');
     } catch (e) {
       log.catch(e);
     } finally {
