6a746acc351d7ae4e50446fca910d07d42b07e61
Josiah Witt <josiah@witt.life>
2023-09-15 10:02:14 -0400

    test: add bare template monitor

---
+++ tools/monitors/bare-template-monitor/scripts/setup.js
@@ -0,0 +1,27 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+const { exec: execCb } = require('node:child_process');
+const { rm } = require('node:fs/promises');
+const { promisify } = require('node:util');
+
+const exec = promisify(execCb);
+
+const setup = async () => {
+  console.log('Cleaning up...');
+  await rm('tmp', { recursive: true });
+
+  console.log('Installing dependencies...');
+  await exec('npm install --no-package-lock');
+
+  console.log('Creating app...');
+  await exec('dx app create --template bare tmp');
+
+  console.log('Installing app dependencies...');
+  await exec('npm install --no-package-lock', {
+    cwd: 'tmp',
+  });
+};
+
+void setup();
