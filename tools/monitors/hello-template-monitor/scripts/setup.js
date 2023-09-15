ba8fa9e6848bb7931a609d3648c8c3ebbb74c2fb
Josiah Witt <josiah@witt.life>
2023-09-15 19:12:32 -0400

    test: update template monitor setup (#4258)

--- tools/monitors/hello-template-monitor/scripts/setup.js
+++ tools/monitors/hello-template-monitor/scripts/setup.js
@@ -17,9 +17,10 @@ const setup = async () => {
 
   console.log('Installing dependencies...');
   await exec('npm install --no-package-lock');
+  await exec('npm exec playwright install');
 
   console.log('Creating app...');
-  await exec('dx app create tmp');
+  await exec('npm exec dx app create tmp');
 
   console.log('Installing app dependencies...');
   await exec('npm install --no-package-lock', {
