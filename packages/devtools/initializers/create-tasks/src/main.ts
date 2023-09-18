d11dea306c87395fc9741eaa9461cde33ae13c11
Josiah Witt <josiah@witt.life>
2023-09-18 13:44:50 -0400

    fix: initializers output directory

--- packages/devtools/initializers/create-tasks/src/main.ts
+++ packages/devtools/initializers/create-tasks/src/main.ts
@@ -3,13 +3,9 @@
 //
 
 import minimist from 'minimist';
-import { exec as execCb } from 'node:child_process';
 import { mkdir } from 'node:fs/promises';
-import { promisify } from 'node:util';
 
 import template from '@dxos/tasks-template';
-
-const exec = promisify(execCb);
 
 void (async () => {
   const args = minimist(process.argv.slice(2), {
@@ -34,11 +30,7 @@ void (async () => {
     interactive,
     input: interactive
       ? { monorepo }
-      : { name, pwa: false, dxosUi: true, tailwind: true, react: true, monorepo, storybook: false },
+      : { name, createFolder: false, dxosUi: true, tailwind: true, react: true, monorepo, storybook: false },
   });
   await result.apply();
-
-  // TODO(wittjosiah): Move this functionality into plate.
-  console.log('Installing dependencies...');
-  await exec('npm install');
 })();
