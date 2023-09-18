1c76085c364a24a7c259ff0c156df2a3ecd6ed4d
Zhenya <zhenyasav@users.noreply.github.com>
2023-09-18 06:01:13 -0400

    feat: fix tasks app (#4248)
    Co-authored-by: Jess Martin <jessmartin@gmail.com>

---
+++ packages/apps/templates/tasks-template/src/template.t.ts
@@ -0,0 +1,9 @@
+import path from 'path';
+import { interactiveDirectory } from '@dxos/plate';
+import inherits from '@dxos/bare-template';
+
+export default interactiveDirectory({
+  src: __filename.endsWith('.ts') ? __dirname : path.resolve(__dirname, '../../src'),
+  inputShape: inherits.inputShape,
+  inherits: (context) => inherits.apply(context),
+});
