7e1f8f448f027b01cd702e7d5b0b6b460aa98d6b
Josiah Witt <josiah@witt.life>
2023-09-18 13:11:53 -0400

    fix(create): default to interactive if no name provided

--- packages/devtools/initializers/create-bare/src/main.ts
+++ packages/devtools/initializers/create-bare/src/main.ts
@@ -1,21 +1,44 @@
 //
 // Copyright 2023 DXOS.org
 //
+
 import minimist from 'minimist';
+import { exec as execCb } from 'node:child_process';
+import { mkdir } from 'node:fs/promises';
+import { promisify } from 'node:util';
 
 import template from '@dxos/bare-template';
+
+const exec = promisify(execCb);
 
 void (async () => {
   const args = minimist(process.argv.slice(2), {
     boolean: ['interactive', 'verbose', 'monorepo'],
   });
-  const { monorepo, interactive, verbose } = { interactive: false, verbose: false, monorepo: false, ...args };
+
+  const name = args._[0];
+  if (name) {
+    await mkdir(name);
+    process.chdir(name);
+  }
+
+  const { monorepo, interactive, verbose } = {
+    interactive: !name,
+    verbose: false,
+    monorepo: false,
+    ...args,
+  };
+
   const result = await template.apply({
     verbose,
     interactive,
     input: interactive
       ? { monorepo }
-      : { pwa: true, dxosUi: true, tailwind: true, react: true, monorepo, storybook: false },
+      : { name, pwa: false, dxosUi: true, tailwind: true, react: true, monorepo, storybook: false },
   });
   await result.apply();
+
+  // TODO(wittjosiah): Move this functionality into plate.
+  console.log('Installing dependencies...');
+  await exec('npm install');
 })();
--- packages/devtools/initializers/create/src/main.ts
+++ packages/devtools/initializers/create/src/main.ts
@@ -3,27 +3,27 @@
 //
 
 import minimist from 'minimist';
-import { exec } from 'node:child_process';
+import { exec as execCb } from 'node:child_process';
 import { mkdir } from 'node:fs/promises';
-import path from 'node:path';
+import { promisify } from 'node:util';
 
 import template from '@dxos/hello-template';
+
+const exec = promisify(execCb);
 
 void (async () => {
   const args = minimist(process.argv.slice(2), {
     boolean: ['interactive', 'verbose', 'monorepo'],
   });
 
-  const currentDir = path.basename(process.cwd());
-  const target = args._[0];
-  if (target) {
-    await mkdir(target);
-    process.chdir(target);
+  const name = args._[0];
+  if (name) {
+    await mkdir(name);
+    process.chdir(name);
   }
 
-  const name = target ?? currentDir;
   const { monorepo, interactive, verbose } = {
-    interactive: false,
+    interactive: !name,
     verbose: false,
     monorepo: false,
     ...args,
@@ -31,9 +31,10 @@ void (async () => {
 
   const result = await template.apply({
     verbose,
+    interactive,
     input: interactive
       ? { monorepo }
-      : { name, pwa: true, dxosUi: true, tailwind: true, react: true, monorepo, storybook: false },
+      : { name, pwa: false, dxosUi: true, tailwind: true, react: true, monorepo, storybook: false },
   });
   await result.apply();
 
