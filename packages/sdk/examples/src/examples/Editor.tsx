f0a79123d1c3bba527b613f1e6f0de58e2490fcc
Josiah Witt <josiah@witt.life>
2023-09-19 07:00:50 -0400

    docs: add editor example (#4274)

---
+++ packages/sdk/examples/src/examples/Editor.tsx
@@ -0,0 +1,46 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import React from 'react';
+
+import { Document } from '@braneframe/types';
+import { MarkdownComposer, useTextModel } from '@dxos/aurora-composer';
+import type { PublicKey } from '@dxos/client';
+import { useQuery, useSpace } from '@dxos/react-client/echo';
+import { useIdentity } from '@dxos/react-client/halo';
+
+const Editor = ({ spaceKey, id }: { spaceKey: PublicKey; id: number }) => {
+  const identity = useIdentity();
+  const space = useSpace(spaceKey);
+  const [doc] = useQuery(space, Document.filter());
+
+  const model = useTextModel({ identity, space, text: doc?.content });
+
+  return (
+    <main className='flex-1 min-w-0'>
+      <MarkdownComposer
+        model={model}
+        slots={{
+          editor: {
+            // TODO(wittjosiah): Copied from plugin-markdown.
+            //   Without this the cursors are cut off at the edges.
+            //   These should be the defaults.
+            markdownTheme: {
+              '&, & .cm-scroller': {
+                display: 'flex',
+                flexDirection: 'column',
+                flex: '1 0 auto',
+                inlineSize: '100%',
+              },
+              '& .cm-content': { flex: '1 0 auto', inlineSize: '100%', paddingBlock: '1rem' },
+              '& .cm-line': { paddingInline: '1rem' },
+            },
+          },
+        }}
+      />
+    </main>
+  );
+};
+
+export default Editor;
--- packages/sdk/examples/src/stories/examples.stories.tsx
+++ packages/sdk/examples/src/stories/examples.stories.tsx
@@ -5,17 +5,30 @@
 import React from 'react';
 
 import '@dxosTheme';
+import { Document } from '@braneframe/types';
 import { ClientDecorator, setupPeersInSpace, ToggleNetworkDecorator } from '@dxos/react-client/testing';
 
-import { TaskListExample } from '../examples';
+import { EditorExample, TaskListExample } from '../examples';
 
 export default {
   title: 'DXOS Examples',
 };
 
-const { spaceKey, clients } = await setupPeersInSpace({ count: 2 });
+const tasksList = await setupPeersInSpace({ count: 2 });
 
 export const TaskList = {
-  render: (args: { id: number }) => <TaskListExample {...args} spaceKey={spaceKey} />,
-  decorators: [ClientDecorator({ clients }), ToggleNetworkDecorator({ clients })],
+  render: (args: { id: number }) => <TaskListExample {...args} spaceKey={tasksList.spaceKey} />,
+  decorators: [ClientDecorator({ clients: tasksList.clients }), ToggleNetworkDecorator({ clients: tasksList.clients })],
+};
+
+const editor = await setupPeersInSpace({
+  count: 2,
+  onCreateSpace: (space) => {
+    space.db.add(new Document());
+  },
+});
+
+export const Editor = {
+  render: (args: { id: number }) => <EditorExample {...args} spaceKey={editor.spaceKey} />,
+  decorators: [ClientDecorator({ clients: editor.clients }), ToggleNetworkDecorator({ clients: editor.clients })],
 };
