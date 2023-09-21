e3d82293282a3e6ac90b5bb8655de7cecf6c8e7a
Zhenya <zhenya@braneframe.com>
2023-09-21 05:25:30 -0700

    fix initializers

--- packages/apps/templates/tasks-template/src/template.t.ts
+++ packages/apps/templates/tasks-template/src/template.t.ts
@@ -4,8 +4,9 @@ import inherits from '@dxos/bare-template';
 
 export default interactiveDirectory({
   src: __filename.endsWith('.ts') ? __dirname : path.resolve(__dirname, '../../src'),
-  inherits: (context) => inherits.apply({ ...context, input: { ...context.input, proto: true } }),
+  inherits: (context) => inherits.apply({ ...context, input: { ...context.input, proto: true, createFolder: false } }),
   inputShape: inherits.inputShape,
+  options: inherits.options.options,
   defaultInput: {
     proto: true,
   },
