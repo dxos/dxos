715c8d6bd95285815422ddc792deea7f572461b0
Josiah Witt <josiah@witt.life>
2023-10-02 12:40:16 -0400

    feat: implement tree

--- packages/ui/aurora-grid/src/components/Tree/Tree.stories.tsx
+++ packages/ui/aurora-grid/src/components/Tree/Tree.stories.tsx
@@ -8,10 +8,46 @@ import { faker } from '@faker-js/faker';
 import React from 'react';
 
 import { Tree } from './Tree';
-import { MosaicContextProvider } from '../../dnd';
-import { FullscreenDecorator } from '../../testing';
+import { useSortedItems } from '../../dnd';
+import { FullscreenDecorator, MosaicDecorator } from '../../testing';
 
 faker.seed(3);
+
+const testItems = [
+  {
+    id: 'Home',
+    children: [],
+  },
+  {
+    id: 'Collections',
+    children: [
+      { id: 'Spring', children: [] },
+      { id: 'Summer', children: [] },
+      { id: 'Fall', children: [] },
+      { id: 'Winter', children: [] },
+    ],
+  },
+  {
+    id: 'About Us',
+    children: [],
+  },
+  {
+    id: 'My Account',
+    children: [
+      { id: 'Addresses', children: [] },
+      {
+        id: 'Order History',
+        children: [
+          { id: 'Order 1', children: [] },
+          { id: 'Order 2', children: [] },
+          { id: 'Order 3', children: [] },
+        ],
+      },
+      { id: 'Payment Methods', children: [] },
+      { id: 'Account Details', children: [] },
+    ],
+  },
+];
 
 export default {
   component: Tree,
@@ -21,10 +57,19 @@ export default {
   },
 };
 
-export const Default = () => {
-  return (
-    <MosaicContextProvider debug>
-      <Tree.Root id='tree' />
-    </MosaicContextProvider>
-  );
+export const Default = {
+  render: () => {
+    const sortedItems = useSortedItems('tree', testItems);
+
+    return (
+      <Tree.Root id='tree' items={sortedItems.map(({ id }) => id)} onMoveItem={console.log}>
+        <div className='flex flex-col'>
+          {sortedItems.map((item, i) => (
+            <Tree.Tile key={item.id} item={item} index={i} />
+          ))}
+        </div>
+      </Tree.Root>
+    );
+  },
+  decorators: [MosaicDecorator],
 };
--- packages/ui/aurora-grid/src/components/Tree/Tree.tsx
+++ packages/ui/aurora-grid/src/components/Tree/Tree.tsx
@@ -2,33 +2,113 @@
 // Copyright 2023 DXOS.org
 //
 
-import React from 'react';
+import { SortableContext, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import React, { PropsWithChildren, forwardRef } from 'react';
+
+import { Tree as AuroraTree, TreeItem as AuroraTreeItem } from '@dxos/aurora';
+import { mx } from '@dxos/aurora-theme';
+
+import {
+  MosaicContainerProps,
+  MosaicContainerProvider,
+  MosaicDataItem,
+  MosaicDraggedItem,
+  MosaicTileComponent,
+  useContainer,
+  useSortedItems,
+} from '../../dnd';
 
 // TODO(burdon): Tree data model that provides a pure abstraction of the plugin Graph.
 //  - The Tree (like Stack, Grid) is a high level container that assembles Radix style Aurora components from a model.
 //  - Models in general should be easily mapped from the Graph and/or ECHO queries.
 //  - See: https://master--5fc05e08a4a65d0021ae0bf2.chromatic.com/?path=/story/examples-tree-sortable--basic-setup
 
-type TreeRootProps = {
-  id: string;
+type TreeRootProps = MosaicContainerProps<any, number> & {
+  items?: MosaicDataItem[];
 };
 
-const TreeRoot = ({ id }: TreeRootProps) => {
-  return <div />;
+const TreeRoot = ({ id, items = [], Component = TreeItem, onMoveItem, children }: PropsWithChildren<TreeRootProps>) => {
+  return (
+    <AuroraTree.Root>
+      <SortableContext id={id} items={items} strategy={verticalListSortingStrategy}>
+        <MosaicContainerProvider container={{ id, Component, onMoveItem }}>
+          {children}
+          {/* TODO(burdon): Component for placeholder at end. */}
+        </MosaicContainerProvider>
+      </SortableContext>
+    </AuroraTree.Root>
+  );
 };
 
 // TODO(burdon): Draggable item.
-// const TreeTile = () => {
-//   return <div />;
-// };
+const TreeTile = ({ item, index, onSelect }: { item: MosaicDataItem; index: number; onSelect?: () => void }) => {
+  const { id: container, Component } = useContainer();
+  const { setNodeRef, attributes, listeners, transform, isDragging } = useSortable({
+    id: item.id,
+    data: { container, item, position: index } satisfies MosaicDraggedItem,
+  });
 
-// TODO(burdon): Pure component that is used by the mosaic overlay.
-// const TreeItem = () => {
-//   return <div />;
-// };
+  return (
+    <AuroraTreeItem.Root collapsible defaultOpen>
+      <Component
+        ref={setNodeRef}
+        isDragging={isDragging}
+        draggableStyle={{
+          transform: transform ? CSS.Transform.toString(Object.assign(transform, { scaleY: 1 })) : undefined,
+        }}
+        draggableProps={{ ...attributes, ...listeners }}
+        className={mx(isDragging && 'opacity-30')}
+        data={item}
+        onSelect={onSelect}
+      />
+    </AuroraTreeItem.Root>
+  );
+};
+
+type TreeData = {
+  id: string;
+  children?: TreeData[];
+};
+
+/**
+ * Pure component that is used by the mosaic overlay.
+ */
+const TreeItem: MosaicTileComponent<TreeData> = forwardRef(
+  ({ draggableStyle, draggableProps, data, isActive, className }, forwardedRef) => {
+    return (
+      <div
+        ref={forwardedRef}
+        style={draggableStyle}
+        className={mx('flex flex-col m-2 p-2 ring bg-white font-mono text-xs', className)}
+        {...draggableProps}
+      >
+        {data.id}
+        {!isActive && data.children && <TreeBranch id={data.id} items={data.children} />}
+      </div>
+    );
+  },
+);
+
+const TreeBranch = ({ id, items }: { id: string; items: TreeData[] }) => {
+  const { Component, onMoveItem } = useContainer();
+  const sortedItems = useSortedItems(id, items);
+  return (
+    <AuroraTreeItem.Body>
+      <SortableContext id={id} items={sortedItems} strategy={verticalListSortingStrategy}>
+        <MosaicContainerProvider container={{ id, Component, onMoveItem }}>
+          {sortedItems.map((child, i) => (
+            <AuroraTree.Branch key={child.id}>
+              <TreeTile item={child} index={i} />
+            </AuroraTree.Branch>
+          ))}
+        </MosaicContainerProvider>
+      </SortableContext>
+    </AuroraTreeItem.Body>
+  );
+};
 
 export const Tree = {
   Root: TreeRoot,
+  Tile: TreeTile,
 };
-
-export type { TreeRootProps };
