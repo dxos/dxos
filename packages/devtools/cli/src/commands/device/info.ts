dee018b7dc085a39e971cf3d8bfde31c21c8c539
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-10-05 17:39:48 +0300

    Rename default device command to info command

--- packages/devtools/cli/src/commands/device/info.ts
+++ packages/devtools/cli/src/commands/device/info.ts
@@ -2,23 +2,31 @@
 // Copyright 2022 DXOS.org
 //
 
+import { sleep } from '@dxos/async';
 import { Client } from '@dxos/client';
 
 import { BaseCommand } from '../../base-command';
+import { printDevices } from '../../util';
 
-export default class Device extends BaseCommand<typeof Device> {
+export default class Info extends BaseCommand<typeof Info> {
   static override enableJsonFlag = true;
   static override description = 'Show device info.';
 
   async run(): Promise<any> {
     return await this.execWithClient(async (client: Client) => {
+      // TODO(mykola): Hack to wait for identity with `client.halo.identity.wait()`.
+      await client.spaces.isReady.wait();
       const device = client.halo.device;
+
       if (!device) {
         this.log('No device found.');
         return;
       }
 
-      this.logToStderr('Device key:', device.deviceKey.toHex());
+      if (!this.flags.json) {
+        printDevices([device], this.flags);
+      }
+
       return device;
     });
   }
