11884fafdac1916bf7bd9c42132c0e847dab0457
Rich Burdon <rich.burdon@gmail.com>
2023-10-16 13:37:34 +0100

    feat: Presenter plugin. (#4431)

---
+++ packages/apps/plugins/plugin-presenter/src/PresenterPlugin.tsx
@@ -0,0 +1,107 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { Presentation } from '@phosphor-icons/react';
+import { deepSignal } from 'deepsignal';
+import React from 'react';
+
+import { isMarkdownContent } from '@braneframe/plugin-markdown';
+import { SPLITVIEW_PLUGIN, SplitViewAction } from '@braneframe/plugin-splitview';
+import { isStack } from '@braneframe/plugin-stack';
+import { type PluginDefinition } from '@dxos/react-surface';
+
+import { PresenterMain, MarkdownSlideMain } from './components';
+import translations from './translations';
+import { PRESENTER_PLUGIN, PresenterContext, type PresenterPluginProvides } from './types';
+
+// TODO(burdon): Only scale markdown content.
+// TODO(burdon): Map stack content; Slide content type (e.g., markdown, sketch, IPFS image, table, etc.)
+
+type PresenterState = {
+  presenting: boolean;
+};
+
+export const PresenterPlugin = (): PluginDefinition<PresenterPluginProvides> => {
+  // TODO(burdon): Do we need context providers if we can get the state from the plugin?
+  const state = deepSignal<PresenterState>({ presenting: false });
+
+  return {
+    meta: {
+      id: PRESENTER_PLUGIN,
+    },
+    provides: {
+      translations,
+      graph: {
+        withPlugins: (plugins) => (parent) => {
+          if (isStack(parent.data)) {
+            parent.addAction({
+              id: 'toggle-presentation',
+              label: ['toggle presentation label', { ns: PRESENTER_PLUGIN }],
+              icon: (props) => <Presentation {...props} />,
+              // TODO(burdon): Allow function so can generate state when activated.
+              //  So can set explicit fullscreen state coordinated with current presenter state.
+              intent: [
+                {
+                  plugin: PRESENTER_PLUGIN,
+                  action: 'toggle-presentation',
+                },
+                {
+                  plugin: SPLITVIEW_PLUGIN,
+                  action: SplitViewAction.TOGGLE_FULLSCREEN,
+                },
+              ],
+              keyBinding: 'shift+meta+p',
+            });
+          }
+        },
+      },
+      context: ({ children }) => {
+        return (
+          <PresenterContext.Provider
+            value={{
+              running: state.presenting,
+              start: () => (state.presenting = true),
+              stop: () => (state.presenting = false),
+            }}
+          >
+            {children}
+          </PresenterContext.Provider>
+        );
+      },
+      component: (data, role) => {
+        if (!data || typeof data !== 'object') {
+          return null;
+        }
+
+        switch (role) {
+          case 'main': {
+            if (isStack(data) && state.presenting) {
+              return PresenterMain;
+            }
+            break;
+          }
+
+          case 'presenter-slide': {
+            if (isMarkdownContent(data)) {
+              return MarkdownSlideMain;
+            }
+            break;
+          }
+        }
+
+        return null;
+      },
+      intent: {
+        resolver: (intent) => {
+          switch (intent.action) {
+            case 'toggle-presentation': {
+              state.presenting = intent.data?.state ?? !state.presenting;
+              break;
+            }
+          }
+        },
+      },
+    },
+  };
+};
---
+++ packages/apps/plugins/plugin-presenter/src/components/Markdown/Container.stories.tsx
@@ -0,0 +1,64 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import '@dxosTheme';
+
+import type { DecoratorFunction } from '@storybook/csf';
+import type { ReactRenderer } from '@storybook/react';
+import React, { type FC } from 'react';
+
+import { mx } from '@dxos/aurora-theme';
+
+import { Container } from './Container';
+import { Slide } from './Slide';
+import { createSlide } from '../../testing';
+
+// TODO(burdon): Factor out.
+const FullscreenDecorator = (className?: string): DecoratorFunction<ReactRenderer, any> => {
+  return (Story) => (
+    <div className={mx('flex fixed inset-0 overflow-hidden', className)}>
+      <Story />
+    </div>
+  );
+};
+
+const Story: FC<{ content: string }> = ({ content }) => {
+  return (
+    <Container className='bg-neutral-200'>
+      <Slide content={content} />
+    </Container>
+  );
+};
+
+export default {
+  component: Story,
+  decorators: [FullscreenDecorator()],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {
+  args: {
+    content: createSlide({ number: 1 }),
+  },
+};
+
+export const Code = {
+  args: {
+    content: createSlide({ code: true }),
+  },
+};
+
+export const List = {
+  args: {
+    content: createSlide({ list: 3 }),
+  },
+};
+
+export const Ordered = {
+  args: {
+    content: createSlide({ ordered: 4 }),
+  },
+};
