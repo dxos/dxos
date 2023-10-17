3002fdafa76ee0628ca914b083dd136c4b4d5554
Josiah Witt <josiah@witt.life>
2023-10-17 13:45:22 -0400

    fix: tasks and todomvc examples (#4424)
    Co-authored-by: Zhenya <zhenya@braneframe.com>
    Co-authored-by: Jess Martin <jessmartin@gmail.com>

--- docs/docs/guide/react/snippets/use-query-typed.tsx
+++ docs/docs/guide/react/snippets/use-query-typed.tsx
@@ -4,26 +4,35 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
 import { ClientProvider } from '@dxos/react-client';
 import { useQuery, useSpaces } from '@dxos/react-client/echo';
 import { useIdentity } from '@dxos/react-client/halo';
 
-import { Task } from './schema';
+import { Task, types } from './schema';
 
 export const App = () => {
   useIdentity();
   const [space] = useSpaces();
-  const tasks = useQuery<Task>(space, Task.filter());
-  return <>
-    {tasks.map((task) => (
-      <div key={task.id}>{task.title} - {task.completed}</div>
-    ))}
-  </>;
+  const tasks: Task[] = useQuery(space, Task.filter());
+  return (
+    <>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          {task.title} - {task.completed}
+        </div>
+      ))}
+    </>
+  );
 };
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
-  <ClientProvider>
+  <ClientProvider
+    onInitialized={async (client) => {
+      client.addSchema(types);
+    }}
+  >
     <App />
-  </ClientProvider>
+  </ClientProvider>,
 );
