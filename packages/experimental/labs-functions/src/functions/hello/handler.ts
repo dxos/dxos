1bf054405a09a5efd3b97f86d27807dd5665aed9
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2023-10-17 17:11:21 +0300

    feat(functions): Revive functions experiment (#4442)

--- packages/experimental/labs-functions/src/functions/hello/handler.ts
+++ packages/experimental/labs-functions/src/functions/hello/handler.ts
@@ -2,7 +2,7 @@
 // Copyright 2023 DXOS.org
 //
 
-import { FunctionContext } from '@dxos/functions';
+import { type FunctionContext } from '@dxos/functions';
 
 export default async (event: any, context: FunctionContext) => {
   const identity = context.client.halo.identity.get();
--- packages/experimental/labs-functions/src/index.ts
+++ packages/experimental/labs-functions/src/index.ts
@@ -2,5 +2,4 @@
 // Copyright 2023 DXOS.org
 //
 
-export * from './bots';
 export * from './functions';
