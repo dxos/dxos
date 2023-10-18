7938a0efe83ed5a8b7c8785deaafed9e6911460a
Rich Burdon <rich.burdon@gmail.com>
2023-10-18 15:21:47 +0100

    feat: Fix presenter theme (#4452)

--- packages/apps/plugins/plugin-presenter/src/components/Markdown/theme.ts
+++ packages/apps/plugins/plugin-presenter/src/components/Markdown/theme.ts
@@ -20,7 +20,7 @@ export const theme = {
     ol: 'my-[16px] ml-24 leading-relaxed list-decimal',
     li: 'pl-6 text-[48px]',
 
-    pre: 'w-full mx-0 my-[32px] p-0 __border-l-[16px] bg-neutral-50/50 p-4 whitespace-pre-line',
+    pre: 'w-full mx-0 my-[32px] p-0 __border-l-[16px] bg-neutral-50/50 p-4 __whitespace-pre-line',
     code: 'p-0 text-[40px]',
   },
 };
