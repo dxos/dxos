c9d6a09377aa666e807f150ece72926d5aaea286
Josiah Witt <josiah@witt.life>
2023-10-24 17:01:59 -0400

    ci: improve report

--- .circleci/scripts/send-test-report.sh
+++ .circleci/scripts/send-test-report.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
+# ROLE_ID=946536498432471080
+ROLE_ID=332330209858813952
 GREEN=4783872
 RED=16711680
-YELLOW=16776960
 
 function notifySuccess() {
-  if [ -z "$DX_DISCORD_WEBHOOK_URL" ]; then return; fi
-  MESSAGE='{ "content": "<@332330209858813952>", "embeds": [{ "title": "Daily testing successful", "description": "'$CIRCLE_BUILD_URL'", "color": '$GREEN' }] }'
+  if [ -z "$DISCORD_TEST_REPORT_WEBHOOK" ]; then return; fi
+  MESSAGE='{ "content": "<@'$ROLE_ID'>", "embeds": [{ "title": "Daily testing successful: '$1'", "description": "'$CIRCLE_BUILD_URL'", "color": '$GREEN' }] }'
   echo $MESSAGE
-  curl -H "Content-Type: application/json" -d "${MESSAGE}" $DX_DISCORD_WEBHOOK_URL
+  curl -H "Content-Type: application/json" -d "${MESSAGE}" $DISCORD_TEST_REPORT_WEBHOOK
 }
 
 function notifyFailure() {
-  if [ -z "$DX_DISCORD_WEBHOOK_URL" ]; then return; fi
-  MESSAGE='{ "content": "<@332330209858813952>", "embeds": [{ "title": "Daily testing failed", "description": "'$CIRCLE_BUILD_URL'", "color": '$RED' }] }'
+  if [ -z "$DISCORD_TEST_REPORT_WEBHOOK" ]; then return; fi
+  MESSAGE='{ "content": "<@'$ROLE_ID'>", "embeds": [{ "title": "Daily testing failed: '$1'", "description": "'$CIRCLE_BUILD_URL'", "color": '$RED' }] }'
   echo $MESSAGE
-  curl -H "Content-Type: application/json" -d "${MESSAGE}" $DX_DISCORD_WEBHOOK_URL
+  curl -H "Content-Type: application/json" -d "${MESSAGE}" $DISCORD_TEST_REPORT_WEBHOOK
 }
 
-if [ "$1" = "success" ]; then
-  notifySuccess
+if [ "$2" = "success" ]; then
+  notifySuccess $1
 else
-  notifyFailure
+  notifyFailure $1
 fi
