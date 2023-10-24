39886049640d2a27587a7808658509ac2b36dead
Rich Burdon <rich.burdon@gmail.com>
2023-10-24 18:02:02 -0400

    feat: Echo testing generator (#4489)
    Co-authored-by: Dmytro Maretskyi <maretskii@gmail.com>

--- packages/apps/plugins/plugin-explorer/src/components/Explorer.tsx
+++ packages/apps/plugins/plugin-explorer/src/components/Explorer.tsx
@@ -2,11 +2,11 @@
 // Copyright 2023 DXOS.org
 //
 
-import React, { type FC, useMemo } from 'react';
+import React, { type FC, useMemo, useState } from 'react';
 
-import { type Space, type TypedObject } from '@dxos/client/echo';
-import { Grid, SVG, SVGContextProvider, Zoom } from '@dxos/gem-core';
-import { Graph, type GraphLayoutNode, Markers } from '@dxos/gem-spore';
+import { type Schema, type Space, type TypedObject } from '@dxos/client/echo';
+import { createSvgContext, Grid, SVG, SVGContextProvider, Zoom } from '@dxos/gem-core';
+import { Graph, GraphForceProjector, type GraphLayoutNode, Markers } from '@dxos/gem-spore';
 
 import { EchoGraphModel } from './graph-model';
 
@@ -17,11 +17,47 @@ type Slots = {
 
 const slots: Slots = {};
 
+const colors = [
+  '[&>circle]:!fill-slate-300',
+  '[&>circle]:!fill-orange-300',
+  '[&>circle]:!fill-green-300',
+  '[&>circle]:!fill-teal-300',
+  '[&>circle]:!fill-cyan-300',
+  '[&>circle]:!fill-sky-300',
+  '[&>circle]:!fill-green-300',
+  '[&>circle]:!fill-indigo-300',
+  '[&>circle]:!fill-purple-300',
+  '[&>circle]:!fill-rose-300',
+];
+
 export const Explorer: FC<{ space: Space }> = ({ space }) => {
   const model = useMemo(() => (space ? new EchoGraphModel().open(space) : undefined), [space]);
+  const [colorMap] = useState(new Map<Schema, string>());
+  const context = createSvgContext();
+  const projector = useMemo(
+    () =>
+      new GraphForceProjector<TypedObject>(context, {
+        forces: {
+          manyBody: {
+            strength: -100,
+          },
+          link: {
+            distance: 200,
+          },
+          radial: {
+            radius: 200,
+            strength: 0.2,
+          },
+        },
+        attributes: {
+          radius: (node: GraphLayoutNode<TypedObject>) => 12,
+        },
+      }),
+    [],
+  );
 
   return (
-    <SVGContextProvider>
+    <SVGContextProvider context={context}>
       <SVG className={slots?.root?.className}>
         <Markers arrowSize={6} />
         <Grid className={slots?.grid?.className} />
@@ -30,9 +66,28 @@ export const Explorer: FC<{ space: Space }> = ({ space }) => {
             model={model}
             drag
             arrows
+            projector={projector}
             labels={{
               text: (node: GraphLayoutNode<TypedObject>) => {
-                return node.data?.label ?? node.data?.title ?? node.data?.name;
+                // TODO(burdon): Use schema.
+                return node.data?.label ?? node.data?.title ?? node.data?.name ?? node.data?.id.slice(0, 8);
+              },
+            }}
+            attributes={{
+              node: (node: GraphLayoutNode<TypedObject>) => {
+                let className: string | undefined;
+                const schema = node.data?.__schema;
+                if (schema) {
+                  className = colorMap.get(schema);
+                  if (!className) {
+                    className = colors[colorMap.size % colors.length];
+                    colorMap.set(schema, className);
+                  }
+                }
+
+                return {
+                  class: className,
+                };
               },
             }}
           />
