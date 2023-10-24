9fb51931a399c998466ea44c79f0838cefbf50d4
Josiah Witt <josiah@witt.life>
2023-10-24 13:58:57 -0400

    chore: todo

--- packages/apps/plugins/plugin-error/src/ErrorPlugin.tsx
+++ packages/apps/plugins/plugin-error/src/ErrorPlugin.tsx
@@ -14,6 +14,7 @@ import translations from './translations';
 // TODO(wittjosiah): This is probably too naive and probably needs to be better integrated with the framework.
 //   For example, if client fails to initialize during plugin initialization and then the client plugin is missing,
 //   how is this reflected in the UI?
+// TODO(wittjosiah): Factor out to @dxos/app-framework & include in plugin set automatically.
 export const ErrorPlugin = (
   { config }: { config: Config } = { config: new Config(Envs(), Local(), Defaults()) },
 ): PluginDefinition<TranslationsProvides> => ({
