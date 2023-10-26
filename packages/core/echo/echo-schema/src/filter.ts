971c74af61cb5a4ebcb17541bb83f0ccf76b95f2
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2023-10-26 14:26:12 +0200

    refactor: Make queries extensible (#4515)

--- packages/core/echo/echo-schema/src/filter.ts
+++ packages/core/echo/echo-schema/src/filter.ts
@@ -3,13 +3,51 @@
 //
 
 import { DocumentModel, Reference } from '@dxos/document-model';
-import { type QueryOptions, ShowDeletedOption } from '@dxos/echo-db';
 import { invariant } from '@dxos/invariant';
+import { type PublicKey } from '@dxos/keys';
 
 import { type EchoObject } from './defs';
 import { getReferenceWithSpaceKey } from './echo-object-base';
 import { type Schema } from './proto';
 import { type Expando, type TypedObject } from './typed-object';
+
+/**
+ * Controls how deleted items are filtered.
+ */
+export enum ShowDeletedOption {
+  /**
+   * Do not return deleted items. Default behaviour.
+   */
+  HIDE_DELETED = 0,
+  /**
+   * Return deleted and regular items.
+   */
+  SHOW_DELETED = 1,
+  /**
+   * Return only deleted items.
+   */
+  SHOW_DELETED_ONLY = 2,
+}
+
+export type QueryOptions = {
+  /**
+   * Controls how deleted items are filtered.
+   */
+  deleted?: ShowDeletedOption;
+
+  /**
+   * Filter by model.
+   * @default * Only DocumentModel.
+   */
+  models?: string[] | null;
+
+  /**
+   * Query only in specific spaces.
+   */
+  spaces?: (PublicKey | { key: PublicKey })[];
+};
+
+export const QUERY_ALL_MODELS = null;
 
 // TODO(burdon): Operators (EQ, NE, GT, LT, IN, etc.)
 export type PropertyFilter = Record<string, any>;
@@ -145,5 +183,9 @@ export class Filter<T extends EchoObject = EchoObject> {
     }
 
     return this.options.models;
+  }
+
+  get searchSpacesPreference(): PublicKey[] | undefined {
+    return this.options.spaces?.map((entry) => ('key' in entry ? entry.key : (entry as PublicKey)));
   }
 }
