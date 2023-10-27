8085c49b62db0330f897caf19e35b0df9f9313d1
Dmytro Maretskyi <maretskii@gmail.com>
2023-10-27 16:18:42 +0200

    Multi-space queries

--- packages/sdk/react-client/src/echo/useQuery.ts
+++ packages/sdk/react-client/src/echo/useQuery.ts
@@ -6,23 +6,37 @@ import { useMemo, useSyncExternalStore } from 'react';
 
 import type { QueryOptions, TypedObject, Query, FilterSource, Space } from '@dxos/client/echo';
 
+import { useClient } from '../client';
+
 type UseQuery = {
   <T extends TypedObject>(space?: Space, filter?: FilterSource<T>, options?: QueryOptions, deps?: any[]): T[];
+  <T extends TypedObject>(filter?: FilterSource<T>, options?: QueryOptions, deps?: any[]): T[];
 };
 
 /**
  * Create subscription.
  */
 // TODO(burdon): Support typed operator filters (e.g., Note.filter(note => ...)).
-export const useQuery: UseQuery = <T extends TypedObject>(
-  space?: Space,
-  filter?: FilterSource<T>,
-  options?: QueryOptions,
-  deps?: any[],
-) => {
+export const useQuery: UseQuery = <T extends TypedObject>(...args: any[]) => {
+  let space: Space | undefined;
+
+  if (args.length === 4) {
+    space = args[0];
+    args.shift();
+  }
+
+  let [filter, options, deps] = args as [filter?: FilterSource<T>, options?: QueryOptions, deps?: any[]];
+
+  options ??= {};
+  if (space) {
+    options.spaces = [space];
+  }
+
+  const client = useClient();
+
   const query = useMemo(
-    () => space?.db.query(filter, options) as Query<T> | undefined,
-    [space?.db, ...(typeof filter === 'function' ? [] : filterToDepsArray(filter)), ...(deps ?? [])],
+    () => client.spaces.query(filter, options) as Query<T> | undefined,
+    [client.spaces, ...(typeof filter === 'function' ? [] : filterToDepsArray(filter)), ...(deps ?? [])],
   );
 
   // https://beta.reactjs.org/reference/react/useSyncExternalStore
