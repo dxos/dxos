60306248f03ddac1bdd8769a5db2cd14e58ff598
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2023-10-27 12:23:56 +0200

    fix: Type comparison in filters (#4519)

--- packages/core/echo/echo-schema/src/index.ts
+++ packages/core/echo/echo-schema/src/index.ts
@@ -8,7 +8,7 @@ export * from './array';
 export * from './database';
 export * from './defs';
 export * from './echo-object-base';
-export * from './query';
+export { Query, type QueryContext, type QueryResult, type QuerySource, type Sort, type Subscription } from './query';
 export * from './hypergraph';
 export { TypeCollection } from './type-collection';
 export * from './serializer';
