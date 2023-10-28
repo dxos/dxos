933059e400073ce4e8b1bb3afe2a884d15440036
Rich Burdon <rich.burdon@gmail.com>
2023-10-28 10:43:43 -0400

    fix: Explorer (#4527)

--- packages/apps/plugins/plugin-explorer/src/components/ExplorerMain.tsx
+++ packages/apps/plugins/plugin-explorer/src/components/ExplorerMain.tsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 
+import { useSearch } from '@braneframe/plugin-search';
 import { type SpacePluginProvides } from '@braneframe/plugin-space';
 import { findPlugin, usePlugins } from '@dxos/app-framework';
 import { Main } from '@dxos/react-ui';
@@ -16,13 +17,14 @@ export const ExplorerMain = () => {
   const { plugins } = usePlugins();
   const spacePlugin = findPlugin<SpacePluginProvides>(plugins, 'dxos.org/plugin/space');
   const space = spacePlugin?.provides?.space.active;
+  const { match } = useSearch();
   if (!space) {
     return null;
   }
 
   return (
     <Main.Content classNames={[baseSurface, fixedInsetFlexLayout, coarseBlockPaddingStart]}>
-      <Explorer space={space} />
+      <Explorer space={space} match={match} />
     </Main.Content>
   );
 };
