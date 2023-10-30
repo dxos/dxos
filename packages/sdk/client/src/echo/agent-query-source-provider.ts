d7dbaef4efdbd0eff10d67de564af48d8f41bf53
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-10-30 18:03:32 +0200

    Add AgentQuerySourceProvider

---
+++ packages/sdk/client/src/echo/agent-query-source-provider.ts
@@ -0,0 +1,113 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { Event } from '@dxos/async';
+import { type Space } from '@dxos/client-protocol';
+import {
+  type EchoObject,
+  type Filter,
+  type QueryResult,
+  type QuerySource,
+  type QuerySourceProvider,
+} from '@dxos/echo-schema';
+import { PublicKey } from '@dxos/keys';
+import { log } from '@dxos/log';
+import { type QueryResponse } from '@dxos/protocols/proto/dxos/agent/query';
+import { type Filter as FilterProto } from '@dxos/protocols/proto/dxos/echo/filter';
+import { type GossipMessage } from '@dxos/protocols/proto/dxos/mesh/teleport/gossip';
+
+const CHANNEL_NAME = 'dxos.org/agent/plugin/search';
+
+export class AgentQuerySourceProvider implements QuerySourceProvider {
+  private readonly _responsePromises = new Map<string, { resolve: (response: QueryResponse) => void }>();
+  private _unsubscribe?: () => void = undefined;
+
+  /**
+   * Constructor.
+   * @param _space Space instance that will be used for messaging with the agent through the gossip protocol.
+   */
+  constructor(private readonly _space: Space) {}
+
+  async open() {
+    this._unsubscribe = this._space.listen(CHANNEL_NAME, (message) => this._handleMessage(message));
+  }
+
+  async close() {
+    this._unsubscribe?.();
+  }
+
+  private _sendRequest(filter: FilterProto) {
+    const id = PublicKey.random().toHex();
+    this._space
+      .postMessage(CHANNEL_NAME, {
+        '@type': 'dxos.agent.query.QueryRequest',
+        filter,
+        id,
+      })
+      .catch((error) => log.catch(error));
+    let cancelRequest: () => void;
+    return {
+      response: new Promise<QueryResponse>((resolve, reject) => {
+        this._responsePromises.set(id, { resolve });
+        cancelRequest = () => {
+          reject(new Error('Request cancelled.'));
+          this._responsePromises.delete(id);
+        };
+      }),
+      cancelRequest: () => {
+        cancelRequest();
+      },
+    };
+  }
+
+  private _handleMessage(message: GossipMessage) {
+    if (message.payload['@type'] !== 'dxos.agent.query.QueryResponse') {
+      return;
+    }
+
+    const response = message.payload as QueryResponse;
+    const responsePromise = this._responsePromises.get(response.queryId);
+    if (!responsePromise) {
+      log('Request for this response was canceled.', { response });
+      return;
+    }
+
+    responsePromise.resolve(response);
+    this._responsePromises.delete(response.queryId);
+  }
+
+  create(): AgentQuerySource {
+    const source = new AgentQuerySource({ sendRequest: this._sendRequest.bind(this) });
+    return source;
+  }
+}
+
+export class AgentQuerySource implements QuerySource {
+  public changed = new Event<void>();
+  private _cancelPreviousRequest?: () => void = undefined;
+  private _results: QueryResult<EchoObject>[] = [];
+
+  constructor(
+    private readonly _params: {
+      sendRequest: (filter: FilterProto) => { response: Promise<QueryResponse>; cancelRequest: () => void };
+    },
+  ) {}
+
+  getResults(): QueryResult<EchoObject>[] {
+    return this._results;
+  }
+
+  update(filter: Filter<EchoObject>): void {
+    if (this._cancelPreviousRequest) {
+      this._cancelPreviousRequest();
+    }
+    const { response, cancelRequest } = this._params.sendRequest(filter.serialize());
+    this._cancelPreviousRequest = cancelRequest;
+    response
+      .then((response) => {
+        this._results = response.results as unknown as QueryResult<EchoObject>[]; // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      })
+      .catch((error) => log.catch(error));
+  }
+}
