e178d0115a6e445fbb9cbea8ed45847875963e50
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-11-01 18:41:54 +0200

    test(metadata): Add ability to change storage version on disk for testing purposes

---
+++ packages/core/echo/echo-pipeline/src/testing/change-metadata.ts
@@ -0,0 +1,25 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { log } from '@dxos/log';
+import { schema } from '@dxos/protocols';
+
+import { type MetadataStore } from '../metadata';
+
+const EchoMetadata = schema.getCodecForType('dxos.echo.metadata.EchoMetadata');
+
+/**
+ * This function will change the storage version in the metadata.
+ * This will break your storage and make it unusable.
+ * Use this only for testing purposes.
+ */
+export const changeStorageVersionInMetadata = async (metadata: MetadataStore, version: number) => {
+  log.info('Changing storage version in metadata. USE ONLY FOR TESTING');
+  await metadata.load();
+  const echoMetadata = metadata.metadata;
+  echoMetadata.version = version;
+  const file = metadata._directory.getOrCreateFile('EchoMetadata');
+  await metadata._writeFile(file, EchoMetadata, echoMetadata);
+  await metadata._directory.flush();
+};
