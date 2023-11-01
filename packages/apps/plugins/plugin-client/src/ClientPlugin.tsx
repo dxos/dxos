07a7a11cae2d2259f929358aa4ee671a3af34458
Josiah Witt <josiah@witt.life>
2023-11-01 12:19:36 -0400

    fix(plugin-client): migrate default component to root (#4559)

--- packages/apps/plugins/plugin-client/src/ClientPlugin.tsx
+++ packages/apps/plugins/plugin-client/src/ClientPlugin.tsx
@@ -82,9 +82,7 @@ export const ClientPlugin = (
 
       // TODO(burdon): Timeout.
       if (client.halo.identity.get()) {
-        console.log('### waiting...');
         await client.spaces.isReady.wait();
-        console.log('### ok');
       }
 
       return {
@@ -103,14 +101,12 @@ export const ClientPlugin = (
 
           return <ClientContext.Provider value={{ client, status }}>{children}</ClientContext.Provider>;
         },
-        components: {
-          default: () => {
-            if (error) {
-              throw error;
-            }
+        root: () => {
+          if (error) {
+            throw error;
+          }
 
-            return null;
-          },
+          return null;
         },
       };
     },
