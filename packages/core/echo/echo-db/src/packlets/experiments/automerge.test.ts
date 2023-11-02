5ef1ce7a57c10d66222fae650fbb0ccd686c967d
Dmytro Maretskyi <maretskii@gmail.com>
2023-11-02 13:09:58 +0100

    xx

--- packages/core/echo/echo-db/src/packlets/experiments/automerge.test.ts
+++ packages/core/echo/echo-db/src/packlets/experiments/automerge.test.ts
@@ -1,35 +1,37 @@
-import * as automerge from "@automerge/automerge"
-import { describe, test } from "@dxos/test"
-import { inspect } from "util"
+import * as automerge from '@automerge/automerge';
+import { describe, test } from '@dxos/test';
+import { inspect } from 'util';
+import * as Y from 'yjs';
 
 describe.only('Automerge', () => {
   test('example', () => {
+    type DocType = { a: number };
 
-    type DocType = { ideas: Array<automerge.Text> }
+    for (let i = 0; i < 10; i++) {
+      let doc1 = automerge.init<DocType>();
+      doc1 = automerge.change(doc1, (d) => {
+        d.a = i;
+      });
 
-    let doc1 = automerge.init<DocType>()
-    doc1 = automerge.change(doc1, d => {
-      d.ideas = [new automerge.Text("an immutable document")]
-    })
+      console.log(automerge.getLastLocalChange(doc1)?.length);
+    }
+  });
 
-    let doc2 = automerge.init<DocType>()
-    doc2 = automerge.merge(doc2, automerge.clone(doc1))
-    doc2 = automerge.change<DocType>(doc2, d => {
-      d.ideas.push(new automerge.Text("which records its history"))
-    })
+  test('yjs', () => {
+    const doc = new Y.Doc();
+    for (let i = 0; i < 10; i++) {
+      const cb = (update: any) => {
+        console.log(update.length);
+      };
 
-    // Note the `automerge.clone` call, see the "cloning" section of this readme for
-    // more detail
-    doc1 = automerge.merge(doc1, automerge.clone(doc2))
-    doc1 = automerge.change(doc1, d => {
-      d.ideas[0].deleteAt(13, 8)
-      d.ideas[0].insertAt(13, "object")
-    })
-
-    let doc3 = automerge.merge(doc1, doc2)
-
-    console.log(doc3)
-    console.log(inspect(automerge.getHistory(doc3), false, null, true))
-    console.log(automerge.getAllChanges(doc3))
-  })
-})
+      try {
+        doc.once('updateV2', cb);
+        doc.transact(() => {
+          doc.getMap().set('a', i);
+        });
+      } finally {
+        doc.off('updateV2', cb);
+      }
+    }
+  });
+});
