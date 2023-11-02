e21bbc74c15c81478bb429db9514cb204cfde6d9
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-11-02 13:50:47 +0200

    Delete promise

--- packages/experimental/labs-functions/src/functions/chess/handler.ts
+++ packages/experimental/labs-functions/src/functions/chess/handler.ts
@@ -11,30 +11,28 @@ export const handler: FunctionHandler<FunctionSubscriptionEvent> = async ({ even
   // TODO(burdon): client.spaces.get() is a more natural API.
   const space = context.client.spaces.get(PublicKey.from(event.space))!;
 
-  return new Promise<void>((resolve) => {
-    scheduleTask(new Context(), async () => {
-      const { Chess } = await import('chess.js');
-      for (const objectId of event.objects) {
-        const game = space.db.query({ id: objectId }).objects[0];
-        if (game && game.pgn) {
-          // TODO(burdon): Trivial engine: https://github.com/josefjadrny/js-chess-engine
-          const chess = new Chess();
-          // TODO(burdon): Rename pgn (isn't pgn).
-          chess.loadPgn(game.pgn);
-          // TODO(burdon): Only trigger if has player credential.
-          if (chess.turn() === 'b') {
-            const moves = chess.moves();
-            if (moves.length) {
-              const move = moves[Math.floor(Math.random() * moves.length)];
-              chess.move(move);
-              game.pgn = chess.pgn();
-              console.log(`move: ${chess.history().length}\n` + chess.ascii());
-            }
+  scheduleTask(new Context(), async () => {
+    const { Chess } = await import('chess.js');
+    for (const objectId of event.objects) {
+      const game = space.db.query({ id: objectId }).objects[0];
+      if (game && game.pgn) {
+        // TODO(burdon): Trivial engine: https://github.com/josefjadrny/js-chess-engine
+        const chess = new Chess();
+        // TODO(burdon): Rename pgn (isn't pgn).
+        chess.loadPgn(game.pgn);
+        // TODO(burdon): Only trigger if has player credential.
+        if (chess.turn() === 'b') {
+          const moves = chess.moves();
+          if (moves.length) {
+            const move = moves[Math.floor(Math.random() * moves.length)];
+            chess.move(move);
+            game.pgn = chess.pgn();
+            console.log(`move: ${chess.history().length}\n` + chess.ascii());
           }
         }
       }
+    }
 
-      context.status(200).succeed({});
-    });
+    context.status(200).succeed({});
   });
 };
