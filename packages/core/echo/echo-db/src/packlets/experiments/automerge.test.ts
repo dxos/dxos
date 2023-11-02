5c8fb3cf895ca578814421b23950f6c6cc154706
Dmytro Maretskyi <maretskii@gmail.com>
2023-11-02 17:57:15 +0100

    xx

--- packages/core/echo/echo-db/src/packlets/experiments/automerge.test.ts
+++ packages/core/echo/echo-db/src/packlets/experiments/automerge.test.ts
@@ -1,37 +1,53 @@
 import * as automerge from '@automerge/automerge';
 import { describe, test } from '@dxos/test';
+import { numericalValues, range } from '@dxos/util';
 import { inspect } from 'util';
 import * as Y from 'yjs';
 
 describe.only('Automerge', () => {
-  test('example', () => {
-    type DocType = { a: number };
+  const CHANGE_COUNT = 10_000;
+  const PROP_COUNT = 4;
 
-    for (let i = 0; i < 10; i++) {
-      let doc1 = automerge.init<DocType>();
+  test('example', () => {
+    const diffSizes: number[] = [];
+
+    let doc1 = automerge.init<any>();
+    for (let i = 0; i < CHANGE_COUNT; i++) {
       doc1 = automerge.change(doc1, (d) => {
-        d.a = i;
+        for(const j of range(PROP_COUNT)) {
+          d[`prop${j}`] = i + j;
+        }
       });
 
-      console.log(automerge.getLastLocalChange(doc1)?.length);
+      diffSizes.push(automerge.saveIncremental(doc1)?.length);
     }
+    console.log('automerge', { CHANGE_COUNT, PROP_COUNT })
+    console.log('full', automerge.save(doc1).length);
+    console.log('diff', numericalValues(diffSizes, x => x));
   });
 
   test('yjs', () => {
+    const diffSizes: number[] = [];
     const doc = new Y.Doc();
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < CHANGE_COUNT; i++) {
       const cb = (update: any) => {
-        console.log(update.length);
+        diffSizes.push(update.length);
       };
 
       try {
         doc.once('updateV2', cb);
         doc.transact(() => {
-          doc.getMap().set('a', i);
+          for(const j of range(PROP_COUNT)) {
+            doc.getMap().set(`prop${j}`, i + j);
+          }
         });
       } finally {
         doc.off('updateV2', cb);
       }
     }
+    console.log('yjs', { CHANGE_COUNT, PROP_COUNT })
+    console.log('full', Y.encodeStateAsUpdateV2(doc).length);
+    console.log('diff', numericalValues(diffSizes, x => x));
   });
+
 });
