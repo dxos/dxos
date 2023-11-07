871ac200bcab5a89a0d580efdd402d16deffa47e
thure <will@braneframe.com>
2023-11-06 16:32:55 -0800

    Only render share action when density is coarse. Use ghost button instead of bare trigger.

--- packages/apps/plugins/plugin-space/src/components/SpacePresence.tsx
+++ packages/apps/plugins/plugin-space/src/components/SpacePresence.tsx
@@ -12,19 +12,21 @@ import {
   Avatar,
   AvatarGroup,
   AvatarGroupItem,
-  DensityProvider,
+  Button,
   type Size,
   type ThemedClassName,
   Tooltip,
+  useDensityContext,
   useTranslation,
 } from '@dxos/react-ui';
-import { getColorForValue, getSize, mx } from '@dxos/react-ui-theme';
+import { getColorForValue, getSize } from '@dxos/react-ui-theme';
 
 import { SPACE_PLUGIN, SpaceAction, type SpacePluginProvides, type ObjectViewer } from '../types';
 
 export const SpacePresence = ({ object }: { object: TypedObject }) => {
   const spacePlugin = usePlugin<SpacePluginProvides>(SPACE_PLUGIN);
   const intentPlugin = useResolvePlugin(parseIntentPlugin);
+  const density = useDensityContext();
   const defaultSpace = useSpace();
   const identity = useIdentity();
 
@@ -50,7 +52,7 @@ export const SpacePresence = ({ object }: { object: TypedObject }) => {
     return space.key.equals(viewer.spaceKey) && object.id === viewer.objectId && Date.now() - viewer.lastSeen < 30_000;
   });
 
-  return <ObjectPresence onShareClick={handleShare} viewers={viewers} />;
+  return <ObjectPresence size={density === 'fine' ? 2 : 4} onShareClick={handleShare} viewers={viewers} />;
 };
 
 export type ObjectPresenceProps = ThemedClassName<{
@@ -69,36 +71,37 @@ export const ObjectPresence = (props: ObjectPresenceProps) => {
     classNames,
   } = props;
   const { t } = useTranslation(SPACE_PLUGIN);
-  return (
+  const density = useDensityContext();
+  return density === 'fine' && viewers.length === 0 ? null : (
     <Tooltip.Root>
-      <Tooltip.Trigger className={mx('flex items-center', classNames)} onClick={onShareClick}>
-        {onShareClick && viewers.length === 0 && (
-          <DensityProvider density={'fine'}>
-            <Users className={mx(getSize(5), 'm-1')} />
-          </DensityProvider>
-        )}
-        {viewers.length > 0 && (
-          <AvatarGroup.Root size={size} classNames={size !== 'px' && size > 3 ? 'm-2 mie-4' : 'm-1 mie-2'}>
-            {viewers.length > 3 && showCount && (
-              <AvatarGroup.Label classNames='text-xs font-system-semibold'>{viewers.length}</AvatarGroup.Label>
-            )}
-            {viewers.slice(0, 3).map((viewer, i) => {
-              const viewerHex = viewer.identityKey.toHex();
-              return (
-                <AvatarGroupItem.Root key={viewerHex} color={getColorForValue({ value: viewerHex, type: 'color' })}>
-                  <Avatar.Frame style={{ zIndex: viewers.length - i }}>
-                    <Avatar.Fallback href={viewerHex} />
-                  </Avatar.Frame>
-                </AvatarGroupItem.Root>
-              );
-            })}
-          </AvatarGroup.Root>
-        )}
+      <Tooltip.Trigger asChild>
+        <Button variant='ghost' classNames={classNames} onClick={onShareClick}>
+          {onShareClick && viewers.length === 0 && <Users className={getSize(size)} />}
+          {viewers.length > 0 && (
+            <AvatarGroup.Root size={size} classNames={size !== 'px' && size > 3 ? 'm-2 mie-4' : 'm-1 mie-2'}>
+              {viewers.length > 3 && showCount && (
+                <AvatarGroup.Label classNames='text-xs font-system-semibold'>{viewers.length}</AvatarGroup.Label>
+              )}
+              {viewers.slice(0, 3).map((viewer, i) => {
+                const viewerHex = viewer.identityKey.toHex();
+                return (
+                  <AvatarGroupItem.Root key={viewerHex} color={getColorForValue({ value: viewerHex, type: 'color' })}>
+                    <Avatar.Frame style={{ zIndex: viewers.length - i }}>
+                      <Avatar.Fallback href={viewerHex} />
+                    </Avatar.Frame>
+                  </AvatarGroupItem.Root>
+                );
+              })}
+            </AvatarGroup.Root>
+          )}
+        </Button>
       </Tooltip.Trigger>
-      <Tooltip.Content collisionPadding={4} classNames='z-[70]'>
-        <span>{viewers.length > 0 ? t('presence label', { count: viewers.length }) : t('share space')}</span>
-        <Tooltip.Arrow />
-      </Tooltip.Content>
+      <Tooltip.Portal>
+        <Tooltip.Content side='bottom' classNames='z-[70]'>
+          <span>{viewers.length > 0 ? t('presence label', { count: viewers.length }) : t('share space')}</span>
+          <Tooltip.Arrow />
+        </Tooltip.Content>
+      </Tooltip.Portal>
     </Tooltip.Root>
   );
 };
