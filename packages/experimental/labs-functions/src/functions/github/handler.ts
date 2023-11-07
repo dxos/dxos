42007e0893c3f473e7ef662ee141053a6bce3538
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-11-07 17:29:13 +0200

    Add logs

--- packages/experimental/labs-functions/src/functions/github/handler.ts
+++ packages/experimental/labs-functions/src/functions/github/handler.ts
@@ -3,8 +3,8 @@
 //
 
 import { Octokit, type RestEndpointMethodTypes } from '@octokit/rest';
-import { Reference } from 'packages/core/echo/document-model/src';
 
+import { Reference } from '@dxos/document-model';
 import { TypedObject } from '@dxos/echo-schema';
 import { type FunctionSubscriptionEvent, type FunctionHandler } from '@dxos/functions';
 import { invariant } from '@dxos/invariant';
@@ -15,12 +15,15 @@ type GithubContributors = RestEndpointMethodTypes['repos']['listContributors']['
 type GithubUser = RestEndpointMethodTypes['users']['getByUsername']['response']['data'];
 
 export const handler: FunctionHandler<FunctionSubscriptionEvent> = async ({ event, context }) => {
+  log.info('Event', event);
   const octokit = new Octokit();
   for (const objectId of event.objects) {
     const project = context.client.spaces.query({ id: objectId }).objects[0];
     if (project && project.repo) {
+      log.info('Fetching contributors for', project.repo);
       const response = await octokit.repos.listContributors({ owner: 'dxos', repo: 'dxos' });
       const contributors: GithubContributors = response.data;
+      log.info('Contributors', { length: contributors.length });
       const space = context.client.spaces.get(PublicKey.from(event.space));
       invariant(space, 'Missing space.');
       await space.waitUntilReady();
