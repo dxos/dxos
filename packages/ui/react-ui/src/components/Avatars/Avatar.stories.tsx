2b083bdd9a9fa74eea38d2440dc629533d9c2b47
thure <will@braneframe.com>
2023-11-06 17:14:11 -0800

    Use `getColorForValue`.

--- packages/ui/react-ui/src/components/Avatars/Avatar.stories.tsx
+++ packages/ui/react-ui/src/components/Avatars/Avatar.stories.tsx
@@ -6,22 +6,12 @@ import '@dxosTheme';
 
 import React, { type PropsWithChildren } from 'react';
 
+import { getColorForValue } from '@dxos/react-ui-theme';
 import { type Size } from '@dxos/react-ui-types';
 
 import { Avatar, useJdenticonHref, type AvatarVariant, type AvatarStatus, type AvatarAnimation } from './Avatar';
 
-const cursorColors = [
-  { color: '#30bced', light: '#30bced33' },
-  { color: '#6eeb83', light: '#6eeb8333' },
-  { color: '#ffbc42', light: '#ffbc4233' },
-  { color: '#ecd444', light: '#ecd44433' },
-  { color: '#ee6352', light: '#ee635233' },
-  { color: '#9ac2c9', light: '#9ac2c933' },
-  { color: '#8acb88', light: '#8acb8833' },
-  { color: '#1be7ff', light: '#1be7ff33' },
-];
-
-const randomColor = () => cursorColors[Math.round(Math.random() * cursorColors.length)]?.color;
+const randomColor = () => getColorForValue({ value: Math.random().toString(16), type: 'color' });
 
 type StorybookAvatarProps = {
   imgSrc?: string;
