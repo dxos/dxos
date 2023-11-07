7084ba77864051baa4b0982ad73cb00e3d1c492d
Dmytro Maretskyi <maretskii@gmail.com>
2023-11-07 13:32:30 +0100

    Fix filters by typename

--- packages/core/echo/echo-schema/src/query/filter.test.ts
+++ packages/core/echo/echo-schema/src/query/filter.test.ts
@@ -10,6 +10,7 @@ import { describe, test } from '@dxos/test';
 
 import { compareType, Filter, filterMatch } from './filter';
 import { Expando } from '../object';
+import { Schema } from '../proto';
 
 describe('Filter', () => {
   test('properties', () => {
@@ -94,4 +95,18 @@ describe('Filter', () => {
       ),
     ).to.be.true;
   });
+
+  test('dynamic schema', () => {
+    const schema = new Schema({
+      typename: 'example.TestSchema',
+    })
+
+    const object = new Expando({
+      title: 'test',
+    }, { schema });
+    expect(object.__schema).to.eq(schema);
+
+    const filter = Filter.typename('example.TestSchema');
+    expect(filterMatch(filter, object)).to.be.true;
+  })
 });
--- packages/core/echo/echo-schema/src/query/filter.ts
+++ packages/core/echo/echo-schema/src/query/filter.ts
@@ -7,7 +7,7 @@ import { invariant } from '@dxos/invariant';
 import { type PublicKey } from '@dxos/keys';
 import { QueryOptions, type Filter as FilterProto } from '@dxos/protocols/proto/dxos/echo/filter';
 
-import { base, getDatabaseFromObject, isTypedObject, type EchoObject, type Expando, type TypedObject } from '../object';
+import { base, getDatabaseFromObject, isTypedObject, type EchoObject, type Expando, type TypedObject, immutable } from '../object';
 import { getReferenceWithSpaceKey } from '../object';
 import { type Schema } from '../proto';
 
@@ -214,25 +214,28 @@ const filterMatchInner = (filter: Filter, object: EchoObject): boolean => {
     return false;
   }
 
-  // TODO(burdon): Should match by default?
-  let match = true;
-
   if (filter.type) {
     if (!isTypedObject(object)) {
       return false;
     }
 
-    const type = object[base]._getType();
-    if (!type) {
-      return false;
-    }
+    // Separate branch for objects with dynamic schema and typename filters.
+    // TODO(dmaretskyi): Better way to check if schema is dynamic.
+    if (filter.type.protocol === 'protobuf' && object.__schema && !object.__schema[immutable]) {
+      if (object.__schema.typename !== filter.type.itemId) {
+        return false;
+      }
+    } else {
+      const type = object[base]._getType();
+      if (!type) {
+        return false;
+      }
 
-    // TODO(burdon): Comment.
-    if (!compareType(filter.type, type, getDatabaseFromObject(object)?._backend.spaceKey)) {
-      return false;
+      // TODO(burdon): Comment.
+      if (!compareType(filter.type, type, getDatabaseFromObject(object)?._backend.spaceKey)) {
+        return false;
+      }
     }
-
-    match = true;
   }
 
   if (filter.properties) {
@@ -243,8 +246,6 @@ const filterMatchInner = (filter: Filter, object: EchoObject): boolean => {
         return false;
       }
     }
-
-    match = true;
   }
 
   if (filter.text !== undefined) {
@@ -256,8 +257,6 @@ const filterMatchInner = (filter: Filter, object: EchoObject): boolean => {
     if (!JSON.stringify(object.toJSON()).toLowerCase().includes(text)) {
       return false;
     }
-
-    match = true;
   }
 
   if (filter.predicate && !filter.predicate(object)) {
@@ -268,11 +267,9 @@ const filterMatchInner = (filter: Filter, object: EchoObject): boolean => {
     if (!filterMatch(andFilter, object)) {
       return false;
     }
-
-    match = true;
   }
 
-  return match;
+  return true;
 };
 
 // Type comparison is a bit weird due to backwards compatibility requirements.
