3d7d4c9c50cd6eb3070c992c4d107ea6161b7480
Nathan A. Ferch <nf+github@marginal.net>
2023-11-08 14:38:40 -0500

    chore: set log filter for prod and staging

--- .circleci/scripts/publish.sh
+++ .circleci/scripts/publish.sh
@@ -55,6 +55,7 @@ for APP in "${APPS[@]}"; do
   if [ $BRANCH = "production" ]; then
     export DX_ENVIRONMENT=production
     export REMOTE_SOURCE=https://halo.dxos.org/vault.html
+    export LOG_FILTER=error
     DX_CONFIG="$ROOT/.circleci/publish-config/config-production.yml"
     VERSION=$(cat package.json | jq -r ".version")
 
@@ -70,6 +71,7 @@ for APP in "${APPS[@]}"; do
   elif [ $BRANCH = "staging" ]; then
     export DX_ENVIRONMENT=staging
     export REMOTE_SOURCE=https://halo.staging.dxos.org/vault.html
+    export LOG_FILTER=error
     DX_CONFIG="$ROOT/.circleci/publish-config/config-staging.yml"
     VERSION=$(cat package.json | jq -r ".version")
 
@@ -81,6 +83,8 @@ for APP in "${APPS[@]}"; do
   else
     export DX_ENVIRONMENT=development
     export REMOTE_SOURCE=https://halo.dev.dxos.org/vault.html
+    # the default per packages/common/log/src/options.ts
+    export LOG_FILTER=info
     DX_CONFIG="$ROOT/.circleci/publish-config/config-development.yml"
 
     $ROOT/packages/devtools/cli/bin/run app publish \
