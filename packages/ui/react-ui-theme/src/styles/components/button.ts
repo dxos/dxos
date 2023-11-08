6980cd0a8bf835a72258378438a12a6fc42b4d91
Will Shown <thure@users.noreply.github.com>
2023-11-07 16:52:10 -0800

    fix: Ghost buttons and tree items apply a background color instead of any text color (#4646)

--- packages/ui/react-ui-theme/src/styles/components/button.ts
+++ packages/ui/react-ui-theme/src/styles/components/button.ts
@@ -23,7 +23,7 @@ export const defaultAppButtonColors =
   'bg-white aria-pressed:bg-primary-100 aria-checked:bg-primary-100 text-neutral-800 aria-pressed:text-primary-800 aria-checked:text-primary-800 dark:bg-neutral-800 dark:aria-pressed:bg-primary-700 dark:aria-checked:bg-primary-700 dark:text-neutral-50 dark:aria-pressed:text-primary-50 dark:aria-checked:text-primary-50';
 export const defaultOsButtonColors = 'bg-white/50 text-neutral-900 dark:bg-neutral-750/50 dark:text-neutral-50';
 export const ghostButtonColors =
-  'hover:bg-transparent dark:hover:bg-transparent hover:text-primary-500 dark:hover:text-primary-300 aria-pressed:text-primary-800 aria-checked:text-primary-800 dark:aria-pressed:text-primary-50 dark:aria-checked:text-primary-50';
+  'hover:bg-neutral-450/10 dark:hover:bg-25/10 hover:text-inherit dark:hover:text-inherit aria-pressed:text-primary-800 aria-checked:text-primary-800 dark:aria-pressed:text-primary-100 dark:aria-checked:text-primary-100';
 
 export type ButtonStyleProps = Partial<{
   inGroup?: boolean;
