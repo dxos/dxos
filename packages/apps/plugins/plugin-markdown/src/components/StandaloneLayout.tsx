9fee712d7660c45832e38fdbcddab122c7a790e9
Will Shown <thure@users.noreply.github.com>
2023-11-08 16:15:53 -0800

    fix(react-ui-editor): Fix broken layouts (#4657)

--- packages/apps/plugins/plugin-markdown/src/components/StandaloneLayout.tsx
+++ packages/apps/plugins/plugin-markdown/src/components/StandaloneLayout.tsx
@@ -6,14 +6,7 @@ import React, { type MutableRefObject, type PropsWithChildren } from 'react';
 
 import { Main } from '@dxos/react-ui';
 import { type ComposerModel, type MarkdownComposerRef } from '@dxos/react-ui-editor';
-import {
-  baseSurface,
-  coarseBlockPaddingStart,
-  inputSurface,
-  mx,
-  surfaceElevation,
-  textBlockWidth,
-} from '@dxos/react-ui-theme';
+import { baseSurface, coarseBlockPaddingStart, mx, textBlockWidth } from '@dxos/react-ui-theme';
 
 import { type MarkdownProperties } from '../types';
 
@@ -28,15 +21,7 @@ export const StandaloneLayout = ({
   return (
     <Main.Content bounce classNames={[baseSurface, coarseBlockPaddingStart]}>
       <div role='none' className={mx(textBlockWidth, 'pli-2')}>
-        <div
-          role='none'
-          className={mx(
-            inputSurface,
-            surfaceElevation({ elevation: 'group' }),
-            'mbs-2 mbe-6 pli-6 rounded',
-            'min-bs-[calc(100dvh-5rem)] flex flex-col',
-          )}
-        >
+        <div role='none' className={mx('mbs-4 mbe-6', 'min-bs-[calc(100dvh-5rem)] flex flex-col')}>
           {children}
         </div>
       </div>
