adde65d67c7dc6df77a433b80d644be93a5fd525
Dmytro Maretskyi <maretskii@gmail.com>
2023-11-14 11:03:03 +0100

    Update automerge import and add tests

--- packages/core/echo/echo-db/src/packlets/experiments/automerge.test.ts
+++ packages/core/echo/echo-db/src/packlets/experiments/automerge.test.ts
@@ -1,8 +1,9 @@
-import * as automerge from '@automerge/automerge';
+import { next as automerge } from '@automerge/automerge';
 import { describe, test } from '@dxos/test';
 import { numericalValues, range } from '@dxos/util';
 import { inspect } from 'util';
 import * as Y from 'yjs';
+
 
 describe.only('Automerge', () => {
   const CHANGE_COUNT = 10_000;
@@ -50,4 +51,45 @@ describe.only('Automerge', () => {
     console.log('diff', numericalValues(diffSizes, x => x));
   });
 
+  test.only('diff', () => {
+    let doc = automerge.init<any>();
+    const before = automerge.getHeads(doc);
+
+    doc = automerge.change(doc, (d) => {
+      d.text = 'foo';
+    });
+
+    const { newDoc } = automerge.changeAt(doc, before, (d) => { });
+    doc = newDoc;
+
+    const heads = automerge.getHeads(doc);
+    const diff = automerge.diff(doc, heads, heads);
+
+    console.log(diff)
+  })
+
+  test.skip('repo', async () => {
+    const { Repo } = await eval('import("@automerge/automerge-repo")');
+
+    const repo = new Repo({
+      network: [],
+      // storage: new IndexedDBStorageAdapter(),
+      sharePolicy: async (peerId, documentId) => true // this is the default
+    })
+    const handle = repo.create()
+
+    handle.change((doc: any) => {
+      doc.text = "hello world"
+    });
+
+    handle.change((doc: any) => {
+      doc.text = 'bar';
+    });
+
+    const heads = automerge.getHeads(handle.docSync());
+    const diff = automerge.diff(handle.docSync(), heads, heads);
+
+    console.log(diff)
+    debugger;
+  })
 });
