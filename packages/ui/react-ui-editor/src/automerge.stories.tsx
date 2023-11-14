b65366420ca6c9180a58591d8f80b8e3086aa9d1
Dmytro Maretskyi <maretskii@gmail.com>
2023-11-14 11:03:20 +0100

    Update dependencies and add Automerge editor component.

---
+++ packages/ui/react-ui-editor/src/automerge.stories.tsx
@@ -0,0 +1,96 @@
+import React, { MutableRefObject, useEffect, useRef, useState } from "react"
+
+import { EditorView } from "@codemirror/view"
+import { basicSetup } from "codemirror"
+import { Prop } from "@automerge/automerge"
+import { plugin as amgPlugin, PatchSemaphore } from "@automerge/automerge-codemirror"
+import { next as automerge, type Doc } from "@automerge/automerge"
+import { Repo, type DocHandle, PeerId, DocumentId } from "@automerge/automerge-repo"
+
+type EditorProps = {
+  handle: DocHandle<{ text: string }>
+  path: Prop[]
+}
+
+function Editor({ handle, path }: EditorProps) {
+  const containerRef = useRef<HTMLDivElement | null>(null)
+  const editorRoot = useRef<EditorView>()
+
+  useEffect(() => {
+    const doc = handle.docSync();
+    const source = doc.text // this should use path
+    const plugin = amgPlugin(doc, path)
+    const semaphore = new PatchSemaphore(plugin)
+    const view = (editorRoot.current = new EditorView({
+      doc: source,
+      extensions: [basicSetup, plugin],
+      dispatch(transaction) {
+        view.update([transaction])
+        semaphore.reconcile(handle, view)
+      },
+      parent: containerRef.current,
+    }))
+    ;window.view = view;
+    ;window.semaphore = semaphore;
+    window.am = automerge;
+
+    const handleChange = ({ doc, patchInfo }) => {
+      semaphore.reconcile(handle, view)
+    }
+
+    handle.addListener("change", handleChange)
+
+    return () => {
+      handle.removeListener("change", handleChange)
+      view.destroy()
+    }
+  }, [])
+
+  return (
+    <div
+      className="codemirror-editor"
+      ref={containerRef}
+      onKeyDown={evt => evt.stopPropagation()}
+    />
+  )
+}
+
+const Story = () => {
+  const handle = useRef<DocHandle<{ text: string }>>()
+  const [,forceUpdate] = useState({})
+
+  useEffect(() => {
+    const repo = new Repo({
+      network: [],
+      // storage: new IndexedDBStorageAdapter(),
+      sharePolicy: async (peerId, documentId) => true // this is the default
+    })
+    
+
+    handle.current = repo.create()
+
+    handle.current.change((doc: any) => {
+      doc.text = "hello world"
+    });
+
+    window.handle = handle.current
+    forceUpdate({})
+  }, [])
+
+  if(!handle.current) {
+    return null;
+  }
+
+  return (
+    <Editor handle={handle.current} path={['text']} />
+  )
+}
+
+export default {
+  title: 'Automerge',
+};
+
+
+export const EditorStory = {
+  render: () => <Story />
+};
