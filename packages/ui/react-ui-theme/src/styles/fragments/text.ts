b2a059e82b18ae2724aa403718403676b2053c9f
thure <will@braneframe.com>
2023-11-13 17:28:48 -0800

    Add combobox components to `SearchList`.

--- packages/ui/react-ui-theme/src/styles/fragments/text.ts
+++ packages/ui/react-ui-theme/src/styles/fragments/text.ts
@@ -3,6 +3,7 @@
 //
 
 export const placeholderText = 'placeholder-neutral-500 dark:placeholder-neutral-400';
+export const staticPlaceholderText = 'text-neutral-500 dark:text-neutral-400';
 
 export const descriptionText = 'text-xs font-normal text-neutral-650 dark:text-neutral-300';
 export const descriptionTextPrimary = 'text-xs font-normal text-white/90';
