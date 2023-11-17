987737cddc8f15fb5d0b72a723c7eea9fa8bb3c6
Josiah Witt <josiah@witt.life>
2023-11-17 08:59:03 -0500

    feat: add wildcard plugin

---
+++ packages/apps/plugins/plugin-wildcard/src/components/Wildcard.stories.tsx
@@ -0,0 +1,18 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import '@dxosTheme';
+
+import { TypedObject } from '@dxos/react-client/echo';
+
+import { Wildcard } from './Wildcard';
+
+export default {
+  component: Wildcard,
+  args: {
+    item: { id: 'test', object: new TypedObject({ title: 'Test', other: 'details' }) },
+  },
+};
+
+export const Default = {};
