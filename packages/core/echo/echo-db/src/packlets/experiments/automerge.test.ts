c054f42349d394c61d1b7cf7fb659a43c43b476f
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2023-11-21 16:12:25 +0100

    feat: Begin automerge integration into database API (#4726)

---
+++ packages/core/echo/echo-db/src/packlets/experiments/automerge.test.ts
@@ -0,0 +1,97 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { next as automerge } from '@automerge/automerge';
+import * as Y from 'yjs';
+
+import { describe, test } from '@dxos/test';
+import { numericalValues, range } from '@dxos/util';
+
+describe.only('Automerge', () => {
+  const CHANGE_COUNT = 10_000;
+  const PROP_COUNT = 4;
+
+  test('example', () => {
+    const diffSizes: number[] = [];
+
+    let doc1 = automerge.init<any>();
+    for (let i = 0; i < CHANGE_COUNT; i++) {
+      doc1 = automerge.change(doc1, (d) => {
+        for (const j of range(PROP_COUNT)) {
+          d[`prop${j}`] = i + j;
+        }
+      });
+
+      diffSizes.push(automerge.saveIncremental(doc1)?.length);
+    }
+    console.log('automerge', { CHANGE_COUNT, PROP_COUNT });
+    console.log('full', automerge.save(doc1).length);
+    console.log(
+      'diff',
+      numericalValues(diffSizes, (x) => x),
+    );
+  });
+
+  test('yjs', () => {
+    const diffSizes: number[] = [];
+    const doc = new Y.Doc();
+    for (let i = 0; i < CHANGE_COUNT; i++) {
+      const cb = (update: any) => {
+        diffSizes.push(update.length);
+      };
+
+      try {
+        doc.once('updateV2', cb);
+        doc.transact(() => {
+          for (const j of range(PROP_COUNT)) {
+            doc.getMap().set(`prop${j}`, i + j);
+          }
+        });
+      } finally {
+        doc.off('updateV2', cb);
+      }
+    }
+    console.log('yjs', { CHANGE_COUNT, PROP_COUNT });
+    console.log('full', Y.encodeStateAsUpdateV2(doc).length);
+    console.log(
+      'diff',
+      numericalValues(diffSizes, (x) => x),
+    );
+  });
+
+  test.skip('diff', () => {
+    let doc = automerge.init<any>();
+    const before = automerge.getHeads(doc);
+
+    doc = automerge.change(doc, (d) => {
+      d.text = 'foo';
+    });
+
+    const { newDoc } = automerge.changeAt(doc, before, (d) => {});
+    doc = newDoc;
+
+    const heads = automerge.getHeads(doc);
+    const diff = automerge.diff(doc, heads, heads);
+
+    console.log(diff);
+  });
+
+  test.only('repo', async () => {
+    // eslint-disable-next-line no-eval
+    const { Repo } = await eval('import("@automerge/automerge-repo")');
+
+    const repo = new Repo({
+      network: [],
+      // storage: new IndexedDBStorageAdapter(),
+      sharePolicy: async (peerId: any, documentId: any) => true, // this is the default
+    });
+    const handle = repo.create();
+
+    handle.change((doc: any) => {
+      doc.text = 'hello world';
+
+      console.log(doc.text);
+    });
+  });
+});
