9b7b56de39836b62c2f7d39f27075b8072691def
Dmytro Maretskyi <maretskii@gmail.com>
2023-11-30 12:15:25 +0100

    Upgrade log decorator

--- packages/common/log/src/decorators.ts
+++ packages/common/log/src/decorators.ts
@@ -7,6 +7,8 @@ import { inspect } from 'node:util';
 import type { LogMethods } from './log';
 import { type CallMetadata } from './meta';
 
+let nextPromiseId = 0;
+
 export const createMethodLogDecorator =
   (log: LogMethods) =>
   (arg0?: never, arg1?: never, meta?: CallMetadata): MethodDecorator =>
@@ -21,8 +23,28 @@ export const createMethodLogDecorator =
       const formattedArgs = args.map((arg: any) => inspect(arg, false, 1, true)).join(', ');
 
       try {
+        const startTime = performance.now();
         const result = method.apply(this, args);
-        log.info(`${propertyKey as string}(${formattedArgs}) => ${inspect(result, false, 1, true)}`, {}, combinedMeta);
+        
+        if (isThenable(result)) {
+          const id = nextPromiseId++;
+          log.info(`${propertyKey as string}(${formattedArgs}) => Promise { #${id} }`, {}, combinedMeta);
+          result.then(
+            resolvedValue => {
+              if(resolvedValue !== undefined) {
+                log.info(`✅ resolve #${id} ${(performance.now() - startTime).toFixed(0)}ms => ${inspect(resolvedValue, false, 1, true)}`, {}, combinedMeta);
+              } else {
+                log.info(`✅ resolve #${id} ${(performance.now() - startTime).toFixed(0)}ms`, {}, combinedMeta);
+              }
+            },
+            err => {
+              log.info(`🔥 reject #${id} #${(performance.now() - startTime).toFixed(0)}ms => ${err}`, {}, combinedMeta);
+            }
+          )
+        } else {
+          log.info(`${propertyKey as string}(${formattedArgs}) => ${inspect(result, false, 1, true)}`, {}, combinedMeta);
+        }
+
         return result;
       } catch (err) {
         log.error(`${propertyKey as string}(${formattedArgs}) 🔥 ${err}`, {}, combinedMeta);
@@ -31,3 +53,5 @@ export const createMethodLogDecorator =
     };
     Object.defineProperty(descriptor.value, 'name', { value: (propertyKey as string) + '$log' });
   };
+
+const isThenable = (obj: any): obj is Promise<unknown> => obj && typeof obj.then === 'function';
