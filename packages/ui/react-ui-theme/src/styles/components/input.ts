ec4fe342f27f9c275157a94605478b636f6eeda2
Will Shown <thure@users.noreply.github.com>
2023-12-01 18:25:09 -0800

    fix(react-ui-theme): Input backgrounds & Switch design (#4830)

--- packages/ui/react-ui-theme/src/styles/components/input.ts
+++ packages/ui/react-ui-theme/src/styles/components/input.ts
@@ -18,14 +18,12 @@ import {
   fineDimensions,
   placeholderText,
   focusRing,
-  hoverColors,
   subduedFocus,
   fineBlockSize,
   coarseBlockSize,
   staticFocusRing,
   descriptionText,
   valenceColorText,
-  contentElevation,
   getSize,
   computeSize,
   sizeValue,
@@ -54,6 +52,18 @@ export const infoInputValence = 'shadow-info-500/50 dark:shadow-info-600/50';
 export const warningInputValence = 'shadow-warning-500/50 dark:shadow-warning-600/50';
 export const errorInputValence = 'shadow-error-500/50 dark:shadow-error-600/50';
 
+const textInputSurfaceFocus =
+  'transition-colors bg-[--input-bg] focus:bg-white dark:focus:bg-neutral-925 border-transparent focus:border-transparent';
+
+const textInputSurfaceHover =
+  'hover:bg-[--input-bg-hover] dark:hover:bg-[--input-bg-hover] focus:hover:bg-white dark:focus:hover:bg-neutral-925';
+
+const booleanInputSurface =
+  'shadow-inner transition-colors bg-neutral-150 dark:bg-neutral-700 aria-checked:bg-primary-550 dark:aria-checked:bg-primary-550 aria-[checked=mixed]:bg-primary-550 dark:aria-[checked=mixed]:bg-primary-550';
+
+const booleanInputSurfaceHover =
+  'hover:bg-neutral-200 dark:hover:bg-neutral-650 hover:aria-checked:bg-primary-600 dark:hover:aria-checked:bg-primary-600 hover:aria-[checked=mixed]:bg-primary-600 dark:hover:aria-[checked=mixed]:bg-primary-600';
+
 export const inputValence = (valence?: MessageValence) => {
   switch (valence) {
     case 'success':
@@ -79,15 +89,19 @@ const sharedSubduedInputStyles: ComponentFragment<InputStyleProps> = (props) => 
 ];
 
 const sharedDefaultInputStyles: ComponentFragment<InputStyleProps> = (props) => [
-  'is-full text-neutral-900 dark:text-white',
+  'is-full text-base rounded text-[color:var(--surface-text)]',
+  textInputSurfaceFocus,
   placeholderText,
   props.density === 'fine' ? fineDimensions : coarseDimensions,
-  props.disabled && staticDisabled,
+  props.disabled ? staticDisabled : textInputSurfaceHover,
 ];
 
 const sharedStaticInputStyles: ComponentFragment<InputStyleProps> = (props) => [
+  'is-full text-base rounded text-[color:var(--surface-text)]',
+  textInputSurfaceFocus,
+  textInputSurfaceHover,
+  props.focused && 'bg-white dark:bg-neutral-925',
   placeholderText,
-  'text-base rounded bg-white/50 text-neutral-900 dark:bg-neutral-700/50 dark:text-white',
   inputValence(props.validationValence),
   props.disabled && staticDisabled,
   props.focused && staticFocusRing,
@@ -97,22 +111,20 @@ export const inputInput: ComponentFunction<InputStyleProps> = (props, ...etc) =>
   props.variant === 'subdued'
     ? mx(...sharedSubduedInputStyles(props), ...etc)
     : props.variant === 'static'
-    ? mx(...sharedStaticInputStyles(props), !props.disabled && contentElevation(props), ...etc)
+    ? mx(...sharedStaticInputStyles(props), ...etc)
     : mx(
-        'rounded text-base bg-white/50 focus-visible:bg-white/50 dark:bg-neutral-700/50 dark:focus-visible:bg-neutral-700/50 border-transparent focus:border-transparent',
-        !props.disabled && focusRing,
-        !props.disabled && hoverColors,
-        inputValence(props.validationValence) || neutralInputValence,
-        !props.disabled && contentElevation(props),
         ...sharedDefaultInputStyles(props),
+        !props.disabled && focusRing,
+        inputValence(props.validationValence) || neutralInputValence,
         ...etc,
       );
 
-export const inputCheckbox: ComponentFunction<InputStyleProps> = ({ size = 5 }, ...etc) =>
+export const inputCheckbox: ComponentFunction<InputStyleProps> = ({ size = 5, disabled }, ...etc) =>
   mx(
     getSize(size),
+    booleanInputSurface,
+    !disabled && booleanInputSurfaceHover,
     'shrink-0 flex items-center justify-center rounded text-white',
-    'bg-neutral-100 dark:bg-neutral-700 aria-checked:bg-primary-600 dark:aria-checked:bg-primary-600 aria-[checked=mixed]:bg-primary-600 dark:aria-[checked=mixed]:bg-primary-600',
     focusRing,
     ...etc,
   );
@@ -120,18 +132,21 @@ export const inputCheckbox: ComponentFunction<InputStyleProps> = ({ size = 5 }, 
 export const inputCheckboxIndicator: ComponentFunction<InputStyleProps> = ({ size = 5 }, ...etc) =>
   mx(getSize(computeSize(sizeValue(size) * 0.7, 4)), ...etc);
 
-export const inputSwitch: ComponentFunction<InputStyleProps> = ({ size = 5 }, ...etc) =>
+export const inputSwitch: ComponentFunction<InputStyleProps> = ({ size = 5, disabled }, ...etc) =>
   mx(
     getSizeHeight(size),
     getSizeWidth(computeSize(sizeValue(size) * 1.75, 9)),
-    'cursor-pointer shrink-0 bg-transparent rounded-full pli-0.5 border-2 border-neutral-500 relative aria-checked:border-primary-500 aria-checked:bg-primary-500 outline-none cursor-default',
+    booleanInputSurface,
+    !disabled && booleanInputSurfaceHover,
+    'cursor-pointer shrink-0 rounded-full pli-1 relative',
+    focusRing,
     ...etc,
   );
 
 export const inputSwitchThumb: ComponentFunction<InputStyleProps> = ({ size = 5 }, ...etc) =>
   mx(
     getSize(size === 'px' ? 'px' : ((size - 2) as Size)),
-    'block bg-neutral-500 rounded-full border-neutral-100 transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[100%] data-[state=checked]:bg-white',
+    'block bg-white rounded-full border-neutral-100 transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[100%]',
     ...etc,
   );
 
