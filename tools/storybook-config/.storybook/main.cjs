00b4cb1baad2d51ec033e4f385ca6ba6b4b5b591
thure <will@braneframe.com>
2023-12-06 18:00:37 -0800

    Consolidate storybook configs in a new package, `storybook-config`.

---
+++ tools/storybook-config/.storybook/main.cjs
@@ -0,0 +1,36 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+const { resolve } = require('path');
+const { mergeConfig } = require('vite');
+
+const turbosnap = require('vite-plugin-turbosnap');
+
+const { ThemePlugin } = require('packages/ui/react-ui-theme/plugin');
+
+module.exports = {
+  stories: [
+    '../../../packages/*/*/src/**/*.stories.{mdx,tsx}'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  viteFinal: async (config) => mergeConfig(config, {
+    plugins: [
+      ThemePlugin({
+        root: __dirname,
+        content: [
+          resolve(__dirname, '../../../packages/*/*/src') + '/**/*.{ts,tsx,js,jsx}',
+        ]
+      }),
+      turbosnap({ rootDir: config.root }),
+    ],
+  })
+};
