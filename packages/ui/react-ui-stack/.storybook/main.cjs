8a45a6d238cbf483647a7533a7f1cea103df3893
Josiah Witt <josiah@witt.life>
2023-12-11 11:02:22 -0500

    chore(react-ui-stack): fix storybook build (#4893)

--- packages/ui/react-ui-stack/.storybook/main.cjs
+++ packages/ui/react-ui-stack/.storybook/main.cjs
@@ -19,6 +19,10 @@ module.exports = {
     options: {}
   },
   viteFinal: async (config) => mergeConfig(config, {
+    build: {
+      // NOTE: Browsers which support top-level await.
+      target: ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15']
+    },
     plugins: [
       ThemePlugin({
         root: __dirname,
