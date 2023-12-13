34ce67b9a64b0fd6cc0cb905c53b8ee8508e4be3
Rich Burdon <rich.burdon@gmail.com>
2023-12-13 11:44:15 -0500

    feat: Delete kai modules. (#4783)
    Co-authored-by: Josiah Witt <josiah@witt.life>

--- packages/experimental/kai-framework/src/index.ts
+++
@@ -1,8 +0,0 @@
-//
-// Copyright 2023 DXOS.org
-//
-
-export * from './containers';
-export * from './hooks';
-export * from './pages';
-export * from './translations';
--- packages/experimental/mosaic/src/components/Card/index.ts
+++
@@ -1,9 +0,0 @@
-//
-// Copyright 2023 DXOS.org
-//
-
-export * from './Card';
-export * from './CardActions';
-export * from './CardContent';
-export * from './CardHeader';
-export * from './util';
