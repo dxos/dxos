89011180fd5ae75ef1021505d1a634ff359eaeeb
thure <will@braneframe.com>
2023-12-13 11:01:35 -0800

    Remove ClientSPaceDecorator.

--- packages/sdk/react-client/src/testing/index.ts
+++ packages/sdk/react-client/src/testing/index.ts
@@ -5,7 +5,6 @@
 export * from '@dxos/client/testing';
 
 export * from './ClientRepeater';
-export * from './ClientSpaceDecorator';
 export * from './FullscreenDecorator';
 export * from './ProfileInitializer';
 export * from './ToggleNetworkDecorator';
