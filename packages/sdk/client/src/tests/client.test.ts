ed67c72ab60ddc134c55a8eef91ced2c045c7445
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-12-14 16:06:57 +0200

    Change test

--- packages/sdk/client/src/tests/client.test.ts
+++ packages/sdk/client/src/tests/client.test.ts
@@ -5,8 +5,9 @@
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { rmSync } from 'node:fs';
+import waitForExpect from 'wait-for-expect';
 
-import { Contact } from '@braneframe/types';
+import { Message, Thread } from '@braneframe/types';
 import { Trigger } from '@dxos/async';
 import { Config } from '@dxos/config';
 import { testWithAutomerge } from '@dxos/echo-schema/testing';
@@ -131,47 +132,38 @@ describe('Client', () => {
       await client1.halo.createIdentity();
       await client2.halo.createIdentity();
 
-      const firstContactQueried = new Trigger<Contact>();
-      const secondContactQueried = new Trigger<Contact>();
-      const firstContactName = 'Rich Ivanov';
-      const secondContactName = 'Dmytro Veremchuk';
+      const threadQueried = new Trigger<Thread>();
 
       // Create space and invite second client.
-      const space = await client1.spaces.create();
-      await space.waitUntilReady();
-      const spaceKey = space.key;
-      await Promise.all(performInvitation({ host: space, guest: client2.spaces }));
+      const space1 = await client1.spaces.create();
+      await space1.waitUntilReady();
+      const spaceKey = space1.key;
 
-      const query = space.db.query(Contact.filter());
+      const query = space1.db.query(Thread.filter());
       query.subscribe(({ objects }) => {
-        if (objects.length === 1 && objects[0].name === firstContactName) {
-          firstContactQueried.wake(objects[0]);
-        }
-        if (objects.length === 2 && objects.some((obj) => obj.name === secondContactName)) {
-          secondContactQueried.wake(objects.find((obj) => obj.name === secondContactName)!);
+        if (objects.length === 1) {
+          threadQueried.wake(objects[0]);
         }
       });
+      await Promise.all(performInvitation({ host: space1, guest: client2.spaces }));
 
-      {
-        // Create contact on first client.
-        const space = client1.spaces.get(spaceKey)!;
-        await space.waitUntilReady();
-        const contact = space.db.add(new Contact());
-        contact.name = firstContactName;
-        await space.db.flush();
-      }
+      // Create Thread on second client.
+      const space2 = client2.spaces.get(spaceKey)!;
+      await space2.waitUntilReady();
+      const thread2 = space2.db.add(new Thread());
+      await space2.db.flush();
 
-      {
-        // Create contact on second client.
-        const space = client2.spaces.get(spaceKey)!;
-        await space.waitUntilReady();
-        const contact = space.db.add(new Contact());
-        contact.name = secondContactName;
-        await space.db.flush();
-      }
+      const thread1 = await threadQueried.wait({ timeout: 1000 });
 
-      expect((await firstContactQueried.wait({ timeout: 1000 })).name).to.eq(firstContactName);
-      expect((await secondContactQueried.wait({ timeout: 1000 })).name).to.eq(secondContactName);
+      const text = 'Hello, Dmytro';
+      const message = space2.db.add(new Message({ blocks: [{ text }] }));
+      await space2.db.flush();
+      thread2.messages.push(message);
+
+      await waitForExpect(() => {
+        expect(thread1.messages.length).to.eq(1);
+        expect(thread1.messages[0].blocks[0].text).to.eq(text);
+      }, 1000);
     });
   });
 });
