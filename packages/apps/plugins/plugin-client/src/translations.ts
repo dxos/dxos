e1aed5dad049e668199a2cb5674f016c89a5be9f
Dmytro Maretskyi <maretskii@gmail.com>
2023-12-18 17:07:03 +0100

    feat: Add automerge switch to settings

---
+++ packages/apps/plugins/plugin-client/src/translations.ts
@@ -0,0 +1,15 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { CLIENT_PLUGIN } from './meta';
+
+export default [
+  {
+    'en-US': {
+      [CLIENT_PLUGIN]: {
+        'enable experimental automerge backend': 'Enable experimental Automerge backend (reload required)',
+      },
+    },
+  },
+];
