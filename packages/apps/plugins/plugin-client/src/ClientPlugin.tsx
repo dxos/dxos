bca74dcc501c6697c3cae3d32261b7fd8dd2c404
Josiah Witt <josiah@witt.life>
2023-12-18 09:06:16 -0500

    feat: migrate from old halo vault

--- packages/apps/plugins/plugin-client/src/ClientPlugin.tsx
+++ packages/apps/plugins/plugin-client/src/ClientPlugin.tsx
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from 'react';
 
 import type { Plugin, PluginDefinition } from '@dxos/app-framework';
 import { type TypeCollection } from '@dxos/client/echo';
-import { InvitationEncoder } from '@dxos/client/invitations';
+import { Invitation, InvitationEncoder } from '@dxos/client/invitations';
 import { Config, Defaults, Envs, Local } from '@dxos/config';
 import { registerSignalFactory } from '@dxos/echo-signals/react';
 import { log } from '@dxos/log';
@@ -36,18 +36,37 @@ export const ClientPlugin = ({
   appKey,
   ...options
 }: ClientPluginOptions): PluginDefinition<{}, ClientPluginProvides> => {
+  let client: Client;
+
   // TODO(burdon): Document.
   registerSignalFactory();
-
-  const client = new Client({ config: new Config(Envs(), Local(), Defaults()), ...options });
 
   return {
     meta,
     initialize: async () => {
       let error: unknown = null;
 
+      client = new Client({ config: new Config(await Envs(), Local(), Defaults()), ...options });
+
+      const oldClient = new Client({
+        config: new Config(
+          {
+            runtime: {
+              client: {
+                remoteSource: 'https://halo.dxos.org/vault.html',
+              },
+            },
+          },
+          await Envs(),
+          Defaults(),
+        ),
+      });
+
       try {
         await client.initialize();
+        await oldClient.initialize();
+
+        console.log({ client, oldClient });
 
         if (types) {
           client.addTypes(types);
@@ -56,8 +75,29 @@ export const ClientPlugin = ({
         // TODO(burdon): Factor out invitation logic since depends on path routing?
         const searchParams = new URLSearchParams(location.search);
         const deviceInvitationCode = searchParams.get('deviceInvitationCode');
-        if (!client.halo.identity.get() && !deviceInvitationCode) {
-          await client.halo.createIdentity();
+        const identity = client.halo.identity.get();
+        if (!identity && !deviceInvitationCode) {
+          // TODO(wittjosiah): Remove.
+          const oldIdentity = oldClient.halo.identity.get();
+          if (oldIdentity) {
+            alert(
+              'Composer must perform some database maintenance to upgrade your identity to the latest version. After continuing, please keep this window open until the app loads.',
+            );
+            const oldObs = oldClient.halo.share();
+            const newObs = client.halo.join(oldObs.get());
+
+            oldObs.subscribe(async (invitation) => {
+              switch (invitation.state) {
+                case Invitation.State.READY_FOR_AUTHENTICATION: {
+                  await newObs.authenticate(invitation.authCode!);
+                }
+              }
+            });
+
+            await newObs.wait();
+          } else {
+            await client.halo.createIdentity();
+          }
         } else if (client.halo.identity.get() && deviceInvitationCode) {
           // Ignore device invitation if identity already exists.
           // TODO(wittjosiah): Identity merging.
