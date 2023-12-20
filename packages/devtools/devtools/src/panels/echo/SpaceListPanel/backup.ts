11527d3bb7182d030873e9efd9d131e58db5f005
Mykola Veremchuk <veremchukmukola@gmail.com>
2023-12-20 16:04:35 +0200

    Delete comments

--- packages/devtools/devtools/src/panels/echo/SpaceListPanel/backup.ts
+++ packages/devtools/devtools/src/panels/echo/SpaceListPanel/backup.ts
@@ -16,10 +16,6 @@ export const importData = async (space: Space, backup: Blob) => {
   try {
     const backupString = await backup.text();
     await new Serializer().import(space.db, JSON.parse(backupString) as SerializedSpace);
-    // const folder = space.properties[Folder.schema.typename];
-    // folder.name = space.key.toHex();
-    // log.info('IMPORTED SPACE', { key: space.key.toHex() });
-    // space.properties.name = space.properties.name + ' - IMPORTED';
   } catch (err) {
     log.catch(err);
   }
