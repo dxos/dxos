712da2ca6570df0ec1e98c144956f125a81b1585
Will Shown <thure@users.noreply.github.com>
2023-12-27 11:39:45 -0800

    fix(plugin-stack,react-ui-stack): Stack DnD & overreactivity (#4975)

--- packages/ui/react-ui-stack/src/components/Stack.stories.tsx
+++ packages/ui/react-ui-stack/src/components/Stack.stories.tsx
@@ -10,7 +10,8 @@ import React, { useRef, useState } from 'react';
 import { Mosaic, type MosaicDropEvent, type MosaicMoveEvent, type MosaicOperation, Path } from '@dxos/react-ui-mosaic';
 import { withTheme } from '@dxos/storybook-utils';
 
-import { Stack, type StackSectionContent, type StackProps, type StackSectionItem } from './Stack';
+import { type StackSectionContent, type StackSectionItem } from './Section';
+import { Stack, type StackProps } from './Stack';
 import { FullscreenDecorator } from '../testing/decorators';
 import { TestObjectGenerator } from '../testing/generator';
 
@@ -121,7 +122,7 @@ const DemoStack = ({
   types,
   count = 8,
   operation = 'transfer',
-  className,
+  classNames,
 }: DemoStackProps) => {
   const [items, setItems] = useState<StackSectionItem[]>(() => {
     const generator = new TestObjectGenerator({ types });
@@ -177,7 +178,7 @@ const DemoStack = ({
   return (
     <Stack
       id={id}
-      className={className}
+      classNames={classNames}
       data-testid={id}
       SectionContent={SectionContent}
       items={items}
