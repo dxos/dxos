6ec84e6cb3083a41b97c1753f56751ee6dd1d7ed
Dmytro Maretskyi <maretskii@gmail.com>
2023-12-27 11:00:21 +0100

    reactive signal test

--- packages/core/echo/echo-schema/src/effect/reactive.test.ts
+++ packages/core/echo/echo-schema/src/effect/reactive.test.ts
@@ -1,26 +1,61 @@
 import { describe } from "@dxos/test"
 import * as S from '@effect/schema/Schema';
+import { compositeRuntime } from '../util/signal';
+import { expect } from "chai";
+
 
 export interface Reactive {} // Follows signal semantics.
 
 export type ReactiveFn = {
-  <T> (schema: S.Schema<T>): (obj: T) => T & Reactive;
   <T extends {}> (obj: T): T & Reactive;
+  // <T> (schema: S.Schema<T>): (obj: T) => T & Reactive;
 
-  typed: <T> (schema: S.Schema<T>) => (obj: T) => T & Reactive;
+  // typed: <T> (schema: S.Schema<T>) => (obj: T) => T & Reactive;
 }
 
-declare const reactive: ReactiveFn;
+const proxies = new WeakMap<object, any>();
 
-describe('Reactive', () => {
+const reactive: ReactiveFn = <T> (target: any): T & Reactive => {
+  const signal = compositeRuntime.createSignal();
+  
+  const proxy = new Proxy(target, {
+    get(target, prop, receiver) {
+      signal.notifyRead();
+      const value = Reflect.get(target, prop, receiver);
+      const existingProxy = proxies.get(value);
+      if(existingProxy) {
+        return existingProxy;
+      }
+
+      if (typeof value === 'object' && value !== null && Object.getPrototypeOf(value) === Object.prototype) {
+        return reactive(value);
+      }
+
+      return value;
+    },
+    set(target, prop, value, receiver): boolean {
+      const result = Reflect.set(target, prop, value, receiver);
+      signal.notifyWrite();
+      return result;
+    }
+  });
+  proxies.set(target, proxy);
+  return proxy;
+}
+
+describe.only('Reactive', () => {
   test('untyped', () => {
     const person = reactive({
       name: 'John',
       age: 42,
     })
+
+    expect(person.age).to.equal(42);
+    person.age = 53;
+    expect(person.age).to.equal(53);
   })
 
-  test('typed', () => {
+  test.skip('typed', () => {
     const Person = S.struct({
       name: S.string,
       age: S.optional(S.number),
@@ -34,7 +69,7 @@ describe('Reactive', () => {
     person.age = 'unknown'; // Runtime error.
   })
 
-  test('storing to echo', () => {
+  test.skip('storing to echo', () => {
     declare const db: any;
 
     const Person = S.struct({
