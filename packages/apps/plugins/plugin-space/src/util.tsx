e948ee066644506bf07bd61918b74b219e024e2e
Rich Burdon <rich.burdon@gmail.com>
2024-01-01 01:04:42 +0000

    fix: Codemirror extensions fixes (#5012)

--- packages/apps/plugins/plugin-space/src/util.tsx
+++ packages/apps/plugins/plugin-space/src/util.tsx
@@ -47,8 +47,11 @@ export const isSpace = (data: unknown): data is Space =>
     : false;
 
 export const getSpaceDisplayName = (space: Space): string | [string, { ns: string }] => {
+  console.log('::::', space.state.get());
   return (space.properties.name?.length ?? 0) > 0
     ? space.properties.name
+    : space.state.get() === SpaceState.CLOSED
+    ? ['close space label', { ns: SPACE_PLUGIN }]
     : space.state.get() !== SpaceState.READY
     ? ['loading space label', { ns: SPACE_PLUGIN }]
     : ['unnamed space label', { ns: SPACE_PLUGIN }];
