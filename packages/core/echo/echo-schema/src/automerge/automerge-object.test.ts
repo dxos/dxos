3d484ef1fe07c9b411e79605fdb101b268d19cf9
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-01-03 13:41:04 +0200

    Add test

--- packages/core/echo/echo-schema/src/automerge/automerge-object.test.ts
+++ packages/core/echo/echo-schema/src/automerge/automerge-object.test.ts
@@ -53,4 +53,16 @@ describe('AutomergeObject', () => {
     expect(contact.tasks[1]).to.eq(task2);
     expect(task2.previous).to.eq(task1);
   });
+
+  test.only('destructuring', () => {
+    setGlobalAutomergePreference(true);
+    afterTest(() => setGlobalAutomergePreference(false));
+
+    const obj = new Task({ title: 'Task' });
+    const { title } = obj;
+    expect(title).to.eq('Task');
+
+    const { ...copy } = { ...obj };
+    expect(copy.title).to.eq('Task');
+  });
 });
