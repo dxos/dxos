4d04b0ab17545d769af726b276a0acec92233633
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-01-03 14:04:55 +0200

    Add `in` test

--- packages/core/echo/echo-schema/src/automerge/automerge-object.test.ts
+++ packages/core/echo/echo-schema/src/automerge/automerge-object.test.ts
@@ -54,7 +54,7 @@ describe('AutomergeObject', () => {
     expect(task2.previous).to.eq(task1);
   });
 
-  test('destructuring', () => {
+  test('destructuring', async () => {
     setGlobalAutomergePreference(true);
     afterTest(() => setGlobalAutomergePreference(false));
 
@@ -64,5 +64,14 @@ describe('AutomergeObject', () => {
 
     const { ...copy } = { ...obj };
     expect(copy.title).to.eq('Task');
+  });
+
+  test('`in` operator', async () => {
+    setGlobalAutomergePreference(true);
+    afterTest(() => setGlobalAutomergePreference(false));
+
+    const obj = new Task({ title: 'Task' });
+    expect('title' in obj).to.eq(true);
+    expect('id' in obj).to.eq(true);
   });
 });
