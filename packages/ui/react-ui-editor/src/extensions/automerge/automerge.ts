42059c4c4baabf3ebd9e91977a3eeb3ecd0433fa
Rich Burdon <rich.burdon@gmail.com>
2024-01-19 11:35:21 -0500

    fix: Show deleted comments in storybook. (#5299)

--- packages/ui/react-ui-editor/src/extensions/automerge/automerge.ts
+++ packages/ui/react-ui-editor/src/extensions/automerge/automerge.ts
@@ -4,20 +4,16 @@
 // Ref: https://github.com/automerge/automerge-codemirror
 //
 
-import { Facet, StateField, type Extension, type Transaction } from '@codemirror/state';
+import { StateField, type Extension, type Transaction } from '@codemirror/state';
 import { ViewPlugin, type EditorView, type PluginValue, type ViewUpdate } from '@codemirror/view';
 
 import { next as A } from '@dxos/automerge/automerge';
 import { type Prop } from '@dxos/automerge/automerge';
 
 import { cursorConverter } from './cursor';
-import { effectType, type IDocHandle, isReconcileTx, type Value } from './defs';
+import { effectType, type IDocHandle, isReconcileTx, semaphoreFacet, type Value } from './defs';
 import { PatchSemaphore } from './semaphore';
 import { Cursor } from '../cursor';
-
-const semaphoreFacet = Facet.define<PatchSemaphore, PatchSemaphore>({
-  combine: (values) => values.at(-1)!, // Take last.
-});
 
 export type AutomergeOptions = {
   handle: IDocHandle;
@@ -25,19 +21,20 @@ export type AutomergeOptions = {
 };
 
 export const automerge = ({ handle, path }: AutomergeOptions): Extension => {
-  // TODO(burdon): Rename.
   const stateField: StateField<Value> = StateField.define({
     create: () => ({
       lastHeads: A.getHeads(handle.docSync()!),
       unreconciledTransactions: [],
       path: path.slice(),
     }),
+
     update: (value: Value, tr: Transaction) => {
       const result: Value = {
         lastHeads: value.lastHeads,
         unreconciledTransactions: value.unreconciledTransactions.slice(),
         path: path.slice(),
       };
+
       let clearUnreconciled = false;
       for (const effect of tr.effects) {
         if (effect.is(effectType)) {
@@ -45,6 +42,7 @@ export const automerge = ({ handle, path }: AutomergeOptions): Extension => {
           clearUnreconciled = true;
         }
       }
+
       if (clearUnreconciled) {
         result.unreconciledTransactions = [];
       } else {
@@ -52,14 +50,15 @@ export const automerge = ({ handle, path }: AutomergeOptions): Extension => {
           result.unreconciledTransactions.push(tr);
         }
       }
+
       return result;
     },
   });
 
-  const semaphore = new PatchSemaphore(stateField);
+  const semaphore = new PatchSemaphore(handle, stateField);
 
   const viewPlugin = ViewPlugin.fromClass(
-    class AutomergeCodemirrorViewPlugin implements PluginValue {
+    class AutomergeViewPlugin implements PluginValue {
       constructor(private readonly _view: EditorView) {
         handle.addListener('change', this._handleChange);
       }
@@ -67,8 +66,9 @@ export const automerge = ({ handle, path }: AutomergeOptions): Extension => {
       update(update: ViewUpdate) {
         if (update.transactions.length > 0 && update.transactions.some((tr) => !isReconcileTx(tr))) {
           // TODO(burdon): This is a hack to ensure that the view is updated after the transaction is applied.
+          //  Cannot trigger update while update is in progress.
           queueMicrotask(() => {
-            this._view.state.facet(semaphoreFacet).reconcile(handle, this._view);
+            this._view.state.facet(semaphoreFacet).reconcile(this._view);
           });
         }
       }
@@ -78,7 +78,7 @@ export const automerge = ({ handle, path }: AutomergeOptions): Extension => {
       }
 
       private _handleChange = () => {
-        this._view.state.facet(semaphoreFacet).reconcile(handle, this._view);
+        this._view.state.facet(semaphoreFacet).reconcile(this._view);
       };
     },
   );
