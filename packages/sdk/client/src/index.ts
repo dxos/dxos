9ef4f4105b36d817eed067b927e97556e8a23982
Josiah Witt <josiah@witt.life>
2024-01-19 14:47:46 -0500

    feat(client-services): add already joined error to invitations

--- packages/sdk/client/src/index.ts
+++ packages/sdk/client/src/index.ts
@@ -19,6 +19,7 @@ export {
   InvalidInvitationExtensionRoleError,
   IdentityNotInitializedError,
   InvalidInvitationError,
+  AlreadyJoinedError,
   InvalidStorageVersionError,
   SpaceNotFoundError,
   EntityNotFoundError,
