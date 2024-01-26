ec324352d8d69f5f7850068bccbbcc828904c008
Josiah Witt <josiah@witt.life>
2024-01-26 10:49:53 -0500

    feat(composer-app): introduce separate devtools entrypoint (#5373)

--- packages/apps/composer-app/vite.config.ts
+++ packages/apps/composer-app/vite.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, './index.html'),
         shell: resolve(__dirname, './shell.html'),
+        devtools: resolve(__dirname, './devtools.html'),
         'script-frame': resolve(__dirname, './script-frame/index.html'),
       },
       output: {
