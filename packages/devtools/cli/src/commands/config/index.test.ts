fd84fd4d06042f48e1ccd775a5c5d29f4dc061bf
Nathan A. Ferch <nf+github@marginal.net>
2024-01-26 14:11:24 -0500

    Revert "debug"
    This reverts commit 6d9b18da82ebf343a92e4ad39b3f9cd686d6c900.

--- packages/devtools/cli/src/commands/config/index.test.ts
+++ packages/devtools/cli/src/commands/config/index.test.ts
@@ -24,12 +24,6 @@ describe('config', () => {
     .stdout()
     .command(['config', '--json', '--config', configPath])
     .it('runs config', (ctx) => {
-      try {
-        expect(JSON.stringify(JSON.parse(ctx.stdout))).to.equal(JSON.stringify(config));
-      } catch (err) {
-        console.log(ctx.stdout);
-        console.error(err);
-        throw err;
-      }
+      expect(JSON.stringify(JSON.parse(ctx.stdout))).to.equal(JSON.stringify(config));
     });
 });
