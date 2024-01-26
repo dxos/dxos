be124a4d08391228b3e49abf667209b6c306a2cc
Nathan A. Ferch <nf+github@marginal.net>
2024-01-26 13:56:57 -0500

    fixup! debug

--- packages/devtools/cli/src/commands/config/index.test.ts
+++ packages/devtools/cli/src/commands/config/index.test.ts
@@ -20,16 +20,7 @@ describe('config', () => {
   const configPath = path.join(__dirname, '../../../config/config-default.yml');
   const config = yaml.load(String(fs.readFileSync(configPath))) as any;
 
-  test
-    .stdout()
-    .command(['config', '--json', '--config', configPath])
-    .it('runs config', (ctx) => {
-      try {
-        expect(JSON.stringify(JSON.parse(ctx.stdout))).to.equal(JSON.stringify(config));
-      } catch (err) {
-        console.log(ctx.stdout);
-        console.error(err);
-        throw err;
-      }
-    });
+  test.command(['config', '--json', '--config', configPath]).it('runs config', (ctx) => {
+    expect(JSON.stringify(JSON.parse('nothing'))).to.equal(JSON.stringify(config));
+  });
 });
