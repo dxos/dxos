9b9aa1759e3355846ef72ac6f0ed2fc5bd4d5a0b
Yaroslav Shevchuk <yarolegovich@Yaroslavs-MBP.lan>
2024-01-31 19:26:14 +0200

    option for generating test report

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, UserConfig } from 'vitest/config';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { FixGracefulFsPlugin } from '@dxos/esbuild-plugins';
+import {join} from "path";
 
 const isDebug = !!process.env.VITEST_DEBUG;
 
 function createNodeConfig() {
   return defineConfig({
     test: {
-      reporters: ['basic'],
+      ...resolveReporterConfig(),
       environment: 'node',
-      include: ['*/node/**.test.ts'],
+      include: ['**.test.ts'],
+      exclude: ["**/node_modules/**", "**/dist/**", '**/browser/**.test.ts']
     },
   });
 }
 
-function createBrowserConfig() {
+function createBrowserConfig(browserName: 'chrome') {
   return defineConfig({
     plugins: [nodePolyfills()],
     resolve: {
@@ -29,7 +31,7 @@ function createBrowserConfig() {
       },
     },
     test: {
-      reporters: ['basic'],
+      ...resolveReporterConfig(),
       include: ['*/browser/**.test.ts'],
 
       isolate: false,
@@ -38,22 +40,39 @@ function createBrowserConfig() {
           singleThread: true,
         },
       },
+
       browser: {
         enabled: true,
         headless: true,
-        name: 'chrome',
-        isolate: true,
+        name: browserName,
+        isolate: false,
       },
     },
   });
 }
 
+function resolveReporterConfig(): UserConfig["test"] {
+  if (Boolean(process.env.VITEST_XML_REPORT)) {
+    return {
+      reporters: ['junit'],
+      outputFile: join(__dirname, `vitest-reports/${String(process.env.NX_TASK_TARGET_PROJECT)}/report.xml`)
+    };
+  }
+  return {
+    reporters: ['basic']
+  };
+}
+
 function resolveConfig() {
-  switch (process.env.VITEST_ENV?.toLowerCase()) {
-    case 'browser':
-      return createBrowserConfig();
+  const environment = (process.env.VITEST_ENV ?? "").toLowerCase();
+  switch (environment) {
+    case 'chrome':
+      return createBrowserConfig(environment);
     case 'node':
     default:
+      if (environment.length > 0 && environment !== 'node') {
+        console.log("Unrecognized VITEST_ENV value, falling back to 'node': " + environment);
+      }
       return createNodeConfig();
   }
 }
