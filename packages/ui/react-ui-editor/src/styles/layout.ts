c8197c1bb0a389504a6eb324d195079d773c12d0
Will Shown <thure@users.noreply.github.com>
2024-02-05 17:42:31 -0800

    fix: Polish spacing & other styles, refactor toolbar implementation (#5543)

---
+++ packages/ui/react-ui-editor/src/styles/layout.ts
@@ -0,0 +1,6 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+export const editorWithToolbarLayout =
+  'fixed inset-0 grid grid-cols-1 grid-rows-[min-content_1fr] justify-center content-start overflow-hidden';
