0923e2eb63d11446327faf0815ee536f3ff2e330
Josiah Witt <josiah@witt.life>
2024-02-05 21:02:34 -0500

    fix e2e

--- packages/sdk/app-framework/src/plugins/IntentPlugin/helpers.ts
+++ packages/sdk/app-framework/src/plugins/IntentPlugin/helpers.ts
@@ -7,4 +7,5 @@ import { type IntentExecution } from './IntentContext';
 /**
  * Check if the chain of intents is undoable.
  */
-export const isUndoable = (chain: IntentExecution[]): boolean => chain.every(({ result }) => result.undoable);
+export const isUndoable = (chain: IntentExecution[]): boolean =>
+  chain.length > 0 && chain.every(({ result }) => result.undoable);
