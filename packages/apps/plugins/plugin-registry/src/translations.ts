19ecdf5a89bdfd6da19da75caf1337a5557e1f88
Josiah Witt <josiah@witt.life>
2024-02-07 17:28:54 -0500

    refactor: cleanup settings and plugin meta (#5553)
    Co-authored-by: Zhenya <zhenyasav@users.noreply.github.com>

--- packages/apps/plugins/plugin-registry/src/translations.ts
+++ packages/apps/plugins/plugin-registry/src/translations.ts
@@ -10,7 +10,7 @@ export default [
       [REGISTRY_PLUGIN]: {
         'plugin name': 'Registry',
         'reload required message': 'Reload required.',
-        'settings show experimental label': 'Show experimental plugins.',
+        'settings show experimental label': 'Show experimental plugins',
         'settings section installed label': 'Installed',
         'settings section recommended label': 'Recommended',
         'settings section experimental label': 'Experimental',
