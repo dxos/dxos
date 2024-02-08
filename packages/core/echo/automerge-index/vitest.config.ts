3521e118784f1402a3abd07876de0a796da8c6cf
Dmytro Maretskyi <maretskii@gmail.com>
2024-02-08 14:39:45 +0100

    Revert config

--- packages/core/echo/automerge-index/vitest.config.ts
+++ packages/core/echo/automerge-index/vitest.config.ts
@@ -8,13 +8,4 @@ import configShared from '../../../../vitest.shared';
 
 console.log(require('process').cwd());
 
-export default mergeConfig(
-  configShared,
-  defineConfig({
-    root: 'packages/core/echo/automerge-index',
-    test: {
-      dir: 'packages/core/echo/automerge-index',
-      root: 'packages/core/echo/automerge-index',
-    },
-  }),
-);
+export default mergeConfig(configShared, defineConfig({}));
