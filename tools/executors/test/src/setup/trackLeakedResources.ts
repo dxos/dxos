c84d476fb54e412255980f0c43f036d1d3d438ae
Dmytro Maretskyi <maretskii@gmail.com>
2024-02-12 22:52:16 +0100

    Lint

--- tools/executors/test/src/setup/trackLeakedResources.ts
+++ tools/executors/test/src/setup/trackLeakedResources.ts
@@ -12,7 +12,7 @@ export const mochaHooks: MochaHooks = {
   afterAll: async () => {
     const timeoutId = setTimeout(() => {
       console.log(
-        `\n\n WARNING: It looks like the process didn't exit after running tests.\n Here are the open handles:\n`,
+        "\n\n WARNING: It looks like the process didn't exit after running tests.\n Here are the open handles:\n",
       );
 
       (global as any).dxDumpLeaks?.();
