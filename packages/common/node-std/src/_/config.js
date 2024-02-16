aae3ee6864291057f0d9645c51a6dc65b9277ce5
Dmytro Maretskyi <maretskii@gmail.com>
2024-02-16 17:31:56 +0100

    Expose config from node-std

---
+++ packages/common/node-std/src/_/config.js
@@ -0,0 +1,14 @@
+export const GLOBALS = ['global', 'Buffer', 'process'];
+
+export const MODULES = [
+  'fs/promises',
+  'assert',
+  'buffer',
+  'crypto',
+  'events',
+  'fs',
+  'path',
+  'process',
+  'stream',
+  'util',
+];
---
+++ packages/common/node-std/types/_/config.d.ts
@@ -0,0 +1,2 @@
+export declare const GLOBALS: string[];
+export declare const MODULES: string[];
