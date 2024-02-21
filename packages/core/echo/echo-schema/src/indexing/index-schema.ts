f2fc92c5bbd4ce51d2febdb1458fa58670c08906
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-02-21 14:16:56 +0200

    Add index-schema

---
+++ packages/core/echo/echo-schema/src/indexing/index-schema.ts
@@ -0,0 +1,67 @@
+//
+// Copyright 2024 DXOS.org
+//
+import * as orama from '@orama/orama';
+
+import { Event } from '@dxos/async';
+import { invariant } from '@dxos/invariant';
+
+import { type IndexingType, type Index, type IndexKind } from './types';
+import { type Filter } from '../query';
+
+export class IndexSchema implements Index {
+  public readonly kind: IndexKind = { kind: 'SCHEMA_MATCH' };
+  public readonly updated = new Event<void>();
+
+  private readonly _orama: Promise<
+    orama.Orama<
+      {
+        schema: string;
+      },
+      orama.IIndex<orama.components.index.Index>,
+      orama.IDocumentsStore<orama.components.documentsStore.DocumentsStore>
+    >
+  >;
+
+  constructor() {
+    this._orama = orama.create({
+      schema: {
+        schema: 'string',
+      },
+    });
+  }
+
+  async addObject(id: string, object: IndexingType) {
+    await orama.insert<any>(await this._orama, { id, ...object });
+    this.updated.emit();
+  }
+
+  async removeObject(id: string) {
+    await orama.remove(await this._orama, id);
+  }
+
+  async updateObject(id: string, object: IndexingType) {
+    await orama.update<any>(await this._orama, id, object);
+  }
+
+  // TODO(mykola): Fix Filter type with new Reactive API.
+  async find(filter: Filter) {
+    invariant(filter.type, 'Filter type is required.');
+    // TODO(mykola): Use Schema URI.
+    const results = await orama.search(await this._orama, { term: filter.type.itemId, exact: true, threshold: 0 });
+    const ids = results.hits.map((hit) => hit.id);
+    return ids;
+  }
+
+  async serialize(): Promise<string> {
+    return JSON.stringify(await orama.save(await this._orama), null, 2);
+  }
+
+  static async load(blob: string): Promise<IndexSchema> {
+    const deserialized = JSON.parse(blob);
+
+    const index = new IndexSchema();
+    await orama.load(await index._orama, deserialized);
+    return index;
+  }
+}
