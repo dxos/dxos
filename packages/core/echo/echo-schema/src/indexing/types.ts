83294f7ca7cf787f39df7b7214b9c435f720432e
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-02-21 14:17:21 +0200

    Add types defs

---
+++ packages/core/echo/echo-schema/src/indexing/types.ts
@@ -0,0 +1,23 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import { type Event } from '@dxos/async';
+
+import { type Filter } from '../query';
+
+export type IndexKind = { kind: 'SCHEMA_MATCH' } | { kind: 'FIELD_MATCH'; field: string } | { kind: 'FULL_TEXT' };
+
+export type IndexingType = Record<string, any>;
+
+export interface Index {
+  kind: IndexKind;
+  updated: Event;
+  addObject: (id: string, object: IndexingType) => Promise<void>;
+  removeObject: (id: string) => Promise<void>;
+  updateObject: (id: string, object: IndexingType) => Promise<void>;
+
+  find: (filter: Filter) => Promise<string[]>;
+
+  serialize(): Promise<string>;
+}
