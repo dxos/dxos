1773346f8a609949ad4fcbbf4fec0d8cb44be374
Josiah Witt <josiah@witt.life>
2024-02-22 16:26:53 -0500

    fix(plugin-thread): hookup standalone threads to c11y sidebar (#5754)
    Co-authored-by: Rich Burdon <rich.burdon@gmail.com>

--- packages/apps/plugins/plugin-markdown/src/components/DocumentMain.tsx
+++ packages/apps/plugins/plugin-markdown/src/components/DocumentMain.tsx
@@ -2,10 +2,9 @@
 // Copyright 2024 DXOS.org
 //
 
-import React, { type FC, useEffect, useMemo } from 'react';
+import React, { type FC, useMemo } from 'react';
 
 import { type Document as DocumentType } from '@braneframe/types';
-import { LayoutAction, useIntentDispatcher } from '@dxos/app-framework';
 import { getSpaceForObject } from '@dxos/react-client/echo';
 import { useIdentity } from '@dxos/react-client/halo';
 import { type Comment, useTextModel } from '@dxos/react-ui-editor';
@@ -19,7 +18,6 @@ const DocumentMain: FC<{ document: DocumentType } & Pick<EditorMainProps, 'toolb
   const identity = useIdentity();
   const space = getSpaceForObject(document);
   const model = useTextModel({ identity, space, text: document.content });
-  const dispatch = useIntentDispatcher();
   const comments = useMemo<Comment[]>(() => {
     return document.comments?.map((comment) => ({ id: comment.thread!.id, cursor: comment.cursor! }));
   }, [document.comments]);
@@ -27,25 +25,6 @@ const DocumentMain: FC<{ document: DocumentType } & Pick<EditorMainProps, 'toolb
   if (!model) {
     return null;
   }
-
-  const hasComments = comments.length > 0;
-
-  // TODO(thure): once Deck is available, this should be refactored so the plugin responsible for related content can
-  //   handle its own rendering.
-  useEffect(() => {
-    if (hasComments) {
-      void dispatch({
-        action: LayoutAction.SET_LAYOUT,
-        data: { element: 'complementary', subject: document, state: true },
-      });
-    }
-    return () => {
-      void dispatch({
-        action: LayoutAction.SET_LAYOUT,
-        data: { element: 'complementary', subject: null, state: false },
-      });
-    };
-  }, [document.id, hasComments]);
 
   return <EditorMain model={model} comments={comments} {...props} />;
 };
