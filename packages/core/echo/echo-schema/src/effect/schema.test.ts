ebae4c9dbfe3f06d9d549f2cb908fc04919aca50
yarolegovich <yarolegovich@gmail.com>
2024-02-24 14:35:00 +0100

    changed example type

--- packages/core/echo/echo-schema/src/effect/schema.test.ts
+++ packages/core/echo/echo-schema/src/effect/schema.test.ts
@@ -7,50 +7,43 @@ import { expect } from 'chai';
 
 import { test } from '@dxos/test';
 
-import { EchoObject, EchoSchema } from './schema';
+import { EchoObject } from './schema';
 
-export class Table extends EchoObject.extend<Table>()({
-  title: S.string,
-  schema: EchoSchema,
-  props: S.array(
-    S.struct({
-      id: S.string,
-      prop: S.string,
-      label: S.string,
-      refProp: S.string,
-      size: S.number,
-    }),
-  ),
+export class Employee extends EchoObject.extend<Employee>()({
+  name: S.string,
+  salary: S.number,
+}) {}
+
+export class Organization extends EchoObject.extend<Organization>()({
+  name: S.string,
+  employees: S.array(Employee),
 }) {}
 
 test('valid properties', () => {
-  const table = new Table({
-    title: 'Example',
-    schema: new EchoSchema({ name: 'Test', version: '1.0.0', schema: '{}' }),
-    props: [],
+  const org = new Organization({
+    name: 'NewComp',
+    employees: [new Employee({ name: 'John', salary: 100 })],
   });
-  const t = table.__schema();
-  S.validateSync(t)(table);
-  expect(table.id).not.to.be.undefined;
+  const t = org.__schema();
+  S.validateSync(t)(org);
+  expect(org.id).not.to.be.undefined;
 });
 
 test('is echo object test', () => {
   const properties = {
-    title: 'Example',
-    schema: new EchoSchema({ name: 'Test', version: '1.0.0', schema: '{}' }),
-    props: [],
+    name: 'NewComp',
+    employees: [new Employee({ name: 'John', salary: 100 })],
   };
-  const table = new Table(properties);
-  S.validateSync(EchoObject)(table);
+  const org = new Organization(properties);
+  S.validateSync(EchoObject)(org);
   expect(() => S.validateSync(EchoObject)(properties)).to.throw();
 });
 
 test('invalid properties', () => {
   expect(
     () =>
-      new Table({
-        schema: new EchoSchema({ name: 'Test', version: '1.0.0', schema: '{}' }),
-        props: [],
+      new Organization({
+        employees: [new Employee({ name: 'John', salary: 100 })],
       } as any),
   ).to.throw();
 });
