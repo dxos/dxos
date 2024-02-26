e18881f35820327558beaa87e4e2aa10e6df0883
Rich Burdon <rich.burdon@gmail.com>
2024-02-26 21:01:40 +0100

    fix: Editor scrolling hack (#5776)

--- packages/ui/react-ui-editor/src/styles/layout.ts
+++ packages/ui/react-ui-editor/src/styles/layout.ts
@@ -5,8 +5,5 @@
 export const editorWithToolbarLayout =
   'grid grid-cols-1 grid-rows-[min-content_1fr] data-[toolbar=disabled]:grid-rows-[1fr] justify-center content-start overflow-hidden';
 
-export const editorFillLayoutRoot = 'bs-full overflow-hidden grid';
-export const editorFillLayoutEditor =
-  'bs-full overflow-hidden [&>.cm-scroller]:bs-full [&>.cm-scroller]:overflow-y-auto';
-
-export const editorHalfViewportOverscrollContent = 'after:block after:min-bs-[50dvh] after:pointer-events-none';
+export const editorFillLayoutRoot = 'bs-full relative';
+export const editorFillLayoutEditor = '!absolute inset-0';
