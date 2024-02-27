3bc2a0d5d0219f13d7d17bb7ec655654eedefea2
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-02-27 16:56:34 +0200

    Add TODO

--- packages/core/echo/echo-schema/src/indexing/util.tsx
+++ packages/core/echo/echo-schema/src/indexing/util.tsx
@@ -4,6 +4,7 @@
 
 import { type File } from '@dxos/random-access-storage';
 
+// TODO(mykola): Make temp file and moveTo desired path.
 export const overrideFile = async (file: File, content: Buffer) => {
   const { size } = await file.stat();
   if (content.length < size) {
