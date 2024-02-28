f7d6ec2e8e362c3e46871eae4ac3918bd11f80c5
yarolegovich <yarolegovich@gmail.com>
2024-02-28 17:23:50 +0100

    simple single bus class

---
+++ packages/core/functions/src/signal/signal-bus.test.ts
@@ -0,0 +1,11 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import { test, describe } from '@dxos/test';
+
+describe('Signal bus', () => {
+  test('loopback', () => {
+    // TODO
+  });
+});
---
+++ packages/core/functions/src/signal/signal-bus.ts
@@ -0,0 +1,49 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import * as S from '@effect/schema/Schema';
+import * as Either from 'effect/Either';
+
+import { type Space } from '@dxos/client/echo';
+import { log } from '@dxos/log';
+
+const Signal = S.struct({
+  id: S.string,
+  kind: S.literal('echo-mutation', 'timer', 'attention', 'suggestion'),
+  metadata: S.struct({
+    created: S.number,
+    source: S.string,
+    triggerSignalId: S.optional(S.string),
+    spaceKey: S.optional(S.string),
+  }),
+  data: S.struct({
+    type: S.string.pipe(S.description('Value schema name')),
+    value: S.any,
+  }),
+});
+export type Signal = S.Schema.From<typeof Signal>;
+
+/**
+ * - filter
+ * - deduping signals
+ * -
+ */
+export class SignalBus {
+  public constructor(private readonly space: Space) {}
+
+  async emit(signal: Signal) {
+    await this.space.postMessage('signals', signal);
+  }
+
+  subscribe(callback: (signal: Signal) => void): () => void {
+    return this.space.listen('signals', (message) => {
+      const decoded = S.decodeEither<Signal, Signal>(Signal)(message.payload);
+      if (Either.isRight(decoded)) {
+        callback(decoded.right);
+      } else {
+        log.warn('malformed signal received', { error: decoded.left });
+      }
+    });
+  }
+}
