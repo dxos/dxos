942186d664da9c78e5d2535ae7155e9360d6e0ab
Yaroslav <yaroslav.shevchuk@bolt.eu>
2024-03-07 18:16:37 +0100

    feat(sentry): crash reporting improvements (#5872)

--- packages/sdk/observability/src/sentry/node.ts
+++ packages/sdk/observability/src/sentry/node.ts
@@ -9,12 +9,11 @@ import {
   addBreadcrumb as naturalAddBreadcrumb,
   captureException as naturalCaptureException,
   captureMessage as naturalCaptureMessage,
-  withScope,
+  withScope as naturalWithScope,
 } from '@sentry/node';
 import type { Event } from '@sentry/node';
-import type { SeverityLevel } from '@sentry/types';
 
-import { log, LogLevel, shouldLog } from '@dxos/log';
+import { log } from '@dxos/log';
 
 import { type InitOptions } from './types';
 
@@ -103,51 +102,15 @@ export const captureException: typeof naturalCaptureException = (exception, capt
   }
 };
 
-// TODO(nf): move to log package as a first-class processor.
-export const enableSentryLogProcessor = (filter?: string | string[] | LogLevel) => {
-  log.runtimeConfig.processors.push(SENTRY_LOG_PROCESSOR);
+export const captureMessage: typeof naturalCaptureMessage = (exception, captureContext) => {
+  try {
+    const eventId = naturalCaptureMessage(exception, captureContext);
+    log('capture message', { exception, eventId, captureContext });
+    return eventId;
+  } catch (err) {
+    log.catch('Failed to capture message', err);
+    return 'unknown';
+  }
 };
 
-export const SENTRY_LOG_PROCESSOR = (config: any, entry: any) => {
-  const { message, level, meta, error } = entry;
-  if (!shouldLog(entry, config.captureFilters)) {
-    return;
-  }
-  // TODO(nf): add rate limiting to avoid spamming Sentry/consuming excessive quota.
-  withScope((scope) => {
-    scope.setLevel(convertLevel(level));
-    scope.setContext('dxoslog', entry.context);
-    if (meta) {
-      scope.setTransactionName(`${getRelativeFilename(meta.F)}:${meta.L}`);
-    }
-    // Is this ever used?
-    if (error) {
-      scope.setExtra('message', message);
-      return naturalCaptureException(error);
-    }
-
-    naturalCaptureMessage(message);
-  });
-};
-
-const convertLevel = (level: LogLevel): SeverityLevel => {
-  if (level === LogLevel.TRACE) {
-    return 'debug';
-  }
-  if (level === LogLevel.WARN) {
-    return 'warning';
-  }
-  return LogLevel[level].toLowerCase() as SeverityLevel;
-};
-
-const getRelativeFilename = (filename: string) => {
-  // TODO(burdon): Hack uses "packages" as an anchor (pre-parse NX?)
-  // Including `packages/` part of the path so that excluded paths (e.g. from dist) are clickable in vscode.
-  const match = filename.match(/.+\/(packages\/.+\/.+)/);
-  if (match) {
-    const [, filePath] = match;
-    return filePath;
-  }
-
-  return filename;
-};
+export const withScope = naturalWithScope;
