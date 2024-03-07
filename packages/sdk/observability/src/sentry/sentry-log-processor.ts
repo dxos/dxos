6007d991eba087c7c93927daa128bd5bbe1f7dcc
yarolegovich <yarolegovich@gmail.com>
2024-03-07 10:42:35 +0200

    chore(sentry): improve sentry log collection
    disable fetch and console integrations: fetch only happens for segment api polluting our breadcrumbs
    console breadcrumbs are going to be collected manually from a custom log processor
    sentry log processor is now shared between node and browser. sets a shared worker tag by examining log message and extracts error from context

---
+++ packages/sdk/observability/src/sentry/sentry-log-processor.ts
@@ -0,0 +1,74 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import { withScope } from '@sentry/node';
+import type { SeverityLevel } from '@sentry/types';
+
+import { type LogConfig, type LogEntry, LogLevel, type LogProcessor, shouldLog } from '@dxos/log';
+import { getPrototypeSpecificInstanceId } from '@dxos/util';
+
+import { captureException, captureMessage } from './';
+
+export const createSentryLogProcessor = (): LogProcessor => {
+  return (config: LogConfig, entry: LogEntry) => {
+    const { message, level, meta, error } = entry;
+    if (!shouldLog(entry, config.captureFilters)) {
+      return;
+    }
+    if (entry.level !== LogLevel.WARN && entry.level !== LogLevel.ERROR) {
+      return;
+    }
+    // TODO(nf): add rate limiting to avoid spamming Sentry/consuming excessive quota.
+    withScope((scope) => {
+      scope.setLevel(convertLevel(level));
+      scope.setContext('dxoslog', entry.context);
+      if (meta) {
+        scope.setTag('transaction', `${getRelativeFilename(meta.F)}:${meta.L}`);
+      }
+      if (message.startsWith('[worker] ')) {
+        scope.setTag('shared_worker', true);
+      }
+
+      let capturedError = error;
+      if (capturedError == null && entry.level === LogLevel.ERROR) {
+        capturedError = Object.values(context).find((v) => v instanceof Error);
+      }
+      if (capturedError) {
+        scope.setExtra('message', message);
+        return captureException(capturedError);
+      }
+
+      let scopePrefix: string | undefined;
+      if (entry.meta?.S) {
+        const scope = entry.meta?.S;
+        const prototype = Object.getPrototypeOf(scope);
+        const id = getPrototypeSpecificInstanceId(scope);
+        scopePrefix = `${prototype.constructor.name}#${id}`;
+      }
+
+      captureMessage(scopePrefix ? `${scopePrefix} ${message}` : message);
+    });
+  };
+};
+const convertLevel = (level: LogLevel): SeverityLevel => {
+  if (level === LogLevel.TRACE) {
+    return 'debug';
+  }
+  if (level === LogLevel.WARN) {
+    return 'warning';
+  }
+  return LogLevel[level].toLowerCase() as SeverityLevel;
+};
+
+const getRelativeFilename = (filename: string) => {
+  // TODO(burdon): Hack uses "packages" as an anchor (pre-parse NX?)
+  // Including `packages/` part of the path so that excluded paths (e.g. from dist) are clickable in vscode.
+  const match = filename.match(/.+\/(packages\/.+\/.+)/);
+  if (match) {
+    const [, filePath] = match;
+    return filePath;
+  }
+
+  return filename;
+};
