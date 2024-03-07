dd3a76f86248c95e81fdb06a493d977d21b6a028
Dmytro Maretskyi <maretskii@gmail.com>
2024-03-07 10:34:10 +0100

    Add composer tag to properties schema

--- packages/sdk/client-protocol/src/schema.ts
+++ packages/sdk/client-protocol/src/schema.ts
@@ -3,4 +3,6 @@ import * as E from '@dxos/echo-schema';
 
 export const PropertiesSchema = S.struct({
   name: S.optional(S.string),
+  // TODO(dmaretskyi): Remove.
+  'composer.dxos.org.opened': S.optional(S.number),
 }).pipe(E.echoObject('dxos.sdk.client.Properties', '0.1.0'));
