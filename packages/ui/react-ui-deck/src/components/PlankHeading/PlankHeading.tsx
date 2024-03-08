0132fdf797f73d7bb3c1d0e069c4a112d1ce96e8
thure <will@braneframe.com>
2024-03-08 15:53:09 -0800

    Use horizontal bar to annotate menu buttons.

--- packages/ui/react-ui-deck/src/components/PlankHeading/PlankHeading.tsx
+++ packages/ui/react-ui-deck/src/components/PlankHeading/PlankHeading.tsx
@@ -31,16 +31,40 @@ const plankHeadingIconProps: IconProps = {
   weight: 'duotone',
 };
 
+const MenuSignifierHorizontal = () => (
+  <svg className='absolute block-end-2' width={20} height={2} viewBox='0 0 20 2' stroke='currentColor'>
+    <line
+      x1={0.5}
+      y1={0.75}
+      x2={19}
+      y2={0.75}
+      strokeWidth={1.25}
+      strokeLinecap='round'
+      strokeDasharray='6 20'
+      strokeDashoffset='-6.5'
+    />
+  </svg>
+);
+
+const MenuSignifierVertical = () => (
+  <svg className='absolute inline-start-1' width={2} height={18} viewBox='0 0 2 18' stroke='currentColor'>
+    <line x1={1} y1={3} x2={1} y2={18} strokeWidth={1.5} strokeLinecap='round' strokeDasharray='0 6' />
+  </svg>
+);
+
 const PlankHeadingButton = forwardRef<HTMLButtonElement, PlankHeadingButtonProps>(
-  ({ attendableId, classNames, ...props }, forwardedRef) => {
+  ({ attendableId, classNames, children, ...props }, forwardedRef) => {
     const hasAttention = useHasAttention(attendableId);
     return (
       <Button
         {...props}
         variant={hasAttention ? 'primary' : 'ghost'}
-        classNames={['m-1 shrink-0 pli-0 min-bs-0 is-[--rail-action] bs-[--rail-action]', classNames]}
+        classNames={['m-1 shrink-0 pli-0 min-bs-0 is-[--rail-action] bs-[--rail-action] relative', classNames]}
         ref={forwardedRef}
-      />
+      >
+        <MenuSignifierHorizontal />
+        {children}
+      </Button>
     );
   },
 );
