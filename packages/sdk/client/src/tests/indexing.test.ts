249c27e4ac945f08f21417af478c526aea67a62a
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-03-08 13:43:59 +0200

    Fix test

--- packages/sdk/client/src/tests/indexing.test.ts
+++ packages/sdk/client/src/tests/indexing.test.ts
@@ -6,16 +6,18 @@ import { Contact } from '@braneframe/types';
 import { Trigger, asyncTimeout } from '@dxos/async';
 import { getHeads } from '@dxos/automerge/automerge';
 import { warnAfterTimeout } from '@dxos/debug';
-import { IndexQueryProvider, IndexStore, Indexer } from '@dxos/indexing';
-import { PublicKey } from '@dxos/keys';
+import { IndexServiceImpl, IndexStore, Indexer } from '@dxos/indexing';
+import { log } from '@dxos/log';
 import { idCodec } from '@dxos/protocols';
+import { IndexKind } from '@dxos/protocols/proto/dxos/echo/indexing';
 import { StorageType, createStorage } from '@dxos/random-access-storage';
 import { afterTest, describe, test } from '@dxos/test';
 
 import { Client } from '../client';
+import { IndexQuerySourceProvider } from '../echo/index-query-source-provider';
 import { TestBuilder } from '../testing';
 
-describe('Index queries', () => {
+describe.only('Index queries', () => {
   test('basic index queries', async () => {
     const builder = new TestBuilder();
     builder.storage = createStorage({ type: StorageType.RAM });
@@ -46,30 +48,14 @@ describe('Index queries', () => {
         return snapshots.filter((snapshot) => snapshot.object);
       },
     });
-    indexer.setIndexConfig({ indexes: [{ kind: 'SCHEMA_MATCH' }] });
+
+    indexer.setIndexConfig({ indexes: [{ kind: IndexKind.Kind.SCHEMA_MATCH }] });
     await indexer.initialize();
 
-    const agentQuerySourceProvider = new IndexQueryProvider({
-      indexer,
-      loadObjects: (ids) =>
-        Promise.all(
-          ids.map(async (id) => {
-            const { objectId, documentId } = idCodec.decode(id);
-            const handle = client.spaces._automergeContext.repo.find(documentId as any);
-            await warnAfterTimeout(1000, 'to long to load doc', () => handle.whenReady());
-            const doc = handle.docSync();
-            const rawSpaceKey = doc.access?.spaceKey ?? doc.experimental_spaceKey;
-
-            if (!rawSpaceKey) {
-              throw new Error('Space key is not available');
-            }
-
-            const spaceKey = PublicKey.from(rawSpaceKey);
-            const space = client.spaces.get(spaceKey);
-            await space!.waitUntilReady();
-            return space!.db.getObjectById(objectId)!;
-          }),
-        ),
+    const service = new IndexServiceImpl({ indexer, automergeHost: services.host!.context.automergeHost });
+    const agentQuerySourceProvider = new IndexQuerySourceProvider({
+      service,
+      spaceList: client.spaces,
     });
     client._graph.registerQuerySourceProvider(agentQuerySourceProvider);
     const space = await client.spaces.create();
@@ -80,7 +66,6 @@ describe('Index queries', () => {
       space.db.add(contact);
       await space.db.flush();
     }
-
     await asyncTimeout(indexingDone, 1000);
 
     {
