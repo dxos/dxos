f03409d959a5d7d60ebb436128b08970abdbeed6
Nathan A. Ferch <nf+github@marginal.net>
2024-03-13 17:55:04 -0400

    fix: allow localStorage.dxlog to override config

--- packages/common/log/src/platform/browser/index.ts
+++ packages/common/log/src/platform/browser/index.ts
@@ -6,6 +6,9 @@ import { type LogOptions } from '../../config';
 
 // NOTE: Implementation for the browser. See `package.json`.
 export const loadOptions = (filepath?: string): LogOptions | undefined => {
+  if (localStorage.getItem('dxlog') === null) {
+    return undefined;
+  }
   try {
     return JSON.parse(localStorage.getItem('dxlog')!);
   } catch (err) {
