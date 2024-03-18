611dfc31409f26e9a73cec66c6084571a285b1e3
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2024-03-18 17:10:40 +0100

    fix: Context dispose mutating callback list during dispose process (#5971)

--- packages/common/context/src/context.test.ts
+++ packages/common/context/src/context.test.ts
@@ -58,4 +58,39 @@ describe('Context', () => {
     void ctx.dispose();
     expect(called).toBeTruthy();
   });
+
+  test('canceling a context with derived contexts calls dispose hooks', async () => {
+    let childCalled = false;
+    let parentCalled = false;
+
+    const parentCtx = new Context();
+    parentCtx.onDispose(() => {
+      parentCalled = true;
+    });
+
+    const childCtx = parentCtx.derive();
+    childCtx.onDispose(() => {
+      childCalled = true;
+    });
+
+    await parentCtx.dispose();
+    expect(parentCalled).toBeTruthy();
+    expect(childCalled).toBeTruthy();
+  });
+
+  test('callbacks are called in reverse order', async () => {
+    const ctx = new Context();
+
+    const order: number[] = [];
+    ctx.onDispose(() => {
+      order.push(1);
+    });
+
+    ctx.onDispose(() => {
+      order.push(2);
+    });
+
+    await ctx.dispose();
+    expect(order).toEqual([2, 1]);
+  });
 });
