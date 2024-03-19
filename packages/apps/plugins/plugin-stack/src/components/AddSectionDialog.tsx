9ac5191208eea81fbe1d279072e00c6dc430b36a
thure <will@braneframe.com>
2024-03-19 14:35:38 -0700

    Add SearchList content to AddSectionDialog.

--- packages/apps/plugins/plugin-stack/src/components/AddSectionDialog.tsx
+++ packages/apps/plugins/plugin-stack/src/components/AddSectionDialog.tsx
@@ -3,10 +3,14 @@
 //
 import React from 'react';
 
-import { Dialog, useTranslation } from '@dxos/react-ui';
+import { usePlugin } from '@dxos/app-framework';
+import { Dialog, toLocalizedString, useTranslation } from '@dxos/react-ui';
+import { SearchList } from '@dxos/react-ui-searchlist';
 import { type AddSectionPosition } from '@dxos/react-ui-stack';
+import { getSize, mx } from '@dxos/react-ui-theme';
 
 import { STACK_PLUGIN } from '../meta';
+import { type StackPluginProvides } from '../types';
 
 type AddSectionDialogProps = { path?: string; position: AddSectionPosition };
 
@@ -22,11 +26,31 @@ export const dataHasAddSectionDialogProps = (data: any): data is { subject: AddS
 
 export const AddSectionDialog = ({ path, position }: AddSectionDialogProps) => {
   const { t } = useTranslation(STACK_PLUGIN);
+  const stackPlugin = usePlugin<StackPluginProvides>(STACK_PLUGIN);
   return (
     <Dialog.Content>
-      <Dialog.Title>{t('add section dialog title')}</Dialog.Title>
-      <span>{path}</span>
-      <span>{position}</span>
+      <Dialog.Title>{t(`add section ${position} dialog title`)}</Dialog.Title>
+      <SearchList.Root label={t('add section input placeholder')}>
+        <SearchList.Input placeholder={t('add section input placeholder')} classNames='pli-3' />
+        <SearchList.Content classNames='min-bs-[12rem] bs-[50dvh] max-bs-[30rem] overflow-auto'>
+          {stackPlugin?.provides?.stack.creators?.map(({ id, icon: Icon, label: propsLabel }) => {
+            const label = toLocalizedString(propsLabel, t);
+            return (
+              <SearchList.Item
+                value={label}
+                key={id}
+                onSelect={() => {
+                  // TODO(thure): Create section
+                }}
+                classNames='flex items-center gap-2 pli-2'
+              >
+                {Icon && <Icon className={mx(getSize(4), 'shrink-0')} />}
+                <span className='grow truncate'>{label}</span>
+              </SearchList.Item>
+            );
+          })}
+        </SearchList.Content>
+      </SearchList.Root>
     </Dialog.Content>
   );
 };
