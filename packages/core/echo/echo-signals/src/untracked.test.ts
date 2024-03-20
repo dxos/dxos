df7e8664f644eb17a1eed51c443d66b9c06cfd89
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-03-20 10:55:16 +0200

    chore: Bump version of preact/signals-core

--- packages/core/echo/echo-signals/src/untracked.test.ts
+++ packages/core/echo/echo-signals/src/untracked.test.ts
@@ -6,7 +6,7 @@ import { effect, signal, untracked } from '@preact/signals-core';
 import { expect } from 'chai';
 
 // TODO(mykola): Unskip on `preact/signals-core` version bump.
-describe.skip('Untracked', () => {
+describe('Untracked', () => {
   it('Nested `untracked` does not cause effect to run', async () => {
     const thisSignal = signal({});
     let updateCount = 0;
