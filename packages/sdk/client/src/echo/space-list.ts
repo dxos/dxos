aae4f610f5b4beae3924050622c9bc14a89f1601
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-03-22 14:41:53 +0200

    Delete async

--- packages/sdk/client/src/echo/space-list.ts
+++ packages/sdk/client/src/echo/space-list.ts
@@ -124,7 +124,7 @@ export class SpaceList extends MulticastObservable<Space[]> implements Echo {
     const gotInitialUpdate = new Trigger();
 
     const spacesStream = this._serviceProvider.services.SpacesService.querySpaces();
-    spacesStream.subscribe(async (data) => {
+    spacesStream.subscribe((data) => {
       let emitUpdate = false;
       const newSpaces = this.get() as SpaceProxy[];
 
