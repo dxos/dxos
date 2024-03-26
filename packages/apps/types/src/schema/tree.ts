f015247b1734f18c5cf3067767153dd339374182
Dmytro Maretskyi <maretskii@gmail.com>
2024-03-26 11:59:30 +0100

    Recursive types

--- packages/apps/types/src/schema/tree.ts
+++ packages/apps/types/src/schema/tree.ts
@@ -7,18 +7,11 @@ import * as S from '@effect/schema/Schema';
 import * as E from '@dxos/echo-schema';
 import { TextV0Type } from './document';
 
-interface _TreeItemType extends E.Identifiable {
-  text: TextV0Type;
-  items: _TreeItemType[];
-  done: boolean;
-}
-const _TreeItemType: S.Schema<_TreeItemType> = S.struct({
+export class TreeItemType extends E.EchoObjectSchema({ typename: 'braneframe.Tree.Item', version: '0.1.0' })({
   text: E.ref(TextV0Type),
-  items: S.array(S.suspend(() => _TreeItemType)),
+  items: S.array(S.suspend((): S.Schema<TreeItemType> => TreeItemType)),
   done: S.boolean,
-}).pipe(E.echoObject('braneframe.Tree.Item', '0.1.0'));
-export interface TreeItemType extends E.ObjectType<typeof _TreeItemType> {}
-export const TreeItemType: S.Schema<TreeItemType> = _TreeItemType;
+}) {}
 
 export class TreeType extends E.EchoObjectSchema({ typename: 'braneframe.Tree', version: '0.1.0' })({
   title: S.string,
