063460d8e1fe8f2bab6ac838732e426caac16b3e
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2024-03-27 14:36:28 -0700

    ✨ Switch column menu to dropdown

--- packages/ui/react-ui-table/src/components/ColumnMenu.tsx
+++ packages/ui/react-ui-table/src/components/ColumnMenu.tsx
@@ -2,15 +2,24 @@
 // Copyright 2023 DXOS.org
 //
 
-import { Check, Trash, X, GearSix } from '@phosphor-icons/react';
+import { Check, Trash, X, GearSix, CaretDown, ArrowDown, ArrowUp } from '@phosphor-icons/react';
 import { type HeaderContext, type RowData } from '@tanstack/react-table';
-import React, { type FC, type PropsWithChildren, useRef, useState } from 'react';
+import React, { type FC, type PropsWithChildren, useRef, useState, useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
-import { Button, DensityProvider, Input, Popover, Select, Separator, useTranslation } from '@dxos/react-ui';
+import {
+  Button,
+  DensityProvider,
+  Input,
+  Popover,
+  Select,
+  Separator,
+  useTranslation,
+  DropdownMenu,
+} from '@dxos/react-ui';
 import { getSize, mx } from '@dxos/react-ui-theme';
 import { safeParseInt } from '@dxos/util';
 
-import { SortButton } from './SortButton';
 import { type TableDef, type ColumnProps, type ColumnType } from '../schema';
 import { translationKey } from '../translations';
 
@@ -29,8 +38,18 @@ export const ColumnMenu = <TData extends RowData, TValue>({ column, ...props }: 
   const header = props.context.header;
 
   const canSort = header.column.getCanSort();
-  const sortDirection = header.column.getIsSorted();
+  // const sortDirection = header.column.getIsSorted();
   const toggleSort = header.column.getToggleSortingHandler();
+
+  const columnSettingsAnchorRef = useRef<any>(null);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
+
+  const [isColumnSettingsOpen, setIsColumnSettingsOpen] = useState(false);
 
   return (
     <div className='flex items-center gap-2'>
@@ -38,8 +57,51 @@ export const ColumnMenu = <TData extends RowData, TValue>({ column, ...props }: 
         {title}
       </div>
       <div className='flex gap-0'>
-        {canSort && toggleSort && <SortButton sortDirection={sortDirection} onClick={toggleSort} />}
-        <ColumnPanel {...props} column={column} />
+        {isMounted && (
+          <ColumnSettingsPanel
+            {...props}
+            column={column}
+            anchorNode={columnSettingsAnchorRef.current}
+            open={isColumnSettingsOpen}
+            setOpen={setIsColumnSettingsOpen}
+          />
+        )}
+
+        <DropdownMenu.Root defaultOpen>
+          <DropdownMenu.Trigger ref={columnSettingsAnchorRef}>
+            <Button variant='ghost'>
+              <CaretDown className={getSize(4)} />
+            </Button>
+          </DropdownMenu.Trigger>
+
+          <DropdownMenu.Content sideOffset={4} collisionPadding={8}>
+            <DropdownMenu.Viewport>
+              {canSort && toggleSort && (
+                <>
+                  <DropdownMenu.Item onClick={toggleSort}>
+                    <span className='grow'>Sort ascending</span>
+                    <span className='opacity-50'>
+                      <ArrowUp className={getSize(4)} />
+                    </span>
+                  </DropdownMenu.Item>
+                  <DropdownMenu.Item onClick={toggleSort}>
+                    <span className='grow'>Sort descending</span>
+                    <span className='opacity-50'>
+                      <ArrowDown className={getSize(4)} />
+                    </span>
+                  </DropdownMenu.Item>
+                </>
+              )}
+              <DropdownMenu.Item onClick={() => setTimeout(() => setIsColumnSettingsOpen(true), 15)}>
+                <span className='grow'>Column settings</span>
+                <span className='opacity-50'>
+                  <GearSix className={mx(getSize(4), 'rotate-90')} />
+                </span>
+              </DropdownMenu.Item>
+            </DropdownMenu.Viewport>
+            <DropdownMenu.Arrow />
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       </div>
     </div>
   );
@@ -51,15 +113,55 @@ const Section: FC<PropsWithChildren & { className?: string }> = ({ children, cla
   </div>
 );
 
-export const ColumnPanel = <TData extends RowData, TValue>({
-  context,
+export const ColumnSettingsPanel = <TData extends RowData, TValue>({
   tableDefs,
   tableDef,
   column,
   onUpdate,
   onDelete,
-}: ColumnMenuProps<TData, TValue>) => {
-  const [open, setOpen] = useState(false);
+  anchorNode,
+  open,
+  setOpen,
+}: ColumnMenuProps<TData, TValue> & { anchorNode: any; open: boolean; setOpen: (b: boolean) => void }) => {
+  return (
+    <Popover.Root open={open} onOpenChange={(o) => setOpen(o)}>
+      {createPortal(<Popover.Anchor></Popover.Anchor>, anchorNode)}
+      <Popover.Portal>
+        <Popover.Content>
+          <Popover.Viewport classNames='w-60'>
+            <DensityProvider density='fine'>
+              <ColumnSettingsForm
+                column={column}
+                tableDefs={tableDefs}
+                tableDef={tableDef}
+                onUpdate={onUpdate}
+                onDelete={onDelete}
+                onClose={() => setOpen(false)}
+              />
+            </DensityProvider>
+          </Popover.Viewport>
+          <Popover.Arrow />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  );
+};
+
+export const ColumnSettingsForm = ({
+  column,
+  tableDef,
+  tableDefs,
+  onUpdate,
+  onDelete,
+  onClose,
+}: {
+  column: ColumnProps;
+  tableDef: TableDef;
+  tableDefs: TableDef[];
+  onUpdate?: ((id: string, column: ColumnProps) => void) | undefined;
+  onDelete?: ((id: string) => void) | undefined;
+  onClose: () => void;
+}) => {
   const [prop, setProp] = useState(column.id);
   const [refTable, setRefTable] = useState(column.refTable);
   const [refProp, setRefProp] = useState(column.refProp);
@@ -71,7 +173,7 @@ export const ColumnPanel = <TData extends RowData, TValue>({
 
   const handleCancel = () => {
     setProp(column.id);
-    setOpen(false);
+    onClose;
   };
 
   const handleSave = () => {
@@ -83,6 +185,7 @@ export const ColumnPanel = <TData extends RowData, TValue>({
 
     // If already exists then check same type.
     const current = tableDef.columns.find((c) => c.id !== column.id && c.id === prop);
+
     if (current) {
       // TODO(burdon): Allow multiple columns with different refProps. Ensure correct table prop is updated.
       if (current.type !== 'ref' /* || current.ref !== refTable */) {
@@ -102,147 +205,131 @@ export const ColumnPanel = <TData extends RowData, TValue>({
       digits: safeParseInt(digits),
     });
 
-    setOpen(false);
+    onClose();
   };
 
   return (
-    <Popover.Root open={open} onOpenChange={(nextOpen) => setOpen(nextOpen)}>
-      <Popover.Trigger asChild>
-        <Button variant='ghost'>
-          <GearSix className={mx(getSize(4), 'rotate-90')} />
+    <div className='flex flex-col gap-1'>
+      <Section>
+        <Input.Root>
+          <Input.Label classNames='mbe-1'>{t('column label label')}</Input.Label>
+          <Input.TextInput
+            placeholder='Column label'
+            value={label ?? ''}
+            onChange={(event) => setLabel(event.target.value)}
+            autoFocus
+          />
+        </Input.Root>
+        <Input.Root>
+          <Input.Label classNames='mbe-1 mbs-3'>{t('property key label')}</Input.Label>
+          <Input.TextInput
+            ref={propRef}
+            placeholder='Property key'
+            // TODO(burdon): Provide hooks for value normalization, ENTER, ESC, etc.
+            value={prop ?? ''}
+            onChange={(event) => setProp(event.target.value.replace(/[^\w_]/g, ''))}
+          />
+        </Input.Root>
+        <Input.Root>
+          <Input.Label classNames='mbe-1 mbs-3'>{t('column type label')}</Input.Label>
+          <Select.Root value={type} onValueChange={setType}>
+            <Select.TriggerButton placeholder='Type' classNames='is-full' />
+            <Select.Portal>
+              <Select.Content>
+                <Select.Viewport>
+                  {(['number', 'boolean', 'string', 'ref'] as ColumnType[]).map((type) => (
+                    <Select.Option key={type} value={type}>
+                      {t(`${type} column type label`)}
+                    </Select.Option>
+                  ))}
+                </Select.Viewport>
+              </Select.Content>
+            </Select.Portal>
+          </Select.Root>
+        </Input.Root>
+      </Section>
+
+      {type === 'number' && (
+        <>
+          <Section>
+            <Input.Root>
+              <Input.Label classNames='mbe-1 mbs-3'>{t('digits label')}</Input.Label>
+              {/* TODO(burdon): Constrain input to numbers. */}
+              <Input.TextInput value={digits ?? ''} onChange={(event) => setDigits(event.target.value)} />
+            </Input.Root>
+          </Section>
+        </>
+      )}
+
+      {/* TODO(burdon): Selectors on same line. */}
+      {type === 'ref' && (
+        <>
+          <Section>
+            <Input.Root>
+              <Input.Label classNames='mbe-1 mbs-3'>Table</Input.Label>
+              <Select.Root value={refTable} onValueChange={setRefTable}>
+                <Select.TriggerButton placeholder='Table' classNames='is-full' />
+                <Select.Portal>
+                  <Select.Content>
+                    <Select.Viewport>
+                      {tableDefs
+                        .filter((t) => t.id !== tableDef.id)
+                        .map(({ id, name }) => (
+                          <Select.Option key={id} value={id}>
+                            {name ?? id}
+                          </Select.Option>
+                        ))}
+                    </Select.Viewport>
+                  </Select.Content>
+                </Select.Portal>
+              </Select.Root>
+            </Input.Root>
+
+            {refTable && (
+              <Input.Root>
+                <Input.Label classNames='mbe-1 mbs-3'>Table property</Input.Label>
+                <Select.Root value={refProp} onValueChange={setRefProp}>
+                  <Select.TriggerButton placeholder='Property' classNames='is-full' />
+                  <Select.Portal>
+                    <Select.Content>
+                      <Select.Viewport>
+                        {tableDefs
+                          .find((tableDef) => tableDef.id === refTable)
+                          ?.columns.map(({ id, label }) => (
+                            <Select.Option key={id} value={id}>
+                              {label ?? id}
+                            </Select.Option>
+                          ))}
+                      </Select.Viewport>
+                    </Select.Content>
+                  </Select.Portal>
+                </Select.Root>
+              </Input.Root>
+            )}
+          </Section>
+        </>
+      )}
+
+      <Separator classNames='mli-2' />
+
+      <Section className='space-b-1.5'>
+        {/* TODO(burdon): Style as DropdownMenuItem. */}
+        <Button variant='primary' classNames='is-full flex gap-2' onClick={handleSave}>
+          <span>{t('save label')}</span>
+          <div className='grow' />
+          <Check className={getSize(5)} />
         </Button>
-      </Popover.Trigger>
-      <Popover.Portal>
-        <Popover.Content>
-          <Popover.Viewport classNames='w-60'>
-            <DensityProvider density='fine'>
-              <div className='flex flex-col gap-1'>
-                <Section>
-                  <Input.Root>
-                    <Input.Label classNames='mbe-1'>{t('column label label')}</Input.Label>
-                    <Input.TextInput
-                      placeholder='Column label'
-                      value={label ?? ''}
-                      onChange={(event) => setLabel(event.target.value)}
-                      autoFocus
-                    />
-                  </Input.Root>
-                  <Input.Root>
-                    <Input.Label classNames='mbe-1 mbs-3'>{t('property key label')}</Input.Label>
-                    <Input.TextInput
-                      ref={propRef}
-                      placeholder='Property key'
-                      // TODO(burdon): Provide hooks for value normalization, ENTER, ESC, etc.
-                      value={prop ?? ''}
-                      onChange={(event) => setProp(event.target.value.replace(/[^\w_]/g, ''))}
-                    />
-                  </Input.Root>
-                  <Input.Root>
-                    <Input.Label classNames='mbe-1 mbs-3'>{t('column type label')}</Input.Label>
-                    <Select.Root value={type} onValueChange={setType}>
-                      <Select.TriggerButton placeholder='Type' classNames='is-full' />
-                      <Select.Portal>
-                        <Select.Content>
-                          <Select.Viewport>
-                            {(['number', 'boolean', 'string', 'ref'] as ColumnType[]).map((type) => (
-                              <Select.Option key={type} value={type}>
-                                {t(`${type} column type label`)}
-                              </Select.Option>
-                            ))}
-                          </Select.Viewport>
-                        </Select.Content>
-                      </Select.Portal>
-                    </Select.Root>
-                  </Input.Root>
-                </Section>
-
-                {type === 'number' && (
-                  <>
-                    <Section>
-                      <Input.Root>
-                        <Input.Label classNames='mbe-1 mbs-3'>{t('digits label')}</Input.Label>
-                        {/* TODO(burdon): Constrain input to numbers. */}
-                        <Input.TextInput value={digits ?? ''} onChange={(event) => setDigits(event.target.value)} />
-                      </Input.Root>
-                    </Section>
-                  </>
-                )}
-
-                {/* TODO(burdon): Selectors on same line. */}
-                {type === 'ref' && (
-                  <>
-                    <Section>
-                      <Input.Root>
-                        <Input.Label classNames='mbe-1 mbs-3'>Table</Input.Label>
-                        <Select.Root value={refTable} onValueChange={setRefTable}>
-                          <Select.TriggerButton placeholder='Table' classNames='is-full' />
-                          <Select.Portal>
-                            <Select.Content>
-                              <Select.Viewport>
-                                {tableDefs
-                                  .filter((t) => t.id !== tableDef.id)
-                                  .map(({ id, name }) => (
-                                    <Select.Option key={id} value={id}>
-                                      {name ?? id}
-                                    </Select.Option>
-                                  ))}
-                              </Select.Viewport>
-                            </Select.Content>
-                          </Select.Portal>
-                        </Select.Root>
-                      </Input.Root>
-
-                      {refTable && (
-                        <Input.Root>
-                          <Input.Label classNames='mbe-1 mbs-3'>Table property</Input.Label>
-                          <Select.Root value={refProp} onValueChange={setRefProp}>
-                            <Select.TriggerButton placeholder='Property' classNames='is-full' />
-                            <Select.Portal>
-                              <Select.Content>
-                                <Select.Viewport>
-                                  {tableDefs
-                                    .find((tableDef) => tableDef.id === refTable)
-                                    ?.columns.map(({ id, label }) => (
-                                      <Select.Option key={id} value={id}>
-                                        {label ?? id}
-                                      </Select.Option>
-                                    ))}
-                                </Select.Viewport>
-                              </Select.Content>
-                            </Select.Portal>
-                          </Select.Root>
-                        </Input.Root>
-                      )}
-                    </Section>
-                  </>
-                )}
-
-                <Separator classNames='mli-2' />
-
-                <Section className='space-b-1.5'>
-                  {/* TODO(burdon): Style as DropdownMenuItem. */}
-                  <Button variant='primary' classNames='is-full flex gap-2' onClick={handleSave}>
-                    <span>{t('save label')}</span>
-                    <div className='grow' />
-                    <Check className={getSize(5)} />
-                  </Button>
-                  <Button classNames='is-full flex gap-2' onClick={handleCancel}>
-                    <span>{t('cancel label', { ns: 'os' })}</span>
-                    <div className='grow' />
-                    <X className={getSize(5)} />
-                  </Button>
-                  <Button classNames='is-full flex gap-2' onClick={() => onDelete?.(column.id)}>
-                    <span>{t('delete label')}</span>
-                    <div className='grow' />
-                    <Trash className={getSize(5)} />
-                  </Button>
-                </Section>
-              </div>
-            </DensityProvider>
-          </Popover.Viewport>
-          <Popover.Arrow />
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+        <Button classNames='is-full flex gap-2' onClick={handleCancel}>
+          <span>{t('cancel label', { ns: 'os' })}</span>
+          <div className='grow' />
+          <X className={getSize(5)} />
+        </Button>
+        <Button classNames='is-full flex gap-2' onClick={() => onDelete?.(column.id)}>
+          <span>{t('delete label')}</span>
+          <div className='grow' />
+          <Trash className={getSize(5)} />
+        </Button>
+      </Section>
+    </div>
   );
 };
