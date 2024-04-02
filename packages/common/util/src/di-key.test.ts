3660b0bda0921dfade4c22bca326dcf6b3b47731
Dmytro Maretskyi <maretskii@gmail.com>
2024-04-02 17:42:46 +0200

    Ignore

--- packages/common/util/src/di-key.test.ts
+++ packages/common/util/src/di-key.test.ts
@@ -10,6 +10,7 @@ test('DiKey', ({ expect }) => {
   const A = DiKey.define('A');
   class B {}
 
+  // eslint-disable-next-line no-self-compare
   expect(DiKey.combine(A, B) === DiKey.combine(A, B)).toBeTruthy();
   expect(DiKey.stringify(DiKey.combine(A, B))).toEqual('A<B>');
 });
