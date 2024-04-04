666d52e879e1ae661c17e3f697d4c86b5bd8db28
yarolegovich <yarolegovich@gmail.com>
2024-04-04 19:16:50 +0300

    removed from tasks

--- packages/apps/todomvc/src/types.ts
+++ packages/apps/todomvc/src/types.ts
@@ -6,11 +6,11 @@ import * as S from '@effect/schema/Schema';
 import * as E from '@dxos/echo-schema';
 import { EchoObjectSchema } from '@dxos/echo-schema';
 
-export class TodoType extends EchoObjectSchema({ typename: 'example.type.Todo', version: '0.1.0' })({
+export class TodoType extends EchoObjectSchema({ typename: 'dxos.app.todomvc.Todo', version: '0.1.0' })({
   title: S.string,
   completed: S.boolean,
 }) {}
 
-export class TodoListType extends EchoObjectSchema({ typename: 'example.type.TodoList', version: '0.1.0' })({
+export class TodoListType extends EchoObjectSchema({ typename: 'dxos.app.todomvc.TodoList', version: '0.1.0' })({
   todos: S.mutable(S.array(E.ref(TodoType))),
 }) {}
