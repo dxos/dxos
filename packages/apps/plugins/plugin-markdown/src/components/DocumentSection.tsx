74aeaa1761a93b73cb4dc3555f3b276cd4b660ad
Will Shown <thure@users.noreply.github.com>
2024-04-04 18:12:28 -0700

    feat(react-ui-stack): Stack section chrome increment (#6195)

--- packages/apps/plugins/plugin-markdown/src/components/DocumentSection.tsx
+++ packages/apps/plugins/plugin-markdown/src/components/DocumentSection.tsx
@@ -16,13 +16,18 @@ import {
   useDocAccessor,
   useTextEditor,
   createMarkdownExtensions,
+  Toolbar,
+  useActionHandler,
+  useFormattingState,
 } from '@dxos/react-ui-editor';
+import { sectionToolbarLayout } from '@dxos/react-ui-stack';
 
 import { MARKDOWN_PLUGIN } from '../meta';
 
 const DocumentSection: FC<{
   document: DocumentType;
   extensions: Extension[];
+  toolbar?: boolean;
 }> = ({ document, extensions }) => {
   const { t } = useTranslation(MARKDOWN_PLUGIN);
   const identity = useIdentity();
@@ -30,10 +35,12 @@ const DocumentSection: FC<{
 
   const { themeMode } = useThemeContext();
   const { doc, accessor } = useDocAccessor(document.content!);
-  const { parentRef } = useTextEditor(
+  const [formattingState, formattingObserver] = useFormattingState();
+  const { parentRef, view: editorView } = useTextEditor(
     () => ({
       doc,
       extensions: [
+        formattingObserver,
         createBasicExtensions({ placeholder: t('editor placeholder') }),
         createMarkdownExtensions({ themeMode }),
         createThemeExtensions({
@@ -45,8 +52,23 @@ const DocumentSection: FC<{
     }),
     [document, extensions, themeMode],
   );
+  const handleAction = useActionHandler(editorView);
 
-  return <div ref={parentRef} className='min-bs-[8rem]' data-testid='composer.markdownRoot' />;
+  return (
+    <div role='none' className='contents group'>
+      {toolbar && (
+        <Toolbar.Root
+          state={formattingState}
+          onAction={handleAction}
+          classNames={['z-[1] invisible group-focus-within:visible', sectionToolbarLayout]}
+        >
+          <Toolbar.Markdown />
+          <Toolbar.Separator />
+        </Toolbar.Root>
+      )}
+      <div ref={parentRef} className='min-bs-[8rem]' data-testid='composer.markdownRoot' />
+    </div>
+  );
 };
 
 export default DocumentSection;
