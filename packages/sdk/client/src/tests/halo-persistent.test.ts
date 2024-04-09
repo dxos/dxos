693079a55d3b73259fb5b3faef042154dacc1ab5
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-04-09 17:15:56 +0300

    Fix test

--- packages/sdk/client/src/tests/halo-persistent.test.ts
+++ packages/sdk/client/src/tests/halo-persistent.test.ts
@@ -48,8 +48,9 @@ describe('Halo', () => {
 
       await waitForCondition({ condition: () => !!client.halo.identity });
       expect(client.halo.identity).exist;
-      // TODO(burdon): Not working.
-      // expect(client.halo.identity!.displayName).to.eq('test-user');
+      await client.spaces.isReady.wait();
+      expect(client.halo.identity.get()?.profile?.displayName).to.eq('test-user');
+      await client.destroy();
     }
   });
 });
