2089c09284b7bd1487f0231481b7afb43cff3605
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-04-15 16:13:16 +0300

    Change default level path

--- packages/sdk/client-services/src/packlets/storage/level.ts
+++ packages/sdk/client-services/src/packlets/storage/level.ts
@@ -5,12 +5,14 @@
 import { Level } from 'level';
 import path from 'node:path';
 
+import { PublicKey } from '@dxos/keys';
 import { type Runtime } from '@dxos/protocols/proto/dxos/config';
 
-import { getRootPath } from './util';
+import { getRootPath, isPersistent } from './util';
 
 export const createLevel = async (config: Runtime.Client.Storage) => {
-  const storagePath = path.join(getRootPath(config), 'level');
+  const persistent = isPersistent(config);
+  const storagePath = persistent ? path.join(getRootPath(config), 'level') : `/tmp/dxos-${PublicKey.random().toHex()}`;
   const level = new Level<string, string>(storagePath);
   await level.open();
   return level;
