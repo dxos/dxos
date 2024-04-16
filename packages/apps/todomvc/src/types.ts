fa306869668ca3f51ed709e90cada622c776ff2f
yarolegovich <yarolegovich@gmail.com>
2024-04-16 14:38:09 +0300

    fixed lint

--- packages/apps/todomvc/src/types.ts
+++ packages/apps/todomvc/src/types.ts
@@ -1,9 +1,8 @@
 //
 // Copyright 2024 DXOS.org
 //
-import { S } from '@dxos/echo-schema';
 import * as E from '@dxos/echo-schema';
-import { TypedObject } from '@dxos/echo-schema';
+import { S, TypedObject } from '@dxos/echo-schema';
 
 export class TodoType extends TypedObject({ typename: 'dxos.app.todomvc.Todo', version: '0.1.0' })({
   title: S.string,
