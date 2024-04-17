ff4680d290eebb495e173a32bfaba3a28482fd4b
Dmytro Maretskyi <maretskii@gmail.com>
2024-04-17 21:03:47 +0200

    Remove log

--- packages/core/echo/echo-pipeline/src/automerge/automerge-host.ts
+++ packages/core/echo/echo-pipeline/src/automerge/automerge-host.ts
@@ -184,7 +184,6 @@ export class AutomergeHost {
   // Methods for client-services.
   //
 
-  @log.method()
   async flush({ documentIds }: FlushRequest): Promise<void> {
     // Note: Wait for all requested documents to be loaded/synced from thin-client.
     await Promise.all(documentIds?.map((id) => this._repo.find(id as DocumentId).whenReady()) ?? []);
