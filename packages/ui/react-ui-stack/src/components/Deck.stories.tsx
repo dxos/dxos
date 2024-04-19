3856dbe8a8f6ff57b39bdeb8de47cc197a9b0c10
thure <will@braneframe.com>
2024-04-19 14:55:20 -0700

    Refactor to use computed state setters.

--- packages/ui/react-ui-stack/src/components/Deck.stories.tsx
+++ packages/ui/react-ui-stack/src/components/Deck.stories.tsx
@@ -15,7 +15,7 @@ import {
   TextAa,
   X,
 } from '@phosphor-icons/react';
-import React, { type Dispatch, type PropsWithChildren, type SetStateAction, useEffect, useState } from 'react';
+import React, { type PropsWithChildren, useState } from 'react';
 
 import { faker } from '@dxos/random';
 import { Button, Main } from '@dxos/react-ui';
@@ -69,7 +69,6 @@ const rollStackPlank = (n: number): PlankProps => ({
 });
 
 const StackPlank = ({ label, items, id, children }: PropsWithChildren<PlankProps>) => {
-  console.log('[stackplank]', items);
   return (
     <>
       <PlankHeading.Root classNames='pli-px'>
@@ -139,86 +138,48 @@ export const StaticBasicStacks = {
   },
 };
 
-const DynamicPlank = ({
-  plank,
+const IncrementButtons = ({
   index,
-  deckSize,
   setOpenPlanks,
-  setNavContent,
-  setC11yContent,
+  openPlanks,
 }: {
-  plank: PlankProps;
-  index?: number;
-  deckSize?: number;
-  setOpenPlanks?: Dispatch<SetStateAction<Set<string>>>;
-  setNavContent?: Dispatch<SetStateAction<string | null>>;
-  setC11yContent?: Dispatch<SetStateAction<string | null>>;
+  index: number;
+  openPlanks: string[];
+  setOpenPlanks: (setter: (prevOpenPlanksSet: Set<string>) => Set<string>) => void;
 }) => {
   return (
-    <StackPlank {...plank}>
-      {(typeof index === 'number' &&
-        typeof deckSize === 'number' &&
-        setOpenPlanks &&
-        setC11yContent &&
-        setNavContent && (
-          <>
-            <Button variant='ghost' classNames='p-1' onClick={() => setNavContent(plank.id)}>
-              <CaretLineLeft />
-            </Button>
-            <Button
-              disabled={index < 1}
-              variant='ghost'
-              classNames='p-1'
-              onClick={() =>
-                setOpenPlanks((prev) => {
-                  return new Set(arrayMove(Array.from(prev), index, index - 1));
-                })
-              }
-            >
-              <CaretLeft />
-            </Button>
-            <Button
-              disabled={index > deckSize - 2}
-              variant='ghost'
-              classNames='p-1'
-              onClick={() =>
-                setOpenPlanks((prev) => {
-                  return new Set(arrayMove(Array.from(prev), index, index + 1));
-                })
-              }
-            >
-              <CaretRight />
-            </Button>
-            <Button variant='ghost' classNames='p-1' onClick={() => setC11yContent(plank.id)}>
-              <CaretLineRight />
-            </Button>
-            <Button
-              variant='ghost'
-              classNames='p-1'
-              onClick={() =>
-                setOpenPlanks((prev) => {
-                  prev.delete(plank.id);
-                  return new Set(Array.from(prev));
-                })
-              }
-            >
-              <X />
-            </Button>
-          </>
-        )) ??
-        null}
-    </StackPlank>
+    <>
+      <Button
+        disabled={index < 1}
+        variant='ghost'
+        classNames='p-1'
+        onClick={() =>
+          setOpenPlanks((prev: Set<string>) => {
+            return new Set([...arrayMove(openPlanks, index, index - 1), ...Array.from(prev)]);
+          })
+        }
+      >
+        <CaretLeft />
+      </Button>
+      <Button
+        disabled={index > openPlanks.length - 2}
+        variant='ghost'
+        classNames='p-1'
+        onClick={() =>
+          setOpenPlanks((prev) => {
+            return new Set([...arrayMove(openPlanks, index, index + 1), ...Array.from(prev)]);
+          })
+        }
+      >
+        <CaretRight />
+      </Button>
+    </>
   );
 };
 
 const MENU = 'menu';
 
 export const DynamicBasicStacks = () => {
-  const [attended] = useState(new Set());
-  const [navOpen, setNavOpen] = useState(true);
-  const [c11yOpen, setC11yOpen] = useState(false);
-  const [navContent, setNavContent] = useState<string | null>(MENU);
-  const [c11yContent, setC11yContent] = useState<string | null>(null);
   const [planks] = useState(
     [...Array(12)]
       .map(() => rollStackPlank(12))
@@ -227,27 +188,53 @@ export const DynamicBasicStacks = () => {
         return acc;
       }, {}),
   );
-  const [openPlanks, setOpenPlanks] = useState<Set<string>>(new Set([MENU]));
+  const [attended] = useState(new Set());
 
-  useEffect(() => {
-    setNavOpen(!!navContent);
-  }, [navContent]);
-  useEffect(() => {
-    setC11yOpen(!!c11yContent);
-  }, [c11yContent]);
+  const [navOpen, setNavOpen] = useState(true);
+  const [c11yOpen, setC11yOpen] = useState(false);
+
+  const [navContent, setDirectNavContent] = useState<string | null>(MENU);
+  const [c11yContent, setDirectC11yContent] = useState<string | null>(null);
+
+  const [openPlanksSet, setDirectOpenPlanks] = useState<Set<string>>(new Set([MENU]));
+  const openPlanks = Array.from(openPlanksSet);
+  const openPlanksInDeck = openPlanks.filter((id) => ![navContent, c11yContent].includes(id));
+
+  const setNavContent = (nextContent: string | null) => {
+    setDirectNavContent(nextContent);
+    setNavOpen(!!nextContent);
+  };
+
+  const setC11yContent = (nextContent: string | null) => {
+    setDirectC11yContent(nextContent);
+    setC11yOpen(!!nextContent);
+  };
+
+  const setOpenPlanks = (setter: (prevOpenPlanksSet: Set<string>) => Set<string>) => {
+    const nextOpenPlanksSet = setter(openPlanksSet);
+    setDirectOpenPlanks(nextOpenPlanksSet);
+    if (navContent && !nextOpenPlanksSet.has(navContent)) {
+      setNavContent(null);
+    }
+    if (c11yContent && !nextOpenPlanksSet.has(c11yContent)) {
+      setC11yContent(null);
+    }
+  };
+
+  console.log('[open planks]', openPlanksInDeck, openPlanksSet);
 
   const menuChildren = (
-    <div role='none' className='p-1'>
+    <>
       <PlankHeading.Root classNames='pli-px'>
         <PlankHeading.Button>
           <Books {...plankHeadingIconProps} />
         </PlankHeading.Button>
         <PlankHeading.Label classNames='grow'>Menu</PlankHeading.Label>
-        {c11yContent === 'menu' ? (
+        {c11yContent === MENU ? (
           <Button variant='ghost' classNames='p-1' onClick={() => setC11yContent(null)}>
             <CaretLineLeft />
           </Button>
-        ) : navContent === 'menu' ? (
+        ) : navContent === MENU ? (
           <Button variant='ghost' classNames='p-1' onClick={() => setNavContent(null)}>
             <CaretLineRight />
           </Button>
@@ -256,30 +243,37 @@ export const DynamicBasicStacks = () => {
             <Button variant='ghost' classNames='p-1' onClick={() => setNavContent(MENU)}>
               <CaretLineLeft />
             </Button>
+            <IncrementButtons
+              openPlanks={openPlanksInDeck}
+              index={openPlanksInDeck.indexOf(MENU)}
+              setOpenPlanks={setOpenPlanks}
+            />
             <Button variant='ghost' classNames='p-1' onClick={() => setC11yContent(MENU)}>
               <CaretLineRight />
             </Button>
           </>
         )}
       </PlankHeading.Root>
-      {Object.values(planks).map(({ label, id }) => {
-        return (
-          <Button
-            key={id}
-            variant={openPlanks.has(id) ? 'primary' : 'default'}
-            classNames='truncate is-full block mlb-2'
-            onClick={() =>
-              setOpenPlanks((prev) => {
-                prev.has(id) ? prev.delete(id) : prev.add(id);
-                return new Set(Array.from(prev));
-              })
-            }
-          >
-            {label}
-          </Button>
-        );
-      })}
-    </div>
+      <div role='none' className='p-1'>
+        {Object.values(planks).map(({ label, id }) => {
+          return (
+            <Button
+              key={id}
+              variant={openPlanksSet.has(id) ? 'primary' : 'default'}
+              classNames='truncate is-full block mlb-2'
+              onClick={() =>
+                setOpenPlanks((prev) => {
+                  prev.has(id) ? prev.delete(id) : prev.add(id);
+                  return new Set(Array.from(prev));
+                })
+              }
+            >
+              {label}
+            </Button>
+          );
+        })}
+      </div>
+    </>
   );
 
   return (
@@ -297,32 +291,60 @@ export const DynamicBasicStacks = () => {
             </Button>
           </Main.Notch>
           <Main.NavigationSidebar>
-            {navContent && (navContent === MENU ? menuChildren : <DynamicPlank plank={planks[navContent]} />)}
+            {navContent &&
+              (navContent === MENU ? (
+                menuChildren
+              ) : (
+                <StackPlank {...planks[navContent]}>
+                  <Button variant='ghost' classNames='p-1' onClick={() => setNavContent(null)}>
+                    <CaretLineRight />
+                  </Button>
+                </StackPlank>
+              ))}
           </Main.NavigationSidebar>
-          <NaturalDeck.Root>
-            {Array.from(openPlanks)
-              .filter((id) => ![navContent, c11yContent].includes(id))
-              .map((id, index, arr) =>
-                id === MENU ? (
-                  <NaturalDeck.Plank key={MENU}>{menuChildren}</NaturalDeck.Plank>
-                ) : (
-                  <NaturalDeck.Plank key={id}>
-                    <DynamicPlank
-                      key={id}
-                      plank={planks[id]}
-                      index={index}
-                      setOpenPlanks={setOpenPlanks}
-                      setC11yContent={setC11yContent}
-                      setNavContent={setNavContent}
-                      deckSize={arr.length}
-                    />
-                  </NaturalDeck.Plank>
-                ),
-              )}
-          </NaturalDeck.Root>
           <Main.ComplementarySidebar>
-            {c11yContent && (c11yContent === MENU ? menuChildren : <DynamicPlank plank={planks[c11yContent]} />)}
+            {c11yContent &&
+              (c11yContent === MENU ? (
+                menuChildren
+              ) : (
+                <StackPlank {...planks[c11yContent]}>
+                  <Button variant='ghost' classNames='p-1' onClick={() => setC11yContent(null)}>
+                    <CaretLineLeft />
+                  </Button>
+                </StackPlank>
+              ))}
           </Main.ComplementarySidebar>
+          <NaturalDeck.Root>
+            {openPlanksInDeck.map((id, index, arr) =>
+              id === MENU ? (
+                <NaturalDeck.Plank key={MENU}>{menuChildren}</NaturalDeck.Plank>
+              ) : (
+                <NaturalDeck.Plank key={id}>
+                  <StackPlank {...planks[id]}>
+                    <Button variant='ghost' classNames='p-1' onClick={() => setNavContent(id)}>
+                      <CaretLineLeft />
+                    </Button>
+                    <IncrementButtons {...{ index, setOpenPlanks, openPlanks: openPlanksInDeck }} />
+                    <Button variant='ghost' classNames='p-1' onClick={() => setC11yContent(id)}>
+                      <CaretLineRight />
+                    </Button>
+                    <Button
+                      variant='ghost'
+                      classNames='p-1'
+                      onClick={() =>
+                        setOpenPlanks((prev) => {
+                          prev.delete(id);
+                          return new Set(Array.from(prev));
+                        })
+                      }
+                    >
+                      <X />
+                    </Button>
+                  </StackPlank>
+                </NaturalDeck.Plank>
+              ),
+            )}
+          </NaturalDeck.Root>
         </Main.Root>
       </AttentionProvider>
     </Mosaic.Root>
