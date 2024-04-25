7df08990f638ef1217af40e89b1d563803af25ff
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-04-25 14:46:54 +0300

    Add getChangeByHash test

--- packages/core/echo/echo-pipeline/src/automerge/automerge-repo.test.ts
+++ packages/core/echo/echo-pipeline/src/automerge/automerge-repo.test.ts
@@ -2,6 +2,9 @@
 // Copyright 2024 DXOS.org
 //
 
+import { expect } from 'chai';
+
+import { change, clone, from, getBackend, getHeads } from '@dxos/automerge/automerge';
 import { Repo } from '@dxos/automerge/automerge-repo';
 import { randomBytes } from '@dxos/crypto';
 import { describe, openAndClose, test } from '@dxos/test';
@@ -10,7 +13,6 @@ import { LevelDBStorageAdapter } from './leveldb-storage-adapter';
 import { createTestLevel } from '../testing';
 
 describe('AutomergeRepo', () => {
-  // Currently failing
   test('flush', async () => {
     const level = createTestLevel();
     const storage = new LevelDBStorageAdapter({ db: level.sublevel('automerge') });
@@ -31,5 +33,29 @@ describe('AutomergeRepo', () => {
     }
   });
 
-  test('getChangeByHash', async () => {});
+  test('getChangeByHash', async () => {
+    const level = createTestLevel();
+    const storage = new LevelDBStorageAdapter({ db: level.sublevel('automerge') });
+    await openAndClose(level, storage);
+
+    const doc = from({ foo: 'bar' });
+    const copy = clone(doc);
+    const newDoc = change(copy, 'change', (doc: any) => {
+      doc.foo = 'baz';
+    });
+
+    {
+      const heads = getHeads(newDoc);
+      const changes = heads.map((hash) => getBackend(newDoc).getChangeByHash(hash));
+      expect(changes.length).to.equal(1);
+      expect(changes[0]).to.not.be.null;
+    }
+
+    {
+      const heads = getHeads(newDoc);
+      const changes = heads.map((hash) => getBackend(doc).getChangeByHash(hash));
+      expect(changes.length).to.equal(1);
+      expect(changes[0]).to.be.null;
+    }
+  });
 });
