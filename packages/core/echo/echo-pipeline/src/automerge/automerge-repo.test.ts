c90c684a8a906f9ab27c3af1bf4d95366559c309
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2024-04-26 15:32:48 +0300

    revert: fix: Fix flush method on automergeDB (#6409) (#6429)

--- packages/core/echo/echo-pipeline/src/automerge/automerge-repo.test.ts
+++ packages/core/echo/echo-pipeline/src/automerge/automerge-repo.test.ts
@@ -2,25 +2,19 @@
 // Copyright 2024 DXOS.org
 //
 
-import { expect } from 'chai';
-
-import { change, clone, from, getBackend, getHeads } from '@dxos/automerge/automerge';
 import { Repo } from '@dxos/automerge/automerge-repo';
 import { randomBytes } from '@dxos/crypto';
-import { describe, openAndClose, test } from '@dxos/test';
+import { StorageType, createStorage } from '@dxos/random-access-storage';
+import { describe, test } from '@dxos/test';
 
-import { LevelDBStorageAdapter } from './leveldb-storage-adapter';
-import { createTestLevel } from '../testing';
+import { AutomergeStorageAdapter } from './automerge-storage-adapter';
 
 describe('AutomergeRepo', () => {
-  test('flush', async () => {
-    const level = createTestLevel();
-    const storage = new LevelDBStorageAdapter({ db: level.sublevel('automerge') });
-    await openAndClose(level, storage);
-
+  // Currently failing
+  test.skip('flush', async () => {
     const repo = new Repo({
       network: [],
-      storage,
+      storage: new AutomergeStorageAdapter(createStorage({ type: StorageType.NODE }).createDirectory()),
     });
     const handle = repo.create<{ field?: string }>();
 
@@ -30,32 +24,6 @@ describe('AutomergeRepo', () => {
         doc.field += randomBytes(1024).toString('hex');
       });
       await p;
-    }
-  });
-
-  test('getChangeByHash', async () => {
-    const level = createTestLevel();
-    const storage = new LevelDBStorageAdapter({ db: level.sublevel('automerge') });
-    await openAndClose(level, storage);
-
-    const doc = from({ foo: 'bar' });
-    const copy = clone(doc);
-    const newDoc = change(copy, 'change', (doc: any) => {
-      doc.foo = 'baz';
-    });
-
-    {
-      const heads = getHeads(newDoc);
-      const changes = heads.map((hash) => getBackend(newDoc).getChangeByHash(hash));
-      expect(changes.length).to.equal(1);
-      expect(changes[0]).to.not.be.null;
-    }
-
-    {
-      const heads = getHeads(newDoc);
-      const changes = heads.map((hash) => getBackend(doc).getChangeByHash(hash));
-      expect(changes.length).to.equal(1);
-      expect(changes[0]).to.be.null;
     }
   });
 });
