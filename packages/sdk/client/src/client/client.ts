893786f1bc2b0c71b0ad22307e31534f7147d568
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-04-26 16:28:00 +0300

    fix: Fix default query behavior

--- packages/sdk/client/src/client/client.ts
+++ packages/sdk/client/src/client/client.ts
@@ -295,7 +295,7 @@ export class Client {
     }
 
     {
-      await this._services?.services.QueryService?.reIndex();
+      await this._services?.services.QueryService?.reIndex(undefined, { timeout: 30_000 });
     }
 
     log.info('Repair succeeded', { repairSummary });
