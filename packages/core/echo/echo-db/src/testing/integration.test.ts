ed0a8bb285725dc5ed93bc7581951199ecb56d91
Dmytro Maretskyi <maretskii@gmail.com>
2024-04-30 14:27:03 +0200

    Add failing reopen test

--- packages/core/echo/echo-db/src/testing/integration.test.ts
+++ packages/core/echo/echo-db/src/testing/integration.test.ts
@@ -8,6 +8,8 @@ import { PublicKey } from '@dxos/keys';
 import { describe, test } from '@dxos/test';
 
 import { EchoTestBuilder } from './echo-test-builder';
+import { async } from 'effect/Stream';
+import { EchoReactiveObject } from '@dxos/echo-schema';
 
 describe('Integration tests', () => {
   test('read/write to one database', async () => {
@@ -19,24 +21,51 @@ describe('Integration tests', () => {
     const object = db.add({ type: 'task', title: 'A' });
     await db.flush();
 
-    const { objects } = await db.query({ type: 'task' }).run();
+    const { objects } = await db.query().run();
     expect(objects).to.deep.eq([object]);
+  });
+
+  test.skip('reopen peer', async () => {
+    await using builder = await new EchoTestBuilder().open();
+    const [spaceKey] = PublicKey.randomSequence();
+    await using peer = await builder.createPeer();
+
+    let object: EchoReactiveObject<any>;
+    let rootUrl: string;
+    {
+      await using db = await peer.createDatabase(spaceKey);
+      rootUrl = db.rootUrl!;
+
+      object = db.add({ type: 'task', title: 'A' });
+      await db.flush();
+    }
+
+    await peer.close();
+    await peer.open();
+
+    {
+      await using db = await peer.openDatabase(spaceKey, rootUrl);
+
+      const { objects } = await db.query().run();
+      expect(objects).to.have.length(1);
+      expect({ ...objects[0] }).to.deep.eq({ ...object });
+    }
   });
 
   test('2 clients', async () => {
     await using builder = await new EchoTestBuilder().open();
     const [spaceKey] = PublicKey.randomSequence();
 
-    const peer = await builder.createPeer();
+    await using peer = await builder.createPeer();
     await using db = await peer.createDatabase(spaceKey);
 
     const object = db.add({ type: 'task', title: 'A' });
     await db.flush();
 
-    const client2 = await peer.createClient();
+    await using client2 = await peer.createClient();
     await using db2 = await peer.openDatabase(spaceKey, db.rootUrl!, { client: client2 });
 
-    const { objects } = await db2.query({ type: 'task' }).run();
+    const { objects } = await db2.query().run();
     expect(objects).to.have.length(1);
     expect({ ...objects[0] }).to.deep.eq({ ...object });
   });
