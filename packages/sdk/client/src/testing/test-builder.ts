3b8565059097f0246f04f95239b7f0a6d6780ad5
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-05-02 18:16:22 +0300

    Fix build

--- packages/sdk/client/src/testing/test-builder.ts
+++ packages/sdk/client/src/testing/test-builder.ts
@@ -9,10 +9,10 @@ import { type ServiceContextRuntimeParams } from '@dxos/client-services/src';
 import { Config } from '@dxos/config';
 import { Context } from '@dxos/context';
 import { raise } from '@dxos/debug';
-import { type LevelDB } from '@dxos/echo-pipeline';
 import { create, Expando } from '@dxos/echo-schema';
 import { invariant } from '@dxos/invariant';
 import { type PublicKey } from '@dxos/keys';
+import { type LevelDB } from '@dxos/kv-store';
 import { log } from '@dxos/log';
 import { MemorySignalManager, MemorySignalManagerContext, WebsocketSignalManager } from '@dxos/messaging';
 import {
