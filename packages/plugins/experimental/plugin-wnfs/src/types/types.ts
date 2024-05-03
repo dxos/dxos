560938988ac71b4681df0c3b2ad24f75cf898b36
Steven Vandevelde <steven.vandevelde@hey.com>
2024-05-03 19:17:17 +0200

    feat: Add plugin-wnfs

---
+++ packages/plugins/experimental/plugin-wnfs/src/types/types.ts
@@ -0,0 +1,27 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import {
+  type FileManagerProvides,
+  type MetadataRecordsProvides,
+  type SurfaceProvides,
+  type TranslationsProvides,
+} from '@dxos/app-framework';
+import { type SchemaProvides } from '@dxos/plugin-client';
+import { type MarkdownExtensionProvides } from '@dxos/plugin-markdown/types';
+
+import { WNFS_PLUGIN } from '../meta';
+
+const WNFS_ACTION = `${WNFS_PLUGIN}/action`;
+
+export enum WnfsAction {
+  CREATE = `${WNFS_ACTION}/create`,
+}
+
+export type WnfsPluginProvides = FileManagerProvides &
+  SurfaceProvides &
+  MetadataRecordsProvides &
+  TranslationsProvides &
+  SchemaProvides &
+  MarkdownExtensionProvides;
