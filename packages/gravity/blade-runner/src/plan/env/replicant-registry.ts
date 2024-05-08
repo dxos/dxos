2e48de338b6dd2e747bb1bc3b77f1e0f56492960
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-05-08 15:42:40 +0300

    Add ReplicantRegistry

---
+++ packages/gravity/blade-runner/src/plan/env/replicant-registry.ts
@@ -0,0 +1,19 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import { type ReplicantBrain } from './interface';
+
+export class ReplicantRegistry {
+  public static readonly instance = new ReplicantRegistry();
+
+  private readonly _replicantBrains = new Map<string, ReplicantBrain<any>>();
+
+  register(brain: { new (): any }) {
+    this._replicantBrains.set(brain.name, brain);
+  }
+
+  get(name: string): ReplicantBrain<any> {
+    return this._replicantBrains.get(name)!;
+  }
+}
