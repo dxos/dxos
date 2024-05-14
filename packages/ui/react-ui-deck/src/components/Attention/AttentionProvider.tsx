7535ebb1fa4b46824db9c5c4c4bd63a2c629a34f
thure <will@braneframe.com>
2024-05-13 17:37:39 -0700

    Start stub attention concept.

--- packages/ui/react-ui-deck/src/components/Attention/AttentionProvider.tsx
+++ packages/ui/react-ui-deck/src/components/Attention/AttentionProvider.tsx
@@ -7,7 +7,7 @@
 // eslint-disable-next-line unused-imports/no-unused-imports
 import { createContext, type Scope, type CreateScope } from '@radix-ui/react-context';
 // eslint-disable-next-line unused-imports/no-unused-imports
-import React from 'react';
+import React, { type FocusEvent, type HTMLAttributes } from 'react';
 
 const ATTENTION_NAME = 'Attention';
 
@@ -20,6 +20,29 @@ const [AttentionProvider, useAttentionContext] = createContext(ATTENTION_NAME, {
 const useHasAttention = (attendableId?: string) => {
   const { attended } = useAttentionContext(ATTENTION_NAME);
   return attended.has(attendableId);
+};
+
+/**
+ * Computes HTML element attributes to apply so the attention system can detect changes
+ * @param attendableId
+ */
+const useAttendable = (attendableId?: string) => {
+  return { 'data-attendable-id': attendableId } as HTMLAttributes<'div'>;
+};
+
+const getAttendables = (cursor: FocusEvent['target'], acc: string[] = []): string[] => {
+  const closestAttendable = cursor.closest('[data-attendable-id]');
+  if (closestAttendable) {
+    acc.push(closestAttendable.getAttribute('data-attendable-id')!);
+    return getAttendables(closestAttendable, acc);
+  } else {
+    return acc;
+  }
+};
+
+const handleFocus = (event: FocusEvent) => {
+  const ids = new Set(getAttendables(event.target));
+  // TODO(thure): update attention context value
 };
 
 export { AttentionProvider, useAttentionContext, useHasAttention, ATTENTION_NAME };
