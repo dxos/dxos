dfbb9c154ec72eedeb623cfc0d5b0887cdf77518
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2024-05-17 00:04:22 +0300

    refactor: Reachitect blade-runner around controlled replicants (#6618)

--- packages/gravity/blade-runner/src/spec/empty.ts
+++ packages/gravity/blade-runner/src/spec/empty.ts
@@ -3,59 +3,40 @@
 //
 
 import { log } from '@dxos/log';
-import { range } from '@dxos/util';
 
-import { type AgentEnv } from '../plan';
-import { type AgentRunOptions, type PlanResults, type Platform, type TestParams, type TestPlan } from '../plan/spec';
-import { TestBuilder as SignalTestBuilder } from '../test-builder';
+import { type ReplicantsSummary, type Platform, type TestParams, type TestPlan, type SchedulerEnvImpl } from '../plan';
+import { DumbReplicant } from '../replicants/dumb-replicant';
 
 export type EmptyTestSpec = {
-  agents: number;
   platform: Platform;
 };
 
 export type EmptyAgentConfig = {
-  agentIdx: number;
+  replicantId: number;
 };
 
-export class EmptyTestPlan implements TestPlan<EmptyTestSpec, EmptyAgentConfig> {
+export class EmptyTestPlan implements TestPlan<EmptyTestSpec> {
   onError?: ((err: Error) => void) | undefined;
 
   defaultSpec(): EmptyTestSpec {
     return {
-      agents: 1,
-      platform: 'chromium',
+      platform: 'nodejs',
     };
   }
 
-  signalBuilder = new SignalTestBuilder();
-
-  async init({ spec, outDir }: TestParams<EmptyTestSpec>): Promise<AgentRunOptions<EmptyAgentConfig>[]> {
-    return range(spec.agents).map((agentIdx) => ({
-      config: {
-        agentIdx,
-      },
-      runtime: {
-        platform: spec.platform,
-      },
-    }));
-  }
-
-  async run(env: AgentEnv<EmptyTestSpec, EmptyAgentConfig>): Promise<void> {
-    const { config } = env.params;
-    const { agentIdx } = config;
-
+  async run(env: SchedulerEnvImpl<EmptyTestSpec>, params: TestParams<EmptyTestSpec>) {
     log.info('run', {
-      message: 'Hello from agent',
-      platform: env.params.runtime.platform,
-      agentIdx,
-      runnerAgentIdx: config.agentIdx,
-      agentId: env.params.agentId.substring(0, 8),
+      message: 'Empty spec hello message, running dumb replicant.',
+      params: env.params,
     });
+
+    const dumbReplicant = await env.spawn(DumbReplicant, { platform: params.spec.platform });
+
+    const result = await dumbReplicant.brain.doSomethingFunny();
+    log.info('result', { result });
   }
 
-  async finish(params: TestParams<EmptyTestSpec>, results: PlanResults): Promise<any> {
-    await this.signalBuilder.destroy();
+  async analyze(params: TestParams<EmptyTestSpec>, results: ReplicantsSummary): Promise<any> {
     log.info('finished shutdown');
   }
 }
