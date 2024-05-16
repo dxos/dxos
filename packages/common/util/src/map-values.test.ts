465ddaff5386e5343ef421626537fc0941388eb6
Dmytro Maretskyi <maretskii@gmail.com>
2024-05-16 14:03:06 +0200

    Add value mapper util

---
+++ packages/common/util/src/map-values.test.ts
@@ -0,0 +1,39 @@
+import { deepMapValues } from './map-values';
+import { test } from 'vitest';
+
+test('deepMapValues', ({ expect }) => {
+  const obj = {
+    a: 1,
+    b: {
+      c: 2,
+    },
+    d: [
+      3,
+      {
+        e: 4,
+      },
+    ],
+  };
+
+  // Double all numbers.
+  const res = deepMapValues(obj, (value, recurse) => {
+    if (typeof value === 'number') {
+      return value * 2;
+    }
+
+    return recurse(value);
+  });
+
+  expect(res).toEqual({
+    a: 2,
+    b: {
+      c: 4,
+    },
+    d: [
+      6,
+      {
+        e: 8,
+      },
+    ],
+  });
+});
