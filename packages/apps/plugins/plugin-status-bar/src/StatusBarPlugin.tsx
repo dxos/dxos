9365e8c3e10386ff95e9aa20e98d426a2a774f1d
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-05-17 15:52:54 -0700

    Simplify status bar plugin

--- packages/apps/plugins/plugin-status-bar/src/StatusBarPlugin.tsx
+++ packages/apps/plugins/plugin-status-bar/src/StatusBarPlugin.tsx
@@ -4,39 +4,13 @@
 
 import React from 'react';
 
-import {
-  type PluginDefinition,
-  type TranslationsProvides,
-  type SurfaceProvides,
-  type IntentResolverProvides,
-  type IntentResolver,
-} from '@dxos/app-framework';
+import { type PluginDefinition, type TranslationsProvides, type SurfaceProvides } from '@dxos/app-framework';
 
 import { StatusBarImpl } from './components';
-import { mkIntentBuilder } from './lib';
-import meta, { STATUS_BAR_PLUGIN } from './meta';
+import meta from './meta';
 import translations from './translations';
 
-// -- Types and constants.
-const STATUS_BAR_ACTION = `${STATUS_BAR_PLUGIN}/action`;
-
-export enum StatusBarAction {
-  PROVIDE_FEEDBACK = `${STATUS_BAR_ACTION}/provide-feedback`,
-}
-
-export namespace StatusBarAction {
-  export type ProvideFeedback = undefined;
-}
-
-type StatusBarActions = {
-  [StatusBarAction.PROVIDE_FEEDBACK]: StatusBarAction.ProvideFeedback;
-};
-
-export const statusBarIntent = mkIntentBuilder<StatusBarActions>(meta.id);
-
-export type StatusBarPluginProvides = SurfaceProvides & TranslationsProvides & IntentResolverProvides;
-
-const resolver: IntentResolver = async (intent, _plugins) => {};
+export type StatusBarPluginProvides = SurfaceProvides & TranslationsProvides;
 
 // -- Root plugin definition.
 export const StatusBarPlugin = (): PluginDefinition<StatusBarPluginProvides> => {
@@ -55,7 +29,6 @@ export const StatusBarPlugin = (): PluginDefinition<StatusBarPluginProvides> => 
           return null;
         },
       },
-      intent: { resolver },
     },
   };
 };
