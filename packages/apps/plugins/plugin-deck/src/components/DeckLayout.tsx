0f0388c0a0428fd766f8f8fec4032cd79e7ead61
thure <will@braneframe.com>
2024-05-16 17:44:39 -0700

    Add Sketch plank.

--- packages/apps/plugins/plugin-deck/src/components/DeckLayout.tsx
+++ packages/apps/plugins/plugin-deck/src/components/DeckLayout.tsx
@@ -61,7 +61,7 @@ export const firstComplementaryId = (active: Location['active']): string | undef
 
 const PlankLoading = () => {
   return (
-    <div role='none' className='grid bs-[100dvh] place-items-center'>
+    <div role='none' className='grid bs-[100dvh] place-items-center row-span-2'>
       <Status indeterminate aria-label='Initializing' />
     </div>
   );
@@ -311,7 +311,7 @@ export const DeckLayout = ({
                             popoverAnchorId,
                           }}
                           limit={1}
-                          fallback={<PlankLoading />}
+                          fallback={PlankLoading}
                         />
                       </>
                     ) : (
