89515c2254fad0746ce0495b91594320b9a10282
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-05-20 20:07:00 -0700

    chore(deck): Deckify plugins for Berlin Demo (#6742)

--- packages/apps/plugins/plugin-table/src/components/TableArticle.tsx
+++ packages/apps/plugins/plugin-table/src/components/TableArticle.tsx
@@ -1,5 +1,5 @@
 //
-// Copyright 2023 DXOS.org
+// Copyright 2024 DXOS.org
 //
 
 import React, { type FC } from 'react';
@@ -8,9 +8,9 @@ import { Table } from '@dxos/react-ui-table';
 
 import { ObjectTable, type ObjectTableProps } from './ObjectTable';
 
-const TableSection: FC<Omit<ObjectTableProps, 'getScrollElement'>> = ({ table }) => (
+const TableArticle: FC<Omit<ObjectTableProps, 'getScrollElement'>> = ({ table }) => (
   <Table.Root>
-    <Table.Viewport classNames='max-bs-96 is-full mlb-2 overflow-auto sticky-top-0'>
+    <Table.Viewport classNames='block-start-[--topbar-size] block-end-0 row-span-2 is-full mlb-2 overflow-auto sticky-top-0'>
       <ObjectTable
         key={table.id} // New component instance per table.
         table={table}
@@ -21,4 +21,4 @@ const TableSection: FC<Omit<ObjectTableProps, 'getScrollElement'>> = ({ table })
   </Table.Root>
 );
 
-export default TableSection;
+export default TableArticle;
