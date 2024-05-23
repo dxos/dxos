6c054ff0a258bac8137b94836f2038156d7ebde6
Dmytro Maretskyi <maretskii@gmail.com>
2024-05-23 13:23:00 +0200

    Join tables util

---
+++ packages/common/util/src/join-tables.ts
@@ -0,0 +1,34 @@
+/**
+ * Joins two untyped tables on the given columns.
+ * @param leftColumn The column to join on in the first table.
+ * @param rightColumn The column to join on in the second table.
+ */
+export const joinTables = (
+  leftColumn: string,
+  rightColumn: string,
+  left: Record<string, any>[],
+  right: Record<string, any>[],
+) => {
+  const map = new Map();
+  const used = new Set();
+  for (const row of right) {
+    map.set(row[rightColumn], row);
+  }
+
+  const result = [];
+  for (const row of left) {
+    const right = map.get(row[leftColumn]);
+    used.add(right);
+
+    result.push(Object.assign(right ?? {}, row));
+  }
+
+  // Add unmatched rows from the right table.
+  for (const row of right) {
+    if (!used.has(row)) {
+      result.push(row);
+    }
+  }
+
+  return result;
+};
