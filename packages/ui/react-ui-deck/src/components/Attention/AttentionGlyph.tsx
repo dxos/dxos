4b08a138b07520f1e94f5181b2e7ee7dfea0d572
thure <will@braneframe.com>
2024-05-24 13:52:36 -0700

    Implement AttentionGlyphCloseButton.

--- packages/ui/react-ui-deck/src/components/Attention/AttentionGlyph.tsx
+++ packages/ui/react-ui-deck/src/components/Attention/AttentionGlyph.tsx
@@ -2,19 +2,29 @@
 // Copyright 2024 DXOS.org
 //
 
+import { Minus } from '@phosphor-icons/react';
 import { type Primitive } from '@radix-ui/react-primitive';
 import React, { type ComponentPropsWithRef, forwardRef } from 'react';
 
 import { type ThemedClassName } from '@dxos/react-ui';
-import { mx } from '@dxos/react-ui-theme';
+import { focusRing, mx } from '@dxos/react-ui-theme';
+
+const attentionGlyphStyles =
+  'inline-block rounded-sm is-3 bs-3 bg-transparent fg-accent transition-colors [[aria-current]_&]:surface-unAccent [[aria-current]_&]:fg-inverse [[data-attention=true]_&]:surface-accent [[data-attention=true]_&]:fg-inverse [[aria-current][data-attention=true]_&]:surface-accent [[aria-current][data-attention=true]_&]:fg-inverse';
+
+const closeIconStyles =
+  'hidden is-3 bs-3 group-[[aria-current]_&:hover]/attentionGlyphButton:block group-[[aria-current]_&:focus]/attentionGlyphButton:block group-[[data-attention=true]_&:hover]/attentionGlyphButton:block group-[[data-attention=true]_&:focus]/attentionGlyphButton:block group-[[aria-current][data-attention=true]_&:hover]/attentionGlyphButton:block group-[[aria-current][data-attention=true]_&:focus]/attentionGlyphButton:block';
+
+const presenceIconStyles =
+  'is-3 bs-3 group-[[aria-current]_&:hover]/attentionGlyphButton:hidden group-[[aria-current]_&:focus]/attentionGlyphButton:hidden group-[[data-attention=true]_&:hover]/attentionGlyphButton:hidden group-[[data-attention=true]_&:focus]/attentionGlyphButton:hidden group-[[aria-current][data-attention=true]_&:hover]/attentionGlyphButton:hidden group-[[aria-current][data-attention=true]_&:focus]/attentionGlyphButton:hidden';
 
 const PresenceOne = () => {
   return (
     <svg
       width='12'
       height='12'
-      className='is-3 bs-3'
       viewBox='0 0 12 12'
+      className={presenceIconStyles}
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
@@ -28,8 +38,8 @@ const PresenceMany = () => {
     <svg
       width='12'
       height='12'
-      className='is-3 bs-3'
       viewBox='0 0 12 12'
+      className={presenceIconStyles}
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
@@ -46,9 +56,6 @@ export type AttentionGlyphProps = {
   presence?: 'none' | 'one' | 'many';
 } & ThemedClassName<Omit<ComponentPropsWithRef<typeof Primitive.span>, 'children'>>;
 
-const attentionGlyphStyles =
-  'inline-block rounded-sm is-3 bs-3 bg-transparent fg-accent transition-colors [[aria-current]_&]:surface-unAccent [[aria-current]_&]:fg-inverse [[data-attention=true]_&]:surface-accent [[data-attention=true]_&]:fg-inverse [[aria-current][data-attention=true]_&]:surface-accent [[aria-current][data-attention=true]_&]:fg-inverse';
-
 export const AttentionGlyph = forwardRef<HTMLSpanElement, AttentionGlyphProps>(
   ({ presence, classNames, ...props }, forwardedRef) => {
     return (
@@ -56,6 +63,27 @@ export const AttentionGlyph = forwardRef<HTMLSpanElement, AttentionGlyphProps>(
         {presence === 'many' && <PresenceMany />}
         {presence === 'one' && <PresenceOne />}
       </span>
+    );
+  },
+);
+
+export const AttentionGlyphCloseButton = forwardRef<HTMLButtonElement, AttentionGlyphProps>(
+  ({ presence, classNames, ...props }, forwardedRef) => {
+    return (
+      <button
+        {...props}
+        className={mx(
+          attentionGlyphStyles,
+          focusRing,
+          'group/attentionGlyphButton pointer-events-none [[aria-current]_&]:pointer-events-auto [[data-attention=true]_&]:pointer-events-auto [[aria-current][data-attention=true]_&]:pointer-events-auto',
+          classNames,
+        )}
+        ref={forwardedRef}
+      >
+        {presence === 'many' && <PresenceMany />}
+        {presence === 'one' && <PresenceOne />}
+        <Minus weight='bold' className={closeIconStyles} />
+      </button>
     );
   },
 );
