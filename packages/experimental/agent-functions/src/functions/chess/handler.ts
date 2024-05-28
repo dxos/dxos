6ef65ec8c8b13417b02f3e129294ae9276155889
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-05-28 16:38:03 -0700

    Implement player selector for chess

--- packages/experimental/agent-functions/src/functions/chess/handler.ts
+++ packages/experimental/agent-functions/src/functions/chess/handler.ts
@@ -28,12 +28,24 @@ export const types = [GameType];
 /**
  * Chess function handler.
  */
-export const handler = subscriptionHandler<Meta>(async ({ event }) => {
+export const handler = subscriptionHandler<Meta>(async ({ event, context }) => {
+  const identity = context.client.halo.identity.get();
+  if (!identity) {
+    return;
+  }
+
+  const identityKey = identity.identityKey.toHex();
+
   // TODO(burdon): Get side from object.
-  const { meta: { level = 1, side = 'b' } = {}, objects } = event.data;
+  const { meta: { level = 1 } = {}, objects } = event.data;
   for (const game of objects ?? []) {
     const engine = new Engine({ pgn: game.pgn, level });
-    if (!engine.state.isGameOver() && engine.state.turn() === side) {
+
+    const side = [];
+    if (game.playerWhite === identityKey) side.push('w');
+    if (game.playerBlack === identityKey) side.push('b');
+
+    if (!engine.state.isGameOver() && side.includes(engine.state.turn())) {
       engine.move();
       engine.print();
 
