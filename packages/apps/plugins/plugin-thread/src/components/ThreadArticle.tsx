af0334e9d9dbb552732a74d8fb0d753ebdb873c7
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-05-28 12:47:59 -0700

    feat(demo): Provide context object to standalone thread messages in deck (#6865)

--- packages/apps/plugins/plugin-thread/src/components/ThreadArticle.tsx
+++ packages/apps/plugins/plugin-thread/src/components/ThreadArticle.tsx
@@ -2,17 +2,15 @@
 // Copyright 2024 DXOS.org
 //
 
-import React, { type FC } from 'react';
-
-import { type ThreadType } from '@braneframe/types';
+import React, { type ComponentProps, type FC } from 'react';
 
 import { ChatContainer } from './ChatContainer';
 
-const ThreadArticle: FC<{ thread: ThreadType }> = ({ thread }) => {
+const ThreadArticle: FC<Pick<ComponentProps<typeof ChatContainer>, 'thread' | 'context'>> = (props) => {
   // TODO(burdon): Factor out Main container across plugins?
   return (
     <div role='none' className='row-span-2'>
-      <ChatContainer thread={thread} />
+      <ChatContainer {...props} />
     </div>
   );
 };
