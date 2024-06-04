9f8a7e1e152cd07dc9f22a22571ce05a952871b7
thure <will@braneframe.com>
2024-06-04 12:56:07 -0700

    Add TextTooltip component to simplify oft-repeated basic tooltips.

---
+++ packages/ui/react-ui-text-tooltip/src/components/TextTooltip.tsx
@@ -0,0 +1,60 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import React, { type CSSProperties, Fragment, type PropsWithChildren, useRef, useState } from 'react';
+
+import { Tooltip, type TooltipContentProps } from '@dxos/react-ui';
+
+export type TextTooltipProps = PropsWithChildren<
+  {
+    text: string;
+    asChild?: boolean;
+    portal?: boolean;
+    zIndex?: CSSProperties['zIndex'];
+    onlyWhenTruncating?: boolean;
+  } & Pick<TooltipContentProps, 'side' | 'sideOffset'>
+>;
+
+export const TextTooltip = ({
+  text,
+  children,
+  onlyWhenTruncating,
+  asChild = true,
+  portal = true,
+  zIndex = 70,
+  sideOffset,
+  side,
+}: TextTooltipProps) => {
+  const ContentRoot = portal ? Tooltip.Portal : Fragment;
+  const content = useRef<HTMLButtonElement | null>(null);
+  const [open, setOpen] = useState(false);
+  return (
+    <Tooltip.Root
+      open={open}
+      onOpenChange={(nextOpen) => {
+        if (onlyWhenTruncating && nextOpen && content.current) {
+          return setOpen(content.current.scrollWidth > content.current.offsetWidth);
+        } else {
+          return setOpen(nextOpen);
+        }
+      }}
+    >
+      <Tooltip.Trigger asChild={asChild} ref={content}>
+        {children}
+      </Tooltip.Trigger>
+      <ContentRoot>
+        <Tooltip.Content
+          {...{
+            side,
+            sideOffset,
+            style: { zIndex },
+          }}
+        >
+          {text}
+          <Tooltip.Arrow />
+        </Tooltip.Content>
+      </ContentRoot>
+    </Tooltip.Root>
+  );
+};
