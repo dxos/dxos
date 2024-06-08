91eaab214d9868b3b797b243301e089cda8d3d80
thure <will@braneframe.com>
2024-06-07 17:20:55 -0700

    Refactor Deck to use subcomponents for Plank.

--- packages/ui/react-ui-deck/src/components/Deck/Deck.tsx
+++ packages/ui/react-ui-deck/src/components/Deck/Deck.tsx
@@ -4,8 +4,17 @@
 
 import { useFocusFinders } from '@fluentui/react-tabster';
 import { useComposedRefs } from '@radix-ui/react-compose-refs';
+import { createContextScope, type Scope } from '@radix-ui/react-context';
 import { Slot } from '@radix-ui/react-slot';
-import React, { type ComponentPropsWithRef, forwardRef, useCallback, useEffect, useRef, useState } from 'react';
+import React, {
+  type ComponentPropsWithRef,
+  forwardRef,
+  type PropsWithChildren,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import { type ThemedClassName, useMediaQuery, useTranslation } from '@dxos/react-ui';
 import { mx } from '@dxos/react-ui-theme';
@@ -14,6 +23,22 @@ import { resizeHandle, resizeHandleVertical } from '../../fragments';
 import { translationKey } from '../../translations';
 
 const MIN_WIDTH = [20, 'rem'] as const;
+
+type ScopedProps<P> = P & { __plankScope?: Scope };
+
+type DeckPlankUnit = 'rem' | 'px';
+
+type PlankContextValue = {
+  unit: DeckPlankUnit;
+  size: number;
+  setSize: (nextSize: number) => void;
+};
+
+const PLANK_NAME = 'DeckPlank';
+
+const [createPlankContext, createPlankScope] = createContextScope(PLANK_NAME, []);
+
+const [PlankProvider, usePlankContext] = createPlankContext<PlankContextValue>(PLANK_NAME);
 
 type DeckRootProps = ThemedClassName<ComponentPropsWithRef<'div'>> & { asChild?: boolean };
 
@@ -38,27 +63,64 @@ const DeckRoot = forwardRef<HTMLDivElement, DeckRootProps>(
   },
 );
 
-type DeckPlankUnit = 'rem' | 'px';
+const DeckPlankRoot = ({
+  defaultSize = 40,
+  children,
+  __plankScope,
+}: PropsWithChildren<ScopedProps<{ defaultSize?: number }>>) => {
+  const [size, setSize] = useState(defaultSize);
+  return (
+    <PlankProvider size={size} setSize={setSize} unit='rem' scope={__plankScope}>
+      {children}
+    </PlankProvider>
+  );
+};
 
 type DeckPlankProps = ThemedClassName<ComponentPropsWithRef<'article'>> & {
-  unit?: DeckPlankUnit;
   scrollIntoViewOnMount?: boolean;
   suppressAutofocus?: boolean;
 };
 
 type DeckPlankResizing = Pick<MouseEvent, 'pageX'> & { size: number } & { [Unit in DeckPlankUnit]: number };
 
-const DeckPlank = forwardRef<HTMLDivElement, DeckPlankProps>(
+const DeckPlankContent = forwardRef<HTMLDivElement, ScopedProps<DeckPlankProps>>(
   // TODO(thure): implement units (currently only `rem` is actually supported)
-  ({ unit = 'rem', classNames, style, children, scrollIntoViewOnMount, suppressAutofocus, ...props }, forwardedRef) => {
-    const { t } = useTranslation(translationKey);
+  ({ __plankScope, classNames, style, children, scrollIntoViewOnMount, suppressAutofocus, ...props }, forwardedRef) => {
     const [isSm] = useMediaQuery('sm', { ssr: false });
 
-    const [size, setSize] = useState<number>(40);
-    const [resizing, setResizing] = useState<null | DeckPlankResizing>(null);
+    const { unit = 'rem', size = 40 } = usePlankContext('DeckPlankContent', __plankScope);
     const articleElement = useRef<HTMLDivElement | null>(null);
     const ref = useComposedRefs(articleElement, forwardedRef);
     const { findFirstFocusable } = useFocusFinders();
+
+    useEffect(() => {
+      if (scrollIntoViewOnMount) {
+        articleElement.current?.scrollIntoView({ inline: 'center', behavior: 'smooth' });
+        !suppressAutofocus && articleElement.current && findFirstFocusable(articleElement.current)?.focus();
+      }
+    }, [scrollIntoViewOnMount]);
+
+    return (
+      <article
+        {...props}
+        style={{ inlineSize: isSm ? `${size}${unit}` : '100dvw', ...style }}
+        className={mx('snap-normal snap-start grid row-span-3 grid-rows-subgrid group', classNames)}
+        ref={ref}
+      >
+        {children}
+      </article>
+    );
+  },
+);
+
+type DeckPlankResizeHandleProps = {};
+
+const DeckPlankResizeHandle = forwardRef<HTMLButtonElement, ScopedProps<DeckPlankResizeHandleProps>>(
+  ({ __plankScope, ...props }, forwardedRef) => {
+    const { t } = useTranslation(translationKey);
+
+    const { unit, size, setSize } = usePlankContext('PlankResizeHandle', __plankScope);
+    const [resizing, setResizing] = useState<null | DeckPlankResizing>(null);
 
     const handlePointerUp = useCallback(({ isPrimary }: PointerEvent) => isPrimary && setResizing(null), []);
     const handlePointerMove = useCallback(
@@ -84,55 +146,44 @@ const DeckPlank = forwardRef<HTMLDivElement, DeckPlankProps>(
       };
     }, [handlePointerUp, handlePointerMove, resizing]);
 
-    useEffect(() => {
-      if (scrollIntoViewOnMount) {
-        articleElement.current?.scrollIntoView({ inline: 'center', behavior: 'smooth' });
-        !suppressAutofocus && articleElement.current && findFirstFocusable(articleElement.current)?.focus();
-      }
-    }, [scrollIntoViewOnMount]);
-
     return (
-      <>
-        <article
-          {...props}
-          style={{ inlineSize: isSm ? `${size}${unit}` : '100dvw', ...style }}
-          className={mx('snap-normal snap-start grid row-span-3 grid-rows-subgrid group', classNames)}
-          ref={ref}
-        >
-          {children}
-        </article>
-        <button
-          data-resizing={`${!!resizing}`}
-          className={resizeButtonStyles}
-          onPointerDown={({ isPrimary, pageX }) => {
-            if (isPrimary) {
-              const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
-              setResizing({ pageX, size, rem, px: 1 });
-            }
-          }}
-          onKeyDown={(event) => {
-            switch (event.key) {
-              case 'ArrowLeft':
-                event.preventDefault();
-                return setSize(size - (unit === 'px' ? 10 : 1));
-              case 'ArrowRight':
-                event.preventDefault();
-                return setSize(size + (unit === 'px' ? 10 : 1));
-            }
-          }}
-        >
-          <span className='sr-only'>{t('resize handle label')}</span>
-        </button>
-      </>
+      <button
+        data-resizing={`${!!resizing}`}
+        className={resizeButtonStyles}
+        onPointerDown={({ isPrimary, pageX }) => {
+          if (isPrimary) {
+            const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+            setResizing({ pageX, size, rem, px: 1 });
+          }
+        }}
+        onKeyDown={(event) => {
+          switch (event.key) {
+            case 'ArrowLeft':
+              event.preventDefault();
+              return setSize(size - (unit === 'px' ? 10 : 1));
+            case 'ArrowRight':
+              event.preventDefault();
+              return setSize(size + (unit === 'px' ? 10 : 1));
+          }
+        }}
+        ref={forwardedRef}
+      >
+        <span className='sr-only'>{t('resize handle label')}</span>
+      </button>
     );
   },
 );
 
-export { DeckRoot, DeckPlank, deckGrid };
+export { DeckRoot, DeckPlankRoot, DeckPlankContent, DeckPlankResizeHandle, deckGrid, createPlankScope };
 
 export const Deck = {
   Root: DeckRoot,
-  Plank: DeckPlank,
 };
 
-export type { DeckPlankProps, DeckPlankUnit, DeckRootProps };
+export const Plank = {
+  Root: DeckPlankRoot,
+  Content: DeckPlankContent,
+  ResizeHandle: DeckPlankResizeHandle,
+};
+
+export type { DeckPlankProps, DeckPlankUnit, DeckRootProps, DeckPlankResizeHandleProps, PlankContextValue };
