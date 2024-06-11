ce5d1092014c41696e068c82db02e6ba2326d806
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-06-11 12:01:45 -0700

    Update polyfill comment

--- packages/common/node-std/src/globals.js
+++ packages/common/node-std/src/globals.js
@@ -7,8 +7,8 @@ import { Buffer } from 'buffer/';
 import { process } from './process';
 
 (() => {
-  // NOTE(Zan): When running in socket context `globalThis.global` is an immutable property.
-  // I suspect they might polyfill `globalThis.global` already.
+  // NOTE(Zan): When running in native context `globalThis.global` is an immutable property.
+  // - Socket already polyfills it.
 
   // Check if `globalThis` is defined and if `global` can be assigned.
   if (typeof globalThis !== 'undefined' && !Object.getOwnPropertyDescriptor(globalThis, 'global')) {
