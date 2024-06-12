d21018853a906b7860e61c3ebcaa4a1ee77eed19
yarolegovich <yarolegovich@gmail.com>
2024-06-12 19:55:35 +0300

    TypedObject for Properties

--- packages/sdk/client-protocol/src/schema.ts
+++ packages/sdk/client-protocol/src/schema.ts
@@ -2,24 +2,14 @@
 // Copyright 2024 DXOS.org
 //
 
-import { EchoObject, S } from '@dxos/echo-schema';
+import { S, TypedObject } from '@dxos/echo-schema';
 
-// export class PropertiesType extends TypedObject({ typename: 'dxos.sdk.client.Properties', version: '0.1.0' })({
-//   name: S.optional(S.String),
-// }) {}
-
-// TODO(burdon): Replace with TypedObject.
-export const PropertiesType = S.Struct(
+export class PropertiesType extends TypedObject({ typename: 'dxos.sdk.client.Properties', version: '0.1.0' })(
   {
     name: S.optional(S.String),
   },
-  {
-    key: S.String,
-    value: S.Any,
-  },
-).pipe(EchoObject('dxos.sdk.client.Properties', '0.1.0'));
-
-export interface PropertiesType extends S.Schema.Type<typeof PropertiesType> {}
+  { record: true },
+) {}
 
 // TODO(burdon): Rename?
 export type PropertiesTypeProps = Pick<PropertiesType, 'name'>;
