5361c4011567f1fea75861e441d5b06ab20603cb
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-06-12 10:43:41 -0700

    Fix lint warning

--- packages/apps/plugins/plugin-deck/src/DeckPlugin.tsx
+++ packages/apps/plugins/plugin-deck/src/DeckPlugin.tsx
@@ -285,7 +285,6 @@ export const DeckPlugin = ({
       },
       intent: {
         resolver: (intent) => {
-          console.log('intent', intent);
           switch (intent.action) {
             case LayoutAction.SET_LAYOUT: {
               return intent.data && handleSetLayout(intent.data as LayoutAction.SetLayout);
