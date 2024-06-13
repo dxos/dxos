1c21069b2ab95cbd9124e5c1657180c08f6b715a
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-06-13 12:42:11 -0700

    feat(native): Get composer MacOS app working again with `socket/next` (#6209)

--- packages/common/node-std/src/globals.js
+++ packages/common/node-std/src/globals.js
@@ -1,11 +1,30 @@
 //
-// Copyright 2022 DXOS.org
+// Copyright 2024 DXOS.org
 //
 
 import { Buffer } from 'buffer/';
 
 import { process } from './process';
 
-globalThis.global = globalThis;
-globalThis.Buffer = Buffer;
-globalThis.process = process;
+(() => {
+  // NOTE(Zan): When running in native context `globalThis.global` is an immutable property.
+  // - Socket already polyfills it.
+
+  // Check if `globalThis` is defined and if `global` can be assigned.
+  if (typeof globalThis !== 'undefined' && !Object.getOwnPropertyDescriptor(globalThis, 'global')) {
+    Object.defineProperty(globalThis, 'global', {
+      value: globalThis,
+      configurable: true,
+      writable: false,
+      enumerable: true,
+    });
+  }
+
+  if (typeof globalThis !== 'undefined' && !globalThis.Buffer) {
+    globalThis.Buffer = Buffer;
+  }
+
+  if (typeof globalThis !== 'undefined' && !globalThis.process) {
+    globalThis.process = process;
+  }
+})();
