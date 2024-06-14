7f832babfa0724a546db7914c701b3a6499d55da
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-06-14 13:19:44 -0700

    chore(composer): Change usages of `space.key` -> `space.id` in composer graph identifiers (#7006)

--- packages/sdk/client/src/echo/util.ts
+++ packages/sdk/client/src/echo/util.ts
@@ -25,11 +25,11 @@ export const getSpace = (object: ReactiveObject<any>): Space | undefined => {
 export const isSpace = (object: unknown): object is Space => object instanceof SpaceProxy;
 
 /**
- * Fully qualified id of a reactive object is a combination of the space key and the object id.
+ * Fully qualified id of a reactive object is a combination of the space id and the object id.
  *
  * @returns Fully qualified id of a reactive object.
  */
 export const fullyQualifiedId = (object: ReactiveObject<any>): string => {
   const space = getSpace(object);
-  return space ? `${space.key.toHex()}:${object.id}` : object.id;
+  return space ? `${space.id}:${object.id}` : object.id;
 };
