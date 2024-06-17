31815e70ee57a912fecd67e12b607055308c52bc
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-06-17 13:33:46 -0700

    Feedback

--- packages/apps/plugins/plugin-stack/src/components/AddSection.tsx
+++ packages/apps/plugins/plugin-stack/src/components/AddSection.tsx
@@ -2,9 +2,8 @@
 // Copyright 2024 DXOS.org
 //
 
-import React, { useCallback } from 'react';
-
 import { useArrowNavigationGroup } from '@fluentui/react-tabster';
+import React, { useCallback } from 'react';
 
 import { StackViewType, type CollectionType } from '@braneframe/types';
 import { usePlugin, useIntentDispatcher } from '@dxos/app-framework';
@@ -12,22 +11,10 @@ import { toLocalizedString, useTranslation } from '@dxos/react-ui';
 import { getSize, mx } from '@dxos/react-ui-theme';
 
 import { STACK_PLUGIN } from '../meta';
-import { type StackPluginProvides } from '../types';
+import { type StackSectionCreator, type StackPluginProvides } from '../types';
 
-const CreatorTile = ({ creator, collection }: { creator: any; collection: CollectionType }) => {
-  const dispatch = useIntentDispatcher();
-  const { t } = useTranslation(STACK_PLUGIN);
-  const { icon: Icon, label } = creator;
-
-  const handleAdd = useCallback(async () => {
-    const { data: section } = (await dispatch(creator.intent)) ?? {};
-    collection.objects = [section];
-    const stack = collection.views[StackViewType.typename];
-    if (stack) {
-      stack.sections[section.id] = {};
-    }
-  }, [collection]);
-
+const CreatorTile = ({ Icon, label, handleAdd }: { Icon: React.FC<any>; label: string; handleAdd: () => void }) => {
+  const onClick = useCallback((_: React.MouseEvent<HTMLDivElement>) => handleAdd(), [handleAdd]);
   const onKeyDown = useCallback(
     (e: React.KeyboardEvent<HTMLDivElement>) => {
       if (e.key === 'Enter' || e.key === ' ') {
@@ -38,8 +25,6 @@ const CreatorTile = ({ creator, collection }: { creator: any; collection: Collec
     [handleAdd],
   );
 
-  const localized = toLocalizedString(label, t);
-
   const classes = mx(
     'h-full',
     'flex items-center gap-2',
@@ -49,25 +34,34 @@ const CreatorTile = ({ creator, collection }: { creator: any; collection: Collec
   );
 
   return (
-    <div
-      role='button'
-      aria-label={localized}
-      tabIndex={0}
-      className={classes}
-      onClick={handleAdd}
-      onKeyDown={onKeyDown}
-    >
+    // TODO(Zan): We should have a pure component for these large buttons?
+    <div role='button' aria-label={label} tabIndex={0} className={classes} onClick={onClick} onKeyDown={onKeyDown}>
       {Icon && <Icon className={mx(getSize(6), 'shrink-0')} />}
-      <span className='text-xs'>{localized}</span>
+      <span className='text-xs'>{label}</span>
     </div>
   );
 };
 
 export const AddSection = ({ collection }: { collection: CollectionType }) => {
   const domAttributes = useArrowNavigationGroup({ axis: 'grid' });
+  const dispatch = useIntentDispatcher();
+  const { t } = useTranslation(STACK_PLUGIN);
 
   const stackPlugin = usePlugin<StackPluginProvides>(STACK_PLUGIN);
   const stackCreators = stackPlugin?.provides?.stack.creators ?? [];
+
+  const handleAdd = useCallback(
+    async (creator: StackSectionCreator) => {
+      const { data: section } = (await dispatch(creator.intent)) ?? {};
+      collection.objects = [section];
+      const stack = collection.views[StackViewType.typename];
+      if (stack) {
+        stack.sections[section.id] = {};
+      }
+    },
+    [collection, dispatch],
+  );
+
   if (stackCreators.length === 0) {
     return null;
   }
@@ -78,9 +72,12 @@ export const AddSection = ({ collection }: { collection: CollectionType }) => {
       role='none'
       className='grid items-center gap-2 [grid-template-columns:repeat(auto-fit,minmax(120px,1fr))]'
     >
-      {stackCreators.map((creator) => (
-        <CreatorTile key={creator.id} collection={collection} creator={creator} />
-      ))}
+      {stackCreators.map((creator) => {
+        const { label, icon } = creator;
+        const localizedLabel = toLocalizedString(label, t);
+
+        return <CreatorTile key={creator.id} label={localizedLabel} Icon={icon} handleAdd={() => handleAdd(creator)} />;
+      })}
     </div>
   );
 };
