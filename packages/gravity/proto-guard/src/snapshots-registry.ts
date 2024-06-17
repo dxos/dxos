5ba1281afba241af7c670315c72c941e537bede8
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-06-17 18:16:39 +0300

    Delete testing

--- packages/gravity/proto-guard/src/snapshots-registry.ts
+++ packages/gravity/proto-guard/src/snapshots-registry.ts
@@ -7,20 +7,54 @@ import { join } from 'node:path';
 
 import { failUndefined } from '@dxos/debug';
 
-import { type SnapshotDescription } from './types';
-import { getStorageDir } from './util';
+import { getBaseDataDir } from './util';
 
-const SNAPSHOTS_PATH = join(getStorageDir(), 'snapshots.json');
+const REGISTRY_FILE = join(getBaseDataDir(), 'registry.json');
+
+export type SnapshotDescription = {
+  /**
+   * User defined name of the snapshot.
+   */
+  name: string;
+
+  /**
+   * Protocols STORAGE_VERSION of the snapshot.
+   */
+  version: number;
+
+  /**
+   * Relative path from root of the package for snapshot dataRoot .
+   * Used in Client's config.
+   */
+  dataRoot: string;
+
+  /**
+   * Relative path from root of the package to the JSON data file generated on snapshot creation.
+   * Compared with loaded data in tests.
+   */
+  jsonDataPath: string;
+
+  /**
+   * ISO string timestamp of the snapshot creation.
+   */
+  timestamp?: string;
+};
 
 export class SnapshotsRegistry {
-  static snapshots: SnapshotDescription[] = JSON.parse(readFileSync(SNAPSHOTS_PATH, 'utf-8').toString());
+  static snapshots: SnapshotDescription[] = JSON.parse(readFileSync(REGISTRY_FILE, 'utf-8').toString());
 
   static registerSnapshot(snapshot: SnapshotDescription) {
     SnapshotsRegistry.snapshots.push(snapshot);
-    writeFileSync(SNAPSHOTS_PATH, JSON.stringify(SnapshotsRegistry.snapshots, null, 2));
+    writeFileSync(REGISTRY_FILE, JSON.stringify(SnapshotsRegistry.snapshots, null, 2));
   }
 
   static getSnapshot(version: number): SnapshotDescription {
     return SnapshotsRegistry.snapshots.find((snapshot) => snapshot.version === version) ?? failUndefined();
+  }
+
+  static getLatestSnapshot(): SnapshotDescription {
+    return SnapshotsRegistry.snapshots.reduce((latest, snapshot) =>
+      snapshot.version > latest.version ? snapshot : latest,
+    );
   }
 }
