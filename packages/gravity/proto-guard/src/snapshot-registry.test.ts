1135b01de6a0707dfce79a9ef3aafe64f50da357
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-06-19 13:22:56 +0300

    Fix test

--- packages/gravity/proto-guard/src/snapshot-registry.test.ts
+++ packages/gravity/proto-guard/src/snapshot-registry.test.ts
@@ -10,10 +10,11 @@ import { SnapshotsRegistry } from './snapshots-registry';
 describe('SnapshotRegistry', () => {
   test('loads correctly', async () => {
     expect(SnapshotsRegistry.snapshots).to.deep.contains({
-      name: 'automerge-nodeFS',
+      name: 'echo-levelDB-transition',
       version: 2,
-      dataRoot: 'snapshots/2/snapshot',
-      jsonDataPath: 'snapshots/2/expected.json',
+      dataRoot: 'snapshots/echo-levelDB-transition/snapshot',
+      jsonDataPath: 'snapshots/echo-levelDB-transition/expected.json',
+      timestamp: '2024-06-18T14:38:16.733Z',
     });
   });
 });
