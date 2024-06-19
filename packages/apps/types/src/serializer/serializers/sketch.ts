c7e815236408b86d96583b0e0d3bd54209636ace
Josiah Witt <josiah@witt.life>
2024-06-19 16:20:41 -0400

    fix build after merge

--- packages/apps/types/src/serializer/serializers/sketch.ts
+++ packages/apps/types/src/serializer/serializers/sketch.ts
@@ -2,10 +2,10 @@
 // Copyright 2024 DXOS.org
 //
 
-import { Expando, loadObjectReferences, createEchoObject, create, getObjectCore } from '@dxos/client/echo';
+import { loadObjectReferences, createEchoObject, create, getObjectCore } from '@dxos/client/echo';
 
 import { type TypedObjectSerializer, validFilename } from './default';
-import { SketchType } from '../../schema';
+import { CanvasType, SketchType } from '../../schema';
 
 export const serializer: TypedObjectSerializer<SketchType> = {
   filename: (object) => ({
@@ -15,7 +15,7 @@ export const serializer: TypedObjectSerializer<SketchType> = {
 
   serialize: async ({ object }): Promise<string> => {
     // TODO(wittjosiah): Implement `toJSON` method?
-    const data = await loadObjectReferences(object, (s) => s.data);
+    const data = await loadObjectReferences(object, (s) => s.canvas);
     const sketch = { ...object, data: { ...data } };
     return JSON.stringify(sketch, null, 2);
   },
@@ -24,30 +24,30 @@ export const serializer: TypedObjectSerializer<SketchType> = {
     const parsed = JSON.parse(content);
 
     if (existingSketch instanceof SketchType) {
-      existingSketch.title = parsed.title;
-      const data = await loadObjectReferences(existingSketch, (s) => s.data);
-      setSketchData(data, parsed.data.content);
+      existingSketch.name = parsed.title;
+      const data = await loadObjectReferences(existingSketch, (s) => s.canvas);
+      setCanvasContent(data, parsed.data.content);
       return existingSketch;
     } else {
-      const data = create(Expando, {});
-      const sketch = createEchoObject(create(SketchType, { title: parsed.title, data }));
+      const canvas = create(CanvasType, { content: {} });
+      const sketch = createEchoObject(create(SketchType, { name: parsed.title, canvas }));
 
       if (!newId) {
         const core = getObjectCore(sketch);
         core.id = parsed.id;
 
-        const dataCore = getObjectCore(data);
-        dataCore.id = parsed.data.id;
+        const canvasCore = getObjectCore(canvas);
+        canvasCore.id = parsed.data.id;
       }
 
-      setSketchData(data, parsed.data.content);
+      setCanvasContent(canvas, parsed.data.content);
 
       return sketch;
     }
   },
 };
 
-const setSketchData = (object: Expando, content: any) => {
+const setCanvasContent = (object: CanvasType, content: any) => {
   object.content = {};
   Object.entries(content).forEach(([key, value]) => {
     object.content[key] = value;
