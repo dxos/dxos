98eae89ffb5423b7af222bfd194f885b8851adcc
Josiah Witt <josiah@witt.life>
2024-06-24 07:09:29 -0400

    docs: fix snippets (#7042)

--- docs/content/guide/echo/typescript/snippets/invite-to-space.ts
+++ docs/content/guide/echo/typescript/snippets/invite-to-space.ts
@@ -10,16 +10,22 @@ const client = new Client();
 (async () => {
   await client.initialize();
   // ensure an identity exists
-  if (!client.halo.identity.get()) await client.halo.createIdentity();
+  if (!client.halo.identity.get()) {
+    await client.halo.createIdentity();
+  }
+
   // create a space
   const space = await client.spaces.create();
+
   // create an invitation to join the space
   const invitation = space.share();
+
   // share this code with a friend, it will be used to locate the peer and
   // establish a secure connection
   const code = InvitationEncoder.encode(invitation.get());
+
   // later we will pass this second authentication code to our friend over a
   // side-channel and they'll send it to us over the new connection which
   // will verify that it's secure.
   const authCode = invitation.get().authCode;
-})()
+})();
