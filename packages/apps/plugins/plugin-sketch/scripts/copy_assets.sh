aa46ff03a0e890f11b2a3fabd9ca67ca46fe0378
Josiah Witt <josiah@witt.life>
2024-06-27 18:14:32 -0400

    chore: remove rsync from copy_assets.sh (#7120)

--- packages/apps/plugins/plugin-sketch/scripts/copy_assets.sh
+++ packages/apps/plugins/plugin-sketch/scripts/copy_assets.sh
@@ -11,4 +11,5 @@ cp -R ${SRC}/fonts ${DEST}
 cp -R ${SRC}/icons ${DEST}
 cp -R ${SRC}/translations ${DEST}
 
-rsync -av --ignore-existing ./assets/ ${DEST}/
+cp -R ./assets/fonts/* ${DEST}/fonts
+cp -R ./assets/icons/icon/* ${DEST}/icons/icon
