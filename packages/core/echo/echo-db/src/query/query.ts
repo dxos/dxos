d39f7ccebdaaca7e4c0818a417b04bf0e23f5fb4
yarolegovich <yarolegovich@gmail.com>
2024-06-27 14:34:16 +0300

    fixed index one-shot queries and improved timeout handling

--- packages/core/echo/echo-db/src/query/query.ts
+++ packages/core/echo/echo-db/src/query/query.ts
@@ -78,7 +78,7 @@ export interface QuerySource {
   /**
    * One-shot query.
    */
-  run(filter: Filter): Promise<QueryResult[]>;
+  run(filter: Filter, options?: { timeout?: number }): Promise<QueryResult[]>;
 
   /**
    * Set the filter and trigger continuous updates.
@@ -196,7 +196,8 @@ export class Query<T extends {} = any> {
     const filter = this._filter;
     const runTasks = [...this._sources.values()].map(async (s) => {
       try {
-        return await asyncTimeout(s.run(filter), timeout.timeout);
+        const sourceTimeout = { timeout: timeout.timeout * 0.95 };
+        return await asyncTimeout(s.run(filter, sourceTimeout), timeout.timeout);
       } catch (err) {
         if (!(err instanceof TimeoutError)) {
           log.catch(err);
