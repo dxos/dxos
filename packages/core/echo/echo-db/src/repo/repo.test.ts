c5c145cb4cc3eb1ad410b7e25a2391625711acd6
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-07-03 17:35:35 +0300

    Add Repo tests

--- packages/core/echo/echo-db/src/repo/repo.test.ts
+++ packages/core/echo/echo-db/src/repo/repo.test.ts
@@ -4,19 +4,21 @@
 
 import { expect } from 'chai';
 
+import { Trigger } from '@dxos/async';
 import { AutomergeHost, DataServiceImpl } from '@dxos/echo-pipeline';
 import { IndexMetadataStore } from '@dxos/indexing';
 import { createTestLevel } from '@dxos/kv-store/testing';
-import { openAndClose } from '@dxos/test';
+import { describe, test, openAndClose } from '@dxos/test';
 
-import { RepoClient } from '../../../dist/lib/browser/chunk-SASG55ZD.mjs';
+import { RepoClient } from '../repo';
+import { TestReplicationNetwork } from '../testing';
 
-describe.only('RepoClient', () => {
+describe('RepoClient', () => {
   test('create document from client', async () => {
     const { host, clientRepo } = await setup();
 
-    const clientHandle = clientRepo.create();
-    const hostHandle = host._repo!.find<{ text: string }>(clientHandle.url);
+    const clientHandle = clientRepo.create<{ text: string }>();
+    const hostHandle = host.repo!.find<{ text: string }>(clientHandle.url);
     const text = 'Hello World!';
     clientHandle.change((doc: any) => {
       doc.text = text;
@@ -24,6 +26,61 @@ describe.only('RepoClient', () => {
 
     await hostHandle.whenReady();
     expect(hostHandle.docSync()?.text).to.equal(text);
+
+    {
+      // Change from another peer.
+      const receivedChange = new Trigger();
+      clientHandle.once('change', () => receivedChange.wake());
+      const text = 'Hello World 2!';
+      hostHandle.change((doc: any) => {
+        doc.text = text;
+      });
+      await receivedChange.wait();
+      expect(clientHandle.docSync().text).to.equal(text);
+    }
+  });
+
+  test('load document from client', async () => {
+    const { host, clientRepo } = await setup();
+
+    const text = 'Hello World!';
+    const hostHandle = host.repo!.create<{ text: string }>({ text });
+    const clientHandle = clientRepo.find<{ text: string }>(hostHandle.url);
+    await clientHandle.whenReady();
+
+    expect(clientHandle.docSync()?.text).to.equal(text);
+  });
+
+  test('two peers exchange document', async () => {
+    const peer1 = await setup();
+    const peer2 = await setup();
+    const network = await new TestReplicationNetwork().open();
+
+    await peer1.host.addReplicator(await network.createReplicator());
+    await peer2.host.addReplicator(await network.createReplicator());
+
+    const text = 'Hello World!';
+    const handle1 = peer1.clientRepo.create<{ text: string }>();
+    const handle2 = peer2.clientRepo.find<{ text: string }>(handle1.url);
+
+    handle1.change((doc: any) => {
+      doc.text = text;
+    });
+
+    await handle2.whenReady();
+    expect(handle2.docSync()?.text).to.equal(text);
+
+    {
+      // Change from another peer.
+      const receivedChange = new Trigger();
+      handle1.once('change', () => receivedChange.wake());
+      const text = 'Hello World 2!';
+      handle2.change((doc: any) => {
+        doc.text = text;
+      });
+      await receivedChange.wait();
+      expect(handle1.docSync().text).to.equal(text);
+    }
   });
 });
 
@@ -37,5 +94,6 @@ const setup = async (kv = createTestLevel()) => {
 
   const dataService = new DataServiceImpl(host);
   const clientRepo = new RepoClient(dataService);
+  await openAndClose(clientRepo);
   return { kv, host, dataService, clientRepo };
 };
