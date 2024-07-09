61ffac9d2f3c738f43805503c118b6ed7e44bff5
Dmytro Maretskyi <maretskii@gmail.com>
2024-07-09 13:00:34 +0200

    WIP

--- packages/core/echo/echo-pipeline/src/automerge/collection-synchronizer.ts
+++ packages/core/echo/echo-pipeline/src/automerge/collection-synchronizer.ts
@@ -1,8 +1,19 @@
 import type { PeerId } from '@dxos/automerge/automerge-repo';
+import { Resource } from '@dxos/context';
 
-export class CollectionSynchronizer {
+export type CollectionSynchronizerParams = {
+  sendCollectionState: (peerId: PeerId, collectionId: string, state: unknown) => void;
+};
 
+export class CollectionSynchronizer extends Resource {
   onCollectionStateQueried(collectionId: string, peerId: PeerId) {}
 
   updateRemoteState(peerId: PeerId, collectionId: string, state: unknown) {}
 }
+
+export type CollectionState = {
+  /**
+   * DocumentId -> Heads.
+   */
+  documents: Record<string, string[]>;
+};
