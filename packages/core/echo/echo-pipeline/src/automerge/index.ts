40d1cece5b3d45ae0584011c8e0e7847e86bbcb9
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2024-07-17 16:04:09 +0200

    feat: Collection sync (#7244)
    Co-authored-by: yarolegovich <yarolegovich@gmail.com>

--- packages/core/echo/echo-pipeline/src/automerge/index.ts
+++ packages/core/echo/echo-pipeline/src/automerge/index.ts
@@ -6,3 +6,5 @@ export * from './automerge-host';
 export * from './leveldb-storage-adapter';
 export * from './mesh-echo-replicator';
 export * from './echo-replicator';
+export { diffCollectionState } from './collection-synchronizer';
+export * from './space-collection';
