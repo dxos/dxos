0dafd64f7580f9d03d314eac0cb45742e355f814
Dmytro Maretskyi <maretskii@gmail.com>
2024-07-22 13:52:15 +0200

    Workaround https://github.com/cloudflare/workers-sdk/issues/5591

--- packages/experimental/cf-labs/src/basic.test.ts
+++ packages/experimental/cf-labs/src/basic.test.ts
@@ -5,6 +5,11 @@ test('2 + 2', ({ expect }) => {
   expect(2 + 2).toBe(4);
 });
 
+test('console.log', () => {
+  const logger = console.log.bind(console);
+  logger('testing');
+});
+
 test('dxos/log', () => {
   log.info('testing');
 });
