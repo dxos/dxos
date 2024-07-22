f750937a944829d2d63063e8bb9c23dfbbfc55e9
Dmytro Maretskyi <maretskii@gmail.com>
2024-07-22 13:39:06 +0200

    Failing log test

--- packages/experimental/cf-labs/src/basic.test.ts
+++ packages/experimental/cf-labs/src/basic.test.ts
@@ -1,5 +1,10 @@
+import { log } from '@dxos/log';
 import { test } from 'vitest';
 
 test('2 + 2', ({ expect }) => {
   expect(2 + 2).toBe(4);
+});
+
+test('dxos/log', () => {
+  log.info('testing');
 });
