04393048aef50789549fd88397fd93ef10cd7ecb
Josiah Witt <josiah@witt.life>
2024-07-23 08:52:34 -0400

    fix: better unknown object filtering
    - metadata object always exists, base condition on whether or not it has keys
    - also include fallback label & icon for edge cases

--- packages/apps/plugins/plugin-space/src/util.tsx
+++ packages/apps/plugins/plugin-space/src/util.tsx
@@ -17,6 +17,7 @@ import {
   type IconProps,
   LockSimpleOpen,
   LockSimple,
+  Placeholder,
 } from '@phosphor-icons/react';
 import React from 'react';
 
@@ -423,7 +424,7 @@ export const createObjectNode = ({
   }
 
   const metadata = resolve(type);
-  if (!metadata) {
+  if (Object.keys(metadata).length === 0) {
     return undefined;
   }
 
@@ -438,8 +439,10 @@ export const createObjectNode = ({
     data: object,
     properties: {
       ...partials,
-      label: metadata.label?.(object) || object.name || metadata.placeholder,
-      icon: metadata.icon,
+      label: metadata.label?.(object) ||
+        object.name ||
+        metadata.placeholder || ['unnamed object label', { ns: SPACE_PLUGIN }],
+      icon: metadata.icon ?? (() => <Placeholder />),
       testId: 'spacePlugin.object',
       persistenceClass: 'echo',
       persistenceKey: space?.id,
