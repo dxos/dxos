742dd125b85451d40cec7ffda2fd99e3d0fe3851
Dmytro Maretskyi <maretskii@gmail.com>
2024-07-23 10:29:47 +0200

    Delete keyring test

--- packages/experimental/cf-labs/src/tests/credentials.test.ts
+++
@@ -1,9 +0,0 @@
-import { test } from 'vitest';
-
-import { Keyring } from '@dxos/keyring';
-
-test('keyring', async () => {
-  const keyring = new Keyring();
-
-  const key = await keyring.createKey();
-});
