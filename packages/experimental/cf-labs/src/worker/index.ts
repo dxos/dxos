f372445c7c05f717ee1f014796269ae02b61cb62
Dmytro Maretskyi <maretskii@gmail.com>
2024-07-23 11:15:13 +0200

    Fix worker type

--- packages/experimental/cf-labs/src/worker/index.ts
+++ packages/experimental/cf-labs/src/worker/index.ts
@@ -2,8 +2,10 @@
 // Copyright 2024 DXOS.org
 //
 
+import type { Request as WorkerRequest, ExecutionContext } from '@cloudflare/workers-types/experimental';
+
 export default {
-  fetch: async (request: any, env: any, ctx: any) => {
+  fetch: async (request: WorkerRequest, env: any, ctx: ExecutionContext) => {
     return new Response('Hello World!');
   },
 };
