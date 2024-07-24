0ba3d836e2b282acae498431c88067e8cc471093
thure <will@braneframe.com>
2024-07-24 12:02:40 -0700

    Restore keyboard shortcuts graph traversal.

--- packages/apps/plugins/plugin-navtree/src/NavTreePlugin.tsx
+++ packages/apps/plugins/plugin-navtree/src/NavTreePlugin.tsx
@@ -18,10 +18,11 @@ import {
   type GraphProvides,
   parseGraphPlugin,
 } from '@dxos/app-framework';
-import { createExtension, isGraphNode, type Node } from '@dxos/app-graph';
+import { createExtension, isAction, isGraphNode, type Node } from '@dxos/app-graph';
 import { create } from '@dxos/echo-schema';
 import { Keyboard } from '@dxos/keyboard';
 import { type LayoutCoordinate } from '@dxos/react-ui-deck';
+import { getHostPlatform } from '@dxos/util';
 
 import {
   CommandsDialogContent,
@@ -32,7 +33,7 @@ import {
   NotchStart,
 } from './components';
 import { CommandsTrigger } from './components/CommandsTrigger';
-import meta, { NAVTREE_PLUGIN } from './meta';
+import meta, { KEY_BINDING, NAVTREE_PLUGIN } from './meta';
 import translations from './translations';
 import { getActions, getChildren, type NavTreeItem, type NavTreeItemGraphNode } from './util';
 
@@ -58,9 +59,41 @@ export const NavTreePlugin = (): PluginDefinition<NavTreePluginProvides> => {
 
       state.root = graph.root as NavTreeItemGraphNode;
       getChildren(graph, state.root);
-      getActions(graph, state.root, [state.root.id]);
+      getActions(graph, state.root);
 
       state.openItemPaths = new Set(['root']);
+
+      // TODO(wittjosiah): Factor out.
+      // TODO(wittjosiah): Handle removal of actions.
+      graph.subscribeTraverse({
+        onlyLoaded: true,
+        visitor: (node, path) => {
+          let shortcut: string | undefined;
+          if (typeof node.properties.keyBinding === 'object') {
+            const availablePlatforms = Object.keys(node.properties.keyBinding);
+            const platform = getHostPlatform();
+            shortcut = availablePlatforms.includes(platform)
+              ? node.properties.keyBinding[platform]
+              : platform === 'ios'
+                ? node.properties.keyBinding.macos // Fallback to macos if ios-specific bindings not provided.
+                : platform === 'linux' || platform === 'unknown'
+                  ? node.properties.keyBinding.windows // Fallback to windows if platform-specific bindings not provided.
+                  : undefined;
+          } else {
+            shortcut = node.properties.keyBinding;
+          }
+
+          if (shortcut && isAction(node)) {
+            Keyboard.singleton.getContext(path.slice(0, -1).join('/')).bind({
+              shortcut,
+              handler: () => {
+                void node.data({ node, caller: KEY_BINDING });
+              },
+              data: node.properties.label,
+            });
+          }
+        },
+      });
 
       // TODO(burdon): Create context and plugin.
       Keyboard.singleton.initialize();
