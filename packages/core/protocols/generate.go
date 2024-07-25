0b75351be7e8039c40809c6f27087ad5ec9e606f
Nathan A. Ferch <nf+github@marginal.net>
2024-07-25 19:34:53 -0400

    chore: build go protobufs

---
+++ packages/core/protocols/generate.go
@@ -0,0 +1,13 @@
+//go:generate protoc --experimental_allow_proto3_optional -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto dxos/keys.proto
+//go:generate protoc --experimental_allow_proto3_optional -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto dxos/config.proto
+//go:generate protoc --experimental_allow_proto3_optional -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto dxos/error.proto
+//go:generate protoc --experimental_allow_proto3_optional -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto dxos/rpc.proto
+//go:generate protoc --experimental_allow_proto3_optional -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto dxos/echo/timeframe.proto
+//go:generate protoc --experimental_allow_proto3_optional -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto dxos/halo/credentials.proto
+//go:generate protoc -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto --go-dxrpc_out=. --go-dxrpc_opt=module=github.com/dxos/dxos/proto dxos/service/publisher.proto
+//go:generate protoc -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto --go-dxrpc_out=. --go-dxrpc_opt=module=github.com/dxos/dxos/proto dxos/service/tunnel.proto
+//go:generate protoc -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto --go-dxrpc_out=. --go-dxrpc_opt=module=github.com/dxos/dxos/proto dxos/mesh/signal.proto
+//go:generate protoc -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto --go-dxrpc_out=. --go-dxrpc_opt=module=github.com/dxos/dxos/proto dxos/service/supervisor.proto
+//go:generate protoc -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto --go-dxrpc_out=. --go-dxrpc_opt=module=github.com/dxos/dxos/proto dxos/service/agentmanager.proto
+//go:generate protoc -I=src/proto --go_out=. --go_opt=module=github.com/dxos/dxos/proto --go-dxrpc_out=. --go-dxrpc_opt=module=github.com/dxos/dxos/proto example/testing/rpc.proto
+package dxos
---
+++ packages/core/protocols/go.mod
@@ -0,0 +1,9 @@
+module github.com/dxos/dxos/proto
+
+go 1.21.0
+
+toolchain go1.21.7
+
+require google.golang.org/protobuf v1.34.2
+
+require github.com/google/go-cmp v0.6.0 // indirect
