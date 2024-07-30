15a6d36f8123998eef91b5052c3ec2f1bfacae5c
yarolegovich <yarolegovich@gmail.com>
2024-07-30 12:53:29 +0300

    fixed build

--- packages/devtools/devtools/src/components/performance/panels/ReplicatorMessagesPanel.tsx
+++ packages/devtools/devtools/src/components/performance/panels/ReplicatorMessagesPanel.tsx
@@ -5,38 +5,54 @@
 import { Database } from '@phosphor-icons/react';
 import React from 'react';
 
+import { type EchoDataStats } from '@dxos/echo-db';
+
 import { type DatabaseInfo } from '../../../hooks';
 import { type CustomPanelProps, Panel } from '../Panel';
+
+type CountsByMessageType = EchoDataStats['replicator']['countByMessageType'];
+
+type MessageTypeSummary = {
+  direction: 'in' | 'out';
+  type: string;
+  quantity: string;
+};
 
 export const ReplicatorMessagesPanel = ({ database, ...props }: CustomPanelProps<{ database?: DatabaseInfo }>) => {
   const replicatorStats = database?.dataStats?.replicator;
   let receivedTotal = 0;
   let sentTotal = 0;
-  const entries: [string, string, string][] = Object.entries(replicatorStats?.countByMessageType ?? ({} as any))
+  const entries: MessageTypeSummary[] = Object.entries(
+    replicatorStats?.countByMessageType ?? ({} as CountsByMessageType),
+  )
     .flatMap(([type, counts]) => {
       sentTotal += counts.sent;
       receivedTotal += counts.received;
       const avgMessageSize = replicatorStats?.avgSizeByMessageType[type];
       const sizeString = avgMessageSize !== undefined ? ` (${formatNumber(avgMessageSize)} bytes)` : '';
       return [
-        ['in', type, `${formatNumber(counts.received)}${sizeString}`],
-        ['out', type, `${formatNumber(counts.sent)}${sizeString}`],
-      ];
+        { direction: 'in', type, quantity: `${formatNumber(counts.received)}${sizeString}` },
+        { direction: 'out', type, quantity: `${formatNumber(counts.sent)}${sizeString}` },
+      ] as MessageTypeSummary[];
     })
-    .sort(([type1, name1], [type2, name2]) => {
-      const cmp = name1.localeCompare(name2);
-      return cmp === 0 ? type1.localeCompare(type2) : cmp;
+    .sort((m1, m2) => {
+      const cmp = m1.type.localeCompare(m2.type);
+      return cmp === 0 ? m1.direction.localeCompare(m2.direction) : cmp;
     });
   return (
     <Panel
       {...props}
       icon={Database}
       title='Database replicator messages'
-      info={`last ${formatNumber(receivedTotal)}-in & ${formatNumber(sentTotal)}-out`}
+      info={
+        <span>
+          `last ${formatNumber(receivedTotal)}-in & ${formatNumber(sentTotal)}-out`
+        </span>
+      }
     >
       <table className='table-auto w-full text-xs font-mono'>
         <tbody>
-          {entries.map(([direction, type, quantity], i) => (
+          {entries.map(({ direction, type, quantity }, i) => (
             <tr key={i}>
               <td className='p-1 text-left'>{direction}</td>
               <td className='p-1 text-center overflow-hidden'>{type}</td>
