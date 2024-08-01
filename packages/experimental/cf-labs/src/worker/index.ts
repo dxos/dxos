c09b6fd4fe32788f8bbf4c3e534d401949828cdf
Dmytro Maretskyi <maretskii@gmail.com>
2024-08-01 13:00:34 +0200

    cloudflare replicator test

--- packages/experimental/cf-labs/src/worker/index.ts
+++ packages/experimental/cf-labs/src/worker/index.ts
@@ -5,12 +5,55 @@
 import type { Request as WorkerRequest, ExecutionContext } from '@cloudflare/workers-types/experimental';
 import { HypercoreFactory } from '@dxos/hypercore';
 import { next } from '@dxos/automerge/automerge';
+import util from 'node:util';
+import { length } from 'effect/Array';
+import { sleep } from '@dxos/async';
 
 export default {
   fetch: async (request: WorkerRequest, env: any, ctx: ExecutionContext) => {
     const factory = new HypercoreFactory();
-    const feed = factory.createFeed(Buffer.from('1111111111111111111111111111111111111111'));
-    console.log('feed', feed.key);
-    return new Response('Hello World!');
+    const core = factory.createFeed(Buffer.from('1111111111111111111111111111111111111111'), {
+      secretKey: Buffer.from('secret'),
+      crypto: {
+        sign: (message: Buffer, secretKey: any, cb: (err: any, msg: any) => void) => {
+          cb(null, message);
+        },
+
+        verify: async (message, signature, key, cb) => {
+          cb(null, true);
+        },
+      },
+      noiseKeyPair: {}, // We're not using noise.
+    });
+
+    const open = util.promisify(core.open.bind(core));
+    await open;
+
+    console.log({ length: core.length });
+
+    const append = util.promisify(core.append.bind(core));
+
+    const seq = await append('test');
+    console.log({ seq, length: core.length });
+
+    console.log('feed', core.key);
+
+    const replicationStream = core.replicate(true, {
+      live: true,
+      upload: true,
+      download: true,
+      noise: false,
+      encrypted: false,
+      maxRequests: 1024,
+    });
+
+    let resp = '';
+    replicationStream.on('data', (msg) => {
+      resp += msg.toString('hex');
+    });
+
+    await sleep(500);
+
+    return new Response(resp);
   },
 };
