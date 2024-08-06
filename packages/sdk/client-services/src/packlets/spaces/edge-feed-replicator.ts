398eee2042d74c68452b3d2f5f248bd6b6f78059
Dmytro Maretskyi <maretskii@gmail.com>
2024-08-06 17:30:06 +0200

    WIP

---
+++ packages/sdk/client-services/src/packlets/spaces/edge-feed-replicator.ts
@@ -0,0 +1,228 @@
+import { Event, Mutex, scheduleMicroTask } from '@dxos/async';
+import { LifecycleState, Resource, type Context } from '@dxos/context';
+import type { FeedBlock, ProtocolMessage } from '@dxos/protocols/feed-replication';
+import { FeedWrapper } from '@dxos/feed-store';
+import { invariant } from '@dxos/invariant';
+import { PublicKey, type SpaceId } from '@dxos/keys';
+import { log } from '@dxos/log';
+import { Message, type Messenger } from '@dxos/edge-client';
+import { arrayToBuffer, bufferToArray, ComplexMap, defaultMap, rangeFromTo } from '@dxos/util';
+import { decode as decodeCbor, encode as encodeCbor } from 'cbor-x';
+
+export type EdgeFeedReplicatorParams = {
+  messenger: Messenger;
+  spaceId: SpaceId;
+};
+
+export class EdgeFeedReplicator extends Resource {
+  private readonly _messenger: Messenger;
+  private readonly _spaceId: SpaceId;
+  private readonly _feeds = new ComplexMap<PublicKey, FeedWrapper<any>>(PublicKey.hash);
+
+  private _connectionCtx?: Context = undefined;
+  private _connected = false;
+  /**
+   * Feed length at service.
+   */
+  private _remoteLength = new ComplexMap<PublicKey, number>(PublicKey.hash);
+
+  /**
+   * Protects against concurrent pushes so that remote length is updated consistently.
+   */
+  private _pushMutex = new ComplexMap<PublicKey, Mutex>(PublicKey.hash);
+
+  constructor({ messenger, spaceId }: EdgeFeedReplicatorParams) {
+    super();
+    this._messenger = messenger;
+    this._spaceId = spaceId;
+  }
+
+  protected override async _open(): Promise<void> {
+    this._ctx.onDispose(
+      this._messenger.addListener(async (message) => {
+        log.info('recv', { message });
+        if (!message.serviceId) return;
+        const [service, ...rest] = message.serviceId.split(':');
+        if (service !== 'hypercore-replicator') return;
+
+        const [spaceId] = rest;
+        log.info('compare spaceID', { spaceId, _spaceId: this._spaceId });
+        if (spaceId !== this._spaceId) return;
+
+        const payload = decodeCbor(message.payload!.value) as ProtocolMessage;
+
+        this._onMessage(payload);
+      }),
+    );
+
+    this._connected = true;
+    this._connectionCtx = this._ctx.derive();
+    for (const feed of this._feeds.values()) {
+      await this._replicateFeed(feed);
+    }
+  }
+
+  protected override async _close(): Promise<void> {
+    this._connected = false;
+
+    this._connected = false;
+    await this._connectionCtx?.dispose();
+    this._connectionCtx = undefined;
+    this._remoteLength.clear();
+  }
+
+  async addFeed(feed: FeedWrapper<any>) {
+    this._feeds.set(feed.key, feed);
+
+    if (this._connected) {
+      await this._replicateFeed(feed);
+    }
+  }
+
+  private _getPushMutex(key: PublicKey) {
+    return defaultMap(this._pushMutex, key, () => new Mutex());
+  }
+
+  private async _replicateFeed(feed: FeedWrapper<any>) {
+    invariant(this._connectionCtx);
+
+    this._sendMessage({
+      type: 'get-metadata',
+      feedKey: feed.key.toHex(),
+    });
+
+    Event.wrap(feed.core as any, 'append').on(this._connectionCtx, async () => {
+      using _guard = await this._getPushMutex(feed.key).acquire();
+
+      if (!this._remoteLength.has(feed.key)) {
+        return;
+      }
+
+      const remoteLength = this._remoteLength.get(feed.key)!;
+      if (remoteLength < feed.length) {
+        await this._pushBlocks(feed, remoteLength, feed.length);
+      }
+    });
+  }
+
+  private _sendMessage(message: ProtocolMessage) {
+    invariant(message.feedKey);
+    const payloadValue = bufferToArray(encodeCbor(message));
+
+    log.info('sending message', { payloadLength: payloadValue.length, feedKey: message.feedKey });
+    this._messenger.send(
+      new Message({
+        source: {
+          identityKey: this._messenger.identityKey.toHex(),
+          peerKey: this._messenger.deviceKey.toHex(),
+        },
+        serviceId: `hypercore-replicator:${this._spaceId}`,
+        payload: { value: payloadValue },
+      }),
+    );
+  }
+
+  private _onMessage(message: ProtocolMessage) {
+    log.info('received message', { message });
+
+    scheduleMicroTask(this._ctx, async () => {
+      switch (message.type) {
+        case 'metadata': {
+          const feedKey = PublicKey.fromHex(message.feedKey);
+          const feed = this._feeds.get(feedKey);
+          if (!feed) {
+            log.warn('Feed not found', { feedKey });
+            return;
+          }
+
+          using _guard = await this._getPushMutex(feed.key).acquire();
+
+          this._remoteLength.set(feedKey, message.length);
+
+          if (message.length > feed.length) {
+            this._sendMessage({
+              type: 'request',
+              feedKey: feedKey.toHex(),
+              range: { from: feed.length, to: message.length },
+            });
+          } else if (message.length < feed.length) {
+            await this._pushBlocks(feed, message.length, feed.length);
+          }
+
+          break;
+        }
+
+        case 'data': {
+          const feedKey = PublicKey.fromHex(message.feedKey);
+          const feed = this._feeds.get(feedKey);
+          if (!feed) {
+            log.warn('Feed not found', { feedKey });
+            return;
+          }
+
+          await this._integrateBlocks(feed, message.blocks);
+          break;
+        }
+
+        default: {
+          log.warn('Unknown message', { ...message });
+        }
+      }
+    });
+  }
+
+  private async _pushBlocks(feed: FeedWrapper<any>, from: number, to: number) {
+    const blocks: FeedBlock[] = await Promise.all(
+      rangeFromTo(from, to).map(async (index) => {
+        const data = await feed.get(index);
+        const proof = await feed.proof(index);
+
+        return {
+          index,
+          data,
+          nodes: proof.nodes,
+          signature: proof.signature,
+        } satisfies FeedBlock;
+      }),
+    );
+
+    log.info('pushing blocks', { feed: feed.key.toHex(), from, to });
+
+    this._sendMessage({
+      type: 'data',
+      feedKey: feed.key.toHex(),
+      blocks,
+    });
+    this._remoteLength.set(feed.key, to);
+  }
+
+  private async _integrateBlocks(feed: FeedWrapper<any>, blocks: FeedBlock[]) {
+    log.info('integrating blocks', { feed: feed.key.toHex(), blocks: blocks.length });
+
+    for (const block of blocks) {
+      if (feed.has(block.index)) {
+        continue;
+      }
+      const blockBuffer = bufferizeBlock(block);
+
+      await feed.put(block.index, blockBuffer.data, { nodes: blockBuffer.nodes, signature: blockBuffer.signature });
+    }
+  }
+}
+
+// hypercore requires buffers
+const bufferizeBlock = (block: FeedBlock): FeedBlock => ({
+  index: block.index,
+  data: block.data,
+  nodes: block.nodes.map((node) => ({
+    index: node.index,
+    hash: arrayToBuffer(node.hash),
+    size: node.size,
+  })),
+  signature: arrayToBuffer(block.signature),
+});
+
+// TODO(dmaretskyi): Protocol types.
+// TODO(dmaretskyi): Automerge core protocol vs websocket protocol.
+// TODO(dmaretskyi): Backpressure.
+// TODO(dmaretskyi): Worker share policy.
