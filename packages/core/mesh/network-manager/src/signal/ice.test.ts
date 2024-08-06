6b5386762679954455079e5cf5580bc69e315b9a
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-08-06 14:37:18 +0300

    Add failure test

--- packages/core/mesh/network-manager/src/signal/ice.test.ts
+++ packages/core/mesh/network-manager/src/signal/ice.test.ts
@@ -6,42 +6,59 @@ import { expect } from 'chai';
 import fetchMock from 'fetch-mock';
 
 import { Config } from '@dxos/config';
-import { log } from '@dxos/log';
 import { test, describe } from '@dxos/test';
 
 import { getIceServers } from './ice';
 
 describe('Ice', () => {
+  const configIceServers = [
+    { urls: 'stun:kube.dxos.org:3478' },
+    { urls: 'turn:kube.dxos.org:3478', username: 'dxos', credential: 'dxos' },
+  ];
+  const providerUrl = 'http://localhost:8787/ice';
+  const providedIceServers: RTCIceServer[] = [
+    {
+      urls: ['stun:stun.cloudflare.com:3478', 'turn:turn.cloudflare.com:3478?transport=udp'],
+      username: 'test-user',
+      credential: 'test-cred',
+    },
+  ];
+
   afterEach(() => {
-    fetchMock.reset();
+    fetchMock.restore();
   });
 
-  test.only('ice provider results are correctlly merged', async () => {
+  test('ice provider results are correctly merged', async () => {
     // set-up mock
-    const providedIceServers = [
-      {
-        urls: ['stun:stun.cloudflare.com:3478', 'turn:turn.cloudflare.com:3478?transport=udp'],
-        username: 'test-user',
-        credential: 'test-cred',
-      },
-    ];
-    fetchMock.getOnce('http://localhost:8787/ice', { iceServers: providedIceServers });
 
-    const configIceServers = [
-      { urls: 'stun:kube.dxos.org:3478' },
-      { urls: 'turn:kube.dxos.org:3478', username: 'dxos', credential: 'dxos' },
-    ];
+    fetchMock.getOnce(providerUrl, { iceServers: providedIceServers });
+
     const config = new Config({
       runtime: {
         services: {
-          iceProviders: [{ url: 'http://localhost:8787/ice' }],
+          iceProviders: [{ url: providerUrl }],
           ice: configIceServers,
         },
       },
     });
 
     const iceServers = await getIceServers(config);
-    log.info('iceServers', { iceServers });
     expect(iceServers).to.deep.eq([...configIceServers, ...providedIceServers]);
+  });
+
+  test('ice provider errors are handled', async () => {
+    // mock error
+    fetchMock.getOnce(providerUrl, 500);
+
+    const config = new Config({
+      runtime: {
+        services: {
+          iceProviders: [{ url: providerUrl }],
+          ice: configIceServers,
+        },
+      },
+    });
+    const iceServers = await getIceServers(config);
+    expect(iceServers).to.deep.eq(configIceServers);
   });
 });
--- packages/core/mesh/network-manager/src/signal/ice.ts
+++ packages/core/mesh/network-manager/src/signal/ice.ts
@@ -3,6 +3,8 @@
 //
 
 import { type Config } from '@dxos/config';
+import { log } from '@dxos/log';
+import { isNotNullOrUndefined } from '@dxos/util';
 
 /**
  * @returns List of ICE servers provided by DXOS config merged with the ICE servers provided by IceProviders.
@@ -10,11 +12,16 @@ import { type Config } from '@dxos/config';
 export const getIceServers = async (config: Config): Promise<RTCIceServer[]> => {
   const iceProviders = config.get('runtime.services.iceProviders');
   const providedIceServers: RTCIceServer[] = [];
+
   if (iceProviders?.length && iceProviders.length > 0) {
     const responses = await Promise.all(
-      iceProviders.map(({ url }) => fetch(url, { method: 'GET' }).then((response) => response.json())),
+      iceProviders.map(({ url }) =>
+        fetch(url, { method: 'GET' })
+          .then((response) => response.json())
+          .catch((err) => log.error('Failed to fetch ICE servers from provider', { url, err })),
+      ),
     );
-    for (const { iceServers } of responses) {
+    for (const { iceServers } of responses.filter(isNotNullOrUndefined)) {
       providedIceServers.push(...iceServers);
     }
   }
