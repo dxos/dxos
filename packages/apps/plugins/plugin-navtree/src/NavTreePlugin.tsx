21563998acf45f06e2d43cbb15d009ddfb2fa596
thure <will@braneframe.com>
2024-08-13 16:22:45 -0700

    Consolidate state without serializing graph.root.

--- packages/apps/plugins/plugin-navtree/src/NavTreePlugin.tsx
+++ packages/apps/plugins/plugin-navtree/src/NavTreePlugin.tsx
@@ -19,7 +19,6 @@ import {
   parseGraphPlugin,
 } from '@dxos/app-framework';
 import { createExtension, type Graph, isAction, isGraphNode, type Node } from '@dxos/app-graph';
-import { create } from '@dxos/echo-schema';
 import { Keyboard } from '@dxos/keyboard';
 import { LocalStorageStore } from '@dxos/local-storage';
 import { type LayoutCoordinate } from '@dxos/react-ui-deck';
@@ -44,14 +43,11 @@ export type NavTreePluginProvides = SurfaceProvides &
   GraphBuilderProvides &
   TranslationsProvides;
 
-type NavTreeState = { root?: NavTreeItemGraphNode };
-
-type NavTreeSettings = { openItemIds?: OpenItemIds };
+type NavTreeState = { root?: NavTreeItemGraphNode; openItemIds: OpenItemIds };
 
 export const NavTreePlugin = (): PluginDefinition<NavTreePluginProvides> => {
-  const state = create<NavTreeState>({});
-
-  const settings = new LocalStorageStore<NavTreeSettings>('dxos.org/settings/navtree', {
+  const state = new LocalStorageStore<NavTreeState>('dxos.org/settings/navtree', {
+    // TODO(thure): Do this dynamically.
     openItemIds: { root: true, 'dxos.org/plugin/space-spaces': true, 'dxos.org/plugin/files': true },
   });
 
@@ -60,7 +56,7 @@ export const NavTreePlugin = (): PluginDefinition<NavTreePluginProvides> => {
 
   const handleOpenItemIdsChange = (nextOpenItemIds: OpenItemIds) => {
     // TODO(thure): This might become a localstorage leak; openItemIds that no longer exist should be removed from this map.
-    settings.values.openItemIds = nextOpenItemIds;
+    state.values.openItemIds = nextOpenItemIds;
     if (graph) {
       void expandOpenGraphNodes(graph, nextOpenItemIds);
     }
@@ -75,14 +71,13 @@ export const NavTreePlugin = (): PluginDefinition<NavTreePluginProvides> => {
         return;
       }
 
-      state.root = graph.root as NavTreeItemGraphNode;
-      getChildren(graph, state.root);
-      getActions(graph, state.root);
+      state.values.root = graph.root as NavTreeItemGraphNode;
+      getChildren(graph, state.values.root);
+      getActions(graph, state.values.root);
 
-      // TODO(thure): Do this dynamically.
-      settings.prop({ key: 'openItemIds', storageKey: 'openItemIds', type: LocalStorageStore.json() });
+      state.prop({ key: 'openItemIds', storageKey: 'openItemIds', type: LocalStorageStore.json<OpenItemIds>() });
 
-      void expandOpenGraphNodes(graph, settings.values.openItemIds);
+      void expandOpenGraphNodes(graph, state.values.openItemIds);
 
       // TODO(wittjosiah): Factor out.
       // TODO(wittjosiah): Handle removal of actions.
@@ -151,12 +146,12 @@ export const NavTreePlugin = (): PluginDefinition<NavTreePluginProvides> => {
 
           switch (role) {
             case 'navigation':
-              if (state.root && settings.values.openItemIds) {
+              if (state.values.root && state.values.openItemIds) {
                 return (
                   <NavTreeContainer
-                    root={state.root}
+                    root={state.values.root}
                     activeIds={data.activeIds as Set<string>}
-                    openItemIds={settings.values.openItemIds}
+                    openItemIds={state.values.openItemIds}
                     onOpenItemIdsChange={handleOpenItemIdsChange}
                     attended={data.attended as Set<string>}
                     popoverAnchorId={data.popoverAnchorId as string}
@@ -171,7 +166,7 @@ export const NavTreePlugin = (): PluginDefinition<NavTreePluginProvides> => {
             }
 
             case 'navbar-start': {
-              if (state.root && data.activeNode) {
+              if (state.values.root && data.activeNode) {
                 return {
                   node: (
                     <NavBarStart
