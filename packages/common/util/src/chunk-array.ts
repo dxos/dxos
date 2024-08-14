425921ec35c7fcaf26d01fd4be1f342aba847d59
Dmytro Maretskyi <maretskii@gmail.com>
2024-08-14 11:50:07 +0200

    feat: Add unlinkObjects method

---
+++ packages/common/util/src/chunk-array.ts
@@ -0,0 +1,7 @@
+export const chunkArray = <T>(array: T[], size: number): T[][] => {
+  const result = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+};
