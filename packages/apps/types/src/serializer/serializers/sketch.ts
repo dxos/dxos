f309928ca2f1ece1d1e7864bc7d256e6324a1ba4
Josiah Witt <josiah@witt.life>
2024-08-15 06:57:33 -0400

    feat: single shot export (#7383)

--- packages/apps/types/src/serializer/serializers/sketch.ts
+++ packages/apps/types/src/serializer/serializers/sketch.ts
@@ -4,45 +4,31 @@
 
 import { create, createEchoObject, getObjectCore, loadObjectReferences } from '@dxos/client/echo';
 
-import { type TypedObjectSerializer, validFilename } from './default';
+import { type TypedObjectSerializer } from './types';
 import { CanvasType, DiagramType } from '../../schema';
 
 export const serializer: TypedObjectSerializer<DiagramType> = {
-  filename: (object) => ({
-    name: validFilename(object.name),
-    extension: 'json',
-  }),
-
   serialize: async ({ object }): Promise<string> => {
-    // TODO(wittjosiah): Implement `toJSON` method?
     const data = await loadObjectReferences(object, (s) => s.canvas);
     const sketch = { ...object, data: { ...data } };
     return JSON.stringify(sketch, null, 2);
   },
 
-  deserialize: async ({ content, object: existingSketch, newId }) => {
+  deserialize: async ({ content, newId }) => {
     const parsed = JSON.parse(content);
+    const canvas = create(CanvasType, { content: {} });
+    const diagram = createEchoObject(create(DiagramType, { name: parsed.title, canvas }));
 
-    if (existingSketch instanceof DiagramType) {
-      existingSketch.name = parsed.title;
-      const data = await loadObjectReferences(existingSketch, (s) => s.canvas);
-      setCanvasContent(data, parsed.data.content);
-      return existingSketch;
-    } else {
-      const canvas = create(CanvasType, { content: {} });
-      const diagram = createEchoObject(create(DiagramType, { name: parsed.title, canvas }));
+    if (!newId) {
+      const core = getObjectCore(diagram);
+      core.id = parsed.id;
 
-      if (!newId) {
-        const core = getObjectCore(diagram);
-        core.id = parsed.id;
-
-        const canvasCore = getObjectCore(canvas);
-        canvasCore.id = parsed.data.id;
-      }
-
-      setCanvasContent(canvas, parsed.data.content);
-      return diagram;
+      const canvasCore = getObjectCore(canvas);
+      canvasCore.id = parsed.data.id;
     }
+
+    setCanvasContent(canvas, parsed.data.content);
+    return diagram;
   },
 };
 
