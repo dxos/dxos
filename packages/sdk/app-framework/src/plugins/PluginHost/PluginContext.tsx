a3b649acfd526800084aa16f91430920d1682cf0
Josiah Witt <josiah@witt.life>
2024-08-15 07:06:17 -0400

    Update packages/sdk/app-framework/src/plugins/PluginHost/PluginContext.tsx
    Co-authored-by: Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>

--- packages/sdk/app-framework/src/plugins/PluginHost/PluginContext.tsx
+++ packages/sdk/app-framework/src/plugins/PluginHost/PluginContext.tsx
@@ -26,7 +26,7 @@ export type PluginContext = {
   /**
    * All available plugins.
    */
-  // This is metadata rather just ids because it includes plugins which have not been fully loaded yet.
+  // This is metadata rather then just ids because it includes plugins which have not been fully loaded yet.
   available: Plugin['meta'][];
 
   /**
