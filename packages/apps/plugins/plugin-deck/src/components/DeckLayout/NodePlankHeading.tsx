9e3e49523acbacae4f63282fc1cd615e3b61cacc
Josiah Witt <josiah@witt.life>
2024-08-20 12:18:32 -0400

    wip: break up deck layout

---
+++ packages/apps/plugins/plugin-deck/src/components/DeckLayout/NodePlankHeading.tsx
@@ -0,0 +1,170 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import { Placeholder } from '@phosphor-icons/react';
+import React, { Fragment, useEffect } from 'react';
+
+import { type Node, useGraph } from '@braneframe/plugin-graph';
+import {
+  LayoutAction,
+  NavigationAction,
+  SLUG_COLLECTION_INDICATOR,
+  SLUG_PATH_SEPARATOR,
+  Surface,
+  useIntentDispatcher,
+  indexInPart,
+  partLength,
+  type LayoutParts,
+  type LayoutPart,
+  type LayoutEntry,
+} from '@dxos/app-framework';
+import { Popover, toLocalizedString, useMediaQuery, useTranslation } from '@dxos/react-ui';
+import { PlankHeading, plankHeadingIconProps } from '@dxos/react-ui-deck';
+import { TextTooltip } from '@dxos/react-ui-text-tooltip';
+
+import { DECK_PLUGIN } from '../../meta';
+
+export const NodePlankHeading = ({
+  node,
+  id,
+  layoutParts,
+  layoutPart,
+  // TODO(wittjosiah): Unused?
+  layoutEntry,
+  popoverAnchorId,
+  pending,
+  flatDeck,
+}: {
+  node?: Node;
+  id?: string;
+  layoutParts?: LayoutParts;
+  layoutPart?: LayoutPart;
+  layoutEntry?: LayoutEntry;
+  popoverAnchorId?: string;
+  pending?: boolean;
+  flatDeck?: boolean;
+}) => {
+  const { t } = useTranslation(DECK_PLUGIN);
+  const { graph } = useGraph();
+  const Icon = node?.properties?.icon ?? Placeholder;
+  const label = pending
+    ? t('pending heading')
+    : toLocalizedString(node?.properties?.label ?? ['plank heading fallback label', { ns: DECK_PLUGIN }], t);
+  const dispatch = useIntentDispatcher();
+  const ActionRoot = node && popoverAnchorId === `dxos.org/ui/${DECK_PLUGIN}/${node.id}` ? Popover.Anchor : Fragment;
+  const [isNotMobile] = useMediaQuery('md');
+
+  useEffect(() => {
+    const frame = requestAnimationFrame(() => {
+      // Load actions for the node.
+      node && graph.actions(node);
+    });
+
+    return () => cancelAnimationFrame(frame);
+  }, [node]);
+
+  // NOTE(Zan): Node ids may now contain a path like `${space}:${id}~comments`
+  const attendableId = id?.split(SLUG_PATH_SEPARATOR).at(0);
+
+  const layoutCoordinate = layoutPart !== undefined && id !== undefined ? { part: layoutPart, entryId: id } : undefined;
+  const index = indexInPart(layoutParts, layoutCoordinate);
+  const length = partLength(layoutParts, layoutPart);
+
+  const canIncrementStart =
+    layoutPart === 'main' && index !== undefined && index > 0 && length !== undefined && length > 1;
+  const canIncrementEnd = layoutPart === 'main' && index !== undefined && index < length - 1 && length !== undefined;
+
+  return (
+    <PlankHeading.Root {...((layoutPart !== 'main' || !flatDeck) && { classNames: 'pie-1' })}>
+      <ActionRoot>
+        {node ? (
+          <PlankHeading.ActionsMenu
+            Icon={Icon}
+            attendableId={attendableId}
+            triggerLabel={t('actions menu label')}
+            actions={graph.actions(node)}
+            onAction={(action) =>
+              typeof action.data === 'function' && action.data?.({ node: action as Node, caller: DECK_PLUGIN })
+            }
+          >
+            <Surface role='menu-footer' data={{ object: node.data }} />
+          </PlankHeading.ActionsMenu>
+        ) : (
+          <PlankHeading.Button>
+            <span className='sr-only'>{label}</span>
+            <Icon {...plankHeadingIconProps} />
+          </PlankHeading.Button>
+        )}
+      </ActionRoot>
+      <TextTooltip text={label} onlyWhenTruncating>
+        <PlankHeading.Label attendableId={node?.id} {...(pending && { classNames: 'fg-description' })}>
+          {label}
+        </PlankHeading.Label>
+      </TextTooltip>
+      {node && layoutPart !== 'complementary' && (
+        // TODO(Zan): What are we doing with layout coordinate here?
+        <Surface role='navbar-end' direction='inline-reverse' data={{ object: node.data }} />
+      )}
+      {/* NOTE(thure): Pinning & unpinning are temporarily disabled */}
+      <PlankHeading.Controls
+        capabilities={{
+          solo: (layoutPart === 'solo' || layoutPart === 'main') && isNotMobile,
+          incrementStart: canIncrementStart,
+          incrementEnd: canIncrementEnd,
+        }}
+        isSolo={layoutPart === 'solo'}
+        onClick={(eventType) => {
+          if (!layoutPart) {
+            return;
+          }
+
+          if (eventType === 'solo') {
+            return dispatch(
+              [
+                {
+                  action: NavigationAction.ADJUST,
+                  data: { type: eventType, layoutCoordinate: { part: 'main', entryId: id } },
+                },
+              ],
+
+              //   // Scroll into view if unsoloing.
+              //   // TODO(Zan): Dispatch this from the layout intent handler.
+              //   layoutPart === 'solo'
+              //     ? {
+              //         action: LayoutAction.SCROLL_INTO_VIEW,
+              //         data: { id: node?.id },
+              //       }
+              //     : undefined,
+              // ].filter((x) => x !== undefined) as Intent[],
+            );
+          }
+
+          // TODO(Zan): Update this to use the new layout actions.
+          return dispatch(
+            eventType === 'close'
+              ? layoutPart === 'complementary'
+                ? {
+                    action: LayoutAction.SET_LAYOUT,
+                    data: {
+                      element: 'complementary',
+                      state: false,
+                    },
+                  }
+                : {
+                    action: NavigationAction.CLOSE,
+                    data: {
+                      activeParts: {
+                        complementary: [`${id}${SLUG_PATH_SEPARATOR}comments${SLUG_COLLECTION_INDICATOR}`],
+                        [layoutPart]: [id],
+                      },
+                    },
+                  }
+              : { action: NavigationAction.ADJUST, data: { type: eventType, layoutCoordinate } },
+          );
+        }}
+        close={layoutCoordinate?.part === 'complementary' ? 'minify-end' : true}
+      />
+    </PlankHeading.Root>
+  );
+};
---
+++ packages/apps/plugins/plugin-deck/src/components/DeckLayout/Plank.tsx
@@ -0,0 +1,144 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import { Plus } from '@phosphor-icons/react';
+import React from 'react';
+
+import { useGraph } from '@braneframe/plugin-graph';
+import {
+  LayoutAction,
+  type LayoutCoordinate,
+  type LayoutEntry,
+  type LayoutPart,
+  type LayoutParts,
+  NavigationAction,
+  Surface,
+  useIntentDispatcher,
+} from '@dxos/app-framework';
+import { Button, Tooltip, useTranslation, type ClassNameValue } from '@dxos/react-ui';
+import { createAttendableAttributes } from '@dxos/react-ui-attention';
+import { Plank as NaturalPlank } from '@dxos/react-ui-deck';
+
+import { NodePlankHeading } from './NodePlankHeading';
+import { PlankContentError, PlankError } from './PlankError';
+import { PlankLoading } from './PlankLoading';
+import { NAV_ID } from './constants';
+import { useNode } from '../../hooks';
+import { DECK_PLUGIN } from '../../meta';
+import { useLayout } from '../LayoutContext';
+
+export type PlankProps = {
+  entry: LayoutEntry;
+  layoutParts: LayoutParts;
+  // TODO(wittjosiah): Remove.
+  part: LayoutPart;
+  boundary?: 'start' | 'end';
+  resizeable?: boolean;
+  flatDeck?: boolean;
+  searchEnabled?: boolean;
+  classNames?: ClassNameValue;
+};
+
+export const Plank = ({
+  entry,
+  layoutParts,
+  part,
+  boundary,
+  resizeable,
+  flatDeck,
+  searchEnabled,
+  classNames,
+}: PlankProps) => {
+  const { t } = useTranslation(DECK_PLUGIN);
+  const dispatch = useIntentDispatcher();
+  const { popoverAnchorId, scrollIntoView } = useLayout();
+  const { graph } = useGraph();
+  const node = useNode(graph, entry.id);
+
+  const attendableAttrs = createAttendableAttributes(entry.id);
+
+  // TODO(Zan): Switch this to the new layout coordinates once consumers can speak that.
+  const coordinate: LayoutCoordinate = {
+    part,
+    entryId: entry.id,
+  };
+
+  return (
+    <NaturalPlank.Root boundary={boundary}>
+      <NaturalPlank.Content
+        {...attendableAttrs}
+        classNames={[!flatDeck && 'surface-base', classNames]}
+        scrollIntoViewOnMount={entry.id === scrollIntoView}
+        suppressAutofocus={entry.id === NAV_ID || !!node?.properties?.managesAutofocus}
+      >
+        {node ? (
+          <>
+            <NodePlankHeading
+              layoutPart='solo'
+              layoutParts={layoutParts}
+              node={node}
+              id={entry.id}
+              popoverAnchorId={popoverAnchorId}
+              flatDeck={flatDeck}
+            />
+            <Surface
+              role='article'
+              data={{
+                ...(entry.path ? { subject: node.data, path: entry.path } : { object: node.data }),
+                coordinate,
+                popoverAnchorId,
+              }}
+              limit={1}
+              fallback={PlankContentError}
+              placeholder={<PlankLoading />}
+            />
+          </>
+        ) : (
+          <PlankError layoutCoordinate={coordinate} id={entry.id} flatDeck={flatDeck} />
+        )}
+      </NaturalPlank.Content>
+      {searchEnabled && resizeable ? (
+        <div role='none' className='grid grid-rows-subgrid row-span-3'>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <Button
+                data-testid='plankHeading.open'
+                variant='ghost'
+                classNames='p-1 w-fit'
+                onClick={() =>
+                  dispatch([
+                    {
+                      action: LayoutAction.SET_LAYOUT,
+                      data: {
+                        element: 'dialog',
+                        component: 'dxos.org/plugin/search/Dialog',
+                        dialogBlockAlign: 'start',
+                        subject: {
+                          action: NavigationAction.SET,
+                          position: 'add-after',
+                          coordinate,
+                        },
+                      },
+                    },
+                  ])
+                }
+              >
+                <span className='sr-only'>{t('insert plank label')}</span>
+                <Plus />
+              </Button>
+            </Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Content side='bottom' classNames='z-[70]'>
+                {t('insert plank label')}
+              </Tooltip.Content>
+            </Tooltip.Portal>
+          </Tooltip.Root>
+          <NaturalPlank.ResizeHandle classNames='row-start-[toolbar-start] row-end-[content-end]' />
+        </div>
+      ) : resizeable ? (
+        <NaturalPlank.ResizeHandle classNames='row-span-3' />
+      ) : null}
+    </NaturalPlank.Root>
+  );
+};
