a413e4e5c129931dee97dbff915a7cd5dd4d1036
thure <will@braneframe.com>
2024-08-21 16:21:23 -0700

    Stub Tabs structure.

--- packages/ui/react-ui-tabs/src/Tabs.tsx
+++ packages/ui/react-ui-tabs/src/Tabs.tsx
@@ -2,8 +2,30 @@
 // Copyright 2024 DXOS.org
 //
 
-const TabsRoot = () => {
-  return null;
+// import '@types/react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+
+type TabsRootProps = TabsPrimitive.TabsProps;
+
+const TabsRoot = TabsPrimitive.Root;
+
+type TabsTablistProps = TabsPrimitive.TabsListProps;
+
+const TabsTablist = TabsPrimitive.List;
+
+type TabsTabProps = TabsPrimitive.TabsTriggerProps;
+
+const TabsTab = TabsPrimitive.Trigger;
+
+type TabsPanelProps = TabsPrimitive.TabsContentProps;
+
+const TabsPanel = TabsPrimitive.Content;
+
+export const Tabs = {
+  Root: TabsRoot,
+  Tablist: TabsTablist,
+  Tab: TabsTab,
+  Panel: TabsPanel,
 };
 
-export { TabsRoot };
+export type { TabsRootProps, TabsTablistProps, TabsTabProps, TabsPanelProps };
