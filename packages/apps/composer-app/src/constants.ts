3e3b5424606a3f2e64cc229c6f2216f5a1e2aff7
Josiah Witt <josiah@witt.life>
2024-08-22 17:59:20 -0400

    chore: ensure splash screen is shown on first load (#7530)

--- packages/apps/composer-app/src/constants.ts
+++ packages/apps/composer-app/src/constants.ts
@@ -8,15 +8,18 @@ export const INITIAL_TITLE = 'README';
 
 // TODO(wittjosiah): Translate.
 export const INITIAL_CONTENT = [
-  `# Welcome to Composer by DXOS
-![img](https://dxos.org/images/logo/dxos-logotype-white.svg)
+  `![img](https://dxos.network/dxos-logotype-blue.png)
+# Welcome to Composer by DXOS
+
+### What is Composer?
+Composer is an extensible application that includes familiar components such as documents, diagrams, and tables. It leverages DXOS — a full stack framework for building collaborative local-first applications. 
+
+With our upcoming SDK, you'll be able to build custom plugins, leverage external APIs and integrate with LLMs. All inside of a private collaborative workspace. 
+
 ### Disclaimer
 Composer is currently in Beta. We are offering an early preview of its collaborative and programmable features, and we would greatly appreciate your feedback to help improve it before the official launch.
 
-Please join our [Discord](https://dxos.org/discord) to share feature requests, bug reports, and general questions about your experience.
-
-### What is Composer?
-Composer is **a new programmable document format**. It blends the power of various popular data structures (e.g. docs, sketches & tables) and allows you to compose them natively inside of a collaborative workspace.`,
+Please join our [Discord](https://dxos.org/discord) to share feature requests, bug reports, and general questions about your experience.`,
   `### Working in Composer
 There are many different "objects" you can work with inside of Composer. The most common are:
 
