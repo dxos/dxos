4ea040f0e5b1aeaddb85eb1a3b8c6b557262fd1d
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-08-26 13:45:03 -0700

    Fast data entry

--- packages/ui/react-ui-table/src/components/Cell/Cell.tsx
+++ packages/ui/react-ui-table/src/components/Cell/Cell.tsx
@@ -4,7 +4,7 @@
 
 import { useFocusableGroup } from '@fluentui/react-tabster';
 import { type Cell as CellType, type RowData } from '@tanstack/react-table';
-import React, { type PropsWithChildren } from 'react';
+import React, { useCallback, type PropsWithChildren } from 'react';
 
 import { tdRoot } from '../../theme';
 import { useTableContext } from '../Table/TableContext';
@@ -18,11 +18,45 @@ const CELL_NAME = 'Cell';
 const FocusableCell = <TData extends RowData, TValue>({ cell, children }: CellProps<TData, TValue>) => {
   const tableContext = useTableContext();
   const domAttributes = useFocusableGroup({ tabBehavior: 'limited' });
-
   const className = tdRoot(tableContext, cell.column.columnDef.meta?.cell?.classNames);
 
+  const handleKeyDown = useCallback((event: React.KeyboardEvent<HTMLTableCellElement>) => {
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    const currentCell = event.currentTarget;
+    const currentRow = currentCell.closest('tr');
+    if (!currentRow) {
+      return;
+    }
+
+    const cellIndex = Array.from(currentRow.cells || []).indexOf(currentCell);
+
+    const focusNextCell = (nextRow: HTMLTableRowElement) => {
+      const nextCell = nextRow.cells[cellIndex];
+      nextCell.focus();
+    };
+
+    const nextSibling = currentRow.nextElementSibling;
+    if (nextSibling instanceof HTMLTableRowElement) {
+      focusNextCell(nextSibling);
+    } else {
+      // Poll for the new 'add row' row to be added to the DOM.
+      const pollForNewRow = () => {
+        const newNextSibling = currentRow.nextElementSibling;
+        if (newNextSibling instanceof HTMLTableRowElement) {
+          focusNextCell(newNextSibling);
+        } else {
+          requestAnimationFrame(pollForNewRow);
+        }
+      };
+      requestAnimationFrame(pollForNewRow);
+    }
+  }, []);
+
   return (
-    <td tabIndex={0} {...domAttributes} className={className}>
+    <td tabIndex={0} {...domAttributes} className={className} onKeyDown={handleKeyDown}>
       {children}
     </td>
   );
