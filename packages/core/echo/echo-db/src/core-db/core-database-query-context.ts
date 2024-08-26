71c49f6bb1f28922d7c70255eefd8a0fb196bf63
Dmytro Maretskyi <maretskii@gmail.com>
2024-08-26 12:53:12 +0200

    TODO

--- packages/core/echo/echo-db/src/core-db/core-database-query-context.ts
+++ packages/core/echo/echo-db/src/core-db/core-database-query-context.ts
@@ -1,36 +1,50 @@
 import { Event } from '@dxos/async';
 import type { QueryContext, QueryResult, QuerySource } from '../query/query';
 import type { Filter } from '../query';
+import type { CoreDatabase } from './core-database';
 
 export class CoreDatabaseQueryContext implements QueryContext {
+  private readonly _coreDatabaseQuerySource: CoreDatabaseQuerySource;
+
   added = new Event<QuerySource>();
   removed = new Event<QuerySource>();
 
+  constructor(coreDatabase: CoreDatabase) {
+    this._coreDatabaseQuerySource = new CoreDatabaseQuerySource(coreDatabase);
+  }
+
   get sources(): QuerySource[] {
-    return [];
+    return [this._coreDatabaseQuerySource];
   }
 
   start(): void {
-    throw new Error('Method not implemented.');
+    this.added.emit(this._coreDatabaseQuerySource);
   }
-  stop(): void {
-    throw new Error('Method not implemented.');
-  }
+
+  stop(): void {}
 }
 
 export class CoreDatabaseQuerySource implements QuerySource {
+  private _lastResult: QueryResult<any>[] = [];
+
   changed = new Event();
 
+  constructor(private readonly _coreDatabase: CoreDatabase) {}
+
   getResults(): QueryResult<any>[] {
-    throw new Error('Method not implemented.');
+    return this._lastResult;
   }
+
   async run(filter: Filter<any>): Promise<QueryResult<any>[]> {
+    
     throw new Error('Method not implemented.');
   }
+
   update(filter: Filter<any>): void {
-    throw new Error('Method not implemented.');
+    
   }
+
   close(): void {
-    throw new Error('Method not implemented.');
+    
   }
 }
