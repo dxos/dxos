a78f92c3bd951c6a2f5c3c1fff34e01fbc7c69b8
Rich Burdon <rich.burdon@gmail.com>
2024-08-27 18:30:41 -0400

    feat: Formatting for numbered lists (#7564)

---
+++ packages/ui/react-ui-editor/src/extensions/markdown/parser.test.ts
@@ -0,0 +1,47 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+// @ts-ignore
+import { testTree } from '@lezer/generator/test';
+import { parser } from '@lezer/markdown';
+
+import { describe, test } from '@dxos/test';
+
+describe('parser', () => {
+  // https://www.markdownguide.org/basic-syntax/#lists-1
+  test('lists', () => {
+    // Indented list must have 4 spaces.
+    const result = parser.parse(
+      [
+        '# H1',
+        '1. one',
+        '2. two',
+        '3. three',
+        '    1. four',
+        '',
+        // TODO(burdon): Test list termination without heading as break.
+        '# H2',
+        '1. one',
+      ].join('\n'),
+    );
+    testTree(
+      result,
+      [
+        'Document(',
+        'ATXHeading1(HeaderMark)',
+        'OrderedList(',
+        'ListItem(ListMark,Paragraph),',
+        'ListItem(ListMark,Paragraph),',
+        'ListItem(ListMark,Paragraph,OrderedList(ListItem(ListMark,Paragraph)))',
+        ')',
+        '',
+        'ATXHeading1(HeaderMark)',
+        'OrderedList(',
+        'ListItem(ListMark,Paragraph),',
+        ')',
+        ')',
+      ].join(''),
+    );
+  });
+});
