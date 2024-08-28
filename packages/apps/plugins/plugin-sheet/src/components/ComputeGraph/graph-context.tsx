c25ad70cef9222e5eede67ad60410d2eb30dc222
Josiah Witt <josiah@witt.life>
2024-08-28 14:46:11 -0400

    feat: poc edge function for sheet (#7579)

--- packages/apps/plugins/plugin-sheet/src/components/ComputeGraph/graph-context.tsx
+++ packages/apps/plugins/plugin-sheet/src/components/ComputeGraph/graph-context.tsx
@@ -2,21 +2,36 @@
 // Copyright 2024 DXOS.org
 //
 
-import React, { createContext, type PropsWithChildren, useContext } from 'react';
+import React, { createContext, type PropsWithChildren, useContext, useEffect } from 'react';
 
-import { raise } from '@dxos/debug';
+import { type Space } from '@dxos/react-client/echo';
 
-import { type ComputeGraph } from './graph';
+import { createComputeGraph, type ComputeGraph } from './graph';
 
-export type ComputeGraphContextType = { graph: ComputeGraph };
-
-const ComputeGraphContext = createContext<ComputeGraphContextType | null>(null);
-
-export const ComputeGraphContextProvider = ({ children, graph }: PropsWithChildren<{ graph: ComputeGraph }>) => {
-  return <ComputeGraphContext.Provider value={{ graph }}>{children}</ComputeGraphContext.Provider>;
+export type ComputeGraphContextType = {
+  graphs: Record<string, ComputeGraph>;
+  setGraph: (key: string, graph: ComputeGraph) => void;
 };
 
-export const useComputeGraph = (): ComputeGraph => {
-  const { graph } = useContext(ComputeGraphContext) ?? raise(new Error('Missing GraphContext'));
+export const ComputeGraphContext = createContext<ComputeGraphContextType>({ graphs: {}, setGraph: () => {} });
+
+export const ComputeGraphContextProvider = ({
+  children,
+  graphs,
+  setGraph,
+}: PropsWithChildren<ComputeGraphContextType>) => {
+  return <ComputeGraphContext.Provider value={{ graphs, setGraph }}>{children}</ComputeGraphContext.Provider>;
+};
+
+export const useComputeGraph = (space: Space): ComputeGraph => {
+  const { graphs, setGraph } = useContext(ComputeGraphContext);
+  const graph = graphs[space.id] ?? createComputeGraph(space);
+
+  useEffect(() => {
+    if (!graphs[space.id]) {
+      setGraph(space.id, graph);
+    }
+  }, [space]);
+
   return graph;
 };
