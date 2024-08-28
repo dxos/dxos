85f3c7f4e82c5a547947e9021460ead6840b5ddd
Will Shown <thure@users.noreply.github.com>
2024-08-28 15:31:49 -0700

    fix(plugin-navtree): Restore keyboard access to tree item tooltips and navigation (#7584)

--- packages/ui/react-ui-text-tooltip/src/components/TextTooltip.tsx
+++ packages/ui/react-ui-text-tooltip/src/components/TextTooltip.tsx
@@ -3,7 +3,15 @@
 //
 
 import { useComposedRefs } from '@radix-ui/react-compose-refs';
-import React, { type CSSProperties, forwardRef, Fragment, type PropsWithChildren, useRef, useState } from 'react';
+import React, {
+  type ComponentPropsWithoutRef,
+  type CSSProperties,
+  forwardRef,
+  Fragment,
+  type PropsWithChildren,
+  useRef,
+  useState,
+} from 'react';
 
 import { Tooltip, type TooltipContentProps } from '@dxos/react-ui';
 
@@ -14,12 +22,25 @@ export type TextTooltipProps = PropsWithChildren<
     portal?: boolean;
     zIndex?: CSSProperties['zIndex'];
     onlyWhenTruncating?: boolean;
-  } & Pick<TooltipContentProps, 'side' | 'sideOffset'>
+    truncateQuery?: string;
+  } & Pick<TooltipContentProps, 'side' | 'sideOffset'> &
+    ComponentPropsWithoutRef<'button'>
 >;
 
 export const TextTooltip = forwardRef<HTMLButtonElement, TextTooltipProps>(
   (
-    { text, children, onlyWhenTruncating, asChild = true, portal = true, zIndex = 70, sideOffset, side },
+    {
+      text,
+      children,
+      onlyWhenTruncating,
+      asChild = true,
+      portal = true,
+      zIndex = 70,
+      sideOffset,
+      side,
+      truncateQuery,
+      ...props
+    },
     forwardedRef,
   ) => {
     const ContentRoot = portal ? Tooltip.Portal : Fragment;
@@ -31,13 +52,16 @@ export const TextTooltip = forwardRef<HTMLButtonElement, TextTooltipProps>(
         open={open}
         onOpenChange={(nextOpen) => {
           if (onlyWhenTruncating && nextOpen && content.current) {
-            return setOpen(content.current.scrollWidth > content.current.offsetWidth);
+            const element: HTMLElement | null = truncateQuery
+              ? content.current.querySelector(truncateQuery)
+              : content.current;
+            return setOpen(element ? element.scrollWidth > element.offsetWidth : false);
           } else {
             return setOpen(nextOpen);
           }
         }}
       >
-        <Tooltip.Trigger asChild={asChild} ref={ref}>
+        <Tooltip.Trigger asChild={asChild} {...props} ref={ref}>
           {children}
         </Tooltip.Trigger>
         <ContentRoot>
