a0ae33e4bd245cc787b9e572158e789082e231e2
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-08-29 08:41:52 -0700

    feedback

--- packages/apps/composer-app/src/playwright/tables.spec.ts
+++ packages/apps/composer-app/src/playwright/tables.spec.ts
@@ -7,26 +7,9 @@ import { expect, type Page, test } from '@playwright/test';
 import { faker } from '@dxos/random';
 
 import { AppManager } from './app-manager';
+import { Table } from './plugins';
 
 faker.seed(0);
-
-const table = {
-  getNameInput: (page: Page) => page.getByTestId('table.settings.name'),
-  getContinueButton: (page: Page) => page.getByTestId('table.settings.continue'),
-  getHeaderCell: (page: Page) => page.getByTestId('table.header-cell'),
-  getDataRow: (page: Page) => page.getByTestId('table.data-row'),
-  createTable: async (page: Page, title: string) => {
-    await table.getNameInput(page).fill(title);
-    await table.getContinueButton(page).click();
-  },
-  getAddColumnButton: (page: Page) => page.getByTestId('table.new-column'),
-  getDeleteRowButton: (page: Page) => page.getByTestId('table.delete-row'),
-  getColumnMenu: (page: Page) => page.getByTestId('table.column-menu'),
-  getOpenColumnSettings: (page: Page) => page.getByTestId('table.open-column-settings'),
-  getColumnSettingsLabel: (page: Page) => page.getByTestId('table.column-settings.label'),
-  getColumnSettingsDelete: (page: Page) => page.getByTestId('table.column-settings.delete'),
-  getColumnSettingsSave: (page: Page) => page.getByTestId('table.column-settings.save'),
-} as const;
 
 test.describe('Table tests', () => {
   let host: AppManager;
@@ -34,6 +17,9 @@ test.describe('Table tests', () => {
   test.beforeEach(async ({ browser }) => {
     host = new AppManager(browser, true);
     await host.init();
+    await host.createSpace();
+    await host.openSettings();
+    await host.enablePlugin('dxos.org/plugin/table');
   });
 
   test.afterEach(async () => {
@@ -41,133 +27,115 @@ test.describe('Table tests', () => {
   });
 
   test('create', async () => {
-    await host.createSpace();
-    await host.openSettings();
-    await host.enablePlugin('dxos.org/plugin/table');
     await host.createObject('tablePlugin');
 
     const title = faker.lorem.sentence();
-    await table.createTable(host.page, title);
+    await Table.createTable(host.page, title);
 
-    await expect(table.getDataRow(host.page)).toHaveCount(1);
+    await expect(Table.getDataRow(host.page)).toHaveCount(1);
   });
 
   test('can add rows', async () => {
-    await host.createSpace();
-    await host.openSettings();
-    await host.enablePlugin('dxos.org/plugin/table');
     await host.createObject('tablePlugin');
 
     const title = faker.lorem.sentence();
-    await table.createTable(host.page, title);
+    await Table.createTable(host.page, title);
 
-    await expect(table.getDataRow(host.page)).toHaveCount(1);
+    await expect(Table.getDataRow(host.page)).toHaveCount(1);
 
-    const cell = table.getDataRow(host.page).last().getByTestId('table.cell').first();
+    const cell = Table.getDataRow(host.page).last().getByTestId('table.cell').first();
     await cell.click();
     await host.page.keyboard.type('test');
     await host.page.keyboard.press('Enter');
 
-    await expect(table.getDataRow(host.page)).toHaveCount(2);
+    await expect(Table.getDataRow(host.page)).toHaveCount(2);
 
-    const cell2 = table.getDataRow(host.page).last().getByTestId('table.cell').first();
+    const cell2 = Table.getDataRow(host.page).last().getByTestId('table.cell').first();
     await cell2.click();
     await host.page.keyboard.type('test2');
     await host.page.keyboard.press('Enter');
 
-    await expect(table.getDataRow(host.page)).toHaveCount(3);
+    await expect(Table.getDataRow(host.page)).toHaveCount(3);
   });
 
   test('can delete rows', async () => {
-    await host.createSpace();
-    await host.openSettings();
-    await host.enablePlugin('dxos.org/plugin/table');
     await host.createObject('tablePlugin');
     const title = faker.lorem.sentence();
-    await table.createTable(host.page, title);
-    await expect(table.getDataRow(host.page)).toHaveCount(1);
+    await Table.createTable(host.page, title);
+    await expect(Table.getDataRow(host.page)).toHaveCount(1);
 
     const ROWS = 5;
     for (let i = 0; i < ROWS; i++) {
-      const cell = table.getDataRow(host.page).last().getByTestId('table.cell').first();
+      const cell = Table.getDataRow(host.page).last().getByTestId('table.cell').first();
       await cell.click();
       await host.page.keyboard.type('test');
       await host.page.keyboard.press('Enter');
       // sleep for a while to make sure the row is added
       await host.page.waitForTimeout(100);
-      await expect(table.getDataRow(host.page)).toHaveCount(i + 2);
+      await expect(Table.getDataRow(host.page)).toHaveCount(i + 2);
     }
 
     for (let i = 0; i < ROWS; i++) {
-      await table.getDeleteRowButton(host.page).first().click();
-      await expect(table.getDataRow(host.page)).toHaveCount(ROWS - i);
+      await Table.getDeleteRowButton(host.page).first().click();
+      await expect(Table.getDataRow(host.page)).toHaveCount(ROWS - i);
     }
 
-    await expect(table.getDataRow(host.page)).toHaveCount(1);
+    await expect(Table.getDataRow(host.page)).toHaveCount(1);
   });
 
   test('can add columns', async () => {
-    await host.createSpace();
-    await host.openSettings();
-    await host.enablePlugin('dxos.org/plugin/table');
     await host.createObject('tablePlugin');
     const title = faker.lorem.sentence();
-    await table.createTable(host.page, title);
-    await expect(table.getHeaderCell(host.page)).toHaveCount(2);
+    await Table.createTable(host.page, title);
+    await expect(Table.getHeaderCell(host.page)).toHaveCount(2);
 
-    await table.getAddColumnButton(host.page).click();
+    await Table.getAddColumnButton(host.page).click();
 
-    await expect(table.getHeaderCell(host.page)).toHaveCount(3);
+    await expect(Table.getHeaderCell(host.page)).toHaveCount(3);
   });
 
   test('can delete columns', async () => {
-    await host.createSpace();
-    await host.openSettings();
-    await host.enablePlugin('dxos.org/plugin/table');
     await host.createObject('tablePlugin');
     const title = faker.lorem.sentence();
-    await table.createTable(host.page, title);
+    await Table.createTable(host.page, title);
 
     const COLUMNS = 4;
-    await expect(table.getHeaderCell(host.page)).toHaveCount(2);
+    await expect(Table.getHeaderCell(host.page)).toHaveCount(2);
 
     for (let i = 0; i < COLUMNS; i++) {
-      await table.getAddColumnButton(host.page).click();
-      await expect(table.getHeaderCell(host.page)).toHaveCount(3 + i);
+      await Table.getAddColumnButton(host.page).click();
+      await expect(Table.getHeaderCell(host.page)).toHaveCount(3 + i);
     }
 
-    await expect(table.getHeaderCell(host.page)).toHaveCount(2 + COLUMNS);
+    await expect(Table.getHeaderCell(host.page)).toHaveCount(2 + COLUMNS);
 
     for (let i = 0; i < COLUMNS; i++) {
-      await table.getColumnMenu(host.page).last().click();
-      await table.getOpenColumnSettings(host.page).click();
-      await table.getColumnSettingsDelete(host.page).click();
-      await expect(table.getHeaderCell(host.page)).toHaveCount(2 + COLUMNS - i - 1);
+      await Table.getColumnMenu(host.page).last().click();
+      await Table.getOpenColumnSettings(host.page).click();
+      await Table.getColumnSettingsDelete(host.page).click();
+      await expect(Table.getHeaderCell(host.page)).toHaveCount(2 + COLUMNS - i - 1);
     }
 
-    await expect(table.getHeaderCell(host.page)).toHaveCount(2);
+    await expect(Table.getHeaderCell(host.page)).toHaveCount(2);
   });
 
   test('can rename columns', async () => {
-    await host.createSpace();
-    await host.openSettings();
-    await host.enablePlugin('dxos.org/plugin/table');
     await host.createObject('tablePlugin');
     const title = faker.lorem.sentence();
 
-    await table.createTable(host.page, title);
-    await expect(table.getHeaderCell(host.page)).toHaveCount(2);
+    await Table.createTable(host.page, title);
+    await expect(Table.getHeaderCell(host.page)).toHaveCount(2);
 
-    await table.getAddColumnButton(host.page).click();
-    await expect(table.getHeaderCell(host.page)).toHaveCount(3);
+    await Table.getAddColumnButton(host.page).click();
+    await expect(Table.getHeaderCell(host.page)).toHaveCount(3);
 
     const newColumnName = faker.lorem.word();
 
-    await table.getColumnMenu(host.page).last().click();
-    await table.getOpenColumnSettings(host.page).click();
-    await table.getColumnSettingsLabel(host.page).fill(newColumnName);
-    await table.getColumnSettingsSave(host.page).click();
+    await Table.getColumnMenu(host.page).last().click();
+    await Table.getOpenColumnSettings(host.page).click();
+    await Table.getColumnSettingsLabel(host.page).fill(newColumnName);
+    await Table.getColumnSettingsSave(host.page).click();
 
-    expect(await table.getHeaderCell(host.page).nth(1).textContent()).toBe(newColumnName);
+    await expect(Table.getHeaderCell(host.page).nth(1)).toHaveText(newColumnName);
   });
 });
