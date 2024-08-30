e18c5f5101f5ded8a3392b5a1cefd426b4f01cb9
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-08-30 10:53:28 -0700

    fix(deck): Overscroll centering (#7603)

--- packages/apps/plugins/plugin-deck/src/util/overscroll.ts
+++ packages/apps/plugins/plugin-deck/src/util/overscroll.ts
@@ -3,6 +3,7 @@
 //
 
 import { type LayoutMode, type LayoutParts } from '@dxos/app-framework';
+import { PLANK_DEFAULTS } from '@dxos/react-ui-deck';
 
 import { type Overscroll } from '../types';
 
@@ -70,11 +71,13 @@ export const calculateOverscroll = (
   const sidebarWidth = sidebarOpen ? '270px' : '0px';
   const complementarySidebarWidth = complementarySidebarOpen ? '360px' : '0px';
 
+  const getPlankSize = (id: string) => (plankSizing[id] ?? PLANK_DEFAULTS.size).toFixed(2) + 'rem';
+
   if (layoutParts.main.length === 1) {
     // Center the plank in the content area.
 
     const plank = layoutParts.main[0];
-    const plankSize = (plankSizing[plank.id] ?? 0).toFixed(2) + 'rem';
+    const plankSize = getPlankSize(plank.id);
     const overscrollPadding = `max(0px, calc(((100dvw - ${sidebarWidth} - ${complementarySidebarWidth} - (${plankSize} + 20px)) / 2)))`;
 
     return { paddingLeft: overscrollPadding, paddingRight: overscrollPadding };
@@ -82,11 +85,11 @@ export const calculateOverscroll = (
     // Center the plank on the screen.
 
     const firstPlank = layoutParts.main[0];
-    const firstPlankInlineSize = (plankSizing[firstPlank.id] ?? 44).toFixed(2) + 'rem';
+    const firstPlankInlineSize = getPlankSize(firstPlank.id);
     const paddingLeft = `max(0px, calc(((100dvw - (${firstPlankInlineSize} + 20px)) / 2) - ${sidebarWidth}))`;
 
     const lastPlank = layoutParts.main[layoutParts.main.length - 1];
-    const lastPlankInlineSize = (plankSizing[lastPlank.id] ?? 44).toFixed(2) + 'rem';
+    const lastPlankInlineSize = getPlankSize(lastPlank.id);
     const paddingRight = `max(0px, calc(((100dvw - (${lastPlankInlineSize} + 20px)) / 2) - ${complementarySidebarWidth}))`;
 
     return { paddingLeft, paddingRight };
