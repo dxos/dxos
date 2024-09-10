c9dce4be6b4ebebd087fa9272ec85b4e8842e848
yarolegovich <yarolegovich@gmail.com>
2024-09-10 17:14:46 +0300

    rtc transport

---
+++ packages/core/mesh/network-manager/src/webrtc/rtc-transport-factory.ts
@@ -0,0 +1,28 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import { getRtcConnectionFactory } from './rtc-connection-factory';
+import { RtcPeerConnection } from './rtc-peer-connection';
+import type { IceProvider } from '../signal';
+import type { TransportFactory } from '../transport';
+
+export const createRtcTransportFactory = (
+  webrtcConfig?: RTCConfiguration,
+  iceProvider?: IceProvider,
+): TransportFactory => {
+  const connectionFactory = getRtcConnectionFactory();
+  return {
+    createTransport: (options) => {
+      // TODO(yaroslav): sendSignal is scoped to a swarm, RtcConnections can be cached if it's scoped to a peer
+      const connection = new RtcPeerConnection(connectionFactory, {
+        ownPeerKey: options.ownPeerKey,
+        remotePeerKey: options.remotePeerKey,
+        sendSignal: options.sendSignal,
+        webrtcConfig,
+        iceProvider,
+      });
+      return connection.createTransportChannel(options);
+    },
+  };
+};
