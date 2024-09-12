e8c4141d28700f06c333db18f59e817f4fb3fe73
Josiah Witt <josiah@witt.life>
2024-09-12 07:34:35 -0400

    more bundler tests

--- packages/plugins/experimental/plugin-script/src/bundler/bundler.test.ts
+++ packages/plugins/experimental/plugin-script/src/bundler/bundler.test.ts
@@ -10,6 +10,35 @@ describe('Bundler', () => {
   test('Basic', async () => {
     const bundler = new Bundler({ platform: 'node', sandboxedModules: [], remoteModules: {} });
     const result = await bundler.bundle('const x = 100'); // TODO(burdon): Test import.
-    expect(result).to.exist;
+    expect(result.bundle).to.exist;
+    expect(result.error).to.not.exist;
+  });
+
+  test('Import', async () => {
+    const bundler = new Bundler({ platform: 'node', sandboxedModules: [], remoteModules: {} });
+    const result = await bundler.bundle(`
+      import { Filter } from './runtime.js';
+
+      const query = Filter.typename('dxos.org/type/Example');
+    `);
+    expect(result.bundle).to.exist;
+    expect(result.error).to.not.exist;
+  });
+
+  test('HTTPS Import', async () => {
+    const bundler = new Bundler({ platform: 'node', sandboxedModules: [], remoteModules: {} });
+    const result = await bundler.bundle(`
+      import { Octokit } from 'https://esm.sh/octokit';
+      const octokit = new Octokit();
+    `);
+    expect(result.bundle).to.exist;
+    expect(result.error).to.not.exist;
+  });
+
+  test('Error', async () => {
+    const bundler = new Bundler({ platform: 'node', sandboxedModules: [], remoteModules: {} });
+    const result = await bundler.bundle("import missing from './module'; missing();");
+    expect(result.bundle).to.not.exist;
+    expect(result.error).to.exist;
   });
 });
