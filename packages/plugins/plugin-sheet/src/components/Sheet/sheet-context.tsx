91927ee5f3bd92dfa0e8b2a447e58f10d0746180
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-09-19 16:46:41 -0700

    Add TODO

--- packages/plugins/plugin-sheet/src/components/Sheet/sheet-context.tsx
+++ packages/plugins/plugin-sheet/src/components/Sheet/sheet-context.tsx
@@ -108,6 +108,8 @@ export const SheetContextProvider = ({
 }: PropsWithChildren<SheetContextProps>) => {
   const graph = useComputeGraph(space, options);
 
+  // TODO(Zan): We should offer a version of set range and set cursor that scrolls to
+  // that cell or range if it is not visible.
   const [cursor, setCursor] = useState<CellAddress>();
   const [range, setRange] = useState<CellRange>();
   const [editing, setEditing] = useState<boolean>(false);
