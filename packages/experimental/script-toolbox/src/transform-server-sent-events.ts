acd3d530cbb2315628422fac32c5eadb950888d2
Dmytro Maretskyi <maretskii@gmail.com>
2024-09-19 18:03:51 +0200

    Lint

--- packages/experimental/script-toolbox/src/transform-server-sent-events.ts
+++ packages/experimental/script-toolbox/src/transform-server-sent-events.ts
@@ -1,10 +1,10 @@
-/**
- * Converts a binary stream in server-sent events format to a stream of it's text content.
- */
 //
 // Copyright 2024 DXOS.org
 //
 
+/**
+ * Converts a binary stream in server-sent events format to a stream of it's text content.
+ */
 export const transformServerSentEvents = (input: ReadableStream): ReadableStream => {
   const { readable, writable } = new TransformStream({
     transform: (chunk, controller) => {
