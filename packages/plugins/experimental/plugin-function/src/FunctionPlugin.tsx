75ba6f5db895a230d1a2c6c5bba6ef57812d6a87
Josiah Witt <josiah@witt.life>
2024-09-19 13:34:37 -0400

    triggers outside of stack

--- packages/plugins/experimental/plugin-function/src/FunctionPlugin.tsx
+++ packages/plugins/experimental/plugin-function/src/FunctionPlugin.tsx
@@ -5,9 +5,12 @@
 import { Function, type IconProps } from '@phosphor-icons/react';
 import React from 'react';
 
-import { type PluginDefinition } from '@dxos/app-framework';
+import { NavigationAction, parseIntentPlugin, resolvePlugin, type PluginDefinition } from '@dxos/app-framework';
 import { create } from '@dxos/echo-schema';
 import { FunctionDef, FunctionTrigger } from '@dxos/functions/types';
+import { parseClientPlugin } from '@dxos/plugin-client';
+import { createExtension, isActionGroup, type ActionGroup } from '@dxos/plugin-graph';
+import { SpaceAction } from '@dxos/plugin-space';
 
 import { TriggerArticle } from './components';
 import meta, { FUNCTION_PLUGIN } from './meta';
@@ -21,6 +24,7 @@ export const FunctionPlugin = (): PluginDefinition<FunctionPluginProvides> => {
       metadata: {
         records: {
           [FunctionTrigger.typename]: {
+            label: (object: any) => (object instanceof FunctionTrigger ? object.name : undefined),
             placeholder: ['object placeholder', { ns: FUNCTION_PLUGIN }],
             icon: (props: IconProps) => <Function {...props} />,
             iconSymbol: 'ph--function--regular',
@@ -30,6 +34,49 @@ export const FunctionPlugin = (): PluginDefinition<FunctionPluginProvides> => {
       translations,
       echo: {
         schema: [FunctionDef, FunctionTrigger],
+      },
+      graph: {
+        builder: (plugins) => {
+          const client = resolvePlugin(plugins, parseClientPlugin)?.provides.client;
+          const dispatch = resolvePlugin(plugins, parseIntentPlugin)?.provides.intent.dispatch;
+          if (!client || !dispatch) {
+            return [];
+          }
+
+          return createExtension({
+            id: FunctionAction.CREATE,
+            filter: (node): node is ActionGroup => isActionGroup(node) && node.id.startsWith(SpaceAction.ADD_OBJECT),
+            actions: ({ node }) => {
+              const id = node.id.split('/').at(-1);
+              const [spaceId, objectId] = id?.split(':') ?? [];
+              const space = client.spaces.get().find((space) => space.id === spaceId);
+              const object = objectId && space?.db.getObjectById(objectId);
+              const target = objectId ? object : space;
+              if (!target) {
+                return;
+              }
+
+              return [
+                {
+                  id: `${FUNCTION_PLUGIN}/create/${node.id}`,
+                  data: async () => {
+                    await dispatch([
+                      { plugin: FUNCTION_PLUGIN, action: FunctionAction.CREATE, data: { space } },
+                      { action: SpaceAction.ADD_OBJECT, data: { target } },
+                      { action: NavigationAction.OPEN },
+                    ]);
+                  },
+                  properties: {
+                    label: ['create object label', { ns: FUNCTION_PLUGIN }],
+                    icon: (props: IconProps) => <Function {...props} />,
+                    iconSymbol: 'ph--function--duotone',
+                    testId: 'functionPlugin.createObject',
+                  },
+                },
+              ];
+            },
+          });
+        },
       },
       stack: {
         creators: [
@@ -49,6 +96,7 @@ export const FunctionPlugin = (): PluginDefinition<FunctionPluginProvides> => {
       surface: {
         component: ({ data, role }) => {
           switch (role) {
+            case 'article':
             case 'section': {
               return data.object instanceof FunctionTrigger ? <TriggerArticle trigger={data.object} /> : null;
             }
