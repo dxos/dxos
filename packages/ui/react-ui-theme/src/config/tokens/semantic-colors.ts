ee86e4e638d80cca590fdb71e5b81e5492543896
thure <will@braneframe.com>
2024-09-19 14:19:01 -0500

    Fix build.

--- packages/ui/react-ui-theme/src/config/tokens/semantic-colors.ts
+++ packages/ui/react-ui-theme/src/config/tokens/semantic-colors.ts
@@ -6,6 +6,7 @@ import type { HelicalArcValue, SemanticLayer } from '@ch-ui/tokens';
 
 import { codeMirrorSememes } from './sememes-codemirror';
 import { peerSememes } from './sememes-peer';
+import { sheetSememes } from './sememes-sheet';
 import { systemSememes } from './sememes-system';
 
 export const semanticColors = {
@@ -15,6 +16,7 @@ export const semanticColors = {
   },
   sememes: {
     // Please define each set of sememes in its own file.
+    ...sheetSememes,
     ...codeMirrorSememes,
     ...peerSememes,
     ...systemSememes,
