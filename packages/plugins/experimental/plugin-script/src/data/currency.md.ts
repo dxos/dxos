6e76545db11f7b551f53aaa4586a927301f17e5e
Dmytro Maretskyi <maretskii@gmail.com>
2024-09-19 17:14:39 +0200

    Add currency scritp

---
+++ packages/plugins/experimental/plugin-script/src/data/currency.md.ts
@@ -0,0 +1,19 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+export default async ({
+  event: {
+    data: { request },
+  },
+  context: { space, ai },
+}) => {
+  const {
+    args: [from = 'EUR', to = 'GBP'],
+  } = await request.json();
+  const res = await fetch(`https://free.ratesdb.com/v1/rates?from=${from}&to=${to}`);
+  const {
+    data: { rates },
+  } = await res.json();
+  return new Response(rates[to].toString());
+};
