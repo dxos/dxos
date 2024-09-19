59e9e16369ba45ef265a504bc271ec0f0600f6aa
thure <will@braneframe.com>
2024-09-19 17:27:04 -0500

    Add status tokens to valence tokens.

--- packages/ui/react-ui-theme/src/config/tokens/sememes-valence.ts
+++ packages/ui/react-ui-theme/src/config/tokens/sememes-valence.ts
@@ -5,26 +5,36 @@
 import { type huePalettes } from './physical-colors';
 import { type ColorSememes } from './types';
 
-const valenceLuminosities = {
+const valenceValues = {
   Text: [550, 300],
   MessageText: [600, 100],
   MessageSurface: [50, 900],
+  StatusSurface: [500, 400],
+  StatusText: [400, 500],
+  StatusStroke: [300, 500],
   Shadow: ['500/.5', '500/.5'],
 };
 
 const valenceHues = {
   success: 'emerald',
   info: 'cyan',
+  active: 'primary',
   warning: 'amber',
   error: 'rose',
-} satisfies Record<string, keyof typeof huePalettes>;
+} satisfies Record<string, keyof typeof huePalettes | 'primary'>;
+
+const exceptions = {
+  inactiveStatusSurface: { light: ['neutral', 100], dark: ['neutral', 600] },
+  inactiveStatusText: { light: ['neutral', 100], dark: ['neutral', 600] },
+  inactiveStatusStroke: { light: ['neutral', 100], dark: ['neutral', 700] },
+} satisfies ColorSememes;
 
 export const valenceSememes: ColorSememes = Object.entries(valenceHues).reduce((acc: ColorSememes, [valence, hue]) => {
-  return Object.entries(valenceLuminosities).reduce((acc: ColorSememes, [aspect, [lightValue, darkValue]]) => {
+  return Object.entries(valenceValues).reduce((acc: ColorSememes, [aspect, [lightValue, darkValue]]) => {
     acc[`${valence}${aspect}`] = {
       light: [hue, lightValue],
       dark: [hue, darkValue],
     };
     return acc;
   }, acc);
-}, {});
+}, exceptions);
