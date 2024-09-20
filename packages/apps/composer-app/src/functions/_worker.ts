39b59300cef7f1117415e71cb8e5a73477274ece
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-09-20 16:54:14 +0300

    Feat: Add LOKI event firing on load

--- packages/apps/composer-app/src/functions/_worker.ts
+++ packages/apps/composer-app/src/functions/_worker.ts
@@ -8,9 +8,40 @@
  * Output _worker.js to <pages_build_output_dir> and deploy via git.
  */
 const handler: ExportedHandler<{ ASSETS: Fetcher }> = {
-  // NOTE: This is just a pass-through. Leaving the worker setup here for future use.
   fetch: async (request, env, _context) => {
-    return env.ASSETS.fetch(request);
+    const assetsPromise = env.ASSETS.fetch(request);
+    let telemetryPromise: Promise<Response> | undefined;
+
+    // Emit telemetry log to Grafana Loki cloud.
+    if (env.DX_LOKI_ENDPOINT && env.DX_LOKI_AUTHORIZATION && request.url && request.url.includes('site.webmanifest')) {
+      telemetryPromise = fetch(env.DX_LOKI_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: env.DX_LOKI_AUTHORIZATION
+        },
+        body: JSON.stringify({ 
+          streams: [
+            {
+              stream: {
+                "service_name": "composer",
+                "environment": env.ENVIRONMENT ?? undefined
+              },
+              values: [[String(Date.now() * 1e6), "Fetch app"]]
+            }
+          ] 
+        })
+      }).then(async (response) => {
+        if (!response.ok) {
+          console.log("LOKI telemetry fetch failed:", await response.text());
+        }
+      }).catch((error) => console.error("LOKI telemetry fetch error:", error));
+    }
+
+    // Wait for both promises to complete.
+    await Promise.all([assetsPromise, telemetryPromise]);
+
+    return assetsPromise;
   },
 };
 
