f1f4e410737a70e6a08fe0e000561e2fd7acd6b9
Rich Burdon <rich.burdon@gmail.com>
2024-09-20 17:48:09 -0400

    fix: Plank borders and misc styles (#7747)

--- packages/plugins/plugin-sheet/src/components/SheetContainer.tsx
+++ packages/plugins/plugin-sheet/src/components/SheetContainer.tsx
@@ -4,25 +4,20 @@
 
 import React from 'react';
 
-import { type LayoutCoordinate } from '@dxos/app-framework';
+import { type LayoutContainerProps } from '@dxos/app-framework';
 import { mx } from '@dxos/react-ui-theme';
 
 import { Sheet, type SheetRootProps } from './Sheet';
 
-const SheetContainer = ({
-  sheet,
-  space,
-  role,
-  coordinate = { part: 'main', entryId: '' },
-}: SheetRootProps & { role?: string; coordinate?: LayoutCoordinate }) => {
+const SheetContainer = ({ sheet, space, role }: LayoutContainerProps<SheetRootProps>) => {
   return (
     <div
       role='none'
       className={mx(
         'flex',
-        role === 'article' && 'row-span-2', // TODO(burdon): Container with toolbar.
+        role === 'article' && 'row-span-2',
         role === 'section' && 'aspect-square border-y border-is border-separator',
-        coordinate.part !== 'solo' && 'border-is border-separator',
+        // coordinate.part !== 'solo' && 'border-is border-separator',
       )}
     >
       <Sheet.Root sheet={sheet} space={space}>
