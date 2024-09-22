99a47408de0825b1f61ffb594ee819fb6f68e90c
Dmytro Maretskyi <maretskii@gmail.com>
2024-09-22 14:40:40 +0200

    LWW sync

---
+++ packages/common/merkle-search-tree/src/immutable/lww-tree.test.ts
@@ -0,0 +1,50 @@
+import { test } from 'vitest';
+import { LWWTree, initSyncState, type ActorID, type SyncMessage } from './lww-tree';
+
+test('sim', async ({ expect }) => {
+  const peer1 = await LWWTree.new({ actor: 'peer1' as ActorID });
+  const peer2 = await LWWTree.new({ actor: 'peer2' as ActorID });
+  let state1 = initSyncState(),
+    state2 = initSyncState();
+
+  const sync = async () => {
+    for (let i = 0; i < 15; i++) {
+      let message1: SyncMessage | null = null,
+        message2: SyncMessage | null = null;
+      console.log('state1', state1);
+      [state1, message1] = await peer1.generateSyncMessage(state1);
+      if (message1) {
+        console.log('1 -> 2', { ...message1, nodes: message1.nodes.map((node) => ({ ...node, items: undefined })) });
+
+        state2 = await peer2.receiveSyncMessage(state2, message1);
+      }
+      console.log('state2', state2);
+
+      [state2, message2] = await peer2.generateSyncMessage(state2);
+      if (message2) {
+        console.log('2 -> 1', { ...message2, nodes: message2.nodes.map((node) => ({ ...node, items: undefined })) });
+        state1 = await peer1.receiveSyncMessage(state1, message2);
+      }
+
+      if (!message1 && !message2) {
+        console.log(`synced in ${i} iterations`);
+        return;
+      }
+    }
+    throw new Error('sync failed');
+  };
+
+  await peer1.set('a', '1');
+  await peer2.set('b', '2');
+  await sync();
+  expect(await peer1.get('a')).toBe('1');
+  expect(await peer2.get('a')).toBe('1');
+  expect(await peer1.get('b')).toBe('2');
+  expect(await peer2.get('b')).toBe('2');
+
+  await peer1.set('a', '3');
+  await peer2.set('a', '4');
+  await sync();
+  expect(await peer1.get('a')).toBe('4');
+  expect(await peer2.get('a')).toBe('4');
+});
---
+++ packages/common/merkle-search-tree/src/immutable/lww-tree.ts
@@ -0,0 +1,158 @@
+import { arrayToString } from '@dxos/util';
+import { Forest, type DigestHex, type Key, type NodeData } from './forest';
+
+export type ActorID = string & { __ActorID: never };
+
+export type LLWTreeParams = {
+  actor: ActorID;
+};
+
+export class LWWTree<T> {
+  static async new(params: LLWTreeParams) {
+    const tree = new LWWTree(params);
+    tree.#currentRoot = await tree.#forest.createTree([]);
+
+    return tree;
+  }
+
+  #actor: ActorID;
+
+  #forest = new Forest();
+
+  #currentRoot!: DigestHex;
+
+  get currentRoot() {
+    return this.#currentRoot;
+  }
+
+  private constructor(params: LLWTreeParams) {
+    this.#actor = params.actor;
+  }
+
+  async get(key: Key): Promise<T | undefined> {
+    const data = await this.#getData(key);
+    return data?.value;
+  }
+
+  async set(key: Key, value: T) {
+    const prevData = await this.#getData(key);
+    const newClock: VersionClock = !prevData
+      ? ([this.#actor, 0] as const)
+      : ([this.#actor, prevData.clock[1] + 1] as const);
+    const data = this.#encode({ clock: newClock, value });
+    this.#currentRoot = await this.#forest.set(this.#currentRoot, key, data);
+  }
+
+  async receiveSyncMessage(state: SyncState, message: SyncMessage): Promise<SyncState> {
+    await this.#forest.insertNodes(message.nodes);
+
+    const remoteRoot = message.root ?? state.remoteRoot;
+
+    if (remoteRoot !== null) {
+      const missing = [...(await this.#forest.missingNodes(remoteRoot))];
+
+      console.log('missing', remoteRoot, missing);
+
+      if (missing.length === 0) {
+        this.#currentRoot = await this.#forest.merge(remoteRoot, this.#currentRoot, this.#merge.bind(this));
+      }
+    }
+
+    return {
+      remoteRoot: remoteRoot,
+      remoteWant: message.want,
+      needsAck: message.nodes.length > 0,
+    };
+  }
+
+  async generateSyncMessage(state: SyncState): Promise<[SyncState, SyncMessage | null]> {
+    if (state.remoteRoot === this.#currentRoot && state.remoteWant.length === 0 && !state.needsAck) {
+      return [state, null];
+    }
+
+    const nodes = await this.#forest.getNodes(state.remoteWant);
+    const want = state.remoteRoot === null ? [] : [...(await this.#forest.missingNodes(state.remoteRoot))];
+
+    return [
+      {
+        remoteRoot: state.remoteRoot,
+        remoteWant: state.remoteWant,
+        needsAck: false,
+      },
+      {
+        root: this.#currentRoot,
+        want,
+        nodes,
+      },
+    ];
+  }
+
+  async #getData(key: Key): Promise<ValueData<T> | undefined> {
+    const res = await this.#forest.get(this.#currentRoot, key);
+    switch (res.kind) {
+      case 'present': {
+        return this.#decode(res.value);
+      }
+
+      case 'missing':
+        return undefined;
+      case 'not-available':
+        throw new Error('Key not available');
+    }
+  }
+
+  async #merge(key: Key, left: Uint8Array | null, right: Uint8Array | null): Promise<Uint8Array> {
+    if (!left) {
+      return right!;
+    }
+    if (!right) {
+      return left!;
+    }
+
+    const leftData = this.#decode(left);
+    const rightData = this.#decode(right);
+
+    const cmp =
+      leftData.clock[1] === rightData.clock[1]
+        ? leftData.clock[0].localeCompare(rightData.clock[0])
+        : leftData.clock[1] - rightData.clock[1];
+    if (cmp >= 0) {
+      return left;
+    } else {
+      return right;
+    }
+  }
+
+  #decode(value: Uint8Array): ValueData<T> {
+    return JSON.parse(textDecoder.decode(value));
+  }
+
+  #encode(data: ValueData<T>): Uint8Array {
+    const { clock, value } = data;
+    return textEncoder.encode(JSON.stringify({ clock, value })); // Field order ensures consistent hashing.
+  }
+}
+
+export type SyncMessage = {
+  root: DigestHex | null;
+  want: DigestHex[];
+  nodes: NodeData[];
+};
+
+export type SyncState = {
+  remoteRoot: DigestHex | null;
+  remoteWant: DigestHex[];
+  needsAck: boolean;
+};
+
+export const initSyncState = (): SyncState => ({ remoteRoot: null, remoteWant: [], needsAck: false });
+
+type VersionClock = readonly [actor: ActorID, version: number];
+
+type ValueData<T> = {
+  clock: VersionClock;
+  value: T;
+};
+
+const textDecoder = new TextDecoder();
+const textEncoder = new TextEncoder();
