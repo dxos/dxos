0b0a0ed71b4e3e945fff01c45e55120b99ab4fda
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-09-23 15:09:23 -0700

    Fix sheet container borders

--- packages/plugins/plugin-sheet/src/components/SheetContainer.tsx
+++ packages/plugins/plugin-sheet/src/components/SheetContainer.tsx
@@ -34,21 +34,24 @@ const SheetContainer = ({
   const isDirectlyAttended = useIsDirectlyAttended(id);
 
   // TODO(Zan): Centralise the toolbar action handler. Current implementation in stories.
-  const handleAction = useCallback((action: ToolbarAction) => {
-    switch (action.type) {
-      case 'comment': {
-        // TODO(Zan): Don't hardcode action id
-        void dispatch({
-          action: 'dxos.org/plugin/thread/action/create',
-          data: {
-            cursor: action.anchor,
-            name: action.cellContent,
-            subject: sheet,
-          },
-        });
+  const handleAction = useCallback(
+    (action: ToolbarAction) => {
+      switch (action.type) {
+        case 'comment': {
+          // TODO(Zan): We shouldn't hardcode the action ID.
+          void dispatch({
+            action: 'dxos.org/plugin/thread/action/create',
+            data: {
+              cursor: action.anchor,
+              name: action.cellContent,
+              subject: sheet,
+            },
+          });
+        }
       }
-    }
-  }, []);
+    },
+    [sheet, dispatch],
+  );
 
   return (
     <div role='none' className={role === 'article' ? 'row-span-2 grid grid-rows-subgrid' : undefined}>
@@ -56,11 +59,11 @@ const SheetContainer = ({
         <div role='none' className={mx('flex flex-0 justify-center overflow-x-auto')}>
           <Toolbar.Root
             onAction={handleAction}
-            classNames={
+            classNames={mx(
               role === 'section'
                 ? ['z-[2] group-focus-within/section:visible', !isDirectlyAttended && 'invisible', sectionToolbarLayout]
-                : ['group-focus-within/editor:border-separator group-[[aria-current]]/editor:border-separator']
-            }
+                : 'group-focus-within/editor:border-separator group-[[aria-current]]/editor:border-separator border-is border-ie border-separator',
+            )}
           >
             {/* TODO(Zan): Restore some of this functionality */}
             {/* <Toolbar.Styles /> */}
