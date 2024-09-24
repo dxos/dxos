0df949ed413f2bc38fb2f22a7f3ba70fa3b7eabf
Dmytro Maretskyi <maretskii@gmail.com>
2024-09-24 11:16:51 +0200

    Fix invite test

--- packages/sdk/client/src/tests/invitations.test.ts
+++ packages/sdk/client/src/tests/invitations.test.ts
@@ -230,7 +230,7 @@ const testSuite = (getParams: () => PerformInvitationParams, getPeers: () => [Se
 
     expect(attempt).to.eq(3);
     expect(guestResult.error).to.exist;
-    await expect(asyncTimeout(hostPromise, 100)).rejects.toThrow();
+    await expect(asyncTimeout(hostPromise, 100)).rejects.toBeDefined();
   });
 
   test('invitation timeout', async () => {
