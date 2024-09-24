b504f6f182fea3d89f1a66f68fbc2c5de8f36a5f
Ian Grant Jeffries <mail@ianjeffries.net>
2024-09-24 10:03:21 -0400

    Add comment

--- packages/plugins/experimental/plugin-map/src/hooks/useStabilizedObject.ts
+++ packages/plugins/experimental/plugin-map/src/hooks/useStabilizedObject.ts
@@ -4,6 +4,15 @@
 
 import { useState, useEffect } from 'react';
 
+/**
+ * Intended use case: when a hook is returning an object whose reference identity
+ * is changing more than you'd like.
+ *
+ * Behavior: this will keep the reference from changing until the customEqual
+ * parameter returns false.
+ *
+ * TODO(seagreen): changes will lag one render behind because of the way this uses useEffect
+ */
 export const useStabilizedObject = <T>(obj: T, customEqual: (oldObject: T, newObject: T) => boolean) => {
   const [stableObj, setStableObj] = useState(obj);
 
