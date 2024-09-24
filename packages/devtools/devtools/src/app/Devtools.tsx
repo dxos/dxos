10b7818a0f831e7345d456c052d5bcc8c20873d4
Rich Burdon <rich.burdon@gmail.com>
2024-09-24 18:57:33 -0400

    refactor: ClientProvider accepts initialized clients (#7799)
    Co-authored-by: Josiah Witt <josiah@witt.life>

--- packages/devtools/devtools/src/app/Devtools.tsx
+++ packages/devtools/devtools/src/app/Devtools.tsx
@@ -2,68 +2,26 @@
 // Copyright 2022 DXOS.org
 //
 
-import React, { useEffect, type FC, useState } from 'react';
+import React from 'react';
 import { HashRouter } from 'react-router-dom';
 
-import { type Observability } from '@dxos/observability';
-import { type Client, ClientProvider } from '@dxos/react-client';
-import { DensityProvider, type ThemeMode, ThemeProvider } from '@dxos/react-ui';
-import { defaultTx } from '@dxos/react-ui-theme';
-
-import { ErrorBoundary } from '../components';
-import { DevtoolsContextProvider, useRoutes, namespace as observabilityNamespace } from '../hooks';
+import { DevtoolsContextProvider, useRoutes } from '../hooks';
 
 const Routes = () => {
   return useRoutes();
 };
 
-// TODO(burdon): Factor out. See copy paste in testbench-app.
-const useThemeWatcher = () => {
-  const [themeMode, setThemeMode] = useState<ThemeMode>('dark');
-  const setTheme = ({ matches: prefersDark }: { matches?: boolean }) => {
-    document.documentElement.classList[prefersDark ? 'add' : 'remove']('dark');
-    setThemeMode(prefersDark ? 'dark' : 'light');
-  };
-
-  useEffect(() => {
-    const modeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    setTheme({ matches: modeQuery.matches });
-    modeQuery.addEventListener('change', setTheme);
-    return () => modeQuery.removeEventListener('change', setTheme);
-  }, []);
-
-  return themeMode;
-};
-
-// TODO(wittjosiah): Migrate devtools to use surface plugins.
-const Telemetry = ({ namespace, observability }: { namespace: string; observability?: Observability }) => {
-  // TODO: Add telemetry.
-  return null;
-};
-
 /**
  * Entrypoint for app and extension (no direct dependency on Client).
  */
-export const Devtools: FC<{
-  client: Client;
-  observability?: Observability;
-  namespace?: string;
-}> = ({ client, observability, namespace = observabilityNamespace }) => {
-  const themeMode = useThemeWatcher();
+export const Devtools = () => {
   return (
-    <ThemeProvider {...{ tx: defaultTx, themeMode }}>
-      <DensityProvider density='fine'>
-        <ErrorBoundary>
-          <ClientProvider client={client}>
-            <DevtoolsContextProvider>
-              <HashRouter>
-                <Telemetry namespace={namespace} observability={observability} />
-                <Routes />
-              </HashRouter>
-            </DevtoolsContextProvider>
-          </ClientProvider>
-        </ErrorBoundary>
-      </DensityProvider>
-    </ThemeProvider>
+    <DevtoolsContextProvider>
+      <HashRouter>
+        <Routes />
+      </HashRouter>
+    </DevtoolsContextProvider>
   );
 };
+
+export default Devtools;
