3342c28a0257391a384581a65187a8f08e13f9c6
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2024-09-25 13:53:28 +0200

    test: Enable retries for client tests (#7822)

--- packages/gravity/proto-guard/vitest.config.ts
+++ packages/gravity/proto-guard/vitest.config.ts
@@ -6,4 +6,12 @@ import { defineConfig, mergeConfig } from 'vitest/config';
 
 import { baseConfig } from '../../../vitest.shared';
 
-export default mergeConfig(baseConfig(), defineConfig({}));
+export default mergeConfig(
+  baseConfig(),
+  defineConfig({
+    test: {
+      // TODO(dmaretskyi): Enabled because client tests were flaky. Remove when that's not the case.
+      retry: 2,
+    },
+  }),
+);
