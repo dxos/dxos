b2c6464a0602037f6aca0a5ce645fae5ecc6eebd
Dmytro Maretskyi <maretskii@gmail.com>
2024-09-26 16:00:17 +0200

    enable paths only for serve

--- packages/apps/testbench-app/vite.config.ts
+++ packages/apps/testbench-app/vite.config.ts
@@ -16,103 +16,107 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { UserConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: true,
-    cors: true,
-    // Set isolation to enable performance.measureUserAgentSpecificMemory
-    // https://web.dev/articles/coop-coep
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin',
-    },
-    https:
-      process.env.HTTPS === 'true'
-        ? {
-            key: './key.pem',
-            cert: './cert.pem',
-          }
-        : undefined,
-    fs: {
-      strict: false,
-      allow: [
-        // TODO(wittjosiah): Not detecting pnpm-workspace?
-        //  https://vitejs.dev/config/server-options.html#server-fs-allow
-        searchForWorkspaceRoot(process.cwd()),
-      ],
-    },
-  },
-  esbuild: {
-    keepNames: true,
-  },
-  build: {
-    sourcemap: true,
-    minify: false,
-    target: ['chrome89', 'edge89', 'firefox89', 'safari15'],
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, './index.html'),
-        shell: resolve(__dirname, './shell.html'),
-      },
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-          dxos: ['@dxos/react-client'],
-          ui: ['@dxos/react-ui', '@dxos/react-ui-theme'],
-          editor: ['@dxos/react-ui-editor'],
+export default defineConfig(
+  (env) =>
+    ({
+      server: {
+        host: true,
+        cors: true,
+        // Set isolation to enable performance.measureUserAgentSpecificMemory
+        // https://web.dev/articles/coop-coep
+        headers: {
+          'Cross-Origin-Embedder-Policy': 'require-corp',
+          'Cross-Origin-Opener-Policy': 'same-origin',
+        },
+        https:
+          process.env.HTTPS === 'true'
+            ? {
+                key: './key.pem',
+                cert: './cert.pem',
+              }
+            : undefined,
+        fs: {
+          strict: false,
+          allow: [
+            // TODO(wittjosiah): Not detecting pnpm-workspace?
+            //  https://vitejs.dev/config/server-options.html#server-fs-allow
+            searchForWorkspaceRoot(process.cwd()),
+          ],
         },
       },
-    },
-  },
-  worker: {
-    format: 'es',
-    plugins: () => [WasmPlugin(), sourceMaps()],
-  },
-  plugins: [
-    sourceMaps(),
-    tsconfigPaths({
-      projects: ['../../../tsconfig.paths.json'],
-    }),
-    ConfigPlugin({
-      env: ['DX_VAULT', 'BASELIME_API_KEY'],
-    }),
-    ThemePlugin({
-      root: __dirname,
-      content: [resolve(__dirname, './index.html'), resolve(__dirname, './src/**/*.{js,ts,jsx,tsx}')],
-    }),
-    WasmPlugin(),
-    // https://github.com/preactjs/signals/issues/269
-    ReactPlugin({ jsxRuntime: 'classic' }),
-    // https://docs.sentry.io/platforms/javascript/sourcemaps/uploading/vite
-    // https://www.npmjs.com/package/@sentry/vite-plugin
-    // sentryVitePlugin({
-    //   org: 'dxos',
-    //   project: 'testbench-app',
-    //   sourcemaps: {
-    //     assets: './packages/apps/testbench-app/out/testbench-app/**',
-    //   },
-    //   authToken: process.env.SENTRY_RELEASE_AUTH_TOKEN,
-    //   disable: process.env.DX_ENVIRONMENT !== 'production',
-    // }),
-    // https://www.bundle-buddy.com/rollup
-    {
-      name: 'bundle-buddy',
-      buildEnd() {
-        const deps: { source: string; target: string }[] = [];
-        for (const id of this.getModuleIds()) {
-          const m = this.getModuleInfo(id);
-          if (m != null && !m.isExternal) {
-            for (const target of m.importedIds) {
-              deps.push({ source: m.id, target });
-            }
-          }
-        }
-        const outDir = join(__dirname, 'out');
-        if (!existsSync(outDir)) {
-          mkdirSync(outDir);
-        }
-        writeFileSync(join(outDir, 'graph.json'), JSON.stringify(deps, null, 2));
+      esbuild: {
+        keepNames: true,
       },
-    },
-  ],
-} as UserConfig);
+      build: {
+        sourcemap: true,
+        minify: false,
+        target: ['chrome89', 'edge89', 'firefox89', 'safari15'],
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, './index.html'),
+            shell: resolve(__dirname, './shell.html'),
+          },
+          output: {
+            manualChunks: {
+              react: ['react', 'react-dom', 'react-router-dom'],
+              dxos: ['@dxos/react-client'],
+              ui: ['@dxos/react-ui', '@dxos/react-ui-theme'],
+              editor: ['@dxos/react-ui-editor'],
+            },
+          },
+        },
+      },
+      worker: {
+        format: 'es',
+        plugins: () => [WasmPlugin(), sourceMaps()],
+      },
+      plugins: [
+        sourceMaps(),
+        env.command === 'serve' &&
+          tsconfigPaths({
+            projects: ['../../../tsconfig.paths.json'],
+          }),
+        ConfigPlugin({
+          env: ['DX_VAULT', 'BASELIME_API_KEY'],
+        }),
+        ThemePlugin({
+          root: __dirname,
+          content: [resolve(__dirname, './index.html'), resolve(__dirname, './src/**/*.{js,ts,jsx,tsx}')],
+        }),
+        WasmPlugin(),
+        // https://github.com/preactjs/signals/issues/269
+        ReactPlugin({ jsxRuntime: 'classic' }),
+        // https://docs.sentry.io/platforms/javascript/sourcemaps/uploading/vite
+        // https://www.npmjs.com/package/@sentry/vite-plugin
+        // sentryVitePlugin({
+        //   org: 'dxos',
+        //   project: 'testbench-app',
+        //   sourcemaps: {
+        //     assets: './packages/apps/testbench-app/out/testbench-app/**',
+        //   },
+        //   authToken: process.env.SENTRY_RELEASE_AUTH_TOKEN,
+        //   disable: process.env.DX_ENVIRONMENT !== 'production',
+        // }),
+        // https://www.bundle-buddy.com/rollup
+        {
+          name: 'bundle-buddy',
+          buildEnd() {
+            const deps: { source: string; target: string }[] = [];
+            for (const id of this.getModuleIds()) {
+              const m = this.getModuleInfo(id);
+              if (m != null && !m.isExternal) {
+                for (const target of m.importedIds) {
+                  deps.push({ source: m.id, target });
+                }
+              }
+            }
+            const outDir = join(__dirname, 'out');
+            if (!existsSync(outDir)) {
+              mkdirSync(outDir);
+            }
+            writeFileSync(join(outDir, 'graph.json'), JSON.stringify(deps, null, 2));
+          },
+        },
+      ],
+    }) as UserConfig,
+);
