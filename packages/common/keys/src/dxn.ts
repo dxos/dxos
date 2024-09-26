2194117506dd2ea3a940ce210c425f6abb100145
Dmytro Maretskyi <maretskii@gmail.com>
2024-09-26 16:14:49 +0200

    Better DXN errors

--- packages/common/keys/src/dxn.ts
+++ packages/common/keys/src/dxn.ts
@@ -57,10 +57,14 @@ export class DXN {
     // Per-type validation.
     switch (kind) {
       case DXN.kind.ECHO:
-        invariant(parts.length === 2);
+        if (parts.length !== 2) {
+          throw new Error(`Invalid "echo" DXN`);
+        }
         break;
       case DXN.kind.TYPE:
-        invariant(parts.length === 1);
+        if (parts.length !== 1) {
+          throw new Error('Invalid "type" DXN');
+        }
         break;
     }
 
