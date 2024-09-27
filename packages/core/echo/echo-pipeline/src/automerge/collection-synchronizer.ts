5d4abed5cd613045f000125491ce4897ca1710ad
Dmytro Maretskyi <maretskii@gmail.com>
2024-09-27 15:46:37 +0200

    Remove logs

--- packages/core/echo/echo-pipeline/src/automerge/collection-synchronizer.ts
+++ packages/core/echo/echo-pipeline/src/automerge/collection-synchronizer.ts
@@ -65,7 +65,6 @@ export class CollectionSynchronizer extends Resource {
   }
 
   setLocalCollectionState(collectionId: string, state: CollectionState) {
-    log.info('setLocalCollectionState', { collectionId, state });
     this._getPerCollectionState(collectionId).localState = state;
 
     queueMicrotask(async () => {
@@ -145,7 +144,6 @@ export class CollectionSynchronizer extends Resource {
    * Callback when a peer sends the state of a collection.
    */
   onRemoteStateReceived(collectionId: string, peerId: PeerId, state: CollectionState) {
-    log.info('onRemoteStateReceived', { collectionId, peerId, state });
     validateCollectionState(state);
     const perCollectionState = this._getPerCollectionState(collectionId);
     perCollectionState.remoteStates.set(peerId, state);
