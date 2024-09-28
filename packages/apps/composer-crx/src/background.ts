292f25f979f8573e43c5ed7c1248629cfbb02cd5
Rich Burdon <rich.burdon@gmail.com>
2024-09-28 00:02:42 -0400

    feat: Composer CRX (#7838)

---
+++ packages/apps/composer-crx/src/background.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import browser from 'webextension-polyfill';
+
+const main = async () => {
+  // eslint-disable-next-line no-console
+  console.log(browser);
+};
+
+void main();
