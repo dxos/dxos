5c446fdf246d4757a84ca7b23c90e29f41538836
Josiah Witt <josiah@witt.life>
2024-10-02 14:28:18 -0400

    ci: add check to gh actions (#7847)

--- packages/gravity/proto-guard/vitest.config.ts
+++ packages/gravity/proto-guard/vitest.config.ts
@@ -7,7 +7,7 @@ import { defineConfig, mergeConfig } from 'vitest/config';
 import { baseConfig } from '../../../vitest.shared';
 
 export default mergeConfig(
-  baseConfig(),
+  baseConfig({ cwd: __dirname }),
   defineConfig({
     test: {
       // TODO(dmaretskyi): Enabled because client tests were flaky. Remove when that's not the case.
