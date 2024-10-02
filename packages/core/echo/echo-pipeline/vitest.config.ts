c0c6b6fcd293b947d5eeb7f42c7ab97e1a1d961f
Josiah Witt <josiah@witt.life>
2024-10-02 14:28:18 -0400

    ci: add check to gh actions (#7847)

--- packages/core/echo/echo-pipeline/vitest.config.ts
+++ packages/core/echo/echo-pipeline/vitest.config.ts
@@ -6,4 +6,4 @@ import { defineConfig, mergeConfig } from 'vitest/config';
 
 import { baseConfig } from '../../../../vitest.shared';
 
-export default mergeConfig(baseConfig({ nodeExternal: true }), defineConfig({}));
+export default mergeConfig(baseConfig({ cwd: __dirname, nodeExternal: true }), defineConfig({}));
