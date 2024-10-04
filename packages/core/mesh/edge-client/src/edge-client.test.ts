2c07f99362cf1f7940da733468df0aaf92d0ac5b
Yaroslav <yaroslav.shevchuk@bolt.eu>
2024-10-04 13:13:42 +0300

    fix(edge): immediately start feed replication if edge-client is connected (#7881)

--- packages/core/mesh/edge-client/src/edge-client.test.ts
+++ packages/core/mesh/edge-client/src/edge-client.test.ts
@@ -4,6 +4,7 @@
 
 import { test, expect, describe, onTestFinished } from 'vitest';
 
+import { Trigger } from '@dxos/async';
 import { PublicKey } from '@dxos/keys';
 import { TextMessageSchema } from '@dxos/protocols/buf/dxos/edge/messenger_pb';
 import { openAndClose } from '@dxos/test-utils';
@@ -29,6 +30,28 @@ describe('EdgeClient', () => {
     await closeConnection();
     await reconnected;
     await expect(client.send(textMessage('Hello world 2'))).resolves.not.toThrow();
+  });
+
+  test('isConnected', async () => {
+    const admitConnection = new Trigger();
+    const { closeConnection, endpoint, cleanup } = await createTestEdgeWsServer(8001, { admitConnection });
+    onTestFinished(cleanup);
+
+    const id = PublicKey.random().toHex();
+    const client = new EdgeClient(id, id, { socketEndpoint: endpoint });
+    await openAndClose(client);
+
+    expect(client.isConnected).toBeFalsy();
+    admitConnection.wake();
+    await expect.poll(() => client.isConnected).toBeTruthy();
+
+    admitConnection.reset();
+    await closeConnection();
+    expect(client.isOpen).is.true;
+    await expect.poll(() => client.isConnected).toBeFalsy();
+
+    admitConnection.wake();
+    await expect.poll(() => client.isConnected).toBeTruthy();
   });
 
   test('set identity reconnects', async () => {
