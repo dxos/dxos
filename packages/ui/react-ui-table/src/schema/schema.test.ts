d5b9516ef4f0962dea616ecf38ea6b9b66fb019d
Rich Burdon <rich.burdon@gmail.com>
2024-10-10 23:51:24 -0400

    chore: table schema (#7905)

--- packages/ui/react-ui-table/src/schema/schema.test.ts
+++ packages/ui/react-ui-table/src/schema/schema.test.ts
@@ -5,59 +5,46 @@
 import { Schema as S } from '@effect/schema';
 import { describe, expect, test } from 'vitest';
 
-import { classifySchemaProperties } from './schema';
+import { getColumnTypes } from './schema';
 
-describe('schema->column-type', () => {
+// TODO(burdon): Realistic data.
+
+describe('getColumnTypes', () => {
   test('basic', () => {
-    const simpleSchema = S.Struct({
+    const schema = S.Struct({
       field1: S.String,
       field2: S.Number,
+      field3: S.Date,
     });
 
-    const columns = classifySchemaProperties(simpleSchema);
-
+    const columns = getColumnTypes(schema);
     expect(columns).to.deep.equal([
       ['field1', 'string'],
       ['field2', 'number'],
+      ['field3', 'date'],
     ]);
   });
 
   test('optional properties', () => {
-    const simpleSchema = S.Struct({
+    const schema = S.Struct({
       field1: S.optional(S.String),
       field2: S.optional(S.Number),
     });
 
-    const columns = classifySchemaProperties(simpleSchema);
-
+    const columns = getColumnTypes(schema);
     expect(columns).to.deep.equal([
       ['field1', 'string'],
       ['field2', 'number'],
     ]);
   });
 
-  test('dates', () => {
-    const simpleSchema = S.Struct({
-      field1: S.Date,
-      field2: S.optional(S.Date),
-    });
-
-    const columns = classifySchemaProperties(simpleSchema);
-
-    expect(columns).to.deep.equal([
-      ['field1', 'date'],
-      ['field2', 'date'],
-    ]);
-  });
-
-  test('Piped validators', () => {
-    const simpleSchema = S.Struct({
+  test('piped validators', () => {
+    const schema = S.Struct({
       name: S.optional(S.String.pipe(S.nonEmptyString(), S.maxLength(10))),
       age: S.Number.pipe(S.negative()),
     });
 
-    const columns = classifySchemaProperties(simpleSchema);
-
+    const columns = getColumnTypes(schema);
     expect(columns).to.deep.equal([
       ['name', 'string'],
       ['age', 'number'],
@@ -65,81 +52,24 @@ describe('schema->column-type', () => {
   });
 
   test('nested schema', () => {
-    const simpleSchema = S.Struct({
+    const schema = S.Struct({
       name: S.String,
-      nested: S.Struct({
-        age: S.Number,
-      }),
-      someUnion: S.Union(S.String, S.Number),
-    });
-
-    const columns = classifySchemaProperties(simpleSchema);
-
-    expect(columns).to.deep.equal([
-      ['name', 'string'],
-      ['nested.age', 'number'],
-      ['someUnion', 'display'],
-    ]);
-  });
-
-  test('deeply nested schema', () => {
-    const simpleSchema = S.Struct({
-      name: S.String,
-      nested: S.Struct({
+      nested1: S.Struct({
         age: S.Number,
         deeplyNested: S.Struct({
-          height: S.Number,
+          height: S.optional(S.Number),
         }),
-      }),
-    });
-
-    const columns = classifySchemaProperties(simpleSchema);
-
-    expect(columns).to.deep.equal([
-      ['name', 'string'],
-      ['nested.age', 'number'],
-      ['nested.deeplyNested.height', 'number'],
-    ]);
-  });
-
-  test('even more deeply nested schema with optionals', () => {
-    const simpleSchema = S.Struct({
-      name: S.String,
-      nested: S.Struct({
-        age: S.Number,
-        deeplyNested: S.optional(
-          S.Struct({
-            height: S.optional(S.Number),
-          }),
-        ),
-      }),
-    });
-
-    const columns = classifySchemaProperties(simpleSchema);
-
-    expect(columns).to.deep.equal([
-      ['name', 'string'],
-      ['nested.age', 'number'],
-      ['nested.deeplyNested.height', 'number'],
-    ]);
-  });
-
-  test('multiple-nested schemata as sibblings', () => {
-    const simpleSchema = S.Struct({
-      name: S.String,
-      nested: S.Struct({
-        age: S.Number,
       }),
       nested2: S.Struct({
         height: S.Number,
       }),
     });
 
-    const columns = classifySchemaProperties(simpleSchema);
-
+    const columns = getColumnTypes(schema);
     expect(columns).to.deep.equal([
       ['name', 'string'],
-      ['nested.age', 'number'],
+      ['nested1.age', 'number'],
+      ['nested1.deeplyNested.height', 'number'],
       ['nested2.height', 'number'],
     ]);
   });
