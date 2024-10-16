0417cf9814e803e414209572f4f899e3c81df403
thure <will@braneframe.com>
2024-10-16 13:53:50 -0700

    Attempt unsuccessfully to use existing components.

--- packages/ui/react-ui-grid/src/Grid/Grid.stories.tsx
+++ packages/ui/react-ui-grid/src/Grid/Grid.stories.tsx
@@ -4,19 +4,52 @@
 
 import '@dxos-theme';
 
-import React from 'react';
+import { type Scope } from '@radix-ui/react-context';
+import React, { type MouseEvent, type MutableRefObject, useCallback } from 'react';
 
+import { DropdownMenu, useDropdownMenuContext } from '@dxos/react-ui';
 import { withTheme } from '@dxos/storybook-utils';
 
 import { Grid, type GridContentProps, type GridRootProps } from './Grid';
 
 type StoryGridProps = GridContentProps & Pick<GridRootProps, 'onEditingChange'>;
 
-const StoryGrid = ({ onEditingChange, ...props }: StoryGridProps) => {
+type ScopedProps<P> = P & { __scopeDropdownMenu?: Scope };
+
+const StoryGridImpl = ({ onEditingChange, __scopeDropdownMenu, ...props }: ScopedProps<StoryGridProps>) => {
+  const { triggerRef, onOpenToggle } = useDropdownMenuContext('StoryGridImpl', __scopeDropdownMenu);
+  const handleClick = useCallback(
+    (event: MouseEvent) => {
+      const closestAction = (event.target as HTMLElement).closest('button[data-story-action]');
+      if (closestAction) {
+        console.log('[setting trigger ref]');
+        (triggerRef as MutableRefObject<HTMLButtonElement>).current = closestAction as HTMLButtonElement;
+        onOpenToggle();
+      }
+    },
+    [onOpenToggle],
+  );
   return (
-    <Grid.Root id='story' onEditingChange={onEditingChange}>
-      <Grid.Content {...props} />
-    </Grid.Root>
+    <>
+      <Grid.Root id='story' onEditingChange={onEditingChange}>
+        <Grid.Content {...props} onClick={handleClick} />
+      </Grid.Root>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content classNames='z-90'>
+          <DropdownMenu.Viewport>
+            <DropdownMenu.Item>Hello</DropdownMenu.Item>
+          </DropdownMenu.Viewport>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </>
+  );
+};
+
+const StoryGrid = (props: StoryGridProps) => {
+  return (
+    <DropdownMenu.Root>
+      <StoryGridImpl {...props} />
+    </DropdownMenu.Root>
   );
 };
 
@@ -34,7 +67,7 @@ export const Basic = {
       grid: {
         '1,1': {
           accessoryHtml:
-            '<button class="ch-button is-6 pli-0.5 min-bs-0 absolute inset-block-1 inline-end-1"><svg><use href="/icons.svg#ph--arrow-right--regular"/></svg></button>',
+            '<button class="ch-button is-6 pli-0.5 min-bs-0 absolute inset-block-1 inline-end-1" data-story-action="menu"><svg><use href="/icons.svg#ph--arrow-right--regular"/></svg></button>',
           value: 'Weekly sales report',
         },
       },
