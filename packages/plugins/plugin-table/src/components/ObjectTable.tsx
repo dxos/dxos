23c386f696b9016d6a62985542e4138bc07a06a1
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-10-17 11:01:25 -0700

    Extract useStarterTable

--- packages/plugins/plugin-table/src/components/ObjectTable.tsx
+++ packages/plugins/plugin-table/src/components/ObjectTable.tsx
@@ -2,25 +2,16 @@
 // Copyright 2024 DXOS.org
 //
 
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 
-import { S, TypedObject, type EchoReactiveObject } from '@dxos/echo-schema';
+import { type EchoReactiveObject } from '@dxos/echo-schema';
 import { useGlobalFilteredObjects } from '@dxos/plugin-search';
-import { PublicKey } from '@dxos/react-client';
 import { create, getSpace, useQuery, Filter } from '@dxos/react-client/echo';
 import { Button, Icon } from '@dxos/react-ui';
-import { FieldValueType } from '@dxos/schema';
 
 import { Table } from './Table';
+import { useStarterTable } from '../hooks/useStarterTable';
 import { type TableType } from '../types';
-
-const createStarterSchema = () => {
-  return TypedObject({ typename: 'example.com/type/' + PublicKey.random().toHex(), version: '0.1.0' })({
-    name: S.optional(S.String),
-    description: S.optional(S.String),
-    quantity: S.optional(S.Number),
-  });
-};
 
 export type ObjectTableProps = {
   table: TableType;
@@ -44,22 +35,7 @@ export const ObjectTable = ({ table }: ObjectTableProps) => {
     space.db.add(create(table.schema, {}));
   }, [table.schema, space]);
 
-  useEffect(() => {
-    if (space && !table.schema) {
-      const schema = space.db.schema.addSchema(createStarterSchema());
-      table.schema = schema;
-      table.view = {
-        query: { schema: {} },
-        fields: [
-          { id: 'name', path: 'name', label: 'Name', type: FieldValueType.String },
-          { id: 'description', path: 'description', label: 'Description', type: FieldValueType.String },
-          { id: 'quantity', path: 'quantity', label: 'Quantity', type: FieldValueType.Number },
-        ],
-      };
-
-      handleNewRow();
-    }
-  }, [table.schema]);
+  useStarterTable({ table, space });
 
   // TODO(Zan): Do we need this?
   if (!table.schema) {
