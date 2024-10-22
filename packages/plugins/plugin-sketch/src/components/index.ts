9f4affb68bfdf6e6bbefae8988f7132b9a4045bf
Rich Burdon <rich.burdon@gmail.com>
2024-10-21 20:03:08 -0400

    fix: ThemeProvider issue with multiple react-ui deps (#7989)

--- packages/plugins/plugin-sketch/src/components/index.ts
+++ packages/plugins/plugin-sketch/src/components/index.ts
@@ -2,9 +2,8 @@
 // Copyright 2023 DXOS.org
 //
 
-import React from 'react';
+import { lazy } from 'react';
 
 export * from './SketchSettings';
 
-// Lazily load components for content surfaces.
-export const SketchContainer = React.lazy(() => import('./SketchContainer'));
+export const SketchContainer = lazy(() => import('./SketchContainer'));
