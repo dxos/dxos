1edd9b0f772011f25a0a4f8804f2b124acd79bd9
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-10-24 12:26:57 -0700

    Fix the build

--- packages/plugins/plugin-table/src/components/Table/NewColumnForm.tsx
+++ packages/plugins/plugin-table/src/components/Table/NewColumnForm.tsx
@@ -8,7 +8,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { create } from '@dxos/echo-schema';
 import { DropdownMenu, useThemeContext } from '@dxos/react-ui';
 import { Field } from '@dxos/react-ui-data';
-import { FieldValueType } from '@dxos/schema';
+import { FieldType, FieldValueType } from '@dxos/schema';
 
 import { type TableModel } from '../../model';
 
@@ -20,10 +20,10 @@ type NewColumnFormProps = {
 
 // TODO(ZaymonFC): A util in `@dxos/schema` should look at the view and the
 // schema and generate the new field. That's why I haven't moved this to ../../seed.
-export const createStarterField = () => {
+export const createStarterField = (): FieldType => {
   // TODO(ZaymonFC): Can't currently supply a schema since it's not in the registry when we
   // try to add it to the table
-  return create({ path: 'newField', type: FieldValueType.Text });
+  return create({ path: 'newField', type: FieldValueType.Text }) as any;
 };
 
 export const NewColumnForm = ({ tableModel, open, close }: NewColumnFormProps) => {
@@ -37,7 +37,7 @@ export const NewColumnForm = ({ tableModel, open, close }: NewColumnFormProps) =
   }, [open]);
 
   const onCreate = useCallback(() => {
-    tableModel?.table?.view?.fields?.push(field as any);
+    tableModel?.table?.view?.fields?.push(field);
     // TODO(ZaymonFC): Use a module to update the view and schema at the same time!
     // TODO(ZaymonFC): Validate: that path and name are unique. Path should be non-empty.
     close();
