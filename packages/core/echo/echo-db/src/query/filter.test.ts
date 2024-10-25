66307780d10c3b24e4d93311f4cf52994ddf7550
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2024-10-25 14:56:39 +0200

    refactor: Use DXNs in the filter protocol (#8035)

--- packages/core/echo/echo-db/src/query/filter.test.ts
+++ packages/core/echo/echo-db/src/query/filter.test.ts
@@ -4,13 +4,12 @@
 
 import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 
-import { Reference } from '@dxos/echo-protocol';
-import { create, createObjectId, S, TypedObject } from '@dxos/echo-schema';
-import { PublicKey } from '@dxos/keys';
+import { type Reference } from '@dxos/echo-protocol';
+import { create, S, TypedObject } from '@dxos/echo-schema';
 import { QueryOptions } from '@dxos/protocols/proto/dxos/echo/filter';
 
 import { Filter } from './filter';
-import { filterMatch, compareType } from './filter-match';
+import { filterMatch } from './filter-match';
 import { ObjectCore } from '../core-db';
 import { getObjectCore } from '../echo-handler';
 import { EchoTestBuilder } from '../testing';
@@ -84,43 +83,6 @@ describe('Filter', () => {
     expect(filterMatch(Filter.not(Filter.or(filter1, filter2)), core)).to.be.true;
   });
 
-  // TODO(burdon): Test schema.
-
-  test('compare types', () => {
-    const spaceKey = PublicKey.random();
-    const objectId = createObjectId();
-
-    expect(compareType(new Reference(objectId, undefined, spaceKey.toHex()), new Reference(objectId), spaceKey)).to.be
-      .true;
-    expect(
-      compareType(new Reference(objectId, undefined, spaceKey.toHex()), new Reference(objectId), PublicKey.random()),
-    ).to.be.false;
-
-    expect(
-      compareType(
-        Reference.fromLegacyTypename('dxos.sdk.client.Properties'),
-        Reference.fromLegacyTypename('dxos.sdk.client.Properties'),
-        spaceKey,
-      ),
-    ).to.be.true;
-    expect(
-      compareType(
-        Reference.fromLegacyTypename('dxos.sdk.client.Properties'),
-        Reference.fromLegacyTypename('dxos.sdk.client.Test'),
-        spaceKey,
-      ),
-    ).to.be.false;
-
-    // Missing host on items created on some versions.
-    expect(
-      compareType(
-        Reference.fromLegacyTypename('dxos.sdk.client.Properties'),
-        new Reference('dxos.sdk.client.Properties', 'protobuf', undefined),
-        spaceKey,
-      ),
-    ).to.be.true;
-  });
-
   test('dynamic schema', async () => {
     class GeneratedSchema extends TypedObject({ typename: 'example.com/dynamic', version: '0.1.0' })({
       title: S.String,
@@ -128,13 +90,13 @@ describe('Filter', () => {
     const { db } = await builder.createDatabase();
     const schema = db.schema.addSchema(GeneratedSchema);
     const obj = db.add(create(schema, { title: 'test' }));
-    const filter = Filter.typename(schema.id);
+    const filter = Filter.schema(schema);
     expect(filterMatch(filter, getObjectCore(obj))).to.be.true;
   });
 
   test('__typename', () => {
     const filter = Filter.from({ __typename: 'example.com/type/Type' });
-    expect(filter.type!.toDXN().toString()).to.equal('dxn:type:example.com/type/Type');
+    expect(filter.type![0].toString()).to.equal('dxn:type:example.com/type/Type');
     expect(filter.properties).to.deep.equal({});
   });
 });
