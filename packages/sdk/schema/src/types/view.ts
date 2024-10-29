da030b4f4318e56c423b2c35700e476b8be82a27
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-10-29 11:30:21 -0700

    Use invariant

--- packages/sdk/schema/src/types/view.ts
+++ packages/sdk/schema/src/types/view.ts
@@ -4,20 +4,17 @@
 
 import { type MutableSchema } from '@dxos/echo-schema';
 import { getProperty } from '@dxos/effect';
+import { invariant } from '@dxos/invariant';
 
 import { schemaForType } from './field';
 import { type FieldType, type ViewType } from './types';
 
 export const addFieldToView = (schema: MutableSchema, view: ViewType, field: FieldType) => {
-  // Validate that the field does not already exist in the view.
-  if (getProperty(schema, field.path)) {
-    throw new Error(`Field already exists: ${field.path}`);
-  }
+  invariant(!getProperty(schema, field.path), `Field path already exists in schema: ${field.path}`);
 
   const propertySchema = schemaForType[field.type];
-  if (!propertySchema) {
-    throw new Error(`No schema for field type: ${field.type}`);
-  }
+  invariant(propertySchema, `No schema for field type: ${field.type}`);
+
   schema.addFields({ [field.path]: propertySchema });
   view.fields.push(field);
 };
