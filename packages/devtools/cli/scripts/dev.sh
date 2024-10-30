55f56a03dc4bb5b6f4e6a1e80dbc72c2128d1d47
yarolegovich <yarolegovich@gmail.com>
2024-10-30 21:13:18 +0200

    update dev cli script

--- packages/devtools/cli/scripts/dev.sh
+++ packages/devtools/cli/scripts/dev.sh
@@ -1,4 +1,14 @@
 # Source into current shell via: `. ./scripts/dev.sh`
 
-export DX_CLI="$(git rev-parse --show-toplevel)/packages/devtools/cli/bin/dev"
-alias dx="$DX_CLI"
+export CLI_ROOT="$(git rev-parse --show-toplevel)/packages/devtools/cli"
+export DX_CLI="$CLI_ROOT/bin/dev.js"
+
+function dx() {
+  pushd $CLI_ROOT > /dev/null
+  cleanup() {
+    popd > /dev/null
+  }
+  trap cleanup EXIT
+
+  $DX_CLI $@
+}
