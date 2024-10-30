101697fe935b4a15fe9033ae1a7237f03305e3e4
yarolegovich <yarolegovich@gmail.com>
2024-10-30 21:40:08 +0200

    fix poping directory when pushd was a no op

--- packages/devtools/cli/scripts/dev.sh
+++ packages/devtools/cli/scripts/dev.sh
@@ -4,11 +4,13 @@ export CLI_ROOT="$(git rev-parse --show-toplevel)/packages/devtools/cli"
 export DX_CLI="$CLI_ROOT/bin/dev.js"
 
 function dx() {
-  pushd $CLI_ROOT > /dev/null
-  cleanup() {
-    popd > /dev/null
-  }
-  trap cleanup EXIT
+  if [[ "$(pwd)" != "$CLI_ROOT" ]]; then
+    pushd $CLI_ROOT > /dev/null
+    cleanup() {
+      popd > /dev/null
+    }
+    trap cleanup EXIT
+  fi
 
   $DX_CLI $@
 }
