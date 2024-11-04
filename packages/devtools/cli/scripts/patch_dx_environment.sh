8e45833cafe22cae02e2b35a7a9c853ff87348b0
Josiah Witt <josiah@witt.life>
2024-11-04 07:51:50 -0500

    chore: misc cleanup (#8123)

--- packages/devtools/cli/scripts/patch_dx_environment.sh
+++ packages/devtools/cli/scripts/patch_dx_environment.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 if [[ -n "${DX_ENVIRONMENT-}" ]]; then
   if [[ "$(uname)" = "Darwin" ]]; then
-    sed -i '' "s/production/$DX_ENVIRONMENT/g" bin/dx
+    sed -i '' "s/production/$DX_ENVIRONMENT/g" bin/dx.js
   else
-    sed -i "s/production/$DX_ENVIRONMENT/g" bin/dx
+    sed -i "s/production/$DX_ENVIRONMENT/g" bin/dx.js
   fi
 fi
