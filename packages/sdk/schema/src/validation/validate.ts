80a00ed14d4b3a9ad634ae0e4481153756669d55
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-11-05 13:33:49 -0800

    Move validation into @dxos/schema

--- packages/sdk/schema/src/validation/validate.ts
+++ packages/sdk/schema/src/validation/validate.ts
@@ -5,11 +5,8 @@
 import { ArrayFormatter, Schema as S } from '@effect/schema';
 import { Effect } from 'effect';
 
-// TODO(ZaymonFC): This error type is anaemic.
-export type ValidationError = { path: string; message: string };
+import { type ValidationError } from './types';
 
-// TODO(ZaymonFC): This could probably be pushed down further to `@dxos/schema`, but the mapping
-// aspect is a UX concern at this point so I'm leaving it here for now.
 export const validateSchema = <T>(schema: S.Schema<T>, data: any): ValidationError[] | undefined => {
   const validator = S.decodeUnknownEither(schema, { errors: 'all' });
   const result = validator(data);
