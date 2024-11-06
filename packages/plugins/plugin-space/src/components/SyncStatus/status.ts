24cb0efff12b1def0c16b4a689c03b704cdb0b0e
Josiah Witt <josiah@witt.life>
2024-11-06 07:35:35 -0500

    refactor: combine sync & save status icons (#8126)

---
+++ packages/plugins/plugin-space/src/components/SyncStatus/status.ts
@@ -0,0 +1,44 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+export type Status = 'saving locally' | 'downloading' | 'uploading' | 'offline persisted' | 'remote synced';
+
+export const getStatus = ({
+  offline,
+  saved,
+  needsToUpload,
+  needsToDownload,
+}: {
+  offline: Boolean;
+  saved: Boolean;
+  needsToUpload: Boolean;
+  needsToDownload: Boolean;
+}): Status => {
+  if (!saved) {
+    return 'saving locally';
+  } else if (!offline && needsToDownload) {
+    return 'downloading';
+  } else if (!offline && needsToUpload) {
+    return 'uploading';
+  } else if (offline && !needsToUpload && !needsToDownload) {
+    return 'offline persisted';
+  } else {
+    return 'remote synced';
+  }
+};
+
+export const getIcon = (status: Status) => {
+  switch (status) {
+    case 'saving locally':
+      return 'ph--download--regular';
+    case 'downloading':
+      return 'ph--cloud-arrow-down--regular';
+    case 'uploading':
+      return 'ph--cloud-arrow-up--regular';
+    case 'offline persisted':
+      return 'ph--check-circle--regular';
+    case 'remote synced':
+      return 'ph--cloud-check--regular';
+  }
+};
