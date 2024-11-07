950ae71231be6dccaabeca95c36f569e444db2a9
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-11-06 17:10:25 -0800

    Enforce property uniqueness

--- packages/ui/react-ui-data/src/components/ViewEditor/ViewEditor.tsx
+++ packages/ui/react-ui-data/src/components/ViewEditor/ViewEditor.tsx
@@ -60,6 +60,15 @@ export const ViewEditor = ({ classNames, schema, view, readonly }: ViewEditorPro
     handleValueChanged(fieldProperties);
   }, [fieldProperties]);
 
+  const handleAdditionalValidation = useCallback(
+    ({ property }: PropertyType) => {
+      if (property && view.fields.find((f) => f.property === property && f !== field)) {
+        return [{ path: 'property', message: `'${property}' is not unique.` }];
+      }
+    },
+    [view.fields, field],
+  );
+
   const handleSelect = useCallback((field: FieldType) => {
     setField((f) => (f === field ? undefined : field));
   }, []);
@@ -129,6 +138,7 @@ export const ViewEditor = ({ classNames, schema, view, readonly }: ViewEditorPro
             autoFocus
             values={fieldProperties}
             schema={fieldSchema}
+            additionalValidation={handleAdditionalValidation}
             onValuesChanged={handleValueChanged}
             onSave={(props) => handleSet(field, props)}
             Custom={(props) => (
