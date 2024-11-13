7375347c3077c100dae0f8ce386733969a36a63d
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-11-13 15:14:01 -0800

    fix translations

--- packages/ui/react-ui-table/src/index.ts
+++ packages/ui/react-ui-table/src/index.ts
@@ -1,8 +1,11 @@
 //
 // Copyright 2024 DXOS.org
 //
+import translations from './translations';
 
 export * from './components';
 export * from './hooks';
 export * from './util';
 export * from './types';
+
+export { translations };
--- packages/ui/react-ui-table/src/translations.ts
+++ packages/ui/react-ui-table/src/translations.ts
@@ -29,4 +29,4 @@ export default [
       },
     },
   },
-];
+] as const;
