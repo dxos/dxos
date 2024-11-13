d62b0e8dea59c5a2ba47d18925bb17373df3967c
thure <will@braneframe.com>
2024-11-12 18:50:31 -0800

    Restore visible `Status`.

--- packages/ui/react-ui/src/components/Status/Status.tsx
+++ packages/ui/react-ui/src/components/Status/Status.tsx
@@ -9,22 +9,23 @@ import { type ThemedClassName } from '../../util';
 
 type StatusProps = ThemedClassName<ComponentPropsWithRef<'span'>> & {
   indeterminate?: boolean;
+  variant?: 'default' | 'main-bottom';
   progress?: number;
 };
 
 const Status = forwardRef<HTMLSpanElement, StatusProps>(
-  ({ classNames, children, progress = 0, indeterminate, ...props }, forwardedRef) => {
+  ({ classNames, children, progress = 0, indeterminate, variant, ...props }, forwardedRef) => {
     const { tx } = useThemeContext();
     return (
       <span
         role='status'
         {...props}
-        className={tx('status.root', 'status', { indeterminate }, classNames)}
+        className={tx('status.root', 'status', { indeterminate, variant }, classNames)}
         ref={forwardedRef}
       >
         <span
           role='none'
-          className={tx('status.bar', 'status__bar', { indeterminate }, classNames)}
+          className={tx('status.bar', 'status__bar', { indeterminate, variant }, classNames)}
           {...(!indeterminate && { style: { width: `${Math.round(progress * 100)}%` } })}
         />
         {children}
