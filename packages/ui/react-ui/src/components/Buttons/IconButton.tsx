83795c7bc3ebf23fd0652d7773c0b5764c3ac432
thure <will@braneframe.com>
2024-11-13 15:37:48 -0800

    Adjust IconButton implementation.

--- packages/ui/react-ui/src/components/Buttons/IconButton.tsx
+++ packages/ui/react-ui/src/components/Buttons/IconButton.tsx
@@ -2,23 +2,54 @@
 // Copyright 2024 DXOS.org
 //
 
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import { Button, type ButtonProps } from './Button';
 import { useThemeContext } from '../../hooks';
 import { Icon, type IconProps } from '../Icon';
+import { Tooltip } from '../Tooltip';
 
-type IconButtonProps = Omit<ButtonProps, 'children'> & Pick<IconProps, 'icon'> & { label: string; srOnly?: boolean };
+type IconButtonProps = Omit<ButtonProps, 'children'> &
+  Pick<IconProps, 'icon'> & { label: string; iconOnly?: boolean; tooltipPortal?: boolean; tooltipZIndex?: string };
 
-const IconButton = ({ icon, label, srOnly, ...props }: IconButtonProps) => {
-  const { tx } = useThemeContext();
-  return (
-    <Button {...props} classNames={tx('iconButton.root', 'iconButton')}>
-      <Icon icon={icon} />
-      <span className={srOnly ? 'sr-only' : undefined}>{label}</span>
-    </Button>
-  );
-};
+const IconOnlyButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ tooltipPortal, tooltipZIndex: zIndex, ...props }, forwardedRef) => {
+    const content = (
+      <Tooltip.Content {...(zIndex && { style: { zIndex } })}>
+        {props.label}
+        <Tooltip.Arrow />
+      </Tooltip.Content>
+    );
+    return (
+      <Tooltip.Root>
+        <Tooltip.Trigger>
+          <LabelledIconButton {...props} ref={forwardedRef} />
+        </Tooltip.Trigger>
+        {tooltipPortal ? <Tooltip.Portal>{content}</Tooltip.Portal> : content}
+      </Tooltip.Root>
+    );
+  },
+);
+
+const LabelledIconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ icon, iconOnly, label, ...props }, forwardedRef) => {
+    const { tx } = useThemeContext();
+    return (
+      <Button {...props} classNames={tx('iconButton.root', 'iconButton')} ref={forwardedRef}>
+        <Icon icon={icon} />
+        <span className={iconOnly ? 'sr-only' : undefined}>{label}</span>
+      </Button>
+    );
+  },
+);
+
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, forwardedRef) =>
+  props.iconOnly ? (
+    <IconOnlyButton {...props} ref={forwardedRef} />
+  ) : (
+    <LabelledIconButton {...props} ref={forwardedRef} />
+  ),
+);
 
 export { IconButton };
 
