06b4f4d8338825f8ec4b06f15e6b1957565199b6
Mykola Veremchuk <veremchukmukola@gmail.com>
2024-11-13 18:58:14 +0200

    Add meta export

--- packages/plugins/plugin-calls/src/meta.tsx
+++ packages/plugins/plugin-calls/src/meta.tsx
@@ -2,13 +2,14 @@
 // Copyright 2023 DXOS.org
 //
 
-import { pluginMeta } from '@dxos/app-framework';
+import { type PluginMeta } from '@dxos/app-framework';
 
 export const CALLS_PLUGIN = 'dxos.org/plugin/calls';
 
-export default pluginMeta({
+export default {
   id: CALLS_PLUGIN,
   name: 'Calls',
   description: 'Perform video calls.',
   icon: 'ph--phone-call--regular',
-});
+  source: 'https://github.com/dxos/dxos/tree/main/packages/plugins/plugin-calls',
+} satisfies PluginMeta;
