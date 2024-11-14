4a478d221b554e1b58eb0ce88143b5d4ec5d2c96
thure <will@braneframe.com>
2024-11-13 20:37:08 -0800

    Fix merge thing.

--- packages/ui/react-ui-table/src/types/index.ts
+++ packages/ui/react-ui-table/src/types/index.ts
@@ -2,8 +2,4 @@
 // Copyright 2024 DXOS.org
 //
 
-<<<<<<<< HEAD:packages/plugins/plugin-table/src/components/TableMain/index.ts
-export * from './TableMain';
-========
 export * from './table';
->>>>>>>> main:packages/ui/react-ui-table/src/types/index.ts
