9ac76a3ef7fe7613f638d37ac1fc86127c2a564d
Josiah Witt <josiah@witt.life>
2024-11-24 11:45:17 -0500

    fix: stale script editor

--- packages/plugins/experimental/plugin-script/src/components/ScriptEditor.tsx
+++ packages/plugins/experimental/plugin-script/src/components/ScriptEditor.tsx
@@ -68,7 +68,6 @@ export const ScriptEditor = ({ classNames, script, env }: ScriptEditorProps) => 
   );
 
   const [view, setView] = useState<ViewType>('editor');
-  const initialValue = useMemo(() => script.source?.content, [script.source]);
   const existingFunctionUrl = fn && getUserFunctionUrlInMetadata(getMeta(fn));
   const [error, setError] = useState<string>();
 
@@ -185,7 +184,7 @@ export const ScriptEditor = ({ classNames, script, env }: ScriptEditorProps) => 
         <TypescriptEditor
           id={script.id}
           env={env}
-          initialValue={initialValue}
+          initialValue={script.source?.content}
           extensions={extensions}
           className='flex is-full bs-full overflow-hidden ch-focus-ring-inset-over-all'
         />
