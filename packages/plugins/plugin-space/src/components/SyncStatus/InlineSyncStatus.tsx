3e620ce22c8353169264d8812af222ca291bb530
Josiah Witt <josiah@witt.life>
2024-12-04 11:20:25 -0500

    feat: improved sync status (#8352)

---
+++ packages/plugins/plugin-space/src/components/SyncStatus/InlineSyncStatus.tsx
@@ -0,0 +1,45 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import React, { useEffect, useState } from 'react';
+
+import { QueryEdgeStatusResponse } from '@dxos/protocols/proto/dxos/client/services';
+import { EdgeReplicationSetting } from '@dxos/protocols/proto/dxos/echo/metadata';
+import { useClient } from '@dxos/react-client';
+import { type Space } from '@dxos/react-client/echo';
+import { Icon, useTranslation } from '@dxos/react-ui';
+
+import { useSpaceSyncState } from './sync-state';
+import { SPACE_PLUGIN } from '../../meta';
+
+const useEdgeStatus = (): QueryEdgeStatusResponse.EdgeStatus => {
+  const [status, setStatus] = useState(QueryEdgeStatusResponse.EdgeStatus.NOT_CONNECTED);
+  const client = useClient();
+
+  useEffect(() => {
+    client.services.services.EdgeAgentService?.queryEdgeStatus().subscribe(({ status }) => {
+      setStatus(status);
+    });
+  }, [client]);
+
+  return status;
+};
+
+export const InlineSyncStatus = ({ space }: { space: Space }) => {
+  const { t } = useTranslation(SPACE_PLUGIN);
+
+  const connectedToEdge = useEdgeStatus() === QueryEdgeStatusResponse.EdgeStatus.CONNECTED;
+  // TODO(wittjosiah): This is not reactive.
+  const edgeSyncEnabled = space.internal.data.edgeReplication === EdgeReplicationSetting.ENABLED;
+  const syncState = useSpaceSyncState(space);
+  if (!connectedToEdge || !edgeSyncEnabled || !syncState || syncState.missingOnLocal === 0) {
+    return null;
+  }
+
+  return (
+    <div role='status' aria-label={t('syncing message')} className='flex items-center'>
+      <Icon icon='ph--arrows-clockwise--regular' size={3} classNames='animate-spin' />
+    </div>
+  );
+};
