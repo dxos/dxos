f4a439bd722841ff6f55d7991fba73e56e6fbac2
Dmytro Maretskyi <maretskii@gmail.com>
2024-12-06 22:15:17 +0100

    Add API

--- packages/core/echo/echo-db/src/proxy-db/mutable-schema-registry.ts
+++ packages/core/echo/echo-db/src/proxy-db/mutable-schema-registry.ts
@@ -5,10 +5,12 @@
 import { type UnsubscribeCallback } from '@dxos/async';
 import {
   getObjectAnnotation,
+  type JsonSchemaType,
   makeStaticSchema,
   MutableSchema,
   type ObjectAnnotation,
   ObjectAnnotationId,
+  type ObjectId,
   type S,
   type StaticSchema,
   StoredSchema,
@@ -19,15 +21,161 @@ import { createStoredSchema } from '@dxos/live-object';
 import { log } from '@dxos/log';
 
 import { type EchoDatabase } from './database';
-import { getObjectCore } from '../echo-handler';
+import { getObjectCore, type ReactiveEchoObject } from '../echo-handler';
 import { Filter } from '../query';
 
 export type SchemaSubscriptionCallback = (schema: MutableSchema[]) => void;
 
 export interface SchemaResolver {
+  query(query: SchemaRegistryQuery): SchemaRegistryLiveQuery<SchemaRecord>;
+
+  /**
+   * Registers the provided schema.
+   *
+   * @returns Schema records after registration.
+   *
+   * The behavior of this method depends on the state of the database. The general
+   * principle is that the schema will be upserted into the space. If an equivalent
+   * schema with the same name and version already exists, the method does nothing.
+   * If a different schema with the same name and version exists, the method throws
+   * an error. If no schema with the same name and version exists, a new schema will
+   * be inserted based on semantic versioning rules.
+   */
+  register(input: RegisterSchemaInput[]): Promise<SchemaRecord[]>;
+
+  /**
+   * @deprecated Use `query()`.
+   */
   getSchema(typename: string): MutableSchema | undefined;
-  query(): Promise<MutableSchema[]>;
+
+  /**
+   * @deprecated Use `query()`.
+   */
   subscribe(cb: SchemaSubscriptionCallback): UnsubscribeCallback;
+}
+
+export type SchemaRegistryQuery = {
+  /**
+   * Filter by schema ID.
+   * Schema id is a DXN with `echo` or `type` kind.
+   */
+  id?: string[];
+
+  /**
+   * One or more typenames to filter by.
+   */
+  typename?: string[];
+
+  /**
+   * [Semver Range](https://docs.npmjs.com/cli/v6/using-npm/semver#ranges) for the schema version.
+   */
+  version?: string;
+};
+
+export interface SchemaRegistryLiveQuery<T> {
+  /**
+   * Returns query results synchronously.
+   * Supports signals notifications.
+   * User must call `subscribe` for reactive notifications to be enabled.
+   */
+  get results(): T[];
+
+  /**
+   * Runs the query and returns all results.
+   */
+  run(): Promise<T[]>;
+
+  /**
+   * Runs the query and returns first result.
+   *
+   * @throws If query returns 0 entries.
+   */
+  first(): Promise<T>;
+
+  /**
+   * Subscribe to the query results reactively.
+   * Enables signals notifications for `results`.
+   */
+  subscribe(cb: (self: this) => void, opts: { fire?: boolean }): UnsubscribeCallback;
+}
+
+/**
+ * Input for schema registration.
+ * Either one of the schema variants must be provided.
+ * The typename and version is read from the schema annotations.
+ */
+export type RegisterSchemaInput = {
+  /**
+   * Schema to register in the Effect format.
+   */
+  schema?: AnyEchoObjectSchema;
+
+  /**
+   * Schema to register in the JSON Schema format.
+   */
+  jsonSchema?: JsonSchemaType;
+};
+
+export type AnyEchoObjectSchema = S.Struct<{ [key: string]: S.Schema.AnyNoContext }>;
+
+export interface SchemaRecord {
+  /**
+   * String identifier for the schema.
+   * In practice it's an `echo` or `type` DXN.
+   */
+  get id(): string;
+
+  get mutable(): boolean;
+
+  /**
+   * @returns Rendered effect schema snapshot for this entry.
+   */
+  getSchema(): AnyEchoObjectSchema;
+
+  /**
+   * @returns This schema snapshot in JSON-schema format.
+   */
+  getJsonSchema(): JsonSchemaType;
+
+  /**
+   * Get backing ECHO object for this schema.
+   */
+  getBackingObject(): Promise<ReactiveEchoObject<StoredSchema> | undefined>;
+
+  /**
+   * Updates typename.
+   *
+   * Only callable if the schema is mutable.
+   */
+  updateTypename(typename: string): Promise<void>;
+
+  /**
+   * Adds fields.
+   *
+   * Only callable if the schema is mutable.
+   */
+  addFields(fields: S.Struct.Fields): Promise<void>;
+
+  /**
+   * Updates fields.
+   *
+   * Only callable if the schema is mutable.
+   */
+  updateFields(fields: S.Struct.Fields): Promise<void>;
+
+  /**
+   * Renames field.
+   *
+   * Only callable if the schema is mutable.
+   */
+  renameField({ from, to }: { from: string; to: string }): Promise<void>;
+
+  /**
+   * Removes fields.
+   *
+   * Only callable if the schema is mutable.
+   */
+  removeFields(fieldNames: string[]): Promise<void>;
 }
 
 export type MutableSchemaRegistryOptions = {
