cf751731287afb03afc21167ce2608ce19d5add6
Rich Burdon <rich.burdon@gmail.com>
2024-12-09 23:06:53 -0500

    test: graph (#8383)

--- packages/plugins/plugin-debug/src/components/SpaceGenerator/ObjectGenerator.tsx
+++ packages/plugins/plugin-debug/src/components/SpaceGenerator/ObjectGenerator.tsx
@@ -2,23 +2,13 @@
 // Copyright 2024 DXOS.org
 //
 
-import {
-  createTLStore,
-  defaultBindingUtils,
-  defaultShapeUtils,
-  defaultShapeTools,
-  defaultTools,
-  Editor,
-  type TLEditorOptions,
-} from '@tldraw/tldraw';
-
 import { type AbstractSchema, type BaseObject } from '@dxos/echo-schema';
 import { create, type ReactiveObject } from '@dxos/live-object';
 import { DocumentType, TextType } from '@dxos/plugin-markdown/types';
 import { addressToA1Notation, createSheet } from '@dxos/plugin-sheet';
 import { type CellValue } from '@dxos/plugin-sheet/types';
 import { SheetType } from '@dxos/plugin-sheet/types';
-import { TLDRAW_SCHEMA, CanvasType, DiagramType } from '@dxos/plugin-sketch/types';
+import { CanvasType, DiagramType } from '@dxos/plugin-sketch/types';
 import { faker } from '@dxos/random';
 import { Filter, type Space } from '@dxos/react-client/echo';
 import { TableType } from '@dxos/react-ui-table';
@@ -26,11 +16,12 @@ import { createView } from '@dxos/schema';
 import { createAsyncGenerator, type ValueGenerator } from '@dxos/schema/testing';
 import { range } from '@dxos/util';
 
-import { drawGraph, generateGraph } from './draw-util';
-
 const generator: ValueGenerator = faker as any;
 
 // TODO(burdon): Add objects to collections.
+// TODO(burdon): Create docs.
+// TODO(burdon): Create sketches.
+// TODO(burdon): Create sheets.
 // TODO(burdon): Create comments.
 // TODO(burdon): Reuse in testbench-app.
 // TODO(burdon): Mutator running in background (factor out): from echo-generator.
@@ -41,23 +32,15 @@ export type ObjectGenerator<T extends BaseObject> = (
   cb?: (objects: ReactiveObject<any>[]) => void,
 ) => Promise<ReactiveObject<T>[]>;
 
-// TODO(burdon): Factor out and create unit tests. See "fuzz" patterns.
 export const staticGenerators = new Map<string, ObjectGenerator<any>>([
-  //
-  // DocumentType
-  //
   [
     DocumentType.typename,
     async (space, n, cb) => {
       const objects = range(n).map(() => {
-        const content = range(faker.number.int({ min: 3, max: 8 }))
-          .map(() => faker.lorem.sentences(faker.number.int({ min: 3, max: 16 })))
-          .join('\n\n');
-
         const obj = space.db.add(
           create(DocumentType, {
             name: faker.commerce.productName(),
-            content: create(TextType, { content }),
+            content: create(TextType, { content: faker.lorem.sentences(5) }),
             threads: [],
           }),
         );
@@ -69,57 +52,36 @@ export const staticGenerators = new Map<string, ObjectGenerator<any>>([
       return objects;
     },
   ],
-  //
-  // DiagramType
-  //
   [
     DiagramType.typename,
     async (space, n, cb) => {
-      const options: Pick<TLEditorOptions, 'bindingUtils' | 'shapeUtils' | 'tools' | 'getContainer'> = {
-        bindingUtils: defaultBindingUtils,
-        shapeUtils: defaultShapeUtils,
-        tools: [...defaultTools, ...defaultShapeTools],
-        getContainer: () => document.body, // TODO(burdon): Fake via JSDOM?
-      };
+      const objects = range(n).map(() => {
+        // TODO(burdon): Generate diagram.
+        const obj = space.db.add(
+          create(DiagramType, {
+            name: faker.commerce.productName(),
+            canvas: create(CanvasType, { content: {} }),
+          }),
+        );
 
-      const objects = await Promise.all(
-        range(n).map(async () => {
-          const store = createTLStore();
-          const editor = new Editor({ ...options, store });
-          const graph = generateGraph();
-          const content = await drawGraph(editor, graph);
-          editor.dispose();
-          store.dispose();
-
-          const obj = space.db.add(
-            create(DiagramType, {
-              name: faker.commerce.productName(),
-              canvas: create(CanvasType, { schema: TLDRAW_SCHEMA, content }),
-            }),
-          );
-
-          return obj;
-        }),
-      );
+        return obj;
+      });
 
       cb?.(objects);
       return objects;
     },
   ],
-  //
-  // SheetType
-  //
+  // TODO(burdon): Create unit tests.
   [
     SheetType.typename,
     async (space, n, cb) => {
       const objects = range(n).map(() => {
-        // TODO(burdon): Reconcile with plugin-sheet/testing
+        const cells: Record<string, CellValue> = {};
         const year = new Date().getFullYear();
         const cols = 4;
-        const rows = 20;
-        const cells: Record<string, CellValue> = {};
+        const rows = 16;
         for (let col = 1; col <= cols; col++) {
-          for (let row = 1; row <= 10; row++) {
+          for (let row = 1; row <= rows; row++) {
             const cell = addressToA1Notation({ col, row });
             if (row === 1) {
               cells[cell] = { value: `${year} Q${col}` };
@@ -134,6 +96,7 @@ export const staticGenerators = new Map<string, ObjectGenerator<any>>([
         }
 
         // TODO(burdon): Set width.
+        // TODO(burdon): Set formatting for columns.
         return space.db.add(
           createSheet({
             name: faker.commerce.productName(),
@@ -160,7 +123,7 @@ export const createGenerator = <T extends BaseObject>(type: AbstractSchema<T>): 
       mutableSchema.find((schema) => schema.typename === type.typename) ?? space.db.schemaRegistry.addSchema(type);
 
     // Create objects.
-    const generate = createAsyncGenerator(generator, schema.schema, space.db);
+    const generate = createAsyncGenerator(generator, schema.schema, { db: space.db });
     const objects = await generate.createObjects(n);
 
     // Find or create table and view.
