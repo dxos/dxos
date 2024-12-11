8b2c7f31143604cd22c7a8f2e4a777e173489c3a
thure <will@braneframe.com>
2024-12-11 12:11:09 -0800

    Adjust remarks.

--- packages/ui/react-ui-kanban/src/defs/kanban.ts
+++ packages/ui/react-ui-kanban/src/defs/kanban.ts
@@ -14,11 +14,14 @@ export const KanbanSchema = S.Struct({
    */
   cardView: S.optional(ref(ViewType)),
   /**
-   * The field the values by which to pivot into columns of the kanban. This should be an enum field on the referred objects, can that be enforced?
+   * The field the values by which to pivot into columns of the kanban. This should be an enum field on the referred
+   * objects, can that be enforced?
    */
   columnField: S.optional(S.String),
   /**
-   * Order of columns by value and cards by id, derivative of the field selected by `columnPivotField` but can that be inferred here?
+   * Order of columns by value and cards by id, derivative of the field selected by `columnPivotField` but can that be
+   * inferred here? Or is this a preference that should apply first, then kanban should continue rendering what it
+   * finds regardless.
    */
   arrangement: S.optional(S.Array(S.Struct({ columnValue: S.String, ids: S.Array(ObjectId) }))),
   // TODO(burdon): Should not import from plugin. Either factor out type or use reverse deps when supported.
