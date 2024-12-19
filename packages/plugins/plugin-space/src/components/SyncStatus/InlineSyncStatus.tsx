c7d8636c05d66862237ae412230d2f2aac41d9ba
Josiah Witt <josiah@witt.life>
2024-12-19 17:11:27 -0500

    fix: quality of life improvements (#8431)

--- packages/plugins/plugin-space/src/components/SyncStatus/InlineSyncStatus.tsx
+++ packages/plugins/plugin-space/src/components/SyncStatus/InlineSyncStatus.tsx
@@ -2,15 +2,18 @@
 // Copyright 2024 DXOS.org
 //
 
-import React, { type CSSProperties, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { useGraph } from '@dxos/plugin-graph';
 import { QueryEdgeStatusResponse } from '@dxos/protocols/proto/dxos/client/services';
 import { EdgeReplicationSetting } from '@dxos/protocols/proto/dxos/echo/metadata';
 import { useClient } from '@dxos/react-client';
 import { type Space } from '@dxos/react-client/echo';
-import { Icon, useTranslation } from '@dxos/react-ui';
+import { Tooltip, useTranslation } from '@dxos/react-ui';
+import { AttentionGlyph, useAttended, useAttention } from '@dxos/react-ui-attention';
 
 import { useSpaceSyncState } from './sync-state';
+import { usePath } from '../../hooks';
 import { SPACE_PLUGIN } from '../../meta';
 
 const useEdgeStatus = (): QueryEdgeStatusResponse.EdgeStatus => {
@@ -26,36 +29,43 @@ const useEdgeStatus = (): QueryEdgeStatusResponse.EdgeStatus => {
   return status;
 };
 
-export const InlineSyncStatus = ({ space }: { space: Space }) => {
+export const InlineSyncStatus = ({ space, open }: { space: Space; open?: boolean }) => {
+  const { t } = useTranslation(SPACE_PLUGIN);
+  const id = space.id;
+  const { hasAttention, isAncestor, isRelated } = useAttention(id);
+  const isAttended = hasAttention || isAncestor || isRelated;
+
+  // TODO(wittjosiah): If the attended node is deep in the graph and the graph is not fully loaded
+  //   this will result in an empty path until the graph is connected.
+  // TODO(wittjosiah): Consider using this indicator for all open nodes instead of just attended.
+  const { graph } = useGraph();
+  const attended = useAttended();
+  const startOfAttention = attended.at(-1);
+  const path = usePath(graph, startOfAttention);
+  const containsAttended = !open && !isAttended && id && path ? path.includes(id) : false;
+
   const connectedToEdge = useEdgeStatus() === QueryEdgeStatusResponse.EdgeStatus.CONNECTED;
   // TODO(wittjosiah): This is not reactive.
   const edgeSyncEnabled = space.internal.data.edgeReplication === EdgeReplicationSetting.ENABLED;
   const syncState = useSpaceSyncState(space);
-  if (!connectedToEdge || !edgeSyncEnabled || !syncState || syncState.missingOnLocal === 0) {
-    return null;
-  }
-
-  return <InlineSyncStatusIndicator />;
-};
-
-export const InlineSyncStatusIndicator = () => {
-  const { t } = useTranslation(SPACE_PLUGIN);
-  const animationProps = useMemo<CSSProperties>(
-    () => ({
-      // Synchronize animations.
-      animationDelay: `-${Date.now() % 2_000}ms`,
-    }),
-    [],
-  );
+  const syncing = connectedToEdge && edgeSyncEnabled && syncState && syncState.missingOnLocal > 0;
 
   return (
-    <div role='status' aria-label={t('syncing message')} className='flex items-center'>
-      <Icon
-        icon='ph--circle-notch--regular'
-        size={3}
-        style={animationProps}
-        classNames='text-subdued animate-[spin_2s_linear_infinite]'
-      />
-    </div>
+    <Tooltip.Root>
+      <Tooltip.Trigger asChild>
+        <AttentionGlyph
+          syncing={syncing}
+          attended={isAttended}
+          containsAttended={containsAttended}
+          classNames='self-center mie-1'
+        />
+      </Tooltip.Trigger>
+      <Tooltip.Portal>
+        <Tooltip.Content side='bottom' classNames='z-[70]'>
+          <span>{t('syncing label')}</span>
+          <Tooltip.Arrow />
+        </Tooltip.Content>
+      </Tooltip.Portal>
+    </Tooltip.Root>
   );
 };
--- packages/ui/react-ui-attention/src/components/AttentionGlyph.stories.tsx
+++ packages/ui/react-ui-attention/src/components/AttentionGlyph.stories.tsx
@@ -4,32 +4,25 @@
 
 import '@dxos-theme';
 
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
+import { Button } from '@dxos/react-ui';
 import { withTheme } from '@dxos/storybook-utils';
+import { range } from '@dxos/util';
 
-import { AttentionGlyph } from './AttentionGlyph';
+import { AttentionGlyph, type AttentionGlyphProps } from './AttentionGlyph';
 
-type StoryProps = {
-  current?: boolean;
-  attention?: boolean;
-};
-
-const Story = ({ current, attention }: StoryProps) => {
-  const itemAttrs = {
-    ...(current && { 'aria-current': 'page' as const }),
-    ...(attention && { 'data-attention': 'true' }),
-  };
+const Story = (props: AttentionGlyphProps) => {
   return (
     <ul className='flex gap-2 mbe-2'>
-      <li {...itemAttrs}>
-        <AttentionGlyph presence='none' />
+      <li>
+        <AttentionGlyph presence='none' {...props} />
       </li>
-      <li {...itemAttrs}>
-        <AttentionGlyph presence='one' />
+      <li>
+        <AttentionGlyph presence='one' {...props} />
       </li>
-      <li {...itemAttrs}>
-        <AttentionGlyph presence='many' />
+      <li>
+        <AttentionGlyph presence='many' {...props} />
       </li>
     </ul>
   );
@@ -46,10 +39,49 @@ export const Default = {
   args: {},
 };
 
-export const Current = {
-  args: { current: true },
+export const Attention = {
+  args: { attended: true },
 };
 
-export const Attention = {
-  args: { current: true, attention: true },
+export const Contains = {
+  args: { containsAttended: true },
+};
+
+export const Syncing = {
+  render: () => {
+    const [spaces, setSpaces] = useState(
+      new Map<string, boolean>(range(8).map((i) => [`space-${i + 1}`, Math.random() > 0.5])),
+    );
+    const [attended, setAttended] = useState(0);
+
+    const handleChangeAttended = useCallback(() => {
+      setAttended((attended) => (attended + 1) % 3);
+    }, []);
+
+    useEffect(() => {
+      const t = setInterval(
+        () => {
+          setSpaces((spaces) => {
+            const space = Array.from(spaces.keys())[Math.floor(Math.random() * spaces.size)];
+            spaces.set(space, !spaces.get(space));
+            return new Map(spaces);
+          });
+        },
+        2_000 + Math.random() * 3_000,
+      );
+      return () => clearInterval(t);
+    });
+
+    return (
+      <div className='flex flex-col p-2 w-[200px]'>
+        <Button onClick={handleChangeAttended}>Change attended</Button>
+        {Array.from(spaces.entries()).map(([space, sync]) => (
+          <div key={space} className='flex items-center'>
+            <div className='grow'>{space}</div>
+            {sync && <AttentionGlyph syncing attended={attended === 1} containsAttended={attended === 2} />}
+          </div>
+        ))}
+      </div>
+    );
+  },
 };
