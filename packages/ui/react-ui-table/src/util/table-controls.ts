393f5464e73ab390f7972c6cba2f7a6484628b4b
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2024-12-20 15:56:57 -0800

    Update packages/ui/react-ui-table/src/util/table-controls.ts
    Co-authored-by: Will Shown <thure@users.noreply.github.com>

--- packages/ui/react-ui-table/src/util/table-controls.ts
+++ packages/ui/react-ui-table/src/util/table-controls.ts
@@ -23,8 +23,8 @@ export const CONTROL_IDENTIFIERS = {
 } as const;
 
 const BASE_CLASSES = {
-  checkbox: 'absolute inset-block-[6px] inline-end-[6px] ch-checkbox',
-  switch: 'absolute inset-block-[4px] inline-end-[4px] ch-checkbox--switch',
+  checkbox: 'absolute inset-block-[.375rem] inline-end-[.375rem] ch-checkbox',
+  switch: 'absolute inset-block-[.25rem] inline-end-[.25rem] ch-checkbox--switch',
 } as const;
 
 const renderAttributes = (data: Record<string, string>) => {
