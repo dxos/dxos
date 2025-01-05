3b0f42f88cc2740cd1e0252ce4f897ab166bc5c4
Dmytro Maretskyi <maretskii@gmail.com>
2025-01-05 18:13:17 +0700

    Comment

--- packages/experimental/assistant/src/cypher/parjs.ts
+++ packages/experimental/assistant/src/cypher/parjs.ts
@@ -1,3 +1,8 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+// Workaround for https://github.com/vitest-dev/vitest/issues/7174.
 import { parjsCombinators, parjs } from '#parjs';
 
 import type { Parjser } from 'parjs';
