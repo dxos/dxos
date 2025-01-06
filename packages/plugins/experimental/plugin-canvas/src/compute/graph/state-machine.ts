b092f1748b8865d9cc4965fd4128cd05e4f0599f
Dmytro Maretskyi <maretskii@gmail.com>
2025-01-06 12:15:55 +0700

    Update StateMachine to enable autoRun by default
    - Changed the _autoRun property in the StateMachine class from false to true, altering its default behavior.
    - This adjustment may impact the execution flow of state machine operations, ensuring they run automatically upon initialization.

--- packages/plugins/experimental/plugin-canvas/src/compute/graph/state-machine.ts
+++ packages/plugins/experimental/plugin-canvas/src/compute/graph/state-machine.ts
@@ -39,7 +39,7 @@ export type StateMachineContext = {
 // TODO(burdon): Move to compute (without hyperformula dependency). Maps onto hyperformula as client runtime?
 // TODO(burdon): Extend Resource.
 export class StateMachine extends Resource {
-  private _autoRun = false;
+  private _autoRun = true;
   // TODO(dmaretskyi): Will be replaced by a sync function that does propagation.
   private _activeTasks: Promise<void>[] = [];
 
