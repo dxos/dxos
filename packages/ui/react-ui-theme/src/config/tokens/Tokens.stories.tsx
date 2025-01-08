624ebd8045e343ac25b35a9e941b851eff814125
thure <will@braneframe.com>
2025-01-07 17:57:40 -0800

    Use new token audit.

--- packages/ui/react-ui-theme/src/config/tokens/Tokens.stories.tsx
+++ packages/ui/react-ui-theme/src/config/tokens/Tokens.stories.tsx
@@ -3,76 +3,55 @@
 //
 
 import '@dxos-theme';
-import { facetSemanticValues, type Facet, resolveNaming, seriesValues, renderHelicalArcTokens } from '@ch-ui/tokens';
+import { auditFacet, type HelicalArcSeries, type TokenAudit, parseAlphaLuminosity } from '@ch-ui/tokens';
 import { type Meta } from '@storybook/react';
 import React from 'react';
 
 import { tokenSet } from './index';
 
-const semanticValues = facetSemanticValues((tokenSet.colors as Facet).semantic);
-const mainCondition = 'p3';
+const colorAudit = auditFacet(tokenSet.colors, { condition: 'p3' });
 
-const physicalSwatches = Object.entries(tokenSet.colors.physical.series)
-  .map(([seriesId, { [mainCondition]: series }]) => {
-    const resolvedNaming = resolveNaming(series?.naming as any);
-    const values = seriesValues(series!, Array.from(semanticValues?.[seriesId] ?? []));
-    return [
-      seriesId,
-      renderHelicalArcTokens({
-        seriesId,
-        conditionId: mainCondition,
-        series: series as any,
-        namespace: tokenSet.colors.physical.namespace,
-        resolvedNaming,
-        values,
-      })
-        .map((declaration) => declaration.split(':')[0])
-        .map((variable) => {
-          const name = variable.substring(tokenSet.colors.physical.namespace!.length + 2);
-          const results = name.split(/-|\\\/\\?/);
-          const [seriesId, shadeValue, opacity] = results;
-          return {
-            seriesId,
-            shadeValue: parseFloat(shadeValue),
-            opacity: typeof opacity !== 'undefined' ? parseFloat(opacity) : 1,
-            name,
-            variable,
-          } satisfies SwatchProps;
-        })
-        .sort((a, b) => {
-          return a.shadeValue - b.shadeValue - (a.opacity - b.opacity);
-        }),
-    ];
-  })
-  .reverse() as [string, SwatchProps[]][];
-
-type SwatchProps = { variable: string; seriesId: string; shadeValue: number; opacity: number; name: string };
-
-const Swatch = ({ variable, seriesId, shadeValue, opacity, name }: SwatchProps) => {
+const Swatch = ({ variableName, value, semantic, physical }: TokenAudit<HelicalArcSeries>) => {
+  const [luminosity, alpha] = parseAlphaLuminosity(value);
   return (
-    <div className='shrink-0 is-16'>
-      <dd className='aspect-square' style={{ background: `var(${variable})` }}></dd>
+    <div className='shrink-0 is-32'>
+      <dd className='aspect-video' style={{ background: `var(${variableName})` }}></dd>
       <dt className='text-xs'>
-        <p>{shadeValue}</p>
-        {opacity < 1 && <p>/ {opacity}</p>}
+        <p>
+          {luminosity}
+          {typeof alpha !== 'undefined' && ` / ${alpha}`}
+        </p>
+        {physical.includes('values') && <p>values</p>}
+        {physical.includes('naming') && <p>naming</p>}
+        {semantic.length > 0 && (
+          <ul>
+            {semantic.map(({ sememeName, conditionId }) => {
+              const sememeCondition = `${sememeName} / ${conditionId}`;
+              return <li key={sememeCondition}>{sememeCondition}</li>;
+            })}
+          </ul>
+        )}
       </dt>
     </div>
   );
 };
 
 export const Audit = () => {
+  if (typeof colorAudit === 'string') {
+    return null;
+  }
   return (
     <>
       <h1>Semantic tokens</h1>
       {}
       <h1>Physical tokens</h1>
-      {physicalSwatches.map(([seriesId, swatches]) => {
+      {Object.entries(colorAudit).map(([seriesId, audits]) => {
         return (
           <>
             <h2 className='mbs-4 mbe-2'>{seriesId}</h2>
             <dl className='flex flex-wrap'>
-              {swatches.map((swatch) => (
-                <Swatch key={swatch.variable} {...swatch} />
+              {audits.map((audit) => (
+                <Swatch key={audit.variableName} {...audit} />
               ))}
             </dl>
           </>
