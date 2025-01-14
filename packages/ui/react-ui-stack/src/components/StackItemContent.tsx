bba6d95aeedbe76eb00c1ae6b685d88ffe180b74
thure <will@braneframe.com>
2025-01-13 16:25:38 -0800

    Add copious explanation.

--- packages/ui/react-ui-stack/src/components/StackItemContent.tsx
+++ packages/ui/react-ui-stack/src/components/StackItemContent.tsx
@@ -10,13 +10,28 @@ import { mx } from '@dxos/react-ui-theme';
 import { useStack } from './StackContext';
 
 export type StackItemContentProps = ThemedClassName<ComponentPropsWithoutRef<'div'>> & {
+  /**
+   * This flag is required in order to clarify a developer experience that seemed like it needed extra boilerplate
+   * (`row-span-2`) or was buggy. See the description of the StackItem.Content component itself for more information.
+   */
   toolbar: boolean;
+  /**
+   * Whether to provide for the layout of a statusbar after the content.
+   */
   statusbar?: boolean;
+  /**
+   * Whether to set a certain aspect ratio on the content, including the toolbar and statusbar. This is provided for
+   * convenience and consistency; it can instead be specified by the `classNames` or `style` props as needed.
+   */
   size?: 'intrinsic' | 'video' | 'square';
 };
 
+/**
+ * This component should be used by plugins for rendering content within a stack item, a.k.a. a “plank” or “section”.
+ * The `toolbar` flag must be provided since this component provides for the layout of content with the toolbar.
+ */
 export const StackItemContent = forwardRef<HTMLDivElement, StackItemContentProps>(
-  ({ children, toolbar = true, statusbar, classNames, size = 'intrinsic', ...props }, forwardedRef) => {
+  ({ children, toolbar, statusbar, classNames, size = 'intrinsic', ...props }, forwardedRef) => {
     const { size: stackItemSize } = useStack();
 
     return (
