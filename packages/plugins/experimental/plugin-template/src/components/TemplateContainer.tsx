dd6f140045420d6f2c71cb9e58f3fb7c9154c883
thure <will@braneframe.com>
2025-01-13 16:57:46 -0800

    Fix TemplateContainer.

--- packages/plugins/experimental/plugin-template/src/components/TemplateContainer.tsx
+++ packages/plugins/experimental/plugin-template/src/components/TemplateContainer.tsx
@@ -4,15 +4,11 @@
 
 import React from 'react';
 
-import { PublicKey } from '@dxos/react-client';
-import { type ReactiveEchoObject, getSpace } from '@dxos/react-client/echo';
+import { type ReactiveEchoObject, fullyQualifiedId } from '@dxos/react-client/echo';
 import { StackItem } from '@dxos/react-ui-stack';
 
 export const TemplateContainer = ({ object }: { object: ReactiveEchoObject<any>; role: string }) => {
-  const space = getSpace(object);
-
   return (
-    // TODO(burdon): Boilerplate.
     <StackItem.Content toolbar={false}>
       <pre className='m-4 p-2 ring'>
         <span>{fullyQualifiedId(object)}</span>
