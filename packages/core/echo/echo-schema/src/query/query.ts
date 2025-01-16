042a0b6aab433d35d9ec7c3af547785c6277708c
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-01-15 16:54:02 -0800

    feat(tables): Persist sort in view query (#8483)

--- packages/core/echo/echo-schema/src/query/query.ts
+++ packages/core/echo/echo-schema/src/query/query.ts
@@ -4,6 +4,18 @@
 
 import { Schema as S } from '@effect/schema';
 
+// TODO(ZaymonFC): Where should this live?
+const SortDirection = S.Union(S.Literal('asc'), S.Literal('desc'));
+export type SortDirectionType = S.Schema.Type<typeof SortDirection>;
+
+// TODO(ZaymonFC): Struct vs pair?
+const FieldSort = S.Struct({
+  fieldId: S.String,
+  direction: SortDirection,
+}).pipe(S.mutable);
+
+export interface FieldSortType extends S.Schema.Type<typeof FieldSort> {}
+
 /**
  * ECHO query object.
  */
@@ -11,6 +23,7 @@ const QuerySchema = S.Struct({
   // TODO(burdon): Rename to typename.
   // typename: S.String,
   type: S.String,
+  sort: S.optional(S.Array(FieldSort)),
 }).pipe(S.mutable);
 
 export interface QueryType extends S.Schema.Type<typeof QuerySchema> {}
