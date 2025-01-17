f2b3c7169b59ad91d370945bd132de7ddd34d21e
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-01-17 14:21:03 +0200

    Delete unused types

--- packages/plugins/experimental/plugin-calls/src/components/types.ts
+++ packages/plugins/experimental/plugin-calls/src/components/types.ts
@@ -3,51 +3,8 @@
 //
 
 import { schema } from '@dxos/protocols/proto';
-import { type RoomState, type UserState } from '@dxos/protocols/proto/dxos/edge/calls';
 
 export const codec = schema.getCodecForType('dxos.edge.calls.UserState');
-
-export type ServerMessage =
-  | {
-      type: 'roomState';
-      state: RoomState;
-    }
-  | {
-      type: 'error';
-      error?: string;
-    }
-  | {
-      type: 'directMessage';
-      from: string;
-      message: string;
-    }
-  | {
-      type: 'muteMic';
-    }
-  | {
-      type: 'partyserver-pong';
-    };
-
-export type ClientMessage =
-  | {
-      type: 'userUpdate';
-      user: UserState;
-    }
-  | {
-      type: 'directMessage';
-      to: string;
-      message: string;
-    }
-  | {
-      type: 'muteUser';
-      id: string;
-    }
-  | {
-      type: 'userLeft';
-    }
-  | {
-      type: 'partyserver-ping';
-    };
 
 export interface SessionDescription {
   type: 'offer' | 'answer';
