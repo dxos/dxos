c58ad2dadb1808ed190f36324c0a7cf5b0201f00
Josiah Witt <josiah@witt.life>
2025-01-19 15:18:25 -0500

    feat: publish scripts to github as gists

--- packages/plugins/experimental/plugin-script/src/components/ScriptSettingsPanel.tsx
+++ packages/plugins/experimental/plugin-script/src/components/ScriptSettingsPanel.tsx
@@ -2,14 +2,20 @@
 // Copyright 2024 DXOS.org
 //
 
-import React, { type ChangeEvent, useCallback } from 'react';
+import { Octokit } from '@octokit/core';
+import React, { type ChangeEvent, useCallback, useState } from 'react';
 
+import { createIntent, SettingsAction, useIntentDispatcher } from '@dxos/app-framework';
 import { FunctionType, type ScriptType, getInvocationUrl, getUserFunctionUrlInMetadata } from '@dxos/functions';
 import { useClient } from '@dxos/react-client';
 import { Filter, getMeta, getSpace, useQuery } from '@dxos/react-client/echo';
 import { Button, Icon, Input, Separator, useControlledValue, useTranslation } from '@dxos/react-ui';
+import { AccessTokenType } from '@dxos/schema';
 
 import { SCRIPT_PLUGIN } from '../meta';
+
+// From https://stackoverflow.com/a/67243723/2804332
+const kebabize = (str: string) => str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase());
 
 export type ScriptSettingsPanelProps = {
   script: ScriptType;
@@ -17,9 +23,11 @@ export type ScriptSettingsPanelProps = {
 
 export const ScriptSettingsPanel = ({ script }: ScriptSettingsPanelProps) => {
   const { t } = useTranslation(SCRIPT_PLUGIN);
+  const { dispatchPromise: dispatch } = useIntentDispatcher();
   const client = useClient();
   const space = getSpace(script);
   const [fn] = useQuery(space, Filter.schema(FunctionType, { source: script }));
+  const [githubToken] = useQuery(space, Filter.schema(AccessTokenType, { source: 'github.com' }));
 
   const [binding, setBinding] = useControlledValue(fn?.binding ?? '');
   const handleBindingChange = useCallback(
@@ -42,42 +50,100 @@ export const ScriptSettingsPanel = ({ script }: ScriptSettingsPanelProps) => {
   // TODO(wittjosiah): Use ClipboardProvider.
   const handleCopy = useCallback(() => functionUrl && navigator.clipboard.writeText(functionUrl), [functionUrl]);
 
+  const handleOpenTokenManager = useCallback(
+    () => dispatch(createIntent(SettingsAction.Open, { plugin: 'dxos.org/plugin/token-manager' })),
+    [],
+  );
+
+  const [publishing, setPublishing] = useState(false);
+  const handlePublish = useCallback(async () => {
+    setPublishing(true);
+    const octokit = new Octokit({ auth: githubToken.token });
+    const source = await script.source.load();
+    const filename = script.name ? kebabize(script.name) : 'composer-script';
+    const files = { [`${filename}.ts`]: { content: source.content } };
+
+    try {
+      const meta = getMeta(script);
+      const githubKey = meta.keys.find(({ source }) => source === 'github.com');
+      const gistId = githubKey?.id;
+      if (gistId) {
+        await octokit.request('PATCH /gists/{gist_id}', {
+          gist_id: gistId,
+          files,
+        });
+      } else {
+        const response = await octokit.request('POST /gists', {
+          public: true,
+          files,
+        });
+        if (response.data.id) {
+          meta.keys.push({ source: 'github.com', id: response.data.id });
+        }
+      }
+    } finally {
+      setPublishing(false);
+    }
+  }, [script, githubToken]);
+
   return (
-    fn && (
-      <>
-        <Separator />
-        <div role='form' className='flex flex-col w-full p-2 gap-4'>
-          <h2>{t('remote function settings heading')}</h2>
-          <Input.Root>
-            <div role='none' className='flex flex-col gap-1'>
-              <Input.Label>{t('function url label')}</Input.Label>
-              <div role='none' className='flex gap-1'>
-                <Input.TextInput
-                  disabled
-                  value={functionUrl}
-                  onChange={(event) => {
-                    fn.name = event.target.value;
-                  }}
-                />
-                <Button onClick={handleCopy}>
-                  <Icon icon='ph--copy--regular' size={4} />
-                </Button>
+    <>
+      {fn && (
+        <>
+          <Separator />
+          <div role='form' className='flex flex-col w-full p-2 gap-4'>
+            <h2>{t('remote function settings heading')}</h2>
+            <Input.Root>
+              <div role='none' className='flex flex-col gap-1'>
+                <Input.Label>{t('function url label')}</Input.Label>
+                <div role='none' className='flex gap-1'>
+                  <Input.TextInput
+                    disabled
+                    value={functionUrl}
+                    onChange={(event) => {
+                      fn.name = event.target.value;
+                    }}
+                  />
+                  <Button onClick={handleCopy}>
+                    <Icon icon='ph--copy--regular' size={4} />
+                  </Button>
+                </div>
               </div>
-            </div>
-          </Input.Root>
-          <Input.Root>
-            <div role='none' className='flex flex-col gap-1'>
-              <Input.Label>{t('function binding label')}</Input.Label>
-              <Input.TextInput
-                placeholder={t('function binding placeholder')}
-                value={binding}
-                onChange={handleBindingChange}
-                onBlur={handleBindingBlur}
-              />
-            </div>
-          </Input.Root>
+            </Input.Root>
+            <Input.Root>
+              <div role='none' className='flex flex-col gap-1'>
+                <Input.Label>{t('function binding label')}</Input.Label>
+                <Input.TextInput
+                  placeholder={t('function binding placeholder')}
+                  value={binding}
+                  onChange={handleBindingChange}
+                  onBlur={handleBindingBlur}
+                />
+              </div>
+            </Input.Root>
+          </div>
+        </>
+      )}
+      <Separator />
+      <div className='flex flex-col w-full p-2 gap-4'>
+        <div>
+          <h2>{t('script publish settings label')}</h2>
+          <p className='text-description text-sm'>{t('script publish settings description')}</p>
         </div>
-      </>
-    )
+        {!githubToken && (
+          <div className='flex flex-col items-center justify-center gap-2'>
+            <span>{t('no github token label')}</span>
+            <Button onClick={handleOpenTokenManager}>{t('open token manager label')}</Button>
+          </div>
+        )}
+        {githubToken && (
+          <div className='flex justify-end'>
+            <Button disabled={publishing} onClick={handlePublish}>
+              {t(publishing ? 'publishing label' : 'publish label')}
+            </Button>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
