1332456cce19cd30a8717fcc2ff6f29d9065a0ce
Will Shown <thure@users.noreply.github.com>
2025-01-20 15:03:23 -0800

    refactor: Use `react-ui-menu` for EditorToolbar (#8487)

--- packages/ui/react-ui-menu/src/defs.ts
+++ packages/ui/react-ui-menu/src/defs.ts
@@ -1,8 +1,8 @@
 //
 // Copyright 2025 DXOS.org
 //
-import { type Node, type Action } from '@dxos/app-graph';
-import { type Label } from '@dxos/react-ui';
+import { type Node, type Action, type ActionGroup, ACTION_GROUP_TYPE } from '@dxos/app-graph';
+import { type ToolbarSeparatorProps, type Label, type IconButtonProps } from '@dxos/react-ui';
 
 export type MenuActionProperties = {
   label: Label;
@@ -17,7 +17,29 @@ export type MenuActionProperties = {
 
 export type MenuAction = Action<MenuActionProperties>;
 
-export type MenuProps<A extends Node = Action<MenuActionProperties>, P extends Node = A> = {
-  actions?: A[];
-  onAction?: (action: P) => void;
+export type MenuActionHandler<A extends Node = MenuAction> = (action: A) => void;
+
+export const MenuSeparatorType = '@dxos/react-ui-toolbar/separator' as const;
+
+export type MenuSeparator = Node<never, Pick<ToolbarSeparatorProps, 'variant'>> & {
+  type: typeof MenuSeparatorType;
+};
+
+export const isSeparator = (node: Node): node is MenuSeparator => node.type === MenuSeparatorType;
+
+export type MenuSingleSelectActionGroup = { selectCardinality: 'single'; value: string };
+export type MenuMultipleSelectActionGroup = { selectCardinality: 'multiple'; value: string[] };
+
+export type MenuItemGroup<P extends Record<string, any> = Record<string, any>> = ActionGroup<P>;
+
+export const isMenuGroup = (node: Node): node is MenuItemGroup => node.type === ACTION_GROUP_TYPE;
+
+export type MenuItem = MenuSeparator | MenuAction | MenuItemGroup;
+
+export type MenuItemsResolver = (group?: MenuItemGroup) => MenuItem[] | null;
+
+export type MenuContextValue<A extends Node = MenuAction> = {
+  resolveGroupItems: MenuItemsResolver;
+  iconSize: IconButtonProps['size'];
+  onAction: MenuActionHandler<A>;
 };
--- packages/ui/react-ui-theme/src/styles/components/toolbar.ts
+++ packages/ui/react-ui-theme/src/styles/components/toolbar.ts
@@ -9,7 +9,7 @@ import { mx } from '../../util';
 export type ToolbarStyleProps = Partial<{}>;
 
 export const toolbarRoot: ComponentFunction<ToolbarStyleProps> = (_props, ...etc) => {
-  return mx('flex shrink-0 items-center gap-2 is-full', ...etc);
+  return mx('flex shrink-0 items-center gap-2 p-1 is-full overflow-x-auto overflow-y-hidden contain-layout', ...etc);
 };
 
 export const toolbarTheme: Theme<ToolbarStyleProps> = {
