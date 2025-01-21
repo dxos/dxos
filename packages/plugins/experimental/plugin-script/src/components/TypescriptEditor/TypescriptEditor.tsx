adf5fca767a0787adc71c1841c2e9ed4aef8362b
Josiah Witt <josiah@witt.life>
2025-01-21 12:51:20 -0500

    feat(plugin-script): give type tooltips reasonable styles

--- packages/plugins/experimental/plugin-script/src/components/TypescriptEditor/TypescriptEditor.tsx
+++ packages/plugins/experimental/plugin-script/src/components/TypescriptEditor/TypescriptEditor.tsx
@@ -3,12 +3,17 @@
 //
 
 import { javascript } from '@codemirror/lang-javascript';
+import { defaultHighlightStyle } from '@codemirror/language';
+import { Prec } from '@codemirror/state';
+import { oneDarkHighlightStyle } from '@codemirror/theme-one-dark';
+import { keymap } from '@codemirror/view';
+import { tags } from '@lezer/highlight';
 import { type VirtualTypeScriptEnvironment } from '@typescript/vfs';
-import { tsSync, tsFacet, tsLinter, tsAutocomplete, tsHover } from '@valtown/codemirror-ts';
 import { continueKeymap } from '@valtown/codemirror-continue';
+import { tsSync, tsFacet, tsLinter, tsAutocomplete, tsHover, type HoverInfo } from '@valtown/codemirror-ts';
 import React from 'react';
 
-import { useThemeContext } from '@dxos/react-ui';
+import { type ThemeMode, useThemeContext } from '@dxos/react-ui';
 import {
   autocomplete,
   createBasicExtensions,
@@ -80,7 +85,14 @@ export const TypescriptEditor = ({
           javascript({ typescript: true }),
           // https://github.com/val-town/codemirror-ts
           autocomplete({ override: env ? [tsAutocomplete()] : [] }),
-          env ? [tsFacet.of({ env, path: `/src/${id}.ts` }), tsSync(), tsLinter(), tsHover()] : [],
+          env
+            ? [
+                tsFacet.of({ env, path: `/src/${id}.ts` }),
+                tsSync(),
+                tsLinter(),
+                tsHover({ renderTooltip: createTooltipRenderer(themeMode) }),
+              ]
+            : [],
           InputModeExtensions.vscode,
         ],
       ].filter(nonNullable),
@@ -93,6 +105,41 @@ export const TypescriptEditor = ({
   return (
     <div ref={parentRef} data-toolbar={toolbar ? 'enabled' : 'disabled'} className={className} {...focusAttributes} />
   );
+};
+
+const createTooltipRenderer = (themeMode: ThemeMode) => {
+  const theme = themeMode === 'dark' ? oneDarkHighlightStyle : defaultHighlightStyle;
+
+  const classFromKind = (_kind: string) => {
+    // E.g., localName, methodName, parameterName, etc.
+    const kind = _kind.endsWith('Name') ? 'name' : _kind;
+    const validTag = kind in tags;
+    if (!validTag) {
+      return '';
+    }
+
+    const tag = tags[kind as keyof typeof tags];
+    if (typeof tag === 'function') {
+      return '';
+    }
+
+    return theme.style([tag]) ?? '';
+  };
+
+  return (info: HoverInfo) => {
+    const div = document.createElement('div');
+    div.className = 'p-1 rounded border border-separator bg-base xs:max-w-80 max-w-lg';
+
+    if (info.quickInfo?.displayParts) {
+      for (const part of info.quickInfo.displayParts) {
+        const span = div.appendChild(document.createElement('span'));
+        span.className = classFromKind(part.kind);
+        span.innerText = part.text;
+      }
+    }
+
+    return { dom: div };
+  };
 };
 
 export default TypescriptEditor;
