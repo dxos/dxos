706dccbd08f116d69219c0bac81514db3413173d
Josiah Witt <josiah@witt.life>
2025-01-22 15:40:21 -0500

    update bundler to understand npm prefix

--- packages/plugins/experimental/plugin-script/src/bundler/bundler.ts
+++ packages/plugins/experimental/plugin-script/src/bundler/bundler.ts
@@ -114,7 +114,7 @@ export class Bundler {
               });
             },
           },
-          httpPlugin,
+          npmPlugin,
         ],
       });
 
@@ -222,23 +222,27 @@ const analyzeSourceFileImports = (code: string): ParsedImport[] => {
   });
 };
 
-const httpPlugin: Plugin = {
-  name: 'http',
+const npmPlugin: Plugin = {
+  name: 'npm',
   setup: (build) => {
-    // Intercept import paths starting with "http:" and "https:" so esbuild doesn't attempt to map them to a file system location.
-    // Tag them with the "http-url" namespace to associate them with this plugin.
-    build.onResolve({ filter: /^https?:\/\// }, (args) => ({
-      path: args.path,
-      namespace: 'http-url',
-    }));
+    // Intercept import paths starting with "npm:" so esbuild doesn't attempt to map them to a file system location.
+    // Map npm module to an esm.sh url.
+    build.onResolve({ filter: /^npm:/ }, (args) => {
+      return {
+        path: new URL(args.path.slice(4), 'https://esm.sh/').toString(),
+        namespace: 'http-url',
+      };
+    });
 
     // We also want to intercept all import paths inside downloaded files and resolve them against the original URL.
     // All of these files will be in the "http-url" namespace.
     // Make sure to keep the newly resolved URL in the "http-url" namespace so imports inside it will also be resolved as URLs recursively.
-    build.onResolve({ filter: /.*/, namespace: 'http-url' }, (args) => ({
-      path: new URL(args.path, args.importer).toString(),
-      namespace: 'http-url',
-    }));
+    build.onResolve({ filter: /.*/, namespace: 'http-url' }, (args) => {
+      return {
+        path: new URL(args.path, args.importer).toString(),
+        namespace: 'http-url',
+      };
+    });
 
     // When a URL is loaded, we want to actually download the content from the internet.
     // This has just enough logic to be able to handle the example import from unpkg.com but in reality this would probably need to be more complex.
