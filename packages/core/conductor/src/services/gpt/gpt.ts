cde5a39c3f49d0360c849d44eb1f4ce72202bcc9
Rich Burdon <rich.burdon@gmail.com>
2025-01-23 10:00:54 +0100

    factored out test utils

---
+++ packages/core/conductor/src/services/gpt/gpt.ts
@@ -0,0 +1,33 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { Context } from 'effect';
+
+import { ImageSource, type AIServiceClient } from '@dxos/assistant';
+import { ECHO_ATTR_TYPE, S } from '@dxos/echo-schema';
+
+import { type GptInput, type GptOutput } from '../../nodes';
+import type { ComputeEffect, ValueBag } from '../../types';
+
+export const IMAGE_TYPENAME = 'example.org/type/Image';
+
+export const MESSAGE_TYPENAME = 'example.org/type/Message';
+
+export const Image = S.Struct({
+  id: S.String,
+  prompt: S.String,
+  source: ImageSource,
+});
+
+export type Image = S.Schema.Type<typeof Image>;
+
+export const isImage = (value: any): value is Image => value?.[ECHO_ATTR_TYPE] === IMAGE_TYPENAME;
+
+export class GptService extends Context.Tag('GptService')<
+  GptService,
+  {
+    readonly invoke: (input: ValueBag<GptInput>) => ComputeEffect<ValueBag<GptOutput>>;
+    readonly getAiServiceClient?: () => AIServiceClient;
+  }
+>() {}
