9b01eb4704b927fe0827092fe8f17f29470e7984
Josiah Witt <josiah@witt.life>
2025-01-23 18:47:00 -0500

    allow import w/o access token

--- packages/plugins/experimental/plugin-script/src/capabilities/intent-resolver.ts
+++ packages/plugins/experimental/plugin-script/src/capabilities/intent-resolver.ts
@@ -5,10 +5,9 @@
 import { Octokit } from '@octokit/core';
 
 import { contributes, Capabilities, createResolver } from '@dxos/app-framework';
-import { Filter } from '@dxos/client/echo';
 import { ScriptType } from '@dxos/functions';
 import { create, makeRef } from '@dxos/live-object';
-import { AccessTokenType, TextType } from '@dxos/schema';
+import { TextType } from '@dxos/schema';
 
 import { templates } from '../templates';
 import { ScriptAction } from '../types';
@@ -20,19 +19,15 @@ export default () =>
       let content = templates[0].source;
       const gistId = gistUrl?.split('/').at(-1);
       if (gistId) {
-        const {
-          objects: [githubToken],
-        } = await space.db.query(Filter.schema(AccessTokenType, { source: 'github.com' })).run();
-        if (githubToken) {
-          // TODO(wittjosiah): Capability which contributes Octokit?
-          const octokit = new Octokit({ auth: githubToken.token });
-          const response = await octokit.request('GET /gists/{gist_id}', {
-            gist_id: gistId,
-          });
-          const gistContent = Object.values(response.data.files ?? {})[0]?.content;
-          if (gistContent) {
-            content = gistContent;
-          }
+        // TODO(wittjosiah): Capability which contributes Octokit?
+        // NOTE: AccessToken not needed to read public gists.
+        const octokit = new Octokit();
+        const response = await octokit.request('GET /gists/{gist_id}', {
+          gist_id: gistId,
+        });
+        const gistContent = Object.values(response.data.files ?? {})[0]?.content;
+        if (gistContent) {
+          content = gistContent;
         }
       }
 
