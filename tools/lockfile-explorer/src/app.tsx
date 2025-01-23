5b9693f242551c85d740ba8b0e43b4da93599696
Dmytro Maretskyi <maretskii@gmail.com>
2025-01-23 17:53:46 +0100

    Lockfile explorer

---
+++ tools/lockfile-explorer/src/app.tsx
@@ -0,0 +1,326 @@
+import React, { useState, useEffect, useRef, type ReactNode } from 'react';
+import { render, Text, Box, useStdout, useInput, Transform, measureElement, Spacer, Newline } from 'ink';
+import {
+  findLockfile,
+  loadLockfile,
+  parsePackageId,
+  setAllToVersion,
+  type LockfileResult,
+  type PackageId,
+  type PackageName,
+  type PackagePath,
+  type VersionId,
+  type VersionSpecifier,
+} from './lockfile';
+import { Input } from './components/Input';
+import fuzzy from 'fuzzy';
+import { entries, keys } from './util/object';
+import { dirname, resolve } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import { readFileSync } from 'node:fs';
+
+const App = () => {
+  const { stdout } = useStdout();
+  const [lockfile, setLockfile] = useState<LockfileResult | null>(null);
+  const [updatedPackages, setUpdatedPackages] = useState<PackageName[]>([]);
+  const [filter, setFilter] = useState<string>('');
+  const [leftSelected, setLeftSelected] = useState<number>(0);
+  const [rightSelected, setRightSelected] = useState<number>(0);
+  const [selectedPanel, setSelectedPanel] = useState(0);
+
+  useEffect(() => {
+    const path = findLockfile(process.argv[2] ?? process.cwd());
+
+    loadLockfile(path).then((lockfile) => {
+      setLockfile(lockfile);
+    });
+  }, []);
+
+  const filtered = fuzzy
+    .filter(filter, entries(lockfile?.packageIndex.packages ?? {}), {
+      extract: ([name]) => name,
+    })
+    .slice(0, 20)
+    .flatMap(({ original: [name, { versions }] }) => {
+      const versionCount = keys(versions).length;
+
+      return entries(versions).map(([version, { dependents, importers }]) => ({
+        name,
+        version,
+        dependents,
+        importers,
+        versionCount,
+      }));
+    });
+
+  useEffect(() => {
+    if (leftSelected >= filtered.length) {
+      setLeftSelected(Math.max(0, filtered.length - 1));
+    }
+  }, [filtered.length]);
+
+  const listContainerRef = useRef(null);
+
+  const selectedPackage = filtered[leftSelected];
+  const selectedPackageVersions =
+    selectedPackage == null
+      ? []
+      : entries(lockfile?.packageIndex.packages[selectedPackage.name]?.versions ?? {}).sort(([a], [b]) =>
+          a === selectedPackage.version ? -1 : b === selectedPackage.version ? 1 : 0,
+        );
+  const versionRowCounts = selectedPackageVersions.map(([version, { dependents, importers }]) => {
+    return dependents.length + importers.length + 1;
+  });
+
+  let selectedVersionRow:
+      | {
+          versionId: VersionId;
+          dependent?: PackageId;
+          importer?: PackagePath;
+          row: [VersionId, { dependents: PackageId[]; importers: PackagePath[] }];
+        }
+      | undefined = undefined,
+    tmp = 0;
+  for (let i = 0; i < selectedPackageVersions.length; i++) {
+    if (rightSelected - tmp === 0) {
+      selectedVersionRow = { versionId: selectedPackageVersions[i][0], row: selectedPackageVersions[i] };
+      break;
+    }
+    tmp += 1;
+    if (rightSelected - tmp < selectedPackageVersions[i][1].importers.length) {
+      selectedVersionRow = {
+        versionId: selectedPackageVersions[i][0],
+        importer: selectedPackageVersions[i][1].importers[rightSelected - tmp - 1],
+        row: selectedPackageVersions[i],
+      };
+      break;
+    }
+    tmp += selectedPackageVersions[i][1].importers.length;
+    if (rightSelected - tmp < selectedPackageVersions[i][1].dependents.length) {
+      selectedVersionRow = {
+        versionId: selectedPackageVersions[i][0],
+        dependent: selectedPackageVersions[i][1].dependents[rightSelected - tmp - 1],
+        row: selectedPackageVersions[i],
+      };
+      break;
+    }
+    tmp += selectedPackageVersions[i][1].dependents.length;
+  }
+
+  const canSetAllToVersion = selectedVersionRow != null;
+
+  useInput((input, key) => {
+    if (key.upArrow) {
+      if (selectedPanel === 0) {
+        setLeftSelected(Math.max(0, leftSelected - 1));
+      } else {
+        setRightSelected(Math.max(0, rightSelected - 1));
+      }
+    }
+
+    if (key.downArrow) {
+      if (selectedPanel === 0) {
+        setLeftSelected(Math.min(filtered.length - 1, leftSelected + 1));
+      } else {
+        setRightSelected(Math.min(versionRowCounts.reduce((a, b) => a + b, 0) - 1, rightSelected + 1));
+      }
+    }
+
+    if (key.rightArrow || key.tab) {
+      if (selectedPanel === 0) {
+        setSelectedPanel(1);
+        setRightSelected(0);
+      }
+    }
+
+    if (key.leftArrow || key.tab) {
+      if (selectedPanel === 1) {
+        setSelectedPanel(0);
+        let selectedSection = 0;
+        for (let i = 0; i < versionRowCounts.length; i++) {
+          selectedSection += versionRowCounts[i];
+          if (selectedSection > rightSelected) {
+            setRightSelected(i);
+            break;
+          }
+        }
+      }
+    }
+
+    if (key.ctrl && input === 's') {
+      setAllToVersionAction();
+    }
+  });
+
+  const setAllToVersionSpecifier: string | undefined = (() => {
+    if (!selectedVersionRow || !lockfile) {
+      return undefined;
+    }
+
+    const importer = selectedVersionRow.importer || selectedVersionRow.row[1].importers[0];
+    if (!importer) {
+      return undefined;
+    }
+
+    const importerSpec = lockfile?.lockfile.importers[importer];
+    const dep =
+      importerSpec?.dependencies?.[selectedPackage.name] ??
+      importerSpec?.optionalDependencies?.[selectedPackage.name] ??
+      importerSpec?.devDependencies?.[selectedPackage.name];
+
+    return dep?.specifier;
+  })();
+
+  const setAllToVersionAction = async () => {
+    if (!setAllToVersionSpecifier || !lockfile) {
+      return;
+    }
+    await setAllToVersion(lockfile, selectedPackage.name, setAllToVersionSpecifier as VersionSpecifier);
+    setUpdatedPackages((updatedPackages) => [...updatedPackages, selectedPackage.name]);
+  };
+
+  const actions: { binding: string; description: ReactNode }[] = [
+    ...(canSetAllToVersion
+      ? [
+          {
+            binding: '^S',
+            description: (
+              <>
+                Change all <Text bold>{selectedPackage.name}</Text> to <Text bold>{setAllToVersionSpecifier}</Text>
+              </>
+            ),
+          },
+        ]
+      : []),
+  ];
+
+  return (
+    <Box flexDirection='column' width={stdout.columns} height={stdout.rows}>
+      <Box flexDirection='row' gap={1}>
+        <Text color='blue'>{'>'}</Text>
+        <Input value={filter} onChange={setFilter} color='blue' />
+      </Box>
+      <Box height={'100%'}>
+        <Box
+          ref={listContainerRef}
+          flexDirection='column'
+          borderStyle='double'
+          paddingLeft={1}
+          paddingRight={1}
+          flexBasis={'50%'}
+          borderColor={selectedPanel === 0 ? 'blue' : 'white'}
+        >
+          {filtered.map(({ name, version, dependents, importers, versionCount }, idx) => {
+            const { width = 0, height = 0 } = listContainerRef.current ? measureElement(listContainerRef.current) : {};
+            const isSelected = leftSelected === idx;
+            const isUpdated = updatedPackages.includes(name);
+            return (
+              <Box key={name + '@' + version} flexDirection='row' width={'100%'}>
+                <Text backgroundColor={isSelected ? '#444' : 'transparent'}>
+                  <Transform transform={(output) => output.padEnd(width - 2, ' ')}>
+                    {isUpdated ? (
+                      <Text color='red'>{'# '}</Text>
+                    ) : versionCount > 1 ? (
+                      <Text color={'yellowBright'}>{'! '}</Text>
+                    ) : (
+                      <Text color='gray'>{'  '}</Text>
+                    )}
+                    <Text bold color='white'>
+                      {name}
+                    </Text>
+                    <Text color={'gray'}>@{ellipsis(version, 40)}</Text>
+                  </Transform>
+                </Text>
+                <Spacer />
+              </Box>
+            );
+          })}
+        </Box>
+        <Box
+          flexDirection='column'
+          borderStyle='double'
+          paddingLeft={1}
+          paddingRight={1}
+          flexBasis={'50%'}
+          borderColor={selectedPanel === 1 ? 'blue' : 'white'}
+        >
+          {selectedPackage &&
+            selectedPackageVersions &&
+            (() => {
+              let rowBase = 0;
+
+              return (
+                <>
+                  {selectedPackageVersions.map(([version, { dependents, importers }]) => {
+                    const relativeSelection = rightSelected - rowBase;
+                    rowBase += dependents.length + importers.length + 1;
+
+                    return (
+                      <Box key={version} flexDirection='column' paddingBottom={1}>
+                        <Text color='blue' bold backgroundColor={relativeSelection === 0 ? '#444' : 'transparent'}>
+                          - {version}
+                        </Text>
+                        <Box paddingLeft={2} flexDirection='column'>
+                          {importers.map((importer, idx) => {
+                            const importerSpec = lockfile?.lockfile.importers[importer];
+                            const dep =
+                              importerSpec?.dependencies?.[selectedPackage.name] ??
+                              importerSpec?.optionalDependencies?.[selectedPackage.name] ??
+                              importerSpec?.devDependencies?.[selectedPackage.name];
+
+                            const isSelected = relativeSelection === idx + 1;
+
+                            return (
+                              <Text backgroundColor={isSelected ? '#444' : 'transparent'}>
+                                {dep?.specifier ?? ''} <Text color='gray'>in local package</Text> {importer}
+                              </Text>
+                            );
+                          })}
+                          {dependents.map((dependent, idx) => {
+                            const { name, version } = parsePackageId(dependent);
+
+                            const packageSpec = lockfile?.lockfile.snapshots[dependent];
+                            const dep =
+                              packageSpec?.dependencies?.[selectedPackage.name] ??
+                              packageSpec?.optionalDependencies?.[selectedPackage.name] ??
+                              packageSpec?.devDependencies?.[selectedPackage.name];
+
+                            const isSelected = relativeSelection === idx + 1 + importers.length;
+
+                            return (
+                              <Text backgroundColor={isSelected ? '#444' : 'transparent'}>
+                                <Text color='gray'>in dependency</Text> <Text>{name}</Text>
+                                <Text color='gray'>@{ellipsis(version, 40)}</Text>
+                              </Text>
+                            );
+                          })}
+                        </Box>
+                      </Box>
+                    );
+                  })}
+                </>
+              );
+            })()}
+        </Box>
+      </Box>
+      <Box flexDirection='row'>
+        {actions.map(({ binding, description }) => (
+          <Text>
+            <Text bold>[{binding}]</Text> <Text color='gray'>{description}</Text>
+          </Text>
+        ))}
+        <Text bold>{' | '}</Text>
+        <Text color='gray'>{lockfile?.path != null ? resolve(lockfile?.path) : ''}</Text>
+      </Box>
+    </Box>
+  );
+};
+
+export const runApp = async () => {
+  render(<App />);
+
+  // const lockfile = await loadLockfile(findLockfile(process.cwd()));
+  // console.log(inspect(lockfile.packageIndex, { depth: null, colors: true }));
+};
+
+const ellipsis = (text: string, length: number) => (text.length > length ? text.slice(0, length) + '...' : text);
---
+++ tools/lockfile-explorer/src/lockfile/lockfile.ts
@@ -0,0 +1,135 @@
+import { existsSync, readFileSync } from 'node:fs';
+import { readFile, writeFile } from 'node:fs/promises';
+import { basename, dirname, join, resolve } from 'node:path';
+import * as yaml from 'yaml';
+import { entries, keys } from '../util/object';
+import type { PackageName, VersionId, VersionSpecifier } from './types';
+import type { PackageId } from './types';
+import type { PackageIndex } from './types';
+import type { Lockfile } from './types';
+
+export const findLockfile = (path: string): string => {
+  if (path.endsWith('pnpm-lock.yaml') && existsSync(path)) {
+    return path;
+  }
+
+  if (existsSync(join(path, 'pnpm-lock.yaml'))) {
+    return join(path, 'pnpm-lock.yaml');
+  }
+
+  if (path === '/') {
+    throw new Error('No lockfile found');
+  }
+
+  return findLockfile(dirname(path));
+};
+
+export const loadLockfile = async (path: string): Promise<LockfileResult> => {
+  const lockfilePath = findLockfile(path);
+  const content = await readFile(lockfilePath, 'utf8');
+  const lockfile = yaml.parse(content);
+
+  return {
+    path: lockfilePath,
+    lockfile,
+    packageIndex: buildIndex(lockfile),
+  };
+};
+
+export type LockfileResult = {
+  path: string;
+  lockfile: Lockfile;
+  packageIndex: PackageIndex;
+};
+
+export const parsePackageId = (packageId: PackageId): { name: PackageName; version: VersionId } => {
+  const splitAt = packageId.indexOf('@', 1);
+  const name = packageId.slice(0, splitAt) as PackageName;
+  const version = packageId.slice(splitAt + 1) as VersionId;
+  return { name, version };
+};
+
+const buildIndex = (lockfile: Lockfile): PackageIndex => {
+  const index: PackageIndex = {
+    packages: {},
+  };
+
+  // for (const packageId of keys(lockfile.packages)) {
+  //   const { name, version } = parsePackageId(packageId);
+  //   index.packages[name] ??= { versions: {} };
+  //   index.packages[name].versions[version] ??= { dependents: [], importers: [] };
+  // }
+
+  for (const [packageId, pkg] of entries(lockfile.snapshots)) {
+    const { name, version } = parsePackageId(packageId);
+    index.packages[name] ??= { versions: {} };
+    index.packages[name].versions[preprocessVersionId(version)] ??= { dependents: [], importers: [] };
+
+    const allDeps: [PackageName, VersionId][] = [
+      ...entries(pkg.dependencies ?? {}),
+      ...entries(pkg.devDependencies ?? {}),
+      ...entries(pkg.optionalDependencies ?? {}),
+    ];
+
+    for (const [depName, depVersion] of allDeps) {
+      index.packages[depName] ??= { versions: {} };
+      index.packages[depName].versions[preprocessVersionId(depVersion)] ??= { dependents: [], importers: [] };
+      index.packages[depName].versions[preprocessVersionId(depVersion)].dependents.push(packageId);
+    }
+  }
+
+  for (const [path, manifest] of entries(lockfile.importers)) {
+    const allDeps = [
+      ...entries(manifest.dependencies ?? {}),
+      ...entries(manifest.devDependencies ?? {}),
+      ...entries(manifest.optionalDependencies ?? {}),
+    ];
+
+    for (const [depName, { version }] of allDeps) {
+      index.packages[depName] ??= { versions: {} };
+      index.packages[depName].versions[preprocessVersionId(version)] ??= { dependents: [], importers: [] };
+      index.packages[depName].versions[preprocessVersionId(version)].importers.push(path);
+    }
+  }
+
+  return index;
+};
+
+const preprocessVersionId = (versionId: VersionId): VersionId => {
+  if (versionId.startsWith('link:.')) {
+    return 'link:<internal>' as VersionId;
+  }
+
+  return versionId;
+};
+
+export const setAllToVersion = async (
+  lockfile: LockfileResult,
+  packageName: PackageName,
+  versionSpecifier: VersionSpecifier,
+) => {
+  for (const [importer, manifest] of entries(lockfile.lockfile.importers)) {
+    for (const [depName, { version, specifier }] of entries({
+      ...(manifest.dependencies ?? {}),
+      ...(manifest.devDependencies ?? {}),
+      ...(manifest.optionalDependencies ?? {}),
+    })) {
+      if (depName !== packageName || specifier === versionSpecifier) {
+        continue;
+      }
+
+      const manifestPath = resolve(dirname(lockfile.path), importer, 'package.json');
+      const contents = await readFile(manifestPath, 'utf8');
+      const json = JSON.parse(contents);
+      if (manifest.dependencies?.[depName]) {
+        json.dependencies[depName] = versionSpecifier;
+      } else if (manifest.devDependencies?.[depName]) {
+        json.devDependencies[depName] = versionSpecifier;
+      } else if (manifest.optionalDependencies?.[depName]) {
+        json.optionalDependencies[depName] = versionSpecifier;
+      }
+
+      await writeFile(manifestPath, JSON.stringify(json, null, 2), { encoding: 'utf8' });
+    }
+  }
+};
