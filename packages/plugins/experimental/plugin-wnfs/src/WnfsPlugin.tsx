8cbc0a2eac2dfc9ff2645d5646e0e621d7276289
Steven Vandevelde <steven@tokono.ma>
2025-01-24 19:08:47 +0100

    fix: merge conflict

--- packages/plugins/experimental/plugin-wnfs/src/WnfsPlugin.tsx
+++ packages/plugins/experimental/plugin-wnfs/src/WnfsPlugin.tsx
@@ -13,7 +13,6 @@ import { meta, WNFS_PLUGIN } from './meta';
 import translations from './translations';
 import { FileType } from './types';
 
-<<<<<<< HEAD
 export const WnfsPlugin = () =>
   definePlugin(meta, [
     defineModule({
@@ -59,86 +58,3 @@ export const WnfsPlugin = () =>
       activate: ReactSurface,
     }),
   ]);
-=======
-export const WnfsPlugin = (): PluginDefinition<WnfsPluginProvides> => {
-  let blockstore: Blockstore.MixedBlockstore | undefined;
-
-  return {
-    meta,
-    ready: async (plugins) => {
-      const client = resolvePlugin(plugins, parseClientPlugin)?.provides.client;
-      invariant(client);
-      const apiHost = client?.config?.values?.runtime?.services?.edge?.url || 'http://localhost:8787';
-      blockstore = Blockstore.create(apiHost);
-      await blockstore.open();
-    },
-    provides: {
-      translations,
-      metadata: {
-        records: {
-          [FileType.typename]: {
-            placeholder: ['file title placeholder', { ns: WNFS_PLUGIN }],
-            icon: (props: IconProps) => <FileCloud {...props} />,
-          },
-        },
-      },
-      echo: {
-        schema: [FileType],
-      },
-      // TODO(burdon): Add intent to upload file.
-      file: {
-        upload: async (file: File, space: Space) => {
-          if (!blockstore) {
-            throw new Error('Blockstore is not ready yet');
-          }
-
-          return await upload({ blockstore, file, space });
-        },
-      },
-      markdown: {
-        extensions: ({ document }: { document?: DocumentType }) => {
-          if (!blockstore) {
-            throw new Error('Blockstore is not ready yet');
-          }
-
-          if (document) {
-            const space = getSpace(document);
-            return space ? [imageExtension({ blockstore, space })] : [];
-          }
-
-          return [];
-        },
-      },
-      surface: {
-        component: ({ data, role, ...props }, forwardedRef) => {
-          // NOT USED IN COMPOSER VIEW
-
-          switch (role) {
-            case 'main':
-              return data.active instanceof FileType ? <FileMain file={data.active} /> : null;
-            case 'slide':
-              return data.slide instanceof FileType ? <FileSlide file={data.slide} cover={false} /> : null;
-            case 'section':
-              return data.object instanceof FileType ? <FileSection file={data.object} /> : null;
-            case 'card': {
-              if (
-                isObject(data.content) &&
-                typeof data.content.id === 'string' &&
-                data.content.object instanceof FileType
-              ) {
-                const cardProps = { ...props, item: { id: data.content.id, object: data.content.object } };
-                return isTileComponentProps(cardProps) ? (
-                  <FileCard {...cardProps} ref={forwardedRef as Ref<HTMLDivElement>} />
-                ) : null;
-              }
-              break;
-            }
-          }
-
-          return null;
-        },
-      },
-    },
-  };
-};
->>>>>>> a8315dbb39 (feat: Track blocks that haven't been stored remotely yet)
