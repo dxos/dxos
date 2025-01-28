3df565ce27b5825ea2ef9d7a427003d0dce8c775
thure <will@braneframe.com>
2025-01-27 22:20:12 -0800

    Adjust spacing, borders, and responsiveness.

--- packages/plugins/plugin-deck/src/components/DeckLayout/Banner.tsx
+++ packages/plugins/plugin-deck/src/components/DeckLayout/Banner.tsx
@@ -14,16 +14,20 @@ export const Banner = ({ variant, classNames }: ThemedClassName<{ variant?: 'top
   return (
     <header
       className={mx(
-        'flex items-stretch pie-2 border-be border-separator p-1',
-        variant === 'topbar' && 'fixed inset-inline-0 block-start-0 bs-[--rail-size]',
+        'flex items-stretch relative',
+        variant === 'topbar'
+          ? 'fixed inset-inline-0 block-start-0 p-1 bs-[--rail-size] border-be border-separator'
+          : 'pbs-1 pli-2',
         classNames,
       )}
     >
       {variant === 'sidebar' ? <CloseSidebarButton /> : <ToggleSidebarButton />}
       <span className='self-center grow mis-1'>Composer</span>
       {variant === 'topbar' && (
-        <div role='none' className='grow grid'>
-          <Surface role='search-input' limit={1} />
+        <div role='none' className='absolute inset-0 pointer-events-none'>
+          <div role='none' className='grid bs-full p-2 max-is-md mli-auto pointer-events-auto'>
+            <Surface role='search-input' limit={1} />
+          </div>
         </div>
       )}
       <span role='none' className='grow' />
