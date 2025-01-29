2161b336acd614efca2153128e4b583119eb72d7
thure <will@braneframe.com>
2025-01-29 14:23:20 -0800

    Fix missing sidebar invoker from empty state when topbar not present. Fix solo mode invoker missing from mobile. Fix other spacing issues.

--- packages/plugins/plugin-deck/src/components/DeckLayout/ContentEmpty.tsx
+++ packages/plugins/plugin-deck/src/components/DeckLayout/ContentEmpty.tsx
@@ -5,15 +5,23 @@
 import React from 'react';
 
 import { Surface } from '@dxos/app-framework';
+import { mx } from '@dxos/react-ui-theme';
+
+import { ToggleSidebarButton } from './SidebarButton';
+import { soloInlinePadding } from '../fragments';
 
 export const ContentEmpty = () => {
   return (
     <div
       role='none'
-      className='min-bs-screen is-dvw sm:is-full flex items-center justify-center p-8'
+      className='min-bs-screen is-dvw sm:is-full p-8 grid grid-rows-[var(--rail-size)_1fr]'
       data-testid='layoutPlugin.firstRunMessage'
     >
-      <div role='none' className='grid place-items-center grid-rows-[min-content_min-content]'>
+      <div role='toolbar' className={mx(soloInlinePadding, 'flex items-stretch')}>
+        <ToggleSidebarButton />
+        <span role='none' className='grow' />
+      </div>
+      <div role='none' className='grid place-items-center'>
         <Surface role='keyshortcuts' />
       </div>
     </div>
---
+++ packages/plugins/plugin-deck/src/components/fragments.ts
@@ -0,0 +1,6 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+export const soloInlinePadding =
+  'pis-[calc(env(safe-area-inset-left)+.25rem)] pie-[calc(env(safe-area-inset-left)+.25rem)]';
