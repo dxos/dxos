89bb2799607d999469c2f7c053199c794091ff0b
Josiah Witt <josiah@witt.life>
2025-02-04 17:36:56 -0500

    calls in sidebar

--- packages/plugins/experimental/plugin-calls/src/CallsPlugin.tsx
+++ packages/plugins/experimental/plugin-calls/src/CallsPlugin.tsx
@@ -3,9 +3,10 @@
 //
 
 import { Capabilities, contributes, defineModule, definePlugin, Events } from '@dxos/app-framework';
+import { DeckCapabilities } from '@dxos/plugin-deck';
 
 import { AppGraphBuilder, ReactSurface } from './capabilities';
-import { meta } from './meta';
+import { CALLS_PLUGIN, meta } from './meta';
 import translations from './translations';
 
 export const CallsPlugin = () =>
@@ -14,6 +15,17 @@ export const CallsPlugin = () =>
       id: `${meta.id}/module/translations`,
       activatesOn: Events.SetupTranslations,
       activate: () => contributes(Capabilities.Translations, translations),
+    }),
+    defineModule({
+      id: `${meta.id}/module/complementary-panels`,
+      activatesOn: Events.Startup,
+      activate: () => [
+        contributes(DeckCapabilities.ComplementaryPanel, {
+          id: 'calls',
+          label: ['calls panel label', { ns: CALLS_PLUGIN }],
+          icon: 'ph--phone-call--regular',
+        }),
+      ],
     }),
     defineModule({
       id: `${meta.id}/module/app-graph-builder`,
