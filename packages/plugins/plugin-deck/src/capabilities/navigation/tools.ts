ef37f44b117b1298e607e0892f8401f0a8929b64
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-02-04 17:30:54 -0800

    Add table querying and data fetching

--- packages/plugins/plugin-deck/src/capabilities/navigation/tools.ts
+++ packages/plugins/plugin-deck/src/capabilities/navigation/tools.ts
@@ -24,10 +24,14 @@ export default () =>
   contributes(Capabilities.Tools, [
     defineTool({
       name: 'show',
-      description: 'Show an item in the app. Use this tool to open an artifact.',
+      // TODO(ZaymonFC): We should update the prompt to teach the LLM the difference between object ids and fully qualified ids.
+      description:
+        'Show an item in the app. Use this tool to open an artifact. When supplying IDs to show, they must be fully qualified like space:object.',
       // TODO(wittjosiah): Refactor Layout/Navigation/Deck actions so that they can be used directly.
       schema: S.Struct({
-        id: S.String.annotations({ description: 'The ID of the item to show.' }),
+        id: S.String.annotations({
+          description: 'The ID of the item to show.',
+        }),
         pivotId: S.optional(
           S.String.annotations({
             description: 'The ID of the chat. If provided, the item will be added after the pivot item.',
