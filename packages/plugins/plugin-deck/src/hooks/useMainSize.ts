361265551cbe137b739052b9956c1e1d4b69a24c
thure <will@braneframe.com>
2025-02-10 21:38:05 -0800

    Fix layout bugs.

--- packages/plugins/plugin-deck/src/hooks/useMainSize.ts
+++ packages/plugins/plugin-deck/src/hooks/useMainSize.ts
@@ -7,7 +7,7 @@ import { useMainContext } from '@dxos/react-ui';
 export const useMainSize = () => {
   const { navigationSidebarState, complementarySidebarOpen } = useMainContext('DeckPluginPlank');
   return {
-    'data-sidebar-inline-start-state': navigationSidebarState ? 'open' : 'closed',
-    'data-sidebar-inline-end-state': complementarySidebarOpen ? 'open' : 'closed',
+    'data-sidebar-inline-start-state': navigationSidebarState,
+    'data-sidebar-inline-end-state': complementarySidebarOpen ? 'expanded' : 'closed',
   };
 };
