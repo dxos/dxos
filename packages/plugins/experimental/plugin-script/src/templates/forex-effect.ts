d0a1ee2aa4d6240bdd94758dc89da3dc292b04af
Dmytro Maretskyi <maretskii@gmail.com>
2025-02-11 11:31:25 +0100

    Add forex effect preset

---
+++ packages/plugins/experimental/plugin-script/src/templates/forex-effect.ts
@@ -0,0 +1,39 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+// @ts-ignore
+import { defineFunction, S } from 'dxos:functions';
+import {
+  HttpClient,
+  HttpClientRequest,
+  HttpClientResponse,
+  HttpClientError,
+  // @ts-ignore
+} from 'https://esm.sh/@effect/platform@0.61.0?deps=effect@3.6.0';
+// @ts-ignore
+import type { Cause } from 'https://esm.sh/effect@3.6.0';
+// @ts-ignore
+import { Effect, Schedule } from 'https://esm.sh/effect@3.6.0';
+
+/**
+ * Returns the exchange rate between two currencies.
+ */
+export default defineFunction({
+  inputSchema: S.Struct({
+    from: S.String.annotations({ description: 'The source currency' }),
+    to: S.String.annotations({ description: 'The target currency' }),
+  }).annotations({ description: 'Returns the exchange rate between two currencies.' }),
+  handler: async ({
+    event: {
+      data: { from, to },
+    },
+  }: any) =>
+    HttpClientRequest.get(`https://free.ratesdb.com/v1/rates?from=${from}&to=${to}`).pipe(
+      HttpClient.fetchOk,
+      HttpClientResponse.json,
+      Effect.timeout('1 second'),
+      Effect.retry(Schedule.exponential(1000).pipe(Schedule.compose(Schedule.recurs(3)))),
+      Effect.map(({ data: { rates } }: any) => rates[to].toString()),
+    ),
+});
