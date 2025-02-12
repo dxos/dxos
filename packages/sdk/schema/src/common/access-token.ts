168aa5779177985ebc0765b43affc485ae7d337e
Dmytro Maretskyi <maretskii@gmail.com>
2025-02-12 16:15:44 +0100

    Add docs to access token

--- packages/sdk/schema/src/common/access-token.ts
+++ packages/sdk/schema/src/common/access-token.ts
@@ -2,14 +2,33 @@
 // Copyright 2025 DXOS.org
 //
 
-import { TitleAnnotationId } from '@effect/schema/AST';
-
 import { Format, S, TypedObject } from '@dxos/echo-schema';
 
 export const AccessTokenSchema = S.Struct({
-  note: S.String.annotations({ [TitleAnnotationId]: 'Note' }),
-  source: Format.Hostname.annotations({ [TitleAnnotationId]: 'Source' }),
-  token: S.String.annotations({ [TitleAnnotationId]: 'Token' }),
+  /**
+   * The domain name of the service that issued the token.
+   * @example `github.com`
+   */
+  source: Format.Hostname.annotations({
+    title: 'Source',
+    description: 'The domain name of the service that issued the token.',
+  }),
+
+  /**
+   * The token provided by the service.
+   */
+  token: S.String.annotations({
+    title: 'Token',
+    description: 'The token provided by the service.',
+  }),
+
+  /**
+   * User-provided note about the token.
+   */
+  note: S.String.annotations({
+    title: 'Note',
+    description: 'User-provided note about the token.',
+  }),
 });
 
 // TODO(wittjosiah): This is a temporary solution, long term these should be stored in HALO.
