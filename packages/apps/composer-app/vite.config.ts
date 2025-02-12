6495d8589c00400885b64182d67c64d695be8e1b
Rich Burdon <rich.burdon@gmail.com>
2025-02-12 01:04:28 -0500

    chore: ios build (#8478)
    Co-authored-by: Josiah Witt <josiah@witt.life>
    Co-authored-by: Mykola Veremchuk <veremchukmukola@gmail.com>

--- packages/apps/composer-app/vite.config.ts
+++ packages/apps/composer-app/vite.config.ts
@@ -177,29 +177,30 @@ export default defineConfig((env) => ({
       manifest: {
         name: 'DXOS Composer',
         short_name: 'Composer',
-        description: 'DXOS Composer Application',
+        description: 'DXOS Composer',
         theme_color: '#003E70',
         icons: [
           {
-            src: 'favicon-16x16.png',
-            sizes: '16x16',
-            type: 'image/png',
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
           },
           {
-            src: 'favicon-32x32.png',
-            sizes: '32x32',
-            type: 'image/png',
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
           {
-            src: 'android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           },
           {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
         ],
       },
     }),
