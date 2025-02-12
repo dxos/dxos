c944e3f8102615dbf647771587298c4f12cb069a
Josiah Witt <josiah@witt.life>
2025-02-12 16:52:14 -0500

    subscribe to schemas

--- packages/plugins/plugin-client/src/capabilities/schema.ts
+++ packages/plugins/plugin-client/src/capabilities/schema.ts
@@ -2,18 +2,35 @@
 // Copyright 2025 DXOS.org
 //
 
+import { effect } from '@preact/signals-core';
+
 import { Capabilities, contributes, type PluginsContext } from '@dxos/app-framework';
+import { type TypedObject } from '@dxos/echo-schema';
 
 import { ClientCapabilities } from './capabilities';
 
+// TODO(wittjosiah): Remove types?
 export default (context: PluginsContext) => {
   const client = context.requestCapability(ClientCapabilities.Client);
 
-  // TODO(wittjosiah): This should subscribe to capabilities and add types for newly added capabilities.
-  const systemSchemas = Array.from(new Set(context.requestCapabilities(ClientCapabilities.SystemSchema).flat()));
-  const schemas = Array.from(new Set(context.requestCapabilities(ClientCapabilities.Schema).flat()));
-  client.addTypes(systemSchemas);
-  client.addTypes(schemas);
+  let previousSystem: TypedObject[] = [];
+  const unsubscribeSystem = effect(() => {
+    const systemSchemas = Array.from(new Set(context.requestCapabilities(ClientCapabilities.SystemSchema).flat()));
+    const newSchemas = systemSchemas.filter((schema) => !previousSystem.includes(schema));
+    previousSystem = systemSchemas;
+    client.addTypes(newSchemas);
+  });
 
-  return contributes(Capabilities.Null, null);
+  let previous: TypedObject[] = [];
+  const unsubscribe = effect(() => {
+    const schemas = Array.from(new Set(context.requestCapabilities(ClientCapabilities.Schema).flat()));
+    const newSchemas = schemas.filter((schema) => !previous.includes(schema));
+    previous = schemas;
+    client.addTypes(newSchemas);
+  });
+
+  return contributes(Capabilities.Null, null, () => {
+    unsubscribeSystem();
+    unsubscribe();
+  });
 };
