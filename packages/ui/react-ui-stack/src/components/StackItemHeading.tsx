1fc1082f6bfec3a9d6ae045c911a87a32f77586b
Will Shown <thure@users.noreply.github.com>
2025-02-12 00:29:22 -0800

    fix: CSS layers (#8575)

--- packages/ui/react-ui-stack/src/components/StackItemHeading.tsx
+++ packages/ui/react-ui-stack/src/components/StackItemHeading.tsx
@@ -23,7 +23,7 @@ export const StackItemHeading = ({ children, classNames, ...props }: StackItemHe
       tabIndex={0}
       {...focusableGroupAttrs}
       className={mx(
-        'flex items-center ch-focus-ring-inset-over-all relative !border-is-0',
+        'flex items-center dx-focus-ring-inset-over-all relative !border-is-0',
         orientation === 'horizontal' ? 'bs-[--rail-size]' : 'is-[--rail-size] flex-col',
         classNames,
       )}
