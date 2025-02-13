9e6556e5cb119254ff3065e4bc4962e8a29d655b
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-02-13 18:02:48 +0200

    Delete space node

--- packages/plugins/experimental/plugin-calls/src/capabilities/react-surface.tsx
+++ packages/plugins/experimental/plugin-calls/src/capabilities/react-surface.tsx
@@ -5,6 +5,7 @@
 import React from 'react';
 
 import { Capabilities, contributes, createSurface } from '@dxos/app-framework';
+import { PublicKey } from '@dxos/react-client';
 import { type Space, isSpace } from '@dxos/react-client/echo';
 
 import { CallsContainer } from '../components';
@@ -14,15 +15,17 @@ import { type Call, isCall } from '../types';
 export default () =>
   contributes(Capabilities.ReactSurface, [
     createSurface({
-      id: `${CALLS_PLUGIN}/article`,
-      role: 'article',
+      id: `${CALLS_PLUGIN}/thread-calls`,
+      role: 'thread-calls',
       filter: (data): data is { subject: Call } => isCall(data.subject),
-      component: ({ data, role }) => <CallsContainer space={data.subject.space} role={role} />,
+      component: ({ data }) => (
+        <CallsContainer space={data.subject.space} roomId={PublicKey.from(data.subject.space.id)} />
+      ),
     }),
     createSurface({
       id: `${CALLS_PLUGIN}/assistant`,
       role: 'complementary--calls',
       filter: (data): data is { subject: Space } => isSpace(data.subject),
-      component: ({ data }) => <CallsContainer space={data.subject} />,
+      component: ({ data }) => <CallsContainer space={data.subject} roomId={PublicKey.from(data.subject.id)} />,
     }),
   ]);
