c7507977ab241d8746a7b702ba6e98ef490333d1
Rich Burdon <rich.burdon@gmail.com>
2025-02-12 21:44:48 -0500

    Special effects components.

---
+++ packages/ui/react-ui-sfx/src/components/index.ts
@@ -0,0 +1,8 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+export * from './Chaos';
+export * from './Matrix';
+export * from './Spinner';
+export * from './Waveform';
---
+++ packages/ui/react-ui-sfx/src/components/motion.stories.tsx
@@ -0,0 +1,47 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import '@dxos-theme';
+
+import type { Meta, StoryObj } from '@storybook/react';
+import React, { useState } from 'react';
+
+import { Button, Toolbar } from '@dxos/react-ui';
+import { withLayout, withTheme } from '@dxos/storybook-utils';
+
+import { Spinner, type SpinnerProps } from './Spinner';
+
+const Render = ({ active: _active }: SpinnerProps) => {
+  const [active, setActive] = useState(_active);
+
+  return (
+    <div className='flex flex-col grow'>
+      <Toolbar.Root>
+        <Button onClick={() => setActive((active) => !active)}>Toggle</Button>
+      </Toolbar.Root>
+      <div className='flex grow items-center justify-center'>
+        <div className='flex w-6 h-6'>
+          <Spinner active={active} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const meta: Meta<SpinnerProps> = {
+  title: 'ui/react-ui-sfx/motion',
+  component: Spinner,
+  render: Render,
+  decorators: [withTheme, withLayout({ fullscreen: true })],
+};
+
+export default meta;
+
+type Story = StoryObj<SpinnerProps>;
+
+export const Default: Story = {
+  args: {
+    active: false,
+  },
+};
