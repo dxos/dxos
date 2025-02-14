5250fb55bb79e909a55a9e22335eeabc0121764c
Will Shown <thure@users.noreply.github.com>
2025-02-13 19:15:40 -0800

    fix(react-ui-stack): Finish implementation of vertical resizing (#8587)

--- packages/ui/react-ui-stack/src/components/StackItemResizeHandle.tsx
+++ packages/ui/react-ui-stack/src/components/StackItemResizeHandle.tsx
@@ -15,7 +15,8 @@ import { DEFAULT_EXTRINSIC_SIZE } from './StackItem';
 
 const REM = parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-const MIN_SIZE = 20;
+const MIN_WIDTH = 20;
+const MIN_HEIGHT = 3;
 
 const measureStackItem = (element: HTMLButtonElement): { width: number; height: number } => {
   const stackItemElement = element.closest('[data-dx-stack-item]');
@@ -23,7 +24,10 @@ const measureStackItem = (element: HTMLButtonElement): { width: number; height: 
 };
 
 const getNextSize = (startSize: number, location: DragLocationHistory, client: 'clientX' | 'clientY') => {
-  return Math.max(MIN_SIZE, startSize + (location.current.input[client] - location.initial.input[client]) / REM);
+  return Math.max(
+    client === 'clientX' ? MIN_WIDTH : MIN_HEIGHT,
+    startSize + (location.current.input[client] - location.initial.input[client]) / REM,
+  );
 };
 
 export type StackItemResizeHandleProps = {};
@@ -81,29 +85,36 @@ export const StackItemResizeHandle = () => {
     <button
       ref={buttonRef}
       className={mx(
-        orientation === 'horizontal' ? 'cursor-col-resize' : 'cursor-row-resize',
-        'group absolute is-3 bs-full inline-end-[-1px] !border-lb-0',
+        'group absolute',
+        orientation === 'horizontal'
+          ? 'cursor-col-resize is-3 bs-full inline-end-[-1px] !border-lb-0 before:inset-block-0 before:inline-end-0 before:is-1'
+          : 'cursor-row-resize bs-3 is-full block-end-[-1px] !border-li-0 before:inset-inline-0 before:block-end-0 before:bs-1',
         'before:transition-opacity before:duration-100 before:ease-in-out before:opacity-0 hover:before:opacity-100 focus-visible:before:opacity-100 active:before:opacity-100',
-        'before:absolute before:block before:inset-block-0 before:inline-end-0 before:is-1 before:bg-accentFocusIndicator',
+        'before:absolute before:block before:bg-accentFocusIndicator',
       )}
     >
       <div
         role='none'
-        className='absolute block-start-0 inline-end-[1px] bs-[--rail-size] flex items-center group-hover:opacity-0 group-focus-visible:opacity-0 group-active:opacity-0'
+        className={mx(
+          'absolute flex items-center group-hover:opacity-0 group-focus-visible:opacity-0 group-active:opacity-0',
+          orientation === 'horizontal'
+            ? 'block-start-0 inline-end-px bs-[--rail-size]'
+            : 'inline-start-0 block-end-px is-[--rail-size] flex justify-center',
+        )}
       >
-        <DragHandleSignifier />
+        <DragHandleSignifier orientation={orientation} />
       </div>
     </button>
   );
 };
 
-const DragHandleSignifier = () => {
+const DragHandleSignifier = ({ orientation }: { orientation: 'horizontal' | 'vertical' }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 256 256'
       fill='currentColor'
-      className='shrink-0 bs-[1em] is-[1em] text-unAccent'
+      className={mx('shrink-0 bs-[1em] is-[1em] text-unAccent', orientation === 'vertical' && 'rotate-90')}
     >
       {/* two pips: <path d='M256,120c-8.8,0-16-7.2-16-16v-56c0-8.8,7.2-16,16-16v88Z' />
       <path d='M256,232c-8.8,0-16-7.2-16-16v-56c0-8.8,7.2-16,16-16v88Z' /> */}
