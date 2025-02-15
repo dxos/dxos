2a703c26c69cb7e5e86506ccc2d560a01748c8d9
Josiah Witt <josiah@witt.life>
2025-02-15 13:45:14 -0500

    remove deprecated settings interface

--- packages/sdk/app-framework/src/plugin-settings/actions.ts
+++ packages/sdk/app-framework/src/plugin-settings/actions.ts
@@ -6,6 +6,8 @@ import { S } from '@dxos/echo-schema';
 
 export const SETTINGS_PLUGIN = 'dxos.org/plugin/settings';
 export const SETTINGS_ACTION = `${SETTINGS_PLUGIN}/action`;
+export const SETTINGS_ID = 'dxos:settings';
+export const SETTINGS_KEY = 'settings';
 
 export namespace SettingsAction {
   export class Open extends S.TaggedClass<Open>()(`${SETTINGS_ACTION}/open`, {
--- packages/sdk/app-framework/src/plugin-settings/app-graph-builder.ts
+++ packages/sdk/app-framework/src/plugin-settings/app-graph-builder.ts
@@ -2,44 +2,29 @@
 // Copyright 2025 DXOS.org
 //
 
-import {
-  Capabilities,
-  contributes,
-  createIntent,
-  type PluginMeta,
-  type PluginsContext,
-  SettingsAction,
-} from '@dxos/app-framework';
+import { createExtension, type Node } from '@dxos/app-graph';
 import { type SettingsStore, type SettingsValue } from '@dxos/local-storage';
-import { createExtension, type Node } from '@dxos/plugin-graph';
 import { nonNullable } from '@dxos/util';
 
-import { SETTINGS_ID, SETTINGS_INTERFACE_PLUGIN } from '../meta';
-
-export const SETTINGS_KEY = 'settings';
-
-const core = [
-  'dxos.org/plugin/deck',
-  'dxos.org/plugin/files',
-  'dxos.org/plugin/observability',
-  'dxos.org/plugin/space',
-  'dxos.org/plugin/token-manager',
-];
+import { SETTINGS_ID, SETTINGS_KEY, SETTINGS_PLUGIN, SettingsAction } from './actions';
+import { Capabilities } from '../common';
+import { contributes, type PluginMeta, type PluginsContext } from '../core';
+import { createIntent } from '../plugin-intent';
 
 export default (context: PluginsContext) =>
   contributes(Capabilities.AppGraphBuilder, [
     createExtension({
-      id: `${SETTINGS_INTERFACE_PLUGIN}/action`,
+      id: `${SETTINGS_PLUGIN}/action`,
       filter: (node): node is Node<null> => node.id === 'root',
       actions: () => [
         {
-          id: SETTINGS_INTERFACE_PLUGIN,
+          id: SETTINGS_PLUGIN,
           data: async () => {
             const { dispatchPromise: dispatch } = context.requestCapability(Capabilities.IntentDispatcher);
             await dispatch(createIntent(SettingsAction.Open));
           },
           properties: {
-            label: ['open settings label', { ns: SETTINGS_INTERFACE_PLUGIN }],
+            label: ['open settings label', { ns: SETTINGS_PLUGIN }],
             icon: 'ph--gear--regular',
             keyBinding: {
               macos: 'meta+,',
@@ -50,14 +35,14 @@ export default (context: PluginsContext) =>
       ],
     }),
     createExtension({
-      id: `${SETTINGS_INTERFACE_PLUGIN}/core`,
+      id: `${SETTINGS_PLUGIN}/core`,
       filter: (node): node is Node<null> => node.id === 'root',
       connector: () => [
         {
           id: SETTINGS_ID,
-          type: SETTINGS_INTERFACE_PLUGIN,
+          type: SETTINGS_PLUGIN,
           properties: {
-            label: ['app settings label', { ns: SETTINGS_INTERFACE_PLUGIN }],
+            label: ['app settings label', { ns: SETTINGS_PLUGIN }],
             icon: 'ph--gear--regular',
             disposition: 'pin-end',
             position: 'fallback',
@@ -67,19 +52,15 @@ export default (context: PluginsContext) =>
       ],
     }),
     createExtension({
-      id: `${SETTINGS_INTERFACE_PLUGIN}/core-plugins`,
+      id: `${SETTINGS_PLUGIN}/core-plugins`,
       filter: (node): node is Node<null> => node.id === SETTINGS_ID,
       connector: () => {
         const manager = context.requestCapability(Capabilities.PluginManager);
         const [settingsStore] = context.requestCapabilities(Capabilities.SettingsStore);
         return [
-          ...core
-            .map((id): [PluginMeta, SettingsStore<SettingsValue>] | null => {
-              const plugin = manager.plugins.find((plugin) => plugin.meta.id === id);
-              if (!plugin) {
-                return null;
-              }
-
+          ...manager.plugins
+            .filter((plugin) => manager.core.includes(plugin.meta.id))
+            .map((plugin): [PluginMeta, SettingsStore<SettingsValue>] | null => {
               const settings = settingsStore?.getStore(plugin.meta.id);
               if (!settings) {
                 return null;
@@ -89,7 +70,7 @@ export default (context: PluginsContext) =>
             })
             .filter(nonNullable)
             .map(([meta, settings]) => ({
-              id: `${SETTINGS_KEY}:${meta.id.split('/').at(-1)}`,
+              id: `${SETTINGS_KEY}:${meta.id.replaceAll('/', ':')}`,
               type: 'category',
               data: settings,
               properties: {
@@ -102,7 +83,7 @@ export default (context: PluginsContext) =>
             id: `${SETTINGS_KEY}:custom-plugins`,
             type: 'collection',
             properties: {
-              label: ['custom plugins label', { ns: SETTINGS_INTERFACE_PLUGIN }],
+              label: ['custom plugins label', { ns: SETTINGS_PLUGIN }],
               icon: 'ph--squares-four--regular',
               role: 'branch',
             },
@@ -111,13 +92,13 @@ export default (context: PluginsContext) =>
       },
     }),
     createExtension({
-      id: `${SETTINGS_INTERFACE_PLUGIN}/custom-plugins`,
+      id: `${SETTINGS_PLUGIN}/custom-plugins`,
       filter: (node): node is Node<null> => node.id === `${SETTINGS_KEY}:custom-plugins`,
       connector: () => {
         const manager = context.requestCapability(Capabilities.PluginManager);
         const [settingsStore] = context.requestCapabilities(Capabilities.SettingsStore);
         return manager.plugins
-          .filter((plugin) => !core.includes(plugin.meta.id))
+          .filter((plugin) => !manager.core.includes(plugin.meta.id))
           .map((plugin): [PluginMeta, SettingsStore<SettingsValue>] | null => {
             const settings = settingsStore?.getStore(plugin.meta.id);
             if (!settings) {
