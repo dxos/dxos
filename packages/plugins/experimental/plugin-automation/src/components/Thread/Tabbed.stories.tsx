be84ec3571350517c43eb2e6af192d1e82ca6a5b
Rich Burdon <rich.burdon@gmail.com>
2025-02-16 22:29:53 -0500

    fix

--- packages/plugins/experimental/plugin-automation/src/components/Thread/Tabbed.stories.tsx
+++ packages/plugins/experimental/plugin-automation/src/components/Thread/Tabbed.stories.tsx
@@ -13,6 +13,8 @@ import { withLayout, withTheme, withSignals } from '@dxos/storybook-utils';
 import { Tabs } from './Tabbed';
 import { ToggleContainer } from './ToggleContainer';
 
+const content = Array.from({ length: 4 }, (_, i) => faker.lorem.paragraphs(3));
+
 const meta: Meta<typeof Tabs> = {
   title: 'plugins/plugin-automation/Tabbed',
   component: Tabs,
@@ -23,8 +25,6 @@ export default meta;
 
 type Story = StoryObj<typeof Tabs>;
 
-const content = Array.from({ length: 4 }, (_, i) => faker.lorem.paragraphs(3));
-
 export const Default: Story = {
   render: () => {
     const [selected, setSelected] = useState(0);
@@ -32,8 +32,8 @@ export const Default: Story = {
       <div className='flex flex-col w-[500px] p-4 bg-attention'>
         <ToggleContainer
           title='Thread'
-          toggle
           classNames='p-1 rounded-lg bg-base border border-neutral-500'
+          toggle
           defaultOpen
         >
           <div className='flex w-full overflow-hidden'>
