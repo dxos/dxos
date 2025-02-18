69de508edb2f56b8b63e24c22ce4c22975d190cf
thure <will@braneframe.com>
2025-02-17 17:24:36 -0800

    Add alias tokens.

--- packages/ui/react-ui-theme/src/config/tokens/index.ts
+++ packages/ui/react-ui-theme/src/config/tokens/index.ts
@@ -6,14 +6,16 @@ import { type TailwindAdapterConfig } from '@ch-ui/tailwind-tokens';
 import adapter from '@ch-ui/tailwind-tokens';
 import { type TokenSet } from '@ch-ui/tokens';
 
+import { aliasColors } from './alias-colors';
 import { physicalColors } from './physical-colors';
 import { semanticColors } from './semantic-colors';
-import { systemSememes } from './sememes-system';
+import { systemAliases, systemSememes } from './sememes-system';
 
 export const tokenSet = {
   colors: {
     physical: physicalColors,
     semantic: semanticColors,
+    alias: aliasColors,
   },
 } satisfies TokenSet;
 
@@ -38,6 +40,11 @@ export const userDefaultTokenSet = {
       conditions: semanticColors.conditions,
       sememes: systemSememes,
       namespace: semanticColors.namespace,
+    },
+    alias: {
+      statements: aliasColors.statements,
+      aliases: systemAliases,
+      namespace: aliasColors.namespace,
     },
   },
 } satisfies TokenSet;
--- packages/ui/react-ui-theme/src/config/tokens/sememes-system.ts
+++ packages/ui/react-ui-theme/src/config/tokens/sememes-system.ts
@@ -2,7 +2,7 @@
 // Copyright 2024 DXOS.org
 //
 
-import { type ColorSememes } from './types';
+import { type ColorAliases, type ColorSememes } from './types';
 
 export const surfaceCadence = {
   dark: [900, 800, 775, 710, 695, 680, 650],
@@ -16,66 +16,53 @@ export const systemSememes = {
   // Surfaces (bg-)
   //
 
-  attention: {
+  'surface-0': {
     light: ['neutral', surfaceCadence.light[0]],
     dark: ['neutral', surfaceCadence.dark[0]],
   },
-  currentRelated: {
-    light: ['neutral', surfaceCadence.light[0]],
-    dark: ['neutral', surfaceCadence.dark[0]],
-  },
-  deck: {
+  'surface-10': {
     light: ['neutral', surfaceCadence.light[4]],
     dark: ['neutral', surfaceCadence.dark[1]],
   },
-  scrim: {
+  'surface-10t': {
     light: ['neutral', `${surfaceCadence.light[4]}/.65`],
     dark: ['neutral', `${surfaceCadence.dark[1]}/.65`],
   },
-  base: {
+  'surface-20': {
     light: ['neutral', surfaceCadence.light[1]],
     dark: ['neutral', surfaceCadence.dark[2]],
   },
-  input: {
+  'surface-20t': {
+    light: ['neutral', `${surfaceCadence.light[1]}/.88`],
+    dark: ['neutral', `${surfaceCadence.dark[2]}/.88`],
+  },
+  'surface-30': {
     light: ['neutral', surfaceCadence.light[2]],
     dark: ['neutral', surfaceCadence.dark[3]],
   },
-  modalSelected: {
-    light: ['neutral', surfaceCadence.light[2]],
-    dark: ['neutral', surfaceCadence.dark[3]],
-  },
-  hoverSurface: {
+  'surface-40': {
     light: ['neutral', surfaceCadence.light[3]],
     dark: ['neutral', surfaceCadence.dark[4]],
   },
-  modalSurface: {
-    light: ['neutral', `${surfaceCadence.light[1]}/.88`],
-    dark: ['neutral', `${surfaceCadence.dark[2]}/.88`],
-    // light: ['neutral', surfaceCadence.light[0]],
-    // dark: ['neutral', surfaceCadence.dark[5]],
-  },
-  accentSurface: {
-    light: ['primary', 500],
-    dark: ['primary', 500],
-  },
-  accentSurfaceHover: {
-    light: ['primary', 600],
-    dark: ['primary', 475],
-  },
-  hoverOverlay: {
+  'surface-90t': {
     light: ['neutral', '450/.1'],
     dark: ['neutral', '450/.1'],
   },
-  attentionRelated: {
-    light: ['neutral', surfaceCadence.light[4]],
-    dark: ['neutral', surfaceCadence.dark[6]],
+
+  'accentSurface-40': {
+    light: ['primary', 600],
+    dark: ['primary', 475],
+  },
+  'accentSurface-50': {
+    light: ['primary', 500],
+    dark: ['primary', 500],
   },
 
   //
   // Borders (border-, divide-)
   //
 
-  separator: {
+  'surface-50': {
     light: ['neutral', surfaceCadence.light[4]],
     dark: ['neutral', surfaceCadence.dark[6]],
   },
@@ -121,3 +108,18 @@ export const systemSememes = {
     dark: ['neutral', 0],
   },
 } satisfies ColorSememes;
+
+export const systemAliases = {
+  // neutral
+  'surface-0': ['attention', 'currentRelated'],
+  'surface-10': ['deck'],
+  'surface-10t': ['scrim'],
+  'surface-20': ['baseSurface'], // todo: rename uses of the `base` token to `baseSurface`
+  'surface-20t': ['modalSurface'],
+  'surface-30': ['input'],
+  'surface-40': ['hoverSurface'],
+  'surface-50': ['separator'],
+  // accent
+  'accentSurface-40': ['accentSurfaceHover'],
+  'accentSurface-50': ['accentSurface'],
+} satisfies ColorAliases;
