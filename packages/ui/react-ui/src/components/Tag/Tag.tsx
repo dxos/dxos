08dd90b9b6921c0aae8d0bb2e350f87047affd52
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-02-19 14:06:13 -0800

    Remove tag.ts

--- packages/ui/react-ui/src/components/Tag/Tag.tsx
+++ packages/ui/react-ui/src/components/Tag/Tag.tsx
@@ -8,17 +8,12 @@ import React, { type ComponentPropsWithRef, forwardRef } from 'react';
 
 import { type ChromaticPalette, type MessageValence, type NeutralPalette } from '@dxos/react-ui-types';
 
-import { useThemeContext } from '../../hooks';
-
 export type TagProps = ComponentPropsWithRef<typeof Primitive.span> & {
   palette?: NeutralPalette | ChromaticPalette | MessageValence;
   asChild?: boolean;
 };
 
-export const Tag = forwardRef<HTMLSpanElement, TagProps>(
-  ({ asChild, palette = 'neutral', className, ...props }, forwardedRef) => {
-    const { tx } = useThemeContext();
-    const Root = asChild ? Slot : Primitive.span;
-    return <Root {...props} className={tx('tag.root', 'dx-tag', className)} data-hue={palette} ref={forwardedRef} />;
-  },
-);
+export const Tag = forwardRef<HTMLSpanElement, TagProps>(({ asChild, palette = 'neutral', ...props }, forwardedRef) => {
+  const Root = asChild ? Slot : Primitive.span;
+  return <Root {...props} className={'dx-tag'} data-hue={palette} ref={forwardedRef} />;
+});
