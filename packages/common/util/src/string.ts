9b7b6941902edc4d41215ea79a4ad6dacf8d1cc5
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-02-19 12:05:48 -0800

    fix

--- packages/common/util/src/string.ts
+++ packages/common/util/src/string.ts
@@ -4,9 +4,11 @@
 
 /**
  * Capitalizes the first letter of a string.
- * @param str The string to capitalize.
- * @returns The capitalized string.
  */
 export const capitalize = (str: string): string => {
+  if (str.length === 0) {
+    return '';
+  }
+
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
