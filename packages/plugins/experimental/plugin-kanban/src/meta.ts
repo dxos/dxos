7c1bb44cd1b0c5ae57afde1c977e025fdcc8ff94
Will Shown <thure@users.noreply.github.com>
2025-02-20 14:29:00 -0800

    fix(react-ui-stack,react-ui-kanban): Keyboard access, DOM order, focus indicators (#8632)

--- packages/plugins/experimental/plugin-kanban/src/meta.ts
+++ packages/plugins/experimental/plugin-kanban/src/meta.ts
@@ -9,7 +9,8 @@ export const KANBAN_PLUGIN = 'dxos.org/plugin/kanban';
 export const meta = {
   id: KANBAN_PLUGIN,
   name: 'Kanban',
-  description: `Kanban allows you to explore Table data in sorted columns defined by your custom schema. You can use Kanbans to track progress or trigger custom automations when cards are moved from one state to another.`,
+  description:
+    'Kanban allows you to explore Table data in sorted columns defined by your custom schema. You can use Kanbans to track progress or trigger custom automations when cards are moved from one state to another.',
   icon: 'ph--kanban--regular',
   source: 'https://github.com/dxos/dxos/tree/main/packages/plugins/experimental/plugin-kanban',
   tags: ['experimental'],
--- packages/plugins/experimental/plugin-script/src/meta.ts
+++ packages/plugins/experimental/plugin-script/src/meta.ts
@@ -9,7 +9,8 @@ export const SCRIPT_PLUGIN = 'dxos.org/plugin/script';
 export const meta = {
   id: SCRIPT_PLUGIN,
   name: 'Scripts',
-  description: `Scripts in Composer allow you to deploy custom functions that run on the edge. These functions can be referenced by your AI agent and called inside of cells in any Sheet. Use scripts to interact with API’s or external data inside of Composer.`,
+  description:
+    'Scripts in Composer allow you to deploy custom functions that run on the edge. These functions can be referenced by your AI agent and called inside of cells in any Sheet. Use scripts to interact with API’s or external data inside of Composer.',
   icon: 'ph--code--regular',
   source: 'https://github.com/dxos/dxos/tree/main/packages/plugins/experimental/plugin-explorer',
   tags: ['experimental'],
