4835fde48656c438871d7a991cbfb93536b6752c
Will Shown <thure@users.noreply.github.com>
2025-02-19 18:04:09 -0800

    fix: Kanban DnD issues (#8623)

--- packages/ui/react-ui-kanban/src/defs/kanban-model.ts
+++ packages/ui/react-ui-kanban/src/defs/kanban-model.ts
@@ -8,6 +8,7 @@ import { type JsonProp, type EchoSchema } from '@dxos/echo-schema';
 import { invariant } from '@dxos/invariant';
 import type { StackItemRearrangeHandler } from '@dxos/react-ui-stack';
 import { type ViewProjection } from '@dxos/schema';
+import { arrayMove } from '@dxos/util';
 
 import { type KanbanType } from './kanban';
 import { computeArrangement } from '../util';
@@ -193,9 +194,12 @@ export class KanbanModel<T extends BaseKanbanItem = { id: string }> extends Reso
     const options = [...(fieldProjection.props.options ?? [])];
     const sourceIndex = options.findIndex((opt) => opt.id === source.id);
     const targetIndex = options.findIndex((opt) => opt.id === target.id);
-    const [movedOption] = options.splice(sourceIndex, 1);
-    const insertIndex = closestEdge === 'right' ? targetIndex + 1 : targetIndex;
-    options.splice(insertIndex, 0, movedOption);
+    const insertIndex =
+      source.id === target.id
+        ? sourceIndex
+        : targetIndex +
+          (targetIndex > sourceIndex ? (closestEdge === 'right' ? 0 : -1) : closestEdge === 'right' ? 1 : 0);
+    arrayMove(options, sourceIndex, insertIndex);
 
     this._projection.setFieldProjection({ ...fieldProjection, props: { ...fieldProjection.props, options } });
   }
--- packages/ui/react-ui/src/components/Tag/Tag.tsx
+++ packages/ui/react-ui/src/components/Tag/Tag.tsx
@@ -8,12 +8,25 @@ import React, { type ComponentPropsWithRef, forwardRef } from 'react';
 
 import { type ChromaticPalette, type MessageValence, type NeutralPalette } from '@dxos/react-ui-types';
 
-export type TagProps = ComponentPropsWithRef<typeof Primitive.span> & {
+import { useThemeContext } from '../../hooks';
+import { type ThemedClassName } from '../../util';
+
+export type TagProps = ThemedClassName<ComponentPropsWithRef<typeof Primitive.span>> & {
   palette?: NeutralPalette | ChromaticPalette | MessageValence;
   asChild?: boolean;
 };
 
-export const Tag = forwardRef<HTMLSpanElement, TagProps>(({ asChild, palette = 'neutral', ...props }, forwardedRef) => {
-  const Root = asChild ? Slot : Primitive.span;
-  return <Root {...props} className={'dx-tag'} data-hue={palette} ref={forwardedRef} />;
-});
+export const Tag = forwardRef<HTMLSpanElement, TagProps>(
+  ({ asChild, palette = 'neutral', classNames, ...props }, forwardedRef) => {
+    const { tx } = useThemeContext();
+    const Root = asChild ? Slot : Primitive.span;
+    return (
+      <Root
+        {...props}
+        className={tx('tag.root', 'dx-tag', { palette }, classNames)}
+        data-hue={palette}
+        ref={forwardedRef}
+      />
+    );
+  },
+);
