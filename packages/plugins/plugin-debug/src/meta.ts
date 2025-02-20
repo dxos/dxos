e9bb01bc18320d250ec8aa05e5839082115468b3
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-02-20 10:53:01 -0800

    chore(tables): Storybook cleanup (#8630)

--- packages/plugins/plugin-debug/src/meta.ts
+++ packages/plugins/plugin-debug/src/meta.ts
@@ -9,7 +9,8 @@ export const DEBUG_PLUGIN = 'dxos.org/plugin/debug';
 export const meta = {
   id: DEBUG_PLUGIN,
   name: 'Debug',
-  description: `The debug plugin is useful for troubleshooting inside of Composer. You can also use the Debug plugin to create test data inside of sheets or tables or explore pre-built automations. Non-technical users will likely not spend much time here, but this plugin is invaluable for developers and technical users.`,
+  description:
+    'The debug plugin is useful for troubleshooting inside of Composer. You can also use the Debug plugin to create test data inside of sheets or tables or explore pre-built automations. Non-technical users will likely not spend much time here, but this plugin is invaluable for developers and technical users.',
   icon: 'ph--bug--regular',
   source: 'https://github.com/dxos/dxos/tree/main/packages/plugins/plugin-debug',
   tags: ['experimental'],
