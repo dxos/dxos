071a3e2d4c90675681e1da3b346066e7e2ebe1cb
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-02-21 17:04:53 +0200

    Fix calls

--- packages/plugins/experimental/plugin-calls/src/types/types.ts
+++ packages/plugins/experimental/plugin-calls/src/types/types.ts
@@ -9,8 +9,8 @@ import { CALLS_PLUGIN } from '../meta';
 /**
  * Endpoint to the calls service.
  */
-// export const CALLS_URL = 'https://calls-service.dxos.workers.dev';
-export const CALLS_URL = 'http://localhost:8787';
+export const CALLS_URL = 'https://calls-service.dxos.workers.dev';
+// export const CALLS_URL = 'http://localhost:8787';
 const CALLS_ACTION = `${CALLS_PLUGIN}/action`;
 
 export enum CallsAction {
