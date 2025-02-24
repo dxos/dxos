a5c631f0f86092b8bf9901f5e978e4bc314b5d3c
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-02-24 22:47:51 +0200

    Index

--- packages/plugins/experimental/plugin-calls/src/capabilities/index.ts
+++ packages/plugins/experimental/plugin-calls/src/capabilities/index.ts
@@ -6,4 +6,7 @@ import { lazy } from '@dxos/app-framework';
 
 export const AppGraphBuilder = lazy(() => import('./app-graph-builder'));
 export const IntentResolver = lazy(() => import('./intent-resolver'));
+export const ReactContext = lazy(() => import('./react-context'));
 export const ReactSurface = lazy(() => import('./react-surface'));
+
+export * from './capabilities';
