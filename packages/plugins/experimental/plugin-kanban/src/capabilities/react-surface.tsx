a4ce20ce3be970a267c69a2935c52cff007733f2
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-02-24 15:51:08 -0800

    Supply initial schema and pivot column when creating new kanban

--- packages/plugins/experimental/plugin-kanban/src/capabilities/react-surface.tsx
+++ packages/plugins/experimental/plugin-kanban/src/capabilities/react-surface.tsx
@@ -2,14 +2,19 @@
 // Copyright 2025 DXOS.org
 //
 
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Capabilities, contributes, createSurface } from '@dxos/app-framework';
+import { type S } from '@dxos/echo-schema';
+import { findAnnotation } from '@dxos/effect';
+import { type CollectionType } from '@dxos/plugin-space/types';
+import { getSpace, isSpace, type Space } from '@dxos/react-client/echo';
+import { type InputProps, SelectInput, useFormValues } from '@dxos/react-ui-form';
 import { type KanbanType } from '@dxos/react-ui-kanban';
 
 import { KanbanContainer, KanbanViewEditor } from '../components';
 import { KANBAN_PLUGIN } from '../meta';
-import { isKanban } from '../types';
+import { isKanban, InitialSchemaAnnotationId, InitialPivotColumnAnnotationId } from '../types';
 
 export default () =>
   contributes(Capabilities.ReactSurface, [
@@ -24,5 +29,66 @@ export default () =>
       role: 'complementary--settings',
       filter: (data): data is { subject: KanbanType } => isKanban(data.subject),
       component: ({ data }) => <KanbanViewEditor kanban={data.subject} />,
+    }),
+    createSurface({
+      id: `${KANBAN_PLUGIN}/create-initial-schema-form-[schema]`,
+      role: 'form-input',
+      filter: (data): data is { prop: string; schema: S.Schema<any>; target: Space | CollectionType | undefined } => {
+        const annotation = findAnnotation<boolean>((data.schema as S.Schema.All).ast, InitialSchemaAnnotationId);
+        return !!annotation;
+      },
+      component: ({ data: { target }, ...inputProps }) => {
+        const props = inputProps as any as InputProps;
+        const space = isSpace(target) ? target : getSpace(target);
+        if (!space) {
+          return null;
+        }
+        const schemata = space?.db.schemaRegistry.query().runSync();
+
+        return <SelectInput {...props} options={schemata.map((schema) => ({ value: schema.typename }))} />;
+      },
+    }),
+    createSurface({
+      id: `${KANBAN_PLUGIN}/create-initial-schema-form-[pivot-column]`,
+      role: 'form-input',
+      filter: (data): data is { prop: string; schema: S.Schema<any>; target: Space | CollectionType | undefined } => {
+        const annotation = findAnnotation<boolean>((data.schema as S.Schema.All).ast, InitialPivotColumnAnnotationId);
+        return !!annotation;
+      },
+      component: ({ data: { target }, ...inputProps }) => {
+        const props = inputProps as any as InputProps;
+        const space = isSpace(target) ? target : getSpace(target);
+        if (!space) {
+          return null;
+        }
+        const schemata = space?.db.schemaRegistry.query().runSync();
+        const { initialSchema } = useFormValues();
+
+        const selectedSchema = useMemo(
+          () => schemata.find((schema) => schema.typename === initialSchema),
+          [schemata, initialSchema],
+        );
+
+        const singleSelectColumns = useMemo(() => {
+          if (!selectedSchema?.jsonSchema?.properties) {
+            return [];
+          }
+
+          const columns = Object.entries(selectedSchema.jsonSchema.properties).reduce<string[]>((acc, [key, value]) => {
+            if (typeof value === 'object' && value?.format === 'single-select') {
+              acc.push(key);
+            }
+            return acc;
+          }, []);
+
+          return columns;
+        }, [selectedSchema?.jsonSchema]);
+
+        if (!initialSchema) {
+          return null;
+        }
+
+        return <SelectInput {...props} options={singleSelectColumns.map((column) => ({ value: column }))} />;
+      },
     }),
   ]);
