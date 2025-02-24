80e55bd5eb9118f9e9d1e915cfb60ca88d850de1
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-02-24 19:37:41 +0200

    Delete scale down parameter

--- packages/plugins/experimental/plugin-calls/src/components/CallContextProvider.tsx
+++ packages/plugins/experimental/plugin-calls/src/components/CallContextProvider.tsx
@@ -32,7 +32,6 @@ export const CallContextProvider: FC<CallContextProviderProps> = ({ children, ro
   const iceServers = config.get('runtime.services.ice') ?? [];
   const maxWebcamFramerate = 24;
   const maxWebcamBitrate = 120_0000;
-  const maxWebcamQualityLevel = 1_080;
 
   const room = useCall({ roomId });
   const userMedia = useUserMedia();
@@ -45,19 +44,10 @@ export const CallContextProvider: FC<CallContextProviderProps> = ({ children, ro
   const [joined, setJoined] = useState(false);
   const [dataSaverMode, setDataSaverMode] = useState(false);
 
-  const scaleResolutionDownBy = useMemo(() => {
-    const videoStreamTrack = userMedia.videoTrack;
-    const { height, width } = tryToGetDimensions(videoStreamTrack);
-    // We need to do this in case camera is in portrait mode.
-    const smallestDimension = Math.min(height, width);
-    return Math.max(smallestDimension / maxWebcamQualityLevel, 1);
-  }, [maxWebcamQualityLevel, userMedia.videoTrack]);
-
   const videoEncodingParams = useStablePojo<RTCRtpEncodingParameters[]>([
     {
       maxFramerate: maxWebcamFramerate,
       maxBitrate: maxWebcamBitrate,
-      scaleResolutionDownBy,
     },
   ]);
   const videoTrackEncodingParams$ = useStateObservable<RTCRtpEncodingParameters[]>(videoEncodingParams);
@@ -122,14 +112,4 @@ const trackObjectToString = (trackObject?: any): string | undefined => {
   }
 
   return trackObject.sessionId + '/' + trackObject.trackName;
-};
-
-const tryToGetDimensions = (videoStreamTrack?: MediaStreamTrack): { height: number; width: number } => {
-  if (!videoStreamTrack || !videoStreamTrack.getCapabilities) {
-    return { height: 0, width: 0 };
-  }
-
-  const height = videoStreamTrack.getCapabilities().height?.max ?? 0;
-  const width = videoStreamTrack.getCapabilities().width?.max ?? 0;
-  return { height, width };
 };
