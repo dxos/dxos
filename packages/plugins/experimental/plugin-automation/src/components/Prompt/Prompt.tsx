3cb0a9158e28955d798066cfa13aa393169ae4f9
Rich Burdon <rich.burdon@gmail.com>
2025-02-26 22:57:38 -0500

    feat: chat prompt (#8676)

--- packages/plugins/experimental/plugin-automation/src/components/Prompt/Prompt.tsx
+++ packages/plugins/experimental/plugin-automation/src/components/Prompt/Prompt.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 
 import { type ThemedClassName, useThemeContext } from '@dxos/react-ui';
 import {
+  type BasicExtensionsOptions,
   createBasicExtensions,
   createThemeExtensions,
   useTextEditor,
@@ -15,16 +16,18 @@ import { mx } from '@dxos/react-ui-theme';
 
 import { createAutocompleteExtension, type AutocompleteOptions } from './prompt-autocomplete';
 
-export type PromptProps = ThemedClassName<AutocompleteOptions & Pick<UseTextEditorProps, 'autoFocus'>>;
+export type PromptProps = ThemedClassName<
+  AutocompleteOptions & Pick<UseTextEditorProps, 'autoFocus'> & Pick<BasicExtensionsOptions, 'lineWrapping'>
+>;
 
-export const Prompt = ({ classNames, autoFocus, onEnter, onSuggest }: PromptProps) => {
+export const Prompt = ({ classNames, autoFocus, lineWrapping = false, onEnter, onSuggest }: PromptProps) => {
   const { themeMode } = useThemeContext();
   const { parentRef } = useTextEditor({
     autoFocus,
     extensions: [
       createBasicExtensions({
         bracketMatching: false,
-        lineWrapping: false,
+        lineWrapping,
         placeholder: 'Ask a question...',
       }),
       createThemeExtensions({ themeMode }),
