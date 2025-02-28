50f10d1eea64673746a8ec7c656dfd38f4ec092e
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-02-28 16:44:04 +0200

    Delete RXJS

--- packages/plugins/experimental/plugin-calls/src/hooks/index.ts
+++ packages/plugins/experimental/plugin-calls/src/hooks/index.ts
@@ -2,17 +2,16 @@
 // Copyright 2025 DXOS.org
 //
 
-export * from './utils';
-
 export * from './useAi';
 export * from './useBroadcastStatus';
 export * from './useCallContext';
 export * from './useCallsGlobalContext';
 export * from './useCallState';
+export * from './useCfCallsConnection';
 export * from './useDebugEnabled';
 export * from './useIsSpeaking';
-export * from './useCfCallsConnection';
 export * from './usePullAudioTracks';
 export * from './usePullVideoTrack';
+export * from './useStablePojo';
 export * from './useTranscription';
 export * from './useUserMedia';
