6670a4d37e7c805bf99c13f1b9f0456a70de5d15
thure <will@braneframe.com>
2025-03-03 10:56:28 -0800

    Restore resize handle workaround.

--- packages/plugins/experimental/plugin-automation/src/components/AmbientChatDialog/AmbientDialog.tsx
+++ packages/plugins/experimental/plugin-automation/src/components/AmbientChatDialog/AmbientDialog.tsx
@@ -11,25 +11,29 @@ import { AUTOMATION_PLUGIN } from '../../meta';
 
 const preventDefault = (event: Event) => event.preventDefault();
 
+const minSize = 5;
+
 export const AmbientDialog = ({ children }: PropsWithChildren) => {
   const [size, setSize] = useState<Size>('min-content');
   const [open, setOpen] = useState(true);
+  // TODO(thure): This seems like a smell, but ResizeHandle uses a ref internally in order to avoid rerendering while dragging. Consider refactoring to use Lit.
+  const [iter, setIter] = useState(0);
 
   // TODO(burdon): Animate open/close.
   // NOTE: We set the min size to 5rem (80px), and the header and prompt bar to 40px each.
   // The dialog has no vertical padding and has box-content so that when closed it collapses to the size of the header and prompt bar.
-  const minSize = 5;
   const handleToggle = useCallback(() => {
     setOpen((open) => {
       setSize(open ? minSize : 'min-content');
+      setIter((iter) => iter + 1);
       return !open;
     });
-  }, []);
+  }, [iter]);
 
   return (
     <div role='none' className='dx-dialog__overlay bg-transparent pointer-events-none' data-block-align='end'>
       <Dialog.Content
-        classNames='relative box-content py-0 px-2 is-[35rem] max-is-none overflow-hidden pointer-events-auto'
+        classNames='relative box-content py-0 px-2 md:is-[35rem] md:max-is-none overflow-hidden pointer-events-auto'
         inOverlayLayout
         {...resizeAttributes}
         style={{
@@ -39,6 +43,7 @@ export const AmbientDialog = ({ children }: PropsWithChildren) => {
         onInteractOutside={preventDefault}
       >
         <ResizeHandle
+          key={iter}
           side='block-start'
           defaultSize='min-content'
           minSize={minSize}
@@ -61,8 +66,8 @@ export const AmbientDialog = ({ children }: PropsWithChildren) => {
 const DialogHeader = ({ open, onToggle }: { open: boolean; onToggle: () => void }) => {
   const { t } = useTranslation(AUTOMATION_PLUGIN);
   return (
-    <div className='flex shrink-0 w-full grid grid-cols-[40px_1fr_40px] items-center overflow-hidden'>
-      <div className='flex w-[40px] h-[40px] items-center justify-center'>
+    <div className='flex shrink-0 w-full grid grid-cols-[var(--rail-action)_1fr_var(--rail-action)] items-center overflow-hidden'>
+      <div className='flex is-[--rail-action] bs-[--rail-action] items-center justify-center'>
         <Dialog.Close>
           <Icon icon='ph--x--regular' />
         </Dialog.Close>
@@ -70,12 +75,12 @@ const DialogHeader = ({ open, onToggle }: { open: boolean; onToggle: () => void 
       <div className='grow'>
         <Dialog.Title classNames='sr-only'>{t('ambient chat dialog title')}</Dialog.Title>
       </div>
-      <div className='flex w-[40px] h-[40px] items-center justify-center'>
+      <div className='flex w-[--rail-action] h-[--rail-action] items-center justify-center'>
         <IconButton
           variant='ghost'
           icon={open ? 'ph--caret-down--regular' : 'ph--caret-up--regular'}
           iconOnly
-          label='Shrink'
+          label={t('collapse label')}
           onClick={onToggle}
         />
       </div>
