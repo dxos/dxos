6a1095db1399d883f324104fd29d6259c2c228b9
Will Shown <thure@users.noreply.github.com>
2025-03-03 18:22:58 -0800

    fix: restore iOS functionality (#8701)

--- packages/plugins/plugin-deck/src/util/useHoistStatusbar.ts
+++ packages/plugins/plugin-deck/src/util/useHoistStatusbar.ts
@@ -5,6 +5,7 @@
 import { useMemo } from 'react';
 
 import { Capabilities, useCapability } from '@dxos/app-framework';
+import { useThemeContext } from '@dxos/react-ui';
 
 import { DECK_PLUGIN } from '../meta';
 import type { DeckSettingsProps } from '../types';
@@ -12,13 +13,8 @@ import type { DeckSettingsProps } from '../types';
 export const useHoistStatusbar = (breakpoint: string) => {
   const enableIdeStyleStatusbar = useCapability(Capabilities.SettingsStore).getStore<DeckSettingsProps>(DECK_PLUGIN)!
     .value.enableIdeStyleStatusbar;
+  const { safeAreaPadding } = useThemeContext();
   return useMemo(() => {
-    return (
-      breakpoint === 'desktop' &&
-      enableIdeStyleStatusbar &&
-      // NOTE(thure): this last predicate depends on a head script that measures `env(safe-area-bottom)` on resize;
-      // see that of composer-app for an example.
-      document.body.getAttribute('data-safe-area-bottom') === '0'
-    );
-  }, [enableIdeStyleStatusbar, breakpoint]);
+    return breakpoint === 'desktop' && enableIdeStyleStatusbar && safeAreaPadding?.bottom === 0;
+  }, [enableIdeStyleStatusbar, breakpoint, safeAreaPadding?.bottom]);
 };
