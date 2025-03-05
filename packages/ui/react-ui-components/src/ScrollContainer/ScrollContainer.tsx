e0d7274919fac410ed92d565477840b67ee1d617
thure <will@braneframe.com>
2025-03-04 18:19:44 -0800

    Use `flex-col-reverse`.

--- packages/ui/react-ui-components/src/ScrollContainer/ScrollContainer.tsx
+++ packages/ui/react-ui-components/src/ScrollContainer/ScrollContainer.tsx
@@ -2,10 +2,10 @@
 // Copyright 2025 DXOS.org
 //
 
-import React, { type PropsWithChildren, forwardRef, useImperativeHandle, useState, useEffect, Children } from 'react';
+import React, { type PropsWithChildren, forwardRef, useImperativeHandle, useState, Children } from 'react';
 
 import { invariant } from '@dxos/invariant';
-import { ScrollArea, AnchoredOverflow, type ThemedClassName } from '@dxos/react-ui';
+import { type ThemedClassName } from '@dxos/react-ui';
 import { mx } from '@dxos/react-ui-theme';
 
 export interface ScrollController {
@@ -15,7 +15,6 @@ export interface ScrollController {
 export type ScrollContainerProps = ThemedClassName<
   PropsWithChildren<{
     fade?: boolean;
-    // TODO(thure): Restore autoScroll prop.
   }>
 >;
 
@@ -26,45 +25,29 @@ export const ScrollContainer = forwardRef<ScrollController, ScrollContainerProps
   ({ children, classNames, fade }, forwardedRef) => {
     const [viewport, setViewport] = useState<HTMLDivElement | null>(null);
 
-    // Controller.
+    // Scroll controller imperative ref
     useImperativeHandle(
       forwardedRef,
       () => ({
         scrollToBottom: () => {
           invariant(viewport);
           // NOTE: Should be instant otherwise scrollHeight might be out of date.
-          viewport.scrollTo({ top: viewport.scrollHeight, behavior: 'instant' });
+          viewport.scrollTo({ top: 0, behavior: 'instant' });
         },
       }),
       [viewport],
     );
 
-    useEffect(() => {
-      if (viewport) {
-        // debugger;
-      }
-    }, [viewport]);
-
-    useEffect(() => {
-      console.log('[child added]');
-    }, [Children.count(children)]);
-
     return (
-      <ScrollArea.Root classNames={mx('relative flex flex-col grow overflow-hidden', classNames)}>
-        {fade && viewport && (
+      <div className={mx('relative flex flex-col-reverse grow overflow-y-auto', classNames)} ref={setViewport}>
+        {fade && (
           <div
             role='none'
             className='z-10 absolute block-start-0 inset-inline-0 bs-24 pointer-events-none bg-gradient-to-b from-[--surface-bg] to-transparent pointer-events-none'
           />
         )}
-        <ScrollArea.Viewport ref={setViewport} classNames='overflow-anchored'>
-          {children}
-          <AnchoredOverflow.Anchor />
-        </ScrollArea.Viewport>
-        <ScrollArea.Scrollbar orientation='vertical'>
-          <ScrollArea.Thumb />
-        </ScrollArea.Scrollbar>
-      </ScrollArea.Root>
+        {[...Children.toArray(children)].reverse()}
+      </div>
     );
   },
 );
