f924c0a0ebac1e6f70d68cc89a9004fcdf480584
Dmytro Maretskyi <maretskii@gmail.com>
2025-03-05 14:06:19 +0100

    Fix prompt

--- packages/plugins/plugin-map/src/capabilities/artifact.ts
+++ packages/plugins/plugin-map/src/capabilities/artifact.ts
@@ -27,7 +27,7 @@ export default () => {
     id: 'plugin-map',
     instructions: `
     Maps:
-    - If the user's message relates to a map, you must return the map as a valid GeoJSON Point with valid coordinates.
+    - If the user's message relates to a map, you must return the map as a valid GeoJSON Point (longitude, latitude) with valid coordinates.
     - If the user's message relates to a collection of points (like in a table) you can specify the typename and the map
         will render and center on those markers.
     `,
