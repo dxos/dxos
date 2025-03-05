1e348a39e39877f7c174fe13b396211d9f9a9adb
Dmytro Maretskyi <maretskii@gmail.com>
2025-03-05 12:21:50 +0100

    Make tool list display functions and services

--- packages/plugins/experimental/plugin-automation/src/components/Toolbox/Toolbox.tsx
+++ packages/plugins/experimental/plugin-automation/src/components/Toolbox/Toolbox.tsx
@@ -2,24 +2,29 @@
 // Copyright 2025 DXOS.org
 //
 
-import React, { Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Capabilities, useCapabilities } from '@dxos/app-framework';
-import { type ArtifactDefinition } from '@dxos/artifact';
+import { type ArtifactDefinition, type Tool } from '@dxos/artifact';
 import { FunctionType } from '@dxos/functions';
+import { log } from '@dxos/log';
 import { Filter, type Space, useQuery } from '@dxos/react-client/echo';
 import { type ThemedClassName } from '@dxos/react-ui';
 import { mx } from '@dxos/react-ui-theme';
 
+import { createToolsFromService } from '../../tools';
+import { ServiceType } from '../../types';
+
 export type ToolboxProps = ThemedClassName<{
   artifacts?: ArtifactDefinition[];
   functions?: FunctionType[];
+  services?: { service: ServiceType; tools: Tool[] }[];
   striped?: boolean;
 }>;
 
 const stripClassNames = 'odd:bg-neutral-50 dark:odd:bg-neutral-800';
 
-export const Toolbox = ({ classNames, artifacts, functions, striped }: ToolboxProps) => {
+export const Toolbox = ({ classNames, artifacts, functions, services, striped }: ToolboxProps) => {
   return (
     <div className={mx('flex overflow-hidden box-content py-2', classNames)}>
       <div className='grid grid-cols-[6rem_8rem_1fr] overflow-y-auto'>
@@ -35,14 +40,28 @@ export const Toolbox = ({ classNames, artifacts, functions, striped }: ToolboxPr
             </div>
           )),
         )}
-        <div className={mx('col-span-full grid grid-cols-subgrid text-xs px-2', striped && stripClassNames)}>
-          {functions?.map(({ name, description }) => (
-            <Fragment key={name}>
-              <div className='text-primary-500 truncate'>{name}</div>
+        {functions?.map(({ name, description }) => (
+          <div
+            key={name}
+            className={mx('col-span-full grid grid-cols-subgrid text-xs px-2', striped && stripClassNames)}
+          >
+            <div className='text-primary-500 truncate'>function</div>
+            <div className='truncate'>{name}</div>
+            <div className='truncate'>{description}</div>
+          </div>
+        ))}
+        {services?.map(({ service, tools }) =>
+          tools.map(({ name, description }, i) => (
+            <div
+              key={`${service.id}-${name}`}
+              className={mx('col-span-full grid grid-cols-subgrid text-xs px-2', striped && stripClassNames)}
+            >
+              <div className='text-primary-500 truncate'>{i === 0 && service.serviceId}</div>
+              <div className='truncate'>{name}</div>
               <div className='truncate'>{description}</div>
-            </Fragment>
-          ))}
-        </div>
+            </div>
+          )),
+        )}
       </div>
     </div>
   );
@@ -51,5 +70,20 @@ export const Toolbox = ({ classNames, artifacts, functions, striped }: ToolboxPr
 export const ToolboxContainer = ({ classNames, space }: ThemedClassName<{ space?: Space }>) => {
   const artifactDefinitions = useCapabilities(Capabilities.ArtifactDefinition);
   const functions = useQuery(space, Filter.schema(FunctionType));
-  return <Toolbox classNames={classNames} artifacts={artifactDefinitions} functions={functions} />;
+
+  const services = useQuery(space, Filter.schema(ServiceType));
+  const [serviceTools, setServiceTools] = useState<{ service: ServiceType; tools: Tool[] }[]>([]);
+  useEffect(() => {
+    log('creating service tools...');
+    queueMicrotask(async () => {
+      const tools = await Promise.all(
+        services.map(async (service) => ({ service, tools: await createToolsFromService(service) })),
+      );
+      setServiceTools(tools);
+    });
+  }, [services]);
+
+  return (
+    <Toolbox classNames={classNames} artifacts={artifactDefinitions} functions={functions} services={serviceTools} />
+  );
 };
