f75598fe44b88d44a7296f8cb9a7fe68f9662355
Rich Burdon <rich.burdon@gmail.com>
2025-03-09 02:53:41 +0700

    chore: ux tweaks (#8748)

--- packages/core/echo/live-object/src/proxy/getter.ts
+++ packages/core/echo/live-object/src/proxy/getter.ts
@@ -12,7 +12,9 @@ export const isDeleted = <T extends BaseObject>(obj: T): boolean => {
   return getProxyHandler(obj).isDeleted(obj) ?? false;
 };
 
-// TODO(burdon): Replace most uses with getTypename.
+/**
+ * @deprecated Use `getTypename` instead.
+ */
 export const getType = <T extends BaseObject>(obj: T | undefined): Reference | undefined => {
   if (obj == null) {
     return undefined;
@@ -24,12 +26,12 @@ export const getType = <T extends BaseObject>(obj: T | undefined): Reference | u
   return undefined;
 };
 
-// TODO(burdon): Move to echo-schema.
 export const getTypename = <T extends BaseObject>(obj: T): string | undefined => {
   const schema = getSchema(obj);
   // Special handling for EchoSchema. objectId is StoredSchema objectId, not a typename.
   if (schema && typeof schema === 'object' && SchemaMetaSymbol in schema) {
     return (schema as any)[SchemaMetaSymbol].typename;
   }
+
   return getType(obj)?.objectId;
 };
