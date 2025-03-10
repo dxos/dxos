a061803d3869f263f854e10559218cf6f751e2e0
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-03-10 16:22:51 -0700

    Include tags in plugin registry items

--- packages/plugins/plugin-registry/src/components/PluginItem.tsx
+++ packages/plugins/plugin-registry/src/components/PluginItem.tsx
@@ -5,7 +5,7 @@
 import React, { useCallback } from 'react';
 
 import { type Plugin } from '@dxos/app-framework';
-import { Icon, Input, Link, ListItem, useTranslation } from '@dxos/react-ui';
+import { Icon, Input, Link, ListItem, Tag, useTranslation } from '@dxos/react-ui';
 import { descriptionText, mx } from '@dxos/react-ui-theme';
 
 import { REGISTRY_PLUGIN } from '../meta';
@@ -53,12 +53,12 @@ export const PluginItem = ({
       labelId={labelId}
       data-testid={`pluginList.${id}`}
       aria-describedby={descriptionId}
-      classNames='w-full h-full grid grid-cols-[48px_1fr_48px] grid-rows-[40px_1fr] p-1 rounded-md border border-separator'
+      classNames='is-full bs-full grid grid-cols-[48px_1fr_48px] grid-rows-[40px_1fr] p-1 rounded-md border border-separator shadow-sm'
     >
-      <div className='flex grow justify-center items-center'>
+      <div className='flex flex-col grow justify-center items-center'>
         <Icon icon={icon} size={6} onClick={handleClick} classNames='text-subdued cursor-pointer' />
       </div>
-      <div className='flex grow items-center truncate cursor-pointer' onClick={handleClick}>
+      <div className='flex grow items-center text-xl font-light truncate cursor-pointer' onClick={handleClick}>
         {name ?? id}
       </div>
       <div className='flex grow justify-center items-center'>
@@ -69,9 +69,18 @@ export const PluginItem = ({
 
       <div />
       {(description || homePage || source) && (
-        <div id={descriptionId} className='col-span-2 flex flex-col w-full gap-2 pb-3'>
+        <div id={descriptionId} className='col-span-2 flex flex-col w-full justify-between gap-2 pb-2'>
+          {plugin?.meta?.tags && plugin.meta.tags.length > 0 && (
+            <div>
+              {plugin.meta.tags.map((tag) => (
+                <Tag key={tag} palette={'indigo'} classNames='text-xs capitalize'>
+                  {tag}
+                </Tag>
+              ))}
+            </div>
+          )}
           <div className='grow'>
-            <p className={mx(descriptionText, 'line-clamp-3 min-w-0 pie-4')}>{description}</p>
+            <p className={mx(descriptionText, 'line-clamp-3 min-w-0 pie-2')}>{description}</p>
           </div>
 
           <div className='flex gap-2 items-center'>
