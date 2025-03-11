fd5c91120fee79a68b75be33cfea760ebcadb2ae
Josiah Witt <josiah@witt.life>
2025-03-11 14:07:32 -0400

    refactor: separate assistant from automations (#8760)

---
+++ packages/plugins/experimental/plugin-assistant/src/capabilities/capabilities.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { defineCapability } from '@dxos/app-framework';
+import { type AIServiceClientImpl } from '@dxos/assistant';
+
+import { ASSISTANT_PLUGIN } from '../meta';
+
+export namespace AssistantCapabilities {
+  export const AiClient = defineCapability<AIServiceClientImpl>(`${ASSISTANT_PLUGIN}/capability/ai-client`);
+}
