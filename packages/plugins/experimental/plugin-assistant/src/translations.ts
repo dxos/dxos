4c7ffa66f3a109d24ab4bfbbaf197ab0bbcb8fa9
Josiah Witt <josiah@witt.life>
2025-03-11 19:30:38 -0400

    fix: automation panel improvements (#8775)

--- packages/plugins/experimental/plugin-assistant/src/translations.ts
+++ packages/plugins/experimental/plugin-assistant/src/translations.ts
@@ -14,7 +14,6 @@ export default [
       [TemplateType.typename]: {
         'typename label': 'Template',
       },
-      // TODO(wittjosiah): Audit translations, most appear unused.
       [ASSISTANT_PLUGIN]: {
         'chat title placeholder': 'AI Chat',
         'template title placeholder': 'Template',
@@ -29,8 +28,6 @@ export default [
         'template placeholder': 'Enter template...',
         'value placeholder': 'Enter value...',
         'select preset template placeholder': 'Select preset',
-        'open automation panel label': 'Show Automations',
-        'automation panel label': 'Automations',
         'service registry label': 'Service Registry',
 
         'assistant dialog title': 'Assistant',
@@ -38,32 +35,6 @@ export default [
 
         'chat input placeholder': 'Ask a question...',
         'chat stop': 'Cancel request.',
-
-        'function select label': 'Function',
-        'function select placeholder': 'Select function',
-        'function enabled': 'Enabled',
-
-        'trigger select label': 'Trigger',
-        'trigger select placeholder': 'Trigger type',
-
-        'trigger type timer': 'Timer',
-        'trigger type webhook': 'Webhook',
-        'trigger type websocket': 'Websocket',
-        'trigger type subscription': 'Subscription',
-        'trigger type email': 'Email',
-        'trigger type queue': 'Queue',
-
-        'trigger filter': 'Filter',
-        'trigger cron': 'Cron',
-        'trigger method': 'Method',
-        'trigger endpoint': 'Endpoint',
-
-        'trigger copy url': 'Copy URL',
-        'trigger copy email': 'Copy Email',
-
-        'trigger meta add': 'Add',
-        'trigger meta remove': 'Remove',
-        'trigger meta prop name placeholder': 'New meta property name',
 
         'prompt placeholder': 'Ask a question...',
         'microphone button': 'Click to speak',
