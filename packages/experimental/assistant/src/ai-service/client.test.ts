9d7c4d05d155116586f483c0e9aa6699fbffa4c7
Dmytro Maretskyi <maretskii@gmail.com>
2025-03-12 17:40:56 +0100

    WIP

--- packages/experimental/assistant/src/ai-service/client.test.ts
+++ packages/experimental/assistant/src/ai-service/client.test.ts
@@ -18,8 +18,12 @@ import { AI_SERVICE_ENDPOINT } from '../testing';
 
 // log.config({ filter: 'debug' });
 
+const MODEL = '@cf/deepseek-ai/deepseek-r1-distill-qwen-32b';
+// const MODEL = '@hf/nousresearch/hermes-2-pro-mistral-7b';
+// const MODEL = DEFAULT_LLM_MODEL;
+
 describe('AI Service Client', () => {
-  test.only('client generation', async () => {
+  test('client generation', async () => {
     const client = new AIServiceClientImpl({
       endpoint: AI_SERVICE_ENDPOINT.LOCAL,
     });
@@ -28,9 +32,7 @@ describe('AI Service Client', () => {
     const threadId = ObjectId.random();
 
     const stream = await client.exec({
-      // model: DEFAULT_LLM_MODEL,
-      // model: '@cf/deepseek-ai/deepseek-r1-distill-qwen-32b',
-      model: '@hf/nousresearch/hermes-2-pro-mistral-7b',
+      model: MODEL,
       spaceId,
       threadId,
       systemPrompt: 'You are a poet',
@@ -52,7 +54,7 @@ describe('AI Service Client', () => {
     });
   });
 
-  test('tool calls', async () => {
+  test.only('tool calls', async () => {
     const client = new AIServiceClientImpl({
       endpoint: AI_SERVICE_ENDPOINT.LOCAL,
     });
@@ -67,72 +69,64 @@ describe('AI Service Client', () => {
       ),
     };
 
-    const spaceId = SpaceId.random();
-    const threadId = ObjectId.random();
-
-    await client.appendMessages([
-      {
-        id: ObjectId.random(),
-        spaceId,
-        threadId,
-        role: 'user',
-        content: [{ type: 'text', text: 'What is the password? Ask the custodian' }],
-      },
-    ]);
-
     {
       const stream1 = await client.exec({
-        model: DEFAULT_LLM_MODEL,
-        spaceId,
-        threadId,
+        model: MODEL,
         systemPrompt: 'You are a helpful assistant.',
+        history: [
+          createStatic(Message, {
+            role: 'user',
+            content: [{ type: 'text', text: 'Call the custodian tool' }],
+            // content: [{ type: 'text', text: 'What tools do you have?' }],
+          }),
+        ],
         tools: [custodian],
       });
 
       for await (const event of stream1) {
-        log('event', event);
+        log.info('event', event);
       }
 
       // TODO(burdon): !!!
       await stream1.complete();
-      const messages: Message[] = [];
+      // const messages: Message[] = [];
 
-      const [message1] = messages;
-      log('full message', { message: message1 });
-      await client.appendMessages([message1]);
+      // const [message1] = messages;
+      // log('full message', { message: message1 });
+      // await client.appendMessages([message1]);
 
-      const toolUse = message1.content.find(({ type }) => type === 'tool_use')!;
-      invariant(toolUse.type === 'tool_use');
-      await client.appendMessages([
-        {
-          id: ObjectId.random(),
-          spaceId,
-          threadId,
-          role: 'user',
-          content: [{ type: 'tool_result', toolUseId: toolUse.id, content: 'password="The sky is blue"' }],
-        },
-      ]);
+      // const toolUse = message1.content.find(({ type }) => type === 'tool_use')!;
+      // invariant(toolUse.type === 'tool_use');
+      // await client.appendMessages([
+      //   {
+      //     id: ObjectId.random(),
+      //     spaceId,
+      //     threadId,
+      //     role: 'user',
+      //     content: [{ type: 'tool_result', toolUseId: toolUse.id, content: 'password="The sky is blue"' }],
+      //   },
+      // ]);
     }
 
-    {
-      const stream2 = await client.exec({
-        model: DEFAULT_LLM_MODEL,
-        spaceId,
-        threadId,
-        systemPrompt: 'You are a helpful assistant.',
-        tools: [custodian],
-      });
+    // {
+    //   const stream2 = await client.exec({
+    //     model: DEFAULT_LLM_MODEL,
+    //     spaceId,
+    //     threadId,
+    //     systemPrompt: 'You are a helpful assistant.',
+    //     tools: [custodian],
+    //   });
 
-      for await (const event of stream2) {
-        log('event', event);
-      }
+    //   for await (const event of stream2) {
+    //     log('event', event);
+    //   }
 
-      await stream2.complete();
-      const messages: Message[] = [];
+    //   await stream2.complete();
+    //   const messages: Message[] = [];
 
-      const [message2] = messages;
-      log('full message', { message: message2 });
-    }
+    //   const [message2] = messages;
+    //   log('full message', { message: message2 });
+    // }
   });
 
   test.skip('image generation', async () => {
