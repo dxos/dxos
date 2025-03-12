74dc13ff266677cab89cf817e06dc600eaf62391
yarolegovich <yarolegovich@gmail.com>
2025-03-12 14:17:04 +0200

    fix encoding

--- packages/core/echo/echo-schema/src/object/json-serializer.ts
+++ packages/core/echo/echo-schema/src/object/json-serializer.ts
@@ -2,11 +2,7 @@
 // Copyright 2025 DXOS.org
 //
 
-import { Reference, encodeReference } from '@dxos/echo-protocol';
-import { deepMapValues } from '@dxos/util';
-
 import { ECHO_ATTR_TYPE, TYPENAME_SYMBOL } from './typename';
-import { Ref } from '../ast';
 
 export const attachTypedJsonSerializer = (obj: any) => {
   const descriptor = Object.getOwnPropertyDescriptor(obj, 'toJSON');
@@ -24,15 +20,10 @@ export const attachTypedJsonSerializer = (obj: any) => {
 
 // NOTE: KEEP as function.
 const typedJsonSerializer = function (this: any, key: string, value: any) {
-  const { id, ...rest } = deepMapValues(this, (value, recurse) => {
-    if (Ref.isRef(value)) {
-      return encodeReference(Reference.fromDXN(value.dxn));
-    }
-    return recurse(value);
-  });
+  const { id, [TYPENAME_SYMBOL]: typename, ...rest } = this;
   return {
     id,
-    [ECHO_ATTR_TYPE]: this[TYPENAME_SYMBOL],
+    [ECHO_ATTR_TYPE]: typename,
     ...rest,
   };
 };
--- packages/core/echo/live-object/src/ref/ref-encoding.test.ts
+++ packages/core/echo/live-object/src/ref/ref-encoding.test.ts
@@ -18,7 +18,12 @@ describe('ref encoding', () => {
     expect(json).toEqual({
       id: contact.id,
       '@type': `dxn:type:${Contact.typename}:${Contact.version}`,
-      tasks: [encodeReference(Reference.fromDXN(makeRef(task).dxn))],
+      tasks: [
+        {
+          ...encodeReference(Reference.fromDXN(makeRef(task).dxn)),
+          target: JSON.parse(JSON.stringify(task)),
+        },
+      ],
     });
   });
 });
