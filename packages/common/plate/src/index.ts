b3fb9f7108cbf47c654987886f19ec6f9c209aa0
Josiah Witt <josiah@witt.life>
2025-03-12 11:30:26 -0400

    refactor: remove circular imports
    Removes circular imports in plate which were causing the docs build to
    fail.

--- packages/common/plate/src/index.ts
+++ packages/common/plate/src/index.ts
@@ -5,6 +5,7 @@
 export * from 'zod';
 export * from './api';
 export * from './DirectoryTemplate';
+export * from './InteractiveDirectoryTemplate';
 export * from './util/templateLiterals';
 export {
   type InputOf,
