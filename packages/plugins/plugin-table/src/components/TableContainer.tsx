9d4c4e0afc9551987504415e44082a030e68d470
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-03-12 14:10:44 -0700

    fix(table): resolve storybook crash (#8790)

--- packages/plugins/plugin-table/src/components/TableContainer.tsx
+++ packages/plugins/plugin-table/src/components/TableContainer.tsx
@@ -96,7 +96,7 @@ const TableContainer = ({ role, table }: { role?: string; table: TableType }) =>
         }
       }
     },
-    [dispatch, space, schema, table, model, handleThreadCreate, handleInsertRow],
+    [handleThreadCreate, handleInsertRow, model],
   );
 
   return (
