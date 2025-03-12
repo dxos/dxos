c38fe8899f9ea5e533f99280b5fe0e3713eaf09b
yarolegovich <yarolegovich@gmail.com>
2025-03-12 14:04:46 +0200

    fix static object ref encoding

--- packages/core/echo/echo-schema/src/object/json-serializer.ts
+++ packages/core/echo/echo-schema/src/object/json-serializer.ts
@@ -2,7 +2,11 @@
 // Copyright 2025 DXOS.org
 //
 
+import { Reference, encodeReference } from '@dxos/echo-protocol';
+import { deepMapValues } from '@dxos/util';
+
 import { ECHO_ATTR_TYPE, TYPENAME_SYMBOL } from './typename';
+import { Ref } from '../ast';
 
 export const attachTypedJsonSerializer = (obj: any) => {
   const descriptor = Object.getOwnPropertyDescriptor(obj, 'toJSON');
@@ -20,10 +24,15 @@ export const attachTypedJsonSerializer = (obj: any) => {
 
 // NOTE: KEEP as function.
 const typedJsonSerializer = function (this: any, key: string, value: any) {
-  const { id, [TYPENAME_SYMBOL]: typename, ...rest } = this;
+  const { id, ...rest } = deepMapValues(this, (value, recurse) => {
+    if (Ref.isRef(value)) {
+      return encodeReference(Reference.fromDXN(value.dxn));
+    }
+    return recurse(value);
+  });
   return {
     id,
-    [ECHO_ATTR_TYPE]: typename,
+    [ECHO_ATTR_TYPE]: this[TYPENAME_SYMBOL],
     ...rest,
   };
 };
