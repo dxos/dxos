bcfcddb880bd09ceed161eb9a9c864684d06ae57
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-03-18 11:34:29 +0200

    Fix query request

--- packages/core/mesh/messaging/src/signal-manager/signal-manager.ts
+++ packages/core/mesh/messaging/src/signal-manager/signal-manager.ts
@@ -4,9 +4,13 @@
 
 import { type Event } from '@dxos/async';
 import { type Lifecycle } from '@dxos/context';
-import { type QueryRequest } from '@dxos/protocols/proto/dxos/echo/query';
 import { type SwarmResponse, type Peer } from '@dxos/protocols/proto/dxos/edge/messenger';
-import { type LeaveRequest, type JoinRequest, type Message } from '@dxos/protocols/proto/dxos/mesh/signal';
+import {
+  type LeaveRequest,
+  type JoinRequest,
+  type Message,
+  type QueryRequest,
+} from '@dxos/protocols/proto/dxos/mesh/signal';
 
 export type PeerInfo = Peer;
 export const PeerInfoHash = ({ peerKey }: PeerInfo) => peerKey;
@@ -26,17 +30,17 @@ export interface SignalManager extends Required<Lifecycle> {
   /**
    * Join swarmKey on signal network, to be discoverable by other peers.
    */
-  join: (params: JoinRequest) => Promise<void>;
+  join: (request: JoinRequest) => Promise<void>;
 
   /**
    * Leave swarmKey on signal network, to stop being discoverable by other peers.
    */
-  leave: (params: LeaveRequest) => Promise<void>;
+  leave: (request: LeaveRequest) => Promise<void>;
 
   /**
    * Query swarm state without joining it.
    */
-  query: (params: QueryRequest) => Promise<void>;
+  query: (request: QueryRequest) => Promise<void>;
 
   /**
    * Send message to peer.
