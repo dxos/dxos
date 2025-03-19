8576b42074513dd0a4de8e13c8f13b27013ad69d
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-03-19 15:40:18 -0700

    lint

--- packages/ui/react-ui-table/src/components/Table/DynamicTable.tsx
+++ packages/ui/react-ui-table/src/components/Table/DynamicTable.tsx
@@ -4,6 +4,7 @@
 
 import React, { useRef, useMemo } from 'react';
 
+import { mx } from '@dxos/react-ui-theme';
 import { type SchemaPropertyDefinition } from '@dxos/schema';
 
 import { Table, type TableController } from './Table';
@@ -15,10 +16,18 @@ type DynamicTableProps = {
   data: any[];
   properties: SchemaPropertyDefinition[];
   tableName?: string;
-  className?: string;
+  classNames?: string;
 };
 
-export const DynamicTable = ({ data, properties, tableName = 'com.example/dynamic_table' }: DynamicTableProps) => {
+// TODO(ZaymonFC): Consider allowing the user to supply a schema directly instead of deriving it from
+//  the properties array. (Both should be viable).
+
+export const DynamicTable = ({
+  data,
+  properties,
+  classNames,
+  tableName = 'com.example/dynamic_table',
+}: DynamicTableProps) => {
   const { table, viewProjection } = useMemo(() => {
     return makeDynamicTable(tableName, properties);
   }, [tableName, properties]);
@@ -38,7 +47,7 @@ export const DynamicTable = ({ data, properties, tableName = 'com.example/dynami
   const tableRef = useRef<TableController>(null);
 
   return (
-    <div className='is-full bs-full grow grid'>
+    <div className={mx('is-full bs-full grow grid', classNames)}>
       <div className='grid min-bs-0 overflow-hidden'>
         <Table.Root>
           <Table.Main ref={tableRef} model={model} presentation={presentation} ignoreAttention />
