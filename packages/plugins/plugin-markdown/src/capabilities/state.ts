751bc4e8eb23e0bd090adfcf3d2a3434245b2c6e
Josiah Witt <josiah@witt.life>
2025-03-23 10:05:07 +0100

    refactor to consolidate state

--- packages/plugins/plugin-markdown/src/capabilities/state.ts
+++ packages/plugins/plugin-markdown/src/capabilities/state.ts
@@ -4,20 +4,24 @@
 
 import { Capabilities, contributes, type PluginsContext } from '@dxos/app-framework';
 import { LocalStorageStore } from '@dxos/local-storage';
-import { type EditorViewMode, createEditorStateStore, createFoldStateStore } from '@dxos/react-ui-editor';
+import { type EditorSelectionState, type FoldState, type EditorViewMode } from '@dxos/react-ui-editor';
 
 import { MarkdownCapabilities } from './capabilities';
 import { MARKDOWN_PLUGIN } from '../meta';
 import { type MarkdownPluginState, type MarkdownSettingsProps } from '../types';
 
 export default (context: PluginsContext) => {
-  const state = new LocalStorageStore<MarkdownPluginState>(MARKDOWN_PLUGIN, { extensionProviders: [], viewMode: {} });
+  const state = new LocalStorageStore<MarkdownPluginState>(MARKDOWN_PLUGIN, {
+    extensionProviders: [],
+    viewMode: {},
+    selectionState: {},
+    foldState: {},
+  });
 
-  state.prop({ key: 'viewMode', type: LocalStorageStore.json<{ [key: string]: EditorViewMode }>() });
-
-  // TODO(wittjosiah): Fold into state.
-  const editorState = createEditorStateStore(`${MARKDOWN_PLUGIN}/editor`);
-  const foldState = createFoldStateStore(`${MARKDOWN_PLUGIN}/fold`);
+  state
+    .prop({ key: 'viewMode', type: LocalStorageStore.json<{ [key: string]: EditorViewMode }>() })
+    .prop({ key: 'selectionState', type: LocalStorageStore.json<{ [key: string]: EditorSelectionState }>() })
+    .prop({ key: 'foldState', type: LocalStorageStore.json<{ [key: string]: FoldState }>() });
 
   const getViewMode = (id: string) => {
     const defaultViewMode = context
@@ -28,10 +32,26 @@ export default (context: PluginsContext) => {
 
   const setViewMode = (id: string, viewMode: EditorViewMode) => (state.values.viewMode[id] = viewMode);
 
+  // TODO(wittjosiah): Remove.
+  const selectionStateStore = {
+    getState: (id: string) => state.values.selectionState[id],
+    setState: (id: string, selectionState: EditorSelectionState) => {
+      state.values.selectionState[id] = selectionState;
+    },
+  };
+
+  // TODO(wittjosiah): Remove.
+  const foldStateStore = {
+    getState: (id: string) => state.values.foldState[id],
+    setState: (id: string, foldState: FoldState) => {
+      state.values.foldState[id] = foldState;
+    },
+  };
+
   return contributes(MarkdownCapabilities.State, {
     state: state.values,
-    editorState,
-    foldState,
+    editorState: selectionStateStore,
+    foldState: foldStateStore,
     getViewMode,
     setViewMode,
   });
