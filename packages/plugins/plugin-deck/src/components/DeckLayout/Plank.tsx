48072292fe1405d0c22113d2405e646d181a8f06
thure <will@braneframe.com>
2025-03-25 12:31:37 -0600

    Add companion signals to PlankImpl.

--- packages/plugins/plugin-deck/src/components/DeckLayout/Plank.tsx
+++ packages/plugins/plugin-deck/src/components/DeckLayout/Plank.tsx
@@ -48,125 +48,132 @@ export type PlankProps = {
 type PlankImplProps = Omit<PlankProps, 'companionId' | 'part'> & {
   part: ResolvedPart;
   surfaceVariant?: string;
+  companioned?: 'primary' | 'companion';
 };
 
-const PlankImpl = memo(({ id = UNKNOWN_ID, part, path, order, active, layoutMode, surfaceVariant }: PlankImplProps) => {
-  const { dispatchPromise: dispatch } = useIntentDispatcher();
-  const { deck, popoverAnchorId, scrollIntoView } = useCapability(DeckCapabilities.DeckState);
-  const { graph } = useAppGraph();
-  const node = useNode(graph, id);
-  const rootElement = useRef<HTMLDivElement | null>(null);
-  const canResize = layoutMode === 'deck';
-  const Root = part === 'solo' ? 'article' : StackItem.Root;
+const PlankImpl = memo(
+  ({ id = UNKNOWN_ID, part, path, order, active, layoutMode, surfaceVariant, companioned }: PlankImplProps) => {
+    const { dispatchPromise: dispatch } = useIntentDispatcher();
+    const { deck, popoverAnchorId, scrollIntoView } = useCapability(DeckCapabilities.DeckState);
+    const { graph } = useAppGraph();
+    const node = useNode(graph, id);
+    const rootElement = useRef<HTMLDivElement | null>(null);
+    const canResize = layoutMode === 'deck';
+    const Root = part === 'solo' ? 'article' : StackItem.Root;
 
-  const attendableAttrs = useAttendableAttributes(id);
-  const index = active ? active.findIndex((entryId) => entryId === id) : 0;
-  const length = active?.length ?? 1;
-  const canIncrementStart = active && index !== undefined && index > 0 && length !== undefined && length > 1;
-  const canIncrementEnd = active && index !== undefined && index < length - 1 && length !== undefined;
+    const attendableAttrs = useAttendableAttributes(id);
+    const index = active ? active.findIndex((entryId) => entryId === id) : 0;
+    const length = active?.length ?? 1;
+    const canIncrementStart = active && index !== undefined && index > 0 && length !== undefined && length > 1;
+    const canIncrementEnd = active && index !== undefined && index < length - 1 && length !== undefined;
 
-  const key = id.split('+')[0];
-  const size = deck.plankSizing[key] as number | undefined;
-  const setSize = useCallback(
-    debounce((nextSize: number) => {
-      return dispatch(createIntent(DeckAction.UpdatePlankSize, { id: key, size: nextSize }));
-    }, 200),
-    [dispatch, key],
-  );
+    const key = id.split('+')[0];
+    const size = deck.plankSizing[key] as number | undefined;
+    const setSize = useCallback(
+      debounce((nextSize: number) => {
+        return dispatch(createIntent(DeckAction.UpdatePlankSize, { id: key, size: nextSize }));
+      }, 200),
+      [dispatch, key],
+    );
 
-  // TODO(thure): Tabster’s focus group should handle moving focus to Main, but something is blocking it.
-  const handleKeyDown = useCallback((event: KeyboardEvent) => {
-    if (event.target === event.currentTarget && event.key === 'Escape') {
-      rootElement.current?.closest('main')?.focus();
-    }
-  }, []);
+    // TODO(thure): Tabster’s focus group should handle moving focus to Main, but something is blocking it.
+    const handleKeyDown = useCallback((event: KeyboardEvent) => {
+      if (event.target === event.currentTarget && event.key === 'Escape') {
+        rootElement.current?.closest('main')?.focus();
+      }
+    }, []);
 
-  useLayoutEffect(() => {
-    if (scrollIntoView === id) {
-      // TODO(wittjosiah): When focused on page load, the focus is always visible.
-      //   Forcing focus to something smaller than the plank prevents large focus ring in the interim.
-      const focusable = rootElement.current?.querySelector('button') || rootElement.current;
-      focusable?.focus({ preventScroll: true });
-      layoutMode === 'deck' && focusable?.scrollIntoView({ behavior: 'smooth', inline: 'center' });
-      // Clear the scroll into view state once it has been actioned.
-      void dispatch(createIntent(LayoutAction.ScrollIntoView, { part: 'current', subject: undefined }));
-    }
-  }, [id, scrollIntoView, layoutMode]);
+    useLayoutEffect(() => {
+      if (scrollIntoView === id) {
+        // TODO(wittjosiah): When focused on page load, the focus is always visible.
+        //   Forcing focus to something smaller than the plank prevents large focus ring in the interim.
+        const focusable = rootElement.current?.querySelector('button') || rootElement.current;
+        focusable?.focus({ preventScroll: true });
+        layoutMode === 'deck' && focusable?.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+        // Clear the scroll into view state once it has been actioned.
+        void dispatch(createIntent(LayoutAction.ScrollIntoView, { part: 'current', subject: undefined }));
+      }
+    }, [id, scrollIntoView, layoutMode]);
 
-  const isSolo = layoutMode === 'solo' && part === 'solo';
-  const isAttendable = isSolo || (layoutMode === 'deck' && part === 'deck');
+    const isSolo = layoutMode === 'solo' && part === 'solo';
+    const isAttendable = isSolo || (layoutMode === 'deck' && part === 'deck');
 
-  const sizeAttrs = useMainSize();
+    const sizeAttrs = useMainSize();
 
-  const data = useMemo(
-    () =>
-      node && {
-        subject: node.data,
-        variant: surfaceVariant,
-        path,
-        popoverAnchorId,
-      },
-    [node, node?.data, path, popoverAnchorId, surfaceVariant],
-  );
+    const data = useMemo(
+      () =>
+        node && {
+          subject: node.data,
+          variant: surfaceVariant,
+          path,
+          popoverAnchorId,
+        },
+      [node, node?.data, path, popoverAnchorId, surfaceVariant],
+    );
 
-  // TODO(wittjosiah): Change prop to accept a component.
-  const placeholder = useMemo(() => <PlankLoading />, []);
+    // TODO(wittjosiah): Change prop to accept a component.
+    const placeholder = useMemo(() => <PlankLoading />, []);
 
-  const className = mx(
-    'attention-surface relative',
-    isSolo && mainIntrinsicSize,
-    isSolo && railGridHorizontal,
-    isSolo ? 'grid absolute inset-0' : '!border-separator border-li',
-  );
+    const className = mx(
+      'attention-surface relative',
+      isSolo && mainIntrinsicSize,
+      isSolo && railGridHorizontal,
+      isSolo
+        ? 'grid absolute inset-0'
+        : companioned === 'companion'
+          ? '!border-separator border-ie'
+          : '!border-separator border-li',
+    );
 
-  return (
-    <Root
-      ref={rootElement}
-      data-testid='deck.plank'
-      tabIndex={0}
-      {...(part === 'solo'
-        ? ({ ...sizeAttrs, className } as any)
-        : {
-            item: { id },
-            size,
-            onSizeChange: setSize,
-            classNames: className,
-            order,
-            role: 'article',
-          })}
-      {...(isAttendable ? attendableAttrs : {})}
-      onKeyDown={handleKeyDown}
-    >
-      {node ? (
-        <>
-          <NodePlankHeading
-            id={id}
-            part={part}
-            node={node}
-            canIncrementStart={canIncrementStart}
-            canIncrementEnd={canIncrementEnd}
-            popoverAnchorId={popoverAnchorId}
-          />
-          <Surface
-            key={node.id}
-            role='article'
-            data={data}
-            limit={1}
-            fallback={PlankContentError}
-            placeholder={placeholder}
-          />
-        </>
-      ) : (
-        <PlankError id={id} part={part} />
-      )}
-      {canResize && <StackItem.ResizeHandle />}
-    </Root>
-  );
-});
+    return (
+      <Root
+        ref={rootElement}
+        data-testid='deck.plank'
+        tabIndex={0}
+        {...(part === 'solo'
+          ? ({ ...sizeAttrs, className } as any)
+          : {
+              item: { id },
+              size,
+              onSizeChange: setSize,
+              classNames: className,
+              order,
+              role: 'article',
+            })}
+        {...(isAttendable ? attendableAttrs : {})}
+        onKeyDown={handleKeyDown}
+      >
+        {node ? (
+          <>
+            <NodePlankHeading
+              id={id}
+              part={part}
+              node={node}
+              canIncrementStart={canIncrementStart}
+              canIncrementEnd={canIncrementEnd}
+              popoverAnchorId={popoverAnchorId}
+            />
+            <Surface
+              key={node.id}
+              role='article'
+              data={data}
+              limit={1}
+              fallback={PlankContentError}
+              placeholder={placeholder}
+            />
+          </>
+        ) : (
+          <PlankError id={id} part={part} />
+        )}
+        {canResize && <StackItem.ResizeHandle />}
+      </Root>
+    );
+  },
+);
 
 const SplitFrame = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <div role='none' className='contents md:grid md:grid-rows-1 md:grid-cols-[1fr_1fr]'>
+    <div role='none' className='contents md:grid md:grid-rows-subgrid md:grid-cols-[1fr_1fr]'>
       {children}
     </div>
   );
@@ -177,13 +184,14 @@ export const Plank = (props: PlankProps) => {
     const Root = props.part === 'solo' ? SplitFrame : Fragment;
     return (
       <Root>
-        <PlankImpl {...props} {...(props.part === 'solo' ? { part: 'solo-primary' } : {})} />
+        <PlankImpl {...props} {...(props.part === 'solo' ? { part: 'solo-primary' } : {})} companioned='primary' />
         <PlankImpl
           {...props}
           {...(props.companionId.startsWith(surfaceVariantSeparator)
             ? { surfaceVariant: props.companionId.substring(2) }
             : { id: props.companionId })}
           {...(props.part === 'solo' ? { part: 'solo-companion' } : { order: props.order! + 1 })}
+          companioned='companion'
         />
       </Root>
     );
