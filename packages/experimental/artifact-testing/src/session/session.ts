0b6ca65f89d49fab0439be1cb5780398d0228f61
Dmytro Maretskyi <maretskii@gmail.com>
2025-03-26 13:35:26 +0100

    Scaffolding

---
+++ packages/experimental/artifact-testing/src/session/session.ts
@@ -0,0 +1,13 @@
+/**
+ * Contains message history, tools, current context.
+ * Current context means the state of the app, time of day, and other contextual information.
+ * It makes requests to the model, its a state machine.
+ * It keeps track of the current goal.
+ * It manages the context window.
+ * Tracks the success criteria of reaching the goal, exposing metrics (stretch)
+ * Could be run locally in the app or remotely.
+ * Could be personal or shared.
+ */
+export class AISession {
+  
+}
