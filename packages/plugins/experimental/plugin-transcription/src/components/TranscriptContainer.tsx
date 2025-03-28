6ff377e8f10615ab26418cf3e83ff8f170e04bad
Dmytro Maretskyi <maretskii@gmail.com>
2025-03-28 13:51:25 +0100

    Lint

--- packages/plugins/experimental/plugin-transcription/src/components/TranscriptContainer.tsx
+++ packages/plugins/experimental/plugin-transcription/src/components/TranscriptContainer.tsx
@@ -7,14 +7,12 @@ import React, { type FC, Fragment } from 'react';
 import { fullyQualifiedId } from '@dxos/client/echo';
 import { DXN } from '@dxos/keys';
 import { useQueue } from '@dxos/react-client/echo';
-import { useEdgeClient } from '@dxos/react-edge-client';
 import { StackItem } from '@dxos/react-ui-stack';
 
 import { type TranscriptBlock, type TranscriptType } from '../types';
 import { Transcript } from './Transcript';
 
 export const TranscriptionContainer: FC<{ transcript: TranscriptType; role: string }> = ({ transcript, role }) => {
-  const edge = useEdgeClient();
   const attendableId = fullyQualifiedId(transcript);
 
   const queue = useQueue<TranscriptBlock>(transcript.queue ? DXN.parse(transcript.queue) : undefined, {
