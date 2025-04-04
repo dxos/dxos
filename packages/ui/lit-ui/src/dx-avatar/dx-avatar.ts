50e8c95a3fd0611d87e1f5c8ca2006d609907ea9
thure <will@braneframe.com>
2025-04-04 14:21:09 -0700

    Continue adding markup.

--- packages/ui/lit-ui/src/dx-avatar/dx-avatar.ts
+++ packages/ui/lit-ui/src/dx-avatar/dx-avatar.ts
@@ -4,14 +4,18 @@
 
 import { html, LitElement } from 'lit';
 import { customElement, state, property } from 'lit/decorators.js';
+import { styleMap } from 'lit/directives/style-map.js';
 
 import { makeId } from '@dxos/react-hooks';
+import { type Size } from '@dxos/react-ui-types';
 
 type ImageLoadingStatus = 'idle' | 'loading' | 'loaded' | 'error';
 
 type AvatarVariant = 'square' | 'circle';
 type AvatarStatus = 'active' | 'inactive' | 'current' | 'error' | 'warning' | 'internal';
 type AvatarAnimation = 'pulse' | 'none';
+
+const rx = '0.25rem';
 
 @customElement('dx-avatar')
 export class DxAvatar extends LitElement {
@@ -45,13 +49,16 @@ export class DxAvatar extends LitElement {
   status: AvatarStatus | undefined = undefined;
 
   @property({ type: String })
-  animation: AvatarAnimation | undefined = 'none';
+  animation: AvatarAnimation = 'none';
 
   @property({ type: Boolean })
-  inGroup: boolean | undefined = false;
+  inGroup: boolean = false;
 
   @property({ type: String })
   hue: string | undefined = undefined;
+
+  @property({ type: String })
+  size: Size = 10;
 
   @state()
   loadingStaus: ImageLoadingStatus = 'idle';
@@ -76,17 +83,81 @@ export class DxAvatar extends LitElement {
   }
 
   override render() {
+    const numericSize = this.size === 'px' ? 1 : Number(this.size);
+    const sizePx = numericSize * 4;
+    const ringWidth = this.status ? (numericSize > 4 ? 2 : numericSize > 3 ? 1 : 1) : 0;
+    const ringGap = this.status ? (numericSize > 12 ? 3 : numericSize > 4 ? 2 : numericSize > 3 ? 1 : 0) : 0;
+    const r = sizePx / 2 - ringGap - ringWidth;
+    const isTextOnly = Boolean(this.label && /[0-9a-zA-Z]+/.test(this.label));
+    const fontScale = (isTextOnly ? 3 : 4) * (1 / 1.612);
     return html`<span class="dx-avatar"
-      >${this.imgSrc &&
-      html`<img
-        alt=${this.label}
-        src=${this.imgSrc}
-        ?crossorigin=${this.imgCrossOrigin}
-        ?referrerpolicy=${this.imgReferrerPolicy}
-        @load=${this.handleLoad}
-        @error=${this.handleError}
-      />`}</span
-    >`;
+      ><svg
+        viewBox=${`0 0 ${sizePx} ${sizePx}`}
+        width=${sizePx}
+        height=${sizePx}
+        class=${tx('avatar.frame', 'avatar__frame', { variant })}
+      >
+        <defs>
+          <mask id=${this.maskId}>
+            {variant === 'circle' ? (
+            <circle fill="white" cx="50%" cy="50%" r=${r} />
+            ) : (
+            <rect
+              fill="white"
+              width=${2 * r}
+              height=${2 * r}
+              x=${ringGap + ringWidth}
+              y=${ringGap + ringWidth}
+              rx=${rx}
+            />
+            )}
+          </mask>
+        </defs>
+        ${
+          this.variant === 'circle'
+            ? html`<circle
+                cx="50%"
+                cy="50%"
+                r=${r}
+                fill=${this.hue ? `var(--dx-${this.hue}Fill)` : 'var(--surface-bg)'}
+              />`
+            : html`<rect
+                fill=${this.hue ? `var(--dx-${this.hue}Fill)` : 'var(--surface-bg)'}
+                x=${ringGap + ringWidth}
+                y=${ringGap + ringWidth}
+                width=${2 * r}
+                height=${2 * r}
+                rx=${rx}
+              />`
+        }
+        ${
+          this.imgSrc &&
+          html`<image
+            preserveAspectRatio="xMidYMid slice"
+            width="100%"
+            height="100%"
+            href=${this.imgSrc}
+            ?crossorigin=${this.imgCrossOrigin}
+            ?referrerpolicy=${this.imgReferrerPolicy}
+            @load=${this.handleLoad}
+            @error=${this.handleError}
+          />`
+        }
+      <text
+        x='50%'
+        y='50%'
+        class=${tx('avatar.fallbackText', 'avatar__fallback-text')}
+      textAnchor='middle'
+      alignmentBaseline='central'
+      fontSize=${this.size === 'px' ? '200%' : this.size * fontScale}
+      mask=${`url(#${this.maskId})`}
+    >
+      </svg>
+      <span
+        role="none"
+        class=${tx('avatar.ring', 'avatar__ring', { size, variant, status, animation })}
+        style=${styleMap({ borderWidth: ringWidth + 'px' })}
+    /></span>`;
   }
 
   override createRenderRoot() {
