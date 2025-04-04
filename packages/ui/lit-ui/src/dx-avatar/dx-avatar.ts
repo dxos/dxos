3f4f6d830500da2846ba19d38efa4cbfb2d5a2ca
thure <will@braneframe.com>
2025-04-03 18:32:02 -0700

    Set up Radix image loading implementation.

--- packages/ui/lit-ui/src/dx-avatar/dx-avatar.ts
+++ packages/ui/lit-ui/src/dx-avatar/dx-avatar.ts
@@ -3,14 +3,58 @@
 //
 
 import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state, property } from 'lit/decorators.js';
 
-export type DxAvatarProps = {};
+type ImageLoadingStatus = 'idle' | 'loading' | 'loaded' | 'error';
 
 @customElement('dx-avatar')
 export class DxAvatar extends LitElement {
+  @property({ type: String })
+  label: string = 'never';
+
+  @property({ type: String })
+  imgSrc: string | undefined = undefined;
+
+  @property({ type: String })
+  imgCrossOrigin: NonNullable<HTMLImageElement['crossOrigin']> | undefined = undefined;
+
+  @property({ type: String })
+  imgReferrerPolicy: HTMLImageElement['referrerPolicy'] | undefined = undefined;
+
+  @state()
+  loadingStaus: ImageLoadingStatus = 'idle';
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.loadingStaus = this.imgSrc ? 'loading' : 'idle';
+  }
+
+  override willUpdate(changedProperties: Map<string, any>) {
+    if (changedProperties.has('imgSrc')) {
+      this.loadingStaus = changedProperties.get('imgSrc') ? 'loading' : 'idle';
+    }
+  }
+
+  private handleLoad() {
+    this.loadingStaus = 'loaded';
+  }
+
+  private handleError() {
+    this.loadingStaus = 'error';
+  }
+
   override render() {
-    return html`<span>Avatar</span>`;
+    return html`<span class="dx-avatar"
+      >${this.imgSrc &&
+      html`<img
+        alt=${this.label}
+        src=${this.imgSrc}
+        ?crossorigin=${this.imgCrossOrigin}
+        ?referrerpolicy=${this.imgReferrerPolicy}
+        @load=${this.handleLoad}
+        @error=${this.handleError}
+      />`}</span
+    >`;
   }
 
   override createRenderRoot() {
