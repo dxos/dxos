57305ff0d6a525f91e27d397981a3fbf8ae999ba
Rich Burdon <rich.burdon@gmail.com>
2025-04-07 16:52:46 -0400

    chore: Refactor telemetry. (#8911)

--- packages/plugins/plugin-meeting/src/meta.ts
+++ packages/plugins/plugin-meeting/src/meta.ts
@@ -8,9 +8,9 @@ export const MEETING_PLUGIN = 'dxos.org/plugin/meeting';
 
 export const meta: PluginMeta = {
   id: MEETING_PLUGIN,
-  name: 'Meeting',
+  name: 'Meetings',
   description:
-    'The Meeting plugin allows you to have realtime voice and video calls inside of Composer. Leveraging Cloudflare Calls infrastructure you can privately create peer-to-peer video channels that allow you to collaborate in realtime inside of Composer. The Meeting plugin also integrates with the Transcription plugin to provide realtime transcriptions and feed your meeting discussion to your LLM to provide nuanced context for automated workflows. ',
+    'The Meetings plugin supports realtime voice and video calls inside of Composer. Leveraging Cloudflare Calls infrastructure you can privately create peer-to-peer video channels that allow you to collaborate in realtime inside of Composer. The Meeting plugin also integrates with the Transcription plugin to provide realtime transcriptions and feed your meeting discussion to your LLM to provide nuanced context for automated workflows.',
   icon: 'ph--phone-call--regular',
   source: 'https://github.com/dxos/dxos/tree/main/packages/plugins/plugin-meeting',
   tags: [],
