3d76902b983b1e4e95dab76a414a62af327ac70a
Rich Burdon <rich.burdon@gmail.com>
2025-04-07 16:52:46 -0400

    chore: Refactor telemetry. (#8911)

--- packages/devtools/cli-base/src/base.ts
+++ packages/devtools/cli-base/src/base.ts
@@ -386,19 +386,20 @@ export abstract class AbstractBaseCommand<T extends typeof Command = any> extend
    */
   override async finally() {
     const endTime = new Date();
-    // TODO(nf): move to observability
+    // TODO(nf): Move to observability.
     const installationId = this._observability?.getTag('installationId');
     const did = this._observability?.getTag('did');
     if (this._observability) {
-      this._observability?.event({
+      this._observability?.track({
         installationId: installationId?.value,
         did: did?.value,
-        name: 'cli.command.run',
+        action: 'cli.command.run',
         properties: {
           status: this._failing ? 'failure' : 'success',
           duration: endTime.getTime() - this._startTime.getTime(),
         },
       });
+
       await this._observability.close();
     }
     if (process.env.DX_TRACK_LEAKS) {
@@ -407,7 +408,7 @@ export abstract class AbstractBaseCommand<T extends typeof Command = any> extend
     }
 
     const stopFailsafe = setTimeout(() => {
-      // TODO: log filter not correctly applied for this
+      // TODO(burdon): Log filter not correctly applied for this.
       if (settings.debug) {
         this.log('timeout waiting for all promises to resolve, forcing exit');
       }
--- packages/sdk/observability/src/segment/types.ts
+++ packages/sdk/observability/src/segment/types.ts
@@ -2,26 +2,56 @@
 // Copyright 2022 DXOS.org
 //
 
+export type Tags = Record<string, string>;
+
 export type SegmentTelemetryOptions = {
   apiKey?: string;
   batchSize?: number;
   enable?: boolean;
-  getTags: () => { [key: string]: string };
+  getTags: () => Tags;
 };
 
-type CommonOptions = {
-  installationId?: string;
-  did?: string;
-  properties?: object;
+// Copied from @segment/analytics-node.
+export type SegmentIdentityOptions =
+  | { userId: string; anonymousId?: string }
+  | { userId?: string; anonymousId: string };
+
+// TODO(burdon): Should require one or the other.
+export type IdentityOptions = { did?: string; installationId?: string };
+
+export type CommonOptions = IdentityOptions & {
   timestamp?: Date;
+  properties?: Record<string, unknown>;
 };
 
+/**
+ * Page views.
+ * https://segment.com/docs/connections/sources/catalog/libraries/server/node/#page
+ */
 export type PageOptions = CommonOptions & {
-  installationId?: string;
-  name?: string;
   category?: string;
+  name?: string;
 };
 
-export type EventOptions = CommonOptions & {
-  name: string;
+/**
+ * Track actions.
+ * https://segment.com/docs/connections/sources/catalog/libraries/server/node/#track
+ */
+export type TrackOptions = CommonOptions & {
+  event?: TelemetryEvent;
+  action: string;
 };
+
+/**
+ * Each event will be mapped to a different Postgres table via segment.
+ */
+export enum TelemetryEvent {
+  /**
+   * Use actions.
+   */
+  ACTION = 'action',
+  /**
+   * High-volume technical metrics.
+   */
+  METRICS = 'metrics',
+}
