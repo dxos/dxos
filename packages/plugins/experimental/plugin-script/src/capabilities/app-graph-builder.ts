503033952097faf23c9bf90d174def83556b6b10
thure <will@braneframe.com>
2025-04-11 19:11:26 -0700

    Fix script companion builder.

--- packages/plugins/experimental/plugin-script/src/capabilities/app-graph-builder.ts
+++ packages/plugins/experimental/plugin-script/src/capabilities/app-graph-builder.ts
@@ -3,6 +3,7 @@
 //
 
 import { Capabilities, contributes, type PluginsContext } from '@dxos/app-framework';
+import { isInstanceOf } from '@dxos/echo-schema';
 import { ScriptType } from '@dxos/functions';
 import { DECK_PLUGIN } from '@dxos/plugin-deck';
 import { createExtension, type Node } from '@dxos/plugin-graph';
@@ -13,19 +14,16 @@ export default (context: PluginsContext) =>
   contributes(Capabilities.AppGraphBuilder, [
     createExtension({
       id: `${SCRIPT_PLUGIN}/script-logs`,
-      filter: (node): node is Node<ScriptType> => node.data instanceof ScriptType,
-      connector: ({ node }) => {
-        return [
-          {
-            id: `${SCRIPT_PLUGIN}/script-logs`,
-            type: `${DECK_PLUGIN}/companion`,
-            data: node.id,
-            properties: {
-              label: ['script logs label', { ns: SCRIPT_PLUGIN }],
-              icon: 'ph--clock-counter-clockwise--regular',
-            },
+      filter: (node): node is Node<ScriptType> => isInstanceOf(ScriptType, node.data),
+      connector: ({ node }) => [
+        {
+          id: `${node.id}/logs`,
+          type: `${DECK_PLUGIN}/companion`,
+          properties: {
+            label: ['script logs label', { ns: SCRIPT_PLUGIN }],
+            icon: 'ph--clock-countdown--regular',
           },
-        ];
-      },
+        },
+      ],
     }),
   ]);
