9da952dfc2ae9cca1356529b911ff18efaff204d
Josiah Witt <josiah@witt.life>
2025-04-14 09:41:02 -0400

    refactor: separate meeting from channel (#8902)

---
+++ packages/ui/react-ui-theme/src/config/tokens/sememes-calls.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import { type ColorSememes } from './types';
+
+export const callsSememes = {
+  activeInCall: {
+    light: ['red', '500'],
+    dark: ['red', '500'],
+  },
+} satisfies ColorSememes;
