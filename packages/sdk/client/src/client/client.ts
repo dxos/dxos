27f73909bb1d18634a05a5649451a096cfc5567f
Josiah Witt <josiah@witt.life>
2025-04-14 09:18:55 -0400

    more pr feedback

--- packages/sdk/client/src/client/client.ts
+++ packages/sdk/client/src/client/client.ts
@@ -215,6 +215,8 @@ export class Client {
 
   /**
    * EDGE client.
+   *
+   * This API is experimental and subject to change.
    */
   get edge(): EdgeHttpClient {
     invariant(this._edgeClient, 'Client not initialized.');
