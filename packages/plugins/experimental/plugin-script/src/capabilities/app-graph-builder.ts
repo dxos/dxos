88b38eec9c368a752d06da2bf4d1744cb2e8e478
Will Shown <thure@users.noreply.github.com>
2025-04-15 11:18:05 -0700

    feat: Companion tabs (#8948)
    Co-authored-by: Josiah Witt <josiah@witt.life>

---
+++ packages/plugins/experimental/plugin-script/src/capabilities/app-graph-builder.ts
@@ -0,0 +1,30 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { Capabilities, contributes, type PluginsContext } from '@dxos/app-framework';
+import { isInstanceOf } from '@dxos/echo-schema';
+import { ScriptType } from '@dxos/functions';
+import { COMPANION_TYPE, SLUG_PATH_SEPARATOR } from '@dxos/plugin-deck/types';
+import { createExtension, type Node } from '@dxos/plugin-graph';
+
+import { SCRIPT_PLUGIN } from '../meta';
+
+export default (context: PluginsContext) =>
+  contributes(Capabilities.AppGraphBuilder, [
+    createExtension({
+      id: `${SCRIPT_PLUGIN}/script-logs`,
+      filter: (node): node is Node<ScriptType> => isInstanceOf(ScriptType, node.data),
+      connector: ({ node }) => [
+        {
+          id: `${node.id}${SLUG_PATH_SEPARATOR}logs`,
+          type: COMPANION_TYPE,
+          data: node.data,
+          properties: {
+            label: ['script logs label', { ns: SCRIPT_PLUGIN }],
+            icon: 'ph--clock-countdown--regular',
+          },
+        },
+      ],
+    }),
+  ]);
