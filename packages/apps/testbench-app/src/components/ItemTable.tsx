1b9785f5004287222b6fe8a73f1afae7f05ea96b
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-04-15 13:31:44 -0700

    Use dynamic table in test-bench app

--- packages/apps/testbench-app/src/components/ItemTable.tsx
+++ packages/apps/testbench-app/src/components/ItemTable.tsx
@@ -2,42 +2,22 @@
 // Copyright 2024 DXOS.org
 //
 
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { type S } from '@dxos/echo-schema';
+import { toJsonSchema, type S } from '@dxos/echo-schema';
 import { type ReactiveObject } from '@dxos/live-object';
-import { Table, useTableModel } from '@dxos/react-ui-table';
+import { DynamicTable } from '@dxos/react-ui-table';
 
 export type ItemTableProps<T> = {
   schema: S.Schema<T>;
   objects?: T[];
 };
 
-// TODO(burdon): Convert to new Table.
 export const ItemTable = <T extends ReactiveObject<any>>({ schema, objects = [] }: ItemTableProps<T>) => {
-  const model = useTableModel({
-    objects,
-  });
-
-  // const columns = useMemo(() => {
-  //   // TODO(burdon): [API]: id is added to schema?
-  //   const [id, ...rest] = schemaToColumnDefs(schema);
-  //   return [
-  //     {
-  //       ...id,
-  //       // TODO(burdon): Sizes are not respected.
-  //       size: 60,
-  //       minSize: 60,
-  //       maxSize: 60,
-  //       cell: (cell) => <span className='px-2 font-mono'>{cell.getValue()?.slice(0, 8)}</span>,
-  //     },
-  //     ...rest,
-  //   ];
-  // }, [schema]);
-
+  const jsonSchema = useMemo(() => toJsonSchema(schema), [schema]);
   return (
-    <Table.Root>
-      <Table.Main model={model} />
-    </Table.Root>
+    <div role='none' className='is-full bs-full'>
+      <DynamicTable data={objects} jsonSchema={jsonSchema} />
+    </div>
   );
 };
