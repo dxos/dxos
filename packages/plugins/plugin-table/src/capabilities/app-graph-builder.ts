0db7747c767fc8d9e571e9bfd6c89329141ccca9
Rich Burdon <rich.burdon@gmail.com>
2025-04-16 21:22:22 -0400

    feat: table companion plank (#8969)

---
+++ packages/plugins/plugin-table/src/capabilities/app-graph-builder.ts
@@ -0,0 +1,31 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { Capabilities, contributes, type PluginsContext } from '@dxos/app-framework';
+import { isInstanceOf } from '@dxos/echo-schema';
+import { COMPANION_TYPE, SLUG_PATH_SEPARATOR } from '@dxos/plugin-deck/types';
+import { createExtension, type Node } from '@dxos/plugin-graph';
+import { TableType } from '@dxos/react-ui-table';
+
+import { meta } from '../meta';
+
+export default (context: PluginsContext) =>
+  contributes(Capabilities.AppGraphBuilder, [
+    // TOOD(burdon): Factor out: make generic?
+    createExtension({
+      id: `${meta.id}/schema`,
+      filter: (node): node is Node<TableType> => isInstanceOf(TableType, node.data),
+      connector: ({ node }) => [
+        {
+          id: [node.id, 'schema'].join(SLUG_PATH_SEPARATOR),
+          type: COMPANION_TYPE,
+          data: node.data,
+          properties: {
+            label: ['companion schema label', { ns: meta.id }],
+            icon: 'ph--database--regular',
+          },
+        },
+      ],
+    }),
+  ]);
