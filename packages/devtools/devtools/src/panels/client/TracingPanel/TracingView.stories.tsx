d072718fd0ecf8c4b9be481f4074b72e5d34b5c4
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-04-17 16:50:37 +0200

    Add TracingView storybook

---
+++ packages/devtools/devtools/src/panels/client/TracingPanel/TracingView.stories.tsx
@@ -0,0 +1,78 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import '@dxos-theme';
+
+import { type StoryObj, type Meta } from '@storybook/react';
+
+import { withTheme } from '@dxos/storybook-utils';
+
+import { TraceView } from './TraceView';
+import { type State } from './types';
+
+const meta: Meta = {
+  title: 'devtools/devtools/TracingPanel',
+  component: TraceView,
+  decorators: [withTheme],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const state: State = {
+  resources: new Map([
+    [
+      1,
+      {
+        resource: {
+          id: 1,
+          className: 'TestResource',
+          instanceId: 1,
+          info: { fields: {} },
+          links: [],
+          metrics: [],
+        },
+        spans: [
+          { id: 1, startTs: '0', endTs: '100', methodName: 'root1', resourceId: 1 },
+          { id: 3, startTs: '10', endTs: '50', methodName: 'child1', parentId: 1, resourceId: 1 },
+        ],
+        logs: [],
+      },
+    ],
+    [
+      2,
+      {
+        resource: {
+          id: 2,
+          className: 'TestResource',
+          instanceId: 2,
+          info: { fields: {} },
+          links: [],
+          metrics: [],
+        },
+        spans: [
+          { id: 2, startTs: '50', endTs: '150', methodName: 'root2', resourceId: 2 },
+          { id: 4, startTs: '60', endTs: '120', methodName: 'child2', parentId: 2, resourceId: 2 },
+        ],
+        logs: [],
+      },
+    ],
+  ]),
+  spans: new Map([
+    [1, { id: 1, startTs: '0', endTs: '100', methodName: 'root1', resourceId: 1 }],
+    [2, { id: 2, startTs: '50', endTs: '150', methodName: 'root2', resourceId: 2 }],
+    [3, { id: 3, startTs: '10', endTs: '50', methodName: 'child1', parentId: 1, resourceId: 1 }],
+    [4, { id: 4, startTs: '60', endTs: '120', methodName: 'child2', parentId: 2, resourceId: 2 }],
+  ]),
+};
+
+export const Default: Story = {
+  args: { state },
+};
+
+// Example with a specific resource view
+export const SingleResource: Story = {
+  args: { state, resourceId: 1 },
+};
