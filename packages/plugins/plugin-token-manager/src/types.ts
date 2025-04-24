0ac538fd137fd5fbd786c83e60cb01eff5af5fd8
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-04-24 10:08:50 -0700

    Emit intent accessTokenCreated

---
+++ packages/plugins/plugin-token-manager/src/types.ts
@@ -0,0 +1,20 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { S } from '@dxos/echo-schema';
+import { AccessTokenType } from '@dxos/schema';
+
+import { TOKEN_MANAGER_PLUGIN } from './meta';
+
+export namespace TokenManagerAction {
+  const TOKEN_MANAGER_ACTION = `${TOKEN_MANAGER_PLUGIN}/action`;
+
+  export class AccessTokenCreated extends S.TaggedClass<AccessTokenCreated>()(
+    `${TOKEN_MANAGER_ACTION}/access-token-created`,
+    {
+      input: AccessTokenType,
+      output: S.Void,
+    },
+  ) {}
+}
