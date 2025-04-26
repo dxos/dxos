05c479cc3de458d8e93a119bd6d82eb95a34b611
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-04-25 17:08:04 -0700

    Only show sync / subscription buttons when appropriate.

--- packages/plugins/plugin-automation/src/capabilities/intent-resolver.ts
+++ packages/plugins/plugin-automation/src/capabilities/intent-resolver.ts
@@ -3,6 +3,9 @@
 //
 
 import { contributes, Capabilities, createResolver } from '@dxos/app-framework';
+import { FunctionTrigger, TriggerKind } from '@dxos/functions';
+import { type DXN } from '@dxos/keys';
+import { create } from '@dxos/live-object';
 
 import { AutomationAction } from '../types';
 
@@ -11,13 +14,18 @@ export default () =>
     createResolver({
       intent: AutomationAction.CreateTriggerFromTemplate,
       resolve: async ({ template }) => {
+        const trigger = create(FunctionTrigger, { enabled: false });
+
         switch (template.type) {
           case 'gmail-sync': {
-            console.log('gmail-sync', template.mailboxId);
+            trigger.spec = { type: TriggerKind.Timer, cron: '*/30 * * * * *' }; // Every 30 seconds.
+
+            // TODO(ZaymonFC): Find the gmail function and set that as the trigger function.
+
             break;
           }
           case 'queue': {
-            console.log('queue', template.queueDXN);
+            trigger.spec = { type: TriggerKind.Queue, queue: (template.queueDXN as DXN).toString() };
             break;
           }
           default: {
@@ -25,6 +33,8 @@ export default () =>
           }
         }
 
+        // TODO: Add new trigger to the space.
+        // TODO: Navigate to the new trigger.
         return { data: undefined };
       },
     }),
--- packages/plugins/plugin-inbox/src/components/Mailbox/MailboxObjectSettings.tsx
+++ packages/plugins/plugin-inbox/src/components/Mailbox/MailboxObjectSettings.tsx
@@ -2,37 +2,72 @@
 // Copyright 2025 DXOS.org
 //
 
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { createIntent, useIntentDispatcher } from '@dxos/app-framework';
+import { FunctionTrigger, TriggerKind } from '@dxos/functions';
 import { AutomationAction } from '@dxos/plugin-automation/types';
-import { Button } from '@dxos/react-ui';
+import { Filter, getSpace, useQuery } from '@dxos/react-client/echo';
+import { Button, useTranslation } from '@dxos/react-ui';
 
+import { INBOX_PLUGIN } from '../../meta';
 import { type MailboxType } from '../../types';
 
 export const MailboxObjectSettings = ({ object }: { object: MailboxType }) => {
+  const { t } = useTranslation(INBOX_PLUGIN);
   const { dispatchPromise: dispatch } = useIntentDispatcher();
+  const space = useMemo(() => getSpace(object), [object]);
+  const triggers = useQuery(space, Filter.schema(FunctionTrigger));
 
-  const handleConfigureSync = () => {
+  const [showHandleSync, setShowHandleSync] = useState(true);
+  useEffect(() => {
+    const syncTrigger = triggers.find((trigger) => trigger.meta?.mailboxId === object.id);
+    if (syncTrigger) {
+      setShowHandleSync(false);
+    }
+  }, [triggers]);
+
+  const [showHandleSubscription, setShowHandleSubscription] = useState(true);
+  useEffect(() => {
+    const subscriptionTrigger = triggers.find((trigger) => {
+      if (trigger.spec?.type === TriggerKind.Queue) {
+        if (trigger.spec.queue === object.queue.dxn.toString()) {
+          return true;
+        }
+      }
+      return false;
+    });
+    if (subscriptionTrigger) {
+      setShowHandleSubscription(false);
+    }
+  }, [triggers]);
+
+  const handleConfigureSync = useCallback(() => {
     void dispatch(
       createIntent(AutomationAction.CreateTriggerFromTemplate, {
         template: { type: 'gmail-sync', mailboxId: object.id },
       }),
     );
-  };
+  }, [dispatch, object.id]);
 
-  const handleConfigureSubscription = () => {
+  const handleConfigureSubscription = useCallback(() => {
     void dispatch(
       createIntent(AutomationAction.CreateTriggerFromTemplate, {
         template: { type: 'queue', queueDXN: object.queue.dxn },
       }),
     );
-  };
+  }, [dispatch, object.queue.dxn]);
 
   return (
     <div className='p-1 flex flex-row gap-1'>
-      <Button onClick={handleConfigureSync}>Configure sync</Button>
-      <Button onClick={handleConfigureSubscription}>Configure mailbox subscription</Button>
+      {showHandleSync && (
+        <Button onClick={handleConfigureSync}>{t('mailbox object settings configure sync button label')}</Button>
+      )}
+      {showHandleSubscription && (
+        <Button onClick={handleConfigureSubscription}>
+          {t('mailbox object settings configure subscription button label')}
+        </Button>
+      )}
     </div>
   );
 };
