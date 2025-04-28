08be6e4266dd4e8c9c08995c0d20083b3959dbc2
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2025-04-28 21:42:15 +0200

    fix(plugin-meeting): Fix participant counter; chore: Update data-generator script template (#9019)

--- packages/plugins/plugin-script/src/templates/data-generator.ts
+++ packages/plugins/plugin-script/src/templates/data-generator.ts
@@ -2,35 +2,10 @@
 // Copyright 2025 DXOS.org
 //
 
-import {
-  EchoObject,
-  defineFunction,
-  S,
-  createStatic,
-  Expando,
-  TypedObject,
-  Ref,
-  LabelAnnotationId,
-  // @ts-ignore
-} from 'dxos:functions';
 // @ts-ignore
-import { randParagraph } from 'https://esm.sh/@ngneat/falso@7.1.1';
-
-export class TextType extends TypedObject({ typename: 'dxos.org/type/Text', version: '0.1.0' })({
-  content: S.String,
-}) {}
-
-export const DocumentSchema = S.Struct({
-  name: S.optional(S.String),
-  fallbackName: S.optional(S.String),
-  content: Ref(TextType),
-  threads: S.mutable(S.Array(Ref(Expando))),
-}).annotations({
-  [LabelAnnotationId]: ['name', 'fallbackName'],
-});
-
-export const DocumentType = DocumentSchema.pipe(EchoObject('dxos.org/type/Document', '0.1.0'));
-export type DocumentType = S.Schema.Type<typeof DocumentType>;
+import { defineFunction, S, createStatic, Expando } from 'dxos:functions';
+// @ts-ignore
+import { randText } from 'https://esm.sh/@ngneat/falso@7.1.1';
 
 export default defineFunction({
   inputSchema: S.Struct({
@@ -41,33 +16,38 @@ export default defineFunction({
 
   outputSchema: S.Struct({}),
 
-  handler: async ({ event: { documentAmount = 10, textSize = 10, mutationAmount = 0 }, context: { space } }: any) => {
+  handler: async ({
+    event: {
+      data: { documentAmount = 1, textSize = 10, mutationAmount = 0 },
+    },
+    context: { space },
+  }: any) => {
     await space.db.graph.schemaRegistry.addSchema([Expando]);
+    const result: Record<string, any> = {};
+    const objects: Expando[] = [];
 
-    const objects = [];
-    // const rootCollection = await space.properties[COLLECTION_TYPENAME]?.load();
+    // Create objects.
     for (let i = 0; i < documentAmount; i++) {
       const obj = space.db.add(
         createStatic(Expando, {
           name: 'scriptGenerated',
-          content: randParagraph({ length: textSize }),
+          content: randText({ charCount: textSize }),
         }),
       );
-      // rootCollection.objects.push(obj);
       objects.push(obj);
+      const objectResult: any = {};
+      // Mutate the object.
+      for (let i = 0; i < mutationAmount; i++) {
+        obj.content = randText({ charCount: textSize });
+      }
+      objectResult.mutations = mutationAmount;
+      objectResult.contentLength = obj.content.length;
+      result[obj.id] = objectResult;
     }
 
-    for (let i = 0; i < mutationAmount; i++) {
-      const obj = objects[Math.floor(Math.random() * objects.length)];
-      obj.content = randParagraph({ length: textSize });
-    }
-
+    // Flush and wait for data to propagate.
     await space.db.flush();
-    // TODO(mykola): This seems to halp with propagating data to space.
-    for (const obj of objects) {
-      await space.db.query({ id: obj.id }).first();
-      console.log('obj', obj.id);
-    }
-    return 'OK';
+    await new Promise((resolve) => setTimeout(resolve, 10_000));
+    return result;
   },
 });
