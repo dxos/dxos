770cf117604f7bae4e119b3c404d67f58fd88257
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2025-04-30 20:25:48 +0200

    feat: Add Edge and Meeting status to devtools overview (#9045)

--- packages/plugins/plugin-debug/src/components/DevtoolsOverviewContainer.tsx
+++ packages/plugins/plugin-debug/src/components/DevtoolsOverviewContainer.tsx
@@ -4,16 +4,20 @@
 
 import React from 'react';
 
-import { StatsPanel } from '@dxos/devtools';
+import { StatsPanel, useStats } from '@dxos/devtools';
+import { type GlobalState, MeetingStatusDetail } from '@dxos/plugin-meeting';
 import { useSyncState, getSyncSummary, SyncStatusDetail } from '@dxos/plugin-space';
 
-export const DevtoolsOverviewContainer = () => {
+export const DevtoolsOverviewContainer = ({ callState }: { callState?: GlobalState }) => {
   const state = useSyncState();
   const summary = getSyncSummary(state);
+  const [stats, refreshStats] = useStats();
 
   return (
     <>
-      <StatsPanel />
+      <StatsPanel stats={stats} onRefresh={refreshStats}>
+        <MeetingStatusDetail state={callState} />
+      </StatsPanel>
       <SyncStatusDetail state={state} summary={summary} />
     </>
   );
