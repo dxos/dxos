2db957e4547016d47652ceeaa1001d498d66a801
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-05-01 16:59:57 -0700

    Render enriched markdown in message

--- packages/plugins/plugin-inbox/src/components/Message/MessageContainer.tsx
+++ packages/plugins/plugin-inbox/src/components/Message/MessageContainer.tsx
@@ -2,23 +2,78 @@
 // Copyright 2025 DXOS.org
 //
 
-import React from 'react';
+import React, { useMemo } from 'react';
 
+import { DXN } from '@dxos/keys';
+import { resolveRef, useClient } from '@dxos/react-client';
+import { type Space } from '@dxos/react-client/echo';
+import { useThemeContext, type ThemedClassName } from '@dxos/react-ui';
+import {
+  createBasicExtensions,
+  createMarkdownExtensions,
+  createThemeExtensions,
+  decorateMarkdown,
+  preview,
+  useTextEditor,
+} from '@dxos/react-ui-editor';
 import { StackItem } from '@dxos/react-ui-stack';
+import { mx } from '@dxos/react-ui-theme';
 import { type MessageType } from '@dxos/schema';
 
+export type MessageProps = ThemedClassName<{
+  space?: Space;
+  message: MessageType;
+}>;
+
+const Message = ({ space, message, classNames }: MessageProps) => {
+  const client = useClient();
+  const { themeMode } = useThemeContext();
+
+  const content = useMemo(() => {
+    return message.blocks
+      .filter((block) => 'text' in block)
+      .map((block) => block.text)
+      .join('\n');
+  }, [message.blocks]);
+
+  // TODO(ZaymonFC): How to prevent
+  const extensions = useMemo(() => {
+    if (space) {
+      return [
+        createBasicExtensions({ readOnly: true, lineWrapping: true }),
+        createMarkdownExtensions({ themeMode }),
+        createThemeExtensions({ themeMode }),
+        decorateMarkdown(),
+        preview({
+          onLookup: async ({ label, ref }) => {
+            const dxn = DXN.parse(ref);
+            if (!dxn) {
+              return null;
+            }
+
+            const object = await resolveRef(client, dxn, space);
+            return { label, object };
+          },
+        }),
+      ];
+    }
+    return [];
+  }, [space, client]);
+
+  const { parentRef } = useTextEditor({ initialValue: content, extensions }, [content, extensions]);
+
+  return <div ref={parentRef} className={mx('overflow-hidden', classNames)} />;
+};
+
 export type MessageContainerProps = {
+  space?: Space;
   message: MessageType;
 };
 
-export const MessageContainer = ({ message }: MessageContainerProps) => {
+export const MessageContainer = ({ space, message }: MessageContainerProps) => {
   return (
-    <StackItem.Content classNames='overflow-y-auto p-2'>
-      {message.blocks
-        .filter((block) => 'text' in block)
-        .map((block, b) => {
-          return <p key={`block--${b}`}>{block.text}</p>;
-        })}
+    <StackItem.Content classNames='p-2 overflow-y-auto'>
+      <Message space={space} message={message} />
     </StackItem.Content>
   );
 };
