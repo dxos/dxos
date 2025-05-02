ac7004536f9eb7ba727e8b5334f1854b2726a435
thure <will@braneframe.com>
2025-05-02 15:00:21 -0700

    Refactor PreviewPlugin to handle the fallback preview.

--- packages/plugins/plugin-preview/src/PreviewPlugin.tsx
+++ packages/plugins/plugin-preview/src/PreviewPlugin.tsx
@@ -2,6 +2,8 @@
 // Copyright 2023 DXOS.org
 //
 
+import React from 'react';
+
 import {
   createIntent,
   Capabilities,
@@ -10,18 +12,23 @@ import {
   defineModule,
   definePlugin,
   LayoutAction,
+  createSurface,
 } from '@dxos/app-framework';
 import { addEventListener } from '@dxos/async';
 import { type Client } from '@dxos/client';
 import { resolveRef } from '@dxos/client';
-import { parseId, type Space } from '@dxos/client/echo';
+import { getSchema, isEchoObject, parseId, type ReactiveEchoObject, type Space } from '@dxos/client/echo';
 import { DXN } from '@dxos/keys';
 import { type DxRefTagActivate } from '@dxos/lit-ui';
 import { log } from '@dxos/log';
 import { ClientCapabilities } from '@dxos/plugin-client';
+import { useTranslation } from '@dxos/react-ui';
 import { type PreviewLinkRef, type PreviewLinkTarget } from '@dxos/react-ui-editor';
+import { Form } from '@dxos/react-ui-form';
+import { descriptionMessage } from '@dxos/react-ui-theme';
 
-import { meta } from './meta';
+import { meta, PREVIEW_PLUGIN } from './meta';
+import translations from './translations';
 
 const customEventOptions = { capture: true, passive: false };
 
@@ -42,7 +49,12 @@ const handlePreviewLookup = async (
 export const PreviewPlugin = () =>
   definePlugin(meta, [
     defineModule({
-      id: `${meta.id}/module/preview-popoevr`,
+      id: `${meta.id}/module/translations`,
+      activatesOn: Events.SetupTranslations,
+      activate: () => contributes(Capabilities.Translations, translations),
+    }),
+    defineModule({
+      id: `${meta.id}/module/preview-popover`,
       activatesOn: Events.Startup,
       activate: (context) => {
         // TODO(wittjosiah): Factor out lookup handlers to other plugins to make not ECHO-specific.
@@ -83,5 +95,26 @@ export const PreviewPlugin = () =>
         );
         return contributes(Capabilities.Null, null, () => cleanup());
       },
+    }),
+    defineModule({
+      id: `${meta.id}/module/react-surface`,
+      activatesOn: Events.SetupReactSurface,
+      activate: () =>
+        contributes(Capabilities.ReactSurface, [
+          createSurface({
+            id: `${PREVIEW_PLUGIN}/fallback-popover`,
+            role: 'popover',
+            filter: (data): data is { subject: ReactiveEchoObject<any> } => isEchoObject(data.subject),
+            component: ({ data }) => {
+              const schema = getSchema(data.subject);
+              const { t } = useTranslation(PREVIEW_PLUGIN);
+              if (!schema) {
+                return <p className={descriptionMessage}>{t('unable to create preview message')}</p>;
+              }
+
+              return <Form schema={schema} values={data.subject} readonly />;
+            },
+          }),
+        ]),
     }),
   ]);
