d71bad081e638de13e86dbab80feb60b1e4c4022
Josiah Witt <josiah@witt.life>
2025-05-03 07:56:08 -0400

    chore: restore migrations stubs (#9096)

--- packages/apps/composer-app/src/migrations/migrations.ts
+++ packages/apps/composer-app/src/migrations/migrations.ts
@@ -4,4 +4,14 @@
 
 import { type Migration } from '@dxos/migrations';
 
-export const __COMPOSER_MIGRATIONS__: Migration[] = [];
+// NOTE: When removing migrations, consider state of space properties which store the version keys of latest migration.
+export const __COMPOSER_MIGRATIONS__: Migration[] = [
+  {
+    version: '2024-06-10-collections',
+    next: async ({ space, builder }) => {},
+  },
+  {
+    version: '2024-06-12/fully-qualified-typenames',
+    next: async ({ space, builder }) => {},
+  },
+];
