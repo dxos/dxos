819a5fc185f6a611755ca0b0d415490d637b29b6
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2025-05-06 16:49:15 -0700

    Add status to organisation type

--- packages/sdk/schema/src/common/organization.ts
+++ packages/sdk/schema/src/common/organization.ts
@@ -3,7 +3,15 @@
 //
 
 import { Type } from '@dxos/echo';
-import { S, Format, AST, GeneratorAnnotationId, LabelAnnotationId } from '@dxos/echo-schema';
+import {
+  S,
+  Format,
+  AST,
+  GeneratorAnnotationId,
+  LabelAnnotationId,
+  PropertyMetaAnnotationId,
+  FormatAnnotationId,
+} from '@dxos/echo-schema';
 
 import { IconAnnotationId } from '../annotations';
 
@@ -15,6 +23,22 @@ export const OrganizationSchema = S.Struct({
   name: S.String.annotations({ title: 'Name', [GeneratorAnnotationId]: 'company.name' }),
   description: S.optional(S.String.annotations({ title: 'Description' })),
   // TODO(wittjosiah): Support ref?
+  status: S.optional(
+    S.Union(S.Literal('active'), S.Literal('inactive'), S.Literal('pending'), S.Literal('archived')).annotations({
+      title: 'Status',
+      [PropertyMetaAnnotationId]: {
+        singleSelect: {
+          options: [
+            { id: 'first', title: 'First', color: 'emerald' },
+            { id: 'second', title: 'Second', color: 'red' },
+            { id: 'third', title: 'Third', color: 'amber' },
+            { id: 'fourth', title: 'Fourth', color: 'indigo' },
+          ],
+        },
+      },
+      [FormatAnnotationId]: 'single-select',
+    }),
+  ),
   image: S.optional(Format.URL.annotations({ title: 'Image' })),
   website: S.optional(
     Format.URL.annotations({
