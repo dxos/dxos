e2197798dae90d8e78ff186d9ed08cfb87aac933
Dmytro Maretskyi <maretskii@gmail.com>
2025-05-07 17:29:46 +0200

    Do not match on document content

--- packages/plugins/plugin-transcription/src/entity-extraction/entity-extraction.test.ts
+++ packages/plugins/plugin-transcription/src/entity-extraction/entity-extraction.test.ts
@@ -16,7 +16,7 @@ const aiService = new AIServiceEdgeClient({
   endpoint: AI_SERVICE_ENDPOINT.REMOTE,
 });
 
-describe.skip('EntityExtraction', { timeout: 180_000 }, () => {
+describe('EntityExtraction', { timeout: 180_000 }, () => {
   test.skip('should process a transcript block', async () => {
     const { transcriptMessages, documents, contacts } = await createTestData();
 
