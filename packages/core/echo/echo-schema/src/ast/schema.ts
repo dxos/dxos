bfeaed68dea2de5753c9989d25d11e079ed1a77b
Dmytro Maretskyi <maretskii@gmail.com>
2025-05-08 12:01:05 +0200

    Fix cycles in echo-schema

--- packages/core/echo/echo-schema/src/ast/schema.ts
+++ packages/core/echo/echo-schema/src/ast/schema.ts
@@ -2,7 +2,9 @@
 // Copyright 2025 DXOS.org
 //
 
+import { DXN } from '@dxos/keys';
 import { type Schema as S } from 'effect';
+import { getTypeAnnotation } from './annotations';
 
 /**
  * For attaching schema to objects.
@@ -32,4 +34,15 @@ export const setSchema = (obj: any, schema: S.Schema.AnyNoContext) => {
     enumerable: false,
     configurable: false,
   });
+};
+
+// TODO(dmaretskyi): Unify with `getTypeReference`.
+export const getSchemaDXN = (schema: S.Schema.All): DXN | undefined => {
+  // TODO(dmaretskyi): Add support for dynamic schema.
+  const objectAnnotation = getTypeAnnotation(schema);
+  if (!objectAnnotation) {
+    return undefined;
+  }
+
+  return DXN.fromTypenameAndVersion(objectAnnotation.typename, objectAnnotation.version);
 };
