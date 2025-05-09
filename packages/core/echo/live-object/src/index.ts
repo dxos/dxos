61a8d220146ff7b330e27e9f27b4ce71aca80e34
Dmytro Maretskyi <maretskii@gmail.com>
2025-05-09 12:54:34 +0200

    Deprecated re-exports

--- packages/core/echo/live-object/src/index.ts
+++ packages/core/echo/live-object/src/index.ts
@@ -3,7 +3,6 @@
 //
 
 // TODO(dmaretskyi): Remove deprecated exports.
-import { Ref } from '@dxos/echo-schema';
 
 export * from './proxy';
 export * from './accessors';
@@ -15,14 +14,34 @@ export * from './live';
 // Deprecated APIs.
 //
 
+import * as EchoSchemaModule from '@dxos/echo-schema';
+
 /**
  * @deprecated Use {@link Ref.make} instead.
  */
-export const makeRef = Ref.make;
+export const makeRef = EchoSchemaModule.Ref.make;
 
 /**
  * @deprecated Use {@link Ref.fromDXN} instead.
  */
-export const refFromDXN = Ref.fromDXN;
+export const refFromDXN = EchoSchemaModule.Ref.fromDXN;
 
-export { RefArray, getObjectMeta, isDeleted, getType } from '@dxos/echo-schema';
+/**
+ * @deprecated Import from `@dxos/echo-schema` instead.
+ */
+export const RefArray = EchoSchemaModule.RefArray;
+
+/**
+ * @deprecated Import from `@dxos/echo-schema` instead.
+ */
+export const getObjectMeta = EchoSchemaModule.getObjectMeta;
+
+/**
+ * @deprecated Import from `@dxos/echo-schema` instead.
+ */
+export const isDeleted = EchoSchemaModule.isDeleted;
+
+/**
+ * @deprecated Import from `@dxos/echo-schema` instead.
+ */
+export const getType = EchoSchemaModule.getType;
