0de15d48537eaddc7c2fca188e8f9de9eee503b9
Josiah Witt <josiah@witt.life>
2025-05-14 21:22:29 -0400

    initial graph

---
+++ packages/sdk/app-graph/src/rx/graph.test.ts
@@ -0,0 +1,131 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { Registry } from '@effect-rx/rx-react';
+import { Option } from 'effect';
+import { describe, expect, test } from 'vitest';
+
+import { invariant } from '@dxos/invariant';
+
+import { getGraph, Graph, ROOT_ID, ROOT_TYPE } from './graph';
+
+const EXAMPLE_TYPE = 'example';
+
+describe.only('RxGraph', () => {
+  test('getGraph', () => {
+    const r = Registry.make();
+    const graph = new Graph();
+    const root = r.get(graph.node(ROOT_ID));
+    invariant(Option.isSome(root));
+    expect(root.value.id).toEqual(ROOT_ID);
+    expect(root.value.type).toEqual(ROOT_TYPE);
+    expect(getGraph(root.value)).toEqual(graph);
+  });
+
+  test('add node', () => {
+    const r = Registry.make();
+    const graph = new Graph();
+    graph.addNode(r, { id: '1', type: EXAMPLE_TYPE });
+    const node = r.get(graph.node('1'));
+    expect(Option.isSome(node)).toEqual(true);
+    invariant(Option.isSome(node));
+    expect(node.value.id).toEqual('1');
+    expect(node.value.type).toEqual(EXAMPLE_TYPE);
+    expect(node.value.data).toEqual(null);
+    expect(node.value.properties).toEqual({});
+  });
+
+  test('remove node', () => {
+    const r = Registry.make();
+    const graph = new Graph();
+
+    {
+      const node = r.get(graph.node('1'));
+      expect(Option.isNone(node)).toEqual(true);
+    }
+
+    {
+      graph.addNode(r, { id: '1', type: EXAMPLE_TYPE });
+      const node = r.get(graph.node('1'));
+      expect(Option.isSome(node)).toEqual(true);
+    }
+
+    {
+      graph.removeNode(r, '1');
+      const node = r.get(graph.node('1'));
+      expect(Option.isNone(node)).toEqual(true);
+    }
+  });
+
+  test('add edge', () => {
+    const r = Registry.make();
+    const graph = new Graph();
+    graph.addEdge(r, { source: '1', target: '2' });
+    const edge = r.get(graph.edge('1'));
+    expect(edge.inbound).toEqual([]);
+    expect(edge.outbound).toEqual(['2']);
+  });
+
+  test('remove edge', () => {
+    const r = Registry.make();
+    const graph = new Graph();
+
+    {
+      graph.addEdge(r, { source: '1', target: '2' });
+      const edge = r.get(graph.edge('1'));
+      expect(edge.inbound).toEqual([]);
+      expect(edge.outbound).toEqual(['2']);
+    }
+
+    {
+      graph.removeEdge(r, { source: '1', target: '2' });
+      const edge = r.get(graph.edge('1'));
+      expect(edge.inbound).toEqual([]);
+      expect(edge.outbound).toEqual([]);
+    }
+  });
+
+  test('get nodes', () => {
+    const r = Registry.make();
+    const graph = new Graph();
+    graph.addNode(r, { id: '1', type: EXAMPLE_TYPE });
+    graph.addNode(r, { id: '2', type: EXAMPLE_TYPE });
+    graph.addEdge(r, { source: '1', target: '2' });
+    const nodes = r.get(graph.nodes('1'));
+    expect(nodes.length).toEqual(1);
+    expect(nodes[0].id).toEqual('2');
+  });
+
+  test('update node', () => {
+    const r = Registry.make();
+    const graph = new Graph();
+    const nodeKey = graph.node('1');
+
+    let count = 0;
+    const cancel = r.subscribe(nodeKey, (_) => {
+      count++;
+    });
+
+    expect(r.get(nodeKey)).toEqual(Option.none());
+    expect(count).toEqual(1);
+
+    expect(r.get(nodeKey)).toEqual(Option.none());
+    expect(count).toEqual(1);
+
+    graph.addNode(r, { id: '1', type: EXAMPLE_TYPE });
+    const node = r.get(nodeKey);
+    expect(Option.isSome(node)).toEqual(true);
+    invariant(Option.isSome(node));
+    expect(node.value.id).toEqual('1');
+    expect(node.value.type).toEqual(EXAMPLE_TYPE);
+    expect(node.value.data).toEqual(null);
+    expect(node.value.properties).toEqual({});
+    expect(count).toEqual(2);
+
+    graph.addNode(r, { id: '1', type: EXAMPLE_TYPE });
+    expect(count).toEqual(2);
+
+    cancel();
+  });
+});
---
+++ packages/sdk/app-graph/src/rx/graph.ts
@@ -0,0 +1,198 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { type Registry, Rx } from '@effect-rx/rx-react';
+import { Option, pipe, Record, Schema } from 'effect';
+
+import { todo } from '@dxos/debug';
+import { invariant } from '@dxos/invariant';
+import { type MakeOptional } from '@dxos/util';
+
+import { type NodeArg, type Node } from '../node';
+
+const graphSymbol = Symbol('graph');
+type DeepWriteable<T> = { -readonly [K in keyof T]: T[K] extends object ? DeepWriteable<T[K]> : T[K] };
+type NodeInternal = DeepWriteable<Node> & { [graphSymbol]: Graph };
+
+export const getGraph = (node: Node): Graph => {
+  const graph = (node as NodeInternal)[graphSymbol];
+  invariant(graph, 'Node is not associated with a graph.');
+  return graph;
+};
+
+export const ROOT_ID = 'root';
+export const ROOT_TYPE = 'dxos.org/type/GraphRoot';
+export const ACTION_TYPE = 'dxos.org/type/GraphAction';
+export const ACTION_GROUP_TYPE = 'dxos.org/type/GraphActionGroup';
+
+export type GraphParams = {
+  nodes?: MakeOptional<Node, 'data' | 'cacheable'>[];
+  edges?: Record<string, string[]>;
+};
+
+type Edge = { source: string; target: string };
+const Edges = Schema.Struct({
+  inbound: Schema.Array(Schema.String),
+  outbound: Schema.Array(Schema.String),
+});
+type Edges = { inbound: string[]; outbound: string[] };
+
+/**
+ * The Graph represents the user interface information architecture of the application constructed via plugins.
+ */
+export class Graph {
+  /**
+   * @internal
+   */
+  readonly _nodes = Record.fromEntries([
+    [
+      ROOT_ID,
+      Rx.make<Option.Option<NodeInternal>>(
+        this._constructNode({ id: ROOT_ID, type: ROOT_TYPE, data: null, properties: {} }),
+      ),
+    ],
+  ]);
+
+  /**
+   * @internal
+   */
+  readonly _edges = Record.empty<string, Rx.Writable<Edges>>();
+
+  readonly node = Rx.family<string, Rx.Writable<Option.Option<NodeInternal>>>((id) => {
+    return pipe(
+      this._nodes,
+      Record.get(id),
+      Option.match({
+        onSome: (rx) => rx,
+        onNone: () => Rx.make<Option.Option<NodeInternal>>(Option.none()),
+      }),
+    );
+  });
+
+  readonly edge = Rx.family<string, Rx.Writable<Edges>>((id) => {
+    return pipe(
+      this._edges,
+      Record.get(id),
+      Option.match({
+        onSome: (rx) => rx,
+        onNone: () => Rx.make<Edges>({ inbound: [], outbound: [] }),
+      }),
+    );
+  });
+
+  nodes(id: string): Rx.Rx<Node[]> {
+    return Rx.readable((get) => {
+      const { outbound } = get(this.edge(id));
+      return outbound
+        .map((id) => get(this.node(id)))
+        .filter(Option.isSome)
+        .map((o) => o.value);
+    });
+  }
+
+  edges(id: string): Rx.Rx<Edges> {
+    return this.edge(id);
+  }
+
+  addNodes(registry: Registry.Registry, nodes: NodeArg<any, Record<string, any>>[]) {
+    Rx.batch(() => nodes.map((node) => this.addNode(registry, node)));
+  }
+
+  addNode(registry: Registry.Registry, { nodes, edges, ...nodeArg }: NodeArg<any, Record<string, any>>) {
+    const { id, type, data = null, properties = {} } = nodeArg;
+    const nodeRx = this.node(id);
+    const node = registry.get(nodeRx);
+    Option.match(node, {
+      onSome: (node) => {
+        const typeChanged = node.type !== type;
+        const dataChanged = node.data !== data;
+        const propertiesChanged = Object.keys(properties).some((key) => node.properties[key] !== properties[key]);
+        if (typeChanged || dataChanged || propertiesChanged) {
+          registry.set(nodeRx, Option.some({ ...node, type, data, properties: { ...node.properties, ...properties } }));
+        }
+      },
+      onNone: () => {
+        registry.set(nodeRx, this._constructNode({ id, type, data, properties }));
+      },
+    });
+
+    if (nodes) {
+      Rx.batch(() => {
+        this.addNodes(registry, nodes);
+        const _edges = nodes.map((node) => ({ source: id, target: node.id }));
+        this.addEdges(registry, _edges);
+      });
+    }
+
+    if (edges) {
+      todo();
+    }
+  }
+
+  removeNodes(registry: Registry.Registry, ids: string[], edges = false) {
+    Rx.batch(() => ids.map((id) => this.removeNode(registry, id, edges)));
+  }
+
+  removeNode(registry: Registry.Registry, id: string, edges = false) {
+    const nodeRx = this.node(id);
+    // TODO(wittjosiah): Is there a way to mark these rx values for garbage collection?
+    registry.set(nodeRx, Option.none());
+    Record.remove(this._nodes, id);
+
+    if (edges) {
+      const { inbound, outbound } = registry.get(this.edge(id));
+      const edges = [
+        ...inbound.map((source) => ({ source, target: id })),
+        ...outbound.map((target) => ({ source: id, target })),
+      ];
+      this.removeEdges(registry, edges);
+    }
+  }
+
+  addEdges(registry: Registry.Registry, edges: Edge[]) {
+    Rx.batch(() => edges.map((edge) => this.addEdge(registry, edge)));
+  }
+
+  addEdge(registry: Registry.Registry, edgeArg: Edge) {
+    const sourceRx = this.edge(edgeArg.source);
+    const source = registry.get(sourceRx);
+    if (!source.outbound.includes(edgeArg.target)) {
+      registry.set(sourceRx, { inbound: source.inbound, outbound: [...source.outbound, edgeArg.target] });
+    }
+
+    const targetRx = this.edge(edgeArg.target);
+    const target = registry.get(targetRx);
+    if (!target.inbound.includes(edgeArg.source)) {
+      registry.set(targetRx, { inbound: [...target.inbound, edgeArg.source], outbound: target.outbound });
+    }
+  }
+
+  removeEdges(registry: Registry.Registry, edges: Edge[]) {
+    Rx.batch(() => edges.map((edge) => this.removeEdge(registry, edge)));
+  }
+
+  removeEdge(registry: Registry.Registry, edgeArg: Edge) {
+    const sourceRx = this.edge(edgeArg.source);
+    const source = registry.get(sourceRx);
+    if (source.outbound.includes(edgeArg.target)) {
+      registry.set(sourceRx, {
+        inbound: source.inbound,
+        outbound: source.outbound.filter((id) => id !== edgeArg.target),
+      });
+    }
+
+    const targetRx = this.edge(edgeArg.target);
+    const target = registry.get(targetRx);
+    if (target.inbound.includes(edgeArg.source)) {
+      registry.set(targetRx, {
+        inbound: target.inbound.filter((id) => id !== edgeArg.source),
+        outbound: target.outbound,
+      });
+    }
+  }
+
+  private _constructNode(node: Omit<Node, typeof graphSymbol>) {
+    return Option.some({ ...node, [graphSymbol]: this });
+  }
+}
