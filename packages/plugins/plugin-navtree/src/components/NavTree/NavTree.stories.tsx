cd1091fb0b3aeea281397cf35e7a20e9002956d5
Rich Burdon <rich.burdon@gmail.com>
2025-05-16 14:52:52 -0400

    test: Color panels (#9203)
    Co-authored-by: thure <will@braneframe.com>

--- packages/plugins/plugin-navtree/src/components/NavTree/NavTree.stories.tsx
+++ packages/plugins/plugin-navtree/src/components/NavTree/NavTree.stories.tsx
@@ -20,12 +20,15 @@ import {
 } from '@dxos/app-framework';
 import { withPluginManager } from '@dxos/app-framework/testing';
 import { live } from '@dxos/live-object';
+import { AttentionPlugin } from '@dxos/plugin-attention';
 import { GraphPlugin } from '@dxos/plugin-graph';
 import { StorybookLayoutPlugin } from '@dxos/plugin-storybook-layout';
 import { ThemePlugin } from '@dxos/plugin-theme';
 import { faker } from '@dxos/random';
-import { Main } from '@dxos/react-ui';
-import { defaultTx } from '@dxos/react-ui-theme';
+import { IconButton, Input, Main, Toolbar } from '@dxos/react-ui';
+import { useAttendableAttributes } from '@dxos/react-ui-attention';
+import { Stack, StackItem } from '@dxos/react-ui-stack';
+import { defaultTx, mx } from '@dxos/react-ui-theme';
 import { withLayout } from '@dxos/storybook-utils';
 
 import { NavTreePlugin } from '../../NavTreePlugin';
@@ -36,13 +39,72 @@ faker.seed(1234);
 
 const StoryState = defineCapability<{ tab: string }>('story-state');
 
+// TODO(burdon): How to adjust existing surfaces for this test?
+// TODO(burdon): How to toggle attention in this test?
+// TODO(burdon): Fix outline (e.g., button in sidebar nav is clipped when focused).
+// TODO(burdon): Consider similar containment of: Table, Sheet, Kanban Column, Form, etc.
+
+const container = 'flex flex-col grow gap-2 p-4 rounded-md';
+
+// TODO(burdon): Factor out PlankHeader.
+const StoryPlankHeading = () => {
+  return (
+    <div className='flex p-1 items-center border-b border-separator'>
+      <IconButton density='coarse' icon='ph--atom--regular' label='Test' iconOnly classNames='w-[40px] h-[40px]' />
+      <StackItem.ResizeHandle />
+    </div>
+  );
+};
+
+const StoryPlank = ({ attendableId }: { attendableId: string }) => {
+  const attentionAttrs = useAttendableAttributes(attendableId);
+
+  return (
+    // TODO(burdon): Simulate Plank layout?
+    <StackItem.Root
+      item={{ id: attendableId }}
+      {...attentionAttrs}
+      classNames='attention-surface border-ie border-separator'
+      size={20}
+    >
+      <StoryPlankHeading />
+      <StackItem.Content toolbar>
+        {/* TODO(burdon): Should the separator be applied by StackItem.Content? */}
+        <Toolbar.Root classNames='border-be border-separator'>
+          <Toolbar.Button>Test</Toolbar.Button>
+        </Toolbar.Root>
+
+        <div className={mx(container, 'm-2 bg-groupSurface')}>
+          <Input.Root>
+            <Input.Label>Level 1</Input.Label>
+          </Input.Root>
+          <div className={mx(container, 'bg-baseSurface')}>
+            <Input.Root>
+              <Input.Label>Level 2</Input.Label>
+              <Input.TextArea placeholder='Enter text' />
+            </Input.Root>
+          </div>
+        </div>
+      </StackItem.Content>
+    </StackItem.Root>
+  );
+};
+
 const DefaultStory = () => {
   const state = useCapability(StoryState);
 
   return (
-    <Main.NavigationSidebar label='Navigation' classNames='grid'>
-      <NavTreeContainer tab={state.tab} />
-    </Main.NavigationSidebar>
+    <Main.Root complementarySidebarState='closed'>
+      <Main.NavigationSidebar label='Navigation' classNames='grid'>
+        <NavTreeContainer tab={state.tab} />
+      </Main.NavigationSidebar>
+      <Main.Content bounce handlesFocus>
+        <Stack orientation='horizontal' size='contain'>
+          <StoryPlank attendableId='space-0:object-0' />
+          <StoryPlank attendableId='space-0:object-1' />
+        </Stack>
+      </Main.Content>
+    </Main.Root>
   );
 };
 
@@ -59,9 +121,10 @@ const meta: Meta<typeof NavTreeContainer> = {
         IntentPlugin(),
         SettingsPlugin(),
         NavTreePlugin(),
+        AttentionPlugin(),
       ],
       capabilities: (context) => [
-        contributes(StoryState, live({ tab: 'default' })),
+        contributes(StoryState, live({ tab: 'space-0' })),
         contributes(Capabilities.IntentResolver, [
           createResolver({
             intent: LayoutAction.UpdateLayout,
@@ -78,6 +141,9 @@ const meta: Meta<typeof NavTreeContainer> = {
     }),
     withLayout({ fullscreen: true }),
   ],
+  parameters: {
+    layout: 'fullscreen',
+  },
 };
 
 export default meta;
