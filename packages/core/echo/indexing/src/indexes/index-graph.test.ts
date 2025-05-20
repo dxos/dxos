995b25180d97ad6d6e3e8dae9ca6a11572ec9e8f
Dmytro Maretskyi <maretskii@gmail.com>
2025-05-20 12:50:34 +0200

    New test

--- packages/core/echo/indexing/src/indexes/index-graph.test.ts
+++ packages/core/echo/indexing/src/indexes/index-graph.test.ts
@@ -3,9 +3,10 @@ import { IndexGraph } from './index-graph';
 import { TestData } from '../testing';
 import { objectPointerCodec, ObjectPointerEncoded } from '@dxos/protocols';
 import { log } from '@dxos/log';
-import { PublicKey } from '@dxos/keys';
+import { DXN, PublicKey } from '@dxos/keys';
 import type { FindResult } from '../types';
 import type { ObjectId } from '@dxos/echo-schema';
+import { encodeReference, Reference } from '@dxos/echo-protocol';
 
 const spaceKey = PublicKey.random();
 
@@ -13,9 +14,8 @@ describe('IndexGraph', () => {
   test('query all', async () => {
     const index = new IndexGraph();
     await index.open();
-
     for (const { id, doc } of TestData.OBJECTS) {
-      await index.update(objectPointerCodec.encode({ spaceKey: spaceKey.toHex(), documentId: id, objectId: id }), doc);
+      await index.update(encodeObjectPointer(id), doc);
     }
 
     // log.info('index', {
@@ -63,8 +63,53 @@ describe('IndexGraph', () => {
       ]);
     }
   });
+
+  test('index updates', async () => {
+    const index = new IndexGraph();
+    await index.open();
+
+    for (const { id, doc } of TestData.OBJECTS) {
+      await index.update(objectPointerCodec.encode({ spaceKey: spaceKey.toHex(), documentId: id, objectId: id }), doc);
+    }
+
+    // Re-assign task1 to Sarah
+    await index.update(encodeObjectPointer(TestData.TASKS.task1.id), {
+      ...TestData.TASKS.task1.doc,
+      data: {
+        ...TestData.TASKS.task1.doc.data,
+        assignedTo: encodeReference(Reference.fromDXN(DXN.fromLocalObjectId(TestData.CONTACTS.sarah.id))),
+      },
+    });
+
+    {
+      const result = await index.find({
+        graph: {
+          kind: 'inbound-reference',
+          anchors: [TestData.CONTACTS.sarah.id],
+          property: 'assignedTo',
+        },
+        typenames: [],
+      });
+      assertResult(result, [TestData.TASKS.task1.id, TestData.TASKS.task2.id]);
+    }
+
+    {
+      const result = await index.find({
+        graph: {
+          kind: 'inbound-reference',
+          anchors: [TestData.CONTACTS.john.id],
+          property: 'assignedTo',
+        },
+        typenames: [],
+      });
+      assertResult(result, []);
+    }
+  });
 });
 
 const assertResult = (result: FindResult[], expected: ObjectId[]) => {
   expect(result.map((r) => objectPointerCodec.decode(r.id).objectId).sort()).toEqual(expected.sort());
 };
+
+const encodeObjectPointer = (id: ObjectId) =>
+  objectPointerCodec.encode({ spaceKey: spaceKey.toHex(), documentId: id, objectId: id });
