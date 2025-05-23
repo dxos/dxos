e6f51f5ee288e5eb860f7e10911beaa90b1c6d88
Dmytro Maretskyi <maretskii@gmail.com>
2025-05-23 13:10:52 +0200

    Fix indexer

--- packages/core/echo/indexing/src/indexing-engine.ts
+++ packages/core/echo/indexing/src/indexing-engine.ts
@@ -223,6 +223,9 @@ export class IndexingEngine extends Resource {
         case IndexKind.Kind.SCHEMA_MATCH:
           updates.push(...(await updateIndexWithObjects(index, documents)));
           break;
+        case IndexKind.Kind.GRAPH:
+          updates.push(...(await updateIndexWithObjects(index, documents)));
+          break;
       }
     }
     return updates;
