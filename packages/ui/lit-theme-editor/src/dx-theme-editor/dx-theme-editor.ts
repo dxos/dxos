3979b420c3c096278631d8626fc1dddf080c00a6
thure <will@braneframe.com>
2025-05-28 12:20:02 -0700

    Provide utils from ThemeEditorPlugin.

--- packages/ui/lit-theme-editor/src/dx-theme-editor/dx-theme-editor.ts
+++ packages/ui/lit-theme-editor/src/dx-theme-editor/dx-theme-editor.ts
@@ -2,13 +2,21 @@
 // Copyright 2025 DXOS.org
 //
 
+import { type TokenSet } from '@ch-ui/tokens';
 import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
+
+import { restore } from './util';
 
 export type DxThemeEditorProps = {};
 
+const bindSeriesDefinitions = ['neutral', 'primary'];
+
 @customElement('dx-theme-editor')
 export class DxThemeEditor extends LitElement {
+  @state()
+  tokenSet: TokenSet = restore();
+
   override render() {
     return html`<span>Theme editor</span>`;
   }
