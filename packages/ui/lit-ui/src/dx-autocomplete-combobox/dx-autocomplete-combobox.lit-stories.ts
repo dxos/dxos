9db5e3d5363c5c194b84efc33a3b5a75cd2eb8a7
thure <will@braneframe.com>
2025-05-29 18:03:10 -0700

    Create autocomplete-combobox to help with alias tokens.

---
+++ packages/ui/lit-ui/src/dx-autocomplete-combobox/dx-autocomplete-combobox.lit-stories.ts
@@ -0,0 +1,24 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import './dx-autocomplete-combobox.ts';
+import './dx-autocomplete-combobox.pcss';
+import { html } from 'lit';
+
+import { type DxAutocompleteComboboxProps } from './dx-autocomplete-combobox';
+
+export default {
+  title: 'dx-autocomplete-combobox',
+  parameters: { layout: 'fullscreen' },
+};
+
+export const Basic = (props: DxAutocompleteComboboxProps) => {
+  return html`<div style="padding:1rem;">
+    <dx-autocomplete-combobox
+      label="Country"
+      options='["Germany", "Japan", "Canada"]'
+      autocomplete="list"
+    ></dx-autocomplete-combobox>
+  </div>`;
+};
