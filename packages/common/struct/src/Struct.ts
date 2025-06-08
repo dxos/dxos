d3842b898e423f411a4b095bafac81072da96690
Dmytro Maretskyi <maretskii@gmail.com>
2025-06-09 00:28:15 +0200

    Minimal serialization module

---
+++ packages/common/struct/src/Struct.ts
@@ -0,0 +1,110 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+export type StructId = string;
+
+/**
+ * Present on structs to denote the type of the struct.
+ */
+export const StructTypeProperty = '@type';
+
+interface StructSerializer<T> {
+  typeId: StructId;
+  serialize: (value: T) => unknown;
+  deserialize: (value: unknown) => T;
+}
+
+export const registerSerializer = <T>(prototype: T, serializer: StructSerializer<T>) => {
+  if (typeof prototype === 'function') {
+    throw new TypeError('Prototype cannot be a function. Use `Class.prototype` when registering a serializer.');
+  }
+
+  const typeId = serializer.typeId;
+  if (typeof typeId !== 'string') {
+    throw new TypeError('[Struct.StructId] must be a string');
+  }
+
+  if (serializersById.has(typeId)) {
+    throw new Error(`Struct serializer already registered: ${typeId}`);
+  }
+
+  if (serializers.has(prototype)) {
+    throw new Error('Struct serializer already registered.');
+  }
+
+  serializers.set(prototype, serializer);
+  serializersById.set(typeId, serializer);
+};
+
+const serializers = new Map<unknown, StructSerializer<any>>();
+const serializersById = new Map<StructId, StructSerializer<any>>();
+
+export const serialize = (value: unknown): unknown => {
+  if (value && typeof value === 'object') {
+    const proto = Object.getPrototypeOf(value);
+    const serializer = serializers.get(proto);
+
+    if (serializer) {
+      const struct = serializer.serialize(value);
+      if (typeof struct !== 'object' || struct === null || Object.getPrototypeOf(struct) !== Object.prototype) {
+        throw new TypeError('Serializer must return a plain object');
+      }
+
+      return Object.assign(
+        {
+          [StructTypeProperty]: serializer.typeId,
+        },
+        struct,
+      );
+    }
+
+    if (proto !== undefined && proto !== Object.prototype && proto !== Array.prototype) {
+      throw new TypeError('Encountered a non-plain object without registered serializer');
+    }
+
+    if (Array.isArray(value)) {
+      return value.map(serialize);
+    }
+
+    const res: any = {};
+    for (const key in value) {
+      res[key] = serialize((value as any)[key]);
+    }
+
+    return res;
+  }
+
+  return value;
+};
+
+export const deserialize = (value: unknown): unknown => {
+  if (Array.isArray(value)) {
+    return value.map(deserialize);
+  }
+
+  if (typeof value === 'object' && value !== null) {
+    if (StructTypeProperty in value) {
+      const type = value[StructTypeProperty];
+      if (typeof type !== 'string') {
+        throw new TypeError('Struct type property ("@type") must be a string');
+      }
+
+      const serializer = serializersById.get(type);
+      if (!serializer) {
+        throw new TypeError(`Unknown struct type: ${type}`);
+      }
+
+      return serializer.deserialize(value);
+    }
+
+    const res: any = {};
+    for (const key in value) {
+      res[key] = deserialize((value as any)[key]);
+    }
+
+    return res;
+  }
+
+  return value;
+};
