75eb04f9127f7fd2d047b7dbcff134d2979e6dcc
Will Shown <thure@users.noreply.github.com>
2025-06-08 14:09:24 -0700

    feat: `react-ui-graph` node popovers (#9267)
    Co-authored-by: Rich Burdon <rich.burdon@gmail.com>

--- packages/ui/react-ui-graph/src/hooks/useSvgContext.ts
+++ packages/ui/react-ui-graph/src/hooks/useSvgContext.ts
@@ -68,8 +68,9 @@ export class SVGContext {
   }
 
   setTransform(transform: ZoomTransform) {
-    this._scale.setTransform(transform);
-    this.resized.emit(this);
+    if (this._scale.setTransform(transform)) {
+      this.resized.emit(this);
+    }
   }
 
   /**
