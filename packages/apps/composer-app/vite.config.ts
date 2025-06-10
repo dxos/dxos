a8322624e31943c5e26eb1fe2a9a7132da73d2de
Dmytro Maretskyi <maretskii@gmail.com>
2025-06-10 17:00:12 +0200

    Enable safe-react transform

--- packages/apps/composer-app/vite.config.ts
+++ packages/apps/composer-app/vite.config.ts
@@ -163,6 +163,14 @@ export default defineConfig((env) => ({
             ],
           },
         ],
+        [
+          '@preact-signals/safe-react/swc',
+          {
+            // you should use `auto` mode to track only components which uses `.value` access.
+            // Can be useful to avoid tracking of server side components
+            mode: 'all',
+          },
+        ],
       ],
     }),
     importMapPlugin({
