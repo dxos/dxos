f60d6a2b30c076e6d10042b8b995774bcd2ed3c2
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2025-06-11 21:31:27 +0200

    feat: Use local xenova model for entity extraction (#9293)

---
+++ packages/core/assistant/src/extraction/quotes.test.ts
@@ -0,0 +1,27 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { describe, test, expect } from 'vitest';
+
+import { insertReferences } from './quotes';
+
+describe('insertReferences', () => {
+  test('should replace quotes with DXN references', () => {
+    const quotes = {
+      references: [{ quote: 'computational irreducibility', id: '01JTG9JW11XGWJZ32AW8ET93D1' }],
+    };
+
+    expect(insertReferences('This is a computational irreducibility test.', quotes)).toBe(
+      'This is a [computational irreducibility][dxn:echo:@:01JTG9JW11XGWJZ32AW8ET93D1] test.',
+    );
+    expect(
+      insertReferences(
+        "And what I'd like to talk today about is Steven Wolfram's concept of a computational irreducibility.",
+        quotes,
+      ),
+    ).toBe(
+      "And what I'd like to talk today about is Steven Wolfram's concept of a [computational irreducibility][dxn:echo:@:01JTG9JW11XGWJZ32AW8ET93D1].",
+    );
+  });
+});
