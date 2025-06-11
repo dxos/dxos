dce0654f758a3e88d6fcb41805abb3c7ebdfa722
Will Shown <thure@users.noreply.github.com>
2025-06-10 17:01:43 -0700

    feat: Surface variant stories (#9302)

--- packages/common/storybook-utils/src/decorators/withTheme.tsx
+++ packages/common/storybook-utils/src/decorators/withTheme.tsx
@@ -54,15 +54,16 @@ export const withTheme: Decorator = (Story, context) => {
     <ThemeProvider tx={defaultTx} themeMode={themeMode} resourceExtensions={context?.parameters?.translations} noCache>
       <Tooltip.Provider>
         <MemoizedStory />
-        <Dialog.Root open={editorOpen} onOpenChange={handleOpenChange}>
+        <Dialog.Root open={editorOpen} onOpenChange={handleOpenChange} modal={false}>
           <div
             role='none'
             className='dx-dialog__overlay bg-transparent pointer-events-none'
             style={{ placeContent: 'end' }}
           >
             <Dialog.Content
-              classNames='relative box-content py-0 px-2 md:is-[35rem] md:max-is-none overflow-y-auto layout-contain'
+              classNames='relative box-content py-0 px-2 md:is-[35rem] md:max-is-none overflow-y-auto layout-contain pointer-events-auto'
               style={{ maxBlockSize: '50dvh' }}
+              onInteractOutside={(event) => event.preventDefault()}
             >
               <Dialog.Title srOnly>Theme Editor</Dialog.Title>
               {editorOpen && <DxThemeEditor />}
--- packages/ui/react-ui-theme/src/config/tokens/alias-colors.ts
+++ packages/ui/react-ui-theme/src/config/tokens/alias-colors.ts
@@ -9,7 +9,7 @@ import { sheetAliases } from './sememes-sheet';
 import { systemAliases } from './sememes-system';
 
 export const aliasColors = {
-  conditions: { root: [':root'], attention: ['[data-is-attention-source], .current-related[aria-current]'] },
+  conditions: { root: [':root, .dark'], attention: ['[data-is-attention-source], .current-related[aria-current]'] },
   aliases: {
     // TODO(thure): Aliases should be merged more elegantly, this causes overwrites.
     ...sheetAliases,
