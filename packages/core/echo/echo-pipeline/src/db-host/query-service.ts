df25ffc315506eaa5636799da510fd75ebba81cf
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-06-11 15:42:10 +0200

    Delete unnecessary warning

--- packages/core/echo/echo-pipeline/src/db-host/query-service.ts
+++ packages/core/echo/echo-pipeline/src/db-host/query-service.ts
@@ -88,10 +88,7 @@ export class QueryServiceImpl extends Resource implements QueryService {
   }
 
   async setConfig(config: IndexConfig): Promise<void> {
-    if (this._params.indexer.initialized) {
-      log.warn('Setting index config, but indexer is already initialized.');
-    }
-    void this._params.indexer.setConfig(config);
+    await this._params.indexer.setConfig(config);
   }
 
   execQuery(request: QueryRequest): Stream<QueryResponse> {
