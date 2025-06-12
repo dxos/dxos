6f20a69f1623c817aebf522fbbdcf2438044fbe1
yarolegovich <yarolegovich@gmail.com>
2025-06-12 11:23:44 +0300

    fix incorrect log level mapping for sentry

--- packages/sdk/observability/src/sentry/sentry-log-processor.ts
+++ packages/sdk/observability/src/sentry/sentry-log-processor.ts
@@ -132,14 +132,25 @@ const formatMessageForSentry = (entry: LogEntry) => {
 };
 
 const convertLevel = (level: LogLevel): SeverityLevel => {
-  if (level === LogLevel.TRACE) {
-    return 'debug';
+  switch (level) {
+    case LogLevel.ERROR:
+      return 'error';
+    case LogLevel.VERBOSE:
+      return 'info';
+    case LogLevel.INFO:
+      return 'info';
+    case LogLevel.WARN:
+      return 'warning';
+    case LogLevel.DEBUG:
+    case LogLevel.TRACE:
+      return 'debug';
+    default:
+      throw never(level);
   }
-  if (level === LogLevel.WARN) {
-    return 'warning';
-  }
+};
 
-  return LogLevel[level].toLowerCase() as SeverityLevel;
+const never = (never: never) => {
+  return new Error('unhandled value');
 };
 
 const getRelativeFilename = (filename: string) => {
