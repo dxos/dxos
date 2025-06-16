5856b1175d90e7cf562efb395c430bec3bc187e6
Rich Burdon <rich.burdon@gmail.com>
2025-06-16 12:21:24 -0400

    feat: AI blueprint story (#9332)
    Co-authored-by: Dmytro Maretskyi <maretskii@gmail.com>
    Co-authored-by: Mykola Veremchuk <veremchukmukola@gmail.com>
    Co-authored-by: Josiah Witt <josiah@witt.life>

--- packages/core/echo/echo/src/Type.ts
+++ packages/core/echo/echo/src/Type.ts
@@ -10,17 +10,22 @@ import { invariant } from '@dxos/invariant';
 import type * as Keys from '@dxos/keys';
 
 /**
- * New EchoObject schema.
+ * ECHO schema.
+ */
+export type Schema = EchoSchema.EchoSchema;
+
+/**
+ * EchoObject schema.
  */
 export const Obj = EchoSchema.EchoObject;
 
 /**
- * New EchoRelation schema.
+ * EchoRelation schema.
  */
 export const Relation = EchoSchema.EchoRelation;
 
 /**
- * New Ref schema.
+ * Ref schema.
  */
 export const Ref: <S extends Obj.Any>(schema: S) => EchoSchema.Ref$<Schema.Schema.Type<S>> = EchoSchema.Ref;
 
@@ -63,8 +68,8 @@ export namespace Ref {
 /**
  * Gets the full DXN of the schema.
  * Will include the version if it's a `type` DXN.
- * @example dxn:example.com/type/Person:0.1.0
- * @example dxn:echo:SSSSSSSSSS:XXXXXXXXXXXXX
+ * @example "dxn:example.com/type/Person:0.1.0"
+ * @example "dxn:echo:SSSSSSSSSS:XXXXXXXXXXXXX"
  */
 export const getDXN = (schema: Obj.Any | Relation.Any): Keys.DXN | undefined => {
   return EchoSchema.getSchemaDXN(schema);
@@ -101,6 +106,7 @@ export type Meta = EchoSchema.TypeAnnotation;
 export const getMeta = (schema: Obj.Any | Relation.Any): Meta | undefined => {
   return EchoSchema.getTypeAnnotation(schema);
 };
+
 export { EntityKind as Kind } from '@dxos/echo-schema';
 
 /**
