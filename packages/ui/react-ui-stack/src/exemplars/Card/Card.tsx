f6fa78f4aee633bb8c93757f58922501b680c512
thure <will@braneframe.com>
2025-06-17 15:41:03 -0700

    Move Card business to StackItem exemplar.

---
+++ packages/ui/react-ui-stack/src/exemplars/Card/Card.tsx
@@ -0,0 +1,29 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { StackItem } from '../../components';
+
+const CardRoot = StackItem.Root;
+
+const CardContent = StackItem.Content;
+
+const CardHeading = StackItem.Heading;
+
+const CardHeadingLabel = StackItem.HeadingLabel;
+
+const CardResizeHandle = StackItem.ResizeHandle;
+
+const CardDragHandle = StackItem.DragHandle;
+
+const CardDragPreview = StackItem.DragPreview;
+
+export const Card = {
+  Root: CardRoot,
+  Content: CardContent,
+  Heading: CardHeading,
+  HeadingLabel: CardHeadingLabel,
+  ResizeHandle: CardResizeHandle,
+  DragHandle: CardDragHandle,
+  DragPreview: CardDragPreview,
+};
