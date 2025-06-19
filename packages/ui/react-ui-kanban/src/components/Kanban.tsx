71bee427c21534f00dd4ff37b0a0e3a445a30377
thure <will@braneframe.com>
2025-06-18 22:56:51 -0700

    Refactor CardStack to support Slot.

--- packages/ui/react-ui-kanban/src/components/Kanban.tsx
+++ packages/ui/react-ui-kanban/src/components/Kanban.tsx
@@ -16,7 +16,6 @@ import {
   CardStackDragPreview,
   Card,
   CardDragPreview,
-  cardStackContainer,
   cardStackHeading,
 } from '@dxos/react-ui-stack';
 
@@ -74,139 +73,139 @@ export const Kanban = ({ model, onAddCard, onRemoveCard }: KanbanProps) => {
         const nextSiblingId = index < array.length - 1 ? array[index + 1].columnValue : undefined;
         const columnItem = useMemo(() => ({ id: columnValue }), [columnValue]);
         return (
-          <StackItem.Root
-            key={columnValue}
-            item={columnItem}
-            size={20}
-            disableRearrange={uncategorized}
-            classNames={cardStackContainer}
-            focusIndicatorVariant='group'
-            prevSiblingId={prevSiblingId}
-            nextSiblingId={nextSiblingId}
-          >
-            <CardStack.Root>
-              <CardStack.Stack
-                id={columnValue}
-                onRearrange={model.handleRearrange}
-                itemsCount={cards.length}
-                getDropElement={getColumnDropElement}
-              >
-                {cards.map((card, cardIndex, cardsArray) => (
-                  <Card.Root asChild key={card.id}>
-                    <StackItem.Root
-                      item={card}
-                      focusIndicatorVariant='group'
-                      onClick={() => singleSelect(card.id)}
-                      prevSiblingId={cardIndex > 0 ? cardsArray[cardIndex - 1].id : undefined}
-                      nextSiblingId={cardIndex < cardsArray.length - 1 ? cardsArray[cardIndex + 1].id : undefined}
-                    >
-                      <Card.Content>
-                        <Card.Toolbar>
-                          <StackItem.DragHandle asChild>
-                            <Card.DragHandle toolbarItem />
-                          </StackItem.DragHandle>
-                          <AttentionGlyph attended={selected === card.id} />
-                          {onRemoveCard && (
-                            <>
-                              <Card.ToolbarSeparator variant='gap' />
-                              <Card.ToolbarIconButton
-                                iconOnly
-                                variant='ghost'
-                                icon='ph--x--regular'
-                                label={t('remove card label')}
-                                onClick={() => onRemoveCard(card)}
-                              />
-                            </>
-                          )}
-                        </Card.Toolbar>
-                        <Surface role='card--kanban' limit={1} data={{ subject: card }} />
-                      </Card.Content>
-                      <StackItem.DragPreview>
-                        {({ item }) => (
-                          <CardDragPreview.Root>
-                            <CardDragPreview.Content>
-                              <Card.Toolbar>
-                                <Card.DragHandle />
-                              </Card.Toolbar>
-                              <Surface role='card--kanban' limit={1} data={{ subject: item }} />
-                            </CardDragPreview.Content>
-                          </CardDragPreview.Root>
-                        )}
-                      </StackItem.DragPreview>
-                    </StackItem.Root>
-                  </Card.Root>
-                ))}
-              </CardStack.Stack>
-
-              {onAddCard && (
-                <CardStack.Footer>
-                  <IconButton
-                    icon='ph--plus--regular'
-                    label={t('add card label')}
-                    onClick={() => handleAddCard(columnValue)}
-                    classNames='is-full'
-                  />
-                </CardStack.Footer>
-              )}
-
-              <StackItem.Heading classNames={cardStackHeading}>
-                <StackItem.DragHandle asChild>
-                  <CardStack.DragHandle />
-                </StackItem.DragHandle>
-                <Tag
-                  palette={color as any}
-                  data-uncategorized={uncategorized}
-                  classNames='mis-1 data-[uncategorized="true"]:mis-2'
+          <CardStack.Root asChild key={columnValue}>
+            <StackItem.Root
+              item={columnItem}
+              size={20}
+              disableRearrange={uncategorized}
+              focusIndicatorVariant='group'
+              prevSiblingId={prevSiblingId}
+              nextSiblingId={nextSiblingId}
+            >
+              <CardStack.Content>
+                <CardStack.Stack
+                  id={columnValue}
+                  onRearrange={model.handleRearrange}
+                  itemsCount={cards.length}
+                  getDropElement={getColumnDropElement}
                 >
-                  {title}
-                </Tag>
-              </StackItem.Heading>
-            </CardStack.Root>
-            <StackItem.DragPreview>
-              {({ item }) => {
-                // Find the column data for this item
-                const columnData = model.arrangedCards.find((col) => col.columnValue === item.id);
-                if (!columnData) {
-                  return null;
-                }
-
-                const { cards, columnValue } = columnData;
-                const { color, title } = model.getPivotAttributes(columnValue);
-                const uncategorized = columnValue === UNCATEGORIZED_VALUE;
-
-                return (
-                  <CardStackDragPreview.Root>
-                    {/* Column Header */}
-                    <CardStackDragPreview.Heading>
-                      <Tag
-                        palette={color as any}
-                        data-uncategorized={uncategorized}
-                        classNames='mis-1 data-[uncategorized="true"]:mis-2'
+                  {cards.map((card, cardIndex, cardsArray) => (
+                    <Card.Root asChild key={card.id}>
+                      <StackItem.Root
+                        item={card}
+                        focusIndicatorVariant='group'
+                        onClick={() => singleSelect(card.id)}
+                        prevSiblingId={cardIndex > 0 ? cardsArray[cardIndex - 1].id : undefined}
+                        nextSiblingId={cardIndex < cardsArray.length - 1 ? cardsArray[cardIndex + 1].id : undefined}
                       >
-                        {title}
-                      </Tag>
-                    </CardStackDragPreview.Heading>
-
-                    {/* Cards Container */}
-                    <CardStackDragPreview.Content itemsCount={cards.length}>
-                      {cards.map((card) => (
-                        <Card.Content key={card.id}>
+                        <Card.Content>
+                          <Card.Toolbar>
+                            <StackItem.DragHandle asChild>
+                              <Card.DragHandle toolbarItem />
+                            </StackItem.DragHandle>
+                            <AttentionGlyph attended={selected === card.id} />
+                            {onRemoveCard && (
+                              <>
+                                <Card.ToolbarSeparator variant='gap' />
+                                <Card.ToolbarIconButton
+                                  iconOnly
+                                  variant='ghost'
+                                  icon='ph--x--regular'
+                                  label={t('remove card label')}
+                                  onClick={() => onRemoveCard(card)}
+                                />
+                              </>
+                            )}
+                          </Card.Toolbar>
                           <Surface role='card--kanban' limit={1} data={{ subject: card }} />
                         </Card.Content>
-                      ))}
-                    </CardStackDragPreview.Content>
+                        <StackItem.DragPreview>
+                          {({ item }) => (
+                            <CardDragPreview.Root>
+                              <CardDragPreview.Content>
+                                <Card.Toolbar>
+                                  <Card.DragHandle />
+                                </Card.Toolbar>
+                                <Surface role='card--kanban' limit={1} data={{ subject: item }} />
+                              </CardDragPreview.Content>
+                            </CardDragPreview.Root>
+                          )}
+                        </StackItem.DragPreview>
+                      </StackItem.Root>
+                    </Card.Root>
+                  ))}
+                </CardStack.Stack>
 
-                    {/* Add Card Button */}
-                    {onAddCard && (
-                      <CardStackDragPreview.Footer>
-                        <IconButton icon='ph--plus--regular' label={t('add card label')} classNames='is-full' />
-                      </CardStackDragPreview.Footer>
-                    )}
-                  </CardStackDragPreview.Root>
-                );
-              }}
-            </StackItem.DragPreview>
-          </StackItem.Root>
+                {onAddCard && (
+                  <CardStack.Footer>
+                    <IconButton
+                      icon='ph--plus--regular'
+                      label={t('add card label')}
+                      onClick={() => handleAddCard(columnValue)}
+                      classNames='is-full'
+                    />
+                  </CardStack.Footer>
+                )}
+
+                <StackItem.Heading classNames={cardStackHeading}>
+                  <StackItem.DragHandle asChild>
+                    <CardStack.DragHandle />
+                  </StackItem.DragHandle>
+                  <Tag
+                    palette={color as any}
+                    data-uncategorized={uncategorized}
+                    classNames='mis-1 data-[uncategorized="true"]:mis-2'
+                  >
+                    {title}
+                  </Tag>
+                </StackItem.Heading>
+              </CardStack.Content>
+              <StackItem.DragPreview>
+                {({ item }) => {
+                  // Find the column data for this item
+                  const columnData = model.arrangedCards.find((col) => col.columnValue === item.id);
+                  if (!columnData) {
+                    return null;
+                  }
+
+                  const { cards, columnValue } = columnData;
+                  const { color, title } = model.getPivotAttributes(columnValue);
+                  const uncategorized = columnValue === UNCATEGORIZED_VALUE;
+
+                  return (
+                    <CardStackDragPreview.Root>
+                      {/* Column Header */}
+                      <CardStackDragPreview.Heading>
+                        <Tag
+                          palette={color as any}
+                          data-uncategorized={uncategorized}
+                          classNames='mis-1 data-[uncategorized="true"]:mis-2'
+                        >
+                          {title}
+                        </Tag>
+                      </CardStackDragPreview.Heading>
+
+                      {/* Cards Container */}
+                      <CardStackDragPreview.Content itemsCount={cards.length}>
+                        {cards.map((card) => (
+                          <Card.Content key={card.id}>
+                            <Surface role='card--kanban' limit={1} data={{ subject: card }} />
+                          </Card.Content>
+                        ))}
+                      </CardStackDragPreview.Content>
+
+                      {/* Add Card Button */}
+                      {onAddCard && (
+                        <CardStackDragPreview.Footer>
+                          <IconButton icon='ph--plus--regular' label={t('add card label')} classNames='is-full' />
+                        </CardStackDragPreview.Footer>
+                      )}
+                    </CardStackDragPreview.Root>
+                  );
+                }}
+              </StackItem.DragPreview>
+            </StackItem.Root>
+          </CardStack.Root>
         );
       })}
     </Stack>
--- packages/ui/react-ui-stack/src/exemplars/CardStack/CardStack.tsx
+++ packages/ui/react-ui-stack/src/exemplars/CardStack/CardStack.tsx
@@ -2,14 +2,16 @@
 // Copyright 2025 DXOS.org
 //
 
-import React, { forwardRef, type PropsWithChildren } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import React, { type ComponentPropsWithoutRef, forwardRef } from 'react';
 
+import type { ThemedClassName } from '@dxos/react-ui';
 import { mx } from '@dxos/react-ui-theme';
 
 import { railGridHorizontalContainFitContent, Stack, type StackProps } from '../../components';
 import { Card } from '../Card';
 
-const cardStackContainer = 'flex flex-col pli-2 plb-2';
+type SharedCardStackProps = ThemedClassName<ComponentPropsWithoutRef<'div'>> & { asChild?: boolean };
 
 const CardStackStack = forwardRef<
   HTMLDivElement,
@@ -38,44 +40,79 @@ const CardStackDragHandle = Card.DragHandle;
 
 const cardStackHeading = 'mli-2 order-first bg-transparent rounded-bs-md flex items-center';
 
-const CardStackHeading = forwardRef<HTMLDivElement, PropsWithChildren<{}>>(({ children }, forwardedRef) => {
-  return (
-    <div role='heading' className={cardStackHeading} ref={forwardedRef}>
-      {children}
-    </div>
-  );
-});
+const CardStackHeading = forwardRef<HTMLDivElement, SharedCardStackProps>(
+  ({ children, classNames, asChild, role = 'heading', ...props }, forwardedRef) => {
+    const Root = asChild ? Slot : 'div';
+    const rootProps = asChild
+      ? { classNames: [cardStackHeading, classNames] }
+      : { className: mx(cardStackHeading, classNames), role };
+    return (
+      <Root {...props} {...rootProps} ref={forwardedRef}>
+        {children}
+      </Root>
+    );
+  },
+);
 
 const cardStackFooter =
   'plb-2 mli-2 border-bs border-transparent [[data-scroll-separator-end="true"]_&]:border-subduedSeparator';
 
-const CardStackFooter = forwardRef<HTMLDivElement, PropsWithChildren<{}>>(({ children }, forwardedRef) => {
-  return (
-    <div role='none' className={cardStackFooter} ref={forwardedRef}>
-      {children}
-    </div>
-  );
-});
-
-const cardStackRoot = mx(
-  'shrink min-bs-0 bg-baseSurface border border-separator rounded-md grid dx-focus-ring-group-x-indicator kanban-drop',
-  railGridHorizontalContainFitContent,
+const CardStackFooter = forwardRef<HTMLDivElement, SharedCardStackProps>(
+  ({ children, classNames, asChild, role = 'none', ...props }, forwardedRef) => {
+    const Root = asChild ? Slot : 'div';
+    const rootProps = asChild
+      ? { classNames: [cardStackFooter, classNames] }
+      : { className: mx(cardStackFooter, classNames), role };
+    return (
+      <Root {...props} {...rootProps} ref={forwardedRef}>
+        {children}
+      </Root>
+    );
+  },
 );
 
-const CardStackRoot = forwardRef<HTMLDivElement, PropsWithChildren<{}>>(({ children }, forwardedRef) => {
-  return (
-    <div role='none' className={cardStackRoot} data-scroll-separator='false' ref={forwardedRef}>
-      {children}
-    </div>
-  );
-});
+const cardStackContent = [
+  'shrink min-bs-0 bg-baseSurface border border-separator rounded-md grid dx-focus-ring-group-x-indicator kanban-drop',
+  railGridHorizontalContainFitContent,
+];
+
+const CardStackContent = forwardRef<HTMLDivElement, SharedCardStackProps>(
+  ({ children, classNames, asChild, role = 'none', ...props }, forwardedRef) => {
+    const Root = asChild ? Slot : 'div';
+    const rootProps = asChild
+      ? { classNames: [...cardStackContent, classNames] }
+      : { className: mx(...cardStackContent, classNames), role };
+    return (
+      <Root {...props} {...rootProps} data-scroll-separator='false' ref={forwardedRef}>
+        {children}
+      </Root>
+    );
+  },
+);
+
+const cardStackRoot = 'flex flex-col pli-2 plb-2';
+
+const CardStackRoot = forwardRef<HTMLDivElement, SharedCardStackProps>(
+  ({ children, classNames, asChild, role = 'none', ...props }, forwardedRef) => {
+    const Root = asChild ? Slot : 'div';
+    const rootProps = asChild
+      ? { classNames: [cardStackRoot, classNames] }
+      : { className: mx(cardStackRoot, classNames), role };
+    return (
+      <Root {...props} {...rootProps} ref={forwardedRef}>
+        {children}
+      </Root>
+    );
+  },
+);
 
 export const CardStack = {
   Root: CardStackRoot,
+  Content: CardStackContent,
   Stack: CardStackStack,
   Heading: CardStackHeading,
   Footer: CardStackFooter,
   DragHandle: CardStackDragHandle,
 };
 
-export { cardStackRoot, cardStackFooter, cardStackHeading, cardStackContainer };
+export { cardStackRoot, cardStackFooter, cardStackHeading, cardStackContent };
