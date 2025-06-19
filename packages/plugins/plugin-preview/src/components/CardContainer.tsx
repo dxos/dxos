1abf4e85b9c41db348a1995ac103958456b9c7e2
thure <will@braneframe.com>
2025-06-19 16:26:31 -0700

    Remove log.

--- packages/plugins/plugin-preview/src/components/CardContainer.tsx
+++ packages/plugins/plugin-preview/src/components/CardContainer.tsx
@@ -9,6 +9,5 @@ import { Card } from '@dxos/react-ui-stack';
 export const CardContainer = ({ role, children }: PropsWithChildren<{ role: string }>) => {
   const Root = role === 'popover' ? 'div' : role === 'card--kanban' ? Fragment : Card.Content;
   const rootProps = role === 'popover' ? { className: 'popover-card-width' } : {};
-  console.log('[card container]', role, rootProps);
   return <Root {...rootProps}>{children}</Root>;
 };
