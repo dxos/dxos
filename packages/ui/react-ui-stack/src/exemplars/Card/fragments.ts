e161d7cf9f4e1d998ecaa3deb27d2f92e9a3c0b1
Will Shown <thure@users.noreply.github.com>
2025-06-19 18:53:43 -0700

    refactor: Move more cardlike patterns into `Card` exemplar (#9368)

--- packages/ui/react-ui-stack/src/exemplars/Card/fragments.ts
+++ packages/ui/react-ui-stack/src/exemplars/Card/fragments.ts
@@ -5,4 +5,10 @@
 export const cardRoot = 'contain-layout pli-2 plb-1 first-of-type:pbs-0 last-of-type:pbe-0';
 
 export const cardContent =
-  'rounded overflow-hidden bg-cardSurface border border-separator dark:border-subduedSeparator dx-focus-ring-group-y-indicator relative min-bs-[--rail-item]';
+  'rounded overflow-hidden bg-cardSurface border border-separator dark:border-subduedSeparator dx-focus-ring-group-y-indicator relative min-bs-[--rail-item] group/card';
+
+export const cardText = 'pli-3 mlb-3';
+
+export const cardHeading = 'text-lg font-medium line-clamp-2';
+
+export const cardChrome = 'pli-1.5 mlb-1.5 [&_.dx-button]:pli-1.5 [&_.dx-button]:text-start [&_.dx-button]:is-full';
