47779c66e2bcc75b670dd37c384caef0be01a80a
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2025-06-23 16:06:09 +0200

    fix: Fix types in invocation panel (#9383)

--- packages/devtools/devtools/src/panels/edge/InvocationTracePanel/ExceptionPanel.tsx
+++ packages/devtools/devtools/src/panels/edge/InvocationTracePanel/ExceptionPanel.tsx
@@ -4,7 +4,6 @@
 
 import React, { type FC, useMemo } from 'react';
 
-import { decodeReference } from '@dxos/echo-protocol';
 import { type TraceEvent, type InvocationSpan } from '@dxos/functions';
 import { useQueue } from '@dxos/react-client/echo';
 import { mx } from '@dxos/react-ui-theme';
@@ -15,7 +14,7 @@ type ExceptionPanelProps = {
 
 export const ExceptionPanel: FC<ExceptionPanelProps> = ({ span }) => {
   const traceQueueDxn = useMemo(() => {
-    return span.invocationTraceQueue ? decodeReference(span.invocationTraceQueue).dxn : undefined;
+    return span.invocationTraceQueue ? span.invocationTraceQueue.dxn : undefined;
   }, [span.invocationTraceQueue]);
 
   const eventQueue = useQueue<TraceEvent>(traceQueueDxn, { pollInterval: 2000 });
