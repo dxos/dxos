be9ee4cfe566cd27583ace7044c78166ed7857d6
Dmytro Maretskyi <maretskii@gmail.com>
2025-06-24 12:54:35 +0200

    Tracing test

--- packages/common/effect/src/tracing.test.ts
+++ packages/common/effect/src/tracing.test.ts
@@ -3,6 +3,16 @@ import { Effect, Layer, Tracer } from 'effect';
 import type { Context } from 'effect/Context';
 import type { Exit } from 'effect/Exit';
 import type { Option } from 'effect/Option';
+import { NodeSdk } from '@effect/opentelemetry';
+import {
+  ConsoleSpanExporter,
+  BatchSpanProcessor,
+  SimpleSpanProcessor,
+  SpanExporter,
+  type Span,
+  type ReadableSpan,
+} from '@opentelemetry/sdk-trace-base';
+import { type ExportResult, ExportResultCode } from '@opentelemetry/core';
 
 const apples = Effect.fn('apples')(function* (count: number) {
   const appleCount = Math.floor(count / 2);
@@ -18,42 +28,61 @@ const fruits = Effect.gen(function* () {
   yield* apples(20);
 });
 
-class MySpan implements Tracer.Span {
-  _tag: 'Span' = 'Span';
-  constructor(
-    public readonly name: string,
-    public readonly parent: Option<Tracer.AnySpan>,
-    public readonly context: Context<never>,
-    public readonly links: readonly Tracer.SpanLink[],
-    public readonly startTime: bigint,
-    public readonly kind: Tracer.SpanKind,
-  ) {
-    this.spanId = Math.random().toString(36).substring(2, 15);
-    this.traceId = Math.random().toString(36).substring(2, 15);
-  }
-
-  spanId: string;
-  traceId: string;
-
-  status: Tracer.SpanStatus = { _tag: 'Started', startTime: BigInt(0) };
-  attributes: ReadonlyMap<string, unknown> = new Map();
-  sampled: boolean = false;
-  end(endTime: bigint, exit: Exit<unknown, unknown>): void {
-    console.log('end', { endTime, exit });
-  }
-  attribute(key: string, value: unknown): void {
-    console.log('attribute', { key, value });
-  }
-  event(name: string, startTime: bigint, attributes?: Record<string, unknown>): void {
-    console.log('event', { name, startTime, attributes });
-  }
-  addLinks(links: ReadonlyArray<Tracer.SpanLink>): void {
-    console.log('addLinks', { links });
-  }
-}
-
 describe('tracing', () => {
-  test('should trace', async () => {
+  test.only('otel tracer', async () => {
+    class MySpanExporter implements SpanExporter {
+      export(spans: ReadableSpan[], resultCallback: (result: ExportResult) => void): void {
+        console.log('export', { spans });
+        resultCallback({ code: ExportResultCode.SUCCESS });
+      }
+      async shutdown(): Promise<void> {}
+      async forceFlush?(): Promise<void> {}
+    }
+    const spanProcessor = new BatchSpanProcessor(new MySpanExporter());
+    const NodeSdkLive = NodeSdk.layer(() => ({
+      resource: { serviceName: 'example' },
+      // Export span data to the console
+      spanProcessor,
+    }));
+    await Effect.runPromise(fruits.pipe(Effect.provide(NodeSdkLive)));
+    await spanProcessor.shutdown();
+  });
+
+  test('custom tracer', async () => {
+    class MySpan implements Tracer.Span {
+      _tag: 'Span' = 'Span';
+      constructor(
+        public readonly name: string,
+        public readonly parent: Option<Tracer.AnySpan>,
+        public readonly context: Context<never>,
+        public readonly links: readonly Tracer.SpanLink[],
+        public readonly startTime: bigint,
+        public readonly kind: Tracer.SpanKind,
+      ) {
+        this.spanId = Math.random().toString(36).substring(2, 15);
+        this.traceId = Math.random().toString(36).substring(2, 15);
+      }
+
+      spanId: string;
+      traceId: string;
+
+      status: Tracer.SpanStatus = { _tag: 'Started', startTime: BigInt(0) };
+      attributes: ReadonlyMap<string, unknown> = new Map();
+      sampled: boolean = false;
+      end(endTime: bigint, exit: Exit<unknown, unknown>): void {
+        console.log('end', { endTime, exit });
+      }
+      attribute(key: string, value: unknown): void {
+        console.log('attribute', { key, value });
+      }
+      event(name: string, startTime: bigint, attributes?: Record<string, unknown>): void {
+        console.log('event', { name, startTime, attributes });
+      }
+      addLinks(links: ReadonlyArray<Tracer.SpanLink>): void {
+        console.log('addLinks', { links });
+      }
+    }
+
     const tracer = Tracer.make({
       context(f, fiber) {
         return f();
