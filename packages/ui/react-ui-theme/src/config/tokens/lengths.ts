052dc068b07dbee1872df9be332648abcd344c37
thure <will@braneframe.com>
2025-06-24 14:15:36 -0700

    Revert naming refactor.

--- packages/ui/react-ui-theme/src/config/tokens/lengths.ts
+++ packages/ui/react-ui-theme/src/config/tokens/lengths.ts
@@ -15,24 +15,13 @@ export const lengthsFacet = {
           unit: 'px',
           initial: 0,
           slope: 1,
-          naming: {
-            noLine: 0,
-            hairLine: 1,
-            thickLine: 2,
-          },
         },
       },
-      trim: {
+      lacuna: {
         root: {
           unit: 'rem',
           initial: 0,
           slope: 0.125,
-          naming: {
-            trimXs: 3,
-            trimSm: 6,
-            trimMd: 9,
-            trimLg: 12,
-          },
         },
       },
     },
@@ -41,7 +30,14 @@ export const lengthsFacet = {
   semantic: {
     namespace: 'dx-',
     conditions: { root: [':root'] },
-    sememes: {},
+    sememes: {
+      noLine: { root: ['line', 0] },
+      hairLine: { root: ['line', 1] },
+      thickLine: { root: ['line', 2] },
+      trimXs: { root: ['lacuna', 3] },
+      trimSm: { root: ['lacuna', 6] },
+      trimMd: { root: ['lacuna', 9] },
+    },
   },
 
   alias: {
@@ -49,15 +45,14 @@ export const lengthsFacet = {
     conditions: {
       fine: [':root, .density-fine, [data-density="fine"]'],
       coarse: ['.density-coarse, [data-density="coarse"]'],
+      flush: ['.density-flush, [data-density="flush"]'],
     },
     aliases: {
-      // lines
-      noLine: { fine: ['focusOffset'] },
-      hairLine: { fine: ['modalLine', 'landmarkLine', 'positionedLine', 'gridGap'] },
-      thickLine: { fine: ['focusLine'] },
-      // spacings
-      trimXs: { fine: ['card-spacing-chrome'] },
-      trimSm: { fine: ['card-spacing-inline', 'card-spacing-block'] },
+      noLine: { root: ['focusOffset'] },
+      hairLine: { root: ['modalLine', 'landmarkLine', 'positionedLine', 'gridGap'] },
+      thickLine: { root: ['focusLine'] },
+      trimSm: { root: ['card-spacing-inline', 'card-spacing-block'] },
+      trimXs: { root: ['card-spacing-chrome'] },
     },
   },
 } satisfies Facet;
