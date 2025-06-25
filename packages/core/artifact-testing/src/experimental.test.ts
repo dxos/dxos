69fa22188d36e9b72e5c61cd9e5dc4e399616b39
Dmytro Maretskyi <maretskii@gmail.com>
2025-06-25 16:26:05 +0200

    Add bp test

--- packages/core/artifact-testing/src/experimental.test.ts
+++ packages/core/artifact-testing/src/experimental.test.ts
@@ -5,14 +5,31 @@
 import { EchoTestBuilder } from '@dxos/echo-db/testing';
 import { beforeAll, describe, test } from 'vitest';
 
-import { EdgeAiServiceClient, OllamaAiServiceClient } from '@dxos/ai';
+import { EdgeAiServiceClient, OllamaAiServiceClient, ToolRegistry } from '@dxos/ai';
 import { AI_SERVICE_ENDPOINT, EXA_API_KEY } from '@dxos/ai/testing';
-import { researchFn } from '@dxos/assistant';
+import {
+  BlueprintMachine,
+  BlueprintParser,
+  createExaTool,
+  createGraphWriterTool,
+  createLocalSearchTool,
+  researchFn,
+  setConsolePrinter,
+  type BlueprintDefinition,
+} from '@dxos/assistant';
 import { Obj } from '@dxos/echo';
-import { type EchoDatabase } from '@dxos/echo-db';
-import { ConfiguredCredentialsService, FunctionExecutor, ServiceContainer, TracingService } from '@dxos/functions';
+import { type EchoDatabase, type QueueFactory } from '@dxos/echo-db';
+import {
+  AiService,
+  ConfiguredCredentialsService,
+  FunctionExecutor,
+  ServiceContainer,
+  TracingService,
+} from '@dxos/functions';
+import { log } from '@dxos/log';
 import { DataType, DataTypes } from '@dxos/schema';
-import { inspect } from 'util';
+import { isNonNullable } from '@dxos/util';
+import { inspect } from 'node:util';
 
 const REMOTE_AI = true;
 const MOCK_SEARCH = false;
@@ -20,13 +37,14 @@ const MOCK_SEARCH = false;
 describe('experimental', () => {
   let builder: EchoTestBuilder;
   let db: EchoDatabase;
+  let queues: QueueFactory;
   let serviceContainer: ServiceContainer;
 
   beforeAll(async () => {
     // TODO(dmaretskyi): Helper to scaffold this from a config.
     builder = await new EchoTestBuilder().open();
 
-    db = (await builder.createDatabase({ indexing: { vector: true } })).db;
+    ({ db, queues } = await builder.createDatabase({ indexing: { vector: true } }));
     db.graph.schemaRegistry.addSchema(DataTypes);
 
     serviceContainer = new ServiceContainer().setServices({
@@ -51,13 +69,66 @@ describe('experimental', () => {
     });
   });
 
-  test('bueprint');
+  test('blueprint', { timeout: 120_000 }, async () => {
+    const blueprintDefinition: BlueprintDefinition = {
+      steps: [
+        {
+          instructions: 'Research information and entities related to the selected objects.',
+          tools: ['search/web_search'],
+        },
+        {
+          instructions:
+            'Based on your research find matching entires that are already in the graph. Do exaustive research.',
+          tools: ['search/local_search'],
+        },
+        {
+          instructions: 'Add researched data to the graph. Make connections to existing objects.',
+          tools: ['search/local_search', 'graph/writer'],
+        },
+      ],
+    };
+
+    const blueprint = BlueprintParser.create().parse(blueprintDefinition);
+
+    const researchQueue = queues.create();
+    const tools = new ToolRegistry(
+      [
+        createExaTool({ apiKey: EXA_API_KEY }),
+        createLocalSearchTool(db, researchQueue),
+        createGraphWriterTool({
+          db,
+          queue: researchQueue,
+          schema: DataTypes,
+          onDone: async (objects) => {
+            await researchQueue.append(objects);
+          },
+        }),
+      ].filter(isNonNullable),
+    );
+
+    const company = db.add(
+      Obj.make(DataType.Organization, {
+        name: 'Notion',
+        website: 'https://www.notion.com',
+      }),
+    );
+    await db.flush({ indexes: true });
+
+    const machine = new BlueprintMachine(tools, blueprint);
+    const { client } = serviceContainer.getService(AiService);
+    setConsolePrinter(machine, true);
+    await machine.runToCompletion({ aiClient: client, input: [company] });
+
+    log.info('researched', {
+      objects: await researchQueue.queryObjects(),
+    });
+  });
 
   test('circuit');
 
   test('conversation');
 
-  test('function', async () => {
+  test('function', { timeout: 120_000 }, async () => {
     db.add(
       Obj.make(DataType.Organization, {
         name: 'Notion',
