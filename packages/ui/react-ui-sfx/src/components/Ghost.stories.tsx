de29d5bbc51d6f310479caa5a9cb15360f715269
Rich Burdon <rich.burdon@gmail.com>
2025-06-27 22:06:44 -0400

    test: WebGL test (#9428)

---
+++ packages/ui/react-ui-sfx/src/components/Ghost.stories.tsx
@@ -0,0 +1,58 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+import '@dxos-theme';
+
+import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+
+import { DXOS } from '@dxos/brand';
+import { withLayout, withTheme } from '@dxos/storybook-utils';
+
+import { Ghost, type GhostProps } from './Ghost';
+
+const meta: Meta<GhostProps> = {
+  title: 'ui/react-ui-sfx/Ghost',
+  component: Ghost,
+  render: (props) => (
+    <>
+      <Ghost {...props} />
+      <div className='inset-0 absolute flex justify-center items-center opacity-5'>
+        <DXOS className='w-[40rem] h-[40rem]' />
+      </div>
+    </>
+  ),
+  decorators: [withTheme, withLayout({ fullscreen: true, classNames: 'bg-black' })],
+};
+
+export default meta;
+
+type Story = StoryObj<GhostProps>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Variant: Story = {
+  args: {
+    DENSITY_DISSIPATION: 3.5,
+    VELOCITY_DISSIPATION: 2,
+    PRESSURE: 0.1,
+    PRESSURE_ITERATIONS: 20,
+    CURL: 2,
+    COLOR_UPDATE_SPEED: 0.3,
+    COLOR_MASK: { r: 0.1, g: 0.1, b: 0.1 },
+  },
+};
+
+export const Trace: Story = {
+  args: {
+    DENSITY_DISSIPATION: 1.5,
+    VELOCITY_DISSIPATION: 20,
+    PRESSURE: 0.1,
+    PRESSURE_ITERATIONS: 20,
+    CURL: 3,
+    COLOR_UPDATE_SPEED: 10,
+  },
+};
