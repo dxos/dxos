d3c57095da07561a9b32c5afb53d64f85b11fef1
Josiah Witt <josiah@witt.life>
2025-07-03 10:00:43 -0400

    check passes

--- .github/workflows/scripts/test-act.sh
+++ .github/workflows/scripts/test-act.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Set up environment variables.
-export ACT_RUNNER_ARCH=arm64
-export ACT_RUNNER_OS=linux
-export ACT_RUNNER_WORKING_DIR=$(pwd)
+# export ACT_RUNNER_ARCH=arm64
+# export ACT_RUNNER_OS=linux
+# export ACT_RUNNER_WORKING_DIR=".github/workflows"
 
 WORKFLOW=${1:-main.yml}
 JOB=${2:-ci}
@@ -12,4 +12,7 @@ JOB=${2:-ci}
 act \
   --job "$JOB" \
   --workflows ".github/workflows/$WORKFLOW" \
-  --eventpath ".github/workflows/testing/test-push-event.json" 
+  --eventpath ".github/workflows/testing/test-push-event.json" \
+  --secret-file .github/workflows/env/.secrets \
+  --container-architecture linux/amd64 \
+  -P ghcr.io/dxos/gh-actions:20.12.1=ghcr.io/dxos/gh-actions:latest
