957a741527e4ab67fa408243b7e004de8be6b185
Rich Burdon <rich.burdon@gmail.com>
2025-07-05 03:14:11 -0400

    chore: Test reporter tool (#9461)

---
+++ .ladle/config.mjs
@@ -0,0 +1,10 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+// https://ladle.dev/docs/config
+
+/** @type {import('@ladle/react').UserConfig} */
+export default {
+  stories: 'packages/devtools/storybook-utils/src/**/*.stories.{js,jsx,ts,tsx,mdx}'
+};
