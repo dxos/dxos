a1d834fae02fd2e2fd0630b1e092126963ab57b9
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2025-07-10 13:46:00 +0200

    feat: Blueprints (chat/artifact modules) (#9498)

---
+++ packages/core/assistant/src/blueprint/blueprint-binding.ts
@@ -0,0 +1,29 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { Schema } from 'effect';
+
+import { Type } from '@dxos/echo';
+
+import { Blueprint } from './blueprint';
+
+/**
+ * Thread message that binds or unbinds blueprints to a conversation.
+ */
+export const BlueprintBinding = Schema.Struct({
+  /**
+   * Blueprints added to a conversation.
+   */
+  added: Type.Ref(Blueprint),
+
+  /**
+   * Blueprints removed from a conversation.
+   */
+  removed: Type.Ref(Blueprint),
+}).pipe(
+  Type.Obj({
+    typename: 'dxos.org/type/BlueprintBinding',
+    version: '0.1.0',
+  }),
+);
