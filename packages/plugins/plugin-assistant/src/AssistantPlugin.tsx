eff81534dbce6308f0575953b0a8e8c85753330a
Will Shown <thure@users.noreply.github.com>
2025-07-09 17:09:47 -0700

    fix: Missing icon (#9493)

--- packages/plugins/plugin-assistant/src/AssistantPlugin.tsx
+++ packages/plugins/plugin-assistant/src/AssistantPlugin.tsx
@@ -44,7 +44,7 @@ export const AssistantPlugin = () =>
         contributes(Capabilities.Metadata, {
           id: Type.getTypename(Sequence),
           metadata: {
-            icon: 'ph--sequence--regular',
+            icon: 'ph--circuitry--regular',
           },
         }),
         contributes(Capabilities.Metadata, {
