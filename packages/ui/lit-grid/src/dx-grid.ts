777519eff91025ef2d543636c845b50917a662a3
thure <will@braneframe.com>
2025-07-15 15:23:09 -0700

    No longer any need to hide content when cell is being edited.

--- packages/ui/lit-grid/src/dx-grid.ts
+++ packages/ui/lit-grid/src/dx-grid.ts
@@ -1335,9 +1335,7 @@ export class DxGrid extends LitElement {
       aria-rowindex=${row}
       style="grid-column:${visCol + 1};grid-row:${visRow + 1}"
     >
-      <div role="none" class="dx-grid__cell__content">
-        ${this.mode !== 'browse' && active ? null : cell?.value}${this.mode !== 'browse' && active ? null : accessory}
-      </div>
+      <div role="none" class="dx-grid__cell__content">${cell?.value}${accessory}</div>
       ${cell?.resizeHandle &&
       this.mode === 'browse' &&
       this.axisResizeable(resizePlane!, cell.resizeHandle, resizeIndex!)
