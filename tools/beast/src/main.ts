34f36a7c85499ef1b2bf3fadd89f11c4f467e87a
Josiah Witt <josiah@witt.life>
2025-07-16 09:03:04 -0400

    restore beast outside of workspace

---
+++ tools/beast/src/main.ts
@@ -0,0 +1,165 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import { cosmiconfig } from 'cosmiconfig';
+import * as process from 'process';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import { log } from '@dxos/log';
+
+import { PackageDependencyBuilder, WorkspaceProcessor } from './nx';
+import { getBaseDir } from './util';
+
+// NOTE: Currently deprecated and not a part of the moon workspace as it was built on top of Nx.
+// TODO(wittjosiah): Consider folding this into toolbox.
+
+export type Config = {
+  exclude?: string[];
+};
+
+const loadConfig = async () => {
+  const explorer = cosmiconfig('beast');
+  return explorer.search();
+};
+
+const main = async () => {
+  const config = await loadConfig();
+
+  log.info(`beast config: ${config?.filepath ?? 'internal defaults'}`);
+
+  yargs(hideBin(process.argv))
+    .scriptName('beast')
+    .option('json', {
+      type: 'boolean',
+    })
+    .option('verbose', {
+      type: 'boolean',
+    })
+    .option('base-dir', {
+      type: 'string',
+      default: getBaseDir(),
+    })
+
+    .command({
+      command: 'list',
+      handler: ({ json, verbose, baseDir }: { json: boolean; verbose?: boolean; baseDir: string }) => {
+        const processor = new WorkspaceProcessor(baseDir, { verbose }).init();
+        const projects = processor.getProjects().map((p) => p.package.name);
+
+        if (json) {
+          log.info(JSON.stringify({ projects }, undefined, 2));
+        } else {
+          projects.forEach((m) => log.info(`- ${m}`));
+        }
+      },
+    })
+
+    .command({
+      command: 'deps [project]',
+      describe: 'Checks for cycles.',
+      builder: (yargs) =>
+        yargs.option('filter', {
+          description: 'Dependency filter',
+          type: 'string',
+          default: '@dxos/*',
+        }),
+      handler: ({
+        json,
+        verbose,
+        baseDir,
+        project: name,
+      }: {
+        json?: boolean;
+        verbose?: boolean;
+        baseDir: string;
+        project?: string;
+      }) => {
+        if (!name) {
+          process.exit(1);
+        }
+
+        const processor = new WorkspaceProcessor(baseDir, { verbose }).init();
+        const project = processor.getProjectByName(name);
+        if (project) {
+          const descendents = [...project.descendents!.values()].sort();
+          if (json) {
+            log.info(
+              JSON.stringify(
+                {
+                  package: project.package.name,
+                  descendents,
+                },
+                undefined,
+                2,
+              ),
+            );
+          } else {
+            log.info(`${project.package.name}`);
+            descendents.forEach((p) => log.info(`- ${p}`));
+          }
+        }
+      },
+    })
+
+    .command({
+      command: 'docs [pattern]',
+      describe: 'Generate docs and dependency diagrams.',
+      builder: (yargs) =>
+        yargs
+          .option('baseUrl', {
+            description: 'Base URL for links',
+            type: 'string',
+            default: 'dxos/dxos/tree/main',
+          })
+          .option('outDir', {
+            description: 'Folder for generated docs',
+            type: 'string',
+            default: './docs',
+          })
+          .option('include', {
+            description: 'Dependency filter',
+            type: 'string',
+            default: '@dxos/*',
+          })
+          .option('exclude', {
+            description: 'Excluded files',
+            type: 'string',
+            // TODO(burdon): Get from config or package annotation (e.g., "dxos/beast" key).
+            default: (config?.config?.exclude ?? []).join(','),
+          }),
+      handler: ({
+        verbose,
+        baseDir,
+        pattern = 'packages/**',
+        baseUrl,
+        outDir,
+        include,
+        exclude = '',
+      }: {
+        verbose?: boolean;
+        baseDir: string;
+        pattern?: string;
+        baseUrl: string;
+        outDir: string;
+        include: string;
+        exclude: string;
+      }) => {
+        const processor = new WorkspaceProcessor(baseDir, { verbose, include }).init();
+        const builder = new PackageDependencyBuilder(processor, {
+          verbose,
+          exclude: exclude?.split(','),
+        });
+        processor.getProjects(pattern).forEach((project) => {
+          if (verbose) {
+            log.info(`Updating: ${project.name.padEnd(32)} ${project.subDir}`);
+          }
+          builder.createDocs(project, outDir, baseUrl);
+        });
+      },
+    })
+    .help().argv;
+};
+
+void main();
---
+++ tools/beast/src/nx/package-dependency-builder.ts
@@ -0,0 +1,281 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import ColorHash from 'color-hash';
+import fs from 'fs';
+import path, { join } from 'path';
+
+import { ClassDiagram, Flowchart, type SubgraphBuilder } from '../mermaid';
+import { type WorkspaceProcessor } from '../nx';
+import { ClassProcessor } from '../ts';
+import { type Project } from '../types';
+import { array } from '../util';
+
+const colorHash = new ColorHash({
+  lightness: 0.95,
+  saturation: 0.6,
+});
+
+type PackageDependencyBuilderOptions = {
+  verbose?: boolean;
+
+  // Don't show links for these packages.
+  exclude?: string[];
+};
+
+/**
+ * Builder for a package dependency graph.
+ */
+export class PackageDependencyBuilder {
+  constructor(
+    private readonly _workspace: WorkspaceProcessor,
+    private readonly _options: PackageDependencyBuilderOptions,
+  ) {}
+
+  /**
+   * Create docs page.
+   */
+  createDocs(project: Project, docsDir: string, baseUrl: string): void {
+    const baseDir = path.join(this._workspace.baseDir, project.subDir, docsDir);
+    if (!fs.existsSync(baseDir)) {
+      fs.mkdirSync(baseDir, { recursive: true });
+    }
+
+    // Build README.
+    // TODO(burdon): Use remark lib (see ridoculous).
+    const sections: string[] = [];
+
+    // TODO(burdon): Support multiple diagrams.
+    const classDiagrams = this.generateClassDiagrams(project);
+    if (classDiagrams) {
+      sections.push('\n## Class Diagrams\n', ...classDiagrams);
+    }
+
+    const packageGraph = this.generatePackageGraph(project, docsDir, baseUrl);
+    if (packageGraph) {
+      sections.push('\n## Dependency Graph\n', packageGraph);
+    }
+
+    const dependenciesTable = this.generateDependenciesTable(project, docsDir);
+    if (dependenciesTable) {
+      sections.push('\n## Dependencies\n', this.generateDependenciesTable(project, docsDir));
+    }
+
+    // prettier-ignore
+    const content = [
+      `# ${project.package.name}\n`,
+      project.package.description,
+      ...sections,
+      ''
+    ];
+
+    fs.writeFileSync(path.join(baseDir, 'README.md'), content.join('\n'));
+  }
+
+  /**
+   * Generate class diagram.
+   */
+  private generateClassDiagrams(project: Project): string[] | undefined {
+    return project.package.beast?.classDiagrams?.map((config) => {
+      const { root, dependencies, glob = 'src/**/*.ts' } = config;
+      const sources = [join(this._workspace.baseDir, project.subDir, glob)];
+
+      dependencies?.forEach((name) => {
+        const project = this._workspace.getProjectByPackage(name);
+        if (project) {
+          sources.push(join(this._workspace.baseDir, project.subDir, glob));
+        }
+      });
+
+      const classProcessor = new ClassProcessor(sources);
+      classProcessor.processFile(join(this._workspace.baseDir, project.subDir, root));
+
+      const classDiagram = new ClassDiagram();
+      classProcessor.getClasses().forEach((def) => {
+        classDiagram.addClass(def);
+      });
+
+      return classDiagram.render();
+    });
+  }
+
+  /**
+   * Create table.
+   */
+  private generateDependenciesTable(project: Project, docsDir: string): string {
+    const dir = project.subDir;
+    const createLink = (p: Project) => {
+      const name = p.package.name;
+      const link = path.join('../', path.relative(dir, p.subDir), docsDir, 'README.md');
+      return `[\`${name}\`](${link})`;
+    };
+
+    const content = ['| Module | Direct |', '|---|---|'];
+
+    array(project.descendents)
+      .sort()
+      .forEach((packageName) => {
+        const sub = this._workspace.getProjectByPackage(packageName)!;
+        const link = createLink(sub);
+        content.push(
+          `| ${link} | ${
+            array(project.dependencies).some((sub) => sub.package.name === packageName) ? '&check;' : ''
+          } |`,
+        );
+      });
+
+    return content.join('\n');
+  }
+
+  /**
+   * Generate mermaid graph.
+   * https://mermaid.live
+   * https://mermaid-js.github.io/mermaid/#/README
+   */
+  generatePackageGraph(project: Project, docsDir: string, baseUrl: string): string {
+    const safeName = (name: string) => name.replace(/@/g, '');
+
+    const flowchart = new Flowchart({
+      linkStyle: {
+        stroke: '#333',
+        'stroke-width': '1px',
+      },
+    });
+
+    flowchart
+      .addClassDef('def', {
+        fill: '#fff',
+        stroke: '#333',
+        'stroke-width': '1px',
+      })
+      .addClassDef('root', {
+        fill: '#fff',
+        stroke: '#333',
+        'stroke-width': '4px',
+      });
+
+    const visited = new Set<Project>();
+
+    //
+    // Links
+    //
+    {
+      const addLinks = (current: Project) => {
+        visited.add(current);
+
+        current.dependencies.forEach((sub) => {
+          if (
+            // Don't link excluded packages.
+            !this._options.exclude?.includes(sub.package.name) &&
+            // Skip any descendents that depend directly on the package.
+            !array(current.dependencies).some((packageName) => packageName.descendents.has(sub.package.name))
+          ) {
+            flowchart.addLink({
+              source: safeName(current.package.name),
+              target: safeName(sub.package.name),
+            });
+          }
+
+          if (!visited.has(sub)) {
+            addLinks(sub);
+          }
+        });
+      };
+
+      addLinks(project);
+    }
+
+    //
+    // Subgraphs
+    //
+    {
+      type Folder = {
+        label: string;
+        folders?: Map<string, Folder>;
+        packages: string[];
+      };
+
+      const root = new Map<string, Folder>();
+
+      //
+      // Map projects into folder tree.
+      //
+      {
+        const getOrCreateFolder = (folders: Map<string, Folder>, parts: string[]): Folder => {
+          const [name, ...rest] = parts;
+          let folder = folders.get(name);
+          if (!folder) {
+            folder = {
+              label: name,
+              packages: [],
+              folders: new Map<string, Folder>(),
+            };
+
+            folders.set(name, folder);
+          }
+
+          if (rest.length) {
+            return getOrCreateFolder(folder.folders!, rest);
+          } else {
+            return folder;
+          }
+        };
+
+        array(visited).forEach((project) => {
+          // Skip top-level "packages" directory.
+          const [, ...parts] = project.subDir.split('/');
+          parts.pop();
+          if (this._options.exclude?.includes(project.package.name)) {
+            parts.push('_');
+          }
+
+          const folder = getOrCreateFolder(root, parts);
+          folder.packages.push(project.package.name);
+        });
+      }
+
+      //
+      // Construct graph tree.
+      //
+      {
+        const process = (graph: SubgraphBuilder, folders: Map<string, Folder>, parent?: Folder) => {
+          array(folders).forEach((folder) => {
+            const hidden = parent && folder.label === '_';
+            const sub = graph.addSubgraph({
+              id: folder.label,
+              label: hidden ? ' ' : folder.label,
+              style: hidden
+                ? {
+                    fill: colorHash.hex(parent.label),
+                    stroke: '#333',
+                    'stroke-dasharray': '5 5',
+                  }
+                : {
+                    fill: colorHash.hex(folder.label),
+                    stroke: '#333',
+                  },
+            });
+
+            folder.packages.forEach((packageName) => {
+              sub.addNode({
+                id: safeName(packageName),
+                label: packageName,
+                className: packageName === project.package.name ? 'root' : 'def',
+                href: path.join(baseUrl, this._workspace.getProjectByPackage(packageName)!.subDir, docsDir),
+              });
+            });
+
+            if (folder.folders) {
+              process(sub, folder.folders, folder);
+            }
+          });
+        };
+
+        process(flowchart, root);
+      }
+    }
+
+    return flowchart.render();
+  }
+}
