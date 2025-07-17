884354f5644cd8e7160a0452275c9fa23469ed2f
Josiah Witt <josiah@witt.life>
2025-07-17 15:22:18 -0400

    ci: improve e2e job stability (#9523)

--- packages/apps/composer-app/src/playwright/playwright.config.ts
+++ packages/apps/composer-app/src/playwright/playwright.config.ts
@@ -10,9 +10,10 @@ export default defineConfig({
   ...e2ePreset(import.meta.dirname),
   timeout: 30_000,
   webServer: {
-    command: 'moon run composer-app:preview',
+    // TODO(wittjosiah): Using vite directly to avoid moon re-building things.
+    command: 'pnpm vite preview',
     port: 4173,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
     timeout: 300_000,
   },
 });
