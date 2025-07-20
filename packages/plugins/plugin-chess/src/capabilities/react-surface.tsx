725b87a9fa00a0ff025c53451c9512a7b5310502
Rich Burdon <rich.burdon@gmail.com>
2025-07-20 14:52:39 +0200

    refactor: simplify chess plugin imports and update meta references

--- packages/plugins/plugin-chess/src/capabilities/react-surface.tsx
+++ packages/plugins/plugin-chess/src/capabilities/react-surface.tsx
@@ -7,22 +7,22 @@ import React from 'react';
 import { Capabilities, contributes, createSurface } from '@dxos/app-framework';
 import { Obj } from '@dxos/echo';
 
-import { ChessContainer } from '../components';
-import { ChessComponent } from '../components/ChessComponent';
-import { CHESS_PLUGIN } from '../meta';
-import { ChessType, isObject } from '../types';
+import { ChessContainer, ChessComponent } from '../components';
+import { meta } from '../meta';
+import { ChessType } from '../types';
 
 export default () =>
   contributes(Capabilities.ReactSurface, [
     createSurface({
-      id: CHESS_PLUGIN,
+      id: meta.id,
       role: ['article', 'section'],
-      filter: (data): data is { subject: ChessType } => isObject(data.subject),
+      filter: (data): data is { subject: ChessType } => Obj.instanceOf(ChessType, data.subject),
       component: ({ data, role }) => <ChessContainer game={data.subject} role={role} />,
     }),
     createSurface({
       id: 'plugin-chess',
       role: 'canvas-node',
+      // TODO(burdon): Should this dereference data.subject? If not why is that required above?
       filter: Obj.instanceOf(ChessType),
       component: ({ data }) => <ChessComponent game={data} />,
     }),
