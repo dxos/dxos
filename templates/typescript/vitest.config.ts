84c2dd41eb00d51aafd8bee6f0fe1a32b14095b6
Dmytro Maretskyi <maretskii@gmail.com>
2025-07-21 18:08:03 +0200

    relative paths

--- templates/typescript/vitest.config.ts
+++ templates/typescript/vitest.config.ts
@@ -4,6 +4,6 @@
 
 import { defineConfig, mergeConfig } from 'vitest/config';
 
-import { baseConfig } from '{{ workspace_root | path_join(part = 'vitest.base.config') }}';
+import { baseConfig } from '{{ workspace_root | path_join(part = 'vitest.base.config') | path_relative(from = dest_dir) }}';
 
 export default mergeConfig(baseConfig({ cwd: __dirname }), defineConfig({}));
