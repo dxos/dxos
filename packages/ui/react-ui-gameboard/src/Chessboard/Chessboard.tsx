18cd945d8f0805329c87adda57b639f1e5d499f3
thure <will@braneframe.com>
2025-07-23 17:17:28 -0700

    Refactor Card.SurfaceRoot and Chessboard to accept `classNames.`

--- packages/ui/react-ui-gameboard/src/Chessboard/Chessboard.tsx
+++ packages/ui/react-ui-gameboard/src/Chessboard/Chessboard.tsx
@@ -5,7 +5,7 @@
 import React, { type PropsWithChildren, useRef, useMemo, useEffect, useState, memo } from 'react';
 import { useResizeDetector } from 'react-resize-detector';
 
-import { useTrackProps } from '@dxos/react-ui';
+import { type ThemedClassName, useTrackProps } from '@dxos/react-ui';
 import { mx } from '@dxos/react-ui-theme';
 import { isNotFalsy } from '@dxos/util';
 
@@ -22,123 +22,127 @@ import {
   useBoardContext,
 } from '../Gameboard';
 
-export type ChessboardProps = PropsWithChildren<{
-  orientation?: Player;
-  showLabels?: boolean;
-  debug?: boolean;
-  rows?: number;
-  cols?: number;
-}>;
+export type ChessboardProps = ThemedClassName<
+  PropsWithChildren<{
+    orientation?: Player;
+    showLabels?: boolean;
+    debug?: boolean;
+    rows?: number;
+    cols?: number;
+  }>
+>;
 
 /**
  * Chessboard layout.
  */
-export const Chessboard = memo(({ orientation, showLabels, debug, rows = 8, cols = 8 }: ChessboardProps) => {
-  useTrackProps({ orientation, showLabels, debug }, Chessboard.displayName, false);
-  const { ref: containerRef, width, height } = useResizeDetector({ refreshRate: 200 });
-  const { model, promoting, onPromotion } = useBoardContext();
+export const Chessboard = memo(
+  ({ orientation, showLabels, debug, rows = 8, cols = 8, classNames }: ChessboardProps) => {
+    useTrackProps({ orientation, showLabels, debug }, Chessboard.displayName, false);
+    const { ref: containerRef, width, height } = useResizeDetector({ refreshRate: 200 });
+    const { model, promoting, onPromotion } = useBoardContext();
 
-  const locations = useMemo<Location[]>(() => {
-    return Array.from({ length: rows }, (_, i) => (orientation === 'black' ? i : rows - 1 - i)).flatMap((row) =>
-      Array.from({ length: cols }).map((_, col) => [row, col] as Location),
-    );
-  }, [orientation, rows, cols]);
-
-  // Use DOM grid layout to position squares.
-  const layout = useMemo(() => {
-    return locations.map((location) => {
-      return (
-        <div
-          key={locationToString(location)}
-          {...{
-            ['data-location' as const]: locationToString(location),
-          }}
-        />
+    const locations = useMemo<Location[]>(() => {
+      return Array.from({ length: rows }, (_, i) => (orientation === 'black' ? i : rows - 1 - i)).flatMap((row) =>
+        Array.from({ length: cols }).map((_, col) => [row, col] as Location),
       );
-    });
-  }, [locations]);
+    }, [orientation, rows, cols]);
 
-  // Build map of square locations to bounds.
-  const [grid, setGrid] = useState<Record<string, DOMRectBounds>>({});
-  const gridRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    setGrid(
-      locations.reduce(
-        (acc, location) => {
-          const square = getSquareLocation(gridRef.current!, location)!;
-          const bounds = getRelativeBounds(gridRef.current!, square);
-          return { ...acc, [locationToString(location)]: bounds };
-        },
-        {} as Record<string, DOMRectBounds>,
-      ),
-    );
-  }, [locations, width, height]);
-
-  // Get the bounds of each square and piece.
-  const positions = useMemo<{ piece: PieceRecord; bounds: DOMRectBounds }[]>(() => {
-    if (!gridRef.current) {
-      return [];
-    }
-
-    return Object.values(model?.pieces.value ?? {})
-      .map((piece) => {
-        if (piece.id === promoting?.id) {
-          return null;
-        }
-
-        const bounds = grid[locationToString(piece.location)];
-        return { piece, bounds };
-      })
-      .filter(isNotFalsy);
-  }, [grid, model?.pieces.value, promoting]);
-
-  return (
-    <div ref={containerRef} className='relative'>
-      <div ref={gridRef} className='grid grid-rows-8 grid-cols-8 aspect-square select-none'>
-        {layout}
-      </div>
-      <div>
-        {locations.map((location) => (
-          <Square
+    // Use DOM grid layout to position squares.
+    const layout = useMemo(() => {
+      return locations.map((location) => {
+        return (
+          <div
             key={locationToString(location)}
-            location={location}
-            label={showLabels ? locationToPos(location) : undefined}
-            bounds={grid[locationToString(location)]}
-            classNames={getSquareColor(location)}
-          />
-        ))}
-      </div>
-      <div className={mx(promoting && 'opacity-50')}>
-        {positions.map(({ bounds, piece }) => (
-          <Piece
-            key={piece.id}
-            piece={piece}
-            bounds={bounds}
-            label={debug ? piece.id : undefined}
-            orientation={orientation}
-            Component={ChessPieces[piece.type as ChessPiece]}
-          />
-        ))}
-      </div>
-      <div>
-        {promoting && (
-          <PromotionSelector
-            grid={grid}
-            piece={promoting}
-            onSelect={(piece) => {
-              onPromotion({
-                from: Object.values(model!.pieces.value).find((p) => p.id === promoting.id)!.location,
-                to: piece.location,
-                piece: promoting.type,
-                promotion: piece.type,
-              });
+            {...{
+              ['data-location' as const]: locationToString(location),
             }}
           />
-        )}
+        );
+      });
+    }, [locations]);
+
+    // Build map of square locations to bounds.
+    const [grid, setGrid] = useState<Record<string, DOMRectBounds>>({});
+    const gridRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      setGrid(
+        locations.reduce(
+          (acc, location) => {
+            const square = getSquareLocation(gridRef.current!, location)!;
+            const bounds = getRelativeBounds(gridRef.current!, square);
+            return { ...acc, [locationToString(location)]: bounds };
+          },
+          {} as Record<string, DOMRectBounds>,
+        ),
+      );
+    }, [locations, width, height]);
+
+    // Get the bounds of each square and piece.
+    const positions = useMemo<{ piece: PieceRecord; bounds: DOMRectBounds }[]>(() => {
+      if (!gridRef.current) {
+        return [];
+      }
+
+      return Object.values(model?.pieces.value ?? {})
+        .map((piece) => {
+          if (piece.id === promoting?.id) {
+            return null;
+          }
+
+          const bounds = grid[locationToString(piece.location)];
+          return { piece, bounds };
+        })
+        .filter(isNotFalsy);
+    }, [grid, model?.pieces.value, promoting]);
+
+    return (
+      <div ref={containerRef} className={mx('relative', classNames)}>
+        <div ref={gridRef} className='grid grid-rows-8 grid-cols-8 aspect-square select-none'>
+          {layout}
+        </div>
+        <div>
+          {locations.map((location) => (
+            <Square
+              key={locationToString(location)}
+              location={location}
+              label={showLabels ? locationToPos(location) : undefined}
+              bounds={grid[locationToString(location)]}
+              classNames={getSquareColor(location)}
+            />
+          ))}
+        </div>
+        <div className={mx(promoting && 'opacity-50')}>
+          {positions.map(({ bounds, piece }) => (
+            <Piece
+              key={piece.id}
+              piece={piece}
+              bounds={bounds}
+              label={debug ? piece.id : undefined}
+              orientation={orientation}
+              Component={ChessPieces[piece.type as ChessPiece]}
+            />
+          ))}
+        </div>
+        <div>
+          {promoting && (
+            <PromotionSelector
+              grid={grid}
+              piece={promoting}
+              onSelect={(piece) => {
+                onPromotion({
+                  from: Object.values(model!.pieces.value).find((p) => p.id === promoting.id)!.location,
+                  to: piece.location,
+                  piece: promoting.type,
+                  promotion: piece.type,
+                });
+              }}
+            />
+          )}
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  },
+);
 
 Chessboard.displayName = 'Chessboard';
 
