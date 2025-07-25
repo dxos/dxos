2e9488b6232890f154431386ac1102566ebfe236
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-07-25 19:21:05 +0200

    delete log

--- packages/sdk/client/src/edge/edge-identity.ts
+++ packages/sdk/client/src/edge/edge-identity.ts
@@ -5,13 +5,11 @@
 import { type EdgeIdentity } from '@dxos/edge-client';
 
 import { type Client } from '../client';
-import { log } from '@dxos/log';
 
 export const createEdgeIdentity = (client: Client): EdgeIdentity => {
   const identity = client.halo.identity.get();
   const device = client.halo.device;
   if (!identity || !device) {
-    log.warn('Identity not available', { identity, device });
     throw new Error('Identity not available');
   }
   return {
