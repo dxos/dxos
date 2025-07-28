0d405924be28990c9fec6bbcf1d60c2962529adc
Dmytro Maretskyi <maretskii@gmail.com>
2025-07-28 18:52:23 +0200

    fix: remove unused BaseErrorOptions import

--- packages/core/functions/src/errors.ts
+++ packages/core/functions/src/errors.ts
@@ -2,7 +2,7 @@
 // Copyright 2025 DXOS.org
 //
 
-import { BaseError, type BaseErrorOptions } from '@dxos/errors';
+import { BaseError } from '@dxos/errors';
 
 export class ServiceNotAvailableError extends BaseError.extend('SERVICE_NOT_AVAILABLE') {
   constructor(serviceName: string) {
