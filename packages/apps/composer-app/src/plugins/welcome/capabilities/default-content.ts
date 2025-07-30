6fd0c7d74dc54657c2c4202e7a54908b6909b2a3
Josiah Witt <josiah@witt.life>
2025-07-30 11:24:23 -0400

    cleanup some records collection rough edges

--- packages/apps/composer-app/src/plugins/welcome/capabilities/default-content.ts
+++ packages/apps/composer-app/src/plugins/welcome/capabilities/default-content.ts
@@ -4,7 +4,7 @@
 
 import { createIntent, LayoutAction } from '@dxos/app-framework';
 import { Capabilities, contributes, type PluginContext } from '@dxos/app-framework';
-import { SPACES } from '@dxos/plugin-space';
+import { SpaceCapabilities, SpaceEvents, SPACES } from '@dxos/plugin-space';
 
 import { INITIAL_CONTENT, INITIAL_DOC_TITLE } from '../../../constants';
 
@@ -31,6 +31,18 @@ export default async (context: PluginContext) => {
 
   const defaultSpaceCollection = defaultSpace.properties[DataType.Collection.typename].target;
   defaultSpaceCollection?.objects.push(Ref.make(readme));
+
+  const records = Obj.make(DataType.QueryCollection, {
+    query: { typename: DataType.StoredSchema.typename },
+  });
+  defaultSpaceCollection?.objects.push(Ref.make(records));
+
+  await context.activatePromise(SpaceEvents.SpaceCreated);
+  const onSpaceCreatedCallbacks = context.getCapabilities(SpaceCapabilities.OnSpaceCreated);
+  const spaceCreatedIntents = onSpaceCreatedCallbacks.map((onSpaceCreated) =>
+    onSpaceCreated({ space: defaultSpace, rootCollection: defaultSpaceCollection }),
+  );
+  await Promise.all(spaceCreatedIntents.map((intent) => dispatch(intent)));
 
   // Ensure the default content is in the graph and connected.
   // This will allow the expose action to work before the navtree renders for the first time.
