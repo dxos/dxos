bc4e2dbf5d258f70ba881a89f9adbb5a159a6960
Dmytro Maretskyi <maretskii@gmail.com>
2025-07-31 17:08:25 +0200

    x

--- tools/dx-compile/src/update-package-exports.ts
+++ tools/dx-compile/src/update-package-exports.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import type { EsbuildExecutorOptions } from './main';
 import { join } from 'node:path';
 
