8f2770ef933d6f00fd8f995825e6ef02806a9cf8
Rich Burdon <rich.burdon@gmail.com>
2025-07-31 22:48:19 +0200

    fix: correct typo in TODO comment from 'ot' to 'to'

--- packages/core/assistant-testing/src/blueprints/index.ts
+++ packages/core/assistant-testing/src/blueprints/index.ts
@@ -2,7 +2,7 @@
 // Copyright 2025 DXOS.org
 //
 
-// TODO(burdon): Move ot registry.
+// TODO(burdon): Move to registry.
 
 export { default as DESIGN_BLUEPRINT } from './design';
 export { default as PLANNING_BLUEPRINT } from './planning';
