2ee84055e4e1142ed942ec795fae49df9d6c5f01
Will Shown <thure@users.noreply.github.com>
2025-08-01 13:40:18 -0700

    fix: Change ‘schema’ to ‘record’ in user-facing strings (#9589)

--- packages/sdk/schema/src/format.ts
+++ packages/sdk/schema/src/format.ts
@@ -83,8 +83,8 @@ export const formatToSchema: Record<FormatEnum, Schema.Schema<FormatSchemaCommon
   [FormatEnum.Boolean]: extend(FormatEnum.Boolean, TypeEnum.Boolean),
   [FormatEnum.Ref]: extend(FormatEnum.Ref, TypeEnum.Ref, {
     referenceSchema: Schema.NonEmptyString.annotations({
-      title: 'Schema',
-      description: 'Schema typename',
+      title: 'Record type',
+      description: 'Name of the record type',
     }),
     referencePath: Schema.optional(
       JsonProp.annotations({
