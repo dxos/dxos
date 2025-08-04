fffde110566515246bcf2986ed760222fb4f7b50
Rich Burdon <rich.burdon@gmail.com>
2025-08-04 20:51:02 +0200

    refactor: move MapCanvasProps type to separate file for better organization

--- packages/ui/react-ui-geo/src/components/types.ts
+++ packages/ui/react-ui-geo/src/components/types.ts
@@ -2,18 +2,4 @@
 // Copyright 2024 DXOS.org
 //
 
-import { type LatLngLiteral } from 'leaflet';
-
-import { type ThemedClassName } from '@dxos/react-ui';
-
-import { type GeoMarker } from '../types';
-
-export { type LatLngLiteral };
-
-export type MapCanvasProps = ThemedClassName<{
-  markers?: GeoMarker[];
-  selected?: string[];
-  center?: LatLngLiteral;
-  zoom?: number;
-  onChange?: (params: { center: LatLngLiteral; zoom: number }) => void;
-}>;
+export { type LatLngLiteral } from 'leaflet';
