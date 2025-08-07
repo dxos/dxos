95001b3265d05fed96fe25b6ac80967b07171721
Josiah Witt <josiah@witt.life>
2025-08-07 11:03:46 -0400

    add todo

--- packages/plugins/plugin-deck/src/capabilities/toolkit.ts
+++ packages/plugins/plugin-deck/src/capabilities/toolkit.ts
@@ -12,6 +12,7 @@ class DeckToolkit extends AiToolkit.make(
     description:
       'Show an item as a companion to an existing plank. This will make the item appear alongside the primary content. When supplying IDs, they must be fully qualified like space:object.',
     parameters: {
+      // TODO(wittjosiah): Currently the chat is calling this with a dxn consistently.
       id: Schema.String.annotations({
         description: 'The ID of the item to show.',
       }),
