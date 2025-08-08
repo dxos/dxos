8d42d95b0a4fe2ae81deb65bcb8cc1db7c081fcb
Dmytro Maretskyi <maretskii@gmail.com>
2025-08-08 23:42:10 +0200

    Instal transformer for logs

--- vitest.all.config.ts
+++ vitest.all.config.ts
@@ -2,13 +2,12 @@
 // Copyright 2024 DXOS.org
 //
 
-// import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
-
+import react from '@vitejs/plugin-react-swc';
 /**
  * Config for the vitest vscode extension.
  */
-export default defineConfig({
+export default defineConfig(async () => ({
   esbuild: {
     target: 'es2020',
   },
@@ -32,12 +31,48 @@ export default defineConfig({
     ],
   },
   plugins: [
-    // TODO(dmaretskyi): Disabled due to an ESM error in the vscode extension.
-    // tsconfigPaths({
-    //   projects: [new URL('./tsconfig.paths.json', import.meta.url).pathname],
-    // }),
+    // Vitest extension for VSCode doesnt support ESM.
+    await import('@dxos/vite-plugin-import-source').then((m) => m.default()),
+
+    // We don't care about react but we want the SWC transforers.
+    react({
+      tsDecorators: true,
+      plugins: [
+        [
+          '@dxos/swc-log-plugin',
+          {
+            to_transform: [
+              {
+                name: 'log',
+                package: '@dxos/log',
+                param_index: 2,
+                include_args: false,
+                include_call_site: true,
+                include_scope: true,
+              },
+              {
+                name: 'invariant',
+                package: '@dxos/invariant',
+                param_index: 2,
+                include_args: true,
+                include_call_site: false,
+                include_scope: true,
+              },
+              {
+                name: 'Context',
+                package: '@dxos/context',
+                param_index: 1,
+                include_args: false,
+                include_call_site: false,
+                include_scope: false,
+              },
+            ],
+          },
+        ],
+      ],
+    }),
   ],
-});
+}));
 
 // TODO(dmaretskyi): Migrate to https://vitest.dev/guide/projects once it is stable.
 // eslint-disable-next-line no-console
