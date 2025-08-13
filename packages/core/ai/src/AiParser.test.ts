360cedb950b481f5d9e5c79e5b5a41efecfab525
Dmytro Maretskyi <maretskii@gmail.com>
2025-08-13 17:03:59 +0200

    Fix

--- packages/core/ai/src/AiParser.test.ts
+++ packages/core/ai/src/AiParser.test.ts
@@ -87,7 +87,7 @@ describe('parser', () => {
             _tag: 'toolCall',
             toolCallId: '123',
             name: 'foo',
-            input: { bar: 'baz' },
+            input: JSON.stringify({ bar: 'baz' },
           },
         ]);
       }),
