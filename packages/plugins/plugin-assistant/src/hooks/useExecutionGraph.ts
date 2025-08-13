3de2cb9be0feec5921cd5f44a85125966981ef06
Dmytro Maretskyi <maretskii@gmail.com>
2025-08-13 16:53:42 +0200

    style: fix linting issues

--- packages/plugins/plugin-assistant/src/hooks/useExecutionGraph.ts
+++ packages/plugins/plugin-assistant/src/hooks/useExecutionGraph.ts
@@ -5,6 +5,7 @@
 import { type Queue } from '@dxos/client/echo';
 import { Obj, type Ref } from '@dxos/echo';
 import { useQueue } from '@dxos/react-client/echo';
+
 import { ExecutionGraph } from '../execution-graph/execution-graph';
 
 export const useExecutionGraph = (traceQueueRef?: Ref.Ref<Queue>) => {
