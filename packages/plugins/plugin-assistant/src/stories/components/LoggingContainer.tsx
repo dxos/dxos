8f986c139eb1e57917072328b71f1280c49dae35
Dmytro Maretskyi <maretskii@gmail.com>
2025-08-13 11:31:50 +0200

    feat: add trace queue to chat for timeline visualization and logging

--- packages/plugins/plugin-assistant/src/stories/components/LoggingContainer.tsx
+++ packages/plugins/plugin-assistant/src/stories/components/LoggingContainer.tsx
@@ -10,13 +10,27 @@ import { Toolbar } from '@dxos/react-ui';
 
 import { Timeline } from '../../components';
 
+import { Filter, Obj } from '@dxos/echo';
+import { useQuery, useQueue } from '@dxos/react-client/echo';
+import { Assistant } from '../../types';
 import { type ComponentProps } from './types';
 
-export const LoggingContainer = (_: ComponentProps) => {
+export const LoggingContainer = ({ space }: ComponentProps) => {
+  const [chat] = useQuery(space, Filter.type(Assistant.Chat));
+  const traceQueue = useQueue(chat?.traceQueue?.dxn);
   return (
     <div className='flex flex-col h-full'>
       <Toolbar.Root classNames='density-coarse border-b border-subduedSeparator'>Timeline</Toolbar.Root>
-      <Timeline branches={[]} commits={[]} />
+      <Timeline
+        branches={[{ name: 'main' }]}
+        commits={
+          traceQueue?.objects?.map((evt) => ({
+            id: evt.id,
+            branch: 'main',
+            message: Obj.getLabel(evt) ?? evt.id,
+          })) ?? []
+        }
+      />
     </div>
   );
 };
