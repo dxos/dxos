fdfaea53ccb1784b5cb596626c408106f687b52a
Josiah Witt <josiah@witt.life>
2025-08-13 09:37:19 -0400

    query

--- packages/devtools/effect-cli/src/commands/spaces/query.ts
+++ packages/devtools/effect-cli/src/commands/spaces/query.ts
@@ -2,7 +2,25 @@
 // Copyright 2025 DXOS.org
 //
 
-import { Command } from '@effect/cli';
-import { Effect } from 'effect';
+import { Command, Options } from '@effect/cli';
+import { Console, Effect, Option } from 'effect';
 
-export const query = Command.make('query', {}, () => Effect.gen(function* () {}));
+import { Filter, SpaceId } from '@dxos/client/echo';
+
+import { ClientService } from '../../services';
+
+const rawSpaceId = Options.text('spaceId').pipe(Options.withDescription('The space ID to query'));
+const typename = Options.text('typename').pipe(Options.withDescription('The typename to query'));
+
+export const query = Command.make('query', { rawSpaceId, typename }, ({ rawSpaceId, typename }) =>
+  Effect.gen(function* () {
+    const client = yield* ClientService;
+    const spaceId = yield* SpaceId.isValid(rawSpaceId) ? Option.some(rawSpaceId) : Option.none();
+    const space = yield* Option.fromNullable(client.spaces.get(spaceId));
+    yield* Effect.tryPromise(() => space.waitUntilReady());
+    const filter = typename?.length ? Filter.typename(typename) : Filter.nothing();
+    // TODO(wittjosiah): Use DatabaseService?
+    const { objects } = yield* Effect.tryPromise(() => space.db.query(filter).run());
+    yield* Console.log(JSON.stringify(objects, null, 2));
+  }).pipe(Effect.catchTag('NoSuchElementException', () => Effect.logError('Space not found'))),
+);
