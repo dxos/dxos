5aa17b6924c3aba1bf0a99c6f98dfd3bff004774
Will Shown <thure@users.noreply.github.com>
2025-08-15 12:30:42 -0700

    feat: Prompt options modal design increment (#9668)

--- packages/plugins/plugin-assistant/src/components/ChatPrompt/ChatOptions.tsx
+++ packages/plugins/plugin-assistant/src/components/ChatPrompt/ChatOptions.tsx
@@ -2,58 +2,239 @@
 // Copyright 2025 DXOS.org
 //
 
-import React, { useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { type AiContextBinder } from '@dxos/assistant';
-import { type Blueprint } from '@dxos/blueprints';
-import { DropdownMenu, Icon, IconButton, useTranslation } from '@dxos/react-ui';
+import { Blueprint } from '@dxos/blueprints';
+import { Filter, Obj, Type } from '@dxos/echo';
+import { type Space, useQuery } from '@dxos/react-client/echo';
+import { Icon, IconButton, Popover, Select, useTranslation } from '@dxos/react-ui';
+import { SearchList } from '@dxos/react-ui-searchlist';
+import { Tabs } from '@dxos/react-ui-tabs';
+import { descriptionMessage, mx } from '@dxos/react-ui-theme';
 
-import { useBlueprints } from '../../hooks';
+import {
+  useActiveBlueprints,
+  useActiveReferences,
+  useBlueprintHandlers,
+  useBlueprints,
+  useReferencesHandlers,
+} from '../../hooks';
 import { meta } from '../../meta';
+import { Assistant } from '../../types';
 
 export type ChatOptionsProps = {
-  context?: AiContextBinder;
+  space: Space;
+  context: AiContextBinder;
   blueprintRegistry?: Blueprint.Registry;
-  onUpdateBlueprint?: (key: string, isActive: boolean) => void;
+  presets?: { id: string; label: string }[];
+  preset?: string;
+  onPresetChange?: (id: string) => void;
+};
+
+const idleFilter = Filter.not(
+  Filter.or(
+    Filter.type(Blueprint.Blueprint),
+    Filter.type(Assistant.Chat),
+    Filter.typename('dxos.org/type/Properties'),
+    Filter.typename('dxos.org/type/View'),
+  ),
+);
+
+const omitFromTypenameOptions = [
+  Blueprint.Blueprint.typename,
+  Assistant.Chat.typename,
+  'dxos.org/type/Properties',
+  'dxos.org/type/View',
+];
+
+const useTypenameOptions = (space?: Space) => {
+  const [schemas, setSchemas] = useState<string[]>([]);
+  useEffect(() => {
+    if (!space) {
+      return;
+    }
+
+    return space.db.schemaRegistry.query().subscribe(
+      (query) => {
+        setSchemas(
+          Array.from(
+            new Set(
+              [...space.db.graph.schemaRegistry.schemas, ...query.results]
+                .map(Type.getTypename)
+                .filter((typename) => !omitFromTypenameOptions.includes(typename)),
+            ),
+          ),
+        );
+      },
+      { fire: true },
+    );
+  }, [space]);
+
+  return schemas;
 };
 
 /**
  * Manages the runtime context for the chat.
  */
-// TODO(burdon): Refactor this as a Dialog (and move the object selector here also).
-export const ChatOptions = ({ context, blueprintRegistry, onUpdateBlueprint }: ChatOptionsProps) => {
+export const ChatOptions = ({
+  space,
+  context,
+  blueprintRegistry,
+  presets,
+  preset,
+  onPresetChange,
+}: ChatOptionsProps) => {
   const { t } = useTranslation(meta.id);
 
-  // TODO(burdon): Possibly constrain query as registry grows.
-  const blueprints = useMemo(() => blueprintRegistry?.query() ?? [], [blueprintRegistry]);
+  const blueprints = useBlueprints({ blueprintRegistry });
+  const activeBlueprints = useActiveBlueprints({ context });
+  const { onUpdateBlueprint } = useBlueprintHandlers({
+    space,
+    context,
+    blueprintRegistry,
+  });
 
-  const activeBlueprints = useBlueprints({ context });
+  const [activeTypename, setActiveTypename] = useState<string>('idle');
+  const typenameOptions = useTypenameOptions(space);
+
+  const activeReferences = useActiveReferences({ context });
+  const { onUpdateReference } = useReferencesHandlers({
+    space,
+    context,
+  });
+  const referenceOptions = useQuery(space, activeTypename === 'idle' ? idleFilter : Filter.typename(activeTypename));
 
   return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>
-        <IconButton icon='ph--plus--regular' variant='ghost' size={5} iconOnly label={t('button add blueprint')} />
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content side='left'>
-          <DropdownMenu.Viewport>
-            {blueprints.map((blueprint) => (
-              <DropdownMenu.CheckboxItem
-                key={blueprint.key}
-                checked={activeBlueprints?.get(blueprint.key) !== undefined}
-                onCheckedChange={(checked) => onUpdateBlueprint?.(blueprint.key, !!checked)}
-                classNames='gap-2'
-              >
-                <div className='flex-1 min-is-0'>{blueprint.name}</div>
-                <DropdownMenu.ItemIndicator asChild>
-                  <Icon icon='ph--check--regular' size={4} />
-                </DropdownMenu.ItemIndicator>
-              </DropdownMenu.CheckboxItem>
-            ))}
-          </DropdownMenu.Viewport>
-          <DropdownMenu.Arrow />
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
+    <Popover.Root>
+      <Popover.Trigger asChild>
+        <IconButton
+          icon='ph--plus--regular'
+          variant='ghost'
+          size={5}
+          iconOnly
+          label={t('context objects placeholder')}
+        />
+      </Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content side='top'>
+          <Tabs.Root
+            orientation='horizontal'
+            defaultValue='blueprints'
+            defaultActivePart='list'
+            classNames='min-is-min is-[calc(100dvw-.5rem)] sm:is-max max-is-[--text-content]'
+          >
+            <Tabs.Viewport classNames='max-bs-[--radix-popover-content-available-height] grid grid-rows-[1fr_min-content] [&_[cmdk-root]]:contents [&_[role="tabpanel"]]:grid [&_[role="tabpanel"]]:grid-rows-[1fr_min-content] [&_[role="listbox"]]:min-bs-0 [&_[role="listbox"]]:overflow-y-auto [&_[role="tabpanel"]]:min-bs-0 [&_[role="tabpanel"]]:pli-cardSpacingChrome [&_[role="tabpanel"][data-state="active"]]:order-first [&_[role="tabpanel"][data-state="inactive"]]:hidden'>
+              <Tabs.Tabpanel value='blueprints'>
+                <SearchList.Root>
+                  <SearchList.Content classNames='plb-cardSpacingChrome'>
+                    {blueprints.map((blueprint) => {
+                      const isActive = activeBlueprints.has(blueprint.key);
+                      return (
+                        <SearchList.Item
+                          classNames='flex gap-2 items-center'
+                          key={blueprint.key}
+                          value={blueprint.name}
+                          onSelect={() => onUpdateBlueprint?.(blueprint.key, !isActive)}
+                        >
+                          <Icon icon='ph--check--regular' classNames={[!isActive && 'invisible']} />
+                          {blueprint.name}
+                        </SearchList.Item>
+                      );
+                    })}
+                  </SearchList.Content>
+                  <SearchList.Input placeholder={t('search placeholder')} classNames='mbe-cardSpacingChrome' />
+                </SearchList.Root>
+              </Tabs.Tabpanel>
+              <Tabs.Tabpanel value='objects'>
+                <SearchList.Root>
+                  {referenceOptions.length ? (
+                    <>
+                      <SearchList.Content classNames='plb-cardSpacingChrome [&:has([cmdk-list-sizer]:empty)]:plb-0'>
+                        {referenceOptions.map((object: any) => {
+                          const label = Obj.getLabel(object) ?? Obj.getTypename(object) ?? object.id;
+                          const value = Obj.getDXN(object).toString();
+                          const isActive = activeReferences.has(value);
+                          return (
+                            <SearchList.Item
+                              classNames='flex gap-2 items-center'
+                              key={value}
+                              value={label}
+                              onSelect={() => onUpdateReference?.(value, !isActive)}
+                            >
+                              <Icon icon='ph--check--regular' classNames={[!isActive && 'invisible']} />
+                              {label}
+                            </SearchList.Item>
+                          );
+                        })}
+                      </SearchList.Content>
+                      <SearchList.Empty classNames={[descriptionMessage, 'mlb-cardSpacingChrome']}>
+                        {t('no reference options label')}
+                      </SearchList.Empty>
+                    </>
+                  ) : (
+                    <p className={mx(descriptionMessage, 'mlb-cardSpacingChrome')}>{t('no reference options label')}</p>
+                  )}
+                  <div role='none' className='grid grid-cols-[min-content_1fr] gap-2 mbe-cardSpacingChrome'>
+                    <Select.Root
+                      value={activeTypename === 'idle' ? undefined : activeTypename}
+                      onValueChange={setActiveTypename}
+                    >
+                      <Select.TriggerButton density='fine' placeholder={t('type filter placeholder')} />
+                      <Select.Portal>
+                        <Select.Content>
+                          <Select.ScrollUpButton />
+                          <Select.Viewport>
+                            <Select.Option value='idle'>{t('idle type filter label')}</Select.Option>
+                            {typenameOptions.map((typename) => (
+                              <Select.Option key={typename} value={typename}>
+                                {t('typename label', { ns: typename, defaultValue: typename })}
+                              </Select.Option>
+                            ))}
+                          </Select.Viewport>
+                          <Select.ScrollDownButton />
+                          <Select.Arrow />
+                        </Select.Content>
+                      </Select.Portal>
+                    </Select.Root>
+                    <SearchList.Input placeholder={t('search placeholder')} classNames='mbe-0' />
+                  </div>
+                </SearchList.Root>
+              </Tabs.Tabpanel>
+              <Tabs.Tabpanel value='model'>
+                <ul role='listbox' className='plb-cardSpacingChrome'>
+                  {presets?.map(({ id, label }) => {
+                    const isActive = preset === id;
+                    return (
+                      <li
+                        role='option'
+                        key={id}
+                        aria-selected={isActive}
+                        tabIndex={0}
+                        className='dx-focus-ring flex gap-2 items-center p-1 rounded-sm select-none cursor-pointer hover:bg-hoverOverlay'
+                        onClick={() => onPresetChange?.(id)}
+                      >
+                        <Icon icon='ph--check--regular' classNames={[!isActive && 'invisible']} />
+                        {label}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </Tabs.Tabpanel>
+              <Tabs.Tablist classNames='sm:overflow-x-hidden p-[--dx-cardSpacingChrome] border-bs border-subduedSeparator order-last'>
+                <Tabs.IconTab
+                  value='blueprints'
+                  icon='ph--blueprint--regular'
+                  label={t('blueprints in context title')}
+                />
+                <Tabs.IconTab value='objects' label={t('objects in context title')} icon='ph--file--regular' />
+                <Tabs.IconTab value='model' label={t('chat model title')} icon='ph--cpu--regular' />
+              </Tabs.Tablist>
+            </Tabs.Viewport>
+          </Tabs.Root>
+          <Popover.Arrow />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 };
