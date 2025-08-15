a0a6cd236d45f45bcc954d64d1ebd79fb1f6f042
Josiah Witt <josiah@witt.life>
2025-08-14 20:52:09 -0400

    fix: gmail sync function (#9664)

--- packages/plugins/plugin-inbox/src/testing/scripts/types.ts
+++ packages/plugins/plugin-inbox/src/testing/scripts/types.ts
@@ -6,6 +6,10 @@
 
 // TODO(dmaretskyi): Move to @dxos/functions
 declare module 'dxos:functions' {
+  export const ObjectId: any; // typeof import('@dxos/functions-sdk').ObjectId;
+
+  export const create: any; // typeof import('@dxos/functions-sdk').create;
+
   export const defineFunction: any; // typeof import('@dxos/functions-sdk').defineFunction;
 
   export const S: any; // typeof import('@dxos/functions-sdk').S;
