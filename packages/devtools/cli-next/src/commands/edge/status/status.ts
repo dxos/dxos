57d548609ea5ad51ca705c8ef6adf3b84128839f
Josiah Witt <josiah@witt.life>
2025-08-19 17:52:40 -0400

    edge status

--- packages/devtools/cli-next/src/commands/edge/status/status.ts
+++ packages/devtools/cli-next/src/commands/edge/status/status.ts
@@ -3,20 +3,37 @@
 //
 
 import { Command } from '@effect/cli';
-import { Console, Effect } from 'effect';
+import { Console, Effect, Option } from 'effect';
 
-import { Common } from '../../options';
+import { createEdgeIdentity } from '@dxos/client/edge';
 
-export const status = Command.make(
-  'status',
-  {
-    json: Common.json,
-  },
-  Effect.fn(function* ({ json }) {
-    if (json) {
-      return yield* Console.log({ status: 'ok' });
-    } else {
-      return yield* Console.log('ok');
-    }
-  }),
-).pipe(Command.withDescription('Show EDGE services status.'));
+import { ClientService } from '../../../services';
+
+export const getStatus = () =>
+  Effect.gen(function* () {
+    const client = yield* ClientService;
+    const identity = yield* Effect.try({
+      try: () => createEdgeIdentity(client),
+      catch: (err) => {
+        return err;
+      },
+    });
+    client.edge.setIdentity(identity);
+    const status = yield* Effect.tryPromise(() => client.edge.getStatus());
+    yield* Console.log(JSON.stringify(status, null, 2));
+  }).pipe(
+    // TODO(wittjosiah): Tagged error.
+    Effect.catchSome((error) => {
+      if (error instanceof Error && error.message === 'Identity not available') {
+        // TODO(wittjosiah): Error coloring for logs.
+        return Option.some(Console.error(error.message));
+      } else {
+        return Option.none();
+      }
+    }),
+  );
+
+// TODO(wittjosiah): Admin functionality to provide to specify an identity.
+export const status = Command.make('status', {}, getStatus).pipe(
+  Command.withDescription('Get the EDGE status for the current identity.'),
+);
