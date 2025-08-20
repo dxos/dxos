1f18a78cd4f5e96a34a1d470d4b34045d0edcfa1
Josiah Witt <josiah@witt.life>
2025-08-20 07:41:08 -0400

    feat: add edge status (#9694)

--- packages/devtools/cli-next/src/commands/edge/functions/deploy/deploy.test.ts
+++ packages/devtools/cli-next/src/commands/edge/functions/deploy/deploy.test.ts
@@ -2,17 +2,9 @@
 // Copyright 2025 DXOS.org
 //
 
-import { beforeEach, describe, it } from '@effect/vitest';
-
-import { TestLogger } from '../../../../testing';
+import { describe, it } from '@effect/vitest';
 
 describe('functions deploy', () => {
-  const testLogger = new TestLogger();
-
-  beforeEach(() => {
-    testLogger.clear();
-  });
-
   // TODO(wittjosiah): Need to create a mock edge to accept the function request.
   it.todo('should deploy a function');
 });
--- packages/devtools/cli-next/src/commands/halo/create/create.test.ts
+++ packages/devtools/cli-next/src/commands/halo/create/create.test.ts
@@ -2,38 +2,34 @@
 // Copyright 2025 DXOS.org
 //
 
-import { beforeEach, describe, expect, it } from '@effect/vitest';
-import { Effect, LogLevel, Option } from 'effect';
+import { describe, expect, it } from '@effect/vitest';
+import { Effect, Option } from 'effect';
 
 import { ClientService } from '../../../services';
-import { TestLogger, testLayer } from '../../../testing';
+import { TestConsole, TestLayer } from '../../../testing';
 
 import { handler } from './create';
 
 describe('halo create', () => {
-  const testLogger = new TestLogger();
-
-  beforeEach(() => {
-    testLogger.clear();
-  });
-
   it('should create an identity without a display name', () =>
     Effect.gen(function* () {
       const client = yield* ClientService;
-      yield* handler({ displayName: Option.none() });
-      const logs = testLogger.getLogsByLevel(LogLevel.Info);
+      yield* handler({ agent: false, displayName: Option.none() });
+      const logger = yield* TestConsole.TestConsole;
+      const logs = logger.logs;
       expect(logs).toHaveLength(2);
-      expect(logs[0].args).toEqual(['Identity key:', client.halo.identity.get()?.identityKey.toHex()]);
-      expect(logs[1].args).toEqual(['Display name:', client.halo.identity.get()?.profile?.displayName]);
-    }).pipe(Effect.provide(testLayer(testLogger)), Effect.scoped, Effect.runPromise));
+      expect(logs[0].args).toEqual([`Identity key: ${client.halo.identity.get()?.identityKey.toHex()}`]);
+      expect(logs[1].args).toEqual([`Display name: ${client.halo.identity.get()?.profile?.displayName}`]);
+    }).pipe(Effect.provide(TestLayer), Effect.scoped, Effect.runPromise));
 
   it('should create an identity with a display name', () =>
     Effect.gen(function* () {
       const client = yield* ClientService;
-      yield* handler({ displayName: Option.some('Example') });
-      const logs = testLogger.getLogsByLevel(LogLevel.Info);
+      yield* handler({ agent: false, displayName: Option.some('Example') });
+      const logger = yield* TestConsole.TestConsole;
+      const logs = logger.logs;
       expect(logs).toHaveLength(2);
-      expect(logs[0].args).toEqual(['Identity key:', client.halo.identity.get()?.identityKey.toHex()]);
-      expect(logs[1].args).toEqual(['Display name:', client.halo.identity.get()?.profile?.displayName]);
-    }).pipe(Effect.provide(testLayer(testLogger)), Effect.scoped, Effect.runPromise));
+      expect(logs[0].args).toEqual([`Identity key: ${client.halo.identity.get()?.identityKey.toHex()}`]);
+      expect(logs[1].args).toEqual([`Display name: ${client.halo.identity.get()?.profile?.displayName}`]);
+    }).pipe(Effect.provide(TestLayer), Effect.scoped, Effect.runPromise));
 });
