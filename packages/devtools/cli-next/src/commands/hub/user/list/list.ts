3dcbc4230d326684015fb9492b79bb2e348c7691
Josiah Witt <josiah@witt.life>
2025-08-20 19:18:44 -0400

    refactor: nested cli configs (#9703)

--- packages/devtools/cli-next/src/commands/hub/user/list/list.ts
+++ packages/devtools/cli-next/src/commands/hub/user/list/list.ts
@@ -11,43 +11,38 @@ import { Config, Console, Effect, pipe } from 'effect';
 import { withRetry } from '@dxos/edge-client';
 
 import { ConfigService } from '../../../../services';
-import { command } from '../../../dx';
-import { Common } from '../../../options';
 
 export const list = Command.make(
   'list',
-  {
-    apiKey: Common.apiKey,
-  },
-  ({ apiKey }) =>
-    Effect.flatMap(command, ({ verbose }) =>
-      Effect.gen(function* () {
-        const config = yield* ConfigService;
-        const baseUrl = config.values?.runtime?.services?.hub?.url ?? 'https://hub.dxos.network';
-        const url = path.join(baseUrl, '/api/user/profile');
-        if (verbose) {
-          yield* Effect.log(`Calling: ${url}`);
-        }
+  {},
+  Effect.fn(function* () {
+    const config = yield* ConfigService;
+    const baseUrl = config.values?.runtime?.services?.hub?.url ?? 'https://hub.dxos.network';
+    const url = path.join(baseUrl, '/api/user/profile');
+    const verbose = yield* Config.boolean('VERBOSE').pipe(Config.withDefault(false));
+    if (verbose) {
+      yield* Effect.log(`Calling: ${url}`);
+    }
 
-        const result = yield* pipe(
-          withRetry(
-            HttpClient.get(url, {
-              headers: {
-                'x-api-key': apiKey,
-              },
-            }),
-          ),
-          Effect.provide(FetchHttpClient.layer),
-          Effect.withSpan('EdgeHttpClient'),
-        );
+    const apiKey = yield* Config.string('DX_HUB_API_KEY');
+    const result = yield* pipe(
+      withRetry(
+        HttpClient.get(url, {
+          headers: {
+            'x-api-key': apiKey,
+          },
+        }),
+      ),
+      Effect.provide(FetchHttpClient.layer),
+      Effect.withSpan('EdgeHttpClient'),
+    );
 
-        const json = yield* Config.boolean('JSON').pipe(Config.withDefault(false));
-        if (json) {
-          return yield* Console.log(result);
-        } else {
-          // TODO(burdon): Output table. Look at @effect/printer.
-          return yield* Console.log((result as any).profiles?.length + ' profiles');
-        }
-      }),
-    ),
+    const json = yield* Config.boolean('JSON').pipe(Config.withDefault(false));
+    if (json) {
+      return yield* Console.log(result);
+    } else {
+      // TODO(burdon): Output table. Look at @effect/printer.
+      return yield* Console.log((result as any).profiles?.length + ' profiles');
+    }
+  }),
 ).pipe(Command.withDescription('List hub users.'));
