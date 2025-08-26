859353ff8315960ca2ccb565a45b3d4f35d0057e
Rich Burdon <rich.burdon@gmail.com>
2025-08-26 17:13:12 +0100

    feat: add type safety to useBinder hook with optional space parameter and return type

--- packages/plugins/plugin-assistant/src/stories/hooks/useBinder.ts
+++ packages/plugins/plugin-assistant/src/stories/hooks/useBinder.ts
@@ -6,11 +6,11 @@ import { useMemo } from 'react';
 
 import { AiContextBinder } from '@dxos/assistant';
 import { Filter } from '@dxos/echo';
-import { useQuery } from '@dxos/react-client/echo';
+import { type Space, useQuery } from '@dxos/react-client/echo';
 
 import { Assistant } from '../../types';
 
-export const useBinder = (space: Space) => {
+export const useBinder = (space: Space | undefined): AiContextBinder | undefined => {
   const chats = useQuery(space, Filter.type(Assistant.Chat));
   const binder = useMemo(() => {
     const queue = chats.at(-1)?.queue.target;
