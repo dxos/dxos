a0d1200146a7a5c77de31a7be8ac871dcd99eeb6
Rich Burdon <rich.burdon@gmail.com>
2025-08-30 17:00:17 +0100

    feat: Memo window size for tauri app. (#9741)

--- packages/plugins/plugin-native/src/capabilities/index.ts
+++ packages/plugins/plugin-native/src/capabilities/index.ts
@@ -4,5 +4,6 @@
 
 import { lazy } from '@dxos/app-framework';
 
-export const Updater = lazy(() => import('./updater'));
 export const Ollama = lazy(() => import('./ollama'));
+export const Updater = lazy(() => import('./updater'));
+export const Window = lazy(() => import('./window'));
