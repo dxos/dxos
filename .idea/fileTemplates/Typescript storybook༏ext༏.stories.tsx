22b371b10c042e0f4dfe8f370feac01896d1353c
Rich Burdon <rich.burdon@gmail.com>
2025-08-30 22:21:07 +0200

    style: reorder imports in Typescript storybook template

--- .idea/fileTemplates/Typescript storybook༏ext༏.stories.tsx
+++ .idea/fileTemplates/Typescript storybook༏ext༏.stories.tsx
@@ -6,9 +6,10 @@ import '@dxos-theme';
 
 import React from 'react';
 
+import { type Meta, type StoryObj } from '@storybook/react-vite';
+
 import { withTheme } from '@dxos/storybook-utils';
 
-import { type Meta, type StoryObj } from '@storybook/react-vite';
 const Component = () => <div>Test</div>;
 
 const meta: Meta<typeof Component> = {
