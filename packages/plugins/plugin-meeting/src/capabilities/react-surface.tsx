bd560167e164d7245171788b19d749ac2b10244d
Rich Burdon <rich.burdon@gmail.com>
2025-08-30 17:00:36 +0100

    fix: Remove not_meta exports and standardize all stories (#9742)

--- packages/plugins/plugin-meeting/src/capabilities/react-surface.tsx
+++ packages/plugins/plugin-meeting/src/capabilities/react-surface.tsx
@@ -10,26 +10,26 @@ import { SettingsStore } from '@dxos/local-storage';
 import { ChannelType } from '@dxos/plugin-thread/types';
 
 import { MeetingContainer, MeetingSettings, MeetingsList } from '../components';
-import { not_meta } from '../meta';
+import { meta } from '../meta';
 import { Meeting } from '../types';
 
 export default () =>
   contributes(Capabilities.ReactSurface, [
     createSurface({
-      id: `${not_meta.id}/plugin-settings`,
+      id: `${meta.id}/plugin-settings`,
       role: 'article',
       filter: (data): data is { subject: SettingsStore<Meeting.Settings> } =>
-        data.subject instanceof SettingsStore && data.subject.prefix === not_meta.id,
+        data.subject instanceof SettingsStore && data.subject.prefix === meta.id,
       component: ({ data: { subject } }) => <MeetingSettings settings={subject.value} />,
     }),
     createSurface({
-      id: `${not_meta.id}/meeting`,
+      id: `${meta.id}/meeting`,
       role: 'article',
       filter: (data): data is { subject: Meeting.Meeting } => Obj.instanceOf(Meeting.Meeting, data.subject),
       component: ({ data }) => <MeetingContainer meeting={data.subject} />,
     }),
     createSurface({
-      id: `${not_meta.id}/meeting-companion`,
+      id: `${meta.id}/meeting-companion`,
       role: 'article',
       filter: (data): data is { subject: Meeting.Meeting | 'meeting'; companionTo: ChannelType } =>
         (Obj.instanceOf(Meeting.Meeting, data.subject) || data.subject === 'meeting') &&
