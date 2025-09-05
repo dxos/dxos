1b137d4c8955a755114daa19a493da3819a3cb88
Will Shown <thure@users.noreply.github.com>
2025-09-05 09:54:20 -0700

    fix: Table button size & alignment (#9775)

--- packages/ui/react-ui-table/src/model/table-presentation.ts
+++ packages/ui/react-ui-table/src/model/table-presentation.ts
@@ -448,8 +448,8 @@ export class TablePresentation<T extends TableRow = TableRow> {
       cells[toPlaneCellIndex({ col: 0, row })] = {
         value: '',
         readonly: true,
-        accessoryHtml: '<dx-icon icon="ph--plus--regular" class="block m-1"></dx-icon>',
-        ...(draftRows.length < 1 && { className: 'dx-grid__row--cta__cell' }),
+        accessoryHtml: '<dx-icon icon="ph--plus--regular" class="contents"></dx-icon>',
+        className: mx('[&>div]:grid [&>div]:place-content-center', draftRows.length < 1 && 'dx-grid__row--cta__cell'),
       };
     }
 
