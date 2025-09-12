eb7e8285a84a6d7bd2fe75657c2f387f5a222e6e
thure <will@braneframe.com>
2025-09-11 17:37:04 -0700

    Add Json React component.

--- packages/ui/react-ui-components/src/components/MarkdownStream/components/Json.tsx
+++ packages/ui/react-ui-components/src/components/MarkdownStream/components/Json.tsx
@@ -4,17 +4,20 @@
 
 import React from 'react';
 
+import { useTranslation } from '@dxos/react-ui';
 import { Json as NaturalJson } from '@dxos/react-ui-syntax-highlighter';
 
+import { translationKey } from '../../../translations';
 import { type XmlComponentProps } from '../extensions';
 
 export type JsonProps = XmlComponentProps<{ data: string }>;
 
 export const Json = ({ data }: JsonProps) => {
+  const { t } = useTranslation(translationKey);
   try {
     const jsonData = JSON.parse(data);
     return <NaturalJson data={jsonData} />;
-  } catch (e) {
-    return <p>error</p>;
+  } catch (_error) {
+    return <p>{t('json error message')}</p>;
   }
 };
