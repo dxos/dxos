6988995b2baad2d970bbd9e9818e9012fd99a6da
Rich Burdon <rich.burdon@gmail.com>
2025-09-12 02:09:09 +0200

    chore: add return type annotations to DOM utility functions

--- packages/ui/react-ui-editor/src/util/dom.ts
+++ packages/ui/react-ui-editor/src/util/dom.ts
@@ -9,21 +9,21 @@ export type Rect = {
   readonly bottom: number;
 };
 
-export const flattenRect = (rect: Rect, left: boolean) => {
+export const flattenRect = (rect: Rect, left: boolean): Rect => {
   const x = left ? rect.left : rect.right;
   return { left: x, right: x, top: rect.top, bottom: rect.bottom };
 };
 
 let scratchRange: Range | null;
 
-export const textRange = (node: Text, from: number, to = from) => {
+export const textRange = (node: Text, from: number, to = from): Range => {
   const range = scratchRange || (scratchRange = document.createRange());
   range.setEnd(node, to);
   range.setStart(node, from);
   return range;
 };
 
-export const clientRectsFor = (dom: Node) => {
+export const clientRectsFor = (dom: Node): DOMRectList => {
   if (dom.nodeType === 3) {
     return textRange(dom as Text, 0, dom.nodeValue!.length).getClientRects();
   } else if (dom.nodeType === 1) {
