7427281181aff50da26f00a0ed7d8e7a9169de32
Rich Burdon <rich.burdon@gmail.com>
2025-09-12 14:46:01 +0200

    feat: add Discord blueprint and message fetching functionality

---
+++ packages/ui/react-ui-components/src/components/MarkdownStream/widgets/SuggestionWidget.ts
@@ -0,0 +1,28 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { WidgetType } from '@codemirror/view';
+
+import { Domino } from '@dxos/react-ui-editor';
+
+/**
+ * Simple prompt widget.
+ */
+export class SuggestionWidget extends WidgetType {
+  constructor(private text: string) {
+    super();
+  }
+
+  override toDOM(): HTMLElement {
+    return Domino.of('button')
+      .classNames('dx-button animate-[fadeIn_0.5s] gap-2')
+      .child(Domino.of<any>('dx-icon').attr('icon', 'ph--lightning--regular'))
+      .text(this.text)
+      .build();
+  }
+
+  override eq(other: WidgetType): boolean {
+    return other instanceof SuggestionWidget && other.text === this.text;
+  }
+}
