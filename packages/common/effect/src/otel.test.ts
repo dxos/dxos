aa7c70dc6ddbb4fbd21e2adfd4222d3cd20f9cd6
Dmytro Maretskyi <maretskii@gmail.com>
2025-09-13 14:43:55 +0200

    Hook-up dxos logger

--- packages/common/effect/src/otel.test.ts
+++ packages/common/effect/src/otel.test.ts
@@ -5,12 +5,18 @@ import { it, beforeAll, afterAll } from '@effect/vitest';
 import { TestContext } from 'vitest';
 import { layerOtel } from './otel';
 import { resourceFromAttributes } from '@opentelemetry/resources';
-import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_CODE_FILE_PATH,
+  ATTR_CODE_LINE_NUMBER,
+  ATTR_CODE_STACKTRACE,
+} from '@opentelemetry/semantic-conventions';
 import { SpanStatusCode, trace } from '@opentelemetry/api';
 import { afterEach, beforeEach } from 'vitest';
 
-import { logs } from '@opentelemetry/api-logs';
+import { logs, SeverityNumber, type Logger } from '@opentelemetry/api-logs';
 import { LoggerProvider, SimpleLogRecordProcessor, ConsoleLogRecordExporter } from '@opentelemetry/sdk-logs';
+import { log, LogLevel, type LogProcessor } from '@dxos/log';
 
 const resource = resourceFromAttributes({
   [ATTR_SERVICE_NAME]: 'test',
@@ -30,6 +36,42 @@ logs.setGlobalLoggerProvider(loggerProvider);
 
 // You can also use global singleton
 const logger = logs.getLogger('test');
+
+const makeOtelLogProcessor = (logger: Logger): LogProcessor => {
+  return (config, entry) => {
+    let severity: SeverityNumber = SeverityNumber.UNSPECIFIED;
+    switch (entry.level) {
+      case LogLevel.DEBUG:
+        severity = SeverityNumber.DEBUG;
+        break;
+      case LogLevel.INFO:
+        severity = SeverityNumber.INFO;
+        break;
+      case LogLevel.WARN:
+        severity = SeverityNumber.WARN;
+        break;
+      case LogLevel.ERROR:
+        severity = SeverityNumber.ERROR;
+        break;
+    }
+
+    logger.emit({
+      body: entry.error ? ('stack' in entry.error ? entry.error.stack : String(entry.error)) : entry.message,
+      severityNumber: severity,
+      attributes: {
+        [ATTR_CODE_FILE_PATH]: entry.meta?.F,
+        [ATTR_CODE_LINE_NUMBER]: entry.meta?.L,
+        [ATTR_CODE_STACKTRACE]: entry.error?.stack,
+        ...(typeof entry.context === 'object'
+          ? entry.context
+          : {
+              ctx: entry.context,
+            }),
+      },
+    });
+  };
+};
+log.addProcessor(makeOtelLogProcessor(logger));
 
 beforeAll(() => {
   sdk.start();
@@ -56,9 +98,7 @@ beforeEach((ctx) => {
 const foo = Effect.fn('foo')(function* () {
   yield* Effect.sleep(Duration.millis(100));
 
-  logger.emit({
-    body: 'log inside foo',
-  });
+  log('log inside foo', { detail: 123 });
 });
 
 const bar = Effect.fn('bar')(function* () {
