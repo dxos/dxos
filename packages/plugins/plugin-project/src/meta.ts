55671bd69a16d8010646c995a09ffd863204e0e8
Josiah Witt <josiah@witt.life>
2025-09-15 15:09:41 -0400

    feat: project plugin scaffolding

---
+++ packages/plugins/plugin-project/src/meta.ts
@@ -0,0 +1,12 @@
+//
+// Copyright 2023 DXOS.org
+//
+
+import { type PluginMeta } from '@dxos/app-framework';
+
+export const meta: PluginMeta = {
+  id: 'dxos.org/plugin/project',
+  name: 'Projects',
+  icon: 'ph--asterisk--regular',
+  source: 'https://github.com/dxos/dxos/tree/main/packages/plugins/plugin-project',
+};
--- packages/sdk/schema/src/common/project.ts
+++ packages/sdk/schema/src/common/project.ts
@@ -8,27 +8,33 @@ import { Type } from '@dxos/echo';
 import { Format, GeneratorAnnotation, LabelAnnotation } from '@dxos/echo-schema';
 
 import { IconAnnotation, ItemAnnotation } from '../annotations';
+import { View } from '../view';
+
+import { Collection } from './collection';
 
 /**
  * Project schema.
  */
-const ProjectSchema = Schema.Struct({
+export const Project = Schema.Struct({
   id: Type.ObjectId,
-  name: Schema.String.pipe(GeneratorAnnotation.set('commerce.productName')),
+  name: Schema.String.pipe(GeneratorAnnotation.set('commerce.productName'), Schema.optional),
   image: Schema.optional(Format.URL),
   description: Schema.optional(Schema.String),
+  // prettier-ignore
+  collections: Schema.Array(
+    Schema.Union(
+      Type.Ref(Collection),
+      Type.Ref(View),
+    ),
+  ).pipe(Schema.mutable),
 }).pipe(
+  Type.Obj({
+    typename: 'dxos.org/type/Project',
+    version: '0.1.0',
+  }),
   Schema.annotations({ title: 'Project' }),
   LabelAnnotation.set(['name']),
   ItemAnnotation.set(true),
   IconAnnotation.set('ph--kanban--regular'),
 );
-
-export const Project = ProjectSchema.pipe(
-  Type.Obj({
-    typename: 'dxos.org/type/Project',
-    version: '0.1.0',
-  }),
-);
-
 export interface Project extends Schema.Schema.Type<typeof Project> {}
