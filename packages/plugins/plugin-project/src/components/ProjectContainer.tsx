25fa15c00e25ed4a04594e6972a525dfb49c0ecb
thure <will@braneframe.com>
2025-09-16 19:20:16 -0700

    Attempt to get basic idle state in-place.

--- packages/plugins/plugin-project/src/components/ProjectContainer.tsx
+++ packages/plugins/plugin-project/src/components/ProjectContainer.tsx
@@ -4,16 +4,28 @@
 
 import React from 'react';
 
-import { fullyQualifiedId } from '@dxos/react-client/echo';
-import { StackItem } from '@dxos/react-ui-stack';
-import { type DataType } from '@dxos/schema';
+import { Ref } from '@dxos/echo-schema';
+import { Stack, StackItem } from '@dxos/react-ui-stack';
+import { type DataType, type View } from '@dxos/schema';
+
+import { ViewCollectionColumn } from './ViewCollectionColumn';
 
 export const ProjectContainer = ({ object }: { object: DataType.Project; role: string }) => {
   return (
     <StackItem.Content>
-      <pre className='m-4 p-2 ring'>
-        <span>{fullyQualifiedId(object)}</span>
-      </pre>
+      <Stack orientation='horizontal' size='contain' rail={false}>
+        {(
+          object.collections.filter((collection) => {
+            if (!Ref.isRef(collection)) {
+              return false;
+            }
+            const typeDXN = collection.dxn.asTypeDXN();
+            return typeDXN?.type === 'dxos.org/type/View';
+          }) as Ref<View>[]
+        ).map((collection) => {
+          return <ViewCollectionColumn key={collection.dxn.toString()} viewRef={collection} />;
+        })}
+      </Stack>
     </StackItem.Content>
   );
 };
---
+++ packages/plugins/plugin-project/src/components/ViewCollectionColumn.tsx
@@ -0,0 +1,57 @@
+//
+// Copyright 2025 DXOS.org
+//
+import React from 'react';
+
+import { Surface } from '@dxos/app-framework';
+import { type AnyEchoObject, type Ref } from '@dxos/echo-schema';
+import { useClient } from '@dxos/react-client';
+import { Filter, getSpace, useQuery, useSchema } from '@dxos/react-client/echo';
+import { Card, CardStack, StackItem, cardStackHeading } from '@dxos/react-ui-stack';
+import { type View } from '@dxos/schema';
+
+export type ViewCollectionColumnProps = {
+  viewRef: Ref<View>;
+};
+
+export const ViewCollectionColumn = ({ viewRef }: ViewCollectionColumnProps) => {
+  // Resolve the view from the viewRef using useQuery
+  const client = useClient();
+  const space = getSpace(viewRef);
+  const echoDxn = viewRef.dxn.asEchoDXN();
+  const [view]: View[] = useQuery(space, echoDxn ? Filter.ids(echoDxn.echoId) : Filter.nothing());
+
+  // Resolve the view.query to its items
+  const schema = useSchema(client, space, view?.query.typename);
+  const items = useQuery(space, schema ? Filter.type(schema) : Filter.nothing());
+
+  if (!view || !view.query || !items) {
+    return null;
+  }
+
+  return (
+    <CardStack.Root asChild>
+      <StackItem.Root item={{ id: viewRef.dxn.toString() }} size={20} focusIndicatorVariant='group'>
+        <CardStack.Content>
+          <StackItem.Heading classNames={cardStackHeading} separateOnScroll>
+            {view.name ?? 'Untitled view'}
+          </StackItem.Heading>
+          <CardStack.Stack id={viewRef.dxn.toString()} itemsCount={items.length}>
+            {items.map((liveMarker) => {
+              const item = liveMarker as unknown as AnyEchoObject;
+              return (
+                <CardStack.Item asChild key={item.id}>
+                  <StackItem.Root item={item} focusIndicatorVariant='group'>
+                    <Card.StaticRoot>
+                      <Surface role='card--intrinsic' limit={1} data={{ subject: item, projection: view.projection }} />
+                    </Card.StaticRoot>
+                  </StackItem.Root>
+                </CardStack.Item>
+              );
+            })}
+          </CardStack.Stack>
+        </CardStack.Content>
+      </StackItem.Root>
+    </CardStack.Root>
+  );
+};
