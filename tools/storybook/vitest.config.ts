ee256dc2d4ccbdbccfed8dfea52ccb911b6d3467
Dmytro Maretskyi <maretskii@gmail.com>
2025-09-22 13:33:34 +0200

    Do not include base confog for storybook tests

--- tools/storybook/vitest.config.ts
+++ tools/storybook/vitest.config.ts
@@ -2,43 +2,34 @@
 // Copyright 2025 DXOS.org
 //
 
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { defineConfig, mergeConfig } from 'vitest/config';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { defineConfig } from 'vitest/config';
 
 // TODO(burdon): Factor out common components.
-import { baseConfig } from '../../vitest.storybook.config';
 
-const dirname =
-  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-export default mergeConfig(
-  baseConfig({ cwd: dirname }),
-  defineConfig({
-    optimizeDeps: {
-      noDiscovery: true,
-      include: [],
-    },
-    plugins: [
-      storybookTest({
-        configDir: path.join(dirname, '.storybook'),
-        // The --ci flag will skip prompts and not open a browser.
-        storybookScript: 'storybook dev --ci',
-        tags: {
-          include: ['test'],
-          exclude: ['experimental'],
-        },
-      }),
-    ],
-    test: {
-      browser: {
-        enabled: true,
-        provider: 'playwright',
-        headless: true,
-        instances: [{ browser: 'chromium' }],
+export default defineConfig({
+  plugins: [
+    storybookTest({
+      configDir: path.join(dirname, '.storybook'),
+      // The --ci flag will skip prompts and not open a browser.
+      storybookScript: 'storybook dev --ci',
+      tags: {
+        include: ['test'],
+        exclude: ['experimental'],
       },
-      setupFiles: ['./.storybook/vitest.setup.ts'],
+    }),
+  ],
+  test: {
+    browser: {
+      enabled: true,
+      provider: 'playwright',
+      headless: true,
+      instances: [{ browser: 'chromium' }],
     },
-  }),
-);
+    setupFiles: ['./.storybook/vitest.setup.ts'],
+  },
+});
