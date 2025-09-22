88dd6fc6891b8d79bf4cbed1d95265722d049573
thure <will@braneframe.com>
2025-09-22 16:12:23 -0700

    Get `status` label and hue from schema.

--- packages/plugins/plugin-preview/src/components/TaskCard.tsx
+++ packages/plugins/plugin-preview/src/components/TaskCard.tsx
@@ -2,29 +2,44 @@
 // Copyright 2025 DXOS.org
 //
 
+import { SchemaAST } from 'effect';
 import React from 'react';
 
-import { useTranslation } from '@dxos/react-ui';
+import { type PropertyMetaAnnotation, PropertyMetaAnnotationId } from '@dxos/echo-schema';
 import { Card, cardNoSpacing, cardSpacing } from '@dxos/react-ui-stack';
 import { mx } from '@dxos/react-ui-theme';
-import { type DataType } from '@dxos/schema';
+import { DataType } from '@dxos/schema';
 
-import { meta } from '../meta';
 import { type PreviewProps } from '../types';
 
 import { CardSubjectMenu } from './CardSubjectMenu';
 
+// TODO(thure): Should this move upstream as a helper? Is there an easier way to get options?
+const getActiveStatusOption = (status?: string) => {
+  const properties = SchemaAST.getPropertySignatures(DataType.Task.ast);
+  const statusProperty = properties.find((p) => p.name === 'status');
+  const statusMeta = SchemaAST.getAnnotation<PropertyMetaAnnotation>(PropertyMetaAnnotationId)(
+    // TODO(thure): Typescript asserts `.type` doesn’t have `.types`, but in runtime it does.
+    (statusProperty!.type as any).types[0],
+  );
+  const options = // TODO(thure): Typescript asserts `statusMeta` doesn’t have `.value`, but in runtime it does.
+    (statusMeta as any).value.singleSelect.options as { id: string; title: string; color: string }[];
+  return options.find(({ id }) => id === status);
+};
+
 export const TaskCard = ({ subject, role }: PreviewProps<DataType.Task>) => {
   const { title, status } = subject;
   // TODO(thure): `getPropertyMetaAnnotation` isn’t working when trying to get option titles from `DataType.Task.ast`.
-  const { t } = useTranslation(meta.id);
+  const statusOption = getActiveStatusOption(status);
   return (
     <Card.SurfaceRoot role={role}>
       <div role='none' className={mx('flex items-center gap-2', cardSpacing)}>
         <Card.Heading classNames={[cardNoSpacing, 'min-is-0 flex-1 truncate']}>{title}</Card.Heading>
-        <span className='dx-tag' data-hue='neutral'>
-          {t(`${status} label`)}
-        </span>
+        {statusOption && (
+          <span className='dx-tag' data-hue={statusOption.color}>
+            {statusOption.title}
+          </span>
+        )}
         <CardSubjectMenu subject={subject} />
       </div>
     </Card.SurfaceRoot>
