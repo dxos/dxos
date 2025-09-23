40f111dd3feb4f2f27c1a8f9062c7f7f9042d26b
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2025-09-23 10:19:48 +0200

    feat: Queue query API (#9830)

---
+++ packages/core/echo/echo-db/src/queue/queue-query-context.ts
@@ -0,0 +1,108 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { Array, pipe } from 'effect';
+
+import { Event } from '@dxos/async';
+import { Context } from '@dxos/context';
+import { Obj } from '@dxos/echo';
+import { filterMatchObjectJSON } from '@dxos/echo-pipeline/filter';
+import type { QueryAST } from '@dxos/echo-protocol';
+import type { BaseObject } from '@dxos/echo-schema';
+import { invariant } from '@dxos/invariant';
+
+import { type QueryContext, type QueryResultEntry, isSimpleSelectionQuery } from '../query';
+
+import { type QueueImpl } from './queue';
+
+export class QueueQueryContext implements QueryContext {
+  readonly #queue: QueueImpl;
+  #runCtx: Context | null = null;
+
+  // Extracted from query.
+  #filter?: QueryAST.Filter = undefined;
+
+  readonly changed = new Event();
+
+  constructor(queue: QueueImpl) {
+    this.#queue = queue;
+  }
+
+  /**
+   * One-shot run.
+   */
+  async run(query: QueryAST.Query): Promise<QueryResultEntry<BaseObject>[]> {
+    const trivial = isSimpleSelectionQuery(query);
+    if (!trivial) {
+      throw new Error('Query not supported.');
+    }
+    const { filter } = trivial;
+
+    const spaceId = this.#queue.dxn.asQueueDXN()!.spaceId;
+
+    const objects = await pipe(
+      await this.#queue.fetchObjectsJSON(),
+      Array.filter((obj) => filterMatchObjectJSON(filter, obj)),
+      Array.map(async (obj) => this.#queue.hydrateObject(obj)),
+      (_) => Promise.all(_),
+    );
+
+    return objects.map((object) => ({
+      id: object.id,
+      spaceId,
+      object,
+    }));
+  }
+
+  /**
+   * Start reactive query.
+   */
+  start(): void {
+    this.#runCtx = Context.default();
+    this.#runCtx.onDispose(this.#queue.beginPolling());
+    this.#queue.updated.on(this.#runCtx, () => {
+      this.changed.emit();
+    });
+  }
+
+  /**
+   * Stop reactive query.
+   */
+  stop(): void {
+    void this.#runCtx?.dispose();
+    this.#runCtx = null;
+  }
+
+  /**
+   * Update the filter (for reactive queries).
+   */
+  update(query: QueryAST.Query): void {
+    const trivial = isSimpleSelectionQuery(query);
+    if (!trivial) {
+      throw new Error('Query not supported.');
+    }
+    const { filter } = trivial;
+    this.#filter = filter;
+    this.changed.emit();
+  }
+
+  /**
+   * Synchronously get the results.
+   */
+  getResults(): QueryResultEntry<BaseObject>[] {
+    invariant(this.#filter);
+
+    const spaceId = this.#queue.dxn.asQueueDXN()!.spaceId;
+    return pipe(
+      this.#queue.getObjectsSync(),
+      // TODO(dmaretskyi): We end-up marshaling objects from JSON and back.
+      Array.filter((obj) => filterMatchObjectJSON(this.#filter!, Obj.toJSON(obj))),
+      Array.map((object) => ({
+        id: object.id,
+        spaceId,
+        object,
+      })),
+    );
+  }
+}
