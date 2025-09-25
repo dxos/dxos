b4780417f4402c9b9ce5d476e82ccfd7a749c56b
thure <will@braneframe.com>
2025-09-25 14:25:17 -0700

    Separate CommandDialog from CommandMenu.

--- packages/ui/react-ui-editor/src/stories/CommandDialog.stories.tsx
+++ packages/ui/react-ui-editor/src/stories/CommandDialog.stories.tsx
@@ -12,7 +12,7 @@ import { mx } from '@dxos/react-ui-theme';
 import { withLayout, withTheme } from '@dxos/storybook-utils';
 
 import { editorWidth } from '../defaults';
-import { type Action, command, floatingMenu } from '../extensions';
+import { type Action, commandDialog } from '../extensions';
 import { str } from '../testing';
 import { createRenderer } from '../util';
 
@@ -66,14 +66,13 @@ const CommandDialog = ({ onAction }: { onAction: (action?: Action) => void }) =>
 };
 
 const meta = {
-  title: 'ui/react-ui-editor/Command',
+  title: 'ui/react-ui-editor/CommandDialog',
   decorators: [withTheme, withLayout({ fullscreen: true })],
   render: () => (
     <EditorStory
       text={str('# Command', '', '', '')}
       extensions={[
-        floatingMenu(),
-        command({
+        commandDialog({
           renderDialog: createRenderer(CommandDialog),
           onHint: () => "Press '/' for commands",
         }),
