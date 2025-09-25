3057a3c9d1ba4d452d552743290edb58305f2a0b
Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
2025-09-25 13:21:54 +0200

    feat: Replace fs-ext with koffi (#9866)

---
+++ packages/common/lock-file/src/locker-subprocess.ts
@@ -0,0 +1,22 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { LockFile } from './lock-file';
+
+const filename = process.argv[2];
+
+console.log('will lock');
+
+const handle = await LockFile.acquire(filename);
+// parents looks for # symbol in the output to know when the lock is acquired
+console.log('# locked');
+
+// Close file handle on stdin close.
+process.stdin.on('data', async (data) => {
+  if (data.toString().trim() === 'close') {
+    console.log('will unlock');
+    await LockFile.release(handle);
+    console.log('unlocked');
+  }
+});
