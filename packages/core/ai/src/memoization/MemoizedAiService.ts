1adb8550c1c8f455480a690bc845bcf625171ad3
Dmytro Maretskyi <maretskii@gmail.com>
2025-09-30 22:15:08 +0200

    First iteration working!

--- packages/core/ai/src/memoization/MemoizedAiService.ts
+++ packages/core/ai/src/memoization/MemoizedAiService.ts
@@ -1,7 +1,12 @@
-import { LanguageModel, Prompt, Tool } from '@effect/ai';
-import type * as AiService from '../AiService';
-import { Effect, Layer, Schema, type Context, Option } from 'effect';
+import { Chat, LanguageModel, Prompt, Response, Tool, Toolkit } from '@effect/ai';
+import * as AiService from '../AiService';
+import { Effect, Layer, Schema, type Context, Option, Stream, Array, Order, pipe, Data } from 'effect';
 import { todo } from '@dxos/debug';
+import { readFile, writeFile } from 'node:fs/promises';
+import { open, type FileHandle } from 'node:fs/promises';
+import { deepEqual } from 'node:assert';
+import { isDeepStrictEqual } from 'node:util';
+import { invariant } from '@dxos/invariant';
 
 export interface MemoizedAiService extends AiService.Service {}
 
@@ -30,6 +35,37 @@ export const make = (options: MakeOptions): MemoizedAiService => {
   };
 };
 
+export const layer = (options: Omit<MakeOptions, 'upstream'>) =>
+  Layer.effect(
+    AiService.AiService,
+    Effect.gen(function* () {
+      const upstream = yield* AiService.AiService;
+      return make({
+        upstream,
+        storePath: options.storePath,
+      });
+    }),
+  );
+
+type TestContextLike = {
+  task: {
+    file: {
+      filepath: string;
+    };
+  };
+};
+
+export const injectIntoTest = <A, E, R>(
+  effect: Effect.Effect<A, E, R | AiService.AiService>,
+  ctx: TestContextLike,
+): Effect.Effect<A, E, R | AiService.AiService> => {
+  return Effect.provide(
+    layer({
+      storePath: ctx.task.file.filepath + '.conversations.json',
+    }),
+  )(effect);
+};
+
 interface MakeModelOptions {
   upstreamModel: LanguageModel.Service;
   modelName: string;
@@ -42,10 +78,32 @@ const makeModel = (options: MakeModelOptions): Effect.Effect<LanguageModel.Servi
   return LanguageModel.make({
     generateText: Effect.fn('MemoizedLanguageModel.generateText')(function* (params) {
       const conversation = getConverstaionFromOptions(options.modelName, params);
-      const continuation = yield* store.getContinuation(conversation);
-      if (Option.isSome(continuation)) {
+      const memoized = yield* store.getMemoizedConversation(conversation);
+      if (Option.isSome(memoized)) {
+        const continuation = getContinuation(params.prompt, memoized.value.history);
+        return toResponseParts(continuation);
+      } else {
+        const chat = yield* Chat.empty;
+
+        const toolkit = Toolkit.make(...(params.tools as never[]));
+
+        const upstreamResult = yield* chat
+          .generateText({
+            prompt: params.prompt,
+            toolkit: yield* toolkit,
+            toolChoice: params.toolChoice as any,
+            disableToolCallResolution: true,
+          })
+          .pipe(Effect.provideService(LanguageModel.LanguageModel, options.upstreamModel));
+
+        const conversation: MemoziedConversation = {
+          parameters: getMemoizedConversationParameters(options.modelName, params),
+          history: yield* chat.history,
+        };
+        yield* store.saveMemoizedConversation(conversation);
+
+        return reinterpretResponseParts(upstreamResult.content);
       }
-      return todo();
     }),
     streamText: (params) => {
       return todo();
@@ -53,7 +111,108 @@ const makeModel = (options: MakeModelOptions): Effect.Effect<LanguageModel.Servi
   });
 };
 
-const getConversationParameters = (model: string, params: LanguageModel.ProviderOptions): ConversationParameters => {
+const getContinuation = (prompt: Prompt.Prompt, memoized: Prompt.Prompt): Prompt.Part[] => {
+  const continuation = memoized.content.slice(prompt.content.length);
+  const message = continuation.at(0);
+  invariant(!!message, 'No message in continuation');
+  invariant(message.role === 'assistant');
+  return [...message.content];
+};
+
+const reinterpretResponseParts = (parts: Response.Part<Record<string, Tool.Any>>[]): Response.PartEncoded[] => {
+  return parts.map((part): Response.PartEncoded => {
+    switch (part.type) {
+      case 'text':
+        return Response.TextPart.pipe(Schema.encodeSync)(part);
+      case 'reasoning':
+        return Response.ReasoningPart.pipe(Schema.encodeSync)(part);
+      case 'finish':
+        return Response.FinishPart.pipe(Schema.encodeSync)(part);
+      case 'response-metadata':
+        return Response.ResponseMetadataPart.pipe(Schema.encodeSync)(part);
+      case 'file':
+        return Response.FilePart.pipe(Schema.encodeSync)(part);
+      case 'source':
+        switch (part.sourceType) {
+          case 'url':
+            return Response.UrlSourcePart.pipe(Schema.encodeSync)(part);
+          case 'document':
+            return Response.DocumentSourcePart.pipe(Schema.encodeSync)(part);
+          default:
+            throw new Error(`Unknown source type: ${(part as any).sourceType}`);
+        }
+      case 'tool-call':
+        return {
+          type: 'tool-call',
+          id: part.id,
+          name: part.name,
+          params: part.params, // TODO: Pass params through the tool schema.
+          metadata: part.metadata,
+          providerExecuted: part.providerExecuted,
+          providerName: part.providerName,
+        };
+      case 'tool-result':
+        return {
+          type: 'tool-result',
+          id: part.id,
+          name: part.name,
+          result: part.result,
+          metadata: part.metadata,
+          providerExecuted: part.providerExecuted,
+          providerName: part.providerName,
+        };
+      default:
+        throw new Error(`Unknown part type: ${(part as any).type}`);
+    }
+  });
+};
+
+const toResponseParts = (parts: Prompt.Part[]): Response.PartEncoded[] => {
+  return parts.map((part): Response.PartEncoded => {
+    switch (part.type) {
+      case 'text':
+        return {
+          type: 'text',
+          metadata: part.options,
+          text: part.text,
+        };
+      case 'reasoning':
+        return {
+          type: 'reasoning',
+          metadata: part.options,
+          text: part.text,
+        };
+      case 'tool-call':
+        return {
+          type: 'tool-call',
+          id: part.id,
+          name: part.name,
+          params: part.params, // TODO: Pass params through the tool schema.
+          metadata: part.options,
+          providerExecuted: part.providerExecuted,
+        };
+      case 'tool-result':
+        return {
+          type: 'tool-result',
+          id: part.id,
+          name: part.name,
+          result: part.result,
+          metadata: part.options,
+        };
+      default:
+        throw new Error(`Unknown part type: ${(part as any).type}`);
+    }
+  });
+};
+
+const toResponseStream = (continuation: Prompt.Part[]): Stream.Stream<Response.StreamPartEncoded> => {
+  return todo();
+};
+
+const getMemoizedConversationParameters = (
+  model: string,
+  params: LanguageModel.ProviderOptions,
+): ConversationParameters => {
   return {
     model,
     tools: params.tools.map((tool) => ({
@@ -66,9 +225,35 @@ const getConversationParameters = (model: string, params: LanguageModel.Provider
 
 const getConverstaionFromOptions = (model: string, params: LanguageModel.ProviderOptions): MemoziedConversation => {
   return {
-    model,
+    parameters: getMemoizedConversationParameters(model, params),
     history: params.prompt,
   };
+};
+
+const isPrefix = (haystack: MemoziedConversation, needle: MemoziedConversation): boolean => {
+  if (
+    haystack.parameters.model !== needle.parameters.model ||
+    haystack.parameters.tools.length !== needle.parameters.tools.length
+  ) {
+    return false;
+  }
+
+  // TODO(dmaretskyi): This might not compare tools correctly.
+  if (!isDeepStrictEqual(haystack.parameters.tools, needle.parameters.tools)) {
+    return false;
+  }
+
+  if (haystack.history.content.length < needle.history.content.length) {
+    return false;
+  }
+
+  for (let i = 0; i < needle.history.content.length; i++) {
+    if (!isDeepStrictEqual(haystack.history.content[i], needle.history.content[i])) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 class MemoizedStore {
@@ -78,8 +263,46 @@ class MemoizedStore {
     this.#path = path;
   }
 
-  getContinuation(conversation: MemoziedConversation): Effect.Effect<Option.Option<MemoziedConversation>> {
-    return todo();
+  getMemoizedConversation(prompted: MemoziedConversation): Effect.Effect<Option.Option<MemoziedConversation>> {
+    return Effect.gen(this, function* () {
+      const stored = yield* this.#loadStore();
+      return pipe(
+        stored.conversations,
+        Array.sortBy(Order.mapInput(Order.number, (x) => x.history.content.length)),
+        Array.findFirst((x) => isPrefix(x, prompted)),
+      );
+    });
+  }
+
+  saveMemoizedConversation(conversation: MemoziedConversation): Effect.Effect<void> {
+    return Effect.gen(this, function* () {
+      const store = yield* this.#loadStore();
+      store.conversations = store.conversations.filter((_) => !isPrefix(_, conversation));
+      store.conversations.push(conversation);
+      yield* this.#saveStore(store);
+    });
+  }
+
+  #loadStore(): Effect.Effect<ConversationStore> {
+    return Effect.promise(async () => {
+      try {
+        const data = await readFile(this.#path, 'utf-8');
+        return Schema.decodeSync(Schema.parseJson(ConversationStore))(data);
+      } catch (err: any) {
+        if (err.code === 'ENOENT') {
+          return { conversations: [] };
+        } else {
+          throw err;
+        }
+      }
+    });
+  }
+
+  #saveStore(store: ConversationStore): Effect.Effect<void> {
+    // TODO(dmaretskyi): Figure out how to make this thread-safe.
+    return Effect.promise(async () => {
+      await writeFile(this.#path, Schema.encodeSync(Schema.parseJson(ConversationStore))(store));
+    });
   }
 }
 
@@ -96,7 +319,12 @@ const ConversationParameters = Schema.Struct({
 type ConversationParameters = Schema.Schema.Type<typeof ConversationParameters>;
 
 const MemoziedConversation = Schema.Struct({
-  model: Schema.String,
+  parameters: ConversationParameters,
   history: Prompt.Prompt,
 });
 type MemoziedConversation = Schema.Schema.Type<typeof MemoziedConversation>;
+
+const ConversationStore = Schema.Struct({
+  conversations: Schema.Array(MemoziedConversation).pipe(Schema.mutable),
+}).pipe(Schema.mutable);
+type ConversationStore = Schema.Schema.Type<typeof ConversationStore>;
--- packages/core/ai/src/memoization/memoization.test.ts
+++ packages/core/ai/src/memoization/memoization.test.ts
@@ -1,12 +1,31 @@
-import { describe, it } from '@effect/vitest';
-import { Effect } from 'effect';
+import { describe, expect, it } from '@effect/vitest';
+import { Effect, Layer } from 'effect';
+import * as MemoizedAiService from './MemoizedAiService';
+import { AiServiceTestingPreset } from '../testing';
+import * as AiService from '../AiService';
+import { LanguageModel } from '@effect/ai';
 
 describe('memoization', () => {
   it.effect(
     'context paths',
     Effect.fnUntraced(function* (ctx) {
       const filepath = ctx.task.file.filepath;
-      console.log({ filepath });
+      expect(filepath.endsWith('memoization.test.ts')).toBe(true);
     }),
+  );
+
+  it.effect(
+    'generate a poem',
+    Effect.fnUntraced(
+      function* (ctx) {
+        const result = yield* LanguageModel.generateText({
+          prompt: 'Write me a long poem!',
+        });
+        console.log(result);
+      },
+      Effect.provide(AiService.model('@anthropic/claude-sonnet-4-0')),
+      MemoizedAiService.injectIntoTest,
+      Effect.provide(AiServiceTestingPreset('direct')),
+    ),
   );
 });
