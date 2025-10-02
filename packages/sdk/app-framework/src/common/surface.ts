f6ce2cb6c087a64f70fd54129ade39dfe60501e0
Josiah Witt <josiah@witt.life>
2025-10-02 09:25:09 -0400

    further refine surface types

--- packages/sdk/app-framework/src/common/surface.ts
+++ packages/sdk/app-framework/src/common/surface.ts
@@ -11,27 +11,28 @@ import { type ErrorBoundary } from '../react';
 /**
  * SurfaceProps are the props that are passed to the Surface component.
  */
-export type SurfaceProps<T extends Record<string, any> = Record<string, unknown>> = PropsWithChildren<
-  {
-    /**
-     * If specified, the Surface will be wrapped in an error boundary.
-     * The fallback component will be rendered if an error occurs.
-     */
-    fallback?: ErrorBoundary['props']['fallback'];
+export type SurfaceProps<T extends Record<string, any> = Record<string, unknown>> = {
+  /**
+   * If specified, the Surface will be wrapped in an error boundary.
+   * The fallback component will be rendered if an error occurs.
+   */
+  fallback?: ErrorBoundary['props']['fallback'];
 
-    /**
-     * If specified, the Surface will be wrapped in a suspense boundary.
-     * The placeholder component will be rendered while the surface component is loading.
-     */
-    placeholder?: ReactNode;
-  } & MakeOptional<CoreSurfaceProps<T>, 'id' | 'data'>
-> &
+  /**
+   * If specified, the Surface will be wrapped in a suspense boundary.
+   * The placeholder component will be rendered while the surface component is loading.
+   */
+  placeholder?: ReactNode;
+} & MakeOptional<CoreSurfaceProps<T>, 'id' | 'data'> &
   // Additional props to pass to the component.
   // These props are not used by Surface itself but may be used by components which resolve the surface.
-  Record<string, any>;
+  // Exclude known prop names to prevent overriding well-defined props.
+  {
+    [K in keyof Record<string, any>]: K extends keyof CoreSurfaceProps<T> | 'fallback' | 'placeholder' ? never : any;
+  };
 
 // NOTE: If `[key: string]: unknown` is included in shared types, when re-used other fields become unknown as well.
-type CoreSurfaceProps<T extends Record<string, any> = Record<string, unknown>> = {
+type CoreSurfaceProps<T extends Record<string, any> = Record<string, unknown>> = PropsWithChildren<{
   /**
    * ID for debugging.
    */
@@ -51,11 +52,9 @@ type CoreSurfaceProps<T extends Record<string, any> = Record<string, unknown>> =
    * If more than one component is resolved, the limit determines how many are rendered.
    */
   limit?: number | undefined;
-};
+}>;
 
-type SurfaceComponentProps<T extends Record<string, any> = Record<string, unknown>> = PropsWithChildren<
-  CoreSurfaceProps<T>
-> &
+type SurfaceComponentProps<T extends Record<string, any> = Record<string, unknown>> = CoreSurfaceProps<T> &
   Record<string, any>;
 
 /**
