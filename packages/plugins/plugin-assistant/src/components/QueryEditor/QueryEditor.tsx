90deea6603d96a5cef3d1d46c8621d86dd2b0e82
Rich Burdon <rich.burdon@gmail.com>
2025-10-03 11:15:34 -0400

    chore: add comments for QueryEditor variants and fix tag filter visibility state management

--- packages/plugins/plugin-assistant/src/components/QueryEditor/QueryEditor.tsx
+++ packages/plugins/plugin-assistant/src/components/QueryEditor/QueryEditor.tsx
@@ -19,6 +19,11 @@ import { meta } from '../../meta';
 
 import { query } from './query-extension';
 
+// TODO(burdon): Reconcile all QueryEditor variants.
+// TODO(burdon): Adjust grammar to support simple queries (e.g., tags).
+//  - Pipeline query editor
+//  - Mailbox search
+
 export type QueryEditorProps = ThemedClassName<
   {
     space?: Space;
