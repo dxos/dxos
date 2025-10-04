44fea967fd479d0c83d234d0fb2ea764ef15055f
Rich Burdon <rich.burdon@gmail.com>
2025-10-04 15:33:43 -0400

    refactor: move value/onChange handling from QueryEditor to Editor component

--- packages/ui/react-ui-components/src/components/QueryEditor/QueryEditor.tsx
+++ packages/ui/react-ui-components/src/components/QueryEditor/QueryEditor.tsx
@@ -2,14 +2,14 @@
 // Copyright 2025 DXOS.org
 //
 
-import React, { forwardRef, useEffect, useMemo } from 'react';
+import React, { forwardRef, useMemo } from 'react';
 
 import { type Space } from '@dxos/client/echo';
-import { type ThemedClassName, useForwardedRef, useThemeContext, useTranslation } from '@dxos/react-ui';
+import { type ThemedClassName, useThemeContext, useTranslation } from '@dxos/react-ui';
 import {
   Editor,
   type EditorProps,
-  EditorView,
+  type EditorView,
   type Extension,
   createBasicExtensions,
   createThemeExtensions,
@@ -23,8 +23,6 @@ export type QueryEditorProps = ThemedClassName<
   {
     space?: Space;
     readonly?: boolean;
-    value?: string;
-    onChange?: (text: string) => void;
   } & EditorProps
 >;
 
@@ -32,38 +30,18 @@ export type QueryEditorProps = ThemedClassName<
  * Query editor with decorations and autocomplete.
  */
 export const QueryEditor = forwardRef<EditorView | null, QueryEditorProps>(
-  ({ space, value, onChange, readonly, ...props }, forwardedRef) => {
+  ({ space, value, readonly, ...props }, forwardedRef) => {
     const { t } = useTranslation(translationKey);
     const { themeMode } = useThemeContext();
-    const editorRef = useForwardedRef(forwardedRef);
-
-    // Extensions.
     const extensions = useMemo<Extension[]>(
       () => [
         createBasicExtensions({ readOnly: readonly, placeholder: t('query placeholder') }),
         createThemeExtensions({ themeMode }),
         query({ space }),
-        EditorView.updateListener.of((update) => {
-          if (update.docChanged) {
-            onChange?.(update.state.doc.toString());
-          }
-        }),
       ],
       [space, readonly],
     );
 
-    // Update content.
-    useEffect(() => {
-      const view = editorRef.current;
-      if (value !== view?.state.doc.toString()) {
-        requestAnimationFrame(() => {
-          view?.dispatch({
-            changes: { from: 0, to: view.state.doc.length, insert: value },
-          });
-        });
-      }
-    }, [value]);
-
-    return <Editor initialValue={value} extensions={extensions} {...props} ref={editorRef} />;
+    return <Editor value={value} extensions={extensions} {...props} ref={forwardedRef} />;
   },
 );
--- packages/ui/react-ui-editor/src/components/Editor/Editor.tsx
+++ packages/ui/react-ui-editor/src/components/Editor/Editor.tsx
@@ -2,31 +2,56 @@
 // Copyright 2025 DXOS.org
 //
 
-import { type EditorView } from '@codemirror/view';
-import React, { forwardRef, useImperativeHandle } from 'react';
+import { EditorView } from '@codemirror/view';
+import React, { forwardRef, useEffect, useImperativeHandle } from 'react';
 
 import { type ThemedClassName } from '@dxos/react-ui';
 import { mx } from '@dxos/react-ui-theme';
 
 import { type UseTextEditorProps, useTextEditor } from '../../hooks';
 
-export type EditorProps = ThemedClassName<UseTextEditorProps>;
+export type EditorProps = ThemedClassName<
+  {
+    value?: string;
+    onChange?: (value: string) => void;
+  } & Omit<UseTextEditorProps, 'initialValue'>
+>;
 
 /**
  * Minimal text editor.
  */
 export const Editor = forwardRef<EditorView | null, EditorProps>(
-  ({ classNames, id, extensions, ...props }, forwardedRef) => {
+  ({ classNames, id, extensions = [], value, onChange, ...props }, forwardedRef) => {
     const { parentRef, focusAttributes, view } = useTextEditor(
       () => ({
         id,
-        extensions,
+        extensions: [
+          extensions,
+          EditorView.updateListener.of((update) => {
+            if (update.docChanged) {
+              onChange?.(update.state.doc.toString());
+            }
+          }),
+        ],
         ...props,
       }),
-      [id, extensions],
+      [id, extensions, onChange],
     );
 
+    // External controller.
     useImperativeHandle<EditorView | null, EditorView | null>(forwardedRef, () => view, [view]);
+
+    // Update content.
+    useEffect(() => {
+      if (value !== view?.state.doc.toString()) {
+        requestAnimationFrame(() => {
+          view?.dispatch({
+            changes: { from: 0, to: view.state.doc.length, insert: value },
+          });
+        });
+      }
+    }, [view, value]);
+
     return <div role='none' className={mx(classNames)} {...focusAttributes} ref={parentRef} />;
   },
 );
