437e225549907aececc4a646e72ea6bee7361dac
Rich Burdon <rich.burdon@gmail.com>
2025-10-08 12:39:33 -0400

    refactor: move add column button to toolbar and improve project component organization (#9960)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -185,6 +185,7 @@ export default tseslint.config(
       'require-yield': 'off',
 
       // React
+      // https://github.com/jsx-eslint/eslint-plugin-react/tree/master/docs/rules
       'react/display-name': 'off',
       'react/function-component-definition': [
         'error',
@@ -194,13 +195,20 @@ export default tseslint.config(
         },
       ],
       'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
+      // 'react/jsx-sort-props': [
+      //   'warn',
+      //   {
+      //     callbacksLast: true,
+      //     noSortAlphabetically: true,
+      //   },
+      // ],
       'react/jsx-tag-spacing': [
         'error',
         {
-          closingSlash: 'never',
-          beforeSelfClosing: 'always',
           afterOpening: 'never',
           beforeClosing: 'never',
+          beforeSelfClosing: 'always',
+          closingSlash: 'never',
         },
       ],
       'react/jsx-wrap-multilines': 'off',
