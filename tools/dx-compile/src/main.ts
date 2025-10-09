6c57f6bb63fd361fe28331a2a71ead7cfc095570
Dmytro Maretskyi <maretskii@gmail.com>
2025-10-09 12:19:48 +0200

    Add check for imports outside source directory

--- tools/dx-compile/src/main.ts
+++ tools/dx-compile/src/main.ts
@@ -18,6 +18,7 @@ import { bundleDepsPlugin } from './bundle-deps-plugin';
 import { esmOutputToCjs } from './esm-output-to-cjs-plugin';
 import { fixRequirePlugin } from './fix-require-plugin';
 import { SwcTransformPlugin } from './swc-transform-plugin';
+import { restrictRelativeImportsPlugin } from './plugin-restrict-relative-imports';
 
 export interface EsbuildExecutorOptions {
   bundle: boolean;
@@ -160,6 +161,9 @@ export default async (options: EsbuildExecutorOptions): Promise<{ success: boole
               }
             : undefined,
         plugins: [
+          restrictRelativeImportsPlugin({
+            allowedDirectory: process.cwd(),
+          }),
           NodeExternalPlugin({
             injectGlobals: options.injectGlobals,
             importGlobals: options.importGlobals,
