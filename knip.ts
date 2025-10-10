943c2923f117716c592bc6552832e6f67b204c38
Dmytro Maretskyi <maretskii@gmail.com>
2025-10-10 10:33:44 +0200

    Remove unused deps

--- knip.ts
+++ knip.ts
@@ -3,7 +3,12 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   entry: ['src/index.ts'],
   project: ['src/**/*.ts'],
-  ignoreDependencies: ['@dxos/node-std'],
+  ignoreDependencies: [
+    //
+    '@dxos/node-std',
+    '@bufbuild/buf',
+    '@bufbuild/protoc-gen-es',
+  ],
   storybook: false,
 };
 
