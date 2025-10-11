81331843d4d3103f4ad9d3229c719459985f8059
Rich Burdon <rich.burdon@gmail.com>
2025-10-11 17:10:46 -0400

    test: remove unused signature parsing test case

--- packages/plugins/plugin-inbox/src/functions/email/util.test.ts
+++ packages/plugins/plugin-inbox/src/functions/email/util.test.ts
@@ -4,8 +4,6 @@
 
 import { describe, test } from 'vitest';
 
-import { trim } from '@dxos/util';
-
 import { createUrl, stripWhitespace, toMarkdown } from './util';
 
 describe('util', () => {
@@ -26,24 +24,5 @@ describe('util', () => {
     expect(markdown).to.equal(
       'Another quick reminder to kindly complete this short questionnaire to indicate your preferred Day.',
     );
-  });
-
-  test('signature', ({ expect }) => {
-    const text = trim`
-      --
-      CITRIN COOPERMAN
-
-      Yaakov Maline
-      Director
-
-      212.832.0400 | 212.331.7472 (Direct) | 212.371.1159 (Fax)
-      50 Rockefeller Plaza, New York, NY 10020 ymaline@citrincooperman.com <mailto:ymaline@citrincooperman.com>
-    `;
-
-    expect(text).to.equal({
-      name: 'Yaakov Maline',
-      org: 'CITRIN COOPERMAN',
-      phone: '212.832.0400',
-    });
   });
 });
