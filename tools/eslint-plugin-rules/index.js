1a1f0393e72989babcb1f24c424033e18925b355
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-10-13 15:56:44 +0200

    Add namespace imports enforcement

--- tools/eslint-plugin-rules/index.js
+++ tools/eslint-plugin-rules/index.js
@@ -5,6 +5,7 @@
 import comment from './rules/comment.js';
 import header from './rules/header.js';
 import noEmptyPromiseCatch from './rules/no-empty-promise-catch.js';
+import enforceNamespaceImports from './rules/enforce-namespace-imports.js';
 import fs from 'node:fs';
 
 const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf8'));
@@ -19,6 +20,7 @@ const plugin = {
     comment,
     header,
     'no-empty-promise-catch': noEmptyPromiseCatch,
+    'enforce-namespace-imports': enforceNamespaceImports,
   },
   configs: {
     recommended: {
@@ -28,6 +30,9 @@ const plugin = {
       rules: {
         'dxos-plugin/header': 'error',
         'dxos-plugin/no-empty-promise-catch': 'error',
+        // Opt-in rule: enforce namespace imports for Effect packages
+        // Enable it repo-wide in root eslint config
+        'dxos-plugin/enforce-namespace-imports': ['error', { packages: ['effect', '@effect/*'] }],
         // TODO(dmaretskyi): Turned off due to large number of errors and no auto-fix.
         // 'dxos-plugin/comment': 'error',
       },
