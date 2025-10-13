45d87835880be48669ac75d07e8af0452e322313
Rich Burdon <rich.burdon@gmail.com>
2025-10-13 13:42:50 -0400

    refactor: remove unnecessary wrapper div from StackMain component

--- packages/plugins/plugin-stack/src/capabilities/react-surface.tsx
+++ packages/plugins/plugin-stack/src/capabilities/react-surface.tsx
@@ -24,11 +24,7 @@ export default () =>
         // This allows the id to be overridden by the surface for situations where the id of the collection
         // is not the same as the id of what is being represented (e.g., a space with a root collection).
         const id = typeof data.id === 'string' ? data.id : undefined;
-        return (
-          <div role='none' className='overflow-auto' style={{ contain: 'layout' }}>
-            <StackMain id={id ?? fullyQualifiedId(data.subject)} collection={data.subject} />
-          </div>
-        );
+        return <StackMain id={id ?? fullyQualifiedId(data.subject)} collection={data.subject} />;
       },
     }),
   );
