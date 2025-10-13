dbcde2ed3f0fd211a91fcfb9f7fbaea619da0961
Rich Burdon <rich.burdon@gmail.com>
2025-10-13 15:11:33 -0400

    feat: implement basic code evaluation in TypescriptEditor with error handling (#9993)
    Co-authored-by: Josiah Witt <josiah@witt.life>

--- packages/ui/react-ui-editor/src/extensions/factories.ts
+++ packages/ui/react-ui-editor/src/extensions/factories.ts
@@ -19,7 +19,7 @@ import {
   placeholder,
   scrollPastEnd,
 } from '@codemirror/view';
-import { vscodeDarkInit, vscodeLightInit } from '@uiw/codemirror-theme-vscode';
+import { vscodeDarkInit, vscodeDarkStyle, vscodeLightInit, vscodeLightStyle } from '@uiw/codemirror-theme-vscode';
 import defaultsDeep from 'lodash.defaultsdeep';
 import merge from 'lodash.merge';
 
@@ -208,6 +208,11 @@ const semanticTokensSettings = {
   },
 };
 
+export const defaultStyles = {
+  dark: vscodeDarkStyle,
+  light: vscodeLightStyle,
+};
+
 /**
  * https://codemirror.net/examples/styling
  */
@@ -223,7 +228,6 @@ export const createThemeExtensions = ({
     EditorView.baseTheme(styles ? merge({}, defaultTheme, styles) : defaultTheme),
     syntaxHighlightingProps && [
       themeMode === 'dark' ? vscodeDarkInit(semanticTokensSettings) : vscodeLightInit(semanticTokensSettings),
-      transparentBackground,
     ],
     slots.editor?.className && EditorView.editorAttributes.of({ class: slots.editor.className }),
     slots.content?.className && EditorView.contentAttributes.of({ class: slots.content.className }),
@@ -277,24 +281,3 @@ export const createDataExtensions = <T>({ id, text, space, identity }: DataExten
 
   return extensions;
 };
-
-const transparentBackground = EditorView.theme({
-  '&': {
-    backgroundColor: 'transparent !important',
-  },
-  '.cm-gutters': {
-    backgroundColor: 'transparent !important',
-  },
-  '.cm-activeLineGutter': {
-    backgroundColor: 'transparent !important',
-  },
-  '.cm-activeLine': {
-    backgroundColor: 'transparent !important',
-  },
-  '.cm-content': {
-    backgroundColor: 'transparent !important',
-  },
-  '.cm-scroller': {
-    backgroundColor: 'transparent !important',
-  },
-});
