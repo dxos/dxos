e3f74ff24c7893c055e9a701e920abad12340485
Dmytro Maretskyi <maretskii@gmail.com>
2025-10-14 11:36:59 +0200

    Extract AI service provider to a separate function

--- packages/core/ai/src/index.ts
+++ packages/core/ai/src/index.ts
@@ -5,7 +5,6 @@
 export * as AiParser from './AiParser';
 export * as AiPreprocessor from './AiPreprocessor';
 export * as AiService from './AiService';
-export * as AiServiceRouter from './AiServiceRouter';
 
 export * from './defs';
 export * from './errors';
