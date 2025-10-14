1644fe8e8a00115cae4897c0c11511b8e319d998
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-10-14 11:57:38 +0200

    Change rules

--- tools/eslint-plugin-rules/index.js
+++ tools/eslint-plugin-rules/index.js
@@ -3,9 +3,9 @@
 //
 
 import comment from './rules/comment.js';
+import effectSubpathImports from './rules/effect-subpath-imports.js';
 import header from './rules/header.js';
 import noEmptyPromiseCatch from './rules/no-empty-promise-catch.js';
-import enforceNamespaceImports from './rules/enforce-namespace-imports.js';
 import fs from 'node:fs';
 
 const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf8'));
@@ -18,9 +18,9 @@ const plugin = {
   },
   rules: {
     comment,
+    'effect-subpath-imports': effectSubpathImports,
     header,
     'no-empty-promise-catch': noEmptyPromiseCatch,
-    'enforce-namespace-imports': enforceNamespaceImports,
   },
   configs: {
     recommended: {
@@ -28,11 +28,9 @@ const plugin = {
         'dxos-plugin': null,
       },
       rules: {
+        'dxos-plugin/effect-subpath-imports': 'error',
         'dxos-plugin/header': 'error',
         'dxos-plugin/no-empty-promise-catch': 'error',
-        // Opt-in rule: enforce namespace imports for Effect packages
-        // Enable it repo-wide in root eslint config
-        'dxos-plugin/enforce-namespace-imports': ['error', { packages: ['effect', '@effect/*'] }],
         // TODO(dmaretskyi): Turned off due to large number of errors and no auto-fix.
         // 'dxos-plugin/comment': 'error',
       },
