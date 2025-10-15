aaa823278e69c78508242bf2a79b21027a521a64
Josiah Witt <josiah@witt.life>
2025-10-15 10:20:48 -0400

    refactor: graph view (#10009)
    Co-authored-by: Dmytro Maretskyi <maretskii@gmail.com>
    Co-authored-by: Dmytro Maretskyi <35851437+dmaretskyi@users.noreply.github.com>
    Co-authored-by: Will Shown <thure@users.noreply.github.com>
    Co-authored-by: Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>

--- packages/plugins/plugin-explorer/src/capabilities/react-surface.tsx
+++ packages/plugins/plugin-explorer/src/capabilities/react-surface.tsx
@@ -6,10 +6,11 @@ import React from 'react';
 
 import { Capabilities, contributes, createSurface } from '@dxos/app-framework';
 import { Obj } from '@dxos/echo';
+import { DataType } from '@dxos/schema';
 
 import { ExplorerContainer } from '../components';
 import { meta } from '../meta';
-import { ViewType } from '../types';
+import { Graph } from '../types';
 
 export default () =>
   contributes(
@@ -17,7 +18,8 @@ export default () =>
     createSurface({
       id: `${meta.id}/article`,
       role: ['article', 'section'],
-      filter: (data): data is { subject: ViewType } => Obj.instanceOf(ViewType, data.subject),
+      filter: (data): data is { subject: DataType.View } =>
+        Obj.instanceOf(DataType.View, data.subject) && Obj.instanceOf(Graph.Graph, data.subject.presentation.target),
       component: ({ data, role }) => <ExplorerContainer view={data.subject} role={role} />,
     }),
   );
