560c834120555390bfd2b69f46eebe5e8dbf8a29
Will Shown <thure@users.noreply.github.com>
2025-10-18 10:44:47 -0700

    feat: Plank encapsulation (#10027)
    Co-authored-by: Rich Burdon <rich.burdon@gmail.com>

--- packages/plugins/plugin-deck/src/components/Plank/Plank.tsx
+++ packages/plugins/plugin-deck/src/components/Plank/Plank.tsx
@@ -68,7 +68,11 @@ export const Plank = memo(({ id = UNKNOWN_ID, companionId, ...props }: PlankProp
   const hasCompanion = !!(companionId && currentCompanion);
 
   return (
-    <PlankContainer solo={props.part === 'solo'} companion={hasCompanion}>
+    <PlankContainer
+      solo={props.part === 'solo'}
+      companion={hasCompanion}
+      encapsulate={!!props.settings?.encapsulatedPlanks}
+    >
       <PlankComponent
         id={id}
         node={node}
@@ -92,7 +96,12 @@ export const Plank = memo(({ id = UNKNOWN_ID, companionId, ...props }: PlankProp
   );
 });
 
-const PlankContainer = ({ children, solo, companion }: PropsWithChildren<{ solo: boolean; companion: boolean }>) => {
+const PlankContainer = ({
+  children,
+  solo,
+  companion,
+  encapsulate,
+}: PropsWithChildren<{ solo: boolean; companion: boolean; encapsulate: boolean }>) => {
   const sizeAttrs = useMainSize();
   if (!solo) {
     return children;
@@ -102,7 +111,13 @@ const PlankContainer = ({ children, solo, companion }: PropsWithChildren<{ solo:
   return (
     <div
       role='none'
-      className={mx('absolute inset-0 grid', companion && 'grid-cols-[6fr_4fr]', railGridHorizontal, mainIntrinsicSize)}
+      className={mx(
+        'absolute inset-[--main-spacing] grid',
+        encapsulate && 'border border-separator rounded overflow-hidden',
+        companion && 'grid-cols-[6fr_4fr]',
+        railGridHorizontal,
+        mainIntrinsicSize,
+      )}
       {...sizeAttrs}
     >
       {children}
@@ -210,6 +225,9 @@ const PlankComponent = memo(
       part === 'deck' && (companioned === 'companion' ? '!border-separator border-ie' : '!border-separator border-li'),
       part.startsWith('solo-') && 'row-span-2 grid-rows-subgrid min-is-0',
       part === 'solo-companion' && '!border-separator border-is',
+      settings?.encapsulatedPlanks &&
+        !part.startsWith('solo') &&
+        'mli-[--main-spacing] !border-separator border rounded overflow-hidden',
     );
 
     return (
