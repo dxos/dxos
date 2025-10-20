5d6485c3bead09321b6944d308ec8eb1feff32f6
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-10-20 13:32:38 +0200

    Add def

---
+++ packages/core/mesh/messaging/src/testing/defs.ts
@@ -0,0 +1,5 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+export const TEST_EDGE_SERVER_URL = 'wss://signal-main.dxos.workers.dev';
--- packages/core/mesh/messaging/src/testing/index.ts
+++ packages/core/mesh/messaging/src/testing/index.ts
@@ -2,6 +2,7 @@
 // Copyright 2024 DXOS.org
 //
 
+export * from './defs';
 export * from './test-builder';
 export * from './test-peer';
 export * from './test-messages';
