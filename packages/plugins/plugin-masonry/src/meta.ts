f2b42137850cd67f2a5c20224ef95d263518defb
thure <will@braneframe.com>
2025-10-21 15:15:54 -0400

    Add plugin to Composer.

--- packages/plugins/plugin-masonry/src/meta.ts
+++ packages/plugins/plugin-masonry/src/meta.ts
@@ -8,7 +8,7 @@ export const meta: PluginMeta = {
   id: 'dxos.org/plugin/masonry',
   name: 'Masonry',
   description: 'Masonry displays items from a View query in a responsive grid.',
-  icon: 'ph--columns--regular',
+  icon: 'ph--wall--regular',
   source: 'https://github.com/dxos/dxos/tree/main/packages/plugins/plugin-masonry',
   screenshots: [],
 };
