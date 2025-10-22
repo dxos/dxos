eccd3dd3c55c1d8760f06f3a9a3c8a308101e7e0
Rich Burdon <rich.burdon@gmail.com>
2025-10-22 14:50:48 -0400

    chore: Dry out cards format and actions (#10047)

--- packages/plugins/plugin-preview/src/types.ts
+++ packages/plugins/plugin-preview/src/types.ts
@@ -2,13 +2,14 @@
 // Copyright 2025 DXOS.org
 //
 
-import type * as Schema from 'effect/Schema';
 import { type PropsWithChildren } from 'react';
 
+import { type Obj } from '@dxos/echo';
 import { type Space } from '@dxos/react-client/echo';
 
-export type PreviewProps<T extends Schema.Schema.Type<any>> = PropsWithChildren<{
+export type PreviewProps<T extends Obj.Any = Obj.Any> = PropsWithChildren<{
   subject: T;
   role?: string;
   activeSpace?: Space;
+  onSelect?: (obj: Obj.Any) => void;
 }>;
