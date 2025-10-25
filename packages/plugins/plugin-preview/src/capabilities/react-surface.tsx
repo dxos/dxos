846b20670b10343aca2d5dbe91281917c3aca571
Josiah Witt <josiah@witt.life>
2025-10-25 17:02:51 -0400

    feat: replace form in 1-up view (#10075)

--- packages/plugins/plugin-preview/src/capabilities/react-surface.tsx
+++ packages/plugins/plugin-preview/src/capabilities/react-surface.tsx
@@ -130,4 +130,20 @@ export default () =>
         );
       },
     }),
+
+    createSurface({
+      id: `${meta.id}/section`,
+      role: ['section'],
+      position: 'fallback',
+      filter: (data): data is { subject: Obj.Any } => Obj.isObject(data.subject),
+      component: ({ data }) => {
+        return (
+          <div role='none' className='flex justify-center'>
+            <div role='none' className='is-96'>
+              <Surface role='card' data={data} limit={1} />
+            </div>
+          </div>
+        );
+      },
+    }),
   ]);
