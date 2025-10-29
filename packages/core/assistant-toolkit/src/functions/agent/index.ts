9f0e635fc9a8095299d51976e63401f11beb095e
Rich Burdon <rich.burdon@gmail.com>
2025-10-29 01:34:39 -0400

    refactor: add type annotation for Agent.prompt function
    - Added explicit FunctionDefinition.Any type to Agent.prompt export to resolve TypeScript error
    - Imported FunctionDefinition type from @dxos/functions package
    - Added TODO comment indicating this is a temporary type fix

--- packages/core/assistant-toolkit/src/functions/agent/index.ts
+++ packages/core/assistant-toolkit/src/functions/agent/index.ts
@@ -2,8 +2,10 @@
 // Copyright 2025 DXOS.org
 //
 
+import { type FunctionDefinition } from '@dxos/functions';
+
 import { default as prompt$ } from './prompt';
 
 export namespace Agent {
-  export const prompt = prompt$;
+  export const prompt: FunctionDefinition.Any = prompt$; // TODO(burdon): Temp fix for TS error.
 }
