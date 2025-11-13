1face754d490aa3688f0e0f64ac4282cec27d171
Rich Burdon <rich.burdon@gmail.com>
2025-11-13 18:49:08 -0500

    feat: add image thumbnail context menu action to Chrome extension
    - Add context menu item for images with "Create Thumbnail..." action
    - Implement image fetch and upload to thumbnail service via background worker
    - Display thumbnail URL in popup with copy-to-clipboard functionality
    - Add storage for passing thumbnail URL from background to popup
    - Add contextMenus, storage, and host_permissions to manifest
    - Include new icons (clipboard, carets, dot) in icon sprite
    - Update README with bundling

---
+++ packages/apps/composer-crx/src/defs.ts
@@ -0,0 +1,7 @@
+//
+// Copyright 2024 DXOS.org
+//
+
+export const HOME_URL = 'https://labs.composer.space';
+
+export const THUMBNAIL_PROP = 'thumbnail-url';
--- packages/apps/composer-crx/src/popup.tsx
+++ packages/apps/composer-crx/src/popup.tsx
@@ -4,16 +4,32 @@
 
 import '@dxos-theme';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { sendMessage } from 'webext-bridge/popup';
 import browser from 'webextension-polyfill';
 
 import { log } from '@dxos/log';
+import { Input, Toolbar } from '@dxos/react-ui';
 
 import { Container, Popup, type PopupProps } from './components';
+import { HOME_URL, THUMBNAIL_PROP } from './defs';
 
 const Root = () => {
+  const [thumbnailUrl, setThumbnailUrl] = useState<string | null>(null);
+
+  // Load thumbnail URL from storage when popup opens then clear it.
+  useEffect(() => {
+    void (async () => {
+      const result = await browser.storage.local.get(THUMBNAIL_PROP);
+      const thumbnailUrl = result?.[THUMBNAIL_PROP] as string;
+      if (thumbnailUrl) {
+        setThumbnailUrl(thumbnailUrl);
+        await browser.storage.local.remove(THUMBNAIL_PROP);
+      }
+    })();
+  }, []);
+
   const handleAdd: PopupProps['onAdd'] = async () => {
     log.info('sending...');
     const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
@@ -39,12 +55,34 @@ const Root = () => {
   };
 
   const handleLaunch: PopupProps['onLaunch'] = async () => {
-    window.open('https://labs.composer.space');
+    window.open(HOME_URL);
   };
 
   return (
-    <Container classNames='is-[300px]'>
-      <Popup onAdd={handleAdd} onSearch={handleSearch} onLaunch={handleLaunch} />
+    <Container classNames='is-[300px] p-2'>
+      {!thumbnailUrl && <Popup onAdd={handleAdd} onSearch={handleSearch} onLaunch={handleLaunch} />}
+      {thumbnailUrl && (
+        <div className='flex flex-col gap-2 is-full'>
+          <Toolbar.Root>
+            <Input.Root>
+              <Input.TextInput disabled value={thumbnailUrl} />
+            </Input.Root>
+            <Toolbar.IconButton
+              icon='ph--clipboard--regular'
+              iconOnly
+              label='Clipboard'
+              onClick={async () => {
+                if (thumbnailUrl) {
+                  await navigator.clipboard.writeText(thumbnailUrl);
+                }
+              }}
+            />
+          </Toolbar.Root>
+          <div className='flex justify-center'>
+            <img src={thumbnailUrl} alt='Thumbnail' />
+          </div>
+        </div>
+      )}
     </Container>
   );
 };
