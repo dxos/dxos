71472b9c8641d6e79405f760b0c90be7da3d91f8
Rich Burdon <rich.burdon@gmail.com>
2025-11-13 20:14:14 -0500

    refactor: add explicit pnpm setup to Tauri publish workflow
    - Add pnpm setup step before moon toolchain to avoid OOM issues
    - Remove trailing whitespace in workflow file
    - Add TODO comment explaining pnpm setup workaround

--- packages/plugins/plugin-inbox/src/components/Calendar/EventtList.tsx
+++ packages/plugins/plugin-inbox/src/components/Calendar/EventtList.tsx
@@ -5,7 +5,7 @@
 import React from 'react';
 
 import { List, ListItem } from '@dxos/react-ui';
-import { attentionSurface, ghostHover, mx } from '@dxos/react-ui-theme';
+import { mx } from '@dxos/react-ui-theme';
 import { type Event } from '@dxos/types';
 
 export type EventListProps = {
@@ -16,16 +16,20 @@ export type EventListProps = {
 
 export const EventList = ({ events = [], selected, onSelect }: EventListProps) => {
   return (
-    <div className={mx('flex is-full overflow-y-scroll', attentionSurface)}>
+    <div className={mx('flex is-full overflow-y-scroll')}>
       <List classNames={mx('is-full divide-y divide-separator')}>
         {events.map((event) => (
           <ListItem.Root
             key={event.id}
             // TODO(burdon): Change bg-activeSurface to use data-active
-            classNames={mx('flex flex-col cursor-pointer', ghostHover, selected === event.id && 'bg-activeSurface')}
+            classNames={mx(
+              'flex flex-col cursor-pointer hover:bg-hoverSurface',
+              selected === event.id && 'bg-activeSurface',
+            )}
             onClick={() => onSelect?.(event)}
           >
             {event.name && <ListItem.Heading classNames='p-2'>{event.name}</ListItem.Heading>}
+            {/* TODO(burdon): Link to contact records. */}
             <div className='flex flex-col p-2'>
               {event.attendees.map(({ email }) => (
                 <div key='value' className='text-sm text-neutral-500'>
