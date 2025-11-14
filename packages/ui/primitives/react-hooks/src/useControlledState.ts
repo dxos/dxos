7fcc7f7702b4d0b358c31c0b803f722875f6f128
Rich Burdon <rich.burdon@gmail.com>
2025-11-14 00:11:23 -0500

    refactor: Support functional updates in useControlledState setValue

--- packages/ui/primitives/react-hooks/src/useControlledState.ts
+++ packages/ui/primitives/react-hooks/src/useControlledState.ts
@@ -4,13 +4,15 @@
 
 import { type Dispatch, type SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
 
+import { useDynamicRef } from './useDynamicRef';
+
 /**
  * A stateful hook with a controlled value.
  * @deprecated Use Radix `useControllableState` (NOTE: `useControlledState` is not compatible with `useControllableState`)
  */
 export const useControlledState = <T>(
   valueParam: T,
-  onChange?: Dispatch<SetStateAction<T>>,
+  onChange?: (value: T) => void,
 ): [T, Dispatch<SetStateAction<T>>] => {
   const [value, setControlledValue] = useState(valueParam);
   useEffect(() => {
@@ -18,13 +20,19 @@ export const useControlledState = <T>(
   }, [valueParam]);
 
   const onChangeRef = useRef(onChange);
+  const valueRef = useDynamicRef(valueParam);
   const setValue = useCallback<Dispatch<SetStateAction<T>>>(
-    (value) => {
+    (nextValue) => {
+      const value = isFunction(nextValue) ? nextValue(valueRef.current) : nextValue;
       setControlledValue(value);
       onChangeRef.current?.(value);
     },
-    [onChangeRef],
+    [valueRef, onChangeRef],
   );
 
   return [value, setValue];
 };
+
+function isFunction(value: unknown): value is (...args: any[]) => any {
+  return typeof value === 'function';
+}
