57e3f0f62c31250656d510b9bc69bf6565b75c0f
Rich Burdon <rich.burdon@gmail.com>
2025-11-14 00:23:17 -0500

    fix: Fix useControllableState bug (#10184)

--- packages/ui/primitives/react-hooks/src/useControlledState.ts
+++ packages/ui/primitives/react-hooks/src/useControlledState.ts
@@ -2,16 +2,37 @@
 // Copyright 2023 DXOS.org
 //
 
-import { useControllableState } from '@radix-ui/react-use-controllable-state';
-import { type Dispatch, type SetStateAction } from 'react';
+import { type Dispatch, type SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
+
+import { useDynamicRef } from './useDynamicRef';
 
 /**
  * A stateful hook with a controlled value.
- * @deprecated Use Radix `useControllableState`.
+ * @deprecated Use Radix `useControllableState` (NOTE: `useControlledState` is not compatible with `useControllableState`)
  */
-// TODO(burdon): Remove and require default.
 export const useControlledState = <T>(
-  controlledValue: T,
+  valueParam: T,
   onChange?: (value: T) => void,
-): [T, Dispatch<SetStateAction<T>>] =>
-  useControllableState<T>({ prop: controlledValue, defaultProp: undefined as unknown as T, onChange });
+): [T, Dispatch<SetStateAction<T>>] => {
+  const [value, setControlledValue] = useState(valueParam);
+  useEffect(() => {
+    setControlledValue(valueParam);
+  }, [valueParam]);
+
+  const onChangeRef = useRef(onChange);
+  const valueRef = useDynamicRef(valueParam);
+  const setValue = useCallback<Dispatch<SetStateAction<T>>>(
+    (nextValue) => {
+      const value = isFunction(nextValue) ? nextValue(valueRef.current) : nextValue;
+      setControlledValue(value);
+      onChangeRef.current?.(value);
+    },
+    [valueRef, onChangeRef],
+  );
+
+  return [value, setValue];
+};
+
+function isFunction(value: unknown): value is (...args: any[]) => any {
+  return typeof value === 'function';
+}
