c65f28fe57f9f6597a0da4b968eefaf7211307ce
Rich Burdon <rich.burdon@gmail.com>
2025-11-13 20:01:02 -0500

    feat: Image upload menu for chrome extension (#10181)

--- packages/plugins/plugin-search/src/components/SearchMain.tsx
+++ packages/plugins/plugin-search/src/components/SearchMain.tsx
@@ -4,11 +4,10 @@
 
 import React, { useState } from 'react';
 
-import { log } from '@dxos/log';
 import { useClient } from '@dxos/react-client';
 import { Filter, type Space, useQuery } from '@dxos/react-client/echo';
 import { useTranslation } from '@dxos/react-ui';
-import { StackItem } from '@dxos/react-ui-stack';
+import { Stack, StackItem } from '@dxos/react-ui-stack';
 import { activeSurface, mx } from '@dxos/react-ui-theme';
 
 import { useGlobalSearch, useGlobalSearchResults, useWebSearch } from '../hooks';
@@ -17,7 +16,7 @@ import { meta } from '../meta';
 import { Searchbar } from './Searchbar';
 import { SearchResults } from './SearchResults';
 
-export const SearchMain = ({ space }: { space: Space }) => {
+export const SearchMain = ({ space }: { space?: Space }) => {
   const { t } = useTranslation(meta.id);
   const client = useClient();
   const { setMatch } = useGlobalSearch();
@@ -27,7 +26,7 @@ export const SearchMain = ({ space }: { space: Space }) => {
   const objects = useQuery(allSpaces ? client.spaces : space, Filter.everything());
   const results = useGlobalSearchResults(objects);
 
-  const { runSearch, results: webResults, isLoading } = useWebSearch({ query });
+  const { runSearch, results: webResults } = useWebSearch({ query });
 
   // TODO(burdon): Activate and/or filter current main (set context).
   const [selected, setSelected] = useState<string>();
@@ -36,25 +35,28 @@ export const SearchMain = ({ space }: { space: Space }) => {
   };
 
   const allResults = [...results, ...webResults];
-  log('results', { results: results.length, webResults: webResults.length });
 
   return (
-    <StackItem.Content toolbar>
-      <Searchbar
-        classNames='pli-2'
-        placeholder={t('search placeholder')}
-        onChange={(text) => {
-          setQuery(text);
-          setMatch?.(text);
-        }}
-        onSubmit={runSearch}
-      />
-      {isLoading && <div className={mx('flex flex-col grow overflow-hidden', activeSurface)}>Loading...</div>}
-      {allResults.length > 0 && (
-        <div className={mx('flex flex-col grow overflow-hidden', activeSurface)}>
-          <SearchResults items={allResults} selected={selected} onSelect={handleSelect} />
-        </div>
-      )}
-    </StackItem.Content>
+    <Stack orientation='vertical' size='contain' itemsCount={1} rail={false} classNames={['bs-full', activeSurface]}>
+      <StackItem.Root item={{ id: 'search' }} role='article'>
+        <StackItem.Content toolbar>
+          <Searchbar
+            classNames='pli-2'
+            placeholder={t('search placeholder')}
+            delay={300}
+            onChange={(text) => {
+              setQuery(text);
+              setMatch?.(text);
+            }}
+            onSubmit={runSearch}
+          />
+          {allResults.length > 0 && (
+            <div className={mx('flex flex-col bs-full overflow-hidden')}>
+              <SearchResults items={allResults} selected={selected} onSelect={handleSelect} />
+            </div>
+          )}
+        </StackItem.Content>
+      </StackItem.Root>
+    </Stack>
   );
 };
