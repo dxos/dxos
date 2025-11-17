efad865c6c97989890e19722549c40c65f333f3e
Dmytro Maretskyi <maretskii@gmail.com>
2025-11-17 11:53:13 +0100

    refactor: consolidate function SDK into dedicated module
    - Moved handler.ts functionality into sdk module for better organization
    - Relocated URL utilities to types directory to group related type definitions
    - Updated exports to reflect new module structure

--- packages/core/functions/src/index.ts
+++ packages/core/functions/src/index.ts
@@ -4,7 +4,6 @@
 
 export * from './errors';
 export * from './example';
-export * from './handler';
+export * from './sdk';
 export * from './services';
 export * from './types';
-export * from './url';
