431e8dd9b8c83013d0c751ddb7c9c3fc90b85dde
Josiah Witt <josiah@witt.life>
2025-11-21 16:21:29 -0500

    feat: link contacts by email in event header

---
+++ packages/plugins/plugin-inbox/src/components/EventArticle/EventAttendee.tsx
@@ -0,0 +1,29 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import React, { useCallback } from 'react';
+
+import { type Space } from '@dxos/react-client/echo';
+import { type Actor } from '@dxos/types';
+
+import { useActorContact } from '../../hooks';
+import { UserIconButton } from '../common';
+
+export type EventAttendeeProps = {
+  attendee: Actor.Actor;
+  space?: Space;
+  onContactCreate?: (actor: Actor.Actor) => void;
+};
+
+export const EventAttendee = ({ attendee, space, onContactCreate }: EventAttendeeProps) => {
+  const contactDxn = useActorContact(space, attendee);
+  const handleContactCreate = useCallback(() => onContactCreate?.(attendee), [attendee]);
+
+  return (
+    <div role='none' className='grid grid-cols-[2rem_1fr] gap-1 items-center'>
+      <UserIconButton value={contactDxn.value} onContactCreate={handleContactCreate} />
+      <h3 className='truncate text-primaryText'>{attendee.name || attendee.email}</h3>
+    </div>
+  );
+};
