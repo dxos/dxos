4de18b415beed1f9d273b952eed5fee75a42f414
Dmytro Maretskyi <maretskii@gmail.com>
2025-11-26 13:44:10 +0100

    Add bp

--- packages/core/assistant-toolkit/src/experimental/code-execution/code-execution.ts
+++ packages/core/assistant-toolkit/src/experimental/code-execution/code-execution.ts
@@ -1,12 +1,14 @@
-import * as Toolkit from '@effect/ai/Toolkit';
-import * as Tool from '@effect/ai/Tool';
-import { trim } from '@dxos/util';
-import * as Schema from 'effect/Schema';
-import { Effect } from 'effect';
-import * as Layer from 'effect/Layer';
+import { ToolId } from '@dxos/ai';
+import { Blueprint } from '@dxos/blueprints';
+import { Filter, Obj, Query, Ref } from '@dxos/echo';
 import { DatabaseService } from '@dxos/echo-db';
 import { Organization, Person, Task } from '@dxos/types';
-import { Filter, Obj, Query, Ref } from '@dxos/echo';
+import { trim } from '@dxos/util';
+import * as Tool from '@effect/ai/Tool';
+import * as Toolkit from '@effect/ai/Toolkit';
+import { Effect } from 'effect';
+import * as Layer from 'effect/Layer';
+import * as Schema from 'effect/Schema';
 
 /**
  * Toolkit for executing JavaScript code with access to the database and core types.
@@ -25,11 +27,11 @@ export class CodeExecutionToolkit extends Toolkit.make(
         return a + b;
       </example>
 
-      You have access to a datbase via the "db" variable. You can query, create and update objects.
+      You have access to a database via the "db" variable. You can query, create and update objects.
 
       <example>
         const { objects } = await db.query(Query.type(Person)).run();
-        return object.map(obj => ({ id: obj.id, name: obj.name }))
+        return objects.map(obj => ({ id: obj.id, name: obj.name }))
       </example>
 
       <example>
@@ -61,6 +63,7 @@ export class CodeExecutionToolkit extends Toolkit.make(
         description: 'Valid JavaScript to execute in the form of a function body.',
       }),
     },
+    dependencies: [DatabaseService],
   }),
 ) {}
 
@@ -68,35 +71,37 @@ export class CodeExecutionToolkit extends Toolkit.make(
  * Code execution tool implemented using unsafe eval (new Function).
  * WARNING: This is a security risk and should only be used in controlled environments.
  */
-export const EvalCodeExecutionLayer = Layer.unwrapEffect(
-  Effect.gen(function* () {
+export const EvalCodeExecutionLayer = CodeExecutionToolkit.toLayer({
+  code_sandbox: Effect.fnUntraced(function* ({ code }) {
     const { db } = yield* DatabaseService;
-    return CodeExecutionToolkit.toLayer({
-      code_sandbox: Effect.fnUntraced(function* ({ code }) {
-        if (!code.includes('return')) {
-          throw new Error('Code must contain a return statement');
-        }
 
-        console.log(`Executing:\n\n${code}`);
+    if (!code.includes('return')) {
+      throw new Error('Code must contain a return statement');
+    }
 
-        const context = {
-          Query,
-          Obj,
-          Filter,
-          Ref,
-          db,
-          Person: Person.Person,
-          Organization: Organization.Organization,
-          Task: Task.Task,
-        };
+    console.log(`Executing:\n\n${code}`);
+    const context = {
+      Query,
+      Obj,
+      Filter,
+      Ref,
+      db,
+      Person: Person.Person,
+      Organization: Organization.Organization,
+      Task: Task.Task,
+    };
+    const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+    const func = new AsyncFunction(...Object.keys(context), code);
+    const result = yield* Effect.promise(() => func(...Object.values(context)));
+    console.log(`Result: ${JSON.stringify(result, null, 2)}`);
 
-        const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
-        const func = new AsyncFunction(...Object.keys(context), code);
-        const result = yield* Effect.promise(() => func(...Object.values(context)));
-        console.log(`Result: ${JSON.stringify(result, null, 2)}`);
-
-        return result;
-      }),
-    });
+    return result;
   }),
-);
+});
+
+export const CodeExecutionBlueprint = Blueprint.make({
+  key: 'dxos.org/blueprint/code-execution',
+  name: 'Code Execution',
+  description: 'Execute JavaScript code with access to the database and core types.',
+  tools: [ToolId.make('code_sandbox')],
+});
