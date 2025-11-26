111470db31d0f62956d6f9cffcaacc2510036b5b
Dmytro Maretskyi <maretskii@gmail.com>
2025-11-26 12:08:01 +0100

    Refactor protocol defnitions

--- packages/core/protocols/src/edge/EdgeFunctionEnv.ts
+++ packages/core/protocols/src/edge/EdgeFunctionEnv.ts
@@ -2,10 +2,6 @@
 // Copyright 2025 DXOS.org
 //
 
-//
-// Copyright 2024 DXOS.org
-//
-
 import { type SpaceId } from '@dxos/keys';
 
 import { type QueryResult, type QueueQuery } from '../queue';
@@ -15,27 +11,81 @@ import { type QueryResult, type QueueQuery } from '../queue';
 API for the edge functions service.
 A function module should be a valid cloudflare worker module.
 
-TODO(dmaretskyi): Describe HTTP api.
+# Fetching metadata (done on deploy)
+
+GET http://functions.dxos.internal/
+X-DXOS-Function-Route: meta
+
+Expected to return `FunctionMetadata` in JSON:
+
+{
+  key: string;
+  name?: string;
+  description?: string;
+  inputSchema?: JsonSchemaType;
+  outputSchema?: JsonSchemaType;
+}
+
+# Invoking the function
+
+POST http://functions.dxos.internal/
+Content-Type: application/json
+X-Trace-Queue-Dxn: dxn:queue:trace:AAAAAA:BBBBBB
+X-Invocation-Id: XXXXXXX
+X-Edge-Env: production
+
+{input_data}
+
+Expected to return `EdgeEnvelope` with the output data:
+
+{
+  success: true,
+  data: unknown
+}
+
+On Error returns `EdgeEnvelope` with error:
+
+{
+  success: false,
+  message: string
+  error: EncodedError
+}
+
 */
+
+/**
+ * Environment available to the function running on Cloudflare.
+ */
+export interface Env {
+  QUEUE_SERVICE: QueueService;
+  DATA_SERVICE: DataService;
+}
+
+/**
+ * Use to trace the execution across multiple services.
+ *
+ * NOTE: Currently unused in functions.
+ */
+interface ExecutionContext {}
 
 /**
  * Database API for other CF services like functions.
  */
 export interface DataService {
-  getSpaceMeta(ctx: unknown, spaceId: SpaceId): Promise<SpaceMeta | undefined>;
-  getDocument(ctx: unknown, spaceId: SpaceId, documentId: string): Promise<RawDocument | undefined>;
+  getSpaceMeta(ctx: ExecutionContext, spaceId: SpaceId): Promise<SpaceMeta | undefined>;
+  getDocument(ctx: ExecutionContext, spaceId: SpaceId, documentId: string): Promise<RawDocument | undefined>;
 
-  query(ctx: unknown, request: QueryRequest): Promise<QueryResponse>;
-  queryDocuments(ctx: unknown, request: QueryRequest): Promise<QueryDocumentsResponse>;
-  queryReferences(ctx: unknown, request: QueryReferencesRequest): Promise<QueryReferencesResponse>;
+  query(ctx: ExecutionContext, request: QueryRequest): Promise<QueryResponse>;
+  queryDocuments(ctx: ExecutionContext, request: QueryRequest): Promise<QueryDocumentsResponse>;
+  queryReferences(ctx: ExecutionContext, request: QueryReferencesRequest): Promise<QueryReferencesResponse>;
 
   // TODO(burdon): Update? Return DocumentEntry?
-  changeDocument(ctx: unknown, spaceId: SpaceId, documentId: string, changes: Uint8Array): Promise<void>;
+  changeDocument(ctx: ExecutionContext, spaceId: SpaceId, documentId: string, changes: Uint8Array): Promise<void>;
 }
 
 export interface QueueService {
-  query(ctx: unknown, queueDXN: string, query: Omit<QueueQuery, 'queueId'>): Promise<QueryResult>;
-  append(ctx: unknown, queueDXN: string, objects: unknown[]): Promise<void>;
+  query(ctx: ExecutionContext, queueDXN: string, query: Omit<QueueQuery, 'queueId'>): Promise<QueryResult>;
+  append(ctx: ExecutionContext, queueDXN: string, objects: unknown[]): Promise<void>;
 }
 
 export type ObjectDocumentJson = {
@@ -73,8 +123,8 @@ export type ObjectSnapshot = {
   type?: string;
   documentId: string;
   objectId: string;
-  // TODO(mykola): Use ObjectStructure.
-  object: any;
+  // TODO(mykola): Use ObjectStructure from @dxos/echo-protocol.
+  object: unknown;
 };
 
 export type ObjectReference = {
@@ -113,11 +163,3 @@ export type CreateDocumentResponse = {
   /** Automerge document ID. */
   documentId: string;
 };
-
-/**
- * Environment variable available to Cloudflare functions.
- */
-export interface Env {
-  QUEUE_SERVICE: QueueService;
-  DATA_SERVICE: DataService;
-}
