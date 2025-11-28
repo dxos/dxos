ba74f2713f0fc15b78824bb811ba13015dbf59d1
Josiah Witt <josiah@witt.life>
2025-11-28 18:28:10 -0500

    revert query-lite changes

--- packages/core/echo/echo-query/src/query-lite/index.ts
+++ packages/core/echo/echo-query/src/query-lite/index.ts
@@ -2,10 +2,4 @@
 // Copyright 2025 DXOS.org
 //
 
-//
-// Light-weight implementation of query execution.
-//
-
-export * as Filter from './Filter';
-export * as Order from './Order';
-export * as Query from './Query';
+export * from './query-lite';
