1d5e2e88092fcf10aaeecd86a1e9316c69c8d243
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-12-04 16:23:58 +0100

    refactor: use FunctionsServiceClient for force running cron triggers
    Replace direct fetch call to edge service with `FunctionsServiceClient.forceRunCronTrigger()` method in AutomationPanel.

--- packages/plugins/plugin-automation/src/components/AutomationPanel/AutomationPanel.tsx
+++ packages/plugins/plugin-automation/src/components/AutomationPanel/AutomationPanel.tsx
@@ -10,6 +10,7 @@ import React, { useMemo, useState } from 'react';
 
 import { Filter, Obj, Tag } from '@dxos/echo';
 import { Function, Script, Trigger } from '@dxos/functions';
+import { FunctionsServiceClient } from '@dxos/functions-runtime/edge';
 import { useTypeOptions } from '@dxos/plugin-space';
 import { type Client, useClient } from '@dxos/react-client';
 import { type Space, getSpace, useQuery } from '@dxos/react-client/echo';
@@ -36,6 +37,7 @@ export type AutomationPanelProps = ThemedClassName<{
 export const AutomationPanel = ({ classNames, space, object, initialTrigger, onDone }: AutomationPanelProps) => {
   const { t } = useTranslation(meta.id);
   const client = useClient();
+  const functionsServiceClient = useMemo(() => FunctionsServiceClient.fromClient(client), [client]);
   const functions = useQuery(space, Filter.type(Function.Function));
   const triggers = useQuery(space, Filter.type(Trigger.Trigger));
   const filteredTriggers = useMemo(() => {
@@ -88,11 +90,7 @@ export const AutomationPanel = ({ classNames, space, object, initialTrigger, onD
   };
 
   const handleForceRunTrigger = async (trigger: Trigger.Trigger) => {
-    const edgeUrl = new URL(client.config.values.runtime!.services!.edge!.url!);
-    edgeUrl.pathname = `/test/functions/${space.id}/triggers/crons/${trigger.id}/run`;
-    await fetch(edgeUrl, {
-      method: 'POST',
-    });
+    await functionsServiceClient.forceRunCronTrigger(space.id, trigger.id);
   };
 
   if (trigger) {
