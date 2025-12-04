ab099d896c458764f94ba4f8d2433d7271b9a598
Dmytro Maretskyi <maretskii@gmail.com>
2025-12-04 22:40:35 +0100

    fixes

--- packages/core/functions-testing/src/functions/forex-effect.ts
+++ packages/core/functions-testing/src/functions/forex-effect.ts
@@ -10,6 +10,7 @@ import * as Schedule from 'effect/Schedule';
 import * as Schema from 'effect/Schema';
 
 import { defineFunction } from '@dxos/functions';
+import * as HttpClientResponse from '@effect/platform/HttpClientResponse';
 
 export default defineFunction({
   key: 'dxos.org/script/forex-effect',
@@ -23,16 +24,21 @@ export default defineFunction({
 
   outputSchema: Schema.String.annotations({ description: 'The exchange rate between the two currencies' }),
 
-  handler: async ({ data: { from, to } }: any) =>
-    Effect.gen(function* () {
-      const res = yield* HttpClientRequest.get(`https://free.ratesdb.com/v1/rates?from=${from}&to=${to}`).pipe(
-        HttpClient.execute,
-        Effect.flatMap((res: any) => res.json),
-        Effect.timeout('1 second'),
-        Effect.retry(Schedule.exponential(1_000).pipe(Schedule.compose(Schedule.recurs(3)))),
-        Effect.scoped,
-      );
+  handler: Effect.fnUntraced(function* ({ data: { from, to } }: any) {
+    const res = yield* HttpClientRequest.get(`https://free.ratesdb.com/v1/rates?from=${from}&to=${to}`).pipe(
+      HttpClient.execute,
+      Effect.flatMap(
+        HttpClientResponse.schemaBodyJson(
+          Schema.Struct({
+            data: Schema.Struct({ rates: Schema.Record({ key: Schema.String, value: Schema.Number }) }),
+          }),
+        ),
+      ),
+      Effect.timeout('1 second'),
+      Effect.retry(Schedule.exponential(1_000).pipe(Schedule.compose(Schedule.recurs(3)))),
+      Effect.scoped,
+    );
 
-      return (res as any).data.rates[to].toString();
-    }).pipe(Effect.provide(FetchHttpClient.layer)),
+    return res.data.rates[to].toString();
+  }, Effect.provide(FetchHttpClient.layer)),
 });
