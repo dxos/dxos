60f64194a389d48f75177766d392bae00aef31f5
Rich Burdon <rich.burdon@gmail.com>
2025-12-04 20:43:34 -0500

    chore: Refactor resolver to use Effect and improve type definitions
    - Update `Resolver` type to return `Effect.Effect` instead of `Promise` with error and context type parameters
    - Rename `ResolverDefinitions` to `ResolverDefinitionMap` for clarity
    - Add JSDoc comment explaining resolver purpose
    - Refactor `createContactResolver` to use Effect.gen for database queries
    - Add `Database.Service` as required context in contact resolver type
    - Move plaintext parser TODO comment from bottom of util.ts to more

--- packages/plugins/plugin-inbox/src/functions/resolver.ts
+++ packages/plugins/plugin-inbox/src/functions/resolver.ts
@@ -2,20 +2,28 @@
 // Copyright 2025 DXOS.org
 //
 
-import { type Person } from '@dxos/types';
+import * as Effect from 'effect/Effect';
 
-export type Resolver<Source, Target> = (source: Source) => Promise<Target | undefined>;
+import { Database, Filter, Query } from '@dxos/echo';
+import { Person } from '@dxos/types';
+
+/**
+ * Attempts to match the given source to a target object using the provided resolution logic.
+ */
+export type Resolver<Source, Target, E = never, R = never> = (
+  source: Source,
+) => Effect.Effect<Target | undefined, E, R>;
 
 /**
  * Defines the source and target types for each resolver kind.
  */
 export type ResolverDefinition = { source: unknown; target: unknown };
 
-export type ResolverDefinitions = Record<string, ResolverDefinition>;
+export type ResolverDefinitionMap = Record<string, ResolverDefinition>;
 
-export type ResolverKind<D extends ResolverDefinitions> = keyof D & string;
+export type ResolverKind<D extends ResolverDefinitionMap> = keyof D & string;
 
-export type ResolverMap<D extends ResolverDefinitions, K extends ResolverKind<D> = ResolverKind<D>> = {
+export type ResolverMap<D extends ResolverDefinitionMap, K extends ResolverKind<D> = ResolverKind<D>> = {
   [P in K]: Resolver<D[P]['source'], D[P]['target'] | undefined>;
 };
 
@@ -29,9 +37,8 @@ export type InboxResolverDefinitions = {
   contact: { source: HasEmail; target: Person.Person };
 };
 
-export const createContactResolver = async (): Resolver<HasEmail, Person.Person | undefined> => {
-  const contacts = await Database.Service.runQuery(Query.select(Filter.type(Person.Person)));
-  return async ({ email }) => {
+export const createContactResolver: Resolver<HasEmail, Person.Person, never, Database.Service> = ({ email }) =>
+  Effect.gen(function* () {
+    const contacts = yield* Database.Service.runQuery(Query.select(Filter.type(Person.Person)));
     return contacts.find((contact) => contact.emails?.some((e) => e.value === email));
-  };
-};
+  });
