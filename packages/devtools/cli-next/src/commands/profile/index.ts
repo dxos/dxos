769f7111571724dd2a750c7c6edaf4bfdbda255a
Josiah Witt <josiah@witt.life>
2025-12-11 10:00:00 -0500

    feat(cli-next): reset profile (#10284)

--- packages/devtools/cli-next/src/commands/profile/index.ts
+++ packages/devtools/cli-next/src/commands/profile/index.ts
@@ -6,8 +6,9 @@ import * as Command from '@effect/cli/Command';
 
 import { create } from './create';
 import { list } from './list';
+import { reset } from './reset';
 
 export const profile = Command.make('profile').pipe(
   Command.withDescription('Profile commands.'),
-  Command.withSubcommands([list, create]),
+  Command.withSubcommands([list, create, reset]),
 );
