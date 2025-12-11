274562252e2a272a77e6747f8f088ce47ad50710
Josiah Witt <josiah@witt.life>
2025-12-11 12:10:01 -0500

    add recover command

--- packages/devtools/cli-next/src/commands/halo/index.ts
+++ packages/devtools/cli-next/src/commands/halo/index.ts
@@ -7,8 +7,9 @@ import * as Command from '@effect/cli/Command';
 import { create } from './create';
 import { identity } from './identity';
 import { join } from './join';
+import { recover } from './recover';
 
 export const halo = Command.make('halo').pipe(
   Command.withDescription('Manage HALO identity.'),
-  Command.withSubcommands([create, identity, join]),
+  Command.withSubcommands([create, identity, join, recover]),
 );
