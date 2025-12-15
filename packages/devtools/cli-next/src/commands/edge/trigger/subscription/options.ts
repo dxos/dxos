a839d2223b6ba8278b4d3bc18214d4d15a96e5a4
Josiah Witt <josiah@witt.life>
2025-12-15 15:42:53 -0500

    feat: add subscription trigger CLI commands
    - Add create and update commands for subscription triggers
    - Support simple type-based queries via --typename option
    - Support optional --deep and --delay subscription options
    - Follows the same pattern as timer triggers

---
+++ packages/devtools/cli-next/src/commands/edge/trigger/subscription/options.ts
@@ -0,0 +1,17 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import * as Options from '@effect/cli/Options';
+
+export const Typename = Options.text('typename').pipe(
+  Options.withDescription('The type name to query for the subscription trigger.'),
+);
+
+export const Deep = Options.boolean('deep', { ifPresent: true }).pipe(
+  Options.withDescription('Watch changes to nested properties (not just creation).'),
+);
+
+export const Delay = Options.number('delay').pipe(
+  Options.withDescription('Debounce changes with a delay in milliseconds.'),
+);
---
+++ packages/devtools/cli-next/src/commands/edge/trigger/subscription/update.ts
@@ -0,0 +1,83 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import * as Command from '@effect/cli/Command';
+import * as Options from '@effect/cli/Options';
+import * as Console from 'effect/Console';
+import * as Effect from 'effect/Effect';
+import * as Option from 'effect/Option';
+
+import { DXN, Database, Filter, Obj, Query, Ref } from '@dxos/echo';
+import { Function, Trigger, getUserFunctionIdInMetadata } from '@dxos/functions';
+
+import { CommandConfig } from '../../../../services';
+import { spaceLayer, withTypes } from '../../../../util';
+import { Common } from '../../../options';
+import { Enabled, Input, TriggerId } from '../options';
+import { prettyPrintTrigger } from '../util';
+
+import { Deep, Delay, Typename } from './options';
+
+export const update = Command.make(
+  'update',
+  {
+    spaceId: Common.spaceId.pipe(Options.optional),
+    id: TriggerId,
+    enabled: Enabled,
+    functionId: Common.functionId.pipe(Options.optional),
+    typename: Typename.pipe(Options.optional),
+    deep: Deep.pipe(Options.optional),
+    delay: Delay.pipe(Options.optional),
+    input: Input.pipe(Options.optional),
+  },
+  ({ id, enabled, functionId, typename, deep, delay, input }) =>
+    Effect.gen(function* () {
+      const { json } = yield* CommandConfig;
+      const dxn = DXN.fromLocalObjectId(id);
+      const trigger = yield* Database.Service.resolve(dxn, Trigger.Trigger);
+      if (trigger.spec?.kind !== 'subscription') {
+        throw new Error('Trigger is not a subscription');
+      }
+
+      trigger.enabled = enabled;
+      if (Option.isSome(typename)) {
+        const queryAst = Query.select(Filter.type(typename.value)).ast;
+        trigger.spec.query = {
+          ast: queryAst,
+        };
+      }
+      if (Option.isSome(deep) || Option.isSome(delay)) {
+        if (!trigger.spec.options) {
+          trigger.spec.options = {};
+        }
+        if (Option.isSome(deep)) {
+          trigger.spec.options.deep = deep.value;
+        }
+        if (Option.isSome(delay)) {
+          trigger.spec.options.delay = delay.value;
+        }
+      }
+      if (Option.isSome(input)) {
+        trigger.input = input.value;
+      }
+      if (Option.isSome(functionId)) {
+        const functions = yield* Database.Service.runQuery(Filter.type(Function.Function));
+        const fn = functions.find((fn) => getUserFunctionIdInMetadata(Obj.getMeta(fn)) === functionId.value);
+        if (!fn) {
+          throw new Error(`Function not found: ${functionId.value}`);
+        }
+        trigger.function = Ref.make(fn);
+      }
+
+      if (json) {
+        yield* Console.log(JSON.stringify(trigger, null, 2));
+      } else {
+        yield* Console.log(yield* prettyPrintTrigger(trigger));
+      }
+    }),
+).pipe(
+  Command.withDescription('Update a subscription trigger.'),
+  Command.provide(({ spaceId }) => spaceLayer(spaceId, true)),
+  Command.provideEffectDiscard(() => withTypes(Function.Function, Trigger.Trigger)),
+);
