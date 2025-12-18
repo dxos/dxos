bc577b8a42afa3ed828870e0a28b99d7264a0992
Josiah Witt <josiah@witt.life>
2025-12-18 17:42:22 -0500

    feat(cli-next): queue triggers (#10332)

--- packages/sdk/schema/src/annotations/view.ts
+++ packages/sdk/schema/src/annotations/view.ts
@@ -4,6 +4,7 @@
 
 import { createAnnotationHelper } from '@dxos/echo/internal';
 
+// TODO(wittjosiah): This won't serialize into echo. Migrate to `Annotation.make` to store in `PropertyMeta`.
 /**
  * Identifies a schema as a view.
  */
