9f94973a3209b9a378e5fb9304da632de1d6e48f
Josiah Witt <josiah@witt.life>
2025-12-21 14:32:45 -0500

    lint

--- packages/plugins/plugin-map-solid/src/components/types.ts
+++ packages/plugins/plugin-map-solid/src/components/types.ts
@@ -2,13 +2,10 @@
 // Copyright 2025 DXOS.org
 //
 
-import { type Accessor } from 'solid-js';
-
-import { type GeoMarker, type LatLngLiteral, type MapMarkersProps } from '@dxos/solid-ui-geo';
+import { type LatLngLiteral, type MapMarkersProps } from '@dxos/solid-ui-geo';
 
 export type GeoControlProps = {
   center?: LatLngLiteral;
   zoom?: number;
   onToggle?: () => void;
 } & Pick<MapMarkersProps, 'markers'>;
-
