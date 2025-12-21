275c4a361676cf1adcdc6d1bc44b1c6556a698d2
Rich Burdon <rich.burdon@gmail.com>
2025-12-21 17:11:26 -0500

    solid exp.

---
+++ packages/ui/solid-ui/src/test.solid-stories.tsx
@@ -0,0 +1,14 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+// import { createJSXDecorator } from 'storybook-solidjs-vite';
+
+// export const solidDecorator = createJSXDecorator((Story, context) => {
+//   return (
+//     <main>
+//       <Story />
+//     </main>
+//   );
+// });
+export default {};
