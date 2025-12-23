0b38c1057a262f703f21ebfcbb228597ee6dd17b
Rich Burdon <rich.burdon@gmail.com>
2025-12-23 20:59:23 +0000

    style: fix prettier formatting in dom.ts (single quotes and spaces)

--- packages/ui/ui/src/dom.ts
+++ packages/ui/ui/src/dom.ts
@@ -2,29 +2,29 @@
 // Copyright 2024 DXOS.org
 //
 
-import $ from "cash-dom";
-import type { Cash } from "cash-dom";
+import $ from 'cash-dom';
+import type { Cash } from 'cash-dom';
 
 export type { Cash };
 
 // From icon-plugin.
-const ICONS_URL = "/icons.svg";
+const ICONS_URL = '/icons.svg';
 
-export const icon = (icon: string) => ICONS_URL + "#" + icon;
+export const icon = (icon: string) => ICONS_URL + '#' + icon;
 
 /**
  * Creates an SVG element wrapped in cash-dom.
  * @param tag SVG element tag name (e.g., 'svg', 'use', 'path')
  */
 const svg = (tag: string): Cash => {
-	return $(document.createElementNS("http://www.w3.org/2000/svg", tag));
+  return $(document.createElementNS('http://www.w3.org/2000/svg', tag));
 };
 
 // Augment cash-dom module.
-declare module "cash-dom" {
-	interface CashStatic {
-		svg(tag: string): Cash;
-	}
+declare module 'cash-dom' {
+  interface CashStatic {
+    svg(tag: string): Cash;
+  }
 }
 
 // Extend cash-dom with svg helper.
