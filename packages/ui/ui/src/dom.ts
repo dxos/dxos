1e967b3a07dc6448dff5fe6e482c60919c00ab7d
Rich Burdon <rich.burdon@gmail.com>
2025-12-23 15:08:50 +0000

    fix: restore standard cash-dom import to fix tests in happy-dom environment

--- packages/ui/ui/src/dom.ts
+++ packages/ui/ui/src/dom.ts
@@ -2,29 +2,32 @@
 // Copyright 2024 DXOS.org
 //
 
-import $, { type Cash } from 'cash-dom';
+import $ from "cash-dom";
+import type { Cash } from "cash-dom";
 
-// From icon-plugin
-const ICONS_URL = '/icons.svg';
+export type { Cash };
 
-export const icon = (icon: string) => ICONS_URL + '#' + icon;
+// From icon-plugin.
+const ICONS_URL = "/icons.svg";
+
+export const icon = (icon: string) => ICONS_URL + "#" + icon;
 
 /**
  * Creates an SVG element wrapped in cash-dom.
  * @param tag SVG element tag name (e.g., 'svg', 'use', 'path')
  */
 const svg = (tag: string): Cash => {
-  return $(document.createElementNS('http://www.w3.org/2000/svg', tag));
+	return $(document.createElementNS("http://www.w3.org/2000/svg", tag));
 };
 
 // Augment cash-dom module.
-declare module 'cash-dom' {
-  interface CashStatic {
-    svg(tag: string): Cash;
-  }
+declare module "cash-dom" {
+	interface CashStatic {
+		svg(tag: string): Cash;
+	}
 }
 
-// Extend cash-dom.
+// Extend cash-dom with svg helper.
 $.svg = svg;
 
 export { $ };
