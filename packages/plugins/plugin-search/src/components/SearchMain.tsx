d7008b707e7815c2c7433e8f96fb0b2471b3a993
Josiah Witt <josiah@witt.life>
2026-01-11 16:53:35 +0700

    refactor: search main

--- packages/plugins/plugin-search/src/components/SearchMain.tsx
+++ packages/plugins/plugin-search/src/components/SearchMain.tsx
@@ -2,19 +2,18 @@
 // Copyright 2023 DXOS.org
 //
 
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
+import { Surface } from '@dxos/app-framework/react';
+import { Obj } from '@dxos/echo';
 import { useClient } from '@dxos/react-client';
 import { Filter, type Space, useQuery } from '@dxos/react-client/echo';
 import { useTranslation } from '@dxos/react-ui';
+import { SearchList } from '@dxos/react-ui-searchlist';
 import { StackItem } from '@dxos/react-ui-stack';
-import { mx } from '@dxos/ui-theme';
 
 import { useGlobalSearch, useGlobalSearchResults, useWebSearch } from '../hooks';
 import { meta } from '../meta';
-
-import { Searchbar } from './Searchbar';
-import { SearchResults } from './SearchResults';
 
 export const SearchMain = ({ space }: { space?: Space }) => {
   const { t } = useTranslation(meta.id);
@@ -26,34 +25,36 @@ export const SearchMain = ({ space }: { space?: Space }) => {
   const objects = useQuery(allSpaces ? client.spaces : space?.db, Filter.everything());
   const results = useGlobalSearchResults(objects);
 
-  const { runSearch, results: webResults } = useWebSearch({ query });
-
-  // TODO(burdon): Activate and/or filter current main (set context).
-  const [selected, setSelected] = useState<string>();
-  const handleSelect = (id: string) => {
-    setSelected((selected) => (selected === id ? undefined : id));
-  };
+  const { results: webResults } = useWebSearch({ query });
 
   const allResults = [...results, ...webResults];
 
-  return (
-    <StackItem.Content toolbar>
-      <Searchbar
-        classNames='pli-2'
-        placeholder={t('search placeholder')}
-        delay={300}
-        onChange={(text) => {
-          setQuery(text);
-          setMatch?.(text);
-        }}
-        onSubmit={runSearch}
-      />
+  const handleSearch = useCallback(
+    (text: string) => {
+      setQuery(text);
+      setMatch?.(text);
+    },
+    [setMatch],
+  );
 
-      {allResults.length > 0 && (
-        <div className={mx('flex flex-col bs-full overflow-hidden')}>
-          <SearchResults items={allResults} selected={selected} onSelect={handleSelect} />
-        </div>
-      )}
+  return (
+    <StackItem.Content>
+      <SearchList.Root onSearch={handleSearch} classNames='flex flex-col bs-full overflow-hidden'>
+        <SearchList.Input placeholder={t('search placeholder')} classNames='pli-2' />
+        <SearchList.Content classNames='overflow-y-auto'>
+          <SearchList.Viewport>
+            {/* TODO(burdon): Support selection on search results. */}
+            {allResults
+              .filter((item) => Obj.getLabel(item.object))
+              .map((item) => (
+                <div key={item.id} role='none' className='pli-2 first:pbs-2 pbe-2'>
+                  <Surface role='card' data={{ subject: item.object }} limit={1} />
+                </div>
+              ))}
+            {allResults.length === 0 && <SearchList.Empty>{t('empty results message')}</SearchList.Empty>}
+          </SearchList.Viewport>
+        </SearchList.Content>
+      </SearchList.Root>
     </StackItem.Content>
   );
 };
--- packages/plugins/plugin-search/src/components/index.ts
+++ packages/plugins/plugin-search/src/components/index.ts
@@ -2,7 +2,6 @@
 // Copyright 2023 DXOS.org
 //
 
-export * from './Searchbar';
 export * from './SearchMain';
 export * from './SearchDialog';
 export * from './SpaceMain';
