fb7c7b27c613141468e90317d5f0878aa8597f96
Josiah Witt <josiah@witt.life>
2026-01-11 05:25:02 -0500

    refactor: search main (#10394)

--- packages/ui/react-ui-searchlist/src/components/SearchList/hooks/index.ts
+++ packages/ui/react-ui-searchlist/src/components/SearchList/hooks/index.ts
@@ -2,6 +2,7 @@
 // Copyright 2025 DXOS.org
 //
 
+export * from './useGlobalFilter';
 export * from './useSearchListInput';
 export * from './useSearchListItem';
 export * from './useSearchListResults';
