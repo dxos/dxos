46a2b5a8a196cc5f4594c505a56f5aadf57c2ecf
Mykola Veremchuk <veremchukmukola@gmail.com>
2026-01-12 22:10:13 +0700

    refactor: clean up dependencies and improve SpaceMain component logic

--- packages/plugins/plugin-simple-layout/src/components/SimpleLayout/SimpleLayout.stories.tsx
+++ packages/plugins/plugin-simple-layout/src/components/SimpleLayout/SimpleLayout.stories.tsx
@@ -3,21 +3,17 @@
 //
 
 import { type Meta, type StoryObj } from '@storybook/react-vite';
-import * as Effect from 'effect/Effect';
 
 import { Capability, Common, Plugin } from '@dxos/app-framework';
 import { withPluginManager } from '@dxos/app-framework/testing';
-import { ClientOperation, ClientPlugin } from '@dxos/plugin-client';
-import { PreviewPlugin } from '@dxos/plugin-preview';
+import { ClientPlugin } from '@dxos/plugin-client';
 import { SearchPlugin } from '@dxos/plugin-search';
 import { SpacePlugin } from '@dxos/plugin-space';
-import { SpaceOperation } from '@dxos/plugin-space/types';
 import { corePlugins } from '@dxos/plugin-testing';
 import { withTheme } from '@dxos/react-ui/testing';
 import { translations as searchTranslation } from '@dxos/react-ui-searchlist';
-import { Collection } from '@dxos/schema';
 
-import { OperationResolver, State } from '../../capabilities';
+import { OperationResolver, type SimpleLayoutStateOptions, State } from '../../capabilities';
 import { meta as pluginMeta } from '../../meta';
 import { type SimpleLayoutPluginOptions } from '../../SimpleLayoutPlugin';
 import { translations } from '../../translations';
@@ -29,39 +25,9 @@ const TestPlugin = Plugin.define<SimpleLayoutPluginOptions>(pluginMeta).pipe(
     id: Capability.getModuleTag(State),
     activatesOn: Common.ActivationEvent.Startup,
     activatesAfter: [Common.ActivationEvent.LayoutReady],
-    activate: () => State({ initialState: { isPopover } }),
+    activate: () => State({ initialState: { isPopover } } satisfies SimpleLayoutStateOptions),
   })),
   Common.Plugin.addOperationResolverModule({ activate: OperationResolver }),
-  Plugin.addModule({
-    id: 'setup',
-    activatesOn: Common.ActivationEvent.OperationInvokerReady,
-    activate: Effect.fnUntraced(function* (context) {
-      const { invoke } = context.getCapability(Common.Capability.OperationInvoker);
-      yield* invoke(ClientOperation.CreateIdentity, {});
-      const { space: work } = yield* invoke(SpaceOperation.Create, { name: 'Work Space' });
-      const { space: sharedProject } = yield* invoke(SpaceOperation.Create, { name: 'Shared Project' });
-
-      // Add collections to Work Space.
-      yield* invoke(SpaceOperation.AddObject, {
-        target: work.db,
-        object: Collection.make({ name: 'Projects', objects: [] }),
-      });
-      yield* invoke(SpaceOperation.AddObject, {
-        target: work.db,
-        object: Collection.make({ name: 'Documents', objects: [] }),
-      });
-
-      // Add collections to Shared Project.
-      yield* invoke(SpaceOperation.AddObject, {
-        target: sharedProject.db,
-        object: Collection.make({ name: 'Tasks', objects: [] }),
-      });
-      yield* invoke(SpaceOperation.AddObject, {
-        target: sharedProject.db,
-        object: Collection.make({ name: 'Notes', objects: [] }),
-      });
-    }),
-  }),
   Plugin.make,
 );
 
@@ -69,8 +35,13 @@ const createPluginManager = ({ isPopover }: { isPopover: boolean }) => {
   return withPluginManager({
     plugins: [
       ...corePlugins(),
-      ClientPlugin({}),
-      PreviewPlugin(),
+      ClientPlugin({
+        onClientInitialized: async ({ client }) => {
+          await client.halo.createIdentity();
+          await client.spaces.create({ name: 'Work Space' });
+          await client.spaces.create({ name: 'Shared Project' });
+        },
+      }),
       SpacePlugin({}),
       SearchPlugin(),
       TestPlugin({ isPopover }),
