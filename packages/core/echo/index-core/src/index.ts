a721efa93923b0b971d348dda2db713afb2e4804
Dmytro Maretskyi <maretskii@gmail.com>
2026-01-12 18:45:47 +0700

    refactor(index-core): remove unnecessary type export for FtsResult build(index-core): add project references to common packages and echo packages build(plugin-table): add log package reference to tsconfig

--- packages/core/echo/index-core/src/index.ts
+++ packages/core/echo/index-core/src/index.ts
@@ -5,6 +5,6 @@
 export { IndexEngine, type IndexDataSource, type DataSourceCursor, type IndexEngineParams } from './index-engine';
 export { IndexTracker, type IndexCursor } from './index-tracker';
 export { type IndexerObject, type Index } from './indexes/interface';
-export { FtsIndex, type FtsResult } from './indexes/fts-index';
+export { FtsIndex } from './indexes/fts-index';
 export { ObjectMetaIndex, type ObjectMeta } from './indexes/object-meta-index';
 export { ReverseRefIndex, type ReverseRef } from './indexes/reverse-ref-index';
