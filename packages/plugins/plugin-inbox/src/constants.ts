7f25b460cd7bef12ab060803730c01dd4ddf2c00
Josiah Witt <josiah@witt.life>
2026-01-13 06:01:57 -0500

    feat: send email (#10408)

---
+++ packages/plugins/plugin-inbox/src/constants.ts
@@ -0,0 +1,8 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { meta } from './meta';
+
+export const COMPOSE_EMAIL_DIALOG = `${meta.id}/ComposeEmailDialog`;
+export const POPOVER_SAVE_FILTER = `${meta.id}/PopoverSaveFilter`;
