c6e2769bd64025d92a49dbf6c2575d2fbcc742ff
Dmytro Maretskyi <maretskii@gmail.com>
2026-01-19 12:00:20 +0700

    fix(functions): update QueueServiceImpl signatures to match interface

--- packages/core/protocols/src/queue.ts
+++ packages/core/protocols/src/queue.ts
@@ -6,6 +6,9 @@ export {
   type QueueService,
   type QueueQuery,
   type QueueQueryResult as QueryResult,
+  type QueryQueueRequest,
+  type InsertIntoQueueRequest,
+  type DeleteFromQueueRequest,
 } from './proto/gen/dxos/client/services.js';
 
 export const KEY_QUEUE_POSITION = 'dxos.org/key/queue-position';
