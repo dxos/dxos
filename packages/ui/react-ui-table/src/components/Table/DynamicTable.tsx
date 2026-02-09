7de5ac23816e1e8d0e3c8a0a5b7a2e89b6f06d75
Josiah Witt <josiah@witt.life>
2026-02-09 14:25:12 -0500

    fix: update schema handling in projection callbacks (#10545)

--- packages/ui/react-ui-table/src/components/Table/DynamicTable.tsx
+++ packages/ui/react-ui-table/src/components/Table/DynamicTable.tsx
@@ -3,10 +3,11 @@
 //
 
 import { RegistryContext } from '@effect-atom/atom-react';
+import type * as Types from 'effect/Types';
 import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
 
 import { type Type } from '@dxos/echo';
-import { type JsonSchemaType } from '@dxos/echo/internal';
+import { type JsonSchema } from '@dxos/echo';
 import { type ThemedClassName, useDefaultValue } from '@dxos/react-ui';
 import { type ProjectionModel } from '@dxos/schema';
 import { mx } from '@dxos/ui-theme';
@@ -23,7 +24,7 @@ export type DynamicTableProps<T extends Type.Entity.Any = Type.Entity.Any> = The
   name?: string; // TODO(burdon): Remove?
   rows: any[];
   properties?: TablePropertyDefinition[];
-  jsonSchema?: JsonSchemaType;
+  jsonSchema?: Types.DeepMutable<JsonSchema.JsonSchema>;
   features?: Partial<TableFeatures>;
   rowActions?: TableRowAction[];
   onRowClick?: (row: any) => void;
--- packages/ui/react-ui-table/src/model/table-model.test.ts
+++ packages/ui/react-ui-table/src/model/table-model.test.ts
@@ -137,7 +137,7 @@ const createTableModel = (registry: Registry.Registry, props: Partial<TableModel
     registry,
     view,
     baseSchema: schema.jsonSchema,
-    change: createDirectChangeCallback(view.projection, schema.jsonSchema),
+    change: createDirectChangeCallback(view.projection, Type.toJsonSchema(schema)),
   });
   projection.normalizeView();
   return new TableModel({
