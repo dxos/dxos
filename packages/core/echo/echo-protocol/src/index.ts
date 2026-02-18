839fefa41ef8018d416d35f4ce54dc226365a641
Mykola Veremchuk <veremchukmukola@gmail.com>
2026-02-18 18:02:34 +0100

    fix: Restore missing exports in echo-protocol index file

--- packages/core/echo/echo-protocol/src/index.ts
+++ packages/core/echo/echo-protocol/src/index.ts
@@ -2,11 +2,11 @@
 // Copyright 2023 DXOS.org
 //
 
-export * from './document-structure';
-export * from './reference';
-export * from './space-doc-version';
 export type * from './collection-sync';
-export * from './space-id';
+export * from './document-structure';
 export * from './echo-feed-codec';
 export * from './foreign-key';
 export * from './query';
+export * from './reference';
+export * from './space-doc-version';
+export * from './space-id';
