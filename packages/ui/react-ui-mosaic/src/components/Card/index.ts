13bb09a5f5321b342e47de44ca0e5197809da749
Rich Burdon <rich.burdon@gmail.com>
2026-03-05 10:27:25 -0500

    refactor(react-ui,react-ui-mosaic): delete Card/Image from mosaic, remove CardDragPreview
    - Delete react-ui-mosaic/src/components/Card/ and Image/ directories (moved to react-ui)
    - Remove Card and Image exports from react-ui-mosaic components/index.ts barrel
    - Update Board/Column.tsx to import Card from @dxos/react-ui
    - Update DefaultStackTile.tsx and Card.stories.tsx to import from @dxos/react-ui
    - Delete CardDragPreview.tsx and styles.ts from react-ui (no consumers)
    - Clean up react-ui/Card/index.ts to only export Card

--- packages/ui/react-ui-mosaic/src/components/Card/index.ts
+++
@@ -1,7 +0,0 @@
-//
-// Copyright 2023 DXOS.org
-//
-
-// Card has moved to @dxos/react-ui. Re-exported here for backward compatibility.
-export { Card, CardDragPreview, cardStyles } from '@dxos/react-ui';
-export type { CardContextValue, CardRootProps, CardToolbarProps, CardMenuProps } from '@dxos/react-ui';
--- packages/ui/react-ui/src/components/Card/index.ts
+++ packages/ui/react-ui/src/components/Card/index.ts
@@ -2,7 +2,4 @@
 // Copyright 2023 DXOS.org
 //
 
-export { styles as cardStyles } from './styles';
-
 export * from './Card';
-export * from './CardDragPreview';
