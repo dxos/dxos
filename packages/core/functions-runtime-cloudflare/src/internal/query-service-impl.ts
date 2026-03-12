47b1fb070b12b0ef14dd2216daff4b8ecde96e6a
Mykola Veremchuk <veremchukmukola@gmail.com>
2026-03-12 11:46:35 +0100

    refactor: Consolidate import statements and streamline execQuery call in QueryServiceImpl

--- packages/core/functions-runtime-cloudflare/src/internal/query-service-impl.ts
+++ packages/core/functions-runtime-cloudflare/src/internal/query-service-impl.ts
@@ -2,16 +2,11 @@
 // Copyright 2024 DXOS.org
 //
 
-
 import { Stream } from '@dxos/codec-protobuf/stream';
 import { NotImplementedError, RuntimeServiceError } from '@dxos/errors';
 import { log } from '@dxos/log';
 import { type EdgeFunctionEnv } from '@dxos/protocols';
-import {
-  type QueryRequest,
-  type QueryResponse,
-  type QueryService,
-} from '@dxos/protocols/proto/dxos/echo/query';
+import { type QueryRequest, type QueryResponse, type QueryService } from '@dxos/protocols/proto/dxos/echo/query';
 
 export class QueryServiceImpl implements QueryService {
   private _queryCount = 0;
@@ -29,10 +24,7 @@ export class QueryServiceImpl implements QueryService {
         try {
           this._queryCount++;
           log.info('begin query', { request });
-          using queryResponse = await this._dataService.execQuery(
-            this._executionContext,
-            request
-          );
+          using queryResponse = await this._dataService.execQuery(this._executionContext, request);
           log.info('query response', { resultCount: queryResponse.results?.length });
           return structuredClone(queryResponse);
         } catch (error) {
