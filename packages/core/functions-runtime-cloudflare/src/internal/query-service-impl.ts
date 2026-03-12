cf50b9784caf69380634aefe3b9a3907994bfdf3
Mykola Veremchuk <veremchukmukola@gmail.com>
2026-03-12 11:41:10 +0100

    refactor: Simplify logging in execQuery by removing spaceId and adjusting error context

--- packages/core/functions-runtime-cloudflare/src/internal/query-service-impl.ts
+++ packages/core/functions-runtime-cloudflare/src/internal/query-service-impl.ts
@@ -30,27 +30,23 @@ export class QueryServiceImpl implements QueryServiceProto {
 
   execQuery(request: QueryRequest): Stream<QueryResponse> {
     log.info('execQuery', { request });
-    const query = QueryAST.Query.pipe(Schema.decodeUnknownSync)(JSON.parse(request.query));
-    const requestedSpaceIds = getTargetSpacesForQuery(query);
-    invariant(requestedSpaceIds.length === 1, 'Only one space is supported');
-    const spaceId = requestedSpaceIds[0];
 
     return Stream.fromPromise<QueryResponse>(
       (async () => {
         try {
           this._queryCount++;
-          log.info('begin query', { spaceId });
+          log.info('begin query', { request });
           using queryResponse = await this._dataService.execQuery(
             this._executionContext,
             request
           );
-          log.info('query response', { spaceId, resultCount: queryResponse.results?.length });
+          log.info('query response', { resultCount: queryResponse.results?.length });
           return structuredClone(queryResponse);
         } catch (error) {
           log.error('query failed', { err: error });
           throw new RuntimeServiceError({
             message: `Query execution failed (queryCount=${this._queryCount})`,
-            context: { spaceId, filter: request.filter, queryCount: this._queryCount },
+            context: { queryCount: this._queryCount },
             cause: error,
           });
         }
