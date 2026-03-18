2433579d2595f2dbd2e2682d996cacb165931e2a
Josiah Witt <josiah@witt.life>
2026-03-18 00:57:06 -0400

    fix: card improvements (#10760)

--- packages/plugins/plugin-assistant/src/containers/Project/ProjectArticle.tsx
+++ packages/plugins/plugin-assistant/src/containers/Project/ProjectArticle.tsx
@@ -3,13 +3,14 @@
 //
 
 import { Atom, useAtomValue } from '@effect-atom/atom-react';
+import * as Function from 'effect/Function';
 import * as Option from 'effect/Option';
 import React, { forwardRef, useMemo, useState } from 'react';
 
 import { Surface } from '@dxos/app-framework/ui';
 import { type SurfaceComponentProps, useObjectMenuItems } from '@dxos/app-toolkit/ui';
 import { type Project } from '@dxos/assistant-toolkit';
-import { Filter, Obj, Query } from '@dxos/echo';
+import { Annotation, Filter, Obj, Query } from '@dxos/echo';
 import { AtomObj, AtomRef } from '@dxos/echo-atom';
 import { useQuery } from '@dxos/react-client/echo';
 import { Card, Panel, ScrollArea, Toolbar } from '@dxos/react-ui';
@@ -81,6 +82,13 @@ export const ProjectArticle = ({ subject: project }: ProjectArticleProps) => {
 const StackTile = forwardRef<HTMLDivElement, MosaicTileProps<Obj.Unknown>>(
   ({ data, location, debug }, forwardedRef) => {
     const objectMenuItems = useObjectMenuItems(data);
+    const icon = Function.pipe(
+      Obj.getSchema(data),
+      Option.fromNullable,
+      Option.flatMap(Annotation.IconAnnotation.get),
+      Option.map(({ icon }) => icon),
+      Option.getOrElse(() => 'ph--placeholder--regular'),
+    );
 
     return (
       <Mosaic.Tile asChild id={data.id} data={data} location={location} debug={debug}>
@@ -88,7 +96,7 @@ const StackTile = forwardRef<HTMLDivElement, MosaicTileProps<Obj.Unknown>>(
           <Menu.Root>
             <Card.Root ref={forwardedRef} data-testid='board-item'>
               <Card.Toolbar>
-                <Card.IconBlock></Card.IconBlock>
+                <Card.Icon icon={icon} />
                 <Card.Title>{Obj.getLabel(data)}</Card.Title>
                 {/* TODO(wittjosiah): Reconcile with Card.Menu. */}
                 <Menu.Trigger asChild disabled={!objectMenuItems?.length}>
