75345c90ecba94e0db07583135494e7ed4f731e2
Mykola Veremchuk <veremchukmukola@gmail.com>
2026-03-21 16:29:30 -0700

    refactor(plugin-daily-summary): restrict schema registration to owned schemas
    - Updated the Daily Summary plugin to ensure that the `addSchemaModule` method only registers schemas owned by the plugin, preventing unnecessary coupling with other domains.
    - Removed the registration of schemas from external plugins, streamlining the plugin's architecture and enhancing maintainability.

--- packages/plugins/plugin-daily-summary/src/DailySummaryPlugin.tsx
+++ packages/plugins/plugin-daily-summary/src/DailySummaryPlugin.tsx
@@ -4,8 +4,6 @@
 
 import { Plugin } from '@dxos/app-framework';
 import { AppPlugin } from '@dxos/app-toolkit';
-import { Trigger } from '@dxos/functions';
-import { Operation } from '@dxos/operation';
 
 import { AppGraphBuilder, BlueprintDefinition, ReactSurface } from './capabilities';
 import { meta } from './meta';
@@ -14,7 +12,6 @@ import { translations } from './translations';
 export const DailySummaryPlugin = Plugin.define(meta).pipe(
   AppPlugin.addAppGraphModule({ activate: AppGraphBuilder }),
   AppPlugin.addBlueprintDefinitionModule({ activate: BlueprintDefinition }),
-  AppPlugin.addSchemaModule({ schema: [Operation.PersistentOperation, Trigger.Trigger] }),
   AppPlugin.addSurfaceModule({ activate: ReactSurface }),
   AppPlugin.addTranslationsModule({ translations }),
   Plugin.make,
