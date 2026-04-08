d62cc2f8b08bedf9c8eb518ced6fcc3492ed8f7d
Rich Burdon <rich.burdon@gmail.com>
2026-04-07 21:27:43 -0400

    feat(plugin-deus): add meta and translations

---
+++ packages/plugins/plugin-deus/src/meta.ts
@@ -0,0 +1,17 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import { type Plugin } from '@dxos/app-framework';
+import { trim } from '@dxos/util';
+
+export const meta: Plugin.Meta = {
+  id: 'org.dxos.plugin.deus',
+  name: 'Deus',
+  description: trim`
+    Deus plugin for structured specs with rich text content.
+  `,
+  icon: 'ph--file-text--regular',
+  iconHue: 'violet',
+  source: 'https://github.com/dxos/dxos/tree/main/packages/plugins/plugin-deus',
+};
