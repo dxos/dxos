ef3938a8a55134d2e666e248916a93f168150805
Rich Burdon <rich.burdon@gmail.com>
2026-04-07 21:28:44 -0400

    feat(plugin-deus): add ReactSurface capability

---
+++ packages/plugins/plugin-deus/src/capabilities/react-surface.tsx
@@ -0,0 +1,30 @@
+//
+// Copyright 2025 DXOS.org
+//
+
+import * as Effect from 'effect/Effect';
+import React from 'react';
+
+import { Capabilities, Capability } from '@dxos/app-framework';
+import { Surface } from '@dxos/app-framework/ui';
+
+import { SpecArticle } from '#containers';
+import { meta } from '#meta';
+import { isSpec, type Spec } from '#types';
+
+export default Capability.makeModule(() =>
+  Effect.succeed(
+    Capability.contributes(
+      Capabilities.ReactSurface,
+      Surface.create({
+        id: `${meta.id}.spec`,
+        role: ['article', 'section', 'slide'],
+        filter: (data): data is { subject: Spec; attendableId: string } =>
+          typeof data.attendableId === 'string' && isSpec(data.subject),
+        component: ({ data: { subject, attendableId }, role }) => (
+          <SpecArticle role={role} subject={subject} attendableId={attendableId} />
+        ),
+      }),
+    ),
+  ),
+);
