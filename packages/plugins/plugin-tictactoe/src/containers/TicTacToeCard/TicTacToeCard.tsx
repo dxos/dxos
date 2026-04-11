2362935225457f5efd2f2a0152f71528eb20021a
Rich Burdon <rich.burdon@gmail.com>
2026-04-11 00:01:48 -0400

    feat(plugin-tictactoe): implement TicTacToeCard container

--- packages/plugins/plugin-tictactoe/src/containers/TicTacToeCard/TicTacToeCard.tsx
+++ packages/plugins/plugin-tictactoe/src/containers/TicTacToeCard/TicTacToeCard.tsx
@@ -6,14 +6,17 @@ import React from 'react';
 
 import { type AppSurface } from '@dxos/app-toolkit/ui';
 
+import { TicTacToeBoard, getWinningCells } from '#components';
 import { type TicTacToe } from '#types';
 
 export type TicTacToeCardProps = AppSurface.ObjectCardProps<TicTacToe.Game>;
 
 export const TicTacToeCard = ({ subject: game }: TicTacToeCardProps) => {
+  const winningCells = getWinningCells(game.board, game.size, game.winCondition);
+
   return (
     <div className='flex items-center justify-center p-2'>
-      <p>{game.name ?? 'Tic-Tac-Toe'}</p>
+      <TicTacToeBoard board={game.board} size={game.size} winningCells={winningCells} disabled />
     </div>
   );
 };
