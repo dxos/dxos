23176cd506f2bda2abec0c0d802034801ba7e5f2
Rich Burdon <rich.burdon@gmail.com>
2026-05-31 02:38:37 -0400

    feat(plugin-duffel): settings + BookingService capabilities

---
+++ packages/plugins/plugin-duffel/src/capabilities/duffel.ts
@@ -0,0 +1,37 @@
+//
+// Copyright 2026 DXOS.org
+//
+
+import * as Effect from 'effect/Effect';
+
+import { Capabilities, Capability } from '@dxos/app-framework';
+import { AppCapabilities } from '@dxos/app-toolkit';
+import { createKvsStore } from '@dxos/effect';
+import { TripCapabilities } from '@dxos/plugin-trip';
+
+import { meta } from '#meta';
+import { makeDuffelBookingService } from '#services';
+import { DuffelCapabilities, Settings } from '#types';
+
+export default Capability.makeModule(
+  Effect.fnUntraced(function* () {
+    const registry = yield* Capability.get(Capabilities.AtomRegistry);
+    const settingsAtom = createKvsStore({
+      key: meta.id,
+      schema: Settings.Settings,
+      defaultValue: () => ({ apiKey: undefined }),
+    });
+
+    const service = makeDuffelBookingService(() => registry.get(settingsAtom).apiKey);
+
+    return [
+      Capability.contributes(DuffelCapabilities.Settings, settingsAtom),
+      Capability.contributes(AppCapabilities.Settings, {
+        prefix: meta.id,
+        schema: Settings.Settings,
+        atom: settingsAtom,
+      }),
+      Capability.contributes(TripCapabilities.BookingService, service),
+    ];
+  }),
+);
