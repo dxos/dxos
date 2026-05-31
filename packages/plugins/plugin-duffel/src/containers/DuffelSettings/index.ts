35b746fdd2bf15a62e01774b74e43a8567df3351
Rich Burdon <rich.burdon@gmail.com>
2026-05-31 02:36:26 -0400

    feat(plugin-duffel): settings panel container

---
+++ packages/plugins/plugin-duffel/src/containers/DuffelSettings/index.ts
@@ -0,0 +1,5 @@
+//
+// Copyright 2026 DXOS.org
+//
+
+export { DuffelSettings as default } from './DuffelSettings';
---
+++ packages/plugins/plugin-duffel/src/containers/index.ts
@@ -0,0 +1,7 @@
+//
+// Copyright 2026 DXOS.org
+//
+
+import { type ComponentType, lazy } from 'react';
+
+export const DuffelSettings: ComponentType<any> = lazy(() => import('./DuffelSettings'));
