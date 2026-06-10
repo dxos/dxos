f45976b41b7791444eb76877d843f6c5ac7d1102
Rich Burdon <rich.burdon@gmail.com>
2026-06-10 15:11:46 -0400

    fix: SB warnings (#11751)

--- packages/sdk/shell/src/composites/StatusDialog/StatusDialog.tsx
+++ packages/sdk/shell/src/composites/StatusDialog/StatusDialog.tsx
@@ -4,11 +4,13 @@
 
 import React from 'react';
 
-import { AlertDialog, useId } from '@dxos/react-ui';
+import { AlertDialog, useId, useTranslation } from '@dxos/react-ui';
 
 import { StatusPanel } from '../../panels';
+import { translationKey } from '../../translations';
 
 export const StatusDialog = () => {
+  const { t } = useTranslation(translationKey);
   const titleId = useId('statusDialog__title');
   return (
     <AlertDialog.Root open>
@@ -16,6 +18,7 @@ export const StatusDialog = () => {
         <AlertDialog.Overlay>
           <AlertDialog.Content aria-labelledby={titleId}>
             <AlertDialog.Body>
+              <AlertDialog.Description srOnly>{t('resetting.message')}</AlertDialog.Description>
               <StatusPanel titleId={titleId} />
             </AlertDialog.Body>
           </AlertDialog.Content>
