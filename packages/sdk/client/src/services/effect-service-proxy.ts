17bf2ac41f2d9481e2f43eb170fb72bec131231d
Dmytro Maretskyi <maretskii@gmail.com>
2026-07-08 17:48:39 +0200

    refactor

--- packages/sdk/client/src/services/effect-service-proxy.ts
+++ packages/sdk/client/src/services/effect-service-proxy.ts
@@ -2,13 +2,10 @@
 // Copyright 2026 DXOS.org
 //
 
-import { Event } from '@dxos/async';
-import { type ClientServices, type ClientServicesProvider, clientServiceBundle } from '@dxos/client-protocol';
-import { Stream as DxosStream } from '@dxos/codec-protobuf';
-import { DataService, QueryService } from '@dxos/protocols';
 import * as BrowserWorker from '@effect/platform-browser/BrowserWorker';
 import * as RpcClient from '@effect/rpc/RpcClient';
 import * as RpcGroup from '@effect/rpc/RpcGroup';
+import { Sink } from 'effect';
 import * as Cause from 'effect/Cause';
 import * as Effect from 'effect/Effect';
 import * as Exit from 'effect/Exit';
@@ -16,7 +13,12 @@ import * as Fiber from 'effect/Fiber';
 import * as Layer from 'effect/Layer';
 import * as Runtime from 'effect/Runtime';
 import * as Scope from 'effect/Scope';
-import * as EffectStream from 'effect/Stream';
+import * as Stream from 'effect/Stream';
+
+import { Event } from '@dxos/async';
+import { type ClientServices, type ClientServicesProvider, clientServiceBundle } from '@dxos/client-protocol';
+import * as PbStream from '@dxos/codec-protobuf/stream';
+import { DataService, QueryService } from '@dxos/protocols';
 
 const AppRpcs = RpcGroup.make().merge(DataService.Rpcs).merge(QueryService.Rpcs);
 
@@ -39,25 +41,31 @@ export class EffectClientServicesProxy implements ClientServicesProvider {
     return this._services;
   }
 
-  toPromise<T, R>(method: (req: T) => Effect.Effect<R, any, never>): (req: T) => Promise<R> {
+  #toPromise<T, R>(method: (req: T) => Effect.Effect<R, any, never>): (req: T) => Promise<R> {
     return (req: T) => Runtime.runPromise(this._runtime)(method(req));
   }
 
-  toStream<T, R>(method: (req: T) => EffectStream.Stream<R, any, never>): (req: T) => DxosStream<R> {
-    return (req: T) => new DxosStream(({ ready, next, close }) => {
-      ready();
-      const fiber = Runtime.runFork(this._runtime)(
-        EffectStream.runForEach(method(req), (item) => Effect.sync(() => next(item))).pipe(
-          Effect.match({
-            onFailure: (cause) => close(new Error(Cause.pretty(cause))),
-            onSuccess: () => close(),
-          }),
-        ),
-      );
-      return () => {
-        Runtime.runPromise(this._runtime)(Fiber.interrupt(fiber)).catch(() => {});
-      };
-    });
+  #toStream<T, R>(method: (req: T) => Stream.Stream<R, any, never>): (req: T) => PbStream.Stream<R> {
+    return (req: T) =>
+      new PbStream.Stream(({ ready, next, close }) => {
+        const fiber = method(req).pipe(
+          Stream.onStart(Effect.sync(ready)),
+          Stream.onDone(() => Effect.sync(() => close())),
+          Stream.onError((error) => Effect.sync(() => close(Cause.prettyErrors(error)[0]))),
+          Stream.runForEach((item) =>
+            Effect.sync(() => next(item)).pipe(
+              Effect.match({
+                onFailure: (cause) => close(new Error(Cause.pretty(cause))),
+                onSuccess: () => close(),
+              }),
+            ),
+          ),
+          Runtime.runFork(this._runtime),
+        );
+        return () => {
+          Runtime.runPromise(this._runtime)(Fiber.interrupt(fiber)).catch(() => {});
+        };
+      });
   }
 
   async open(): Promise<void> {
@@ -72,29 +80,26 @@ export class EffectClientServicesProxy implements ClientServicesProvider {
     );
 
     this._client = await Runtime.runPromise(this._runtime)(
-      RpcClient.make(AppRpcs, { disableTracing: true }).pipe(
-        Effect.provide(clientLayer),
-        Scope.extend(this._scope!),
-      ),
+      RpcClient.make(AppRpcs, { disableTracing: true }).pipe(Effect.provide(clientLayer), Scope.extend(this._scope!)),
     );
 
     this._services = {
       DataService: {
-        subscribe: this.toStream(this._client.DataService.subscribe),
-        updateSubscription: this.toPromise(this._client.DataService.updateSubscription as any),
-        createDocument: this.toPromise(this._client.DataService.createDocument as any),
-        update: this.toPromise(this._client.DataService.update as any),
-        flush: this.toPromise(this._client.DataService.flush as any),
-        getDocumentHeads: this.toPromise(this._client.DataService.getDocumentHeads as any),
-        waitUntilHeadsReplicated: this.toPromise(this._client.DataService.waitUntilHeadsReplicated as any),
-        reIndexHeads: this.toPromise(this._client.DataService.reIndexHeads as any),
-        updateIndexes: this.toPromise(this._client.DataService.updateIndexes as any),
-        subscribeSpaceSyncState: this.toStream(this._client.DataService.subscribeSpaceSyncState as any),
+        subscribe: this.#toStream(this._client.DataService.subscribe),
+        updateSubscription: this.#toPromise(this._client.DataService.updateSubscription),
+        createDocument: this.#toPromise(this._client.DataService.createDocument),
+        update: this.#toPromise(this._client.DataService.update as any),
+        flush: this.#toPromise(this._client.DataService.flush),
+        getDocumentHeads: this.#toPromise(this._client.DataService.getDocumentHeads),
+        waitUntilHeadsReplicated: this.#toPromise(this._client.DataService.waitUntilHeadsReplicated),
+        reIndexHeads: this.#toPromise(this._client.DataService.reIndexHeads),
+        updateIndexes: this.#toPromise(this._client.DataService.updateIndexes),
+        subscribeSpaceSyncState: this.#toStream(this._client.DataService.subscribeSpaceSyncState),
       },
       QueryService: {
-        setConfig: this.toPromise(this._client.QueryService.setConfig as any),
-        execQuery: this.toStream(this._client.QueryService.execQuery as any),
-        reindex: this.toPromise(this._client.QueryService.reindex as any),
+        setConfig: this.#toPromise(this._client.QueryService.setConfig),
+        execQuery: this.#toStream(this._client.QueryService.execQuery),
+        reindex: this.#toPromise(this._client.QueryService.reindex),
       },
     };
   }
