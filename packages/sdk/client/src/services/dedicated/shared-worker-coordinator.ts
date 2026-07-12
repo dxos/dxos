9ea00082e6a9ddbc6cd1cef90d3484ee93473efd
Dmytro Maretskyi <maretskii@gmail.com>
2026-07-12 17:23:14 +0200

    comment

--- packages/sdk/client/src/services/dedicated/shared-worker-coordinator.ts
+++ packages/sdk/client/src/services/dedicated/shared-worker-coordinator.ts
@@ -2,6 +2,7 @@
 // Copyright 2026 DXOS.org
 //
 
+import { log } from '@dxos/log';
 import { SharedWorkerCoordinator as BaseSharedWorkerCoordinator } from '@dxos/worker-framework/coordinator';
 
 const defaultCreateWorker = () =>
@@ -10,8 +11,12 @@ const defaultCreateWorker = () =>
     name: 'dxos-coordinator-worker',
   });
 
+// TODO(dmaretskyi): remove this.
 export class SharedWorkerCoordinator extends BaseSharedWorkerCoordinator {
   constructor(createWorker: () => SharedWorker = defaultCreateWorker) {
     super({ createWorker });
+    // #region DEBUG
+    log.info('[DEBUG] SharedWorkerCoordinator ctor', { usingDefaultEntrypoint: createWorker === defaultCreateWorker });
+    // #endregion DEBUG
   }
 }
