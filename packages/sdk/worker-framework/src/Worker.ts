14c57d09388f621b9f88fdae848d489d3c8cff3d
Dmytro <dmytro@braneframe.com>
2026-09-21 07:29:10 +0000

    worker-framework: drop a stray blank line from the merge

--- packages/sdk/worker-framework/src/Worker.ts
+++ packages/sdk/worker-framework/src/Worker.ts
@@ -48,7 +48,6 @@ export const displace = (storageLockKey: string, displaceChannel = displaceChann
   }
 };
 
-
 const sessionProtocols = (clientToWorker: MessagePort, workerToClient: MessagePort) =>
   Layer.merge(
     RpcServer.layerProtocolWorkerRunner.pipe(Layer.provide(BrowserWorkerRunner.layerMessagePort(clientToWorker))),
